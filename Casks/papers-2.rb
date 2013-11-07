03f2b2429d11cbaf805c5b3547abdf06dcdae63c
Joshua Guy <jglenes@gmail.com>
2013-11-07 10:56:37 -0500

    Removing checksum to comply with 'latest'

--- Casks/papers-2.rb
+++ Casks/papers-2.rb
@@ -2,6 +2,6 @@ class Papers2 < Cask
   url 'http://www.papersapp.com/papers2/download'
   homepage 'http://www.papersapp.com/mac/'
   version 'latest'
-  sha1 'e8c859aa9c40230637bb642cb155be948a1cc581'
+  no_checksum
   link 'Papers2.app'
 end
