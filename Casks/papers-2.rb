cbce90d5bb0fe487edf9af858d466d1e961906d8
Joshua Guy <jglenes@gmail.com>
2013-11-07 09:02:14 -0500

    Adding Papers 2 Cask

---
+++ Casks/papers-2.rb
@@ -0,0 +1,7 @@
+class Papers2 < Cask
+  url 'http://www.papersapp.com/papers2/download'
+  homepage 'http://www.papersapp.com/mac/'
+  version 'latest'
+  sha1 'e8c859aa9c40230637bb642cb155be948a1cc581'
+  link 'Papers2.app'
+end
