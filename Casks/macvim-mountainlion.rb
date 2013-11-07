f8eb074ef6c23ab0f67692d43ce3385c439fb433
Fernando Paredes <nano@fdp.io>
2013-11-06 19:31:54 -0800

    MacVim for Mountain Lion

---
+++ Casks/macvim-mountainlion.rb
@@ -0,0 +1,7 @@
+class MacvimMountainlion < Cask
+  url 'https://github.com/eee19/macvim/releases/download/snapshot-72/MacVim-snapshot-72-Mountain-Lion.tbz'
+  homepage 'http://code.google.com/p/macvim/'
+  version '7.4-72'
+  sha1 'bc3b899634d73908ddba5afd9b9a74778988aec3'
+  link 'MacVim-snapshot-72/MacVim.app'
+end
