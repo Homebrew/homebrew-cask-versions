7f849e84b254f4226eaea7116697d3f0185fc388
Scott Reeves <cottser@mailworks.org>
2013-11-09 19:31:49 -0500

    Update TimeMachineScheduler to beta 3

--- Casks/timemachinescheduler-beta.rb
+++ Casks/timemachinescheduler-beta.rb
@@ -1,8 +1,8 @@
 class TimemachineschedulerBeta < Cask
-  url 'http://www.klieme.com/Downloads/TimeMachineScheduler/TimeMachineScheduler_4.0b2Full.zip'
+  url 'http://www.klieme.com/Downloads/TimeMachineScheduler/TimeMachineScheduler_4.0b3Full.zip'
   homepage 'http://www.klieme.com/TimeMachineScheduler.html'
-  version '4.0b2(470)'
-  sha1 '81325c8158db9e87c9291ecdc28e554c30e903af'
-  nested_container 'TimeMachineScheduler_4.0b2.dmg'
+  version '4.0b3(483)'
+  sha1 '0fd37298479db41329e12a748c9f6ba82ae5b4de'
+  nested_container 'TimeMachineScheduler_4.0b3.dmg'
   link 'TimeMachineScheduler.app'
 end
