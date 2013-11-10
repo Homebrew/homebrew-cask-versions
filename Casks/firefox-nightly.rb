823702c1c720264d3debff697dfc1784c1b5af72
Scott Reeves <cottser@mailworks.org>
2013-11-10 13:06:30 -0500

    Fix no_checksum for Firefox Nightly

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -2,6 +2,6 @@ class FirefoxNightly < Cask
   url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-28.0a1.en-US.mac.dmg'
   homepage 'http://nightly.mozilla.org'
   version '28.0-alpha1'
-  sha1 'no_checksum'
+  no_checksum
   link 'FirefoxNightly.app'
 end
