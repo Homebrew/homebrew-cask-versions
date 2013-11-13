3122e0d5ebffe873e54b7dcdda66c088bb79e129
Chulki Lee <chulki@asperasoft.com>
2013-11-13 11:38:06 -0800

    rubymine-eap 132.1053

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -1,7 +1,7 @@
 class RubymineEap < Cask
-  url 'http://download.jetbrains.com/ruby/RubyMine-132.666.dmg'
+  url 'http://download.jetbrains.com/ruby/RubyMine-132.1053.dmg'
   homepage 'http://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
-  version '132.666'
-  sha1 '7cd523ce36fca925007327c4bfe41e73e775a5de'
+  version '132.1053'
+  sha1 '25eec125892916f59b473e0b522eca9e5a1ee62f'
   link 'RubyMine EAP.app'
 end
