db953a5a0f2dfa291371c01007f4094e328d18c2
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 14:40:44 +0800

    Cocktail Cask for OS X Snow Leopard

---
+++ Casks/cocktail-snowleopard.rb
@@ -0,0 +1,7 @@
+class CocktailSnowleopard < Cask
+  url 'http://usa.maintain.se/CocktailSLE.dmg'
+  homepage 'http://maintain.se/cocktail'
+  version '5.1'
+  sha1 '3ae07a713ae5e071358a21037e026ba519e34a52'
+  link 'Cocktail.app'
+end
