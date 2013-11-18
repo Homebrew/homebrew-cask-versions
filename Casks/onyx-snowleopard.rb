b9387ba16dc374bc7913018d6dd587166263a907
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 18:14:47 +0800

    Add OnyX cask for Snow Leopard

---
+++ Casks/onyx-snowleopard.rb
@@ -0,0 +1,7 @@
+class OnyxSnowleopard < Cask
+  url 'http://www.titanium.free.fr/download/106/OnyX.dmg'
+  homepage 'http://www.titanium.free.fr/downloadonyx.php'
+  version '2.4.0'
+  sha1 '5aea8cf00bd77eff95ea875daecad0183eb03871'
+  link 'OnyX.app'
+end
