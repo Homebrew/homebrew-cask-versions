7296a915bf8697aa2694f6d35aae7fc28154a7cc
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 14:37:27 +0800

    Cocktail Cask for OS X Lion

---
+++ Casks/cocktail-lion.rb
@@ -0,0 +1,7 @@
+class CocktailLion < Cask
+  url 'http://usa.maintain.se/CocktailLionEdition.dmg'
+  homepage 'http://maintain.se/cocktail'
+  version '5.6'
+  sha1 '15b29030f1635b4245f23b43251914ca1accdd10'
+  link 'Cocktail.app'
+end
