7484222fb211c3bace2d6fc021b3bee307af6720
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 17:21:19 +0800

    Create Onyx cask for Mountain Lion

---
+++ Casks/onyx-mountainlion.rb
@@ -0,0 +1,7 @@
+class OnyxMountainlion < Cask
+  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
+  homepage 'http://www.titanium.free.fr/downloadonyx.php'
+  version '2.7.4'
+  sha1 'dadd7f82fd972e73a7a8e360583b8d4d7963482c'
+  link 'OnyX.app'
+end
