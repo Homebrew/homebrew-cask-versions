24bccc978bcf4a3a440af7a15b25233d94b91660
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-18 17:47:39 +0800

    Add OnyX for Lion cask

---
+++ Casks/onyx-lion.rb
@@ -0,0 +1,7 @@
+class OnyxLion < Cask
+  url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
+  homepage 'http://www.titanium.free.fr/downloadonyx.php'
+  version '2.4.8'
+  sha1 '830c72d93494c4d77b1e216fda25d77b56660a0e'
+  link 'OnyX.app'
+end
