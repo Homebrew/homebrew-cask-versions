03122e02d1241fb4a440c1024c2e4e5b820af71c
Roland Walker <walker@pobox.com>
2013-11-19 10:37:04 -0500

    Add GNU Emacs nightly builds
    revision 24.3.50r115148 is included with this commit

---
+++ Casks/emacs-nightly.rb
@@ -0,0 +1,7 @@
+class EmacsNightly < Cask
+  url 'http://emacsformacosx.com/emacs-builds/Emacs-2013-11-19_01-33-37-115148-universal-10.6.8.dmg'
+  homepage 'http://emacsformacosx.com/'
+  version '24.3.50r115148'
+  sha1 'e22c2ed951507f1f05090eeaa95bd5a3a561bb47'
+  link 'Emacs.app'
+end
