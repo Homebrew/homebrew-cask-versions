a1557dd56e564e5b0ea3f65e12f90d6226a1bd82
Roland Walker <walker@pobox.com>
2013-11-19 10:21:45 -0500

    Add WebKit nightly builds
    version r159487 included in this commit

---
+++ Casks/webkit-nightly.rb
@@ -0,0 +1,7 @@
+class WebkitNightly < Cask
+  url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r159487.dmg'
+  homepage 'http://nightly.webkit.org/'
+  version 'r159487'
+  sha1 '6fe6da20920ddb73fd7003f1c73e0848a0159ee9'
+  link 'WebKit.app'
+end
