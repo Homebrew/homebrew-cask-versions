d6aed4eda70ed545de18379ffefd6dfadfc602cb
Joshua Guy <jglenes@gmail.com>
2013-11-21 10:02:48 -0500

    RStudio Preview v0.98.484

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,7 +1,7 @@
 class RstudioPreview < Cask
-  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.480.dmg'
+  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.484.dmg'
   homepage 'http://www.rstudio.com/ide/download/preview'
-  version '0.98.480'
-  sha1 'c0d4681b4328b4e19a004125c4d15e748a49bf4e'
+  version '0.98.484'
+  sha1 '5e3ab521dafec2accc22cf688aa1379f49beffd3'
   link 'RStudio.app'
 end
