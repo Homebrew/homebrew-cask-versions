35e4aa8af6c668fe434336608afc50306dbe8b69
Roland Walker <walker@pobox.com>
2013-11-21 09:56:43 -0500

    Add OpenRefine.app beta
    This commit includes OpenRefine.app version 2.6-beta.1 .  This is the
    successor to Google Refine.app.  Upon release of OpenRefine, `openrefine.rb`
    should replace `google-refine.rb` in the main repository.

---
+++ Casks/openrefine.rb
@@ -0,0 +1,7 @@
+class Openrefine < Cask
+  url 'https://github.com/OpenRefine/OpenRefine/releases/download/2.6-beta.1/openrefine-mac-2.6-beta.1.dmg'
+  homepage 'http://openrefine.org/'
+  version '2.6-beta.1'
+  sha1 'b042edf638307bbedcd1390e4cb253070668f64c'
+  link 'OpenRefine.app'
+end
