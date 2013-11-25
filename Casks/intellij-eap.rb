59686971935cb7d2bf435784d68b32c0e4a85e59
Lee Hanxue <hanxue.developer@gmail.com>
2013-11-25 21:18:00 +0800

    Add IntelliJ 13 Early Access Program

---
+++ Casks/intellij-eap.rb
@@ -0,0 +1,7 @@
+class IntellijEap < Cask
+  url 'http://download.jetbrains.com/idea/ideaIU-133.79.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+  version '133.79'
+  sha1 '5823b2b12f0e225b8ca6db1f3fac1636a8811715'
+  link 'IntelliJ IDEA 13 EAP.app'
+end
