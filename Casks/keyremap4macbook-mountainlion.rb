78233494abf58d275a55e5f40065b259d0250842
Peter Jaros <peter.a.jaros@gmail.com>
2013-11-28 18:31:15 -0500

    keyremap4macbook for Mountain Lion.

---
+++ Casks/keyremap4macbook-mountainlion.rb
@@ -0,0 +1,7 @@
+class Keyremap4macbookMountainlion < Cask
+  url 'https://pqrs.org/macosx/keyremap4macbook/files/KeyRemap4MacBook-8.4.0.dmg'
+  homepage 'https://pqrs.org/macosx/keyremap4macbook/'
+  version '8.4.0'
+  sha1 'c2839a17437eebeb3e70684154b1715beefda511'
+  install 'KeyRemap4MacBook.pkg'
+end
