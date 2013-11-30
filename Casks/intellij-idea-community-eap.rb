feb73638ee08a525ae96f74039063e6605cae32c
Pavel Sakun <pavel.sakun@gmail.com>
2013-11-30 22:16:19 +0300

    Updates for jetbrains idea EAP
    + Cask for community edition
    * Ultimate edition updated to 133.162
    * Ultimate edition cask renamed to include full product name

---
+++ Casks/intellij-idea-community-eap.rb
@@ -0,0 +1,7 @@
+class IntellijIdeaCommunityEap < Cask
+  url 'http://download.jetbrains.com/idea/ideaIC-133.162.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+  version '133.162'
+  sha1 'e832a40f8162b94d0814fce75f36a58bb1b5e54f'
+  link 'IntelliJ IDEA 13 CE.app'
+end
---
+++ Casks/intellij-idea-ultimate-eap.rb
@@ -0,0 +1,7 @@
+class IntellijIdeaUltimateEap < Cask
+  url 'http://download.jetbrains.com/idea/ideaIU-133.162.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+  version '133.162'
+  sha1 'c5f1e3781401084169ee5b70b3feddf53375b9fa'
+  link 'IntelliJ IDEA 13.app'
+end
