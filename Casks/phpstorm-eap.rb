a17399076f67954bf31b2b7e9b61f98c8b4d3c24
Oleg Kislitsyn <olegafx@live.com>
2013-12-03 01:41:08 +0400

    Add cask for EAP version of PhpStorm

---
+++ Casks/phpstorm-eap.rb
@@ -0,0 +1,7 @@
+class PhpstormEap < Cask
+  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-133.168.dmg'
+  homepage 'https://www.jetbrains.com/phpstorm/index.html'
+  version '133.168'
+  sha1 '6432cf678601106f8f9c11ed328539dd1f0282b9'
+  link 'PhpStorm EAP.app'
+end
