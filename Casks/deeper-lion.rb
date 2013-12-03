60cbe89ac47536c6247447f1e011d9291fbe00b1
mAAdhaTTah <jamesorodig@gmail.com>
2013-12-02 21:54:03 -0500

    Add Deeper versions

---
+++ Casks/deeper-lion.rb
@@ -0,0 +1,7 @@
+class DeeperLion < Cask
+  url 'http://www.titanium.free.fr/download/107/Deeper.dmg'
+  homepage 'http://www.titanium.free.fr/downloaddeeper.php'
+  version 'latest'
+  no_checksum
+  link 'Deeper.app'
+end
---
+++ Casks/deeper-snowleopard.rb
@@ -0,0 +1,7 @@
+class DeeperSnowleopard < Cask
+  url 'http://www.titanium.free.fr/download/106/Deeper.dmg'
+  homepage 'http://www.titanium.free.fr/downloaddeeper.php'
+  version 'latest'
+  no_checksum
+  link 'Deeper.app'
+end
