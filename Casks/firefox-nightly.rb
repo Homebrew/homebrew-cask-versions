18d4411323b85437e0393cd5aa876324cbf8c15d
sani <sani@users.noreply.github.com>
2013-12-12 02:49:08 +0000

    Update firefox-nightly.rb

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,7 +1,7 @@
 class FirefoxNightly < Cask
-  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-28.0a1.en-US.mac.dmg'
-  homepage 'http://nightly.mozilla.org'
-  version '28.0-alpha1'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-29.0a1.en-US.mac.dmg'
+  homepage 'http://nightly.mozilla.org/'
+  version '29.0a1'
   no_checksum
   link 'FirefoxNightly.app'
 end
