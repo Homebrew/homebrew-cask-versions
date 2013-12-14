f5f0cac7b69319b52cf32ffbeaad6db14ea9cc5a
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2013-12-14 12:39:33 +1100

    MiddleClick (ML and SL versions)
    Based directly off my original cask for MiddleClick for Mavericks.

---
+++ Casks/middleclick-mountainlion.rb
@@ -0,0 +1,7 @@
+class MiddleclickMountainlion < Cask
+  url 'http://clement.beffa.org/labs/downloads/MiddleClick_ml.zip'
+  homepage 'http://clement.beffa.org/labs/projects/middleclick'
+  version 'latest'
+  no_checksum
+  link 'MiddleClick.app'
+end
