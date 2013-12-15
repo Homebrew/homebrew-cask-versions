15a323f1995f735b5eefed249de9f8a0d5fe8406
Chris Chernoff <chris@burnedpixel.com>
2013-12-15 01:14:31 -0800

    Fix Wimoweh Beta URL Missed a rogue _ before the file extension, not sure why the dev has it there.

--- Casks/wimoweh-beta.rb
+++ Casks/wimoweh-beta.rb
@@ -1,5 +1,5 @@
 class WimowehBeta < Cask
-  url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA.zip'
+  url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA_.zip'
   homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
   version '0.66-beta'
   sha1 '5d408c963e0b4318cbbe933e17facde8dad74a51'
