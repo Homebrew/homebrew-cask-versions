242c4faf4997b0ac5203e5cc839bb7d71b9924ed
Chris Chernoff <chris@burnedpixel.com>
2013-12-15 01:01:40 -0800

    Add Wimoweh Beta Cask

---
+++ Casks/wimoweh-beta.rb
@@ -0,0 +1,7 @@
+class WimowehBeta < Cask
+  url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA.zip'
+  homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
+  version '0.66-beta'
+  sha1 '5d408c963e0b4318cbbe933e17facde8dad74a51'
+  link 'Wimoweh.app'
+end
