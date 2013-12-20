4c8e7078fb6c262bff8d6809e37f38d6789cc8d5
Michael Ford <fanquake@users.noreply.github.com>
2013-12-20 16:07:18 +0800

    Sublime Text Dev 3058

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,7 +1,7 @@
 class SublimeTextDev < Cask
-  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203056.dmg'
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203058.dmg'
   homepage 'http://www.sublimetext.com/3dev'
-  version 'build3056'
-  sha256 'ab23d1a80ac83f61e0b6f87860d6f8647d140446f78fd5c38df2f6f50ebf5d5f'
+  version 'build3058'
+  sha1 '2f1e94f15bbd463fae34b44c310314dbbef5df35'
   link 'Sublime Text.app'
 end
