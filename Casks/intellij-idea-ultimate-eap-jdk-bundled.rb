9c7a7106c7018d275eb3589af04fcbb55f0d8915
Beng Hee Eu <beng@benghee.eu>
2013-12-28 02:38:54 +0800

    IntelliJ IDEA Ultimate 13.0.2 EAP 133.370 with bundled JRE 1.7.0_45

---
+++ Casks/intellij-idea-ultimate-eap-jdk-bundled.rb
@@ -0,0 +1,7 @@
+class IntellijIdeaUltimateEapJdkBundled < Cask
+  url 'http://download.jetbrains.com/idea/ideaIU-133.370-jdk-bundled.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+  version '133.370'
+  sha1 'a97c44e9ca4326cfbd4db52e5c2b2e7ed4008d24'
+  link 'IntelliJ IDEA 13 EAP.app'
+end
