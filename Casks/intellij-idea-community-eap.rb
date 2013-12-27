57cfd3e66c2c7397ce4f0af7b0456f263212cee5
Beng Hee Eu <beng@benghee.eu>
2013-12-27 22:36:56 +0800

    IntelliJ IDEA Community Edition 13.0.2 EAP 133.370

--- Casks/intellij-idea-community-eap.rb
+++ Casks/intellij-idea-community-eap.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaCommunityEap < Cask
-  url 'http://download.jetbrains.com/idea/ideaIC-133.162.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIC-133.370.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '133.162'
-  sha1 'e832a40f8162b94d0814fce75f36a58bb1b5e54f'
-  link 'IntelliJ IDEA 13 CE.app'
+  version '133.370'
+  sha1 'f49e98dd1e32567195b89b478ecee66330c40c1e'
+  link 'IntelliJ IDEA 13 CE EAP.app'
 end
