48938ed69e00ac00b9d2da44545d08b0eb50f2a4
Lincoln Lee <git@lincoln.hk>
2014-01-01 22:59:55 +0800

    Remove line number

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,7 +1,7 @@
- 1 class OperaDeveloper < Cask
- 2   url 'http://get.geo.opera.com/pub/opera-developer/20.0.1353.0/mac/Opera_Developer_20.0.1353.0_Setup.dmg'
- 3   homepage 'http://www.opera.com/developer'
- 4   version '20.0.1353.0'
- 5   sha1 '71f06b753a2bab1facbe64ae2cc06ec6e963da46'
- 6   link 'Opera Developer.app'
- 7 end
+class OperaDeveloper < Cask
+  url 'http://get.geo.opera.com/pub/opera-developer/20.0.1353.0/mac/Opera_Developer_20.0.1353.0_Setup.dmg'
+  homepage 'http://www.opera.com/developer'
+  version '20.0.1353.0'
+  sha1 '71f06b753a2bab1facbe64ae2cc06ec6e963da46'
+  link 'Opera Developer.app'
+end
