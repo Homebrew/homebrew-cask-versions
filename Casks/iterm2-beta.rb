8c1d3a9fd2b0c0a0cfae562707b4977e8646a52c
Felipe "Zoc" Silveira <TheZoc@users.noreply.github.com>
2014-01-16 04:21:10 -0200

    Updated iTerm2 Beta to version 1.0.0.20140112

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 class Iterm2Beta < Cask
-  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20131228.zip'
+  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140112.zip'
   homepage 'http://www.iterm2.com/'
-  version '1.0.0.20131228'
-  sha1 'd771ed0cbb7d422539befc529c5ece6cf09011e5'
+  version '1.0.0.20140112'
+  sha1 '89020eebda23c1cf0cea89343a50e0a7a415b172'
   link 'iTerm.app'
 end
