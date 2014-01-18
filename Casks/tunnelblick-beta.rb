33a2205230b8b2dd26e14f9eb48bdf007404ccb9
Beng Hee Eu <beng@benghee.eu>
2014-01-18 12:30:19 +0800

    Tunnelblick 3.4beta20

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,7 +1,7 @@
 class TunnelblickBeta < Cask
-  url 'http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.4beta18.dmg'
+  url 'http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_3.4beta20.dmg'
   homepage 'https://code.google.com/p/tunnelblick/'
-  version '3.4beta18'
-  sha1 'edfe6e0b865475a2c022f2b142acc421d5c9104e'
+  version '3.4beta20'
+  sha1 '1e3b716e05981f242f0933ffc6f7084cf5e339bd'
   link 'Tunnelblick.app'
 end
