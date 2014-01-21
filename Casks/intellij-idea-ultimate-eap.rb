cacd54a99607e9658a7747d70d5881a9f1d1fdf8
Beng Hee Eu <beng@benghee.eu>
2014-01-22 00:24:49 +0800

    IntelliJ IDEA Ultimate 13.0.2 EAP 133.609

--- Casks/intellij-idea-ultimate-eap.rb
+++ Casks/intellij-idea-ultimate-eap.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaUltimateEap < Cask
-  url 'http://download.jetbrains.com/idea/ideaIU-133.471.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIU-133.609.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '133.471'
-  sha1 'eb5e2fc99ddb79430f9d51e13d2249ded339b92a'
+  version '133.609'
+  sha1 '7eaae62e9470cb194fdb395649613e630d665db4'
   link 'IntelliJ IDEA 13 EAP.app'
 end
