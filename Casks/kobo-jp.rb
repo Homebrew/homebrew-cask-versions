2d62c90a8f5aed208e2235d00372d072060cd09e
tcnksm <nsd22843@gmail.com>
2014-01-26 20:58:56 +0900

    Add Kobo.app for Japanese version (3.5.2)

---
+++ Casks/kobo-jp.rb
@@ -0,0 +1,7 @@
+class KoboJp < Cask
+  url 'http://download.kobobooks.com/desktop/RakutenBooks/KoboSetup.dmg'
+  homepage 'http://kobo.rakuten.co.jp'
+  version 'latest'
+  no_checksum
+  link 'Kobo.app'
+end
