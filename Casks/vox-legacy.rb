e22e6d74b0e8bf24c8d76988179b0492360155e0
Vítor Galvão <info@vitorgalvao.com>
2014-01-26 21:26:36 +0000

    added legacy version of vox

---
+++ Casks/vox-legacy.rb
@@ -0,0 +1,7 @@
+class VoxLegacy < Cask
+  url 'http://cloud.coppertino.com/vox/Vox_0.3b2.dmg'
+  homepage 'http://coppertino.com/vox/'
+  version '0.3b2'
+  sha1 '52d7f9fe5eb3d72dc50f269ec8435b4bb90b0771'
+  link 'Vox.app'
+end
