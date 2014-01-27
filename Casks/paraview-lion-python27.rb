fa8badf6c973ae82a158d318e536e211799ebe15
Geoffrey Oxberry <goxberry@gmail.com>
2014-01-27 00:18:51 -0800

    Use caveats DSL in Paraview Lion Python 2.7.

--- Casks/paraview-lion-python27.rb
+++ Casks/paraview-lion-python27.rb
@@ -4,12 +4,10 @@ class ParaviewLionPython27 < Cask
   version '4.1.0'
   sha1 '6da244c25bd500e44f1f020a0e44f5a239708a40'
   link 'paraview.app'
-
-  def caveats; <<-EOS.undent
+  caveats <<-EOS.undent
     This version of ParaView is for OS X Lion (10.7) or Mountain Lion (10.8)
     and should be installed if the system Python version is 2.6. If your
     OS version is Lion (10.7) or later, and your system Python version is
     2.7 or later, you may install this cask or the standard paraview cask.
     EOS
-  end
 end
