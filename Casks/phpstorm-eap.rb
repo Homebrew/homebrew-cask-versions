8a890da48810429711dc1af2920f7ecfa1a7097f
olegafx <olegafx@live.com>
2014-01-27 10:51:09 +0300

    Update phpstorm-eap.rb
    Bump up version.

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,7 +1,7 @@
 class PhpstormEap < Cask
-  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-133.626.dmg'
+  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-133.679.dmg'
   homepage 'https://www.jetbrains.com/phpstorm/index.html'
-  version '133.626'
-  sha1 'f248cb99b6bc1113c6e6a37fdcc950fec2c01ddc'
+  version '133.679'
+  sha1 '1725b50cd1014203878091e41b118bbde56e6760'
   link 'PhpStorm EAP.app'
 end
