2fa2c97f2cec451758b2363b2762dc7e11bc1ded
Michael Ford <fanquake@users.noreply.github.com>
2014-02-09 15:47:12 +0800

    Firefox Aurora 29.0a2

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,7 +1,7 @@
 class FirefoxAurora < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-28.0a2.en-US.mac.dmg'
-  homepage 'http://www.mozilla.org/en-US/firefox/aurora/'
-  version '28.0a2'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-29.0a2.en-US.mac.dmg'
+  homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
+  version '29.0a2'
   no_checksum
   link 'FirefoxAurora.app'
 end
