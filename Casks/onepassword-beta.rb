3ebc482078778695536bda6376c581bf0bf3de22
Camillus Gerard Cai <cgcai@qxcg.net>
2014-03-12 16:00:48 +0800

    Deconflict naming of 1Password beta.
    Also updated package to 4.3.BETA-1.

---
+++ Casks/onepassword-beta.rb
@@ -0,0 +1,7 @@
+class OnepasswordBeta < Cask
+  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.3.BETA-1.zip'
+  homepage 'https://agilebits.com/onepassword'
+  version '4.3.BETA-1'
+  sha256 '5249909397cf0a3646021a18170fd04a0a70965db79fd489318f3e76a4ae7495'
+  link '1Password.app'
+end
