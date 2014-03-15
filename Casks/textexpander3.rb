c1499be4e0095e17f6f5a025f986aeed43e1a1a1
Roland Walker <walker@pobox.com>
2014-03-15 11:51:41 -0400

    sha256 checksum in textexpander3.rb

--- Casks/textexpander3.rb
+++ Casks/textexpander3.rb
@@ -2,6 +2,6 @@ class Textexpander3 < Cask
   url 'http://cdn.smilesoftware.com/TextExpander_3.4.2.zip'
   homepage 'http://www.smilesoftware.com/TextExpander/index.html'
   version '3.4.2'
-  sha1 '0aaf97b5112baef982ede24c5569f2b9e81296a8'
+  sha256 '87859d7efcbfe479e7b78686d4d3f9be9983b2c7d68a6122acea10d4efbb1bfa'
   link 'TextExpander.app'
 end
