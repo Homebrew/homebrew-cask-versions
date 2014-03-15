fb4b95e350e983e1a656b7ed472175c872ee8341
Roland Walker <walker@pobox.com>
2014-03-15 11:51:21 -0400

    sha256 checksum in keepassx2.rb

--- Casks/keepassx2.rb
+++ Casks/keepassx2.rb
@@ -2,6 +2,6 @@ class Keepassx2 < Cask
   url 'http://www.keepassx.org/dev/attachments/download/59/KeePassX-2.0-alpha5.dmg'
   homepage 'http://www.keepassx.org/'
   version '2.0.0-alpha5'
-  sha1 '0d2a32e1f03ee30b70e99f7ea344eafe63c463d0'
+  sha256 '7d1e3de8446421d1c4cb920364907de3d03cd3eaedfa96602fac8983479a5303'
   link 'KeePassX.app'
 end
