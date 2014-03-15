46b03b99f1337d78b31aca6b2ed9786d1f7fa561
Roland Walker <walker@pobox.com>
2014-03-15 11:51:43 -0400

    sha256 checksum in thunderbird-ja.rb

--- Casks/thunderbird-ja.rb
+++ Casks/thunderbird-ja.rb
@@ -2,6 +2,6 @@ class ThunderbirdJa < Cask
   url 'http://download-installer.cdn.mozilla.net/pub/thunderbird/releases/24.2.0/mac/ja-JP-mac/Thunderbird%2024.2.0.dmg'
   homepage 'http://www.mozilla.jp/thunderbird/'
   version '24.2.0'
-  sha1 'aeb874de260df0cfb0cec06d24d9fc3baf343059'
+  sha256 'a3e4d03924ee555865b90bb95f8043811e15bfd20358e5b265b5b1cfc4812b7d'
   link 'Thunderbird.app'
 end
