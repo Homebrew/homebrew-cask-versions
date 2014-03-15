a74296f55247ba125989e0224274a32f0c7eb4a1
Roland Walker <walker@pobox.com>
2014-03-15 11:51:37 -0400

    sha256 checksum in rubymine-eap.rb

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -2,6 +2,6 @@ class RubymineEap < Cask
   url 'http://download.jetbrains.com/ruby/RubyMine-134.1146.dmg'
   homepage 'http://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
   version '134.1146'
-  sha1 'de169b061f3916bd7aed5fce99d467e039e190fb'
+  sha256 '214b6ac3d8b1a318cbca212e841f7e5fdbf2263ff72e3fb020661f941ab327f4'
   link 'RubyMine EAP.app'
 end
