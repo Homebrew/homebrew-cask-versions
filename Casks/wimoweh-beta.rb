5379b2cb5d57c9b427665d5d222d7a7986a5a781
Roland Walker <walker@pobox.com>
2014-03-15 11:51:52 -0400

    sha256 checksum in wimoweh-beta.rb

--- Casks/wimoweh-beta.rb
+++ Casks/wimoweh-beta.rb
@@ -2,6 +2,6 @@ class WimowehBeta < Cask
   url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA_.zip'
   homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
   version '0.66-beta'
-  sha1 '5d408c963e0b4318cbbe933e17facde8dad74a51'
+  sha256 '8be363dc4764299df68220da67cf08d6f913fbf1bfe14059cd47bf99728d97fc'
   link 'Wimoweh.app'
 end
