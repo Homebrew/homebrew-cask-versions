e762e241d3ff60960fd9dc17a37fc41a2f25fda7
Roland Walker <walker@pobox.com>
2014-03-15 11:51:06 -0400

    sha256 checksum in cocktail-lion.rb

--- Casks/cocktail-lion.rb
+++ Casks/cocktail-lion.rb
@@ -2,6 +2,6 @@ class CocktailLion < Cask
   url 'http://usa.maintain.se/CocktailLionEdition.dmg'
   homepage 'http://maintain.se/cocktail'
   version '5.6'
-  sha1 '15b29030f1635b4245f23b43251914ca1accdd10'
+  sha256 'd2c72042bfc8ce6948667b2eb23a95114be753346288d2e476dcca1c28dbf79a'
   link 'Cocktail.app'
 end
