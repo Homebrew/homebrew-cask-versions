dea73bb1e3ae5213ca55bcb1c5a04af954def33b
Roland Walker <walker@pobox.com>
2014-03-15 11:51:11 -0400

    sha256 checksum in eclipse-jee.rb

--- Casks/eclipse-jee.rb
+++ Casks/eclipse-jee.rb
@@ -2,6 +2,6 @@ class EclipseJee < Cask
   url 'http://download.eclipse.org/technology/epp/downloads/release/kepler/SR1/eclipse-jee-kepler-SR1-macosx-cocoa-x86_64.tar.gz'
   homepage 'http://eclipse.org'
   version '4.3.1'
-  sha1 'eda969782c5de49b54fa1dacb5d59e7dd1be29a0'
+  sha256 '7b4dfed810101b2bc8d2832c461455e9a11ee6a325112f9561af2a99ad7ff164'
   link 'eclipse/Eclipse.app'
 end
