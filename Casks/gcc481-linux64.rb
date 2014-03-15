3bc1006f161d142c3944c66d7086d7a8db65d6f7
Roland Walker <walker@pobox.com>
2014-03-15 11:51:14 -0400

    sha256 checksum in gcc481-linux64.rb

--- Casks/gcc481-linux64.rb
+++ Casks/gcc481-linux64.rb
@@ -2,6 +2,6 @@ class Gcc481Linux64 < Cask
   url 'http://crossgcc.rts-software.org/download/gcc-4.8.1-for-linux32-linux64/gcc-4.8.1-for-linux64.dmg'
   homepage 'http://crossgcc.rts-software.org/doku.php?id=start'
   version '4.8.1'
-  sha1 'b25102b4f8bb4cebb042c64bb6ea823ce4d22058'
+  sha256 'ed9cd7a8483f68da333bf94bb09cc6cf6f5fa2328c77dc60c13cdd318d8d805a'
   install 'gcc-4.8.1-for-linux64.pkg'
 end
