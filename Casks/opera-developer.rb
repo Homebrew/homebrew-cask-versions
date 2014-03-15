ae0a43bd6752df083924c8cb69e0311b76310462
Roland Walker <walker@pobox.com>
2014-03-15 11:51:33 -0400

    sha256 checksum in opera-developer.rb

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -2,6 +2,6 @@ class OperaDeveloper < Cask
   url 'http://get.geo.opera.com/pub/opera-developer/20.0.1353.0/mac/Opera_Developer_20.0.1353.0_Setup.dmg'
   homepage 'http://www.opera.com/developer'
   version '20.0.1353.0'
-  sha1 '71f06b753a2bab1facbe64ae2cc06ec6e963da46'
+  sha256 '13aab747e6634140361a50fcecc1ac33e3944e113d0bf724246e3d75e9f9a409'
   link 'Opera Developer.app'
 end
