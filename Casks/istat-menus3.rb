413dce5aa3b3292914aab38af695e3f570174779
Roland Walker <walker@pobox.com>
2014-03-15 11:51:17 -0400

    sha256 checksum in istat-menus3.rb

--- Casks/istat-menus3.rb
+++ Casks/istat-menus3.rb
@@ -2,6 +2,6 @@ class IstatMenus3 < Cask
   url 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.27.zip'
   homepage 'http://bjango.com/mac/istatmenus/'
   version '3.27'
-  sha1 '91ce9b2c21e8eb51dfa9eaf860614690739100ea'
+  sha256 '31b613aad5b4228087696f0c928a6018f8606534acbb3917e55635c51073bdf6'
   link 'iStat Menus.app'
 end
