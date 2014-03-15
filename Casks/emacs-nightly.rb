5f64e227244c37d41c3da6b38fcbb5128727edbb
Roland Walker <walker@pobox.com>
2014-03-15 11:51:13 -0400

    sha256 checksum in emacs-nightly.rb

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -2,6 +2,6 @@ class EmacsNightly < Cask
   url 'http://emacsformacosx.com/emacs-builds/Emacs-2013-11-19_01-33-37-115148-universal-10.6.8.dmg'
   homepage 'http://emacsformacosx.com/'
   version '24.3.50r115148'
-  sha1 'e22c2ed951507f1f05090eeaa95bd5a3a561bb47'
+  sha256 '903e0f8acb5e3e84e46f1787d85b17b4be88ec7956d17adb6b7918c5f2bdf2cc'
   link 'Emacs.app'
 end
