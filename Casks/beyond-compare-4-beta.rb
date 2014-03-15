e74d43fa166960505ffde638b1072ee1d73291a1
Roland Walker <walker@pobox.com>
2014-03-15 11:51:04 -0400

    sha256 checksum in beyond-compare-4-beta.rb

--- Casks/beyond-compare-4-beta.rb
+++ Casks/beyond-compare-4-beta.rb
@@ -2,6 +2,6 @@ class BeyondCompare4Beta < Cask
   url 'http://www.scootersoftware.com/BCompareOSX-4.0.0.17451.zip'
   homepage 'http://www.scootersoftware.com/beta.php'
   version '4.0.0.17451'
-  sha1 'ae61d7c9e555d05cce3fca1cd00f98d9785af942'
+  sha256 '2b82f41d5b79b4c3454b644f0145eec1ca3a4fbc7fc811ea59639d9388560fe0'
   link 'Beyond Compare.app'
 end
