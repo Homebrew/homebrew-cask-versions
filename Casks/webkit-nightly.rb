1aabc8afa635d8c3667e85e2648e2b4d2e2530ed
Roland Walker <walker@pobox.com>
2014-03-15 11:51:50 -0400

    sha256 checksum in webkit-nightly.rb

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -2,6 +2,6 @@ class WebkitNightly < Cask
   url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r159487.dmg'
   homepage 'http://nightly.webkit.org/'
   version 'r159487'
-  sha1 '6fe6da20920ddb73fd7003f1c73e0848a0159ee9'
+  sha256 '6d59673fdac0c380f66c1da6ae5742377b68add2f0bb33737bd7e1cec7ba821c'
   link 'WebKit.app'
 end
