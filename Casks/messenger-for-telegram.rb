1af2fc2121f903929f7ea5773669df29741dcb5c
Florian Harbecke <florian@harbecke.me>
2014-03-16 19:38:18 +0100

    Updated Messenger-for-Telegram.app to 1.2 (127)

--- Casks/messenger-for-telegram.rb
+++ Casks/messenger-for-telegram.rb
@@ -1,7 +1,7 @@
 class MessengerForTelegram < Cask
-  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/001/002/018/original/811af9ec47f6b11e614e2da1d34a73ec/Messenger_for_Telegram.app.zip'
+  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/001/026/095/original/69c66159f4481a814d163bc0b7087c96/Messenger_for_Telegram.app.zip'
   homepage 'https://vk.com/telegram_osx'
-  version '1.2'
-  sha256 'c0bfa96ca1694762f1735709b680861713d00efa0cce7ab4b16985d7a614b1fe'
+  version '1.2 (127)'
+  sha256 '15184ce9597dadfd557920a6479efe65c22ed50bcd9d03fc68c7d4fdade07990'
   link 'Messenger for Telegram.app'
 end
