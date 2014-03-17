8d1a8869b455976e65f40b34dfccfb7be2f2d749
Florian Harbecke <florian@harbecke.me>
2014-03-17 09:03:45 +0100

    Added Firefox Beta (DE)

---
+++ Casks/firefox-beta-de.rb
@@ -0,0 +1,7 @@
+class FirefoxBetaDe < Cask
+  url 'https://download.mozilla.org/?product=firefox-28.0b9&os=osx&lang=de'
+  homepage 'https://www.mozilla.org/de/firefox/beta/'
+  version '28.0b9'
+  sha256 'ffa05b160e4431168f9258982dd705fab8baa5b9607739eafee97cf6698eacca'
+  link 'Firefox.app'
+end
