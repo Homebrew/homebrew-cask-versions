e6103c16a56c43091c0cf60b425268c1de34e91a
Ben Kreeger <ben@kree.gr>
2014-03-19 12:26:56 -0500

    Adding a cask for Vagrant v1.4.3.
    Squashed commit of the following:
    
    commit e76734d02d096d1a5acf8288d7f12c6cae5436f3
    Author: Ben Kreeger <ben@kree.gr>
    Date:   Wed Mar 19 12:22:48 2014 -0500
    
        Ensuring name is correct.
    
    commit 25f3a557f751c037934d35421990fa00c7571543
    Author: Ben Kreeger <ben@kree.gr>
    Date:   Wed Mar 19 12:21:04 2014 -0500
    
        Adding a cask for Vagrant v1.4.3.

---
+++ Casks/vagrant143.rb
@@ -0,0 +1,8 @@
+class Vagrant143 < Cask
+  url 'https://dl.bintray.com/mitchellh/vagrant/Vagrant-1.4.3.dmg'
+  homepage 'http://www.vagrantup.com'
+  version '1.4.3'
+  sha1 '78d20fbe44704d91b7958af57308eb0fde147ed4'
+  install 'Vagrant.pkg'
+  uninstall :script => 'uninstall.tool', :input => %w[Yes]
+end
