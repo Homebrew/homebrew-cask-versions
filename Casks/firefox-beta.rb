b760704908e842f6cf5b863fd7e0c0853fa2a4fb
Chris Hasenpflug <github@chris.hasenpflug.us>
2014-03-23 00:11:50 -0500

    Correct Firefox Beta 29.0b1 checksum

--- Casks/firefox-beta.rb
+++ Casks/firefox-beta.rb
@@ -2,6 +2,6 @@ class FirefoxBeta < Cask
   url 'https://download.mozilla.org/?product=firefox-29.0b1&os=osx&lang=en-US'
   homepage 'https://www.mozilla.org/en-US/firefox/beta/'
   version '29.0b1'
-  sha256 '5f13ff1fdae28e09c50ebaf915802e275918ca32e411fcd7a61f466efe68a042'
+  sha256 'f2894b188e1738360f269c1190370c3880c6886306486ba7da626ab2eeef6368'
   link 'Firefox.app'
 end
