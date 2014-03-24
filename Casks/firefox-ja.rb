c85d2b5bd2ac4befd6e96749fe90c3e070ff20e7
DriftwoodJP <driftwood14@gmail.com>
2014-03-24 18:04:21 +0900

    added Firefox for Japanese

---
+++ Casks/firefox-ja.rb
@@ -0,0 +1,7 @@
+class FirefoxJa < Cask
+  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=ja-JP-mac'
+  homepage 'http://www.mozilla.jp/firefox/'
+  version 'latest'
+  no_checksum
+  link 'Firefox.app'
+end
