0184660b4c148c8efa6118fb107c0ce43c950d09
Tolbkni Kao <tolbkni@gmail.com>
2014-03-25 20:31:21 +0800

    Add Firefox Beta for Chinese (Simplified)

---
+++ Casks/firefox-beta-cn.rb
@@ -0,0 +1,7 @@
+class FirefoxBetaCn < Cask
+  url 'https://download.mozilla.org/?product=firefox-29.0b2&os=osx&lang=zh-CN'
+  homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
+  version '29.0b2'
+  sha256 '9ed32e46990d9f16139c31002a4fd93739dec401c9c6cb518c1a0bf78ee0f435'
+  link 'Firefox.app'
+end
