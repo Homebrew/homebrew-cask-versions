d0bff56d16da4dc606b53962efbf599dcc0c6520
Tolbkni Kao <tolbkni@gmail.com>
2014-03-25 20:29:20 +0800

    Add Firefox for Chinese (Simplified)

---
+++ Casks/firefox-cn.rb
@@ -0,0 +1,7 @@
+class FirefoxCn < Cask
+  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=zh-CN'
+  homepage 'https://www.mozilla.org/zh-CN/firefox/'
+  version 'latest'
+  no_checksum
+  link 'Firefox.app'
+end
