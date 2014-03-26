748d991ee9a3a6a2db240a1d1c1f2c5059177bcf
Florian Harbecke <florian@harbecke.me>
2014-03-26 17:49:10 +0100

    Updated Messenger-for-Telegram.app to Latest
    Always install latest version :)
    I am wondering if this can be moved to phinze-cask?

--- Casks/messenger-for-telegram.rb
+++ Casks/messenger-for-telegram.rb
@@ -1,7 +1,7 @@
 class MessengerForTelegram < Cask
-  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/001/068/354/original/2c1cc472626684ae4494cc2f2a4d7fba/Messenger_for_Telegram.zip'
+  url 'https://rink.hockeyapp.net/api/2/apps/c55f5e74ae5d0ad254df29f71a1b5f0e/?format=zip'
   homepage 'https://vk.com/telegram_osx'
-  version '1.2 (131)'
-  sha256 '3d7f2c48f041ee15b0063d99edea892efbbb7b4648fbf870745dab44c48ab8e4'
+  version 'latest'
+  no_checksum
   link 'Messenger for Telegram.app'
 end
