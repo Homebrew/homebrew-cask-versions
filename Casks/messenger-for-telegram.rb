4b5b29a15be921112da3e38f8e72f6eff01c6177
Florian Harbecke <florian@harbecke.me>
2014-03-26 19:48:42 +0100

    Removed messenger-for-telegram as I moved it to phinze-cask

--- Casks/messenger-for-telegram.rb
+++
@@ -1,7 +0,0 @@
-class MessengerForTelegram < Cask
-  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/001/069/921/original/8d435fc51615aef756f3adb6c8ed0a79/Messenger_for_Telegram.app.zip'
-  homepage 'https://vk.com/telegram_osx'
-  version '1.2.134'
-  sha256 'b772d45232d10d7723343231acef259169774a79ccf085cfa44d648cc48abbce'
-  link 'Messenger for Telegram.app'
-end
