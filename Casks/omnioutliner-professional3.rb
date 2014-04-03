51919d90072840ef78a8657a71f240a503ca4979
DriftwoodJP <driftwood14@gmail.com>
2014-04-03 14:36:00 +0900

    added omnioutliner-professional3

---
+++ Casks/omnioutliner-professional3.rb
@@ -0,0 +1,7 @@
+class OmnioutlinerProfessional3 < Cask
+  url 'http://www.omnigroup.com/ftp/pub/software/MacOSX/10.4/OmniOutlinerPro-3.10.6.dmg'
+  homepage 'http://www.omnigroup.com/omnioutliner/'
+  version '3.10.6'
+  sha256 'ec8ee1ec00870a1cc5920d2e899cd255eef6564c8e9944eadfdf5297f1cb348e'
+  link 'OmniOutliner Professional.app'
+end
