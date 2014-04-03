1e2c1c49ff63145258799ef659e517dcc97be471
Aaron Wolen <aaron@wolen.com>
2014-04-03 15:34:18 -0400

    Update RStudio preview to 0.98.752

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,7 +1,7 @@
 class RstudioPreview < Cask
-  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.484.dmg'
+  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.752.dmg'
   homepage 'http://www.rstudio.com/ide/download/preview'
-  version '0.98.484'
-  sha256 '244529c9a0cd941b77f155bdffb6d438fd618382bcefb212711d834752791854'
+  version '0.98.752'
+  sha256 '7393ef30b8e49b20e385d0828177400fb91b775e2c078fec7878a43d3a4e5e10'
   link 'RStudio.app'
 end
