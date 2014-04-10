d5367acb7439375ee279b53b8a4d92aa52b8d644
Sébastien Balayn <sebastien.balayn@gmail.com>
2014-04-10 16:45:17 +0200

    Update firefox-nightly.rb
    Sorry, wrong checksum :-(

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -2,6 +2,6 @@ class FirefoxNightly < Cask
   url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-31.0a1.en-US.mac.dmg'
   homepage 'http://nightly.mozilla.org/'
   version '31.0a1'
-  sha256 '1b252bb1c0f0fae1a8976395b6481534657ffbc05d91f111779755f55ec9e656'
+  sha256 'ad6ca5707ef3c1e478d5f04be4f51eeca6599e94ec9609860e028f271cfafe3e'
   link 'FirefoxNightly.app'
 end
