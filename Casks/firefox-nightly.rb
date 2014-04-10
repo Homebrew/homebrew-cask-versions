49965ac2321ed470f1dfe614177b72f3eb30fc45
Sébastien Balayn <sebastien.balayn@gmail.com>
2014-04-10 16:25:44 +0200

    Update firefox-nightly.rb
    Adding the sha 256 sum

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -2,6 +2,6 @@ class FirefoxNightly < Cask
   url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-31.0a1.en-US.mac.dmg'
   homepage 'http://nightly.mozilla.org/'
   version '31.0a1'
-  no_checksum
+  sha256 '1b252bb1c0f0fae1a8976395b6481534657ffbc05d91f111779755f55ec9e656'
   link 'FirefoxNightly.app'
 end
