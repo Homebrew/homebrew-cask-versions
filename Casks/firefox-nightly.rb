88586f7f659f3322040b6a2e57de1f7521cc0657
Sébastien Balayn <sebastien.balayn@gmail.com>
2014-04-10 16:13:49 +0200

    Update firefox-nightly.rb

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,7 +1,7 @@
 class FirefoxNightly < Cask
-  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-30.0a1.en-US.mac.dmg'
+  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-31.0a1.en-US.mac.dmg'
   homepage 'http://nightly.mozilla.org/'
-  version '30.0a1'
+  version '31.0a1'
   no_checksum
   link 'FirefoxNightly.app'
 end
