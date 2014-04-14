5150fe86391a3653288f817fadb2f52488cac4fb
Morgan Estes <morgan@golftailor.com>
2014-04-14 11:45:18 -0500

    Updated PhpStorm 8.0 EAP to 136.1575.

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,7 +1,7 @@
 class PhpstormEap < Cask
-  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-136.1476.dmg'
+  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-136.1575.dmg'
   homepage 'https://www.jetbrains.com/phpstorm/index.html'
-  version '136.1476'
-  sha256 '84e6b1afaef5a58ac74738c780dbe79b47afd4d6290f0df738f2a05fedec7a72'
+  version '136.1575'
+  sha256 '8f0b5a4c4605996bb4af7bbdb709ac06ec9daf454483da60db0ab63d74faab45'
   link 'PhpStorm EAP.app'
 end
