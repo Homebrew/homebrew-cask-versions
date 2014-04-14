575243a0efde87feeb8984e7f76555d042560df1
niclasgelin <github@niclasgelin.se>
2014-04-14 18:49:45 +0200

    Update firefox-nightly.rb
    Fix for Error SHA2 mismatch

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -2,6 +2,6 @@ class FirefoxNightly < Cask
   url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-31.0a1.en-US.mac.dmg'
   homepage 'http://nightly.mozilla.org/'
   version '31.0a1'
-  sha256 'ad6ca5707ef3c1e478d5f04be4f51eeca6599e94ec9609860e028f271cfafe3e'
+  sha256 '28adf442f339ac3272d1ad14d657e902fbe1e7d6ebb4e007103513aca5a1990c'
   link 'FirefoxNightly.app'
 end
