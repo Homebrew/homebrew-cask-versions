7ae985a71f29515b3d51cc40608fdc6b4a2515e4
Morgan Estes <morgan@golftailor.com>
2014-04-23 10:16:46 -0500

    Version bump of PhpStorm, for real this time :).

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,7 +1,7 @@
 class PhpstormEap < Cask
   url 'http://download.jetbrains.com/webide/PhpStorm-EAP-136.1672.dmg'
   homepage 'https://www.jetbrains.com/phpstorm/index.html'
-  version '136.1575'
+  version '136.1672'
   sha256 '633aa5021cdc871343254c2c3c2c3beb1914b4b2b64db776ee5b9e49ee276248'
   link 'PhpStorm EAP.app'
 end
