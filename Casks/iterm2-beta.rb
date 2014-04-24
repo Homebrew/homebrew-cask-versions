fac84c3bd50ec955406c2f2beafcbba649940cea
Beng Hee Eu <beng@benghee.eu>
2014-04-24 20:09:38 +0800

    iTerm2 1.0.0.20140421 beta

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 class Iterm2Beta < Cask
-  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140403.zip'
+  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140421.zip'
   homepage 'http://www.iterm2.com/'
-  version '1.0.0.20140403'
-  sha256 'b33fe4880489af93514109308427705c30ccd8661549c5eff143cc58bb961f03'
+  version '1.0.0.20140421'
+  sha256 'd9f3dd198febf46c272a8d4b473e35cae4cdfbf1963a7796114df53b5cb06c63'
   link 'iTerm.app'
 end
