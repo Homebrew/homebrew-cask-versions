03b466a3ddc5913337599e5c7585976392ee60c8
Nathan Kleyn <nathan@nathankleyn.com>
2014-04-30 17:11:47 +0100

    Update IntelliJ EAP casks to version 135.760.

--- Casks/intellij-idea-ultimate-eap.rb
+++ Casks/intellij-idea-ultimate-eap.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaUltimateEap < Cask
-  url 'http://download.jetbrains.com/idea/ideaIU-133.609.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIU-135.760.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '133.609'
-  sha1 '7eaae62e9470cb194fdb395649613e630d665db4'
+  version '135.760'
+  sha1 '1fc2eb4ac9a4d221a7d025d03503aa986e934942'
   link 'IntelliJ IDEA 13 EAP.app'
 end
