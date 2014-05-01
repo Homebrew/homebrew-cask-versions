6e6d18d9d8ee48bc8e8c1fb737775fa66a923ced
Lincoln Lee <git@lincoln.hk>
2014-05-01 20:16:54 +0800

    Add utorrent beta

---
+++ Casks/utorrent-beta.rb
@@ -0,0 +1,7 @@
+class UtorrentBeta < Cask
+  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
+  homepage 'http://www.utorrent.com/'
+  version 'latest'
+  no_checksum
+  link 'uTorrent.app'
+end
