281620696b968ff57644ce991b643e0682315b96
Morgan Estes <morgan.estes@gmail.com>
2014-05-01 10:38:43 -0500

    Fixed typo in PhpStorm EAP sha256 line.

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -2,6 +2,6 @@ class PhpstormEap < Cask
   url 'http://download.jetbrains.com/webide/PhpStorm-EAP-138.38.dmg'
   homepage 'http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
   version '138.38'
-  sha256brew '10bd216cab733340447a2ef531d24284a319284cfa673fa71b908affefa55278'
+  sha256 '10bd216cab733340447a2ef531d24284a319284cfa673fa71b908affefa55278'
   link 'PhpStorm EAP.app'
 end
