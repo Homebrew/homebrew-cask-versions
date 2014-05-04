1d213a935c1b0a8ff131f88d0d62e8976e0c70db
Tolbkni Kao <tolbkni@gmail.com>
2014-05-04 13:37:08 +0800

    Update firefox beta, aurora, nightly versions.
    Update firefox beta 29.0 to 30.0
    Update firefox aurora 30.0 t0 31.0
    Update firefox nightly 31.0 to 32.0
    Update firefox ux 30.0 to 32.0

--- Casks/firefox-ux.rb
+++ Casks/firefox-ux.rb
@@ -1,7 +1,7 @@
 class FirefoxUx < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-ux/firefox-30.0a1.en-US.mac.dmg'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-ux/firefox-32.0a1.en-US.mac.dmg'
   homepage 'http://people.mozilla.org/~jwein/ux-nightly/'
-  version '30.0a1'
-  sha1 'cb13c23a8179a4dd32ab631ae7a9588095688150'
+  version '32.0a1'
+  sha1 'ab7e12526e86c21333ea8e11d8e7cb0bbcab0dfa'
   link 'FirefoxUX.app'
 end
