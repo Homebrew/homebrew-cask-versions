587ab11d2b6750f36d7942305607e24b3aa08aec
Michael Ford <fanquake@users.noreply.github.com>
2014-05-04 17:09:23 +0800

    Firefox nightly 32.0a1

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,7 +1,7 @@
 class FirefoxNightly < Cask
-  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-31.0a1.en-US.mac.dmg'
-  homepage 'http://nightly.mozilla.org/'
-  version '31.0a1'
-  sha256 '28adf442f339ac3272d1ad14d657e902fbe1e7d6ebb4e007103513aca5a1990c'
+  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-32.0a1.en-US.mac.dmg'
+  homepage 'https://nightly.mozilla.org/'
+  version '32.0a1'
+  sha256 '93b8f58ec5708e33d7b1035400172ede8bffb4200ab0b09e29bae9e2ff3e8653'
   link 'FirefoxNightly.app'
 end
