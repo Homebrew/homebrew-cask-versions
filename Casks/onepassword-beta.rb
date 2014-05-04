61132db9d6a9c1240eadc5931a883c3904e8318c
Michael Ford <fanquake@users.noreply.github.com>
2014-05-04 16:58:59 +0800

    1Password 4.3.1.BETA-7

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -1,7 +1,7 @@
 class OnepasswordBeta < Cask
-  url 'http://i.agilebits.com/dist/1P/mac4/1Password-4.3.BETA-2.zip'
+  url 'http://aws.cachefly.net/dist/1P/mac4/1Password-4.3.1.BETA-7.zip'
   homepage 'https://agilebits.com/onepassword'
-  version '4.3.BETA-2'
-  sha256 'e482ac949207dc05ce0f9beb45f8dec46f4f2e9de18bbd62fdcb864905300321'
+  version '4.3.1.BETA-7'
+  sha256 '350e60b2998812c1ea19e8959a7b9b615547011699a83f7295af37d8a3c85bbb'
   link '1Password.app'
 end
