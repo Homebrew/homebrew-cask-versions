1086c1c0ee793a90f3e57032a04bfba5841d02e1
ndr <ndr@qef.io>
2014-05-06 00:16:34 +0200

    Update Sublime Text dev to Build 3062

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,8 +1,8 @@
 class SublimeTextDev < Cask
-  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203061.dmg'
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203062.dmg'
   homepage 'http://www.sublimetext.com/3dev'
-  version 'Build 3061'
-  sha256 '2c49418b2d9c021c276f1b84bcdeca45fc433cc3c3787c0297b264d14fd0e5c9'
+  version 'Build 3062'
+  sha256 '641459fb786f3db59c0a99b2017c7eec1ccb7a0949764fc417e4c916a8b85264'
   link 'Sublime Text.app'
   binary 'Sublime Text.app/Contents/SharedSupport/bin/subl'
   caveats do
