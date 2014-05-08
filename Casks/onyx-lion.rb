b468622baf2367b640db0a2f6de53977fe76dd0c
Roland Walker <walker@pobox.com>
2014-05-08 09:42:15 -0400

    update onyx-lion to sha56

--- Casks/onyx-lion.rb
+++ Casks/onyx-lion.rb
@@ -2,6 +2,6 @@ class OnyxLion < Cask
   url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
   version '2.4.8'
-  sha1 '830c72d93494c4d77b1e216fda25d77b56660a0e'
+  sha256 'e637359020d3514cf1fe40d1f616692919c41aed926a395bbc7093b256e42e35'
   link 'OnyX.app'
 end
