d8c78d736b01368b8db958be8af77ccd00cde7fa
Ngoc Pham <ngocphamm@users.noreply.github.com>
2014-05-09 02:57:30 -0400

    Update PHPStorm-EAP to latest build 138.84

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,7 +1,7 @@
 class PhpstormEap < Cask
-  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-138.38.dmg'
+  url 'http://download.jetbrains.com/webide/PhpStorm-EAP-138.84.dmg'
   homepage 'http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
-  version '138.38'
-  sha256 '10bd216cab733340447a2ef531d24284a319284cfa673fa71b908affefa55278'
+  version '138.84'
+  sha256 '15ecf78338296d4936269bb040f427e739b7fdfac6fc0a0534882de7ab027263'
   link 'PhpStorm EAP.app'
 end
