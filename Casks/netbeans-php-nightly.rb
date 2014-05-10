fd6943e4622be1f90ef0b4065b38dbba3a0cbb07
Morgan Estes <morgan.estes@gmail.com>
2014-05-09 21:35:29 -0500

    Moving to NetBeans PHP nightly after version 8.0 was released.

---
+++ Casks/netbeans-php-nightly.rb
@@ -0,0 +1,7 @@
+class NetbeansPhpNightly < Cask
+  url 'http://bits.netbeans.org/download/trunk/nightly/latest/bundles/netbeans-trunk-nightly-201405090001-php-macosx.dmg'
+  homepage 'https://netbeans.org/'
+  version '201405090001'
+  sha256 '77ae100aa581507f88b40392a13e0109742fda0b18c2ca226a0cc436c694841a'
+  install 'NetBeans Dev 201405090001.mpkg'
+end
