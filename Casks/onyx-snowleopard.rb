14ae8ec86ff73d2586e100837833efa779e632dc
Lincoln Lee <git@lincoln.hk>
2014-05-12 20:39:36 +0800

    Update OnyX. Switch sha1 to sha256

--- Casks/onyx-snowleopard.rb
+++ Casks/onyx-snowleopard.rb
@@ -2,6 +2,6 @@ class OnyxSnowleopard < Cask
   url 'http://www.titanium.free.fr/download/106/OnyX.dmg'
   homepage 'http://www.titanium.free.fr/downloadonyx.php'
   version '2.4.0'
-  sha1 '188c15090988b379dce9f0fafa7b686c6eff2ada147cde150b16e1ae01e4e9e4'
+  sha256 '188c15090988b379dce9f0fafa7b686c6eff2ada147cde150b16e1ae01e4e9e4'
   link 'OnyX.app'
 end
