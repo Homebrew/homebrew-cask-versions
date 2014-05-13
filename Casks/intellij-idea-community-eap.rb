936cba218e9f2212be5e685fcd143ec4a7732846
Luke Horvat <lukehorvat@gmail.com>
2014-05-14 01:51:29 +1000

    Update intellij-idea-community-eap to 135.863

--- Casks/intellij-idea-community-eap.rb
+++ Casks/intellij-idea-community-eap.rb
@@ -1,7 +1,7 @@
 class IntellijIdeaCommunityEap < Cask
-  url 'http://download.jetbrains.com/idea/ideaIC-135.815.dmg'
+  url 'http://download.jetbrains.com/idea/ideaIC-135.863.dmg'
   homepage 'https://www.jetbrains.com/idea/index.html'
-  version '135.815'
-  sha256 '2da97bd1ad08fdf46dca94fc87469a325198da9dce8b6e8f021bbe3bfcd20aed'
+  version '135.863'
+  sha256 '623aac77c3fea84ca6677d5777f7c1c72e3cd5b0ba680d3a465452767e78db89'
   link 'IntelliJ IDEA 13 CE EAP.app'
 end
