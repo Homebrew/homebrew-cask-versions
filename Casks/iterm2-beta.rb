88a6571d58bbdcaa5b01ce1236682d3c065bd0b9
ndr <ndr@qef.io>
2014-05-20 06:02:18 +0200

    Update iTerm 2 beta to 1.0.0.20140518

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 class Iterm2Beta < Cask
-  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140421.zip'
+  url 'http://www.iterm2.com/downloads/beta/iTerm2-1_0_0_20140518.zip'
   homepage 'http://www.iterm2.com/'
-  version '1.0.0.20140421'
-  sha256 'd9f3dd198febf46c272a8d4b473e35cae4cdfbf1963a7796114df53b5cb06c63'
+  version '1.0.0.20140518'
+  sha256 'e10a1c859714f5449a0314629b24594205324bc80faad219c1f312872f64daa1'
   link 'iTerm.app'
 end
