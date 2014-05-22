150dad4f947a936ed09f2c3b59e9d90ff125999a
Tolbkni Kao <tolbkni@gmail.com>
2014-05-23 00:47:47 +0800

    Upgrade TextMate.app to v2.0-alpha.9543

--- Casks/textmate2.rb
+++ Casks/textmate2.rb
@@ -1,7 +1,7 @@
 class Textmate2 < Cask
-  url 'https://github.com/textmate/textmate/releases/download/v2.0-alpha.9497/TextMate_2.0-alpha.9497.tbz'
+  url 'https://github.com/textmate/textmate/releases/download/v2.0-alpha.9543/TextMate_2.0-alpha.9543.tbz'
   homepage 'http://macromates.com/'
-  version '2.0-alpha.9497'
-  sha256 '1910d43fc7230d61af50d6c6da7ba20f86df571828f8361049c839a88123121d'
+  version '2.0-alpha.9543'
+  sha256 'dae2e13b8bb853353611caba1aa6416eaca2155e6c859dd0f92953625e6b2b09'
   link 'TextMate.app'
 end
