c998f1ff657b837e09478b0b305cea1ba42f2ef2
Beng Hee Eu <beng@benghee.eu>
2014-05-24 09:31:04 +0800

    Charles 3.9.1 OpenJDK version

---
+++ Casks/charles-openjdk.rb
@@ -0,0 +1,7 @@
+class CharlesOpenjdk < Cask
+  url 'http://www.charlesproxy.com/assets/release/3.9.1/charles-proxy-3.9.1-openjdk.dmg'
+  homepage 'http://www.charlesproxy.com/'
+  version '3.9.1'
+  sha256 'f427853b7e23a1a1c40c3d4b2298a72a28d66303bb567a3300821a4369499369'
+  link 'Charles.app'
+end
