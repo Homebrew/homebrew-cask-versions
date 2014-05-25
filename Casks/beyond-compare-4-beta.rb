387c8436312999853bed223b57eba456fe3f1588
Beng Hee Eu <beng@benghee.eu>
2014-05-25 18:38:41 +0800

    Suppress move to /Applications prompt for Beyond Compare 4 beta

--- Casks/beyond-compare-4-beta.rb
+++ Casks/beyond-compare-4-beta.rb
@@ -4,4 +4,9 @@ class BeyondCompare4Beta < Cask
   version '4.0.0.17905'
   sha256 'f67490c52cf13343d9fda55f864519f94d13fe2c1faa8b81ee18ab026bee093e'
   link 'Beyond Compare.app'
+
+  after_install do
+    # Don't ask to move the app bundle to /Applications
+    system '/usr/bin/defaults', 'write', 'com.ScooterSoftware.BeyondCompare', 'moveToApplicationsFolderAlertSuppress', '-bool', 'true'
+  end
 end
