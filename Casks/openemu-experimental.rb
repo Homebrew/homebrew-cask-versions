eb7d63709553782eb03935230a62c10df4c4eac7
Vincent Klaiber <vincent@schimpanz.com>
2014-06-01 13:30:46 +0200

    Update OpenEmu Experimental to version 1.0.3

--- Casks/openemu-experimental.rb
+++ Casks/openemu-experimental.rb
@@ -1,7 +1,7 @@
 class OpenemuExperimental < Cask
-  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.2/OpenEmu_1.0.2-experimental.zip'
+  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.3/OpenEmu_1.0.3-experimental.zip'
   homepage 'http://openemu.org/'
-  version '1.0.2'
-  sha256 '0914ea5b43b38fdebbbae51f3e080f244882d99b9d0c90d1a592f5c2023df107'
+  version '1.0.3'
+  sha256 'cc5dce40a25ab224991ec0a8f772673983e81daa3b49f332e03d14e3b97d5b2e'
   link 'OpenEmu.app'
 end
