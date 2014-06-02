d70e0796253275417b93bfb430495d574182a4da
Radek Simko <radek.simko@gmail.com>
2014-06-02 21:11:03 +0100

    Java 6 links & checksum updated

--- Casks/java6.rb
+++ Casks/java6.rb
@@ -1,8 +1,8 @@
 class Java6 < Cask
-  url 'http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg'
-  homepage 'http://support.apple.com/downloads/DL1572'
+  url 'http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2014-001.dmg'
+  homepage 'http://support.apple.com/kb/DL1572'
   version '1.6.0_65'
-  sha256 '81e1155e44b2c606db78487ca1a02e31dbb3cfbf7e0581a4de3ded9e635a704e'
+  sha256 '97bc9b3c47af1f303710c8b15f2bcaedd6b40963c711a18da8eac1e49690a8a0'
   install 'JavaForOSX.pkg'
   uninstall :pkgutil => 'com.apple.pkg.JavaForMacOSX107'
 end
