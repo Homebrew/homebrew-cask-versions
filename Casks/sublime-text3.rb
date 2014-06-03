17a97e3055c9d358b7c61a507286e86ac896c354
Roland Walker <walker@pobox.com>
2014-06-02 23:13:40 -0400

    add appcast for sublime-text3.rb

--- Casks/sublime-text3.rb
+++ Casks/sublime-text3.rb
@@ -1,5 +1,6 @@
 class SublimeText3 < Cask
   url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
+  appcast 'http://www.sublimetext.com/updates/3/stable/appcast_osx.xml'
   homepage 'http://www.sublimetext.com/3'
   version 'Build 3059'
   sha256 '057c12afed359ea93f996b2e73973fa8ea2bfb14a3e61a5e13b467c5c38d4ca5'
