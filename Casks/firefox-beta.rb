51ccd377a608f49bcd21d07d9b15eb1a09efa6b9
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2014-06-13 14:51:19 +1000

    Updated multiple casks for Firefox beta localisations
    - English (US)
    - English (GB)
    - German (DE)
    - Simplified Chinese (CN)

--- Casks/firefox-beta.rb
+++ Casks/firefox-beta.rb
@@ -1,7 +1,7 @@
 class FirefoxBeta < Cask
-  url 'https://download.mozilla.org/?product=firefox-30.0b1&os=osx&lang=en-US'
+  url 'https://download.mozilla.org/?product=firefox-31.0b1&os=osx&lang=en-US'
   homepage 'https://www.mozilla.org/en-US/firefox/beta/'
-  version '30.0b1'
-  sha256 'fc27ec52d3f4c16537e87d909e7c5a7fa09c6e54143b63185e288bfddee3bfcf'
+  version '31.0b1'
+  sha256 '26bd7d38762b74323a6a3f7c4ac4e2480ab06097d1d8ebf8bfae98c2e3ffab1c'
   link 'Firefox.app'
 end
