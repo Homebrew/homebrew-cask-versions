cac9f68bd8c797ed9b7f3ae7936a75f5c415557a
Laurent Cozic <laurent@pogopixels.com>
2014-06-13 23:50:03 -0300

    Added Thunderbird FR

---
+++ Casks/thunderbird-fr.rb
@@ -0,0 +1,7 @@
+class ThunderbirdFr < Cask
+  url 'http://download-installer.cdn.mozilla.net/pub/thunderbird/releases/24.6.0/mac/fr/Thunderbird%2024.6.0.dmg'
+  homepage 'https://www.mozilla.org/fr/thunderbird/'
+  version '24.6.0'
+  sha256 'cf53ff2727d6699ed3b866794285bfd955af4fe177646455327eca78094a2d38'
+  link 'Thunderbird.app'
+end
