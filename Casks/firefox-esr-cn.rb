76a1c45d00061bff03ccbe576a3c6a4c0b53b96b
Havanna <registerdedicated@yeah.net>
2014-06-28 12:08:41 +0800

    Create firefox-esr-cn.rb

---
+++ Casks/firefox-esr-cn.rb
@@ -0,0 +1,7 @@
+class FirefoxEsrCn < Cask
+  url 'https://download.mozilla.org/?product=firefox-24.6.0esr&os=osx&lang=zh-CN'
+  homepage 'http://www.mozilla.org/en-US/firefox/organizations/faq/'
+  version '24.6.0'
+  sha256 '711b536b48c6e5a61480604e3b3555560d12638781a77f5f30b2e9cd6788b1db'
+  link 'Firefox.app'
+end
