a2244170a42b3a083b68fea318c35e4f7a339dea
goofansu <goofan.su@gmail.com>
2014-07-09 10:36:35 +0800

    Add Typinator 5.9

---
+++ Casks/typinator59.rb
@@ -0,0 +1,9 @@
+class Typinator59 < Cask
+  version '5.9'
+  sha256 '7513b11ba2f224a3ad2dd05a425e44b57a584d9d0a6ee1b30bc6f4a05ee7618d'
+
+  url 'http://www.ergonis.com/downloads/archive/typinator/Typinator59-Install.dmg'
+  homepage 'http://www.ergonis.com/products/typinator/'
+
+  link 'Typinator.app'
+end
