9a0e338d725e75204abc862194473aa884bbfe43
fainder <fainder.info@gmail.com>
2014-07-20 03:35:34 +0900

    Add Firefox 32.0a2 Japanese

---
+++ Casks/firefox-aurora-ja.rb
@@ -0,0 +1,7 @@
+class FirefoxAuroraJa < Cask
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora-l10n/firefox-32.0a2.ja-JP-mac.mac.dmg'
+  homepage 'https://www.mozilla.org/ja/firefox/channel/#aurora'
+  version '32.0a2'
+  sha256 'f234246291f2e5d8b6035caf9378682561deb3cb4711d8667f37f341328e3ccf'
+  link 'FirefoxAurora.app'
+end
