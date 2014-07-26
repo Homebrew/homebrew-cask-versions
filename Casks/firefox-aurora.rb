2dcc008a46c76cfb6e09719654e1f857fb4f43c7
Radek Simko <radek.simko@gmail.com>
2014-07-26 16:38:49 +0200

    Cask reformatted according to the latest recommedations

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,7 +1,9 @@
 class FirefoxAurora < Cask
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-32.0a2.en-US.mac.dmg'
-  homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
   version '32.0a2'
   sha256 :no_check
+
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-32.0a2.en-US.mac.dmg'
+  homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
+
   link 'FirefoxAurora.app'
 end
