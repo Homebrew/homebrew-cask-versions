65ee79db7448c0ee441df1e33c18a4fc201ac9aa
Radek Simko <radek.simko@gmail.com>
2014-07-26 16:44:35 +0200

    Firefox Aurora updated to v33.02a

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,8 +1,8 @@
 class FirefoxAurora < Cask
-  version '32.0a2'
-  sha256 :no_check
+  version '33.0a2'
+  sha256 'abfccdd97d9faa7a5f81c3d7e763054ed90c6a01e14cff309e1a94bc35090927'
 
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-32.0a2.en-US.mac.dmg'
+  url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
 
   link 'FirefoxAurora.app'
