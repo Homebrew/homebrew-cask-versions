b8fe505975acf7a7dfeb57af086d5b4da5cb2296
Djuri Baars <dsbaars@gmail.com>
2014-07-29 16:43:56 +0200

    Added Adobe Lightroom 4.4

---
+++ Casks/lightroom4.rb
@@ -0,0 +1,7 @@
+class Lightroom4 < Cask
+  url 'http://download.adobe.com/pub/adobe/lightroom/mac/4.x/Lightroom_4_LS11_mac_4_4.dmg'
+  homepage 'http://www.adobe.com/support/downloads/thankyou.jsp?ftpID=5566&fileID=5578'
+  version '4.4'
+  sha256 'fb94bd221041e2e846d9eb269587a179253b5e36560a20f499a5891f85e77aa3'
+  install 'Adobe Photoshop Lightroom 4.pkg'
+end
