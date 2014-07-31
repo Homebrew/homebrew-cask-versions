b95ac0ccd5cf1f40e0522e4ff555660c14f3e43e
Federico Bond <federicobond@gmail.com>
2014-07-31 17:44:25 -0300

    Update Processing3 to 3.0a2

--- Casks/processing3.rb
+++ Casks/processing3.rb
@@ -1,8 +1,8 @@
 class Processing3 < Cask
-  version '3.0a1'
-  sha256 '34347efaea73b510314ffaaae0a2cb8ce52e64a4004247b3ec8d56c80ae845bc'
+  version '3.0a2'
+  sha256 'abe0928faa6318ed7938a2012af5dabf60268b32e86e950a51b9859968f1725c'
 
-  url 'http://download.processing.org/processing-3.0a1-macosx.zip'
+  url 'http://download.processing.org/processing-3.0a2-macosx.zip'
   homepage 'http://processing.org/'
 
   link 'Processing.app'
