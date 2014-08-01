b0b2d909d93e6ed44bc95f14c12c879572b882df
Michael Ford <fanquake@users.noreply.github.com>
2014-08-01 09:39:01 +0800

    Arduino Beta 1.5.7-r2

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -1,7 +1,7 @@
 class ArduinoBeta < Cask
-  url 'http://downloads.arduino.cc/arduino-1.5.6-r2-macosx.zip'
+  url 'http://downloads.arduino.cc/arduino-1.5.7-macosx-java7-r2.zip'
   homepage 'http://arduino.cc/'
-  version '1.5.6-r2'
-  sha256 '8389fc8f2ec59e262ac04dbfd27ce0b61f26d6a4aa1bb3d6f9d8389c97e93d39'
+  version '1.5.7-r2'
+  sha256 'd58697e5b5b8877c5072e43f7f1c5a0fde3a917f864a231ef173b019774a552a'
   link 'Arduino.app'
 end
