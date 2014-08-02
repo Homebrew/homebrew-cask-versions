492d749575b8b51bdc4f8d11c986824efddf32aa
Roland Walker <walker@pobox.com>
2014-08-02 12:18:27 -0400

    update papers2 to use version 'latest'

--- Casks/papers2.rb
+++ Casks/papers2.rb
@@ -1,7 +1,7 @@
 class Papers2 < Cask
   url 'http://www.papersapp.com/papers2/download'
   homepage 'http://www.papersapp.com/mac/'
-  version '2'
+  version 'latest'
   sha256 :no_check
   link 'Papers2.app'
 end
