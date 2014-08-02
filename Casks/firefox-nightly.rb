8dfdcbdbadc9a1a03b687ff978e4939a65b1f9d2
Roland Walker <walker@pobox.com>
2014-08-02 12:18:49 -0400

    update firefox-nightly to use version 'latest'

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,7 +1,7 @@
 class FirefoxNightly < Cask
   url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-33.0a1.en-US.mac.dmg'
   homepage 'https://nightly.mozilla.org/'
-  version '33.0a1'
+  version 'latest'
   sha256 :no_check
   link 'FirefoxNightly.app'
 end
