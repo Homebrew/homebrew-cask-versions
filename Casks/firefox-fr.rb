a345ecb7525c40a2c515a26d90630856133446df
Roland Walker <walker@pobox.com>
2014-08-02 12:17:05 -0400

    typo: version 'latest'

--- Casks/firefox-fr.rb
+++ Casks/firefox-fr.rb
@@ -1,7 +1,7 @@
 class FirefoxFr < Cask
   url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=fr'
   homepage 'http://www.mozilla.org/fr/firefox/new/'
-  version 'lastest'
+  version 'latest'
   sha256 :no_check
   link 'Firefox.app'
 end
