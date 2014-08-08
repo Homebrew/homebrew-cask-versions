5f439291be4cc136dba84feae71373400486dbce
ndr <ndr-qef@users.noreply.github.com>
2014-08-08 03:51:00 +0200

    Change Google Chrome Canary homepage to HTTPS

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,6 +1,6 @@
 class GoogleChromeCanary < Cask
   url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
-  homepage 'http://www.google.com/chrome/browser/canary.html'
+  homepage 'https://www.google.com/chrome/browser/canary.html'
   version 'latest'
   sha256 :no_check
   link 'Google Chrome Canary.app'
