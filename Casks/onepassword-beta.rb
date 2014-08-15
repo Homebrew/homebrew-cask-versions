62ac4bd5b93a64dc8546ffb263c0934076025f0f
Les Aker <me@lesaker.org>
2014-08-15 14:56:16 -0400

    Update onepassword-beta.rb
    Use HTTPS and use correct application name

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -1,7 +1,7 @@
 class OnepasswordBeta < Cask
-  url 'http://aws.cachefly.net/dist/1P/mac4/1Password-4.3.1.BETA-7.zip'
+  url 'https://aws.cachefly.net/dist/1P/mac4/1Password-4.3.1.BETA-7.zip'
   homepage 'https://agilebits.com/onepassword'
   version '4.3.1.BETA-7'
   sha256 '350e60b2998812c1ea19e8959a7b9b615547011699a83f7295af37d8a3c85bbb'
-  link '1Password.app'
+  link '1Password 4.app'
 end
