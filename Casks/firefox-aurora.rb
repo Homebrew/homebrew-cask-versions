94bf680c2cc3326f73aa739609040852eb472ec1
Han Su Kim <han@edx.org>
2014-08-18 09:57:25 -0400

    adding no_check

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,6 +1,7 @@
 class FirefoxAurora < Cask
   version '33.0a2'
-
+  sha256 :no_check
+  
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
 
