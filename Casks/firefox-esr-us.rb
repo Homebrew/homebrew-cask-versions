b91af9ab64f32ec3886658e8d7c35221b2d3aec9
Michael Ford <fanquake@users.noreply.github.com>
2014-08-18 12:11:24 +0800

    Firefox ESR US 31.0

--- Casks/firefox-esr-us.rb
+++ Casks/firefox-esr-us.rb
@@ -1,7 +1,7 @@
 class FirefoxEsrUs < Cask
-  url 'https://download.mozilla.org/?product=firefox-24.6.0esr-SSL&os=osx&lang=en-US'
+  url 'https://download.mozilla.org/?product=firefox-31.0esr-SSL&os=osx&lang=en-US'
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/faq/'
-  version '24.6.0'
-  sha256 'f7813363017daa723b6adf6660d2866081e0915f32aca695e5f25961b5baee74'
+  version '31.0'
+  sha256 'a6fe27ebcafcf130aa906ab4a93a7898b81ea7d69f13cc24eeaba57a47fe8f4d'
   link 'Firefox.app'
 end
