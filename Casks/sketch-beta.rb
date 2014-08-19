59b105245e80533354ea44bf92221abb5eadc5dc
Vincent Klaiber <vincent@schimpanz.com>
2014-08-19 09:12:30 +0200

    Add Sketch Beta.app v3.1

---
+++ Casks/sketch-beta.rb
@@ -0,0 +1,9 @@
+class SketchBeta < Cask
+  version '3.1' 
+  sha256 'b567fe5ed071822d8917573d52e93acdc921dc924b77eee9ede2a41e88ce03ba'
+  
+  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/37?format=zip&avtoken=985ff642ddb110cfeb7998ffbc5993ef80bbfcf7'
+  homepage 'http://bohemiancoding.com/sketch/beta/'
+  
+  link 'Sketch Beta.app'
+end
