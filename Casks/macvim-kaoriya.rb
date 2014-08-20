ed7002197171103edbb1bfbc2a85877c140dbd77
Tsuyoshi Aizu <aizu@bascule.co.jp>
2014-08-20 13:01:50 +0900

    updated macvim-koriya to 7.4.383-20140805

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,7 +1,7 @@
 class MacvimKaoriya < Cask
-  url 'https://github.com/splhack/macvim/releases/download/20140107/macvim-kaoriya-20140107.dmg'
-  version '7.4.133-20140107'
+  url 'https://github.com/splhack/macvim/releases/download/20140805/macvim-kaoriya-20140805.dmg'
+  version '7.4.383-20140805'
   homepage 'https://code.google.com/p/macvim-kaoriya/'
-  sha256 '0a87ec0cb28e3b40eec6deb90e9e41ba44937f80de0c154cf80865414a6900b3'
+  sha256 'a47e42e32ddb3dd26e65e1ccfac532474da2a89fb516eb3a480b4f0ddabe4235'
   link 'MacVim.app'
 end
