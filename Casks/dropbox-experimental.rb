fd55a8ffc2b1f7db42a1b38a8136e4ea44f60099
chee <chee@fainder.info>
2014-08-21 10:34:30 +0900

    Upgrade Dropbox Experimental Build to v2.11.9

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -1,5 +1,5 @@
 class DropboxExperimental < Cask
-  url 'https://dl-web.dropbox.com/u/17/Dropbox%202.11.9.dmg'
+  url 'https://dl.dropboxusercontent.com/u/17/Dropbox%202.11.9.dmg'
   homepage 'https://www.dropbox.com/'
   version '2.11.9'
   sha256 'd1aa49f96196baf8370c2d89570fc49fce7f420a61c6345298187f3e74cacc8f'
