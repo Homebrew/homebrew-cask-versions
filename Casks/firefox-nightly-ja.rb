f08fd4269f66aefef582a60250972d440fa0bc64
fainder <chee@fainder.info>
2014-08-23 22:52:39 +0900

    Add Firefox Nightly Ja to v34.01a Cask

---
+++ Casks/firefox-nightly-ja.rb
@@ -0,0 +1,7 @@
+class FirefoxNightlyJa < Cask
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/firefox-34.0a1.ja-JP-mac.mac.dmg'
+  homepage 'https://nightly.mozilla.org/'
+  version 'latest'
+  sha256 :no_check
+  link 'FirefoxNightly.app'
+end
