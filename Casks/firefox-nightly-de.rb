64415badc163f26e1be3d67848baf2623ea3925d
fainder <chee@fainder.info>
2014-08-23 22:55:17 +0900

    Add Firefix Nightly De to v34.0a1 Cask

---
+++ Casks/firefox-nightly-de.rb
@@ -0,0 +1,7 @@
+class FirefoxNightlyDe < Cask
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/firefox-34.0a1.de.mac.dmg'
+  homepage 'https://nightly.mozilla.org/'
+  version 'latest'
+  sha256 :no_check
+  link 'FirefoxNightly.app'
+end
