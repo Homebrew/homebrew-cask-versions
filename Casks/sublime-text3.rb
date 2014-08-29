2fc5b41455f25449ffa6e1b0f440d84526a8ff24
wfdd <wfdd@users.noreply.github.com>
2014-08-29 11:54:35 +0300

    new sublime beta version

--- Casks/sublime-text3.rb
+++ Casks/sublime-text3.rb
@@ -1,9 +1,9 @@
 class SublimeText3 < Cask
-  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203059.dmg'
+  url 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg'
   appcast 'http://www.sublimetext.com/updates/3/stable/appcast_osx.xml'
   homepage 'http://www.sublimetext.com/3'
-  version 'Build 3059'
-  sha256 '057c12afed359ea93f996b2e73973fa8ea2bfb14a3e61a5e13b467c5c38d4ca5'
+  version 'Build 3065'
+  sha256 '373dbfcddab7fb288351763402361cd4045f4a9510eb32f81938d0c156d161c3'
   link 'Sublime Text.app'
   binary 'Sublime Text.app/Contents/SharedSupport/bin/subl'
   caveats do
