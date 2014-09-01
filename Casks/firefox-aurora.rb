1b4ccec691bc663773e5890c6da01d167155cb4d
Jason <jasonmichaelroth@gmail.com>
2014-09-01 08:14:54 -0600

    fix firefox-aurora hash
    Got this trying to install:
    
    $ brew cask install firefox-aurora
    ==> Downloading
    https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-33.0a2.en-US
    ==> Note: running "brew update" may fix checksum errors
    Error: SHA2 mismatch
    Expected: c20186fa1b548033e5e589b782449fcd5f48415597321ffe247d8189c70b4302
    Actual: 35045ef119d1779ce2a4639bd7219d41498aef8e58e011e8d1bdda93ba1bded0
    Archive: /Library/Caches/Homebrew/firefox-aurora-33.0a2.dmg
    To retry an incomplete download, remove the file above.
    
    Confirmed the sha against the download from the mozilla.org aurora download
    
    $ shasum -a 256 ~/Downloads/firefox-33.0a2.en-US.mac.dmg
    35045ef119d1779ce2a4639bd7219d41498aef8e58e011e8d1bdda93ba1bded0 /Users/jason/Downloads/firefox-33.0a2.en-US.mac.dmg

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,6 +1,6 @@
 class FirefoxAurora < Cask
   version '33.0a2'
-  sha256 'c20186fa1b548033e5e589b782449fcd5f48415597321ffe247d8189c70b4302'
+  sha256 '35045ef119d1779ce2a4639bd7219d41498aef8e58e011e8d1bdda93ba1bded0'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
