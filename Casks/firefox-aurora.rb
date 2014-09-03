ee1dc6d20171b71128fcd64bcc43ead5f4cab329
Michael Ford <fanquake@users.noreply.github.com>
2014-09-03 10:16:30 +0800

    Update Firefox Aurora SHA256
    Fixes #395

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,6 +1,6 @@
 class FirefoxAurora < Cask
   version '33.0a2'
-  sha256 'c20186fa1b548033e5e589b782449fcd5f48415597321ffe247d8189c70b4302'
+  sha256 'd40b6a98209169c44508473a60017ced554e7fb228651718b27e105661448954'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
