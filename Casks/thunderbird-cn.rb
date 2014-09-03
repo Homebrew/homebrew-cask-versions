e15dfc7e437222825eb133856086377131a31c49
Havanna <registerdedicated@yeah.net>
2014-09-03 10:25:27 +0800

    Upgrade thunderbird-cn to 31.1.0

--- Casks/thunderbird-cn.rb
+++ Casks/thunderbird-cn.rb
@@ -1,6 +1,6 @@
 class ThunderbirdCn < Cask
-  version '31.0'
-  sha256 'a3b9b3e2684ab8faed71606a7f8d76257bf8065518294ccd96b705201c14eb80'
+  version '31.1.0'
+  sha256 'e51ed574b980b6f5c4f5160e5c8b51a118a4474045918c276dc92d31e536fcb1'
   url "http://ftp.mozilla.org/pub/mozilla.org/thunderbird/releases/#{version}/mac/zh-CN/Thunderbird%20#{version}.dmg"
   homepage 'https://www.mozilla.org/zh-CN/thunderbird/'
   link 'Thunderbird.app'
