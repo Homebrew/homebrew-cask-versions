ef7f07ec658f081ed62e9380dc1549448b2554b7
Daniel Cardoso <danielcardosods@gmail.com>
2014-09-04 14:14:57 -0400

    Update firefox-nightly.rb

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,7 +1,9 @@
 class FirefoxNightly < Cask
-  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-34.0a1.en-US.mac.dmg'
-  homepage 'https://nightly.mozilla.org/'
-  version 'latest'
-  sha256 :no_check
+  version '35.0a1'
+  sha256 '57be19b8e36cf675fa9e6adc6e40e91de947a1dccdd585a9ef81f63176a8be6f'
+ 
+  url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-#{version}.en-US.mac.dmg"
+  homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
+   
   link 'FirefoxNightly.app'
 end
