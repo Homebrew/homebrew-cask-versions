3ae348325919c8ae80c4bf4836968d9d625a29e5
Michael Ford <fanquake@gmail.com>
2014-09-05 15:15:23 +0800

    Format Onyx Mountainlion

--- Casks/onyx-mountainlion.rb
+++ Casks/onyx-mountainlion.rb
@@ -1,7 +1,9 @@
 class OnyxMountainlion < Cask
-  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
-  homepage 'http://www.titanium.free.fr/downloadonyx.php'
   version '2.7.4'
   sha256 'e025125b06fc78322347f8549871c67e570131af83e8bb18b62ed43b65d7369d'
+
+  url 'http://joel.barriere.pagesperso-orange.fr/dl/108/OnyX.dmg'
+  homepage 'http://www.titanium.free.fr/downloadonyx.php'
+
   link 'OnyX.app'
 end
