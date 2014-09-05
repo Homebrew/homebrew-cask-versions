ee77d10fd6bb94a42e2de6980667d1fa37c981be
Michael Ford <fanquake@gmail.com>
2014-09-05 15:04:30 +0800

    Format IntelliJ IDEA Ultimate EAP

--- Casks/intellij-idea-ultimate-eap.rb
+++ Casks/intellij-idea-ultimate-eap.rb
@@ -1,7 +1,9 @@
 class IntellijIdeaUltimateEap < Cask
-  url 'http://download.jetbrains.com/idea/ideaIU-138.777.dmg'
-  homepage 'https://www.jetbrains.com/idea/index.html'
   version '138.777'
   sha256 '0653d9aca0a7a8e5cd296c0c1121319cb802e1307bd85ce6d11dab91b127ab64'
+
+  url 'http://download.jetbrains.com/idea/ideaIU-138.777.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+
   link 'IntelliJ IDEA 14 EAP.app'
 end
