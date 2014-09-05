dcebbe4e1c5e0e242c316caa350f9409c876b722
Michael Ford <fanquake@gmail.com>
2014-09-05 15:21:39 +0800

    Format Subethaedit3

--- Casks/subethaedit3.rb
+++ Casks/subethaedit3.rb
@@ -1,7 +1,9 @@
 class Subethaedit3 < Cask
-  url 'http://subethaedit.net/downloads/SubEthaEdit-354.zip'
-  homepage 'http://www.codingmonkeys.de/subethaedit/'
   version '3.5.4'
   sha256 '72d3948e5da167ac3113ec8f56aa424965b0ec153df4ba8042ce5893a547e290'
+
+  url 'http://subethaedit.net/downloads/SubEthaEdit-354.zip'
+  homepage 'http://www.codingmonkeys.de/subethaedit/'
+
   link 'SubEthaEdit.app'
 end
