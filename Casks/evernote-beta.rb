58ebfd6f18a16c962f25d9a89958523f1313cf69
Michael Ford <fanquake@gmail.com>
2014-09-05 14:44:51 +0800

    Format Evernote Beta

--- Casks/evernote-beta.rb
+++ Casks/evernote-beta.rb
@@ -1,7 +1,9 @@
 class EvernoteBeta < Cask
-  url 'http://cdn1.evernote.com/mac-smd/prerelease/Evernote_BETA_5.6.0_Beta_6_450650.zip'
-  homepage 'https://evernote.com/'
   version '5.6beta6'
   sha256 '4cdd361c873ce94ba5b3abfe98d967267644f99be700c1af1a2c56b2bfe06f54'
+
+  url 'http://cdn1.evernote.com/mac-smd/prerelease/Evernote_BETA_5.6.0_Beta_6_450650.zip'
+  homepage 'https://evernote.com/'
+
   link 'Evernote.app'
 end
