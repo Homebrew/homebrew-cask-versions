8bff8140c9b38406eb34decf1a321a8fc9d3768a
Michael Ford <fanquake@users.noreply.github.com>
2014-09-05 16:48:55 +0800

    Tunnelblick Beta 3.4beta36_r2945

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,6 +1,6 @@
 class TunnelblickBeta < Cask
-  version '3.4beta34_r2935'
-  sha256 '50d25744a73029fcfae3abe00bc3c7a056a938b6e6bd2084be75cdfe00d87bf7'
+  version '3.4beta36_r2945'
+  sha256 '36cb55bdb2ddb4460ef974e21d2a068c32e2f99598c4414251036b1601c3d3a8'
   
   url "http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg"
   homepage 'https://code.google.com/p/tunnelblick/'
