5851502e43b8a90073cb5a1ae1f69798569bfb37
Michael Ford <fanquake@gmail.com>
2014-09-05 15:15:03 +0800

    Format Onyx Lion

--- Casks/onyx-lion.rb
+++ Casks/onyx-lion.rb
@@ -1,7 +1,9 @@
 class OnyxLion < Cask
-  url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
-  homepage 'http://www.titanium.free.fr/downloadonyx.php'
   version '2.4.8'
   sha256 'e637359020d3514cf1fe40d1f616692919c41aed926a395bbc7093b256e42e35'
+
+  url 'http://www.titanium.free.fr/download/107/OnyX.dmg'
+  homepage 'http://www.titanium.free.fr/downloadonyx.php'
+
   link 'OnyX.app'
 end
