7cc4689ead209ee7a7bdc973160133d781d16403
Michael Ford <fanquake@gmail.com>
2014-09-05 14:41:52 +0800

    Format DeeperLion

--- Casks/deeper-lion.rb
+++ Casks/deeper-lion.rb
@@ -1,7 +1,9 @@
 class DeeperLion < Cask
-  url 'http://www.titanium.free.fr/download/107/Deeper.dmg'
-  homepage 'http://www.titanium.free.fr/downloaddeeper.php'
   version 'latest'
   sha256 :no_check
+
+  url 'http://www.titanium.free.fr/download/107/Deeper.dmg'
+  homepage 'http://www.titanium.free.fr/downloaddeeper.php'
+
   link 'Deeper.app'
 end
