c1d92b472f077bb6f8fb07450265653889262753
Michael Ford <fanquake@gmail.com>
2014-09-05 14:59:00 +0800

    Format FireFox Nightly

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,7 +1,9 @@
 class FirefoxNightly < Cask
-  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-34.0a1.en-US.mac.dmg'
-  homepage 'https://nightly.mozilla.org/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-34.0a1.en-US.mac.dmg'
+  homepage 'https://nightly.mozilla.org/'
+
   link 'FirefoxNightly.app'
 end
