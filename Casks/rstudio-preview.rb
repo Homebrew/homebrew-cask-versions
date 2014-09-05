e405d9f624bb28727c9c8e8714ce825824895cd9
Michael Ford <fanquake@gmail.com>
2014-09-05 15:20:08 +0800

    Format Rstudio Preview

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,7 +1,9 @@
 class RstudioPreview < Cask
-  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.932.dmg'
-  homepage 'http://www.rstudio.com/ide/download/preview'
   version '0.98.932'
   sha256 'ca652a4afd70eeb6cc7d5a016312ba535f0a000f97deec6e2367503045666c1e'
+
+  url 'https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-0.98.932.dmg'
+  homepage 'http://www.rstudio.com/ide/download/preview'
+
   link 'RStudio.app'
 end
