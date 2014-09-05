1a6ade67db32d430a9ad88825a679599422a6832
Michael Ford <fanquake@gmail.com>
2014-09-05 15:11:49 +0800

    Format Middleclick Snowleopard

--- Casks/middleclick-snowleopard.rb
+++ Casks/middleclick-snowleopard.rb
@@ -1,7 +1,9 @@
 class MiddleclickSnowleopard < Cask
-  url 'http://clement.beffa.org/labs/downloads/MiddleClick.zip'
-  homepage 'http://clement.beffa.org/labs/projects/middleclick'
   version 'latest'
   sha256 :no_check
+
+  url 'http://clement.beffa.org/labs/downloads/MiddleClick.zip'
+  homepage 'http://clement.beffa.org/labs/projects/middleclick'
+
   link 'MiddleClick.app'
 end
