bfdeb8d674978098cea0a5a7f97eeabd87c5e36b
Michael Ford <fanquake@gmail.com>
2014-09-05 15:25:38 +0800

    Format Utorrent Beta

--- Casks/utorrent-beta.rb
+++ Casks/utorrent-beta.rb
@@ -1,7 +1,9 @@
 class UtorrentBeta < Cask
-  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
-  homepage 'http://www.utorrent.com/'
   version 'latest'
   sha256 :no_check
+
+  url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
+  homepage 'http://www.utorrent.com/'
+
   link 'uTorrent.app'
 end
