402bd33f8ea8d39a52894c6c376c173daa6c025e
Jon Chen <bsd@voltaire.sh>
2014-09-05 08:51:55 -0400

    bump onepassword-beta to -19

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -1,8 +1,8 @@
 class OnepasswordBeta < Cask
-  version '4.5.BETA-18'
-  sha256 'cffff2f5c9b8ea62b851f39c8b5741044a7cfaa1861344af0bf67c2e604a28ee'
+  version '4.5.BETA-19'
+  sha256 '1d7669e4588aa0ca19f3fd5107a7f147067ddbd75a93deced4049d57fda4af3a'
 
-  url 'https://aws.cachefly.net/dist/1P/mac4/1Password-4.5.BETA-18.zip'
+  url 'https://aws.cachefly.net/dist/1P/mac4/1Password-4.5.BETA-19.zip'
   homepage 'https://agilebits.com/onepassword'
 
   link '1Password 4.app'
