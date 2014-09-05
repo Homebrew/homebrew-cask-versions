0aa39b26adbd87b3d98b9dd276a5aff54023d1d2
Michael Ford <fanquake@gmail.com>
2014-09-05 15:28:45 +0800

    Format Webkit Nightly

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -1,7 +1,9 @@
 class WebkitNightly < Cask
-  url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r159487.dmg'
-  homepage 'http://nightly.webkit.org/'
   version 'r159487'
   sha256 '6d59673fdac0c380f66c1da6ae5742377b68add2f0bb33737bd7e1cec7ba821c'
+
+  url 'http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r159487.dmg'
+  homepage 'http://nightly.webkit.org/'
+
   link 'WebKit.app'
 end
