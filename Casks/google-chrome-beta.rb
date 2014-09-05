1b89dc389dc5e86b26011a7b5cc7c70e4983b7c9
Michael Ford <fanquake@gmail.com>
2014-09-05 15:02:03 +0800

    Format Google Chrome Beta

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,7 +1,9 @@
 class GoogleChromeBeta < Cask
-  url 'http://dl.google.com/chrome/mac/beta/googlechrome.dmg'
-  homepage 'https://www.google.com/intl/en/chrome/browser/beta.html'
   version 'latest'
   sha256 :no_check
+
+  url 'http://dl.google.com/chrome/mac/beta/googlechrome.dmg'
+  homepage 'https://www.google.com/intl/en/chrome/browser/beta.html'
+
   link 'Google Chrome.app'
 end
