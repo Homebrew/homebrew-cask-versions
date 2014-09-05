bfc838fb9f26c202f70560db4088d99f4608de9a
Michael Ford <fanquake@gmail.com>
2014-09-05 15:04:04 +0800

    Format IntelliJ IDEA Ultimate12

--- Casks/intellij-idea-ultimate12.rb
+++ Casks/intellij-idea-ultimate12.rb
@@ -1,7 +1,9 @@
 class IntellijIdeaUltimate12 < Cask
-  url 'http://download.jetbrains.com/idea/ideaIU-12.1.7.dmg'
-  homepage 'https://www.jetbrains.com/idea/index.html'
   version '12.1.7'
   sha256 '5fd6130f66e2a64a4083c77e4dc1efde153bee333ce729553f39d0b106508466'
+
+  url 'http://download.jetbrains.com/idea/ideaIU-12.1.7.dmg'
+  homepage 'https://www.jetbrains.com/idea/index.html'
+
   link 'IntelliJ IDEA 12.app'
 end
