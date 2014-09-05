dee0314c7558a880683a37c4323dc82f4916043d
Michael Ford <fanquake@gmail.com>
2014-09-05 15:24:53 +0800

    Format Transmit3

--- Casks/transmit3.rb
+++ Casks/transmit3.rb
@@ -1,7 +1,9 @@
 class Transmit3 < Cask
-  url 'http://panic.com/museum/transmit/Transmit%203.7.zip'
-  homepage 'https://www.panic.com/transmit'
   version '3.7'
   sha256 '311f4d0f559c966fe7e5a4a6e0327dfa7a4c06a9c942ec11d58e6ed210f4b689'
+
+  url 'http://panic.com/museum/transmit/Transmit%203.7.zip'
+  homepage 'https://www.panic.com/transmit'
+
   link 'Transmit.app'
 end
