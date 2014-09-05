570bc7712fafe7090ab503d54f94d5992895d970
Michael Ford <fanquake@gmail.com>
2014-09-05 14:47:33 +0800

    Format FireFox Beta JA

--- Casks/firefox-beta-ja.rb
+++ Casks/firefox-beta-ja.rb
@@ -1,7 +1,9 @@
 class FirefoxBetaJa < Cask
-  url 'https://download.mozilla.org/?product=firefox-32.0b1&os=osx&lang=ja-JP-mac'
-  homepage 'http://www.mozilla.org/ja/firefox/channel/#beta'
   version '32.0b1'
   sha256 'ea4ef1545a6e628d81e4d391c95a648af3f8a0f164e615851d06b67bc266357a'
+
+  url 'https://download.mozilla.org/?product=firefox-32.0b1&os=osx&lang=ja-JP-mac'
+  homepage 'http://www.mozilla.org/ja/firefox/channel/#beta'
+
   link 'Firefox.app'
 end
