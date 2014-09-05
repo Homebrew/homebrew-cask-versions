bee739c152cc64d1884e54d1935a1fe6e22c2067
Michael Ford <fanquake@gmail.com>
2014-09-05 14:45:57 +0800

    Format FireFox Beta CN

--- Casks/firefox-beta-cn.rb
+++ Casks/firefox-beta-cn.rb
@@ -1,7 +1,9 @@
 class FirefoxBetaCn < Cask
-  url 'https://download.mozilla.org/?product=firefox-32.0b1&os=osx&lang=zh-CN'
-  homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
   version '32.0b1'
   sha256 '782e468b0277fd38f57205acf88f7dc3266b0f751ff40859d4772b8f22c7f13a'
+
+  url 'https://download.mozilla.org/?product=firefox-32.0b1&os=osx&lang=zh-CN'
+  homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
+
   link 'Firefox.app'
 end
