e94e066f7ec4001d801a235e811212ee680335d3
Michael Ford <fanquake@gmail.com>
2014-09-05 15:11:31 +0800

    Format Middleclick Mountainlion

--- Casks/middleclick-mountainlion.rb
+++ Casks/middleclick-mountainlion.rb
@@ -1,7 +1,9 @@
 class MiddleclickMountainlion < Cask
-  url 'http://clement.beffa.org/labs/downloads/MiddleClick_ml.zip'
-  homepage 'http://clement.beffa.org/labs/projects/middleclick'
   version 'latest'
   sha256 :no_check
+
+  url 'http://clement.beffa.org/labs/downloads/MiddleClick_ml.zip'
+  homepage 'http://clement.beffa.org/labs/projects/middleclick'
+
   link 'MiddleClick.app'
 end
