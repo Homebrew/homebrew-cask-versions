2c5b46f9868798044a6d0ba40595447f152d9c90
Michael Ford <fanquake@gmail.com>
2014-09-05 15:29:12 +0800

    Format Wimoweh Beta

--- Casks/wimoweh-beta.rb
+++ Casks/wimoweh-beta.rb
@@ -1,7 +1,9 @@
 class WimowehBeta < Cask
-  url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA_.zip'
-  homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
   version '0.66-beta'
   sha256 '8be363dc4764299df68220da67cf08d6f913fbf1bfe14059cd47bf99728d97fc'
+
+  url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA_.zip'
+  homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
+
   link 'Wimoweh.app'
 end
