afa8aa7861bad7233e0e0b463273bc01500c9483
Michael Ford <fanquake@gmail.com>
2014-09-05 15:09:39 +0800

    Format Lightroom3

--- Casks/lightroom3.rb
+++ Casks/lightroom3.rb
@@ -1,7 +1,9 @@
 class Lightroom3 < Cask
-  url 'http://download.adobe.com/pub/adobe/lightroom/mac/3.x/Lightroom_3_LS11_mac_3_3.dmg'
-  homepage 'http://www.adobe.com/support/downloads/thankyou.jsp?ftpID=4927&fileID=4588'
   version '3.3'
   sha256 '52b338d4a3d91d5979c8552da6ab8fdcfe4b4bcd3ad279f878f1fcd02d8b00b8'
+
+  url 'http://download.adobe.com/pub/adobe/lightroom/mac/3.x/Lightroom_3_LS11_mac_3_3.dmg'
+  homepage 'http://www.adobe.com/support/downloads/thankyou.jsp?ftpID=4927&fileID=4588'
+
   install 'Adobe Lightroom 3.pkg'
 end
