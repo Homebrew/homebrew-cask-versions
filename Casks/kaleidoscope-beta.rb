e8636130b30841d8a45d68f0950bbe023c791425
Michael Ford <fanquake@gmail.com>
2014-09-05 15:08:02 +0800

    Format Kaleidoscope Beta

--- Casks/kaleidoscope-beta.rb
+++ Casks/kaleidoscope-beta.rb
@@ -1,7 +1,9 @@
 class KaleidoscopeBeta < Cask
-  url 'http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.1.0-131.zip'
-  homepage 'http://www.kaleidoscopeapp.com/'
   version '2.1.0'
   sha256 '28605375d3a5ab8e4d65f5b03ccb8d0601ad1065abb33b02b3c4a879e39a7424'
+
+  url 'http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-2.1.0-131.zip'
+  homepage 'http://www.kaleidoscopeapp.com/'
+
   link 'Kaleidoscope.app'
 end
