58766ceb3fea572f6e566fc4fdf767ea3a415ce0
Michael Ford <fanquake@gmail.com>
2014-09-05 14:36:47 +0800

    Format Camtwist Beta

--- Casks/camtwist-beta.rb
+++ Casks/camtwist-beta.rb
@@ -1,7 +1,9 @@
 class CamtwistBeta < Cask
-  url 'http://camtwiststudio.com/beta/CamTwist_3.0.dmg'
-  homepage 'http://camtwiststudio.com/'
   version '3.0beta'
   sha256 'bef72de07368de34c58a3bca31efeac388f129c02661a3d62a745be17e729679'
+
+  url 'http://camtwiststudio.com/beta/CamTwist_3.0.dmg'
+  homepage 'http://camtwiststudio.com/'
+
   uninstall :script => { :executable => 'Manual Install/CamTwist/uninstall.command' }
 end
