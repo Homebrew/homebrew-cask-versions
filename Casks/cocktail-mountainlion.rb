ab0541b388704987e1eb62f1febcbe7aef573178
Michael Ford <fanquake@gmail.com>
2014-09-05 14:38:34 +0800

    Format Cocktail Mountain Lion

--- Casks/cocktail-mountainlion.rb
+++ Casks/cocktail-mountainlion.rb
@@ -1,7 +1,9 @@
 class CocktailMountainlion < Cask
-  url 'http://usa.maintain.se/CocktailMLE.dmg'
-  homepage 'http://maintain.se/cocktail'
   version 'latest'
   sha256 :no_check
+
+  url 'http://usa.maintain.se/CocktailMLE.dmg'
+  homepage 'http://maintain.se/cocktail'
+
   link 'Cocktail.app'
 end
