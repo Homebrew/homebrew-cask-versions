abc32d099aa43aa55fb1ce0e895104333da060ad
Michael Ford <fanquake@gmail.com>
2014-09-05 15:19:49 +0800

    Format Quicktime Player7

--- Casks/quicktime-player7.rb
+++ Casks/quicktime-player7.rb
@@ -1,8 +1,10 @@
 class QuicktimePlayer7 < Cask
-  url 'http://support.apple.com/downloads/DL923/en_US/QuickTimePlayer7.6.6_SnowLeopard.dmg'
-  homepage 'http://support.apple.com/kb/dl923'
   version '7.6.6'
   sha256 '704b5eee2ad953f301a652033db317e6997b9e5910db4b1f8d511b04ab62f2bc'
+
+  url 'http://support.apple.com/downloads/DL923/en_US/QuickTimePlayer7.6.6_SnowLeopard.dmg'
+  homepage 'http://support.apple.com/kb/dl923'
+
   install 'QuickTimePlayer7.6.6_SnowLeopard.pkg'
   uninstall :pkgutil => 'com.apple.pkg.QuickTimePlayer7.6.6_SnowLeopard',
             :files   => '/Applications/Utilities/QuickTime Player 7.app'
