cac456f608633c952beb8458341a1dc0e8145fe7
Michael Ford <fanquake@gmail.com>
2014-09-05 14:37:27 +0800

    Format Charles Open JDK

--- Casks/charles-openjdk.rb
+++ Casks/charles-openjdk.rb
@@ -1,7 +1,9 @@
 class CharlesOpenjdk < Cask
-  url 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-openjdk.dmg'
-  homepage 'http://www.charlesproxy.com/'
   version '3.9.2'
   sha256 '40e34c96ff6fff028031865b56d158225f17bf0758f4a2e7d0d28c92fb30504e'
+
+  url 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-openjdk.dmg'
+  homepage 'http://www.charlesproxy.com/'
+
   link 'Charles.app'
 end
