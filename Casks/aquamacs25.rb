1e759dadbf3edbd4698ebeb1f317dd35bf15666d
Michael Ford <fanquake@gmail.com>
2014-09-05 14:33:03 +0800

    Format Aquamacs25

--- Casks/aquamacs25.rb
+++ Casks/aquamacs25.rb
@@ -1,7 +1,9 @@
 class Aquamacs25 < Cask
-  url 'http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-2.5.dmg'
-  homepage 'http://aquamacs.org/'
   version '2.5'
   sha256 '5857848d8d46bba43d160c02393b098a370e2156608be24b288419f668210be9'
+
+  url 'http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-2.5.dmg'
+  homepage 'http://aquamacs.org/'
+
   link 'Aquamacs.app'
 end
