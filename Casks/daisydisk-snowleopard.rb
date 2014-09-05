c3b4ec66a7fbae9e80935523333a5708fe4111c3
Michael Ford <fanquake@gmail.com>
2014-09-05 14:41:29 +0800

    Format DaisyDisk Snowleopard

--- Casks/daisydisk-snowleopard.rb
+++ Casks/daisydisk-snowleopard.rb
@@ -1,7 +1,9 @@
 class DaisydiskSnowleopard < Cask
-  url 'http://www.daisydiskapp.com/downloads/DaisyDisk_2_1_2.dmg'
-  homepage 'http://www.daisydiskapp.com/'
   version '2.1.2'
   sha256 'd0a606dee19e524d6fa7b79fd48b3b9865123ca4126fb8805f8e96c317b57b31'
+
+  url 'http://www.daisydiskapp.com/downloads/DaisyDisk_2_1_2.dmg'
+  homepage 'http://www.daisydiskapp.com/'
+
   link 'DaisyDisk.app'
 end
