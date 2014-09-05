bcc707b4487dd62c43489a5229e0a4f938ce8e93
Michael Ford <fanquake@gmail.com>
2014-09-05 14:58:22 +0800

    Format FireFox FR

--- Casks/firefox-fr.rb
+++ Casks/firefox-fr.rb
@@ -1,7 +1,9 @@
 class FirefoxFr < Cask
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=fr'
-  homepage 'http://www.mozilla.org/fr/firefox/new/'
   version 'latest'
   sha256 :no_check
+
+  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=fr'
+  homepage 'http://www.mozilla.org/fr/firefox/new/'
+
   link 'Firefox.app'
 end
