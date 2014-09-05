887e20731e7fc8f84a901f918c41170a5afeea31
Michael Ford <fanquake@gmail.com>
2014-09-05 14:35:12 +0800

    Format Arq3

--- Casks/arq3.rb
+++ Casks/arq3.rb
@@ -1,7 +1,9 @@
 class Arq3 < Cask
-  url 'http://www.haystacksoftware.com/arq/Arq_3.3.4.zip'
-  homepage 'http://www.haystacksoftware.com/arq/'
   version '3.3.4'
   sha256 '2b4317b83d14090764f2134d4cc0fa3ee18b102974cff935f47a8afb4b3860a2'
+  
+  url 'http://www.haystacksoftware.com/arq/Arq_3.3.4.zip'
+  homepage 'http://www.haystacksoftware.com/arq/'
+
   link 'Arq.app'
 end
