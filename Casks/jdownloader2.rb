befbee4fb13cfbbf6f49465dc8fcbece8cf5f4cd
Michael Ford <fanquake@gmail.com>
2014-09-05 15:07:40 +0800

    Format JDownloader2

--- Casks/jdownloader2.rb
+++ Casks/jdownloader2.rb
@@ -1,12 +1,14 @@
 class Jdownloader2 < Cask
+  version 'latest'
+  sha256 :no_check
+
   if MacOS.version < :lion
     url 'http://installer.jdownloader.org/JD2SilentSetup_10_6orlower.dmg'
   else
     url 'http://installer.jdownloader.org/JD2SilentSetup.dmg'
   end
   homepage 'http://jdownloader.org/download/offline'
-  version 'latest'
-  sha256 :no_check
+
   caskroom_only true
   caveats do
     manual_installer 'JDownloader Installer.app'
