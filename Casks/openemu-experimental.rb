0652d7c4414b24789e13cd8f7d89a5f7f29cf8a4
Michael Ford <fanquake@gmail.com>
2014-09-05 15:16:02 +0800

    Format Openemu Experimental

--- Casks/openemu-experimental.rb
+++ Casks/openemu-experimental.rb
@@ -1,7 +1,9 @@
 class OpenemuExperimental < Cask
-  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.3/OpenEmu_1.0.3-experimental.zip'
-  homepage 'http://openemu.org/'
   version '1.0.3'
   sha256 'cc5dce40a25ab224991ec0a8f772673983e81daa3b49f332e03d14e3b97d5b2e'
+
+  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.3/OpenEmu_1.0.3-experimental.zip'
+  homepage 'http://openemu.org/'
+
   link 'OpenEmu.app'
 end
