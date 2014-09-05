b192aa3c81cdbc7ee1152037f7bf29c803bfda18
Michael Ford <fanquake@gmail.com>
2014-09-05 14:41:02 +0800

    Format Command Line Tools Mountain Lion

--- Casks/command-line-tools-mountain-lion.rb
+++ Casks/command-line-tools-mountain-lion.rb
@@ -1,7 +1,9 @@
 class CommandLineToolsMountainLion < Cask
-  url 'http://devimages.apple.com/downloads/xcode/command_line_tools_for_osx_mountain_lion_april_2014.dmg'
-  homepage 'https://developer.apple.com/xcode/downloads/'
   version 'mountain_lion_april_2014.dmg'
   sha256 '2ce8cb402efe7a1fe104759d9f32bed3c9b5e9f9db591f047702ae5dc7f3d1ac'
+
+  url 'http://devimages.apple.com/downloads/xcode/command_line_tools_for_osx_mountain_lion_april_2014.dmg'
+  homepage 'https://developer.apple.com/xcode/downloads/'
+
   link 'Command Line Tools (Mountain Lion).mpkg'
 end
