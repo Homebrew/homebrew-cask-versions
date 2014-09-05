64a971654c18e27bcff16fd25495b65b77375c72
Michael Ford <fanquake@gmail.com>
2014-09-05 14:36:06 +0800

    Format Beyond Compare 4 Beta

--- Casks/beyond-compare-4-beta.rb
+++ Casks/beyond-compare-4-beta.rb
@@ -1,8 +1,10 @@
 class BeyondCompare4Beta < Cask
-  url 'http://www.scootersoftware.com/BCompareOSX-4.0.0.17905.zip'
-  homepage 'http://www.scootersoftware.com/beta.php'
   version '4.0.0.17905'
   sha256 'f67490c52cf13343d9fda55f864519f94d13fe2c1faa8b81ee18ab026bee093e'
+
+  url 'http://www.scootersoftware.com/BCompareOSX-4.0.0.17905.zip'
+  homepage 'http://www.scootersoftware.com/beta.php'
+
   link 'Beyond Compare.app'
 
   after_install do
