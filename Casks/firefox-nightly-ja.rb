6b7559a863decbf993ceab1e6b3727a8fa0b7bc3
fainder <chee@fainder.info>
2014-09-06 23:05:10 +0900

    Upgrade Firefox Nightly Ja to v35.0a1

---
+++ Casks/firefox-nightly-ja.rb
@@ -0,0 +1,9 @@
+class FirefoxNightlyJa < Cask
+  version '35.0a1'
+  sha256 '9cde18aabb838684c41e124b50b67006e842f29b12d4f9fa9ab3c5fa964aa12f'
+
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/firefox-#{version}.ja-JP-mac.mac.dmg'
+  homepage 'https://nightly.mozilla.org/'
+
+  link 'FirefoxNightly.app'
+end
