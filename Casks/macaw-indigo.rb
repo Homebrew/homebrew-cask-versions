5f014270f6646a21c6379aaf2e8a8377a689b72b
Michael Ford <fanquake@users.noreply.github.com>
2014-09-07 12:23:08 +0800

    Macaw Indigo 1.5.4.0

--- Casks/macaw-indigo.rb
+++ Casks/macaw-indigo.rb
@@ -1,6 +1,6 @@
 class MacawIndigo < Cask
-  version '1.0.13.1'
-  sha256 'f523f371c1dd3ebfc3a083065c7056af3cb076ddb7c699c30a11627dba656892'
+  version '1.5.4.0'
+  sha256 'a98bc077cff020951fed2fc04e47b4182cd1c93b9882a53f89955b9b4f7a37f7'
 
   url "http://indigo.macaw.co/#{version}/MacawIndigo#{version}.dmg"
   appcast 'http://indigo.macaw.co/appcast.xml'
