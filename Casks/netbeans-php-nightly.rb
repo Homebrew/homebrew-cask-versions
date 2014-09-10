36f08309a3778154255b0762d38abde9bd25b611
Roland Walker <walker@pobox.com>
2014-09-10 11:37:18 -0400

    netbeans-php-nightly.rb

--- Casks/netbeans-php-nightly.rb
+++ Casks/netbeans-php-nightly.rb
@@ -4,7 +4,7 @@ class NetbeansPhpNightly < Cask
   sha256 '310e50c21614ec748ddbc3657b3dc51634ec5831ae28a18d76cff1c2c29d0088'
 
   url "http://bits.netbeans.org/download/trunk/nightly/latest/bundles/netbeans-trunk-nightly-#{version}-php-macosx.dmg"
-  install "NetBeans Dev #{version}.mpkg"
+  pkg "NetBeans Dev #{version}.mpkg"
 
   uninstall :files => [
                        '/Applications/NetBeans'
