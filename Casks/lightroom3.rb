763147d365685bd1adfbfebc5b7e062f8f646a48
Roland Walker <walker@pobox.com>
2014-09-10 11:37:18 -0400

    lightroom3.rb

--- Casks/lightroom3.rb
+++ Casks/lightroom3.rb
@@ -5,5 +5,5 @@ class Lightroom3 < Cask
   url 'http://download.adobe.com/pub/adobe/lightroom/mac/3.x/Lightroom_3_LS11_mac_3_3.dmg'
   homepage 'http://www.adobe.com/support/downloads/thankyou.jsp?ftpID=4927&fileID=4588'
 
-  install 'Adobe Lightroom 3.pkg'
+  pkg 'Adobe Lightroom 3.pkg'
 end
