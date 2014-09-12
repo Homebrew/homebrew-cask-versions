fd884b40fad73349aaff6161d38491457c8ff120
Daniel Perez Alvarez <unindented@gmail.com>
2014-09-12 12:49:34 +0100

    Update firefox-aurora to 34.0a2

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,6 +1,6 @@
 class FirefoxAurora < Cask
-  version '33.0a2'
-  sha256 'd40b6a98209169c44508473a60017ced554e7fb228651718b27e105661448954'
+  version '34.0a2'
+  sha256 '9509558af0d04f352487a08f236d3b9bfa72bc324e5ddd2c17147071efdd07d7'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
