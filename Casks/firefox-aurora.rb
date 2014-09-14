02797c4c93dd2d4e6e4c9d6d4ca5d9fc608ba578
Vítor Galvão <info@vitorgalvao.com>
2014-09-14 15:13:29 +0100

    Update firefox-aurora.rb

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,6 +1,6 @@
 class FirefoxAurora < Cask
   version '34.0a2'
-  sha256 '9509558af0d04f352487a08f236d3b9bfa72bc324e5ddd2c17147071efdd07d7'
+  sha256 'a9f79375224c8edd978478a7ce3c8633c51ec76afc67d507cecf17dc7056278c'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
