02b40f45f180d26ad6203b78df42852924fc3ebb
Roland Walker <walker@pobox.com>
2014-09-15 12:09:21 -0400

    app stanza in mucommander-nightly.rb

--- Casks/mucommander-nightly.rb
+++ Casks/mucommander-nightly.rb
@@ -5,5 +5,5 @@ class MucommanderNightly < Cask
   url 'http://www.mucommander.com/download/nightly/mucommander-current.app.tar.gz'
   homepage 'http://www.mucommander.com/index.php'
 
-  link 'muCommander-0_9_0/muCommander.app'
+  app 'muCommander-0_9_0/muCommander.app'
 end
