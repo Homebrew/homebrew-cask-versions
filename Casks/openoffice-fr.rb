ed36dc94b7bf356292e321ce4e5d544db013cd25
Roland Walker <walker@pobox.com>
2014-09-15 12:09:21 -0400

    app stanza in openoffice-fr.rb

--- Casks/openoffice-fr.rb
+++ Casks/openoffice-fr.rb
@@ -5,5 +5,5 @@ class OpenofficeFr < Cask
   url 'http://downloads.sourceforge.net/project/openofficeorg.mirror/4.0.1/binaries/fr/Apache_OpenOffice_4.0.1_MacOS_x86_install_fr.dmg'
   homepage 'http://www.openoffice.org/fr/Telecharger/'
 
-  link 'OpenOffice.app'
+  app 'OpenOffice.app'
 end
