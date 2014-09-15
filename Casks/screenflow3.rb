d14565cecd35a398f381271a5fc5e4a58daaa715
Roland Walker <walker@pobox.com>
2014-09-15 12:09:22 -0400

    app stanza in screenflow3.rb

--- Casks/screenflow3.rb
+++ Casks/screenflow3.rb
@@ -5,5 +5,5 @@ class Screenflow3 < Cask
   url 'http://www.telestream.net/download-files/screenflow/3-0/ScreenFlow-3.0.dmg'
   homepage 'http://www.telestream.net/screenflow/'
 
-  link 'ScreenFlow.app'
+  app 'ScreenFlow.app'
 end
