7caea8d543337042545616fa65a7e6749e78d301
Roland Walker <walker@pobox.com>
2014-09-15 12:09:22 -0400

    app stanza in subethaedit3.rb

--- Casks/subethaedit3.rb
+++ Casks/subethaedit3.rb
@@ -5,5 +5,5 @@ class Subethaedit3 < Cask
   url 'http://subethaedit.net/downloads/SubEthaEdit-354.zip'
   homepage 'http://www.codingmonkeys.de/subethaedit/'
 
-  link 'SubEthaEdit.app'
+  app 'SubEthaEdit.app'
 end
