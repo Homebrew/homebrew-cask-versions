760a118ab2abe38b346fa1bea747790a3cfa9bda
Roland Walker <walker@pobox.com>
2014-09-15 12:09:22 -0400

    app stanza in wimoweh-beta.rb

--- Casks/wimoweh-beta.rb
+++ Casks/wimoweh-beta.rb
@@ -5,5 +5,5 @@ class WimowehBeta < Cask
   url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA_.zip'
   homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
 
-  link 'Wimoweh.app'
+  app 'Wimoweh.app'
 end
