20c144764f6d4cec5514ebee68ccc98f33a4c18a
Michael Ford <fanquake@users.noreply.github.com>
2014-09-15 10:37:52 +0800

    Dropbox Experimental 2.11.13

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -1,8 +1,8 @@
 class DropboxExperimental < Cask
-  version '2.11.11'
-  sha256 '37db20d5cddb6d5e67b2150707092761d4de5c77c82e4fc0f887b1ffd897eca7'
+  version '2.11.13'
+  sha256 '2d36acd4a53657eca20538587c72df97685de39a9b0226f18797d464a7d84bdf'
 
-  url 'https://dl.dropboxusercontent.com/u/17/Dropbox%202.11.11.dmg'
+  url 'https://dl.dropboxusercontent.com/u/17/Dropbox%202.11.13.dmg'
   homepage 'https://www.dropbox.com/'
 
   link 'Dropbox.app'
