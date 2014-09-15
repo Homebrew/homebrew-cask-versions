5f717b193308f6394bee70717527ad7a333dd111
fainder <chee@fainder.info>
2014-09-16 05:33:18 +0900

    Add Clipmenu Development Release v0.4.4a12

---
+++ Casks/clipmenu-dev.rb
@@ -0,0 +1,9 @@
+class ClipmenuDev < Cask
+  version '0.4.4a12'
+  sha256 '05232fc411db989702527f1939b619df2482ed09c2a59363cc5a750e0f544652'
+
+  url 'http://dl.dropbox.com/u/1140644/clipmenu/ClipMenu_0.4.4a12.zip'
+  homepage 'http://www.clipmenu.com/'
+
+  link 'ClipMenu.app'
+end
