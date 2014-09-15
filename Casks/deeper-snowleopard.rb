6d4618d4b3032d2165b5a8b56b450fd02bf4f532
Roland Walker <walker@pobox.com>
2014-09-15 12:09:20 -0400

    app stanza in deeper-snowleopard.rb

--- Casks/deeper-snowleopard.rb
+++ Casks/deeper-snowleopard.rb
@@ -5,5 +5,5 @@ class DeeperSnowleopard < Cask
   url 'http://www.titanium.free.fr/download/106/Deeper.dmg'
   homepage 'http://www.titanium.free.fr/downloaddeeper.php'
 
-  link 'Deeper.app'
+  app 'Deeper.app'
 end
