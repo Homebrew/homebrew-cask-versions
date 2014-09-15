cad930f1d589737259833b2a02e5fcaf63c7d409
Roland Walker <walker@pobox.com>
2014-09-15 12:09:20 -0400

    app stanza in airmail-beta.rb

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -5,5 +5,5 @@ class AirmailBeta < Cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/beta/'
 
-  link 'AirMail Beta.app'
+  app 'AirMail Beta.app'
 end
