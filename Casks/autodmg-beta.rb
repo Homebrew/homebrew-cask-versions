6f68cad7252a96ac59e10a829244c5eaf2c4c971
Sebastian Nash <sebnash@gmail.com>
2014-09-16 12:02:57 +0500

    Add AutoDMG v1.5b359
    Added AutoDMG's beta version (Yosemite support).

---
+++ Casks/autodmg-beta.rb
@@ -0,0 +1,9 @@
+class AutodmgBeta < Cask
+  version '1.5b359'
+  sha256 'a0eb343fe44df8d347cbb29ab3f07f45847e649eeb295162787cf729e17d274e'
+
+  url 'https://github.com/MagerValp/AutoDMG/releases/download/v1.5b359/AutoDMG-1.5b359.dmg'
+  homepage 'https://github.com/MagerValp/AutoDMG'
+
+  app 'AutoDMG.app'
+end
