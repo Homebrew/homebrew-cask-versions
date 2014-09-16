4771a6c9c485c6f489417d3ba174a2e3d4a02be7
Roland Walker <walker@pobox.com>
2014-09-16 11:29:28 -0400

    cvt to app stanza, dash-beta

--- Casks/dash-beta.rb
+++ Casks/dash-beta.rb
@@ -5,5 +5,5 @@ class DashBeta < Cask
   url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/002/042/895/original/620a14dce3c435dc9f9d23e57d8ae668/Dash_Beta.app.zip'
   homepage 'http://kapeli.com/dash'
 
-  link 'Dash Beta.app'
+  app 'Dash Beta.app'
 end
