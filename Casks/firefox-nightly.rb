9a04ff588bfd390662512618d05bececead9cb62
Roland Walker <walker@pobox.com>
2014-09-17 07:43:45 -0400

    :latest as symbol, firefox-nightly

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,5 +1,5 @@
 class FirefoxNightly < Cask
-  version 'latest'
+  version :latest
   sha256 :no_check
 
   url 'http://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-35.0a1.en-US.mac.dmg'
