7cf42d50c035f940b33db1c53dd602d2b2ad11ed
Sebastian Nash <sebnash@gmail.com>
2014-09-22 16:13:23 +0500

    Add HandBrakeCLI Nightly Build
    Added HandBrakeCLI's nightly build (version '6419svn').

---
+++ Casks/handbrakecli-nightly.rb
@@ -0,0 +1,9 @@
+class HandbrakecliNightly < Cask
+  version '6419svn'
+  sha256 '3fcd67ddda23ba406789f0f00c9d501fd85a6c6348385ba6da8550db0fcfefb2'
+
+  url 'http://download.handbrake.fr/nightly/HandBrake-6419svn-MacOSX.6_CLI_x86_64.dmg'
+  homepage 'http://handbrake.fr'
+
+  binary 'HandBrakeCLI'
+end
