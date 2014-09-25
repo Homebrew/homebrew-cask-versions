ff4930a626e0ccc3d8edab6e1e06c3cec20be0f6
Sebastian Nash <sebnash@gmail.com>
2014-09-25 21:19:02 +0500

    Update 1Password to v4.5.BETA-24
    1Password v4.5.BETA-24 release 2014-09-25.

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -1,9 +1,9 @@
 class OnepasswordBeta < Cask
-  version '4.5.BETA-21'
-  sha256 '9074406e2cf7500cf7f6c58c557e42e9c861907cd26ecc3eea739ad4edf0ee7a'
+  version '4.5.BETA-24'
+  sha256 '3c6c468c2dd1df101f8055921999a5bae874269480bb5fd95a1153a1beab15b4'
 
-  url 'https://aws.cachefly.net/dist/1P/mac4/1Password-4.5.BETA-21.zip'
-  homepage 'https://agilebits.com/onepassword'
+  url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+  homepage 'https://agilebits.com/onepassword/mac'
 
   app '1Password 4.app'
 end
