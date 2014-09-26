cba6b0c0ea9a6dc8991f8457152ce48071468453
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in firefox-esr-us

--- Casks/firefox-esr-us.rb
+++ Casks/firefox-esr-us.rb
@@ -2,7 +2,7 @@ class FirefoxEsrUs < Cask
   version '31.0'
   sha256 'a6fe27ebcafcf130aa906ab4a93a7898b81ea7d69f13cc24eeaba57a47fe8f4d'
 
-  url 'https://download.mozilla.org/?product=firefox-31.0esr-SSL&os=osx&lang=en-US'
+  url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/faq/'
 
   app 'Firefox.app'
