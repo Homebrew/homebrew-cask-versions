caae10e3157aa3c57ecc2e4db87a2068c102eb9a
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in vagrant127

--- Casks/vagrant127.rb
+++ Casks/vagrant127.rb
@@ -2,7 +2,7 @@ class Vagrant127 < Cask
   version '1.2.7'
   sha256 '75c720eda0cbe6b2a2b19e38757ba4c34fbc57095ab4cc1459609bd242418129'
 
-  url 'http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/Vagrant-1.2.7.dmg'
+  url "http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/Vagrant-#{version}.dmg"
   homepage 'http://www.vagrantup.com'
 
   pkg 'Vagrant.pkg'
