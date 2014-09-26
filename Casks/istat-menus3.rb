b0a4e97fbc022d3f1355d65e8e008a89aa814f61
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in istat-menus3

--- Casks/istat-menus3.rb
+++ Casks/istat-menus3.rb
@@ -2,7 +2,7 @@ class IstatMenus3 < Cask
   version '3.27'
   sha256 '31b613aad5b4228087696f0c928a6018f8606534acbb3917e55635c51073bdf6'
 
-  url 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.27.zip'
+  url "http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus#{version}.zip"
   homepage 'http://bjango.com/mac/istatmenus/'
 
   app 'iStat Menus.app'
