cdeac07c90f040092cd77ef98be82c4b4e44ea76
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in dropbox-experimental

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -2,7 +2,7 @@ class DropboxExperimental < Cask
   version '2.11.13'
   sha256 '2d36acd4a53657eca20538587c72df97685de39a9b0226f18797d464a7d84bdf'
 
-  url 'https://dl.dropboxusercontent.com/u/17/Dropbox%202.11.13.dmg'
+  url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
   homepage 'https://www.dropbox.com/'
 
   app 'Dropbox.app'
