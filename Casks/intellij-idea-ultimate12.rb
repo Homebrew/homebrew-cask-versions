0d2bcac42d8edd86316d0ac54a57c93fe70ea68f
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in intellij-idea-ultimate12

--- Casks/intellij-idea-ultimate12.rb
+++ Casks/intellij-idea-ultimate12.rb
@@ -2,7 +2,7 @@ class IntellijIdeaUltimate12 < Cask
   version '12.1.7'
   sha256 '5fd6130f66e2a64a4083c77e4dc1efde153bee333ce729553f39d0b106508466'
 
-  url 'http://download.jetbrains.com/idea/ideaIU-12.1.7.dmg'
+  url "http://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   homepage 'https://www.jetbrains.com/idea/index.html'
 
   app 'IntelliJ IDEA 12.app'
