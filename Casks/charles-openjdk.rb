9502f972b41fa1b6994044a924f00e381316c775
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in charles-openjdk

--- Casks/charles-openjdk.rb
+++ Casks/charles-openjdk.rb
@@ -2,7 +2,7 @@ class CharlesOpenjdk < Cask
   version '3.9.2'
   sha256 '40e34c96ff6fff028031865b56d158225f17bf0758f4a2e7d0d28c92fb30504e'
 
-  url 'http://www.charlesproxy.com/assets/release/3.9.2/charles-proxy-3.9.2-openjdk.dmg'
+  url "http://www.charlesproxy.com/assets/release/#{version}/charles-proxy-#{version}-openjdk.dmg"
   homepage 'http://www.charlesproxy.com/'
 
   app 'Charles.app'
