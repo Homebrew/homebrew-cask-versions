cccea632abde5469acf237735b871b7f37b08a84
Roland Walker <walker@pobox.com>
2014-09-26 10:30:18 -0400

    whitespace nits in adium-beta

--- Casks/adium-beta.rb
+++ Casks/adium-beta.rb
@@ -1,7 +1,7 @@
 class AdiumBeta < Cask
   version '1.5.10b3'
   sha256 'c058d859cdf4df2703b47ad9be47f31dc8187306e2f7ebab3cdd5516e90123dd'
-  
+
   url "https://adiumx.cachefly.net/Adium_#{version}.dmg"
   homepage 'https://beta.adium.im/'
 
