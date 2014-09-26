0e949eba5db16dd715e255e60ca8c6e2c047395a
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in rubymine-eap

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -2,7 +2,7 @@ class RubymineEap < Cask
   version '138.1968'
   sha256 'ffefa3d5a547909fcd6b76f1a1172771fe28072c79b1d2a6fe180330a64af467'
 
-  url 'http://download.jetbrains.com/ruby/RubyMine-138.1968.dmg'
+  url "http://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   homepage 'http://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
 
   app 'RubyMine EAP.app'
