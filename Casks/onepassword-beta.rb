4d2e44d063620e4650cb0f935091ff3428c9771b
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in onepassword-beta

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -2,7 +2,7 @@ class OnepasswordBeta < Cask
   version '4.5.BETA-21'
   sha256 '9074406e2cf7500cf7f6c58c557e42e9c861907cd26ecc3eea739ad4edf0ee7a'
 
-  url 'https://aws.cachefly.net/dist/1P/mac4/1Password-4.5.BETA-21.zip'
+  url "https://aws.cachefly.net/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword'
 
   app '1Password 4.app'
