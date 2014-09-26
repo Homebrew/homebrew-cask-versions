a8058f576b17717c02bd698674f65963bd4cbd8c
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in thunderbird-fr

--- Casks/thunderbird-fr.rb
+++ Casks/thunderbird-fr.rb
@@ -2,7 +2,7 @@ class ThunderbirdFr < Cask
   version '24.6.0'
   sha256 'cf53ff2727d6699ed3b866794285bfd955af4fe177646455327eca78094a2d38'
 
-  url 'http://download-installer.cdn.mozilla.net/pub/thunderbird/releases/24.6.0/mac/fr/Thunderbird%2024.6.0.dmg'
+  url "http://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/fr/Thunderbird%20#{version}.dmg"
   homepage 'https://www.mozilla.org/fr/thunderbird/'
 
   app 'Thunderbird.app'
