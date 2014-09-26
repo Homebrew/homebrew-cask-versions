074285dd367e6216b94df5c36141d3cc9c063343
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in openemu-experimental

--- Casks/openemu-experimental.rb
+++ Casks/openemu-experimental.rb
@@ -2,7 +2,7 @@ class OpenemuExperimental < Cask
   version '1.0.3'
   sha256 'cc5dce40a25ab224991ec0a8f772673983e81daa3b49f332e03d14e3b97d5b2e'
 
-  url 'https://github.com/OpenEmu/OpenEmu/releases/download/v1.0.3/OpenEmu_1.0.3-experimental.zip'
+  url "https://github.com/OpenEmu/OpenEmu/releases/download/v#{version}/OpenEmu_#{version}-experimental.zip"
   homepage 'http://openemu.org/'
 
   app 'OpenEmu.app'
