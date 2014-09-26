03125c3033d8f41ff2303ca036a19feb24a0e1cb
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in thunderbird-beta

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -2,7 +2,7 @@ class ThunderbirdBeta < Cask
   version '33.0b1'
   sha256 '96db91d29cf5ec4e01e1c877eb804c9894e699e57c0d6d55219b1f53cbdb0dab'
 
-  url 'https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/33.0b1/mac/en-US/Thunderbird%2033.0b1.dmg'
+  url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg"
   homepage 'https://www.mozilla.org/en-US/thunderbird/all-beta.html'
 
   app 'Thunderbird.app'
