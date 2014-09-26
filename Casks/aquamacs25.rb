7e59801f1acfeda1cfcac1706ace8b3c65c6385d
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in aquamacs25

--- Casks/aquamacs25.rb
+++ Casks/aquamacs25.rb
@@ -2,7 +2,7 @@ class Aquamacs25 < Cask
   version '2.5'
   sha256 '5857848d8d46bba43d160c02393b098a370e2156608be24b288419f668210be9'
 
-  url 'http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-2.5.dmg'
+  url "http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-#{version}.dmg"
   homepage 'http://aquamacs.org/'
 
   app 'Aquamacs.app'
