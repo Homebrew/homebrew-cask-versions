e63cfb91681c2e2ddce15780ca6667c150be8f41
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in webstorm-eap

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -2,7 +2,7 @@ class WebstormEap < Cask
   version '138.1988'
   sha256 '869708d2d0ca44b9c9a39eaf633e13e759346cbafe0d101a8b0d29f739f40b54'
 
-  url 'http://download-cf.jetbrains.com/webstorm/WebStorm-EAP-138.1988.dmg'
+  url "http://download-cf.jetbrains.com/webstorm/WebStorm-EAP-#{version}.dmg"
   homepage 'http://confluence.jetbrains.com/display/WI/WebStorm+9+EAP'
 
   app 'Webstorm 9 EAP.app'
