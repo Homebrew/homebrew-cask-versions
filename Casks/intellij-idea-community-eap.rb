926de46c3ae3e637fad8af98fb9eae7b67affd10
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in intellij-idea-community-eap

--- Casks/intellij-idea-community-eap.rb
+++ Casks/intellij-idea-community-eap.rb
@@ -2,7 +2,7 @@ class IntellijIdeaCommunityEap < Cask
   version '138.2210.3'
   sha256 'b92a8b3d7cc2e569d35f2548fd2151e75e97cca08613957767d86664194e50cd'
 
-  url 'http://download.jetbrains.com/idea/ideaIC-138.2210.3.dmg'
+  url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   homepage 'https://www.jetbrains.com/idea/index.html'
 
   app 'IntelliJ IDEA 14 CE EAP.app'
