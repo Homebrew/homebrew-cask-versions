c0673455c1096dd37b3b61c28ecb635fca5fb4e0
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in zephyros52

--- Casks/zephyros52.rb
+++ Casks/zephyros52.rb
@@ -3,7 +3,7 @@ class Zephyros52 < Cask
   version '5.2'
   sha256 '7e6dc16d4639b27e0e3a20537bc6a36d0f11900027888d7b39d884d3668d04e3'
 
-  url 'https://raw.github.com/sdegutis/zephyros/master/Builds/Zephyros-5.2.app.tar.gz'
+  url "https://raw.github.com/sdegutis/zephyros/master/Builds/Zephyros-#{version}.app.tar.gz"
   homepage 'https://github.com/sdegutis/zephyros'
 
   app 'Zephyros.app'
