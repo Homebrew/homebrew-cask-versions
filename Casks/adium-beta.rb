c6f5f917d8c741c84a2af5f613130a6ebe094cb8
Roland Walker <walker@pobox.com>
2014-09-26 08:16:57 -0400

    re-use version in adium-beta

--- Casks/adium-beta.rb
+++ Casks/adium-beta.rb
@@ -2,7 +2,7 @@ class AdiumBeta < Cask
   version '1.5.10b3'
   sha256 'c058d859cdf4df2703b47ad9be47f31dc8187306e2f7ebab3cdd5516e90123dd'
   
-  url 'https://adiumx.cachefly.net/Adium_1.5.10b3.dmg'
+  url "https://adiumx.cachefly.net/Adium_#{version}.dmg"
   homepage 'https://beta.adium.im/'
 
   app 'Adium.app'
