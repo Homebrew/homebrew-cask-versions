1a759dc172bb399be449965a2b0058df95bcb8ab
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in quicktime-player7

--- Casks/quicktime-player7.rb
+++ Casks/quicktime-player7.rb
@@ -2,10 +2,10 @@ class QuicktimePlayer7 < Cask
   version '7.6.6'
   sha256 '704b5eee2ad953f301a652033db317e6997b9e5910db4b1f8d511b04ab62f2bc'
 
-  url 'http://support.apple.com/downloads/DL923/en_US/QuickTimePlayer7.6.6_SnowLeopard.dmg'
+  url "http://support.apple.com/downloads/DL923/en_US/QuickTimePlayer#{version}_SnowLeopard.dmg"
   homepage 'http://support.apple.com/kb/dl923'
 
-  pkg 'QuickTimePlayer7.6.6_SnowLeopard.pkg'
-  uninstall :pkgutil => 'com.apple.pkg.QuickTimePlayer7.6.6_SnowLeopard',
+  pkg "QuickTimePlayer#{version}_SnowLeopard.pkg"
+  uninstall :pkgutil => "com.apple.pkg.QuickTimePlayer#{version}_SnowLeopard",
             :delete  => '/Applications/Utilities/QuickTime Player 7.app'
 end
