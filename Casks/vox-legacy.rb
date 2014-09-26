9c33de786bba3fbe7570930f120c91f0df48ef62
Roland Walker <walker@pobox.com>
2014-09-26 08:16:58 -0400

    re-use version in vox-legacy

--- Casks/vox-legacy.rb
+++ Casks/vox-legacy.rb
@@ -2,7 +2,7 @@ class VoxLegacy < Cask
   version '0.3b2'
   sha256 '997b98a0b215ab1469fdfc0ce73764bd722fe944074ca8e61be6069dbecbe711'
 
-  url 'http://cloud.coppertino.com/vox/Vox_0.3b2.dmg'
+  url "http://cloud.coppertino.com/vox/Vox_#{version}.dmg"
   homepage 'http://coppertino.com/vox/'
 
   app 'Vox.app'
