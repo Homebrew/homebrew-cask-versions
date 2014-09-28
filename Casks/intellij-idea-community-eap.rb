1859548fe0e9d54251d7a4b2d52212377697e001
Julien Lavergne <lavergne.julien@gmail.com>
2014-09-29 00:33:45 +0200

    FIX - updated to last Idea Community Public Preview 138.2458.8

--- Casks/intellij-idea-community-eap.rb
+++ Casks/intellij-idea-community-eap.rb
@@ -2,7 +2,7 @@ class IntellijIdeaCommunityEap < Cask
   version '14-PublicPreview'
   sha256 'bcea58e2f59356e9b10e33d40adbcd7ec711bbd15c410d53f3a3695c517f2b67'
 
-  url 'http://download.jetbrains.com/idea/ideaIC-#{version}.dmg'
+  url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   homepage 'https://www.jetbrains.com/idea/index.html'
 
   app 'IntelliJ IDEA 14 CE EAP.app'
