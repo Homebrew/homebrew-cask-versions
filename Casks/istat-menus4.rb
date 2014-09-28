45d5dbcf1d36487d133b008fc9519689c87a56da
Vítor Galvão <info@vitorgalvao.com>
2014-09-28 15:05:22 +0100

    fixed istatmenu-4’s sha256
    Closes #442.

--- Casks/istat-menus4.rb
+++ Casks/istat-menus4.rb
@@ -1,6 +1,6 @@
 class IstatMenus4 < Cask
   version '4.22'
-  sha256 'b3157b6a5967a0612b1e2fc3eb961675fb7f85dd7966f5ab5f776f9b2cee51aa'
+  sha256 '4e6873b24d18578e359b9363f02b9c98bdeda999afab1aaa8802af254ecb35b6'
 
   url "http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip"
   homepage 'http://bjango.com/mac/istatmenus/'
