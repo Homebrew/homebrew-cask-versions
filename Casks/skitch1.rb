df61d14025ced6ed2f946d2cc29064aee2f19cee
Roland Walker <walker@pobox.com>
2014-10-01 09:03:04 -0400

    add license stanza, skitch1

--- Casks/skitch1.rb
+++ Casks/skitch1.rb
@@ -4,6 +4,7 @@ class Skitch1 < Cask
 
   url 'http://evernote.com/download/get.php?file=SkitchMac_v1'
   homepage 'http://blog.evernote.com/blog/2012/11/21/skitch-and-evernote-a-letter-from-keith-lang/'
+  license :unknown
 
   app 'Skitch.app'
 end
