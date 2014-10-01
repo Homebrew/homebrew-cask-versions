9803dfede3f95e813ace6397053157e7cf835e18
Roland Walker <walker@pobox.com>
2014-10-01 09:03:03 -0400

    add license stanza, firefox-ja

--- Casks/firefox-ja.rb
+++ Casks/firefox-ja.rb
@@ -4,6 +4,7 @@ class FirefoxJa < Cask
 
   url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=ja-JP-mac'
   homepage 'http://www.mozilla.jp/firefox/'
+  license :oss
 
   app 'Firefox.app'
 end
