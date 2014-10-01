c44a1f2343941db9db43cc8bb6af7bd38f13f382
Roland Walker <walker@pobox.com>
2014-10-01 09:03:02 -0400

    add license stanza, deeper-lion

--- Casks/deeper-lion.rb
+++ Casks/deeper-lion.rb
@@ -4,6 +4,7 @@ class DeeperLion < Cask
 
   url 'http://www.titanium.free.fr/download/107/Deeper.dmg'
   homepage 'http://www.titanium.free.fr/downloaddeeper.php'
+  license :unknown
 
   app 'Deeper.app'
 end
