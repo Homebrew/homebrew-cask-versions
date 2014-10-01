ff60c622e2a4dc8f283cd38f50640d9fdda7b0ff
Roland Walker <walker@pobox.com>
2014-10-01 09:03:05 -0400

    add license stanza, utorrent-beta

--- Casks/utorrent-beta.rb
+++ Casks/utorrent-beta.rb
@@ -4,6 +4,7 @@ class UtorrentBeta < Cask
 
   url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
   homepage 'http://www.utorrent.com/'
+  license :unknown
 
   app 'uTorrent.app'
 end
