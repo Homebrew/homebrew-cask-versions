8084ab12a11c9123134c3e280975667cdf625e5e
Roland Walker <walker@pobox.com>
2014-10-01 09:03:04 -0400

    add license stanza, sketchup-pro

--- Casks/sketchup-pro.rb
+++ Casks/sketchup-pro.rb
@@ -5,6 +5,7 @@ class SketchupPro < Cask
   # downloads can be found at http://www.sketchup.com/download/all
   url 'https://dl.trimble.com/sketchup/SketchUpPro-en.dmg'
   homepage 'http://www.sketchup.com/intl/en/'
+  license :unknown
 
   suite 'SketchUp 2014'
 end
