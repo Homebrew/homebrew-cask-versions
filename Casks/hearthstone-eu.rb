734b6065674a999d623bf514fc30d3ade1ac5462
Roland Walker <walker@pobox.com>
2014-10-01 09:03:03 -0400

    add license stanza, hearthstone-eu

--- Casks/hearthstone-eu.rb
+++ Casks/hearthstone-eu.rb
@@ -4,6 +4,7 @@ class HearthstoneEu < Cask
 
   url 'http://dist.blizzard.com/downloads/hs-installers/a6029a1d625c79252defff3914fb6e67/retail.1/Hearthstone-Setup-enGB.zip'
   homepage 'http://eu.battle.net/hearthstone/en/'
+  license :unknown
 
   caveats do
     manual_installer 'Hearthstone-Setup-enGB.app'
