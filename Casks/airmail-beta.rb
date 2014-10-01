3edadc1b40565ea01284088934bf119d7d6d7f73
Roland Walker <walker@pobox.com>
2014-10-01 09:03:02 -0400

    add license stanza, airmail-beta

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -4,6 +4,7 @@ class AirmailBeta < Cask
 
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/beta/'
+  license :unknown
 
   app 'AirMail Beta.app'
 end
