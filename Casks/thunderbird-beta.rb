490ba43a9627d253e6fc4d49fe7003c9145e0c0e
Henry Tian <htian@live.com>
2014-10-02 18:28:48 -0500

    Update Thunderbird Beta to 33.0b1 (Sep 24, 2014 version)

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -1,6 +1,6 @@
 class ThunderbirdBeta < Cask
   version '33.0b1'
-  sha256 '96db91d29cf5ec4e01e1c877eb804c9894e699e57c0d6d55219b1f53cbdb0dab'
+  sha256 '45549be5ddf4aaf7c50e08fa71dd8e5542cb36c8ca19498866cd0b8ebb3b6385'
 
   url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg"
   homepage 'https://www.mozilla.org/en-US/thunderbird/all-beta.html'
