4d821ba0bca12563cf2de38655490c3280034781
fainder <chee@fainder.info>
2014-10-03 09:54:31 +0900

    Fix checksum in Firefox nightly ja

--- Casks/firefox-nightly-ja.rb
+++ Casks/firefox-nightly-ja.rb
@@ -1,6 +1,6 @@
 class FirefoxNightlyJa < Cask
   version '35.0a1'
-  sha256 '9cde18aabb838684c41e124b50b67006e842f29b12d4f9fa9ab3c5fa964aa12f'
+  sha256 '0ae86c7ff262e234a2dbe59cd5fefdb5e6f646cce08b2c9cb9abc3e533ab26a4'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/firefox-#{version}.ja-JP-mac.mac.dmg"
   homepage 'https://nightly.mozilla.org/'
