610a6ee3bf0a987aca4e56aef837d269c935052e
Kir Belevich <kir@soulshine.in>
2014-10-05 23:03:26 +0700

    Upgrade 1Password 4.app to v4.5.BETA-26

--- Casks/onepassword-beta.rb
+++ Casks/onepassword-beta.rb
@@ -1,6 +1,6 @@
 class OnepasswordBeta < Cask
-  version '4.5.BETA-25'
-  sha256 'fca119b0614da273a3b2e7d101bae9a9dec3df106ad4bc99d0f14e2ade99e0e2'
+  version '4.5.BETA-26'
+  sha256 '11ff41a61fcf52061dd1fcbdcb19725d8b0ec171efb479f5bdbfbb87fb68233e'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   homepage 'https://agilebits.com/onepassword/mac'
