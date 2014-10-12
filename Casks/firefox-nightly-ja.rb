9c10c7d307eada13eb3af1813ac13281524095cf
fainder <chee@fainder.info>
2014-10-12 22:24:31 +0900

    Update checksum to Firefox nightly ja

--- Casks/firefox-nightly-ja.rb
+++ Casks/firefox-nightly-ja.rb
@@ -1,6 +1,6 @@
 class FirefoxNightlyJa < Cask
   version '35.0a1'
-  sha256 '0ae86c7ff262e234a2dbe59cd5fefdb5e6f646cce08b2c9cb9abc3e533ab26a4'
+  sha256 'e024fd2ddc17cec0cdf8969b9128423209b54a8833245044ca8548a9fce7e878'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/firefox-#{version}.ja-JP-mac.mac.dmg"
   homepage 'https://nightly.mozilla.org/'
