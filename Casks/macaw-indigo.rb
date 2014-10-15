35e43be5d1184ca8bc9878c933f6bc3d0ea3cd5c
Roland Walker <walker@pobox.com>
2014-10-15 11:49:58 -0400

    add appcast :sha256 in macaw-indigo

--- Casks/macaw-indigo.rb
+++ Casks/macaw-indigo.rb
@@ -3,7 +3,8 @@ class MacawIndigo < Cask
   sha256 'a98bc077cff020951fed2fc04e47b4182cd1c93b9882a53f89955b9b4f7a37f7'
 
   url "http://indigo.macaw.co/#{version}/MacawIndigo#{version}.dmg"
-  appcast 'http://indigo.macaw.co/appcast.xml'
+  appcast 'http://indigo.macaw.co/appcast.xml',
+          :sha256 => 'ff896638f2868f4d670176611abfc284dbe935ab7264574f57845b202d348617'
   homepage 'http://indigo.macaw.co/'
   license :unknown
 
