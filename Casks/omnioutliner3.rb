d5abb3313a56eeb8df8a74a29d0a490900ad27ea
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2014-10-18 12:11:23 +0200

    Add OmniOutliner Standard v3

---
+++ Casks/omnioutliner3.rb
@@ -0,0 +1,10 @@
+class Omnioutliner3 < Cask
+  version :latest
+  sha256 :no_check
+
+  url 'http://omnigroup.com/download/latest/omnioutliner-3'
+  homepage 'http://www.omnigroup.com/omnioutliner/'
+  license :closed
+
+  app 'OmniOutliner.app'
+end
