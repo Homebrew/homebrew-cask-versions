530511af5900eb95cc5e0aa677e9909ed4525230
Hannes Van De Vreken <vandevreken.hannes@gmail.com>
2014-10-18 18:21:47 +0200

    New version op PHPStorm available

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 class PhpstormEap < Cask
-  version '138.2502'
-  sha256 '61c1872a4e487f955956cba93eb8a2f5f743c8dd1aff1eac1c96555b306cdb81'
+  version '139.105'
+  sha256 '9821ed1d1728fdcc833e896d7f2802643edd1707'
 
   url "http://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg"
   homepage 'http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
