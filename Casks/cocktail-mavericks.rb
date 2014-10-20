48a5c7e089d1082b26f223d718006a13d8f672dd
Felix Zandanel <felix@zandanel.me>
2014-10-20 23:02:12 +0200

    Add cocktail v7.6.1 (Mavericks edition)

---
+++ Casks/cocktail-mavericks.rb
@@ -0,0 +1,10 @@
+class CocktailMavericks < Cask
+  version '7.6.1'
+  sha256 :no_check
+
+  url 'http://usa.maintain.se/CocktailME.dmg'
+  homepage 'http://maintain.se/cocktail'
+  license :unknown
+
+  app 'Cocktail.app'
+end
