a888ddb476e9313b32832db666881d4207981716
elyase <yaser.martinez@gmail.com>
2014-10-21 18:51:37 +0200

    Update rstudio-preview to version 0.98.1079

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 class RstudioPreview < Cask
-  version '0.98.932'
-  sha256 'ca652a4afd70eeb6cc7d5a016312ba535f0a000f97deec6e2367503045666c1e'
+  version '0.98.1079'
+  sha256 'f504400b9cb6d01f7504beedc6ad41b1b10ca8bab8a238eacfe3989cce0b0c9e'
 
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
   homepage 'http://www.rstudio.com/ide/download/preview'
