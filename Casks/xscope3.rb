5e574d5add8f487aea187664e884887d5aad5429
Lincoln Lee <git@lincoln.hk>
2014-10-23 15:47:57 +0800

    Add Xscope 3

---
+++ Casks/xscope3.rb
@@ -0,0 +1,10 @@
+class Xscope3 < Cask
+  version '3.6.3'
+  sha256 '1e5e46f50ecd81e35cf819fbd04b2c9726beddc24e86cdd2f6ae5dc60aeb4e4b'
+
+  url 'https://iconfactory.com/assets/software/xscope/xScope-3.6.3.zip'
+  appcast 'http://iconfactory.com/appcasts/xScope/appcast.xml'
+  homepage 'http://iconfactory.com/software/xscope'
+
+  app 'xScope.app'
+end
