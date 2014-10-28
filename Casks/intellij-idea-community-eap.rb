e1a7a0d60a7c478f614cca527ed219499526110f
Alex Varju <alex@varju.ca>
2014-10-27 21:45:59 -0700

    Upgrade IntelliJ CE EAP to 14 RC1 (139.144.2)

--- Casks/intellij-idea-community-eap.rb
+++ Casks/intellij-idea-community-eap.rb
@@ -1,9 +1,9 @@
 class IntellijIdeaCommunityEap < Cask
-  version '14'
-  sha256 'bcea58e2f59356e9b10e33d40adbcd7ec711bbd15c410d53f3a3695c517f2b67'
+  version '139.144.2'
+  sha256 '4f06f206d4e3435d0d2b2c803637db7645ab8c762fbdbd6038384971074e16b8'
 
-  url "http://download.jetbrains.com/idea/ideaIC-#{version}-PublicPreview.dmg"
-  homepage 'https://www.jetbrains.com/idea/index.html'
+  url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
+  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14+EAP'
 
-  app "IntelliJ IDEA #{version} CE EAP.app"
+  app 'IntelliJ IDEA 14 CE EAP.app'
 end
