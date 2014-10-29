2396cc79986e382fc62bdaa9e572942405355e21
Alex Varju <alex@varju.ca>
2014-10-29 15:36:29 -0700

    Update IntelliJ CE EAP to 139.222.5

--- Casks/intellij-idea-community-eap.rb
+++ Casks/intellij-idea-community-eap.rb
@@ -1,6 +1,6 @@
 class IntellijIdeaCommunityEap < Cask
-  version '139.144.2'
-  sha256 '4f06f206d4e3435d0d2b2c803637db7645ab8c762fbdbd6038384971074e16b8'
+  version '139.222.5'
+  sha256 '67435b03dc9fe08753823235c089e367d989ef62c61ee35e104cf036fbebe885'
 
   url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14+EAP'
