f79e8446c7b27ec4e4dc3f3fa4243bed83314bd5
Vincent Klaiber <vincent@schimpanz.com>
2014-10-30 15:51:11 +0100

    Update Sketch Beta.app to version 3.2

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,8 +1,8 @@
 class SketchBeta < Cask
-  version '3.1.1'
-  sha256 '4d4785f3356079f224bd64302ab17e651ba00752e364f08f146e67196a0a0fbc'
+  version '3.2'
+  sha256 'b6211a6d9d80ace4e9b315fa676800bebbf502c6da5541e473c21e0105f1a153'
 
-  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/49?format=zip&avtoken=2ab011a5b37200a2947be20cb2dd21af7af9265c'
+  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/52?format=zip&avtoken=0073114b9e08a31f185c4d0ebf024275946fd465'
   homepage 'http://bohemiancoding.com/sketch/beta/'
   license :unknown
 
