7e38c58a83ec655baf32736dc2ec6bb7a4329fba
Maurice Kelly <osv-github@chatswood.org.uk>
2014-10-30 10:56:12 +0000

    Added AppCode EAP as 3.1.0

---
+++ Casks/appcode-eap.rb
@@ -0,0 +1,10 @@
+class AppcodeEap < Cask
+  version '3.1.0'
+  sha256 'e642a32107c9b6f0d2a9923ece6c50875f0fc66f6341ca8765af4c1696d65f65'
+
+  url 'http://download.jetbrains.com/objc/AppCode-139.261.dmg'
+  homepage 'http://confluence.jetbrains.com/display/OBJC/AppCode+EAP'
+  license :commercial
+
+  app 'AppCode EAP.app'
+end
