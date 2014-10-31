9357f0634ee57f837f3350b1011ee40dfdeca4d4
Josh Butts <josh@joshbutts.com>
2014-10-30 21:21:48 -0500

    Remove sha check and version from FirefoxAurora
    Sha is likely to change without notice even for the same dmg filename (was currently broken because of this)

--- Casks/firefox-aurora.rb
+++ Casks/firefox-aurora.rb
@@ -1,8 +1,8 @@
 class FirefoxAurora < Cask
-  version '35.0a2'
-  sha256 'c27899ebdb9d3b44ccac8ad904a03b421b65208e66937c62d795aab1d0014999'
+  version :latest
+  sha256 :no_check
 
-  url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
+  url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-35.0a2.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/aurora/'
   license :oss
 
