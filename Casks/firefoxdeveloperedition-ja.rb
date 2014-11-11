ec04d8c19e29444e7704458bc28e85e93082f272
zchee <chee@zsh.us>
2014-11-12 03:00:26 +0900

    Upgrade Firefox Dev Edition to 35.0a2

--- Casks/firefoxdeveloperedition-ja.rb
+++ Casks/firefoxdeveloperedition-ja.rb
@@ -1,6 +1,6 @@
 class FirefoxdevelopereditionJa < Cask
-  version '33.0a2'
-  sha256 'ea56c43a6e93789765af356ce812c64a51cb9d0da33d21bef6d4aed54428a1a7'
+  version '35.0a2'
+  sha256 '3f990fbf7fe911efd27aaa22fae6b7e42f5f132792b08efc90ffa453fb2b6d78'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora-l10n/firefox-#{version}.ja-JP-mac.mac.dmg"
   homepage 'https://www.mozilla.org/ja/firefox/developer/'
