47a711ec7614925da81216e9da0540596a0589d1
kTitan <alexander.schneider@gmail.com>
2014-11-11 16:06:10 +0100

    Upgrade dropbox-experimental to v2.11.37

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -1,6 +1,6 @@
 class DropboxExperimental < Cask
-  version '2.11.31'
-  sha256 '70d2f24b776c330678573b5a04362c066ac3ab6e17d05ca34c21e8bccb5da898'
+  version '2.11.37'
+  sha256 '259f288f5c117d261994f8041c993befe4206e9029eb79da8be3a564b045db49'
 
   url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
   homepage 'https://www.dropbox.com/'
