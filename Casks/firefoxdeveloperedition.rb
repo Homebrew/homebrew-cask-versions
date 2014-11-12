779b5b1cf43edf8db2bc14f6da9277f979181cde
Constantine Romanov <senz@users.noreply.github.com>
2014-11-12 19:02:51 +0300

    Update firefoxdeveloperedition.rb
    Checksum changed once again

--- Casks/firefoxdeveloperedition.rb
+++ Casks/firefoxdeveloperedition.rb
@@ -1,6 +1,6 @@
 class Firefoxdeveloperedition < Cask
   version '35.0a2'
-  sha256 'f4fad137c894c0a1824195717a1252757e1fbb65192d4bb8ec8fc77083edb175'
+  sha256 '046bd7e7018c5569202abfd82b006a3e999ee2650b704dcc229dce4b85054fd0'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/developer/'
