18e95bcbbf8276649396de4527ea1c84dcde4e4b
Roland Walker <walker@pobox.com>
2014-11-13 12:25:40 -0500

    new-style header in keyremap4macbook-mountainlion

--- Casks/keyremap4macbook-mountainlion.rb
+++ Casks/keyremap4macbook-mountainlion.rb
@@ -1,4 +1,4 @@
-class Keyremap4macbookMountainlion < Cask
+cask :v1 => 'keyremap4macbook-mountainlion' do
   version '8.4.0'
   sha256 '39fb33460f1abec99235c2095bf766d112cc62204c944cc43fd46f3cdb5cc099'
 
