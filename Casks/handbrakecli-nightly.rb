7009d794122798d8d7af9735646f01f872223bbd
Vítor Galvão <info@vitorgalvao.com>
2014-11-14 17:16:11 +0000

    updated handbrakecli-nightly to 6509svn

--- Casks/handbrakecli-nightly.rb
+++ Casks/handbrakecli-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'handbrakecli-nightly' do
-  version '6507svn'
-  sha256 'a68c96f1e4fdfd9207f319d69831d5f0499ad67df240314dfc0d7e51fe3a10fe'
+  version '6509svn'
+  sha256 '5beb028c999cebf8112413c46821b348d2584139fbe2070704522ade45c44e63'
 
   url "http://download.handbrake.fr/nightly/HandBrake-#{version}-MacOSX.6_CLI_x86_64.dmg"
   homepage 'http://handbrake.fr'
