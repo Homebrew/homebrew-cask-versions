3238d2b52e09770c0523bc70fbf5ef88696ea151
Vítor Galvão <info@vitorgalvao.com>
2014-11-14 17:35:27 +0000

    Update arduino-beta.rb

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'arduino-beta' do
-  version '1.5.7-r2'
-  sha256 'd58697e5b5b8877c5072e43f7f1c5a0fde3a917f864a231ef173b019774a552a'
+  version '1.5.8'
+  sha256 'f5678cf9146031af52519d79dfcb16a81aa9c7eb68e516b34c679139048fb000'
 
-  url 'http://downloads.arduino.cc/arduino-1.5.7-macosx-java7-r2.zip'
+  url "http://downloads.arduino.cc/arduino-#{version}-macosx-java7-r2.zip"
   homepage 'http://arduino.cc/'
   license :unknown
 
