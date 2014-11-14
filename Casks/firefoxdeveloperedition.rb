4c270f05bd79cddcb6d03940ddad3bc5f92e24d4
Thomas Carlsen <thomascarlsen86@gmail.com>
2014-11-14 12:40:45 +0100

    Update firefoxdeveloperedition.rb
    ```sh
    Error: SHA2 mismatch
    Expected: a70dcf0643ee998c9850e70bc0beb476dbc1d8b421632ca73cf4f45a69d61305
    Actual: ee13e28aa50c0bc6c58f86bbd2f2a8b4fef3a71aeb5536b4b80f131e93a463db
    Archive: /Library/Caches/Homebrew/firefoxdeveloperedition-35.0a2.dmg
    To retry an incomplete download, remove the file above.
    ```

--- Casks/firefoxdeveloperedition.rb
+++ Casks/firefoxdeveloperedition.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefoxdeveloperedition' do
   version '35.0a2'
-  sha256 'a70dcf0643ee998c9850e70bc0beb476dbc1d8b421632ca73cf4f45a69d61305'
+  sha256 'ee13e28aa50c0bc6c58f86bbd2f2a8b4fef3a71aeb5536b4b80f131e93a463db'
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-aurora/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://www.mozilla.org/en-US/firefox/developer/'
