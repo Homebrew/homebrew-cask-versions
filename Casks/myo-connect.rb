7fc5ab9761df3663d2e719f410ddf1c31d821cfe
Matt Felsen <mattfelsen@gmail.com>
2014-11-15 16:03:46 -0500

    Add Myo Connect v0.6.0

---
+++ Casks/myo-connect.rb
@@ -0,0 +1,15 @@
+cask :v1 => 'myo-connect' do
+  version '0.6.0'
+  sha256 'a259b2812285d104ea0f9f48b8a550e1a674ec2377591d950578e185e2b02d5d'
+
+  url 'https://s3.amazonaws.com/thalmicdownloads/mac/0.6.0/MyoConnect.dmg'
+  homepage 'https://developer.thalmic.com'
+  license :unknown
+  tags :vendor => 'Thalmic Labs'
+
+  app 'Myo Connect.app'
+
+  zap :delete=> [
+  	             '~/Library/Preferences/com.thalmic.Myo Connect.plist'
+  	            ]
+end
