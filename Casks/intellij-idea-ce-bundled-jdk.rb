e41c59b3164a262adb78e0883d5e8130317f9e39
plessbd <plessbd@users.noreply.github.com>
2014-11-16 16:00:15 -0500

    Changed double to single quotes where asked

--- Casks/intellij-idea-ce-bundled-jdk.rb
+++ Casks/intellij-idea-ce-bundled-jdk.rb
@@ -1,9 +1,9 @@
 cask :v1 => intellij-idea-ce-bundled-jdk do
-  version "14"
-  sha256 "09bb078252e2f6af6b58605ad3a380a71c8cc53f8e697e31fe03fcadb2152b07"
+  version '14'
+  sha256 '09bb078252e2f6af6b58605ad3a380a71c8cc53f8e697e31fe03fcadb2152b07'
 
   url "http://download.jetbrains.com/idea/ideaIC-#{version}-jdk-bundled.dmg"
-  homepage "https://www.jetbrains.com/idea/"
+  homepage 'https://www.jetbrains.com/idea/'
   license :oss
 
   app "IntelliJ IDEA #{version} CE.app"
