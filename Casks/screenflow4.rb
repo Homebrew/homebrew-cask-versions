70488c39e5187a0fa145c6dcf3e8a1f599ee9532
Vítor Galvão <info@vitorgalvao.com>
2014-11-16 23:44:13 +0000

    Create screenflow4.rb

---
+++ Casks/screenflow4.rb
@@ -0,0 +1,10 @@
+cask :v1 => 'screenflow4' do
+  version '4.5.3'
+  sha256 '48209b7fd5387e07c63bd4502ad073dde96460ee30e1a8b19fe2977e8b274914'
+
+  url "http://www.telestream.net/download-files/screenflow/4-5/ScreenFlow-#{version}.dmg"
+  homepage 'http://www.telestream.net/screenflow/'
+  license :unknown
+
+  app 'ScreenFlow.app'
+end
