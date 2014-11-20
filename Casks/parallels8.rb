6e1106b006d9f73a6d12eb7a1f0f307be5883ed8
jeffbaue <jeffbaue@efi.com>
2014-11-19 16:13:22 -0800

    Fix URL for Parallels Desktop.app v8.0
    Add version and homepage

--- Casks/parallels8.rb
+++ Casks/parallels8.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'parallels8' do
-  version :latest
-  sha256 :no_check
+  version '8.0.18619.1001606'
+  sha256 '5b87b2c240e71176cad5097ed7f1928598c9e001da4ad1d8a4516708908c4305'
 
-  url 'http://www.parallels.com/directdownload/pd8'
-  homepage 'http://www.parallels.com/products/desktop/'
+  url 'http://download.parallels.com/desktop/v8/update3.hotfix1/ParallelsDesktop-8.0.18619.1001606.dmg'
+  homepage 'http://kb.parallels.com/en/114623'
   license :closed
 
   pkg 'Install.mpkg'
