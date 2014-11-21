2df0944c9478dd29cb071a00bf1c0672e6db2138
Alex Varju <alex@varju.ca>
2014-11-20 18:34:13 -0800

    update intellij-idea-eap and intellij-idea-ce-eap to 139.463.4

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'intellij-idea-eap' do
-  version '139.223.8'
-  sha256 '63c18d657fa03170aed825049d900aaf8a25618c4c4a1191c670face3216b48a'
+  version '139.463.4'
+  sha256 '9c121f506136f9c658fe3246c6f69947d67184d523c8a3fdaf94e5065b80a918'
 
   url "http://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14+EAP'
   license :commercial
 
-  app 'IntelliJ IDEA 14.app'
+  app 'IntelliJ IDEA 14 EAP.app'
 end
