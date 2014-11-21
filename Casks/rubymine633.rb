500b6f24f3e6e532289ee2198a974cd22177b9e0
Tiago Moraes <tiagoefmoraes@gmail.com>
2014-11-21 11:54:00 -0300

    Add Rubymine.app v6.3.3

---
+++ Casks/rubymine633.rb
@@ -0,0 +1,18 @@
+cask :v1 => 'rubymine633' do
+  version '6.3.3'
+  sha256 'c79216de02f2564ea60592420342ab9fb5014da7e7c96f92e2856dc49f2090dd'
+
+  url "http://download-cf.jetbrains.com/ruby/RubyMine-#{version}.dmg"
+  homepage 'http://www.jetbrains.com/ruby/'
+  license :unknown
+
+  app 'RubyMine.app'
+
+  postflight do
+    system '/usr/libexec/PlistBuddy', '-c', 'Set :JVMOptions:JVMVersion 1.6+', "#{staged_path}/RubyMine.app/Contents/Info.plist"
+  end
+  zap :delete => [
+                  "~/Library/Application Support/RubyMine#{version.gsub('.','')}",
+                  "~/Library/Preferences/RubyMine#{version.gsub('.','')}",
+                 ]
+end
