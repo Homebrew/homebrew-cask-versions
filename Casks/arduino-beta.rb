0637a4d8376ca5365bb3ec414566ad52de15a4cf
mindrones <mindrones@users.noreply.github.com>
2014-12-03 17:24:46 +0100

    arduino-beta: fixing the download URL
    the download URL is not available, raising the following error:
        curl: (22) The requested URL returned error: 403
        Error: Download failed on Cask 'arduino-beta' with message:
        Download failed: http://downloads.arduino.cc/arduino-1.5.8-macosx-java7-r2.zip

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -2,7 +2,7 @@ cask :v1 => 'arduino-beta' do
   version '1.5.8'
   sha256 'f5678cf9146031af52519d79dfcb16a81aa9c7eb68e516b34c679139048fb000'
 
-  url "http://downloads.arduino.cc/arduino-#{version}-macosx-java7-r2.zip"
+  url 'http://arduino.cc/download.php?f=/arduino-1.5.8-macosx-java7.zip'
   homepage 'http://arduino.cc/'
   license :unknown
 
