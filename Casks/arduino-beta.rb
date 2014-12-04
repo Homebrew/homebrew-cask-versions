8b991fd92335f0a1c0b4501d85a5e69ae526f428
mindrones <mindrones@users.noreply.github.com>
2014-12-04 11:47:37 +0100

    forgot to use " instead of ' (#{version} substitution wouldn't work)

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -2,7 +2,7 @@ cask :v1 => 'arduino-beta' do
   version '1.5.8'
   sha256 'f5678cf9146031af52519d79dfcb16a81aa9c7eb68e516b34c679139048fb000'
 
-  url 'http://arduino.cc/download.php?f=/arduino-#{version}-macosx-java7.zip'
+  url "http://arduino.cc/download.php?f=/arduino-#{version}-macosx-java7.zip"
   homepage 'http://arduino.cc/'
   license :unknown
 
