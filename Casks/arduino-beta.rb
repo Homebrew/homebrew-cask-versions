065e1f84d01047b44d265f1a7792ec3dd0c63f01
mindrones <mindrones@users.noreply.github.com>
2014-12-05 12:04:44 +0100

    For Mac OS X versions lower than 10.7.x install the version for java 6, otherwise install the version for java7

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -1,10 +1,20 @@
 cask :v1 => 'arduino-beta' do
   version '1.5.8'
-  sha256 'f5678cf9146031af52519d79dfcb16a81aa9c7eb68e516b34c679139048fb000'
 
-  url "http://arduino.cc/download.php?f=/arduino-#{version}-macosx-java7.zip"
+  if MacOS.version < :lion
+    depends_on :java => '6' # 
+
+    sha256 'c69f870f99ccaf2abb3618845559daea9ef8c52a68ff42b5e21ae3a69cf845fa'
+    url "http://arduino.cc/download.php?f=/arduino-#{version}-macosx.zip"
+  else
+    depends_on :java => '7'
+
+    sha256 'f5678cf9146031af52519d79dfcb16a81aa9c7eb68e516b34c679139048fb000'
+    url "http://arduino.cc/download.php?f=/arduino-#{version}-macosx-java7.zip"
+  end
+
   homepage 'http://arduino.cc/'
   license :unknown
 
-  app 'Arduino.app'
+  app 'Arduino-Beta.app'
 end
