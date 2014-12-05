6acdc7439d3b1eb9c46a1979bb6691233b82134a
mindrones <mindrones@users.noreply.github.com>
2014-12-05 12:38:46 +0100

    oops, forgot a comment in, removing

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -2,7 +2,7 @@ cask :v1 => 'arduino-beta' do
   version '1.5.8'
 
   if MacOS.version < :lion
-    depends_on :java => '6' # 
+    depends_on :java => '6'
 
     sha256 'c69f870f99ccaf2abb3618845559daea9ef8c52a68ff42b5e21ae3a69cf845fa'
     url "http://arduino.cc/download.php?f=/arduino-#{version}-macosx.zip"
