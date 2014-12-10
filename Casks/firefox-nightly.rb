ea1157698f1aea68077f1e9ebc6b512ce7d5a52f
Vítor Galvão <info@vitorgalvao.com>
2014-12-10 00:01:07 +0000

    firefox-nightly.rb: update to 37.0a1

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -2,7 +2,7 @@ cask :v1 => 'firefox-nightly' do
   version :latest
   sha256 :no_check
 
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-36.0a1.en-US.mac.dmg'
+  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-37.0a1.en-US.mac.dmg'
   homepage 'https://nightly.mozilla.org/'
   license :oss
 
