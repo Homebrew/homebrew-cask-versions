055a37f518a238d0091727805a0507eb487e45bf
Basil Shubin <basil.shubin@gmail.com>
2014-12-15 16:50:50 +0600

    Update spacey.rb

--- Casks/spacey.rb
+++ Casks/spacey.rb
@@ -1,7 +1,8 @@
 cask :v1 => 'spacey' do
   version :latest
   sha256 :no_check
-  url "http://files.windowflow.com/Spacey.zip"
+  
+  url 'http://files.windowflow.com/Spacey.zip'
   homepage 'http://www.most-advantageous.com/spacey/'
   license :closed
 
