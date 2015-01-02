b7bb93c342126d9d908708ca9e9d82e1d75cea42
zchee <zchee@zsh.us>
2015-01-02 11:15:33 +0900

    Upgrade Sketch Beta to v3.2.2

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sketch-beta' do
-  version '3.2.1'
-  sha256 '75be5326ae7c6d639effb112c5a4f7c93e55e786d7c807a01e80a9479455699c'
+  version '3.2.2'
+  sha256 '275633effb38fa6e2d963efaa8a2fa9e614638fff67db89ab34acc1e7a469a8e'
 
-  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/64?format=zip&avtoken=772f3a4aa948c0f0ffe925c35a81648c8f40d69d'
+  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/65?format=zip&avtoken=4cf67d09dadfaa99e993df78664101d36228f5c0'
   homepage 'http://bohemiancoding.com/sketch/beta/'
   license :unknown
 
