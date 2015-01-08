8598c87013e56bdf0675d0e0174d6c738c08c12a
Sam Nguyen <sam.nguyen@sendgrid.com>
2015-01-07 16:11:42 -0800

    Add vagrant v1.6.5

---
+++ Casks/vagrant165.rb
@@ -0,0 +1,12 @@
+cask :v1 => 'vagrant165' do
+  version '1.6.5'
+  sha256 'a94a16b9ed5f63460f64110738067aea029238f8d826c8dd90c5c34615a5be1e'
+
+  url "https://dl.bintray.com/mitchellh/vagrant/vagrant_#{version}.dmg"
+  homepage 'http://www.vagrantup.com'
+  license :mit
+
+  pkg 'Vagrant.pkg'
+  uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] },
+            :pkgutil => 'com.vagrant.vagrant'
+end
