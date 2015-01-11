806f9a18caf34b849f912b4a696f76a4d142356e
Vítor Galvão <info@vitorgalvao.com>
2015-01-11 20:16:45 +0000

    updated wireshark-dev (1.99.1)

--- Casks/wireshark-dev.rb
+++ Casks/wireshark-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'wireshark-dev' do
-  version '1.99.0'
-  sha256 '76f5f86cfda5874f6c72be4047b7daee593b73117190da884739bc10508e9a27'
+  version '1.99.1'
+  sha256 '493d4fc48c60898f6bced96eeb0b1e340c9ca28b07bbced04ed69e553f16713f'
 
   url "http://www.wireshark.org/download/osx/Wireshark%20#{version}%20Intel%2064.dmg"
   homepage 'http://www.wireshark.org'
