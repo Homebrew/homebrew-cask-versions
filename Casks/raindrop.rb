a2c7e64f4e559eeab96f639e7cc1d17b678936a4
Lucas Motta <mail@lucasmotta.com>
2015-01-15 16:55:33 +0000

    Added Raindrop.io beta app

---
+++ Casks/raindrop.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'raindrop' do
+  version '3.3.4'
+  sha256 '00ce96dc7f82a69f3e70b72a7eea5315f99f8e1c825bd5f6bc13c643c18bbc4c'
+
+  url 'https://raindrop.io/releases/Raindrop.app.zip'
+  name 'Raindrop'
+  homepage 'https://raindrop.io'
+  license :unknown
+
+  app 'Raindrop.app'
+end
