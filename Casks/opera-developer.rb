cf785ea93e31f5a862ed17770ffb1d96ee0235dd
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-15 17:01:53 -0500

    Update opera-developer to 28.0.1738.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera-developer' do
-  version '20.0.1353.0'
-  sha256 '13aab747e6634140361a50fcecc1ac33e3944e113d0bf724246e3d75e9f9a409'
+  version '28.0.1738.0'
+  sha256 'c489d8fb4e5c514c9a072df41c935164f03f89e64abc752a5995921648da8914'
 
   url "http://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/developer'
