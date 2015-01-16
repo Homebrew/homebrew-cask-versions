8cfdfd3f4a7e285b96c9ee657b6dbd2929f493e0
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-15 20:36:12 -0500

    Updated thunderbird-beta to correct sha256sum

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-beta' do
   version '34.0b1'
-  sha256 '775e548e592f2788d991fe41ff2093bc902ceaa19bef83429e613375c7fa7887'
+  sha256 '7f6025c689633e893203490f11504b4790c46eca88393d7ab678b5405b8a8acb'
 
   url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg"
   homepage 'https://www.mozilla.org/en-US/thunderbird/all-beta.html'
