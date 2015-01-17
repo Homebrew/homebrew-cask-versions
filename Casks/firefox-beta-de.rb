f2b2b4c530ab0cffb942b116f35b4f1247f31693
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-17 14:13:50 -0500

    Updated Firefox Betas, Nightlies, and ESRs Beta Releases - Version bump, license update ESR Releases - Version bump, license update, homepage update Nightlies - Version bump, license update, download update

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'firefox-beta-de' do
-  version '32.0b1'
-  sha256 'f1476898076d9ed5e207e5933b16f7c5fdb13681a2bcacd3c2fefa7c58241553'
+  version '36.0b1'
+  sha256 'bca3bfed105d7749d4ecb88072897bb6947bbc69dbcca75e14186733077ce2e6'
 
   url "https://download.mozilla.org/?product=firefox-#{version}&os=osx&lang=de"
   homepage 'https://www.mozilla.org/de/firefox/beta/'
-  license :oss
+  license :mpl
 
   app 'Firefox.app'
 end
--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'firefox-nightly' do
-  version :latest
-  sha256 :no_check
+  version '38.0a1'
+  sha256 'd0a1f9bf78de8c2b21e0e3fc90e878744699ef086e6dfa14e9551b4e1a618b98'
 
-  url 'https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-37.0a1.en-US.mac.dmg'
+  url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://nightly.mozilla.org/'
-  license :oss
+  license :mpl
 
   app 'FirefoxNightly.app'
 end
