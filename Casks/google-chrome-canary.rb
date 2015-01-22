05fc82ebe69d9eff870f6caae65ad5854ad63bbb
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-22 16:37:01 -0500

    Updated google-chrome-canary urls, license and added zap stanza

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -3,8 +3,31 @@ cask :v1 => 'google-chrome-canary' do
   sha256 :no_check
 
   url 'https://storage.googleapis.com/chrome-canary/GoogleChromeCanary.dmg'
-  homepage 'https://www.google.com/chrome/browser/canary.html'
-  license :closed
+  homepage 'https://www.google.com/chrome/browser/canary.html?platform=mac'
+  license :gratis
+  tags :vendor => 'Google'
 
   app 'Google Chrome Canary.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Google/Chrome Canary',
+                  '~/Library/Caches/Google/Chrome Canary',
+                  '~/Library/Caches/com.google.Chrome',
+                  '~/Library/Caches/com.google.Chrome.helper.EH',
+                  '~/Library/Caches/com.google.Keystone.Agent',
+                  '~/Library/Caches/com.google.SoftwareUpdate',
+                  '~/Library/Google/GoogleSoftwareUpdate',
+                  '~/Library/Logs/GoogleSoftwareUpdateAgent.log',
+                 ],
+      :rmdir  => [
+                  '~/Library/Caches/Google',
+                  '~/Library/Google',
+                 ]
+
+  caveats <<-EOS.undent
+    The Mac App Store version of 1Password won't work with a Homebrew-Cask-linked Google Chrome. To bypass this limitation, you need to either:
+
+      + Move Google Chrome to your /Applications directory (the app itself, not a symlink).
+      + Install 1Password from outside the Mac App Store (licenses should transfer automatically, but you should contact AgileBits about it).
+  EOS
 end
