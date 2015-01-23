9611f5591438a92c792b17b7cbc3b81f34d874a2
msb <max.schwaab@westernacher.com>
2015-01-23 10:14:15 +0100

    Upgrade java7 to jdk-7u76
    Changed version and download url to u76 and adjusted checksum.

--- Casks/java7.rb
+++ Casks/java7.rb
@@ -1,15 +1,15 @@
 cask :v1 => 'java7' do
-  version '1.7.0_72'
-  sha256 'fa5ea69efba30650b3e719f79b1ec3abd0ab9d878adf766b0c77bd7678c7cffe'
+  version '1.7.0_76'
+  sha256 '3857c163aa61b9e5bf9c69cbe47c502ac7683e0bc03782e742be34d4b57164ba'
 
-  url 'http://download.oracle.com/otn-pub/java/jdk/7u72-b14/jdk-7u72-macosx-x64.dmg',
+  url 'http://download.oracle.com/otn-pub/java/jdk/7u76-b13/jdk-7u76-macosx-x64.dmg',
       :cookies => {
                     'oraclelicense' => 'accept-securebackup-cookie'
                   }
   homepage 'http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html'
   license :unknown
 
-  pkg 'JDK 7 Update 72.pkg'
+  pkg 'JDK 7 Update 76.pkg'
   postflight do
     system '/usr/bin/sudo', '-E', '--',
       '/usr/libexec/PlistBuddy', '-c', 'Add :JavaVM:JVMCapabilities: string BundledApp', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Info.plist"
@@ -28,7 +28,7 @@ cask :v1 => 'java7' do
     system '/usr/bin/sudo', '-E', '--',
       '/bin/ln', '-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/jre/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/jdk#{version}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"
   end
-  uninstall :pkgutil => 'com.oracle.jdk7u72',
+  uninstall :pkgutil => 'com.oracle.jdk7u76',
             :delete => '/System/Library/Frameworks/JavaVM.framework/Versions/CurrentJDK'
   caveats <<-EOS.undent
     This Cask makes minor modifications to the JRE to prevent any packaged
