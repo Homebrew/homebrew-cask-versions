6420e37e1ed48d7524d035145ee4223e0774d6b1
Marius Bergmann <marius@yeai.de>
2015-01-26 22:54:14 +0100

    Rename existing scansnap-manager versions to reflect supported models

--- Casks/scansnap-manager-s300m-s500m-s510m-fi-5110eoxm-legacy.rb
+++ Casks/scansnap-manager-s300m-s500m-s510m-fi-5110eoxm-legacy.rb
@@ -1,15 +1,18 @@
-cask :v1 => 'scansnap-manager-v22l11' do
+cask :v1 => 'scansnap-manager-s300m-s500m-s510m-fi-5110eoxm-legacy' do
   version '2.2L11'
   sha256 '63d22a3ea7b01a32b0064bacad74dd82359ed366f5039d0fb6519e78cfee5beb'
 
   url 'http://www.fujitsu.com/downloads/IMAGE/driver/ss/ScanSnap_V22L11.dmg'
-  name 'ScanSnap Manager for Mac (V2.2L11)'
+  name 'ScanSnap Manager for Fujitsu ScanSnap S300M'
+  name 'ScanSnap Manager for Fujitsu ScanSnap S500M'
+  name 'ScanSnap Manager for Fujitsu ScanSnap S510M'
+  name 'ScanSnap Manager for Fujitsu ScanSnap fi-5110EOXM'
   homepage 'http://www.fujitsu.com/global/support/computing/peripheral/scanners/software/mac.html'
   license :commercial
 
   pkg 'ScanSnap_V22L11.pkg'
 
-  caveats "Install cask `scansnap-manager-v22l16` after to update for compatibility with OS X 10.6+"
+  caveats "Install cask `scansnap-manager-s300m-s510m` after to update for compatibility with OS X 10.6+"
 
   uninstall :pkgutil => 'jp.co.pfu.ScanSnap.V10L10',
             :quit => [
@@ -26,4 +29,5 @@ cask :v1 => 'scansnap-manager-v22l11' do
               'jp.co.pfu.ScanSnap.V10L10'
             ]
 
+  depends_on :macos => [:tiger, :leopard]
 end
