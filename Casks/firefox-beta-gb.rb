79ff24f39c671f179e2f34bdd35b35cae1e1dbba
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-27 12:46:28 -0500

    Updated firefox-beta localized versions to 36.0b3

--- Casks/firefox-beta-gb.rb
+++ Casks/firefox-beta-gb.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-gb' do
-  version '36.0b1'
-  sha256 '8ef105c26f697895901a58b6a212b5390648704e9b1a4b0108473cdc12da7da0'
+  version '36.0b3'
+  sha256 '65c03ec7294bcf1365c09a138ee4338b22f2d99965a359f5d4edd19dcba9a33c'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-GB"
   homepage 'https://www.mozilla.org/en-GB/firefox/beta/'
