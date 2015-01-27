1d601e2c657cb5d0aed622d402919fbf7e49dd0c
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-27 12:46:28 -0500

    Updated firefox-beta localized versions to 36.0b3

--- Casks/firefox-beta-cn.rb
+++ Casks/firefox-beta-cn.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-cn' do
-  version '36.0b1'
-  sha256 '26fc7da08b829a62c00d15110d1e5478527facab7cf88f0b140eea3cdc828404'
+  version '36.0b3'
+  sha256 'a188706dd04965cf1e03cf41fb3f0b85ecd89ee1df0ba353edb8a5bac1aac148'
 
   url "https://download.mozilla.org/?product=firefox-#{version}&os=osx&lang=zh-CN"
   homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
