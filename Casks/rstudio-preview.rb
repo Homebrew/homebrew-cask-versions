4734f018abc6048d073070518dfab58fa1867261
Ryan Duryea <aguynamedryan@gmail.com>
2015-01-27 18:07:21 -0800

    Double quotes => Single quotes

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -2,7 +2,7 @@ cask :v1 => 'rstudio-preview' do
   version :latest
   sha256 :no_check
 
-  url "http://www.rstudio.org/download/latest/preview/desktop/mac/RStudio-latest.dmg"
+  url 'http://www.rstudio.org/download/latest/preview/desktop/mac/RStudio-latest.dmg'
   homepage 'http://www.rstudio.com/ide/download/preview'
   license :unknown
 
