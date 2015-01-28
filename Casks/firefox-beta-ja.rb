c8268a5b146adce5cdd84b3d3d7b37bc81a3bdc5
Christian Torrez <zerrot@users.noreply.github.com>
2015-01-27 19:27:58 -0500

    Upgraded firefox-beta releases to version 36.0b4

--- Casks/firefox-beta-ja.rb
+++ Casks/firefox-beta-ja.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-ja' do
-  version '36.0b3'
-  sha256 '3c7a17ffad3eac9e30f1c6fcd0e216cf2b55f63f0a6d9d598a56cb6cbd018e90'
+  version '36.0b4'
+  sha256 'a6a6eb7def1f23542e9babecc4dd99632939aaaa2573a331080f942a452574fe'
 
   url "https://download.mozilla.org/?product=firefox-#{version}&os=osx&lang=ja-JP-mac"
   homepage 'http://www.mozilla.org/ja/firefox/channel/beta'
