2d93f975b458a422039ba263439456a2fe4355c0
zchee <zchee@zsh.us>
2015-01-30 11:23:30 +0900

    Fix checksum in Google Japanese IME Dev build

--- Casks/google-japanese-ime-dev.rb
+++ Casks/google-japanese-ime-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'google-japanese-ime-dev' do
-  version 'dev'
-  sha256 'd3f786d3462e0294f29bc1f8998163489f812a1ae650661e9a26a92b731c2ef8'
+  version :latest
+  sha256 :no_check
 
   url 'https://dl.google.com/japanese-ime/dev/GoogleJapaneseInput.dmg'
   homepage 'https://www.google.co.jp/ime/'
