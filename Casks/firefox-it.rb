c72f99421dada723307415a4454a6a9012973858
Yuriy Kolesnikov <yurikoles@gmail.com>
2015-02-03 12:59:04 +0200

    update firefox casks

--- Casks/firefox-it.rb
+++ Casks/firefox-it.rb
@@ -3,8 +3,15 @@ cask :v1 => 'firefox-it' do
   sha256 :no_check
 
   url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=it'
-  homepage 'http://www.mozilla.org/it/firefox/new/'
-  license :oss
+  name 'Firefox'
+  homepage 'https://www.mozilla.org/it/firefox/desktop'
+  license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
