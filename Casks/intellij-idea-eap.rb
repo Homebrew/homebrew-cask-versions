d7af2421abc673c48ace9d24f0c6034bde61bdd6
zchee <zchee@zsh.us>
2015-02-06 02:58:52 +0900

    Upgrade Intellij Idea EAP to v140.2110.5

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'intellij-idea-eap' do
-  version '139.872.1'
-  sha256 'f9ea80b72d0d1aa78685cda89eb6e7a1719636901957193b167fb884e40e47c4'
+  version '140.2110.5'
+  sha256 '971cea43259cf517c209dd646c6c48220c97f22e35c0773b644b8c7578aea1c3'
 
   url "http://download-cf.jetbrains.com/idea/ideaIU-#{version}.dmg"
-  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14+EAP'
+  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14.1+EAP'
   license :commercial
 
   app 'IntelliJ IDEA 14 EAP.app'
