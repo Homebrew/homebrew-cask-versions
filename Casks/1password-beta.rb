4d09f8bb2d070bbce6127add6584b5c6c9b82838
Vincent Klaiber <vincentklaiber@gmail.com>
2015-02-07 09:37:14 +0100

    Update 1Password.app beta cask.
    Added new version number, name and zap.

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,10 +1,18 @@
 cask :v1 => '1password-beta' do
-  version '5.3.BETA-4'
-  sha256 'bb2c2ca63869e916e59bb71e38847090bdae471188cce6f85e644d2bd52dae1d'
+  version '5.3.BETA-5'
+  sha256 'a03fe7f803d666890193b4b2c098a1fe553d128d7cd59d02fc81759601b41423'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
+  name '1Password'
   homepage 'https://agilebits.com/onepassword/mac'
   license :commercial
 
   app '1Password 5.app'
+
+  zap :delete => [
+                    '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+                    '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+                    '~/Library/Containers/com.agilebits.onepassword-osx',
+                    '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
+                   ]
 end
