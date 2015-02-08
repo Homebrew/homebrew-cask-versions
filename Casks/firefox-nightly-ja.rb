be34082338abb58d1158ac8383f211da5d99a06e
Christian Torrez <zerrot@users.noreply.github.com>
2015-02-08 12:38:50 -0500

    Updated firefox-nightlies to :no_check sha
    This is required since the nightlies are updated in place.

--- Casks/firefox-nightly-ja.rb
+++ Casks/firefox-nightly-ja.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-nightly-ja' do
   version '38.0a1'
-  sha256 '666a6a38572f0d452458f0da0992636b09ce3cb9f42288cffcc3450085f0f4fe'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-mozilla-central-l10n/firefox-#{version}.ja-JP-mac.mac.dmg"
   homepage 'https://nightly.mozilla.org/'
--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-nightly' do
   version '38.0a1'
-  sha256 'd0a1f9bf78de8c2b21e0e3fc90e878744699ef086e6dfa14e9551b4e1a618b98'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-#{version}.en-US.mac.dmg"
   homepage 'https://nightly.mozilla.org/'
