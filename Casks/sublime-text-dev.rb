ad2bbefbca382ffd725309fdd05b9b0c3e0267e3
Chris Burke <chris.burke@cakedc.com>
2015-02-17 15:33:24 +0000

    Update sublime-text-dev to Build 3070

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sublime-text-dev' do
-  version '3069'
-  sha256 '96f0ed890839bc554e8fd26b03c9404218feacb4aefbedc0fac9cdd87799654d'
+  version '3070'
+  sha256 'b398456b1ea72a1ae10da6d687612698056ffb76a6e534faa50c90a6a98ecf0c'
 
   url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%20#{version}.dmg"
   name 'Sublime Text'
