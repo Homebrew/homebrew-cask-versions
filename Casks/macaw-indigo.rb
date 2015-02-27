990cb87b9d319b88f00b295ce7fe582eb768ce6a
Nicolas Genty <nicolas.genty@gmail.com>
2015-02-27 19:40:34 +0100

    upd indigo 1.5.13.1

--- Casks/macaw-indigo.rb
+++ Casks/macaw-indigo.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'macaw-indigo' do
-  version '1.5.4.0'
-  sha256 'a98bc077cff020951fed2fc04e47b4182cd1c93b9882a53f89955b9b4f7a37f7'
+  version '1.5.13.1'
+  sha256 'ad87a7e547e5f66b5e60d43b7e3fff7e37935fc1b37d160ed9800125c964ffd8'
 
   url "http://indigo.macaw.co/#{version}/MacawIndigo#{version}.dmg"
   appcast 'http://indigo.macaw.co/appcast.xml',
-          :sha256 => 'ff896638f2868f4d670176611abfc284dbe935ab7264574f57845b202d348617'
+          :sha256 => 'a1b3cf974b3c105a97b571a0d65364d896ca94070ed3218cf05725d7e7f44f50'
   homepage 'http://indigo.macaw.co/'
   license :commercial
 
