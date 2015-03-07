c8a747b3d9f5794340c209acebeed5a3802bb970
zchee <zcheeee@gmail.com>
2015-03-07 16:15:22 +0900

    Upgrade Rubymine EAP to v140.2694

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'rubymine-eap' do
-  version '139.262'
-  sha256 'e255b6a0f94fee55e72c9969919619c3431bf6507e678e0ac9303edc0d441072'
+  version '140.2694'
+  sha256 '7376d5b04b49e503c203a2b1c9034a690835ea601847753710f3c8ec53566ebb'
 
   url "http://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   homepage 'http://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
   license :unknown
 
-  app 'RubyMine.app'
+  app 'RubyMine EAP.app'
 end
