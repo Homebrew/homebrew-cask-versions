1c175a55c8b7893f8f7f0cb2a25beb1d90aa85e4
Chris Burke <chris.burke@cakedc.com>
2015-03-10 08:41:44 +0000

    Update sublime-text-dev to 3075

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sublime-text-dev' do
-  version '3074'
-  sha256 'c49c915db44bf016ada8bd73a856b44da25fc8b2e905515703e89fb277c53e79'
+  version '3075'
+  sha256 '5b41f42392b101d2d96cf4195554900423db64a908755f327039e4ff9191ce1f'
 
   # rackcdn.com is the official download host per the vendor homepage
   url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%20#{version}.dmg"
