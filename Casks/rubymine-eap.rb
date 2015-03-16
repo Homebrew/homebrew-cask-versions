94256fadcdc16c704a91bf8d9bdc37b60df261e2
Sebastian Röder <no-spam-github@sebastianroeder.de>
2015-03-16 22:05:39 +0100

    Use RubyMine EAP version with bundled JDK 8 patched by JetBrains
    The bundled JDK fixes many Java-related problems on OS X 10.10

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -2,7 +2,7 @@ cask :v1 => 'rubymine-eap' do
   version '141.96'
   sha256 '1fb0c817a788dd2e3f01bb7ad942701073b86a66456a905af733984d202b2f3b'
 
-  url "http://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
+  url "http://download.jetbrains.com/ruby/RubyMine-#{version}-custom-jdk-bundled.dmg"
   homepage 'http://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
   license :commercial
 
