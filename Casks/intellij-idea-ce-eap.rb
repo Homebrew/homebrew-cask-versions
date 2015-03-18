8c73976bc1ee35d7c82005cad13400108e4763fe
Alex Varju <alex@varju.ca>
2015-03-18 10:25:03 -0700

    upgrade intellij-ce-eap to 141.175.3

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'intellij-idea-ce-eap' do
-  version '140.2110.5'
-  sha256 'f9bc38f14eadea7067df952bd1dfb223a0a10d22f541a76379896cd088e93cda'
+  version '141.175.3'
+  sha256 '888a873904f3db7fcb6e5fcf07057f8a57bcca9587c5f1f5717dbd52a56676d1'
 
   url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14.1+EAP'
