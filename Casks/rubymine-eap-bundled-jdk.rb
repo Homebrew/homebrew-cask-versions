ed18230d153ad9dc66754e3112ebdfeee2285c25
zchee <zcheeee@gmail.com>
2015-03-19 11:00:11 +0900

    Add RubyMine EAP JDK bundled build

---
+++ Casks/rubymine-eap-bundled-jdk.rb
@@ -0,0 +1,15 @@
+cask :v1 => 'rubymine-eap-bundled-jdk' do
+  version '141.96'
+  sha256 '1fb0c817a788dd2e3f01bb7ad942701073b86a66456a905af733984d202b2f3b'
+
+  url "http://download.jetbrains.com/ruby/RubyMine-#{version}-custom-jdk-bundled.dmg"
+  homepage 'http://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
+  license :commercial
+
+  app 'RubyMine EAP.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/RubyMine70',
+                  '~/Library/Preferences/RubyMine70',
+                 ]
+end
