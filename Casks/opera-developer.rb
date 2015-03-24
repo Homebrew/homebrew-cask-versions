359b93c99f8cb2082285fb2071d661978273fe4b
Victor Popkov <Dragon.vctr@gmail.com>
2015-03-24 18:27:55 +0200

    Upgrade Opera Developer.app to v30.0.1812.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera-developer' do
-  version '29.0.1795.14'
-  sha256 'ce0e8ecdac8c1fda65369acc5d9539faad529e0970c4a6de0c5ed832e29d34e0'
+  version '30.0.1812.0'
+  sha256 'ec032b5545358e5358f96cdf16f5eb813f726727d9fcb878520f2669473f2bc7'
 
   url "http://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/developer'
