4a1b45f989eb9015c08cff1938d576b8af90fda5
Sebastian Röder <no-spam-github@sebastianroeder.de>
2015-03-25 23:02:06 +0100

    Upgrade IntelliJ Idea CE with bundled JDK to 14.1
    - Unify with other IntelliJ-based formulas (see #879)

--- Casks/intellij-idea-ce-bundled-jdk.rb
+++ Casks/intellij-idea-ce-bundled-jdk.rb
@@ -1,17 +1,19 @@
 cask :v1 => 'intellij-idea-ce-bundled-jdk' do
-  version '14'
-  sha256 '09bb078252e2f6af6b58605ad3a380a71c8cc53f8e697e31fe03fcadb2152b07'
+  version '14.1'
+  sha256 '62c8b150ff82c31f2b6355a6f43b8ef69913b8f387cef6ddb4659622abb59a52'
 
-  url "http://download.jetbrains.com/idea/ideaIC-#{version}-jdk-bundled.dmg"
+  url "https://download.jetbrains.com/idea/ideaIC-#{version}-custom-jdk-bundled.dmg"
+  name 'IntelliJ IDEA'
   homepage 'https://www.jetbrains.com/idea/'
-  license :oss
+  license :apache
 
-  app "IntelliJ IDEA #{version} CE.app"
-  
+  app 'IntelliJ IDEA 14 CE.app'
+
   zap :delete => [
-                  "~/Library/Application Support/IdeaIC#{version}",
-                  "~/Library/Preferences/IdeaIC#{version}",
-                  "~/Library/Caches/IdeaIC#{version}",
-                  "~/Library/Logs/IdeaIC#{version}",
+                  '~/Library/Preferences/com.jetbrains.intellij.ce.plist',
+                  '~/Library/Preferences/IdeaIC14',
+                  '~/Library/Application Support/IdeaIC14',
+                  '~/Library/Caches/IdeaIC14',
+                  '~/Library/Logs/IdeaIC14',
                  ]
 end
