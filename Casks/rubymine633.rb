889f7a824a1f43ca3749c302b17ef6d3ee151439
Sebastian Röder <no-spam-github@sebastianroeder.de>
2015-03-30 23:08:52 +0200

    Clean up RubyMine 6.3.3 formula
    - Unify with other IntelliJ-based formulas (see #879)

--- Casks/rubymine633.rb
+++ Casks/rubymine633.rb
@@ -2,17 +2,30 @@ cask :v1 => 'rubymine633' do
   version '6.3.3'
   sha256 'c79216de02f2564ea60592420342ab9fb5014da7e7c96f92e2856dc49f2090dd'
 
-  url "http://download-cf.jetbrains.com/ruby/RubyMine-#{version}.dmg"
-  homepage 'http://www.jetbrains.com/ruby/'
-  license :unknown
+  url "https://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
+  name 'RubyMine'
+  homepage 'https://confluence.jetbrains.com/display/RUBYDEV/Previous+RubyMine+Releases'
+  license :commercial
 
   app 'RubyMine.app'
 
-  postflight do
-    plist_set(':JVMOptions:JVMVersion', '1.6+')
-  end
   zap :delete => [
-                  "~/Library/Application Support/RubyMine#{version.gsub('.','')}",
-                  "~/Library/Preferences/RubyMine#{version.gsub('.','')}",
+                  '~/Library/Preferences/com.jetbrains.rubymine.plist',
+                  '~/Library/Preferences/RubyMine60',
+                  '~/Library/Application Support/RubyMine60',
+                  '~/Library/Caches/RubyMine60',
+                  '~/Library/Logs/RubyMine60',
+                  '/usr/local/bin/mine',
                  ]
+
+  caveats <<-EOS.undent
+    #{token} requires Java 6 like any other IntelliJ-based IDE.
+    You can install it with
+
+      brew cask install caskroom/homebrew-versions/java6
+
+    The vendor (JetBrains) doesn't support newer versions of Java (yet)
+    due to several critical issues, see details at
+    https://intellij-support.jetbrains.com/entries/27854363
+  EOS
 end
