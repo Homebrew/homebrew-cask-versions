83e918e3940229167e8279bd1bbaca90642189ee
Michael Ford <fanquake@gmail.com>
2015-03-31 19:09:40 +0800

    Firefox Beta GB 37.0b7

--- Casks/firefox-beta-gb.rb
+++ Casks/firefox-beta-gb.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-gb' do
-  version '37.0b2'
-  sha256 '649e005972610ab11bc33ab1ba6d3ee6040d7f27a6a5637b4699bf8f0f02f63d'
+  version '37.0b7'
+  sha256 '9cf64561a2abf2b4e15802d0a2fdfc8f81191983e74256b26da2e9dc7e9d4b11'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-GB"
   homepage 'https://www.mozilla.org/en-GB/firefox/beta/'
