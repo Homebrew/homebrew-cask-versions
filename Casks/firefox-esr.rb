8c3ec6a806a5db565079208b838391cedd450b7d
Michael Ford <fanquake@gmail.com>
2015-03-31 20:02:41 +0800

    Firefox ESR 31.5.3

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-esr' do
-  version '31.5.0'
-  sha256 'e3e385b6cf0ae3ea0c8e816c997c96819ad6ae8b947b2130f2922b38aa4ac3e7'
+  version '31.5.3'
+  sha256 '90676a07495781cf5bb3e758011a156da0ba74bb6bf994a36e96ec6243f6902a'
 
   url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
