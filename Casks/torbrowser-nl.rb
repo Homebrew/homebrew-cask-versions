a0365430b1eeb5e1349e8629d50457605b81ebda
Michael Ford <fanquake@gmail.com>
2015-03-31 18:19:21 +0800

    TorBrowser NL 4.0.5

--- Casks/torbrowser-nl.rb
+++ Casks/torbrowser-nl.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'torbrowser-nl' do
-  version '4.0.4'
-  sha256 'f47c930676ba663c11fa2fbf50bed83f194e51258d4f01717adf3a03dc0da220'
+  version '4.0.5'
+  sha256 '1ecfaf3de303c101871f1c6a7f77beb71fda4f172bfa333e2962fa44a871b532'
 
   url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_nl.dmg"
   gpg "#{url}.asc",
       :key_id => '416f061063fee659'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   app 'TorBrowser.app'
 end
