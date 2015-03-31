2d737b4f48f4227103021693ac7db43dad7345cd
Michael Ford <fanquake@gmail.com>
2015-03-31 19:09:28 +0800

    Firefox Beta DE 37.0b7

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-de' do
-  version '37.0b2'
-  sha256 '140dd45ca602d25be58e97921e43bfd35cc2baba46adfab63fcb966135e26232'
+  version '37.0b7'
+  sha256 '46e8a9fd86f5a37a7f5a01e2fd4ccc46174403f007215e3ac0eeef5ddca9127b'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   homepage 'https://www.mozilla.org/de/firefox/beta/'
