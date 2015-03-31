dd50893b864ca5f959a065d8008cb05b632fd2f0
Michael Ford <fanquake@gmail.com>
2015-03-31 19:10:08 +0800

    Firefox Beta RU 37.0b7

--- Casks/firefox-beta-ru.rb
+++ Casks/firefox-beta-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-ru' do
-  version '37.0b2'
-  sha256 '2f2216352491d534ace48148004570857c829df00fb0ef0f5572393f0d84d10b'
+  version '37.0b7'
+  sha256 'aa321d108121d10e17f31e5309543de49b9d59c53f0bc09e5bb44469f472c7e9'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ru"
   homepage 'https://www.mozilla.org/ru/firefox/channel/#beta'
