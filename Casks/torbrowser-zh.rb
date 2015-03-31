82f6b3cfe37a116ddf78bfbfde0eeccda855e905
Michael Ford <fanquake@gmail.com>
2015-03-31 18:25:57 +0800

    TorBrowser ZH 4.0.5

--- Casks/torbrowser-zh.rb
+++ Casks/torbrowser-zh.rb
@@ -1,12 +1,12 @@
 cask :v1 => 'torbrowser-zh' do
-  version '4.0.4'
-  sha256 '833c009c56afb0112246788fe8d87f9cad7a7aab5e372e4dcc95327be49885c0'
+  version '4.0.5'
+  sha256 'bd05b785d59ad613661be5b2260a1212476d03f4b8b4e4df34f481c26e65fa30'
 
   url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_zh-CN.dmg"
   gpg "#{url}.asc",
       :key_id => '416f061063fee659'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :oss
 
   app 'TorBrowser.app'
 end
