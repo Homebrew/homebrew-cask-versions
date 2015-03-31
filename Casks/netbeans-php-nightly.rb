3155b71f64234607cd1c96b351b689bcad751c9b
Michael Ford <fanquake@gmail.com>
2015-03-31 18:31:40 +0800

    Reoder Netbeans PHP Nightly Cask

--- Casks/netbeans-php-nightly.rb
+++ Casks/netbeans-php-nightly.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'netbeans-php-nightly' do
-  homepage 'https://netbeans.org/'
-  license :unknown
   version '201502110001'
   sha256 'c8cda323f4b0c03bc639e1f39633f5b8ba9278da712520fc4b7f6328622f9b70'
 
   url "http://bits.netbeans.org/download/trunk/nightly/latest/bundles/netbeans-trunk-nightly-#{version}-cpp-macosx.dmg"
+  homepage 'https://netbeans.org/'
+  license :unknown
+  
   pkg "NetBeans Dev #{version}.mpkg"
 
   uninstall :delete => '/Applications/NetBeans'
