e96ef1088ced9937e66e3148c07f41935ede4461
Vincent Klaiber <vincentklaiber@gmail.com>
2015-04-01 16:01:07 +0200

    Update SpotifyBeta.app to latest version.

--- Casks/spotifybeta.rb
+++ Casks/spotifybeta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'spotifybeta' do
-  version '1.0.0.1093.g2eba9b4b-1679'
-  sha256 'ac5e99bfb448d3f4ee46b1a95bd428c5d8f27e193b44895754ac4150a0f56a60'
+  version '1.0.0.1212.gc1771003-2016'
+  sha256 '7e702d08520e4ff8569afb12504788321f3e81b436685a72e0336f10f05720b8'
 
   url "http://download.spotify.com/beta/spotify-app-#{version}.dmg"
   name 'Spotify Beta'
