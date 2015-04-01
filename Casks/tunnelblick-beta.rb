453d4a4af44298cd99a3110f83f61d0f82cae8aa
Chris Burke <chris.burke@cakedc.com>
2015-04-01 19:32:46 +0100

    Update tunnelblick-beta to 3.5beta10

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'tunnelblick-beta' do
-  version '3.5beta08_build_4236'
-  sha256 '60badae32c6013e006f011833417b468d51b6750d7417f006ebc8756265149b6'
+  version '3.5beta10_build_4262'
+  sha256 '77b2c0699b26242de1b207d2ed6f06341b0672781cba51d7ecae28646a670f71'
 
   url "http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg"
   homepage 'https://code.google.com/p/tunnelblick/'
