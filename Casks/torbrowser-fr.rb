5c64cefc083de0a4dabdca17297ead844da138b0
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-02 09:40:10 +0300

    torbrowser-fr.rb: upgrade to v4.0.6, change signing key

--- Casks/torbrowser-fr.rb
+++ Casks/torbrowser-fr.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'torbrowser-fr' do
-  version '4.0.5'
-  sha256 '8a1a84b62a8269cd3082c5d61e3a59496d285cc928cccf0ebb2739b82c79da10'
+  version '4.0.6'
+  sha256 'f704525c7f3f2c2fdab9cb6c63a4fe2137882bb287fe41d550fb22cd34f06cca'
 
   url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_fr.dmg"
   gpg "#{url}.asc",
-      :key_id => '416f061063fee659'
+      :key_id => '4e2c6e8793298290'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
 
