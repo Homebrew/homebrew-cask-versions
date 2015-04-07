e2d3a8d70a07797b150d8c117ee8ba9d7f93a83b
Hans-Christian Halfbrodt <hc@Bondis-2.local>
2015-04-07 16:21:41 +0200

    Upgrade thunderbird-de to 31.6.0

--- Casks/thunderbird-de.rb
+++ Casks/thunderbird-de.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-de' do
-  version '31.5.0'
-  sha256 'e797f9e2d4d18674cb6b0cc931491dc4f986e1988d0b1ecd60055e133b5ccc94'
+  version '31.6.0'
+  sha256 '2839fd8dcc29108f9b55a801e7533e101dec171195ef3399191e68c8e4e741c8'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=de"
   name 'Mozilla Thunderbird'
