67fd979498084fbf06994504954410395f122e1e
ndr <ndr@qef.io>
2015-04-07 12:58:38 +0100

    1password-beta: update to 5.4.BETA-1
    … and normalize whitespace.

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => '1password-beta' do
-  version '5.3.BETA-31'
-  sha256 '7473823efce85cfed5777c86e2ed141dc3d75d8a5bf1e15701e1338c5b009afc'
+  version '5.4.BETA-1'
+  sha256 '226b990f8d7e00dbf248cf951d546f3780529fc2446a2adfbc24e54c743cba63'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
@@ -10,9 +10,9 @@ cask :v1 => '1password-beta' do
   app '1Password 5.app'
 
   zap :delete => [
-                    '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
-                    '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
-                    '~/Library/Containers/com.agilebits.onepassword-osx',
-                    '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
-                   ]
+                  '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+                  '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
+                  '~/Library/Containers/com.agilebits.onepassword-osx',
+                  '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
+                 ]
 end
