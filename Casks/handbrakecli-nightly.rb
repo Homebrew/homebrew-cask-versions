4109f7de4529090b6f718c8d52c662cc53f695c0
Sebastian Nash <sebnash@gmail.com>
2015-04-09 14:04:52 +0100

    Update HandbrakeCLI Nightly to v7073svn
    HandBrakeCLI Nightly v7073svn built 2015-04-09.

--- Casks/handbrakecli-nightly.rb
+++ Casks/handbrakecli-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'handbrakecli-nightly' do
-  version '7031svn'
-  sha256 '29584bd078000dc79c95c56aa9483e7bc5d6cce048349414e4016bcea68ef76d'
+  version '7073svn'
+  sha256 '50a374f8b8984735e8f2b49d5be78f2dc713d433e3982ce539697c31fea190de'
 
   url "http://download.handbrake.fr/nightly/HandBrake-#{version}-MacOSX.6_CLI_x86_64.dmg"
   homepage 'http://handbrake.fr'
