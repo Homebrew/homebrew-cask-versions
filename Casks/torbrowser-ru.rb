13db906fdb101690ad213cf2e44a25e5bad3232c
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-10 09:46:56 +0300

    torbrowser-ru.rb: upgrade to v4.0.8

--- Casks/torbrowser-ru.rb
+++ Casks/torbrowser-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-ru' do
-  version '4.0.6'
-  sha256 '641ed1696e5bc63d56c57cc80ebd0e834d87005933851808b479fd381d7040b5'
+  version '4.0.8'
+  sha256 'f8ce2c005244c02e543f1bed4f5c19f7177e0ad8454e8f32552f6ba37bba620e'
 
   url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_ru.dmg"
   gpg "#{url}.asc",
