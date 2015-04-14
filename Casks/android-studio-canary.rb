b82a01cd8d33f1a1971c3ededff6d2d1adedccb2
Falko Richter <deadfalkon+github@gmail.com>
2015-04-14 06:46:38 +0200

    added version back

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,4 +1,5 @@
 cask :v1 => 'android-studio-canary' do
+  version '1.2.0.8'
   sha256 '0efab53a2b5510f9164987b363ec02a60382a55d3de9c2e0308d233e3ca26507'
 
   url "http://dl.google.com/dl/android/studio/ide-zips/1.2.0.8/android-studio-ide-141.1845774-mac.zip"
