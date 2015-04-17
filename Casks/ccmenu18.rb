335190154674e66c37e3412b7bfca8a2da9c8b96
Michael <fanquake@gmail.com>
2015-04-17 12:40:41 +0800

    CCMenu 1.8.1

--- Casks/ccmenu18.rb
+++ Casks/ccmenu18.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'ccmenu18' do
-  version '1.8'
-  sha256 'e2d36083388b4c68c1dff396393f4d3b2df614ff09deca1fdd9bbba286f7faa5'
+  version '1.8.1'
+  sha256 '153a72602c6f9d5900569fbc514d4e5e273712e9b6321bc18174c6c57a46416f'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/ccmenu/CCMenu/#{version}/ccmenu-#{version}-b.dmg"
   appcast 'http://ccmenu.sourceforge.net/update-stable.xml',
-          :sha256 => 'a72951f416906d309cb7ec8d233bf5546540641efa99332e0f1bdde119b51cac'
+          :sha256 => '3104a3a4e9b3cffee27464abdf5704d6d31814527326c796ae7064bfbab4b797'
   name 'CCMenu'
   homepage 'http://ccmenu.org/'
   license :bsd
