e38edc084b08ce4153c2480becd1c81a13ca881d
fenque <fenque@users.noreply.github.com>
2015-04-26 16:41:19 +0300

    Update firefox-nightly.rb

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'firefox-nightly' do
-  version '39.0a1'
+  version '40.0a1'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-#{version}.en-US.mac.dmg"
