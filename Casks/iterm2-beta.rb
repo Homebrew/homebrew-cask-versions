bf67072b0f4247a28152a3ff71a6597abfc8cf4e
Christian Torrez <zerrot@users.noreply.github.com>
2015-04-27 13:44:25 -0400

    Updated iterm2-beta to 2.1

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'iterm2-beta' do
-  version '2.0.0.20150412'
-  sha256 '8f1d2ecb24ee5c42cb6d83408ccd955207ad0daabd7b11199ce92decdeb07528'
+  version '2.1'
+  sha256 '65885d3c5d9168a0742f969e091464a92fb809691951749587a84e37529e44c8'
 
-  url 'https://iterm2.com/downloads/beta/iTerm2-2_0_0_20150412.zip'
+  url 'https://iterm2.com/downloads/beta/iTerm2-2_1.zip'
   homepage 'http://www.iterm2.com/'
   license :oss
 
