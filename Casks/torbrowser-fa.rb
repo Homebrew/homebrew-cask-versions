b8be2116fb7115fd0ddeab4ea77f4106b899f932
Victor Popkov <Dragon.vctr@gmail.com>
2015-04-29 03:39:40 +0300

    torbrowser-fa.rb: upgrade to v4.5, update url and gpg key id

--- Casks/torbrowser-fa.rb
+++ Casks/torbrowser-fa.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'torbrowser-fa' do
-  version '4.0.8'
-  sha256 'e8bba0a5056e56e08e8bb755781ab1c24261d406c8d369c3ce165b5388146ece'
+  version '4.5'
+  sha256 '5d46931da89399c6c87b7afa8da4a9f381a698e3c4b51541929eec35de40e282'
 
-  url "https://www.torproject.org/dist/torbrowser/#{version}/TorBrowser-#{version}-osx32_fa.dmg"
+  url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_fa.dmg"
   gpg "#{url}.asc",
-      :key_id => '4e2c6e8793298290'
+      :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
 
