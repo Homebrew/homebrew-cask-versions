4b3f0c485177449cf4c519d47529baa50583bb20
Yurii Kolesnykov <yurikoles@gmail.com>
2015-05-04 19:39:15 +0300

    modify virtualbox urls

--- Casks/virtualbox422492790.rb
+++ Casks/virtualbox422492790.rb
@@ -2,7 +2,7 @@ cask :v1 => 'virtualbox422492790' do
   version '4.2.24-92790'
   sha256 '46b422e62118a75519d24b7a0dd3a84081da3827734b25e1ee121eed5c0b6af9'
 
-  url "http://download.virtualbox.org/virtualbox/4.2.24/VirtualBox-#{version}-OSX.dmg"
+  url "http://download.virtualbox.org/virtualbox/#{version.split('-')[0]}/VirtualBox-#{version}-OSX.dmg"
   homepage 'http://www.virtualbox.org'
   license :unknown
 
