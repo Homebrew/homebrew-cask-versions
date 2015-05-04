b24653139d441f00a26b126d69f5da415ca5db0f
Yurii Kolesnykov <yurikoles@gmail.com>
2015-05-04 19:20:01 +0300

    virtualbox 5.0 beta2
    Closes #956.

---
+++ Casks/virtualbox-beta.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'virtualbox-beta' do
+  version '5.0.0_BETA2-99639'
+  sha256 '3ed03339ba395900ef01824c86e14657a4e5d4aec1c630172e7541b6c585d49a'
+
+  url "http://download.virtualbox.org/virtualbox/#{version.split('-')[0]}/VirtualBox-#{version}-OSX.dmg"
+  homepage 'http://www.virtualbox.org'
+  license :unknown
+
+  pkg 'VirtualBox.pkg'
+  uninstall :script => { :executable => 'VirtualBox_Uninstall.tool', :args => %w[--unattended] }
+end
