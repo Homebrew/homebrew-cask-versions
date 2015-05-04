577a380f12fadcc8c677f6e58209917c9188f974
Yurii Kolesnykov <yurikoles@gmail.com>
2015-05-04 19:39:15 +0300

    modify virtualbox urls

--- Casks/virtualbox43691406.rb
+++ Casks/virtualbox43691406.rb
@@ -2,7 +2,7 @@ cask :v1 => 'virtualbox43691406' do
   version '4.3.6-91406'
   sha256 '60678b7cc7dc741b800cfc99c7ff73a185c548af877071eb82422f354e76bca2'
 
-  url "http://download.virtualbox.org/virtualbox/4.3.6/VirtualBox-#{version}-OSX.dmg"
+  url "http://download.virtualbox.org/virtualbox/#{version.split('-')[0]}/VirtualBox-#{version}-OSX.dmg"
   homepage 'http://www.virtualbox.org'
   license :unknown
 
