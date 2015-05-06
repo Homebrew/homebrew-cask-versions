c2cd5bff6b9c104d2f50cba7a9f6aefa755330be
Murray Tannock <murraytannock@gmail.com>
2015-05-06 11:26:41 +0000

    Add julia-nightly.

---
+++ Casks/julia-nightly.rb
@@ -0,0 +1,17 @@
+cask :v1 => 'julia-nightly' do
+  version '0.4.0-90f7cd0980'
+  sha256 'f0fe1adf14f065772473a3bafa35b59a808b142861134396c47f43e4da8db49b'
+
+  # amazonaws.com is the official download host per vendor homepage
+  url "https://s3.amazonaws.com/julianightlies/bin/osx/x64/#{version.sub(%r{(\d+\.\d+).*},'\1')}/julia-#{version}-osx.dmg"
+  name 'Julia'
+  homepage 'http://julialang.org'
+  license :mit
+
+  app "Julia-#{version.sub(%r{(.+)-(.+)},'\1-dev-\2')}.app"
+  binary "Julia-#{version.sub(%r{(.+)-(.+)},'\1-dev-\2')}.app/Contents/Resources/julia/bin/julia"
+
+  depends_on :macos => '>= :lion'
+
+  zap :delete => '~/.julia'
+end
