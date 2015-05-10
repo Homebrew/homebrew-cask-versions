5aa65269ee50019636d47a8f362dac3dfec7a92f
Michael <fanquake@gmail.com>
2015-05-10 19:52:07 +0800

    Thunderbird 38.0b4

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-beta' do
-  version '37.0b1'
-  sha256 'a035e47ea01639df6f5fe05e7e115814fcca04005ef329a06968e4ab86c354e7'
+  version '38.0b4'
+  sha256 '07e8d88b0a1c37b9770f24b35e513fd27eefb7d34dca20236499692573739503'
 
   url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg"
   homepage 'https://www.mozilla.org/en-US/thunderbird/all-beta.html'
