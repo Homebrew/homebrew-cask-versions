c3ab01ae4e68b0ebeb773bfa38b3f1a0f174981e
Michael <fanquake@gmail.com>
2015-05-10 21:51:58 +0800

    IntelliJ IDEA CE EAP
    http://download.jetbrains.com/idea/ideaIC-141.1009.5.dmg

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'intellij-idea-ce-eap' do
-  version '141.175.3'
-  sha256 '888a873904f3db7fcb6e5fcf07057f8a57bcca9587c5f1f5717dbd52a56676d1'
+  version '141.1009.5'
+  sha256 '185afd9f8d164e9488c751d09ce3b7bc8cf1bc2299f897c58f3c533c31fb10cd'
 
   url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14.1+EAP'
