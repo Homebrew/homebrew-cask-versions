39a81426b885311237bf082c481e15825823b930
Michael <fanquake@gmail.com>
2015-05-10 19:21:39 +0800

    Adium Nightly 1.5.11b2r5922

--- Casks/adium-nightly15.rb
+++ Casks/adium-nightly15.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'adium-nightly15' do
-  version '1.5.11hgr5899'
-  sha256 'e07b3a29b0ed0dcf97a5d77a18b32653c6a6ac598b8c60ebf2d2cf333d4f9275'
+  version '1.5.11b2r5922'
+  sha256 '828c2e2f21a2e500aeb580d018afba0f360956334d655a8280d4f9ba09af7b9d'
 
   url "http://nightly.adium.im/adium-adium-1.5.11/Adium_#{version}.dmg"
   homepage 'http://nightly.adium.im/?repo_branch=adium-adium-1.5.11'
