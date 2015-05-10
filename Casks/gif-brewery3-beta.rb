51d15fe123d14b81752d8a481707fe599fa666f7
Michael <fanquake@gmail.com>
2015-05-10 22:18:29 +0800

    Gif Brewery3 Beta7
    http://www.helloresolven.com/betas/gifbrewery/GIFBrewery3-beta7.zip

--- Casks/gif-brewery3-beta.rb
+++ Casks/gif-brewery3-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'gif-brewery3-beta' do
-  version '3.0-b3'
-  sha256 'b017f00a6a8f3a563ae74d405d2140d2a5ea1a438c482a4ce51716785d2df254'
+  version '3.0-b7'
+  sha256 '6caba96079a4dff44c7825d5934be07b7a66cab1307771f873734673d99d1906'
 
   url "http://www.helloresolven.com/betas/gifbrewery/GIFBrewery-#{version}.zip"
   homepage 'http://www.helloresolven.com/betas/gifbrewery/'
