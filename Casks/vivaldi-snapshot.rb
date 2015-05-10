6b5d809f06e2ea382f66ad906abc76d5d11efa3e
Victor Popkov <Dragon.vctr@gmail.com>
2015-05-10 11:33:53 +0300

    Upgrade Vivaldi.app to v1.0.167.2 (Snapshot)

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vivaldi-snapshot' do
-  version '1.0.156.2'
-  sha256 '5bf824d69cc78746c0644b4b257e79adc49a4960ff690b3f7a09bca7f1c0d3a9'
+  version '1.0.167.2'
+  sha256 'ded8ebb78e7f49bfdc56ed4444782dc90ec0d51965c672076f5c8e5109515608'
 
   url "https://vivaldi.com/download/download.php?f=Vivaldi.#{version}.dmg"
   name 'Vivaldi'
