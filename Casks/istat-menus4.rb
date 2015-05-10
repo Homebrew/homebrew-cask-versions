a7f749f956eef0113f2154973a8c85d59e934e07
Michael <fanquake@gmail.com>
2015-05-10 22:37:09 +0800

    Update istat menus 4 download link to https

--- Casks/istat-menus4.rb
+++ Casks/istat-menus4.rb
@@ -2,7 +2,7 @@ cask :v1 => 'istat-menus4' do
   version '4.22'
   sha256 '4e6873b24d18578e359b9363f02b9c98bdeda999afab1aaa8802af254ecb35b6'
 
-  url "http://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip"
+  url "https://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip"
   homepage 'http://bjango.com/mac/istatmenus/'
   license :unknown
 
