b5edb2dcb67c5bd48087e741153c71899964908e
Michael <fanquake@gmail.com>
2015-05-11 21:34:34 +0800

    Oclint Nightly 0.9.dev.02251e4

--- Casks/oclint-nightly.rb
+++ Casks/oclint-nightly.rb
@@ -1,15 +1,15 @@
 cask :v1 => 'oclint-nightly' do
-  version 'nightly'
+  version '0.9.dev.02251e4'
   sha256 'b37aa6e04fe7545d7daf7a93c0afd49998946ee918ea5bb1782dd17e0deb4c2f'
 
-  url 'http://archives.oclint.org/nightly/oclint-0.9.dev.648e9af-x86_64-darwin-12.4.0.tar.gz'
+  url 'http://archives.oclint.org/nightly/oclint-#{version}-x86_64-darwin-14.0.0.tar.gz'
   homepage 'http://oclint.org'
   license :unknown
 
-  binary 'oclint-0.9.dev.648e9af/bin/oclint'
-  binary 'oclint-0.9.dev.648e9af/bin/oclint-json-compilation-database'
-  binary 'oclint-0.9.dev.648e9af/bin/oclint-xcodebuild'
-  binary 'oclint-0.9.dev.648e9af/lib/oclint', :target => '/usr/local/lib/oclint'
+  binary 'oclint-#{version}/bin/oclint'
+  binary 'oclint-#{version}/bin/oclint-json-compilation-database'
+  binary 'oclint-#{version}/bin/oclint-xcodebuild'
+  binary 'oclint-#{version}/lib/oclint', :target => '/usr/local/lib/oclint'
   caveats do
     files_in_usr_local
   end
