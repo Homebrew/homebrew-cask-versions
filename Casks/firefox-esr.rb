bd2d89e4491a2ab56c964cd3913d2df85e1b4819
Michael <fanquake@gmail.com>
2015-05-11 21:45:59 +0800

    Firefox ESR 31.6.0
    https://download.mozilla.org/?product=firefox-31.6.0esr-SSL&os=osx&lang=en-US

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-esr' do
-  version '31.5.3'
-  sha256 '90676a07495781cf5bb3e758011a156da0ba74bb6bf994a36e96ec6243f6902a'
+  version '31.6.0'
+  sha256 '5adc5cb64ba7c02e2723adab242eac4603c85650273c4563223935a6efd6a343'
 
   url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
