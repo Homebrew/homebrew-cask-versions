7c4b462aa0d6e76cd7544685cbcd385593c80787
Michael <fanquake@gmail.com>
2015-05-11 21:15:38 +0800

    Open Office FR 4.1.1

--- Casks/openoffice-fr.rb
+++ Casks/openoffice-fr.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'openoffice-fr' do
-  version '4.0.1'
-  sha256 'e8d61fe93acf484c564ddc44638f314cdaa2c6e99d6a055386df9d9e3d0e6e6d'
+  version '4.1.1'
+  sha256 '4e1fb74fbb6b714c49b7a0f82646c25a2ee749459f44fc2624b8220e7c256c23'
 
-  url "http://downloads.sourceforge.net/project/openofficeorg.mirror/#{version}/binaries/fr/Apache_OpenOffice_#{version}_MacOS_x86_install_fr.dmg"
-  homepage 'http://www.openoffice.org/fr/Telecharger/'
+  url "http://downloads.sourceforge.net/project/openofficeorg.mirror/#{version}/binaries/fr/Apache_OpenOffice_#{version}_MacOS_x86-64_install_fr.dmg"
+  homepage 'https://www.openoffice.org/fr/Telecharger/'
   license :oss
 
   app 'OpenOffice.app'
