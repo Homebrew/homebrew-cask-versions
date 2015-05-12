2194782e9657d1443d95a72195b9c94f93ddb76e
Christian Torrez <zerrot@users.noreply.github.com>
2015-05-11 22:39:48 -0400

    Updated 1password-beta to 5.4.BETA-13

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => '1password-beta' do
-  version '5.4.BETA-12'
-  sha256 '60dabcc5f560a7a43251272a96013fd429a21aad0a4a43a28bf2ffb60fa59178'
+  version '5.4.BETA-13'
+  sha256 '75a3b4ce185603f4d73bb85b684acdb6ec61d82297d41a0f67e2b0ff8b9e28c0'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
