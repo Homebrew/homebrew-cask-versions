b03f98c65acdcd5ae13bb9dda7627a6be9ebc916
Michael <fanquake@gmail.com>
2015-05-12 15:50:53 +0800

    Update Acorn3 URL and hompage

--- Casks/acorn3.rb
+++ Casks/acorn3.rb
@@ -2,8 +2,8 @@ cask :v1 => 'acorn3' do
   version '3.5.2'
   sha256 'ffc4cd551b9eb2ebadfe8e59c95e84b1f59538d7915eff63dd6c3efdca7858e6'
 
-  url "http://flyingmeat.com/download/Acorn-#{version}.zip"
-  homepage 'http://flyingmeat.com/acorn/'
+  url "https://secure.flyingmeat.com/download/Acorn-#{version}.zip"
+  homepage 'https://secure.flyingmeat.com/acorn/'
   license :closed
 
   app 'Acorn.app'
