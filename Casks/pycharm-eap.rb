0d7dfd1e37406beb087f4cfe13a6ff060a7cddd7
Michael <fanquake@gmail.com>
2015-05-12 15:39:37 +0800

    PyCharm EAP 141.1075

--- Casks/pycharm-eap.rb
+++ Casks/pycharm-eap.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'pycharm-eap' do
-  version '139.1525'
-  sha256 '9ee396a9971001c7912930fe0c054e3154127f7c09f8be77a71d42180887c8b9'
+  version '141.1075'
+  sha256 'ec9c79b709142364b5315dd0d98fc9b9a9ee0d2c15fe9745e1e4fceb35620578'
 
   url "http://download.jetbrains.com/python/pycharm-professional-#{version}.dmg"
   homepage 'http://confluence.jetbrains.com/display/PYH/JetBrains+PyCharm+Preview+(EAP)'
   license :unknown
 
-  app 'PyCharm 4.0.5 EAP.app'
+  app 'PyCharm 4.5 EAP.app'
 end
