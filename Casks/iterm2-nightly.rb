6d6778c36022c4b62109bd8dc598c7cad59a0956
Michael <fanquake@gmail.com>
2015-05-12 15:54:39 +0800

    Update Iterm2 Nightly URL and Homepage

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -2,8 +2,8 @@ cask :v1 => 'iterm2-nightly' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.iterm2.com/nightly/latest'
-  homepage 'http://www.iterm2.com/'
+  url 'https://www.iterm2.com/nightly/latest'
+  homepage 'https://www.iterm2.com/'
   license :oss
 
   app 'iTerm.app'
