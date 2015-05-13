857283f8102cb86ce7197301384fa3773ebb912a
Ian Walter <ianwalter@fastmail.com>
2015-05-13 10:40:42 -0400

    Fixing version, sha256, and url

--- Casks/libreoffice43.rb
+++ Casks/libreoffice43.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'libreoffice43' do
-  version '4.3.6'
-  sha256 '02b78ed4e58090af93782fa6986493c115e92eae9bbc878c26cbd12633735445'
+  version '4.3.7'
+  sha256 'ad8cb940218ac52e240a7e26e6b079da31eb6f6205da732802b445d474353e8f'
 
-  url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86.dmg"
+  url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   name 'LibreOffice'
   homepage 'https://www.libreoffice.org/'
   license :mpl
