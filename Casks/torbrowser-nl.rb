4e195b4b55377ae88155c9e7875bf99aaf9f59f7
Michael Ford <fanquake@gmail.com>
2015-05-13 20:43:03 +0800

    Tor Browser NL 4.5.1

--- Casks/torbrowser-nl.rb
+++ Casks/torbrowser-nl.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'torbrowser-nl' do
-  version '4.5'
-  sha256 'ed351f032d93b8cdb4e2e4d4af6ee6ca400864b8e7e7be9f861da549e42beb03'
+  version '4.5.1'
+  sha256 '4e754dc76b95c65a8fb1674c20f8aff09ed220349ed05c7cfc30463de030b49a'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_nl.dmg"
   gpg "#{url}.asc",
       :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
+  name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
 
