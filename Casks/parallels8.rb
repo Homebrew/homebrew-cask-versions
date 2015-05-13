58f433325220e00217a6d40c886e3333ba270019
Michael <fanquake@gmail.com>
2015-05-13 21:10:27 +0800

    Use #{version} in Parallels8 URL

--- Casks/parallels8.rb
+++ Casks/parallels8.rb
@@ -2,7 +2,7 @@ cask :v1 => 'parallels8' do
   version '8.0.18619.1001606'
   sha256 '5b87b2c240e71176cad5097ed7f1928598c9e001da4ad1d8a4516708908c4305'
 
-  url 'http://download.parallels.com/desktop/v8/update3.hotfix1/ParallelsDesktop-8.0.18619.1001606.dmg'
+  url 'http://download.parallels.com/desktop/v8/update3.hotfix1/ParallelsDesktop-#{version}.dmg'
   homepage 'http://kb.parallels.com/en/114623'
   license :closed
 
