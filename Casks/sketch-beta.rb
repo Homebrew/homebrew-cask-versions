cd4bd71c2be19b930492d2ae266c7662caeed25e
Michael <fanquake@gmail.com>
2015-05-13 21:17:13 +0800

    Sketch Beta 3.3.2

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sketch-beta' do
-  version '3.3'
-  sha256 '24324aa78755812667041afb99f5731c6c6f4d26d398b8e0ffaac7f7dc035d59'
+  version '3.3.2'
+  sha256 'd85fb4b25dcc281a4211b73a668cf9aa25caa3fb9e50875b4d6fafec65ad1bc1'
 
-  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/76?format=zip&avtoken=52bdf01b70c97772a8ed003204ac0e34529a9bc6'
+  url 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/95?format=zip&avtoken=cf895f8e545b066b458d504fe4e3d8eeecc732db'
   homepage 'http://bohemiancoding.com/sketch/beta/'
   license :commercial
 
