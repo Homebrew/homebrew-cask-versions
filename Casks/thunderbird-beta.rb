4502f549f626c9657fdc4144ff461af3e51454b0
Michael Ford <fanquake@gmail.com>
2015-05-13 18:40:09 +0800

    Add tag stanza to Thunderbird-beta
    Update URL

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -2,9 +2,10 @@ cask :v1 => 'thunderbird-beta' do
   version '38.0b4'
   sha256 '07e8d88b0a1c37b9770f24b35e513fd27eefb7d34dca20236499692573739503'
 
-  url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg"
+  url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/thunderbird/all-beta.html'
   license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Thunderbird.app'
 end
