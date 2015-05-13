d3b5428677616da812698ab8a0d9196144f2b533
Michael Ford <fanquake@gmail.com>
2015-05-13 18:43:01 +0800

    Thunderbird JA 31.6.0
    Update homepage URL
    Update license
    Add tag stanza

--- Casks/thunderbird-ja.rb
+++ Casks/thunderbird-ja.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'thunderbird-ja' do
-  version '31.1.1'
-  sha256 'aa5da47afcc4f71e40f86b94c6033d86d707690a80a8610e1a5f9c09a5ca4629'
+  version '31.6.0'
+  sha256 '6b6210481826e5365da88f1460cd0b5a2936aa9b601b5482faecbde34b1fd5c0'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=ja-JP-mac"
-  homepage 'http://www.mozilla.jp/thunderbird/'
-  license :oss
+  homepage 'https://www.mozilla.jp/thunderbird/'
+  license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Thunderbird.app'
 end
