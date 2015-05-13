9f6aeef276287439d227923573e09399269d3314
Michael Ford <fanquake@gmail.com>
2015-05-13 18:56:26 +0800

    Firefox Beta CN 38.0.5b1
    Add tag stanza

--- Casks/firefox-beta-cn.rb
+++ Casks/firefox-beta-cn.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'firefox-beta-cn' do
-  version '38.0b9'
-  sha256 '3fa651cb44d00ec4ec0f5fb1d7d274096febb79ccab838790242d23b0efd6895'
+  version '38.0.5b1'
+  sha256 '62438a8eff9b2c31ecd45e2a0f5a7e916473d2ee158ee56e89925b691e43e986'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
   homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
   license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
 end
