906156b35b750fb8b979ad4ae8367214442db939
Michael Ford <fanquake@gmail.com>
2015-05-13 20:44:54 +0800

    TorBrowser RU 4.5.1

--- Casks/torbrowser-ru.rb
+++ Casks/torbrowser-ru.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'torbrowser-ru' do
-  version '4.5'
-  sha256 '2bad98adaeef9beab8c47cb6e881efba0f865184a8f51ae04410f8d46f9d1891'
+  version '4.5.1'
+  sha256 '96b39fe5152ca286f405b91095c580c410ca36432949a323de6f1c4dde3f45cb'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_ru.dmg"
   gpg "#{url}.asc",
       :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
+  name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
 
