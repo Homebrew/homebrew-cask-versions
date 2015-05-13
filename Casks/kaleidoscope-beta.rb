60011862d5bf6e5f6b3d875e982580c11e15aef9
Michael <fanquake@gmail.com>
2015-05-13 21:13:31 +0800

    Kaleidoscope Beta 2.1.0-134

--- Casks/kaleidoscope-beta.rb
+++ Casks/kaleidoscope-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'kaleidoscope-beta' do
   version '2.1.0'
-  sha256 '28605375d3a5ab8e4d65f5b03ccb8d0601ad1065abb33b02b3c4a879e39a7424'
+  sha256 'e16360273fb6d37560c7974b95728d82a0482268641753c49f903bf36445d35b'
 
-  url "http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-#{version}-131.zip"
+  url "http://cdn.kaleidoscopeapp.com/releases/Kaleidoscope-#{version}-134.zip"
   homepage 'http://www.kaleidoscopeapp.com/'
   license :unknown
 
