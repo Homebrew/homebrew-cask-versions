75d5876facd685a200ba838bac3a91c7e11883c5
Michael Ford <fanquake@gmail.com>
2015-05-13 21:00:07 +0800

    Add tag stanza to Firefox Developer Edition JA

--- Casks/firefoxdeveloperedition-ja.rb
+++ Casks/firefoxdeveloperedition-ja.rb
@@ -5,6 +5,7 @@ cask :v1 => 'firefoxdeveloperedition-ja' do
   url "https://download.mozilla.org/?product=firefox-aurora-latest-l10n&os=osx&lang=ja-JP-mac"
   homepage 'https://www.mozilla.org/ja/firefox/developer/'
   license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'FirefoxDeveloperEdition.app'
 end
