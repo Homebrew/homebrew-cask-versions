d20b05adb20263c0fdb329afa45b327b4cb18ecf
Michael Ford <fanquake@gmail.com>
2015-05-13 21:01:58 +0800

    Firefox ESR RU 38.0
    Add tag stanza

--- Casks/firefox-esr-ru.rb
+++ Casks/firefox-esr-ru.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'firefox-esr-ru' do
-  version '31.5.0'
-  sha256 'b708414c5265cd75b147a129efeccd37d053fa67c1011c22cb80adc528950d18'
+  version '38.0'
+  sha256 'af4e2aef6ed0cd43ef5f2fb1214e34b5ffa79cf3b090a3eca12db77cec672240'
 
   url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=ru"
   homepage 'https://www.mozilla.org/ru/firefox/organizations/'
   license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
 end
