56df0250040cae601f3550e3a63da2ebb879cf4a
Michael Ford <fanquake@gmail.com>
2015-05-13 20:24:47 +0800

    Firefox Beta JA 38.0.5b1
    Add tag stanza

--- Casks/firefox-beta-ja.rb
+++ Casks/firefox-beta-ja.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'firefox-beta-ja' do
-  version '37.0b7'
-  sha256 '456e4d84f544edd57659ba64b3d98b98d10e4b48dad2dfa64429ec963c5e39a4'
+  version '38.0.5b1'
+  sha256 'e3da6aa7b564b09221b84576884f62be5d079320faba8276030d93ac44b2a229'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
-  homepage 'http://www.mozilla.org/ja/firefox/channel/beta'
+  homepage 'https://www.mozilla.org/ja/firefox/channel/beta'
   license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
 end
