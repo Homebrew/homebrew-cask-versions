d4be33f89f295768b6208af6651502f487545a7f
Michael Ford <fanquake@gmail.com>
2015-05-13 20:51:26 +0800

    Firefox Beta UK 38.0.5b1

--- Casks/firefox-beta-uk.rb
+++ Casks/firefox-beta-uk.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'firefox-beta-uk' do
-  version '37.0b7'
-  sha256 '7b75f3e583f5b244a09750afb27b0d1cc9f6771c2066f59f959f8d7cf58b3b4a'
+  version '38.0.5b1'
+  sha256 '5079c8f030ba0ba9669fbce189a13bb4a1c7e2e2f58bf56e5e823109089ac7f2'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=uk"
   homepage 'https://www.mozilla.org/uk/firefox/channel/#beta'
   license :mpl
+  tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
 end
