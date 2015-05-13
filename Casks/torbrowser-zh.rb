fb6d6e9a754a411ff484a0a502325ebba386d4ac
Michael Ford <fanquake@gmail.com>
2015-05-13 20:46:41 +0800

    TorBrowser ZH 4.5.1

--- Casks/torbrowser-zh.rb
+++ Casks/torbrowser-zh.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'torbrowser-zh' do
-  version '4.5'
-  sha256 'f6a6a8f592aa4c5ca7f424033f791f34cf4b96c89f1182b97c02584652b8070b'
+  version '4.5.1'
+  sha256 'a66e99de1b1e3a3fdd32c6954c9f635ae157e47c89a0888240d30d7e2c363aed'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_zh-CN.dmg"
   gpg "#{url}.asc",
       :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
+  name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
 
