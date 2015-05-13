31703ea33f98e111efbb7d4e9783acd06a99d7fd
Michael Ford <fanquake@gmail.com>
2015-05-13 20:37:53 +0800

    TorBrowser FA 4.5.1

--- Casks/torbrowser-fa.rb
+++ Casks/torbrowser-fa.rb
@@ -1,10 +1,11 @@
 cask :v1 => 'torbrowser-fa' do
-  version '4.5'
-  sha256 '5d46931da89399c6c87b7afa8da4a9f381a698e3c4b51541929eec35de40e282'
+  version '4.5.1'
+  sha256 'faf5da971b676af179404fb384a4545d1d737d6ab14577440ba75e42ac9c84fd'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_fa.dmg"
   gpg "#{url}.asc",
       :key_id => 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
+  name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   license :oss
 
