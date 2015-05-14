f36ec420fb0dcbcbc1eb7e24682b0459f5a3faf3
Jiangge Zhang <tonyseek@gmail.com>
2015-05-14 15:05:23 +0800

    Upgrade iTerm2 Beta to 2.1.1

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'iterm2-beta' do
-  version '2.1'
-  sha256 '65885d3c5d9168a0742f969e091464a92fb809691951749587a84e37529e44c8'
+  version '2.1.1'
+  sha256 'b8f1bbd11cdb3e26fd9fab6971c28ebeb422361b2cc5fd6e4a843836d5dedeb0'
 
-  url 'https://iterm2.com/downloads/beta/iTerm2-2_1.zip'
+  url 'https://iterm2.com/downloads/beta/iTerm2-2_1_1.zip'
   homepage 'http://www.iterm2.com/'
   license :oss
 
