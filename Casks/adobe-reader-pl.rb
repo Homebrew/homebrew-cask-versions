a95b123424a85bd95965479396b80d856cf2a76d
pgkos <pg.kosinski@gmail.com>
2015-05-15 18:29:33 +0200

    Add Adobe Reader 11 Polish version

---
+++ Casks/adobe-reader-pl.rb
@@ -0,0 +1,20 @@
+cask :v1 => 'adobe-reader-pl' do
+  version '11.0.00'
+  sha256 'cfeeef917cec3175ab7673c9afd9df081794d952c5e919e29a962df21e041b37'
+
+  url "http://ardownload.adobe.com/pub/adobe/reader/mac/#{version.to_i}.x/#{version}/pl_PL/AdbeRdr#{version.gsub('.', '')}_pl_PL.dmg"
+  name 'Adobe Reader'
+  homepage 'http://www.adobe.com/pl/products/reader.html'
+  license :gratis
+  tags :vendor => 'Adobe'
+
+  pkg 'Adobe Reader XI Installer.pkg'
+
+  uninstall :pkgutil => "com.adobe.acrobat.reader.#{version.gsub('.', '')}.*",
+            :delete => '/Applications/Adobe Reader.app'
+  zap       :delete => [
+                        "~/Library/Application Support/Adobe/Acrobat/#{version.sub(%r{(\d+)\.(\d+).*},'\1.\2')}",
+                        '~/Library/Preferences/com.adobe.Reader.plist',
+                        '~/Library/Caches/com.adobe.Reader'
+                       ]
+end
