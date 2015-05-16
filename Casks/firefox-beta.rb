37233a265aa15e8d2a74f30bba62750818207a58
David Staron <david@staron.nl>
2015-05-16 22:46:06 +0200

    Upgrade Firefox-beta to 38.0.5b2

--- Casks/firefox-beta.rb
+++ Casks/firefox-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta' do
-  version '38.0.5b1'
-  sha256 'e3da6aa7b564b09221b84576884f62be5d079320faba8276030d93ac44b2a229'
+  version '38.0.5b2'
+  sha256 '51054175be49e60f1210ab76cd9cd46116d9c272efc3181c660f277f8a727216'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-US"
   homepage 'https://www.mozilla.org/en-US/firefox/channel/#beta'
