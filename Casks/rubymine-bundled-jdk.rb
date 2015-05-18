9a795b5e68c352e316bdb31733309e916228f7dc
Sebastian Röder <no-spam-github@sebroeder.de>
2015-05-18 14:47:54 +0200

    Upgrade RubyMine with bundled JDK to 7.1.2
    Also add conflicts_with stanza for RubyMine without bundled JDK

--- Casks/rubymine-bundled-jdk.rb
+++ Casks/rubymine-bundled-jdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rubymine-bundled-jdk' do
-  version '7.1'
-  sha256 '2e9fced43c8e14ffbc82a72a8f6cde1cbab50861db079162ca5ff34c668ba57c'
+  version '7.1.2'
+  sha256 '8d96d6b0484039055335618939a50b25a97cf13c93c2137dbafe5e4986725095'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}-custom-jdk-bundled.dmg"
   name 'RubyMine'
@@ -17,4 +17,6 @@ cask :v1 => 'rubymine-bundled-jdk' do
                   '~/Library/Logs/RubyMine70',
                   '/usr/local/bin/mine',
                  ]
+
+  conflicts_with :cask => 'rubymine'
 end
