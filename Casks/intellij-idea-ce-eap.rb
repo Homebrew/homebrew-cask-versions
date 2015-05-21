8d488892f6a5686efe9aca234d0baeae433bf936
Yurii Kolesnykov <yurikoles@gmail.com>
2015-05-22 00:40:20 +0300

    intellij-idea-ce-eap 141.1192.2

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'intellij-idea-ce-eap' do
-  version '141.1009.5'
-  sha256 '185afd9f8d164e9488c751d09ce3b7bc8cf1bc2299f897c58f3c533c31fb10cd'
+  version '141.1192.2'
+  sha256 '18f8ecfa66e2e687cab6ddd844bde8996ecb54eb93279a8e8ce5e43a9aa6def3'
 
   url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14.1+EAP'
-  license :oss
+  license :apache
 
   app 'IntelliJ IDEA 14 CE EAP.app'
 end
