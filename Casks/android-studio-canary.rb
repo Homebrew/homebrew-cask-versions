3198d8a5802969739f97795bebf20a728a2c70ad
Yurii Kolesnykov <yurikoles@gmail.com>
2015-05-22 00:02:13 +0300

    android-studio-canary 1.2.1.1

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'android-studio-canary' do
-  version '1.2.0.12'
-  sha256 '203562c1696506c3c0b7c6ab240d06fdcc903db7ae8c9287d6beb1183bbef914'
+  version '1.2.1.1'
+  sha256 '217dc31340a6068bbc32e448595332e4289346e558bfc513e8ae7d50cb1f21c2'
 
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.1890965-mac.zip"
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.1903250-mac.zip"
   homepage 'http://tools.android.com/download/studio'
   license :apache
 
@@ -11,7 +11,7 @@ cask :v1 => 'android-studio-canary' do
   caveats <<-EOS.undent
     If you have Java 7 or above installed, you may want to use it as Android Studio JDK, for example:
 
-    export STUDIO_JDK=/Library/Java/JavaVirtualMachines/jdk1.8.0_25.jdk
+    export STUDIO_JDK=/Library/Java/JavaVirtualMachines/jdk1.8.0_45.jdk
 
     Please take a look at this post: http://tools.android.com/recent/androidstudio1rc3_releasecandidate3released
   EOS
