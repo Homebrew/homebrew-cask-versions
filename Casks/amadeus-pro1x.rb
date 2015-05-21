f1e56c0bcf7c5236220487b7eb7e1d428ef73018
shimeike <shimeike@gmail.com>
2015-05-21 15:53:37 +1000

    Added Amadeus Pro.app v1.5.5
    - legacy version of this app

---
+++ Casks/amadeus-pro1x.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'amadeus-pro1x' do
+  version '1.5.5'
+  sha256 '29e5d6a2567c9b1a418667b99844de186a6621992f3398583c959aed2383d0b6'
+
+  url 'http://www.hairersoft.com/Downloads/AmadeusProOld.zip'
+  name 'Amadeus Pro'
+  homepage 'http://www.hairersoft.com/pro.html'
+  license :unknown
+
+  app 'Amadeus Pro.app'
+end
