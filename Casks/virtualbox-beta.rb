e91bb2c69bd9b10e4ab2fad99892a43fca21a303
Yurii Kolesnykov <yurikoles@gmail.com>
2015-05-21 23:53:20 +0300

    virtualbox 5.0 beta4

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'virtualbox-beta' do
-  version '5.0.0_BETA3-100143'
-  sha256 '98df952b5b7102ce670502a6f1b95b1a78dcf44faf2c69a8970dc6da46f3ea50'
+  version '5.0.0_BETA4-100374'
+  sha256 '5102194a9e5f14d0497998b58c95f8616fc45c54e2f753e80ebd668f90a51562'
 
   url "http://download.virtualbox.org/virtualbox/#{version.split('-')[0]}/VirtualBox-#{version}-OSX.dmg"
   homepage 'http://www.virtualbox.org'
