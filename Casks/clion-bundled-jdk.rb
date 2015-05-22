cc408a35ca2b447d6400688bb760a3d51ea1fce4
Andy Stanton <andystanton@gmail.com>
2015-05-22 13:32:14 +0100

    clion-bundled-jdk 1.0.3

--- Casks/clion-bundled-jdk.rb
+++ Casks/clion-bundled-jdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'clion-bundled-jdk' do
-  version '1.0.2'
-  sha256 '0f96e6320a5edbd721a60fed880034f800eb906b56d18290e37877e53a18048b'
+  version '1.0.3'
+  sha256 'ecdbae5b037422f56ff7175d617808324ec0df7c2474a4852841937f285c301f'
 
   url "https://download.jetbrains.com/cpp/CLion-#{version}-custom-jdk-bundled.dmg"
   name 'CLion'
