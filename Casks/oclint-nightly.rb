9b0ae7fae5e13185e42f81ac336fc4c3e2083530
Anne Celestino <hubwub@tagspace.me>
2015-05-24 11:54:57 -0500

    Fixed oclint-nightly cask
    - oclint-nightly fails on brew cask audit due to URL
    - the URL has single quotes originally, changed to double quotes
    - Double quotes needed if using #{version}

--- Casks/oclint-nightly.rb
+++ Casks/oclint-nightly.rb
@@ -2,14 +2,14 @@ cask :v1 => 'oclint-nightly' do
   version '0.9.dev.02251e4'
   sha256 'b37aa6e04fe7545d7daf7a93c0afd49998946ee918ea5bb1782dd17e0deb4c2f'
 
-  url 'http://archives.oclint.org/nightly/oclint-#{version}-x86_64-darwin-14.0.0.tar.gz'
+  url "http://archives.oclint.org/nightly/oclint-#{version}-x86_64-darwin-14.0.0.tar.gz"
   homepage 'http://oclint.org'
   license :unknown
 
-  binary 'oclint-#{version}/bin/oclint'
-  binary 'oclint-#{version}/bin/oclint-json-compilation-database'
-  binary 'oclint-#{version}/bin/oclint-xcodebuild'
-  binary 'oclint-#{version}/lib/oclint', :target => '/usr/local/lib/oclint'
+  binary "oclint-#{version}/bin/oclint"
+  binary "oclint-#{version}/bin/oclint-json-compilation-database"
+  binary "oclint-#{version}/bin/oclint-xcodebuild"
+  binary "oclint-#{version}/lib/oclint", :target => '/usr/local/lib/oclint'
   caveats do
     files_in_usr_local
   end
