63df8be909a766ea19e7b6bd13dcb0ed77a8a12d
Anne Celestino <hubwub@tagspace.me>
2015-05-24 10:58:28 -0500

    oclint-nightly fails on brew cask audit due to URL

--- Casks/oclint-nightly.rb
+++ Casks/oclint-nightly.rb
@@ -2,7 +2,7 @@ cask :v1 => 'oclint-nightly' do
   version '0.9.dev.02251e4'
   sha256 'b37aa6e04fe7545d7daf7a93c0afd49998946ee918ea5bb1782dd17e0deb4c2f'
 
-  url 'http://archives.oclint.org/nightly/oclint-#{version}-x86_64-darwin-14.0.0.tar.gz'
+  url "http://archives.oclint.org/nightly/oclint-#{version}-x86_64-darwin-14.0.0.tar.gz"
   homepage 'http://oclint.org'
   license :unknown
 
