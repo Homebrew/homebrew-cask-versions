4ad9a9ecc63ccb801426dfd58c50eecbf3a1152b
mdv <melnikovdv@gmail.com>
2015-06-01 11:09:01 +0300

    android-studio-canary 1.3.0.0 fixed sha256 mismatch

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'android-studio-canary' do
   version '1.3.0.0'
-  sha256 'ccfc0b85914420c28c5594b7d43c8445e687ce8749fee9edcc7afb31bd333b01'
+  sha256 '2b87c2f7b690869285def936721ab0d37a6767873ae94cf4805f888dd240fd26'
 
   url "http://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.1962279-mac.zip"
   homepage 'http://tools.android.com/download/studio'
