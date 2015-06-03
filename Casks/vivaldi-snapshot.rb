05d0dc2cb6da3a592c190da3b2ad10d32faeecc2
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-03 05:41:55 +0300

    Update sha256 checksum for Vivaldi.app (Snapshot)

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vivaldi-snapshot' do
   version '1.0.190.2'
-  sha256 'ec0c8e79d566a815626f208d09e88102e78932a4c857eb293328415d7ecc02e9'
+  sha256 'e82ae34e6960b47cf21ba271b2e0002e409cda1695c8efb6037b30e517abae69'
 
   url "https://vivaldi.com/download/download.php?f=Vivaldi.#{version}.dmg"
   name 'Vivaldi'
