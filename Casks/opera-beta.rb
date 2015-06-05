6c5d7a4d176958513217671ea1e1e6a4deb2d719
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-05 12:07:58 +0300

    Upgrade Opera Beta.app to v30.0.1835.49

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera-beta' do
-  version '30.0.1835.47'
-  sha256 '97817b3a4bbf6eb7e92493f241665012f42592b3b404e888ad3cb05f61c8b39c'
+  version '30.0.1835.49'
+  sha256 '561fa244e03e91056655248f627b28294ae4e3bb5102c5db41737c3bd0db13d3'
 
   url "http://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/computer/beta'
