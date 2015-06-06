deb117c826ea7af9e27181f0d9cc311bcdb2f499
Vítor Galvão <info@vitorgalvao.com>
2015-06-06 23:20:03 +0100

    processing3.rb: version 3.0a9

--- Casks/processing3.rb
+++ Casks/processing3.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'processing3' do
-  version '3.0a5'
-  sha256 '4aca3ca91e83c01a27ebb16f34360ef9dc3678397ab10d2a305653a910dbb55f'
+  version '3.0a9'
+  sha256 '1eb6df0322354ffd1a4805048b2b1102f9b397e49fba54fa6b993b0fe01e9d09'
 
   url "http://download.processing.org/processing-#{version}-macosx.zip"
   homepage 'http://processing.org/'
-  license :unknown
+  license :gpl
 
   app 'Processing.app'
 end
