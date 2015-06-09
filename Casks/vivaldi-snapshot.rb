09b145fb3680bb638f7ddd922c8cf5fe570c45a5
Victor Popkov <Dragon.vctr@gmail.com>
2015-06-09 17:04:55 +0300

    vivaldi-snapshot.rb: upgrade to v1.0.196.2

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'vivaldi-snapshot' do
-  version '1.0.190.2'
-  sha256 'e82ae34e6960b47cf21ba271b2e0002e409cda1695c8efb6037b30e517abae69'
+  version '1.0.196.2'
+  sha256 '7f8c6d39b490c6212d5aae88a02ff9c067239896dfab244f9c3e89a515371b9f'
 
   url "https://vivaldi.com/download/download.php?f=Vivaldi.#{version}.dmg"
   name 'Vivaldi'
