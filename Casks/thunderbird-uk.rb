1bfff178333d0702cfd6211984c76a0619a8ccdf
Yurii Kolesnykov <yurikoles@gmail.com>
2015-06-16 19:07:51 +0300

    Update localized thunderbirds to 38.0.1; Add thunderbird-beta uk & ru

--- Casks/thunderbird-uk.rb
+++ Casks/thunderbird-uk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-uk' do
-  version '31.7.0'
-  sha256 '3849a1dfabc3809c018b4125cbc07becdbaab414ef2cf55c7f829ae79c977af5'
+  version '38.0.1'
+  sha256 'f7b3918160bac05842b295bf99bfe96cbba9697ed423dec1012319282992bc99'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=uk"
   name 'Mozilla Thunderbird'
