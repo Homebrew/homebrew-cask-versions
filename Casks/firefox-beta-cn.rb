e0e5f72742765ee04c98b5a6e3d6e1c3a4d8a089
fanquake <fanquake@gmail.com>
2015-06-17 00:04:23 +0800

    FireFox Beta CN 39.0b5

--- Casks/firefox-beta-cn.rb
+++ Casks/firefox-beta-cn.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-cn' do
-  version '38.0.5b1'
-  sha256 '62438a8eff9b2c31ecd45e2a0f5a7e916473d2ee158ee56e89925b691e43e986'
+  version '39.0b5'
+  sha256 '95c8beb8007f89ebaabacdab4c1e3a494e50e05c3c6e7d96a52c68c57b5ae58f'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
   homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
