7ad1c67126c2da02fc2c4fbd3ff711702ba382fb
fanquake <fanquake@gmail.com>
2015-06-17 00:18:55 +0800

    Firefox Beta DE 39.0b5

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-de' do
-  version '38.0.5b1'
-  sha256 '7b019293087e9852e192665a4ec538d6b2109b558f937b74b13e073f14287cf6'
+  version '39.0b5'
+  sha256 'c3e72cff81322e290296db2c1c031e77800c2a5cb879d12c023f3c5c118dbad3'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   homepage 'https://www.mozilla.org/de/firefox/beta/'
