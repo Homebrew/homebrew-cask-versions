cd1b60ec5f4a7fef39f7ac6483a2cd98f3b15afa
fanquake <fanquake@gmail.com>
2015-06-16 23:48:17 +0800

    Tor Browser TR 4.5.2

--- Casks/torbrowser-tr.rb
+++ Casks/torbrowser-tr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-tr' do
-  version '4.5.1'
-  sha256 '8d84ee1a4193f668587b36716dc9e201b51a18649ed6bf82301f87fbdba154dd'
+  version '4.5.2'
+  sha256 '75c242ae2be143e1ff30f68c91580e0ef9f5cc7866952412bc44dd88b0a9320a'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_tr.dmg"
   gpg "#{url}.asc",
