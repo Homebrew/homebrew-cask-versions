d2244be83d11a1aa45c8e53a647f548eb2cc4847
invlpg <invlpg@gmail.com>
2015-06-16 23:30:25 +0800

    Fix typo of cuda-z-beta

--- Casks/cuda-z-beta.rb
+++ Casks/cuda-z-beta.rb
@@ -2,7 +2,7 @@ cask :v1 => 'cuda-z-beta' do
   version '0.10.244-SVN'
   sha256 'd449e55f0c3c6110df03efe978c37891698d8a0d1eebc9dc76954bc8384e6ef3'
 
-  url 'https://downloads.sourceforge.net/project/cuda-z/cuda-z/Beta/CUDA-Z-#{version}.dmg'
+  url "https://downloads.sourceforge.net/project/cuda-z/cuda-z/Beta/CUDA-Z-#{version}.dmg"
   homepage 'http://cuda-z.sourceforge.net'
   license :oss
 
