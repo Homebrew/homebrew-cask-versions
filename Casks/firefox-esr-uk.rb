5898f3083928cbd68eb1d55c946458d1815dc7b5
fanquake <fanquake@gmail.com>
2015-06-17 00:49:31 +0800

    Firefox ESR UK 38.0.1

--- Casks/firefox-esr-uk.rb
+++ Casks/firefox-esr-uk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-esr-uk' do
-  version '38.0'
-  sha256 '66697d73b9d9c138953c7933fdcfdb5ac37535c35506526c441832ac2a1730c0'
+  version '38.0.1'
+  sha256 'a403737175eee03b449762aef6d87f1ddf889747e65ed6384214123bd31d7896'
 
   url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=uk"
   homepage 'https://www.mozilla.org/uk/firefox/organizations/'
