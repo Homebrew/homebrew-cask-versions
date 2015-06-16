6a7d65b982eab75da3efe7d474a98e7726fd70ee
fanquake <fanquake@gmail.com>
2015-06-17 00:45:51 +0800

    Firefox ESR RU 38.0.1

--- Casks/firefox-esr-ru.rb
+++ Casks/firefox-esr-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-esr-ru' do
-  version '38.0'
-  sha256 'af4e2aef6ed0cd43ef5f2fb1214e34b5ffa79cf3b090a3eca12db77cec672240'
+  version '38.0.1'
+  sha256 'ebc6f10a0672dd2c0283e19783066c33a0b3df1869808f148afdc5b0139ab12c'
 
   url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=ru"
   homepage 'https://www.mozilla.org/ru/firefox/organizations/'
