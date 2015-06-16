7fdcc144841731c88001413eddc6f750019df59d
fanquake <fanquake@gmail.com>
2015-06-16 23:52:24 +0800

    Tor Browser CN 4.5.2

--- Casks/torbrowser-zh.rb
+++ Casks/torbrowser-zh.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-zh' do
-  version '4.5.1'
-  sha256 'a66e99de1b1e3a3fdd32c6954c9f635ae157e47c89a0888240d30d7e2c363aed'
+  version '4.5.2'
+  sha256 'd3bef6d2ae4476949bb65f82db34aaeee5e01d8bf4b32c59f3812c15428688cc'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_zh-CN.dmg"
   gpg "#{url}.asc",
