b7d5b74b2d1fb48c6da398f9ad58acab96a2c681
Chris Narburgh <Chris.Narburgh@imc-chicago.com>
2015-06-19 12:07:29 -0500

    Update IntelliJ IDEA CE to EAP 15; version 142.2491.4.

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,10 +1,19 @@
 cask :v1 => 'intellij-idea-ce-eap' do
-  version '141.1192.2'
-  sha256 '18f8ecfa66e2e687cab6ddd844bde8996ecb54eb93279a8e8ce5e43a9aa6def3'
+  version '142.2491.4'
+  sha256 '280e852beecff74301ecd402aa07109e9387930693d9bfd6357fc327d401e776'
 
-  url "http://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
-  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+14.1+EAP'
+  url "https://download.jetbrains.com/idea/ideaIC-#{version}-custom-jdk-bundled.dmg"
+  name 'IntelliJ IDEA EAP :: CE'
+  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+15+EAP'
   license :apache
 
-  app 'IntelliJ IDEA 14 CE EAP.app'
+  app 'IntelliJ IDEA 15 CE EAP.app'
+
+  zap :delete => [
+		  '~/Library/Preferences/com.jetbrains.intellij.plist',
+                  '~/Library/Application Support/IdeaIC15',
+                  '~/Library/Preferences/IdeaIC15',
+                  '~/Library/Caches/IdeaIC15',
+                  '~/Library/Logs/IdeaIC15',
+                 ]
 end
