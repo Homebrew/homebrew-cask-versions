e17961806353e81bc6a61dbfb6ff266013ddf72e
Koichi Shiraishi <zcheeee@gmail.com>
2015-06-27 03:11:00 +0900

    Upgrade MacVim-Kaoriya to 20150623 and add binary,zap,caveats
    Signed-off-by: Koichi Shiraishi <zcheeee@gmail.com>

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -1,10 +1,25 @@
 cask :v1 => 'macvim-kaoriya' do
-  version '7.4.712-20150429'
-  sha256 'dc88078d53814eb86c860d7c9bf05d2fb4d7cd4ef76601bdf8518e8643dd4474'
+  version '7.4.712-20150623'
+  sha256 '47911a1e28daf1aef2115197750bf73ef7104a84a188195595525c148d0871f9'
 
-  url 'https://github.com/splhack/macvim-kaoriya/releases/download/20150429/macvim-kaoriya-20150429.dmg'
+  url 'https://github.com/splhack/macvim-kaoriya/releases/download/20150623/macvim-kaoriya-20150623.dmg'
   homepage 'https://github.com/splhack/macvim-kaoriya'
   license :oss
 
   app 'MacVim.app'
+  binary "MacVim.app/Contents/MacOS/Vim"
+
+  zap :delete => [
+                  '~/Library/Preferences/org.vim.MacVim.LSSharedFileList.plist',
+                  '~/Library/Preferences/org.vim.MacVim.plist',
+                 ]
+
+  caveats do
+    files_in_usr_local
+    <<-EOS.undent
+      Note that homebrew also provides a compiled macvim Formula that links its
+      binary to /usr/local/bin/Vim. It's not recommended to install both the
+      Cask and the Formula of MacVim.
+    EOS
+  end
 end
