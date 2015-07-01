a9c7772973b91465921ad1ff68bfa570df9dc5e5
Andrew Kreps <andrew.kreps@gmail.com>
2015-07-01 11:21:07 -0700

    phpstorm-eap: Version bump to 141.1815

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'phpstorm-eap' do
-  version '141.1717'
-  sha256 'af6087323b15205d10ede18e4aea176c6dab569adc47d0eb00d4de07ad88693d'
+  version '141.1815'
+  sha256 '4e66c2f600b67d9c29614d7ba78b1d3fbf051d21728bb3df00bb39b3548429bf'
 
   url "http://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg"
   homepage 'http://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
