006ffa26ea045874fd5629fdc3f771378cf8a8f3
Dmitry Rodionov <i.am.rodionovd@gmail.com>
2015-07-06 15:42:06 +0500

    Update iterm2-nightly.rb
    * Add values for `name` and `appcast` keys
    * Specify the license (it's GPL)
    * Add `zap` stanza

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -3,8 +3,15 @@ cask :v1 => 'iterm2-nightly' do
   sha256 :no_check
 
   url 'https://www.iterm2.com/nightly/latest'
+  appcast 'https://iterm2.com/appcasts/nightly.xml'
+  name 'iTerm2'
+  name 'iTerm 2'
   homepage 'https://www.iterm2.com/'
-  license :oss
+  license :gpl
 
   app 'iTerm.app'
+  
+  zap :delete => [
+      '~/Library/Preferences/com.googlecode.iterm2.plist',
+  ]
 end
