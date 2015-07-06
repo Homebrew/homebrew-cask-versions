f8acf088aea28e456e7599ff979e1330005cd95c
Victor Popkov <Dragon.vctr@gmail.com>
2015-07-06 06:25:26 +0300

    torbrowser-nl.rb: upgrade to v4.5.3

--- Casks/torbrowser-nl.rb
+++ Casks/torbrowser-nl.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-nl' do
-  version '4.5.2'
-  sha256 '4ca65c51cbdf826b63f5aba19caf80574e57959f83672a19bfec42f4d8627230'
+  version '4.5.3'
+  sha256 'f6328227a889fa7944700ade2501dc775b520708fcd7809ecf717dde36cffed1'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_nl.dmg"
   gpg "#{url}.asc",
