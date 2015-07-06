ddc14b9c4c76f596f6772dae5c4818c66dc8d4fc
Peter Lewis <peter@peterlew.is>
2015-07-06 09:05:21 +0100

    Update blender-beta to latest and fixed URL schema
    Amended omission of empty line in previous pull request

--- Casks/blender-beta.rb
+++ Casks/blender-beta.rb
@@ -1,6 +1,7 @@
 cask :v1 => 'blender-beta' do
   version '2.75-80f344f'
   sha256 '5815d43f9c6f22f90a7a78eed3076e93a9ea0dfa377251ec28ed85f127f03a55'
+
   url "https://builder.blender.org/download/blender-#{version}-OSX-10.6-x86_64.zip"
   name 'Blender'
   homepage 'https://www.blender.org/'
