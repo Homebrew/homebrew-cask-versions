f70281c8c2ded4205270fb0b7836ae3d12e31570
Miles Crabill <mcrabill@mozilla.com>
2015-07-08 10:45:59 -0700

    firefox-beta*.rb: upgrade to 40.0b2

--- Casks/firefox-beta-gb.rb
+++ Casks/firefox-beta-gb.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-gb' do
-  version '39.0b5'
-  sha256 '8171c2a05e303b0366020f7dde59a65bafaf16dff7d4a63b84b912610ee6539d'
+  version '40.0b2'
+  sha256 'a8040975c75e85e7fc93162b4cb0e09c2e4d936380a89da5ba80561f90076024'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-GB"
   homepage 'https://www.mozilla.org/en-GB/firefox/beta/'
