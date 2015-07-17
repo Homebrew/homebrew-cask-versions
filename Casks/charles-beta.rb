14afcc9e886580da4b6120eafdc4b9819dc58de0
Lukas Spieß <lumaxis@users.noreply.github.com>
2015-07-17 18:07:44 +0200

    Update charles-beta to v3.11b2

--- Casks/charles-beta.rb
+++ Casks/charles-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'charles-beta' do
-  version '3.11b1'
-  sha256 '05ebf3b95a84d16ca434d1d4611bff1373a9e90c76c8002d59607c1c78a04bed'
+  version '3.11b2'
+  sha256 '878355debe41814ef35cac2fbf59fe6b98ad9e75b62a3db7d2358ca863f65ce6'
 
   url "http://www.charlesproxy.com/assets/release/#{version.gsub(/b\d$/, '')}/charles-proxy-#{version}-applejava.dmg"
   homepage 'http://www.charlesproxy.com/download/beta/'
