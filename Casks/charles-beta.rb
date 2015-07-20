f375d277031cdbe178375290a3a07eabfc9a98f8
Evan Kinney <evan@evan.cat>
2015-07-20 15:07:56 -0700

    modify download URL for charles-beta to reflect JDK changes

--- Casks/charles-beta.rb
+++ Casks/charles-beta.rb
@@ -2,7 +2,7 @@ cask :v1 => 'charles-beta' do
   version '3.11b2'
   sha256 '878355debe41814ef35cac2fbf59fe6b98ad9e75b62a3db7d2358ca863f65ce6'
 
-  url "http://www.charlesproxy.com/assets/release/#{version.gsub(/b\d$/, '')}/charles-proxy-#{version}-applejava.dmg"
+  url "http://www.charlesproxy.com/assets/release/#{version.gsub(/b\d$/, '')}/charles-proxy-#{version}.dmg"
   homepage 'http://www.charlesproxy.com/download/beta/'
   license :commercial
 
