4f845c0c46cbb61ed037378f4afd38119b3064f0
Andy Bardagjy <andybardagjy@gmail.com>
2015-07-21 07:28:29 -0700

    Update kicad-nightly.rb
    Gah! Small typo in download URL..

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'kicad-nightly' do
-  version 'r5967.20150721-035418.dmg'
+  version 'r5967.20150721-035418'
   sha256 '3b45e0f963ea14653cac86d239d0f3e3a26f1c2ea73985d8a167691418caa9fd'
 
   url "http://downloads.kicad-pcb.org/osx/kicad-#{version}.dmg"
