f84ead1d653a923aef09cf0d5cac8fbe7952daf0
Christian Torrez <zerrot@users.noreply.github.com>
2015-07-27 04:52:49 -0400

    Updated dropbox-experimental to 3.9.30

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'dropbox-experimental' do
-  version '3.8.3'
-  sha256 '7d61f66f9f8005b39d4d804ef993ab6448ad79f054f92ef03749eedc0e898c47'
+  version '3.9.30'
+  sha256 '528888a8c65e0c0e9470f8ce23d7cb95b06abb37174befd55d7432129e1954f2'
 
   url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
-  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/204512565-Release-Candidate-3-8-3'
+  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/203352329-Experimental-Build-3-9-30'
   license :gratis
 
   app 'Dropbox.app'
