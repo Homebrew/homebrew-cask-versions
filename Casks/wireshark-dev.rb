78e84389ed6c75582bb02c5aa7e7e2fd11e18067
Max Gensthaler <max@gensthaler.de>
2015-07-31 15:48:22 +0200

    Update wireshark-dev to 1.99.8

--- Casks/wireshark-dev.rb
+++ Casks/wireshark-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'wireshark-dev' do
-  version '1.99.6'
-  sha256 'b73cb3684dd3270a6a4051dbb3feee284c37f87729a42448bd7016b498da494a'
+  version '1.99.8'
+  sha256 'a6d2c14b74bfb27d3cf69ddda3919ace7b56facc7a9f6b3959ad630667ead240'
 
   url "https://2.na.dl.wireshark.org/osx/Wireshark%20#{version}%20Intel%2064.dmg"
   homepage 'http://www.wireshark.org'
