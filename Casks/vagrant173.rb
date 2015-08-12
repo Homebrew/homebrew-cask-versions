011a4edc6a53bdc6fadb3c6ccd71e44bebd329c8
Szymon Szukalski <szymon@skas.io>
2015-08-12 17:14:27 +1000

    Add cask for Vagrant 1.7.3

---
+++ Casks/vagrant173.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'vagrant173' do
+  version '1.7.3'
+  sha256 '3c6078d8ce9a9c01589b089034f0afcbe2f4908a7f77a1471360fc8011a18dc8'
+
+  # bintray.com is the official download host per the vendor homepage
+  url "https://dl.bintray.com/mitchellh/vagrant/vagrant_#{version}.dmg"
+  homepage 'http://www.vagrantup.com'
+  license :mit
+
+  pkg 'Vagrant.pkg'
+
+  uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] },
+            :pkgutil => 'com.vagrant.vagrant'
+end
