27c68d219c34b74ad9b43ce1a1de08071262f65b
Michael <fanquake@gmail.com>
2015-08-13 18:24:05 +0800

    Netbeans PHP Nightly 201508120002

--- Casks/netbeans-php-nightly.rb
+++ Casks/netbeans-php-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'netbeans-php-nightly' do
-  version '201508050002'
-  sha256 '0e97420bdd4de793ed06f46b9a0179fa91bdedae77626bc43923d0eb8f2b46c3'
+  version '201508120002'
+  sha256 '135155885a9c4be1747d4ef8a687aee79569dc44eb21359c86c01b19bce7726e'
 
   url "http://bits.netbeans.org/download/trunk/nightly/latest/bundles/netbeans-trunk-nightly-#{version}-cpp-macosx.dmg"
   homepage 'https://netbeans.org/'
