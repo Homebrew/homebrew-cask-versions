e1d80f612409c14b0d7dca4bf34de960827b470b
Michael <fanquake@gmail.com>
2015-08-13 18:38:10 +0800

    Emacs Nightly 2015-08-13_01-41-00-e4de91d

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'emacs-nightly' do
-  version '2015-06-16_01-41-01-32a4883'
-  sha256 '692b2eb9ec83b05aceb29719cfdcac4a0581cf12e94ba5e61ec04252810de128'
+  version '2015-08-13_01-41-00-e4de91d'
+  sha256 '0a5924ddff0da3b8975bae6532f4bc65af6958b5fd9e3c65374d9522a6f1055d'
 
   url "http://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
   homepage 'http://emacsformacosx.com/'
