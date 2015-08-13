46d059bf96f9fe378a92d2c5284c38d7c9d147ec
Michael <fanquake@gmail.com>
2015-08-13 18:40:53 +0800

    Android Studio Canary 1.3.1.0 build 141.2135290

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'android-studio-canary' do
-  version '1.3.0.10'
-  sha256 '309e9c085b03a790def41aaf8ee6ad7026fb14bd88a9924a90fddeadf1a65e88'
+  version '1.3.1.0'
+  sha256 '967f1750fc9cb86080c9761439421269b7a57864ab6a429f9c82ba9cafdc1e2a'
 
-  url "http://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.2117773-mac.zip"
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.2135290-mac.zip"
   name 'Android Studio Canary'
   homepage 'https://sites.google.com/a/android.com/tools/download/studio/canary'
   license :apache
