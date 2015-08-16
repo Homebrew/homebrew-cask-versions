8b6cd7a297941a727c5ad6d6e3f237d8ad3e5f78
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-16 18:28:32 +0300

    torbrowser-fr.rb: upgrade to v5.0

--- Casks/torbrowser-fr.rb
+++ Casks/torbrowser-fr.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-fr' do
-  version '4.5.3'
-  sha256 '2ad7c0d276086973c3c795722f68677abfc623e2054c4e689bc3ba6dedcd44a9'
+  version '5.0'
+  sha256 '0b26ece08668136914f28aebb677d8f76a59a66d32781737cbc0c1b31a42c85a'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_fr.dmg"
   gpg "#{url}.asc",
