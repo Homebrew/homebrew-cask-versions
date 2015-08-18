1235c0e23de704cca0eeed6820f2393e185ec2f3
mAAdhaTTah <jamesorodig@gmail.com>
2015-08-18 09:59:27 -0400

    Bump version number on phpstorm-bundled-jdk
    Also fixes string interpolation

--- Casks/phpstorm-bundled-jdk.rb
+++ Casks/phpstorm-bundled-jdk.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'phpstorm-bundled-jdk' do
-  version '9.0.1'
+  version '9.0.2'
   sha256 '274f050445971108e503655cad01d407ee24b078a9a8d7075bd260bea4c3eb35'
 
-  url 'http://download-cf.jetbrains.com/webide/PhpStorm-#{version}-custom-jdk-bundled.dmg'
+  url "http://download-cf.jetbrains.com/webide/PhpStorm-#{version}-custom-jdk-bundled.dmg"
   name 'PhpStorm'
   homepage 'https://www.jetbrains.com/phpstorm/'
   license :commercial
