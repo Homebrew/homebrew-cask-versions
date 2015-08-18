056f68d23d259176802cfcc6be0efac11b8874b0
Michael <fanquake@gmail.com>
2015-08-18 17:44:11 +0800

    AppCode EAP Bundled JDK 141.2454.1

--- Casks/appcode-eap-bundled-jdk.rb
+++ Casks/appcode-eap-bundled-jdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'appcode-eap-bundled-jdk' do
-  version '141.2391.1'
-  sha256 'a042131eb621e16ff8031142e3e3219064f027852a01b6793866654697acab37'
+  version '141.2454.1'
+  sha256 'cb4708eb4ddb59587bd7eb3a72bb4eed6ba6cb9a85fbc4a3d010027198b63ac9'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version}-custom-jdk-bundled.dmg"
   name 'AppCode'
