7b3394dcc0d48739c8f107e75d3db3b5e5898685
mahori <mahori@users.noreply.github.com>
2015-08-21 08:58:33 +0900

    Add cask for Parallels Desktop 10 for Mac

---
+++ Casks/parallels10.rb
@@ -0,0 +1,33 @@
+cask :v1 => 'parallels10' do
+  version '10.2.2-29105'
+  sha256 '1065e1b85b5a3ccce0eecafe425dc29e17a9f71a8eed81716ca705aa1b738639'
+
+  url "http://download.parallels.com/desktop/v#{version[/^\w+/]}/#{version.sub(/-.*$/, '')}/ParallelsDesktop-#{version}.dmg"
+  name 'Parallels Desktop'
+  homepage 'https://www.parallels.com/products/desktop/'
+  license :commercial
+
+  app 'Parallels Desktop.app'
+
+  postflight do
+    # Set the file to visible, since it was hidden in the dmg
+    system '/usr/bin/SetFile', '-a', 'v', staged_path.join('Parallels Desktop.app')
+  end
+
+  uninstall :delete => [
+                         '/usr/bin/prl_convert',
+                         '/usr/bin/prl_disk_tool',
+                         '/usr/bin/prl_perf_ctl',
+                         '/usr/bin/prlctl',
+                         '/usr/bin/prlsrvctl',
+                       ]
+  zap       :delete => [
+                         '~/.parallels_settings',
+                         '~/Library/Caches/com.parallels.desktop.console',
+                         '~/Library/Preferences/com.parallels.desktop.console.LSSharedFileList.plist',
+                         '~/Library/Preferences/com.parallels.desktop.console.plist',
+                         '~/Library/Preferences/com.parallels.Parallels Desktop Statistics.plist',
+                         '~/Library/Preferences/com.parallels.Parallels Desktop.plist',
+                         '~/Library/Preferences/com.parallels.Parallels.plist',
+                        ]
+end
