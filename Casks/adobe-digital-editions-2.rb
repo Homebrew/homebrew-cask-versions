5d1f2a82075d893b1b494e3366b09db1ae78bdf4
Muratcan Simsek <mail@muratcansimsek.com>
2015-08-22 14:33:58 +0300

    Add Adobe Digital Editions v2.0.1
    New versions of ADE uses a new DRM scheme,
    it is not broken yet so we cannot reformat or
    backup ebooks. Old version 2.0.1 is cracked and works
    with every epub store.

---
+++ Casks/adobe-digital-editions-2.rb
@@ -0,0 +1,14 @@
+cask :v1 => 'adobe-digital-editions-2' do
+  version '2.0.1'
+  sha256 :no_check    # required as upstream package is updated in-place
+
+  url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_2.0_Installer.dmg"
+  name 'Adobe Digital Editions 2.0.1'
+  homepage 'https://www.adobe.com/solutions/ebook/digital-editions.html'
+  license :gratis
+
+  pkg 'Digital Editions 2.0 Installer.pkg', :allow_untrusted => true
+
+  uninstall :pkgutil => 'com.adobe.adobedigitaleditions.app', :delete => '/Applications/Adobe Digital Editions.app'
+  
+end
