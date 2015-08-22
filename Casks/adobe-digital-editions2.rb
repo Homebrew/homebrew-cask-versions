3c7e052e48bc765a059dac05511e26eec9013bc6
Muratcan Simsek <mail@muratcansimsek.com>
2015-08-22 20:53:51 +0300

    Changed file name, made fixes

--- Casks/adobe-digital-editions2.rb
+++ Casks/adobe-digital-editions2.rb
@@ -1,8 +1,8 @@
-cask :v1 => 'adobe-digital-editions-2' do
+cask :v1 => 'adobe-digital-editions2' do
   version '2.0.1'
   sha256 'dcaec3e2cbb2faa7720a2ff06d13af4fe0433cdf991c76eeeed28cb0019b69c0' 
 
-  url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_2.0_Installer.dmg"
+  url 'http://download.adobe.com/pub/adobe/digitaleditions/ADE_2.0_Installer.dmg'
   name 'Adobe Digital Editions 2.0.1'
   homepage 'https://www.adobe.com/solutions/ebook/digital-editions.html'
   license :gratis
