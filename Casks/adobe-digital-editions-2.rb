b219a0a175e2d4473ab7d9b2c3131dc685011b5a
Muratcan Simsek <mail@muratcansimsek.com>
2015-08-22 15:14:37 +0300

    Fixing extra line and sha256

--- Casks/adobe-digital-editions-2.rb
+++ Casks/adobe-digital-editions-2.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'adobe-digital-editions-2' do
   version '2.0.1'
-  sha256 :no_check    # required as upstream package is updated in-place
+  sha256 'dcaec3e2cbb2faa7720a2ff06d13af4fe0433cdf991c76eeeed28cb0019b69c0' 
 
   url "http://download.adobe.com/pub/adobe/digitaleditions/ADE_2.0_Installer.dmg"
   name 'Adobe Digital Editions 2.0.1'
@@ -10,5 +10,4 @@ cask :v1 => 'adobe-digital-editions-2' do
   pkg 'Digital Editions 2.0 Installer.pkg', :allow_untrusted => true
 
   uninstall :pkgutil => 'com.adobe.adobedigitaleditions.app', :delete => '/Applications/Adobe Digital Editions.app'
-  
 end
