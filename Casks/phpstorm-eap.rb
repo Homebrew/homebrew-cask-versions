a56c7538325a9c9179fd31d3bd3bcca8fc03c354
Michael <fanquake@gmail.com>
2015-08-27 21:08:19 +0800

    PhpStorm EAP 142.4289

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'phpstorm-eap' do
-  version '141.2325'
-  sha256 'd9fb715b9c6a351bd30f25e62aa1f6fae2938b17065999efd1375e5a3f4fedba'
+  version '142.4289'
+  sha256 '6592f54f863756b19ed75062ee46180c98836b39e1e522b0e97c012398660826'
 
-  url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg"
+  url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version}-custom-jdk-bundled.dmg"
   name 'PhpStorm EAP'
   homepage 'https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
   license :commercial
@@ -18,12 +18,4 @@ cask :v1 => 'phpstorm-eap' do
                   '~/.WebIde90',
                  ]
 
-  caveats <<-EOS.undent
-    #{token} requires Java 6 like any other IntelliJ-based IDE.
-    You can install it with
-    brew cask install caskroom/homebrew-versions/java6
-    The vendor (JetBrains) doesn't support newer versions of Java (yet)
-    due to several critical issues, see details at
-    https://intellij-support.jetbrains.com/entries/27854363
-  EOS
 end
