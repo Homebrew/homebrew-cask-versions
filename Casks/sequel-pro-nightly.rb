cde8d7e78249a0383ef108736014e8bea691b075
Michael <fanquake@gmail.com>
2015-08-27 11:50:12 +0800

    Sequel Pro Nightly r4552d5daa1

--- Casks/sequel-pro-nightly.rb
+++ Casks/sequel-pro-nightly.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'sequel-pro-nightly' do
-  version '0df35b718c'
+  version 'r4552d5daa1'
   sha256 '5ecd09439a00e1c1d52dde936d160fa2123f0aee426aa2d61aad62344e32e14e'
 
-  url "http://nightly.sequelpro.com/builds/Sequel_Pro_r#{version}.dmg"
+  url "http://nightly.sequelpro.com/builds/Sequel_Pro_#{version}.dmg"
   name 'Sequel Pro'
   homepage 'http://nightly.sequelpro.com/'
   license :mit
