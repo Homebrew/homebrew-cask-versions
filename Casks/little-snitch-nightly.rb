248c878cb0d328f9e3ed5dc68c2ebbb63c435950
Michael <fanquake@gmail.com>
2015-08-27 11:10:57 +0800

    Little Snitch Nightly 3.6 - 4350

--- Casks/little-snitch-nightly.rb
+++ Casks/little-snitch-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'little-snitch-nightly' do
-  version '3.6-nightly-(4348)'
-  sha256 '05db23d4dc754eda32dcac727fee320a6bb272ff1f7b76f9ad527ace91679441'
+  version '3.6-nightly-(4350)'
+  sha256 'c4826574e8a7fef291733bc5b394121f0935b57450d864a9d6331e2c2908e4d3'
 
   url "http://www.obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version}.dmg"
   homepage 'http://www.obdev.at/products/littlesnitch/index.html'
