3264691bd3529a2d62f458d1f528e92f3d8633d6
Michael <fanquake@gmail.com>
2015-08-27 21:27:07 +0800

    Rubymine Bundled JDK 7.1.4

--- Casks/rubymine-bundled-jdk.rb
+++ Casks/rubymine-bundled-jdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rubymine-bundled-jdk' do
-  version '7.1.2'
-  sha256 '8d96d6b0484039055335618939a50b25a97cf13c93c2137dbafe5e4986725095'
+  version '7.1.4'
+  sha256 '4f5df21a17eb8bb337756db485ef489f720b87f6f97398afb6358e5cd9261071'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}-custom-jdk-bundled.dmg"
   name 'RubyMine'
