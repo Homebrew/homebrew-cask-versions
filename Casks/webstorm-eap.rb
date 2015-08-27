bb0899aea3e46279d4c94030c7ee3b376cdf1086
Michael <fanquake@gmail.com>
2015-08-27 12:11:40 +0800

    WebStorm EAP 142.4148

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,19 +1,21 @@
 cask :v1 => 'webstorm-eap' do
-  version '141.1108'
-  sha256 'ba572e9784a50c01bc750206781927812fc32c380a4fc697175325477228e971'
+  version '142.4148'
+  sha256 'd6151549527cd82deb5ebb514df9d845bfd2692130c5495f2b39d76b06e6aafe'
 
-  url "http://download.jetbrains.com/webstorm/WebStorm-EAP-#{version}.dmg"
-  homepage 'https://confluence.jetbrains.com/display/WI/WebStorm+10+EAP'
+  url "http://download.jetbrains.com/webstorm/WebStorm-EAP-#{version}-custom-jdk-bundled.dmg"
+  name 'WebStorm'
+  homepage 'https://confluence.jetbrains.com/display/WI/WebStorm+11+EAP'
   license :commercial
 
-  app 'WebStorm 10 EAP.app'
-
-  postflight do
-    plist_set(':JVMOptions:JVMVersion', '1.6+')
-  end
+  app 'WebStorm 11 EAP.app'
+  binary 'WebStorm.app/Contents/MacOS/webstorm'
 
   zap :delete => [
-                  '~/Library/Application Support/WebStorm10',
-                  '~/Library/Preferences/WebStorm10',
+                  '~/.WebStorm11',
+                  '~/Library/Preferences/com.jetbrains.webstorm.plist',
+                  '~/Library/Preferences/WebStorm11',
+                  '~/Library/Application Support/WebStorm11',
+                  '~/Library/Caches/WebStorm11',
+                  '~/Library/Logs/WebStorm11',
                  ]
 end
