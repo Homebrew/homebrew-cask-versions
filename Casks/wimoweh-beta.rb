f15b006ac792716fad2e75a0d433c709b3c4c176
Michael <fanquake@gmail.com>
2015-08-27 13:13:20 +0800

    Wimoweh 1.1.52 Beta
    https://www.serialangels.co.uk/sa-content/uploads/2015/08/Wimoweh.1.1.52.BETA_.zip

--- Casks/wimoweh-beta.rb
+++ Casks/wimoweh-beta.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'wimoweh-beta' do
-  version '0.66-beta'
-  sha256 '8be363dc4764299df68220da67cf08d6f913fbf1bfe14059cd47bf99728d97fc'
+  version '1.1.52.BETA'
+  sha256 'ab7e8a298a1b8bb5bdfb6e96865eb77cec6391df2292d0f3b8851ca54215c0e1'
 
-  url 'http://www.serialangels.co.uk/sa-content/uploads/2013/12/Wimoweh.0.66.BETA_.zip'
+  url "https://www.serialangels.co.uk/sa-content/uploads/2015/08/Wimoweh.#{version}_.zip"
   homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
   license :unknown
 
