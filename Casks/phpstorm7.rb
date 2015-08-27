1cb5d5805f5d3f99ec47b546951756a4ab5a4a60
Michael <fanquake@gmail.com>
2015-08-27 21:13:56 +0800

    PhpStorm 7 - Replace postflight hack with caveat
    Per #879

--- Casks/phpstorm7.rb
+++ Casks/phpstorm7.rb
@@ -8,13 +8,20 @@ cask :v1 => 'phpstorm7' do
 
   app 'PhpStorm.app'
 
-  postflight do
-    plist_set(':JVMOptions:JVMVersion', '1.6+')
-  end
-
   zap :delete => [
                   '~/Library/Application Support/WebIde70',
                   '~/Library/Preferences/WebIde70',
                   '~/Library/Preferences/com.jetbrains.PhpStorm.plist',
                  ]
+  
+  caveats <<-EOS.undent
+    #{token} requires Java 6 like any other IntelliJ-based IDE.
+    You can install it with
+
+      brew cask install caskroom/homebrew-versions/java6
+
+    The vendor (JetBrains) doesn't support newer versions of Java (yet)
+    due to several critical issues, see details at
+    https://intellij-support.jetbrains.com/entries/27854363
+  EOS
 end
