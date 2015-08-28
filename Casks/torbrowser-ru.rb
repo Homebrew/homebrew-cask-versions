3dfc55526cb31529282f3c13f1d85a764ae94771
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-28 18:35:25 +0300

    torbrowser-ru.rb: upgrade to v5.0.2

--- Casks/torbrowser-ru.rb
+++ Casks/torbrowser-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-ru' do
-  version '5.0.1'
-  sha256 '45675b538edaf1bbdbf769a77142cebc0f5f843a4b7bb1776c800b10968ef946'
+  version '5.0.2'
+  sha256 '7176ff88a12755a430b1ada680792a27c227db7b50c367370da941fe598ab517'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_ru.dmg"
   gpg "#{url}.asc",
