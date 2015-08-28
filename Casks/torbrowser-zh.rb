f56cdfaddecfc3834d9b6c6d0f3b08006c3580fc
Victor Popkov <Dragon.vctr@gmail.com>
2015-08-28 18:35:40 +0300

    torbrowser-zh.rb: upgrade to v5.0.2

--- Casks/torbrowser-zh.rb
+++ Casks/torbrowser-zh.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-zh' do
-  version '5.0.1'
-  sha256 'b4dfc8395e01ab8ced43b31795d8beb2dd88149859911ab07a0a7872dd5f2548'
+  version '5.0.2'
+  sha256 'd9dd3aff1bdad5d682fffd5d497412e135e295bb612e41b2ec53cf3de615db65'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_zh-CN.dmg"
   gpg "#{url}.asc",
