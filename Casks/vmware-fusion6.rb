af52c60efa9f414a8f4815e9b6378f532b63aa27
Michael <fanquake@gmail.com>
2015-08-28 15:00:00 +0800

    Update VMware Fusion 6 Cask stanzas

--- Casks/vmware-fusion6.rb
+++ Casks/vmware-fusion6.rb
@@ -3,11 +3,14 @@ cask :v1 => 'vmware-fusion6' do
   sha256 '7036242845e1ebfeb0c3c430a45b2ebe519a33ec5cdc8955fa4ca03117946693'
 
   url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}-light.dmg"
-  homepage 'http://www.vmware.com/products/fusion/'
-  license :closed
+  name 'VMware Fusion'
+  homepage 'https://www.vmware.com/products/fusion/'
+  license :commercial
+  tags :vendor => 'VMware'
 
   binary 'VMware Fusion.app/Contents/Library/vmrun'
   app 'VMware Fusion.app'
+  
   uninstall_preflight do
     system '/usr/bin/sudo', '-E', '--',
            '/usr/sbin/chown', '-R', '--', "#{Etc.getpwuid(Process.euid).name}:staff", "#{staged_path}/VMware Fusion.app"
