0d65540101252b9d71d7a6c6796ab70f39871983
mahori <mahori@users.noreply.github.com>
2015-08-28 11:02:57 +0900

    Add cask for VMware Fusion 7

---
+++ Casks/vmware-fusion7.rb
@@ -0,0 +1,29 @@
+cask :v1 => 'vmware-fusion7' do
+  version '7.1.2-2779224'
+  sha256 '93e809ece4f915fcb462affabfce2d7c85eb08314b548e2cde44cb2a67ad7d76'
+
+  url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
+  name 'VMware Fusion'
+  homepage 'https://www.vmware.com/products/fusion/'
+  license :commercial
+  tags :vendor => 'VMware'
+
+  binary 'VMware Fusion.app/Contents/Library/vmnet-cfgcli'
+  binary 'VMware Fusion.app/Contents/Library/vmnet-cli'
+  binary 'VMware Fusion.app/Contents/Library/vmrun'
+  binary 'VMware Fusion.app/Contents/Library/vmware-vdiskmanager'
+  binary 'VMware Fusion.app/Contents/Library/VMware OVF Tool/ovftool'
+  app 'VMware Fusion.app'
+
+  uninstall_preflight do
+    set_ownership "#{staged_path}/VMware Fusion.app"
+  end
+
+  zap :delete => [
+                  # note: '~/Library/Application Support/VMware Fusion' is not safe
+                  # to delete. In older versions, VM images were located there.
+                  '~/Library/Caches/com.vmware.fusion',
+                  '~/Library/Logs/VMware',
+                  '~/Library/Logs/VMware Fusion',
+                 ]
+end
