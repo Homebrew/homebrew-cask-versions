99790dc7eb427fdb827cbd4fd3b1761d90ca2610
Michael <fanquake@gmail.com>
2015-08-29 12:25:52 +0800

    Add zap and name stanzas to Firefox Nightly

--- Casks/firefox-nightly.rb
+++ Casks/firefox-nightly.rb
@@ -3,9 +3,16 @@ cask :v1 => 'firefox-nightly' do
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://ftp.mozilla.org/pub/mozilla.org/firefox/nightly/latest-trunk/firefox-#{version}.en-US.mac.dmg"
+  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://nightly.mozilla.org/'
   license :mpl
   tags :vendor => 'Mozilla'
 
   app 'FirefoxNightly.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
