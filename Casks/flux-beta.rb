1a6303ae29b6536a913fe3f37a225167660d5893
Michael <fanquake@gmail.com>
2015-08-29 17:18:58 +0800

    Add name and zap stanza to Flux Beta

--- Casks/flux-beta.rb
+++ Casks/flux-beta.rb
@@ -3,12 +3,15 @@ cask :v1 => 'flux-beta' do
   sha256 :no_check
 
   url 'https://justgetflux.com/mac/fluxbeta.zip'
+  name 'f.lux'
   homepage 'https://justgetflux.com/'
-  license :unknown
+  license :gratis
 
   app 'Flux.app'
 
   postflight do
     suppress_move_to_applications
   end
+  
+  zap :delete => '~/Library/Preferences/org.herf.Flux.plist'
 end
