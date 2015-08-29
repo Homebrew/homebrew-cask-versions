318abe7b465aa7112622833f37bb594247987fda
Michael <fanquake@gmail.com>
2015-08-29 17:51:47 +0800

    Google Japanese IME Dev: Add name/tags stanzas

--- Casks/google-japanese-ime-dev.rb
+++ Casks/google-japanese-ime-dev.rb
@@ -2,11 +2,15 @@ cask :v1 => 'google-japanese-ime-dev' do
   version :latest
   sha256 :no_check
 
+  # google.com is the official download host per the vendor homepage
   url 'https://dl.google.com/japanese-ime/dev/GoogleJapaneseInput.dmg'
+  name 'Google Japanese Input Method Editor'
   homepage 'https://www.google.co.jp/ime/'
-  license :unknown
+  license :gratis
+  tags :vendor => 'Google'
 
   pkg 'GoogleJapaneseInput.pkg'
+  
   uninstall :pkgutil => 'com.google.pkg.GoogleJapaneseInput',
             :launchctl => [
                            'com.google.inputmethod.Japanese.Converter',
