6be6ef0ccac11a34752b15cd071dfef637e56a91
Michael <fanquake@gmail.com>
2015-08-29 17:34:50 +0800

    Emacs Nightly 2015-08-28_01-41-37-572f68e
    Add name stanzas

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,8 +1,10 @@
 cask :v1 => 'emacs-nightly' do
-  version '2015-08-26_01-41-37-920a0ad'
-  sha256 '85f8f5a25b7b8bcdf668e18140e6bbdd17db1007ff9fa4b5bcd9597ebf2f60a0'
+  version '2015-08-28_01-41-37-572f68e'
+  sha256 'a470ef2b6f98e0e1be4a39d2fcedaf692f6e9b345682b8dcdc386ef5313e4d4a'
 
   url "http://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
+  name 'Emacs for Mac OS X'
+  name 'Emacs'
   homepage 'http://emacsformacosx.com/'
   license :gpl
 
