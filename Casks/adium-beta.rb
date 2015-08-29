0612ce09203dd0b2541b0e7e9d2f60ba7f1b4fef
Michael <fanquake@gmail.com>
2015-08-29 11:56:34 +0800

    Add missing stanzas to Adium Beta

--- Casks/adium-beta.rb
+++ Casks/adium-beta.rb
@@ -3,8 +3,15 @@ cask :v1 => 'adium-beta' do
   sha256 'e7690718f14defa3bc08cd3949a4eab52e942abd47f7ac2ce7157ed7295658c6'
 
   url "https://adiumx.cachefly.net/Adium_#{version}.dmg"
+  name 'Adium'
   homepage 'https://beta.adium.im/'
-  license :oss
+  license :gpl
 
   app 'Adium.app'
+
+  zap :delete => [
+                  '~/Library/Caches/Adium',
+                  '~/Library/Caches/com.adiumX.adiumX',
+                  '~/Library/Preferences/com.adiumX.adiumX.plist',
+                 ]
 end
