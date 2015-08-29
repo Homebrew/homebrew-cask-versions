02866e7b1e5c543644539828e9cc217770a07346
Michael <fanquake@gmail.com>
2015-08-29 18:17:37 +0800

    Kicad Nightly r6131.20150828-035306

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'kicad-nightly' do
-  version 'r6127.20150826-035253'
-  sha256 '325cca87f445dbfc18bdbe720923027ff70d3da3c2119e230da772cb81b2a406'
+  version 'r6131.20150828-035306'
+  sha256 'fd42075c39b2ce84e23b03a739cd8b8be0302ad0ecf91e62d9108239c3423e45'
 
   url "http://downloads.kicad-pcb.org/osx/kicad-#{version}.dmg"
+  name 'KiCad'
   homepage 'http://www.kicad-pcb.org'
   license :gpl
 
