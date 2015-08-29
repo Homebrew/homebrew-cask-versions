99f8f278edc8600a8b28865f13f4d187210e6f41
Michael <fanquake@gmail.com>
2015-08-29 12:00:35 +0800

    Add missing stanzas to Adium Nightly16

--- Casks/adium-nightly16.rb
+++ Casks/adium-nightly16.rb
@@ -2,9 +2,16 @@ cask :v1 => 'adium-nightly16' do
   version '1.6hgr5915'
   sha256 '70619777433df646a7251a10eb74baecdc5a47be085cd83d35f3750fcfea9e1b'
 
-  url "http://nightly.adium.im/adium-adium-1.6/Adium_#{version}.dmg"
+  url "http://nightly.adium.im/adium-adium-1.6/Adium_#{version}.dmg
+  name 'Adium'
   homepage 'http://nightly.adium.im/?repo_branch=adium-adium-1.6'
-  license :oss
+  license :gpl
 
   app 'Adium.app'
+
+  zap :delete => [
+                  '~/Library/Caches/Adium',
+                  '~/Library/Caches/com.adiumX.adiumX',
+                  '~/Library/Preferences/com.adiumX.adiumX.plist',
+                 ]
 end
