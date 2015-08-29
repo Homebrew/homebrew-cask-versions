3c8fc28b7ce5b0b363fbbf1cc3fd6d3944f118f9
Michael <fanquake@gmail.com>
2015-08-29 17:54:16 +0800

    Little Snitch Nightly: Add name and zap stanzas
    Update URLS to HTTPS
    Update License

--- Casks/little-snitch-nightly.rb
+++ Casks/little-snitch-nightly.rb
@@ -2,9 +2,17 @@ cask :v1 => 'little-snitch-nightly' do
   version '3.6-nightly-(4350)'
   sha256 'c4826574e8a7fef291733bc5b394121f0935b57450d864a9d6331e2c2908e4d3'
 
-  url "http://www.obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version}.dmg"
-  homepage 'http://www.obdev.at/products/littlesnitch/index.html'
-  license :unknown
+  url "https://www.obdev.at/downloads/littlesnitch/nightly/LittleSnitch-#{version}.dmg"
+  name 'Little Snitch'
+  homepage 'https://www.obdev.at/products/littlesnitch/index.html'
+  license :commercial
 
   installer :manual => 'Little Snitch Installer.app'
+  
+  zap :delete => [
+                  '~/Library/Preferences/at.obdev.LittleSnitchNetworkMonitor.plist',
+                  '~/Library/Application Support/Little Snitch/rules.usr.xpl',
+                  '~/Library/Application Support/Little Snitch/configuration.xpl',
+                  '~/Library/Application Support/Little Snitch/configuration.user.xpl',
+                 ]
 end
