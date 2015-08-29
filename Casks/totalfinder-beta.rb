eca88a0302b7d64301331947fc607ca32efbbf95
Michael <fanquake@gmail.com>
2015-08-29 19:40:14 +0800

    TotalFinder Beta 1.7.1

--- Casks/totalfinder-beta.rb
+++ Casks/totalfinder-beta.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'totalfinder-beta' do
-  version '1.6.12'
-  sha256 'c7d2d41610fd2ec0e944120755ebd7fbcc7518d5d5b0f34ff55da3cb467831b8'
+  version '1.7.1'
+  sha256 '32a5c8be84ef5bc305e582085f455b09c502fd6e447c4fdb08fba0eba9cc51de'
 
-  url 'http://downloads.binaryage.com/TotalFinder-1.6.12.dmg'
-  homepage 'http://totalfinder.binaryage.com/'
-  license :unknown
+  url "http://downloads.binaryage.com/TotalFinder-#{version}.dmg"
+  homepage 'http://totalfinder.binaryage.com/beta-changes'
+  license :commercial
 
   pkg 'TotalFinder.pkg'
   uninstall :pkgutil => 'com.binaryage.pkg.totalfinder.app',
