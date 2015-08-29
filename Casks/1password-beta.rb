0f36fc8644cf4a70033e9efb679b4edf144886af
Michael <fanquake@gmail.com>
2015-08-29 17:42:43 +0800

    1Password Beta 5.4.BETA-37

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,13 +1,13 @@
 cask :v1 => '1password-beta' do
-  version '5.4.BETA-36'
-  sha256 '3e6bff9eae1113b78adbd484fb77a98234f9ac08180afd7f67e6b6d8c1159de6'
+  version '5.4.BETA-37'
+  sha256 '8eda86b6c94538ee0158788924fe6e7082a2ef6093c728bbdf1d3c0b125692e8'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
   homepage 'https://agilebits.com/onepassword/mac'
   license :commercial
 
-  app '1Password 5.app'
+  app "1Password #{version.to_i}.app"
 
   zap :delete => [
                   '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
