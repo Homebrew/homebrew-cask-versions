7126a0de2e189faf55bb474d47b4a5d2bfc86a6e
Michael <fanquake@gmail.com>
2015-08-29 13:24:24 +0800

    Firefox Beta 41.0b5
    Add name and zap stanzas

--- Casks/firefox-beta.rb
+++ Casks/firefox-beta.rb
@@ -1,11 +1,18 @@
 cask :v1 => 'firefox-beta' do
-  version '40.0b2'
-  sha256 '7ebad1fd0183d750755838f983c389ae61bb3e3f63e1c9e20af5608eca4b5f94'
+  version '41.0b5'
+  sha256 '662cca6ec4265e059e158d15d468f60e1dc213554576a7a03bcd566f5f3163b1'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-US"
+  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/channel/#beta'
   license :mpl
   tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
