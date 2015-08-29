27b1bf3b5edfdd7c817134580a0edcd8519d397b
Michael <fanquake@gmail.com>
2015-08-29 12:39:39 +0800

    Firefox ESR 38.2.1

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,11 +1,18 @@
 cask :v1 => 'firefox-esr' do
-  version '38.2.0'
-  sha256 '183a133cade74188081f378b3365f2db52a541048be08b61fb07f2d8fb0b262e'
+  version '38.2.1'
+  sha256 '75a97747fd165571e8104c6de0a9f33f66d11807f65211e3898ead5e92cf83c7'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
+  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
   license :mpl
   tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
