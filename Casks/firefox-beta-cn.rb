225a6743629dac617b8af3f7acd37da6d8f20a41
Michael <fanquake@gmail.com>
2015-08-29 12:23:51 +0800

    Firefox Beta CN 41.0b5

--- Casks/firefox-beta-cn.rb
+++ Casks/firefox-beta-cn.rb
@@ -1,11 +1,18 @@
 cask :v1 => 'firefox-beta-cn' do
-  version '40.0b2'
-  sha256 'f23bd254febb023fb59d2b3facb6172f2557705a9096d9ebf01a9e11307e9df6'
+  version '41.0b5'
+  sha256 'f403a0e26ba68da2b71e8020fd95100ddfdf8dabd0b72e48b26dfeddc2ca0cd3'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
+  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/zh-CN/firefox/beta/'
   license :mpl
   tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
