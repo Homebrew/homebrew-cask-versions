8a461f2f4d08ccac1538a5cbc1b09d00dcce9881
Michael <fanquake@gmail.com>
2015-08-29 18:33:22 +0800

    Charles OpenJDK Add name stanza
    Update license

--- Casks/charles-openjdk.rb
+++ Casks/charles-openjdk.rb
@@ -3,8 +3,9 @@ cask :v1 => 'charles-openjdk' do
   sha256 '817dd2ba6167682563ca62c32dfa32afefab95f7d26f5625c2f82bee7d849e58'
 
   url "http://www.charlesproxy.com/assets/release/#{version}/charles-proxy-#{version}-openjdk.dmg"
+  name 'Charles'
   homepage 'http://www.charlesproxy.com/'
-  license :unknown
+  license :commercial
 
   app 'Charles.app'
 end
