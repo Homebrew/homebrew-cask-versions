5b1241afcf3202ed8e804e3f34410fef5e8383b4
Michael <fanquake@gmail.com>
2015-08-29 09:25:13 +0800

    Tunnelblick Beta 3.6beta08_build_4371

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,10 +1,23 @@
 cask :v1 => 'tunnelblick-beta' do
-  version '3.5beta10_build_4262'
-  sha256 '77b2c0699b26242de1b207d2ed6f06341b0672781cba51d7ecae28646a670f71'
+  version '3.6beta08_build_4371'
+  sha256 '141b3cd3a4595a608e0a41be245dd67a80d531a3bcc717b97ab49a0e9d07f360'
 
-  url "http://downloads.sourceforge.net/project/tunnelblick/All%20files/Tunnelblick_#{version}.dmg"
-  homepage 'https://code.google.com/p/tunnelblick/'
-  license :oss
+  url "https://tunnelblick.net/release/Tunnelblick_#{version}.dmg"
+  appcast 'https://www.tunnelblick.net/appcast.rss',
+          :sha256 => '7fa119cda4d782dc61cb75895c70b3572652df737c908270c48a09d67a874592'
+  name 'Tunnelblick'
+  homepage 'https://tunnelblick.net'
+  license :gpl
 
   app 'Tunnelblick.app'
+
+  uninstall :launchctl => 'net.tunnelblick.tunnelblick.LaunchAtLogin',
+            :quit      => 'net.tunnelblick.tunnelblick'
+
+  depends_on :macos => '>= :tiger'
+
+  caveats <<-EOS.undent
+    For security reasons, #{token} must be installed to /Applications,
+    and will request to be moved at launch.
+  EOS
 end
