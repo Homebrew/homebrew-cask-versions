2bc22530d9fc9250b1ae203e8f20c4b7490d6254
Michael <fanquake@gmail.com>
2015-08-29 17:22:39 +0800

    Add zap stanza to iterm2 beta
    Update license and homepage URL

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -3,8 +3,10 @@ cask :v1 => 'iterm2-beta' do
   sha256 '18cf11393ed81d351d739257185bc33414c440c4281e576f9d0b54405970f902'
 
   url 'https://iterm2.com/downloads/beta/iTerm2-2_9_20150626.zip'
-  homepage 'http://www.iterm2.com/'
-  license :oss
+  homepage 'https://www.iterm2.com/'
+  license :gpl
 
   app 'iTerm.app'
+  
+  zap :delete => '~/Library/Preferences/com.googlecode.iterm2.plist'
 end
