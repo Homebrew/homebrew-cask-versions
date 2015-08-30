de76713541ea220b7bd123d5e19d07cb53e774e4
Michael <fanquake@gmail.com>
2015-08-30 08:42:19 +0800

    IntelliJ IDEA CE EAP 142.4245.2

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'intellij-idea-ce-eap' do
-  version '142.2491.4'
-  sha256 '280e852beecff74301ecd402aa07109e9387930693d9bfd6357fc327d401e776'
+  version '142.4245.2'
+  sha256 '0416b6abbb71ac62954f0104fbef52cef008891964c616e3890c8510cb27ec54'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version}-custom-jdk-bundled.dmg"
   name 'IntelliJ IDEA EAP :: CE'
