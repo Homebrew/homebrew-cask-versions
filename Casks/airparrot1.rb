ddfd5bc3b9b36cea373d1a315658fee3abc05796
Michael <fanquake@gmail.com>
2015-08-31 17:28:07 +0800

    AirParrot1: Add name and update license stanza

--- Casks/airparrot1.rb
+++ Casks/airparrot1.rb
@@ -3,9 +3,10 @@ cask :v1 => 'airparrot1' do
   sha256 :no_check
 
   url 'http://download.airsquirrels.com/AirParrot/Mac/AirParrot.dmg'
+  name 'AirParrot'
   appcast 'http://airparrot.com/updates/AirParrot.xml'
   homepage 'http://www.airsquirrels.com/airparrot/'
-  license :unknown
+  license :commercial
 
   app 'AirParrot.app'
 end
