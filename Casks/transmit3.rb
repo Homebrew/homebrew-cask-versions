cb21d5a684aa42c2f3c3906b44cfeacd5b63f230
Michael <fanquake@gmail.com>
2015-09-01 08:39:22 +0800

    Transmit3 Add name and zap stanza

--- Casks/transmit3.rb
+++ Casks/transmit3.rb
@@ -3,8 +3,14 @@ cask :v1 => 'transmit3' do
   sha256 '311f4d0f559c966fe7e5a4a6e0327dfa7a4c06a9c942ec11d58e6ed210f4b689'
 
   url "http://panic.com/museum/transmit/Transmit%20#{version}.zip"
+  name 'Transmit'
   homepage 'https://www.panic.com/transmit'
-  license :unknown
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Transmit.app'
+
+  zap :delete => [
+                  '~/Library/Preferences/com.panic.Transmit.plist',
+                  '~/Library/Application Support/Transmit',
+                 ]
 end
