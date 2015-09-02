14e2327b4d77910fbdf5d713cc68d3d83de8e9be
Michael <fanquake@gmail.com>
2015-09-02 16:44:51 +0800

    Fix SHA256 of Java6 Cask

--- Casks/java6.rb
+++ Casks/java6.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'java6' do
   version '1.6.0_65'
-  sha256 '1b7b88c7c7ca3a1c50eacee1977dee974a50157ff7c88d0e73902bd98fc58a86'
+  sha256 '8f40dbf821e21410feab4d9e2e533c42518897b7cac5edfad895e91016f918fc'
 
   url 'https://support.apple.com/downloads/DL1572/en_US/javaforosx.dmg'
   homepage 'https://support.apple.com/kb/DL1572'
