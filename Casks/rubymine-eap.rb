c5f17680dd59fa43a93e3531abc961af46197884
Lukas Elmer <lukas.elmer@gmail.com>
2015-09-03 14:55:31 +0200

    Update rubymine-eap.rb

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rubymine-eap' do
-  version '142.4083.1'
-  sha256 '93242b2485889bdbae6afb245728da2511a473a40795fb76eb6b9c54815f6de0'
+  version '142.4245.2'
+  sha256 '9fe33d27853377dff1f9a8828dd4e01108b3c56c016a98931f99c9a978953d3a'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}-custom-jdk-bundled.dmg"
   name 'RubyMine EAP'
