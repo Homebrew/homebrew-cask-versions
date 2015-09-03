f851ea30bfa07e6f47443c5b1e2c86cc8cff975a
Michael <fanquake@gmail.com>
2015-09-03 09:11:11 +0800

    Handbrake CLI Nightly 0.10.2-794-8d06a57

--- Casks/handbrakecli-nightly.rb
+++ Casks/handbrakecli-nightly.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'handbrakecli-nightly' do
-  version '0.10.2-788-4fb5fbd'
-  sha256 '9105700eb0bcc818d7bff96bdf1b15de56beeebe4ffae1dfd89cba740be46347'
+  version '0.10.2-794-8d06a57'
+  sha256 'bfd9f01b2d758a955267d19606fcb61d3ced3ad5b81b1642af015d968280ee98'
 
-  url "http://download.handbrake.fr/nightly/HandBrake-#{version}-MacOSX.7_CLI_x86_64.dmg"
+  url "http://download.handbrake.fr/nightly/HandBrake-#{version}-master-MacOSX.7_CLI_x86_64.dmg"
   name 'HandBrake'
   homepage 'https://handbrake.fr'
   license :gpl
