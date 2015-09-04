5e4626ed5cf7ff01b56b6dbc462cf9019291e728
fanquake <fanquake@gmail.com>
2015-09-04 14:04:33 +0800

    Firefox Beta DE 41.0b6

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -1,11 +1,17 @@
 cask :v1 => 'firefox-beta-de' do
-  version '40.0b2'
-  sha256 '4660ab57ed5c3b5c1b5dd2436df2c8c5cf1f353d245bbf2e80b17336268ee634'
+  version '41.0b6'
+  sha256 '4e4a13131fac4263729e56c638d53b1c2acf76462ea711fb88ccfdb07753cc25'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
+  name 'Firefox'
   homepage 'https://www.mozilla.org/de/firefox/beta/'
   license :mpl
   tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
