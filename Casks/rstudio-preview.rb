483cf1483441f452528f6bfce2f175d3c719ff60
Michael <fanquake@gmail.com>
2015-09-04 11:09:10 +0800

    Rstudio Preview: Add name and zap stanzas

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -2,9 +2,13 @@ cask :v1 => 'rstudio-preview' do
   version :latest
   sha256 :no_check
 
+  # rstudio.org is the official download host per the vendor homepage
   url 'http://www.rstudio.org/download/latest/preview/desktop/mac/RStudio-latest.dmg'
+  name 'RStudio'
   homepage 'http://www.rstudio.com/ide/download/preview'
-  license :unknown
+  license :affero
 
   app 'RStudio.app'
+  
+  zap :delete => '~/.rstudio-desktop'
 end
