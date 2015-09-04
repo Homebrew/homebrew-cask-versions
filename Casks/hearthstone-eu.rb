d2443c2bd3580360a20abf74e3e2e788bda4bb87
Vítor Galvão <info@vitorgalvao.com>
2015-09-04 14:16:44 +0100

    hearthstone-eu.rb: fix url

--- Casks/hearthstone-eu.rb
+++ Casks/hearthstone-eu.rb
@@ -2,7 +2,7 @@ cask :v1 => 'hearthstone-eu' do
   version :latest
   sha256 :no_check
 
-  url 'http://dist.blizzard.com/downloads/hs-installers/a6029a1d625c79252defff3914fb6e67/retail.1/Hearthstone-Setup-enGB.zip'
+  url 'http://eu.battle.net/download/getInstaller?os=mac&installer=Hearthstone-Setup-enGB.zip'
   homepage 'http://eu.battle.net/hearthstone/en/'
   license :unknown
 
