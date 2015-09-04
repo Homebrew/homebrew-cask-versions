5ea8411e231116e729734a5a157ea8b5c8a05ccb
Michael <fanquake@gmail.com>
2015-09-04 11:13:48 +0800

    PyCharm CE Bundled JDK 4.5.4

--- Casks/pycharm-ce-bundled-jdk.rb
+++ Casks/pycharm-ce-bundled-jdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pycharm-ce-bundled-jdk' do
-  version '4.5.1'
-  sha256 '8929fa6e995a895244731a1ac2ab888593decb7d0592ba560280e845ee4ebe31'
+  version '4.5.4'
+  sha256 '1d73dcad768da8e5bf4de0af928144ed3f4e2ea49dc3e26c8980cf9598b86b8a'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}-jdk-bundled.dmg"
   name 'PyCharm Community Edition'
