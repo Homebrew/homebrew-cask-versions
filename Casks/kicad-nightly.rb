d2d9f5e8cd34bea33a14899c3c7d77cef31df565
Michael <fanquake@gmail.com>
2015-09-04 12:18:06 +0800

    Kicad Nightly: remove extra space from SHA256

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'kicad-nightly' do
   version 'r6149.20150903-035321'
-  sha256 'b9c1cb234e5efd2c2266719e128865d6a01a4986a1c1a0dee7337d665e8c7e6b '
+  sha256 'b9c1cb234e5efd2c2266719e128865d6a01a4986a1c1a0dee7337d665e8c7e6b'
 
   url "http://downloads.kicad-pcb.org/osx/kicad-#{version}.dmg"
   name 'KiCad'
