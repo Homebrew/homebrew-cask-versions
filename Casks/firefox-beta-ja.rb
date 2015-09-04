7b9a73a2af039a1f09d5b26f9dcef92c653282c6
fanquake <fanquake@gmail.com>
2015-09-04 14:11:19 +0800

    Firefox Beta JA 41.0b6

--- Casks/firefox-beta-ja.rb
+++ Casks/firefox-beta-ja.rb
@@ -1,11 +1,17 @@
 cask :v1 => 'firefox-beta-ja' do
-  version '40.0b2'
-  sha256 '27f4a4ee16f2f00090673afb642ec00d4c7c14433c0803e41108206793b5ad76'
+  version '41.0b6'
+  sha256 'd046670e40afe56a1bd8abc4da71a88922166509848f3b3196a1a7197dc16e76'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
+  name 'Firefox'
   homepage 'https://www.mozilla.org/ja/firefox/channel/beta'
   license :mpl
   tags :vendor => 'Mozilla'
 
   app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
 end
