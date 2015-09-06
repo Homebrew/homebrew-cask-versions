16bdf13cfbb910e638b300f725db879275d08140
Michael <fanquake@gmail.com>
2015-09-06 18:21:26 +0800

    KeePassX: Add name and update URL & homepage

--- Casks/keepassx0.rb
+++ Casks/keepassx0.rb
@@ -2,8 +2,9 @@ cask :v1 => 'keepassx0' do
   version '0.4.3'
   sha256 '15ce2e950ab78ccb6956de985c1fcbf11d27df6a58ab7bf19e106f0a1dc2fedd'
 
-  url "http://downloads.sourceforge.net/keepassx/KeePassX-#{version}.dmg"
-  homepage 'http://www.keepassx.org/'
+  url "https://www.keepassx.org/releases/KeePassX-#{version}.dmg"
+  name "KeePassX"
+  homepage 'https://www.keepassx.org/'
   license :oss
 
   app 'KeePassX.app'
