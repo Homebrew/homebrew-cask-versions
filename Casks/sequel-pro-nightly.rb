0d2cd0b1e49fc4e3c3481459985c5f32b7319636
RJ Hsiao <rj19860725@gmail.com>
2015-09-09 21:58:13 +0800

    Update sequel-pro-nightly
    As title

--- Casks/sequel-pro-nightly.rb
+++ Casks/sequel-pro-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sequel-pro-nightly' do
-  version 'r4552d5daa1'
-  sha256 'fc3b19e66fd92cecff5707783db3537f0662f4df6ac0792a5bc9669101295f21'
+  version 'r6f00eaa2e6'
+  sha256 '514b8f9da31154d97ceab05e6f4b3211126dcd2a64af7eec322eb7bbec6d6bbb'
 
   url "http://nightly.sequelpro.com/builds/Sequel_Pro_#{version}.dmg"
   name 'Sequel Pro'
