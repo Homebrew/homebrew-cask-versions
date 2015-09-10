0de39287746e87b45b4058ce84e07a3775790824
Michael <fanquake@gmail.com>
2015-09-10 22:01:45 +0800

    Thunderbird DE 38.2.0

--- Casks/thunderbird-de.rb
+++ Casks/thunderbird-de.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-de' do
-  version '38.0.1'
-  sha256 'a40e1381362591307047a906196861e1227f0f031e8bbd481da81f870e51e67f'
+  version '38.2.0'
+  sha256 'f88ab22e2105674c4ac3e63956be7493f9251de3e366d5a3e8134420f50e8688'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=de"
   name 'Mozilla Thunderbird'
