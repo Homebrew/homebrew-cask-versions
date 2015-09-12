af7471f5242c893d028cd92993370f0382f71747
Vítor Galvão <info@vitorgalvao.com>
2015-09-12 01:15:15 +0100

    timemachinescheduler-beta.rb: version interpolation

--- Casks/timemachinescheduler-beta.rb
+++ Casks/timemachinescheduler-beta.rb
@@ -1,11 +1,11 @@
 cask :v1 => 'timemachinescheduler-beta' do
-  version '4.0b8(510)'
+  version '4.0b8'
   sha256 '8f05280e3375c6d7d7a7767ed3ae4be2c69b6c7826153b8ec28f566b51404d59'
 
-  url 'http://www.klieme.com/Downloads/TimeMachineScheduler/TimeMachineScheduler_4.0b8Full.zip'
+  url "http://www.klieme.com/Downloads/TimeMachineScheduler/TimeMachineScheduler_#{version}Full.zip"
   homepage 'http://www.klieme.com/TimeMachineScheduler.html'
   license :unknown
 
-  container :nested => 'TimeMachineScheduler_4.0b8.dmg'
+  container :nested => "TimeMachineScheduler_#{version}.dmg"
   app 'TimeMachineScheduler.app'
 end
