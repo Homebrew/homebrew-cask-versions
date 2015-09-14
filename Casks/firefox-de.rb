618211aeb55ed1eb20b0660e5b4ada77fcbcd1fb
Michael <fanquake@gmail.com>
2015-09-14 10:52:18 +0800

    Firefox DE: Switch to versioned download

--- Casks/firefox-de.rb
+++ Casks/firefox-de.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-de' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 '477b973186bd73614c87f33def4b21fb72e84d98d5c2df1a33aa6efc4008ccfa'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=de'
+  url 'https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de'
   name 'Firefox'
   homepage 'https://www.mozilla.org/de/firefox/desktop'
   license :mpl
