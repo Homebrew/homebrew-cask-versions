2f45f3be1a4b2d5c1eb7327e82b9375d20635a56
Michael <fanquake@gmail.com>
2015-09-14 10:22:59 +0800

    Firefox Beta RU 41.0b9

--- Casks/firefox-beta-ru.rb
+++ Casks/firefox-beta-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-ru' do
-  version '41.0b6'
-  sha256 'c74cb8c9b68a787444d69c198ff9f79d76cf3de11dec784ac9df285dc9cee38c'
+  version '41.0b9'
+  sha256 'f177a767561cd5e01291709d63e05925dac6b88a21538f277680cc909e9eb892'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ru"
   name 'Firefox'
