b8911e71ff1dba52bf07f7861b3166f3fdc6ec18
Michael <fanquake@gmail.com>
2015-09-14 11:30:58 +0800

    Firefox PT: Switch to versioned download

--- Casks/firefox-pt.rb
+++ Casks/firefox-pt.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-pt' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 '9d295f63bbc05bbbc73d491bfa223ef41f41ef2d73986e3c1c5b9fefbfed3eda'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=pt-BR'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=pt-BR"
   name 'Firefox'
   homepage 'https://www.mozilla.org/pt/firefox/new'
   license :mpl
