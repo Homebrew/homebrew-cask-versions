05ea3666ed6e62bf0fc65ab419435c2ea931efb9
Michael <fanquake@gmail.com>
2015-09-14 13:47:02 +0800

    0xdbe EAP 142.4685.2
    Update homepage stanza

--- Casks/0xdbe-eap.rb
+++ Casks/0xdbe-eap.rb
@@ -1,11 +1,11 @@
 cask :v1 => '0xdbe-eap' do
-  version '142.4250.1'
-  sha256 'f0b3026daaa243ff6ec8fa6ea80f9521dcd8234068f359cf2c72d00b3a705713'
+  version '142.4685.2'
+  sha256 'd752edceb345d1ca5df07e23b7a6c2414fe823e6329f46f2dd579cdc89c25169'
 
   url "https://download.jetbrains.com/dbe/0xdbe-#{version}.dmg"
   name '0xDBE EAP'
   name '0xDBE'
-  homepage 'https://www.jetbrains.com/dbe/eap/'
+  homepage 'https://confluence.jetbrains.com/display/DBE/0xDBE+1.0+EAP'
   license :commercial
 
   app '0xDBE EAP.app'
