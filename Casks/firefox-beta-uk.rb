1e5ee07b32c9aab31235bb5cbf0995661515eaf4
Michael <fanquake@gmail.com>
2015-09-14 10:12:56 +0800

    Firefox UK Beta 41.0b9

--- Casks/firefox-beta-uk.rb
+++ Casks/firefox-beta-uk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta-uk' do
-  version '41.0b6'
-  sha256 'c21afeee6b6bbd10b32e39a8949e47fc5953c55ff317dc21380d390375cb4e7f'
+  version '41.0b9'
+  sha256 'eea9bcf5b3c2a1be7e2a72bbf90478fffd838d8d1be659751db96eb9e19b110b'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=uk"
   name 'Firefox'
