54a80f25842c6b943142dce971975a64be1c073e
Michael <fanquake@gmail.com>
2015-09-14 11:27:30 +0800

    Firefox PL: Switch to versioned download

--- Casks/firefox-pl.rb
+++ Casks/firefox-pl.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-pl' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 '52973dbd4ef272abd04291a466d26af1f5fb0a077c7c193c61f01bc59ab3602c'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=pl'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=pl"
   name 'Firefox'
   homepage 'https://www.mozilla.org/pl/firefox/desktop'
   license :mpl
