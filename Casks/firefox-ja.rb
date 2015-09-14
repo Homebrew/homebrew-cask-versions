0a904f867baf54d8b669fd224d3ebdae07e7c886
Michael <fanquake@gmail.com>
2015-09-14 11:17:59 +0800

    Firefox JA: Switch to versioned download

--- Casks/firefox-ja.rb
+++ Casks/firefox-ja.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-ja' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 'dcf692ffc6cdc15755db3a7ef0a5c64bc2c750e951d17cd649face1c82f410a3'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=ja-JP-mac'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
   name 'Firefox'
   homepage 'http://www.mozilla.jp/firefox/'
   license :mpl
