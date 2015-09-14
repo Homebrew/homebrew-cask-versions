7ab10a29aa6c9ad5b6f2cd2612ad439bd2d236f9
Michael <fanquake@gmail.com>
2015-09-14 11:43:49 +0800

    Firefox TW: Switch to versioned download

--- Casks/firefox-tw.rb
+++ Casks/firefox-tw.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-tw' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 '9b781d626b379c298f61b0315860086ebac3d98d6bca22c53e393c9bc6d287ce'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=zh-TW'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-TW"
   name 'Firefox'
   homepage 'http://mozilla.com.tw/firefox/new/'
   license :mpl
