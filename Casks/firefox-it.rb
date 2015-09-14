922b7faa29efc1aefc90d4997fe02b4ea8cfdaac
Michael <fanquake@gmail.com>
2015-09-14 11:15:12 +0800

    Firefox IT: Switch to versioned download

--- Casks/firefox-it.rb
+++ Casks/firefox-it.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-it' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 '464791ce0c94311540a85f5e3636a3d6bc4ad9c3622c5a1a398092510db33360'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=it'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=it"
   name 'Firefox'
   homepage 'https://www.mozilla.org/it/firefox/desktop'
   license :mpl
