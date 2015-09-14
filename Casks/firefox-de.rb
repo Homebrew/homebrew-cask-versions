c14658be86fa46d7ca7ec0955239cdc8cbbe5827
Michael <fanquake@gmail.com>
2015-09-14 10:55:32 +0800

    Firefox DE: Correct url stanza

--- Casks/firefox-de.rb
+++ Casks/firefox-de.rb
@@ -2,7 +2,7 @@ cask :v1 => 'firefox-de' do
   version '40.0.3'
   sha256 '477b973186bd73614c87f33def4b21fb72e84d98d5c2df1a33aa6efc4008ccfa'
 
-  url 'https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   name 'Firefox'
   homepage 'https://www.mozilla.org/de/firefox/desktop'
   license :mpl
