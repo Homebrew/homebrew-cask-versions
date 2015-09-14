fa8ef8da6c77cec0579179fce8a6ed493e1a48ce
Michael <fanquake@gmail.com>
2015-09-14 11:01:42 +0800

    Firefox GL: Switched to versioned download

--- Casks/firefox-gl.rb
+++ Casks/firefox-gl.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'firefox-gl' do
-  version :latest
-  sha256 :no_check
+  version '40.0.3'
+  sha256 'd4d8e86fcdecbbf9b5ef11eb1e231defc5358cd3061ebb55b1ef6570069e652b'
 
-  url 'https://download.mozilla.org/?product=firefox-latest&os=osx&lang=gl'
+  url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=gl"
   name 'Firefox'
   homepage 'https://www.mozilla.org/gl/firefox/new'
   license :mpl
