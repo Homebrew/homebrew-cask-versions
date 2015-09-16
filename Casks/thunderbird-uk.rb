bfc32f9241b6b498c4a07adb88a3bd6b3fe50c5c
Michael <fanquake@gmail.com>
2015-09-16 22:09:45 +0800

    Thunderbird UK 38.2.0

--- Casks/thunderbird-uk.rb
+++ Casks/thunderbird-uk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-uk' do
-  version '38.0.1'
-  sha256 'f7b3918160bac05842b295bf99bfe96cbba9697ed423dec1012319282992bc99'
+  version '38.2.0'
+  sha256 'e3e806836ac9cdc0003ee9a0e7d62045edb75399f5f796622834dc19315f1d27'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=uk"
   name 'Mozilla Thunderbird'
