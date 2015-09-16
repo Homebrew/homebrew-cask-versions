8572019e3065fe4fb0e4a98215cb9b15f41c8bd6
Michael <fanquake@gmail.com>
2015-09-16 22:07:06 +0800

    Thunderbird RU 38.2.0

--- Casks/thunderbird-ru.rb
+++ Casks/thunderbird-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-ru' do
-  version '38.0.1'
-  sha256 '846209f52343ad50c5f8b33dfaa1e22dddbc4ab73039fbf53e69303ff82648f2'
+  version '38.2.0'
+  sha256 'c5a17345a0965814b0988c14db17bd76e70af6ccf3b1f4abc01e309976d3bc12'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=ru"
   name 'Mozilla Thunderbird'
