da1711a6bb49d4a761de56a130a24b881ccccb51
Michael <fanquake@gmail.com>
2015-09-17 11:00:49 +0800

    Wireshark Dev 1.99.9

--- Casks/wireshark-dev.rb
+++ Casks/wireshark-dev.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'wireshark-dev' do
-  version '1.99.8'
-  sha256 'a6d2c14b74bfb27d3cf69ddda3919ace7b56facc7a9f6b3959ad630667ead240'
+  version '1.99.9'
+  sha256 'b936f35e6a83112b95d6ed512650ae2d0fb4a5fd9a639c783119e1baaf394973'
 
   url "https://2.na.dl.wireshark.org/osx/Wireshark%20#{version}%20Intel%2064.dmg"
-  homepage 'http://www.wireshark.org'
+  homepage 'https://www.wireshark.org'
   license :gpl
 
   pkg "Wireshark #{version} Intel 64.pkg"
