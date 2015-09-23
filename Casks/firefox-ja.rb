140d10674c0e3479bae286bc12a4b9727c678781
Michael <fanquake@gmail.com>
2015-09-23 19:00:29 +0800

    Firefox JA 41.0

--- Casks/firefox-ja.rb
+++ Casks/firefox-ja.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'firefox-ja' do
-  version '40.0.3'
-  sha256 'dcf692ffc6cdc15755db3a7ef0a5c64bc2c750e951d17cd649face1c82f410a3'
+  version '41.0'
+  sha256 'e3317d398d4d8ac8dcde5b04c22da1e2881fcd061d8a88f73a2a3eac5c329211'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
   name 'Firefox'
-  homepage 'http://www.mozilla.jp/firefox/'
+  homepage 'https://www.mozilla.org/ja/firefox/desktop/'
   license :mpl
   tags :vendor => 'Mozilla'
 
