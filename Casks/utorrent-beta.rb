bdbe512e217482152a2185f6c9acdfdae5e9fce5
Michael <fanquake@gmail.com>
2015-09-23 18:38:29 +0800

    µTorrent Beta: Add zap & name stanza

--- Casks/utorrent-beta.rb
+++ Casks/utorrent-beta.rb
@@ -3,8 +3,19 @@ cask :v1 => 'utorrent-beta' do
   sha256 :no_check
 
   url 'http://download-new.utorrent.com/endpoint/utmac/os/osx/track/beta/'
+  name 'µTorrent'
   homepage 'http://www.utorrent.com/'
-  license :unknown
+  license :freemium
+
+  zap :delete => [
+                  '~/Library/Application Support/uTorrent',
+                  '~/Library/Preferences/com.bittorrent.uTorrent.plist',
+                  '~/Library/Preferences/com.bittorrent.uTorrent-Installer.plist',
+                  '~/Library/Saved Application State/com.bittorrent.uTorrent.savedState',
+                  '~/Library/Saved Application State/com.bittorrent.uTorrent-Installer.savedState',
+                  '~/Library/Caches/com.bittorrent.uTorrent',
+                  '~/Library/Caches/com.bittorrent.uTorrent-Installer'
+                 ]
 
   app 'uTorrent.app'
 end
