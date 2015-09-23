d5ca8d2921b8428c6bfe6c0f3ec89144a99940b9
Michael <fanquake@gmail.com>
2015-09-23 18:07:58 +0800

    TorBrowser DE 5.0.3

--- Casks/torbrowser-de.rb
+++ Casks/torbrowser-de.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-de' do
-  version '5.0.2'
-  sha256 'b578c0d1073d6ab029deb997ee7c6e626ac0c454726941521bcbf0c6abeab571'
+  version '5.0.3'
+  sha256 '547ed7f67c29e9ea01f120ad0f89fbf54ed76218d740f5415e4f9d35bfb15c1b'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_de.dmg"
   gpg "#{url}.asc",
