32e3ac08b416db9a04623d8030d7c108cc7e718d
Michael <fanquake@gmail.com>
2015-09-23 20:27:13 +0800

    Audacity210: Update url stanza

--- Casks/audacity210.rb
+++ Casks/audacity210.rb
@@ -2,9 +2,10 @@ cask :v1 => 'audacity210' do
   version '2.1.0'
   sha256 '0485ab70a86ab6d500b9365b24d279f10971b9260f0b537142b48832f8f84389'
 
-  url "http://downloads.sourceforge.net/project/audacity/audacity/#{version}/audacity-macosx-ub-#{version}.dmg"
+  # oldfoss.com is the official download host per the vendor homepage
+  url "http://www.oldfoss.com/Audacity/download/audacity-macosx-ub-#{version}.dmg"
   name 'Audacity'
-  homepage 'http://audacity.sourceforge.net/'
+  homepage 'http://audacityteam.org'
   license :gpl
 
   app 'Audacity/Audacity.app'
