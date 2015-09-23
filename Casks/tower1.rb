8f277507161555fb8fee0f4dc96691d3c774a19a
Michael <fanquake@gmail.com>
2015-09-23 20:32:38 +0800

    Tower1: Add name and update license stanza

--- Casks/tower1.rb
+++ Casks/tower1.rb
@@ -5,8 +5,9 @@ cask :v1 => 'tower1' do
   url 'http://www.git-tower.com/download-v1'
   appcast 'https://macapps.fournova.com/tower1-1060/updates.xml',
           :sha256 => '0a053ca1cf31f5dd6512a4528a4d793a4993c2e180002c4967511a6dc9bfbf87'
+  name 'Tower'
   homepage 'http://www.git-tower.com/'
-  license :unknown
+  license :commercial
 
   app 'Tower.app'
 end
