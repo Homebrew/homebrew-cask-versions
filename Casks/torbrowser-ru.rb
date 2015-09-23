559d17fb204a82ec38eae4e617e95f91a00ab1d0
Michael <fanquake@gmail.com>
2015-09-23 18:28:06 +0800

    TorBrowser RU 5.0.3

--- Casks/torbrowser-ru.rb
+++ Casks/torbrowser-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'torbrowser-ru' do
-  version '5.0.2'
-  sha256 '7176ff88a12755a430b1ada680792a27c227db7b50c367370da941fe598ab517'
+  version '5.0.3'
+  sha256 '62986d348b82d5a30f9f1cdc84b594990884e547f9bdecbafbc2028522b1b270'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_ru.dmg"
   gpg "#{url}.asc",
