ce7b96c9e3ccb98bfd9d91b1413bd286ed704c87
Michael <fanquake@gmail.com>
2015-09-23 18:52:36 +0800

    Aquamacs25 Add name and zap stanza

--- Casks/aquamacs25.rb
+++ Casks/aquamacs25.rb
@@ -3,8 +3,11 @@ cask :v1 => 'aquamacs25' do
   sha256 '5857848d8d46bba43d160c02393b098a370e2156608be24b288419f668210be9'
 
   url "http://braeburn.aquamacs.org/releases/Aquamacs-Emacs-#{version}.dmg"
+  name 'Aquamacs'
   homepage 'http://aquamacs.org/'
-  license :oss
+  license :gpl
 
   app 'Aquamacs.app'
+  
+  zap :delete => '~/Library/Caches/Aquamacs Emacs'
 end
