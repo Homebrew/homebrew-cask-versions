54d57813af2a5e3dbcfbcdbf4e94f802826bf988
Michael <fanquake@gmail.com>
2015-09-23 18:41:01 +0800

    VLC Nightly: Add name stanza

--- Casks/vlc-nightly.rb
+++ Casks/vlc-nightly.rb
@@ -3,7 +3,9 @@ cask :v1 => 'vlc-nightly' do
   sha256 :no_check
 
   url 'http://nightlies.videolan.org/build/macosx-intel/last'
-  homepage 'http://www.videolan.org/vlc/'
+  name 'VLC'
+  name 'VLC media player'
+  homepage 'https://www.videolan.org/vlc/'
   license :oss
 
   app 'vlc-3.0.0-git/VLC.app'
