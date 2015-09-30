8d983ee9b3a998ff66ddffb1fcae40e29a0a8fd3
mahori <mahori@users.noreply.github.com>
2015-10-01 08:28:09 +0900

    Upgrade Thunderbird (Japanese) to 38.3.0

--- Casks/thunderbird-ja.rb
+++ Casks/thunderbird-ja.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-ja' do
-  version '38.2.0'
-  sha256 '2014b6fac54d398f63c442cc531ddfa77b604ecd616a98e67b2197225135398b'
+  version '38.3.0'
+  sha256 '0b023a4173b05f59589124db197bac413c4d81f8d53894cb0e2c57ef2b2d53c4'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=ja-JP-mac"
   name 'Mozilla Thunderbird'
