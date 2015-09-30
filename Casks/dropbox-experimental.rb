20ddf47c382e34363826b6b36b9131aeeec53403
Michael <fanquake@gmail.com>
2015-09-30 22:54:56 +0800

    Dropbox Experimental 3.11.31

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -1,9 +1,9 @@
 cask :v1 => 'dropbox-experimental' do
-  version '3.10.5'
-  sha256 '2a384b96100c7a7f1f93829f2b17ba369f1e9e92d9cf8bab948a0dfb8d142e8e'
+  version '3.11.31'
+  sha256 'd0e0129bfe6c20d84fea09bad9becba06f65f08f66bcf3376c8357b924124b35'
 
   url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
-  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/205056979-Release-Candidate-3-10-5'
+  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/203204043-Experimental-Build-3-11-31'
   license :gratis
 
   app 'Dropbox.app'
