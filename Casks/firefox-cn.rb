0bddfd7b0a63f41cd95180abe8b54fe3dd7e0987
Michael <fanquake@gmail.com>
2015-10-01 14:24:21 +0800

    Firefox CN 41.0.1

--- Casks/firefox-cn.rb
+++ Casks/firefox-cn.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-cn' do
-  version '41.0'
-  sha256 'e3317d398d4d8ac8dcde5b04c22da1e2881fcd061d8a88f73a2a3eac5c329211'
+  version '41.0.1'
+  sha256 '9774877ff3ed2a972d0d73fece3ce1cb3b68303921f15e5046fcd4ad65a8dc18'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
   name 'Firefox'
