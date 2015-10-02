ef89fbe3cd0b8425c78d767b6cdd427c598da253
Michael <fanquake@gmail.com>
2015-10-02 13:38:00 +0800

    SketchUpPro 2015

--- Casks/sketchup-pro.rb
+++ Casks/sketchup-pro.rb
@@ -3,9 +3,16 @@ cask :v1 => 'sketchup-pro' do
   sha256 :no_check
 
   # downloads can be found at http://www.sketchup.com/download/all
+  # trimble.com is the official download host per the vendor homepage
   url 'https://dl.trimble.com/sketchup/SketchUpPro-en.dmg'
+  name 'SketchUp'
   homepage 'http://www.sketchup.com/intl/en/'
-  license :unknown
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
-  suite 'SketchUp 2014'
+  suite 'SketchUp 2015'
+
+  zap :delete => [
+                  '~/Library/Application Support/SketchUp 2015',
+                  '~/Library/Caches/com.sketchup.SketchUp.2015',
+                 ]
 end
