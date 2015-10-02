83847d91529a5f4e874169567e13fce80c0698e1
Michael <fanquake@gmail.com>
2015-10-02 13:41:26 +0800

    Skitch1: Add name, update license stanza

--- Casks/skitch1.rb
+++ Casks/skitch1.rb
@@ -3,8 +3,9 @@ cask :v1 => 'skitch1' do
   sha256 :no_check
 
   url 'http://evernote.com/download/get.php?file=SkitchMac_v1'
+  name 'Skitch'
   homepage 'http://blog.evernote.com/blog/2012/11/21/skitch-and-evernote-a-letter-from-keith-lang/'
-  license :unknown
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'Skitch.app'
 end
