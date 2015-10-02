ed3f2b216d72b863e29f37805527e01bc4004be5
Michael <fanquake@gmail.com>
2015-10-02 13:35:32 +0800

    Sizeup-x11: Add name, switch urls to HTTPS

--- Casks/sizeup-x11.rb
+++ Casks/sizeup-x11.rb
@@ -2,12 +2,14 @@ cask :v1 => 'sizeup-x11' do
   version :latest
   sha256 :no_check
 
-  url 'http://www.irradiatedsoftware.com/downloads/SizeUpX11.zip'
-  appcast 'http://www.irradiatedsoftware.com/updates/profiles/sizeup.php'
-  homepage 'http://www.irradiatedsoftware.com/sizeup/index.html'
-  license :unknown
+  url 'https://www.irradiatedsoftware.com/downloads/SizeUpX11.zip'
+  appcast 'https://www.irradiatedsoftware.com/updates/profiles/sizeup.php'
+  name 'SizeUp'
+  homepage 'https://www.irradiatedsoftware.com/sizeup/index.html'
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'SizeUp.app'
+  
   zap :delete => [
                   '~/Library/Preferences/com.irradiatedsoftware.SizeUp.plist',
                   '~/Library/Application Support/SizeUp',
