f05d5d06a4685a019bed5258e6cd7cbe3edade62
Michael <fanquake@gmail.com>
2015-10-02 13:22:43 +0800

    Arq3: Fix license, add name stanza

--- Casks/arq3.rb
+++ Casks/arq3.rb
@@ -3,8 +3,9 @@ cask :v1 => 'arq3' do
   sha256 '2b4317b83d14090764f2134d4cc0fa3ee18b102974cff935f47a8afb4b3860a2'
 
   url "http://www.haystacksoftware.com/arq/Arq_#{version}.zip"
+  name 'Arq'
   homepage 'http://www.haystacksoftware.com/arq/'
-  license :unknown
+  license :commercial
 
   app 'Arq.app'
 end
