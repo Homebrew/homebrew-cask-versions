8cb916b7ef0434a024c6cb14e3b04b1142ac4106
Michael <fanquake@gmail.com>
2015-10-02 10:08:46 +0800

    Firefox DE 41.0.1

--- Casks/firefox-de.rb
+++ Casks/firefox-de.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-de' do
-  version '41.0'
-  sha256 '7db86c4c08710c16f58f5f2bef1239e6ffed5f576d9b7bd68bf2b8018a90dfdc'
+  version '41.0.1'
+  sha256 'd668dbd18bcea8285bc8abb2d6401db60cebb53ebc9b8a144568228dfeb0b051'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   name 'Firefox'
