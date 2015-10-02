43e0182a99ca59f3f6dbb3e6dd363e20f6b1924c
Victor Popkov <victor@popkov.me>
2015-10-02 21:33:21 +0300

    opera-beta.rb: upgrade to v33.0.1990.11

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera-beta' do
-  version '32.0.1948.19'
-  sha256 '54241ca897aca1f2578eddbdc2394f8a83b76777e52dbcc7eae2468f32a399af'
+  version '33.0.1990.11'
+  sha256 '8ae4bb5a9d4c443d0061cc5373b9a16fec2e6f42bc786f32e72bed9cb9d9cb80'
 
   url "http://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/computer/beta'
