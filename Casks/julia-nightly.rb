d6af3edf41368d9c5f875554f53ac2d36c1692cc
Michael <fanquake@gmail.com>
2015-10-06 18:31:48 +0800

    Julia Nightly 0.5.0-a3d04ab05e

--- Casks/julia-nightly.rb
+++ Casks/julia-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'julia-nightly' do
-  version '0.5.0-b22e43a2bd'
-  sha256 'f9949e05c48b8914af8a5783746993af6955ec662153449ff1f366f3af23c9b1'
+  version '0.5.0-a3d04ab05e'
+  sha256 'a8b953b3f3a9381a403d8b7f3c50d38d6403f631e2e1477d7cdd78a6b2f46aae'
 
   # amazonaws.com is the official download host per vendor homepage
   url "https://s3.amazonaws.com/julianightlies/bin/osx/x64/#{version.sub(%r{(\d+\.\d+).*},'\1')}/julia-#{version}-osx.dmg"
