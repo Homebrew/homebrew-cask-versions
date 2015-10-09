19987482d89085c89b3521dee29b5ea3ec8fb725
Michael <fanquake@gmail.com>
2015-10-09 14:39:03 +0800

    PyCharm CE EAP 143.24

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'pycharm-ce-eap' do
-  version '142.4957'
-  sha256 '17d50688993b914a72487934914353ef396f8baedec0acb0a79d5ce11c4a2dba'
+  version '143.24'
+  sha256 '161052cf354bd33f70d067575f4e9dc45d26fd9f451091e3d67282d1696b2932'
 
-  url "http://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
+  url "http://download.jetbrains.com/python/pycharm-community-#{version}-jdk-bundled.dmg"
   name 'PyCharm Community Edition'
   homepage 'http://confluence.jetbrains.com/display/PYH/JetBrains+PyCharm+Preview+(EAP)'
   license :commercial
