c5f845cae2cf6c1a86a0a39b22b806a37898d599
Michael <fanquake@gmail.com>
2015-10-10 21:53:18 +0800

    Mozart2: Add name

--- Casks/mozart2.rb
+++ Casks/mozart2.rb
@@ -2,7 +2,9 @@ cask :v1 => 'mozart2' do
   version '2.0.0'
   sha256 '4f077b0658557e532bfa7519977ce9870c55350a1cb395ef77bcab623c70ee04'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url 'http://downloads.sourceforge.net/project/mozart-oz/v2.0.0-alpha.0/mozart2-2.0.0-alpha.0%2Bbuild.4116.bdeaf6c-x86_64-darwin.dmg'
+  name 'Mozart'
   homepage 'http://www.mozart-oz.org'
   license :oss
 
