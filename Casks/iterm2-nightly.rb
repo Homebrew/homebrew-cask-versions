7946f69b4a75e7062cb10b046933b95d94f42ca2
Michael <fanquake@gmail.com>
2015-10-10 16:58:26 +0800

    Switch Iterm2 Nightly to versioned download

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'iterm2-nightly' do
-  version :latest
-  sha256 :no_check
+  version '2_9_20151006'
+  sha256 '1b066544e3c2507c0ec43d6999ed5b450589651fc519f490dc289fb1a4a3d936'
 
-  url 'https://www.iterm2.com/nightly/latest'
+  url "https://www.iterm2.com/downloads/nightly/iTerm2-#{version}-nightly.zip"
   appcast 'https://iterm2.com/appcasts/nightly.xml'
   name 'iTerm2'
   name 'iTerm 2'
