1e9f4efc2f1b8da3359511504502d9cdbdc67d20
Victor Popkov <victor@popkov.me>
2015-10-10 22:57:34 +0300

    opera-developer.rb: upgrade to v34.0.2011.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera-developer' do
-  version '34.0.1996.0'
-  sha256 '5cbf7dbf38769c3626926e7bbb4a53a829f6559fc2b30e904a010cd46b9b0032'
+  version '34.0.2011.0'
+  sha256 '6f2841ec279587d7e3f92912183ed428a9b9de9b80f5dd295b7b4f6011bb096b'
 
   url "http://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/developer'
