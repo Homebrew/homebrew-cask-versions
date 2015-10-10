8873603dc67ace21b1f3ff6402a89e1d26a3831e
Michael <fanquake@gmail.com>
2015-10-10 21:48:55 +0800

    Istat Menus4: Add name

--- Casks/istat-menus4.rb
+++ Casks/istat-menus4.rb
@@ -2,9 +2,11 @@ cask :v1 => 'istat-menus4' do
   version '4.22'
   sha256 '4e6873b24d18578e359b9363f02b9c98bdeda999afab1aaa8802af254ecb35b6'
 
-  url "https://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip"
+  # amazonaws.com is the official download host per the vendor homepage
+  url "https://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip
+  name 'iStats Menus'
   homepage 'http://bjango.com/mac/istatmenus/'
-  license :unknown
+  license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
 
   app 'iStat Menus.app'
 end
