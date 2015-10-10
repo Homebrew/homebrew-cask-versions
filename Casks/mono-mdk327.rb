d674eff1da3a796660899acc1265a7270b738ceb
Michael <fanquake@gmail.com>
2015-10-10 21:54:00 +0800

    Mono MDK327: Add name

--- Casks/mono-mdk327.rb
+++ Casks/mono-mdk327.rb
@@ -3,9 +3,11 @@ cask :v1 => 'mono-mdk327' do
   sha256 '261a9ed737f2e8185149857c1a8238bf26a32e7e28ae412c668841fcd77ebfcb'
 
   url "http://download.mono-project.com/archive/#{version}/macos-10-x86/MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
+  name 'Mono'
   homepage 'http://mono-project.com/'
   license :oss
 
   pkg "MonoFramework-MDK-#{version}.macos10.xamarin.x86.pkg"
+  
   uninstall :pkgutil => 'com.xamarin.mono-MDK.pkg'
 end
