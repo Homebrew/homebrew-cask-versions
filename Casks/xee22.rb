0b54b0d7479c5a4456e8c34cfe2389a00fd06271
Michael <fanquake@gmail.com>
2015-10-10 21:59:57 +0800

    Xee22 Update URL and homepage

--- Casks/xee22.rb
+++ Casks/xee22.rb
@@ -2,8 +2,8 @@ cask :v1 => 'xee22' do
   version '2.2'
   sha256 '9cc173b24cfa76185a48f050c587690917f104f6a165d52ffc67e52793385f1c'
 
-  url "https://xee.googlecode.com/files/Xee#{version}.zip"
-  homepage 'https://code.google.com/p/xee/'
+  url "http://wakaba.c3.cx/releases/Xee/Xee#{version}.zip"
+  homepage 'http://xee.c3.cx/'
   license :oss
 
   app 'Xee.app'
