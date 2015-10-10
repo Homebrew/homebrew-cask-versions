0709fea1dbaf5a1e5ed7f85c177801c6076f04bc
Michael <fanquake@gmail.com>
2015-10-10 21:57:16 +0800

    Vagrant127: Add name, update license and homepage

--- Casks/vagrant127.rb
+++ Casks/vagrant127.rb
@@ -3,8 +3,9 @@ cask :v1 => 'vagrant127' do
   sha256 '75c720eda0cbe6b2a2b19e38757ba4c34fbc57095ab4cc1459609bd242418129'
 
   url "http://files.vagrantup.com/packages/7ec0ee1d00a916f80b109a298bab08e391945243/Vagrant-#{version}.dmg"
-  homepage 'http://www.vagrantup.com'
-  license :unknown
+  name 'Vagrant'
+  homepage 'https://www.vagrantup.com'
+  license :mit
 
   pkg 'Vagrant.pkg'
   uninstall :script => { :executable => 'uninstall.tool', :input => %w[Yes] }
