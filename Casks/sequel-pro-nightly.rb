fba07f16a84308e334a9b5f02e755f4bfcaa7c5e
Michael <fanquake@gmail.com>
2015-10-11 10:04:12 +0800

    Sequel Pro Nightly r2cb875fb99

--- Casks/sequel-pro-nightly.rb
+++ Casks/sequel-pro-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'sequel-pro-nightly' do
-  version 'r71c013de73'
-  sha256 '52356757d71065a95f5b95bde29f780e9a7bf4d6decb8a0123bed2653a68a5f5'
+  version 'r2cb875fb99'
+  sha256 '9f744fc5af974970670026279e996156e23abd52f45193da3d6da3926b2b54c2'
 
   url "http://nightly.sequelpro.com/builds/Sequel_Pro_#{version}.dmg"
   name 'Sequel Pro'
