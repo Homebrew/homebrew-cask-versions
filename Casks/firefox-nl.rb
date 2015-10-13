7dbbba9739557427ccb8077acb9c7f630e211360
gmkey <gijs@keij.org>
2015-10-13 15:14:35 +0200

    Firefox NL 41.0.1

--- Casks/firefox-nl.rb
+++ Casks/firefox-nl.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-nl' do
-  version '41.0'
-  sha256 '28183d1c7be86ba2d9b8f3189daeb12d3a4b10014355a98774366d504c25cf7b'
+  version '41.0.1'
+  sha256 'f9ba439657be0f94a2ffc52b3870ca69828ed7bbf7731359950036522e93189b'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=nl"
   name 'Firefox'
