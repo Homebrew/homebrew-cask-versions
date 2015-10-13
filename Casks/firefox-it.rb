363dcc197f2098915d42da5dc25ed6a3f452edd5
gmkey <gijs@keij.org>
2015-10-13 16:56:49 +0200

    Firefox IT 41.0.1

--- Casks/firefox-it.rb
+++ Casks/firefox-it.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-it' do
-  version '41.0'
-  sha256 'e3317d398d4d8ac8dcde5b04c22da1e2881fcd061d8a88f73a2a3eac5c329211'
+  version '41.0.1'
+  sha256 '352fff67fa55fc640de36336919d424fc4c62344724e971cf5849a884c76490c'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=it"
   name 'Firefox'
