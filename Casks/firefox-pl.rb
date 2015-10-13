bdd8a5e5baf970dcdd58a1785867b9ede822c2f6
gmkey <gijs@keij.org>
2015-10-13 18:30:15 +0200

    Firefox PL 41.0.1

--- Casks/firefox-pl.rb
+++ Casks/firefox-pl.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-pl' do
-  version '41.0'
-  sha256 'f224cbd621b15e8f6d54450cdabfc7d2132860511db02e8cce50d1c9a85c3d3f'
+  version '41.0.1'
+  sha256 'c98e7be565e2b3ca39fa9b5169665641ac98be8a9de5853e5892c3a6b31e9fbd'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=pl"
   name 'Firefox'
