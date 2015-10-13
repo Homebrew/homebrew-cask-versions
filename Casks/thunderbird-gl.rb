bb2e8fcc398d7a8994e7f307895de3851f68b9ee
gmkey <gijs@keij.org>
2015-10-13 12:36:36 +0200

    Thunderbird GL 38.3.0

--- Casks/thunderbird-gl.rb
+++ Casks/thunderbird-gl.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-gl' do
-  version '38.2.0'
-  sha256 'b88856ba6ef00210c86d568ee4a10c0108d62ef7d4e5e15968d81037410d7b72'
+  version '38.3.0'
+  sha256 'c87500764b61741a7b26ebf6d54b641891f80003c5794a2657c14c8bf56c29f1'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=gl"
   name 'Mozilla Thunderbird'
