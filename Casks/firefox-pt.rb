612941786b1febaffa986cef3412dd7b3097ae5f
gmkey <gijs@keij.org>
2015-10-13 17:33:23 +0200

    Firefox PT 41.0.1

--- Casks/firefox-pt.rb
+++ Casks/firefox-pt.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-pt' do
-  version '41.0'
-  sha256 'cc606d64717d66cd8500016fa1a5bf35f880c5cd5c878ba77df01d3870e43617'
+  version '41.0.1'
+  sha256 '27047ff2694f0f43f4ce6d5b22e87f797f1dd64d28f49d01843ed68e09e53023'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=pt-BR"
   name 'Firefox'
