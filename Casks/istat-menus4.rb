b931b61b54a92d03e75c9435f6ee26f67ea1aa4b
Michael <fanquake@users.noreply.github.com>
2015-10-16 07:34:56 +0800

    Istatmenu4: Add missing quote
    Fixes #1338

--- Casks/istat-menus4.rb
+++ Casks/istat-menus4.rb
@@ -3,7 +3,7 @@ cask :v1 => 'istat-menus4' do
   sha256 '4e6873b24d18578e359b9363f02b9c98bdeda999afab1aaa8802af254ecb35b6'
 
   # amazonaws.com is the official download host per the vendor homepage
-  url "https://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip
+  url "https://s3.amazonaws.com/bjango/files/istatmenus4/istatmenus#{version}.zip"
   name 'iStats Menus'
   homepage 'http://bjango.com/mac/istatmenus/'
   license :unknown    # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
