2b59ac02e2643ccbf515dff923f66ffbb668e101
Adrian Sieber <mail@adriansieber.com>
2015-10-16 19:44:26 +0200

    Add firefox version 18.0

---
+++ Casks/firefox18.rb
@@ -0,0 +1,18 @@
+cask :v1 => 'firefox' do
+  version '18.0'
+  sha256 '48bb743b075fae970ac547e007e86c547c2d283037281378855240287f4b782d'
+
+  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}/mac/en-US/Firefox%20#{version}.dmg"
+  name 'Firefox'
+  name 'Mozilla Firefox'
+  homepage 'https://www.mozilla.org/en-US/firefox/'
+  license :mpl
+  tags :vendor => 'Mozilla'
+
+  app 'Firefox.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Firefox',
+                  '~/Library/Caches/Firefox',
+                 ]
+end
