f83ad4b23627c59e4f6fadd3a5885052cdd20483
Michael <fanquake@users.noreply.github.com>
2015-10-17 14:51:33 +0800

    Sketch Beta 3.4 15575

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,8 +1,9 @@
 cask :v1 => 'sketch-beta' do
   version '3.4'
-  sha256 '009d91cd2690ed32fb60b28cb98f30422b91d0717a1332df060b28c27d73acca'
+  sha256 'f4329183d84c8954748e9c70b26d11f187100f3bd9a62a85eb69a2e90d510d8a'
 
-  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/005/842/902/original/ae580fbb47df864a3157526dbd9db1f9/Sketch_Beta.zip'
+  # cloudfront.net is the official download host per the vendor homepage
+  url 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/006/587/790/original/f683c0a5877f048cbe57429c71ffe36b/Sketch_Beta.zip'
   homepage 'http://bohemiancoding.com/sketch/beta/'
   license :commercial
 
