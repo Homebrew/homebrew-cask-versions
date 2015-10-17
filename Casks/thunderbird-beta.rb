26f0874414509924ead754e845913e1ad22c4011
Michael <fanquake@users.noreply.github.com>
2015-10-17 15:00:07 +0800

    Thunderbird Beta 42.0b2

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'thunderbird-beta' do
-  version '41.0b2'
-  sha256 '8ee47c8344a144d7b0a2ef069226aa1310bcdc83538a747fb8cc667ae3a3bf1f'
+  version '42.0b2'
+  sha256 '98081c29a30ffbe77de900de1149fba018bffb1134f7e70bfb96367cfbeec276'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=en-US"
   name 'Mozilla Thunderbird'
