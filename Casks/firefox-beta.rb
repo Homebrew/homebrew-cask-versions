e8f7f44385a3d9106a57aa9c8fc09693699e62df
Michael <fanquake@users.noreply.github.com>
2015-10-21 07:41:04 +0800

    Firefox Beta 42.0b7

--- Casks/firefox-beta.rb
+++ Casks/firefox-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-beta' do
-  version '42.0b6'
-  sha256 '5fcdd823d20e59736d3450a779f352ec84e5e7176d84e4ae3f6dac091e11922b'
+  version '42.0b7'
+  sha256 'abc1f855bcc36bf3ba354d84c4305dceef40beae998ab3ad8a4e87599ea8e47e'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-US"
   name 'Firefox'
