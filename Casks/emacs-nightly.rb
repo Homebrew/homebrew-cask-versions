63f3eb5086343d627ac35f07fd2eda6d012e528c
Michael <fanquake@users.noreply.github.com>
2015-10-21 07:34:27 +0800

    Emacs Nightly 2015-10-20_01-41-01-10df031

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'emacs-nightly' do
-  version '2015-10-17_01-41-00-4328d3d'
-  sha256 '8b9ece15a1357fb9f8f78c3ecd902acc4803fcbdedb9f6c643cd175977cc4cf0'
+  version '2015-10-20_01-41-01-10df031'
+  sha256 '0dace20bd517d1339f0e89d6c84baacb609872ef096a60abbb4ad82dff44b44c'
 
   url "http://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
   name 'Emacs for Mac OS X'
