ae0b966b4f824bcb6fba1f5cc7654513b8486e92
Vítor Galvão <info@vitorgalvao.com>
2015-10-21 01:49:26 +0100

    airmail-beta.rb: added vendor homepage comment

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -2,6 +2,7 @@ cask :v1 => 'airmail-beta' do
   version :latest
   sha256 :no_check
 
+  # hockeyapp.net is the official download host per the vendor homepage
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   homepage 'http://airmailapp.com/beta/'
   license :unknown
