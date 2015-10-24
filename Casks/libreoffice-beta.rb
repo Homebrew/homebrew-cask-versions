d9285be166af0d3f89117f55c3e34f82494fe814
Michael <fanquake@users.noreply.github.com>
2015-10-24 15:01:18 +0800

    Libre Office Beta 5.0.3.1

--- Casks/libreoffice-beta.rb
+++ Casks/libreoffice-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'libreoffice-beta' do
-  version '5.0.2.1'
-  sha256 '168f48a00da59eafeb9ddfac168cafe590e8bd87afa660d7f6c74def48a82ee9'
+  version '5.0.3.1'
+  sha256 '6abfb3a480ec03dff85872da4be7ca4a9450506ca9320b787c2fcce6e88b8f00'
 
   url "https://download.documentfoundation.org/libreoffice/testing/#{version.split('.')[0..-2].join('.')}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   gpg "#{url}.asc",
