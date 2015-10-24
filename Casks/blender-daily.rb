3631e0205b647c18e6c5ade349318ce1798a16a3
Michael <fanquake@users.noreply.github.com>
2015-10-24 09:47:13 +0800

    Blender Daily 2.76-6222505

--- Casks/blender-daily.rb
+++ Casks/blender-daily.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'blender-daily' do
-  version '2.76-bfdb420'
-  sha256 '61beb0ee8cca77a4b053dc86f71f9351a7e4bff9eaeb9e7dc7964987706f0ee2'
+  version '2.76-6222505'
+  sha256 'a153a5d480463db03712c1383bd8dc3791781cb38281fc35f44a505b89450558'
 
   url "https://builder.blender.org/download/blender-#{version}-OSX-10.6-x86_64.zip"
   name 'Blender'
