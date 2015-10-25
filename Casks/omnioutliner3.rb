51086ab6176869c6e9dcc11ab7495bde1e8c0aad
Marco Pivetta <ocramius@gmail.com>
2015-10-25 15:08:01 +0100

    OmniOutliner - Syntax error fix (causes `brew cask audit` hard failure)

--- Casks/omnioutliner3.rb
+++ Casks/omnioutliner3.rb
@@ -2,7 +2,7 @@ cask :v1 => 'omnioutliner3' do
   version :latest
   sha256 :no_check
 
-  url 'http://omnigroup.com/download/latest/omnioutliner-3
+  url 'http://omnigroup.com/download/latest/omnioutliner-3'
   name 'OmniOutliner'
   homepage 'http://www.omnigroup.com/omnioutliner/'
   license :commercial
