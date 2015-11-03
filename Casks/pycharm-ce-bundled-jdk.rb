7b20a7eb6539aace09e8190b40ff14450fee79f5
Kenzie Togami <ket1999@gmail.com>
2015-11-02 16:47:22 -0800

    Update PyCharm to 5.0

--- Casks/pycharm-ce-bundled-jdk.rb
+++ Casks/pycharm-ce-bundled-jdk.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pycharm-ce-bundled-jdk' do
-  version '4.5.4'
-  sha256 '1d73dcad768da8e5bf4de0af928144ed3f4e2ea49dc3e26c8980cf9598b86b8a'
+  version '5.0'
+  sha256 '770596ac464012439e2ff25b7ba3c49996f916a5af18a94a582c8427e61ca4e6'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}-jdk-bundled.dmg"
   name 'PyCharm Community Edition'
