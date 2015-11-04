0ab644375fd44b72561894d62ff76d9f6e53f080
Michael <fanquake@users.noreply.github.com>
2015-11-04 10:58:44 +0800

    Firefox ESR RU 38.4.0

--- Casks/firefox-esr-ru.rb
+++ Casks/firefox-esr-ru.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'firefox-esr-ru' do
-  version '38.3.0'
-  sha256 '98cdd81ef9803d51b4f6a2424f094e4478385a0b6d8b9332168863f4b7360d4e'
+  version '38.4.0'
+  sha256 '5c1226b4acd02713a9e166004ba8e55aa6300c59078e1d84c34ac19f81edac0a'
 
   url "https://download.mozilla.org/?product=firefox-#{version}esr-SSL&os=osx&lang=ru"
   name 'Firefox'
