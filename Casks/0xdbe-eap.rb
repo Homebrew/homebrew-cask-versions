9cddd90da375a8e2d5b28dfe492e957dbb47010c
Michael <fanquake@users.noreply.github.com>
2015-11-04 11:44:45 +0800

    0xdbe EAP 143.615.4

--- Casks/0xdbe-eap.rb
+++ Casks/0xdbe-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => '0xdbe-eap' do
-  version '143.324.5'
-  sha256 '5a465fc906d1cb2c694f4ef1b3c138f1d882d0c35ad1a2b3db8e7600aef3e4df'
+  version '143.615.4'
+  sha256 'bd53c5be436b39fd70984544ff76f57b8f050a01923f327b0ad1f52ff038a27d'
 
   url "https://download.jetbrains.com/dbe/0xdbe-#{version}-custom-jdk-bundled.dmg"
   name '0xDBE EAP'
