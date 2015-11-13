0108d1890ee715e2211595412e0cc0ccf55925fa
Michael Ford <fanquake@users.noreply.github.com>
2015-11-13 23:21:47 +0800

    Xscope3: Update license, use version in URL

--- Casks/xscope3.rb
+++ Casks/xscope3.rb
@@ -2,10 +2,10 @@ cask :v1 => 'xscope3' do
   version '3.6.3'
   sha256 '1e5e46f50ecd81e35cf819fbd04b2c9726beddc24e86cdd2f6ae5dc60aeb4e4b'
 
-  url 'https://iconfactory.com/assets/software/xscope/xScope-3.6.3.zip'
+  url 'https://iconfactory.com/assets/software/xscope/xScope-#{version}.zip'
   appcast 'http://iconfactory.com/appcasts/xScope/appcast.xml'
   homepage 'http://iconfactory.com/software/xscope'
-  license :unknown # todo: change license and remove this comment; ':unknown' is a machine-generated placeholder
+  license :commercial
 
   app 'xScope.app'
 end
