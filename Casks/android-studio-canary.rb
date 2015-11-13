63eb4a74e92b965ce69381d88ed775d97d85a9d2
Michael Ford <fanquake@users.noreply.github.com>
2015-11-13 23:05:54 +0800

    Android Studio Canary 1.5.0.3

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,8 +1,8 @@
 cask :v1 => 'android-studio-canary' do
-  version '1.5.0.1'
-  sha256 'f20846d312b56c7abfc80ed79a1425a9b045bf2c8de9df9a5fccb4e28a1dcdb3'
+  version '1.5.0.3'
+  sha256 'cfc666dcf7c7df67e0a0f5e1a6179e12e28c9763a126ae0a7416911dbe47ee4f'
 
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.2343905-mac.zip"
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-ide-141.2415497-mac.zip"
   name 'Android Studio Canary'
   homepage 'https://sites.google.com/a/android.com/tools/download/studio/canary'
   license :apache
