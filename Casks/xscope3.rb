ad7727dd2f86e489c0694bb1f7200047ac168fc3
Michael Ford <fanquake@users.noreply.github.com>
2015-11-13 23:22:08 +0800

    Xscope3: Correct URL stanza

--- Casks/xscope3.rb
+++ Casks/xscope3.rb
@@ -2,7 +2,7 @@ cask :v1 => 'xscope3' do
   version '3.6.3'
   sha256 '1e5e46f50ecd81e35cf819fbd04b2c9726beddc24e86cdd2f6ae5dc60aeb4e4b'
 
-  url 'https://iconfactory.com/assets/software/xscope/xScope-#{version}.zip'
+  url "https://iconfactory.com/assets/software/xscope/xScope-#{version}.zip"
   appcast 'http://iconfactory.com/appcasts/xScope/appcast.xml'
   homepage 'http://iconfactory.com/software/xscope'
   license :commercial
