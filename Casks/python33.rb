bdf1e43d44b51d2953bed29d617e422059f75637
Michael Ford <fanquake@users.noreply.github.com>
2015-11-13 23:26:04 +0800

    Python33: Update uninstall, add name stanza

--- Casks/python33.rb
+++ Casks/python33.rb
@@ -3,19 +3,23 @@ cask :v1 => 'python33' do
   sha256 '7e59f823f82da5ec7e2af4449a5e33c09f5b755a8acd9cec98371da8c2b2b52b'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.6.dmg"
+  name 'Python'
   homepage 'https://www.python.org/'
   license :oss
 
   pkg 'Python.mpkg'
 
-  # todo there seems to be a nonstandard installation receipt at
-  # /Library/Receipts/PythonApplications-3.3.pkg/Contents/Archive.bom
-  # which :pkgutil does not know how to find
-  uninstall :delete => [
-                        "/Library/Receipts/Python*-#{version.slice(/\d+\.\d+/)}.pkg",
-                        "/Applications/Python #{version.slice(/\d+\.\d+/)}",
-                        "/Library/Frameworks/Python.Framework/Versions/#{version.slice(/\d+\.\d+/)}",
-                        "/Library/Python/#{version.slice(/\d+\.\d+/)}",
+  uninstall :pkgutil => [
+                        "org.python.Python.PythonApplications-#{version.slice(/\d+\.\d+/)}",
+                        "org.python.Python.PythonDocumentation-#{version.slice(/\d+\.\d+/)}",
+                        "org.python.Python.PythonFramework-#{version.slice(/\d+\.\d+/)}",
+                        "org.python.Python.PythonUnixTools-#{version.slice(/\d+\.\d+/)}",
+                        ],
+            :delete => [
+                       "/Library/Receipts/PythonInstallPip-#{version.slice(/\d+\.\d+/)}",
+                       "/Applications/Python #{version.slice(/\d+\.\d+/)}",
+                       "/Library/Frameworks/Python.Framework/Versions/#{version.slice(/\d+\.\d+/)}",
+                       "/Library/Python/#{version.slice(/\d+\.\d+/)}",
                        ]
   zap :delete => "~/Library/Python/#{version.slice(/\d+\.\d+/)}"
 end
