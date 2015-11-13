35ed752e2fabc4d67b70beb8a0531f7e18da17ba
Michael Ford <fanquake@users.noreply.github.com>
2015-11-13 23:27:15 +0800

    Python32: Update uninstall, add name stanza

--- Casks/python32.rb
+++ Casks/python32.rb
@@ -1,21 +1,27 @@
-cask :v1 => 'python32' do
+cask :v1 => 'python32' 
+  # note: "32" is a trailing version number on the Cask token, which
+  # breaks our own token conventions, for consistency with Homebrew
   version '3.2.5'
   sha256 '581abb199d40271dbb68a8de4f1e4d5a1a7ab45cae8e817a2d9532be7a26a164'
 
   url "https://www.python.org/ftp/python/#{version}/python-#{version}-macosx10.6.dmg"
+  name 'Python'
   homepage 'http://www.python.org/'
   license :oss
 
   pkg 'Python.mpkg'
 
-  # todo there seems to be a nonstandard installation receipt at
-  # /Library/Receipts/PythonApplications-3.2.pkg/Contents/Archive.bom
-  # which :pkgutil does not know how to find
-  uninstall :delete => [
-                        "/Library/Receipts/Python*-#{version.slice(/\d+\.\d+/)}.pkg",
-                        "/Applications/Python #{version.slice(/\d+\.\d+/)}",
-                        "/Library/Frameworks/Python.Framework/Versions/#{version.slice(/\d+\.\d+/)}",
-                        "/Library/Python/#{version.slice(/\d+\.\d+/)}",
+  uninstall :pkgutil => [
+                        "org.python.Python.PythonApplications-#{version.slice(/\d+\.\d+/)}",
+                        "org.python.Python.PythonDocumentation-#{version.slice(/\d+\.\d+/)}",
+                        "org.python.Python.PythonFramework-#{version.slice(/\d+\.\d+/)}",
+                        "org.python.Python.PythonUnixTools-#{version.slice(/\d+\.\d+/)}",
+                        ],
+            :delete => [
+                       "/Library/Receipts/PythonInstallPip-#{version.slice(/\d+\.\d+/)}",
+                       "/Applications/Python #{version.slice(/\d+\.\d+/)}",
+                       "/Library/Frameworks/Python.Framework/Versions/#{version.slice(/\d+\.\d+/)}",
+                       "/Library/Python/#{version.slice(/\d+\.\d+/)}",
                        ]
   zap :delete => "~/Library/Python/#{version.slice(/\d+\.\d+/)}"
 end
