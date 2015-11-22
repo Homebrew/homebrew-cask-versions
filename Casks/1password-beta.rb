dbea9813c79c372eb8b4a0e52d473ab4b1871e48
Michael Ford <fanquake@users.noreply.github.com>
2015-11-22 16:49:25 +0800

    1Password Beta 5.5.BETA-24

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => '1password-beta' do
-  version '5.5.BETA-22'
-  sha256 'f022e62a3c7c54e2179e2c6b1f69a5a659c0cb3668b0e87928cd474cd15f4c3d'
+  version '5.5.BETA-24'
+  sha256 'd833700071d54cfb5452ac22accc6bda2057dff4bb282839d20b4b53bcf508a2'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
