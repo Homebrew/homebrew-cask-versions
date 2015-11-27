f1b692dd4b3bab42c3425cf9f88c22f8d3050b92
Michael Ford <fanquake@users.noreply.github.com>
2015-11-27 16:44:07 +0800

    Telegram Dev 0.9.12

--- Casks/telegram-dev.rb
+++ Casks/telegram-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'telegram-dev' do
-  version '0.9.7.dev'
-  sha256 'd4705e32075ece04e468be84a59ebb49f626be87acb697737f1eaaa77da7ba0a'
+  version '0.9.12.dev'
+  sha256 '4dc0d1cb9454231daedfe01043a2b27acbeed5acddfa8aa5c93d97797a14feeb'
 
   # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
