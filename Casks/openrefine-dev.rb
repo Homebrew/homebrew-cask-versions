d3078a8f60dbfb1dccf848c99a1000e6624c2986
Michael Ford <fanquake@users.noreply.github.com>
2015-11-27 16:33:19 +0800

    OpenRefine Dev 2.6-rc.2

--- Casks/openrefine-dev.rb
+++ Casks/openrefine-dev.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'openrefine-dev' do
-  version '2.6-rc1'
-  sha256 'e2351adc641b8d828ee9cc4c55f595b2cc65b83b29502bb671f55039c0536f33'
+  version '2.6-rc.2'
+  sha256 '7703016e4935961ce89c26c0c4c780011329321ed32367082f329dae38704037'
 
   url "https://github.com/OpenRefine/OpenRefine/releases/download/v#{version}/openrefine-mac-#{version}.dmg"
   name 'OpenRefine'
