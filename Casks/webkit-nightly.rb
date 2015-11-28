d5f063974aecb2b7dc90af400e73f786f7b3ba2f
Michael Ford <fanquake@users.noreply.github.com>
2015-11-28 19:57:25 +0800

    Webkit Nightly r192783

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'webkit-nightly' do
-  version 'r192774'
-  sha256 '4504e549031be31e20732ff02c1251eb6eee62e717628376930b89b7bb6da779'
+  version 'r192783'
+  sha256 '73e3000d1a22a3e1f2b573be3b8e8090fff81f3605e1a01c14b9480849e8fa12'
 
   url "http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-#{version}.dmg"
   name 'WebKit Nightly'
