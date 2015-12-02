8123869e7ff45610aa0a0f822645d8829a501825
Vítor Galvão <info@vitorgalvao.com>
2015-12-02 12:07:53 +0000

    adobe-photoshop-lightroom600.rb: move uninstall and zap nearer the bottom

--- Casks/adobe-photoshop-lightroom600.rb
+++ Casks/adobe-photoshop-lightroom600.rb
@@ -9,12 +9,6 @@ cask :v1 => 'adobe-photoshop-lightroom600' do
   homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
   license :commercial
 
-  uninstall :delete => "/Applications/Adobe Lightroom/Adobe Lightroom.app"
-  zap       :delete => [
-                        '~/Library/Application Support/Adobe/Lightroom',
-                        "~/Library/Preferences/com.adobe.Lightroom#{version.to_i}.plist",
-                       ]
-
   # staged_path not available in Installer/Uninstall Stanza, workaround by nesting with preflight/postflight
   # see https://github.com/caskroom/homebrew-cask/pull/8887
   # and https://github.com/caskroom/homebrew-versions/pull/296
@@ -28,6 +22,12 @@ cask :v1 => 'adobe-photoshop-lightroom600' do
     system '/usr/bin/killall', '-kill', 'SafariNotificationAgent'
     system '/usr/bin/sudo', '-E', '--', "#{staged_path}/Install.app/Contents/MacOS/Install", "--mode=silent", "--deploymentFile=#{staged_path}/deploy/AdobeLightroom6.remove.xml"
   end
+
+  uninstall :delete => "/Applications/Adobe Lightroom/Adobe Lightroom.app"
+  zap       :delete => [
+                        '~/Library/Application Support/Adobe/Lightroom',
+                        "~/Library/Preferences/com.adobe.Lightroom#{version.to_i}.plist",
+                       ]
 
   caveats 'Installation or Uninstallation may fail with Exit Code 19 (Conflicting Processes running) if Browsers, Safari Notification Service or SIMBL Services are running or Adobe Creative Cloud or any other Adobe Products are already installed. See Logs in /Library/Logs/Adobe/Installers if Installation or Uninstallation fails, to identify the conflicting processes.'
 end
