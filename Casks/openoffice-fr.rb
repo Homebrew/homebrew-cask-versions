739fe97ff84f6e7a7a91969afa83f1aea13ac3b8
Michael Ford <fanquake@users.noreply.github.com>
2015-12-03 14:26:38 +0800

    OpenOffice FR 4.1.2

--- Casks/openoffice-fr.rb
+++ Casks/openoffice-fr.rb
@@ -1,10 +1,15 @@
 cask :v1 => 'openoffice-fr' do
-  version '4.1.1'
-  sha256 '4e1fb74fbb6b714c49b7a0f82646c25a2ee749459f44fc2624b8220e7c256c23'
+  version '4.1.2'
+  sha256 '93486d9104321cbc2e1b8c3797cc6ae7f7507fd9108121f8d4ccc2b688f26d2b'
 
+  # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/openofficeorg.mirror/#{version}/binaries/fr/Apache_OpenOffice_#{version}_MacOS_x86-64_install_fr.dmg"
+  name 'OpenOffice'
   homepage 'https://www.openoffice.org/fr/Telecharger/'
-  license :oss
+  license :apache
+  tags :vendor => 'Apache'
 
   app 'OpenOffice.app'
+
+  zap :delete => '~/Library/Application Support/OpenOffice'
 end
