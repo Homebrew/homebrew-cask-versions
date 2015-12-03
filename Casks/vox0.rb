1b98acf85589c5bf133a56f8959317e771a89307
adityadalal924 <github@adityadalal.com>
2015-12-03 12:22:14 -0500

    update vox cask name

--- Casks/vox0.rb
+++ Casks/vox0.rb
@@ -1,11 +1,11 @@
-cask :v1 => 'vox-legacy' do
+cask :v1 => 'vox0' do
   version '0.3b2'
   sha256 '997b98a0b215ab1469fdfc0ce73764bd722fe944074ca8e61be6069dbecbe711'
 
   url "http://cloud.coppertino.com/vox/Vox_#{version}.dmg"
   name 'VOX'
   homepage 'http://coppertino.com/vox/'
-  license :freemium
+  license :gratis
 
   app 'Vox.app'
 end
