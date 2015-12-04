cba09b3cde1f88829e9db8b4148b435cdee52078
Michael Ford <fanquake@users.noreply.github.com>
2015-12-04 10:36:48 +0800

    PhpStorm EAP 143.1212

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'phpstorm-eap' do
-  version '143.1044'
-  sha256 '3c0f6e2113789f86a24d9548172ab04195f364f6f3ab9e645b7fa0c51538ac9f'
+  version '143.1212'
+  sha256 'e8327809b042ad94c5fccbfa17e78278df7ab3999329398c155750cf68f04510'
 
   url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version}-custom-jdk-bundled.dmg"
   name 'PhpStorm EAP'
