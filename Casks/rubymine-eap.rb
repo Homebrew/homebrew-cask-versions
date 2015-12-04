e2ae3a8bfec490299478fecec49756ffa5f360ee
Michael Ford <fanquake@users.noreply.github.com>
2015-12-04 10:37:32 +0800

    RubyMine 143.1183.12

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'rubymine-eap' do
-  version '143.1015.13'
-  sha256 '3f53bafa8efc619c6e8e2d7a7a7719185f37e73be88365b99493a59d6bb48820'
+  version '143.1183.12'
+  sha256 '586b8327feb053b05d8f50aa9526cc7d10400ed3006c8151ab767fbf20fae419'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}-custom-jdk-bundled.dmg"
   name 'RubyMine EAP'
