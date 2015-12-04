acebed344bb887efac2494b3f411e0712c371748
Michael Ford <fanquake@users.noreply.github.com>
2015-12-04 10:27:32 +0800

    OpenOffice GL 4.1.2

--- Casks/openoffice-gl.rb
+++ Casks/openoffice-gl.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'openoffice-gl' do
-  version '4.1.1'
-  sha256 'a41bcfbd6aeaf86c65ec27db78ad0fa8e93482dbc75336557fed7a42c8234d37'
+  version '4.1.2'
+  sha256 'd92b8edb217e3c87287e67fdce07876e098ae4d75c760e5678d2beca9f6f7c60'
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/openofficeorg.mirror/#{version}/binaries/gl/Apache_OpenOffice_#{version}_MacOS_x86-64_install_gl.dmg"
