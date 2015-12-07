8189d8f51f238c27f0ca8526c5f4546416085a57
Brad Dixon <rbdixon@gmail.com>
2015-12-07 09:27:52 -0500

    Add the latest beta of Saleae Logic Analyzer 1.2.5.
    This version is recommended for certain hardware versions by Saleae.

---
+++ Casks/logic-beta.rb
@@ -0,0 +1,11 @@
+cask :v1 => 'logic-beta' do
+  version '1.2.5'
+  sha256 '6699a0da5b14c85da662d2df3b8c6453459a49c1be7e833854bbb000bfce9288'
+
+  url "http://downloads.saleae.com/betas/#{version}/Logic-#{version}-Darwin.dmg"
+  name 'Logic'
+  homepage 'https://www.saleae.com/'
+  license :commercial
+
+  app 'Logic.app'
+end
