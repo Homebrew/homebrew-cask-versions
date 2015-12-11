1f0322518a3663ab710ae06c4cad371a7fb3571b
Michael Ford <fanquake@users.noreply.github.com>
2015-12-11 22:20:35 +0800

    Dropbox Experimental 3.13.43

--- Casks/dropbox-experimental.rb
+++ Casks/dropbox-experimental.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'dropbox-experimental' do
-  version '3.13.42'
-  sha256 'f9401a603f5d9ba44e18730db7f2a99d807101a34e3fc7966048db155061ccdd'
+  version '3.13.43'
+  sha256 'f50154b7ae4977472776a00a2fb5c2154c210dd048b33a3d68a6b19c09f733c1'
 
   url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
   homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/203866863-Experimental-Build-3-13-42'
