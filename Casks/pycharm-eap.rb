9c5352d31760c1f77a642a79bbd084f5f13d89ce
Michael Ford <fanquake@users.noreply.github.com>
2015-12-11 15:37:00 +0800

    PyCharm EAP 143.1183.2

--- Casks/pycharm-eap.rb
+++ Casks/pycharm-eap.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'pycharm-eap' do
-  version '143.1015.4'
-  sha256 '658b251db011f6c0d9c705ebd48de6ca7d9737571ff0e621e9e0049988d602f4'
+  version '143.1183.2'
+  sha256 '8f17dc6a2580b4b715eb81a998df5918032eaaa6b3dd447628cdfa3400650592'
 
   url "http://download.jetbrains.com/python/pycharm-professional-#{version}-jdk-bundled.dmg"
   name 'PyCharm'
