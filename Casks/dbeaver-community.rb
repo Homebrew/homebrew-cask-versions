feb6696817517be99dbfe69e9df6c90636158e9d
Michael Ford <fanquake@users.noreply.github.com>
2015-12-12 17:01:18 +0800

    DBeaver Community 3.5.6

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'dbeaver-community' do
-  version '3.5.5'
-  sha256 'bd7b43a2e124a376cc7435dac02d1854ba600f1e661de2eab42acf47fed42e3d'
+  version '3.5.6'
+  sha256 '416b55adfa76a8d31a652126ad543107866e1e233d1e162fd437591ceaf0e9df'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
