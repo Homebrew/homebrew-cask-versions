90cd88362c09af6a55a1f5370b26d01fa10e31c4
tmn <mail@trimn.net>
2015-12-12 14:58:24 +0100

    Update Vivaldi-snapshot to 1.0.344.24

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,10 +1,10 @@
 cask :v1 => 'vivaldi-snapshot' do
-  version '1.0.340.7'
-  sha256 'ee72cb54031e264b3ae095b2802d282d5877af83af2d0d0561956debeac6e30a'
+  version '1.0.344.24'
+  sha256 '772c7d5c043e24d9f84c0178b35e8b920cf03a1d1b1307ab1334fca789d6dab2'
 
   url "https://vivaldi.com/download/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml',
-          :sha256 => '69a2cea3880e51491355144f74ceb69946161289a7a96ffb024c9f55d6607e91'
+          :sha256 => '19c7540da92d6eb6a7f7d006772a755d706ede5bfa98dffd3d25085d59a0d40f'
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
