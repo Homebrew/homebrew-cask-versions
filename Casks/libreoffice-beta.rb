3978aa3cbcab0bd16ba26a16dc9dc6e6271a3f40
adityadalal924 <github@adityadalal.com>
2015-12-13 11:12:45 -0500

    updated libreoffice-beta (5.1.0.0.beta2)

--- Casks/libreoffice-beta.rb
+++ Casks/libreoffice-beta.rb
@@ -1,13 +1,14 @@
 cask :v1 => 'libreoffice-beta' do
-  version '5.0.3.1'
-  sha256 '6abfb3a480ec03dff85872da4be7ca4a9450506ca9320b787c2fcce6e88b8f00'
+  version '5.1.0.0.beta2'
+  sha256 '4ff5f22273b7e9e8abaabbbcd62bbbb06c2c90c9c45030f00a324f0cca26f73b'
 
-  url "https://download.documentfoundation.org/libreoffice/testing/#{version.split('.')[0..-2].join('.')}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
+  url "https://download.documentfoundation.org/libreoffice/testing/#{version.split('.')[0..2].join('.')}/mac/x86_64/LibreOfficeDev_#{version}_MacOS_x86-64.dmg"
   gpg "#{url}.asc",
       :key_id => 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
   name 'LibreOffice'
+  name 'LibreOfficeDev'
   homepage 'https://www.libreoffice.org/'
   license :mpl
 
-  app 'LibreOffice.app'
+  app 'LibreOfficeDev.app'
 end
