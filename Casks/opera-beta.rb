d5f9293c1cc76a536e44eccd0c448322accb2b2c
tmn <mail@trimn.net>
2015-12-15 23:48:27 +0100

    Update Opera-beta to 35.0.2066.10

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'opera-beta' do
-  version '34.0.2036.24'
-  sha256 '0c4753e027050cf1f94417f9512fe26ec691f3461d7c228927d0c391f5dc725e'
+  version '35.0.2066.10'
+  sha256 'dd83f85e764da6e64997ead057b7e3af1e848bf6b2bfc2b188a49c9a04d2c081'
 
   url "http://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   homepage 'http://www.opera.com/computer/beta'
