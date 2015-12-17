8ff61f615c51aacd7a3f27834620e0c1df4a1322
Michael Ford <fanquake@users.noreply.github.com>
2015-12-17 12:47:33 +0800

    Firefox Nightly JA 46.0a1

--- Casks/firefox-nightly-ja.rb
+++ Casks/firefox-nightly-ja.rb
@@ -1,5 +1,5 @@
 cask :v1 => 'firefox-nightly-ja' do
-  version '45.0a1'
+  version '46.0a1'
   sha256 :no_check # required as upstream package is updated in-place
 
   url "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central-l10n/firefox-#{version}.ja.mac.dmg"
