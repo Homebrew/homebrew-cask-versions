dff400bb33b50b09187974c1abcac8692d4a17cd
Michael Ford <fanquake@users.noreply.github.com>
2015-12-17 13:24:20 +0800

    Netbeans CPP Nightly 201512160002

--- Casks/netbeans-cpp-nightly.rb
+++ Casks/netbeans-cpp-nightly.rb
@@ -1,6 +1,6 @@
 cask :v1 => 'netbeans-cpp-nightly' do
-  version '201512011444'
-  sha256 '88bd89d3d7038f4919c666098c1f6cfaa6fdf3b12a0307d34c2c34086d815c09'
+  version '201512160002'
+  sha256 '59d1dd923f5ccc29e411fe6053a8d29c97b42bd57ccd2497cfc21437d98eef1b'
 
   url "http://bits.netbeans.org/download/trunk/nightly/latest/bundles/netbeans-trunk-nightly-#{version}-cpp-macosx.dmg"
   name 'NetBeans IDE for C/C++'
