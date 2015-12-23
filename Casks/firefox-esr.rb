3bf2fec2ecdd673eccadfdfd804fa9ca57451440
Michael Ford <fanquake@users.noreply.github.com>
2015-12-23 22:52:05 +0800

    Firefox ESR 38.5.2

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr' do
-  version '38.5.1'
-  sha256 'f1f3051548ea0041416be3a26be73ca6c57ec2ef89305a87f5237dc2fb3f295c'
+  version '38.5.2'
+  sha256 '10d76f41fcec23f5b8af4ed1b271c78950c980b66c5e9021e9cf93daaf8a43da'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
   name 'Firefox'
