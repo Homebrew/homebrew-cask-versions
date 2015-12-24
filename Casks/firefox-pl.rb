58a9e6061175d53fdbd9f11f07bfce00a6ad4686
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:26:23 +0000

    firefox-pl.rb: name changed to conform to new rules

--- Casks/firefox-pl.rb
+++ Casks/firefox-pl.rb
@@ -3,7 +3,7 @@ cask 'firefox-pl' do
   sha256 'afce57831a222f9ee722b6cc7a825461ad4ec7170588e755fd8a021efce99bde'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=pl"
-  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/pl/firefox/desktop'
   license :mpl
 
