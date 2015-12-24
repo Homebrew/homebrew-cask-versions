fcb811281e2738161b564618cef6a3c9191fa2e0
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:26:23 +0000

    firefox-beta-de.rb: name changed to conform to new rules

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -3,7 +3,7 @@ cask 'firefox-beta-de' do
   sha256 '2465a89e18fedb31c0678caa3e5111989e75bd45754d95c8192bdc9d6e19ef19'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
-  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/de/firefox/beta/'
   license :mpl
 
