fd0ed2985bc89cb3701d78c21d0bdb16a8072cee
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:26:23 +0000

    firefox-uk.rb: name changed to conform to new rules

--- Casks/firefox-uk.rb
+++ Casks/firefox-uk.rb
@@ -3,7 +3,7 @@ cask 'firefox-uk' do
   sha256 '4e3d06df05c14499f344faa58130f62117b303a27d84764d50e6e697068fb597'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=uk"
-  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/uk/firefox/new'
   license :mpl
 
