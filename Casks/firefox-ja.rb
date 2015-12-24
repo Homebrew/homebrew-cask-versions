64dd70e858fa81b92992422b8bbc0982233cd66e
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:26:23 +0000

    firefox-ja.rb: name changed to conform to new rules

--- Casks/firefox-ja.rb
+++ Casks/firefox-ja.rb
@@ -3,7 +3,7 @@ cask 'firefox-ja' do
   sha256 '24b96b9f77f6c8dd6ad142f3beec5bc4014125d57d2302766329126cfa9f246e'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
-  name 'Firefox'
+  name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/ja/firefox/desktop/'
   license :mpl
 
