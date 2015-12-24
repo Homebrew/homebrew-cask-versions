ff3579b1969e904a0bee632dec51ed125b2e4b0b
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:26:23 +0000

    openoffice-gl.rb: name changed to conform to new rules

--- Casks/openoffice-gl.rb
+++ Casks/openoffice-gl.rb
@@ -4,7 +4,7 @@ cask 'openoffice-gl' do
 
   # sourceforge.net is the official download host per the vendor homepage
   url "http://downloads.sourceforge.net/project/openofficeorg.mirror/#{version}/binaries/gl/Apache_OpenOffice_#{version}_MacOS_x86-64_install_gl.dmg"
-  name 'OpenOffice'
+  name 'Apache OpenOffice'
   homepage 'http://www.openoffice.org/gl/Descargar/'
   license :apache
 
