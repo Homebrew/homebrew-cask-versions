1e89b68cc223717d9a705d687c3409f37ce0419d
Vítor Galvão <info@vitorgalvao.com>
2015-12-24 19:26:23 +0000

    virtualbox-beta.rb: name changed to conform to new rules

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -3,7 +3,7 @@ cask 'virtualbox-beta' do
   sha256 'c190f66421211b6f0ccbfc67740bf442f5e9ed2c95202846a8873a8f6f8e7af5'
 
   url "http://download.virtualbox.org/virtualbox/#{version.split('-')[0]}/VirtualBox-#{version}-OSX.dmg"
-  name 'VirtualBox'
+  name 'Oracle VirtualBox'
   homepage 'https://www.virtualbox.org/wiki/Testbuilds'
   license :gpl
 
