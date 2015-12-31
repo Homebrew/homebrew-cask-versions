9a79c24b0539172d4f1c7b90bc8c13e346cf49a0
Satoshi Matsumoto <kaorimatz@gmail.com>
2015-12-31 14:33:53 +0900

    Upgrade IntelliJ IDEA CE EAP to 144.2608.2

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,19 +1,20 @@
 cask 'intellij-idea-ce-eap' do
-  version '143.1183.10'
-  sha256 '8f9597e732ed7e169d21026c73e5da4ac841fcf44f1c98250bb44fd317169753'
+  version '144.2608.2'
+  sha256 '21d2fd409de9924d04a235bf5343033a52d84658fdf96e16d6aab377beab0fea'
 
-  url "https://download.jetbrains.com/idea/ideaIC-#{version}-custom-jdk-bundled.dmg"
+  url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   name 'IntelliJ IDEA EAP :: CE'
-  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+15+EAP'
+  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+16+EAP'
   license :apache
 
-  app 'IntelliJ IDEA 15 CE EAP.app'
+  app 'IntelliJ IDEA 16 CE EAP.app'
 
   zap :delete => [
-		  '~/Library/Preferences/com.jetbrains.intellij.plist',
-                  '~/Library/Application Support/IdeaIC15',
-                  '~/Library/Preferences/IdeaIC15',
-                  '~/Library/Caches/IdeaIC15',
-                  '~/Library/Logs/IdeaIC15',
+                  '~/Library/Application Support/IdeaIC16',
+                  '~/Library/Caches/IdeaIC16',
+                  '~/Library/Logs/IdeaIC16',
+                  '~/Library/Preferences/com.jetbrains.intellij.ce.plist',
+                  '~/Library/Preferences/IdeaIC16',
+                  '~/Library/Saved Application State/com.jetbrains.intellij.ce.savedState',
                  ]
 end
