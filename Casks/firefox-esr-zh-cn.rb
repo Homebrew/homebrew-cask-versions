f6dfc3b4938f131f5680b8258a71f8393964b246
Zirro <code@zirro.se>
2016-01-05 08:49:32 +0100

    #1529 firefox-esr-cn -> firefox-esr-zh-cn (including whitespace removal)

--- Casks/firefox-esr-zh-cn.rb
+++ Casks/firefox-esr-zh-cn.rb
@@ -1,4 +1,4 @@
-cask 'firefox-esr-cn' do
+cask 'firefox-esr-zh-cn' do
   version '38.4.0'
   sha256 '7a2094e6483990906cd316309e03ec404f50de1077ea0f4332ecf649bad7e3ac'
 
@@ -7,9 +7,9 @@ cask 'firefox-esr-cn' do
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/zh-CN/firefox/organizations/'
   license :mpl
-  
+
   app 'Firefox.app'
-  
+
   zap :delete => [
                   '~/Library/Application Support/Firefox',
                   '~/Library/Caches/Firefox',
