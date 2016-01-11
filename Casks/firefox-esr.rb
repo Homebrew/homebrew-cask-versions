fde3ba9c81156902512cf6de3532f3406d7e614b
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:43:55 +0000

    firefox-esr.rb: fixed name stanzas

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -3,7 +3,6 @@ cask 'firefox-esr' do
   sha256 '10d76f41fcec23f5b8af4ed1b271c78950c980b66c5e9021e9cf93daaf8a43da'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
-  name 'Firefox'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
   license :mpl
