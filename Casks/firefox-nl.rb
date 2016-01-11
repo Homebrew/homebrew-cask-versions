3d1f0c82065c70b48afd27e7a18cdf62b4cb1e7c
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:44:00 +0000

    firefox-nl.rb: fixed name stanzas

--- Casks/firefox-nl.rb
+++ Casks/firefox-nl.rb
@@ -3,7 +3,6 @@ cask 'firefox-nl' do
   sha256 'a62b187e171f43d907732d65e748638775af5bca25dc0e0cac5bbfdd579dd36a'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=nl"
-  name 'Firefox'
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/nl/firefox/desktop'
   license :mpl
