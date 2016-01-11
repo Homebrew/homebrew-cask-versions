ea680035cdcd81d393488cd453bb7283cf52e6ce
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:45:26 +0000

    pycharm-edu.rb: fixed name stanzas

--- Casks/pycharm-edu.rb
+++ Casks/pycharm-edu.rb
@@ -3,9 +3,8 @@ cask 'pycharm-edu' do
   sha256 '2c3b98051c90e5647ada4e4f3b33ad1c974b88ee1f30d1631e7a874e8dcafe3d'
 
   url "https://download.jetbrains.com/python/pycharm-edu-#{version}.dmg"
-  name 'PyCharm'
+  name 'Jetbrains PyCharm Educational Edition'
   name 'PyCharm Edu'
-  name 'PyCharm Educational Edition'
   homepage 'https://www.jetbrains.com/pycharm-edu/'
   license :apache
 
