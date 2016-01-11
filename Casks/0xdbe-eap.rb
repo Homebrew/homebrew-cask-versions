30ad01931ce1a28f0a76b4ead00302e558c88500
Vítor Galvão <info@vitorgalvao.com>
2016-01-11 18:43:33 +0000

    0xdbe-eap.rb: fixed name stanzas

--- Casks/0xdbe-eap.rb
+++ Casks/0xdbe-eap.rb
@@ -4,10 +4,8 @@ cask '0xdbe-eap' do
 
   url "https://download.jetbrains.com/dbe/0xdbe-#{version}-custom-jdk-bundled.dmg"
   name '0xDBE EAP'
-  name '0xDBE'
   homepage 'https://confluence.jetbrains.com/display/DBE/0xDBE+1.0+EAP'
   license :commercial
 
   app '0xDBE EAP.app'
-
 end
