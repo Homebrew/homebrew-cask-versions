2f31e15fa79cc2c9ca0295e000bd220b3cc9a2fc
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:10:04 +0000

    updated sublime-text3 (3083)

--- Casks/sublime-text3.rb
+++ Casks/sublime-text3.rb
@@ -1,11 +1,11 @@
 cask 'sublime-text3' do
-  version 'Build 3083'
+  version '3083'
   sha256 'fe6dd8d8192fdb01988f99289e5bc1d9a4e66cf67548e144002051c23369a5ff'
 
   # rackcdn.com is the official download host per the vendor homepage
-  url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20#{version.sub(' ', '%20')}.dmg"
+  url "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/3/stable/appcast_osx.xml',
-          :sha256 => 'bb8df68fe6a87966c4662948f4f147ac5dd0d1cd3a8d5342c56a11a018ff81e9'
+          :sha256 => '3e72a8db0cbef96c6d9884f145787f0e1ef07a89846889d61a6a3257464a29fa'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/3'
   license :closed
