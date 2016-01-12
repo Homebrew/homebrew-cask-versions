4182ce13023d549f1c6b2b2bf984cb7048146674
Vítor Galvão <info@vitorgalvao.com>
2016-01-12 22:14:41 +0000

    dupeguru-me.rb: appcast sha256 without pubDate

--- Casks/dupeguru-me.rb
+++ Casks/dupeguru-me.rb
@@ -4,7 +4,7 @@ cask 'dupeguru-me' do
 
   url "https://download.hardcoded.net/dupeguru_me_osx_#{version.dots_to_underscores}.dmg"
   appcast 'https://www.hardcoded.net/updates/dupeguru_me.appcast',
-          :sha256 => '63c7ff19e3f4f83bd4bdf75c42e9ece7b3a5a0552b7b13160f62712a67551309'
+          :sha256 => 'f723dcab61cd784e37771b074817f6758463271c4179ce372609405996febc5e'
   name 'dupeGuru Music Edition'
   homepage 'https://www.hardcoded.net/dupeguru_me/'
   license :bsd
