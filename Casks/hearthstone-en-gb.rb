2404f6ce7f7065f0f2c919d399992e529287fca2
Tommy Sparber <tommy@sparber.net>
2016-01-12 18:19:10 +1100

    Update hearthstone to use HTTPS

--- Casks/hearthstone-en-gb.rb
+++ Casks/hearthstone-en-gb.rb
@@ -2,7 +2,7 @@ cask 'hearthstone-en-gb' do
   version :latest
   sha256 :no_check
 
-  url 'http://eu.battle.net/download/getInstaller?os=mac&installer=Hearthstone-Setup-enGB.zip'
+  url 'https://eu.battle.net/download/getInstaller?os=mac&installer=Hearthstone-Setup-enGB.zip'
   homepage 'http://eu.battle.net/hearthstone/en/'
   license :unknown
 
