73eba4e76504b4263ac63bd27e811a3f2c23d11a
adityadalal924 <github@adityadalal.com>
2016-01-13 01:29:45 -0500

    updated beaker-electron (1.5b1-0-g0d1b2b0)

--- Casks/beaker-electron.rb
+++ Casks/beaker-electron.rb
@@ -1,6 +1,6 @@
 cask 'beaker-electron' do
-  version '1.4.2-0-ge55c059'
-  sha256 '32d6ecf6a1ebfc32c3e1b2dfc1edb33eaeab6f83d3f9d74dd9b64e08bd24e3e6'
+  version '1.5b1-0-g0d1b2b0'
+  sha256 '29a2cf921f897c459183a58de436ba68e9b9771ed690a2fe440f9ae0dff5247a'
 
   # cloudfront.net is the official download host per the vendor homepage
   url "https://d299yghl10frh5.cloudfront.net/beaker-notebook-#{version}-electron-mac.dmg"
