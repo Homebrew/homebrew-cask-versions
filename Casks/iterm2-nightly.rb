f1f482d2e27a00136d2811bfef752d03d2ef0f7a
Michael Ford <fanquake@users.noreply.github.com>
2016-01-15 11:50:43 +0800

    Iterm2 Nightly 2.9.20160114

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-nightly' do
-  version '2.9.20160112'
-  sha256 'd547c8b93d53700cfa54152fe528bc929d1429002b6662cf7bd7a1536dd45094'
+  version '2.9.20160114'
+  sha256 '6cd3beeaccba47b6559511b12a270ee2bd70443430b5eccc19dfab6ab89df811'
 
   url "https://www.iterm2.com/downloads/nightly/iTerm2-#{version.dots_to_underscores}-nightly.zip"
   appcast 'https://iterm2.com/appcasts/nightly.xml',
