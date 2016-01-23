137cf04d5cc755d1521c2765b72be92cf1e3f406
Victor Popkov <victor@popkov.me>
2016-01-23 21:28:55 +0200

    vivaldi-snapshot.rb: upgrade to v1.0.377.10

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,10 +1,10 @@
 cask 'vivaldi-snapshot' do
-  version '1.0.365.3'
-  sha256 'c734929c0238eb1901ee9bc210bb73a5c960871c078cfa8228bba9cce023ceda'
+  version '1.0.377.10'
+  sha256 '466201ddcea244a9fcb0cf8d45e297d3214fb33f8d5032923432b72653116fb5'
 
   url "https://vivaldi.com/download/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml',
-          checkpoint: 'e88253ae777589ebf57b6e5921d531c91c7789d6bdab7fcd9156d45635723b41'
+          checkpoint: '1d350f1f6fd8ff4e9796e47716a2b193bef4fdbea209a37fa0bbe135067e634b'
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
