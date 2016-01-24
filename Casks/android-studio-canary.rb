51c216a9283401583a475d4c3f4ae1e261311378
Lionel Schinckus <valcapri@me.com>
2016-01-24 23:11:29 +0100

    Update Android Studio Canary to 2.0 Preview 7 and prevent conflict with Android Studio 1.5

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,13 +1,16 @@
 cask 'android-studio-canary' do
-  version '2.0.0.5-143.2532994'
-  sha256 '9d86b86a902fc756ecdc600f39a5b847fc119d7af80303f6f30d65974ae6bd07'
+  version '2.0.0.7-143.2554821'
+  sha256 'aec07fb4542d29b84dd36327ccb478e5897e347fbf26449b4573ac450a0790fe'
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.sub(%r{-.*}, '')}/android-studio-ide-#{version.sub(%r{.*?-}, '')}-mac.zip"
   name 'Android Studio Canary'
   homepage 'https://sites.google.com/a/android.com/tools/download/studio/canary'
   license :apache
 
-  app 'Android Studio.app'
+  # To prevent a conflict with Android Studio 1.5, we should rename Android Studio to Android Studio 2
+  # For more information, please visit : http://tools.android.com/tips/using-multiple-android-studio-versions
+  # Please note that Android Studio 2.0 Preview, use a different folder for user data and settings than Android Studio 1.5
+  app 'Android Studio.app', :target => 'Android Studio 2.app'
 
   zap delete: [
                 '~/Library/Preferences/AndroidStudio*',
