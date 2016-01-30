37df1c7d095e2bd53d9cc1cb6221573f1ce5ea95
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 00:55:00 +0000

    updated royal-tsx-beta (2.2.1.1000)

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,10 +1,10 @@
 cask 'royal-tsx-beta' do
-  version '2.2.1.5'
-  sha256 'abcf55a65d4ffcee7fe90d12ffc26c6f186b1a257b47aa7df6a7290d18c775c3'
+  version '2.2.1.1000'
+  sha256 '5622c6d7f3593c5b0f6056614f1afe45bceb9143ca05cb14867f8581de017a47'
 
   url "http://v2.royaltsx.com/updates/royaltsx_#{version}.dmg"
   appcast 'http://v2.royaltsx.com/updates_beta.php',
-          checkpoint: 'f5a30300841e66f37005c35d327498682477c3cf3024bd850af9dd2af7fcd8ec'
+          checkpoint: '0241765a554d894e57fab64b53395091bcb575003ebaed65da1f119ad4fbee40'
   name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
