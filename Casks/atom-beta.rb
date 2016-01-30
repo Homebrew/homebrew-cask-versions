5363566907504cf336949708f5027e748591ac23
Vítor Galvão <info@vitorgalvao.com>
2016-01-30 00:46:49 +0000

    updated atom-beta (1.5.0-beta1)

--- Casks/atom-beta.rb
+++ Casks/atom-beta.rb
@@ -1,11 +1,11 @@
 cask 'atom-beta' do
-  version '1.5.0-beta0'
-  sha256 'ec66c77ed426899621ddb223842addf7bafedb8fd5eaf4ee06790b8ca068256c'
+  version '1.5.0-beta1'
+  sha256 'd92285bdbcf66cb3c9c19f3d23b262227f5d78e1007704fe16ee6b622a93341f'
 
   # github.com is the official download host per the vendor homepage
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: 'f6eb5ed94e0d68fd64ad7be63ddacfaea7b3e29e237648c44c3b22e87c8b5ef8'
+          checkpoint: 'ce646677e7049b0adc3aacf827d010649c52948e32a4d00df973db946f79add4'
   name 'Github Atom Beta'
   homepage 'https://atom.io/beta'
   license :mit
