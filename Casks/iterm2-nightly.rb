186a7ab30cfe0aa9fbeed2e4335e7dbd4e74181e
Vítor Galvão <info@vitorgalvao.com>
2016-01-31 19:35:44 +0000

    updated iterm2-nightly (2.9.20160131)

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,10 +1,10 @@
 cask 'iterm2-nightly' do
-  version '2.9.20160129'
-  sha256 '28001b8048dcb80ab575c573b0f46acab7e9cb2aaa0bf6f32ad0af8defcb1424'
+  version '2.9.20160131'
+  sha256 'dd6692926e252edcccf00a7b96eb0fd3a56727e6a92207f472ad5d6903fe6c5d'
 
   url "https://iterm2.com/downloads/nightly/iTerm2-#{version.dots_to_underscores}-nightly.zip"
   appcast 'https://iterm2.com/appcasts/nightly.xml',
-          checkpoint: '96d8f3953488b032de3a566e39555c48c5debfdffd747c78121ca99274164104'
+          checkpoint: '8cf2341d40fcdbbd5ab46f351649a7f5a79d2eb287f06c19a78857ac9ef10050'
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
   license :gpl
