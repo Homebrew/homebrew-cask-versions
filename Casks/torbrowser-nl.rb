a85466bff6abfd343efc0a6aefdea1886257b14f
Victor Popkov <victor@popkov.me>
2016-02-08 22:25:45 +0200

    torbrowser-nl.rb: upgrade to v5.5.1

--- Casks/torbrowser-nl.rb
+++ Casks/torbrowser-nl.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-nl' do
-  version '5.5'
-  sha256 'd6cc1c2777b39228ff3941eb3458d0c66bb8709db1505b2042f752bd57a0f5af'
+  version '5.5.1'
+  sha256 'f9766bb6b36f4e5af4afeafa2c8bcc176f46140857f91dcb7d2e8009ffd7109f'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_nl.dmg"
   name 'Tor Browser'
