adc1288255ea0310c60b34bb43c54bdba09643a5
Halil Özgür <halil.ozgur@gmail.com>
2016-02-08 12:32:48 +0200

    Upgrade iTerm2 beta to 2.9.20160206

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-beta' do
-  version '2.9.20160113'
-  sha256 '608a67aedf53026236cdcbe41ef71e97d2ed9bee05a5769f02e0e1b7d5bed4e9'
+  version '2.9.20160206'
+  sha256 'b9c680b089bad6829246019e1ebb24017992fbbbd89d58b13f0ac6d9816ff5a5'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
