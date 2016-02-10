1b73439a173a7c93c9772a5463c6968c8bc82b5b
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 20:08:59 +0000

    updated rstudio-preview (0.99.878)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,9 +1,9 @@
 cask 'rstudio-preview' do
-  version :latest
-  sha256 :no_check
+  version '0.99.878'
+  sha256 'fc8019678ae647c3f5d8d7a583fd6d40cf599cc5c056c51d68f2243c5472031b'
 
-  # rstudio.org is the official download host per the vendor homepage
-  url 'https://www.rstudio.org/download/latest/preview/desktop/mac/RStudio-latest.dmg'
+  # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
+  url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'http://www.rstudio.com/ide/download/preview'
   license :affero
