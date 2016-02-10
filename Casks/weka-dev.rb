32e376577b61c54c31f2a002898cde7bacbb3e6b
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 20:23:55 +0000

    updated weka-dev (3.7.13)

--- Casks/weka-dev.rb
+++ Casks/weka-dev.rb
@@ -2,7 +2,7 @@ cask 'weka-dev' do
   version '3.7.13'
   sha256 'ca8b73d9978b9f9e8a280fef3fe9f1edfd16e2f0aff3b26ad4ace367310f79c9'
 
-  # sourceforge.net is the official download host per the vendor homepage
+  # sourceforge.net/sourceforge/weka was verified as official when first introduced to the cask
   url "http://downloads.sourceforge.net/sourceforge/weka/weka-#{version.gsub('.', '-')}-oracle-jvm.dmg"
   name 'Weka'
   homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
