a40fbddc7e1c848005ccfa1e80d27b1fbac86420
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 20:06:23 +0000

    updated scansnap-manager-s1500 (latest)

--- Casks/scansnap-manager-s1500.rb
+++ Casks/scansnap-manager-s1500.rb
@@ -2,7 +2,7 @@ cask 'scansnap-manager-s1500' do
   version :latest
   sha256 :no_check
 
-  # pfultd.com is the official download host per the vendor homepage
+  # pfultd.com was verified as official when first introduced to the cask
   url 'http://origin.pfultd.com/downloads/IMAGE/driver/ss/mgr/m-s1500/MacS1500ManagerV32L80WW.dmg'
   name 'ScanSnap Manager for Fujitsu ScanSnap s1500'
   homepage 'https://www.fujitsu.com/global/support/products/computing/peripheral/scanners/scansnap/software/s1500.html'
