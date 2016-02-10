24ea63cab81fefebf7bfaacf60ffa88667f02d79
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:34:29 +0000

    updated affinity-designer-beta (latest)

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -2,7 +2,7 @@ cask 'affinity-designer-beta' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/affinity-beta was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/affinity-beta/download/Affinity+Designer+Customer+Beta.dmg'
   name 'Serif Affinity Designer'
   homepage 'https://affinity.serif.com/en-us/'
