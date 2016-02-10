8c1f34bac2a5a2c68e7474593fdb997bd9c1d120
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:50:33 +0000

    updated clipmenu-alpha (1.0.0a1)

--- Casks/clipmenu-alpha.rb
+++ Casks/clipmenu-alpha.rb
@@ -2,7 +2,7 @@ cask 'clipmenu-alpha' do
   version '1.0.0a1'
   sha256 'ce92a6efa7397d39e0d73bdd5394438ceb1f66543c2e4695969d7acfaad1434e'
 
-  # dropbox.com is the official download host per the vendor homepage
+  # dropbox.com/u/1140644 was verified as official when first introduced to the cask
   url "https://dl.dropbox.com/u/1140644/clipmenu/ClipMenu-#{version}.dmg"
   name 'ClipMenu'
   homepage 'http://www.clipmenu.com/'
