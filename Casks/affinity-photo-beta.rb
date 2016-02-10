56b4fbdde3e7e90b4c42511eae2af041b37bea39
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:35:08 +0000

    updated affinity-photo-beta (latest)

--- Casks/affinity-photo-beta.rb
+++ Casks/affinity-photo-beta.rb
@@ -2,7 +2,7 @@ cask 'affinity-photo-beta' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/affinity-beta was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/affinity-beta/download/Affinity+Photo+Customer+Beta.dmg'
   name 'Serif Affinity Photo'
   homepage 'https://affinity.serif.com/en-us/photo/'
