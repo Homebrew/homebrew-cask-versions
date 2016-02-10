edcad64000fa9d10debc608e05c665f7f91e3b22
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 20:16:29 +0000

    updated spyder-py2 (2.3.8)

--- Casks/spyder-py2.rb
+++ Casks/spyder-py2.rb
@@ -1,9 +1,11 @@
 cask 'spyder-py2' do
-  version '2.3.7'
-  sha256 'f1bbc488a884decaa695dadded870b2679f32470dcdf687b7fab186328088187'
+  version '2.3.8'
+  sha256 '14e521e742b086a76b17f8cca1c00f68a3e6bdaf483262e814ee6d45cafb742f'
 
-  # bitbucket.org is the official download host per the vendor homepage
+  # bitbucket.org/spyder-ide/ was verified as official when first introduced to the cask
   url "https://bitbucket.org/spyder-ide/spyderlib/downloads/spyder-#{version}-py2.7.dmg"
+  appcast 'https://github.com/spyder-ide/spyder/releases.atom',
+          checkpoint: 'cb54582f0a53c1226fa89099b928c7137f8465a66bac39c750e8b4b7714faca3'
   name 'Spyder-Py2'
   homepage 'https://github.com/spyder-ide/spyder'
   license :mit
