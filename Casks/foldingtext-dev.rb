a7683cd410bac48cc12459884c703d7774cba53e
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 19:52:28 +0000

    updated foldingtext-dev (latest)

--- Casks/foldingtext-dev.rb
+++ Casks/foldingtext-dev.rb
@@ -2,7 +2,7 @@ cask 'foldingtext-dev' do
   version :latest
   sha256 :no_check
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # foldingtext.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://foldingtext.s3.amazonaws.com/FoldingText-Dev.dmg'
   name 'FoldingText'
   homepage 'http://www.foldingtext.com'
