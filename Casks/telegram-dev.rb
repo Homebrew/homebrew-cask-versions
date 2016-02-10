39c07a96c22c4376d76bc1eb82f5c620588169cc
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 20:19:55 +0000

    updated telegram-dev (0.9.14.dev)

--- Casks/telegram-dev.rb
+++ Casks/telegram-dev.rb
@@ -2,10 +2,9 @@ cask 'telegram-dev' do
   version '0.9.14.dev'
   sha256 'db94e511830d2e5103c82894b0af9d5fae5e7240ab2a2a940a32d757aefdd8ae'
 
-  # tdesktop.com is the official download host per the vendor homepage
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   name 'Telegram'
-  homepage 'https://telegram.org/'
+  homepage 'https://tdesktop.com/'
   license :gpl
 
   app 'Telegram.app'
