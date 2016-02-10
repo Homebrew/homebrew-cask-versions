c3666bdc4ed9ad654e3a0504d7fd288067facb91
Vítor Galvão <info@vitorgalvao.com>
2016-02-10 20:21:23 +0000

    updated tower-beta (2.3.4-303,303-efc97691)

--- Casks/tower-beta.rb
+++ Casks/tower-beta.rb
@@ -2,7 +2,7 @@ cask 'tower-beta' do
   version '2.3.4-303,303-efc97691'
   sha256 '500d53c0e18d89b1c91be3515adff581d713043db2774b8f8302464f26553677'
 
-  # amazonaws.com is the official download host per the vendor homepage
+  # amazonaws.com/apps/tower2-mac was verified as official when first introduced to the cask
   url "https://fournova-app-updates.s3.amazonaws.com/apps/tower2-mac/#{version.after_comma}/Tower-2-#{version.before_comma}.zip"
   appcast 'https://updates.fournova.com/updates/tower2-mac/beta',
           checkpoint: 'dabce6c5802426a7c9275daf30fb07bb77f19643e0ed5b5ae53a20a7882f30a4'
