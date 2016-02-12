9751afecb5f2890a73830cc7ca48a50df5ea70e8
Victor Popkov <victor@popkov.me>
2016-02-12 19:19:15 +0200

    torbrowser-cn.rb: upgrade to 5.5.2

--- Casks/torbrowser-cn.rb
+++ Casks/torbrowser-cn.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-cn' do
-  version '5.5.1'
-  sha256 '489afdca410bd40936ac600ae68bf8df4a91d9c4e75c341253b0da2a48616477'
+  version '5.5.2'
+  sha256 'e069e3d298781bccb648851eed947d2c2cf28c82b874365e24df9ffd425567db'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_zh-CN.dmg"
   name 'Tor Browser'
