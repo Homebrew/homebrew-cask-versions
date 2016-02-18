e20172ba3bb93d649a55602bad472436d29fc24e
adidalal <adidalal@users.noreply.github.com>
2016-02-18 01:46:24 -0500

    updated beaker-electron (1.5.1-0-g6e6bdd3)

--- Casks/beaker-electron.rb
+++ Casks/beaker-electron.rb
@@ -1,6 +1,6 @@
 cask 'beaker-electron' do
-  version '1.5b1-0-g0d1b2b0'
-  sha256 '29a2cf921f897c459183a58de436ba68e9b9771ed690a2fe440f9ae0dff5247a'
+  version '1.5.1-0-g6e6bdd3'
+  sha256 '9ddd90bc780c183e263a61bccb2b7dc74b042b1589c800b5f3a0c6922a8ce48a'
 
   # d299yghl10frh5.cloudfront.net was verified as official when first introduced to the cask
   url "https://d299yghl10frh5.cloudfront.net/beaker-notebook-#{version}-electron-mac.dmg"
