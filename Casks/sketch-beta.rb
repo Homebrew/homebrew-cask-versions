02887db5b440361e0abe259a4b683d15f09025ce
Vítor Galvão <info@vitorgalvao.com>
2016-02-27 02:48:58 +0000

    updated sketch-beta (3.6,190)

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,11 +1,11 @@
 cask 'sketch-beta' do
-  version '3.6,187'
-  sha256 'cbde5776c1c20a376d5c8827e87f310d0d4093e34ffacedca0ad5806cfe83c12'
+  version '3.6,190'
+  sha256 'e47d52db396540da53ce946626d714939e58cab161fc9ef76795ee62986327a8'
 
   # hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b',
-          checkpoint: '14cb48636fa4c6d689a884399d20b9c18cd3ec8ee5d62374d3a0eb70e6f31180'
+          checkpoint: 'f103c2b52712176446896c92061ffa4883113aae4480af6756c769b82b31036a'
   name 'Sketch'
   homepage 'http://bohemiancoding.com/sketch/beta/'
   license :commercial
