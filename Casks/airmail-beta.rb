68db1175c1f80f20eb77a04a2c23593be225e177
Ephemera <obliviscence+git@gmail.com>
2016-03-03 11:14:14 +0900

    airmail-beta: update to 2.6.1,352

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 cask 'airmail-beta' do
-  version '2.6.1,351'
-  sha256 'c658b6f8d938f76c3f0090c48b2b49bc4b5eab2eebb0219dcdfb251dc77617cc'
+  version '2.6.1,352'
+  sha256 '8ddc915a1e7e5031ff592925d55d8016992e17f497d90135d4dbafac127ee6f3'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
