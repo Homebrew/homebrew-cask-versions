3ff5f9b479bea02be91f8964d742d0f02eaeb646
Vítor Galvão <info@vitorgalvao.com>
2016-03-04 17:21:35 +0000

    updated vivaldi-snapshot (1.0.403.20)

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,10 +1,10 @@
 cask 'vivaldi-snapshot' do
-  version '1.0.403.17'
-  sha256 '0b5ba8fdb0ef8b9b5cbfc65e2f5e5f48bec5bfaf432ddbc1f368688261629c44'
+  version '1.0.403.20'
+  sha256 'af76d59e1c6e8cf3f5a68aa63d4a2819ab91b32bf0df9590b778c95db371117e'
 
   url "https://vivaldi.com/download/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml',
-          checkpoint: '15894f01f752c138d51b4b4352e09edd70c2328f67733775bde4d6a188145df3'
+          checkpoint: 'd202ffc8b42281460c640e1196ee620ef1de2303ccfa26514d745dbc65f401bc'
   name 'Vivaldi'
   homepage 'https://vivaldi.com'
   license :gratis
