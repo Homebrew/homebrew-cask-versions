56ef68aeec751575441b4574538d1b99b832c05f
Vítor Galvão <info@vitorgalvao.com>
2016-03-04 17:16:20 +0000

    updated airmail-beta (2.6.1,352)

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -5,7 +5,7 @@ cask 'airmail-beta' do
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04',
-          checkpoint: '391aa223ba036528ea652f2c09c53847693ca27e450d98800f014fbeae3afb3d'
+          checkpoint: 'fa45a73c4d4dc64377e3e5ffbd356fd416a573d676c8d9eb74465d1100f52a01'
   name 'AirMail'
   homepage 'http://airmailapp.com/beta/'
   license :unknown # TODO: change license and remove this comment; ':unknown' is a machine-generated placeholder
