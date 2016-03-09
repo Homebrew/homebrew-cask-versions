8dadea26e28b8c66aadf5997b3c8109d69f78547
Victor Popkov <Dragon.vctr@gmail.com>
2016-03-09 23:15:52 +0200

    torbrowser-pl.rb: upgrade to 5.5.3

--- Casks/torbrowser-pl.rb
+++ Casks/torbrowser-pl.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-pl' do
-  version '5.5.2'
-  sha256 'f845ab054ed85ba4bf896c682c18c00a46ed6fabbdb40848bc60d695fa5a2456'
+  version '5.5.3'
+  sha256 'bb9dc4b36c85e00a1a31301282dcef327c0f9ffaf48942b3385306fe6a82d117'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_pl.dmg"
   name 'Tor Browser'
