48c52fcd7fb77e9076ac2b075767d5c88afb0146
Tom Janson <tom.janson@rwth-aachen.de>
2016-03-09 19:35:06 +0100

    Update Firefox ESR (all locales) to 45.0

--- Casks/firefox-esr-ru.rb
+++ Casks/firefox-esr-ru.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr-ru' do
-  version '38.6.1'
-  sha256 '5ad748abed1c78923988222ca5bb69730123d4756e8ae128e15b5ad4e39db4fe'
+  version '45.0'
+  sha256 '2ba35b286d72ab8f5dba491c9d7e78308d428d560c2de58ef25112c2a5e0fae4'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/ru/Firefox%20#{version}esr.dmg"
   name 'Mozilla Firefox'
