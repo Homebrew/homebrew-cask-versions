51e5ea90762abd62562ab852d4e6039fd0c32c20
Vítor Galvão <info@vitorgalvao.com>
2016-03-10 20:17:24 -0500

    updated xamarin-studio-preview (6.0.0.3668)

--- Casks/xamarin-studio-preview.rb
+++ Casks/xamarin-studio-preview.rb
@@ -4,7 +4,7 @@ cask 'xamarin-studio-preview' do
 
   url "http://download.xamarin.com/roslyn-preview/XamarinStudio-#{version}.dmg"
   appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '855d95a330efdd9cc13cbaabc6e49dfcda111db3b16cbc6345e3a0d17e769972'
+          checkpoint: '14727c4cd976ca3d96a7a9c362bebd064a49c4f24d32296d33767e3a76b4a685'
   name 'Xamarin Studio Preview'
   homepage 'https://xamarin.com/studio'
   license :gpl
