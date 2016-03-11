70612d02be6429dc8e636f17e71c842ccec364f2
Peter Josling <peterjosling@gmail.com>
2016-03-11 01:01:34 +0000

    updated intellij-idea-eap (145.256.33)

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,19 +1,19 @@
 cask 'intellij-idea-eap' do
-  version '145.184.1'
-  sha256 '934a2eb8f649d9913afcb885ccf26f30d061983d5ac04a9ac6b1fb40f95f6579'
+  version '145.256.33'
+  sha256 'fc023c598cc3cdf2f4a51ae53b9fda8a6a99f9f56d02b6bc6b57492b4e7816da'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   name 'IntelliJ IDEA EAP'
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+16+EAP'
   license :commercial
 
-  app 'IntelliJ IDEA 16 EAP.app'
+  app 'IntelliJ IDEA 2016 EAP.app'
 
   zap delete: [
                 '~/Library/Preferences/com.jetbrains.intellij.plist',
-                '~/Library/Application Support/IntelliJIdea16',
-                '~/Library/Preferences/IntelliJIdea16',
-                '~/Library/Caches/IntelliJIdea16',
-                '~/Library/Logs/IntelliJIdea16',
+                '~/Library/Application Support/IntelliJIdea2016.1',
+                '~/Library/Preferences/IntelliJIdea2016.1',
+                '~/Library/Caches/IntelliJIdea2016.1',
+                '~/Library/Logs/IntelliJIdea2016.1',
               ]
 end
