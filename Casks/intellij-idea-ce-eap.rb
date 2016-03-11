261a9616c96676e4c7835ff208ba5fa4ab8ea5db
Peter Josling <peterjosling@gmail.com>
2016-03-11 01:05:22 +0000

    updated intellij-idea-ce-eap (145.256.33)

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,20 +1,20 @@
 cask 'intellij-idea-ce-eap' do
-  version '145.184.1'
-  sha256 '16f784e54965c4e7fcbaa5629f736f2aadb25bd4d9c8754ef1f2c0e502377f56'
+  version '145.256.33'
+  sha256 '447a5948b498097ffd4737a68d022962557ff607bfc37d990851360dd67344fc'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   name 'IntelliJ IDEA EAP :: CE'
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+16+EAP'
   license :apache
 
-  app 'IntelliJ IDEA 16 CE EAP.app'
+  app 'IntelliJ IDEA 2016 CE EAP.app'
 
   zap delete: [
-                '~/Library/Application Support/IdeaIC16',
-                '~/Library/Caches/IdeaIC16',
-                '~/Library/Logs/IdeaIC16',
+                '~/Library/Application Support/IdeaIC2016.1',
+                '~/Library/Caches/IdeaIC2016.1',
+                '~/Library/Logs/IdeaIC2016.1',
                 '~/Library/Preferences/com.jetbrains.intellij.ce.plist',
-                '~/Library/Preferences/IdeaIC16',
+                '~/Library/Preferences/IdeaIC2016.1',
                 '~/Library/Saved Application State/com.jetbrains.intellij.ce.savedState',
               ]
 end
