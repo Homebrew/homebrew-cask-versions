6c5aeb2f070fd67eddd398b3cb300c8357f52dbd
Victor Popkov <victor@popkov.me>
2016-03-19 03:17:28 +0200

    torbrowser-fr.rb: upgrade to 5.5.4

--- Casks/torbrowser-fr.rb
+++ Casks/torbrowser-fr.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-fr' do
-  version '5.5.3'
-  sha256 '0dfebd49ace909d9b45b05bad41b0ca7b2127a91d54bee3cdd36ab1c409571b5'
+  version '5.5.4'
+  sha256 'c84a4fda4bdebba1d0df695f08ecab6fffa4e70de136e5bda7323d0105956059'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_fr.dmg"
   name 'Tor Browser'
