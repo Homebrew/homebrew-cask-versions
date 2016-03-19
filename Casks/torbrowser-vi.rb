068352c1d3ab096e7b3eb091c1f9e95acd23d7b8
Victor Popkov <victor@popkov.me>
2016-03-19 03:17:31 +0200

    torbrowser-vi.rb: upgrade to 5.5.4

--- Casks/torbrowser-vi.rb
+++ Casks/torbrowser-vi.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-vi' do
-  version '5.5.3'
-  sha256 '0e8d456ac11b39fe2f01e13454510407f30172cd9cf7d792415713a8efa88770'
+  version '5.5.4'
+  sha256 'a71bbfaa2fb738a88d18b1b4b5d693a4941d4e5c7e7b5b9a486c2d8b87ddae85'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_vi.dmg"
   name 'Tor Browser'
