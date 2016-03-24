9b951b1e74c92adbdcf80d3b97613b583a3945f4
Chris Rebert <github@chrisrebert.com>
2016-03-23 19:35:51 -0700

    Update WebKit Nightly to r198607
    http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-r198607.dmg

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -1,6 +1,6 @@
 cask 'webkit-nightly' do
-  version 'r194284'
-  sha256 'c06cae8dd3d9ecacf89a440a19ac5fda5587695c5c6f5e58e41eea40ba685827'
+  version 'r198607'
+  sha256 '7ed00fcc62a4f8ba334df551eba9e08cf54e8bf6abce2765b785061cc0c3e93c'
 
   url "http://builds.nightly.webkit.org/files/trunk/mac/WebKit-SVN-#{version}.dmg"
   name 'WebKit Nightly'
