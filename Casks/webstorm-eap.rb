3fe120769520a7c3d293463f520e325496b48b64
Furkan Tunalı <furkan@nod.st>
2016-03-25 00:37:54 +0200

    Update webstorm-eap.rb
    Upgrading version 145.596.9

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,14 +1,14 @@
 cask 'webstorm-eap' do
-  version '145.256.40'
-  sha256 'd4768500500a6349db252edfcf705f388d7d4273a56a7d8006dcbb9848d5bb72'
+  version '145.596.9'
+  sha256 'fae5fc7baf973f2d4f1b378ff411c7d4b4c2fdcf8c8bb0faff58b8342758cfb2'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-EAP-#{version}.dmg"
   name 'WebStorm'
   homepage 'https://confluence.jetbrains.com/display/WI/WebStorm+EAP'
   license :commercial
 
-  app 'WebStorm 2016 EAP.app'
-  binary 'WebStorm 2016 EAP.app/Contents/MacOS/webstorm'
+  app 'WebStorm 2016.1 EAP.app'
+  binary 'WebStorm 2016.1 EAP.app/Contents/MacOS/webstorm'
 
   zap delete: [
                 '~/.WebStorm2016.1',
