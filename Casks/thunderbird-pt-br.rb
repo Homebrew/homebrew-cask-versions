60ddf3ed304bf52f39086e43b1ad86926e40caa1
mahori <mahori@users.noreply.github.com>
2016-03-26 16:14:02 +0900

    Upgrade Thunderbird (localized version) to 38.7.1

--- Casks/thunderbird-pt-br.rb
+++ Casks/thunderbird-pt-br.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-pt-br' do
-  version '38.7.0'
-  sha256 'f0bf5f8c2eda8cffdad2c816603b7f85a0c889f5fdba3539e6b6dbe4ca44450e'
+  version '38.7.1'
+  sha256 '338483c6a008778fccd4e17746b8ddaefa25a38977bab073fcafd6035b495d30'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=pt-BR"
   name 'Mozilla Thunderbird'
