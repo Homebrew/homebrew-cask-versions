1cd0eae6215e9e95373891acb15be6d0a4a91390
Yurii Kolesnykov <yurikoles@gmail.com>
2016-03-28 17:42:45 +0300

    Fixed paths in RubyMine-EAP's zap

--- Casks/rubymine-eap.rb
+++ Casks/rubymine-eap.rb
@@ -1,20 +1,21 @@
 cask 'rubymine-eap' do
-  version '145.184.8'
-  sha256 'b9de2b0543d3d47932666788b86fc67e9cd5951526d62b1220370bf9209d6e4a'
+  version '2016.1-RC'
+  sha256 'a7f620c6841a3abdc73fa477697d9ffe2bb772d0ec0c50a3eb4fc868803ef982'
 
   url "https://download.jetbrains.com/ruby/RubyMine-#{version}.dmg"
   name 'RubyMine EAP'
   homepage 'https://confluence.jetbrains.com/display/RUBYDEV/RubyMine+EAP'
   license :commercial
 
-  app 'RubyMine EAP.app'
+  app 'RubyMine.app'
 
-  zap delete: [
-                '~/Library/Preferences/com.jetbrains.rubymine-EAP.plist',
-                '~/Library/Preferences/RubyMine70',
-                '~/Library/Application Support/RubyMine-EAP',
-                '~/Library/Caches/RubyMine70',
-                '~/Library/Logs/RubyMine70',
-                '/usr/local/bin/mine',
+  zap delete: [                
+                "~/Library/Preferences/com.jetbrains.rubymine.plist",
+
+                "~/Library/Preferences/RubyMine#{version.split('-')[0]}",
+                "~/Library/Application Support/RubyMine",
+                "~/Library/Caches/RubyMine#{version.split('-')[0]}",
+                "~/Library/Logs/RubyMine#{version.split('-')[0]}",
+                "/usr/local/bin/mine",
               ]
 end
