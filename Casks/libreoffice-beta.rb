4384aad5ae17f63833f9d22efa116077bbe614a4
Yurii Kolesnykov <yurikoles@gmail.com>
2016-03-30 14:13:19 +0300

    LibreOffice 5.1.2.1

--- Casks/libreoffice-beta.rb
+++ Casks/libreoffice-beta.rb
@@ -1,8 +1,8 @@
 cask 'libreoffice-beta' do
-  version '5.1.0.2'
-  sha256 '2955fc75c6f496a9055e4f8fd4075a506ca64f4a72df77a4c0e75e886a98849b'
+  version '5.1.2.1'
+  sha256 'cf211d607658b1f678d6ce9602e975b186b8abf389f995022cdbe48904ba6c1e'
 
-  url "https://download.documentfoundation.org/libreoffice/testing/#{version.split('.')[0..2].join('.')}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
+  url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   name 'LibreOfficeDev'
   homepage 'https://www.libreoffice.org/download/pre-releases/'
   license :mpl
