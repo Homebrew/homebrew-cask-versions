2d084bbbd7313ddaad3bcdc93c57d89ef4009463
Tom Janson <tom.janson@rwth-aachen.de>
2016-04-02 02:47:05 +0200

    Update Appcode EAP (2016.1)
    This is the current stable release.
    The Appcode EAP is suspended until a new preview is released.

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,5 +1,5 @@
 cask 'appcode-eap' do
-  version '2016.1-RC2'
+  version '2016.1'
   sha256 'dec7447da549f6c2033e6bea8587413888e881f0019a436a711ed07fc42c2b58'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version}.dmg"
@@ -12,9 +12,16 @@ cask 'appcode-eap' do
   app 'AppCode.app'
 
   zap delete: [
-                '~/Library/Preferences/AppCode2016.1',
-                '~/Library/Application Support/AppCode2016.1',
-                '~/Library/Caches/AppCode2016.1',
-                '~/Library/Logs/AppCode2016.1',
+                "~/.Appcode#{version.major_minor}",
+                # TODO: expand/glob for '~/Library/Preferences/jetbrains.appcode.*.plist',
+                "~/Library/Preferences/AppCode#{version.major_minor}",
+                "~/Library/Application Support/AppCode#{version.major_minor}",
+                "~/Library/Caches/AppCode#{version.major_minor}",
+                "~/Library/Logs/AppCode#{version.major_minor}",
               ]
+
+  caveats <<-EOS.undent
+    Please manually change to the EAP update channel via:
+       Preferences > Appearance & Behavior > System Settings > Updates
+    EOS
 end
