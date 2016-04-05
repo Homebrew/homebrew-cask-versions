e8eec79e0385ad1d4606ee19e1a868ef52d7a88a
Yurii Kolesnykov <yurikoles@gmail.com>
2016-04-05 07:46:43 +0300

    Android Studio Canary – use new version API

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,8 +1,8 @@
 cask 'android-studio-canary' do
-  version '2.1.0.3-143.2730271'
+  version '2.1.0.3,143.2730271'
   sha256 'f8bff65f08e55f3990958f43ed52dad040d5b7aedfbde1dd2859b6747b4ffd01'
 
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version.sub(%r{-.*}, '')}/android-studio-ide-#{version.sub(%r{.*?-}, '')}-mac.zip"
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Canary'
   homepage 'https://sites.google.com/a/android.com/tools/download/studio/canary'
   license :apache
@@ -10,11 +10,11 @@ cask 'android-studio-canary' do
   app 'Android Studio.app'
 
   zap delete: [
-                '~/Library/Preferences/AndroidStudio*',
+                "~/Library/Preferences/AndroidStudioPreview#{version.major_minor}",
                 '~/Library/Preferences/com.google.android.studio.plist',
-                '~/Library/Application Support/AndroidStudio*',
-                '~/Library/Logs/AndroidStudio*',
-                '~/Library/Caches/AndroidStudio*',
+                "~/Library/Application Support/AndroidStudioPreview#{version.major_minor}",
+                "~/Library/Logs/AndroidStudioPreview#{version.major_minor}",
+                "~/Library/Caches/AndroidStudioPreview#{version.major_minor}",
               ],
       rmdir:  '~/AndroidStudioProjects'
 
