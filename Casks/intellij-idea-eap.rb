28257b224ba0203136ed4fb63175dad51585fbe0
Jerzerak <jerzerak@gmail.com>
2016-04-07 14:20:19 -0400

    Update intellij-idea-eap (145.844.1)

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,10 +1,10 @@
 cask 'intellij-idea-eap' do
-  version '145.256.33'
-  sha256 'fc023c598cc3cdf2f4a51ae53b9fda8a6a99f9f56d02b6bc6b57492b4e7816da'
+  version '145.844.1'
+  sha256 'e9c2e727689fccd3feb3355f440fbbcf84dbb5c4b9cfd67b4022f2b1de74a906'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   name 'IntelliJ IDEA EAP'
-  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+16+EAP'
+  homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+2016.1+EAP'
   license :commercial
 
   app 'IntelliJ IDEA 2016 EAP.app'
