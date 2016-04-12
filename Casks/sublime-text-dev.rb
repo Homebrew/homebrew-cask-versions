26703f5f93a80eb1d22eba1bbe91285994971900
Jerzerak <jerzerak@gmail.com>
2016-04-12 00:07:10 -0400

    Update sublime-text-dev (3110) (#1905)

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,10 +1,10 @@
 cask 'sublime-text-dev' do
-  version '3109'
-  sha256 'd4e8eb93776805c6d103266126967329e38027f74e7a21b6d6686f8a1dd973a4'
+  version '3110'
+  sha256 'bfd7c38fac0c765b71de819bc909b177dc67f0bbbf059c41a1372982c76efdc4'
 
   url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/3/dev/appcast_osx.xml',
-          checkpoint: '3070f461e217e9ec3639962224cf49b728271a795e29c67e361151607ca54563'
+          checkpoint: 'abad49682d91e183522efc01ee2cb67a1588be1bd52ab041b1de1b88f00a2691'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/3dev'
   license :closed
