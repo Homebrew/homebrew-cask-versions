5b561f0c826630057585b467ccafd3fa792341cc
Michael Ford <fanquake@users.noreply.github.com>
2016-04-14 09:43:39 +0800

    Transmission Nightly 14731

--- Casks/transmission-nightly.rb
+++ Casks/transmission-nightly.rb
@@ -1,10 +1,10 @@
 cask 'transmission-nightly' do
-  version '14698,6419'
+  version '14731'
   sha256 'a13599cf7f393c100bdd7d073a55e3cfbb571260686fcdbd8bbc91927418f82d'
 
-  url "https://build.transmissionbt.com/job/trunk-mac/#{version.after_comma}/artifact/release/Transmission-#{version.before_comma}.dmg"
+  url "https://build.transmissionbt.com/job/trunk-mac/lastSuccessfulBuild/artifact/release/Transmission-#{version}.dmg"
   name 'Transmission'
-  homepage 'http://www.transmissionbt.com/'
+  homepage 'https://www.transmissionbt.com'
   license :gpl
 
   app 'Transmission.app'
