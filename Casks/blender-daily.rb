dca4bf3a9407de503fdbda4cf7496abe4af6df99
Michael Ford <fanquake@users.noreply.github.com>
2016-04-14 17:56:49 +0800

    Blender Daily 2.77-4a64b36

--- Casks/blender-daily.rb
+++ Casks/blender-daily.rb
@@ -1,6 +1,6 @@
 cask 'blender-daily' do
-  version '2.77-9777c5b'
-  sha256 '2050a3242e8bcd55f6d30686b3fd664e0d5f69eaa1cc5d19256d6042fb5ba2b2'
+  version '2.77-4a64b36'
+  sha256 'b930590f60c66861dde54d45da21cb673e020b7eda43b8e4d2eca920fe9c9170'
 
   url "https://builder.blender.org/download/blender-#{version}-OSX-10.6-x86_64.zip"
   name 'Blender'
