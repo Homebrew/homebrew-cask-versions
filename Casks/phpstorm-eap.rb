878bc700cd1826eb5487bc5e596db0eb75e1487f
Tom Janson <tom.janson@rwth-aachen.de>
2016-04-14 16:54:15 +0200

    Fix PhpStorm 2016.1.1 EAP: version / zap stanza
    see comment on #1889 [1]
    
    [1]: https://github.com/caskroom/homebrew-versions/pull/1889#issuecomment-209691729)

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,13 +1,13 @@
 cask 'phpstorm-eap' do
-  version '145.844.5'
+  version '2016.1.1,145.844.5'
   sha256 'e339ff19d9458262c80386b0bbf74004887e286d4c6149df83332c29e9f8a29c'
 
-  url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg"
+  url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version.after_comma}.dmg"
   name 'PhpStorm EAP'
   homepage 'https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
   license :commercial
 
-  app 'PhpStorm 2016.1.1 EAP.app'
+  app "PhpStorm #{version.before_comma} EAP.app"
 
   uninstall delete: '/usr/local/bin/pstorm'
 
