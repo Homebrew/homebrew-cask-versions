e066ec17767434789e0accddd988ed8363b5eaf8
Justin Hall <justin.r.hall@gmail.com>
2016-04-16 12:51:32 -0600

    Update Android Studio Canary to 2.1 Beta (#1917)

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,6 +1,6 @@
 cask 'android-studio-canary' do
-  version '2.1.0.3,143.2730271'
-  sha256 'f8bff65f08e55f3990958f43ed52dad040d5b7aedfbde1dd2859b6747b4ffd01'
+  version '2.1.0.5,143.2759333'
+  sha256 '8a76eb901646d340b4edf2fb0b0475d457a2b30ef019fa4b5c2f5891695d47ea'
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Canary'
