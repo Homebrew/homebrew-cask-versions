1e0b3e71d06767a72a6c56d80dacb736cf80ea1d
Victor Popkov <victor@popkov.me>
2016-04-20 23:19:29 +0300

    updated opera-beta (37.0.2178.22) (#1926)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '37.0.2178.19'
-  sha256 '0709011ff4b28701bc100c4dec8284f7997fb01f5c3a4ffed063537971b92564'
+  version '37.0.2178.22'
+  sha256 'c52026e2cdd3f94ea49c208c2d3c5c4ef51f1025410fedc4a579f3825da14b60'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
