c132c4796cd7f6b5ed6a48fdb4487ea6469036d4
Michael Ford <fanquake@users.noreply.github.com>
2016-04-21 20:37:36 +0800

    Logic Beta 1.2.8

--- Casks/logic-beta.rb
+++ Casks/logic-beta.rb
@@ -1,6 +1,6 @@
 cask 'logic-beta' do
-  version '1.2.5'
-  sha256 '6699a0da5b14c85da662d2df3b8c6453459a49c1be7e833854bbb000bfce9288'
+  version '1.2.8'
+  sha256 '3faba40c461c764f7570b981f607e0c5f5e94a227116f318e9ed3712ce024c65'
 
   url "http://downloads.saleae.com/betas/#{version}/Logic-#{version}-Darwin.dmg"
   name 'Logic'
