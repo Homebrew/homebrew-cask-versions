ed5c5e932d01eca609283e10b84ef7b617a2a306
Bruce Sun <bruce.sun@tubemogul.com>
2016-04-21 13:43:55 +0800

    Upgrade pycharm-ce to version 2016.1.2

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-ce' do
-  version '2016.1'
-  sha256 'fc9f27d0d83940623173dec8843c80c95eccfe9cead95a585ba0964f62681bd5'
+  version '2016.1.2'
+  sha256 'e68b9fa401500311bcc3cbbcbd6b3ab02849dcac3bdb6864389aec17c6c7e992'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
   name 'Jetbrains PyCharm Community Edition'
