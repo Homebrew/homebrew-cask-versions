5fe351f6ceb5a66c09c0b06644fd101f0a1c518d
Victor Popkov <victor@popkov.me>
2016-04-22 23:11:20 +0300

    updated iterm2-nightly (2.9.20160422) (#1931)

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,10 +1,10 @@
 cask 'iterm2-nightly' do
-  version '2.9.20160420'
-  sha256 '21cf84e58ea8725f23dbf5071c069d32bbb6a14d9d51558c4aebfcf40ddfceb2'
+  version '2.9.20160422'
+  sha256 '9006f0a971f9e80ce5347379c587c1433350df72b692a7374fe2fb79a9c9a300'
 
   url "https://iterm2.com/downloads/nightly/iTerm2-#{version.dots_to_underscores}-nightly.zip"
   appcast 'https://iterm2.com/appcasts/nightly.xml',
-          checkpoint: '40151318054c2d80fc6c3a1b7ba354f5525b6c98544d6e3dd2f0accc68e069b9'
+          checkpoint: '193d7eb8e1301b5084c8d66a12ae3f25d750d0a10d00e1a56011b88671b201e6'
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
   license :gpl
