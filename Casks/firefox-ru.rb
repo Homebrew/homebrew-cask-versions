e347dbb15d8d956da1bd072f36b12540e82dc687
Yurii Kolesnykov <yurikoles@gmail.com>
2016-04-26 21:46:53 +0300

    firefox uk & ru 46.0

--- Casks/firefox-ru.rb
+++ Casks/firefox-ru.rb
@@ -1,6 +1,6 @@
 cask 'firefox-ru' do
-  version '45.0.2'
-  sha256 'afd2212c0303a71b44a94f4360bf8ae642e57309108dda07002c9b3523b01195'
+  version '46.0'
+  sha256 '19aa0a2ec64767f746afd23001114548fa5added53953b43dfcb0ce1f2614f27'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ru"
   name 'Mozilla Firefox'
