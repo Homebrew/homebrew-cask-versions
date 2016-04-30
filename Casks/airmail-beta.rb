304242280e63d90c0a63c6b9d896c3f251d0b94e
Yurii Kolesnykov <yurikoles@gmail.com>
2016-04-30 12:20:55 +0300

    Don't track version of airmail-beta

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 cask 'airmail-beta' do
-  version '2.6.1,355'
-  sha256 'ac6abb0be3ce0c815bc582fdce1e799b89daf9417896887c36496df3ffccab69'
+  version :latest
+  sha256 :no_check
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
