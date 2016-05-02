96dc3b413b6a9e962463460896f9b80c35eac911
inailuig <inailuig@users.noreply.github.com>
2016-05-02 17:43:28 +0200

    updated telegram-dev (0.9.46.alpha) (#1959)

--- Casks/telegram-dev.rb
+++ Casks/telegram-dev.rb
@@ -1,6 +1,6 @@
 cask 'telegram-dev' do
-  version '0.9.43.dev'
-  sha256 '212b1b527a645a91a68c4a6a0e67a99397062bf9136ff96cb15e60aa9e495550'
+  version '0.9.46.alpha'
+  sha256 '6eac9eb01b9c1dda323771291b8ad4b319800d7b00a93b5ad3d7080323d0030a'
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   name 'Telegram'
