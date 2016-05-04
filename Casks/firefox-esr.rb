0d1cf72bde0fece1ead59a75ae396ae1bc586e13
Michael Ford <fanquake@users.noreply.github.com>
2016-05-04 10:14:37 +0800

    Firefox ESR 45.1.1

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr' do
-  version '45.1.0'
-  sha256 'd86830cff20a23192a719a1799968b4eb410016e6ba251e40df126a8d96327b7'
+  version '45.1.1'
+  sha256 '0f3b42692237456af0c8989ef51f7487a462380c67adbe4c130ab253d5226f51'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
   name 'Mozilla Firefox'
