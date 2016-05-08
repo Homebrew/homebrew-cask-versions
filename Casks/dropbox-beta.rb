ce605aef030e3c4eb7af3d583d036cc5488ea651
Michael Ford <fanquake@users.noreply.github.com>
2016-05-08 22:00:34 +0800

    Dropbox Beta 3.19.35

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,10 +1,10 @@
 cask 'dropbox-beta' do
-  version '3.19.34'
-  sha256 '724491bc6f14940aa883e82665845b72716a57d4235b3b837f87cd5ac0a01143'
+  version '3.19.35'
+  sha256 'd8663c384227b49db952e3561f64a5d9459715199aec7bdf8a09cd7ff99bd230'
 
   url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
   name 'Dropbox'
-  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/206565816-Beta-Build-3-19-33'
+  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/205610373-Beta-Build-3-19-35'
   license :gratis
 
   app 'Dropbox.app'
