d4577afe4034db9ee96243c8b21e3e0a88d35166
Victor Popkov <victor@popkov.me>
2016-05-09 22:52:18 +0300

    updated airmail-beta (2.6.1,362)

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,11 +1,11 @@
 cask 'airmail-beta' do
-  version '2.6.1,360'
-  sha256 'd28b200703c7369c03cdba8a8a8e75718cf9cd23ba5862031e4ae2e3db4ebacd'
+  version '2.6.1,362'
+  sha256 '84684e0a8234fcbbe0a63b3422b39869b5837c6a92e3e8d4cdb5df37c848ed48'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04?format=zip'
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04',
-          checkpoint: '32d79ffe649ea698e68673885fd2cd843b76f35e7f36840ea48458e4fdda4667'
+          checkpoint: '6d7529efacd43349a85b5c8c942387928f56372665890945dd98313a40fbb500'
   name 'AirMail'
   homepage 'http://airmailapp.com/beta/'
   license :commercial
