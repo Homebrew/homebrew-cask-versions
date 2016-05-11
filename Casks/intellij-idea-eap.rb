0553db021bd4952b220c02dff76b567b2847fda9
Tom Janson <tom.janson@rwth-aachen.de>
2016-05-11 19:28:01 +0200

    updated intellij-idea-eap (2016.1.2)

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,19 +1,19 @@
 cask 'intellij-idea-eap' do
-  version '145.970.4'
-  sha256 '663b9ed3d867ccecfcc40d559a56e8f4486ce95691106d690bf271d3f04f82dc'
+  version '2016.1.2'
+  sha256 '8e104524832891e9eb8c567a404f63038caa1f73e403811440e95f8b63164ecf'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   name 'IntelliJ IDEA EAP'
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+2016.1+EAP'
   license :commercial
 
-  app 'IntelliJ IDEA 2016.1 EAP.app'
+  app "IntelliJ IDEA #{version.major_minor} EAP.app"
 
   zap delete: [
                 '~/Library/Preferences/com.jetbrains.intellij.plist',
-                '~/Library/Application Support/IntelliJIdea2016.1',
-                '~/Library/Preferences/IntelliJIdea2016.1',
-                '~/Library/Caches/IntelliJIdea2016.1',
-                '~/Library/Logs/IntelliJIdea2016.1',
+                "~/Library/Application Support/IntelliJIdea#{version.major_minor}",
+                "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+                "~/Library/Caches/IntelliJIdea#{version.major_minor}",
+                "~/Library/Logs/IntelliJIdea#{version.major_minor}",
               ]
 end
