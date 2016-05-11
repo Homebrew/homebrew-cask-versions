01532fc57bb3eccb58f841011b2b7005e54a7462
Tom Janson <priv.tom.janson@gmail.com>
2016-05-11 21:41:25 +0200

    updated intellij-idea-ce-eap (2016.1.2) (#2019)

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,20 +1,29 @@
 cask 'intellij-idea-ce-eap' do
-  version '145.970.4'
-  sha256 'f19e88c1ada9c186f9ced31e59b908e14b7cf0f09fbc91098af8cab9841ef9f7'
+  version '2016.1.2'
+  sha256 'f5cf4c7574e4001f08ff8132dd2ec3ef6d3348b98b280b24b2f533a999cceaef'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
-  name 'IntelliJ IDEA EAP :: CE'
+  name 'IntelliJ IDEA Community Edition'
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+2016.1+EAP'
   license :apache
 
   app 'IntelliJ IDEA CE.app'
 
   zap delete: [
-                '~/Library/Application Support/IdeaIC2016.1',
-                '~/Library/Caches/IdeaIC2016.1',
-                '~/Library/Logs/IdeaIC2016.1',
+                "~/Library/Application Support/IdeaIC#{version.major_minor}",
+                "~/Library/Caches/IdeaIC#{version.major_minor}",
+                "~/Library/Logs/IdeaIC#{version.major_minor}",
                 '~/Library/Preferences/com.jetbrains.intellij.ce.plist',
-                '~/Library/Preferences/IdeaIC2016.1',
+                "~/Library/Preferences/IdeaIC#{version.major_minor}",
                 '~/Library/Saved Application State/com.jetbrains.intellij.ce.savedState',
               ]
+
+  # remove this when this cask is updated to an EAP release
+  caveats <<-EOS.undent
+    There is currently no EAP preview release. Instead, the latest stable
+    version will be installed.
+    To receive future EAP releases via the IDE's built-in update system, go to
+       Preferences > Appearance & Behavior > System Settings > Updates
+    and select the Early Access Program channel.
+  EOS
 end
