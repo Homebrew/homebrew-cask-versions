bde6bfd2c569cef815a89541ccc1b5eb80580069
Jerzerak <jerzerak@gmail.com>
2016-05-12 16:10:36 -0400

    Update visual-studio-code-insiders.rb (#2028)

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,9 +1,9 @@
 cask 'visual-studio-code-insiders' do
-  version '1f0657703be5609d70088b0385bc27db8cc03a41'
+  version '1.1.0,1f0657703be5609d70088b0385bc27db8cc03a41'
   sha256 'ae207e4e3e5656c4f9f3b3e5769f74ee40a59225af6a72f69d29279f92fcf5f8'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
-  url "https://az764295.vo.msecnd.net/insider/#{version}/VSCode-darwin-insider.zip"
+  url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
   name 'Microsoft Visual Studio Code'
   name 'VS Code - Insiders'
   homepage 'https://code.visualstudio.com/insiders'
@@ -12,6 +12,7 @@ cask 'visual-studio-code-insiders' do
   auto_updates true
 
   app 'Visual Studio Code - Insiders.app'
+  binary 'Visual Studio Code - Insiders.app/Contents/Resources/app/bin/code'
 
   zap delete: [
                 '~/Library/Application Support/Code - Insiders',
