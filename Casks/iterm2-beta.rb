6b9e65d10b9d187da039ac8573044f38321cac68
Anto GG <antogg+github@gmail.com>
2016-05-13 08:50:59 +0700

    updated iterm2-beta (2.9.20160510) (#2029)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-beta' do
-  version '2.9.20160426'
-  sha256 '439800354dad6fd1c209e8b066ef8cfb683f81f8c1de40b839853fa31f93c1da'
+  version '2.9.20160510'
+  sha256 '4c8273c95a0200c175199f96d448700381838dca754ec5e1cfa41b934c305642'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
