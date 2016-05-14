72b78a9a44764298600469b7ce8471b6c1d355e5
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-05-14 08:38:41 +0800

    updated firefox-beta-ja (47.0b5) (#2032)

--- Casks/firefox-beta-ja.rb
+++ Casks/firefox-beta-ja.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-ja' do
-  version '47.0b4'
-  sha256 '8cc611cfc79f34430983b844b630f8302ec48e9f89a6b72af007e8f8e489a4b0'
+  version '47.0b5'
+  sha256 'fae82598d38828e6f8d3687e4212661f9cb3dc281d24d25958a717e29e10ecb4'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
   name 'Mozilla Firefox'
