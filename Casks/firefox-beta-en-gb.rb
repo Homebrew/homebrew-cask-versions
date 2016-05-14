08b99e24369f28b80ce2e53608ccac4a46e5d366
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-05-14 08:38:44 +0800

    updated firefox-beta-en-gb (47.0b5) (#2031)

--- Casks/firefox-beta-en-gb.rb
+++ Casks/firefox-beta-en-gb.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-en-gb' do
-  version '47.0b4'
-  sha256 '0181f08b48bd0195d5d40545914a7d21930734e738631a5648de2cd814a12679'
+  version '47.0b5'
+  sha256 '35c605daccd15ca3af596feed2647455301feab6a056bae9f811567207598652'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=en-GB"
   name 'Mozilla Firefox'
