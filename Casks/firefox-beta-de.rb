0175d5b7219a15090bfb5cd3c6d0ad422477d734
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-05-14 08:38:22 +0800

    updated firefox-beta-de (47.0b5) (#2030)

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-de' do
-  version '47.0b4'
-  sha256 'a764726a461bf8a3b46e46926d909f98fde2e28dc70e8c325bd290b68610aaee'
+  version '47.0b5'
+  sha256 '9ac233a3a1a97e09a3ea20c00de86d4fc64eadbc588d2ebb9f0ab0be6ed88e3a'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   name 'Mozilla Firefox'
