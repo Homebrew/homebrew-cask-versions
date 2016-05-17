1ed205a4426a4d37c889b9c474ea725f7238ed99
Tommy Sparber <tommy@sparber.net>
2016-05-17 22:33:01 +0200

    updated libreoffice-beta (5.1.3.2)

--- Casks/libreoffice-beta.rb
+++ Casks/libreoffice-beta.rb
@@ -1,8 +1,10 @@
 cask 'libreoffice-beta' do
-  version '5.1.2.1'
-  sha256 'cf211d607658b1f678d6ce9602e975b186b8abf389f995022cdbe48904ba6c1e'
+  version '5.1.3.2'
+  sha256 '969597335139d626a2fff98089fd3e2d4d7bd8ddd6753b355ec7a9d8889f4b5b'
 
   url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
+  appcast 'https://download.documentfoundation.org/libreoffice/testing/',
+          checkpoint: 'b7b14688525109c2709454f77524a6a17110a48e1e2b8b5492d69080dae0e02e'
   name 'LibreOfficeDev'
   homepage 'https://www.libreoffice.org/download/pre-releases/'
   license :mpl
