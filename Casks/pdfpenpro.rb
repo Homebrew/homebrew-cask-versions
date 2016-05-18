68efac5d550cb18c6d6404240b7b6e52954c667f
Victor Popkov <victor@popkov.me>
2016-05-19 00:51:52 +0300

    updated pdfpenpro (801.3,1463500658) (#2056)

--- Casks/pdfpenpro.rb
+++ Casks/pdfpenpro.rb
@@ -1,10 +1,10 @@
 cask 'pdfpenpro' do
-  version '800.19,1461645430'
-  sha256 '56b1cef046b8d8961987771217bf19c67cccdf8d9ccf999410fa2fcffeb3e7f7'
+  version '801.3,1463500658'
+  sha256 '52d5370b47bc33d37f3aa94eb6e8bd14127158f5a2e95236d37b16997f133fee'
 
   url "https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/#{version.before_comma}/#{version.after_comma}/PDFpenPro-#{version.before_comma}.zip"
   appcast 'https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml',
-          checkpoint: '6a1f150c4418765813e37e6abd23cee562d113fa5bded2b5d65e022463851ae3'
+          checkpoint: '65958504da8c8667cf8fc7aca82fde50638e840c17f369929f83d07d8d3fe3c9'
   name 'PDFpenPro'
   homepage 'https://www.smilesoftware.com/PDFpenPro/'
   license :commercial
