79dba7483edd4e59bc6ecb40fc513eb1057df3d8
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-05-19 11:04:08 +0800

    updated firefox-beta-ru (47.0b6) (#2061)

--- Casks/firefox-beta-ru.rb
+++ Casks/firefox-beta-ru.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-ru' do
-  version '47.0b5'
-  sha256 '5407714edd73e61f670858d628d0c7860efcf7ac17b6fc554cf6ed1650b81986'
+  version '47.0b6'
+  sha256 'e035292d9e0fb01933107265254cd40562401f25480c37e886897e8e1d53f8ea'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ru"
   name 'Mozilla Firefox'
