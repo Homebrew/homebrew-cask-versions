730ccd09914c162671e14d824c734de37c24aded
djmason <djmason@gmail.com>
2016-05-24 13:22:02 +1000

    updated iterm2-beta (2.9.20160523) (#2082)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-beta' do
-  version '2.9.20160510'
-  sha256 '4c8273c95a0200c175199f96d448700381838dca754ec5e1cfa41b934c305642'
+  version '2.9.20160523'
+  sha256 '843ac6da4459c6c3318ef39f349d24c6295dc4b176a4cd4c0d3d82cf1664aa6d'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
