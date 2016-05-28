01e346975e080f2522553e4811966847fb648bbc
Jerzerak <jerzerak@gmail.com>
2016-05-28 15:47:41 -0400

    intellij-idea-ce-eap 162.426.1
    -  Amend app stanza

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,13 +1,13 @@
 cask 'intellij-idea-ce-eap' do
-  version '162.74.16'
-  sha256 'a3bd2e724200014647fd9bff531b0ad24d5250a9756481c22da611ec67c2cfab'
+  version '162.426.1'
+  sha256 '2e69ffbfb7b34944c20363a109b219b42e5315c1249bbc33962620f4b1434414'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   name 'IntelliJ IDEA Community Edition EAP'
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+2016.2+EAP'
   license :apache
 
-  app 'IntelliJ IDEA CE.app'
+  app 'IntelliJ IDEA 2016.2 CE EAP.app'
 
   uninstall delete: '/usr/local/bin/idea'
 
