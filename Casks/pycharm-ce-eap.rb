00f8b86fcdefb0024745a8fad2892455fdbb9cc5
Jerzerak <jerzerak@gmail.com>
2016-05-28 18:02:38 -0400

    PyCharm CE EAP 2016.1.4 (#2111)

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,9 +1,10 @@
 cask 'pycharm-ce-eap' do
-  version '2016.1.3'
-  sha256 '3da97fd5b103d602fafd433549d43a6a6503ce9cfeb6be34da8ffe26a6eb0267'
+  version '2016.1.4'
+  sha256 '6c535f5a17a1d90a65c4e656b41d335f71d58f9014f79fb5e20255f9c3d6c75c'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
-  name 'PyCharm Community Edition'
+  name 'Jetbrains PyCharm Community Edition'
+  name 'PyCharm CE'
   homepage 'https://confluence.jetbrains.com/display/PYH/JetBrains+PyCharm+Preview+(EAP)'
   license :apache
 
@@ -15,7 +16,6 @@ cask 'pycharm-ce-eap' do
 
   zap delete: [
                 "~/.PyCharm#{version.major_minor}",
-                # TODO: expand/glob for '~/Library/Preferences/jetbrains.pycharm.*.plist',
                 "~/Library/Preferences/PyCharm#{version.major_minor}",
                 "~/Library/Application Support/PyCharm#{version.major_minor}",
                 "~/Library/Caches/PyCharm#{version.major_minor}",
