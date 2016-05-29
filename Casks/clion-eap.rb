42df077294ef402247126a8471db8f312ea83e22
Jerzerak <jerzerak@gmail.com>
2016-05-29 03:21:49 -0400

    CLion EAP 162.426.5 (#2118)
    - Amend `name` stanza
    - Amend `app` stanza
    - Amend `zap` stanza
    - Remove `caveats` stanza

--- Casks/clion-eap.rb
+++ Casks/clion-eap.rb
@@ -1,31 +1,21 @@
 cask 'clion-eap' do
-  version '2016.1.2'
-  sha256 'cc3af250ba762741cfc9e3bdcb62863336e89ebf506043f73f043c51f9f626dd'
+  version '162.426.5'
+  sha256 '05ecf954104ef8e0ad352b1e120e396b0418a8e31f24b14f9e1fe308a55d0b9f'
 
   url "https://download.jetbrains.com/cpp/CLion-#{version}.dmg"
-  name 'CLion'
+  name 'CLion EAP'
   homepage 'https://confluence.jetbrains.com/display/CLION/Early+Access+Program'
   license :commercial
 
   conflicts_with cask: 'clion'
 
-  app 'CLion.app'
+  app 'CLion 2016.2 EAP.app'
 
   zap delete: [
-                "~/.CLion#{version.major_minor}",
-                # TODO: expand/glob for '~/Library/Preferences/jetbrains.clion.*.plist',
-                "~/Library/Preferences/CLion#{version.major_minor}",
-                "~/Library/Application Support/CLion#{version.major_minor}",
-                "~/Library/Caches/CLion#{version.major_minor}",
-                "~/Library/Logs/CLion#{version.major_minor}",
+                '~/.CLion2016.2',
+                '~/Library/Preferences/CLion2016.2',
+                '~/Library/Application Support/CLion2016.2',
+                '~/Library/Caches/CLion2016.2',
+                '~/Library/Logs/CLion2016.2',
               ]
-
-  # remove this when this cask is updated to an EAP release
-  caveats <<-EOS.undent
-    There is currently no EAP preview release. Instead, the latest stable
-    version will be installed.
-    To receive future EAP releases via the IDE's built-in update system, go to
-       Preferences > Appearance & Behavior > System Settings > Updates
-    and select the Early Access Program channel.
-  EOS
 end
