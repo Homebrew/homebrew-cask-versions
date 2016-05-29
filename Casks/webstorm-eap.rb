9293e8b8a2b1befec62d7ac9a29253e0d68f8050
Jerzerak <jerzerak@gmail.com>
2016-05-29 03:21:14 -0400

    WebStorm EAP 162.426.3 (#2116)
    - Amend `url` stanza
    - Amend `name` stanza
    - Add `conflicts_with` stanza
    - Amend `app` stanza
    - Amend `zap` stanza
    - Remove `caveats` stanza

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,31 +1,23 @@
 cask 'webstorm-eap' do
-  version '2016.1.2'
-  sha256 '26f48c66d0e13f97f47e21e360c5f78b94c13d6e1a46e0f96f9d3a7f26abcb3e'
+  version '162.426.3'
+  sha256 '71165af62a00f0b2773d80de2f77b058979dfdf40f2d4dfb48c10e930d313c72'
 
-  url "https://download.jetbrains.com/webstorm/WebStorm-#{version}.dmg"
-  name 'WebStorm'
+  url "https://download.jetbrains.com/webstorm/WebStorm-EAP-#{version}.dmg"
+  name 'WebStorm EAP'
   homepage 'https://confluence.jetbrains.com/display/WI/WebStorm+EAP'
   license :commercial
 
-  app 'WebStorm.app'
+  conflicts_with cask: 'webstorm'
+
+  app 'WebStorm 2016.2 EAP.app'
 
   uninstall delete: '/usr/local/bin/wstorm'
 
   zap delete: [
-                "~/.WebStorm#{version.major_minor}",
-                # TODO: expand/glob for '~/Library/Preferences/jetbrains.webstorm.*.plist',
-                "~/Library/Preferences/WebStorm#{version.major_minor}",
-                "~/Library/Application Support/WebStorm#{version.major_minor}",
-                "~/Library/Caches/WebStorm#{version.major_minor}",
-                "~/Library/Logs/WebStorm#{version.major_minor}",
+                '~/.WebStorm2016.2',
+                '~/Library/Preferences/WebStorm2016.2',
+                '~/Library/Application Support/WebStorm2016.2',
+                '~/Library/Caches/WebStorm2016.2',
+                '~/Library/Logs/WebStorm2016.2',
               ]
-
-  # remove this when this cask is updated to an EAP release
-  caveats <<-EOS.undent
-    There is currently no EAP preview release. Instead, the latest stable
-    version will be installed.
-    To receive future EAP releases via the IDE's built-in update system, go to
-       Preferences > Appearance & Behavior > System Settings > Updates
-    and select the Early Access Program channel.
-  EOS
 end
