4caea71e0a80fabbe62163751d90c6d490bdc0e4
Jerzerak <jerzerak@gmail.com>
2016-05-29 03:51:56 -0400

    VirtualBox Beta 5.0.21-107545 & 4.3.32-103443
    - Amend `url` stanza
    - Add `auto_updates` stanza
    - Add `zap` stanza

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -1,11 +1,18 @@
 cask 'virtualbox-beta' do
-  version '5.0.21-107302'
-  sha256 'f670826547df8357d3c9098985ac620d5aa8959b85d0d888543f0404f3247663'
+  if MacOS.release <= :lion
+    version '4.3.32-103443'
+    sha256 '08defbf310b7ba5852fa8dd951438bb9b1528bb1544211568861986110e807f7'
+  else
+    version '5.0.21-107545'
+    sha256 '5b6e74e99daf72d2f67d5bf588d314be4d0b2086f8af9ee66b58e16ce80d6a45'
+  end
 
-  url "http://download.virtualbox.org/virtualbox/#{version.split('-')[0]}/VirtualBox-#{version}-OSX.dmg"
+  url "https://virtualbox.org/download/testcase/VirtualBox-#{version}-OSX.dmg"
   name 'Oracle VirtualBox'
   homepage 'https://www.virtualbox.org/wiki/Testbuilds'
   license :gpl
+
+  auto_updates true
 
   pkg 'VirtualBox.pkg'
   binary '/Applications/VirtualBox.app/Contents/MacOS/VBoxManage'
@@ -13,4 +20,10 @@ cask 'virtualbox-beta' do
 
   uninstall script:  { executable: 'VirtualBox_Uninstall.tool', args: %w[--unattended] },
             pkgutil: 'org.virtualbox.pkg.*'
+
+  zap delete: [
+                '/Library/Application Support/VirtualBox',
+                '~/Library/VirtualBox',
+              ],
+      rmdir:  '~/VirtualBox VMs'
 end
