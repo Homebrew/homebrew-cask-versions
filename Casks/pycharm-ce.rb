9dcd424c71d51fced72f54a0b9a703a6f7d6292c
Jerzerak <jerzerak@gmail.com>
2016-05-29 03:23:00 -0400

    PyCharm CE 2016.1.4 (#2109)
    - Add `conflicts_with` stanza
    - Add `uninstall` stanza
    - Add `zap` stanza

--- Casks/pycharm-ce.rb
+++ Casks/pycharm-ce.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-ce' do
-  version '2016.1.3'
-  sha256 '3da97fd5b103d602fafd433549d43a6a6503ce9cfeb6be34da8ffe26a6eb0267'
+  version '2016.1.4'
+  sha256 '6c535f5a17a1d90a65c4e656b41d335f71d58f9014f79fb5e20255f9c3d6c75c'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version}.dmg"
   name 'Jetbrains PyCharm Community Edition'
@@ -8,5 +8,17 @@ cask 'pycharm-ce' do
   homepage 'https://www.jetbrains.com/pycharm'
   license :apache
 
+  conflicts_with cask: 'pycharm-ce-eap'
+
   app 'PyCharm CE.app'
+
+  uninstall delete: '/usr/local/bin/charm'
+
+  zap delete: [
+                "~/.PyCharm#{version.major_minor}",
+                "~/Library/Caches/PyCharm#{version.major_minor}",
+                "~/Library/Preferences/PyCharm#{version.major_minor}",
+                "~/Library/Logs/PyCharm#{version.major_minor}",
+                "~/Library/Application Support/PyCharm#{version.major_minor}",
+              ]
 end
