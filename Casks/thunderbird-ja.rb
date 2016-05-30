f5100ccc4e24167dcf0b5c16450d3faffb08b18a
mahori <mahori@users.noreply.github.com>
2016-05-31 08:23:55 +0900

    updated thunderbird-localized (45.1.1) (#2135)

--- Casks/thunderbird-ja.rb
+++ Casks/thunderbird-ja.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-ja' do
-  version '45.1.0'
-  sha256 'c8ce6165c80573214ef8b3b4c857c57ed06b7efe348a4c73e4d59cf9f4edb69d'
+  version '45.1.1'
+  sha256 '34fabe2e171d3a5df7807d7525838585940e5b271da9dd8ef48963f214ff3723'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=ja-JP-mac"
   name 'Mozilla Thunderbird'
--- Casks/thunderbird-zh-cn.rb
+++ Casks/thunderbird-zh-cn.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-zh-cn' do
-  version '45.1.0'
-  sha256 'baec64fbc422e761b01487a981c00dd1c4743af64c59bff3c031eadf23687618'
+  version '45.1.1'
+  sha256 '27b3a519c08847187b1a4f7b02ae2316cf72a5cb6598ec01bb53b527ee17edef'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=zh-CN"
   name 'Mozilla Thunderbird'
