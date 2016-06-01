06ac685641a821a11f29c14a6149538079fe7da2
Philipp Weißmann <github@philipp-weissmann.de>
2016-06-01 17:06:37 +0200

    Adds omnigraffle-5 (#2143)

---
+++ Casks/omnigraffle5.rb
@@ -0,0 +1,13 @@
+cask 'omnigraffle5' do
+  version '5.4.4'
+  sha256 '7bcc64093f46bd4808b1a4cb86cf90c0380a5c5ffffd55ce8f742712818558df'
+
+  url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.6/OmniGraffle-#{version}.dmg"
+  name 'OmniGraffle 5'
+  homepage 'https://www.omnigroup.com/omnigraffle/'
+  license :commercial
+
+  app 'OmniGraffle 5.app'
+
+  zap delete: '~/Library/Application Support/The Omni Group/OmniGraffle'
+end
