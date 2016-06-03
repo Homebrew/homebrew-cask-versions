950c0e423912228285b7eab094a5626b3bfaa676
Jerzerak <jerzerak@gmail.com>
2016-06-03 05:45:53 -0400

    IntelliJ IDEA EAP 162.646.4 (#2151)

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-eap' do
-  version '162.426.1'
-  sha256 '0a0d22076e80b02c8113d8b407896e7316920e7924ba1301f8db37ec5fc03c79'
+  version '162.646.4'
+  sha256 '572ec5b3d7d8961ce75c2d54573cd50c51f2dd70ab369bce3086bc3b347e7fd8'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
   name 'IntelliJ IDEA EAP'
