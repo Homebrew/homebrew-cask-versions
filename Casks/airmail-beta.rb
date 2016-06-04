daa4db539632f28e32567540a72d4e8c1e6efbb0
Maurice Kelly <osv-github@chatswood.org.uk>
2016-06-04 15:45:31 +0100

    Update airmail-beta to version 3.0,369
    This commit updates the version, checkpoint and sha256 stanzas.

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,11 +1,11 @@
 cask 'airmail-beta' do
-  version '3.0,368'
-  sha256 'b1da1a93f570c78e3507b75518b18d4f010fcd915b3be7477b0afc460801ae63'
+  version '3.0.0.369,248'
+  sha256 '5f93aa3f6c5f34a31002a2071c8b4f8948958cea4236a91ca97aeec46f98a037'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
-  url 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/247?format=zip&'
+  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04',
-          checkpoint: '8a87130261608652a570838658fe20503d580407c704fbcb885f49d661e290e1'
+          checkpoint: 'dc31be4a6b08bfd479186e6aff447be49d0e76f41bd82c0ae150d75bd8f49dff'
   name 'AirMail'
   homepage 'http://airmailapp.com/beta/'
   license :commercial
