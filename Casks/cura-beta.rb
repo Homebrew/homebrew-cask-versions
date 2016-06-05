2d291b4eb45ca3fc87b823f8f6714fe7b67d6516
Michael Ford <fanquake@users.noreply.github.com>
2016-06-05 14:11:28 +0800

    cura beta 2.1.1

--- Casks/cura-beta.rb
+++ Casks/cura-beta.rb
@@ -1,6 +1,6 @@
 cask 'cura-beta' do
-  version '2.1.0'
-  sha256 '074a50f06be491de79e1bdb37669a6923d6f99390d9d7e62bf400a3d7913e0b5'
+  version '2.1.1'
+  sha256 '7d9e2da7f561bb443ea13b12c5d333360c4635ee625681a3d65833da7740bc1d'
 
   url "https://software.ultimaker.com/Cura_open_beta/Cura-#{version}-Darwin.dmg"
   name 'Cura'
