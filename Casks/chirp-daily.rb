f125530539915d5c07bd862fb9339658af515b9d
John Sockwell <jsockwell@spies.ws>
2016-06-06 15:04:16 -0400

    Update chirp-daily to v20160604

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,6 +1,6 @@
 cask 'chirp-daily' do
-  version '20160601'
-  sha256 '6914564ac65f7b66d8601dba752d82f42d4dda66bb6c4528db0bdd4c40967cca'
+  version '20160604'
+  sha256 '87ff73fc21a151b59d87400cc6fe9d860fdb7d702e2ae7a754b6744b089769bc'
 
   url "http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
