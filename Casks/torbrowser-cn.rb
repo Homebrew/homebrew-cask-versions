457ca138c8c61113a5e7d308b7a0fe6893bbbae9
Victor Popkov <victor@popkov.me>
2016-06-08 01:17:21 +0300

    torbrowser-cn.rb: upgrade to 6.0.1

--- Casks/torbrowser-cn.rb
+++ Casks/torbrowser-cn.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-cn' do
-  version '6.0'
-  sha256 '0e9b2da8b4b24c576cf977e96c107f50be11f73defcbcbf9a72d95bd6cb3e8bb'
+  version '6.0.1'
+  sha256 '8b95b7dc2e76ce924f93446ae60fd28f3911fbc2567a99eb1d66b03be28ce1a4'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_zh-CN.dmg"
   name 'Tor Browser'
