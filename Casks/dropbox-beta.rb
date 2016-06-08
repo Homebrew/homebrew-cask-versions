e777c9b9419d624482c19b7bcff6e1b77b9ee11b
Sidney <sidney.bofah@googlemail.com>
2016-06-08 07:31:48 +0200

    Update dropbox-beta.rb
    https://www.dropboxforum.com/hc/en-us/community/posts/206873686-Beta-Build-5-3-20

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,10 +1,10 @@
 cask 'dropbox-beta' do
-  version '5.3.18'
-  sha256 'a762c0ea383176ecacdee173f3dd96912c15ac84418469afaf88d8fc54f678e4'
+  version '5.3.20'
+  sha256 '354a9e70923c0f0a9b0553dbe4925596d663d9c465f661add72d1dde2f6caa40'
 
-  url "https://dl.dropboxusercontent.com/u/17/Dropbox%20#{version}.dmg"
+  url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
   name 'Dropbox'
-  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/206833706-Beta-Build-5-3-18'
+  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/206873686-Beta-Build-5-3-20'
   license :gratis
 
   app 'Dropbox.app'
