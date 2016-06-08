39537b109638109fe84cb8086e3b9b24a826f518
Tom Janson <tom.janson@rwth-aachen.de>
2016-06-08 21:53:16 +0200

    updated firefox-esr-zh-cn (45.2.0)

--- Casks/firefox-esr-zh-cn.rb
+++ Casks/firefox-esr-zh-cn.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr-zh-cn' do
-  version '45.0'
-  sha256 '4310cf16c2b51b8a7f1bcd1fdd6db00f64a505f6f16c9ba7ead8722d08452ccf'
+  version '45.2.0'
+  sha256 '5b9aa224c4bfa3bf960b62a0a1bba72d81b46cc95f1c09454549adaa4681a02f'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/zh-CN/Firefox%20#{version}esr.dmg"
   name 'Mozilla Firefox'
