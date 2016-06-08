d01800b3441239ba6b93d3da2f962c46fe0a9846
Tom Janson <tom.janson@rwth-aachen.de>
2016-06-08 21:49:15 +0200

    updated firefox-esr (45.2.0)

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr' do
-  version '45.1.1'
-  sha256 '0f3b42692237456af0c8989ef51f7487a462380c67adbe4c130ab253d5226f51'
+  version '45.2.0'
+  sha256 '789e708ca817ceccfd9075826f16b95023752be0a868abe491c3dc7875e7926c'
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
   name 'Mozilla Firefox'
