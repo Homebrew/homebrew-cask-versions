045ad3a9e7f37160214e18b36b09fd92b1dd5e62
Vítor Galvão <info@vitorgalvao.com>
2016-06-14 02:07:56 +0100

    Create google-chrome-dev.rb

---
+++ Casks/google-chrome-dev.rb
@@ -0,0 +1,26 @@
+cask 'google-chrome-dev' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://dl.google.com/chrome/mac/dev/googlechrome.dmg'
+  name 'Google Chrome'
+  homepage 'https://www.google.com/chrome/browser/desktop/index.html?extra=devchannel'
+  license :gratis
+
+  app 'Google Chrome.app'
+
+  zap delete: [
+                '~/Library/Application Support/Google/Chrome',
+                '~/Library/Caches/Google/Chrome',
+                '~/Library/Caches/com.google.Chrome',
+                '~/Library/Caches/com.google.Chrome.helper.EH',
+                '~/Library/Caches/com.google.Keystone.Agent',
+                '~/Library/Caches/com.google.SoftwareUpdate',
+                '~/Library/Google/GoogleSoftwareUpdate',
+                '~/Library/Logs/GoogleSoftwareUpdateAgent.log',
+              ],
+      rmdir:  [
+                '~/Library/Caches/Google',
+                '~/Library/Google',
+              ]
+end
