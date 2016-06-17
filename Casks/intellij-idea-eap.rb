afd43ff50c5f453fd50b1ca10cfc07983283302a
Maciej Matys <metyl@users.noreply.github.com>
2016-06-18 00:26:11 +0200

    Add major_minor to intellij-idea-eap version
    This also fixes issues with broken zap stanza.

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,8 +1,8 @@
 cask 'intellij-idea-eap' do
-  version '162.917.19'
+  version '2016.2-162.917.19'
   sha256 'd9db67c434e81a262e2443ed020155766869a163e27ab792d86252467bbd112f'
 
-  url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.sub(%r{.*?-}, '')}.dmg"
   name 'IntelliJ IDEA EAP'
   homepage 'https://confluence.jetbrains.com/display/IDEADEV/IDEA+2016.2+EAP'
   license :commercial
