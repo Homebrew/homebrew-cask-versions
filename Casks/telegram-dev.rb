60b4c2de3841f17b7e47294b96837e84a65c27c2
Yurii Kolesnykov <yurikoles@gmail.com>
2016-06-18 13:11:38 +0300

    updated telegram-dev (0.9.51) (#2214)

--- Casks/telegram-dev.rb
+++ Casks/telegram-dev.rb
@@ -1,6 +1,6 @@
 cask 'telegram-dev' do
-  version '0.9.49'
-  sha256 'b70eaaf50674b5752b044bcbce3855de314c0e00e25a8e2f530658beeb324315'
+  version '0.9.51'
+  sha256 '7d898a6ddc2c9a2722d37867d22b422d63cf218f721dbb8942d67696b2f6aadd'
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   name 'Telegram'
