527f7c18ca6b96e98aaa61b960d182d4011c772f
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-06-21 13:08:10 +0800

    updated thunderbird-beta-uk (47.0b2) (#2222)

--- Casks/thunderbird-beta-uk.rb
+++ Casks/thunderbird-beta-uk.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-beta-uk' do
-  version '45.2b1'
-  sha256 '6c379f61ae6dcc2baefbe3d48f1abba77f67680cdf1fb31d2ad61f261bead7f7'
+  version '47.0b2'
+  sha256 'aab26ebb9f1d4f16ca77f5f1651d8e739372e11922be916acb314ce25156a38e'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=uk"
   name 'Mozilla Thunderbird'
