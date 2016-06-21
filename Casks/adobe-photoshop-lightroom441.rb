d4af04857da676d71b63f9f65915ae542e7ad6c9
Elliott Hilaire <elliott.hilaire@gmail.com>
2016-06-21 16:19:00 +0100

    Add Adobe Photoshop Lightroom 4.4.1 (#2219)
    * Add Adobe Photoshop Lightroom 4.4.1
    
    * Refactor Adobe Lightroom 4
    
    Use `version.major` and `version.dots_to_underscores`
    
    * Refactor Adobe Lightroom 5
    
    Use `verson.major` and `version.dots_to_underscores`

--- Casks/adobe-photoshop-lightroom441.rb
+++ Casks/adobe-photoshop-lightroom441.rb
@@ -1,6 +1,6 @@
-cask 'adobe-photoshop-lightroom571' do
-  version '5.7.1'
-  sha256 '155a91e2c90927a05ccaa244a99fed4784fa7cf26d08c634f5f111629f6b0418'
+cask 'adobe-photoshop-lightroom441' do
+  version '4.4.1'
+  sha256 '6d703aa5ea0022141c88746829e80c60f1169d746022655937c4196a6ca35dad'
 
   url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.major}.x/Lightroom_#{version.major}_LS11_mac_#{version.dots_to_underscores}.dmg"
   name 'Adobe Photoshop Lightroom'
