c96f44f9a789136104af73482494911ba0497000
Victor Popkov <victor@popkov.me>
2016-06-22 01:50:24 +0300

    torbrowser-vi.rb: upgrade to 6.0.2

--- Casks/torbrowser-vi.rb
+++ Casks/torbrowser-vi.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-vi' do
-  version '6.0.1'
-  sha256 '0ee02d8313acad5b7a228261354e3adcaddbb5148284fbb7c036468043472d0a'
+  version '6.0.2'
+  sha256 'd162f201efa3d9c5fd510848e48256172621a8d37c1e9719e6d6d8a8145670be'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_vi.dmg"
   name 'Tor Browser'
