b23fdd542201035b1644e3e9dea77171e995a3c5
Elliott Hilaire <elliott@Kallisti.local>
2016-06-21 09:56:43 +0100

    Refactor Adobe Lightroom 5
    Use `verson.major` and `version.dots_to_underscores`

--- Casks/adobe-photoshop-lightroom571.rb
+++ Casks/adobe-photoshop-lightroom571.rb
@@ -2,19 +2,19 @@ cask 'adobe-photoshop-lightroom571' do
   version '5.7.1'
   sha256 '155a91e2c90927a05ccaa244a99fed4784fa7cf26d08c634f5f111629f6b0418'
 
-  url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.to_i}.x/Lightroom_#{version.to_i}_LS11_mac_#{version.gsub('.', '_')}.dmg"
+  url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.major}.x/Lightroom_#{version.major}_LS11_mac_#{version.dots_to_underscores}.dmg"
   name 'Adobe Photoshop Lightroom'
   homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
   license :commercial
 
-  pkg "Adobe Photoshop Lightroom #{version.to_i}.pkg"
+  pkg "Adobe Photoshop Lightroom #{version.major}.pkg"
 
-  uninstall pkgutil: "com.adobe.Lightroom#{version.to_i}",
-            quit:    "com.adobe.Lightroom#{version.to_i}",
-            delete:  "/Applications/Adobe Photoshop Lightroom #{version.to_i}.app"
+  uninstall pkgutil: "com.adobe.Lightroom#{version.major}",
+            quit:    "com.adobe.Lightroom#{version.major}",
+            delete:  "/Applications/Adobe Photoshop Lightroom #{version.major}.app"
 
   zap       delete: [
                       '~/Library/Application Support/Adobe/Lightroom',
-                      "~/Library/Preferences/com.adobe.Lightroom#{version.to_i}.plist",
+                      "~/Library/Preferences/com.adobe.Lightroom#{version.major}.plist",
                     ]
 end
