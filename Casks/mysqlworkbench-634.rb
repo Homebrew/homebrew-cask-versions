f640dcfcb07399a664dedee0e7ec232de5546097
小野 詠子 <onoeico@gmail.com>
2016-06-21 11:33:18 +0900

    Add a new version of the Cask "mysqlworkbench".
        * MySQL Workbench 6.3.4

---
+++ Casks/mysqlworkbench-634.rb
@@ -0,0 +1,22 @@
+cask 'mysqlworkbench-634' do
+  version '6.3.4'
+  sha256 'af214391dfc9c6bdb7640bf286732ce7d4500721906d9a0ad3b107db8f15e57a'
+
+  url "https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community-#{version}-osx-x86_64.dmg"
+  name 'MySQL Workbench'
+  homepage 'https://www.mysql.com/products/workbench'
+  license :gpl
+  gpg "#{url}.asc",
+      key_id: '8c718d3b5072e1f5'
+
+  app 'MySQLWorkbench.app'
+
+  zap delete: [
+                '~/Library/Application Support/MySQL/Workbench',
+                '~/Library/Preferences/com.oracle.mysql.workbench.plist',
+                '~/Library/Preferences/com.oracle.MySQLWorkbench.plist',
+                '~/Library/Saved Application State/com.oracle.mysql.workbench.savedState',
+                '~/Library/Saved Application State/com.oracle.MySQLWorkbench.savedState',
+                '~/Library/Caches/com.oracle.mysql.workbench',
+              ]
+end
