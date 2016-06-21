6c04a0a6fd3481fbd4c16753974d464a7627a56d
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-06-21 13:08:03 +0800

    updated thunderbird-beta-ru (47.0b2) (#2221)

--- Casks/thunderbird-beta-ru.rb
+++ Casks/thunderbird-beta-ru.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-beta-ru' do
-  version '45.2b1'
-  sha256 '50bbc2843de76a17566b3af1aec3af0349891b31dd3fa542eb206644acfc5cba'
+  version '47.0b2'
+  sha256 '66fe6a6369c4fa31b4e3eb84967969e194b24c401050d6a3c89a3876ac2c491b'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=ru"
   name 'Mozilla Thunderbird'
