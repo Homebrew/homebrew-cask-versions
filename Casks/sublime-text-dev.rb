451eac8ab437bdfccd397fbab9d335d0e1710494
Victor Popkov <victor@popkov.me>
2016-06-22 20:20:28 +0300

    updated sublime-text-dev (3117)

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,10 +1,10 @@
 cask 'sublime-text-dev' do
-  version '3113'
-  sha256 'e9458769440d582f0a8bc3dd686096f095be9e5b4ca8ff5bee2ec53271111f84'
+  version '3117'
+  sha256 'a8db08bfa6b3ce60d9b18861f96aadedfe2ffbcbe2b2a30c8cbc3e81aa084b97'
 
   url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/3/dev/appcast_osx.xml',
-          checkpoint: '63695de8650790a4a0fd0bf748f73d5d77dc8c5a19d337023edc18265060f717'
+          checkpoint: 'd724256e6f3a0686ff39cedc66e8d98db3198794857023b340ba171246fc10e5'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/3dev'
   license :closed
