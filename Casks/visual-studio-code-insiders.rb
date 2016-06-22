4f69f2d7377f3452e546df108166ba7b8d4de760
Michael Ford <fanquake@users.noreply.github.com>
2016-06-22 11:44:27 +0800

    Visual Studio Code Insider 1.3.0

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code-insiders' do
-  version '1.1.0,1f0657703be5609d70088b0385bc27db8cc03a41'
-  sha256 'ae207e4e3e5656c4f9f3b3e5769f74ee40a59225af6a72f69d29279f92fcf5f8'
+  version '1.3.0,5474147bb83618975409dad7d8aa96151d7d1ea1'
+  sha256 '05e55c2e130315358dd9ce3c1c039e4bb7e07b4c6edcebe0acf30bc639a81aef'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
