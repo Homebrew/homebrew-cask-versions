c30ee600335f5bc82ebf9a33024b27439abc3840
Victor Popkov <victor@popkov.me>
2016-06-24 23:13:00 +0300

    updated sketch-beta (39,31494) (#2243)

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,11 +1,11 @@
 cask 'sketch-beta' do
-  version '39,31443'
-  sha256 '0f8d7e5235a48770cee9592b11b8d05157b551aff5ce38881a36e312f398af1f'
+  version '39,31494'
+  sha256 '40ae04bac4d6dab515f5130b3f5552b7b9746e2a9b6ad667177975c36c6cc9f0'
 
   # hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b',
-          checkpoint: 'c7dafefaa44404836b9a7601c32386fa611d83e1fdd65f51a6970c44e5617655'
+          checkpoint: 'd915a73e05f96604d3a8715b5472b8b99ba381364bee1d44fb8f49fdcc474643'
   name 'Sketch'
   homepage 'http://bohemiancoding.com/sketch/beta/'
   license :commercial
