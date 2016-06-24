86da9eea4e66e0708579a912a0d5a3620b971d41
RJ Hsiao <rj19860725@gmail.com>
2016-06-25 03:59:17 +0800

    updated iterm2-beta (3.0.3) (#2242)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -2,7 +2,7 @@ cask 'iterm2-beta' do
   version '3.0.3'
   sha256 '4987591f76eb69ee9e6aa8450b0121275045bd5d7c3f3c767a125b2fb28370ae'
 
-  url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}-preview.zip"
+  url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
   license :gpl
