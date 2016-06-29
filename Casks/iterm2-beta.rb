3923376d892ac3e32389bd9c65069a1f306a2500
Aditya Dalal <adidalal@users.noreply.github.com>
2016-06-29 01:38:38 -0400

    updated iterm2-beta (3.0.3) (#2267)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-beta' do
   version '3.0.3'
-  sha256 '4987591f76eb69ee9e6aa8450b0121275045bd5d7c3f3c767a125b2fb28370ae'
+  sha256 '9073c757b53de6f070be3955a214af9c21548d1dff1f41e74e447b9be726f119'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
