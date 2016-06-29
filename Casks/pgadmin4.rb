df9376a9daee297e2d232f912e115c4b883333ba
Andrey Novikov <envek@envek.name>
2016-06-29 19:28:23 +0300

    pgAdmin4 1.0-beta2

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -1,6 +1,6 @@
 cask 'pgadmin4' do
-  version '1.0-beta1'
-  sha256 'ba004dcedff2a5d9bcc02ba50b3b0b429cd05a0f2ceba962a8afbea9120448f2'
+  version '1.0-beta2'
+  sha256 'b6523c47e552e64e3112f9bb9902faf0a21905023f491de8997658aa876ca077'
 
   # postgresql.org is the official download host per the vendor homepage
   url "https://ftp.postgresql.org/pub/pgadmin3/pgadmin4/v#{version}/osx/pgadmin4-#{version}.dmg"
