4e54f978783e89849bc79273960d4e50569173ef
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-06-29 13:39:42 +0800

    updated firefox-beta-zh-cn (48.0b4) (#2263)

--- Casks/firefox-beta-zh-cn.rb
+++ Casks/firefox-beta-zh-cn.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-zh-cn' do
-  version '48.0b1'
-  sha256 '554ce2ef1c5472a7003e6b459991f39f706b49d89cac3b61d8fd41ef3cd80b7f'
+  version '48.0b4'
+  sha256 '2891be5b82bee7dec7ac22ff6cdef6a3ee0cde9a7f35291b21337efb82adbd0b'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
   name 'Mozilla Firefox'
