c1d419b0555e7f33fb6fc4f5e73f303f03e3fa94
mahori <mahori@users.noreply.github.com>
2016-06-29 09:35:01 +0900

    updated firefox-gl (47.0.1) (#2248)

--- Casks/firefox-gl.rb
+++ Casks/firefox-gl.rb
@@ -1,6 +1,6 @@
 cask 'firefox-gl' do
-  version '47.0'
-  sha256 'c72964f29dc6fcc7beef9ae8f0385eb39d4658af95380ed15e8db52685eb6af9'
+  version '47.0.1'
+  sha256 '91061c4b93b5ac6174f353959cf909802a8f804e1fd44f38749cdd6993850153'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=gl"
   name 'Mozilla Firefox'
