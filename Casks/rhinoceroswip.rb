593997918aa5a7ad4f5117aa17a6a84f324659b5
Carlos Precioso <cprecioso@users.noreply.github.com>
2016-07-01 12:11:07 +0200

    Update rhinoceroswip to 5C249w (#2273)

--- Casks/rhinoceroswip.rb
+++ Casks/rhinoceroswip.rb
@@ -1,12 +1,22 @@
 cask 'rhinoceroswip' do
-  version '5C131w'
-  sha256 'a550976da9e8272240723451c46d2aaf7e76bb26b6d5539be224e5c81a39e53a'
+  version '5C249w'
+  sha256 'bb58586441ff7c6c122178189dcb7668478f61df36f39287fc62975b52953c23'
 
   # mcneel.com was verified as official when first introduced to the cask
   url "https://files.mcneel.com/Releases/Rhino/5.0/Mac/RhinoWIP_#{version}.dmg"
+  appcast 'https://files.mcneel.com/rhino/5.0/mac/5CwipUpdates.xml',
+          checkpoint: '03da618fc2af5b2357da7b0160ddce9816e1b4a1b4729bdb576a8959c758fd60'
   name 'Rhinoceros WIP'
   homepage 'http://www.rhino3d.com/download/rhino-for-mac/5/wip'
   license :commercial
 
+  auto_updates true
+
   app 'RhinoWIP.app'
+
+  zap delete: [
+                '~/Library/Preferences/com.mcneel.rhinoceros.plist',
+                '~/Library/Caches/com.mcneel.rhinoceros',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.mcneel.rhinoceros.sfl',
+              ]
 end
