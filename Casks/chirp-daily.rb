1cd8538efddba859465c5891cfcda4846bf02457
Aditya Dalal <adidalal@users.noreply.github.com>
2016-07-02 10:26:29 -0400

    updated chirp-daily (20160702)

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,8 +1,8 @@
 cask 'chirp-daily' do
-  version '20160611'
-  sha256 'fc365195cc6b8b92fc2590ca616dc47cd9abe56c6f815c60f61cf19cddb7688a'
+  version '20160702'
+  sha256 '3129ea9c018453e874b16a7fd209214977555f92b8b7663bb0bd2775e3119b08'
 
-  url "http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
+  url "http://trac.chirp.danplanet.com/chirp_daily/daily-#{version}/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
   homepage 'http://chirp.danplanet.com/projects/chirp/wiki/Home'
   license :oss
