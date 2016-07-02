8a1505e1c0e8a133fb053f5ed5a9cbf281bcf448
blogabe <blogabe@rajab.us>
2016-07-02 15:49:32 -0700

    1password 6.5 beta 6 (#2281)

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask '1password-beta' do
-  version '6.5.BETA-6'
-  sha256 '4b7800b6d3af2086a0f70ae18dd045e26c26d867594f7eaac076726ac5975f83'
+  version '6.5.BETA-7'
+  sha256 'b54b1551113eeed1ed4106aa5ad94b802c6d0a7cb60c04756e2875c6e1b13284'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
