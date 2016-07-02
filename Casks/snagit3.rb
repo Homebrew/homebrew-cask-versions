1f0e7f388e69f74e858f85522fb806f77a223bb6
Travis Carden <TravisCarden@users.noreply.github.com>
2016-07-02 06:07:39 -0500

    Add Snagit.app v3.3.7. (#2277)

---
+++ Casks/snagit3.rb
@@ -0,0 +1,11 @@
+cask 'snagit3' do
+  version '3.3.7'
+  sha256 '4a0f3f9209011c70d5e3698e0b75d6cff73c0fb98d61d90aedd5884b0463ec3a'
+
+  url 'http://download.techsmith.com/snagitmac/enu/337/snagit.dmg'
+  name 'Snagit'
+  homepage 'https://www.techsmith.com/snagit.html'
+  license :commercial
+
+  app 'Snagit.app'
+end
