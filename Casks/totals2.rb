b6ea2f988f12f85b47242c49635d436c502c0032
Ezbon Jacob <ezjacob@gmail.com>
2016-07-02 09:59:00 -0400

    added Totals v2

---
+++ Casks/totals2.rb
@@ -0,0 +1,11 @@
+cask 'totals2' do
+  version '2.2.14'
+  sha256 'f731d549502cd5f3d6402783d2dfed4745930280f6af2fc9eba6e9aae94034ee'
+
+  url "http://www.kedisoft.com/dl/totals/Totals-#{version}.dmg"
+  name 'Totals'
+  homepage 'http://www.kedisoft.com/totals/'
+  license :commercial
+
+  app 'Totals.app'
+end
