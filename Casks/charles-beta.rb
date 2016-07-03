0cb70c1716e62e2b2d08b6aee236b8548e108867
Michael Ford <fanquake@users.noreply.github.com>
2016-07-03 17:17:41 +0800

    Charles 3.11.6b2

--- Casks/charles-beta.rb
+++ Casks/charles-beta.rb
@@ -1,6 +1,6 @@
 cask 'charles-beta' do
-  version '3.11.5b5'
-  sha256 'a51095421e0c742b466a082725072e52fe8554ca96ba2ea311005f766469894b'
+  version '3.11.6b2'
+  sha256 'd27e33aefaa4fd627870ca84a44309a7d95ac9b4ddbf99b43d16a2f6dd835bfc'
 
   url "https://www.charlesproxy.com/assets/release/#{version.gsub(%r{b\d$}, '')}/charles-proxy-#{version}.dmg"
   name 'Charles'
