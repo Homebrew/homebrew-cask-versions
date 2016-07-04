e952f0046239621f560ae475bfe5082c110a3567
mahori <mahori@users.noreply.github.com>
2016-07-04 19:11:42 +0900

    updated thunderbird-it (45.2.0)

--- Casks/thunderbird-it.rb
+++ Casks/thunderbird-it.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-it' do
-  version '45.1.1'
-  sha256 'c9547c71d3288bcf1d517db3f57360dc06b2d980993ee7a86155abbde32651aa'
+  version '45.2.0'
+  sha256 '3b7bbced9617b59bb23e35d9e84077b17e7e90020fd575888b9f3d66dce8d3e2'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=it"
   name 'Mozilla Thunderbird'
