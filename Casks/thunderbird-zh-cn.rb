7ae0338f753bb227fd12c0695408a25e522222b0
mahori <mahori@users.noreply.github.com>
2016-07-04 20:50:15 +0900

    updated thunderbird-zh-cn (45.2.0) (#2294)

--- Casks/thunderbird-zh-cn.rb
+++ Casks/thunderbird-zh-cn.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-zh-cn' do
-  version '45.1.1'
-  sha256 '27b3a519c08847187b1a4f7b02ae2316cf72a5cb6598ec01bb53b527ee17edef'
+  version '45.2.0'
+  sha256 'd6d5b0a2856b4d2328af7d7ed93388cbb363e91e53be7879ab7d2965811ef41b'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=zh-CN"
   name 'Mozilla Thunderbird'
