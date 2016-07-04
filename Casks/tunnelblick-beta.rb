014197ddf9c69f82b527644447a79f4b71056283
R. Cody Erben <erben22@mtnaircomputer.net>
2016-07-04 01:29:23 -0600

    updated tunnelblick-beta (3.6.5beta04_build_4565)

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,10 +1,8 @@
 cask 'tunnelblick-beta' do
-  version '3.6.4beta04_build_4562'
-  sha256 'e41dfef1f480ec5be5fb25641e7a4c720915c4d2752f3d60eebf70f893d40f7b'
+  version '3.6.5beta04_build_4565'
+  sha256 'a27adf5fae695263b8a698d2f5b75f191fe70b65d07af99f57bfdde29cb904d5'
 
   url "https://tunnelblick.net/release/Tunnelblick_#{version}.dmg"
-  appcast 'https://www.tunnelblick.net/appcast.rss',
-          checkpoint: '91e22e98032e0143efb02618a5ea3cb1927c40d7e877bc609b2287da09da7a5c'
   name 'Tunnelblick'
   homepage 'https://tunnelblick.net'
   license :gpl
