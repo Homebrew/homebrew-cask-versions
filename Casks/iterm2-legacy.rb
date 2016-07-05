52a8f6a347ea7f3c1c1e4fbc93397221c676ddbf
Steve Huff <steve.huff@runkeeper.com>
2016-07-05 11:59:23 -0400

    feedback from @adidalal
    * removed appcast
    * added conflict
    * use `dots_to_underscores()`

--- Casks/iterm2-legacy.rb
+++ Casks/iterm2-legacy.rb
@@ -3,14 +3,13 @@ cask 'iterm2-legacy' do
   version '2.1.4'
   sha256 '1062b83e7808dc1e13362f4a83ef770e1c24ea4ae090d1346b49f6196e9064cd'
 
-  url "https://iterm2.com/downloads/stable/iTerm2-#{version.gsub('.', '_')}.zip"
-  appcast 'https://iterm2.com/appcasts/final.xml',
-          checkpoint: '6f840c00431993ab1151c01facfd8d4dfbb13a94589657a6cb5d72505db3f448'
+  url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
   license :gpl
 
   auto_updates true
+  conflicts_with cask: 'iterm2'
   depends_on macos: '>= :lion'
   depends_on arch: :intel
 
