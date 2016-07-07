504dbc5ea8749244fb62f122510de69fac49aefb
Everton Moreth <everton.moreth@wpensar.com.br>
2016-07-07 11:13:48 -0300

    Updated webstorm

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,15 +1,15 @@
 cask 'webstorm-eap' do
-  version '2016.2,162.1120.12'
-  sha256 'b6ecfd6e33c49fd4e7a85d73d37371ff3b078ab67fc6e2335c256e0581f58534'
+  version '2016.2,RC'
+  sha256 '2da54eca01306e2d88347b90f09ee4da98dbe6315e53c67df0a555fdb2751686'
 
-  url "https://download.jetbrains.com/webstorm/WebStorm-EAP-#{version.after_comma}.dmg"
+  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.before_comma}-#{version.after_comma}.dmg"
   name 'WebStorm EAP'
   homepage 'https://confluence.jetbrains.com/display/WI/WebStorm+EAP'
   license :commercial
 
   conflicts_with cask: 'webstorm'
 
-  app "WebStorm #{version.before_comma} EAP.app"
+  app "WebStorm.app"
 
   uninstall delete: '/usr/local/bin/wstorm'
 
