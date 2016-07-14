eca1412eb991a3d00d99bc9404e74906e1f751a2
Michael Ford <fanquake@users.noreply.github.com>
2016-07-14 19:50:26 +0800

    Tor Browser JA 6.0.2

--- Casks/torbrowser-ja.rb
+++ Casks/torbrowser-ja.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-ja' do
-  version '6.0.1'
-  sha256 'c317b4d964e47e889a36628e52c918ba33368f5f3492bd1ca3029a475e8d3aeb'
+  version '6.0.2'
+  sha256 '6580d555e826ac1bc70d0e0e7dfafaa6bfed131a068df95acdb47d694a9e21b8'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_ja.dmg"
   name 'Tor Browser'
