81427f402d35bedbcd2feebc99506d00ca7a4d7c
Michael Ford <fanquake@users.noreply.github.com>
2016-07-16 15:25:03 +0800

    Teleram Desktop Dev 0.9.57

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -1,11 +1,13 @@
 cask 'telegram-desktop-dev' do
-  version '0.9.55'
-  sha256 '0585d11681400956223c1c0fbdc62fecdda8f6aeca29eb9ed0cf7fd7e199806f'
+  version '0.9.57'
+  sha256 '05b76d616b6bb2f8f15f0b9aa4885537fcc65f54b72fba2ed05453524175fc76'
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   name 'Telegram'
   homepage 'https://tdesktop.com/'
   license :gpl
+
+  depends_on macos: '>= :mountain_lion'
 
   # Renamed to avoid conflict with telegram
   app 'Telegram.app', target: 'Telegram Desktop.app'
