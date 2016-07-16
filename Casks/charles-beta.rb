5f8230c31fdf624c12b41249226683507c7bb09a
Michael Ford <fanquake@users.noreply.github.com>
2016-07-16 15:44:52 +0800

    Charles Beta 3.11.6b3

--- Casks/charles-beta.rb
+++ Casks/charles-beta.rb
@@ -1,11 +1,13 @@
 cask 'charles-beta' do
-  version '3.11.6b2'
-  sha256 'd27e33aefaa4fd627870ca84a44309a7d95ac9b4ddbf99b43d16a2f6dd835bfc'
+  version '3.11.6b3'
+  sha256 '07960cf8da976fb9ac78673bcbcee32a8c8dbda40cb3f9da790da832b73c02aa'
 
   url "https://www.charlesproxy.com/assets/release/#{version.gsub(%r{b\d$}, '')}/charles-proxy-#{version}.dmg"
   name 'Charles'
   homepage 'https://www.charlesproxy.com/download/beta/'
   license :commercial
+
+  depends_on macos: '>= :lion'
 
   app 'Charles.app'
 
