8f0f2274a39c362f6856dce8e3d92f054d0e073b
Michael Ford <fanquake@users.noreply.github.com>
2016-07-16 15:10:31 +0800

    Beaker Electron 1.6-0-gb7c81a9

--- Casks/beaker-electron.rb
+++ Casks/beaker-electron.rb
@@ -1,6 +1,6 @@
 cask 'beaker-electron' do
-  version '1.5.1-0-g6e6bdd3'
-  sha256 '9ddd90bc780c183e263a61bccb2b7dc74b042b1589c800b5f3a0c6922a8ce48a'
+  version '1.6-0-gb7c81a9'
+  sha256 'd4c41c9341fc76e07b3f7733386bdd6cdaf111ab883d11cd7f5d2fdf2a0e04d8'
 
   # d299yghl10frh5.cloudfront.net was verified as official when first introduced to the cask
   url "https://d299yghl10frh5.cloudfront.net/beaker-notebook-#{version}-electron-mac.dmg"
