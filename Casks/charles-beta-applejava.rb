a1b23878291043edf61ecce0470511f426c826d7
Michael Ford <fanquake@users.noreply.github.com>
2016-07-16 15:45:40 +0800

    Charles Beta AppleJava 3.11.6b3

--- Casks/charles-beta-applejava.rb
+++ Casks/charles-beta-applejava.rb
@@ -1,6 +1,6 @@
 cask 'charles-beta-applejava' do
-  version '3.11.5b5'
-  sha256 'fd524f43a4979f3ef3fba54f3dca6f3635b11d8d9a793f73500ad7fe569dd5d2'
+  version '3.11.6b3'
+  sha256 '2f005c976fd4776835122e73349df65121bc6e3b3f775e395c855b3b14e9888b'
 
   url "https://www.charlesproxy.com/assets/release/#{version.gsub(%r{b\d$}, '')}/charles-proxy-#{version}-applejava.dmg"
   name 'Charles'
