82b154cde768bdd8dd91fd674b6bc36833553e13
zhuyue <zhuyue.sut@icloud.com>
2016-07-19 22:11:40 +0800

    Update iterm2-nightly.rb for only 20160719
    maybe, update every two days.

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,10 +1,10 @@
 cask 'iterm2-nightly' do
   version '3.0.3'
-  sha256 'f572f06354d12aec1240826fa6519860dfde7f5fea05b26425c166c21745ace8'
+  sha256 '3585a83abfdec1a54bd4ff61786099328aa3f3cae471227d54a2a98926e71b6f'
 
   url "http://www.iterm2.com/nightly/iTerm2-#{version.dots_to_underscores}.zip"
   appcast 'https://iterm2.com/appcasts/nightly.xml',
-          checkpoint: '66ef943cc1b62b61a76fad7ab73a261d1d0866af72442b4c4a9c809965816529'
+          checkpoint: '083908c06413f4aa9b12aa9945049401bf56e48e8755849c6a20d6b3288382f1'
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
   license :gpl
