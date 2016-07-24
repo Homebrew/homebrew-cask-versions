b1ab204a4a3a29263c4f7c8c9ee5e16820cc1451
Rick van Hattem <wolph@wol.ph>
2016-07-24 19:09:58 +0200

    Forgot to update the download url comment

--- Casks/pgadmin4.rb
+++ Casks/pgadmin4.rb
@@ -2,7 +2,7 @@ cask 'pgadmin4' do
   version '1.0-beta3'
   sha256 'b93bcae497d531cc870c3feb9dd5424b1f16793fdcceb03158bd07ac1a073e62'
 
-  # postgresql.org is the official download host per the vendor homepage
+  # ftp.postgresql.org was verified as official when first introduced to the cask
   url "https://ftp.postgresql.org/pub/pgadmin3/pgadmin4/v#{version}/osx/pgadmin4-#{version}.dmg"
   name 'pgAdmin4'
   homepage 'http://pgadmin.org'
