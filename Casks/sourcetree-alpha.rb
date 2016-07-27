d1ca6f69a0a48adc4f29d4ec8c887a0f74aad33d
Sidney <sidney.bofah@googlemail.com>
2016-07-28 01:49:23 +0200

    Add SourceTree Alpha Cask (#2350)

---
+++ Casks/sourcetree-alpha.rb
@@ -0,0 +1,37 @@
+cask 'sourcetree-alpha' do
+  version '2.4a1'
+  sha256 '976f37de466ad5621f3df1b7bfc54d9520cc135ba5c1fab8d0c4f7b10451d47f'
+
+  # atlassian.com was verified as official when first introduced to the cask
+  url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.zip"
+  appcast 'https://www.sourcetreeapp.com/update/SparkleAppcastAlpha.xml',
+          checkpoint: 'cb8a71de3ab44bd1e2026909e48a910373037cd820f7234022841b497bef0e7f'
+  name 'Atlassian SourceTree'
+  homepage 'https://www.sourcetreeapp.com/'
+  license :gratis
+
+  auto_updates true
+  depends_on macos: '>= 10.10'
+
+  app 'SourceTree Alpha.app'
+  binary "#{appdir}/SourceTree Alpha.app/Contents/Resources/stree"
+
+  postflight do
+    suppress_move_to_applications
+  end
+
+  uninstall launchctl: 'com.atlassian.SourceTreePrivilegedHelper2',
+            quit:      'com.torusknot.SourceTreeNotMAS'
+
+  zap delete: [
+                '~/Library/Application Support/SourceTree',
+                '~/Library/Caches/com.torusknot.SourceTreeNotMAS',
+                '~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist',
+                '~/Library/Preferences/com.torusknot.SourceTreeNotMAS.LSSharedFileList.plist',
+                '~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState',
+              ]
+
+  caveats do
+    files_in_usr_local
+  end
+end
