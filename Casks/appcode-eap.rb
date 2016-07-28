5e175bd343c08e89330fcf44b7c6529565567e88
Yurii Kolesnykov <yurikoles@gmail.com>
2016-07-28 20:48:16 +0300

    updated appcode-eap (162.1531.4)

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,6 +1,6 @@
 cask 'appcode-eap' do
-  version '162.1236.3'
-  sha256 '849b51b3915c80112ce85de99fc5b994e39b012031531bda3c6b969c66ada842'
+  version '162.1531.4'
+  sha256 'dcee0ceab8de20acaaab32d14afbee5ca4b509fdfc3d45ae0ecb42eb4bed11e5'
 
   url "https://download.jetbrains.com/objc/AppCode-#{version}.dmg"
   name 'AppCode EAP'
@@ -9,7 +9,7 @@ cask 'appcode-eap' do
 
   conflicts_with cask: 'appcode'
 
-  app 'AppCode 2016.2 EAP.app'
+  app 'AppCode 2016.2.1 EAP.app'
 
   zap delete: [
                 '~/Library/Preferences/AppCode2016.2',
