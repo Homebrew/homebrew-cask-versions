320532a0ede139628344ad366dd1fbb16f0a29a5
Michael Ford <fanquake@users.noreply.github.com>
2016-08-02 14:35:33 +0800

    Dropbox Beta 8.3.16

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,11 +1,11 @@
 cask 'dropbox-beta' do
-  version '7.3.29'
-  sha256 'b895827a9c0bce54a169048ae845a0602262c4fdff54b0c6cece8980ee579f44'
+  version '8.3.16'
+  sha256 '9bd1c4fd0abfc3e4a71a2180b051ff3c8344315c82317befbb6c7601044091b2'
 
   # dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
   name 'Dropbox'
-  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/206689743-Beta-Build-7-3-29'
+  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/209679306-Beta-Build-8-3-16'
   license :gratis
 
   app 'Dropbox.app'
