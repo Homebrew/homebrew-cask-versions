d632ff9640cb3d787a8e69299e15586cd220cdba
suin <suinyeze@gmail.com>
2016-08-02 18:39:13 +0900

    Follow app name brand name Airmail, not AirMail
    The brand name is "Airmail" and the original filename is "Airmail Beta.app" but not "AirMail Beta".

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -6,9 +6,9 @@ cask 'airmail-beta' do
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04',
           checkpoint: 'c6232721b90c6ffd5d759c5b9911ec196b19c21c07bae51b326bc4341a642bfa'
-  name 'AirMail'
+  name 'Airmail'
   homepage 'http://airmailapp.com/beta/'
   license :commercial
 
-  app 'AirMail Beta.app'
+  app 'Airmail Beta.app'
 end
