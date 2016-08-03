1d9fb3404fbca56b7fbb32ff25af1207df9f8826
mahori <mahori@users.noreply.github.com>
2016-08-03 09:27:20 +0900

    updated firefox-de (48.0)

--- Casks/firefox-de.rb
+++ Casks/firefox-de.rb
@@ -1,6 +1,6 @@
 cask 'firefox-de' do
-  version '47.0.1'
-  sha256 'ea2e0c6e9e33728cd720825ccd0b3af63f8ba5fe766eb6283cec07c52f59bd39'
+  version '48.0'
+  sha256 '8e7805a19d9b9f083bef4d215493acdce2785e1b10f1d557cc3b1cfcc7582427'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   name 'Mozilla Firefox'
