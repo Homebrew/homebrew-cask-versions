c48d39369b1e285206c15e9b60e20db2df994f66
Michael Ford <fanquake@users.noreply.github.com>
2016-08-03 15:35:02 +0800

    Webstorm EAP 2016.2.1

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'webstorm-eap' do
-  version '2016.2,RC'
-  sha256 '2da54eca01306e2d88347b90f09ee4da98dbe6315e53c67df0a555fdb2751686'
+  version '2016.2.1,RC'
+  sha256 'e68b5a699f0b8883ddfd279c68cb8f8cd7f54da2d24af236362cb6835f756608'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.before_comma}-#{version.after_comma}.dmg"
   name 'WebStorm EAP'
