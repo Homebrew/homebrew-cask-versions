e8ad8959c5764c58f8c99acea67b950c58de9b33
mahori <mahori@users.noreply.github.com>
2016-08-03 11:36:59 +0900

    updated firefox-ja (48.0) (#2385)

--- Casks/firefox-ja.rb
+++ Casks/firefox-ja.rb
@@ -1,6 +1,6 @@
 cask 'firefox-ja' do
-  version '47.0.1'
-  sha256 'ed499d5f5ebdfc73bed81d6823369fcbf4853eeabfe120fdaf52ef553351c8f2'
+  version '48.0'
+  sha256 '92b156f77116efe90ac9a3695ca7b3eaf2a47a7a37c458d4aaa6a1fafa72a5b2'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
   name 'Mozilla Firefox'
