6e7a35acdb2f0893059b4e990e81931cdeb652c0
Michael Ford <fanquake@users.noreply.github.com>
2016-08-03 10:41:55 +0800

    Firefox ESR 45.3.0

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr' do
-  version '45.2.0'
-  sha256 '789e708ca817ceccfd9075826f16b95023752be0a868abe491c3dc7875e7926c'
+  version '45.3.0'
+  sha256 '553bb7f6a07e938d2fcb3ce08e4ad04b8448ed7a38048b680103a08c5000cf3d'
 
   # mozilla.net was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
