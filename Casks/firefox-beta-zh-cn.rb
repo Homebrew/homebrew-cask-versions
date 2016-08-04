2222dd8af38b4c7ac094fa4f7db8e6828aa0763d
Tom Janson <tom.janson@rwth-aachen.de>
2016-08-04 23:56:50 +0200

    updated firefox-beta-zh-cn (49.0b1)

--- Casks/firefox-beta-zh-cn.rb
+++ Casks/firefox-beta-zh-cn.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-zh-cn' do
-  version '48.0b4'
-  sha256 '2891be5b82bee7dec7ac22ff6cdef6a3ee0cde9a7f35291b21337efb82adbd0b'
+  version '49.0b1'
+  sha256 '821edaefe799f1760c2475bac87764bff34d1e610fbd668700797294a8914bf2'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
   name 'Mozilla Firefox'
