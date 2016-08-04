72bc8a80be25344d12de68f927c0e1be5f90da51
Tom Janson <tom.janson@rwth-aachen.de>
2016-08-04 23:53:15 +0200

    updated firefox-beta-de (49.0b1)

--- Casks/firefox-beta-de.rb
+++ Casks/firefox-beta-de.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-de' do
-  version '48.0b4'
-  sha256 '72bb7423840396e0aef43d7972736a2568fe42d61d526db15d3a647ea39f0bf6'
+  version '49.0b1'
+  sha256 'ba7511ce45075a8af32b1ec5d8349e641e6ee71cf3b6c45d21161c27ad312379'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=de"
   name 'Mozilla Firefox'
