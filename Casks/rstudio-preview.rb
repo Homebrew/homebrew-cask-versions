0b9467e7e1a533312d9e36fc70834fe08c49a9fb
Michael Ford <fanquake@users.noreply.github.com>
2016-08-08 09:58:44 +0800

    RStudio Preview 0.99.1280

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '0.99.1277'
-  sha256 '5b05defbb7b02bcbfa9e719cfa13946b8a8fc9b080a3dbd46b95e96987d62926'
+  version '0.99.1280'
+  sha256 '3da3f89789e51b3504bd40e2b3f2ca9d499133e1083c94251dead58b471da1d2'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
