b5ee2ac747951fc9a0f33d595cc8823b7cf298a4
Victor Popkov <victor@popkov.me>
2016-08-08 19:47:32 +0300

    torbrowser-de.rb: upgrade to 6.0.3

--- Casks/torbrowser-de.rb
+++ Casks/torbrowser-de.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-de' do
-  version '6.0.2'
-  sha256 '4ea91c9d360b1e40d7f6b4e123474935ba16ddc5dbaead38015c861300117e12'
+  version '6.0.3'
+  sha256 'd5f4d87927c9cee90dfd74f09afd103d7b1c2a837197cca20431e7504bf28314'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_de.dmg"
   name 'Tor Browser'
