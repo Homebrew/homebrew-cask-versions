600b8d71ac8a18211ba0e154290dedb035bfc00f
Michael Ford <fanquake@users.noreply.github.com>
2016-08-09 08:52:47 +0800

    Firefox Nightly JA 51.0a1

--- Casks/firefoxnightly-ja.rb
+++ Casks/firefoxnightly-ja.rb
@@ -1,6 +1,6 @@
 cask 'firefoxnightly-ja' do
-  version '50.0a1'
-  sha256 '359353e183173c29f6f920446e835c5e21df35cc8558a18443590e11e762c26b'
+  version '51.0a1'
+  sha256 '7b500b7526bc73f0f18ff7a6560a32dacf7b071156ef4046684c0c97f20be814'
 
   url "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central-l10n/firefox-#{version}.ja-JP-mac.mac.dmg"
   name 'Mozilla Firefox'
