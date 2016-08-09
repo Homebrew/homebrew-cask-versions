23434f12bf98ca2862d02b730d236c174138be7a
Victor Popkov <victor@popkov.me>
2016-08-09 23:02:07 +0300

    updated atom-beta (1.10.0-beta4) (#2422)

--- Casks/atom-beta.rb
+++ Casks/atom-beta.rb
@@ -1,11 +1,11 @@
 cask 'atom-beta' do
-  version '1.10.0-beta3'
-  sha256 '6eaf77c6b31deaa5256ba594c5d07eb0c0dc515e4eb6bb47a546919b474c5080'
+  version '1.10.0-beta4'
+  sha256 'be4fff5787e3552b2afc658e09c25a9268f80a3542513ae95cfe4709c91b3d32'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: '2342858bafc32a24c7ccb6a314288611d543189a8eec331d023127b4fd183ab6'
+          checkpoint: 'fb7b5cbf83f562fa57ca81cd45b758e1a2d9c42469a4094e632040996c7979ac'
   name 'Github Atom Beta'
   homepage 'https://atom.io/beta'
   license :mit
