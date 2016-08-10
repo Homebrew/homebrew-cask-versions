0ed2c78556825cbe0050a84e17cf977020d1b8d4
Ryan Balfanz <ryan@ryanbalfanz.net>
2016-08-09 20:32:12 -0700

    1 file inspected, 9 offenses detected, 9 offenses corrected

--- Casks/processing2.rb
+++ Casks/processing2.rb
@@ -2,8 +2,6 @@ cask 'processing2' do
   version '2.2.1'
   sha256 '8c237b3eb50626e8ffc648bfdeddaa18ceffbd6a48f8fec77a8eab5b774971fc'
 
-  conflicts_with cask: 'processing'
-
   url "http://download.processing.org/processing-#{version}-macosx.zip"
   appcast 'https://github.com/processing/processing/releases.atom',
           checkpoint: 'b731bdafbc7e9c7c9026d6a8c124c0dfc48d18f502fd7ae89cc445dc0bd02bee'
@@ -11,7 +9,9 @@ cask 'processing2' do
   homepage 'https://processing.org/'
   license :gpl
 
-  app "Processing.app"
+  conflicts_with cask: 'processing'
+
+  app 'Processing.app'
 
   zap delete: '~/Library/Processing/preferences.txt'
 end
