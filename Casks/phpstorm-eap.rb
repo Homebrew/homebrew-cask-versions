4032113bc296a0cc3206595d8590ef4ab46283dc
hao wu(吴昊) <wumouse@qq.com>
2016-08-11 10:23:26 +0800

    Upgrade phpstorm-eap to 162.1628.23

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'phpstorm-eap' do
-  version '162.1447.5'
-  sha256 '2f550848244519ca610ac8504ccec0590c1599cc0c28a156e6b633621be7584e'
+  version '162.1628.23'
+  sha256 'e88b5714429b4eeaeb8d8d23489cd168416ce4541f606bf2f5c5b66a8888e378'
 
   url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg"
   name 'PhpStorm EAP'
