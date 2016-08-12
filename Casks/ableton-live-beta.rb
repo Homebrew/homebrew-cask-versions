15575c51177062d6c797414239e511e12c8557c3
yutarody <yutaro.ver05+github@gmail.com>
2016-08-13 00:00:20 +0900

    update ableton-live-beta 9.7b4

--- Casks/ableton-live-beta.rb
+++ Casks/ableton-live-beta.rb
@@ -1,11 +1,11 @@
 cask 'ableton-live-beta' do
-  version '9.7b1'
+  version '9.7b4'
 
   if Hardware::CPU.is_32_bit?
-    sha256 '28c1eb2163ec58afffdac4d8419697ec290f69c17466c12ace45f576285d2efc'
+    sha256 '48dbe8d0648a62e5778d7a5551205cee91abd0cacc5e76ad8bc94a810b217683'
     url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_beta_#{version.no_dots}_32.dmg"
   else
-    sha256 'b90394adbe5f90467f4a14bb966079ce4b4c1e897abb3b7224d41fd395de0e20'
+    sha256 '5bc6d02891df5702af956f29efab70223fd5ca92a3746b96d84bc1abd8cd8ac0'
     url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_beta_#{version.no_dots}_64.dmg"
   end
 
