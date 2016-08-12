0b26defa57c1290b115df387acc43712f22f64a5
Patrick Kennedy <pkq@users.noreply.github.com>
2016-08-12 11:36:46 -0700

    updated rstudio-preview (0.99.1283) (#2436)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '0.99.1282'
-  sha256 '621491dbf4474deeee129a0ada28e2f4ea92c15da9986c9606fe1a9561527482'
+  version '0.99.1283'
+  sha256 '227df00d360ee3d5436d1c97864b409f0ef0b17b530472d17989827f359254bb'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
