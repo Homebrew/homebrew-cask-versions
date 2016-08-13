90b142774a8a9f7ae0c08d79bb1c2ddbc8219c04
pseudoku <decrement@jnz.ch>
2016-08-13 05:02:53 -0700

    Mirror kicad.rb formula from homebrew-cask (#2438)
    Contents of Kicad and kicad directories end up together on a case-insensitive filesystem. This hack installs the files in their intended location.

--- Casks/kicad-nightly.rb
+++ Casks/kicad-nightly.rb
@@ -4,9 +4,14 @@ cask 'kicad-nightly' do
 
   url "http://downloads.kicad-pcb.org/osx/nightly/kicad-#{version}.dmg"
   name 'KiCad'
-  homepage 'http://www.kicad-pcb.org'
+  homepage 'http://www.kicad-pcb.org/'
   license :gpl
 
-  suite 'Kicad'
-  artifact 'kicad', target: Pathname.new(File.expand_path('~')).join('Library/Application Support/kicad')
+  suite 'Kicad-apps', target: 'Kicad'
+  artifact 'kicad', target: "#{ENV['HOME']}/Library/Application Support/kicad"
+
+  preflight do
+    system_command '/bin/mkdir', args: ['--', "#{staged_path}/Kicad-apps"]
+    system_command '/bin/mv', args: ['--', *Dir["#{staged_path}/Kicad/*.app"], "#{staged_path}/Kicad-apps/"]
+  end
 end
