a33da64be9b28fdb95f3be768fb2812811851cb7
Markus Reiter <me@reitermark.us>
2016-08-15 16:01:51 +0200

    Use version conversions where possible.

--- Casks/adobe-reader-pt-br.rb
+++ Casks/adobe-reader-pt-br.rb
@@ -2,14 +2,14 @@ cask 'adobe-reader-pt-br' do
   version '11.0.00'
   sha256 '432b83f2d895c42c496226214e5b700e1ee078bc03b4501500b9a783d63e4dad'
 
-  url "http://ardownload.adobe.com/pub/adobe/reader/mac/#{version.major}.x/#{version}/pt_BR/AdbeRdr#{version.gsub('.', '')}_pt_BR.dmg"
+  url "http://ardownload.adobe.com/pub/adobe/reader/mac/#{version.major}.x/#{version}/pt_BR/AdbeRdr#{version.no_dots}_pt_BR.dmg"
   name 'Adobe Reader'
   homepage 'https://www.adobe.com/br/products/reader.html'
   license :gratis
 
   pkg 'Adobe Reader XI Installer.pkg'
 
-  uninstall pkgutil: "com.adobe.acrobat.reader.#{version.gsub('.', '')}.*",
+  uninstall pkgutil: "com.adobe.acrobat.reader.#{version.no_dots}.*",
             delete:  '/Applications/Adobe Reader.app'
 
   zap       delete: [
--- Casks/scansnap-manager-sv600.rb
+++ Casks/scansnap-manager-sv600.rb
@@ -2,7 +2,7 @@ cask 'scansnap-manager-sv600' do
   version '6.2L10'
   sha256 '7273034398e9a57eb0fa89167c9e801ad2bf9fe56b52b3d9591628e978168afb'
 
-  url "https://www.fujitsu.com/downloads/IMAGE/driver/ss/mgr/m-sv600/MacScanSnapV#{version.gsub('.', '')}WW.dmg"
+  url "https://www.fujitsu.com/downloads/IMAGE/driver/ss/mgr/m-sv600/MacScanSnapV#{version.no_dots}WW.dmg"
   name 'ScanSnap Manager for Fujitsu ScanSnap SV600'
   homepage 'https://www.fujitsu.com/global/support/computing/peripheral/scanners/software/'
   license :gratis
