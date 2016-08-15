289d90e7524a0c08a674c006baa8b4f53bf9a327
Yurii Kolesnykov <yurikoles@gmail.com>
2016-08-15 03:14:43 +0300

    updated firefox-beta-ja (49.0b3) (#2450)

--- Casks/firefox-beta-ja.rb
+++ Casks/firefox-beta-ja.rb
@@ -1,6 +1,6 @@
 cask 'firefox-beta-ja' do
-  version '49.0b1'
-  sha256 '56da8a4821b8208a5fd2ff917075959dc4a18c883b97fdefefbc609c1bf71514'
+  version '49.0b3'
+  sha256 'e7e800f320db288fd06155209784bfa5d29edd4e9159d814707f324ef85e33a9'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
   name 'Mozilla Firefox'
