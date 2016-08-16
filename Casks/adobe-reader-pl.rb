53ebef099defab4174ba486ec5dc0a7a51492eaa
Markus Reiter <me@reitermark.us>
2016-08-16 15:38:59 +0200

    Use version conversions where possible. (#2460)

--- Casks/adobe-reader-pl.rb
+++ Casks/adobe-reader-pl.rb
@@ -2,14 +2,14 @@ cask 'adobe-reader-pl' do
   version '11.0.00'
   sha256 'cfeeef917cec3175ab7673c9afd9df081794d952c5e919e29a962df21e041b37'
 
-  url "http://ardownload.adobe.com/pub/adobe/reader/mac/#{version.major}.x/#{version}/pl_PL/AdbeRdr#{version.gsub('.', '')}_pl_PL.dmg"
+  url "http://ardownload.adobe.com/pub/adobe/reader/mac/#{version.major}.x/#{version}/pl_PL/AdbeRdr#{version.no_dots}_pl_PL.dmg"
   name 'Adobe Reader'
   homepage 'https://www.adobe.com/pl/products/reader.html'
   license :gratis
 
   pkg 'Adobe Reader XI Installer.pkg'
 
-  uninstall pkgutil: "com.adobe.acrobat.reader.#{version.gsub('.', '')}.*",
+  uninstall pkgutil: "com.adobe.acrobat.reader.#{version.no_dots}.*",
             delete:  '/Applications/Adobe Reader.app'
 
   zap       delete: [
