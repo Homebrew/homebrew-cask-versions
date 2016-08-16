38d6fabd4ee96b0e3686ae797fe22c6b01958534
Victor Popkov <victor@popkov.me>
2016-08-16 20:58:58 +0300

    torbrowser-ar.rb: upgrade to 6.0.4

--- Casks/torbrowser-ar.rb
+++ Casks/torbrowser-ar.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-ar' do
-  version '6.0.3'
-  sha256 '4e6cf878fd3c4f0f418de7e57678978e8fa121cd1af7745721867ce420956b82'
+  version '6.0.4'
+  sha256 '258540b701aede788449fedaece13a42ebc79114b4b6fdb67e352aa9c9a355f9'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_ar.dmg"
   name 'Tor Browser'
