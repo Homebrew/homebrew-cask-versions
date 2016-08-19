509e44a03b7671eb1668a0a900b2b8fd6cb429eb
mahori <mahori@users.noreply.github.com>
2016-08-19 11:23:19 +0900

    updated firefox-zh-cn (48.0.1) (#2475)

--- Casks/firefox-zh-cn.rb
+++ Casks/firefox-zh-cn.rb
@@ -1,6 +1,6 @@
 cask 'firefox-zh-cn' do
-  version '48.0'
-  sha256 '39147558cfaeba6f4a4e6fa1f329537809b7447e9ec90c6857a8614a259295b7'
+  version '48.0.1'
+  sha256 '24ba28d411fa8fda7683a8ac12c7a0cd918a7b0fe71840c243efbbfce2fd8ee1'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-CN"
   name 'Mozilla Firefox'
