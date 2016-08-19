46c8183b78e5c14be6d65bd8767338c4b395f9e0
mahori <mahori@users.noreply.github.com>
2016-08-19 11:26:20 +0900

    updated firefox-fr (48.0.1) (#2484)

--- Casks/firefox-fr.rb
+++ Casks/firefox-fr.rb
@@ -1,6 +1,6 @@
 cask 'firefox-fr' do
-  version '48.0'
-  sha256 'd40634ef4cef1732d77557afa0e68495fa9ea08a29dc99cd69f06a6c0874a476'
+  version '48.0.1'
+  sha256 '463f3964929a730829bdd4d6cbcaaad35cbd3816b1826a63e81eb5c14e3a43df'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=fr"
   name 'Mozilla Firefox'
