1bc7011d9a47bc9ab80f7c63a20252bc64460b72
mahori <mahori@users.noreply.github.com>
2016-08-19 11:06:16 +0900

    updated firefox-zh-tw (48.0.1)

--- Casks/firefox-zh-tw.rb
+++ Casks/firefox-zh-tw.rb
@@ -1,6 +1,6 @@
 cask 'firefox-zh-tw' do
-  version '48.0'
-  sha256 '645878ec11c398382c6fa0925aa74c9a004a02c5c0a4e6a40b5daad74e548714'
+  version '48.0.1'
+  sha256 'a974f7c44d5cc802b56f2d5eca370ea53b0f9779363c956cd49768e35a864d02'
 
   # mozilla.org was verified as official when first introduced to the cask
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=zh-TW"
