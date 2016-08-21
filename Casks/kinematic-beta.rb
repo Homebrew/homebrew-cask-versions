45823a5c7d3d19b065d533b070f73c10e0a0403e
Andrew Sardone <andrew@andrewsardone.com>
2016-08-21 11:37:46 -0400

    Change kinematic-beta checksum w/ v0.12.0 download
    I don't know if this the best route, but right now when I try to install
    the kinematic-beta I'm told the checksum doesn't match:
    
    ```
    ❯ brew cask install kinematic-beta
    ==> Downloading https://github.com/docker/kitematic/releases/download/v0.12.0/Kitematic-0
    ==> Verifying checksum for Cask kinematic-beta
    ==> Note: running "brew update" may fix sha256 checksum errors
    Error: sha256 mismatch
    Expected: 161d702e25cfda4337c5f9b1315ad1487b287cd581e8951caee7ff529595370c
    Actual: 0d6770197ed1db1c9a653c78bc51bb0d1483882233cf2d2bf688609b1a5251ef
    File: /Users/andrew/Library/Caches/Homebrew/Cask/kinematic-beta--0.12.0.zip
    To retry an incomplete download, remove the file above.
    ```
    
    I snagged the value via a direct download of kitematic from GitHub:
    
    ```bash
    curl -sL https://github.com/docker/kitematic/releases/download/v0.12.0/Kitematic-0.12.0-Mac.zip \
        | shasum -a 256
    ```

--- Casks/kinematic-beta.rb
+++ Casks/kinematic-beta.rb
@@ -1,6 +1,6 @@
 cask 'kinematic-beta' do
   version '0.12.0'
-  sha256 '161d702e25cfda4337c5f9b1315ad1487b287cd581e8951caee7ff529595370c'
+  sha256 '0d6770197ed1db1c9a653c78bc51bb0d1483882233cf2d2bf688609b1a5251ef'
 
   # github.com/docker/kitematic was verified as official when first introduced to the cask
   url "https://github.com/docker/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
