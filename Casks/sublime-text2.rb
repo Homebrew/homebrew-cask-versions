aa71b8a45e3183d9535b2fb7a25beb2f9a77cd8a
Christoph Blecker <admin@toph.ca>
2016-08-23 19:45:55 -0700

    Add sublime-text2 (2.0.2)

---
+++ Casks/sublime-text2.rb
@@ -0,0 +1,25 @@
+cask 'sublime-text2' do
+  version '2.0.2'
+  sha256 '906e71e19ae5321f80e7cf42eab8355146d8f2c3fd55be1f7fe5c62c57165add'
+
+  url "https://download.sublimetext.com/Sublime%20Text%20#{version}.dmg"
+  appcast 'https://www.sublimetext.com/updates/2/stable/appcast_osx.xml',
+          checkpoint: '1f88d22b2ed5ba5a9213accf500ca8f1a9723b391fbbbc45ffb23cd98c98519c'
+  name 'Sublime Text 2'
+  homepage 'https://www.sublimetext.com/2'
+  license :closed
+
+  app 'Sublime Text 2.app'
+  binary "#{appdir}/Sublime Text 2.app/Contents/SharedSupport/bin/subl"
+
+  zap delete: [
+                '~/Library/Application Support/Sublime Text 2',
+                '~/Library/Preferences/com.sublimetext.2.plist',
+                '~/Library/Caches/com.sublimetext.2',
+                '~/Library/Saved Application State/com.sublimetext.2.savedState',
+              ]
+
+  caveats do
+    files_in_usr_local
+  end
+end
