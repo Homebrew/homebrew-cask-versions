1824fd21e79acb91ccc6b40a93e9d40afca90795
Caleb Evans <caleb@calebevans.me>
2016-08-25 15:49:49 -0700

    Add alfred-2 (2.8.4_437)

---
+++ Casks/alfred-2.rb
@@ -0,0 +1,30 @@
+cask 'alfred-2' do
+  version '2.8.4_437'
+  sha256 'def068cc763213aba11c07c18cd432740c81026deb31711699f7545e757846b5'
+
+  url "https://cachefly.alfredapp.com/Alfred_#{version}.zip"
+  name 'Alfred'
+  homepage 'https://www.alfredapp.com/'
+  license :freemium
+
+  auto_updates true
+  accessibility_access true
+
+  app 'Alfred 2.app'
+
+  postflight do
+    suppress_move_to_applications key: 'suppressMoveToApplications'
+  end
+
+  uninstall quit:       'com.runningwithcrayons.Alfred-2',
+            login_item: 'Alfred 2'
+
+  zap delete: [
+                '~/Library/Application Support/Alfred 2',
+                '~/Library/Caches/com.runningwithcrayons.Alfred-2',
+                '~/Library/Caches/com.runningwithcrayons.Alfred-Preferences',
+                '~/Library/Preferences/com.runningwithcrayons.Alfred-2.plist',
+                '~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist',
+                '~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState',
+              ]
+end
