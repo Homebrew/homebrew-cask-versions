77cfd43c23dd4d95bd553cae305c4bb380af1d8f
Michael Ford <fanquake@users.noreply.github.com>
2016-08-28 16:07:56 +0800

    PyCharm CE EAP 162.1812.1

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-ce-eap' do
-  version '2016.2,162.646.12'
-  sha256 'bba580d0bccc54891e548e6ee878266a97f6b1544e4c1ea2a820573b2a92dfab'
+  version '2016.2.2,162.1812.1'
+  sha256 '458f37e4931835fd5c85bcdcdad98c8d69e49c49d91f806118345e3421e509a6'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.after_comma}.dmg"
   name 'Jetbrains PyCharm Community Edition EAP'
