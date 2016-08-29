fbe4c1058a7ef733262e4c4644d68243ea3ced79
Maciej Matys <metyl@users.noreply.github.com>
2016-08-29 15:12:02 +0200

    Add IntelliJ IDEA 2016.3 EAP (build 163.3094.26)

---
+++ Casks/intellij-idea-next-eap.rb
@@ -0,0 +1,22 @@
+cask 'intellij-idea-next-eap' do
+  version '2016.3,163.3094.26'
+  sha256 '781c04045250bd694e1d75803c983b8a9d39d6c355ed84770e0b813e8efdabb8'
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
+  name "IntelliJ IDEA #{version.major_minor} EAP"
+  homepage "https://confluence.jetbrains.com/display/IDEADEV/IDEA+#{version.major_minor}+EAP"
+  license :commercial
+
+  auto_updates true
+
+  app "IntelliJ IDEA #{version.before_comma} EAP.app"
+
+  uninstall delete: '/usr/local/bin/idea'
+
+  zap delete: [
+                "~/Library/Caches/IntelliJIdea#{version.major_minor}",
+                "~/Library/Logs/IntelliJIdea#{version.major_minor}",
+                "~/Library/Application Support/IntelliJIdea#{version.major_minor}",
+                "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+              ]
+end
