8b867c952365411f3d364edbe17791f2531df5fc
Tobias Erthal <android@akgbensheim.de>
2016-08-29 14:13:13 +0200

    Added "Vega.app" (32bit / latest) cask
    Added 32bit version of the latest release of "Vega.app" as new cask
    vega-32bit.rb

---
+++ Casks/vega-32bit.rb
@@ -0,0 +1,12 @@
+cask 'vega-32bit' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://dist.subgraph.com/downloads/Vega.dmg'
+  name 'Vega'
+  name 'Vega (32bit)'
+  homepage 'https://subgraph.com/vega/index.en.html'
+  license :unknown
+
+  app 'Vega.app'
+end
