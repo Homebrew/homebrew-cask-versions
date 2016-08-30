631e6b830f01d95f402341feced89c83c3771179
Pavlos Vinieratos <pvinis@gmail.com>
2016-08-30 22:01:32 +0200

    update nightly to 30-08-30 (#2510)

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,10 +1,10 @@
 cask 'emacs-nightly' do
-  version '2016-07-24_01-41-37-4d34210256c6e07cb713ece8d7ad998a873f0f94'
-  sha256 '33f010be39abd47b5574e88c00278b19498c7b801cb6538dedce38d50485482d'
+  version '2016-08-30_01-41-34-bcd2d911f35fd28f0a0051f237dc76f15cea4b4a'
+  sha256 'd8a8df1fe2db86e6716b8a72085b560e9a3e5dcbf88176bf05ed0a01df3421a8'
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
   appcast 'https://emacsformacosx.com/atom/daily',
-          checkpoint: 'b43f26933d1d0ab8a623e3f95ad15f11753a514c01137d881988a5e532636435'
+          checkpoint: '63f71e03147c101f5e52cae2441f136a8da15fe2f688fefe40e31bde45070a81'
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
   license :gpl
