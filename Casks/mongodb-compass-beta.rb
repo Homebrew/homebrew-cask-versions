29d8b25b5597fa49dd2569d5d82936a7c0d0f66d
Alexandre Alonso <alex4551@gmail.com>
2016-08-31 04:05:21 +0800

    Add Mongo Compass Beta (#2496)

---
+++ Casks/mongodb-compass-beta.rb
@@ -0,0 +1,11 @@
+cask 'mongodb-compass-beta' do
+  version '1.3.0-beta.4'
+  sha256 'cddd5b7e4f834ae852755dcbc3c06f3f09c8280045ddde80aed47ce242569df6'
+
+  url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
+  name 'MongoDB Compass'
+  homepage 'https://www.mongodb.com/products/compass'
+  license :commercial
+
+  app 'MongoDB Compass Beta.app'
+end
