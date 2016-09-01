fb07e7520ff9186f41869bdc4ccbb530d7dd1766
Adam Lavin <adam@adamlav.in>
2016-09-01 14:57:37 +0100

    updated sequel-pro-nightly download url

--- Casks/sequel-pro-nightly.rb
+++ Casks/sequel-pro-nightly.rb
@@ -2,7 +2,7 @@ cask 'sequel-pro-nightly' do
   version 'r50a0f18540'
   sha256 '9edb2e6c023088175d1a136e1ad6647819e6315eb4f48dbcec353ec150edac15'
 
-  url "http://nightly.sequelpro.com/builds/Sequel_Pro_#{version}.dmg"
+  url "https://sequelpro.com/Sequel_Pro_#{version}.dmg"
   name 'Sequel Pro'
   homepage 'http://nightly.sequelpro.com/'
   license :mit
