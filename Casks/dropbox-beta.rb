146afa3314c47c7c28f2938c5cd7d14da1c14c18
Michael Ford <fanquake@users.noreply.github.com>
2016-09-03 12:13:50 +0800

    Dropbox Beta 10.3.20

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,11 +1,11 @@
 cask 'dropbox-beta' do
-  version '9.3.43'
-  sha256 '455f6b3be82c4601166a1607e11a6683c4851099f0e1e8d9332539a8276e7583'
+  version '10.3.20'
+  sha256 '38f4b1980392ee68030e420e1540be71e7b2db97eb04dd843274a723a8731e0a'
 
   # dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
   name 'Dropbox'
-  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/210600206-Beta-Build-9-3-43'
+  homepage 'https://www.dropboxforum.com/hc/en-us/community/posts/208785043-Beta-Build-10-3-20'
   license :gratis
 
   app 'Dropbox.app'
