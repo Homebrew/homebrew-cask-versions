a063a5e1b7ad60e2519f5e549db2d7b5569a506c
AVS <milus-avs@MBP-Wojciech.avsystem.in>
2016-09-05 11:09:57 +0200

    Update IntelliJ IDEA CE to 2016.2.3

--- Casks/intellij-idea-ce.rb
+++ Casks/intellij-idea-ce.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-ce' do
-  version '2016.2.2'
-  sha256 '47641ade715a1fed88ab8d3656f9505e420d180a5df79cf469b21217ba6c99f6'
+  version '2016.2.3'
+  sha256 '3a180ff30a8c9b7bf8bf38f6297fcd9ec6d8994e2b0489b97bfbeead362fb3b8'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version}.dmg"
   name 'IntelliJ IDEA Community Edition'
