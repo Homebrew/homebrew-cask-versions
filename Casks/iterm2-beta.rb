a8f649e1421522addc313ea6e6a3ffac44b66589
RJ Hsiao <rj19860725@gmail.com>
2016-09-05 11:49:25 +0800

    Update iterm2-beta to 3.0.9

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-beta' do
-  version '3.0.8'
-  sha256 'fae85a7fd1c0c7706031b103dab6c2cf65bb6a82b4d0fbb0be9f75467875ae64'
+  version '3.0.9'
+  sha256 '4e4b9decbc2327c795fa114156213d95482a882eab188825dfeb8df1ed13b9ab'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
