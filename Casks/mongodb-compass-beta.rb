14fad67631bdbc082b36bd4b9a55f31cbf253852
Alex Alonso <alex4551@gmail.com>
2016-09-06 12:13:21 +0800

    update mongodb compass version 1.3.0-beta-5

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-beta' do
-  version '1.3.0-beta.4'
-  sha256 'cddd5b7e4f834ae852755dcbc3c06f3f09c8280045ddde80aed47ce242569df6'
+  version '1.3.0-beta.5'
+  sha256 'ef11cd1fdf4598b15333f6332dc35c1c2afaa4f72353b38c16693a28340a77bc'
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
   name 'MongoDB Compass'
