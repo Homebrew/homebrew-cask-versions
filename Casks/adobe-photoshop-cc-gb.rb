6c1d7a57b80402fe857d721da7771f9bc800a60e
Daniel Bayley <daniel.bayley@me.com>
2016-09-08 16:18:09 +0100

    Add British English version of Adobe Photoshop

---
+++ Casks/adobe-photoshop-cc-gb.rb
@@ -0,0 +1,40 @@
+cask 'adobe-photoshop-cc-gb' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://trials3.adobe.com/AdobeProducts/PHSP/16/osx10/Photoshop_16_LS20.dmg',
+      user_agent: :fake,
+      cookies:    { 'MM_TRIALS' => '1234' }
+  name 'Adobe Photoshop CC 2015'
+  homepage 'https://adobe.com/products/photoshop'
+  license :commercial
+
+  conflicts_with cask: 'adobe-photoshop-cc'
+
+  preflight do
+    system '/usr/bin/sudo', '-E', '--', "#{staged_path}/Adobe Photoshop CC 2015/Install.app/Contents/MacOS/Install", '--mode=silent', "--deploymentFile=#{staged_path}/Adobe\ Photoshop\ CC\ 2015/Deployment/en_GB_Deployment.xml"
+  end
+
+  uninstall_preflight do
+    uninstall_xml = "#{staged_path}/uninstall.xml"
+
+    IO.write uninstall_xml, <<-EOF.undent
+      <?xml version="1.0" encoding="utf-8"?>
+      <Deployment>
+        <Properties>
+          <Property name="removeUserPrefs">0</Property>
+          <Property name="mediaSignature">{2614BC86-757D-4293-9E25-E4E16F370A9E}</Property>
+        </Properties>
+        <Payloads>
+          <Payload adobeCode="{2614BC86-757D-4293-9E25-E4E16F370A9E}">
+            <Action>remove</Action>
+          </Payload>
+        </Payloads>
+      </Deployment>
+    EOF
+
+    system '/usr/bin/sudo', '-E', '--', "#{staged_path}/Adobe Photoshop CC 2015/Install.app/Contents/MacOS/Install", '--mode=silent', "--deploymentFile=#{uninstall_xml}"
+  end
+
+  uninstall rmdir: '/Applications/Utilities/Adobe Installers'
+end
