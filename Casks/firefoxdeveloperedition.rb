8eca727df887760cf8522225ce76e56401fd6197
Markus Reiter <me@reitermark.us>
2016-09-09 05:08:26 +0200

    FirefoxDeveloperEdition: Use system language.

--- Casks/firefoxdeveloperedition.rb
+++ Casks/firefoxdeveloperedition.rb
@@ -2,9 +2,29 @@ cask 'firefoxdeveloperedition' do
   version :latest
   sha256 :no_check
 
-  url 'https://download.mozilla.org/?product=firefox-aurora-latest-ssl&os=osx&lang=en-US'
+  language 'en', default: true do
+    'en-US'
+  end
+
+  language 'ja' do
+    'ja-JP-mac'
+  end
+
+  language 'ru' do
+    'ru'
+  end
+
+  language 'uk' do
+    'uk'
+  end
+
+  language 'zh' do
+    'zh-CN'
+  end
+
+  url "https://download.mozilla.org/?product=firefox-aurora-latest#{language == 'en-US' ? 'ssl' : '-l10n'}&os=osx&lang=#{language}"
   name 'Mozilla Firefox Developer Edition'
-  homepage 'https://www.mozilla.org/en-US/firefox/developer/'
+  homepage 'https://www.mozilla.org/firefox/developer/'
   license :mpl
 
   app 'FirefoxDeveloperEdition.app'
