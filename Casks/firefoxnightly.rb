f77681f1d7754f725f97ff0cd8cc471b0a412089
Markus Reiter <me@reitermark.us>
2016-09-09 05:00:49 +0200

    FirefoxNightly: Use system language.

--- Casks/firefoxnightly.rb
+++ Casks/firefoxnightly.rb
@@ -2,7 +2,23 @@ cask 'firefoxnightly' do
   version '52.0a1'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://archive.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-#{version}.en-US.mac.dmg"
+  language 'en', default: true do
+    'en-US'
+  end
+
+  language 'ja' do
+    'ja-JP-mac'
+  end
+
+  language 'ru' do
+    'ru'
+  end
+
+  language 'uk' do
+    'uk'
+  end
+
+  url "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central#{language == 'en-US' ? '' : '-l10n'}/firefox-#{version}.#{language}.mac.dmg"
   name 'Mozilla Firefox'
   homepage 'https://nightly.mozilla.org/'
   license :mpl
