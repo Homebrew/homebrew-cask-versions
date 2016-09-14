df77b0ad7d1dda43ee079221c5583d00ac3b7b12
Alyssa Ross <hi@alyssa.is>
2016-09-14 15:09:16 +0100

    Add Slack.app v2.2.6-beta1

---
+++ Casks/slack-beta.rb
@@ -0,0 +1,27 @@
+cask 'slack-beta' do
+  version '2.2.6-beta1'
+  sha256 '7166abdf83772bcfc4ada304499dd6b1de0aafb4d9fdde0980a3a665e1d7c1d4'
+
+  # slack-ssb-updates.global.ssl.fastly.net was verified as official when first introduced to the cask
+  url "https://slack-ssb-updates.global.ssl.fastly.net/mac_external_beta/Slack-#{version}-macOS.zip"
+  name 'Slack'
+  homepage 'https://slack.com/beta/osx'
+  license :gratis
+
+  auto_updates true
+
+  app 'Slack.app'
+
+  uninstall quit: 'com.tinyspeck.slackmacgap'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tinyspeck.slackmacgap.sfl',
+                '~/Library/Application Support/Slack',
+                '~/Library/Caches/com.tinyspeck.slackmacgap',
+                '~/Library/Containers/com.tinyspeck.slackmacgap',
+                '~/Library/Containers/com.tinyspeck.slackmacgap.SlackCallsService',
+                '~/Library/Cookies/com.tinyspeck.slackmacgap.binarycookies',
+                '~/Library/Preferences/com.tinyspeck.slackmacgap.plist',
+                '~/Library/Saved Application State/com.tinyspeck.slackmacgap.savedState',
+              ]
+end
