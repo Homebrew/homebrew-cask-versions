12d584dc0db48a4a693d33f5f38f24d49b688d5c
Roberto Nogueira <enogrob@gmail.com>
2016-09-15 10:10:42 +0000

    Update thebrain9 cask to version 9.0.107.0 (#2587)

--- Casks/thebrain9.rb
+++ Casks/thebrain9.rb
@@ -1,6 +1,6 @@
 cask 'thebrain9' do
-  version '9.0.101.0'
-  sha256 '01c35e65b24e0de65b11a55148d68c3e1eb7fbec66934f49eae6bb6da30e88dc'
+  version '9.0.107.0'
+  sha256 '15011129e764b8637b9846c59b4d921b44bb0fe0eba06d988fa8ef7a6b279f1f'
 
   url "http://updater.thebrain.com/files/TheBrain#{version}.dmg"
   name 'TheBrain 9'
