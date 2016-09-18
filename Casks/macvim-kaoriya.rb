8f1d99a9c9692eaa0f36b4657cee3191138d3d99
delphinus <delphinus@remora.cx>
2016-09-18 21:47:53 +0900

    Update macvim-kaoriya to 20160913

--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -3,13 +3,13 @@ cask 'macvim-kaoriya' do
     version '7.4:20130911'
     sha256 'd9fc6e38de1852e4ef79e9ea78afa60e606bf45066cff031e349d65748cbfbce'
   else
-    version '8.0:20160912'
-    sha256 'bba8653f5259e993b92b052dcbe6080352b35a964aebed8fcd593ac8e4eeaad6'
+    version '8.0:20160913'
+    sha256 '0d476609682cb728afa58c03e11cc7a0b54f32a25b891d420fef8ea7a3975e64'
   end
 
   url "https://github.com/splhack/macvim-kaoriya/releases/download/#{version.after_colon}/MacVim-KaoriYa-#{version.after_colon}.dmg"
   appcast 'https://github.com/splhack/macvim-kaoriya/releases.atom',
-          checkpoint: '21e3e2796d582c3646f339eec11365fb979d731bb4c14ee7c5cd5bb9893625a6'
+          checkpoint: 'c7e4d41c7e982e81884856fa0e8e3e5eb3f80428d99874313d59b47d59d1065f'
   name 'MacVim KaoriYa'
   homepage 'https://github.com/splhack/macvim-kaoriya'
   license :oss
