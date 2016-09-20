4b2fdd43ac31ad26332f651205e9030dd50fc452
Yurii Kolesnykov <yurikoles@gmail.com>
2016-09-20 16:24:39 +0300

    Update torbrowser-pl to 6.0.5

--- Casks/torbrowser-pl.rb
+++ Casks/torbrowser-pl.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-pl' do
-  version '6.0.4'
-  sha256 '96afb7def95e00d34961e75b1e3a2a784a7b85df8838ea90344f3a4ff834817b'
+  version '6.0.5'
+  sha256 'ea00ef6da19f8c52c32af5b5b21f389610eaea2751f602ef9749a043de6a807a'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_pl.dmg"
   name 'Tor Browser'
