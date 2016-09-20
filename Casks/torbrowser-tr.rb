5a87e7a411a5f9a16055003c46a6ce02bb866d4e
Yurii Kolesnykov <yurikoles@gmail.com>
2016-09-20 16:25:50 +0300

    Update torbrowser-tr to 6.0.5

--- Casks/torbrowser-tr.rb
+++ Casks/torbrowser-tr.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-tr' do
-  version '6.0.4'
-  sha256 '6009d37c96e3080a8aa481480979baf990d371ca3164c195aeb8a2ab0dd83842'
+  version '6.0.5'
+  sha256 '32b207fa9fbae37cc78b43eac4cef4c0966069dc94176ee035c0bf6390caee2a'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_tr.dmg"
   name 'Tor Browser'
