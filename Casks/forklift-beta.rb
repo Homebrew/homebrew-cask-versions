9cf3051275bd4e7e52b1a0853146b0f2415eefaf
Artem Medvedev <amedvedev@outlook.com>
2016-09-22 19:13:00 +0300

    Fixed version definition

--- Casks/forklift-beta.rb
+++ Casks/forklift-beta.rb
@@ -1,9 +1,9 @@
 cask 'forklift-beta' do
-  version '3b1'
+  version '3.b1'
   sha256 'abbad60a17efc262809e94517dfda90b510867942aa9e6de4a032c68bdd897c5'
 
-  url "http://download.binarynights.com/ForkLift#{version}.zip"
-  appcast 'http://update.binarynights.com/ForkLift3/update.xml',
+  url "http://download.binarynights.com/ForkLift#{version.no_dots}.zip"
+  appcast "http://update.binarynights.com/ForkLift#{version.major}/update.xml",
           checkpoint: 'e9637840c3af9bb45202af5a3905eabd6f82e0f2e47f5a552cd65f308b5dab46'
   name 'ForkLift'
   homepage 'http://www.binarynights.com/'
@@ -12,9 +12,9 @@ cask 'forklift-beta' do
   app 'ForkLift.app'
 
   zap delete: [
-                '~/Library/Preferences/com.binarynights.ForkLift-3.plist',
+                "~/Library/Preferences/com.binarynights.ForkLift-#{version.major}.plist",
                 '~/Library/Application Support/ForkLift',
-                '~/Library/Saved Application State/com.binarynights.ForkLift-3.savedState',
-                '~/Library/Caches/com.binarynights.ForkLift-3',
+                "~/Library/Saved Application State/com.binarynights.ForkLift-#{version.major}.savedState",
+                "~/Library/Caches/com.binarynights.ForkLift-#{version.major}",
               ]
 end
