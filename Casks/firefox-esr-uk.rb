e79687c1fb3f566e48fb3ae5a6dc8cba585e04dd
Tom Janson <tom.janson@rwth-aachen.de>
2016-09-22 18:34:56 +0200

    Update firefox-esr-uk to 45.4.0

--- Casks/firefox-esr-uk.rb
+++ Casks/firefox-esr-uk.rb
@@ -1,6 +1,6 @@
 cask 'firefox-esr-uk' do
-  version '45.3.0'
-  sha256 'caf251ad76d6431052cc12bcab699c84447045520da1042d1a826cbb27995aee'
+  version '45.4.0'
+  sha256 '80914bfd90e2ac05da3c18ea77662f783b8e11786458bf68b215bf7961ad9629'
 
   # mozilla.net was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/uk/Firefox%20#{version}esr.dmg"
