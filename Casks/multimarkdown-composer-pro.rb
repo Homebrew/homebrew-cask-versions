40f36690c035182b2af4b1d8de60ff9ac5cbec49
ickc <ickc@users.noreply.github.com>
2016-09-24 03:00:08 -0700

    homepage made more specific

--- Casks/multimarkdown-composer-pro.rb
+++ Casks/multimarkdown-composer-pro.rb
@@ -5,7 +5,7 @@ cask 'multimarkdown-composer-pro' do
   # amazonaws.com was verified as official when first introduced to the cask
   url "http://files.fletcherpenney.net.s3.amazonaws.com/MultiMarkdown%20Composer%20Pro%20%28Beta%29-#{version}.zip"
   name 'MultiMarkdown Composer Pro (Beta)'
-  homepage 'http://multimarkdown.com'
+  homepage 'http://support.multimarkdown.com/kb/future-plans/where-can-i-download-the-beta-for-v3'
   license :commercial
 
   app 'MultiMarkdown Composer Pro (Beta).app'
