96190676985f7ddec692eef2e738f48acac659fd
valbeat <kj1ktk@gmail.com>
2016-09-27 03:58:30 +0900

    Add dash2

--- Casks/dash2.rb
+++ Casks/dash2.rb
@@ -3,8 +3,6 @@ cask 'dash2' do
   sha256 'a7de5e00528611f86d1024164b1b5d536ce039344b6ca3c211918c7458f7e412'
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
-  appcast "https://kapeli.com/Dash#{version.major}.xml",
-          checkpoint: '7ebd8e2c02a8c003e0fc7098fefc2313fc8bdd670a1ed711d2b89f05b163e0cc'
   name 'Dash'
   homepage 'https://kapeli.com/dash'
   license :commercial
