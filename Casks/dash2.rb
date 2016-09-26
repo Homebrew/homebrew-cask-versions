911b36bb90dffe481a2c433782ec1b853d468134
valbeat <kj1ktk@gmail.com>
2016-09-27 04:30:36 +0900

    Add Dash2 (#2661)

---
+++ Casks/dash2.rb
@@ -0,0 +1,21 @@
+cask 'dash2' do
+  version '2.2.6'
+  sha256 'a7de5e00528611f86d1024164b1b5d536ce039344b6ca3c211918c7458f7e412'
+
+  url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
+  name 'Dash'
+  homepage 'https://kapeli.com/dash'
+  license :commercial
+
+  app 'Dash.app'
+
+  postflight do
+    suppress_move_to_applications
+  end
+
+  zap delete: [
+                '~/Library/Application Support/Dash',
+                '~/Library/Preferences/com.kapeli.dash.plist',
+                '~/Library/Preferences/com.kapeli.dashdoc.plist',
+              ]
+end
