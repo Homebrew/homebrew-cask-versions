767387753631df6844336ba724ceb3f772efcc56
Ender Ekici <enderekici@users.noreply.github.com>
2016-09-28 10:01:22 +0300

    3.7.5. Update
    updated version number and sha256 sum.

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '3.7.4'
-  sha256 'ad1dc5bd003bd02d1edd1d24e35b12cb169979cc0a660d5a5b107a42dfeca786'
+  version '3.7.5'
+  sha256 '7f33cc34cc00f214954d43205fe1812f4b64fc17888c8474cda93417c556f327'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
