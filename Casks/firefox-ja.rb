1bd379c05a3434cc3fdadc8d34988c8f60074556
mahori <mahori@users.noreply.github.com>
2016-09-30 09:41:28 +0900

    Update firefox-ja to 49.0

--- Casks/firefox-ja.rb
+++ Casks/firefox-ja.rb
@@ -1,6 +1,6 @@
 cask 'firefox-ja' do
-  version '48.0.1'
-  sha256 '2a4166887ad66bdc16be0fb075708cb924c25ced190904e71f7746c8699ae185'
+  version '49.0'
+  sha256 '39172b39e9d7c7902192ec524785061d593700c697a22812076137c7f4a53d01'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=ja-JP-mac"
   name 'Mozilla Firefox'
