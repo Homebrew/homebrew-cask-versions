f85148ffc6bf4202e10dbd9ec5896e01bd9e45c4
mahori <mahori@users.noreply.github.com>
2016-09-30 11:36:49 +0900

    Update firefox-it to 49.0 (#2681)

--- Casks/firefox-it.rb
+++ Casks/firefox-it.rb
@@ -1,6 +1,6 @@
 cask 'firefox-it' do
-  version '48.0.1'
-  sha256 '9e83393dd749d381ca01cf95ab849a38fdf97c09bf5afd023249e7b7fc1fdf89'
+  version '49.0'
+  sha256 '8f2ae12a20f61589b6b5aee7666d1de6756c19272204ca0f591854dd3fac3cce'
 
   url "https://download.mozilla.org/?product=firefox-#{version}-SSL&os=osx&lang=it"
   name 'Mozilla Firefox'
