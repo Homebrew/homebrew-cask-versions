0ea26eabfd8fb4b650fd71a3667769b7f50a3539
Victor Popkov <victor@popkov.me>
2016-10-01 22:56:20 +0300

    Update royal-tsx-beta to 3.0.0.38

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,11 +1,11 @@
 cask 'royal-tsx-beta' do
-  version '3.0.0.37'
-  sha256 'd99f9b4620e1c16fc95ca42eb040360f0c67963b4bacd707f1b403eb4d319e0c'
+  version '3.0.0.38'
+  sha256 '46c3aad21cf740a73e71ba689d74247be0df276bdbe18bfb598fe3a58376c4e9'
 
   # royaltsx-v3.royalapplications.com was verified as official when first introduced to the cask
   url "https://royaltsx-v3.royalapplications.com/updates/royaltsx_#{version}.dmg"
   appcast 'https://royaltsx-v3.royalapplications.com/updates_beta.php',
-          checkpoint: '20b31ecd489b85b41916a397865c0f83c9bf4e3a30712952b37ff23d5e6574ce'
+          checkpoint: 'bf00291a67cf04dca47de4d0f51021fafe4317a292fbee677c31984fb1b89ac5'
   name 'Royal TSX'
   homepage 'http://www.royaltsx.com'
   license :freemium
