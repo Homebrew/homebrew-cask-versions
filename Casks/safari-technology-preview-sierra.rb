220ddd080fee938b0fcc7becd35a6c5ee708523b
ickc <ickc@users.noreply.github.com>
2016-10-01 12:33:14 -0700

    Add safari-technology-preview-sierra

---
+++ Casks/safari-technology-preview-sierra.rb
@@ -0,0 +1,25 @@
+cask 'safari-technology-preview-sierra' do
+  version :latest
+  sha256 :no_check
+
+  url 'http://appldnld.apple.com/STP/031-81899-20160926-E7046BC6-8434-11E6-83C5-ACC333D2D062/SafariTechnologyPreview.dmg'
+  name 'Safari Technology Preview'
+  homepage 'https://developer.apple.com/safari/download/'
+  license :gratis
+
+  pkg 'Safari Technology Preview.pkg'
+
+  uninstall delete: '/Applications/Safari Technology Preview.app'
+
+  zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.apple.safaritechnologypreview.sfl',
+                '~/Library/Caches/com.apple.SafariTechnologyPreview',
+                '~/Library/Preferences/com.apple.SafariTechnologyPreview.plist',
+                '~/Library/SafariTechnologyPreview',
+                '~/Library/Saved Application State/com.apple.SafariTechnologyPreview.savedState',
+                '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview-com.apple.Safari.UserRequests.plist',
+                '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview-com.apple.Safari.WebFeedSubscriptions.plist',
+                '~/Library/SyncedPreferences/com.apple.SafariTechnologyPreview.plist',
+                '~/Library/WebKit/com.apple.SafariTechnologyPreview',
+              ]
+end
