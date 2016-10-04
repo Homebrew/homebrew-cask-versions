5baa7f58cec1fc7c4c55206ae54a8fb0eed193d5
mahori <mahori@users.noreply.github.com>
2016-10-04 18:32:54 +0900

    Update thunderbird-gl to 45.4.0 (#2702)

--- Casks/thunderbird-gl.rb
+++ Casks/thunderbird-gl.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-gl' do
-  version '45.3.0'
-  sha256 'e55163af518cd04c5226adca7e7f7ab24d82c7ce78964e77ae931ca7d129c86b'
+  version '45.4.0'
+  sha256 '4f03e389ce1972db0b9225fd1272b52667ed09636fb83e53c096c774fecdd8e4'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=gl"
   name 'Mozilla Thunderbird'
