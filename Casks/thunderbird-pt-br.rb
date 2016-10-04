ca52d19306b2380a031a97e65716e30623e65265
mahori <mahori@users.noreply.github.com>
2016-10-04 18:31:49 +0900

    Update thunderbird-pt-br to 45.4.0 (#2706)

--- Casks/thunderbird-pt-br.rb
+++ Casks/thunderbird-pt-br.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-pt-br' do
-  version '45.3.0'
-  sha256 '52deef211b46c17b1184894b5313e27b963d99d2acf36229a97727549c68b680'
+  version '45.4.0'
+  sha256 '314ec5dd4962acb9d0545fe2817a043aaae36cb1b576ec5083c9ef42cf22b53f'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=pt-BR"
   name 'Mozilla Thunderbird'
