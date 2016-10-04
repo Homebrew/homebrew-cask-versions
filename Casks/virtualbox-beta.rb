1df6a3a0609969b6b9c6dcd882e3ecbaf305a625
Mert Akengin <mert@n0pe.me>
2016-10-04 21:48:02 +0300

    Update virtualbox-beta to 5.1.7,111038

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -3,8 +3,8 @@ cask 'virtualbox-beta' do
     version '4.3.32-103443'
     sha256 '08defbf310b7ba5852fa8dd951438bb9b1528bb1544211568861986110e807f7'
   else
-    version '5.1.3,109189'
-    sha256 'f1499a7fb9d79e76d9c778bf788700d2f4fe0956273ed25036ed8cdbb3d77817'
+    version '5.1.7,111038'
+    sha256 '8c018c9af1656abf5c2c868c8010734c9d3e48e868cd85190089e35fb4e5ca37'
   end
 
   url "http://www.virtualbox.org/download/testcase/VirtualBox-#{version.before_comma}-#{version.after_comma}-OSX.dmg"
