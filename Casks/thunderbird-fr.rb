c1c04a1048d5a576ae6f3b7b955a3f1f29f09752
mahori <mahori@users.noreply.github.com>
2016-10-04 18:32:05 +0900

    Update thunderbird-fr to 45.4.0 (#2701)

--- Casks/thunderbird-fr.rb
+++ Casks/thunderbird-fr.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-fr' do
-  version '45.3.0'
-  sha256 '4579c2d343a09d5a2ed0909455cc2a8d4ddc43a0760cabbf3cfa64cba871c222'
+  version '45.4.0'
+  sha256 '6f6318bb2bb11071e06e83ff52f3fb5b96e2e72b461667fdf173c1c8121d09b5'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=fr"
   name 'Mozilla Thunderbird'
