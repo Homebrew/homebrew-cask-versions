4914fdda297451d6fb5325a391c64cbb0427e6df
mahori <mahori@users.noreply.github.com>
2016-10-04 18:32:12 +0900

    Update thunderbird-nl to 45.4.0 (#2705)

--- Casks/thunderbird-nl.rb
+++ Casks/thunderbird-nl.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-nl' do
-  version '45.3.0'
-  sha256 '17dfec5caec0216dd5d2caefc2284c354514434ebae2117d2a9639d369a0668a'
+  version '45.4.0'
+  sha256 '6c1402dd7bdddfe6dfecd96dc48eface9f388bb1798f6a8842602b55bf57450a'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=nl"
   name 'Mozilla Mozilla Thunderbird'
