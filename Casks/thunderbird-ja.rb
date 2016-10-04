bf3ab71ced575c3ea4e812e565aceb51be4e1b2e
mahori <mahori@users.noreply.github.com>
2016-10-04 18:32:28 +0900

    Update thunderbird-ja to 45.4.0 (#2704)

--- Casks/thunderbird-ja.rb
+++ Casks/thunderbird-ja.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-ja' do
-  version '45.3.0'
-  sha256 '0b7b46c00d56abcff68f6b8bbc89c40f267c2163c79f48e9fb01db2eedb840d4'
+  version '45.4.0'
+  sha256 'b5b73d0aa26dea74c7473875e8e112fe5ac4bb57a6d5042376475470154ae076'
 
   # mozilla.org was verified as official when first introduced to the cask
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=ja-JP-mac"
