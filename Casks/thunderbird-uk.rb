343e40c9a70bff03ef9c029579f20af4621f6c1a
mahori <mahori@users.noreply.github.com>
2016-10-04 14:40:53 +0900

    Update thunderbird-uk to 45.4.0

--- Casks/thunderbird-uk.rb
+++ Casks/thunderbird-uk.rb
@@ -1,6 +1,6 @@
 cask 'thunderbird-uk' do
-  version '45.3.0'
-  sha256 '41ea1e0b5e168ce21a6eb07fe2989ff05c67312fbe8c47972b207beda91a1113'
+  version '45.4.0'
+  sha256 '7cc800300dee7298cc2b2b1636f4a5213c15b24add42d928a7306a0d5478e1e6'
 
   url "https://download.mozilla.org/?product=thunderbird-#{version}&os=osx&lang=uk"
   name 'Mozilla Thunderbird'
