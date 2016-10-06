3124d872dad1ef80b3cd66db0a39c7a6b09a5d3b
pat-s <patrick.schratz@gmail.com>
2016-10-06 19:10:37 +0200

    Update rstudio-preview to 1.0.35

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.0.33'
-  sha256 '8aa17732859c9df8ccfdfa8c7d500df2087edad3c00bfe7bde29b726789e9928'
+  version '1.0.35'
+  sha256 '78f73d46ffac877e1dd6f2eddc898a2b371431d634852f76ac59347848f29bd3'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
