55f4e38d16290385f5fbe276a41aac77597abb44
Markus Reiter <me@reitermark.us>
2016-10-10 16:18:03 +0200

    Firefox ESR: Use system language.

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,11 +1,30 @@
 cask 'firefox-esr' do
   version '45.4.0'
-  sha256 '4e40d588b838709822bd3ccc1edbd13f055df8daf73d1024bcc6c9cbe07ef1e9'
 
-  # mozilla.net was verified as official when first introduced to the cask
-  url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/en-US/Firefox%20#{version}esr.dmg"
-  name 'Mozilla Firefox'
-  homepage 'https://www.mozilla.org/en-US/firefox/organizations/'
+  language 'en', default: true do
+    sha256 '4e40d588b838709822bd3ccc1edbd13f055df8daf73d1024bcc6c9cbe07ef1e9'
+    'en-US'
+  end
+
+  language 'ru' do
+    sha256 '06a001fb135e78a92c23538db9bbc9111107676fc4dd17540b9af54cb09f5c97'
+    'ru'
+  end
+
+  language 'uk' do
+    sha256 '80914bfd90e2ac05da3c18ea77662f783b8e11786458bf68b215bf7961ad9629'
+    'uk'
+  end
+
+  language 'zh' do
+    sha256 '6e3909b983a5a03452c3247b99976083e349c2410e8f223c7b812fbd95e33ea9'
+    'zh-CN'
+  end
+
+  url "https://ftp.mozilla.org/pub/firefox/releases/#{version}esr/mac/#{language}/Firefox%20#{version}esr.dmg"
+  name 'Mozilla Firefox ESR'
+  name 'Mozilla Firefox Extended Support Release'
+  homepage 'https://www.mozilla.org/firefox/organizations/'
   license :mpl
 
   app 'Firefox.app'
