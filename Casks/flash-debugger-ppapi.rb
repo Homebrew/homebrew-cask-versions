0bba56e9bf82a3761305de92001edd1af1946d1f
Robert Lu <robberphex@gmail.com>
2016-10-12 10:44:33 +0800

    add flash-debugger-ppapi for 23.0.0.185

---
+++ Casks/flash-debugger-ppapi.rb
@@ -0,0 +1,21 @@
+cask 'flash-debugger-ppapi' do
+  version '23.0.0.185'
+  sha256 '91c1eb540ed0ce798e069d0dd3bd86077e612e2933b88e7331de702a414a757e'
+
+  # macromedia.com was verified as official when first introduced to the cask
+  url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_ppapi_debug.dmg"
+  appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
+          checkpoint: '159582e9a7da281f6d866c000f4f2803c7306e89be44075e6793c142286077e5'
+  name 'Adobe Flash Player Plugin content debugger - PPAPI (for Opera and chromium)'
+  homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
+
+  pkg 'Install Adobe Pepper Flash Player Debugger.app/Contents/Resources/Adobe Flash Player Debugger.pkg'
+
+  uninstall pkgutil: 'com.adobe.pkg.PepperFlashPlayer',
+            delete:  '/Library/Internet Plug-Ins/PepperFlashPlayer'
+
+  zap       delete: [
+                      '~/Library/Caches/Adobe/Flash Player',
+                      '~/Library/Logs/FlashPlayerInstallManager.log',
+                    ]
+end
