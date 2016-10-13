6aa11f75820b4ebbe4932c297b9562ca03c52f0d
Vítor Galvão <info@vitorgalvao.com>
2016-10-13 19:41:43 +0100

    Update and rename flash-debugger-npapi.rb to flash-player-debugger-npapi.rb

--- Casks/flash-player-debugger-npapi.rb
+++ Casks/flash-player-debugger-npapi.rb
@@ -1,4 +1,4 @@
-cask 'flash-debugger-npapi' do
+cask 'flash-player-debugger-npapi' do
   version '23.0.0.185'
   sha256 'bf8ceaf302a87e697404482d337cff30d1e47e634f34b2afb72abac2761230ac'
 
@@ -6,7 +6,7 @@ cask 'flash-debugger-npapi' do
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_plugin_debug.dmg"
   appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pl.xml',
           checkpoint: '159582e9a7da281f6d866c000f4f2803c7306e89be44075e6793c142286077e5'
-  name 'Adobe Flash Player Plugin content debugger - NPAPI (for Safari and Firefox)'
+  name 'Adobe Flash Player NPAPI content debugger (plugin for Safari and Firefox)'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
   pkg 'Install Adobe Flash Player Debugger.app/Contents/Resources/Adobe Flash Player Debugger.pkg'
