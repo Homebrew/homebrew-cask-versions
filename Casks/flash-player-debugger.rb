cbb903540f84eda78cba4ecb53cd864f478ab686
Vítor Galvão <info@vitorgalvao.com>
2016-10-13 19:44:18 +0100

    Update and rename flash-debugger-projector.rb to flash-player-debugger.rb

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,13 +1,14 @@
-cask 'flash-debugger-projector' do
+cask 'flash-player-debugger' do
   version '23.0.0.185'
   sha256 'f1a7b29230bee1a8125d005d69becc9d6a63e7192291c5478b1f1f5e68d3e9b0'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
-  name 'Adobe Flash Player projector content debugger (standalone version)'
+  name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
-  app 'Flash Player.app'
+  # Renamed to avoid conflict with flash-player.
+  app 'Flash Player.app', target: 'Flash Player Debugger.app'
 
   zap delete: [
                 '~/Library/Caches/Adobe/Flash Player',
