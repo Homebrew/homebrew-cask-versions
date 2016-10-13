9efbf2535134f1751434df55ad14ca8973a5d0f2
Michael Ford <fanquake@users.noreply.github.com>
2016-10-13 14:27:02 +0800

    TorBrowser 6.5a3

--- Casks/torbrowser-alpha.rb
+++ Casks/torbrowser-alpha.rb
@@ -1,6 +1,6 @@
 cask 'torbrowser-alpha' do
-  version '6.5a2'
-  sha256 '4ba0d6430941d168914325c5f4f8dc6a8b853c12c0892248e81679a8e83766ae'
+  version '6.5a3'
+  sha256 '3d902e6e368fcb0ed07c27874e97341714f0c2db29f91a17e4bc6a0246a62af2'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
   name 'Tor Browser'
