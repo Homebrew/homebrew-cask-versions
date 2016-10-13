1879730b15db923a7637f48e245bab1c3dc7b78a
Robert LU <robberphex@gmail.com>
2016-10-14 02:49:27 +0800

    add Adobe Flash debuggers (#2749)

---
+++ Casks/flash-player-debugger.rb
@@ -0,0 +1,17 @@
+cask 'flash-player-debugger' do
+  version '23.0.0.185'
+  sha256 'f1a7b29230bee1a8125d005d69becc9d6a63e7192291c5478b1f1f5e68d3e9b0'
+
+  # macromedia.com was verified as official when first introduced to the cask
+  url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa.dmg"
+  name 'Adobe Flash Player projector content debugger'
+  homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
+
+  # Renamed to avoid conflict with flash-player.
+  app 'Flash Player.app', target: 'Flash Player Debugger.app'
+
+  zap delete: [
+                '~/Library/Caches/Adobe/Flash Player',
+                '~/Library/Logs/FlashPlayerInstallManager.log',
+              ]
+end
