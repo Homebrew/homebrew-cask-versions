63bd0eab17a0f7147831ae6386f6cd64bb33e833
Victor Popkov <victor@popkov.me>
2016-10-15 11:08:31 +0300

    Update opera-beta to 41.0.2353.23 (#2762)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '41.0.2353.10'
-  sha256 'e3c6bb1104dd179c498ab5f1e9a735b97200688f7f87e8426fd2bd5f527b3f78'
+  version '41.0.2353.23'
+  sha256 '41881d7039d98b577c855fdec2635b9a5a205be307e4277d0f1e8b8bdfe344c4'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
