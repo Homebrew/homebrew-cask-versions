04410b98968b0b8b09fcb803b4dc4c8a3562ab0f
Leonardo Pistone <leonardo@pistone.me>
2016-10-18 02:26:44 +0200

    Update pycharm-ce-eap to 163.6110.26 (#2769)

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-ce-eap' do
-  version '2016.3,163.5219.9'
-  sha256 'aa0f3a61f3a4fbaa08c0b582009040ebd5ec2b53c789a1ed5ea6e8afb3abfe02'
+  version '2016.3,163.6110.26'
+  sha256 'cb342657fb6e480d155ea799c9f6137f4758c43b819501c04a0eed858d5e66f1'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.after_comma}.dmg"
   name 'Jetbrains PyCharm Community Edition EAP'
