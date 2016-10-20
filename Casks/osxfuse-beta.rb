5af461acae8b8966fd68e9a6bd8118a1644a62e8
neutric <neutric@users.noreply.github.com>
2016-10-20 12:29:45 +0200

    Update osxfuse-beta to 3.5.3 (#2788)

--- Casks/osxfuse-beta.rb
+++ Casks/osxfuse-beta.rb
@@ -1,11 +1,11 @@
 cask 'osxfuse-beta' do
-  version '3.5.2'
-  sha256 '49ed3b3cf015cd9ca113de901a57cbd2cd8f4b5afe93259c10b5fddc7256f947'
+  version '3.5.3'
+  sha256 '9d48cbfe360bead9e9fd31bc95e18a90f03be7c4be5b5c62acd7df98c8c0c80b'
 
   # github.com/osxfuse/osxfuse was verified as official when first introduced to the cask
   url "https://github.com/osxfuse/osxfuse/releases/download/osxfuse-#{version}/osxfuse-#{version}.dmg"
   appcast 'https://github.com/osxfuse/osxfuse/releases.atom',
-          checkpoint: 'c03f1a0c1c927f221f97ecee08b1fe6d957f3c81e1a75167654a14f7f54c7262'
+          checkpoint: '10e99a2fd180976d11ac1d3f730bf7415438cf29138e0e7c3fa110f0b569145e'
   name 'OSXFUSE'
   homepage 'https://osxfuse.github.io/'
 
