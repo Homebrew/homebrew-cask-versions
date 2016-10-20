609317985b3492c5c370c08cd79235c0b767031a
Kolen Cheung <christian.kolen@gmail.com>
2016-10-19 17:59:50 -0700

    update multimarkdown-composer-beta and multimarkdown-composer-pro to v3.0b47

--- Casks/multimarkdown-composer-beta.rb
+++ Casks/multimarkdown-composer-beta.rb
@@ -1,6 +1,6 @@
 cask 'multimarkdown-composer-beta' do
-  version '3.0b46'
-  sha256 '673570ff77487dddbe19751a1ab83a2c77eeaa9e3242ac88eefe7b755bf4d636'
+  version '3.0b47'
+  sha256 '44c9fec7f025e43634edd74cfd83599d1c8bbe160fb60abd7605226ccbd63aca'
 
   # files.fletcherpenney.net.s3.amazonaws.com was verified as official when first introduced to the cask
   url "http://files.fletcherpenney.net.s3.amazonaws.com/MultiMarkdown%20Composer%20%28Non-Sandboxed%29-#{version}.zip"
