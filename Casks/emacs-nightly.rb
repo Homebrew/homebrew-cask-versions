fbaa4b9b091260e078943186b3192afd49e34aab
Victor Popkov <victor@popkov.me>
2016-10-21 20:40:03 +0300

    Update emacs-nightly to 2016-10-21_01-44-43-56f8384bb3f20d55693a3a1db8aa2cf490e6d18e

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,10 +1,10 @@
 cask 'emacs-nightly' do
-  version '2016-10-06_01-43-38-6e6c08815347a78008211613c9ddfa0acd7f0ef8'
-  sha256 '53a53cf2c62dcaada4ab6ec0e114ab9ca930688bbba455a90c6d3b316a7b0830'
+  version '2016-10-21_01-44-43-56f8384bb3f20d55693a3a1db8aa2cf490e6d18e'
+  sha256 'afe81b37ebf741db92ee1f2e603a21df2b67cb6ec09d7f70d99f4b9bb6b20b7d'
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
   appcast 'https://emacsformacosx.com/atom/daily',
-          checkpoint: 'e99da4f3923fdfcd3a28efae9fae7e624d070cc210bb45099ff570caef2eab00'
+          checkpoint: 'e852c334334af7d6d2368ae86162f98df15c479abdc9064361580172dfed4ae1'
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
