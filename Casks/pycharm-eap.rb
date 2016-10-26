e574a148e333f745d25c0433ce99db598eee1c31
Michael Ford <fanquake@users.noreply.github.com>
2016-10-26 11:30:01 +0800

    PyCharm EAP 163.6512.5 (#2811)

--- Casks/pycharm-eap.rb
+++ Casks/pycharm-eap.rb
@@ -1,21 +1,21 @@
 cask 'pycharm-eap' do
-  version '162.1812.1'
-  sha256 '33a3a046ec6743b41b5899c404ae7e6fb3a076fe6063afa4190170171bf23878'
+  version '2016.3,163.6512.5'
+  sha256 '9a3503e9aa143ab548e1d6641db52773687f14a13a8c0a1825694d329dd41e73'
 
-  url "https://download.jetbrains.com/python/pycharm-professional-#{version}.dmg"
+  url "https://download.jetbrains.com/python/pycharm-professional-#{version.after_comma}.dmg"
   name 'PyCharm EAP'
   homepage 'https://confluence.jetbrains.com/display/PYH/JetBrains+PyCharm+Preview+(EAP)'
 
   conflicts_with cask: 'pycharm'
 
-  app 'PyCharm 2016.2 EAP.app'
+  app 'PyCharm #{version.before_comma} EAP.app'
 
   uninstall delete: '/usr/local/bin/charm'
 
   zap delete: [
-                '~/Library/Preferences/PyCharm2016.2',
-                '~/Library/Application Support/PyCharm2016.2',
-                '~/Library/Caches/PyCharm2016.2',
-                '~/Library/Logs/PyCharm2016.2',
+                '~/Library/Preferences/PyCharm#{version.before_comma}',
+                '~/Library/Application Support/PyCharm#{version.before_comma}',
+                '~/Library/Caches/PyCharm#{version.before_comma}',
+                '~/Library/Logs/PyCharm#{version.before_comma}',
               ]
 end
