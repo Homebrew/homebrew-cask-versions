279af6e040af53da3a3e54b79cf7373a515496b0
Artem Medvedev <amedvedev@outlook.com>
2016-10-27 20:05:04 +0300

    Update webstorm-eap to EAP,163.6957.17

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'webstorm-eap' do
-  version 'EAP,163.6512.13'
-  sha256 'ebc5360df75bc59bdc243c76c81b9806fee8efc08465f16ac5d97faa01640d67'
+  version 'EAP,163.6957.17'
+  sha256 '48fa7c3b4cf721cf38e787a6e4e90705004c51839a72010ca849bc7534464144'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.before_comma}-#{version.after_comma}.dmg"
   name 'WebStorm EAP'
