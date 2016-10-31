b9886b81bd6be812b15c609427383962a2c27865
neutric <neutric@users.noreply.github.com>
2016-10-31 12:44:00 +0100

    Add OpenSCAD 2016.10.28 (#2834)

---
+++ Casks/openscad-snapshot.rb
@@ -0,0 +1,10 @@
+cask 'openscad-snapshot' do
+  version '2016.10.28'
+  sha256 '2ab89b438fc25bbfc7ebc1bd1aa131f2be3d7b4a70659d1fc5ef67f2c2c618c5'
+
+  url "http://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
+  name 'OpenSCAD'
+  homepage 'http://www.openscad.org/downloads.html#snapshots'
+
+  app 'OpenSCAD.app'
+end
