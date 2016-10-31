228b4d30b173d90b932d2680df722e1ffd283a6f
Michael Ford <fanquake@users.noreply.github.com>
2016-10-31 09:21:25 +0800

    Thunderbird Beta 50.0b2

--- Casks/thunderbird-beta.rb
+++ Casks/thunderbird-beta.rb
@@ -1,18 +1,18 @@
 cask 'thunderbird-beta' do
-  version '49.0b1'
+  version '50.0b2'
 
   language 'en', default: true do
-    sha256 '7edbe8645f4c14fc9fa40646353db96009b5bfa726179bd655568ff7e172874f'
+    sha256 'f924a127173df26ae8eb36c12c924bec5e5ef679325c7ec5390ab7a177208aa2'
     'en-US'
   end
 
   language 'ru' do
-    sha256 '7fb61dd169418bc832758fb2b5f4c5a0fd033fbd17fb53e8f7e2a0f5301ba557'
+    sha256 'c8e1e1d11d7a087d7b0e17e06b92a9680a5510a1710740a3dc5611558b56ccc1'
     'ru'
   end
 
   language 'uk' do
-    sha256 'e25269f3776be5d46b4b60ff7c683f2694b8285392f1c686d4592015373491df'
+    sha256 'e2dccf8b8631fe5d468f1bab2f8976de95bc23f1218b9e8fc4680ba7088c3891'
     'uk'
   end
 
