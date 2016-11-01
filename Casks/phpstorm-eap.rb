0230c1dbc539dd9a54f2a135fcff56515c33bf96
Michael Ford <fanquake@users.noreply.github.com>
2016-11-01 10:21:35 +0800

    PhpStorm EAP 163.6957.13

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,21 +1,21 @@
 cask 'phpstorm-eap' do
-  version '163.6512.15'
-  sha256 '8795d9c220aa13a9006f15ee85a5eda8efe69ec4ba627bbc68ec34b3acf56845'
+  version '2016.3,163.6957.13'
+  sha256 '94ebdbe52bfb9fbf7fbd4e2b62cdb3c59d7aa0484aa13a44a3c4527a1d457604'
 
-  url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version}.dmg"
+  url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version.after_comma}.dmg"
   name 'PhpStorm EAP'
   homepage 'https://confluence.jetbrains.com/display/PhpStorm/PhpStorm+Early+Access+Program'
 
   conflicts_with cask: 'phpstorm'
 
-  app 'PhpStorm 2016.3 EAP.app'
+  app 'PhpStorm #{version.major_minor} EAP.app'
 
   uninstall delete: '/usr/local/bin/pstorm'
 
   zap delete: [
-                '~/Library/Preferences/PhpStorm2016.2',
-                '~/Library/Caches/PhpStorm2016.2',
-                '~/Library/Logs/PhpStorm2016.2',
-                '~/Library/Application Support/PhpStorm2016.2',
+                '~/Library/Preferences/PhpStorm#{version.major_minor}',
+                '~/Library/Caches/PhpStorm#{version.major_minor}',
+                '~/Library/Logs/PhpStorm#{version.major_minor}',
+                '~/Library/Application Support/PhpStorm#{version.major_minor}',
               ]
 end
