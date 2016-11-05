3f519d59dd74b1949ef3256848e502346463b985
Eliezio Oliveira <eliezio.oliveira@gmail.com>
2016-11-05 10:55:30 -0200

    Update clion-eap (2016.3 EAP) to build 163.7342.13 (#2848)

--- Casks/clion-eap.rb
+++ Casks/clion-eap.rb
@@ -1,6 +1,6 @@
 cask 'clion-eap' do
-  version '163.6957.27'
-  sha256 'c0dbaf67f2fe85200c8a86297af700cb3bb37978f78aa1aafdc4bb37e3085623'
+  version '163.7342.13'
+  sha256 'c4dc48b103328f5cae5d4c4e0790a08adb60782735bd03eab187697fe762d346'
 
   url "https://download.jetbrains.com/cpp/CLion-#{version}.dmg"
   name 'CLion EAP'
