7546d19522a2759a329a3103543774ce359b84ff
Kenzie Togami <ket1999@gmail.com>
2016-11-06 09:17:41 -0800

    Upgrade PHPStorm EAP to 2016.3,163.7342.9.

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'phpstorm-eap' do
-  version '2016.3,163.6957.13'
-  sha256 '94ebdbe52bfb9fbf7fbd4e2b62cdb3c59d7aa0484aa13a44a3c4527a1d457604'
+  version '2016.3,163.7342.9'
+  sha256 'f42bce001f593926a32c40b262844f8934f01dfef74d260d3d66017c5e62225b'
 
   url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version.after_comma}.dmg"
   name 'PhpStorm EAP'
