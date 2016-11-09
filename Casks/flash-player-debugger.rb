ac420b1273559fdbc165c400d32359b4eb041cde
Vítor Galvão <info@vitorgalvao.com>
2016-11-09 23:22:05 +0000

    Update flash-player-debugger to 23.0.0.207 (#2861)

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -1,9 +1,11 @@
 cask 'flash-player-debugger' do
-  version '23.0.0.205'
-  sha256 '7abfa624584663588c54015804c8b6a5d26002ab43ef3ad00e3c929a994d29e6'
+  version '23.0.0.207'
+  sha256 'ba05661a347ff596f53bb8a5af6c0acbcef1e7492262a8ba6f80927310d8e7d1'
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
+  appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
+          checkpoint: '50c4e19caa48710cf812e1549e5179124552b4bf760dc9aa719e09dd86d10fbe'
   name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
