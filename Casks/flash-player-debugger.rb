5104f9136992f19c705cc4ad1e778c20123861b1
Vítor Galvão <info@vitorgalvao.com>
2016-11-09 22:53:47 +0000

    Update flash-player-debugger.rb

--- Casks/flash-player-debugger.rb
+++ Casks/flash-player-debugger.rb
@@ -4,6 +4,8 @@ cask 'flash-player-debugger' do
 
   # macromedia.com was verified as official when first introduced to the cask
   url "https://fpdownload.macromedia.com/pub/flashplayer/updaters/#{version.major}/flashplayer_#{version.major}_sa_debug.dmg"
+  appcast 'http://fpdownload2.macromedia.com/get/flashplayer/update/current/xml/version_en_mac_pep.xml',
+          checkpoint: '50c4e19caa48710cf812e1549e5179124552b4bf760dc9aa719e09dd86d10fbe'
   name 'Adobe Flash Player projector content debugger'
   homepage 'https://www.adobe.com/support/flashplayer/debug_downloads.html'
 
