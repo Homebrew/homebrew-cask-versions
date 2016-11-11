a5034526edecf2f5234dbe9b3b0b1d472caf3808
Gabriel Stefanini <gabrielusvicente@gmail.com>
2016-11-10 22:09:00 -0200

    Update dbeaver-community to 3.7.8 (#2869)

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,10 +1,10 @@
 cask 'dbeaver-community' do
-  version '3.7.6'
-  sha256 '25f3508e47b77f039d89de090112908c1e50760885db307d9b930e1a94bb5d55'
+  version '3.7.8'
+  sha256 '161e2ad5ead5b4d40a834471e75db32ddebc666e4e1c8f7cfe7437f39c997366'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
   homepage 'http://dbeaver.jkiss.org/'
 
-  app 'Dbeaver.app'
+  app 'DBeaver.app'
 end
