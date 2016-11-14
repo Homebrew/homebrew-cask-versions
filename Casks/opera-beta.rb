766296a0fa9528b81fee39336398521554e39d41
Victor Popkov <victor@popkov.me>
2016-11-14 22:13:00 +0200

    Update opera-beta homepage (https)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -4,7 +4,7 @@ cask 'opera-beta' do
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
-  homepage 'http://www.opera.com/computer/beta'
+  homepage 'https://www.opera.com/computer/beta'
 
   app 'Opera Beta.app'
 end
