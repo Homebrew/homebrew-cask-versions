91feaec97bf49eae74ddc3652a08b297f553576d
Victor Popkov <victor@popkov.me>
2016-11-14 22:13:54 +0200

    Update umsatz-mini homepage (https)

--- Casks/umsatz-mini.rb
+++ Casks/umsatz-mini.rb
@@ -4,7 +4,7 @@ cask 'umsatz-mini' do
 
   url 'http://umsatz-programm.de/ladungen/UmsatzMini2015.zip'
   name 'Umsatz Mini 2015'
-  homepage 'http://umsatz-programm.de'
+  homepage 'https://umsatz-programm.de'
 
   app 'Umsatz Mini 2015.app'
 end
