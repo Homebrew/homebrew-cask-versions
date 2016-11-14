f7298e6addc04e3bef80a8fc1fcdb17e9ec434b6
Maciej Matys <metyl@users.noreply.github.com>
2016-11-14 12:34:26 +0100

    Add next to the name

--- Casks/intellij-idea-next-ce-eap.rb
+++ Casks/intellij-idea-next-ce-eap.rb
@@ -3,7 +3,7 @@ cask 'intellij-idea-next-ce-eap' do
   sha256 'b3bb048d00c4a1f8a4e1e2d2f601be30f4a031752be4048fc71d07d802970440'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version.after_comma}.dmg"
-  name 'IntelliJ IDEA Community Edition EAP'
+  name 'IntelliJ IDEA Next Community Edition EAP'
   homepage "https://confluence.jetbrains.com/display/IDEADEV/IDEA+#{version.major_minor}+EAP"
 
   auto_updates true
