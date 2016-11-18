e2df5e8b40fc2017175e8db9289e5ae17db1cbb2
Sidney <sidney.bofah@googlemail.com>
2016-11-18 03:20:28 +0100

    Update ableton-live-beta to 9.7.1b3 (#2889)

--- Casks/ableton-live-beta.rb
+++ Casks/ableton-live-beta.rb
@@ -1,6 +1,6 @@
 cask 'ableton-live-beta' do
-  version '9.7b4'
-  sha256 '5bc6d02891df5702af956f29efab70223fd5ca92a3746b96d84bc1abd8cd8ac0'
+  version '9.7.1b3'
+  sha256 '8d5744c93b7d1f0346427a8bf945bdc7343b6f707eb4d9450365552557d54f80'
 
   url "http://cdn2-downloads.ableton.com/channels/#{version}/ableton_live_beta_#{version.no_dots}_64.dmg"
   name 'Ableton Live Beta'
