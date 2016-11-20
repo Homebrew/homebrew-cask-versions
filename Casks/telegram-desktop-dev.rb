b95ee7d2aa626b7d752bc052aed335bccd2fd97f
Victor Popkov <victor@popkov.me>
2016-11-20 23:09:12 +0200

    Update telegram-desktop-dev homepage (domain)

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -2,9 +2,10 @@ cask 'telegram-desktop-dev' do
   version '0.10.2.alpha'
   sha256 '5ecbaa9dc519e6a0e0be23ce4bf6db8fcef2305203aaa216dcd1232e1c918a9d'
 
+  # tdesktop.com was verified as official when first introduced to the cask
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   name 'Telegram'
-  homepage 'https://tdesktop.com/'
+  homepage 'https://desktop.telegram.org/'
 
   depends_on macos: '>= :mountain_lion'
 
