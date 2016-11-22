96905ad99ed4c2253c7fe58ca0433989a4444baa
Vítor Galvão <info@vitorgalvao.com>
2016-11-22 20:42:21 +0000

    visual-paradigm-ce.rb: it now uses app installer

--- Casks/visual-paradigm-ce.rb
+++ Casks/visual-paradigm-ce.rb
@@ -2,16 +2,10 @@ cask 'visual-paradigm-ce' do
   version '13.2,20161101'
   sha256 '365da7caed0765fc86a0fec105648268587abb162fd97d037c6fcea803a408aa'
 
-  url "https://eu6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.dots_to_underscores.gsub(',', '_')}_OSX_WithJRE.dmg"
+  url "https://eu6.visual-paradigm.com/visual-paradigm/vpce#{version.before_comma}/#{version.after_comma}/Visual_Paradigm_CE_#{version.before_comma.dots_to_underscores}_#{version.after_comma}_OSX_WithJRE.dmg"
   name 'Visual Paradigm'
   homepage 'https://www.visual-paradigm.com/'
 
-  installer script: 'Visual Paradigm.app/Contents/MacOS/JavaApplicationStub',
-            args:   ['-q'],
-            sudo:   false
-
-  uninstall script: {
-                      executable: "/Applications/Visual Paradigm CE #{version.before_comma}/uninstaller/uninstall.app/Contents/MacOS/JavaApplicationStub",
-                      args:       ['-q'],
-                    }
+  # Renamed to avoid conflict with visual-paradigm.
+  app 'Visual Paradigm.app', target: 'Visual Paradigm CE.app'
 end
