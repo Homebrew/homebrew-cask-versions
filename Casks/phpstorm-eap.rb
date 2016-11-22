af95d7c8ccf4dd0811be0ac6e20cb9516f521bc0
Aurimas Niekis <aurimas@niekis.lt>
2016-11-22 14:05:30 +0100

    Updated PHPStorm EAP version to 2016.3,163.7743.39 (#2908)

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'phpstorm-eap' do
-  version '2016.3,163.7342.9'
-  sha256 'f42bce001f593926a32c40b262844f8934f01dfef74d260d3d66017c5e62225b'
+  version '2016.3,163.7743.39'
+  sha256 '8e0204dd0b6dc39bd6ccd1fd7c80389463b284db8462f2a772f58b02ccb41280'
 
   url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version.after_comma}.dmg"
   name 'PhpStorm EAP'
