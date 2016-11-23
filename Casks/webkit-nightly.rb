157c52386a6887809ba7012006ee170e93221e77
Cheong Yip <wingy3181@gmail.com>
2016-11-24 08:09:17 +1100

    Fix sha256 value for Webkit Nightly r208420 (#2915)

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -1,6 +1,6 @@
 cask 'webkit-nightly' do
   version 'r208420'
-  sha256 '80926485ab5f0c4731f811a7955338c166c72549817826cf60bb6fd75a36d274'
+  sha256 'a7341ff5af993b15ac9f1cab0a91c77a74ac98ba83877439738dc90a2deefebd'
 
   url "https://builds-nightly.webkit.org/files/trunk/mac/WebKit-SVN-#{version}.dmg"
   name 'WebKit Nightly'
