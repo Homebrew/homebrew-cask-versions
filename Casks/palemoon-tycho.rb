f94b087e475a48353ac53a889fb0b0993c2d8755
Conor Randall <ibrokemypie@bastardi.net>
2016-11-28 09:44:39 +1100

    Add Pale Moon Tycho Version 27.0.0
    Adds Pale Moon version 27.0.0, codename Tycho. Putting in versions as version 26.5.0 is stable before Tycho rebase which breaks functionality.

---
+++ Casks/palemoon-tycho.rb
@@ -0,0 +1,10 @@
+cask 'palemoon-tycho' do
+  version '27.0.0'
+  sha256 '59504762c6935b35ca33b06f31cb799cb5dedfb9ca04a527dd83dab0991b01c1'
+
+  url 'https://mac.palemoon.org/dist/palemoon-27.0.0.mac64.dmg'
+  name 'Pale Moon Tycho'
+  homepage 'https://www.palemoon.org/'
+
+  app 'PaleMoon.app'
+end
