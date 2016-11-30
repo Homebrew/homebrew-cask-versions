2a491dc03243823d5c803b0a22dc2c1c595f5f04
Conor Randall <ibrokemypie@bastardi.net>
2016-11-30 23:31:22 +1100

    Update palemoon.rb to 27.0.1

--- Casks/palemoon.rb
+++ Casks/palemoon.rb
@@ -1,6 +1,6 @@
 cask 'palemoon' do
-  version '27.0.0'
-  sha256 '59504762c6935b35ca33b06f31cb799cb5dedfb9ca04a527dd83dab0991b01c1'
+  version '27.0.1'
+  sha256 'b004dabe39b79cbe9afe39d5c43c3ca6152c78b29a118e335426b1215e0aa94b'
 
   url "https://mac.palemoon.org/dist/palemoon-#{version}.mac64.dmg"
   name 'Pale Moon'
