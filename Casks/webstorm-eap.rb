c012ecfad82e2625a392d45e3b2022a8b34ae271
Artem Medvedev <amedvedev@outlook.com>
2016-12-01 20:00:17 +0200

    Update webstorm-eap to 2016.3.2 EAP,163.9166.8

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,14 +1,14 @@
 cask 'webstorm-eap' do
-  version 'RC,2016.3'
-  sha256 'a5ca297846ba9403e8db07c9c638890f81553719b8e075d8e15ec6743f662a11'
+  version '2016.3.2 EAP,163.9166.8'
+  sha256 'b22e658a8a8df9cbdeac77bd176f4f51e2ad3b48eb3d96a45fa49892ae520d91'
 
-  url "https://download.jetbrains.com/webstorm/WebStorm-#{version.after_comma}-#{version.before_comma}.dmg"
+  url "https://download-cf.jetbrains.com/webstorm/WebStorm-EAP-#{version.after_comma}.dmg"
   name 'WebStorm EAP'
   homepage 'https://confluence.jetbrains.com/display/WI/WebStorm+EAP'
 
   conflicts_with cask: 'webstorm'
 
-  app 'WebStorm.app'
+  app "WebStorm #{version.before_comma}.app"
 
   uninstall delete: '/usr/local/bin/wstorm'
 
