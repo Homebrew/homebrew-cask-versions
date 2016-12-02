ef2765fd9164d613029aa5bf7735f6494b394cb3
Artem Medvedev <amedvedev@outlook.com>
2016-12-02 23:33:09 +0200

    Update 1password-beta to 6.6.BETA-1 (#2959)

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask '1password-beta' do
-  version '6.5.2.BETA-1'
-  sha256 'b3e599270f78f120ed4be2a4e7e7a57fa9d9a902de257d6decac697989c418a9'
+  version '6.6.BETA-1'
+  sha256 'f19a61e5b159b9d594d9f2a4a22bffc68e68c20d58fc6a64032a6038af1fd137'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
