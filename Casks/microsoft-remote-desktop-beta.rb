11a50d82fe12f5b2b7ce769c4a212aec13f4a122
Eric Veenendaal <eric@veenendaal.co>
2016-12-07 10:59:08 -0600

    Adding appcast
    microsoft-remote-desktop-beta 8.2.29

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -3,10 +3,12 @@ cask 'microsoft-remote-desktop-beta' do
   sha256 '6cd39d7ea3be3458ed9936bb083e5a71dcad9dd129cbb50fafa9584f89d7c982'
 
   url 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/70?format=zip&avtoken=c56d931d468c2e8955b6c3755489de04b2d1601c'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06'
   name 'Microsoft Remote Desktop Beta'
   homepage 'https://rink.hockeyapp.net/apps/5e0c144289a51fca2d3bfa39ce7f2b06/'
 
   app 'Microsoft Remote Desktop Beta.app'
+
   zap delete: [
                 '~/Library/Preferences/com.microsoft.rdc.osx.beta',
                 '~/Library/Caches/com.microsoft.rdc.osx.beta',
