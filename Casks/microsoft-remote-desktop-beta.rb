d96ed4ed8f8a33c59edf671720b54d82d781b2f1
Vítor Galvão <info@vitorgalvao.com>
2016-12-07 19:27:05 +0000

    Update microsoft-remote-desktop-beta.rb

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,8 +1,8 @@
 cask 'microsoft-remote-desktop-beta' do
-  version '8.2.29'
+  version '8.2.29,70'
   sha256 '6cd39d7ea3be3458ed9936bb083e5a71dcad9dd129cbb50fafa9584f89d7c982'
 
-  url 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/70?format=zip&avtoken=c56d931d468c2e8955b6c3755489de04b2d1601c'
+  url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
           checkpoint: 'df69689859ed2ac4659bb3a3d162bdb5b87fe707f937f966994748d55bc90288'
   name 'Microsoft Remote Desktop Beta'
