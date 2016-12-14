371cf834f8fac783f17883de54a07b0010f0dcfe
Alexander Hamann <neutric@users.noreply.github.com>
2016-12-14 16:32:32 +0100

    Update phpstorm-eap to 2016.3.2,163.10154.2

--- Casks/phpstorm-eap.rb
+++ Casks/phpstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'phpstorm-eap' do
-  version '2016.3.1,163.9166.11'
-  sha256 '2aa42446f5738dd4ad3d7acac02458b8cc7d72820670611edeb61b30161f3d86'
+  version '2016.3.2,163.10154.2'
+  sha256 '44835d034c2855d036822adf5b9f15bb74f2ba03d8ae0402559693541f92f78b'
 
   url "https://download.jetbrains.com/webide/PhpStorm-EAP-#{version.after_comma}.dmg"
   name 'PhpStorm EAP'
@@ -13,9 +13,9 @@ cask 'phpstorm-eap' do
   uninstall delete: '/usr/local/bin/pstorm'
 
   zap delete: [
-                "~/Library/Preferences/PhpStorm#{version.major_minor}",
+                "~/Library/Application Support/PhpStorm#{version.major_minor}",
                 "~/Library/Caches/PhpStorm#{version.major_minor}",
                 "~/Library/Logs/PhpStorm#{version.major_minor}",
-                "~/Library/Application Support/PhpStorm#{version.major_minor}",
+                "~/Library/Preferences/PhpStorm#{version.major_minor}",
               ]
 end
