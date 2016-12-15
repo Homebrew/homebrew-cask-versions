c3c6f7f0527ba0daddd8fb6776054365cd336ed8
neutric <neutric@users.noreply.github.com>
2016-12-15 01:32:40 +0100

    Update clion-eap to 2016.3.2,163.10154.14 (#3013)

--- Casks/clion-eap.rb
+++ Casks/clion-eap.rb
@@ -1,19 +1,19 @@
 cask 'clion-eap' do
-  version '163.9166.5'
-  sha256 'e59f30f8c06989fec5204f89d22ad3edbcbe5c34d71e87bbcd1a79802351c1dc'
+  version '2016.3.2,163.10154.14'
+  sha256 '0f1f473b75848b204a21b411703dba91695052678a6c9db0ac4b592f4f59c7a3'
 
-  url "https://download.jetbrains.com/cpp/CLion-#{version}.dmg"
+  url "https://download.jetbrains.com/cpp/CLion-#{version.after_comma}.dmg"
   name 'CLion EAP'
   homepage 'https://confluence.jetbrains.com/display/CLION/Early+Access+Program'
 
   conflicts_with cask: 'clion'
 
-  app 'CLion 2016.3.1 EAP.app'
+  app "CLion #{version.before_comma} EAP.app"
 
   zap delete: [
-                '~/Library/Preferences/CLion2016.3',
-                '~/Library/Application Support/CLion2016.3',
-                '~/Library/Caches/CLion2016.3',
-                '~/Library/Logs/CLion2016.3',
+                "~/Library/Application Support/CLion#{version.major_minor}",
+                "~/Library/Caches/CLion#{version.major_minor}",
+                "~/Library/Logs/CLion#{version.major_minor}",
+                "~/Library/Preferences/CLion#{version.major_minor}",
               ]
 end
