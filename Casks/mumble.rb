f987d31872fbe94a63bc66111f2277dd40e2709c
Kenzie Togami <ket1999@gmail.com>
2016-12-18 16:43:24 -0800

    mumble 1.3.0~1797~g6ac0553~snapshot (#3032)

--- Casks/mumble.rb
+++ Casks/mumble.rb
@@ -1,6 +1,6 @@
 cask 'mumble' do
-  version '1.3.0~1703~gf47df77~snapshot'
-  sha256 '37f206a8261c77652bf1500b91a538a906589480584967c9995186da16492aec'
+  version '1.3.0~1797~g6ac0553~snapshot'
+  sha256 'd07ec0dc0cdcfbd2e96757b77a968a6bd078e39f3c74b7485ff8b465e1e0815c'
 
   url "https://dl.mumble.info/Mumble-#{version}.dmg"
   name 'Mumble'
