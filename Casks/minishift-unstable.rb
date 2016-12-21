6c553abcad80bc5cdbf2b4a54ce76ab163e10ea8
Jeremy Brown <jeremy@tenfourty.com>
2016-12-21 15:42:29 +0100

    fixes to pass `brew cask audit` and `brew cask style`

--- Casks/minishift-unstable.rb
+++ Casks/minishift-unstable.rb
@@ -3,8 +3,7 @@ cask 'minishift-unstable' do
   sha256 '2d4020615bd86b814c678fedbf6072488101c26298a90fb081904c6913d88497'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
-  appcast 'https://github.com/minishift/minishift/releases.atom',
-    checkpoint: 'f78e6fe63a0e0f8f3cae4f97daebe8528e8a38a7d023c6630598b128ee6c8fed'
+  appcast 'https://github.com/minishift/minishift/releases.atom', checkpoint: 'f78e6fe63a0e0f8f3cae4f97daebe8528e8a38a7d023c6630598b128ee6c8fed'
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
@@ -12,7 +11,6 @@ cask 'minishift-unstable' do
   container type: :naked
 
   binary 'minishift-#{version}-darwin-amd64/minishift', target: 'minishift'
-
 
   postflight do
     set_permissions "#{staged_path}/minishift", '0755'
