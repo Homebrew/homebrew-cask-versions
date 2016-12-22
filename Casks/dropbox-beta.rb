ed3cf4c95b26b19f3dd18af7e4c1919bdbfc5f60
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-22 09:27:21 +0800

    Update dropbox-beta.rb

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,6 +1,6 @@
 cask 'dropbox-beta' do
   version '17.3.28'
-  sha256 'ca3bb28124339420a13f4470329ee437a2a8f2fef0091ceb704f3610de96d17d'
+  sha256 '8ff3fbcd6b128f85c7b530010b8b42e24aba486303e976552f0ed975af077abf'
 
   # clientupdates.dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
