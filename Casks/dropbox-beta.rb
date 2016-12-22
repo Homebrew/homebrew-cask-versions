b0bdc5971259bdf2be2aaa6680191df3ce0d6b91
Miccal Matthews <miccal.matthews@gmail.com>
2016-12-22 09:31:00 +0800

    Update dropbox-beta.rb

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,11 +1,11 @@
 cask 'dropbox-beta' do
   version '17.3.28'
-  sha256 '8ff3fbcd6b128f85c7b530010b8b42e24aba486303e976552f0ed975af077abf'
+  sha256 'ca3bb28124339420a13f4470329ee437a2a8f2fef0091ceb704f3610de96d17d'
 
   # clientupdates.dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
   appcast 'https://www.dropbox.com/release_notes/rss.xml',
-          checkpoint: 'bc6e6e917ade0cadefdf23611c726afd4c362a9758062063457e2afed05b5181'
+          checkpoint: '8ff3fbcd6b128f85c7b530010b8b42e24aba486303e976552f0ed975af077abf'
   name 'Dropbox'
   homepage 'https://www.dropboxforum.com/t5/Desktop-client-builds/bd-p/101003016'
 
