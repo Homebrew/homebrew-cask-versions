3bd3859755a0d83afa0ea7d0c7b2878388daafe6
Jeremy Brown <jeremy@tenfourty.com>
2016-12-22 12:55:30 +0100

    fixing style issue

--- Casks/minishift-unstable.rb
+++ Casks/minishift-unstable.rb
@@ -3,7 +3,8 @@ cask 'minishift-unstable' do
   sha256 '2d4020615bd86b814c678fedbf6072488101c26298a90fb081904c6913d88497'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
-  appcast 'https://github.com/minishift/minishift/releases.atom', checkpoint: 'f78e6fe63a0e0f8f3cae4f97daebe8528e8a38a7d023c6630598b128ee6c8fed'
+  appcast 'https://github.com/minishift/minishift/releases.atom',
+          checkpoint: 'f78e6fe63a0e0f8f3cae4f97daebe8528e8a38a7d023c6630598b128ee6c8fed'
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
