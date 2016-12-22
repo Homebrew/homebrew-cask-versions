5a77f537b7f1d80549bf92e6623d53369fcb8e16
Bernd Krannich <bernd@krannich.net>
2016-12-22 21:28:59 +0100

    Update forklift-beta to 3.b7

--- Casks/forklift-beta.rb
+++ Casks/forklift-beta.rb
@@ -1,6 +1,6 @@
 cask 'forklift-beta' do
-  version '3.b6'
-  sha256 '75bf961282e0c90d0d790673917b80d5380bb42b7c4677959797990533d1cfa8'
+  version '3.b7'
+  sha256 '9dadcd800194b29f0db4660a8b55b6229d91ec140526eb89c100aa54f6c61137'
 
   url "http://download.binarynights.com/ForkLift#{version.no_dots}.zip"
   appcast "http://update.binarynights.com/ForkLift#{version.major}/update.xml",
