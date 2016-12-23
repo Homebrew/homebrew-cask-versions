da21f22712141a7b380b1a70cf697d18db9751e9
Joao C Costa <joaocc@users.noreply.github.com>
2016-12-23 11:55:30 +0000

    fix SHA256 for dbeaver-ce 3.8.2

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
   version '3.8.2'
-  sha256 '5e169b4fee723f8a31515eb2ad03b79e57bf0a499c686745e54e08cde51231db'
+  sha256 'c265f3ed323b1da91ffa7cf2b78e01930fa1c11623b63f60045e148d87fb949e'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
