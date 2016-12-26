e46d3162d16eec0ada9f49f99072e4f316f3e333
Nainterceptor <demette.gael@gmail.com>
2016-12-26 17:59:42 +0100

    Update Earlybird v52.0a2
    Mozilla has changed URL to download ealybird (beta channel of thunderbird)

--- Casks/earlybird.rb
+++ Casks/earlybird.rb
@@ -1,8 +1,8 @@
 cask 'earlybird' do
-  version '51.0a2'
+  version '52.0a2'
   sha256 :no_check # required as upstream package is updated in-placea
 
-  url "https://ftp.mozilla.org/pub/mozilla.org/thunderbird/nightly/latest-comm-aurora/thunderbird-#{version}.en-US.mac.dmg"
+  url "https://ftp.mozilla.org/pub/thunderbird/nightly/latest-comm-aurora/thunderbird-#{version}.en-US.mac.dmg"
   name 'Earlybird'
   name 'Thunderbird Nightly'
   homepage 'https://www.mozilla.org/en-US/thunderbird/channel/'
