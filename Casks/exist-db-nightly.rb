c8f7dc8392483bfea61752205e227b2c0580337b
Joe Wicentowski <joewiz@gmail.com>
2016-12-29 01:01:09 -0600

    Remove nokogiri dependency as instructed
    - Replaced nokogiri with io.read.scan as proposed by @claui.
    - Addressed all brew cask style concerns
    - Tested the resulting script and confirmed that it successfully grabbed the latest nightly

--- Casks/exist-db-nightly.rb
+++ Casks/exist-db-nightly.rb
@@ -4,12 +4,12 @@ cask 'exist-db-nightly' do
 
   # static.adamretter.org.uk/exist-nightly was verified as official when first introduced to the cask
   url do
-    require 'nokogiri'
     require 'open-uri'
     base_url = 'http://static.adamretter.org.uk/exist-nightly'
     builds_url = "#{base_url}/table.html"
-    builds_doc = Nokogiri::HTML(open(builds_url))
-    latest_build_filename = builds_doc.xpath('//tr[td/a]').sort_by { |a| a.xpath('td[1]').text }.last.xpath('td[3]/a[3]/@href')
+    latest_build_filename = open(builds_url) do |io|
+      io.read.scan(%r{<tr>.*?<td>(.*?)</td>.*?<a href="([^\"]+)">dmg}m).max[1]
+    end
     "#{base_url}/#{latest_build_filename}"
   end
   name 'eXist-db'
