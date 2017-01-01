46de9701ec32057c45137a86faa9aa10bee80773
Alexander Hamann <neutric@users.noreply.github.com>
2017-01-01 22:41:34 +0100

    Update pycharm-ce-eap to 2017.1,171.2014.23

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,11 +1,11 @@
 cask 'pycharm-ce-eap' do
-  version '2016.3,163.6110.26'
-  sha256 'cb342657fb6e480d155ea799c9f6137f4758c43b819501c04a0eed858d5e66f1'
+  version '2017.1,171.2014.23'
+  sha256 '97bfbf9fda8eb57590170d77f46d22dbdae690f4019560be02bb60359463e403'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.after_comma}.dmg"
   name 'Jetbrains PyCharm Community Edition EAP'
   name 'PyCharm CE EAP'
-  homepage 'https://confluence.jetbrains.com/display/PYH/JetBrains+PyCharm+Preview+(EAP)'
+  homepage 'https://www.jetbrains.com/pycharm/nextversion/'
 
   conflicts_with cask: 'pycharm-ce'
 
@@ -14,9 +14,9 @@ cask 'pycharm-ce-eap' do
   uninstall delete: '/usr/local/bin/charm'
 
   zap delete: [
-                "~/Library/Preferences/PyCharm#{version.before_comma.major_minor}",
                 "~/Library/Application Support/PyCharm#{version.before_comma.major_minor}",
                 "~/Library/Caches/PyCharm#{version.before_comma.major_minor}",
                 "~/Library/Logs/PyCharm#{version.before_comma.major_minor}",
+                "~/Library/Preferences/PyCharm#{version.before_comma.major_minor}",
               ]
 end
