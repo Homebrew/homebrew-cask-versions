91f42de8fe3fb10e1828ce873d10e7def4557880
Maciej Matys <metyl@users.noreply.github.com>
2017-01-02 12:24:49 +0100

    Update intellij-idea-next-eap (2017.1 EAP) to build 171.2014.21

--- Casks/intellij-idea-next-eap.rb
+++ Casks/intellij-idea-next-eap.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-next-eap' do
-  version '2017.1,171.1834.9'
-  sha256 '9cff1d8112b741b061d8dc75d0f8d02ebb78ebd3f46c6adac67abb8bb0dfbd29'
+  version '2017.1,171.2014.21'
+  sha256 '61eabc83c1a3315c3b33b5d1b4a773ef628540595f101285db63aea72c61c1fd'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA Next EAP'
