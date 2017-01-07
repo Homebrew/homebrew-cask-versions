4e9a9816813574c10cd0886a92d062fdf344a20f
Oran Shuster <thristle666@gmail.com>
2017-01-07 23:00:45 +0200

    Update airmail-beta.rb

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,7 +1,6 @@
 cask 'airmail-beta' do
-  version '3.2.402,278'
+  version '3.2.403,279'
   sha256 'b022a6bbea4cbdf61fde8beda3903dd32fed17b2c47331d809c63bd5ae655962'
-
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04',
