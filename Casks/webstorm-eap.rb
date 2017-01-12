cf54f3be7020ca40d4b0e1f5e4326eada4464888
Alexander Hamann <neutric@users.noreply.github.com>
2017-01-12 20:01:14 +0100

    Update webstorm-eap to 2017.1,171.2272.15

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'webstorm-eap' do
-  version '2017.1,171.2014.24'
-  sha256 'a3fbd88f14908e536ab58f1ec24db1cb63a10580f858e1b13dc886ae23559d41'
+  version '2017.1,171.2272.15'
+  sha256 'c9743d1616fb018aacc82c14ede3bc24391d228d23bca68d1a3226c6c434ddaf'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-EAP-#{version.after_comma}.dmg"
   name 'WebStorm EAP'
