6196c7d19a8bf3757594459bf84c107171f71094
Roman <sanyer@users.noreply.github.com>
2017-01-14 14:47:51 +0100

    Update safari-technology-preview to latest (#3136)

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -5,7 +5,7 @@ cask 'safari-technology-preview' do
   if MacOS.version <= :el_capitan
     url 'http://appldnld.apple.com/STP/031-81900-20160926-E7046BDA-8434-11E6-83C5-ADC333D2D062/SafariTechnologyPreview.dmg'
   else
-    url 'http://appldnld.apple.com/STP/031-81899-20160926-E7046BC6-8434-11E6-83C5-ACC333D2D062/SafariTechnologyPreview.dmg'
+    url 'https://secure-appldnld.apple.com/STP/031-95977-20170111-2BE113BA-E6C0-11E6-8F82-EDF6D45B5B9D/SafariTechnologyPreview.dmg'
   end
   name 'Safari Technology Preview'
   homepage 'https://developer.apple.com/safari/download/'
