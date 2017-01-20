124102e49b55c39e646ab27c6cb5e00710f3f932
jdollar <joel.dollarhide@passportinc.com>
2017-01-19 22:09:19 -0500

    Updating kodi-development from rc2 to rc3
    Kodi v17 Krypton has updated from develpoment release rc2 to rc3. Updating the cask to point to the latest development release.

--- Casks/kodi-development.rb
+++ Casks/kodi-development.rb
@@ -1,6 +1,6 @@
 cask 'kodi-development' do
-  version '17.0-Krypton_rc2'
-  sha256 'e7ad26409e822fe9c7514dadd98e5804e521ec3c195f38a926922663ee65a088'
+  version '17.0-Krypton_rc3'
+  sha256 '6f6ef8166ef2ad59c0e4bfd2d1396b9c293c6a029424eb577faf3b70b752a272'
 
   url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   name 'Kodi-Development'
