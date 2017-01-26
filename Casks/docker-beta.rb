f8b7166af8a0a839162fbf7f0525b68f729bad42
Jean-Philippe Roemer <jp@roemer.im>
2017-01-26 03:11:11 +0100

    Update docker-beta to 1.13.0,15140

--- Casks/docker-beta.rb
+++ Casks/docker-beta.rb
@@ -1,10 +1,10 @@
 cask 'docker-beta' do
-  version '1.13.0.15084'
-  sha256 'cd6bc97f4d3b1b70c8202a2921b06f02f9edbdc7d4badba92285f9bacd5ea965'
+  version '1.13.0,15140'
+  sha256 '966faa3505f0ad04005e15ef6cee40de175a0044b4e9d02736713719701871dc'
 
-  url "https://download.docker.com/mac/beta/#{version}/Docker.dmg"
+  url "https://download.docker.com/mac/beta/#{version.after_comma}/Docker.dmg"
   appcast 'https://download.docker.com/mac/beta/appcast.xml',
-          checkpoint: 'd1df9b75158b5125ef8ceee0a9cbfbb2e5c1868d7a4172361336579b74263c45'
+          checkpoint: '219193c2d2400fb15baead71720ec9deceb97180bdca01bf7e4cfb35d1c5d7fa'
   name 'Docker for Mac Beta'
   homepage 'https://www.docker.com/products/docker'
 
