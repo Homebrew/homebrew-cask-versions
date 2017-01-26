6826bc9c2066cf40585ef34e98fcd3fbf192c016
Hugo Stijns <hugo@boosboos.net>
2017-01-26 13:01:27 +0100

    Update DBeaver Community to 3.8.4

--- Casks/dbeaver-community.rb
+++ Casks/dbeaver-community.rb
@@ -1,6 +1,6 @@
 cask 'dbeaver-community' do
-  version '3.8.3'
-  sha256 'e10673c476b9f0849f2fb2d7f77a644922a7bd351cc2897f4bfa7561611b1b56'
+  version '3.8.4'
+  sha256 '134377b618cd782aa9ba86cd97c47f99f4da6739a6f27e1e4ec1b526fe21a984'
 
   url "http://dbeaver.jkiss.org/files/#{version}/dbeaver-ce-#{version}-macos.dmg"
   name 'DBeaver Community Edition'
