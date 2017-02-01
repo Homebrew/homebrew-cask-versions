e4e8048df25815980026180ccf2bc380691824d6
Jean-Philippe Roemer <jp@roemer.im>
2017-02-01 04:13:27 +0100

    Update airmail-beta to 3.2.406,282

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -5,7 +5,7 @@ cask 'airmail-beta' do
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
   appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04',
-          checkpoint: '2e93f5eb8de5cf1ff0ca2c9036aec30335d2ddeddb1205617fb92586a8ecc833'
+          checkpoint: 'ac1c64901488165a98f2fa2f6944f873d2b76507bf83bbb30aecc5f0bc1d905c'
   name 'Airmail'
   homepage 'http://airmailapp.com/beta/'
 
