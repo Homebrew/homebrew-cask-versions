3b2d7a26d4846e72ba4679cd0b03eccaa75ead30
neutric <neutric@users.noreply.github.com>
2017-02-02 22:46:33 +0100

    Update pycharm-ce-eap to 2017.1,171.2822.19 (#3262)

--- Casks/pycharm-ce-eap.rb
+++ Casks/pycharm-ce-eap.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-ce-eap' do
-  version '2017.1,171.2613.10'
-  sha256 '5c1c19bde1fc1ac8cc1f6b9af78fbc47a4ca1f9104cabffe04634fcb2d7a5dcf'
+  version '2017.1,171.2822.19'
+  sha256 '0e283eaedc781e4ad8aeea1aab677a8d1e7960f49f517b3de8357860f1b992c2'
 
   url "https://download.jetbrains.com/python/pycharm-community-#{version.after_comma}.dmg"
   name 'Jetbrains PyCharm Community Edition EAP'
