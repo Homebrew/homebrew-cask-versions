872901b9cf0aa1beab79ab60e8f7bd771d3d7e33
Alexander Hamann <neutric@users.noreply.github.com>
2017-02-08 00:42:28 +0100

    Update vivaldi-snapshot to 1.7.735.39

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,10 +1,10 @@
 cask 'vivaldi-snapshot' do
-  version '1.7.735.36'
-  sha256 '49d9bd5b9efd67bfb6c71c1096f7183e14cbcd700a121b72f8a31ad468e92391'
+  version '1.7.735.39'
+  sha256 'a20cc088a502ca6fc829c3c9218c5ce85096284583d6502ec129e08febe97119'
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml',
-          checkpoint: '74d6a00ed2eab939b15ff011f1b69fb0fe379fc21d8eb6046c95942393c3cd21'
+          checkpoint: '7f322b89efcdd172882b514cfc1b0674877a1181d61cfd7a215fd4d78ea3d6ec'
   name 'Vivaldi'
   homepage 'https://vivaldi.com/'
 
