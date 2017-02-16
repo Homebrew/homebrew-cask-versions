9e5232934d3c2db46f6a25ad482c8fe18cc5ecbc
Joe Wicentowski <joewiz@gmail.com>
2017-02-16 09:52:50 -0500

    Update openrefine-dev to 2.7-rc.1

--- Casks/openrefine-dev.rb
+++ Casks/openrefine-dev.rb
@@ -1,6 +1,6 @@
 cask 'openrefine-dev' do
-  version '2.6-rc.2'
-  sha256 '7703016e4935961ce89c26c0c4c780011329321ed32367082f329dae38704037'
+  version '2.7-rc.1'
+  sha256 'fade2d4a3663f183daa50a87e71ddfbef7a54dfe12c25d728b3e1a5a9fb8286c'
 
   # github.com/OpenRefine/OpenRefine was verified as official when first introduced to the cask
   url "https://github.com/OpenRefine/OpenRefine/releases/download/#{version}/openrefine-mac-#{version}.dmg"
