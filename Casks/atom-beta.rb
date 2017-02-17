6396fa105a18083a938b2653c6aeaae10981c248
Alexander Hamann <neutric@users.noreply.github.com>
2017-02-17 21:56:58 +0100

    Update atom-beta to 1.15.0-beta3

--- Casks/atom-beta.rb
+++ Casks/atom-beta.rb
@@ -1,11 +1,11 @@
 cask 'atom-beta' do
-  version '1.15.0-beta2'
-  sha256 'be7acfd21682e51782d7f74eb0459c260033c9523196ac5fe02afeb8322ab206'
+  version '1.15.0-beta3'
+  sha256 '318082dfd114b05a6826e0bf36c92ec6f9c847046f0ca8df50a4316a2912a583'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: 'f79edc88be01a0fbc9ebd94230475943c8c3d70b336ec5cc69b2b2e2e6bc7629'
+          checkpoint: 'a01ad847b1d124b9e8b10f370d3577cd0f3e4f8af4b47cb0d8091b02628d5c9c'
   name 'Github Atom Beta'
   homepage 'https://atom.io/beta'
 
