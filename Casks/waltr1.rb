119648ff7755241cd403f12d97bd363e6f6567d5
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-17 17:46:36 +0800

    Create waltr1.rb (#3321)
    * Create waltr1.rb
    
    * Update waltr1.rb

---
+++ Casks/waltr1.rb
@@ -0,0 +1,11 @@
+cask 'waltr1' do
+  version :latest
+  sha256 :no_check
+
+  # devmate.com/com.softorino.Waltr was verified as official when first introduced to the cask
+  url 'https://dl.devmate.com/com.softorino.Waltr/WALTR.zip'
+  name 'WALTR'
+  homepage 'https://softorino.com/waltr'
+
+  app 'Waltr.app'
+end
