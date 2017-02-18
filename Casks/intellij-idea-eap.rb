12aec6d76e8c19b9f72e0a1b58e7f36287f02638
Alexander Hamann <neutric@users.noreply.github.com>
2017-02-18 13:52:19 +0100

    Update intellij-idea-eap to 2016.3.5,163.13906.6

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-eap' do
-  version '2016.3.4,163.12024.11'
-  sha256 '0b24a485d84a249b79d0e486aadfeeb93495256498e39792289b75f50e488ecf'
+  version '2016.3.5,163.13906.6'
+  sha256 'f6a88cf63fe12eabc920c8df1d5fcf95989c788f5ac3997879c9ef870a4dd967'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA EAP'
