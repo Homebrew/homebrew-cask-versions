ca81c38e4c0e0bcc26625213ca16e585917e98b0
Andrew Wade <awade@ligo.caltech.edu>
2017-02-17 16:48:58 -0800

    Update omnigraffle6 to 6.6.2

--- Casks/omnigraffle6.rb
+++ Casks/omnigraffle6.rb
@@ -1,6 +1,6 @@
 cask 'omnigraffle6' do
-  version '6.6.1'
-  sha256 '7671d46ccd0b53a5917b0ccba5971fe1f1d7990b2d636f25c941b11b03c6e23c'
+  version '6.6.2'
+  sha256 'f0b05a654686c42703cddef646a2519235b45d26bd06988a6e644aa96c0eb828'
 
   url "http://downloads.omnigroup.com/software/MacOSX/10.10/OmniGraffle-#{version}.dmg"
   name 'OmniGraffle 6'
