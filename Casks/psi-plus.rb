6ea5d3795cd968674b4102d241be07cfd57ef917
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-21 10:39:14 +0800

    Updates

--- Casks/psi-plus.rb
+++ Casks/psi-plus.rb
@@ -2,10 +2,10 @@ cask 'psi-plus' do
   version '0.16.545.538,2016-04-26'
   sha256 '137730bba82e4050f45f6d7d3c06ebd2e183eb23b30df93d35bb5d1563fae79d'
 
-  # downloads.sourceforge.net/project/psiplus was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/project/psiplus/Mac-OS-X/psi-plus-#{version.before_comma}-qt4-git-#{version.after_comma}-macosx.dmg"
+  # downloads.sourceforge.net/psiplus/Mac-OS-X was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/psiplus/Mac-OS-X/psi-plus-#{version.before_comma}-qt4-git-#{version.after_comma}-macosx.dmg"
   appcast 'https://sourceforge.net/projects/psiplus/rss',
-          checkpoint: '6bfd1ea5481b4565924ecad3a1a631808fa9b7a484d14bb72d624365338183c4'
+          checkpoint: 'c2997be42f402406e09df8a5bae045270400021852728c0cb2b6d8d119dfc0e8'
   name 'Psi+'
   homepage 'http://psi-plus.com/'
 
