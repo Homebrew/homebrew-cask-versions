d57aa453cfcd938080a3e8ca90ce47dbabf5b600
Aditya Dalal <adidalal@users.noreply.github.com>
2017-02-23 06:54:07 -0500

    Update dash3 to 3.4.4

--- Casks/dash3.rb
+++ Casks/dash3.rb
@@ -1,10 +1,10 @@
 cask 'dash3' do
-  version '3.4.3'
-  sha256 '752c7b966709afa35759396b0b79172e8d2c54aff4c00ba9c01b3dd294df786b'
+  version '3.4.4'
+  sha256 'a7fd1a759965d26a0de6a43b32aec857e3222fd02d4cfc6d0ef66a92da63d526'
 
   url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
   appcast "https://kapeli.com/Dash#{version.major}.xml",
-          checkpoint: '8f674d3070ac1650ccdb286030e843c5ba41afdd4a5df002a9c956193bc35a89'
+          checkpoint: '51e03215a540b6316b8f3212843645dcba5eb719be296ba23db956dca2aea48d'
   name 'Dash'
   homepage 'https://kapeli.com/dash'
 
