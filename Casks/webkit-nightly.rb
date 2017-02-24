6a66fac244b4745cbb5cc79288ab278671e9d0a1
Samuel3008 <samuel.badr@gmail.com>
2017-02-24 11:17:28 +0100

    Update webkit-nightly to r212809 (#3356)

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -1,6 +1,6 @@
 cask 'webkit-nightly' do
-  version 'r208420'
-  sha256 'a7341ff5af993b15ac9f1cab0a91c77a74ac98ba83877439738dc90a2deefebd'
+  version 'r212809'
+  sha256 '771c0a92b927c4974efcd4bd3095fe90e3949c52bf4f570c8cb9931da67ec0cf'
 
   url "https://builds-nightly.webkit.org/files/trunk/mac/WebKit-SVN-#{version}.dmg"
   name 'WebKit Nightly'
