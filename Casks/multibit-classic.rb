a3d2ea6ab9e903b31152ddd3a050b2d2cc951692
Miccal Matthews <miccal.matthews@gmail.com>
2017-02-28 06:25:19 +0800

    Create multibit-classic.rb

---
+++ Casks/multibit-classic.rb
@@ -0,0 +1,11 @@
+cask 'multibit-classic' do
+  version '0.5.19'
+  sha256 'f84aefa0b3762e36659ea3e71806f747db4198641d658d88c8772978b23f99dc'
+
+  url "https://multibit.org/releases/multibit-classic/multibit-classic-#{version}/multibit-classic-macos-#{version}.dmg"
+  name 'MultiBit'
+  homepage 'https://multibit.org/'
+  gpg "#{url}.asc", key_id: '23f7fb7b'
+
+  app 'MultiBit.app'
+end
