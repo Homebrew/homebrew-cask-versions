8d7a17b4bb015ae3ebc4970cf0c841127f86a84b
Maciej Matys <metyl@users.noreply.github.com>
2017-03-03 19:26:39 +0100

    Update IntelliJ IDEA 2017.1 EAP platform to Public Preview build 171.3691.12 (#3400)
    * Update intellij-idea-next-eap (2017.1 EAP) to build 171.3691.12
    * Update intellij-idea-next-ce-eap (2017.1 EAP) to build 171.3691.12

--- Casks/intellij-idea-next-eap.rb
+++ Casks/intellij-idea-next-eap.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-next-eap' do
-  version '2017.1,171.3566.24'
-  sha256 '3e0b5db19f552ba1d5964c3702073849136200a058cd7731ec497bac13895bb2'
+  version '2017.1,171.3691.12'
+  sha256 '1ca4bdcb08d21416addff2bb9ab843765a136d2594032386f86909495887809e'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA Ultimate next version EAP'
