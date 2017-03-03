b62b9fe3d18901481e660be28c1d13ac48ca95b3
Maciej Matys <metyl@users.noreply.github.com>
2017-03-03 14:01:57 +0100

    Update IntelliJ IDEA 2016.3.5 EAP platform to RC build 163.13906.16 (#3399)
    * Update intellij-idea-eap (2016.3 EAP) to build 163.13906.16
    
    * Update intellij-idea-ce-eap (2016.3 EAP) to build 163.13906.16

--- Casks/intellij-idea-ce-eap.rb
+++ Casks/intellij-idea-ce-eap.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-ce-eap' do
-  version '2016.3.5,163.13906.6'
-  sha256 'd7309b1244b7ebb1bbda2df893c3122fea192765d2394f137167d1258acf5900'
+  version '2016.3.5,163.13906.16'
+  sha256 '9cfb98432a17fbc3c11b4dfe8f89b867bbe53fcd236d597d8ad2000a00b629d6'
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA Community Edition EAP'
@@ -9,7 +9,7 @@ cask 'intellij-idea-ce-eap' do
 
   auto_updates true
 
-  app "IntelliJ IDEA #{version.before_comma} CE EAP.app"
+  app 'IntelliJ IDEA CE.app'
 
   uninstall delete: '/usr/local/bin/idea'
 
