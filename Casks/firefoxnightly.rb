ad54afa8d80ca31e78e0003105b7b6a6b86f437f
Lukáš Hlubuček <lukas.hlubucek@gmail.com>
2017-03-07 07:11:55 +0100

    Fix url for firefox nightly

--- Casks/firefoxnightly.rb
+++ Casks/firefoxnightly.rb
@@ -1,24 +1,8 @@
 cask 'firefoxnightly' do
-  version '53.0a1'
+  version '55.0a1'
   sha256 :no_check # required as upstream package is updated in-place
 
-  language 'en', default: true do
-    'en-US'
-  end
-
-  language 'ja' do
-    'ja-JP-mac'
-  end
-
-  language 'ru' do
-    'ru'
-  end
-
-  language 'uk' do
-    'uk'
-  end
-
-  url "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central#{language == 'en-US' ? '' : '-l10n'}/firefox-#{version}.#{language}.mac.dmg"
+  url "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-#{version}.en-US.mac.dmg"
   name 'Mozilla Firefox'
   homepage 'https://www.mozilla.org/en-US/firefox/channel/desktop/'
 
