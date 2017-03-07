54b37da833fef14e32fb8904bfe13cd5192b125d
Victor Popkov <victor@popkov.me>
2017-03-07 11:40:23 +0200

    Update docker-beta to 17.03.0-ce-mac2,15657 (#3415)

--- Casks/docker-beta.rb
+++ Casks/docker-beta.rb
@@ -1,10 +1,10 @@
 cask 'docker-beta' do
-  version '17.03.0-ce-mac1,15587'
-  sha256 '6a3bada125706a4ac99608dd61d70ea55b0a8ab78740ee41d1f0d29e84c96870'
+  version '17.03.0-ce-mac2,15657'
+  sha256 '14d72c0c986d4955e12fa3cba094e9c2a4b03ed244fa43151661cf537965ef1c'
 
   url "https://download.docker.com/mac/beta/#{version.after_comma}/Docker.dmg"
   appcast 'https://download.docker.com/mac/beta/appcast.xml',
-          checkpoint: '35a2c3188a02bb52839ebb16e71273f104a676ab52ffae7323171960eb87c3aa'
+          checkpoint: 'b2690032da2b2bd33b0e8ec005dad379f1a8064bd7137bda4123aed075daabd1'
   name 'Docker for Mac Beta'
   homepage 'https://www.docker.com/community-edition'
 
