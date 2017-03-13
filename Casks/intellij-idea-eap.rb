d6938a9a82b7dd5a93f26ac8dd5dec798b953b1c
Maciej Matys <metyl@users.noreply.github.com>
2017-03-13 21:35:51 +0100

    Revert "Update intellij-idea-eap to 2017.1,171.3780.15 (#3446)"
    This reverts commit ec5ae65ba89ee0d0b2bd9eb91e089d4d1843a9cf.

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,8 +1,8 @@
 cask 'intellij-idea-eap' do
-  version '2017.1,171.3780.15'
-  sha256 '26071b76c684797a3720fbcec116c0616950ee0d42e762215ebe188c39de9277'
+  version '2016.3.5,163.13906.16'
+  sha256 '136e679a751c4a098b2c7195efa526f8b3b51b010a96f568c875986b8eb11a71'
 
-  url "https://download-cf.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA Ultimate EAP'
   homepage "https://confluence.jetbrains.com/display/IDEADEV/IDEA+#{version.major_minor}+EAP"
 
