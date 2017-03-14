13b81701ac73ffe80cffadee3e669feaf6e657f0
Victor Popkov <victor@popkov.me>
2017-03-14 03:33:04 +0200

    Update royal-tsx-beta to 3.1.1.1 (#3450)

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,10 +1,10 @@
 cask 'royal-tsx-beta' do
-  version '3.1.0.1000'
-  sha256 'ab2985a14a46603e7b374d0280a7b23d09ffc48a8cc572d799820b812a6942da'
+  version '3.1.1.1'
+  sha256 '706c6d7710af4306723a682d897f3a22be9e282b89022a76ef3d9dd7a3cbb53e'
 
   url "https://royaltsx-v3.royalapplications.com/updates/royaltsx_#{version}.dmg"
   appcast 'https://royaltsx-v3.royalapplications.com/updates_beta.php',
-          checkpoint: '3f43633839fc91f71275134e6ad3249d8cd93a5bd37e98e10efe2325c7aba984'
+          checkpoint: '6772f2a49d3508cad94f8cc363549275bb5ca35342d38cd01a127ec93133107c'
   name 'Royal TSX'
   homepage 'https://www.royalapplications.com/ts/osx/features'
 
