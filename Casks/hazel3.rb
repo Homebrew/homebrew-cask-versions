dd5530d557dfa5c12166b24e891c231d7175c924
Konrad Kierys <konrad.kierys@gmail.com>
2017-03-15 00:54:14 +0100

    Add Hazel v3.3.8

---
+++ Casks/hazel3.rb
@@ -0,0 +1,16 @@
+cask 'hazel3' do
+  version '3.3.8'
+  sha256 '7bcea2495aacd3245a42e4dafc3f0cec6bde5f1dacdd9b53bea6560a6bff8f50'
+
+  url "http://www.noodlesoft.com/Downloads/Hazel-#{version}.dmg"
+  name 'Hazel'
+  homepage 'https://www.noodlesoft.com/'
+
+  prefpane 'Install Hazel.app/Contents/Resources/Hazel.prefPane'
+
+  zap delete: [
+                '~/Library/Application Support/Hazel',
+                '~/Library/Preferences/com.noodlesoft.Hazel.plist',
+                '~/Library/Preferences/com.noodlesoft.HazelHelper.plist',
+              ]
+end
