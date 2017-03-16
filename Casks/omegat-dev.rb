4d344d3c83e52dcd355904bf1a39caf39c4c5595
Victor Popkov <victor@popkov.me>
2017-03-16 23:00:46 +0200

    Update omegat-dev to 4.1.1 (#3461)

--- Casks/omegat-dev.rb
+++ Casks/omegat-dev.rb
@@ -1,11 +1,11 @@
 cask 'omegat-dev' do
-  version '4.1.0'
-  sha256 '32ecddcf71a92973bb3578d009aadfac762742df9b7eacf2cc60c75251f84762'
+  version '4.1.1'
+  sha256 '4a3b03e98e61527f14442a49eb6563b1a6df0d7bf22e22be751e6cff63734739'
 
   # downloads.sourceforge.net/omegat was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/omegat/OmegaT%20-%20Latest/OmegaT%20#{version}/OmegaT_#{version}_Beta_Mac_Signed.zip"
   appcast 'https://sourceforge.net/projects/omegat/rss?path=/OmegaT%20-%20Latest',
-          checkpoint: 'cb4a14253651e4e411a07161493400a78880897bb0e162eb62130b415f400fd8'
+          checkpoint: '7775eec68c7ea509552572bd1304925c654554cac557ee3d0ea5b3cba6522a0e'
   name 'OmegaT Development'
   homepage 'http://www.omegat.org/'
 
