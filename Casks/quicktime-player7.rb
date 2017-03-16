f4b5332338cf94341c06cfbfb6fade5314f573c8
ericpardee <github@ericpardee.com>
2017-03-16 11:44:34 -0700

    version interpolation

--- Casks/quicktime-player7.rb
+++ Casks/quicktime-player7.rb
@@ -2,13 +2,13 @@ cask 'quicktime-player7' do
   version '7.6.6'
   sha256 '954c2376d2d747821614dc802249cf3c708a4792abed08945d7261de3894e759'
 
-  url 'http://support.apple.com/downloads/DL923/en_US/QuickTimePlayer7.6.6_SnowLeopard.dmg'
+  url "http://support.apple.com/downloads/DL923/en_US/QuickTimePlayer#{version}_SnowLeopard.dmg"
   name 'QuickTime Player 7'
   homepage 'https://support.apple.com/kb/dl923'
 
   depends_on macos: '>= :snow_leopard'
 
-  pkg 'QuickTimePlayer7.6.6_SnowLeopard.pkg'
+  pkg "QuickTimePlayer#{version}_SnowLeopard.pkg"
 
-  uninstall pkgutil: 'com.apple.pkg.QuickTimePlayer7.6.6_SnowLeopard'
+  uninstall pkgutil: "com.apple.pkg.QuickTimePlayer#{version}_SnowLeopard"
 end
