175f6b73cee9c1c57d459069691af35c89c87394
neutric <neutric@users.noreply.github.com>
2017-03-17 03:46:13 +0100

    Update clion-eap to RC,2017.1 (#3474)

--- Casks/clion-eap.rb
+++ Casks/clion-eap.rb
@@ -1,19 +1,19 @@
 cask 'clion-eap' do
-  version '2017.1,171.3780.24'
-  sha256 '611e5ced6ebe9566550727490c6373aae01b4ecbcbd3a834acca2c2b4856ed1d'
+  version 'RC,2017.1'
+  sha256 '3d8371cfa8362c1d50cd536ed0309ddf47090abcdb98aeb71b48bc79c589382e'
 
-  url "https://download.jetbrains.com/cpp/CLion-#{version.after_comma}.dmg"
+  url "https://download.jetbrains.com/cpp/CLion-#{version.after_comma}-#{version.before_comma}.dmg"
   name 'CLion EAP'
   homepage 'https://confluence.jetbrains.com/display/CLION/Early+Access+Program'
 
   conflicts_with cask: 'clion'
 
-  app "CLion #{version.before_comma} EAP.app"
+  app "CLion #{version.after_comma} EAP.app"
 
   zap delete: [
-                "~/Library/Application Support/CLion#{version.major_minor}",
-                "~/Library/Caches/CLion#{version.major_minor}",
-                "~/Library/Logs/CLion#{version.major_minor}",
-                "~/Library/Preferences/CLion#{version.major_minor}",
+                "~/Library/Application Support/CLion#{version.after_comma.major_minor}",
+                "~/Library/Caches/CLion#{version.after_comma.major_minor}",
+                "~/Library/Logs/CLion#{version.after_comma.major_minor}",
+                "~/Library/Preferences/CLion#{version.after_comma.major_minor}",
               ]
 end
