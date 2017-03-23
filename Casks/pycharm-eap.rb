1aafd397764f307bc19cb7b036c2b8122472d703
neutric <neutric@users.noreply.github.com>
2017-03-23 17:11:31 +0100

    Update pycharm-eap to 2017.1,171.3780.111 (#3513)

--- Casks/pycharm-eap.rb
+++ Casks/pycharm-eap.rb
@@ -1,6 +1,6 @@
 cask 'pycharm-eap' do
-  version '2017.1,171.3780.47'
-  sha256 '45524488a020824fa13512cb4303e0769985247f9183e301528aff5c594390b5'
+  version '2017.1,171.3780.111'
+  sha256 'aeeb9efb861d0cc953179477a326ffa5e19db09192dc13de252030c01126afe1'
 
   url "https://download.jetbrains.com/python/pycharm-professional-#{version.after_comma}.dmg"
   name 'PyCharm EAP'
