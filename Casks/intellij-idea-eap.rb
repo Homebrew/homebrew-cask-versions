a85139176022020029272a943d17e80c0bc2b695
Alexander Hamann <neutric@users.noreply.github.com>
2017-03-27 16:47:46 +0200

    Update intellij-idea-eap to 2016.3.6,163.15188.11

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,14 +1,14 @@
 cask 'intellij-idea-eap' do
-  version '2016.3.6,163.15188.5'
-  sha256 'ec13af0f026169dd2534b611f4aa32c75591e41f179e0e35a4549071a1c64a00'
+  version '2016.3.6,163.15188.11'
+  sha256 '69a7af4773d03927cac5b3427e09dee7a3cae46bf3b7e57e7abbe6d6fe6f72ea'
 
-  url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
   name 'IntelliJ IDEA Ultimate EAP'
   homepage "https://confluence.jetbrains.com/display/IDEADEV/IDEA+#{version.major_minor}+EAP"
 
   auto_updates true
 
-  app "IntelliJ IDEA #{version.before_comma} EAP.app"
+  app 'IntelliJ IDEA.app'
 
   uninstall delete: '/usr/local/bin/idea'
 
