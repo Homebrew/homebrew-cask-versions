026b7245d6e08e66ac90f74babac1d508cb8ace8
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-01 21:36:57 +0200

    Update intellij-idea-eap-nextversion to 2017.1.1,171.4073.17

--- Casks/intellij-idea-eap-nextversion.rb
+++ Casks/intellij-idea-eap-nextversion.rb
@@ -1,6 +1,6 @@
 cask 'intellij-idea-eap-nextversion' do
-  version '2017.1,171.3780.52'
-  sha256 '699ff2dfe76e6b13de19246ccc1746f14fd512ef731ea535ecd60369364798a3'
+  version '2017.1.1,171.4073.17'
+  sha256 'cfed9056b542f33c3685fe9886e87b2977661fe10939a8521a555516739c76ca'
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA Ultimate next version EAP'
@@ -8,7 +8,7 @@ cask 'intellij-idea-eap-nextversion' do
 
   auto_updates true
 
-  app 'IntelliJ IDEA.app'
+  app "IntelliJ IDEA #{version.before_comma} EAP.app"
 
   uninstall delete: '/usr/local/bin/idea'
 
