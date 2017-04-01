104eb0750e297eeecf15c8af5c547a93a402623b
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-01 08:15:08 +0200

    Update visual-studio-code-insiders to 1.11.0,cc46bbf5b0412e464f89280e828d602fbc06a0f4

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code-insiders' do
-  version '1.11.0,c995ecf2e94a00aa7eda81ed842e787b999e83ae'
-  sha256 '2a71829abeb408a53c4fe0606f0f68579b584e2ca2a026ba8d3aad55c4439d36'
+  version '1.11.0,cc46bbf5b0412e464f89280e828d602fbc06a0f4'
+  sha256 '2fc0e6c82cdab9eaef58e08e8c51516060c71f4b239bbb82ecd66ea12da7c29a'
 
   # az764295.vo.msecnd.net was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
