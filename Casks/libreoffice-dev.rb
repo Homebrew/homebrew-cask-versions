af24fa1dfe15b39a03473122cd8ac84eddda1c22
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-06 16:39:00 +0200

    Update libreoffice-dev to 5.3.2.2

--- Casks/libreoffice-dev.rb
+++ Casks/libreoffice-dev.rb
@@ -1,11 +1,11 @@
 cask 'libreoffice-dev' do
-  version '5.3.2.1'
-  sha256 '1f5aae7cca603d10a339dc2ee6ba76db04956afffe2eb80acdd126ebabcdc7be'
+  version '5.3.2.2'
+  sha256 '5b9751480fe659c54cd736f78573dedeecca4199bab8e59bef4f6508c0c91d7e'
 
   # documentfoundation.org/libreoffice was verified as official when first introduced to the cask
   url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   appcast 'https://download.documentfoundation.org/libreoffice/testing/',
-          checkpoint: 'e0dfe7b5050b6b0a824137405b15121d68679581b66d7141b2a0b6d2c0487cdb'
+          checkpoint: '8bb40d554cf67ae4b2f462a18f1c4bdc551e463ab312b918a7b8331eff070ade'
   name 'LibreOffice Fresh Prerelease'
   homepage 'https://www.libreoffice.org/download/pre-releases/'
   gpg "#{url}.asc", key_id: 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
