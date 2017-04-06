dcac564fd50c029d58ff3ada742be92c0135f184
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-06 16:27:53 +0200

    Update iterm2-beta to 3.1.beta.3

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,6 +1,6 @@
 cask 'iterm2-beta' do
-  version '3.1.beta.2'
-  sha256 '734c9fe31761f5da993458e94d198f30921053bde0a0ef1bbedcdfdab07a7114'
+  version '3.1.beta.3'
+  sha256 'f8a49c5160c9a48d1d1f5f4eb12092cb09181bcc327ef70655a65251ce0d253e'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name 'iTerm2'
