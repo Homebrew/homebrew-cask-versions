2dd3eead9cb3092fbb8ea747b7d069ba50599f1a
neutric <neutric@users.noreply.github.com>
2017-04-08 11:17:44 +0200

    Update android-studio-canary to 2.4.0.4,171.3883974 (#3606)

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,6 +1,6 @@
 cask 'android-studio-canary' do
-  version '2.4.0.3,171.3870562'
-  sha256 '41adf1d98060f6a87c3187947894140a33920eb094e537a41cfa4285a58c9ac6'
+  version '2.4.0.4,171.3883974'
+  sha256 'c543f623aa24ace236aba477ccc16a915dfda5c90d1e26d23dc0e98e4a406c08'
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Canary'
