c6e063a6232d540a09399cec0a67bef29e486531
neutric <neutric@users.noreply.github.com>
2017-04-10 21:10:12 +0200

    Update opera-beta to 45.0.2552.225 (#3614)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '45.0.2552.89'
-  sha256 '4003fd26ab33289ea3da4c8ec76c680531198b2f8730034dbb41adf29fc5d943'
+  version '45.0.2552.225'
+  sha256 '34fdb0bb236260d2ca690645da42361f80e9539f8b861881eb0c72cc7fec231a'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
