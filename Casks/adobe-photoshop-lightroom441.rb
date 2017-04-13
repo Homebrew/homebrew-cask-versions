cc6caaa9588e17d9cd9c86080135a4ca02f63ef8
Gobinath Mallaiyan <i@gobinath.com>
2017-04-13 11:24:45 -0400

    Update adobe-photoshop-lightroom441 to 4.4.1

--- Casks/adobe-photoshop-lightroom441.rb
+++ Casks/adobe-photoshop-lightroom441.rb
@@ -2,7 +2,7 @@ cask 'adobe-photoshop-lightroom441' do
   version '4.4.1'
   sha256 '6d703aa5ea0022141c88746829e80c60f1169d746022655937c4196a6ca35dad'
 
-  url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.major}.x/Lightroom_#{version.major}_LS11_mac_#{version.dots_to_underscores}.dmg"
+  url "http://download.adobe.com/pub/adobe/lightroom/mac/#{version.major}.x/Lightroom_#{version.major}_LS11_mac_#{version.dots_to_underscores}.dmg", allow_untrusted: true
   name 'Adobe Photoshop Lightroom'
   homepage 'https://www.adobe.com/products/photoshop-lightroom.html'
 
