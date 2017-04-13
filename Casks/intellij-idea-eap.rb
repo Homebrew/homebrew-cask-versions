8be407765ff2dca511c362fbeb859adbefe45713
Maciej Matys <metyl@users.noreply.github.com>
2017-04-13 14:04:31 +0200

    Update intellij-idea-eap (2017.1.2) to build 171.4249.4

--- Casks/intellij-idea-eap.rb
+++ Casks/intellij-idea-eap.rb
@@ -1,14 +1,14 @@
 cask 'intellij-idea-eap' do
-  version '2016.3.6,163.15188.11'
-  sha256 '69a7af4773d03927cac5b3427e09dee7a3cae46bf3b7e57e7abbe6d6fe6f72ea'
+  version '2017.1.2,171.4249.4'
+  sha256 '2a094a9655de0d845c38af2b10b1f446771063fc1114877e3a9fde69e782f175'
 
-  url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.after_comma}.dmg"
   name 'IntelliJ IDEA Ultimate EAP'
   homepage "https://confluence.jetbrains.com/display/IDEADEV/IDEA+#{version.major_minor}+EAP"
 
   auto_updates true
 
-  app 'IntelliJ IDEA.app'
+  app "IntelliJ IDEA #{version.before_comma} EAP.app"
 
   uninstall_postflight do
     ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'idea') }.each { |path| File.delete(path) if File.exist?(path) }
