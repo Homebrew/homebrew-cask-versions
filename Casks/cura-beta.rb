a553988f99c1bb47c2c463e86d564b3fe4274609
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-14 18:47:12 -0400

    Update cura-beta.rb
    - Use HTTPS

--- Casks/cura-beta.rb
+++ Casks/cura-beta.rb
@@ -2,7 +2,7 @@ cask 'cura-beta' do
   version '2.5.0'
   sha256 'd9267b607da851ac383c78a2b44a1456eecf4d64a85d7c7e41a3e7749a6212dd'
 
-  url "http://software.ultimaker.com/Cura_open_beta/Cura-#{version}-BETA-2-Darwin.dmg"
+  url "https://software.ultimaker.com/Cura_open_beta/Cura-#{version}-BETA-2-Darwin.dmg"
   name 'Cura'
   homepage 'https://ultimaker.com/en/products/cura-software'
 
