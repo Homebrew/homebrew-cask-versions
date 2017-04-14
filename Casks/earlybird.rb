6d60348348e1a2cfda1fe7db099da81ae72e0274
Gobinath Mallaiyan <i@gobinath.com>
2017-04-14 18:40:46 -0400

    Update earlybird to 54.0a2

--- Casks/earlybird.rb
+++ Casks/earlybird.rb
@@ -1,5 +1,5 @@
 cask 'earlybird' do
-  version '52.0a2'
+  version '54.0a2'
   sha256 :no_check # required as upstream package is updated in-placea
 
   url "https://ftp.mozilla.org/pub/thunderbird/nightly/latest-comm-aurora/thunderbird-#{version}.en-US.mac.dmg"
