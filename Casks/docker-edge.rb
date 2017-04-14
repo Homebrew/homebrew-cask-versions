534e93b95c971a6e07ce350172f4cb68a6f9a468
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-14 08:23:16 +0800

    Update and rename docker-beta.rb to docker-edge.rb (#3638)

--- Casks/docker-edge.rb
+++ Casks/docker-edge.rb
@@ -1,11 +1,11 @@
-cask 'docker-beta' do
+cask 'docker-edge' do
   version '17.05.0-ce-rc1-mac8,16582'
   sha256 '82fe284fd4022778324f1030b1675ba827e2ec95f466663fa0645a1b112c43c3'
 
   url "https://download.docker.com/mac/edge/#{version.after_comma}/Docker.dmg"
   appcast 'https://download.docker.com/mac/edge/appcast.xml',
           checkpoint: '4458e68fcf0bb0d80d6d9f64d042d919556690a9bf1fd70e78236b767d27fe4f'
-  name 'Docker for Mac Beta'
+  name 'Docker Community Edition for Mac (Edge)'
   homepage 'https://www.docker.com/community-edition'
 
   auto_updates true
