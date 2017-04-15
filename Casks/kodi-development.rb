ce7fd193c83d5ae887e769dc891764ef3695449c
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-15 00:06:12 -0400

    Update kodi-development to 20170410-7f7f7e8 (#3655)

--- Casks/kodi-development.rb
+++ Casks/kodi-development.rb
@@ -1,8 +1,8 @@
 cask 'kodi-development' do
-  version '17.0-Krypton_rc3'
-  sha256 '6f6ef8166ef2ad59c0e4bfd2d1396b9c293c6a029424eb577faf3b70b752a272'
+  version '20170410-7f7f7e8'
+  sha256 '5fa8d2861514fa9e9f6f7870b5490d393bdc0abf368feac31a7224543221977d'
 
-  url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
+  url "http://mirrors.kodi.tv/nightlies/osx/x86_64/Krypton/kodi-#{version}-Krypton-x86_64.dmg"
   name 'Kodi-Development'
   homepage 'https://kodi.tv/'
 
