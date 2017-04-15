80c25d9e8e4d5ef67d4e75e0d8286faa2e9888cb
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-15 00:06:45 -0400

    Update quickbooks-desktop2014 to 2014 (#3657)

--- Casks/quickbooks-desktop2014.rb
+++ Casks/quickbooks-desktop2014.rb
@@ -1,6 +1,6 @@
 cask 'quickbooks-desktop2014' do
   version '2014'
-  sha256 '1bf5daa4ca99a6e55cc065fa3e3109bfc5f51e793645e7d40a65eaaf4ac43176'
+  sha256 'fb195fe528c6290e565082109e6031149eb3173426928c7adaa064c3920cac1f'
 
   url "https://http-download.intuit.com/http.intuit/Downloads/#{version}/Latest/QuickBooksProMac#{version}.dmg"
   name 'QuickBooks Desktop'
