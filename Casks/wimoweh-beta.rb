6154228fc35fd918c8bce51de529469516d6854c
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-04-15 00:04:18 -0400

    Update wimoweh-beta to 1.1.58.BETA (#3647)

--- Casks/wimoweh-beta.rb
+++ Casks/wimoweh-beta.rb
@@ -2,7 +2,7 @@ cask 'wimoweh-beta' do
   version '1.1.58.BETA'
   sha256 '4c8bd162b78e85018583e46b94eaa81b66d46668918f96fc1049d9bad580fd49'
 
-  url "https://www.serialangels.co.uk/sa-content/uploads/2015/08/Wimoweh.#{version}_.zip"
+  url "https://www.serialangels.co.uk/sa-content/uploads/2016/04/Wimoweh.#{version}_.zip"
   name 'Wimoweh'
   homepage 'http://www.serialangels.co.uk/index.php/wimoweh/'
 
