fd85448ae2b343134c10db7691fd635664f27bda
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-18 07:55:05 +0800

    Update appcode-eap.rb

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -2,7 +2,7 @@ cask 'appcode-eap' do
   version '2017.1.1,171.4073.44'
   sha256 'a8c11b8e1da48ed4851f2fcc395bb124a1c1b99ff1d8cf4b112770c24a529088'
 
-  url 'https://download.jetbrains.com/objc/AppCode-2017.1.1-RC.dmg'
+  url "https://download.jetbrains.com/objc/AppCode-#{version.before_comma}-RC.dmg"
   name 'AppCode EAP'
   homepage 'https://confluence.jetbrains.com/display/OBJC/AppCode+EAP'
 
