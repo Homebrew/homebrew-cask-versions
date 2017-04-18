cf6627627097913b9ef9db78dc06e7fd6d55325c
Jeremy Sarda <hackur@users.noreply.github.com>
2017-04-18 00:13:40 -0700

    Updated EAP Minor Revision

--- Casks/webstorm-eap.rb
+++ Casks/webstorm-eap.rb
@@ -1,6 +1,6 @@
 cask 'webstorm-eap' do
-  version '2017.1.1,171.4073.15'
-  sha256 'f0e5208109cd9992a231732e92955ebfe736993484e72d05a32d206129a6e9b5'
+  version '2017.1.1,171.4073.31'
+  sha256 'e73fe3ae991fb71d9ff0cccbe510579dd4bff2b96c4853c295edb018ab0fdbf9'
 
   url "https://download.jetbrains.com/webstorm/WebStorm-#{version.after_comma}.dmg"
   name 'WebStorm EAP'
