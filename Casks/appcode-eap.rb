60da618d009a1ec841325ceec8e97237769a2999
Bojan Dimovski <bojan@users.noreply.github.com>
2017-04-18 02:10:38 +0200

    Update to AppCode EAP 2017.1.1 RC (171.4073.44). (#3674)
    * Update to AppCode EAP 2017.1.1 RC (171.4073.44).
    
    * Update appcode-eap.rb

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,8 +1,8 @@
 cask 'appcode-eap' do
-  version '2017.1.1,171.4073.29'
-  sha256 '53969819f0b195edd5c3aa8da521afdf2d6dff59caf38dbf704a79e747352a51'
+  version '2017.1.1,171.4073.44'
+  sha256 'a8c11b8e1da48ed4851f2fcc395bb124a1c1b99ff1d8cf4b112770c24a529088'
 
-  url "https://download.jetbrains.com/objc/AppCode-#{version.after_comma}.dmg"
+  url "https://download.jetbrains.com/objc/AppCode-#{version.before_comma}-RC.dmg"
   name 'AppCode EAP'
   homepage 'https://confluence.jetbrains.com/display/OBJC/AppCode+EAP'
 
