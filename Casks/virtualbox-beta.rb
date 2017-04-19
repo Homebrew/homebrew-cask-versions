1122f29d6dbf50141b2964182bdccc11d6a743d0
commitay <commitay1@gmail.com>
2017-04-20 08:25:58 +1000

    Update virtualbox-beta to 5.1.21-114652 (#3690)
    Also, remove OS X Lion and below support.

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -1,11 +1,6 @@
 cask 'virtualbox-beta' do
-  if MacOS.version <= :lion
-    version '4.3.32-103443'
-    sha256 '08defbf310b7ba5852fa8dd951438bb9b1528bb1544211568861986110e807f7'
-  else
-    version '5.1.19-114236'
-    sha256 '977e1af4911e81074ebe95cfe3bad4054431b2d5d66c344efd6905afbf2125de'
-  end
+  version '5.1.21-114652'
+  sha256 '508c260334c20e31c27de5a04e2a60b5523deb96ce927cda797dd93872641d5f'
 
   url "https://www.virtualbox.org/download/testcase/VirtualBox-#{version}-OSX.dmg"
   name 'Oracle VirtualBox'
