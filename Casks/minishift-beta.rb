94b261424dbb87dc2361bc7e574797933adb6ced
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2017-04-20 12:36:56 +0200

    Update minishift-beta to 1.0.0-rc.2 (#3697)

--- Casks/minishift-beta.rb
+++ Casks/minishift-beta.rb
@@ -1,10 +1,10 @@
 cask 'minishift-beta' do
-  version '1.0.0-rc.1'
-  sha256 'be9c96ca8b081b52adbbdaa5fb49380e1be4fc5e048ce336b279f83bcb070af0'
+  version '1.0.0-rc.2'
+  sha256 'e2570cf193704311d99a4f4e189e8eb115678bc15803b702b9f6228d0da26d08'
 
   url "https://github.com/minishift/minishift/releases/download/v#{version}/minishift-#{version}-darwin-amd64.tgz"
   appcast 'https://github.com/minishift/minishift/releases.atom',
-          checkpoint: '88f9d8b5367d3fd4715f4b1c8ae9106019fe8782ea5b963eca91813715ee6227'
+          checkpoint: '6e296c5520db6de506eae461334bb50b1d9303fe5c8d1998f686fc1340e04f46'
   name 'Minishift'
   homepage 'https://github.com/minishift/minishift'
 
