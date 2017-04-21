c876333f115e0585cdd29af7f5738b1a944765a2
Scott Humphries <git@sscotth.io>
2017-04-20 20:53:16 -0500

    Update Audacity Nightly to use dynamic url

--- Casks/audacity-nightly.rb
+++ Casks/audacity-nightly.rb
@@ -1,9 +1,22 @@
 cask 'audacity-nightly' do
-  version '2.2.0-8928bd4'
-  sha256 '28b95d0c29c04726704af44965c1db1883f3baad9404539e3cd146560558b5ac'
+  version :latest
+  sha256 :no_check
 
   # audacity.homerow.net was verified as official when first introduced to the cask
-  url "http://www.audacity.homerow.net/index.php?dir=mac/audacity-nightly-2017.03.31-06.00-8928bd4/&file=audacity-macosx-#{version}.dmg"
+  url do
+    require 'open-uri'
+    base_url = 'http://www.audacity.homerow.net/index.php?dir=mac/'
+    builds_url = "#{base_url}&sort=m_time&sort_mode=d"
+    open(builds_url) do |builds|
+      pattern = %r{(?<=\/index.php\?dir=mac\/)audacity-nightly-\d{4}\.\d{2}\.\d{2}-\d{2}\.\d{2}-[[:xdigit:]]{7}(?=\/\">)}
+      latest_build_directory = "#{base_url}#{pattern.match(builds.read)}"
+      open(latest_build_directory) do |build|
+        pattern = %r{(?<=\/index.php\?dir=mac\/audacity-nightly-\d{4}\.\d{2}\.\d{2}-\d{2}\.\d{2}-[[:xdigit:]]{7}\/&amp;file=)audacity-macosx-\d.*\.dmg(?=\">)}
+        latest_build_filename = pattern.match(build.read).to_s
+        "#{latest_build_directory}&file=#{latest_build_filename}"
+      end
+    end
+  end
   name 'Audacity'
   homepage 'http://www.audacityteam.org/'
 
