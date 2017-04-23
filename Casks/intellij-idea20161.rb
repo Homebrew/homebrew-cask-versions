076cd1c5cd7af4d0013897f1d19b97f076600e6f
Dmitry <condemil@users.noreply.github.com>
2017-04-23 13:15:15 +0200

    Bring back intellij-idea 2016.1 (#3714)
    After removal in https://github.com/caskroom/homebrew-versions/pull/3683

---
+++ Casks/intellij-idea20161.rb
@@ -0,0 +1,25 @@
+cask 'intellij-idea20161' do
+  version '2016.1.4,145.2070'
+  sha256 'c6ed2455cfeeb542c646e6a3d639d32e85ebe8ead87a0f3744dfa3c0a920e2f9'
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
+  name 'IntelliJ IDEA Ultimate'
+  homepage 'https://www.jetbrains.com/idea/'
+
+  auto_updates true
+
+  app 'IntelliJ IDEA.app'
+
+  uninstall_postflight do
+    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'idea') }.each { |path| File.delete(path) if File.exist?(path) }
+  end
+
+  zap delete: [
+                "~/Library/Caches/IntelliJIdea#{version.major_minor}",
+                "~/Library/Logs/IntelliJIdea#{version.major_minor}",
+                "~/Library/Application Support/IntelliJIdea#{version.major_minor}",
+                "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+                '~/Library/Preferences/com.jetbrains.intellij.plist',
+                '~/Library/Saved Application State/com.jetbrains.intellij.savedState',
+              ]
+end
