76f5957e87a8c1a12aae56ad0d17d3ea461cab24
Alexander Hamann <neutric@users.noreply.github.com>
2017-04-24 15:47:17 +0200

    Update opera-developer to 46.0.2573.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask 'opera-developer' do
-  version '46.0.2567.0'
-  sha256 '60bb547ab91487885f9bf36529a85e72ed95c6d1b55ea6382ec095a6ab6582d1'
+  version '46.0.2573.0'
+  sha256 '35b1d6c06a650ed322baa459339126555d4d2ebe7ae4663cf080c7bb33f7fdc6'
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name 'Opera Developer'
