00ac19214f4a86d5afb86a3c16baeca57048ca9f
Miccal Matthews <miccal.matthews@gmail.com>
2017-04-26 08:23:03 +0800

    Update telegram-alpha.rb

--- Casks/telegram-alpha.rb
+++ Casks/telegram-alpha.rb
@@ -5,7 +5,7 @@ cask 'telegram-alpha' do
   # hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f',
-          checkpoint: '11b32a662cd63d3108aa23b4f13deb4100485cdc36387a2f7ea5b35d3f1d228e'
+          checkpoint: '407d011c90c85dc65687d97cbffd9749436fb966f39345ae94c5a99ca3753058'
   name 'Telegram for macOS'
   name 'Telegram Swift'
   homepage 'https://macos.telegram.org/'
