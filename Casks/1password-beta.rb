d537badb2e10dc02fb967ffe9bfb57734073d4e1
Kevin Hwang <kevinhwang@eng.ucsd.edu>
2017-04-27 16:20:01 -0700

    Update 1password-beta to 6.7.1

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask '1password-beta' do
-  version '6.7.BETA-5'
-  sha256 'cd2888013645885d8f1a249f65cbbb718146df655905225d999bdf857b596545'
+  version '6.7.1'
+  sha256 '86144a2e4f39880dd483d1aae3a57ee25ce8f6a9d87c8b4056cfc1c48721faf3'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
