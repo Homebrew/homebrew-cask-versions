b2e7e91e506688b7df99d3c3cc59956c0ffb0ee1
Vítor Galvão <info@vitorgalvao.com>
2017-04-28 22:25:53 +0100

    Update webkit-nightly.rb

--- Casks/webkit-nightly.rb
+++ Casks/webkit-nightly.rb
@@ -4,9 +4,7 @@ cask 'webkit-nightly' do
 
   url do
     require 'open-uri'
-    open('https://webkit.org/downloads/') do |landing_page|
-      landing_page.read[/https:\/\/builds-nightly.webkit.org\/files\/trunk\/mac\/WebKit-SVN-r\d+.dmg/]
-    end
+    open('https://webkit.org/downloads/') { |landing_page| landing_page.read[/https:\/\/builds-nightly.webkit.org\/files\/trunk\/mac\/WebKit-SVN-r\d+.dmg/] }
   end
   name 'WebKit Nightly'
   homepage 'https://webkit.org/downloads/'
