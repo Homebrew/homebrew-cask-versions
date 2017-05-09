9c697502340492f221a05bc01241236ae9968fca
Piotr Pachalko <piotrpachalko@users.noreply.github.com>
2017-05-09 17:48:19 +0200

    Update chirp-daily to 20170501 (#3793)

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,6 +1,6 @@
 cask 'chirp-daily' do
-  version '20170414'
-  sha256 '30e829cc3bc409f68f98ab9e6a0b93283f3a3ae0078763d88f3e0b688faf51b4'
+  version '20170501'
+  sha256 '1429d952e9024ca9237e6e1ad59d06b400ca594fb5383d5b8024bfcc4ae71be7'
 
   url "http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
