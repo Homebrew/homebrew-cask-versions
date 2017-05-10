d27df882a5162f3af91ac7507b6a55b40fe627e9
Gobinath Mallaiyan <i@gobinath.com>
2017-05-10 13:14:16 -0400

    Update electron-beta to 1.6.9

--- Casks/electron-beta.rb
+++ Casks/electron-beta.rb
@@ -1,11 +1,11 @@
 cask 'electron-beta' do
-  version '1.6.8'
-  sha256 '40fb4f098321f1a1988d379e4a617df59513ad8e6faa9a3c741a6934f8366ee4'
+  version '1.6.9'
+  sha256 '02b2393be8b454c06fa2906f127d1fe51708369727b48cd293e1f064c0c8a2fe'
 
   # github.com/electron/electron was verified as official when first introduced to the cask
   url "https://github.com/electron/electron/releases/download/v#{version}/electron-v#{version}-darwin-x64.zip"
   appcast 'https://github.com/electron/electron/releases.atom',
-          checkpoint: '6e4158cccd5bed9d4d5734713f194467bd2798560e9046b5369b68a19421d4ca'
+          checkpoint: '362b5578ff0e87bbfaf94f5ac126e404da2cf3bb41a4dacd2274f4367dbced8b'
   name 'Electron'
   homepage 'https://electron.atom.io/'
 
