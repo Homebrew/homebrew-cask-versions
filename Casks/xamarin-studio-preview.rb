00e6a2c8bee3bf0d65e5c95a6dbc88da2d455810
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2017-05-10 20:55:40 -0400

    Update xamarin-studio-preview to 6.0.0.3668 (#3810)

--- Casks/xamarin-studio-preview.rb
+++ Casks/xamarin-studio-preview.rb
@@ -3,8 +3,8 @@ cask 'xamarin-studio-preview' do
   sha256 '1ed29cac15b64a6bce00f81f7898ac21c55d4c04b33372c0bf61a1d11d5ee389'
 
   url "http://download.xamarin.com/roslyn-preview/XamarinStudio-#{version}.dmg"
-  appcast 'https://static.xamarin.com/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
-          checkpoint: '14727c4cd976ca3d96a7a9c362bebd064a49c4f24d32296d33767e3a76b4a685'
+  appcast 'https://xampubdl.blob.core.windows.net/static/installer_assets/v3/Mac/Universal/InstallationManifest.xml',
+          checkpoint: '477c85901ee0146c2f34467c8cfc1f3a94bcb96c5467bb8b5cfc1ed765b3676e'
   name 'Xamarin Studio Preview'
   homepage 'https://www.xamarin.com/studio'
 
