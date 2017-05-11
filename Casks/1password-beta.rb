ac4e9a7a5bfa88a4037e77fb2cd82b7d65201ef7
Miccal Matthews <miccal.matthews@gmail.com>
2017-05-12 07:18:05 +0800

    Update 1password-beta.rb

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask '1password-beta' do
   version '6.8.BETA-3'
-  sha256 '3a875bb6b50a071dc5dd2ad1cc20cf4c6513ba2c69ee65118aa29e132a80ded3'
+  sha256 'c3cc083d923d1d8095244d7eff2042f5a39dadd85166685b292f9bf2df0ef022'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   name '1Password'
