eb806694d622785969deea8d4ed4e14d5c04426c
commitay <commitay@users.noreply.github.com>
2017-05-13 06:24:00 +1000

    Update docker-edge to 17.05.0-ce-mac11,17656

--- Casks/docker-edge.rb
+++ Casks/docker-edge.rb
@@ -1,10 +1,10 @@
 cask 'docker-edge' do
-  version '17.05.0-ce-mac9,17434'
-  sha256 'f1d3a66116354122ca508c7377cb47a76c7b21b9e5243d68233d214eab7421fe'
+  version '17.05.0-ce-mac11,17656'
+  sha256 '1ae2d1664b834508ec507268e5802eb73c30840a996c6cbc14214ab29a1ec8bd'
 
   url "https://download.docker.com/mac/edge/#{version.after_comma}/Docker.dmg"
   appcast 'https://download.docker.com/mac/edge/appcast.xml',
-          checkpoint: '92cb5cb1c9e4fa9fdbddd722c5dc40057bdc9d1f81678c708368919bd2a4acb8'
+          checkpoint: '1b7dd6a5192139d07c425bd76203ca7c0b8818abd78d26784a13fb34e3d0e34f'
   name 'Docker Community Edition for Mac (Edge)'
   homepage 'https://www.docker.com/community-edition'
 
