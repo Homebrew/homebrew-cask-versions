606472a39c75c7c3776bebfcf5c979beb1be2e2e
Yurii Kolesnykov <yurikoles@gmail.com>
2017-05-12 17:51:42 +0300

    Update telegram-desktop-dev to 1.0.37.alpha (#3823)
    add conflict with telegram-desktop

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -1,12 +1,13 @@
 cask 'telegram-desktop-dev' do
-  version '1.0.29'
-  sha256 'fda92753e5311eec0b545bebcc1909164bd7c7f63bff434692ea7bcc4925da88'
+  version '1.0.37.alpha'
+  sha256 '4d9220bfb06f608cdfab3acd90bca40054688b88973c511d31648a9ffc4fce5f'
 
   # tdesktop.com was verified as official when first introduced to the cask
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
   name 'Telegram'
   homepage 'https://desktop.telegram.org/'
 
+  conflicts_with cask: 'telegram-desktop'
   depends_on macos: '>= :mountain_lion'
 
   # Renamed to avoid conflict with telegram
