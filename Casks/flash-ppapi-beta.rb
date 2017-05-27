4af684aa2cbfc4f3cb980110090efba7df0db19c
Conan1985 <youwei_lu@hotmail.com>
2017-05-27 15:45:14 -0400

    Add Flash Player Beta - PPAPI 26.0.0.115.

---
+++ Casks/flash-ppapi-beta.rb
@@ -0,0 +1,19 @@
+cask 'flash-ppapi-beta' do
+  version '26.0.0.115'
+  sha256 '2e2c25374cd33e63149ad806d220219e9cecfb188855350099450fab6a4f6cef'
+
+  # macromedia.com was verified as official when first introduced to the cask
+  url 'https://fpdownload.macromedia.com/pub/labs/flashruntimes/flashplayer/install_flash_player_osx_ppapi.dmg'
+  name 'Adobe Flash Player PPAPI (for Opera and Chromium based application)'
+  homepage 'http://labs.adobe.com/technologies/flashruntimes/flashplayer/'
+
+  pkg 'Install Adobe Pepper Flash Player.app/Contents/Resources/Adobe Flash Player.pkg'
+
+  uninstall pkgutil: 'com.adobe.pkg.PepperFlashPlayer',
+            delete:  '/Library/Internet Plug-Ins/PepperFlashPlayer'
+
+  zap       delete: [
+                      '~/Library/Caches/Adobe/Flash Player',
+                      '~/Library/Logs/FlashPlayerInstallManager.log',
+                    ]
+end
