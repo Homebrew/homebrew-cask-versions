c6fea0fb083981066fc67b957d113016f87bbd7b
Victor Popkov <victor@popkov.me>
2017-06-02 23:18:45 +0300

    Update sublime-text-dev to 3133

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,10 +1,10 @@
 cask 'sublime-text-dev' do
-  version '3132'
-  sha256 'c35624d671c1730d20409b0e7d7937782e4a3560fde4de52a0c7a0e151b3dae0'
+  version '3133'
+  sha256 'e753ae7c1475abb63241e5065cdab9ae572063a6c4544286368f88f2f9e64800'
 
   url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/3/dev/appcast_osx.xml',
-          checkpoint: 'd505121641ff1b5b9c0aaadee9c3f437a3a973cbd93ffd6fdd9f8f999e2da83c'
+          checkpoint: 'a05c4b47ce51e20f1936dbc8c4aaa226304a94832a6cbc3e03a2be7844ea2091'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/3dev'
 
