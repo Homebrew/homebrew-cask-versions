333a0bc82b38f5c2255cb8677b7ed77b5d93917c
rien333 <rien334@gmail.com>
2017-06-21 18:34:28 +0200

    Update blender-daily to 2.78-9e08019b74c

--- Casks/blender-daily.rb
+++ Casks/blender-daily.rb
@@ -1,6 +1,6 @@
 cask 'blender-daily' do
-  version '2.78-18e1c8d9fa6'
-  sha256 'f3199d165f66c249d28283aff14eb39163125eefd6a224b46258b554fcd228c2'
+  version '2.78-9e08019b74c'
+  sha256 '8f1ac625ab231b7a4423954faf594fa491c69c59617a8791f3b7383b407076d5'
 
   url "https://builder.blender.org/download/blender-#{version}-OSX-10.6-x86_64.zip"
   name 'Blender'
