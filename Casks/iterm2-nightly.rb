6e5b6c980329f7b0a8e347681b0248ec70fe02da
Joshua Jabbour <code@joshuajabbour.com>
2017-06-23 14:10:36 -0700

    Update iterm2-nightly.rb: zaps and appcast (#4046)
    Just bringing this formula in line with the [iTerm2 stable formula](https://github.com/caskroom/homebrew-cask/blob/master/Casks/iterm2.rb).

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,19 +1,25 @@
 cask 'iterm2-nightly' do
+  # note: "2" is not a version number, but an intrinsic part of the product name
   version :latest
   sha256 :no_check
 
   url 'https://www.iterm2.com/nightly/latest'
   appcast 'https://iterm2.com/appcasts/nightly.xml',
-          checkpoint: '6cf0d7368f02b12bb025ac9561ec82955adc664d1aa850b9e07e852ff70f4825'
+          checkpoint: 'e794f198e02f5cf454ee4fc892c37f2bed8d453579a4adce756602c1b8507fd8'
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
+
+  auto_updates true
+  depends_on macos: '>= 10.10'
 
   app 'iTerm.app'
 
   zap delete: [
-                '~/Library/Preferences/com.googlecode.iterm2.plist',
-                '~/Library/Caches/com.googlecode.iterm2',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl',
                 '~/Library/Application Support/iTerm',
                 '~/Library/Application Support/iTerm2',
+                '~/Library/Caches/com.googlecode.iterm2',
+                '~/Library/Preferences/com.googlecode.iterm2.plist',
+                '~/Library/Saved Application State/com.googlecode.iterm2.savedState',
               ]
 end
