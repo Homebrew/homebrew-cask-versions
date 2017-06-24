708d7c1363f7ba89a5e8a9bde45797add21876c1
commitay <commitay@users.noreply.github.com>
2017-06-24 23:40:10 +1000

    Update iterm2-nightly - remove appcast

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -4,8 +4,6 @@ cask 'iterm2-nightly' do
   sha256 :no_check
 
   url 'https://www.iterm2.com/nightly/latest'
-  appcast 'https://iterm2.com/appcasts/nightly.xml',
-          checkpoint: 'e794f198e02f5cf454ee4fc892c37f2bed8d453579a4adce756602c1b8507fd8'
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
 
