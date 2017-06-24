1fc60945b038bf333736521f6e3620fad6f0a5c6
Nikolay Rybak <mykola.rybak@gmail.com>
2017-06-24 19:02:09 +0300

    Update tunnelblick-beta to 3.7.2beta03_build_4840

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,6 +1,6 @@
 cask 'tunnelblick-beta' do
-  version '3.7.2beta02_build_4830'
-  sha256 'aeb4494444f0acbc6b8f45de9b30ba85eb96cccc921852f6a308438c4297bbbf'
+  version '3.7.2beta03_build_4840'
+  sha256 'b489b3c94e5d3c689cac6d4e52e829efb18e19f0e5c0d6271b76a2e68f333064'
 
   url "https://tunnelblick.net/release/Tunnelblick_#{version}.dmg"
   appcast 'https://www.tunnelblick.net/appcast.rss',
