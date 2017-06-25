af493f7a03410f19ded814d5ee214679f15aba9d
commitay <commitay@users.noreply.github.com>
2017-06-26 07:26:13 +1000

    Add forklift2 2.6.6
    https homepage

---
+++ Casks/forklift2.rb
@@ -0,0 +1,15 @@
+cask 'forklift2' do
+  version '2.6.6'
+  sha256 '0fa2bb7927d07c0c13b964b517b500ea6a77a6a569becba3ea392b2f82c05fce'
+
+  url "http://download.binarynights.com/ForkLift#{version}.zip"
+  name 'ForkLift'
+  homepage 'https://www.binarynights.com/'
+
+  app 'ForkLift.app'
+
+  zap delete: [
+                '~/Library/Preferences/com.binarynights.ForkLift2.plist',
+                '~/Library/Caches/com.binarynights.ForkLift2',
+              ]
+end
