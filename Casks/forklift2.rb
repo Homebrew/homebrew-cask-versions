a0b806b48a7e1359b8a34aac9b526bd011f42e5e
commitay <commitay1@gmail.com>
2017-06-26 18:36:08 +1000

    Add forklift2 2.6.6 (#4057)
    * Add forklift2 2.6.6
    
    
    https homepage
    
    * add conflict
    
    * Update forklift2.rb

---
+++ Casks/forklift2.rb
@@ -0,0 +1,17 @@
+cask 'forklift2' do
+  version '2.6.6'
+  sha256 '0fa2bb7927d07c0c13b964b517b500ea6a77a6a569becba3ea392b2f82c05fce'
+
+  url "http://download.binarynights.com/ForkLift#{version}.zip"
+  name 'ForkLift'
+  homepage 'https://www.binarynights.com/'
+
+  conflicts_with cask: 'forklift'
+
+  app 'ForkLift.app'
+
+  zap delete: [
+                "~/Library/Preferences/com.binarynights.ForkLift#{version.major}.plist",
+                "~/Library/Caches/com.binarynights.ForkLift#{version.major}",
+              ]
+end
