4be94f413f25b15d7339af1010ab302d386f012c
Stefan Zwanenburg <stefanhetzwaantje@gmail.com>
2017-06-26 23:32:10 +0200

    Add krita-devel 4.0.0-prealpha

---
+++ Casks/krita-devel.rb
@@ -0,0 +1,13 @@
+cask 'krita-devel' do
+  version '4.0.0-prealpha'
+  sha256 'f63313ea07fcc1329d4d696153c5d6877940e5a23ec77d432075bd2e88fbe274'
+
+  # download.kde.org/unstable/krita was verified as official when first introduced to the cask
+  url "http://download.kde.org/unstable/krita/#{version}/krita-#{version}.dmg"
+  name 'Krita'
+  homepage 'https://krita.org/'
+
+  depends_on macos: '>= :mavericks'
+
+  app 'Krita.app', target: 'Krita-devel.app'
+end
