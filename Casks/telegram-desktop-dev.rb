c2e692690290fd9245156e1bd17892233fa39007
Yurii Kolesnykov <yurikoles@gmail.com>
2017-07-01 23:28:41 +0300

    Update telegram-desktop-dev to 1.1.9 (#4083)

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -1,6 +1,6 @@
 cask 'telegram-desktop-dev' do
-  version '1.1.7'
-  sha256 '52c80d990bfce7a58caebca57c7d5f23e8de8df98aad7ec3406ae56bd0165b85'
+  version '1.1.9'
+  sha256 '89bbb2408ff93990bb0ddba281ba441d2f412ee53321264b5ddc46ee2b0c9f05'
 
   # tdesktop.com was verified as official when first introduced to the cask
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
