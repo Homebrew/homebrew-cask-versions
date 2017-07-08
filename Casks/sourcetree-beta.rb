0bc407a2feddf5473cc96ca47fa4005b7650dfd3
Victor Popkov <victor@popkov.me>
2017-07-08 21:53:16 +0300

    Update sourcetree-beta to 2.6b2 (#4119)

--- Casks/sourcetree-beta.rb
+++ Casks/sourcetree-beta.rb
@@ -1,11 +1,11 @@
 cask 'sourcetree-beta' do
-  version '2.6b1'
-  sha256 '243a8be2782bb1fe59a55ab092a3a14876c7ddb93a677850e0183e48ee7b229d'
+  version '2.6b2'
+  sha256 '4a6a35c4bcbb9eb7bcaf08ec4646e4487e96ddf78d7d91356f19fab2ea3cecfb'
 
   # atlassian.com was verified as official when first introduced to the cask
   url "https://downloads.atlassian.com/software/sourcetree/SourceTree_#{version}.zip"
   appcast 'https://www.sourcetreeapp.com/update/SparkleAppcastBeta.xml',
-          checkpoint: '4510da3b6c7c550256a3b6fcf34fc0f3b8680a0d14989a32c2e9b5e9bc88b940'
+          checkpoint: '8a63255e8c1e84c09ed81f8ada9ff6310e479899a1d11d48e5c702755b7e4b5a'
   name 'Atlassian SourceTree'
   homepage 'https://www.sourcetreeapp.com/'
 
