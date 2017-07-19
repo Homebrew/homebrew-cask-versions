8354d12bdbffc2bf9f8b987b6d905af1cc894a65
Victor Popkov <victor@popkov.me>
2017-07-19 22:17:07 +0300

    Update jabref-beta to 4.0-beta2 (#4172)

--- Casks/jabref-beta.rb
+++ Casks/jabref-beta.rb
@@ -1,11 +1,11 @@
 cask 'jabref-beta' do
-  version '4.0-beta'
-  sha256 '0d8c6ff5979027a3c8c5da23b2ff77ae2dc62cdb0baf695e61d5da35fac0e551'
+  version '4.0-beta2'
+  sha256 'c5b764bd74486a5a348a3ae98a3c848867b6608d516801a296d903812a37cd9d'
 
   # github.com/JabRef/jabref was verified as official when first introduced to the cask
-  url 'https://github.com/JabRef/jabref/releases/download/v4.0-beta/JabRef_macos_4_0_0-beta.dmg'
+  url "https://github.com/JabRef/jabref/releases/download/v#{version}/JabRef_macos_#{version.dots_to_underscores}.dmg"
   appcast 'https://github.com/JabRef/jabref/releases.atom',
-          checkpoint: '7714d6c5bea930a04dc3caa1c6434b47e40dbae33ec472dca9bae0b10731157d'
+          checkpoint: 'a05f548db846150e797fb5690642b423cb72e83afde629a45ca80774ea99c7a1'
   name 'JabRef Beta'
   homepage 'https://www.jabref.org/'
 
