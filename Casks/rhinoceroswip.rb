4869b7027d9828e7e65ae74e9dadd12117124769
Victor Popkov <victor@popkov.me>
2017-07-19 21:15:24 +0300

    Update rhinoceroswip to 5E140w

--- Casks/rhinoceroswip.rb
+++ Casks/rhinoceroswip.rb
@@ -1,11 +1,11 @@
 cask 'rhinoceroswip' do
-  version '5E119w'
-  sha256 '80d0d592d9e7c325ba869b8d6b6dbecae72f221727ba685a39f637308db3a276'
+  version '5E140w'
+  sha256 '4cbbd60a4a0fd320f1e2d448bf937b2b1e7acc0968100f94b7883badadfec651'
 
   # mcneel.com was verified as official when first introduced to the cask
   url "https://files.mcneel.com/Releases/Rhino/5.0/Mac/RhinoWIP_#{version}.dmg"
   appcast 'https://files.mcneel.com/rhino/5.0/mac/5CwipUpdates.xml',
-          checkpoint: 'f0e8c3b19394e86b3396d5226d3561aa9559b3094d955cd662f2639c86ae345e'
+          checkpoint: 'f334e6a91c3dbec8ed8f23f428e0d1722845110ceee4aec10342f02d552681a2'
   name 'Rhinoceros WIP'
   homepage 'https://www.rhino3d.com/download/rhino-for-mac/5/wip'
 
