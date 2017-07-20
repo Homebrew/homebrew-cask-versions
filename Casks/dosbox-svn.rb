d150c03ddbdfeb0077c69120e76898c586dc00ed
Michiel Sikma <michiel@sikma.org>
2017-07-20 23:12:17 +0200

    dosbox-svn (latest) Add appropriate URL comment

--- Casks/dosbox-svn.rb
+++ Casks/dosbox-svn.rb
@@ -2,6 +2,7 @@ cask 'dosbox-svn' do
   version :latest
   sha256 :no_check
 
+  # dropbox.com was verified as official when first introduced to the cask
   url 'http://www.dropbox.com/s/kbrf5elbcu66kst/Dosbox-Snapshot.dmg?dl=1'
   name 'DOSBox SVN'
   homepage 'https://www.dosbox.com/wiki/SVN_Builds#Plain.2Fvanilla.2Fclean_SVN_builds/'
