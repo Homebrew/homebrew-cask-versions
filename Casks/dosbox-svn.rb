aa0eb1b079e3f8bfd68b791c3d42695454174577
commitay <commitay@users.noreply.github.com>
2017-07-21 07:56:45 +1000

    dl.dropboxusercontent.com

--- Casks/dosbox-svn.rb
+++ Casks/dosbox-svn.rb
@@ -2,8 +2,8 @@ cask 'dosbox-svn' do
   version :latest
   sha256 :no_check
 
-  # dropbox.com/s/kbrf5elbcu66kst was verified as official when first introduced to the cask
-  url 'http://www.dropbox.com/s/kbrf5elbcu66kst/Dosbox-Snapshot.dmg'
+  # dl.dropboxusercontent.com/s/kbrf5elbcu66kst was verified as official when first introduced to the cask
+  url 'http://dl.dropboxusercontent.com/s/kbrf5elbcu66kst/Dosbox-Snapshot.dmg'
   name 'DOSBox SVN'
   homepage 'https://www.dosbox.com/wiki/SVN_Builds#Plain.2Fvanilla.2Fclean_SVN_builds/'
 
