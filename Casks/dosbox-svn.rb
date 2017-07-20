8fc16230204dd18a4b4ee5b0bd463dcb1719ce8a
Michiel Sikma <michiel@sikma.org>
2017-07-20 23:09:14 +0200

    Remove comment, change to http

--- Casks/dosbox-svn.rb
+++ Casks/dosbox-svn.rb
@@ -2,8 +2,7 @@ cask 'dosbox-svn' do
   version :latest
   sha256 :no_check
 
-  # Snapshot, automatically rebuilt from trunk, created by user Dominus (#dosbox irc.freenode.org)
-  url 'https://www.dropbox.com/s/kbrf5elbcu66kst/Dosbox-Snapshot.dmg?dl=1'
+  url 'http://www.dropbox.com/s/kbrf5elbcu66kst/Dosbox-Snapshot.dmg?dl=1'
   name 'DOSBox SVN'
   homepage 'https://www.dosbox.com/wiki/SVN_Builds#Plain.2Fvanilla.2Fclean_SVN_builds/'
 
