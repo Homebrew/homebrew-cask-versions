56c13661474fcae5da20030f4e6d70d87e197422
Michiel Sikma <michiel@wedemandhtml.com>
2017-07-20 20:33:04 +0200

    Update DOSBox SVN formula
    The URL for the snapshots has changed (in part because of a policy change by Dropbox). The new URL is a direct link to new builds generated from trunk automatically.

--- Casks/dosbox-svn.rb
+++ Casks/dosbox-svn.rb
@@ -2,8 +2,8 @@ cask 'dosbox-svn' do
   version :latest
   sha256 :no_check
 
-  # dl.dropbox.com/u/7737184 was verified as official when first introduced to the cask
-  url 'http://dl.dropbox.com/u/7737184/Dosbox/Dosbox-Snapshot.dmg'
+  # Snapshot, automatically rebuilt from trunk, created by user Dominus (#dosbox irc.freenode.org)
+  url 'https://www.dropbox.com/s/kbrf5elbcu66kst/Dosbox-Snapshot.dmg?dl=1'
   name 'DOSBox SVN'
   homepage 'https://www.dosbox.com/wiki/SVN_Builds#Plain.2Fvanilla.2Fclean_SVN_builds/'
 
