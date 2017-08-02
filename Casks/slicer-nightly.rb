9bc23ae9c109f6d9d9f64d547bcdcb7782e5d05d
Fernando <fepegar@gmail.com>
2017-08-02 02:07:52 +0100

    Add slicer-nightly

---
+++ Casks/slicer-nightly.rb
@@ -0,0 +1,10 @@
+cask 'slicer-nightly' do
+  version '26186'
+  sha256 '41d50f7531bef1b759101f97ca1295d2f332133e8e06419e8d285d89552ee6d7'
+
+  url 'http://download.slicer.org/bitstream/672116'
+  name '3D Slicer Nightly'
+  homepage 'https://www.slicer.org/'
+
+  app 'Slicer.app'
+end
