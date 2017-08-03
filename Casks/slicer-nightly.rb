4fef856e2e5b9ae8b9a5858bb66bd9e18635521d
Fernando <fepegar@gmail.com>
2017-08-03 06:54:35 +0100

    Add slicer-nightly (3D Slicer nightly build) (#4225)
    * Add slicer-nightly
    
    * Update slicer-nightly.rb
    
    * Update slicer-nightly.rb

---
+++ Casks/slicer-nightly.rb
@@ -0,0 +1,11 @@
+cask 'slicer-nightly' do
+  version '4.7.0.26186,672116'
+  sha256 '41d50f7531bef1b759101f97ca1295d2f332133e8e06419e8d285d89552ee6d7'
+
+  # slicer.kitware.com/midas3 was verified as official when first introduced to the cask
+  url "http://slicer.kitware.com/midas3/download?bitstream=#{version.after_comma}"
+  name '3D Slicer Nightly'
+  homepage 'https://www.slicer.org/'
+
+  app 'Slicer.app'
+end
