6db115b574bff03329c79de57c57727016911911
commitay <commitay@users.noreply.github.com>
2017-08-10 11:27:32 +1000

    Update dotnet-preview.rb

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -1,12 +1,15 @@
-cask 'dotnet-preview' do
-  version '2.0.0-preview2'
-  sha256 'a1682bbc89e931ab2a734056604985c59add9e08a1d56d7eb954cd0beab925e3'
-  url "https://download.microsoft.com/download/7/C/3/7C310A63-13AC-49A4-9666-4CB26388F852/dotnet-runtime-#{version}-25407-01-osx-x64.pkg"
-  name '.NET Core 2.0 Preview'
+cask 'dotnet-sdk-preview' do
+  version '2.0.0-preview2-006497'
+  sha256 'de675118efc09f80d4f2927b791b64806e7fbbd640b56d408b92d4e6ce4b167c'
+
+  url "https://download.microsoft.com/download/6/1/B/61B3E81F-5509-48D2-BB4F-5189E23CD29A/dotnet-sdk-#{version}-osx-x64.pkg"
+  name '.NET Core SDK Preview'
   homepage 'https://www.microsoft.com/net/core/preview#macos'
-  depends_on formula: 'openssl'
-  pkg "dotnet-runtime-#{version}-25407-01-osx-x64.pkg"
+
+  pkg "dotnet-sdk-#{version}-osx-x64.pkg"
+
   uninstall pkgutil: 'com.microsoft.dotnet.sharedframework.Microsoft.NETCore.App.2*',
-            delete: '/etc/paths.d/dotnet'
+            delete:  '/etc/paths.d/dotnet'
+
   zap trash: '~/.nuget'
 end
