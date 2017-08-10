70f093af8d805881fed50feac28d2ea5f66d2cb9
commitay <commitay@users.noreply.github.com>
2017-08-10 11:33:58 +1000

    Update dotnet-preview.rb

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -3,7 +3,7 @@ cask 'dotnet-preview' do
   sha256 'a1682bbc89e931ab2a734056604985c59add9e08a1d56d7eb954cd0beab925e3'
 
   url "https://download.microsoft.com/download/7/C/3/7C310A63-13AC-49A4-9666-4CB26388F852/dotnet-runtime-#{version}-osx-x64.pkg"
-  name '.NET Core 2.0 Preview'
+  name '.NET Core Preview'
   homepage 'https://www.microsoft.com/net/core/preview#macos'
 
   depends_on formula: 'openssl'
