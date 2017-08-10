23474e63cca3ebd3aab471cbef629d02f55af376
Victor Popkov <victor@popkov.me>
2017-08-10 23:18:44 +0300

    Update toggldesktop-beta to 7.4.58 (#4262)

--- Casks/toggldesktop-beta.rb
+++ Casks/toggldesktop-beta.rb
@@ -1,11 +1,11 @@
 cask 'toggldesktop-beta' do
-  version '7.4.52'
-  sha256 'e1f3d35a47d0b150f46aba5457833873932e735a232a1b8eeb5995222fa0bed9'
+  version '7.4.58'
+  sha256 'd911ec53fdc4c0ffc678d024eb47cd3a1564c0882afd1884e4be4a434f5a52fe'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_beta_appcast.xml',
-          checkpoint: 'b726b959ada19efb3ea5bd2ab6085e6ae55ccb059cba0750226b0e2a6046a96d'
+          checkpoint: 'c6b053eba1131199ddf91f4d95993821d15141991e65ed6f6dc5b673873ae425'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
 
