644b55bb9bc8a06742f82044240e384b7e7d1f08
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-08-14 19:02:32 +0900

    Update iterm2-beta to 3.1.beta.7 (#4279)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,11 +1,11 @@
 cask 'iterm2-beta' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version '3.1.beta.6'
-  sha256 '6c44f5433d37f4b481f31151fc9b96766c0559ea99a181bb9c1f867bfd9a2339'
+  version '3.1.beta.7'
+  sha256 '98f7cbfbe3ca7babae137338522a5535622ef33c06d2363c81850128822b48d5'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   appcast 'https://iterm2.com/appcasts/testing3.xml',
-          checkpoint: '492b4e108051e054d5015d6cf6ed957a8c429da5b27279c3add8945d08017291'
+          checkpoint: '1f5f58e0134568646392e173eff1974cf0044c8b561a913e742516e84cc1c1fd'
   name 'iTerm2'
   homepage 'https://www.iterm2.com/'
 
