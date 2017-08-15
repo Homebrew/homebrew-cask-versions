e5fc109dca8f1eb7542b26f5be578fa024eb326e
commitay <commitay@users.noreply.github.com>
2017-08-15 11:16:59 +1000

    Update smartgit-rc to 17.1-preview-12

--- Casks/smartgit-rc.rb
+++ Casks/smartgit-rc.rb
@@ -1,8 +1,10 @@
 cask 'smartgit-rc' do
-  version '17.1-preview-10'
-  sha256 'e8011401f99b9b20d350987af6bc55dea31481202ea2a1f5e21ad7450ef32cb3'
+  version '17.1-preview-12'
+  sha256 'ea4f9c688cc73096a619aeda5004999851401ccf3255e117378e217e42acf490'
 
   url "https://www.syntevo.com/static/smart/download/smartgit/smartgit-macosx-#{version}.dmg"
+  appcast 'https://www.syntevo.com/smartgit/changelog-eap.txt',
+          checkpoint: 'af1524ed49e92d054e6f58b83b5d0f0f0309c57b113619daeece50e9b5adc98a'
   name 'SmartGit'
   homepage 'https://www.syntevo.com/smartgit/'
 
