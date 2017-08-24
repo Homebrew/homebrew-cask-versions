26870f80de899564bde6a22bac9dbdfc9f8bccea
Wes Baker <wes@wesbaker.com>
2017-08-24 10:48:21 -0400

    Update mongodb-compass-beta to 1.9.0-beta.2

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,6 +1,6 @@
 cask 'mongodb-compass-beta' do
-  version '1.8.0-beta.1'
-  sha256 '39c8eb78ffcce72b25b6d3c3fb84a27498ed35cb320913e5bda1ee311efb0b59'
+  version '1.9.0-beta.2'
+  sha256 '7d4a213efb4e9b7b29d5f556c09109b2c236d3a82b7394ab209ef6404935c81b'
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
   name 'MongoDB Compass'
