c67f9ed6ca704346e56a7b142cc8851200dba63b
Fernando <fepegar@gmail.com>
2017-09-03 05:03:46 +0100

    Update slicer-nightly to 4.7.0.26329,683761

--- Casks/slicer-nightly.rb
+++ Casks/slicer-nightly.rb
@@ -1,6 +1,6 @@
 cask 'slicer-nightly' do
-  version '4.7.0.26325,683041'
-  sha256 'b5d9853a879633ca8f4c4ce0f68039736f57d74b6e8f4b8d23149c9cde0d5be4'
+  version '4.7.0.26329,683761'
+  sha256 'e44e2dae268b7bb27c34cb55d695c995e453303b76aba00d5619b7faa9a2246c'
 
   # slicer.kitware.com/midas3 was verified as official when first introduced to the cask
   url "http://slicer.kitware.com/midas3/download?bitstream=#{version.after_comma}"
