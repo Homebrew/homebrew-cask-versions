794827c43a69d06bf8816a0e4fff9cb8ef6decd3
Stephan Leicht Vogt <stephan@jessie.ai>
2017-09-05 16:47:27 +0200

    add separate version 2 of audirvana-plus

---
+++ Casks/audirvana-plus2.rb
@@ -0,0 +1,20 @@
+cask 'audirvana-plus2' do
+  version '2.6.8'
+  sha256 '7db18cdd2754c2d366a840dea937e21a86fd73d1fbaf1214aaf517e85a81256a'
+
+  url "https://audirvana.com/delivery/AudirvanaPlus_#{version}.dmg"
+  appcast "https://audirvana.com/delivery/audirvanaplus#{version.major}_appcast.xml",
+          checkpoint: 'efa186f1e291808dfb3f3911a108886b8a0e830a25a220bbb3e647890edfcc6d'
+  name "Audirvana Plus #{version.major}"
+  homepage 'https://audirvana.com/'
+
+  app 'Audirvana Plus.app'
+
+  zap delete: [
+                '/Library/LaunchDaemons/com.audirvana.Audirvana-Plus.plist',
+                '/Library/PrivilegedHelperTools/com.audirvana.Audirvana-Plus',
+                '~/Library/Caches/com.audirvana.Audirvana-Plus',
+                '~/Library/Cookies/com.audirvana.Audirvana-Plus.binarycookies',
+                '~/Library/Preferences/com.audirvana.Audirvana-Plus.plist',
+              ]
+end
