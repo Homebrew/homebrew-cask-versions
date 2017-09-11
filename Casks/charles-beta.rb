a91b758110ebd9f633f527f309ef49d82576ec2c
commitay <commitay@users.noreply.github.com>
2017-09-11 14:00:47 +1000

    Update charles-beta to 4.2b4 (#4412)

--- Casks/charles-beta.rb
+++ Casks/charles-beta.rb
@@ -1,6 +1,6 @@
 cask 'charles-beta' do
-  version '4.2b3'
-  sha256 '47120f0755c5a6036d7a4d9fe5d8d3088229ed4db6a62535965e6b42a43b3e08'
+  version '4.2b4'
+  sha256 'cdf7278182704e475a045292c3be5e7b3b9abb68a1b564ba2e51e6142533beb8'
 
   url "https://www.charlesproxy.com/assets/release/#{version.gsub(%r{b\d$}, '')}/charles-proxy-#{version}.dmg"
   name 'Charles'
