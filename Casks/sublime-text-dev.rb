dba2458308b3fc35a4a8d3d7e9bf7001d9724af5
Nathanael Esayeas <nathane@users.noreply.github.com>
2017-09-15 00:24:23 -0500

    Update sublime-text-dev Build 3144 (#4429)
    * Update sublime-text-dev.rb to 3144
    
    * zap

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,10 +1,10 @@
 cask 'sublime-text-dev' do
-  version '3142'
-  sha256 'a6f8e159ce52ae70708a4ff209c43e76f7cb4eaf91cb0183f1a9c15a205f7f03'
+  version '3144'
+  sha256 '78ec9af16d34c365e815536fbb5bda7862313776c9642dc6e2dfe97728402699'
 
   url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version}.dmg"
   appcast 'https://www.sublimetext.com/updates/3/dev/appcast_osx.xml',
-          checkpoint: 'cf5a64923592c5c705a0c4b2cd78e3efc4c59683321ae6f28bb9cf88714d7303'
+          checkpoint: 'a83a9fba63075a2d17cf019ea422848a5f16e806e91538763ca0a2451ad86add'
   name 'Sublime Text'
   homepage 'https://www.sublimetext.com/3dev'
 
@@ -16,9 +16,11 @@ cask 'sublime-text-dev' do
   uninstall quit: 'com.sublimetext.3'
 
   zap delete: [
-                '~/Library/Application Support/Sublime Text 3',
                 '~/Library/Caches/com.sublimetext.3',
-                '~/Library/Preferences/com.sublimetext.3.plist',
                 '~/Library/Saved Application State/com.sublimetext.3.savedState',
+              ],
+      trash:  [
+                '~/Library/Application Support/Sublime Text 3',
+                '~/Library/Preferences/com.sublimetext.3.plist',
               ]
 end
