e789d2e4acabe7f39d4de5a082bd67e0bab50a4b
commitay <commitay@users.noreply.github.com>
2017-09-17 14:57:07 +1000

    Update virtualbox-extension-pack-beta to 5.2.0_BETA3,118015

--- Casks/virtualbox-extension-pack-beta.rb
+++ Casks/virtualbox-extension-pack-beta.rb
@@ -1,10 +1,10 @@
 cask 'virtualbox-extension-pack-beta' do
-  version '5.2.0_BETA2,117563'
-  sha256 '30711e0aa417910cf26e1abc1da08d575d45c330fd593decf35b7d04d87f7ca7'
+  version '5.2.0_BETA3,118015'
+  sha256 '6801a5ec29eb82b680acbc45e000b6a96f2f38ccd24e6cde53f4a9030dbe578b'
 
   url "http://download.virtualbox.org/virtualbox/#{version.before_comma}/Oracle_VM_VirtualBox_Extension_Pack-#{version.before_comma}-#{version.after_comma}.vbox-extpack"
   appcast 'http://download.virtualbox.org/virtualbox/LATEST-BETA.TXT',
-          checkpoint: '4e9ae5d62ebfe2ab6e9fc1d7bd64c69f0c25b601b1c08ba92711adda7c6c0049'
+          checkpoint: '1f753c9849a06ca6ad4b2df06d022a2dc42bee734c87d98759f911be52eb537b'
   name 'Oracle VirtualBox Extension Pack'
   homepage 'https://www.virtualbox.org/'
 
