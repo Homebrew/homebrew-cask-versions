0750f129562942692b215f95ab040ab8dbce1fe0
commitay <commitay@users.noreply.github.com>
2017-09-19 21:24:44 +1000

    Update sketch-beta to 47,45359

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,11 +1,11 @@
 cask 'sketch-beta' do
-  version '47,45292'
-  sha256 'f8d3435015a6bfd15c6c7b97b1c10f199917e0938c35470a9d8c8787a546b426'
+  version '47,45359'
+  sha256 'd33b7f348ee2e931b39b262bf536f9b1f72dd92f8cff1304952d3c7e0516cc1c'
 
   # hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b',
-          checkpoint: 'e4b8621536790ba164fb95eaf47ec013d62f3a3d6b6e67cebc8db028e24a1faf'
+          checkpoint: '036d03892f6e7a13147239d2f4cbec8b0985062b5c852a511e5f5e5e7909b7df'
   name 'Sketch'
   homepage 'https://www.sketchapp.com/beta/'
 
