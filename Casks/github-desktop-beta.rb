e641e1c1067f9bbf13f49ebe2427518b0cc46ae4
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-09-20 14:30:26 +0200

    Add Github Desktop.app 1.0.1-beta0 (#4459)
    * Add Github Desktop.app 1.0.1-beta0
    
    * Added sha256 checksum
    
    * zap

---
+++ Casks/github-desktop-beta.rb
@@ -0,0 +1,32 @@
+cask 'github-desktop-beta' do
+  version '1.0.1-beta0-e1b95d2d'
+  sha256 'c14de67276c0bba88153b1217e54a0afe7de5875279e822d387234d73e595f6e'
+
+  # githubusercontent.com was verified as official when first introduced to the cask
+  url "https://desktop.githubusercontent.com/releases/#{version}/GitHubDesktop.zip"
+  appcast 'https://github.com/desktop/desktop/releases.atom',
+          checkpoint: 'bdaa9a05920b09f4b9f258dde1f9a88eab56f1e48233dcbc83f5113afce29013'
+  name 'GitHub Desktop'
+  homepage 'https://desktop.github.com/'
+
+  auto_updates true
+
+  app 'GitHub Desktop.app'
+  binary "#{appdir}/GitHub Desktop.app/Contents/Resources/app/static/github.sh", target: 'github'
+
+  zap delete: [
+                '~/Library/Application Support/ShipIt_stderr.log',
+                '~/Library/Application Support/ShipIt_stdout.log',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.github.GitHubClient.sfl',
+                '~/Library/Caches/com.github.GitHubClient',
+                '~/Library/Caches/com.github.GitHubClient.ShipIt',
+              ],
+      trash:  [
+                '~/Library/Application Support/GitHub Desktop',
+                '~/Library/Application Support/com.github.GitHubClient',
+                '~/Library/Application Support/com.github.GitHubClient.ShipIt',
+                '~/Library/Preferences/com.github.GitHubClient.helper.plist',
+                '~/Library/Preferences/com.github.GitHubClient.plist',
+              ],
+      rmdir:  '~/.config/git'
+end
