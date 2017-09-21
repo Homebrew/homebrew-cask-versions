a915ad3dc79a26db5a785145b026561d76e33c51
Victor Popkov <victor@popkov.me>
2017-09-21 23:43:11 +0300

    Update vivaldi-snapshot to 1.13.966.3 (#4474)

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,10 +1,10 @@
 cask 'vivaldi-snapshot' do
-  version '1.12.955.20'
-  sha256 '1cecb3905632fcb733ed72e311e77bc6bc1e973b1f00279d1887ca0d0d5038f2'
+  version '1.13.966.3'
+  sha256 'abfc1221910d8c7af0a6d79b6f8f923ed96a7f44b14f7579a99f641636320a46'
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml',
-          checkpoint: '510700dcab3c812237c444c991525c60d851315efeab20dce0c5ac3e9d10b00f'
+          checkpoint: 'bf7c659f070c9926411e227c86132e89e5314b8f13edceac13eed57339cce3e0'
   name 'Vivaldi'
   homepage 'https://vivaldi.com/'
 
