666d227709d64e5ed1d5a39dbf2a824d96bd6d1e
Yash Shah <yash.shah7197@live.com>
2017-09-24 02:52:37 +0530

    Update android-studio-canary to 3.0.0.14,171.4333198 (#4484)
    * Update Android Studio beta to Beta 6 
    
    Update Android Studio beta to Beta 6 (version 3.0.0.10, 171.4333198)
    
    * Update Android Studio Beta to Beta 6
    
    Update Android Studio Beta to Beta 6 (version 3.0.0.14, 171.4333198)

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,6 +1,6 @@
 cask 'android-studio-canary' do
-  version '3.0.0.10,171.4263559'
-  sha256 'b2acdc0f9a13052d4e2870b302fe259fd710519ac4d90b0c1c928633a1b52b4f'
+  version '3.0.0.14,171.4333198'
+  sha256 '386dafc2846542acbd8280ed89fe41434d2de5347d5ae41548e8d7c508e94e09'
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Canary'
