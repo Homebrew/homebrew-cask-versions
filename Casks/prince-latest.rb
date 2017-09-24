f3e8d0375c89f38f69da02398e39a909ec728d1d
commitay <commitay@users.noreply.github.com>
2017-09-24 11:13:55 +1000

    appcast

--- Casks/prince-latest.rb
+++ Casks/prince-latest.rb
@@ -3,6 +3,8 @@ cask 'prince-latest' do
   sha256 '3c882cbbcdd646e27263105fce8079391d703c8243ad640b4b2db3364dadce5f'
 
   url "https://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
+  appcast 'https://www.princexml.com/latest/',
+          checkpoint: '53edcc6bb96a56e42143629a6cc380229ef1f27ed372e550f012e3f71a10f548'
   name 'Prince'
   homepage 'https://www.princexml.com/'
 
