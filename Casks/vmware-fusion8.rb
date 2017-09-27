3f8f1e7565703b78c0fafe4bdfa3cbec125fd946
commitay <commitay@users.noreply.github.com>
2017-09-27 15:56:39 +1000

    Add vmware-fusion8

---
+++ Casks/vmware-fusion8.rb
@@ -0,0 +1,73 @@
+cask 'vmware-fusion8' do
+  version '8.5.8-5824040'
+  sha256 'fe74972b36960b43a092619105d5053d391d39c55f04991ea3fdce2e9680c035'
+
+  url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
+  name 'VMware Fusion'
+  homepage 'https://www.vmware.com/products/fusion.html'
+
+  auto_updates true
+
+  app 'VMware Fusion.app'
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-bridge"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-cfgcli"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-cli"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-dhcpd"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-natd"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-netifup"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmnet-sniffer"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmrun"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmss2core"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-aewp"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-authd"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-cloneBootCamp"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-id"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-ntfs"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-rawdiskAuthTool"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-rawdiskCreator"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-remotemks"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-usbarbitrator"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-vdiskmanager"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-vmdkserver"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-vmx"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-vmx-debug"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/vmware-vmx-stats"
+  binary "#{appdir}/VMware Fusion.app/Contents/Library/VMware OVF Tool/ovftool"
+
+  postflight do
+    system_command "#{appdir}/VMware Fusion.app/Contents/Library/Initialize VMware Fusion.tool",
+                   args: ['set'],
+                   sudo: true
+  end
+
+  uninstall_preflight do
+    set_ownership "#{appdir}/VMware Fusion.app"
+  end
+
+  zap delete: [
+                '/Library/Logs/VMware Fusion Services.log',
+                '/Library/Logs/VMware USB Arbitrator Service.log',
+                '/Library/Logs/VMware',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.vmware.fusion.sfl',
+                '~/Library/Caches/com.vmware.fusion',
+                '~/Library/Logs/VMware Fusion',
+                '~/Library/Logs/VMware Graphics Service.log',
+                '~/Library/Logs/VMware',
+                '~/Library/Preferences/com.vmware.fusion.LSSharedFileList.plist',
+                '~/Library/Preferences/com.vmware.fusion.LSSharedFileList.plist.lockfile',
+                '~/Library/Saved Application State/com.vmware.fusion.savedState',
+                '~/Library/WebKit/com.vmware.fusion',
+              ],
+      trash:  [
+                '/Library/Application Support/VMware',
+                '/Library/Preferences/VMware Fusion',
+                '~/Library/Application Support/VMware Fusion',
+                '~/Library/Preferences/VMware Fusion',
+                '~/Library/Preferences/com.vmware.fusion.plist',
+                '~/Library/Preferences/com.vmware.fusion.plist.lockfile',
+                '~/Library/Preferences/com.vmware.fusionDaemon.plist',
+                '~/Library/Preferences/com.vmware.fusionDaemon.plist.lockfile',
+                '~/Library/Preferences/com.vmware.fusionStartMenu.plist',
+                '~/Library/Preferences/com.vmware.fusionStartMenu.plist.lockfile',
+              ]
+end
