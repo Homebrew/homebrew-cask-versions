80d9f623657ac3dc94f75d92d1ae4a4dce15074e
commitay <commitay@users.noreply.github.com>
2017-09-28 10:28:59 +1000

    soulseek-rc

--- Casks/soulseek-rc.rb
+++ Casks/soulseek-rc.rb
@@ -2,8 +2,8 @@ cask 'soulseek-rc' do
   version '2017-7-29'
   sha256 '2ccc407192e91a3a15ab60214e1799145434d666251e801b540701c406825758'
 
-  # dropbox.com/s/jx7790ztbn147ko/SoulseekQt- was verified as official when first introduced to the cask
-  url "https://www.dropbox.com/s/jx7790ztbn147ko/SoulseekQt-#{version}.dmg?dl=1"
+  # dl.dropboxusercontent.com/s/jx7790ztbn147ko was verified as official when first introduced to the cask
+  url "https://dl.dropboxusercontent.com/s/jx7790ztbn147ko/SoulseekQt-#{version}.dmg"
   name 'Soulseek'
   homepage 'https://www.slsknet.org/'
 
