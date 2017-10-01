054a54a26767b1e78d515fc44c2324c2868a78cd
commitay <commitay@users.noreply.github.com>
2017-10-02 08:56:31 +1000

    verified comment

--- Casks/textexpander5.rb
+++ Casks/textexpander5.rb
@@ -2,6 +2,7 @@ cask 'textexpander5' do
   version '5.1.4'
   sha256 'a299cdd809c3a5822375eec18a4644a785d7c442d6357a3a6f141c2e2b92a657'
 
+  # dl.smilesoftware.com/com.smileonmymac.textexpander was verified as official when first introduced to the cask
   url 'http://dl.smilesoftware.com/com.smileonmymac.textexpander/TextExpander.zip'
   name 'TextExpander'
   homepage 'https://textexpander.com/textexpander-standalone-apps/'
