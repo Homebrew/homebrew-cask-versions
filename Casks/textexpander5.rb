a48a4cc5302058e1106024cb4db619c1ae3139bc
Jesse Claven <jesse.claven@gmail.com>
2017-10-02 03:26:11 +0200

    Add legacy TextExpander.app v5.1.4 (#4542)
    * Add legacy TextExpander.app v5.1.4
    
    As TextExpander v6.0.0 and above are cloud-only, there are users who
    won't be migrating and will continue to use the 'standalone' version.
    
    The reason the CDN URL isn't used like for other versions is because
    the latest v5 version (5.1.4) does not exist on that CDN. The latest
    is v5.1.1.
    
    * Update textexpander5.rb
    
    * Update textexpander5.rb
    
    * verified comment
    
    * https

---
+++ Casks/textexpander5.rb
@@ -0,0 +1,18 @@
+cask 'textexpander5' do
+  version '5.1.4'
+  sha256 'a299cdd809c3a5822375eec18a4644a785d7c442d6357a3a6f141c2e2b92a657'
+
+  # dl.smilesoftware.com/com.smileonmymac.textexpander was verified as official when first introduced to the cask
+  url 'https://dl.smilesoftware.com/com.smileonmymac.textexpander/TextExpander.zip'
+  name 'TextExpander'
+  homepage 'https://textexpander.com/textexpander-standalone-apps/'
+
+  accessibility_access true
+  conflicts_with cask: 'textexpander'
+
+  app 'TextExpander.app'
+
+  uninstall login_item: 'TextExpander'
+
+  zap trash: '~/Library/Application Support/TextExpander/'
+end
