a895098c02275433c7a735b698bebfc0ed55e793
Fardjad Davari <public@fardjad.com>
2017-10-05 01:09:57 +0330

    Fix #4547 (palemoon 27.5.0)
    The app dir name in latest DMG is **Palemoon.app**.
    Also, the name should be **Palemoon** instead of **Pale Moon**.

--- Casks/palemoon.rb
+++ Casks/palemoon.rb
@@ -3,8 +3,8 @@ cask 'palemoon' do
   sha256 '019686a754204f0691722d332d28678509a32cbfadbde17a0d005435ff9da369'
 
   url "https://mac.palemoon.org/dist/palemoon-#{version}.mac64.dmg"
-  name 'Pale Moon'
+  name 'Palemoon'
   homepage 'https://www.palemoon.org/'
 
-  app 'PaleMoon.app'
+  app 'Palemoon.app'
 end
