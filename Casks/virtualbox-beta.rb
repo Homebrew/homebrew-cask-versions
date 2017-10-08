96ab41a4566f805cf6ef711db57ed4b19581ea22
commitay <commitay@users.noreply.github.com>
2017-10-08 22:00:15 +1000

    Update virtualbox-beta: zap

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -26,8 +26,16 @@ cask 'virtualbox-beta' do
             pkgutil: 'org.virtualbox.pkg.*'
 
   zap delete: [
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox.sfl',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualboxvm.sfl',
+                '~/Library/Saved Application State/org.virtualbox.app.VirtualBox.savedState',
+                '~/Library/Saved Application State/org.virtualbox.app.VirtualBoxVM.savedState',
+              ],
+      trash:  [
                 '/Library/Application Support/VirtualBox',
                 '~/Library/VirtualBox',
+                '~/Library/Preferences/org.virtualbox.app.VirtualBox.plist',
+                '~/Library/Preferences/org.virtualbox.app.VirtualBoxVM.plist',
               ],
       rmdir:  '~/VirtualBox VMs'
 end
