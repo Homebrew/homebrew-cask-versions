bfbf8fa2f5c5c610069dd31cc298a7ea96dd2bd3
Victor Popkov <victor@popkov.me>
2017-10-10 21:05:51 +0300

    Update telegram-alpha to 3.4-107494,653

--- Casks/telegram-alpha.rb
+++ Casks/telegram-alpha.rb
@@ -1,11 +1,11 @@
 cask 'telegram-alpha' do
-  version '3.4-107390,644'
-  sha256 'f894ac07e254798e1d9d7b89ffd631a38487b2e1ee6cbd0b1f1b08cf1681bea8'
+  version '3.4-107494,653'
+  sha256 '10415eb3909f6356cab70e30f9183e106ce2dc850cc3356a86c38edcb1ff2711'
 
   # hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f',
-          checkpoint: 'f1a044508256d33720abc94605f9ba6de1de165070f0e1bdfe3eaf0363e4cbd1'
+          checkpoint: '0a87c681977196ab79298f14ba5ff2b4ba68c3e468c86ebd333adfbe67b8ffc7'
   name 'Telegram for macOS'
   name 'Telegram Swift'
   homepage 'https://macos.telegram.org/'
