6314ee92e7763b1ddf22d3ea2faad55d9cfdc264
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-10-13 07:26:57 +0900

    Update atom-beta to 1.22.0-beta1

--- Casks/atom-beta.rb
+++ Casks/atom-beta.rb
@@ -1,11 +1,11 @@
 cask 'atom-beta' do
-  version '1.22.0-beta0'
-  sha256 'fe73bc865fa70aa9c7a829645c5083c323e81a1e541f46177f735a45e6aa0656'
+  version '1.22.0-beta1'
+  sha256 'c0c4dbaab19e89716a3fa0828016c0e0c97675af3aa054a0b6e6e5a966372f11'
 
   # github.com/atom/atom was verified as official when first introduced to the cask
   url "https://github.com/atom/atom/releases/download/v#{version}/atom-mac.zip"
   appcast 'https://github.com/atom/atom/releases.atom',
-          checkpoint: 'bbaf2d1fb5ef01af50f2a44b206fad6c5c57ece6956740846ea37a502ba8ea31'
+          checkpoint: '98158c2517a1f5f47508fc47fdc1826990b61096a482b042f5cac6312cf5a22b'
   name 'Github Atom Beta'
   homepage 'https://atom.io/beta'
 
