e1737dec94336c5576db40fc3da3e98e395f9723
commitay <commitay@users.noreply.github.com>
2017-10-13 14:27:22 +1000

    Update palemoon to 27.5.1

--- Casks/palemoon.rb
+++ Casks/palemoon.rb
@@ -1,6 +1,6 @@
 cask 'palemoon' do
-  version '27.5.0'
-  sha256 '019686a754204f0691722d332d28678509a32cbfadbde17a0d005435ff9da369'
+  version '27.5.1'
+  sha256 '40951232738cb643a3365e42fd7730975413a4d8ee0632e6ccb78cf618d8bfda'
 
   url "https://mac.palemoon.org/dist/palemoon-#{version}.mac64.dmg"
   name 'Palemoon'
