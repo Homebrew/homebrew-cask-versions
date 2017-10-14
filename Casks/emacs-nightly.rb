ba32cc124a4a9aca65e0ad9c3d6be867b28e18dd
commitay <commitay@users.noreply.github.com>
2017-10-14 13:09:31 +1000

    style

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -6,7 +6,7 @@ cask 'emacs-nightly' do
     require 'open-uri'
     base_url = 'https://emacsformacosx.com/atom/daily'
     file = open(base_url).read.scan(%r{href="([^"]+.dmg)"}).flatten.first
-    "#{file}"
+    file.to_s
   end
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
