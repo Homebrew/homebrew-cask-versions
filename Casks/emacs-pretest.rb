7ab7024600e916381d1c648fbb425718c0520d59
Alexandre Pauzies <alexandre@pauzies.com>
2017-10-13 22:51:36 -0400

    Update emacs-pretest to 26.0.90

--- Casks/emacs-pretest.rb
+++ Casks/emacs-pretest.rb
@@ -1,10 +1,10 @@
 cask 'emacs-pretest' do
-  version '25.2-rc2'
-  sha256 '1f5cc002a706915551d406f9bd465229fee749538a54f10fc52b27add597304d'
+  version '26.0.90'
+  sha256 '85ed39cda9795ca1951b0ecae40d5c965a1f0b13e1cc4691ba5ccb1145ac178f'
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-pretest-#{version}-universal.dmg"
   appcast 'https://emacsformacosx.com/atom/pretest',
-          checkpoint: 'ea71786763eedb4198a155cc3f0c7728f0757ea8dc3473c6494d7e8b99ec2ff8'
+          checkpoint: '855d4c518cad52c8b643fb9c39ebcacfd2fcf63b8fd3183cc6a0eb2690440375'
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
