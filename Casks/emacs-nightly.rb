3ad2e4b1feb5291ad0d48828dc2a4ac1adb8dcfa
commitay <commitay@users.noreply.github.com>
2017-10-14 13:01:21 +1000

    Update emacs-nightly to latest

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,10 +1,13 @@
 cask 'emacs-nightly' do
-  version '2017-06-11_01-41-11-517a0394d2ba15b517e2a2b7384a7cc98b11b80d'
-  sha256 '37819d941188f8ee76adf9f228a6bfde5eee0c4dfd7a1799e3ff9dd4b10dfb8c'
+  version :latest
+  sha256 :no_check
 
-  url "https://emacsformacosx.com/emacs-builds/Emacs-#{version}-universal.dmg"
-  appcast 'https://emacsformacosx.com/atom/daily',
-          checkpoint: 'd29661d8fa407320834e735f781ac37ac1778728da64473291eae3fad985c909'
+  url do
+    require 'open-uri'
+    base_url = 'https://emacsformacosx.com/atom/daily'
+    file = open(base_url).read.scan(%r{href="([^"]+.dmg)"}).flatten.first
+    "#{file}"
+  end
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
