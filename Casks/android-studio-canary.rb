aaeef4ca0a07a26b4f40546a6240aef548f6d61a
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-20 16:08:51 +0800

    Update android-studio-canary.rb

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -2,6 +2,7 @@ cask 'android-studio-canary' do
   version '3.0.0.17,171.4402976'
   sha256 'b96a13a25562c44a3b213c25afa0c64eab888e653f71b16b1edf940efecabfdf'
 
+  # google.com/dl/android/studio was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Canary'
   homepage 'https://developer.android.com/studio/preview/'
