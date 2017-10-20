e64a5c0266d5f9922a371dadc38ad5b165c2542c
commitay <commitay@users.noreply.github.com>
2017-10-20 17:26:04 +1000

    Update torbrowser-alpha to 7.5a6

--- Casks/torbrowser-alpha.rb
+++ Casks/torbrowser-alpha.rb
@@ -1,12 +1,11 @@
 cask 'torbrowser-alpha' do
-  version '7.5a5'
-  sha256 'd6dd4420a6b3c9700c300aa1fb19604b22633d571bfd3506ff160afe8bfd6714'
+  version '7.5a6'
+  sha256 '8c5efca1fef27fa74f2feea0f1208244876cd9d55ed941ed348364a0b514ef0e'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
   name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
-  gpg "#{url}.asc",
-      key_id: 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
+  gpg "#{url}.asc", key_id: 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
 
   app 'TorBrowser.app'
 end
