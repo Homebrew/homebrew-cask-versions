43db473fb8f5920a5e39ab8ca49d694d19ce8770
ayayk <git@ayatk.com>
2017-10-20 16:20:41 +0900

    Update android-studio-canary to 3.0.0.17,171.4402976

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,6 +1,6 @@
 cask 'android-studio-canary' do
-  version '3.0.0.16,171.4392136'
-  sha256 '5ffba5bec8b8795ccacfe6f7116822b2dbb865f3e536d711e60d1b3575d930f7'
+  version '3.0.0.17,171.4402976'
+  sha256 'b96a13a25562c44a3b213c25afa0c64eab888e653f71b16b1edf940efecabfdf'
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Canary'
