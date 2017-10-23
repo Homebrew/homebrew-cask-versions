7c69271725cfee52dcebde70629bb4a9c58ecdad
David Yantis <404NetworkError@users.noreply.github.com>
2017-10-23 01:00:43 -0500

    Add keka-beta 1.1.0,5 (#4651)
    * Add keka-beta 1.1.0,5
    
    * Updated dependent OS version
    
    * Added auto_updates
    
    * Update keka-beta.rb
    
    * Update keka-beta.rb
    
    * Update keka-beta.rb

---
+++ Casks/keka-beta.rb
@@ -0,0 +1,24 @@
+cask 'keka-beta' do
+  version '1.1.0,5'
+  sha256 '6e36747e75d108add96d77911797234e509643f9d973cc08ba80c3a0ccca68a0'
+
+  # github.com/aonez/Keka was verified as official when first introduced to the cask
+  url "https://github.com/aonez/Keka/releases/download/v#{version.before_comma}-beta.#{version.after_comma}/Keka-#{version.before_comma}-beta.#{version.after_comma}.dmg"
+  appcast 'https://github.com/aonez/Keka/releases.atom',
+          checkpoint: '3c19d0972d4868f6cd0ddd0dcfc42494606be820db6af7ff5688f4cad65b15f6'
+  name 'Keka'
+  homepage 'http://www.kekaosx.com/'
+
+  auto_updates true
+  conflicts_with cask: 'keka'
+  depends_on macos: '>= :mountain_lion'
+
+  app 'Keka.app'
+
+  zap delete: [
+                '~/Library/Application Support/Keka',
+                '~/Library/Caches/com.aone.keka',
+                '~/Library/Preferences/com.aone.keka.plist',
+                '~/Library/Saved Application State/com.aone.keka.savedState',
+              ]
+end
