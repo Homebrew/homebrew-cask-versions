8286a4606d07d24b609b0947dc13c4d6f1909280
commitay <commitay@users.noreply.github.com>
2017-10-24 17:18:30 +1000

    Rename android-studio-canary to android-studio-preview

--- Casks/android-studio-preview.rb
+++ Casks/android-studio-preview.rb
@@ -1,12 +1,13 @@
-cask 'android-studio-canary' do
+cask 'android-studio-preview' do
   version '3.0.0.17,171.4402976'
   sha256 'b96a13a25562c44a3b213c25afa0c64eab888e653f71b16b1edf940efecabfdf'
 
   # google.com/dl/android/studio was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
-  name 'Android Studio Canary'
+  name 'Android Studio Preview'
   homepage 'https://developer.android.com/studio/preview/'
 
+  # Renamed to avoid conflict with android-studio.
   app 'Android Studio.app', target: 'Android Studio Preview.app'
 
   zap delete: [
