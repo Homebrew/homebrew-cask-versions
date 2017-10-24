1b6cdc8e799eb44ae1ed459f76d7e61d16a6bb96
Miccal Matthews <miccal.matthews@gmail.com>
2017-10-25 06:54:04 +0800

    Update skype-preview.rb

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,8 +1,9 @@
 cask 'skype-preview' do
-  version :latest
-  sha256 :no_check
+  version '8.9.76.64297'
+  sha256 'db0fde68ea1a8c3b45783135d4b5deece8928bcce6ce2adbafc8755cf149ef41'
 
-  url 'https://go.skype.com/mac.preview.download'
+  # endpoint920510.azureedge.net/s4l/s4l/download/mac was verified as official when first introduced to the cask
+  url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg"
   name 'Skype Preview'
   homepage 'https://www.skype.com/en/insider/'
 
