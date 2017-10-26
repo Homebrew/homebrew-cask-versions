1d3e6bae8f80c5990c73c6bbaf72d02b5d0dfb72
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-10-26 11:46:40 +0900

    Update github-beta to 1.0.5-beta1-fd971fb7 (#4664)

--- Casks/github-beta.rb
+++ Casks/github-beta.rb
@@ -1,11 +1,11 @@
 cask 'github-beta' do
-  version '1.0.5-beta0-93cfe3bf'
-  sha256 '23ab2df5749bf882180e053444abbca48c17b3d7f63aca0096f7f2ae488ef7fa'
+  version '1.0.5-beta1-fd971fb7'
+  sha256 '98968c3af7ffbbd4caf32c9bc3ba5421568984e310ee77b0121a9571edce9fb6'
 
   # githubusercontent.com was verified as official when first introduced to the cask
   url "https://desktop.githubusercontent.com/releases/#{version}/GitHubDesktop.zip"
   appcast 'https://github.com/desktop/desktop/releases.atom',
-          checkpoint: '552ef7130866a5149974e9232452dd34f9b483b40195a01b080073c52f1cfc71'
+          checkpoint: '03e38f6eba88d3a75d498cf9050f71885d2568457d08db320369b777e2d1e5a1'
   name 'GitHub Desktop'
   homepage 'https://desktop.github.com/'
 
