5e2ab7b6b75d5f23089344e34e6e26a63234d4fb
commitay <commitay@users.noreply.github.com>
2017-10-27 17:53:10 +1000

    java6

--- Casks/java6.rb
+++ Casks/java6.rb
@@ -1,8 +1,8 @@
 cask 'java6' do
-  version '1.6.0_65'
-  sha256 '8f40dbf821e21410feab4d9e2e533c42518897b7cac5edfad895e91016f918fc'
+  version '1.6.0_65-b14-468'
+  sha256 'a6ea47965542b5c06787f832f3ae5be65da6c6ed91664b0c0ed4994650d98244'
 
-  url 'http://supportdownload.apple.com/download.info.apple.com/Apple_Support_Area/Apple_Software_Updates/Mac_OS_X/downloads/031-29055.20150831-0f779fb2-4bf4-11e5-a8d8-/javaforosx.dmg'
+  url 'https://support.apple.com/downloads/DL1572/en_US/javaforosx.dmg'
   name 'Java Standard Edition Development Kit'
   homepage 'https://support.apple.com/kb/DL1572'
 
