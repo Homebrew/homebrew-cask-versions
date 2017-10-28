05be8e370dfcd8235bb2eecfc099b688b2d37bc0
Yurii Kolesnykov <yurikoles@gmail.com>
2017-10-28 04:52:32 +0300

    Update android-studio-preview to 3.1.0.0,171.4415322 (#4673)
    * Update android-studio-preview to 3.1.0.0,171.4415322
    
    * remove target

--- Casks/android-studio-preview.rb
+++ Casks/android-studio-preview.rb
@@ -1,14 +1,13 @@
 cask 'android-studio-preview' do
-  version '3.0.0.18,171.4408382'
-  sha256 'f75c99356259c42e00b05af1627bc06c74819850b02b95e7ff4c28e9b397120f'
+  version '3.1.0.0,171.4415322'
+  sha256 'a1945308fd7b01ab3360a6d1b7570cf1e48cb5788f09783b9851d1d54246558d'
 
   # google.com/dl/android/studio was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Preview'
   homepage 'https://developer.android.com/studio/preview/'
 
-  # Renamed to avoid conflict with android-studio.
-  app 'Android Studio.app', target: 'Android Studio Preview.app'
+  app "Android Studio #{version.major_minor} Preview.app"
 
   zap delete: [
                 "~/Library/Application Support/AndroidStudioPreview#{version.major_minor}",
