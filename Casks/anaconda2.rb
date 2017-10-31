6a82e015d5714275db9dfcc43a6c1c7425f52de8
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-10-31 11:48:17 +0900

    Update anaconda2 to 5.0.1

--- Casks/anaconda2.rb
+++ Casks/anaconda2.rb
@@ -1,6 +1,6 @@
 cask 'anaconda2' do
-  version '5.0.0'
-  sha256 'd85198c63657924fae11b6ea5961f50d81d09a1185d6f0a9a9d5bc69eb788ccc'
+  version '5.0.1'
+  sha256 'e3a9a5c84cb89ff079b0781ba773a3433d490fe0cfc24042c613a5674748d87b'
 
   # repo.continuum.io/archive was verified as official when first introduced to the cask
   url "https://repo.continuum.io/archive/Anaconda2-#{version}-MacOSX-x86_64.sh"
