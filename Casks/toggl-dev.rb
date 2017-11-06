ac995b33af2d4f7b1235fabb59e36ddd5774e5de
Victor Popkov <victor@popkov.me>
2017-11-06 23:56:20 +0200

    Update toggl-dev sha256 and checkpoint

--- Casks/toggl-dev.rb
+++ Casks/toggl-dev.rb
@@ -1,11 +1,11 @@
 cask 'toggl-dev' do
   version '7.4.79'
-  sha256 '3bd36690042561b360d61ad325b13c56fb8dcaed58ad30e112bfd8e2a593d1cb'
+  sha256 'ea4b12f914acb8377ad2d19da0b9d2660d1b4a315887155ae906cdc0206800fa'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_dev_appcast.xml',
-          checkpoint: 'ce2e1866b316b4f6957fb95c6dd6c9a029134ad21649773bbb10ad16af5131fd'
+          checkpoint: 'fb4aafcc9805560e7ffb7ee8476497cdbdc87beb70c4fae414c4bc725358e7bd'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
 
