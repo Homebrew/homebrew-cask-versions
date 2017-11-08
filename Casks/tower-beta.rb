f95ac407a498fc6d16feec7a20bad7ecacf5ed56
Victor Popkov <victor@popkov.me>
2017-11-08 20:04:15 +0200

    Update tower-beta to 2.6.4-357,357-a5932bd4

--- Casks/tower-beta.rb
+++ Casks/tower-beta.rb
@@ -1,11 +1,11 @@
 cask 'tower-beta' do
-  version '2.6.3-355,355-a7440d07'
-  sha256 '2c1207bb586ec22a930aee08056edb0c0304e6138e2d0c46733536f6f8599e86'
+  version '2.6.4-357,357-a5932bd4'
+  sha256 'c969606ee68e4094785918aeefcd8c1c94b3e23934658675ccb36e5500d6078d'
 
   # amazonaws.com/apps/tower2-mac was verified as official when first introduced to the cask
   url "https://fournova-app-updates.s3.amazonaws.com/apps/tower2-mac/#{version.after_comma}/Tower-2-#{version.before_comma}.zip"
   appcast 'https://updates.fournova.com/updates/tower2-mac/beta',
-          checkpoint: '313e45dc96cae042456c0b9fad047f3dd8d51a9b8e746cdfce50bf08386e6551'
+          checkpoint: '79f26e99ed87cacb90a583c11092f9ae96de75604811ec21e0469bbba480fc55'
   name 'Tower'
   homepage 'https://www.git-tower.com/'
 
