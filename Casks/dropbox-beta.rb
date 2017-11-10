8ea20a708cc0501becfb5266139bab2dbc2cd4d5
miccal <miccal.matthews@gmail.com>
2017-11-10 10:45:19 +0800

    Update dropbox-beta to 39.3.48

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,6 +1,6 @@
 cask 'dropbox-beta' do
-  version '37.3.25'
-  sha256 'cd1bf14d2f01e1652ae55d9e6b68fa070ca182ece20710ab9a3fb3c99a3adecd'
+  version '39.3.48'
+  sha256 '02a8a0e00c0f94fa286900aee7fd39eedf5b4ad372f5c8f4cb5904fdcf8f3739'
 
   # clientupdates.dropboxstatic.com was verified as official when first introduced to the cask
   url "https://clientupdates.dropboxstatic.com/client/Dropbox%20#{version}.dmg"
@@ -13,13 +13,16 @@ cask 'dropbox-beta' do
 
   zap delete: [
                 '/Library/DropboxHelperTools',
+                '/System/Library/CoreServices/CoreTypes.bundle/Contents/Resources/SidebarDropBoxFolder.icns',
                 '~/Library/Application Scripts/com.getdropbox.dropbox.garcon',
                 '~/Library/Caches/CloudKit/com.apple.bird/iCloud.com.getdropbox.Dropbox',
                 '~/Library/Caches/com.dropbox.DropboxMacUpdate',
                 '~/Library/Caches/com.getdropbox.dropbox',
                 '~/Library/Caches/com.getdropbox.DropboxMetaInstaller',
+                '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.dropbox.DropboxMacUpdate',
                 '~/Library/Containers/com.getdropbox.dropbox.garcon',
                 '~/Library/Group Containers/com.getdropbox.dropbox.garcon',
+                '~/Library/LaunchAgents/com.dropbox.DropboxMacUpdate.agent.plist',
                 '~/Library/Logs/Dropbox_debug.log',
               ],
       trash:  [
@@ -27,6 +30,7 @@ cask 'dropbox-beta' do
                 '~/Library/Application Support/Dropbox',
                 '~/Library/Dropbox',
                 '~/Library/Preferences/com.dropbox.DropboxMacUpdate.plist',
+                '~/Library/Preferences/com.dropbox.DropboxMonitor.plist',
                 '~/Library/Preferences/com.dropbox.tungsten.helper.plist',
                 '~/Library/Preferences/com.getdropbox.dropbox.plist',
               ]
