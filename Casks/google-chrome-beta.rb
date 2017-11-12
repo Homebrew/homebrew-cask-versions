1dde959e1cb5605e1de14c6150580662bcf5604d
miccal <miccal.matthews@gmail.com>
2017-11-12 19:12:03 +0800

    Update google-chrome-beta to latest

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -8,17 +8,37 @@ cask 'google-chrome-beta' do
 
   app 'Google Chrome.app'
 
+  uninstall launchctl: [
+                         'com.google.keystone.agent',
+                         'com.google.keystone.daemon',
+                       ]
+
   zap delete: [
-                '~/Library/Application Support/Google/Chrome',
+                '/Library/Google',
+                '/Library/Caches/com.google.SoftwareUpdate.*',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.google.chrome.app.*.sfl*',
+                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.google.chrome.sfl*',
                 '~/Library/Caches/Google/Chrome',
                 '~/Library/Caches/com.google.Chrome',
-                '~/Library/Caches/com.google.Chrome.helper.EH',
+                '~/Library/Caches/com.google.Chrome.helper.*',
+                '~/Library/Caches/com.google.Keystone',
                 '~/Library/Caches/com.google.Keystone.Agent',
                 '~/Library/Caches/com.google.SoftwareUpdate',
                 '~/Library/Google/GoogleSoftwareUpdate',
+                '~/Library/Google/Google Chrome Brand.plist',
+                '~/Library/LaunchAgents/com.google.keystone.agent.plist',
                 '~/Library/Logs/GoogleSoftwareUpdateAgent.log',
+                '~/Library/Saved Application State/com.google.Chrome.app.*.savedState',
+                '~/Library/Saved Application State/com.google.Chrome.savedState',
+                '~/Library/WebKit/com.google.Chrome',
+              ],
+      teash:  [
+                '~/Library/Application Support/Google/Chrome',
+                '~/Library/Preferences/com.google.Chrome.plist',
+                '~/Library/Preferences/com.google.Keystone.Agent.plist',
               ],
       rmdir:  [
+                '~/Library/Application Support/Google',
                 '~/Library/Caches/Google',
                 '~/Library/Google',
               ]
