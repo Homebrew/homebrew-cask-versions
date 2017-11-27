79b7290bd479295bfeb19014174fa12ac776b1c3
Sven Assmann <sven@d34dl0ck.me>
2017-11-27 13:22:41 +0100

    changes to fit the token reference guidlines

--- Casks/virtualbox51.rb
+++ Casks/virtualbox51.rb
@@ -1,9 +1,9 @@
-cask 'virtualbox-51' do
+cask 'virtualbox51' do
   version '5.1.30,118389'
   sha256 'bc1c6c341fcc04a8e214f9e4ced44058c6239f30ddd0e1a923f04c8d01c92d91'
 
   url "http://download.virtualbox.org/virtualbox/#{version.before_comma}/VirtualBox-#{version.before_comma}-#{version.after_comma}-OSX.dmg"
-  name 'Oracle VirtualBox 5.1'
+  name 'Oracle VirtualBox @ v5.1'
   homepage 'https://www.virtualbox.org/wiki/Download_Old_Builds_5_1'
 
   pkg 'VirtualBox.pkg'
