2af11785f2a08245d4d9498be59da7081bc2a552
commitay <commitay@users.noreply.github.com>
2017-11-27 18:52:04 +1000

    1.39.3

--- Casks/exoduseden.rb
+++ Casks/exoduseden.rb
@@ -1,8 +1,8 @@
 cask 'exoduseden' do
-  version '1.38.2'
-  sha256 'ddea4d83ed1a418ccde40d28dc5e3b29b909e0ca627479c563b142adad4fd0cc'
+  version '1.39.3'
+  sha256 '0c6a50c7cf865d6d9afc398a9dc8804c4338493a08876ed646b6e1967fc3f2c0'
 
-  # exodusbin.azureedge.net/releases/exodus-eden-macos was verified as official when first introduced to the cask
+  # exodusbin.azureedge.net/releases was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-eden-macos-#{version}.dmg"
   name 'Exodus Eden'
   homepage 'https://www.exodus.io/eden/'
