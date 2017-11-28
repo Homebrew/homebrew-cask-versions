832899fb56aace5e73be48c285454e78c1b98cec
Thomas Parisot <oncletom@users.noreply.github.com>
2017-11-28 09:05:59 +0000

    Remove trailing space

--- Casks/firefoxdeveloperedition.rb
+++ Casks/firefoxdeveloperedition.rb
@@ -15,7 +15,7 @@ cask 'firefoxdeveloperedition' do
     sha256 '05f05d523ff7f8417d3e89782be45b515f6e892519e7a56844ed8638e86611d1'
     'en-US'
   end
-  
+
   language 'fr' do
     sha256 '2c78a25bd772da057a074f11c4022a21be040cd526a56e2bca13d240f221ddc6'
     'fr'
