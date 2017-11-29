f285ae9174cb2081b800cb46ea67019e675d924d
miccal <miccal.matthews@gmail.com>
2017-11-30 06:54:23 +0800

    Update libreoffice-dev.rb

--- Casks/libreoffice-dev.rb
+++ Casks/libreoffice-dev.rb
@@ -10,23 +10,19 @@ cask 'libreoffice-dev' do
   homepage 'https://www.libreoffice.org/download/pre-releases/'
   gpg "#{url}.asc", key_id: 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
 
-  conflicts_with cask: [
-                         'libreoffice',
-                         'libreoffice-still',
-                       ]
   depends_on macos: '>= :mountain_lion'
 
-  app 'LibreOffice.app'
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/gengal"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/regmerge"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/regview"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/senddoc"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/ui-previewer"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/uno"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/unoinfo"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/unopkg"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/uri-encode"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/xpdfimport"
+  app 'LibreOfficeDev.app'
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/gengal"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/regmerge"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/regview"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/senddoc"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/ui-previewer"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/uno"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/unoinfo"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/unopkg"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/uri-encode"
+  binary "#{appdir}/LibreOfficeDev.app/Contents/MacOS/xpdfimport"
   # shim script (https://github.com/caskroom/homebrew-cask/issues/18809)
   shimscript = "#{staged_path}/soffice.wrapper.sh"
   binary shimscript, target: 'soffice'
@@ -34,7 +30,7 @@ cask 'libreoffice-dev' do
   preflight do
     IO.write shimscript, <<~EOS
       #!/bin/sh
-      '#{appdir}/LibreOffice.app/Contents/MacOS/soffice' "$@"
+      '#{appdir}/LibreOfficeDev.app/Contents/MacOS/soffice' "$@"
     EOS
   end
 
