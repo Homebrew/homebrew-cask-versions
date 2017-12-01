933cb7bce8c5b99ce7ce2de64c2636f2923c10e5
Victor Popkov <victor@popkov.me>
2017-12-01 10:03:07 +0200

    Update opera-developer to 51.0.2791.0 (#4891)

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask 'opera-developer' do
-  version '51.0.2781.0'
-  sha256 '266bbe9eaf745e508534c7400719f6ecef74aee19ff8947d5713b5c7dfc7e457'
+  version '51.0.2791.0'
+  sha256 'b8015baef981d05abf67d5ed65e06c43d4d05a8f38a8d30a28b609992d0be09a'
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name 'Opera Developer'
