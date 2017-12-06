29710c2eb19cdaccc7a409ebf2acab268b2bee57
miccal <miccal.matthews@gmail.com>
2017-12-06 20:07:09 +0800

    Update torbrowser-alpha.rb (#4916)

--- Casks/torbrowser-alpha.rb
+++ Casks/torbrowser-alpha.rb
@@ -3,6 +3,8 @@ cask 'torbrowser-alpha' do
   sha256 'e45c7b6e10202e9e1f6f03e55c16bd70a6690568a47eab6470054a7a7c3b2a0f'
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
+  appcast 'https://dist.torproject.org/torbrowser/',
+          checkpoint: '19bba157bc3a7789f5ce3141153264e300c0358f85b7bbd32b748b4c66468680'
   name 'Tor Browser'
   homepage 'https://www.torproject.org/projects/torbrowser.html'
   gpg "#{url}.asc", key_id: 'ef6e286dda85ea2a4ba7de684e2c6e8793298290'
