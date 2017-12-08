3cbc5f2acfc415e6057e94ddf45a33cc1893878b
Shoichi Aizawa <shoichiaizawa@gmail.com>
2017-12-08 19:51:59 +0900

    Update hyper-canary to 2.0.0-canary.8 (#4931)

--- Casks/hyper-canary.rb
+++ Casks/hyper-canary.rb
@@ -1,11 +1,11 @@
 cask 'hyper-canary' do
-  version '2.1.1'
-  sha256 '5672e408cfe07008ac4eb1c01b86251837b9b743434a14b29ef372a4ef9f6148'
+  version '2.0.0-canary.8'
+  sha256 '1dddb18db9af04350b8c520610ca16b9527e783b8746088d2a9f6da62574aa7e'
 
   # github.com/zeit/hyper was verified as official when first introduced to the cask
   url "https://github.com/zeit/hyper/releases/download/#{version}/hyper-#{version}-mac.zip"
   appcast 'https://github.com/zeit/hyper/releases.atom',
-          checkpoint: '5c7198ff0aee36409854b085f9cdbf451f8b8e7e55d23a0bcdaf046b23fec074'
+          checkpoint: '2ff6fa3fa92f451c706094cb81bc842646fb1b4e8fd9559a19028b199712739e'
   name 'Hyper'
   homepage 'https://hyper.is/'
 
