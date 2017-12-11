acd4cbcbc94fa8f33c4208aff5bd569f7d3688f6
commitay <commitay@users.noreply.github.com>
2017-12-11 10:29:57 +1000

    Update adobe-air-beta to latest

--- Casks/adobe-air-beta.rb
+++ Casks/adobe-air-beta.rb
@@ -5,7 +5,7 @@ cask 'adobe-air-beta' do
   # fpdownload.macromedia.com was verified as official when first introduced to the cask
   url 'https://fpdownload.macromedia.com/pub/labs/flashruntimes/air/AdobeAIR.dmg'
   name 'Adobe AIR Beta'
-  homepage 'http://labs.adobe.com/downloads/air.html'
+  homepage 'https://labs.adobe.com/downloads/air.html'
 
   installer script: {
                       executable: 'Adobe AIR Installer.app/Contents/MacOS/Adobe AIR Installer',
