57d7de63bd2dc4c60ec5eb7a56de3e9d0d851a23
commitay <commitay@users.noreply.github.com>
2017-12-13 16:25:44 +1000

    Update chirp-daily to 20171204

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,10 +1,10 @@
 cask 'chirp-daily' do
-  version '20171013'
-  sha256 'c47deef8b3b33e22ed4256de39c78d413a4b9b3759056089eefccfd83f476b08'
+  version '20171204'
+  sha256 '87a673893576531fe704d24a7f4bc144444108d5557a972e91570655fcedc5e3'
 
-  url "http://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
+  url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
-  homepage 'http://chirp.danplanet.com/projects/chirp/wiki/Home'
+  homepage 'https://chirp.danplanet.com/projects/chirp/wiki/Home'
 
   depends_on cask: 'kk7ds-python-runtime'
 
