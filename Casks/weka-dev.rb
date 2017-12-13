f63146734858b575446adb8900c5152e6d7e63c2
commitay <commitay@users.noreply.github.com>
2017-12-13 15:02:28 +1000

    Update weka-dev to 3.9.1

--- Casks/weka-dev.rb
+++ Casks/weka-dev.rb
@@ -5,9 +5,9 @@ cask 'weka-dev' do
   # sourceforge.net/weka was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-oracle-jvm.dmg"
   appcast 'https://sourceforge.net/projects/weka/rss',
-          checkpoint: '39aecb6a42219cfc46e42fb5c6518c4a3defdf1c3c285e2c9672589ca2f16e34'
+          checkpoint: '0dc0c9a8e7e619b9f81f7ff80b888bc3cf074373e521bffac5e61d73dc6fd40b'
   name 'Weka'
-  homepage 'http://www.cs.waikato.ac.nz/ml/weka/'
+  homepage 'https://www.cs.waikato.ac.nz/ml/weka/'
 
   app "weka-#{version.dots_to_hyphens}-oracle-jvm.app"
 end
