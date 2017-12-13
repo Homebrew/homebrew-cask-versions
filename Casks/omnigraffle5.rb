baab479c08a91d776c519d178637798fd29929d0
commitay <commitay@users.noreply.github.com>
2017-12-13 17:07:42 +1000

    Update omnigraffle5 to 5.4.4 (#4948)

--- Casks/omnigraffle5.rb
+++ Casks/omnigraffle5.rb
@@ -2,7 +2,7 @@ cask 'omnigraffle5' do
   version '5.4.4'
   sha256 '7bcc64093f46bd4808b1a4cb86cf90c0380a5c5ffffd55ce8f742712818558df'
 
-  url "http://www.omnigroup.com/ftp1/pub/software/MacOSX/10.6/OmniGraffle-#{version}.dmg"
+  url "https://downloads.omnigroup.com/software/MacOSX/10.6/OmniGraffle-#{version}.dmg"
   name 'OmniGraffle 5'
   homepage 'https://www.omnigroup.com/omnigraffle/'
 
