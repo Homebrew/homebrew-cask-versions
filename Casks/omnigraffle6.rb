b263200d326227f5f7434864a5234fa12c6690fe
commitay <commitay@users.noreply.github.com>
2017-12-13 15:10:50 +1000

    Update omnigraffle6 to 6.6.2

--- Casks/omnigraffle6.rb
+++ Casks/omnigraffle6.rb
@@ -2,7 +2,7 @@ cask 'omnigraffle6' do
   version '6.6.2'
   sha256 'f0b05a654686c42703cddef646a2519235b45d26bd06988a6e644aa96c0eb828'
 
-  url "http://downloads.omnigroup.com/software/MacOSX/10.10/OmniGraffle-#{version}.dmg"
+  url "https://downloads.omnigroup.com/software/MacOSX/10.10/OmniGraffle-#{version}.dmg"
   name 'OmniGraffle 6'
   homepage 'https://www.omnigroup.com/omnigraffle/'
 
