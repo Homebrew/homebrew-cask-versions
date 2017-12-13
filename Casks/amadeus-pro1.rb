aea42cb99fb938fcd044a4f8763433da508eba84
commitay <commitay@users.noreply.github.com>
2017-12-13 16:33:02 +1000

    Update amadeus-pro1 to 1.5.5 (#4945)

--- Casks/amadeus-pro1.rb
+++ Casks/amadeus-pro1.rb
@@ -2,9 +2,9 @@ cask 'amadeus-pro1' do
   version '1.5.5'
   sha256 '29e5d6a2567c9b1a418667b99844de186a6621992f3398583c959aed2383d0b6'
 
-  url 'http://www.hairersoft.com/Downloads/AmadeusProOld.zip'
+  url 'https://www.hairersoft.com/Downloads/AmadeusProOld.zip'
   name 'Amadeus Pro'
-  homepage 'http://www.hairersoft.com/pro.html'
+  homepage 'https://www.hairersoft.com/pro.html'
 
   app 'Amadeus Pro.app'
 end
