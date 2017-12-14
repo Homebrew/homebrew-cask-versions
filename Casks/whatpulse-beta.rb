738eade39739d8c8c0c37a4df3b0c2b8c964a89f
commitay <commitay@users.noreply.github.com>
2017-12-14 15:00:18 +1000

    Update whatpulse-beta to 2.7.2b1 (#4957)

--- Casks/whatpulse-beta.rb
+++ Casks/whatpulse-beta.rb
@@ -2,7 +2,7 @@ cask 'whatpulse-beta' do
   version '2.7.2b1'
   sha256 '54dc8c55cdca3d2f19bcd7ea788ac6aa2c57f6c7629e82de32ee93cc6211e0ad'
 
-  url "http://static.whatpulse.org/files/beta/whatpulse-mac-#{version}.dmg"
+  url "https://static.whatpulse.org/files/beta/whatpulse-mac-#{version}.dmg"
   name 'WhatPulse'
   homepage 'https://whatpulse.org/'
 
