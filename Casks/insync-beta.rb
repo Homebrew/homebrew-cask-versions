1f9901766a11fd43547a15215aabf3cceb21ea59
commitay <commitay@users.noreply.github.com>
2017-12-15 08:16:13 +1000

    use forums as homepage for download links

--- Casks/insync-beta.rb
+++ Casks/insync-beta.rb
@@ -2,10 +2,10 @@ cask 'insync-beta' do
   version '1.4.1.37037'
   sha256 '0fca8d6b3240406608141e480f4238d41f6ddab75cbf8d53e1591d128068052f'
 
-  # d2t3ff60b2tol4.cloudfront.net/builds/Insync was verified as official when first introduced to the cask
+  # d2t3ff60b2tol4.cloudfront.net/builds was verified as official when first introduced to the cask
   url "https://d2t3ff60b2tol4.cloudfront.net/builds/Insync-#{version}.dmg"
   name 'Insync'
-  homepage 'https://www.insynchq.com/'
+  homepage 'https://forums.insynchq.com/'
 
   conflicts_with cask: 'insync'
 
