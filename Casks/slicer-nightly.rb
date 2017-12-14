d12f7a5047db193c5e30a796233d6275a9e1276a
commitay <commitay@users.noreply.github.com>
2017-12-14 15:03:27 +1000

    Update slicer-nightly: https (#4958)

--- Casks/slicer-nightly.rb
+++ Casks/slicer-nightly.rb
@@ -3,7 +3,7 @@ cask 'slicer-nightly' do
   sha256 '162df5983013e8c5e262f9e40c23eca1e0b33b18dd3844192e5adde3369bc243'
 
   # slicer.kitware.com/midas3 was verified as official when first introduced to the cask
-  url "http://slicer.kitware.com/midas3/download?bitstream=#{version.after_comma}"
+  url "https://slicer.kitware.com/midas3/download?bitstream=#{version.after_comma}"
   name '3D Slicer Nightly'
   homepage 'https://www.slicer.org/'
 
