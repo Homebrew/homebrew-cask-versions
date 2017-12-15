0c66d3521ec0dc4c767b8e99b0a06edb19e2b57d
Yurii Kolesnykov <yurikoles@gmail.com>
2017-12-15 22:54:16 +0200

    new cask: insync-beta-1.4.1.37037 (#4962)

---
+++ Casks/insync-beta.rb
@@ -0,0 +1,13 @@
+cask 'insync-beta' do
+  version '1.4.1.37037'
+  sha256 '0fca8d6b3240406608141e480f4238d41f6ddab75cbf8d53e1591d128068052f'
+
+  # d2t3ff60b2tol4.cloudfront.net/builds was verified as official when first introduced to the cask
+  url "https://d2t3ff60b2tol4.cloudfront.net/builds/Insync-#{version}.dmg"
+  name 'Insync'
+  homepage 'https://forums.insynchq.com/'
+
+  conflicts_with cask: 'insync'
+
+  app 'Insync.app'
+end
