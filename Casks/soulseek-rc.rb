0af81fb9ff3fdb5628b4bd1340838f3dc9af5d7f
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-12-21 14:30:16 +0100

    Update soulseek-rc to 2017-11-21

--- Casks/soulseek-rc.rb
+++ Casks/soulseek-rc.rb
@@ -1,9 +1,9 @@
 cask 'soulseek-rc' do
-  version '2017-7-29'
-  sha256 '2ccc407192e91a3a15ab60214e1799145434d666251e801b540701c406825758'
+  version '2017-11-21'
+  sha256 'ec9889bed69107fe56fd78745a668ef574bdd7d29ddd370464136109cbc4f11f'
 
-  # dl.dropboxusercontent.com/s/jx7790ztbn147ko was verified as official when first introduced to the cask
-  url "https://dl.dropboxusercontent.com/s/jx7790ztbn147ko/SoulseekQt-#{version}.dmg"
+  # dl.dropboxusercontent.com/s/ruvio22uu1onddi was verified as official when first introduced to the cask
+  url "https://dl.dropboxusercontent.com/s/ruvio22uu1onddi/SoulseekQt-#{version}.dmg"
   name 'Soulseek'
   homepage 'https://www.slsknet.org/'
 
