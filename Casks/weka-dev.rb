7db4025320ab5a90bd955b0f3810cb59196c2968
Victor Popkov <victor@popkov.me>
2017-12-22 19:55:54 +0200

    Update weka-dev to 3.9.2

--- Casks/weka-dev.rb
+++ Casks/weka-dev.rb
@@ -1,11 +1,11 @@
 cask 'weka-dev' do
-  version '3.9.1'
-  sha256 'eecbe8dd3ea1b2a10195077f19c0667c465cd37a8ae135c9e7f2748c0966060c'
+  version '3.9.2'
+  sha256 'dc5051bd460f16abc0a63ad4039a75a15aa442f2de4d043e44e91e33e7d28115'
 
   # sourceforge.net/weka was verified as official when first introduced to the cask
   url "https://downloads.sourceforge.net/weka/weka-#{version.dots_to_hyphens}-oracle-jvm.dmg"
   appcast 'https://sourceforge.net/projects/weka/rss',
-          checkpoint: '0dc0c9a8e7e619b9f81f7ff80b888bc3cf074373e521bffac5e61d73dc6fd40b'
+          checkpoint: '923267a39eca50cb2a0ab52ddee8c144a80a4357aeb6ac21906e74e08887706d'
   name 'Weka'
   homepage 'https://www.cs.waikato.ac.nz/ml/weka/'
 
