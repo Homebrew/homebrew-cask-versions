d0db7144255a109a768251d904f13ec0541edb30
martinschmelzer <schmelzer.martin@gmx.de>
2017-12-24 02:17:58 +0100

    Update rstudio-preview to 1.1.402 (#5016)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.1.393'
-  sha256 'a3218b558b9cc51ff4aeb9293050153f19fb55ab3580a2a3a63351cfea7816d5'
+  version '1.1.402'
+  sha256 '1e3afa03266c6464b205bb0fe4f58f8b062609a1a2f7bf46b8538be912575113'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
