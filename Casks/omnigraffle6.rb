7a8a7452691d1d963bb7ea2983760b24ee7ed80e
Tommaso Barbato <epistrephein@gmail.com>
2017-12-29 09:03:39 +0100

    Add zap stanza to omnigraffle6 6.6.2 (#5031)

--- Casks/omnigraffle6.rb
+++ Casks/omnigraffle6.rb
@@ -7,4 +7,13 @@ cask 'omnigraffle6' do
   homepage 'https://www.omnigroup.com/omnigraffle/'
 
   app 'OmniGraffle.app'
+
+  zap trash: [
+               '~/Library/Application Scripts/com.omnigroup.OmniGraffle6',
+               '~/Library/Application Scripts/com.omnigroup.OmniSoftwareUpdate.OSUCheckService',
+               '~/Library/Containers/com.omnigroup.OmniGraffle6',
+               '~/Library/Containers/com.omnigroup.OmniSoftwareUpdate.OSUCheckService',
+               '~/Library/Preferences/com.omnigroup.OmniGraffle6.plist',
+               '~/Library/Saved Application State/com.omnigroup.OmniGraffle6.savedState',
+             ]
 end
