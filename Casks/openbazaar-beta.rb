99dd9b309fe2b29cab2a57eb96209a53021a1d74
Victor Popkov <victor@popkov.me>
2017-12-31 23:13:16 +0200

    Update openbazaar-beta to 2.1.0-rc2

--- Casks/openbazaar-beta.rb
+++ Casks/openbazaar-beta.rb
@@ -1,11 +1,11 @@
 cask 'openbazaar-beta' do
-  version '2.1.0-rc1'
-  sha256 '57d79aedbfce514ddd5f181bd3445d5c6229bd3ebd96d9306f902de9a5aa8630'
+  version '2.1.0-rc2'
+  sha256 '7f5983d3f8da01a59451627f1fc1819aa6222f95b3dd988ae12eeee07c55185b'
 
   # github.com/OpenBazaar/openbazaar-desktop was verified as official when first introduced to the cask
   url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version}/OpenBazaar#{version.major}-#{version.major_minor_patch}.dmg"
   appcast 'https://github.com/OpenBazaar/openbazaar-desktop/releases.atom',
-          checkpoint: 'f03b7af797ea8a25a87c6deeed7aa1a500c1c8cc3eff29f95026c7b858d1caf9'
+          checkpoint: 'd971d6bc22d6dd49e7b1cee0f219941b2b746a03f81d736032ed70222daa3d1f'
   name 'OpenBazaar Beta'
   homepage 'https://www.openbazaar.org/'
 
