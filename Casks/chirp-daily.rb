187af9dddcf03cea2bd6a9c3bc46732d999e8f5c
Chuanchan <williamtsai1111@gmail.com>
2018-01-04 06:26:01 +0800

    Update chirp-daily to 20171229

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,6 +1,6 @@
 cask 'chirp-daily' do
-  version '20171204'
-  sha256 '87a673893576531fe704d24a7f4bc144444108d5557a972e91570655fcedc5e3'
+  version '20171229'
+  sha256 'bf0b9c81f934706982913f774b2d00a76a436338717ed140d4de63de5efa21ce'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
