fa244a1ea176426ac06fa97aee2b184ee5b8d761
Simone Carletti <weppos@weppos.net>
2018-01-09 13:34:29 +0100

    Update exoduseden to 1.42.0

--- Casks/exoduseden.rb
+++ Casks/exoduseden.rb
@@ -1,6 +1,6 @@
 cask 'exoduseden' do
-  version '1.40.0'
-  sha256 'd508c2d85f18a59c0cdbce57874fd9be2776481b30269d4a438e45acd48edf71'
+  version '1.42.0'
+  sha256 '3dea3ac16a75e0cd074f90eeb0d2ffff65def7ba477f0c90991b9ad7a841a941'
 
   # exodusbin.azureedge.net/releases was verified as official when first introduced to the cask
   url "https://exodusbin.azureedge.net/releases/exodus-eden-macos-#{version}.dmg"
