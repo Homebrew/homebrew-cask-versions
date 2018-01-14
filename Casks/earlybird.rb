7d3e5a58f205c7d5932c792a76ad783f406f8039
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-01-14 22:42:45 +0000

    Update earlybird to 58.0b3

--- Casks/earlybird.rb
+++ Casks/earlybird.rb
@@ -1,11 +1,14 @@
 cask 'earlybird' do
-  version '57.0a1'
+  version '58.0b3'
   sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://ftp.mozilla.org/pub/thunderbird/nightly/latest-comm-central/thunderbird-#{version}.en-US.mac.dmg"
+  # download-installer.cdn.mozilla.net/pub/thunderbird/releases was verified as official when first introduced to the cask
+  url "https://download-installer.cdn.mozilla.net/pub/thunderbird/releases/#{version}/mac/en-US/Thunderbird%20#{version}.dmg"
   name 'Earlybird'
   name 'Thunderbird Nightly'
   homepage 'https://www.mozilla.org/en-US/thunderbird/channel/'
+
+  depends_on macos: '>= :mavericks'
 
   app 'Earlybird.app'
 end
