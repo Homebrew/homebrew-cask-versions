07ec84eb119a5817223b462201bcc1a17c1a03b0
Jussi <wickedsp1d3r@users.noreply.github.com>
2018-02-04 16:37:45 +0200

    Update palemoon to 27.7.2 (#5199)

--- Casks/palemoon.rb
+++ Casks/palemoon.rb
@@ -1,6 +1,6 @@
 cask 'palemoon' do
-  version '27.7.0'
-  sha256 'ec7049d57771fde31214ea9e87606184a44993aa3c1a1b912a4e144abcbcfbad'
+  version '27.7.2'
+  sha256 '7af4150b92112ee938cd94dc42e8708b5bf92e1dc85868242761758d9684e762'
 
   url "https://mac.palemoon.org/dist/palemoon-#{version}.mac64.dmg"
   name 'Palemoon'
