5a924e41965a3d06a9968f6b1d4422c9c0dadcb1
Oleg <epushiron@gmail.com>
2018-02-07 17:46:25 +0500

    Add ableton-live-lite v9.7.5 (#5211)

---
+++ Casks/ableton-live-lite9.rb
@@ -0,0 +1,10 @@
+cask 'ableton-live-lite9' do
+  version '9.7.5'
+  sha256 'c91876c86e06717c86b4e1df7168c9916c789a7e0eb2ea2d43fec3abe892c183'
+
+  url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_lite_#{version}_64.dmg"
+  name 'Ableton Live Lite'
+  homepage 'https://www.ableton.com/en/products/live-lite/'
+
+  app "Ableton Live #{version.major} Lite.app"
+end
