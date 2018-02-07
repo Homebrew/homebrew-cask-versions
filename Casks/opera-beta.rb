cc017da72017aeb215f5d709ba3b54d289c07346
commitay <commitay@users.noreply.github.com>
2018-02-07 22:46:41 +1000

    Update opera-beta to 51.0.2830.23 (#5212)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '51.0.2830.16'
-  sha256 '4a12fafa8ee10e5d7d46e34206c7db8734a0eb5bc1474c6378c40127410b7a74'
+  version '51.0.2830.23'
+  sha256 '7c120ad2f47abb030a82aaef59a9384720b0103b78a2f831e3e92ef24a3b38be'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
