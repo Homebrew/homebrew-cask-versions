b436e33e95d2563691d192a490800ec586e2fcd5
Christian Battaglia <cdbattags@users.noreply.github.com>
2018-02-13 13:34:30 -0500

    Update prince-latest to 20171214 (#5242)

--- Casks/prince-latest.rb
+++ Casks/prince-latest.rb
@@ -1,10 +1,10 @@
 cask 'prince-latest' do
-  version '20171026'
-  sha256 '8c0467f1565aa3062c40acfb41ea682b1be064eb9122d0387ba89f5d32891963'
+  version '20171214'
+  sha256 '4e7d714698d7fe99c1bb4fb5161fa779d0bc78a61f3834b2ecf2a1476673c77a'
 
   url "https://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
   appcast 'https://www.princexml.com/latest/',
-          checkpoint: 'babd91d1315897fafd95b2713636a1e93e732ee7196cc83123d89c4855f7cba8'
+          checkpoint: 'd7c85f6a9e40d2f62e1891095a144927be9c078fb761646e531c6f9e04d21fb9'
   name 'Prince'
   homepage 'https://www.princexml.com/'
 
