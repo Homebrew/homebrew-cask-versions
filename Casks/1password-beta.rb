ff53304355848e861c3ed80f442eba73fa17863f
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-15 21:04:00 +0000

    Update 1password-beta to 6.8.7.BETA-1 (#5254)

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,10 +1,10 @@
 cask '1password-beta' do
-  version '6.8.6'
-  sha256 '8a2e922d3fbf0cafcf530301b1d5db315fc37fb5ac5c732feecbc0b6190a47d9'
+  version '6.8.7.BETA-1'
+  sha256 'cb3725312232badd72e4cf2ff78629b04de2a5e062500143c125a74196edef5b'
 
   url "https://cache.agilebits.com/dist/1P/mac4/1Password-#{version}.zip"
   appcast 'https://app-updates.agilebits.com/product_history/OPM4',
-          checkpoint: '3d8108f7ec801f308b2bf7de4fea46fb7f84535d217d25c1c616360197907dbe'
+          checkpoint: 'd954361ee0797b0fa7357c476b536131b8d9aad075749730c6fa03f233fe971c'
   name '1Password'
   homepage 'https://agilebits.com/downloads'
 
