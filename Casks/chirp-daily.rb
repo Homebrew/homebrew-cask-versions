f05cdd65002b09206ce9d6f6b1291476325c0ef9
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-02-17 22:07:14 +0000

    Update chirp-daily to 20180210

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,6 +1,6 @@
 cask 'chirp-daily' do
-  version '20180123'
-  sha256 'ea797cca99fb5703a684dfce0c492995ccf7fca057a9b69e0ac233f500cefc50'
+  version '20180210'
+  sha256 '7aecad976b074ae39d6485e64e3fabe0081283fe9e7a41796cb440b0bb44033a'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
