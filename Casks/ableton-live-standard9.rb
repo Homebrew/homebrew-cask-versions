bb3330ce629be2a36209d1c748a78dbd88e57396
Murray Royston-Ward <15369632+mroystonward@users.noreply.github.com>
2018-02-22 05:51:00 +0000

    Add Ableton Live Standard v9.7.5 (#5280)
    Legacy install formula for Ableton Live 9 Standard, specifically
    version 9.7.5 (latest stable of version 9)

---
+++ Casks/ableton-live-standard9.rb
@@ -0,0 +1,13 @@
+cask 'ableton-live-standard9' do
+  version '9.7.5'
+  sha256 'db7953c5f3fa52c331c7186e97b70d0d22d269a02ed054014025b2ddde72d130'
+
+  url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_standard_#{version}_64.dmg"
+  name 'Ableton Live 9 Standard'
+  homepage 'https://www.ableton.com/en/live/'
+
+  app "Ableton Live #{version.major} Standard.app"
+
+  zap trash: '~/Library/*/*[Aa]bleton*',
+      rmdir: '~/Music/Ableton/Factory Packs'
+end
