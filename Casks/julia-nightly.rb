1406cf69564f2b3c79b000e50da3a15b6ca1c7d0
commitay <commitay@users.noreply.github.com>
2018-02-25 14:38:32 +1000

    Update julia-nightly.rb

--- Casks/julia-nightly.rb
+++ Casks/julia-nightly.rb
@@ -1,6 +1,6 @@
 cask 'julia-nightly' do
-  version '0.7.0-e4b6677561'
-  sha256 '8065e24f0a5292d0a26b9ea4ca50de623960450b69bdc1888b041ae73c8956a2'
+  version '0.7'
+  sha256 :no_check # required as upstream package is updated in-place
 
   url 'https://julialangnightlies-s3.julialang.org/bin/mac/x64/julia-latest-mac64.dmg'
   name 'Julia'
@@ -8,8 +8,8 @@ cask 'julia-nightly' do
 
   depends_on macos: '>= :mountain_lion'
 
-  app "Julia-#{version.sub(%r{(.+)-(.+)}, '\1-DEV-\2')}.app"
-  binary "#{appdir}/Julia-#{version.sub(%r{(.+)-(.+)}, '\1-DEV-\2')}.app/Contents/Resources/julia/bin/julia"
+  app "Julia-#{version}.app"
+  binary "#{appdir}/Julia-#{version}.app/Contents/Resources/julia/bin/julia"
 
   zap trash: '~/.julia'
 end
