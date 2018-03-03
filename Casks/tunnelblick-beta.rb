66265b01b989719f01ada50b1d1990dbcd2398be
Victor Popkov <victor@popkov.me>
2018-03-03 22:00:04 +0200

    Update tunnelblick-beta to 3.7.6beta01,5020

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,11 +1,11 @@
 cask 'tunnelblick-beta' do
-  version '3.7.5beta08,5003'
-  sha256 'd8ae57598af1ea029a46ef96ef5569428ebe70a863c2b3310cf474bb021416d2'
+  version '3.7.6beta01,5020'
+  sha256 'aa14720c194623fad1d0c9d46eebd46b85264d6c536d3cb9aeef6ba271744210'
 
   # github.com/Tunnelblick/Tunnelblick/releases/download was verified as official when first introduced to the cask
   url "https://github.com/Tunnelblick/Tunnelblick/releases/download/v#{version.before_comma}/Tunnelblick_#{version.before_comma}_build_#{version.after_comma}.dmg"
   appcast 'https://github.com/Tunnelblick/Tunnelblick/releases.atom',
-          checkpoint: '7a61b553040d1d65b8d9462dbb12a9d67a4840da6496678e4f46e56f6e392165'
+          checkpoint: '2ccf3cbdbaa130cd6c324956dcbf8d55c4601149281ed847685ee8ff3fedcba2'
   name 'Tunnelblick'
   homepage 'https://www.tunnelblick.net/'
   gpg "#{url}.asc", key_id: '76df975a1c5642774fb09868ff5fd80e6bb9367e'
