33294bc261add98e8a60b7ac0428daf4c8a2a276
Martin Schmelzer <martin@Martins-MacBook-Pro-2.local>
2018-03-04 15:09:30 +0100

    Update rstudio-preview to 1.1.430

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.1.419'
-  sha256 '3af70da34340a9d10eda4f0b765610cbd5d6bfa94236b23692d056361cf8c7b2'
+  version '1.1.430'
+  sha256 '67d5cdb6c960f9322979d2219d48267be29b1c99a78714c1228fb4e904a7ed23'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
