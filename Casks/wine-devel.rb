69b44080f292e180038204aa21d857ea30268fc6
RexTg <rexx0520@gmail.com>
2018-03-20 20:43:27 +0800

    wine-devel is now version 3.4

--- Casks/wine-devel.rb
+++ Casks/wine-devel.rb
@@ -1,5 +1,5 @@
 cask 'wine-devel' do
-  version '3.3'
+  version '3.4'
   sha256 'fd6d4f36e821ea70cd625a9e07e626d170aa352bb064067860bd94836b7fad99'
 
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-devel-#{version}.pkg"
