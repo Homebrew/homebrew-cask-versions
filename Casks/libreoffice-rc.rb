c1bd431732cf8895a54782becceb6ead8a09654c
miccal <miccal.matthews@gmail.com>
2018-04-01 19:35:31 +0800

    Update libreoffice-rc to 6.0.3.2

--- Casks/libreoffice-rc.rb
+++ Casks/libreoffice-rc.rb
@@ -1,11 +1,11 @@
 cask 'libreoffice-rc' do
-  version '6.0.3.1'
-  sha256 'd9420b8d4b6c22755057eb1dabdf5bc549638ebb0c77dc17433524a6d6a2fa98'
+  version '6.0.3.2'
+  sha256 'acb71b1d1b8c8857a0150d5cf9db06515abe82458fd4684be3b88d98d722fd98'
 
   # documentfoundation.org/libreoffice was verified as official when first introduced to the cask
   url "https://download.documentfoundation.org/libreoffice/testing/#{version.major_minor_patch}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   appcast 'https://download.documentfoundation.org/libreoffice/testing/',
-          checkpoint: '853c7148d98793a82c382b055261a55dece73cb32c7392b8c0004ab35fa2204c'
+          checkpoint: '3332e95a1e3a6373c1e19e46f9f877a044ca52756058c9e5523ae96e5ebadb69'
   name 'LibreOffice Release Candidate'
   homepage 'https://www.libreoffice.org/download/pre-releases/'
   gpg "#{url}.asc", key_id: 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
