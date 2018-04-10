70e1eef03eead1661d7970b984314cdc00b7d023
Victor Popkov <victor@popkov.me>
2018-04-10 22:08:17 +0300

    Update sketch-beta to 50,54675 (#5498)

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,11 +1,11 @@
 cask 'sketch-beta' do
-  version '49.1,51145'
-  sha256 'c6f428ded9927a7bae635b81f1f06d83d0ef03688b597c293b4ebf28012d1d4b'
+  version '50,54675'
+  sha256 '1cb1683dfd5711a518fe713fc4f277c944a7fe494a968c48420e1a8b7f60c48b'
 
   # hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b',
-          checkpoint: 'c661f2bcbd1b9589f9aec337598c1fa70ac731c62d81d77283e8165c3c12639f'
+          checkpoint: 'a713ec9278648b4baa18a13bbcd3c9a8c634cb1f6521954692cd08f0b98a7343'
   name 'Sketch'
   homepage 'https://www.sketchapp.com/beta/'
 
