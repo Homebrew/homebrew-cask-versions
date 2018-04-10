780422617c353f50bda395613ed97a0f79ded712
David Yantis <404NetworkError@users.noreply.github.com>
2018-04-10 04:12:10 -0500

    Update insync-beta to 1.4.5.37069 (#5494)

--- Casks/insync-beta.rb
+++ Casks/insync-beta.rb
@@ -1,6 +1,6 @@
 cask 'insync-beta' do
-  version '1.4.1.37037'
-  sha256 '0fca8d6b3240406608141e480f4238d41f6ddab75cbf8d53e1591d128068052f'
+  version '1.4.5.37069'
+  sha256 '3d0f63d709b65ff44fc32aae34a2456809ac2e53ed9f4e1effbe35ab5b5dce5f'
 
   # d2t3ff60b2tol4.cloudfront.net/builds was verified as official when first introduced to the cask
   url "https://d2t3ff60b2tol4.cloudfront.net/builds/Insync-#{version}.dmg"
