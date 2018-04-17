c653a4f07a726dcb7d7bd7a6f1d2605142490f0b
commitay <commitay@users.noreply.github.com>
2018-04-18 04:12:17 +1000

    Update tower-beta.rb

--- Casks/tower-beta.rb
+++ Casks/tower-beta.rb
@@ -3,7 +3,7 @@ cask 'tower-beta' do
   sha256 'b9e23dd307d6351b93767ec1e264388671ac32acf08563a3b7c16bcb9011598a'
 
   # amazonaws.com/apps/tower3-mac was verified as official when first introduced to the cask
-  url "https://fournova-app-updates.s3.amazonaws.com/apps/tower#{version.major}-mac/#{version.after_comma}/Tower-#{version.before_comma}.zip"
+  url "https://fournova-app-updates.s3.amazonaws.com/apps/tower3-mac/#{version.after_comma}/Tower-#{version.before_comma}.zip"
   appcast "https://updates.fournova.com/updates/tower#{version.major}-mac/beta",
           checkpoint: '3c7e1f95c3a3573514979035aa8a37af6a8368b89307c8841609ec884b5fb493'
   name 'Tower'
