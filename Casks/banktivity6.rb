a31f42488226aae28400fdcdf00a01e726214c18
Gobinath Mallaiyan <gobinathm@users.noreply.github.com>
2018-04-20 10:14:10 -0400

    Update banktivity6.rb

--- Casks/banktivity6.rb
+++ Casks/banktivity6.rb
@@ -1,11 +1,11 @@
-cask 'banktivity5' do
+cask 'banktivity6' do
   version '6.3'
   sha256 '1c5719fd1fbbf5a1f444dac75762d58aeb1dfe93daec02f9afc8bf06168c8207'
 
   # iggsoft.com was verified as official when first introduced to the cask
   url 'https://www.iggsoft.com/banktivity/Banktivity6_Web.dmg'
-  name 'Banktivity 5'
-  homepage 'https://www.iggsoftware.com/support/articles/ibank-5/where-can-i-download-ibank-for-mac/'
+  name 'Banktivity 6'
+  homepage 'https://www.iggsoftware.com/banktivity/'
 
-  app 'Banktivity 5.app'
+  app 'Banktivity 6.app'
 end
