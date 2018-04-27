9f1ddbd2eb20477512675ad0a8250dd46e74e9fb
404NetworkError <404NetworkError@users.noreply.github.com>
2018-04-27 00:29:47 -0500

    Update tunnelblick-beta to 3.7.6beta04,5050

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,11 +1,11 @@
 cask 'tunnelblick-beta' do
-  version '3.7.6beta03,5031'
-  sha256 'ecfbe42cdc349b0cd619601abcfd7e09f3e3ca991d5e0249a01c1f3e4c6f60fc'
+  version '3.7.6beta04,5050'
+  sha256 'c2ae7f2ce658d9c099cf3c765e226f2e9097e854eb5e40ee3a8f1871742ab4e4'
 
   # github.com/Tunnelblick/Tunnelblick/releases/download was verified as official when first introduced to the cask
   url "https://github.com/Tunnelblick/Tunnelblick/releases/download/v#{version.before_comma}/Tunnelblick_#{version.before_comma}_build_#{version.after_comma}.dmg"
   appcast 'https://github.com/Tunnelblick/Tunnelblick/releases.atom',
-          checkpoint: '2730fc7136477c65bc843987409c7e61646c931034d0bf346f23570aafad05dd'
+          checkpoint: '621b15a521b4a4d9a6a7692314c81f20498725b17c1a238ce95896b260b75f1c'
   name 'Tunnelblick'
   homepage 'https://www.tunnelblick.net/'
   gpg "#{url}.asc", key_id: '76df975a1c5642774fb09868ff5fd80e6bb9367e'
