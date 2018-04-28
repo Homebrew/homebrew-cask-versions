a65836896315e88cb1a5db75bf35f7eaaac059da
Sergio (kallookoo) <sergio@dsergio.com>
2018-04-28 21:36:38 +0200

    Added all the langs present in the download page.
    Added all the languages present in the download page.
    Some languages have an alias:
    - Chinese (Simplified) zh-CN or zh
    - English (US) en-US or en  "Note: is the default lang for install"
    - Portuguese (Portugal) pt-PT or pt
    - Spanish (Spain) es-ES or es

--- Casks/firefox-developer-edition.rb
+++ Casks/firefox-developer-edition.rb
@@ -2,40 +2,514 @@ cask 'firefox-developer-edition' do
   version :latest
   sha256 :no_check
 
-  language 'cs' do
-    'cs'
+  # Acholi
+  language 'ach' do
+    'ach'
   end
 
-  language 'de' do
-    'de'
+  # Afrikaans
+  language 'af' do
+    'af'
   end
 
-  language 'en', default: true do
-    'en-US'
+  # Albanian
+  language 'sq' do
+    'sq'
   end
 
-  language 'fr' do
-    'fr'
+  # Arabic
+  language 'ar' do
+    'ar'
   end
 
-  language 'ja' do
-    'ja-JP-mac'
+  # Aragonese
+  language 'an' do
+    'an'
   end
 
-  language 'ru' do
-    'ru'
+  # Armenian
+  language 'hy-AM' do
+    'hy-AM'
   end
 
-  language 'uk' do
-    'uk'
+  # Assamese
+  language 'as' do
+    'as'
   end
 
+  # Asturian
+  language 'ast' do
+    'ast'
+  end
+
+  # Azerbaijani
+  language 'az' do
+    'az'
+  end
+
+  # Basque
+  language 'eu' do
+    'eu'
+  end
+
+  # Belarusian
+  language 'be' do
+    'be'
+  end
+
+  # Bengali (Bangladesh)
+  language 'bn-BD' do
+    'bn-BD'
+  end
+
+  # Bengali (India)
+  language 'bn-IN' do
+    'bn-IN'
+  end
+
+  # Bosnia
+  language 'bs' do
+    'bs'
+  end
+
+  # Breton
+  language 'br' do
+    'br'
+  end
+
+  # Bulgarian
+  language 'bg' do
+    'bg'
+  end
+
+  # Burmese
+  language 'my' do
+    'my'
+  end
+
+  # Catalan
+  language 'ca' do
+    'ca'
+  end
+
+  # Chinese (Simplified)
+  language 'zh-CN' do
+    'zh-CN'
+  end
+
+  # Alias of Chinese (Simplified)
+  language 'zh' do
+    'zh-CN'
+  end
+
+  # Chinese (Traditional)
   language 'zh-TW' do
     'zh-TW'
   end
 
-  language 'zh' do
-    'zh-CN'
+  # Croatian
+  language 'hr' do
+    'hr'
+  end
+
+  # Czech
+  language 'cs' do
+    'cs'
+  end
+
+  # Danish
+  language 'da' do
+    'da'
+  end
+
+  # Dutch
+  language 'nl' do
+    'nl'
+  end
+
+  # English (British)
+  language 'en-GB' do
+    'en-GB'
+  end
+
+  # English (South African)
+  language 'en-ZA' do
+    'en-ZA'
+  end
+
+  # English (US)
+  language 'en-US' do
+    'en-US'
+  end
+
+  # Alias of English (US)
+  language 'en', default: true do
+    'en-US'
+  end
+
+  # Esperanto
+  language 'eo' do
+    'eo'
+  end
+
+  # Estonian
+  language 'et' do
+    'et'
+  end
+
+  # Finnish
+  language 'fi' do
+    'fi'
+  end
+
+  # French
+  language 'fr' do
+    'fr'
+  end
+
+  # Frisian
+  language 'fy-NL' do
+    'fy-NL'
+  end
+
+  # Fulah
+  language 'ff' do
+    'ff'
+  end
+
+  # Gaelic (Scotland)
+  language 'gd' do
+    'gd'
+  end
+
+  # Galician
+  language 'gl' do
+    'gl'
+  end
+
+  # Georgian
+  language 'ka' do
+    'ka'
+  end
+
+  # German
+  language 'de' do
+    'de'
+  end
+
+  # Greek
+  language 'el' do
+    'el'
+  end
+
+  # Guarani
+  language 'gn' do
+    'gn'
+  end
+
+  # Gujarati (India)
+  language 'gu-IN' do
+    'gu-IN'
+  end
+
+  # Hebrew
+  language 'he' do
+    'he'
+  end
+
+  # Hindi (India)
+  language 'hi-IN' do
+    'hi-IN'
+  end
+
+  # Hungarian
+  language 'hu' do
+    'hu'
+  end
+
+  # Icelandic
+  language 'is' do
+    'is'
+  end
+
+  # Indonesian
+  language 'id' do
+    'id'
+  end
+
+  # Interlingua
+  language 'ia' do
+    'ia'
+  end
+
+  # Irish
+  language 'ga-IE' do
+    'ga-IE'
+  end
+
+  # Italian
+  language 'it' do
+    'it'
+  end
+
+  # Japanese
+  language 'ja' do
+    'ja-JP-mac'
+  end
+
+  # Kabyle
+  language 'kab' do
+    'kab'
+  end
+
+  # Kannada
+  language 'kn' do
+    'kn'
+  end
+
+  # Kaqchikel
+  language 'cak' do
+    'cak'
+  end
+
+  # Kazakh
+  language 'kk' do
+    'kk'
+  end
+
+  # Khmer
+  language 'km' do
+    'km'
+  end
+
+  # Korean
+  language 'ko' do
+    'ko'
+  end
+
+  # Latvian
+  language 'lv' do
+    'lv'
+  end
+
+  # Ligurian
+  language 'lij' do
+    'lij'
+  end
+
+  # Lithuanian
+  language 'lt' do
+    'lt'
+  end
+
+  # Lower Sorbian
+  language 'dsb' do
+    'dsb'
+  end
+
+  # Macedonian
+  language 'mk' do
+    'mk'
+  end
+
+  # Maithili
+  language 'mai' do
+    'mai'
+  end
+
+  # Malay
+  language 'ms' do
+    'ms'
+  end
+
+  # Malayalam
+  language 'ml' do
+    'ml'
+  end
+
+  # Marathi
+  language 'mr' do
+    'mr'
+  end
+
+  # Nepali
+  language 'ne-NP' do
+    'ne-NP'
+  end
+
+  # Norwegian (Bokmål)
+  language 'nb-NO' do
+    'nb-NO'
+  end
+
+  # Norwegian (Nynorsk)
+  language 'nn-NO' do
+    'nn-NO'
+  end
+
+  # Occitan (Lengadocian)
+  language 'oc' do
+    'oc'
+  end
+
+  # Odia
+  language 'or' do
+    'or'
+  end
+
+  # Persian
+  language 'fa' do
+    'fa'
+  end
+
+  # Polish
+  language 'pl' do
+    'pl'
+  end
+
+  # Portuguese (Brazilian)
+  language 'pt-BR' do
+    'pt-BR'
+  end
+
+  # Portuguese (Portugal)
+  language 'pt-PT' do
+    'pt-PT'
+  end
+
+  # Alias of Portuguese (Portugal)
+  language 'pt' do
+    'pt-PT'
+  end
+
+  # Punjabi (India)
+  language 'pa-IN' do
+    'pa-IN'
+  end
+
+  # Romanian
+  language 'ro' do
+    'ro'
+  end
+
+  # Romansh
+  language 'rm' do
+    'rm'
+  end
+
+  # Russian
+  language 'ru' do
+    'ru'
+  end
+
+  # Serbian
+  language 'sr' do
+    'sr'
+  end
+
+  # Sinhala
+  language 'si' do
+    'si'
+  end
+
+  # Slovak
+  language 'sk' do
+    'sk'
+  end
+
+  # Slovenian
+  language 'sl' do
+    'sl'
+  end
+
+  # Songhai
+  language 'son' do
+    'son'
+  end
+
+  # Spanish (Argentina)
+  language 'es-AR' do
+    'es-AR'
+  end
+
+  # Spanish (Chile)
+  language 'es-CL' do
+    'es-CL'
+  end
+
+  # Spanish (Mexico)
+  language 'es-MX' do
+    'es-MX'
+  end
+
+  # Spanish (Spain)
+  language 'es-ES' do
+    'es-ES'
+  end
+
+  # Alias of Spanish (Spain)
+  language 'es' do
+    'es-ES'
+  end
+
+  # Swedish
+  language 'sv-SE' do
+    'sv-SE'
+  end
+
+  # Tamil
+  language 'ta' do
+    'ta'
+  end
+
+  # Telugu
+  language 'te' do
+    'te'
+  end
+
+  # Thai
+  language 'th' do
+    'th'
+  end
+
+  # Turkish
+  language 'tr' do
+    'tr'
+  end
+
+  # Ukrainian
+  language 'uk' do
+    'uk'
+  end
+
+  # Upper Sorbian
+  language 'hsb' do
+    'hsb'
+  end
+
+  # Urdu
+  language 'ur' do
+    'ur'
+  end
+
+  # Uzbek
+  language 'uz' do
+    'uz'
+  end
+
+  # Vietnamese
+  language 'vi' do
+    'vi'
+  end
+
+  # Welsh
+  language 'cy' do
+    'cy'
+  end
+
+  # Xhosa
+  language 'xh' do
+    'xh'
   end
 
   url "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=osx&lang=#{language}"
