4dc98fa9d1defe2464f1c985684cda2122e34730
acevif <acevif@users.noreply.github.com>
2018-05-04 06:55:17 +0900

    Update camtwist-beta to 3.4.1 (#5619)
    * Update camtwist-beta to 3.4.1
    
    * Update camtwist-beta.rb

--- Casks/camtwist-beta.rb
+++ Casks/camtwist-beta.rb
@@ -1,6 +1,6 @@
 cask 'camtwist-beta' do
-  version '3.1'
-  sha256 'ecd19d1f3c126d5397ed4f0a5de53920a2b27baab47acc06754632ebc6e7c545'
+  version '3.4.1'
+  sha256 'c287e03f4bba0484108852a7a1d664498132a801455f97be3c6f37d2e0765428'
 
   url "http://camtwiststudio.com/beta/CamTwist_#{version}.dmg"
   name 'CamTwist'
@@ -10,6 +10,7 @@ cask 'camtwist-beta' do
 
   pkg 'CamTwist.pkg'
 
-  uninstall script:  { executable: 'Manual Install/CamTwist/uninstall.command' },
-            pkgutil: 'com.allocinit.camtwist.*'
+  uninstall script:    { executable: 'Manual Install/CamTwist/uninstall.command' },
+            pkgutil:   'com.allocinit.camtwist.*',
+            launchctl: 'com.allocinit.camtwist.dal'
 end
