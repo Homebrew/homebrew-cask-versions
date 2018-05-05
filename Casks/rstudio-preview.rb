775ea85e09ba4562e3598a6e2dc8ebc164005b09
Martin Schmelzer <martin@Martins-MacBook-Pro.local>
2018-05-05 15:16:10 +0200

    Update rstudio-preview to 1.1.446

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.1.444'
-  sha256 '37b1181da721f34bb3a8fdec66cd94b42592dca5ca4eb7d90ece971e1e2b2382'
+  version '1.1.446'
+  sha256 '44cb54fda79ad3ee78b0aa290ef64464bc2d5daaf5fca777fe4a9df1d4ecc4f7'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
