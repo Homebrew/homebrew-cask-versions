83ed20c8c6be0fa4643542702140fe22000326c4
Markus Reiter <me@reitermark.us>
2018-05-09 22:32:19 +0200

    Fix Casks using `gpg`. (#5633)

--- Casks/libreoffice-still.rb
+++ Casks/libreoffice-still.rb
@@ -5,7 +5,7 @@ cask 'libreoffice-still' do
   # documentfoundation.org was verified as official when first introduced to the cask
   url "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   appcast 'https://download.documentfoundation.org/libreoffice/stable/',
-          checkpoint: 'c26bcf3b01ab0598b785b4bf917c3c6188a0ab7daf66fa2aae31e7c5499bbc86'
+          checkpoint: '0810423ee662c57a69ca39a40ee9c1c1ce8f2d1584451ac14e6428edae048a80'
   name 'LibreOffice Still'
   homepage 'https://www.libreoffice.org/download/libreoffice-still/'
   gpg "#{url}.asc", key_id: 'c2839ecad9408fbe9531c3e9f434a1efafeeaea3'
