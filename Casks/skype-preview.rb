50bb93843c46c36d4f3a92a2620d5974670ad6dc
commitay <commitay@users.noreply.github.com>
2018-05-11 07:58:30 +1000

    Update skype-preview to 8.21.76.6 (#5657)

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask 'skype-preview' do
-  version '8.20.76.8'
-  sha256 '3cb334b1c5cefde606b372b556cc0da478cfd7e97e46fcd7cf4f04bc81dad75a'
+  version '8.21.76.6'
+  sha256 'fd0dc9b464c662433db20d8a08c550a58786bba50a89f3792af60cd01ba809ed'
 
   # endpoint920510.azureedge.net/s4l/s4l/download/mac was verified as official when first introduced to the cask
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg"
