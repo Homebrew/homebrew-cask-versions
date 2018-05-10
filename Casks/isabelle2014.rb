2a93b67bdeb240f254568283681fc2cfb7f74063
Shiv <shvenkat@users.noreply.github.com>
2018-05-10 15:33:50 -0700

    Re-add Isabelle 2014, the last version supported by Proof General.

---
+++ Casks/isabelle2014.rb
@@ -0,0 +1,10 @@
+cask 'isabelle2014' do
+  version '2014'
+  sha256 'e4a4b5f454d6cb24d5ef7271adc54e9fa7109addda8c6707bd7414b4d4061a23'
+
+  url 'https://isabelle.in.tum.de/website-Isabelle2014/dist/Isabelle2014.dmg'
+  name 'Isabelle2014'
+  homepage 'https://isabelle.in.tum.de/website-Isabelle2014/index.html'
+
+  app 'Isabelle2014.app'
+end
