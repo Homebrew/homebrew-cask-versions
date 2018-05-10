1520f90e35afdfdbf0462148565dc1569b740955
L. E. Segovia <amyspark@users.noreply.github.com>
2018-05-09 22:40:06 -0300

    Add Scribus development version 1.5.4 (#5647)
    * Add Scribus development version 1.5.4
    
    * Add zap stanza and conflicts_with for scribus-dev

---
+++ Casks/scribus-dev.rb
@@ -0,0 +1,21 @@
+cask 'scribus-dev' do
+  version '1.5.4'
+  sha256 '6f31b0b9bf27c952d820188343e2be73ab990be772e34551b030d9c04fa4f5b8'
+
+  # sourceforge.net/scribus was verified as official when first introduced to the cask
+  url "https://downloads.sourceforge.net/scribus/scribus-devel/#{version}/scribus-#{version}.dmg"
+  appcast 'https://sourceforge.net/projects/scribus/rss?path=/scribus-devel',
+          checkpoint: 'f6f2167e26e07332197f8c876c08794f578705414ef4f4128b12f914584e22e9'
+  name 'Scribus'
+  homepage 'https://www.scribus.net/'
+
+  conflicts_with cask: 'scribus'
+
+  app 'Scribus.app'
+
+  zap trash: [
+               '~/Library/Application Support/Scribus',
+               '~/Library/Preferences/Scribus',
+               '~/Library/Saved Application State/net.scribus.savedState',
+             ]
+end
