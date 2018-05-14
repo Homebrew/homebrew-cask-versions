8c901385b752384709f42757cbb7c463fb41c990
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2018-05-14 21:45:02 +0100

    Update smartgit-rc to 18.1-rc-3 (#5679)

--- Casks/smartgit-rc.rb
+++ Casks/smartgit-rc.rb
@@ -1,10 +1,10 @@
 cask 'smartgit-rc' do
-  version '18.1-rc-1'
-  sha256 '79d883cd9d4c3f997eeda88823e64042b0890f35b336febb4ffd4d30c2aa1240'
+  version '18.1-rc-3'
+  sha256 'f992df0b378760ed516a7dbb481a1f6a9a014fe6933b0a232dbcef7df5ac9149'
 
   url "https://www.syntevo.com/downloads/smartgit/smartgit-macosx-#{version.dots_to_underscores}.dmg"
   appcast 'https://www.syntevo.com/smartgit/changelog-eap.txt',
-          checkpoint: '3c93dd80e173233d4c46c5aeb1b2a430dbdb7a82f07bfb77a3052fbe64d61928'
+          checkpoint: 'f362b7a8f7f10ddcb210314c10d43698ed88ae98b422f05d073320d6c584e6af'
   name 'SmartGit'
   homepage 'https://www.syntevo.com/smartgit/'
 
