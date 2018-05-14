c8d0f5eefe26e592d8aa3d84020680bf3bdac28f
commitay <commitay@users.noreply.github.com>
2018-05-14 19:40:33 +1000

    Update sublime-text-dev (#5673)

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,24 +1,24 @@
 cask 'sublime-text-dev' do
-  version '3175'
+  version '3.175'
   sha256 '93d7c9162edf315b46e02049e45add15f8ca309820f649ba6ddd4ae0597cd6b3'
 
-  url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version}.dmg"
-  appcast 'https://www.sublimetext.com/updates/3/dev/appcast_osx.xml',
+  url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version.no_dots}.dmg"
+  appcast "https://www.sublimetext.com/updates/#{version.major}/dev/appcast_osx.xml",
           checkpoint: 'cd95fa60e07e5f31f91b06d276f6f4d8147d8acc4b2a9dbb27ad45037570eccb'
   name 'Sublime Text'
-  homepage 'https://www.sublimetext.com/3dev'
+  homepage "https://www.sublimetext.com/#{version.major}dev"
 
   conflicts_with cask: 'sublime-text'
 
   app 'Sublime Text.app'
   binary "#{appdir}/Sublime Text.app/Contents/SharedSupport/bin/subl"
 
-  uninstall quit: 'com.sublimetext.3'
+  uninstall quit: "com.sublimetext.#{version.major}"
 
   zap trash: [
-               '~/Library/Application Support/Sublime Text 3',
-               '~/Library/Caches/com.sublimetext.3',
-               '~/Library/Preferences/com.sublimetext.3.plist',
-               '~/Library/Saved Application State/com.sublimetext.3.savedState',
+               "~/Library/Application Support/Sublime Text #{version.major}",
+               "~/Library/Caches/com.sublimetext.#{version.major}",
+               "~/Library/Preferences/com.sublimetext.#{version.major}.plist",
+               "~/Library/Saved Application State/com.sublimetext.#{version.major}.savedState",
              ]
 end
