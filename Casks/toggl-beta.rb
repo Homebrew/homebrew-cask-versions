7429e98a1d6ade38b2812257c93d20727cf00dda
= <=>
2018-05-15 10:20:07 -0400

    Update toggl-beta to 7.4.159

--- Casks/toggl-beta.rb
+++ Casks/toggl-beta.rb
@@ -1,11 +1,11 @@
 cask 'toggl-beta' do
-  version '7.4.155'
-  sha256 'db8fe1b569aa294857d75d98e2bf78451230ba8d491ed403be9409427d4a1fda'
+  version '7.4.159'
+  sha256 'ce9ccd1b0be4ee05b1cd5553f38421305e8740836ee997b9ba98c7a53e804e79'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_beta_appcast.xml',
-          checkpoint: '598582cb65cec5966006cfe6df67d7b621c57f95fe15675dbb2779bbcc534dff'
+          checkpoint: 'bbcd4b6f700eee2aefb52de49b84ba74bf8a6754f10a7bbe27d50588d9e05085'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
 
