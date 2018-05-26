a82547acf00168bbd280497f9e3af6ced465f315
commitay <commitay@users.noreply.github.com>
2018-05-26 19:38:57 +1000

    Update mi2.rb

--- Casks/mi2.rb
+++ Casks/mi2.rb
@@ -3,7 +3,7 @@ cask 'mi' do
   sha256 'f594e33451fd70cf16c36af205f59294609d910836a91c877e9d2f74261b5d38'
 
   url "https://www.mimikaki.net/download/mi#{version}.dmg"
-  name 'Mi'
+  name 'mi'
   homepage 'https://www.mimikaki.net/'
 
   app 'mi.app'
