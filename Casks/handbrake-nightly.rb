cb81ee998a2550e4638e128389d30d03c31ac67b
Peter Dyhr <paadyhr@gmail.com>
2018-06-04 22:18:41 +0200

    Remove 'file =' assignment and 'file.to_s' line

--- Casks/handbrake-nightly.rb
+++ Casks/handbrake-nightly.rb
@@ -5,8 +5,7 @@ cask 'handbrake-nightly' do
   url do
     require 'open-uri'
     base_url = 'https://handbrake.fr/nightly.php'
-    file = URI(base_url).open.read.scan(%r{href="([^"]+.dmg)"}).flatten.first
-    file.to_s
+    URI(base_url).open.read.scan(%r{href="([^"]+.dmg)"}).flatten.first
   end
   name 'HandBrake Nightly'
   homepage 'https://handbrake.fr/nightly.php'
