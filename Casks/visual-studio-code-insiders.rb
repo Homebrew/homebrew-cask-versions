a4d54a57e1f15c6d02d4e2014c29582ab3eb546f
Stefano Canducci <steno@fastmail.fm>
2018-06-05 08:23:22 +0200

    Update visual-studio-code-insiders to 1.24.0,f67e6397855fb25becbdf33df185f96da7fdd108

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,11 +1,11 @@
 cask 'visual-studio-code-insiders' do
-  version '1.24.0,bfd155e6039f64d2795c6b514a029ebd01a33527'
-  sha256 '2f350accb1c2264ec14565f206c5ca25952bd4df15993e711ca7a142cf820670'
+  version '1.24.0,f67e6397855fb25becbdf33df185f96da7fdd108'
+  sha256 '6d31ca33def869d07a4908de93cdeac54aa205db3ec1053f8c198bb53eb01aac'
 
   # az764295.vo.msecnd.net/insider was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/insider/VERSION',
-          checkpoint: '97be2c7797c87006624e41c932e8073b37aa1062890b83a9604ba5367ae5c305'
+          checkpoint: 'baf79fd2130d53acfeef40edd562ef9601905d2e6bc3c99d35fa9f9ee230f24b'
   name 'Microsoft Visual Studio Code'
   name 'VS Code - Insiders'
   homepage 'https://code.visualstudio.com/insiders'
