670874366d6022bc8e8525ccf1ccee60d461b263
Martin Schmelzer <martin@Martins-MacBook-Pro.local>
2018-06-06 23:06:40 +0200

    Updated download url and version

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,9 +1,9 @@
 cask 'rstudio-preview' do
-  version '1.1.446'
-  sha256 '44cb54fda79ad3ee78b0aa290ef64464bc2d5daaf5fca777fe4a9df1d4ecc4f7'
+  version '1.2.679'
+  sha256 'b3f951ea7205c3af77096cb26089194b760e92df72f46948cce3d3fb2f371bca'
 
   # amazonaws.com/rstudio-dailybuilds was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/rstudio-dailybuilds/RStudio-#{version}.dmg"
+  url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'https://www.rstudio.com/products/rstudio/download/preview/'
 
