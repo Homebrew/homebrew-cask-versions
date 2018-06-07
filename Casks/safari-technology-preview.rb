10249b7a300282496b41c5b1d6a79128f7424fc4
commitay <commitay@users.noreply.github.com>
2018-06-08 08:20:47 +1000

    safari-technology-preview: set sha256 for each macos releae

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,11 +1,12 @@
 cask 'safari-technology-preview' do
   version '58'
-  sha256 :no_check
 
   if MacOS.version <= :high_sierra
     url 'https://secure-appldnld.apple.com/STP/091-87399-20180606-FBA72DC9-E8A3-446A-A4E2-B32DBB7175AB/SafariTechnologyPreview.dmg'
+    sha256 '0f85e2b97802854034e22c7f133987dcb47e811bf6bff563c31e6753a5336eb6'
   else
     url 'https://secure-appldnld.apple.com/STP/091-81948-20180606-A6156C55-3298-4E25-9905-10135203B756/SafariTechnologyPreview.dmg'
+    sha256 '5ca5557516b11dfd7b34dd442c9f97f5ac3aca7eb77dbc75d89ac839c9b3fb3f'
   end
 
   name 'Safari Technology Preview'
