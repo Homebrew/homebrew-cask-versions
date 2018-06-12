4be4afaab62dcd5caa2786a871cc11c18a3ea92d
David Yantis <404NetworkError@users.noreply.github.com>
2018-06-12 09:25:21 -0400

    Update hex-fiend-beta to 2.9.0,4 (#5814)
    * Update hex-fiend-beta to 2.9.0,4
    
    * Update depends_on

--- Casks/hex-fiend-beta.rb
+++ Casks/hex-fiend-beta.rb
@@ -1,15 +1,16 @@
 cask 'hex-fiend-beta' do
-  version '2.9.0,3'
-  sha256 'b5f37face5fd909c8a10d3abce52c25ebb7badf2c0f3860ac869feb71822d77b'
+  version '2.9.0,4'
+  sha256 'c3cd3aa704e123475a54bfa3f9a74c2e20594f40b69f51832518392bdd49848f'
 
   # github.com/ridiculousfish/HexFiend was verified as official when first introduced to the cask
   url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version.before_comma}-beta-#{version.after_comma}/Hex_Fiend_#{version.major_minor}.dmg"
   appcast 'https://github.com/ridiculousfish/HexFiend/releases.atom',
-          checkpoint: 'a08db7b7bd0ea6df35d193897ac5c5b11ed894703d9df5f67dfbd2d3828060de'
+          checkpoint: '3646b93dde0a520765c3e8290da11c1e16e78bdd93bc3287fb84f9e27b4bce0d'
   name 'Hex Fiend'
   homepage 'http://ridiculousfish.com/hexfiend/'
 
   conflicts_with cask: 'hex-fiend'
+  depends_on macos: '>= :lion'
 
   app 'Hex Fiend.app'
   binary "#{appdir}/Hex Fiend.app/Contents/Resources/hexf"
