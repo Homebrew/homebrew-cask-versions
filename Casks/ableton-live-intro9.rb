c71561d95a7d355a32c5c171cc7ef479224d5adb
commitay <commitay@users.noreply.github.com>
2018-06-15 21:33:39 +1000

    Update ableton-live casks: add appcasts

--- Casks/ableton-live-intro9.rb
+++ Casks/ableton-live-intro9.rb
@@ -3,6 +3,7 @@ cask 'ableton-live-intro9' do
   sha256 'dcb419ff6ddf0e7728f0e645310730b79892d79a8d1943dc95c871e046c7f028'
 
   url "http://cdn-downloads.ableton.com/channels/#{version}/ableton_live_intro_#{version}_64.dmg"
+  appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
   name 'Ableton Live 9 Intro'
   homepage 'https://www.ableton.com/en/live/'
 
