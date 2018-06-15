702d30db8540ac6e93b24e714bdb37f5929d1317
commitay <commitay@users.noreply.github.com>
2018-06-15 23:03:28 +1000

    Update waltr1 to 1.7.3,1480013435

--- Casks/waltr1.rb
+++ Casks/waltr1.rb
@@ -1,9 +1,10 @@
 cask 'waltr1' do
-  version :latest
-  sha256 :no_check
+  version '1.7.3,1480013435'
+  sha256 '729129129346228fc5d94af6993da2e32e98158f3978ac16b4d5f82b0e83db8b'
 
   # devmate.com/com.softorino.Waltr was verified as official when first introduced to the cask
-  url 'https://dl.devmate.com/com.softorino.Waltr/WALTR.zip'
+  url "https://dl.devmate.com/com.softorino.Waltr/#{version.before_comma}/#{version.after_comma}/WALTR-#{version.before_comma}.zip"
+  appcast 'https://updates.devmate.com/com.softorino.Waltr.xml'
   name 'WALTR'
   homepage 'https://softorino.com/waltr/'
 
