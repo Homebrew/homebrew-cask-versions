18b3d01bec98d14164921fc2e7c1302664f14cbc
Joe Wicentowski <joewiz@gmail.com>
2018-06-14 22:41:41 -0400

    Align cask with exist-db
    See https://github.com/Homebrew/homebrew-cask/blob/master/Casks/exist-db.rb

--- Casks/exist-db5.rb
+++ Casks/exist-db5.rb
@@ -5,7 +5,13 @@ cask 'exist-db5' do
   # dl.bintray.com/existdb was verified as official when first introduced to the cask
   url "https://dl.bintray.com/existdb/releases/eXist-db-#{version}.dmg"
   name 'eXist-db'
-  homepage 'http://exist-db.org/exist/apps/homepage/index.html'
+  homepage 'https://exist-db.org/exist/apps/homepage/index.html'
 
   app 'eXist-db.app'
+
+  zap trash: '~/Library/Application Support/org.exist'
+
+  caveats do
+    depends_on_java '8'
+  end
 end
