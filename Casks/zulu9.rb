d4955e9cfc43c7e79c611477ce4b3a41bc586eec
commitay <commitay@users.noreply.github.com>
2018-06-18 23:06:27 +1000

    Update zulu9 to 9.0.7.1 (#5839)

--- Casks/zulu9.rb
+++ Casks/zulu9.rb
@@ -1,6 +1,6 @@
 cask 'zulu9' do
-  version '9.0.4.1'
-  sha256 '77a3dc5f83a88c07dbab195aa54117c1619eb31c1bb59908bf6e0d6931b5cc79'
+  version '9.0.7.1'
+  sha256 'ddc362256ce2a06a37993cbe1ae2c166191444c23875d10c38bef910d5b5e6ac'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version}-jdk#{version.major_minor_patch}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
