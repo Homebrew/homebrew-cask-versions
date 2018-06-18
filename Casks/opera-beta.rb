887c73f965d5ec3955dda2a5b14cd26b0e087c2a
brigitte zmoelnig <brigitte@corecode.io>
2018-06-18 23:17:08 +0200

    Update opera-beta to 54.0.2952.23

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '54.0.2952.8'
-  sha256 'e56b960903880da13702de74de7d9bf87ff24f6fb376c3360332b2bca8d263ab'
+  version '54.0.2952.23'
+  sha256 '2dad300cae1d095ce75dad97da9ab8282503f17efddab21a62aae11cb2bdb3f4'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
