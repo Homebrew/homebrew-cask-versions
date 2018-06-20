77f2a9bc6ee871ffc4f2ea3ec45fe153b3b64a1b
Victor Popkov <victor@popkov.me>
2018-06-20 23:01:55 +0300

    Update airmail-beta to 3.5.5.493,349

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 cask 'airmail-beta' do
-  version '3.5.5.486,345'
-  sha256 '6c537da439fd0af50873183bb9e2b40cf34e915803471e605382fa2fcc93263b'
+  version '3.5.5.493,349'
+  sha256 '8c8377f25688f68ef0096a56ba37164935598dc17e7826cc1438f6a10cfce4ef'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
