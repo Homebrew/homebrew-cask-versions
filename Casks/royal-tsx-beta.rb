694149077106e8290935255d1e1395d6b2a5c4f7
Victor Popkov <victor@popkov.me>
2018-06-21 20:55:06 +0300

    Update royal-tsx-beta to 3.3.1.1

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,6 +1,6 @@
 cask 'royal-tsx-beta' do
-  version '3.3.0.1000'
-  sha256 '825e17a500940848b35cf48696bb7cb50ba56be4d408f46403bc5d0a9988d5e1'
+  version '3.3.1.1'
+  sha256 'ce79a71bcf68176d56269e12cbe863386eef8267b4d7012ed46f55fe34dffa84'
 
   url "https://royaltsx-v3.royalapplications.com/updates/royaltsx_#{version}.dmg"
   appcast 'https://royaltsx-v3.royalapplications.com/updates_beta.php'
