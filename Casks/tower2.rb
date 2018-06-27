0eecb70ae7db631c4a7b83e21f8f5d7707b75ad0
Eduard Rozenberg <2648417+edrozenberg@users.noreply.github.com>
2018-06-27 09:31:34 -0700

    Added Cask tower2 (#5877)
    Since Tower.app v3 has switched to subscriptions (booh!), adding this tower2 alternate version cask.
    
    This is simply a copy of an older revision of the tower.rb cask from the homebrew-cask project, with cask name changed from ‘tower’ to ‘tower2’.

---
+++ Casks/tower2.rb
@@ -0,0 +1,19 @@
+cask 'tower2' do
+  version '2.6.6-359,b84d867f'
+  sha256 '3283e5b750336e5c14273b51376481bebc44bd03c5c028df52a5d0091ddf946a'
+
+  # fournova-app-updates.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://fournova-app-updates.s3.amazonaws.com/apps/tower#{version.major}-mac/#{version.split('-').last.tr(',', '-')}/Tower-#{version.major}-#{version.before_comma}.zip"
+  appcast "https://updates.fournova.com/updates/tower#{version.major}-mac/stable"
+  name 'Tower'
+  homepage 'https://www.git-tower.com/'
+
+  app 'Tower.app'
+  binary "#{appdir}/Tower.app/Contents/MacOS/gittower"
+
+  zap trash: [
+               "~/Library/Application Support/com.fournova.Tower#{version.major}",
+               "~/Library/Caches/com.fournova.Tower#{version.major}",
+               "~/Library/Preferences/com.fournova.Tower#{version.major}.plist",
+             ]
+end
