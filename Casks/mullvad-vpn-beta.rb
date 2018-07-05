0a35b5e9658a87fb814f5149857646149eb54750
404NetworkError <404NetworkError@users.noreply.github.com>
2018-07-05 09:06:08 -0400

    Add mullvad-vpn-beta 2018.2-beta1

---
+++ Casks/mullvad-vpn-beta.rb
@@ -0,0 +1,25 @@
+cask 'mullvad-vpn-beta' do
+  version '2018.2-beta1'
+  sha256 '668bc01d566db74306c66012403c40dbfa3f68e3a4ca6858b3ad4b5afafc53af'
+
+  # github.com/mullvad/mullvadvpn-app was verified as official when first introduced to the cask
+  url "https://github.com/mullvad/mullvadvpn-app/releases/download/#{version}/MullvadVPN-#{version}.pkg"
+  appcast 'https://github.com/mullvad/mullvadvpn-app/releases.atom'
+  name 'Mullvad'
+  homepage 'https://mullvad.net/'
+  gpg "#{url}.asc", key_id: 'a1198702fc3e0a09a9ae5b75d5a1d4f266de8ddf'
+
+  conflicts_with cask: 'mullvadvpn'
+
+  pkg "MullvadVPN-#{version}.pkg"
+
+  uninstall pkgutil: 'net.mullvad.vpn'
+
+  zap trash: [
+               '~/Library/Application Support/Mullvad VPN',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/net.mullvad.vpn.sfl*',
+               '~/Library/Logs/Mullvad VPN',
+               '~/Library/Preferences/net.mullvad.vpn.plist',
+               '~/Library/Preferences/net.mullvad.vpn.helper.plist',
+             ]
+end
