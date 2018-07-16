63bae800559d7e19b4abd19f529b3a5ce69cb5bb
Jonas Weismueller <dev@weismueller.org>
2018-07-17 01:13:28 +0200

    change to kodi snapshots directory

--- Casks/kodi-development.rb
+++ Casks/kodi-development.rb
@@ -2,7 +2,7 @@ cask 'kodi-development' do
   version '18.0-Leia_alpha2'
   sha256 '94bf58db6144e9037e6aba8ae1f513369c696fb87169f63ac4ec3d9068e05d08'
 
-  url "http://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
+  url "http://mirrors.kodi.tv/snapshots/osx/x86_64/kodi-#{version}-x86_64.dmg"
   name 'Kodi-Development'
   homepage 'https://kodi.tv/'
 
