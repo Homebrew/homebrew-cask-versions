6969d0b1c39008c2835b58aa1948d484e75d2488
commitay <commitay@users.noreply.github.com>
2018-07-21 10:35:06 +1000

    Update powershell-preview to 6.1.0-preview.4 (#5985)
    * Update powershell-preview to 6.1.0-preview.4
    
    * Update powershell-preview.rb

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -1,6 +1,6 @@
 cask 'powershell-preview' do
-  version '6.1.0-preview.3'
-  sha256 '95efa9d091de87746f877814aee56d2ab8f25fddf596aba2653b1782dd2ba362'
+  version '6.1.0-preview.4'
+  sha256 '5ce2b78621042fca2d1d0f02b01911e125e1ea324b96a1d1be5e75c95a0c8d34'
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom'
@@ -12,7 +12,7 @@ cask 'powershell-preview' do
 
   pkg "powershell-#{version}-osx-x64.pkg"
 
-  uninstall pkgutil: 'com.microsoft.powershell'
+  uninstall pkgutil: 'com.microsoft.powershell-preview'
 
   zap trash: [
                '~/.cache/powershell',
