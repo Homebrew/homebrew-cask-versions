7aaedeba585e3242f4a4eff7361a5410e462acc2
404NetworkError <404NetworkError@users.noreply.github.com>
2018-07-27 11:15:20 -0400

    Update hex-fiend-beta to 2.10b5

--- Casks/hex-fiend-beta.rb
+++ Casks/hex-fiend-beta.rb
@@ -1,6 +1,6 @@
 cask 'hex-fiend-beta' do
-  version '2.10b4'
-  sha256 'a12ac47dff6c31e6ab9ce3e58463a4c93df967407f4024ea57cc703183d2c978'
+  version '2.10b5'
+  sha256 'd9defeeb11ffd6871b21361b066b5952a5f5353f982a97fe46f13f0a3a986000'
 
   # github.com/ridiculousfish/HexFiend was verified as official when first introduced to the cask
   url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.major_minor}.dmg"
