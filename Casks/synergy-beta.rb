5c73f48abf87776d20fb9acbea1646afea69fd7e
Ryan Duryea <aguynamedryan@gmail.com>
2018-07-28 03:12:28 -0700

    Add Synergy 2.0.12(beta) (#6012)
    Because #50109 reverted back to Synergy v1.10 as the stable version,
    I'm resurrecting the previous version of the formula and submitting it as
    synergy-beta for those who want to continue using Synergy v2 while it's
    in beta.

---
+++ Casks/synergy-beta.rb
@@ -0,0 +1,10 @@
+cask 'synergy-beta' do
+  version '2.0.12,b1807-50472cde'
+  sha256 '41927a6a52a523797122a7d6a9c8817d09cce9966ad08118c693345f51aa41e9'
+
+  url "https://binaries.symless.com/v#{version.before_comma}/Synergy_v#{version.before_comma}-beta_#{version.after_comma}.dmg"
+  name 'Synergy'
+  homepage 'https://symless.com/synergy'
+
+  app 'Synergy.app'
+end
