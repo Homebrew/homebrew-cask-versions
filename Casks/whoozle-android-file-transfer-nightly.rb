9063d4937de6ccd5049f9301940aa050b3a33494
commitay <commitay@users.noreply.github.com>
2018-08-07 09:46:11 +1000

    Update whoozle-android-file-transfer-nightly.rb

--- Casks/whoozle-android-file-transfer-nightly.rb
+++ Casks/whoozle-android-file-transfer-nightly.rb
@@ -1,15 +1,16 @@
 cask 'whoozle-android-file-transfer-nightly' do
-  version 'continuous'
+  version :latest
   sha256 :no_check
 
   # github.com/whoozle/android-file-transfer-linux was verified as official when first introduced to the cask
-  url "https://github.com/whoozle/android-file-transfer-linux/releases/download/#{version}/AndroidFileTransferForLinux.dmg"
-  appcast 'https://github.com/whoozle/android-file-transfer-linux/releases.atom'
+  url 'https://github.com/whoozle/android-file-transfer-linux/releases/download/continuous/AndroidFileTransferForLinux.dmg'
   name 'Android File Transfer'
   homepage 'https://whoozle.github.io/android-file-transfer-linux/'
 
-  conflicts_with cask: ['android-file-transfer',
-                        'whoozle-android-file-transfer']
+  conflicts_with cask: [
+                         'android-file-transfer',
+                         'whoozle-android-file-transfer',
+                       ]
 
   app 'Android File Transfer for Linux.app'
 end
