cc98899adb461634424ae9c6873a2caf1c31482e
Markus Reiter <me@reitermark.us>
2018-08-16 06:06:20 +0200

    Add `mcedit-beta` 2.0.0-beta12.

---
+++ Casks/mcedit-beta.rb
@@ -0,0 +1,21 @@
+cask 'mcedit-beta' do
+  version '2.0.0-beta12'
+  sha256 '6c92dcc582cd1cf979a41ba825d49dca464bb20c128a28d8d27ab1b9f7a3d5aa'
+
+  # github.com/mcedit/mcedit2 was verified as official when first introduced to the cask
+  url "https://github.com/mcedit/mcedit2/releases/download/#{version}/mcedit2-macosx64-#{version}.zip"
+  appcast 'https://github.com/mcedit/mcedit2/releases.atom'
+  name 'MCEdit 2.0'
+  homepage 'https://www.mcedit.net/'
+
+  suite "mcedit2-macosx64-#{version}", target: 'MCEdit 2'
+
+  zap trash:  [
+                '~/Library/Application Support/pymclevel',
+                '~/Library/Preferences/mcedit.ini',
+              ],
+      delete: [
+                '~/Library/Application Support/CrashReporter/MCEdit 2_*.plist',
+                '~/Library/Saved Application State/net.mcedit.mcedit2.savedState',
+              ]
+end
