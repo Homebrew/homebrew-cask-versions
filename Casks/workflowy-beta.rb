b303edda3d24a8b0605ca19475e50cfd1f0fe3b3
Victor Popkov <victor@popkov.me>
2018-08-17 22:28:36 +0300

    Update workflowy-beta to 1.1.1-beta.605

--- Casks/workflowy-beta.rb
+++ Casks/workflowy-beta.rb
@@ -1,6 +1,6 @@
 cask 'workflowy-beta' do
-  version '1.1.1-beta.543'
-  sha256 'cec416ccf403c1f43f1aa08dd3eeef0d0f1dfd4d47259f730e96ef0c07f5616b'
+  version '1.1.1-beta.605'
+  sha256 '30036444412f0eadc7e3a880fe86298e0823bfe93bbfe5b76cb9268a807f5fd9'
 
   # github.com/workflowy/desktop-beta was verified as official when first introduced to the cask
   url "https://github.com/workflowy/desktop-beta/releases/download/v#{version}/WorkFlowy-Beta.dmg"
