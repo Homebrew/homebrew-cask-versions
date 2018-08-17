6c0399b7a81e23ca29fb9bd67bc0f22082d3112a
mackoz <wxlu@ucdavis.edu>
2018-08-17 04:09:38 -0700

    Update airmail-beta to 3.5.5.505,361 (#6093)

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 cask 'airmail-beta' do
-  version '3.5.5.503,359'
-  sha256 '903e42f1733c719661f4aae99ac9c0efeae57077c4bc7d53d6b1180e5528a27c'
+  version '3.5.5.505,361'
+  sha256 '2b6dd81560bbddd034b86861048aac973c646791703e69fc8e3934c60c8d5190'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
