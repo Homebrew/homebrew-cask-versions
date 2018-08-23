d4e3321990535cfa98c4ff6e04a852f762a8a9cb
Victor Popkov <victor@popkov.me>
2018-08-23 22:38:48 +0300

    Update powershell-preview to 6.1.0-rc.1 (#6133)

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -1,6 +1,6 @@
 cask 'powershell-preview' do
-  version '6.1.0-preview.4'
-  sha256 '5ce2b78621042fca2d1d0f02b01911e125e1ea324b96a1d1be5e75c95a0c8d34'
+  version '6.1.0-rc.1'
+  sha256 '26ec5668ec9e93cc511654986c1f4d88db9cf20c668014850e31422e715f3b81'
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom'
