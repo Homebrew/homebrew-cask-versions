be6342fca58691ad96b7dd0f4f38790a1e1db9f5
miccal <miccal.matthews@gmail.com>
2018-08-26 14:21:34 +0800

    Update vlc-nightly.rb

--- Casks/vlc-nightly.rb
+++ Casks/vlc-nightly.rb
@@ -11,7 +11,29 @@ cask 'vlc-nightly' do
   name 'VLC media player'
   homepage 'https://www.videolan.org/vlc/'
 
+  auto_updates true
+  conflicts_with cask: 'vlc'
   depends_on macos: '>= :lion'
 
   app 'VLC.app'
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/vlc.wrapper.sh"
+  binary shimscript, target: 'vlc'
+
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/VLC.app/Contents/MacOS/VLC' "$@"
+    EOS
+  end
+
+  zap trash: [
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.videolan.vlc.sfl*',
+               '~/Library/Application Support/org.videolan.vlc',
+               '~/Library/Application Support/VLC',
+               '~/Library/Preferences/org.videolan.vlc',
+               '~/Library/Preferences/org.videolan.vlc.plist',
+               '~/Library/Saved Application State/org.videolan.vlc.savedState',
+               '~/Library/Caches/org.videolan.vlc',
+             ]
 end
