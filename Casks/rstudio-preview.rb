4f4c5dc5ef4957ec4e9b65762086e2333e26795b
snp <chernobrovkin@gmail.com>
2018-08-28 10:49:35 +0200

    New version is available

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.2.679'
-  sha256 'b3f951ea7205c3af77096cb26089194b760e92df72f46948cce3d3fb2f371bca'
+  version '1.2.907'
+  sha256 '2f251cb073b571ce42d7b368aa04285a207c2407c3861f00d544743a8c245bd0'
 
   # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
