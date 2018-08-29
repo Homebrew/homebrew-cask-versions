066f79741636009b1a73c54e427c10baa5b49925
Claudia <claui@users.noreply.github.com>
2018-08-29 10:45:53 +0200

    Update vitamin-r3 to 3.00b2; add stanzas
    This commit makes the following changes:
    
    - Bump version
    - Add `auto_updates true`
    - Add minimum macOS version (source: Sparkle metadata, homepage)
    
    Note: If upstream keeps pushing releases so frequently without
    versioned URLs, I’m going to remove the appcast and switch the
    version to `:latest`.

--- Casks/vitamin-r3.rb
+++ Casks/vitamin-r3.rb
@@ -1,11 +1,14 @@
 cask 'vitamin-r3' do
-  version '3.00b1'
-  sha256 '47e95920e11cd0d9f9aa8d64ac7a9b8d4ce6c7b2728557b09d738771e38d76f9'
+  version '3.00b2'
+  sha256 '4725de4a2d871eb428320e5ad236543de9c51e56153c14d424f1193d5abe0f6a'
 
   url "http://www.publicspace.net/download/signedVitamin#{version.major}.zip"
   appcast "http://www.publicspace.net/app/vitamin#{version.major}.xml"
   name 'Vitamin-R'
   homepage 'http://www.publicspace.net/Vitamin-R/'
+
+  auto_updates true
+  depends_on macos: '>= :yosemite'
 
   app 'Vitamin-R 3.app'
 end
