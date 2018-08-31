f132e2d58dd4ef65a865633a67e456ddfcc7013d
Victor Popkov <victor@popkov.me>
2018-08-31 23:35:24 +0300

    Update signal-beta to 1.16.0-beta.1

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,6 +1,6 @@
 cask 'signal-beta' do
-  version '1.15.5-beta.1'
-  sha256 'd00eaa0a07cff91b9174ed278ed0cbfa8238f5c8832e8d93026264c1bb11859f'
+  version '1.16.0-beta.1'
+  sha256 'e07b598ad3dd626ed4e7155f5d9dfaab6222418b486342b8cef6a62f58e1ad9f'
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
