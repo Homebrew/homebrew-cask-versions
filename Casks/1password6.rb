e15309b4b5ec6c5ff29c39b8895c3a8217161d06
commitay <commitay@users.noreply.github.com>
2018-09-05 09:21:25 +1000

    Update 1password6 to 6.8.9

--- Casks/1password6.rb
+++ Casks/1password6.rb
@@ -2,7 +2,6 @@ cask '1password6' do
   version '6.8.9'
   sha256 'd7cc24dc354f27441929350b9e6e2e4a710d6ed0bdab06f0e9be07160fe04200'
 
-  # 1password.com was verified as official when first introduced to the cask
   url "https://c.1password.com/dist/1P/mac4/1Password-#{version}.zip"
   appcast 'https://app-updates.agilebits.com/product_history/OPM4'
   name '1Password'
