d8185e2fd0c6cb3e8b03001a5044c9dc6620ac16
commitay <commitay@users.noreply.github.com>
2018-09-05 09:04:17 +1000

    Update 1password-beta.rb

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -2,7 +2,6 @@ cask '1password-beta' do
   version '7.2.BETA-1'
   sha256 'cb3dfed9ed5e7e1eabca3d836619dfa050e4542336d224c70f7cd3a1e1c46451'
 
-  # 1password.com was verified as official when first introduced to the cask
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
   name '1Password'
