d28a2874064538c8dc400c483b4ebe8d26d79a35
Claudia <claui@users.noreply.github.com>
2018-09-05 07:31:15 +0200

    Update vitamin-r3 to 3.00b3

--- Casks/vitamin-r3.rb
+++ Casks/vitamin-r3.rb
@@ -1,6 +1,6 @@
 cask 'vitamin-r3' do
-  version '3.00b2'
-  sha256 '4725de4a2d871eb428320e5ad236543de9c51e56153c14d424f1193d5abe0f6a'
+  version '3.00b3'
+  sha256 '16c02a666526052c096435d402137556bf9e204bfdf05dfe36b2ed9cc1c42490'
 
   url "http://www.publicspace.net/download/signedVitamin#{version.major}.zip"
   appcast "http://www.publicspace.net/app/vitamin#{version.major}.xml"
