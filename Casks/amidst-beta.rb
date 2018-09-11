317a146b824c1a8ceaa41a1958ff0aad64ed7f05
Victor Popkov <victor@popkov.me>
2018-09-11 22:57:08 +0300

    Update amidst-beta to 4.3-beta5 (#6230)

--- Casks/amidst-beta.rb
+++ Casks/amidst-beta.rb
@@ -1,6 +1,6 @@
 cask 'amidst-beta' do
-  version '4.3-beta4'
-  sha256 'f6238c6bac873bd64140db5e1cf6490b7fb23e7fbd1d04b272119a5098aec38e'
+  version '4.3-beta5'
+  sha256 '1ad7e7043b619dfcdc2445e87bb8c97d64e47f8f66616d127994700d8046ac73'
 
   url "https://github.com/toolbox4minecraft/amidst/releases/download/v#{version}/amidst-v#{version.dots_to_hyphens}.zip"
   appcast 'https://github.com/toolbox4minecraft/amidst/releases.atom'
