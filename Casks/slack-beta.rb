422c993c1d88fe5d3c93735bc432582740be684d
L. E. Segovia <13498015+amyspark@users.noreply.github.com>
2018-09-14 01:21:21 +0000

    slack-beta: sync zap stanza, add conflicts_with

--- Casks/slack-beta.rb
+++ Casks/slack-beta.rb
@@ -8,18 +8,24 @@ cask 'slack-beta' do
   homepage 'https://slack.com/beta/osx'
 
   auto_updates true
+  conflicts_with cask: 'slack'
 
   app 'Slack.app'
 
   uninstall quit: 'com.tinyspeck.slackmacgap'
 
   zap trash: [
+               '~/Library/Application Scripts/com.tinyspeck.slackmacgap',
                '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tinyspeck.slackmacgap.sfl*',
                '~/Library/Application Support/Slack',
                '~/Library/Caches/com.tinyspeck.slackmacgap',
+               '~/Library/Caches/com.tinyspeck.slackmacgap.ShipIt',
                '~/Library/Containers/com.tinyspeck.slackmacgap',
                '~/Library/Containers/com.tinyspeck.slackmacgap.SlackCallsService',
                '~/Library/Cookies/com.tinyspeck.slackmacgap.binarycookies',
+               '~/Library/Group Containers/*.com.tinyspeck.slackmacgap',
+               '~/Library/Group Containers/*.slack',
+               '~/Library/Preferences/com.tinyspeck.slackmacgap.helper.plist',
                '~/Library/Preferences/com.tinyspeck.slackmacgap.plist',
                '~/Library/Saved Application State/com.tinyspeck.slackmacgap.savedState',
              ]
