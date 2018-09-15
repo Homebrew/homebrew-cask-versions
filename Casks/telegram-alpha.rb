1653948393a447a065d039e99aacc634f7721fff
Nikolas Tapia <ntapia@dim.uchile.cl>
2018-09-14 22:09:01 -0300

    Update telegram-alpha to 4.3.3-139438,1335

--- Casks/telegram-alpha.rb
+++ Casks/telegram-alpha.rb
@@ -1,6 +1,6 @@
 cask 'telegram-alpha' do
-  version '4.3.2-138861,1331'
-  sha256 '0bf31f5ace5e19d9d90ab93f0f1c3ad679d5f9f7628e2b2a5686021d2d1b0811'
+  version '4.3.3-139438,1335'
+  sha256 '13678d4de060ba50894e176b6beb51c08eb4cf312bec635222c96b247ec3f4e9'
 
   # hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f/app_versions/#{version.after_comma}?format=zip"
