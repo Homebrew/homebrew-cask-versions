ef6aae71c5c47b092e073d0d188a593e88acd4d3
Kevin Schaich <schaich.kevin@gmail.com>
2018-09-15 20:45:05 -0700

    Fix audit/style

--- Casks/brave-dev.rb
+++ Casks/brave-dev.rb
@@ -3,14 +3,14 @@ cask 'brave-dev' do
   sha256 '28b11716604546f96dca92018415995c3e1af6dc5767ef4efa6e62b24e1c8b93'
 
   url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser-Dev.dmg"
-  appcast 'https://github.com/brave/browser-browser/releases.atom'
+  appcast 'https://github.com/brave/brave-browser/releases.atom'
   name 'Brave Dev'
-  homepage 'https://brave.com/'
+  homepage 'https://github.com/brave/brave-browser'
 
   auto_updates true
   depends_on macos: '>= :mavericks'
 
-  app 'Brave-Browser-Dev.app'
+  app 'Brave Browser Dev.app'
 
   zap trash: [
                '~/Library/Application Support/brave',
