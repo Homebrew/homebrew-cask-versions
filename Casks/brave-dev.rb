992ad75297df28de9265a6512f08445a26c98515
Kevin Schaich <schaich.kevin@gmail.com>
2018-09-15 20:28:57 -0700

    Create brave-dev.rb

---
+++ Casks/brave-dev.rb
@@ -0,0 +1,20 @@
+cask 'brave-dev' do
+  version '0.55.4'
+  sha256 '28b11716604546f96dca92018415995c3e1af6dc5767ef4efa6e62b24e1c8b93'
+
+  url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser-Dev.dmg"
+  appcast 'https://github.com/brave/browser-browser/releases.atom'
+  name 'Brave Dev'
+  homepage 'https://brave.com/'
+
+  auto_updates true
+  depends_on macos: '>= :mavericks'
+
+  app 'Brave-Browser-Dev.app'
+
+  zap trash: [
+               '~/Library/Application Support/brave',
+               '~/Library/Preferences/com.electron.brave.plist',
+               '~/Library/Saved Application State/com.electron.brave.savedState',
+             ]
+end
