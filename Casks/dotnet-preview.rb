aaa9264a3c0e796f16c74037a1078ede56164fab
Yurii Kolesnykov <root@yurikoles.com>
2018-09-26 19:52:20 +0300

    new cask dotnet-preview v2.2.0-preview2-26905-02

---
+++ Casks/dotnet-preview.rb
@@ -0,0 +1,18 @@
+cask 'dotnet-preview' do
+  version '2.2.0-preview2-26905-02'
+  sha256 '26c9ad4fe67510f5714f58a5d31ae657675dd9f918e4de083e02eb99609f10cf'
+
+  url "https://download.microsoft.com/download/5/B/A/5BA1012E-5112-45C2-8369-152B49A6AA3B/dotnet-runtime-#{version}-osx-x64.pkg"
+  appcast 'https://www.microsoft.com/net/download/macos'
+  name '.Net Core Runtime'
+  homepage 'https://www.microsoft.com/net/core#macos'
+
+  depends_on macos: '>= :sierra'
+
+  pkg "dotnet-runtime-#{version}-osx-x64.pkg"
+
+  uninstall pkgutil: "com.microsoft.dotnet.*#{version.major_minor}*"
+
+  zap delete: '/etc/paths.d/dotnet',
+      trash:  '~/.nuget'
+end
