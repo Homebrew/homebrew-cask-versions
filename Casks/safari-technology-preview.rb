d5302583715919d136495487d2eba8a959ed4964
commitay <commitay@users.noreply.github.com>
2018-09-27 15:03:23 +1000

    Update safari-technology-preview to 66 (#6317)

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,12 +1,12 @@
 cask 'safari-technology-preview' do
-  version '65'
+  version '66'
 
   if MacOS.version <= :high_sierra
-    url 'https://secure-appldnld.apple.com/STP/041-04652-20180910-76E7269A-B217-11E8-B40C-C08B7A641E38/SafariTechnologyPreview.dmg'
-    sha256 '10c9c997d2042e190aa3ad30508fd77f673d3f850468338cb0b67226b185cb03'
+    url 'https://secure-appldnld.apple.com/STP/041-08190-20180925-61411BD2-9CD4-11E8-9FDB-19F091474B07/SafariTechnologyPreview.dmg'
+    sha256 '502c28dff03d407304996cdaa6d383dcfda63750fa99729c3a22bbcb46bd4611'
   else
-    url 'https://secure-appldnld.apple.com/STP/041-04649-20180910-76E7269A-B217-11E8-B40C-C08B7A641E38/SafariTechnologyPreview.dmg'
-    sha256 '188d221b33cc6c5e1b56d146c11555bc8e5b5ad367c07ee15b8fffe91052b356'
+    url 'https://secure-appldnld.apple.com/STP/041-08724-20180925-61411BD2-9CD4-11E8-9FDB-19F091474B07/SafariTechnologyPreview.dmg'
+    sha256 '3656402b9eb61e33b27be41a73d018c26e70678e4b9de2fdb47911403d66b987'
   end
 
   name 'Safari Technology Preview'
