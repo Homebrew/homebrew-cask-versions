33c8db75d36c031af9d7da1d3a7449d1e11c21bb
Scott Humphries <git@sscotth.io>
2018-09-27 11:50:10 -0500

    Add sha check to Quickbooks2016

--- Casks/quickbooks2016.rb
+++ Casks/quickbooks2016.rb
@@ -1,6 +1,6 @@
 cask 'quickbooks2016' do
   version '2016'
-  sha256 :no_check # required as upstream package is updated in-place
+  sha256 'e0cd259ea71c5c1af9de3e1e6434c32f7182f18bc29688afa428706ff4966f91'
 
   url "https://http-download.intuit.com/http.intuit/Downloads/#{version}/Latest/QuickBooksMac#{version}.dmg"
   name 'QuickBooks Desktop'
