62782d34a396a3e36a68bdb7407268bf802c3071
Oli Walker <oliwalker@btinternet.com>
2018-10-04 13:59:46 +0100

    Update visual-studio-code-insiders to 1.28.0,431ef9da3cf88a7e164f9d33bf62695e07c6c2a9 (#6354)

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code-insiders' do
-  version '1.28.0,85d970acdee48f6dcecf909a0faeb3c6882217a9'
-  sha256 '068df7d88b037d978a1af75ee4cf379e33a00b3043f19f7b5c949b272a2ddea1'
+  version '1.28.0,431ef9da3cf88a7e164f9d33bf62695e07c6c2a9'
+  sha256 '347c80313e7788bf9a2310e5cdf4b50299b5ab65dea5b17f9916f2a6810c2883'
 
   # az764295.vo.msecnd.net/insider was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
