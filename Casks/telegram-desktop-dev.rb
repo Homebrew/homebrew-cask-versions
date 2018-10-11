76122ea432ba8fc233ad975ae8a8181494dfa6ff
Yurii Kolesnykov <root@yurikoles.com>
2018-10-11 18:50:15 +0300

    telegram-desktop-dev: update to 1.4.3 stable, add zap.

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -1,15 +1,23 @@
 cask 'telegram-desktop-dev' do
-  version '1.3.16'
-  sha256 '6845d0bb1c9dca30b80cb8845a8d582f28dfe25e6b9e76287ce64529b124e184'
+  version '1.4.3'
+  sha256 '06944ae5345992a03517fd6a9fed1689dfc3000a4a894f313948ee1f2649e69e'
 
   # tdesktop.com was verified as official when first introduced to the cask
-  url "https://updates.tdesktop.com/tmac/tsetup.#{version}.alpha.dmg"
-  name 'Telegram'
+  url "https://updates.tdesktop.com/tmac/tsetup.#{version}.dmg"
+  appcast 'https://github.com/telegramdesktop/tdesktop/releases.atom'
+  name 'Telegram Desktop'
   homepage 'https://desktop.telegram.org/'
 
+  auto_updates true
   conflicts_with cask: 'telegram-desktop'
   depends_on macos: '>= :mountain_lion'
 
   # Renamed to avoid conflict with telegram
   app 'Telegram.app', target: 'Telegram Desktop.app'
+
+  zap trash: [
+               '~/Library/Application Support/Telegram Desktop',
+               '~/Library/Preferences/com.tdesktop.Telegram.plist',
+               '~/Library/Saved Application State/com.tdesktop.Telegram.savedState',
+             ]
 end
