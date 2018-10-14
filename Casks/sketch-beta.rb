9d3b8b8199083abbe7aff72fc63f42b5748e2bcc
Scott Humphries <sscotth@users.noreply.github.com>
2018-10-14 07:46:51 -0500

    Update sketch-beta to 52.2.0,67145 (#6397)

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,6 +1,6 @@
 cask 'sketch-beta' do
-  version '52.2.0,67122'
-  sha256 'b3bb9ee1da34ceb2ee43df59022738f7dbeecfb3b43c8310d94e7ebf96e61c4d'
+  version '52.2.0,67145'
+  sha256 '87a5c1f16bd48435c28184143507f3e45f2d981d266abcf85b49a61caa7828c7'
 
   # hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/0172d48cceec171249a8d850fb16276b/app_versions/#{version.after_comma}?format=zip"
