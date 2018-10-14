5e1eaa18f815c8614dabd52666fc490be721c80d
Stephen Rogers <inbox.c7r@steve-rogers.com>
2018-10-14 15:05:17 -0400

    Parameterize embedded references to app name and version.

--- Casks/alfred2.rb
+++ Casks/alfred2.rb
@@ -1,25 +1,28 @@
 cask 'alfred2' do
-  version '2.9_446'
+  version_major '2'
+  version_minor_micro '9_446'
   sha256 '26a99cc0b96a4acee02bbc162ee433d9f1b20518ba8acc9691564b2c3698ed6a'
 
-  url "https://cachefly.alfredapp.com/Alfred_#{version}.zip"
   name 'Alfred'
   homepage 'https://www.alfredapp.com/'
+  version "#{version_major}.#{version_minor_micro}"
+
+  url "https://cachefly.alfredapp.com/#{name}_#{version}.zip"
 
   auto_updates true
   accessibility_access true
 
-  app 'Alfred 2.app'
+  app "#{name} #{version_major}.app"
 
-  uninstall quit:       'com.runningwithcrayons.Alfred-2',
-            login_item: 'Alfred 2'
+  uninstall quit: "com.runningwithcrayons.#{name}-#{version_major}",
+            login_item: "#{name} #{version_major}"
 
   zap trash: [
-               '~/Library/Application Support/Alfred 2',
-               '~/Library/Caches/com.runningwithcrayons.Alfred-2',
-               '~/Library/Caches/com.runningwithcrayons.Alfred-Preferences',
-               '~/Library/Preferences/com.runningwithcrayons.Alfred-2.plist',
-               '~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist',
-               '~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState',
+               "~/Library/Application Support/#{name} #{version_major}",
+               "~/Library/Caches/com.runningwithcrayons.#{name}-#{version_major}",
+               "~/Library/Caches/com.runningwithcrayons.#{name}-Preferences",
+               "~/Library/Preferences/com.runningwithcrayons.#{name}-#{version_major}.plist",
+               "~/Library/Preferences/com.runningwithcrayons.#{name}-Preferences.plist",
+               "~/Library/Saved Application State/com.runningwithcrayons.#{name}-Preferences.savedState",
              ]
 end
