fd16b505bf3f033403e10b85700b4f1ca01b3665
Yurii Kolesnykov <root@yurikoles.com>
2018-10-23 14:21:42 +0300

    Update virtualbox-extension-pack-beta to 6.0.0_BETA1,125994 (#6445)

--- Casks/virtualbox-extension-pack-beta.rb
+++ Casks/virtualbox-extension-pack-beta.rb
@@ -1,6 +1,6 @@
 cask 'virtualbox-extension-pack-beta' do
-  version '5.2.0_RC1,118201'
-  sha256 'fd73ff4e6a1c8677c0252a94d21423d1e59191c3ab2df00891d37d559899f0c2'
+  version '6.0.0_BETA1,125994'
+  sha256 '401783b228db7ffbe4812618ce1b02c11d89c0f6cfbde886f7bfd9ce2faf9224'
 
   url "https://download.virtualbox.org/virtualbox/#{version.before_comma}/Oracle_VM_VirtualBox_Extension_Pack-#{version.before_comma}-#{version.after_comma}.vbox-extpack"
   appcast 'https://download.virtualbox.org/virtualbox/LATEST-BETA.TXT'
