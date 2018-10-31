8bc5d00392c114a347eb9c8470fde9494750bed0
AlixBa <alix.baggio@outlook.fr>
2018-10-31 15:08:15 +0100

    Add zulu10

---
+++ Casks/zulu10.rb
@@ -0,0 +1,33 @@
+cask 'zulu10' do
+  version '10.3,5:10.0.2'
+  sha256 'dfc5a9cae3fbfb45d565bcd067829b7273704c976ff00a63794c9f9742fe4f76'
+
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}+#{version.after_comma.before_colon}-jdk#{version.after_colon}-macosx_x64.dmg",
+      referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
+  name 'Azul Zulu Java Standard Edition Development Kit'
+  homepage 'https://www.azul.com/downloads/zulu/zulu-mac/'
+
+  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+
+  postflight do
+    system_command '/bin/mv',
+                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}+#{version.after_comma.before_colon}.jdk"],
+                   sudo: true
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}+#{version.after_comma.before_colon}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk"],
+                   sudo: true
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Home", '/Library/Java/Home'],
+                   sudo: true
+    system_command '/usr/libexec/PlistBuddy',
+                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Info.plist"],
+                   sudo: true
+  end
+
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}",
+            delete:  [
+                       "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}+#{version.after_comma.before_colon}.jdk",
+                       "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk",
+                       '/Library/Java/Home',
+                     ]
+end
