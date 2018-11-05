d1f8a0c33ba674658245b5ece0a8ef64667a30d8
Jonas Weismueller <jonas.weismueller@blue-yonder.com>
2018-11-05 18:47:22 +0100

    update kodi-development to latest beta version 18.0-Leia_beta5

--- Casks/kodi-development.rb
+++ Casks/kodi-development.rb
@@ -1,6 +1,6 @@
 cask 'kodi-development' do
-  version '18.0-Leia_beta4'
-  sha256 '476c5baeafaf47be367114a5708e01765da68e7329fa05dcb99100d6cd0de251'
+  version '18.0-Leia_beta5'
+  sha256 '55e305b7d451fe3e1710fe61a6fb283c79b2a23ed572d9957a99dcb8a823e567'
 
   url "https://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   name 'Kodi-Development'
