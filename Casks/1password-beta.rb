73bf6aa83e806e1c996d12be32b175804497e451
Corey Gouker <corey@coreygo.com>
2018-11-07 17:04:18 -0800

    Update 1password-beta to 7.2.2.BETA-5 (#6509)

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask '1password-beta' do
-  version '7.2.2.BETA-4'
-  sha256 '6b4a345ef635c604c9673bd32b9d05884b7302f356fba39d9a777ecc301bcf7c'
+  version '7.2.2.BETA-5'
+  sha256 'b712ea932fe2454d6e26f0d02b333e4add01bd69262c4c552204db97d4d76234'
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
