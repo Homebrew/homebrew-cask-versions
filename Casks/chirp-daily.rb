424f9efe37f298a704a82774fc8ef9cf3e90208c
Nick Silkey <nsilkey@digitalocean.com>
2018-11-15 09:50:02 -0600

    Update chirp-daily
    While 20180412 is still up and not 404, the LATEST slug will never get
    you there.  20181018 is current as of Thu Nov 15 15:53:37 UTC 2018.

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,6 +1,6 @@
 cask 'chirp-daily' do
-  version '20180412'
-  sha256 '727dad77ba80ac685108fb1b389d11a7e0daa7451ed25c242812e9a537e04ff2'
+  version '20181018'
+  sha256 'afa80f12f89989625513020d22cfe759b19309f92f1461499045d5feed55a288'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
