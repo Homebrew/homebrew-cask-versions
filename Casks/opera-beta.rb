e1ac2b12eb858428578a7416358eec2f77bcf02d
Victor Popkov <victor@popkov.me>
2018-11-16 21:25:20 +0200

    Update opera-beta to 57.0.3098.36

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '57.0.3098.14'
-  sha256 'd6bb200f9a655031ff045395b2616d7a4e2b33d1e5787e30c8fc50588b378038'
+  version '57.0.3098.36'
+  sha256 '57c3d39ba63b24c1bfb77db809365d4865539706ea7913ca22abf3859c8acd65'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
