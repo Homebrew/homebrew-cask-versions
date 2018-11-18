36e76fb5b7bd183401be5e84f4a5624c0ca8fea1
Vítor Galvão <info@vitorgalvao.com>
2018-11-18 15:44:49 +0000

    Update affinity-photo-beta to 1.7.0.99

--- Casks/affinity-photo-beta.rb
+++ Casks/affinity-photo-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-photo-beta' do
-  version '1.7.0.98'
-  sha256 '171fe590ccbff74a7d856ae8f207ed50d45c65d44509d5503598a7bd3b1a7e5d'
+  version '1.7.0.99'
+  sha256 'd4575348e788e266ce8e3b44443de010f60dd1b9fe871acc5268bd527254dc9c'
 
   # affinity-beta.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://affinity-beta.s3.amazonaws.com/download/Affinity%20Photo%20Customer%20Beta%20%28#{version}%29.dmg"
