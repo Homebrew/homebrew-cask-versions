e31ccbdb830773a4e6cc5844baab9df0ea895331
Vítor Galvão <info@vitorgalvao.com>
2018-11-21 10:25:15 +0000

    Update affinity-designer-beta to 1.7.0.3 (#6574)

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-designer-beta' do
-  version '1.7.0.2'
-  sha256 '8bb7e45d80ca4bc58ff1820c084747e7bb776e13c213866143146ca91026936f'
+  version '1.7.0.3'
+  sha256 'c333ac58ec8db106a47ab46b70ca824621044cafde7bc7cd09043d4d0707c69b'
 
   # amazonaws.com/affinity-beta was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/affinity-beta/download/Affinity%20Designer%20Beta.dmg'
