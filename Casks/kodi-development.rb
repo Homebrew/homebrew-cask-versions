f575fc575a3e4ed5d094795a61709bdb27d6a568
Jonas Weismueller <dev@weismueller.org>
2018-11-22 21:17:56 +0100

    new version of kodi-development rc1

--- Casks/kodi-development.rb
+++ Casks/kodi-development.rb
@@ -1,6 +1,6 @@
 cask 'kodi-development' do
-  version '18.0-Leia_beta5'
-  sha256 '55e305b7d451fe3e1710fe61a6fb283c79b2a23ed572d9957a99dcb8a823e567'
+  version '18.0-Leia_rc1'
+  sha256 'cc1f9de0c4261b52846084244a97cdbbdd9940f28d4b8fd026148a274374a6e7'
 
   url "https://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   name 'Kodi-Development'
