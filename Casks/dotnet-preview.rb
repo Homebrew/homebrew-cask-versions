0c63e42a780bcb1a6575aa8aec11c2ff8fadc961
Yurii Kolesnykov <root@yurikoles.com>
2018-12-05 11:20:31 +0200

    Update dotnet-preview to 3.0.0-preview-27122-01

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-preview' do
-  version '2.2.0-preview2-26905-02'
-  sha256 '26c9ad4fe67510f5714f58a5d31ae657675dd9f918e4de083e02eb99609f10cf'
+  version '3.0.0-preview-27122-01'
+  sha256 'b619abf79ffda8ad4048c8143ac882749dd32132ed1e1a1cfd2ffceb3ee5e543'
 
-  url "https://download.microsoft.com/download/5/B/A/5BA1012E-5112-45C2-8369-152B49A6AA3B/dotnet-runtime-#{version}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/69507539-d683-4ab5-a5c5-6fe356ce0e6a/ec078cea28f02351a1aa63b5615f1e63/dotnet-runtime-#{version}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.Net Core Runtime'
   homepage 'https://www.microsoft.com/net/core#macos'
