86ccd76a9649503fbffd081119fdc8e365f23af9
Yurii Kolesnykov <root@yurikoles.com>
2018-12-05 19:33:34 +0200

    Update dotnet-sdk-preview to 3.0.100-preview-009812 (#6659)

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk-preview' do
-  version '2.2.100-preview2-009404'
-  sha256 '0df3645b21197a50bfeaeb34887230cd194c8a160834115f761cbfa3e7019015'
+  version '3.0.100-preview-009812'
+  sha256 '87bf32be80cd24a9086f82adcf0d9fe698ab6e45957657feb8ef409fa61923b4'
 
-  url "https://download.microsoft.com/download/D/5/9/D593CD8F-04E7-425D-962C-86FF4C90B1DA/dotnet-sdk-#{version}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/4331e5b4-528a-422d-a03e-6f4f877adc33/54651267c4af9d4b6dedb661f267085d/dotnet-sdk-#{version}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
