b54272c35378cac8f4a26aa34f090831ba89c317
Yusuf Kursad Kaya <ykursadkaya@gmail.com>
2018-12-08 20:41:42 +0300

    Update loopback.rb

--- Casks/loopback1.rb
+++ Casks/loopback1.rb
@@ -2,7 +2,7 @@ cask 'loopback1' do
   version '1.2.1'
   sha256 '8be69661a740a4966ad7048b5ab68a1322ae8a373b4a15f23d1fc02ed44e32b5'
 
-  url 'https://rogueamoeba.com/legacy/downloads/Loopback-121.zip'
+  url "https://rogueamoeba.com/legacy/downloads/Loopback-#{version.no_dots}.zip"
   name 'Loopback 1'
   homepage 'https://rogueamoeba.com/legacy/'
 
