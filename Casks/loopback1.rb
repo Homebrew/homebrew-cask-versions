3168367db5fb5b8cbf8f34be4589c7c256a292eb
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2018-12-08 20:58:21 +0300

    Add appcast

--- Casks/loopback1.rb
+++ Casks/loopback1.rb
@@ -3,6 +3,7 @@ cask 'loopback1' do
   sha256 '8be69661a740a4966ad7048b5ab68a1322ae8a373b4a15f23d1fc02ed44e32b5'
 
   url "https://rogueamoeba.com/legacy/downloads/Loopback-#{version.no_dots}.zip"
+  appcast 'https://rogueamoeba.net/ping/versionCheck.cgi?format=sparkle&bundleid=com.rogueamoeba.Loopback&system=10140'
   name 'Loopback'
   homepage 'https://rogueamoeba.com/legacy/'
 
