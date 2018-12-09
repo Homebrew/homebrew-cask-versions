b333004d1ba82bf0a5d3e759b97bde1731dad03c
Jonas Weismueller <jonas.weismueller@gmail.com>
2018-12-09 23:45:50 +0100

    update kodi development to latest Leia_rc2 version (#6692)

--- Casks/kodi-development.rb
+++ Casks/kodi-development.rb
@@ -1,6 +1,6 @@
 cask 'kodi-development' do
-  version '18.0-Leia_rc1'
-  sha256 'cc1f9de0c4261b52846084244a97cdbbdd9940f28d4b8fd026148a274374a6e7'
+  version '18.0-Leia_rc2'
+  sha256 '1fa307e5abb90f49cdce0bae8f061ebab3d867d117aec871ecdde1a809fe9b93'
 
   url "https://mirrors.kodi.tv/releases/osx/x86_64/kodi-#{version}-x86_64.dmg"
   name 'Kodi-Development'
