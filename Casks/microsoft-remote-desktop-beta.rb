78a583d4b26aa97ccefec79819681ad3c6c5987f
Oli Walker <oliwalker@btinternet.com>
2018-12-11 01:21:20 +0000

    Update microsoft-remote-desktop-beta to 10.2.4.1426,245 (#6696)

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-remote-desktop-beta' do
-  version '10.2.4.1420,244'
-  sha256 'ae498c9a24e8ce1d44f845e1472fc919777b05503f38aba881757c042ad7141d'
+  version '10.2.4.1426,245'
+  sha256 '0dd82eb00f48156baed0278afc3314429aec4c74786cce6214b627332b9d236a'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06'
