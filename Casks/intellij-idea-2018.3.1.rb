64e93a3e7762e1db8f5cdec9ff259b6901086877
daejoon <kkode1911@gmail.com>
2018-12-19 15:34:34 +0900

    Update intellij-idea to 2018.3.1

---
+++ Casks/intellij-idea-2018.3.1.rb
@@ -0,0 +1,26 @@
+cask 'intellij-idea-2018.3.1' do
+  version '2018.3.1,183.4588.61'
+  sha256 '8e056c944ab52b18ab0045eb500a007201caae6c16a7f0dc112abec54737c89d'
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.before_comma}.dmg"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release'
+  name 'IntelliJ IDEA Ultimate'
+  homepage 'https://www.jetbrains.com/idea/'
+
+  auto_updates true
+
+  app 'IntelliJ IDEA.app'
+
+  uninstall_postflight do
+    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'idea') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
+  end
+
+  zap trash: [
+               '~/Library/Preferences/com.jetbrains.intellij.plist',
+               "~/Library/Caches/IntelliJIdea#{version.major_minor}",
+               "~/Library/Logs/IntelliJIdea#{version.major_minor}",
+               "~/Library/Application Support/IntelliJIdea#{version.major_minor}",
+               "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+               '~/Library/Saved Application State/com.jetbrains.intellij.savedState',
+             ]
+end
