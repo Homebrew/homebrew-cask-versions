806ef879ea906ee8b310d8f667ae26a923ba3f96
Sebastian Riehm <s.riehm@iwm.rwth-aachend.e>
2019-01-01 13:26:54 +0100

    Update hex-fiend-beta to 2.11.0b4

--- Casks/hex-fiend-beta.rb
+++ Casks/hex-fiend-beta.rb
@@ -1,9 +1,9 @@
 cask 'hex-fiend-beta' do
-  version '2.10b6'
-  sha256 'e07d242d2cf0733f0c548e776a226ab9c4eb943f16c41cc407a3e0a7b903fdb4'
+  version '2.11.0b4'
+  sha256 'ac64e7f0f8d011ee6272a2adc2c65a99780756e75381b1511c9ad94bc0044a22'
 
   # github.com/ridiculousfish/HexFiend was verified as official when first introduced to the cask
-  url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.major_minor}.dmg"
+  url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.sub(%r{\w\d$}, '')}.dmg"
   appcast 'https://github.com/ridiculousfish/HexFiend/releases.atom'
   name 'Hex Fiend'
   homepage 'https://ridiculousfish.com/hexfiend/'
