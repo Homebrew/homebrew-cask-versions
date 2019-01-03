31f6f4a8ecb3e738a3a65569255fec1b30e7803d
Yurii Kolesnykov <root@yurikoles.com>
2019-01-03 22:59:49 +0200

    Update digikam-beta.rb

--- Casks/digikam-beta.rb
+++ Casks/digikam-beta.rb
@@ -1,6 +1,6 @@
 cask 'digikam-beta' do
-  version '6.0.0-beta2'
-  sha256 'e175052a9a4a301a85556975eb327d13703cd35ae44421bed68eec0b4abd13b7'
+  version '5.9.0-01'
+  sha256 'd3b76fcabfbd281289702d2c3a1f6ab63cda3b8f45cc54e642fee329cb0f197c  '
 
   # download.kde.org/unstable/digikam was verified as official when first introduced to the cask
   url "https://download.kde.org/unstable/digikam/digiKam-#{version}-MacOS-x86-64.pkg"
