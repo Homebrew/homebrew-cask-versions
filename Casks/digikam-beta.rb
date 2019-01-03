e1246dea0986be6536b2acf58bc2e569e31c441a
Yurii Kolesnykov <root@yurikoles.com>
2019-01-03 23:06:44 +0200

    fix url

--- Casks/digikam-beta.rb
+++ Casks/digikam-beta.rb
@@ -3,7 +3,7 @@ cask 'digikam-beta' do
   sha256 'd3b76fcabfbd281289702d2c3a1f6ab63cda3b8f45cc54e642fee329cb0f197c  '
 
   # download.kde.org/unstable/digikam was verified as official when first introduced to the cask
-  url "https://download.kde.org/unstable/digikam/digiKam-#{version}-MacOS-x86-64.pkg"
+  url "https://download.kde.org/stable/digikam/digiKam-#{version}-MacOS-x86-64.pkg"
   appcast 'https://files.kde.org/digikam/'
   name 'digiKam'
   homepage 'https://www.digikam.org/'
