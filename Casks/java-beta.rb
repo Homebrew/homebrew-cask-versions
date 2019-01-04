284a62a5f0b75320c0374048477609636b47ce7b
makigumo <makigumo@users.noreply.github.com>
2019-01-04 21:16:04 +0100

    Update java-beta to 12,26 (#6801)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,6 +1,6 @@
 cask 'java-beta' do
-  version '12,25'
-  sha256 '1cee48805344d8686312157304a927cdf543e5fbdef2d9e10cceffe2f76cbe4e'
+  version '12,26'
+  sha256 '9bb23522de0bcaf7c98be1d17b31f03362f2361c88e8e3248c52313a33332068'
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK - Early Access'
