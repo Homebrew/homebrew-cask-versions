35d5c1804a8bb2f6b1c3ac1978bff7f371db269b
Yurii Kolesnykov <root@yurikoles.com>
2019-01-09 01:28:54 +0200

    Update digikam-beta to 6.0.0-beta3,20181228T124823 (#6804)

--- Casks/digikam-beta.rb
+++ Casks/digikam-beta.rb
@@ -1,16 +1,16 @@
 cask 'digikam-beta' do
-  version '6.0.0-beta2'
-  sha256 'e175052a9a4a301a85556975eb327d13703cd35ae44421bed68eec0b4abd13b7'
+  version '6.0.0-beta3,20181228T124823'
+  sha256 '4852602061b96368254431aaf0424e6cf5e8d5f00d8b77eca3953d18e18d3cd7'
 
-  # download.kde.org/unstable/digikam was verified as official when first introduced to the cask
-  url "https://download.kde.org/unstable/digikam/digiKam-#{version}-MacOS-x86-64.pkg"
+  # files.kde.org/digikam was verified as official when first introduced to the cask
+  url "https://files.kde.org/digikam/digiKam-#{version.before_comma}-#{version.after_comma}-MacOS-x86-64.pkg"
   appcast 'https://files.kde.org/digikam/'
   name 'digiKam'
   homepage 'https://www.digikam.org/'
 
   conflicts_with cask: 'digikam'
 
-  pkg "digikam-#{version}-MacOS-x86-64.pkg"
+  pkg "digiKam-#{version.before_comma}-#{version.after_comma}-MacOS-x86-64.pkg"
 
   uninstall pkgutil: 'org.freedesktop.digikam',
             delete:  '/Applications/digiKam/'
