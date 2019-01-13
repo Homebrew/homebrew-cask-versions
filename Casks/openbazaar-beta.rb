646fa901ab2b56f5f7860af2ec59d9f26f05d43c
Victor Popkov <victor@popkov.me>
2019-01-13 21:11:06 +0200

    Update openbazaar-beta to 2.3.0-rc9 (#6841)

--- Casks/openbazaar-beta.rb
+++ Casks/openbazaar-beta.rb
@@ -1,6 +1,6 @@
 cask 'openbazaar-beta' do
-  version '2.3.0-rc3'
-  sha256 '639ce1573e5991900a94b74be437657ff61e7bd5bb3698c0f964722ca99b3525'
+  version '2.3.0-rc9'
+  sha256 '078f0458ce2107b0e54297f8d0b9c55c521974ff5dd50fddd18cd630a1617cdf'
 
   # github.com/OpenBazaar/openbazaar-desktop was verified as official when first introduced to the cask
   url "https://github.com/OpenBazaar/openbazaar-desktop/releases/download/v#{version}/OpenBazaar#{version.major}-#{version.major_minor_patch}.dmg"
