b0ebc06402e3ab831333c9219d493f38ddeae0a8
Vítor Galvão <info@vitorgalvao.com>
2019-01-19 19:00:20 +0000

    Update pext-nightly.rb

--- Casks/pext-nightly.rb
+++ Casks/pext-nightly.rb
@@ -5,25 +5,21 @@ cask 'pext-nightly' do
   # github.com/Pext/Pext was verified as official when first introduced to the cask
   url do
     require 'open-uri'
+    require 'json'
     base_url = 'https://github.com/Pext/Pext/releases/continuous/'
-    versions = URI(base_url.to_s)
-               .open
-               .read
-               .scan(%r{href="/.*/Pext\-((?:\d+\.?)+\.\d+\.\w+)\.dmg"})
-               .flatten
+    latest_version = URI(base_url.to_s)
+                       .open
+                       .read
+                       .scan(%r{href="/.*/Pext\-((?:\d+\.?)+\.\d+\.\w+)\.dmg"})
+                       .flatten
+                       .last
 
-    latest_version = versions.last
-
-    file = "Pext-#{latest_version}.dmg"
-
-    "https://github.com/Pext/Pext/releases/download/continuous/#{file}"
+    "https://github.com/Pext/Pext/releases/download/continuous/Pext-#{latest_version}.dmg"
   end
   name 'Pext'
   homepage 'https://pext.io/'
 
-  conflicts_with cask: [
-                         'pext',
-                       ]
+  conflicts_with cask: 'pext'
 
   app 'Pext.app'
 end
