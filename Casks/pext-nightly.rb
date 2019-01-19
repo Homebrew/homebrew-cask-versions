521edce5074013b13f5999183acf01ea63cd3873
Sylvia van Os <sylvia@hackerchick.me>
2019-01-19 23:03:49 +0100

    Add Pext nightly (#6867)
    * Add Pext nightly
    
    * Update pext-nightly.rb
    
    * Using the API is less error prone than parsing the page
    
    * Update pext-nightly.rb
    
    * Update pext-nightly.rb
    
    * Update pext-nightly.rb
    
    * Update pext-nightly.rb
    
    * Update pext-nightly.rb
    
    * Update pext-nightly.rb
    
    * Update pext-nightly.rb

---
+++ Casks/pext-nightly.rb
@@ -0,0 +1,19 @@
+cask 'pext-nightly' do
+  version :latest
+  sha256 :no_check
+
+  # github.com/Pext/Pext was verified as official when first introduced to the cask
+  url do
+    require 'open-uri'
+    require 'json'
+
+    JSON.parse(open('https://api.github.com/repos/Pext/Pext/releases/tags/continuous').read)['assets']
+        .find { |r| r['browser_download_url'] =~ %r{/.*\.dmg} }['browser_download_url']
+  end
+  name 'Pext'
+  homepage 'https://pext.io/'
+
+  conflicts_with cask: 'pext'
+
+  app 'Pext.app'
+end
