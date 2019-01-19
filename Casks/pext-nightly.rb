1997d83c425eea749c9b99a44b7d6fc98ebfb7dd
Sylvia van Os <sylvia@hackerchick.me>
2019-01-19 22:40:14 +0100

    Update pext-nightly.rb

--- Casks/pext-nightly.rb
+++ Casks/pext-nightly.rb
@@ -7,9 +7,8 @@ cask 'pext-nightly' do
     require 'open-uri'
     require 'json'
 
-    JSON.parse(open('https://api.github.com/repos/Pext/Pext/releases', 'User-Agent' => 'Homebrew').read)
-        .select { |r| r['prerelease'] }[0]['assets']
-        .select { |k| k['browser_download_url'] =~ %r{/.*dmg/} }[0]['browser_download_url']
+    JSON.parse(open('https://api.github.com/repos/Pext/Pext/releases/tags/continuous').read)['assets']
+         .find { |r| r['browser_download_url'] =~ %r{/.*\.dmg} }['browser_download_url']
   end
   name 'Pext'
   homepage 'https://pext.io/'
