4352d1d75eb330fb918b747fef58bd8609be20a7
Vítor Galvão <info@vitorgalvao.com>
2019-01-30 15:36:24 +0000

    Update openscad-snapshot.rb

--- Casks/openscad-snapshot.rb
+++ Casks/openscad-snapshot.rb
@@ -2,7 +2,7 @@ cask 'openscad-snapshot' do
   version '2018.11.25'
   sha256 '128cd274dfc4b041f1ecf3d0bc911e8721713e3be2f353ee6b1f6994385cdd66'
 
-  url "http://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
+  url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
   appcast 'https://www.openscad.org/inc/mac_snapshot_links.js'
   name 'OpenSCAD'
   homepage 'https://www.openscad.org/downloads.html#snapshots'
