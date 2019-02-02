910ccc108f5b265ab28940403c19a70d610e5188
Duane J. May <duane.may@cengage.com>
2019-02-02 01:15:57 -0500

    Update zulu7 to 7.0.211,7.27.0.1-ca

--- Casks/zulu7.rb
+++ Casks/zulu7.rb
@@ -1,6 +1,6 @@
 cask 'zulu7' do
-  version '7.0.201,7.25.0.5'
-  sha256 '8eca975d0515cd21c0859b2de9aad255638842fadc9747c3b8a589101a7eb985'
+  version '7.0.211,7.27.0.1-ca'
+  sha256 '284669d0dbc2949384861e7cec9d1bf888cc7f2a47a814c2c55489db949d823c'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
