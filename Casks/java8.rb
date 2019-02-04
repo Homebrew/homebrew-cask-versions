01b8a797e6b024b6134ec20f9bfad7f133525236
Enrico <enrico204@gmail.com>
2019-02-04 17:42:19 +0100

    Add Oracle JDK 8 update 202 (#6934)
    * Add Oracle JDK 8 update 202
    
    * Replace java8.rb instead of creating a new cask

--- Casks/java8.rb
+++ Casks/java8.rb
@@ -1,12 +1,43 @@
 cask 'java8' do
-  version '8.202.03,07_nov_2018'
-  sha256 'a5c312fbe58add9b124920d799f2e61a3608bd9b7872d67a76b279a845447975'
+  version '8u202,b08:1961070e4c9b4e26a04e7f5a083f551e'
+  sha256 'b41367948cf99ca0b8d1571f116b7e3e322dd1ebdfd4d390e959164d75b97c20'
 
-  url "https://download.java.net/java/early_access/jdk#{version.major}/b#{version.patch}/BCL/jdk-#{version.major}u#{version.minor}-ea-bin-b03-macosx-x86_64-#{version.after_comma}.dmg"
-  name 'OpenJDK'
-  homepage 'https://jdk.java.net/'
+  url "https://download.oracle.com/otn-pub/java/jdk/#{version.before_comma}-#{version.after_comma.before_colon}/#{version.after_colon}/jdk-#{version.before_comma}-macosx-x64.dmg",
+      cookies: {
+                 'oraclelicense' => 'accept-securebackup-cookie',
+               }
+  name 'Java 8 Standard Edition Development Kit'
+  homepage 'https://www.oracle.com/technetwork/java/javase/overview/index.html'
 
-  pkg "JDK #{version.major} Update #{version.minor}.pkg"
+  # auto_updates true: JDK does not auto-update
+  depends_on macos: '>= :yosemite'
 
-  uninstall pkgutil: "com.oracle.jdk#{version.major}u#{version.minor}"
+  pkg 'JDK 8 Update 202.pkg'
+
+  postflight do
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home", '/Library/Java/Home'],
+                   sudo: true
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/MacOS", '/Library/Java/MacOS'],
+                   sudo: true
+    system_command '/bin/mkdir',
+                   args: ['-p', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries"],
+                   sudo: true
+    system_command '/bin/ln',
+                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/lib/server/libjvm.dylib", "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents/Home/bundle/Libraries/libserver.dylib"],
+                   sudo: true
+  end
+
+  uninstall pkgutil: "com.oracle.jdk#{version.before_comma}",
+            delete:  [
+                       "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk/Contents",
+                       '/Library/Java/Home',
+                       '/Library/Java/MacOS',
+                     ],
+            rmdir:   "/Library/Java/JavaVirtualMachines/jdk-#{version.before_comma}.jdk"
+
+  caveats do
+    license 'https://www.oracle.com/technetwork/java/javase/terms/license/javase-license.html'
+  end
 end
