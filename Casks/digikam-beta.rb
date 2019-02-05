84a34a80e7f5b2938ed108fca8b6ea815978329a
Chun-Tang Wang <c.tang.wang@gmail.com>
2019-02-05 18:11:40 +0800

    Update digikam-beta url to pre-releases. (#6938)

--- Casks/digikam-beta.rb
+++ Casks/digikam-beta.rb
@@ -2,9 +2,9 @@ cask 'digikam-beta' do
   version '6.0.0-beta3,20181228T124823'
   sha256 '4852602061b96368254431aaf0424e6cf5e8d5f00d8b77eca3953d18e18d3cd7'
 
-  # files.kde.org/digikam was verified as official when first introduced to the cask
-  url "https://files.kde.org/digikam/digiKam-#{version.before_comma}-#{version.after_comma}-MacOS-x86-64.pkg"
-  appcast 'https://files.kde.org/digikam/'
+  # download.kde.org/unstable/digikam was verified as official when first introduced to the cask
+  url "https://download.kde.org/unstable/digikam/digiKam-#{version.before_comma}-#{version.after_comma}-MacOS-x86-64.pkg"
+  appcast "https://download.kde.org/unstable/digikam/digiKam-#{version.before_comma}-#{version.after_comma}-MacOS-x86-64.pkg.mirrorlist"
   name 'digiKam'
   homepage 'https://www.digikam.org/'
 
