5c8ff2a6acc5556609f691da480769d82a84e683
Markus Reiter <me@reitermark.us>
2019-02-06 21:14:13 +0100

    Remove workarounds in Java casks.

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -4,30 +4,12 @@ cask 'zulu8' do
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
-  name 'Azul Zulu Java Standard Edition Development Kit'
+  name 'Azul Zulu Java 8 Standard Edition Development Kit'
   homepage 'https://www.azul.com/downloads/zulu/zulu-mac/'
+
+  depends_on macos: '>= :yosemite'
 
   pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
-  postflight do
-    system_command '/bin/mv',
-                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk"],
-                   sudo: true
-    system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk"],
-                   sudo: true
-    system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk/Contents/Home", '/Library/Java/Home'],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk/Contents/Info.plist"],
-                   sudo: true
-  end
-
-  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}",
-            delete:  [
-                       "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}.jdk",
-                       "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk",
-                       '/Library/Java/Home',
-                     ]
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 end
