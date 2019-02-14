30115203a77b4ce281844448c54202d9796cde09
Michael hurni <michael.hurni@gmail.com>
2019-02-14 07:47:26 +0100

    AppCode EAP 2019.1 (#6972)
    * AppCode EAP 2019.1
    
    * Update appcode-eap.rb

---
+++ Casks/appcode-eap.rb
@@ -0,0 +1,19 @@
+cask 'appcode-eap' do
+  version '191.5109.16'
+  sha256 'e1eb95a61b0e131241fb69cd145ee30ef40d25b8b8174ce3797b6c8e920436da'
+
+  url "https://download.jetbrains.com/objc/AppCode-#{version}.dmg"
+  name 'AppCode EAP'
+  homepage 'https://www.jetbrains.com/objc/nextversion/'
+
+  conflicts_with cask: 'appcode'
+
+  app 'AppCode 2019.1 EAP.app'
+
+  zap delete: [
+                '~/Library/Preferences/AppCode2019.1',
+                '~/Library/Application Support/AppCode2019.1',
+                '~/Library/Caches/AppCode2019.1',
+                '~/Library/Logs/AppCode2019.1',
+              ]
+end
