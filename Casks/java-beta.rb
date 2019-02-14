9d4b75937531dc07b86d7a78754a845e6f11ac9a
makigumo <makigumo@users.noreply.github.com>
2019-02-14 07:59:46 +0100

    java-beta: fix url

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -2,7 +2,7 @@ cask 'java-beta' do
   version '12,31'
   sha256 '026992c066e5b909f3987e9f3e01a9be51ac12a320cceb471f4d48e6c743ac11'
 
-  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
+  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}+#{version.after_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK Early Access Java Development Kit'
   homepage 'https://jdk.java.net/'
 
