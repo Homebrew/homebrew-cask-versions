9998adbc1bc9c355d3e80e998457d69e98892e41
Duane May <duanemay@users.noreply.github.com>
2019-02-15 02:16:44 -0500

    Remove workarounds in Zulu 10 (#6991)
    * Remove workarounds in Zulu 10
    
    * Update zulu10.rb

--- Casks/zulu10.rb
+++ Casks/zulu10.rb
@@ -9,25 +9,6 @@ cask 'zulu10' do
 
   pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
-  postflight do
-    system_command '/bin/mv',
-                   args: ['-f', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}+#{version.after_comma.before_colon}.jdk"],
-                   sudo: true
-    system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}+#{version.after_comma.before_colon}.jdk", "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk"],
-                   sudo: true
-    system_command '/bin/ln',
-                   args: ['-nsf', '--', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Home", '/Library/Java/Home'],
-                   sudo: true
-    system_command '/usr/libexec/PlistBuddy',
-                   args: ['-c', 'Add :JavaVM:JVMCapabilities: string JNI', "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk/Contents/Info.plist"],
-                   sudo: true
-  end
-
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}",
-            delete:  [
-                       "/Library/Java/JavaVirtualMachines/zulu-#{version.before_comma}+#{version.after_comma.before_colon}.jdk",
-                       "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk",
-                       '/Library/Java/Home',
-                     ]
+            rmdir:   '/Library/Java/JavaVirtualMachines'
 end
