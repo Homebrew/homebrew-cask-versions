b5292d7b2070b24de53f00f247e01f39e06cffb8
Shein Htike <chocolateshein.htike@gmail.com>
2019-02-17 09:26:27 -0500

    Update java-beta to 12,32 (#6996)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,8 +1,8 @@
 cask 'java-beta' do
-  version '12,31'
-  sha256 '026992c066e5b909f3987e9f3e01a9be51ac12a320cceb471f4d48e6c743ac11'
+  version '12,32'
+  sha256 '1f9f548ddedf9ab4b954eb445480e71e991bc6634f02af740bea5775b87768b3'
 
-  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}+#{version.after_comma}_osx-x64_bin.tar.gz"
+  url "https://download.java.net/java/GA/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK Early Access Java Development Kit'
   homepage 'https://jdk.java.net/'
 
