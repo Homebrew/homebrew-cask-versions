ebb253738c96236d9aec9fa44fd9ae1887438cdc
Denis Nikulin <denisnikulin@gmail.com>
2019-02-18 15:44:09 +0000

    Update telegram-alpha to 4.9.5-159604,1862 (#6999)
    Squashed and auto-merged via `brew cask automerge`.

--- Casks/telegram-alpha.rb
+++ Casks/telegram-alpha.rb
@@ -1,6 +1,6 @@
 cask 'telegram-alpha' do
-  version '4.9.5-159421,1860'
-  sha256 '8270b7ba12ceda1c7077ba4cc87612520e07dc184338430eaa1b28ae5026afdd'
+  version '4.9.5-159604,1862'
+  sha256 '63eacf42724f325935611d3fe597dbec446cebfbd0f7fc689d07e6b4906845f3'
 
   # hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f/app_versions/#{version.after_comma}?format=zip"
