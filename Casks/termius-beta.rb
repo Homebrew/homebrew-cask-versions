d0c1d4454b2545c3b211339593304b5566afd0c2
Alejandro Leal <ajleal@msn.com>
2019-02-26 11:17:09 -0500

    termius-beta.rb (#7025)
    * Create termius-beta.rb
    
    * Update termius-beta.rb

---
+++ Casks/termius-beta.rb
@@ -0,0 +1,20 @@
+cask 'termius-beta' do
+  version '4.1.4'
+  sha256 '9bb825f4be9067c30df311e9ab9c2c2fda1b208e1b44637db95506fc3497b056'
+
+  # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
+  url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac-beta/Termius+Beta.dmg'
+  appcast 'https://www.termius.com/mac-os'
+  name 'Termius Beta'
+  homepage 'https://www.termius.com/beta-program'
+
+  app 'Termius Beta.app'
+
+  zap trash: [
+               '~/.termius',
+               '~/Library/Application Support/Termius Beta',
+               '~/Library/Saved Application State/com.termius-beta-dmg.mac.savedState',
+               '/Library/Preferences/com.termius-beta-dmg.mac.plist',
+               '~/Library/Logs/Termius Beta',
+             ]
+end
