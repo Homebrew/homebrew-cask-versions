835a129633a19ed29d9ed2396b3e17610d0ca119
Vítor Galvão <info@vitorgalvao.com>
2019-02-26 04:27:17 +0000

    Update psi-plus.rb

--- Casks/psi-plus.rb
+++ Casks/psi-plus.rb
@@ -1,9 +1,9 @@
 cask 'psi-plus' do
-  version '1.4.567-macOS10.12'
+  version '1.4.567'
   sha256 '28c2e46dde32f02ea05636fa5af804e51c39756ee6cb5005f8a77666ec75cd44'
 
   # downloads.sourceforge.net/ was verified as official when first introduced to the cask
-  url "https://downloads.sourceforge.net/Psi+-#{version}-x86_64.dmg"
+  url "https://downloads.sourceforge.net/Psi+-#{version}-macOS10.12-x86_64.dmg"
   appcast 'https://sourceforge.net/projects/psiplus/rss'
   name 'Psi+'
   homepage 'https://psi-plus.com/'
