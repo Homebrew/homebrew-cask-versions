321af8bf012209db5a9789d63616cf1118b9d076
thirdtapbot <44350140+thirdtapbot@users.noreply.github.com>
2019-03-10 18:20:43 +0000

    Casks/affinity-designer-beta.rb: Updated to 1.7.0.6 (#7068)

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-designer-beta' do
-  version '1.7.0.5'
-  sha256 '416f3c638bbf09f85a3b632599e4e2e32100cdf387835b2dfb9dfdd720f3e3fc'
+  version '1.7.0.6'
+  sha256 'b4d4a12bdb90f6e256a40ae735ff9aa2029d733ce6f87c0c4f4a6a698f45584c'
 
   # amazonaws.com/affinity-beta was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/affinity-beta/download/Affinity%20Designer%20Beta.dmg'
