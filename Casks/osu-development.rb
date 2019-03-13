9ebf2847b7100f09f4a1aacac1fa62897fff70ec
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2019-03-13 23:54:47 +0100

    Adding osu!lazer (#7067)
    * Adding osu!lazer
    
    Adding the new development build of osu!lazer
    
    * Update and rename osu-lazer.rb to osu.rb
    
    * Update and rename osu.rb to osu-development.rb
    
    * Update osu-development.rb

---
+++ Casks/osu-development.rb
@@ -0,0 +1,20 @@
+cask 'osu-development' do
+  version '2019.308.0'
+  sha256 '15d1e456081643758a76d7a3651fc4a3868b2defd17aed7b69602543b7dafad7'
+
+  url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
+  appcast 'https://github.com/ppy/osu/releases.atom'
+  name 'osu!lazer'
+  homepage 'https://github.com/ppy/osu/'
+
+  auto_updates true
+  conflicts_with cask: 'osu'
+  depends_on macos: '>= :sierra'
+
+  app 'osu!.app'
+
+  zap trash: [
+               '~/.local/share/osu',
+               '~/Library/Saved Application State/sh.ppy.osu.lazer.savedState',
+             ]
+end
