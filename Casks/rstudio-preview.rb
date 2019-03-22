54341e82fe87853a09589ea02f017e7b6c54b107
Yihui Xie <xie@yihui.name>
2019-03-22 12:46:54 -0500

    Update rstudio-preview to 1.2.1330

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,5 +1,5 @@
 cask 'rstudio-preview' do
-  version '1.2.1303'
+  version '1.2.1330'
   sha256 '20ffc81ed0f84819306cd7dce26d31a22e350053faa81472159bdea28b261c51'
 
   # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
