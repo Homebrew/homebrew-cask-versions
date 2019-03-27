2b916492b41d1c088b00c1bbbc77a96832e01bcb
Yurii Kolesnykov <root@yurikoles.com>
2019-03-27 21:29:22 +0200

    Update qt-creator-dev to 4.9.0-rc1

--- Casks/qt-creator-dev.rb
+++ Casks/qt-creator-dev.rb
@@ -1,8 +1,8 @@
 cask 'qt-creator-dev' do
-  version '4.8.0'
-  sha256 'dbbcefe1233cb29f0d20565273b6e5262e60190bcddcc55ed5be97ff57284b7a'
+  version '4.9.0-rc1'
+  sha256 'ffda2d1f05dbf6d4f58524f115309158e7a528b30bf3669662f207364769e491'
 
-  url "https://download.qt.io/official_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
+  url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator Dev'
   homepage 'https://www1.qt.io/developers/'
 
