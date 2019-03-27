9ed095e7a192e007bff3ee3e156d2d87f0750831
rpaulucci3 <rafaelpaulucci2016@gmail.com>
2019-03-27 00:57:31 -0400

    Add mono-mdk-visual-studio (5.16)

---
+++ Casks/mono-mdk-visual-studio.rb
@@ -0,0 +1,33 @@
+cask 'mono-mdk-visual-studio' do
+  version '5.16.1.0'
+  sha256 '0007b2a606afd3a523f66a8f6f2ae8c4c49c247f93a9fb6325c45a5bcc1b3307'
+
+  url "https://download.mono-project.com/archive/#{version.major_minor_patch}/macos-10-universal/MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
+  appcast 'https://www.mono-project.com/download/vs/'
+  name 'Mono'
+  homepage 'https://www.mono-project.com/'
+
+  pkg "MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
+
+  uninstall delete:  [
+                       "/Library/Frameworks/Mono.framework/Versions/#{version.major_minor_patch}",
+                       '/private/etc/paths.d/mono-commands',
+                     ],
+            pkgutil: 'com.xamarin.mono-*',
+            rmdir:   [
+                       '/Library/Frameworks/Mono.framework/Versions',
+                       '/Library/Frameworks/Mono.framework',
+                     ]
+
+  caveats <<~EOS
+    This is a version specific for Visual Studio users.
+
+    Installing #{token} removes mono and mono dependant formula binaries in
+    /usr/local/bin and adds #{token} to /private/etc/paths.d/
+    You may want to:
+
+      brew unlink {formula} && brew link {formula}
+
+    and/or remove /private/etc/paths.d/mono-commands
+  EOS
+end
