65ed9aa0f08c458426479db877c6e609d8c9cbef
Stu Bevan <stu@mees.st>
2019-04-14 09:52:48 +0100

    TinderBox 7.app v7.5.6

---
+++ Casks/tinderbox7.rb
@@ -0,0 +1,11 @@
+cask 'tinderbox7' do
+  version '7.5.6'
+  sha256 'b057489d884f0dd7ff2ea09904ecb1964fd317d53f88935cbac79b017f3e2223'
+
+  url 'https://www.eastgate.com/download/tbx756.dmg'
+  appcast 'https://eastgate.com/Tinderbox/download.html'
+  name 'Tinderbox 7'
+  homepage 'https://eastgate.com/Tinderbox/'
+
+  app 'Tinderbox 7.app'
+end
