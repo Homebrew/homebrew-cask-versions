faeabd95b7f959168c2d4e54539d413f9f6f899a
Claudia <claui@users.noreply.github.com>
2019-04-16 12:41:48 +0200

    Update airmail-beta to 3.7.0.544,389

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 cask 'airmail-beta' do
-  version '3.7.0.543,388'
-  sha256 '0d08a06011e0c34e31f25be007350c087011c6f26fc7b25e20e90346ec517167'
+  version '3.7.0.544,389'
+  sha256 'c728f9ab2ce9f694f17ee6f74b0033c3dfde68e82742463da3268c73f6b2f5ad'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
