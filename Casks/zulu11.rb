c6b9ff18e940aae7c5194fc6ea032d5854d6168e
Joe Hegarty <joeh@ea.com>
2019-04-18 13:51:24 -0700

    Upgrade zulu11 to 11.0.2 and zulu8 to 8.0.212

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -1,6 +1,6 @@
 cask 'zulu11' do
-  version '11.29.11,11.0.2'
-  sha256 '712e97c0dc96e86a08a325666132715561b5a11ce8c17a39fadba28c31c8429c'
+  version '11.31.11,11.0.3'
+  sha256 '738d36bb51747780f9f43f484e18bb411d350edf88cccc175a8b6335f4f22182'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
