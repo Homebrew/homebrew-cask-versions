a15647edee8b53d82340c114c9e78d1dfe8d8d4f
makigumo <makigumo@users.noreply.github.com>
2019-04-19 16:15:58 +0200

    update java-beta to 13,17 (#7266)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,6 +1,6 @@
 cask 'java-beta' do
-  version '13,15'
-  sha256 '83552082d062d297be6fac630a1989df94f536b4f4b8a10fbfac8ade67115130'
+  version '13,17'
+  sha256 '6d3bdb14879516248c8eab3ca0d0be9e7d610311b37c754706e8f241cb057b96'
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK Early Access Java Development Kit'
