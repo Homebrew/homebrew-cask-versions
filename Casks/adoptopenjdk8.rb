88ddc47e527241d538be3dcf269801bade9617bc
Vítor Galvão <info@vitorgalvao.com>
2019-04-20 20:09:22 +0100

    adoptopenjdk8.rb: remove uninstall pkgutil array (#7271)

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -10,7 +10,5 @@ cask 'adoptopenjdk8' do
 
   pkg "OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
 
-  uninstall pkgutil: [
-                       "net.adoptopenjdk.#{version.before_comma}.jdk",
-                     ]
+  uninstall pkgutil: "net.adoptopenjdk.#{version.before_comma}.jdk"
 end
