376b21ec0dd1fb0f3060b54cfbfd6b782b0448fd
Vítor Galvão <info@vitorgalvao.com>
2019-04-27 23:29:24 +0100

    Update chirp-daily.rb

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -2,7 +2,7 @@ cask 'chirp-daily' do
   version '20190424'
   sha256 '1129d18bfe69c9db9f0937c53002f3b30544fa7fe8fe16cfd719b8e43467a7a3'
 
-  url "https://trac.chirp.danplanet.com/chirp_daily/daily-#{version}/chirp-daily-#{version}.app.zip"
+  url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   name 'CHIRP'
   homepage 'https://chirp.danplanet.com/projects/chirp/wiki/Home'
 
