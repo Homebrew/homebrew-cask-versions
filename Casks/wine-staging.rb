866a209d6e236de435116034e97b66578b4a7581
rpaulucci3 <rafaelpaulucci2016@gmail.com>
2019-04-28 02:15:26 -0400

    Update wine-staging from 4.6 to 4.7

--- Casks/wine-staging.rb
+++ Casks/wine-staging.rb
@@ -1,6 +1,6 @@
 cask 'wine-staging' do
-  version '4.6'
-  sha256 '0c64e3b960e0613c56f4667202bdd3ba8640119271b43e139a44ccb90d0c5342'
+  version '4.7'
+  sha256 'd2e537af4c553a6727b109000617a83041f9cf4b0286a41dccf70911f6e7a7f7'
 
   # dl.winehq.org/wine-builds/macosx was verified as official when first introduced to the cask
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-staging-#{version}.pkg"
