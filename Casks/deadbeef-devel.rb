d1a28238555b3b3d94d7747753e0caaa2b72563b
Viktor Szakats <commit@vszakats.net>
2019-05-19 07:52:45 +0000

    Secure URLs

--- Casks/deadbeef-devel.rb
+++ Casks/deadbeef-devel.rb
@@ -2,9 +2,10 @@ cask 'deadbeef-devel' do
   version :latest
   sha256 :no_check
 
+  # downloads.sourceforge.net/deadbeef was verified as official when first introduced to the cask
   url 'https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-osx-x86_64.zip'
   name 'DeaDBeeF'
-  homepage 'http://deadbeef.sourceforge.net/'
+  homepage 'https://deadbeef.sourceforge.io/'
 
   app 'deadbeef.app'
 
