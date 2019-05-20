363874e835dda7d27f78b587ddbd09251d4ff392
Vítor Galvão <info@vitorgalvao.com>
2019-05-20 23:29:16 +0100

    Update microsoft-edge-canary.rb

--- Casks/microsoft-edge-canary.rb
+++ Casks/microsoft-edge-canary.rb
@@ -3,11 +3,11 @@ cask 'microsoft-edge-canary' do
   sha256 'e56952a821f1a2d88e4243c2e02ac42e00eac0ceaa66f80f68feec7007d8a0c1'
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
-  url 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeCanary-76.0.161.0.pkg'
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeCanary-#{version}.pkg"
   name 'Microsoft Edge Canary'
   homepage 'https://www.microsoftedgeinsider.com/'
 
-  pkg 'MicrosoftEdgeCanary-76.0.161.0.pkg'
+  pkg "MicrosoftEdgeCanary-#{version}.pkg"
 
   uninstall pkgutil: 'com.microsoft.Edge.Canary'
 end
