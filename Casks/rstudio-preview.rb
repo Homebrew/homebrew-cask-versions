53924bd7cb6be5281f510d8b42bf4947a05dc9e6
Kenneth Hung <hung3k@gmail.com>
2019-05-20 16:45:24 -0700

    Update rstudio-preview from 1.2.1511 to 1.2.1522

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.2.1511'
-  sha256 'ea52e682245f871361454b2fe6b2be4922b6391ef3d9b69165bebfd0403fa842'
+  version '1.2.1522'
+  sha256 '5f998412b3718abcb97dfb824c8f1ebeddb2207cbb55aa2d3c61cb7cc2682dc3'
 
   # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
