52f0de2097b0f62c06422eed9a0afb1f8d37c7df
thirdtapbot <44350140+thirdtapbot@users.noreply.github.com>
2019-05-23 09:37:42 +0100

    affinity-photo-beta: Updated to 1.7.0.128 (#7394)

--- Casks/affinity-photo-beta.rb
+++ Casks/affinity-photo-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-photo-beta' do
-  version '1.7.0.127'
-  sha256 '0b897892593449aaf8cc819ad1aeb4a13ab2239e3e45424a99a73af7fc4937ee'
+  version '1.7.0.128'
+  sha256 '68dac8f44aed45b483acef82a803f6aa513431ad5616d1162c3cbb9df6a80fff'
 
   # affinity-beta.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Photo%20Customer%20Beta.dmg'
