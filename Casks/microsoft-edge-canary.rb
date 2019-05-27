3321d9d307db01aa9170afd7e08bfc0653553385
Vítor Galvão <info@vitorgalvao.com>
2019-05-27 01:37:39 +0100

    Update microsoft-edge-canary.rb

--- Casks/microsoft-edge-canary.rb
+++ Casks/microsoft-edge-canary.rb
@@ -3,7 +3,7 @@ cask 'microsoft-edge-canary' do
     def self.file_version
       require 'open-uri'
       appcast = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDCN01-chk.xml'
-      appcast_version = URI(appcast.to_s).open.read.scan(%r{<string>(.*)<\/string>})[2][0].split('.')
+      appcast_version = URI(appcast).read.scan(%r{<string>(.*)<\/string>})[2].join.split('.')
       appcast_version[0] + '.0.' + appcast_version[1] + '.0'
     end
   end
@@ -12,16 +12,11 @@ cask 'microsoft-edge-canary' do
   sha256 :no_check
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
-  url do
-    require 'open-uri'
-    base_url = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/'
-    file = "MicrosoftEdgeCanary-#{Utils.file_version}.pkg"
-    "#{base_url}#{file}"
-  end
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeCanary-#{version}.pkg"
   name 'Microsoft Edge Canary'
   homepage 'https://www.microsoftedgeinsider.com/'
 
-  pkg "MicrosoftEdgeCanary-#{Utils.file_version}.pkg"
+  pkg "MicrosoftEdgeCanary-#{version}.pkg"
 
   uninstall pkgutil: 'com.microsoft.Edge.Canary'
 end
