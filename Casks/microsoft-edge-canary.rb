86d3c75f44e264e9bc82526e7ac8d69def3d2504
Lyon J Till <lyon.till@rackspace.com>
2019-05-29 22:11:10 +0000

    Add Microsoft Edge Canary v76.0.161.0 (#7379)
    * Add Microsoft Edge Canary v76.0.161.0
    
    * Update microsoft-edge-canary.rb
    
    * Added auto_updates block
    
    * Removed trailing whitespace
    
    * Update microsoft-edge-canary.rb
    
    * Update microsoft-edge-canary.rb

---
+++ Casks/microsoft-edge-canary.rb
@@ -0,0 +1,22 @@
+cask 'microsoft-edge-canary' do
+  module Utils
+    def self.file_version
+      require 'open-uri'
+      appcast = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDCN01-chk.xml'
+      appcast_version = URI(appcast).read.scan(%r{<string>(.*)<\/string>})[2].join.split('.')
+      appcast_version[0] + '.0.' + appcast_version[1] + '.0'
+    end
+  end
+
+  version Utils.file_version.to_s
+  sha256 :no_check
+
+  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeCanary-#{version}.pkg"
+  name 'Microsoft Edge Canary'
+  homepage 'https://www.microsoftedgeinsider.com/'
+
+  pkg "MicrosoftEdgeCanary-#{version}.pkg"
+
+  uninstall pkgutil: 'com.microsoft.Edge.Canary'
+end
