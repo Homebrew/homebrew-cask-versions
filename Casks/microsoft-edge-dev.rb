977e47106394059e1f755901570d4af73edfc9f4
Corey Gouker <corey@coreygo.com>
2019-06-05 12:10:08 -0700

    Add Microsoft Edge Dev v76.0.176.1
    <!-- If there’s a checkbox you can’t complete for any reason, that's okay, just explain in detail why you weren’t able to do so. -->
    
    After making all changes to the cask:
    
    - [x] `brew cask audit --download {{cask_file}}` is error-free.
    - [x] `brew cask style --fix {{cask_file}}` reports no offenses.
    - [x] The commit message includes the cask’s name and version.
    - [x] The submission is for [a stable version](https://github.com/Homebrew/homebrew-cask/blob/master/doc/development/adding_a_cask.md#finding-a-home-for-your-cask) or [documented exception](https://github.com/Homebrew/homebrew-cask/blob/master/doc/development/adding_a_cask.md#but-there-is-no-stable-version).
    
    Additionally, if **adding a new cask**:
    
    - [x] Named the cask according to the [token reference].
    - [x] `brew cask install {{cask_file}}` worked successfully.
    - [x] `brew cask uninstall {{cask_file}}` worked successfully.
    - [x] Checked there are no [open pull requests] for the same cask.
    - [x] Checked the cask was not [already refused].
    - [x] Checked the cask is submitted to [the correct repo].
    
    [token reference]: https://github.com/Homebrew/homebrew-cask/blob/master/doc/cask_language_reference/token_reference.md
    [open pull requests]: https://github.com/Homebrew/homebrew-cask-versions/pulls
    [already refused]: https://github.com/Homebrew/homebrew-cask-versions/search?q=is%3Aclosed&type=Issues
    [the correct repo]: https://github.com/Homebrew/homebrew-cask/blob/master/doc/development/adding_a_cask.md#finding-a-home-for-your-cask
    [version-checksum]: https://github.com/Homebrew/homebrew-cask/blob/master/doc/cask_language_reference/stanzas/sha256.md#updating-the-sha256

---
+++ Casks/microsoft-edge-dev.rb
@@ -0,0 +1,24 @@
+cask 'microsoft-edge-dev' do
+  module Utils
+    def self.file_version
+      require 'open-uri'
+      appcast = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDDV01-chk.xml'
+      appcast_version = URI(appcast).read.scan(%r{<string>(.*)<\/string>})[2].join.split('.')
+      appcast_version[0] + '.0.' + appcast_version[1] + '.1'
+    end
+  end
+
+  version Utils.file_version.to_s
+  sha256 :no_check
+
+  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg"
+  name 'Microsoft Edge Dev'
+  homepage 'https://www.microsoftedgeinsider.com/'
+
+  auto_updates true
+
+  pkg "MicrosoftEdgeDev-#{version}.pkg"
+
+  uninstall pkgutil: 'com.microsoft.Edge.Dev'
+end
