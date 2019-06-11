719a726276468f457de79d24068b8434857e334e
Yurii Kolesnykov <root@yurikoles.com>
2019-06-11 20:06:44 +0300

    whoozle-android-file-transfer-nightly: doesn't conflicts with android-file-transfer

--- Casks/whoozle-android-file-transfer-nightly.rb
+++ Casks/whoozle-android-file-transfer-nightly.rb
@@ -7,10 +7,7 @@ cask 'whoozle-android-file-transfer-nightly' do
   name 'Android File Transfer'
   homepage 'https://whoozle.github.io/android-file-transfer-linux/'
 
-  conflicts_with cask: [
-                         'android-file-transfer',
-                         'whoozle-android-file-transfer',
-                       ]
+  conflicts_with cask: 'whoozle-android-file-transfer'
 
   app 'Android File Transfer for Linux.app'
   binary "#{appdir}/Android File Transfer for Linux.app/Contents/SharedSupport/bin/aft-mtp-cli"
