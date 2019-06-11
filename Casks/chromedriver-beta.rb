e5ebcf0ea16cc57cb4491ea29b8fb7eb3b77053a
Vítor Galvão <info@vitorgalvao.com>
2019-06-11 12:41:47 +0100

    Update chromedriver-beta.rb

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -4,7 +4,6 @@ cask 'chromedriver-beta' do
 
   # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
-  appcast 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
   name 'ChromeDriver'
   homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
 
