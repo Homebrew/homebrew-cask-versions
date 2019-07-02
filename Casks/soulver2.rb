a56731db6dca0415aec06fa210fb79a262a78fcb
Khaldoon Mutahar <git@mutahar.me>
2019-07-02 19:57:58 +0800

    Update soulver2 from 2.6.9-6055 to 2.7.0-6068

--- Casks/soulver2.rb
+++ Casks/soulver2.rb
@@ -1,6 +1,6 @@
 cask 'soulver2' do
-  version '2.6.9-6055'
-  sha256 'e8f96895e43d28177f458404bfc082ebc940c66ce6f425cdceeedab9389272bf'
+  version '2.7.0-6068'
+  sha256 '798c9e76ed617a5c2522728e798aa7c8e271407137173de24b73cc111d674a76'
 
   url "https://www.acqualia.com/files/sparkle/soulver_#{version}.zip"
   appcast "https://www.acqualia.com/soulver/appcast/soulver#{version.major}.xml"
