9ab76a20fad31d4efce2f3e23e04c52b28cf6c67
LanikSJ <laniksj@gmail.com>
2019-07-04 09:50:18 -0700

    Update telegram-alpha from 5.4-175291,2159 to 5.5-175803,2175

--- Casks/telegram-alpha.rb
+++ Casks/telegram-alpha.rb
@@ -1,6 +1,6 @@
 cask 'telegram-alpha' do
-  version '5.4-175291,2159'
-  sha256 '634d4074e6ed1fc4841c3c7abe70573f7a852cdb1522efea08f2c6befe97f3b2'
+  version '5.5-175803,2175'
+  sha256 '5233a692609a10665ed1eb2579f25c6597d0f70d94fe12eb954e48f762e62e02'
 
   # hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f/app_versions/#{version.after_comma}?format=zip"
