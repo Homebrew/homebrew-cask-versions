a54686b710df33d76b17457893794dc688d94f75
Vítor Galvão <info@vitorgalvao.com>
2019-07-06 13:15:41 +0100

    Update telegram-desktop-dev.rb

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -4,7 +4,8 @@ cask 'telegram-desktop-dev' do
 
   # github.com/telegramdesktop/tdesktop was verified as official when first introduced to the cask
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version.major_minor_patch}/tsetup.#{version}.dmg"
-  appcast 'https://github.com/telegramdesktop/tdesktop/releases.atom'
+  appcast 'https://github.com/telegramdesktop/tdesktop/releases.atom',
+          configuration: version.major_minor_patch
   name 'Telegram Desktop'
   homepage 'https://desktop.telegram.org/'
 
