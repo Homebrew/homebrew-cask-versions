429baf56d83bc38ba07b33c2bbee39eb7f2da4d1
Vítor Galvão <info@vitorgalvao.com>
2019-07-10 12:19:34 +0100

    Update microsoft-edge-dev.rb

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -4,7 +4,8 @@ cask 'microsoft-edge-dev' do
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg"
-  appcast 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDDV01-chk.xml'
+  appcast 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDDV01-chk.xml',
+          configuration: "#{version.major}.#{version.patch}"
   name 'Microsoft Edge Dev'
   homepage 'https://www.microsoftedgeinsider.com/'
 
