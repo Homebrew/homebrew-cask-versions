048f701cb4bce139e4581113e171b47627d47d8d
JounQin <admin@1stg.me>
2019-07-14 15:38:14 +0800

    remove appcast

--- Casks/unlox-beta.rb
+++ Casks/unlox-beta.rb
@@ -3,7 +3,6 @@ cask 'unlox-beta' do
   sha256 'e0fb5f35b12d1bcfd553a63fdf5b2a4e85d910311a5f0ffa13538f41d7ba1da0'
 
   url 'https://unlox.it/beta/download/'
-  appcast 'https://unlox.it/download/update.php'
   name 'Unlox Beta'
   homepage 'https://unlox.it/get'
 
