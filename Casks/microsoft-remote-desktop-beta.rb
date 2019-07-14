7a396995f2bd552ebc9dbf52354f7b94c66e729a
Vítor Galvão <info@vitorgalvao.com>
2019-07-14 21:43:01 +0100

    Update microsoft-remote-desktop-beta.rb

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -3,7 +3,8 @@ cask 'microsoft-remote-desktop-beta' do
   sha256 'af1a40624855b64314f63805ec9f7a93ed4987b90506d7e2ba963ea52c6258ca'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
-  appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06'
+  appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
+          configuration: version.major_minor_patch
   name 'Microsoft Remote Desktop Beta'
   homepage 'https://rink.hockeyapp.net/apps/5e0c144289a51fca2d3bfa39ce7f2b06/'
 
