0fb8b477090cd18df5748578f6e55e5d196003b7
Alejandro Leal <alealr@amazon.com>
2019-07-18 16:53:08 -0400

    Update termius-beta to v4.9.2  (#7627)
    * Create termius-beta.rb
    
    * Update termius-beta.rb
    
    * update to v.4.2.0
    
    * update to v4.2.5
    
    * update to v4.2.7
    
    * Update to v4.3.0
    
    * Update to v4.4.1
    
    * Update to v4.5.0
    
    * Update termius-beta.rb
    
    * update to 4.6.0
    
    * Update termius-beta to v 4.6.0
    
    * Update to version 4.7.2
    
    * update to v 4.8.1
    
    * Update to v.4.9.1
    
    Update to v.4.9.1
    
    * Update termius-beta to v4.9.2

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,6 +1,6 @@
 cask 'termius-beta' do
-  version '4.9.1'
-  sha256 'df958e3b600f3be9c6786723ec98020075f678ef6ea36a3cf8604f5efc288768'
+  version '4.9.2'
+  sha256 '900da280146dccfbca2bf0f4983989e674b3e732a8e612e53187101916d23d62'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac-beta/Termius+Beta.dmg'
