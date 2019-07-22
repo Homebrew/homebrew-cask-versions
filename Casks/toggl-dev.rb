e7ddf272df360676eff9274768d58f9bb0564382
Jordan <15076623+jor-dan@users.noreply.github.com>
2019-07-22 14:51:14 -0700

    Update toggl-dev from 7.4.373 to 7.4.461 (#7649)

--- Casks/toggl-dev.rb
+++ Casks/toggl-dev.rb
@@ -1,6 +1,6 @@
 cask 'toggl-dev' do
-  version '7.4.373'
-  sha256 'acd5aea0080a8b16bb3f989b568d0e2063de9bfae8473fb60e75472695f891ff'
+  version '7.4.461'
+  sha256 '1e67dc16b006ae12e4ed5dc660fb8ce8be0d25d4762747a5becc44959bc5ea99'
 
   # github.com/toggl/toggldesktop was verified as official when first introduced to the cask
   url "https://github.com/toggl/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
