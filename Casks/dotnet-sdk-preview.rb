4d95432a5ef0c6885b3b75d22e9604424dd2f752
Vítor Galvão <info@vitorgalvao.com>
2019-07-27 00:59:59 +0100

    Update dotnet-sdk-preview.rb

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk-preview' do
-  version '3.0.100-preview7-012821,64cb8405-ee15-4a9a-bf25-1201531f4519/b619596c137a08b204fc79a213bb9763'
+  version '3.0.100-preview7-012821,64cb8405-ee15-4a9a-bf25-1201531f4519:b619596c137a08b204fc79a213bb9763'
   sha256 '44de66897f4f5a1991e46d6e4689682067a5bef1cd1b28827bc70fd095547f40'
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   appcast "https://dotnet.microsoft.com/download/dotnet-core/#{version.major_minor}"
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
