20482da4150b6f1f5bf90077c73783256e4fa41c
Vítor Galvão <info@vitorgalvao.com>
2019-07-26 12:25:06 +0100

    Update dotnet-sdk-preview.rb

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,8 +1,8 @@
 cask 'dotnet-sdk-preview' do
-  version '3.0.100-preview7-012821'
+  version '3.0.100-preview7-012821,64cb8405-ee15-4a9a-bf25-1201531f4519/b619596c137a08b204fc79a213bb9763'
   sha256 '44de66897f4f5a1991e46d6e4689682067a5bef1cd1b28827bc70fd095547f40'
 
-  url "https://download.visualstudio.microsoft.com/download/pr/64cb8405-ee15-4a9a-bf25-1201531f4519/b619596c137a08b204fc79a213bb9763/dotnet-sdk-#{version}-osx-x64.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   appcast 'https://www.microsoft.com/net/download/macos'
   name '.NET Core SDK'
   homepage 'https://www.microsoft.com/net/core#macos'
@@ -14,7 +14,7 @@ cask 'dotnet-sdk-preview' do
                        ]
   depends_on macos: '>= :sierra'
 
-  pkg "dotnet-sdk-#{version}-osx-x64.pkg"
+  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   binary '/usr/local/share/dotnet/dotnet'
 
   uninstall pkgutil: [
