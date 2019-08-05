4cf6e5aa46c4a6d8d44f319ec64b21e723e67c21
Denis Nikulin <denisnikulin@gmail.com>
2019-08-05 20:11:50 +0100

    Update telegram-alpha.rb (#7703)

--- Casks/telegram-alpha.rb
+++ Casks/telegram-alpha.rb
@@ -1,6 +1,6 @@
 cask 'telegram-alpha' do
-  version '5.6-179046,2220'
-  sha256 '808591770b679b3695450316079d95897d5094b62f107999a743066490fefc0b'
+  version '5.6-179051,2221'
+  sha256 'e0ba273fb88ba32964811f40953c2577f4ff8ad61bb0c249b33ba4adb3b98db1'
 
   # hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/6ed2ac3049e1407387c2f1ffcb74e81f/app_versions/#{version.after_comma}?format=zip"
