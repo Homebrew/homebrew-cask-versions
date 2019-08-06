c2856526b72256d6d6e57bf443255dc8b58f346d
LanikSJ <laniksj@gmail.com>
2019-08-05 18:21:56 -0700

    Update signal-beta from 1.26.0-beta.3 to 1.26.0-beta.4

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,6 +1,6 @@
 cask 'signal-beta' do
-  version '1.26.0-beta.3'
-  sha256 '18725d6c628b45fbcc6f5103a0ac4e241a2bfe31c4d06546c123a022634a0b31'
+  version '1.26.0-beta.4'
+  sha256 '88a749725ae5d1fe23f0407b5fcbb16a1cb1a144162190b050d673bf1ea2fbc2'
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{version}.zip"
   appcast 'https://github.com/signalapp/Signal-Desktop/releases.atom'
