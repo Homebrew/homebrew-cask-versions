39235510ba969784b3debeded749e6044c6f47fe
Alejandro Leal <alealr@amazon.com>
2019-08-07 19:21:09 -0400

    update termius to v4.9.7

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,6 +1,6 @@
 cask 'termius-beta' do
-  version '4.9.5'
-  sha256 'dfa64837c028ca4435e84e1ae36d63155df3be94b117c21061a641598d99303b'
+  version '4.9.7'
+  sha256 '09ef6afe972c8c21e6211cba0281bd4efe36e1e5badb61d8b695510b5ba9dc2a'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://s3.amazonaws.com/termius.desktop.autoupdate/mac-beta/Termius+Beta.dmg'
