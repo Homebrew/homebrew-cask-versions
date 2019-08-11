8d3f94df85e2425aa3773394b1a57298beaba851
Viktor Szakats <vszakats@users.noreply.github.com>
2019-08-11 12:59:31 +0000

    move S3 domains to subdomain-style (#7736)
    Also delete 'vitamin-r3', which was broken by now and the
    beta version was released as stable.

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -2,8 +2,8 @@ cask 'affinity-designer-beta' do
   version '1.7.2.2 - RC1'
   sha256 '18aa8b63bdf791402b7e0372566b6e94ac5c8593ca7a2d606c30dedd709f0cba'
 
-  # amazonaws.com/affinity-beta was verified as official when first introduced to the cask
-  url 'https://s3.amazonaws.com/affinity-beta/download/Affinity%20Designer%20Beta.dmg'
+  # affinity-beta.s3.amazonaws.com was verified as official when first introduced to the cask
+  url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg'
   appcast 'https://forum.affinity.serif.com/index.php?/forum/15-designer-beta-on-mac/'
   name 'Serif Affinity Designer'
   homepage 'https://affinity.serif.com/en-us/'
--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -2,8 +2,8 @@ cask 'rstudio-preview' do
   version '1.2.1555'
   sha256 'b7d5da73bae20b67b7d9535e8f8b8551f17ac18aba933032ee4a5dcf91744f0c'
 
-  # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
+  # rstudio-ide-build.s3.amazonaws.com was verified as official when first introduced to the cask
+  url "https://rstudio-ide-build.s3.amazonaws.com/desktop/macos/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'https://www.rstudio.com/products/rstudio/download/preview/'
 
