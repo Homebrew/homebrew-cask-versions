85b67d0e1903bfe23090504f26deb741df352da4
Draque <draque@Hackintosh-N7.lan>
2019-08-18 23:40:18 -0700

    Update wine-devel from 4.13 to 4.14

--- Casks/wine-devel.rb
+++ Casks/wine-devel.rb
@@ -1,6 +1,6 @@
 cask 'wine-devel' do
-  version '4.13'
-  sha256 '96f87b2058016e0730511a8cb9eb861f06c3b6bf17746fd65e86110f4a97b620'
+  version '4.14'
+  sha256 'e55fe10488d54c1c88ba00cd0988c8c242426f3957660188775687e174e9d8b1'
 
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-devel-#{version}.pkg"
   appcast 'https://dl.winehq.org/wine-builds/macosx/download.html'
