c110954cf523cd23da0493ebb01ed56189351f11
Corey Gouker <corey@coreygo.com>
2019-08-20 11:16:15 -0700

    Add Microsoft Edge Beta v77.0.235.9

---
+++ Casks/microsoft-edge-beta.rb
@@ -0,0 +1,33 @@
+cask 'microsoft-edge-beta' do
+  version '77.0.235.9'
+  sha256 '3d262fa3ed1f4642f62edd8cb274709b739add1adf7eee932c666273fed0f084'
+
+  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
+  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg"
+  appcast 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDBT01-chk.xml',
+          configuration: "#{version.major}.#{version.patch}"
+  name 'Microsoft Edge Beta'
+  homepage 'https://www.microsoftedgeinsider.com/'
+
+  auto_updates true
+
+  pkg "MicrosoftEdgeBeta-#{version}.pkg"
+
+  uninstall pkgutil: 'com.microsoft.Edge.Beta'
+
+  zap launchctl: [
+                   'com.microsoft.autoupdate.helper',
+                   'com.microsoft.update.agent',
+                 ],
+      pkgutil:   [
+                   'com.microsoft.package.Microsoft_AutoUpdate.app',
+                 ],
+      trash:     [
+                   '/Library/PrivilegedHelperTools/com.microsoft.autoupdate.helper',
+                   '~/Library/Preferences/com.microsoft.Edge.Beta.plist',
+                 ],
+      rmdir:     [
+                   '/Library/Application Support/Microsoft',
+                   '~/Library/Application Support/Microsoft Edge Beta',
+                 ]
+end
