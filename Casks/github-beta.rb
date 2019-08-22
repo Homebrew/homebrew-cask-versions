5464e03be2cbc0075bb73c944dcf9d42ef32b788
Shoichi Aizawa <shoichiaizawa@gmail.com>
2019-08-23 08:20:21 +0900

    Update github-beta from 2.1.2-beta1-b5568722 to 2.1.2-beta2-1d24dcd0 (#7787)

--- Casks/github-beta.rb
+++ Casks/github-beta.rb
@@ -1,6 +1,6 @@
 cask 'github-beta' do
-  version '2.1.2-beta1-b5568722'
-  sha256 '9785ff0ef3c25a90c104ce984ee92e75f07f49e244d1a0a7b46d7e1bfaa377c5'
+  version '2.1.2-beta2-1d24dcd0'
+  sha256 'c43d62fc5f9c3adcb591787a52ce83f50ce3e481ac1b363c6a9ba16f1ca387e1'
 
   # githubusercontent.com was verified as official when first introduced to the cask
   url "https://desktop.githubusercontent.com/releases/#{version}/GitHubDesktop.zip"
