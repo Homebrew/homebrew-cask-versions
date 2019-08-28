3b208c1daf66f809977fbe092cbf796163784e25
Sander Kromwijk <sanderk@palantir.com>
2019-08-28 11:44:31 -0400

    Add zulu12

---
+++ Casks/zulu12.rb
@@ -0,0 +1,14 @@
+cask 'zulu12' do
+  version '12.0.2,12.3.11_1-ca'
+  sha256 '181b65f9a88534b97784974bb17235dfcdc031e9ecd3f54dd4fb29464e143d5d'
+
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+      referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
+  name 'Azul Zulu Java Standard Edition Development Kit'
+  homepage 'https://www.azul.com/downloads/zulu/zulu-mac/'
+
+  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}",
+            rmdir:   '/Library/Java/JavaVirtualMachines'
+end
