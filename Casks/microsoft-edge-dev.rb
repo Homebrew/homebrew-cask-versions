975a8fb016bd95b1c7a1301ef22c170ea9ec52c2
Corey Gouker <corey@coreygo.com>
2019-08-28 12:48:55 -0700

    Update microsoft-edge-dev from 78.0.244.0 to 78.0.249.1

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-edge-dev' do
-  version '78.0.244.0'
-  sha256 'a37a7dad8491e757db6cec680cebdfbf6669845d0f2dca1040277d892c198627'
+  version '78.0.249.1'
+  sha256 'b20091c6d9d907dbf02812d7637983ae746c5293504b2249536a6b1113c9715d'
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg"
