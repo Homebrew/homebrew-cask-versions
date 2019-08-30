c5d6f116bc6abce2611c12db233e778e980978af
Corey Gouker <corey@coreygo.com>
2019-08-30 12:30:06 -0700

    Update microsoft-edge-canary

--- Casks/microsoft-edge-canary.rb
+++ Casks/microsoft-edge-canary.rb
@@ -4,6 +4,7 @@ cask 'microsoft-edge-canary' do
       require 'open-uri'
       appcast = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDCN01-chk.xml'
       appcast_version = URI(appcast).read.scan(%r{<string>(.*)<\/string>})[2].join.split('.')
+      # Parsing logic clarified by vendor, please review https://git.io/fjxFB prior to modification
       "#{appcast_version[0]}.0.#{appcast_version[1]}.#{appcast_version[2][6..-1]}"
     end
   end
