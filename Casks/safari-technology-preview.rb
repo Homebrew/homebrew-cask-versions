d42a0fa4b46a0404350a306a4811f69a66b70873
Philip Jägenstedt <philip@foolip.org>
2019-09-08 15:05:07 +0200

    Update safari-technology-preview to 91 (#7850)

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,14 +1,13 @@
 cask 'safari-technology-preview' do
-  version '90,061-02262-20190820-07f36bc0-0154-437e-bbbb-fcd0ca56dfd7'
-
   if MacOS.version <= :mojave
-    url 'https://secure-appldnld.apple.com/STP/061-04045-20190821-10654810-39f2-4cbe-a92d-dedd7c4f85ef/SafariTechnologyPreview.dmg'
-    sha256 '0b921d4cd7a3e7ed52dc62835c3f62f9f4115295d5f68ab104a24ae1ddf61f4c'
+    version '91,061-10708-20190903-4bf79804-9f40-4b31-97c9-c914330c30b1'
+    sha256 'ad39ec6525db9c95eaa9559ad446ab24bf179cdb599a8d5b47e01f4b395e1897'
   else
-    url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
-    sha256 '658e82c429784969ffbf01312cd785c5e8b8cacd25a4e265590c5f585df1cbbb'
+    version '91,061-10706-20190903-6cbd1abb-4b46-4a3f-9229-5e3f89061692'
+    sha256 'f9db8462b5dc2433e4af3deb1c354985c135bf8ce4173bda34f0602466fd7187'
   end
 
+  url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
   appcast 'https://developer.apple.com/safari/download/'
   name 'Safari Technology Preview'
   homepage 'https://developer.apple.com/safari/download/'
