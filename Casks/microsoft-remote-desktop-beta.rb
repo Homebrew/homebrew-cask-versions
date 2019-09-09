e1ade02b781e60ac76a9390e39ed25e84f9cd0a7
huxuan <i@huxuan.org>
2019-09-09 16:32:33 +0800

    Update microsoft-remote-desktop-beta from 10.3.1.1647,319 to 10.3.2.1650,320

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-remote-desktop-beta' do
-  version '10.3.1.1647,319'
-  sha256 '26c1a5ddc53364472a0fe6833539980980ef9cf1fc9e855f17206427f7b06355'
+  version '10.3.2.1650,320'
+  sha256 '57a6aa66a8e45f74f338d6a9e8242bb48d96b529b05f1c6932b5b9654668c57b'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
