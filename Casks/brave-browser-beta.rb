c6b4b3d66724dfe206e7e0f2d1a5d937d94266cd
David Yantis <404NetworkError@users.noreply.github.com>
2019-09-09 22:50:31 -0400

    Update brave-browser-beta from 0.69.119 to 0.70.93 (#7860)

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser-beta' do
-  version '0.69.119'
-  sha256 '035ee6135f73e267955e9207dd65abc33d9ef4956f7613efc9b68b1d7e22e47c'
+  version '0.70.93'
+  sha256 '17d92c766b65d8640a042f4a2d6b122d3c809070d9b80ad03983916f2f2c9226'
 
   url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser-Beta.dmg"
   appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/beta/appcast.xml'
