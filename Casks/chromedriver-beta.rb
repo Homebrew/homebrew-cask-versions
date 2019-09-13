219f9529e25d529698c3c0fbcc2c954a0b544e67
Rob Trame <Redapted@users.noreply.github.com>
2019-09-13 09:32:34 -0600

    Update chromedriver-beta from 77.0.3865.40 to 78.0.3904.11 (#7878)

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -1,6 +1,6 @@
 cask 'chromedriver-beta' do
-  version '77.0.3865.40'
-  sha256 '6fd7ad8944f762933e2a62aa1908de26c94a81f5cb6673446700794ae0574313'
+  version '78.0.3904.11'
+  sha256 '8fc187209976e5752ce3053f30be5632e6be776331f20e0ea4b7f213e833b98e'
 
   # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
