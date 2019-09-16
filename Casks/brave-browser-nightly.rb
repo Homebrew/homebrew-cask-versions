dd611ac25c579ee6ef48e63111298fae22f1cbec
gottabeleGIT <sandesh.manik@icloud.com>
2019-09-16 06:43:09 -0500

    Updated URL to be always up-to-date

--- Casks/brave-browser-nightly.rb
+++ Casks/brave-browser-nightly.rb
@@ -2,12 +2,11 @@ cask 'brave-browser-nightly' do
   version :latest
   sha256 :no_check
 
+  # URL_SECTION was verified as official when first introduced to the cask
   url "https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser-Nightly.dmg"
-  appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/nightly/appcast.xml'
   name 'Brave Nightly'
   homepage 'https://github.com/brave/brave-browser'
 
-  auto_updates true
   depends_on macos: '>= :mavericks'
 
   app 'Brave Browser Nightly.app'
