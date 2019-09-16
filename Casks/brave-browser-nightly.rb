aea52ef5c2522ec103a95d714c291a787b902286
gottabeleGIT <sandesh.manik@icloud.com>
2019-09-16 06:29:21 -0500

    Update brave-browser-nightly.rb

--- Casks/brave-browser-nightly.rb
+++ Casks/brave-browser-nightly.rb
@@ -1,8 +1,8 @@
 cask 'brave-browser-nightly' do
-  version '0.72.11'
-  sha256 'e3f305d035642bc0b8a0e505e7bd2c5590884effed8c127fb5eed7995e5fef9d'
+  version :latest
+  sha256 :no_check
 
-  url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser-Nightly.dmg"
+  url "https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser-Nightly.dmg"
   appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/nightly/appcast.xml'
   name 'Brave Nightly'
   homepage 'https://github.com/brave/brave-browser'
