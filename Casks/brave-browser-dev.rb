121ef64fc7aaafaed2773fb07ab70ca1226e857e
Vítor Galvão <info@vitorgalvao.com>
2019-09-18 14:23:22 +0100

    brave-browser-dev.rb: set home to the website instead of github (#7895)
    * brave-browser-dev.rb: set home to the website instead of github
    
    * Update brave-browser-dev.rb

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -2,10 +2,11 @@ cask 'brave-browser-dev' do
   version '0.71.74'
   sha256 'a51ac9d59fc38991c33136e1035c96416890e6ca3fead896d3d63cdc90330323'
 
+  # github.com/brave/brave-browser was verified as official when first introduced to the cask
   url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser-Dev.dmg"
   appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/dev/appcast.xml'
   name 'Brave Dev'
-  homepage 'https://github.com/brave/brave-browser'
+  homepage 'https://brave.com/download-dev/'
 
   auto_updates true
   depends_on macos: '>= :mavericks'
