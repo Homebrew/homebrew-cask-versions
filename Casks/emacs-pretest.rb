468add89ff0820183891ed0330ba9d2345a225a3
Vítor Galvão <info@vitorgalvao.com>
2019-09-20 14:39:24 +0100

    Update emacs-pretest.rb

--- Casks/emacs-pretest.rb
+++ Casks/emacs-pretest.rb
@@ -7,7 +7,8 @@ cask 'emacs-pretest' do
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
-  conflicts_with formula: 'emacs', cask: ['emacs', 'emacs-nightly']
+  conflicts_with cask: ['emacs', 'emacs-nightly']
+  conflicts_with formula: 'emacs'
 
   app 'Emacs.app'
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
