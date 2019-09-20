b07ead5460bb41631d1911da599db2548a7532ed
Tom Brown <tom@goradar.com>
2019-09-19 23:01:58 -0400

    emacs-nightly.rb: Fix style issues.

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -10,7 +10,7 @@ cask 'emacs-nightly' do
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
-  conflicts_with formula: 'emacs', cask: [ 'emacs', 'emacs-pretest' ]
+  conflicts_with formula: 'emacs', cask: ['emacs', 'emacs-pretest']
 
   app 'Emacs.app'
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
