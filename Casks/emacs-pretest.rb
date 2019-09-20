c226537919089e2e1e85244750ec332b052f7817
Tom Brown <tom@goradar.com>
2019-09-19 23:01:14 -0400

    emacs-pretest.rb: Fix style issues.

--- Casks/emacs-pretest.rb
+++ Casks/emacs-pretest.rb
@@ -7,7 +7,7 @@ cask 'emacs-pretest' do
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
-  conflicts_with formula: 'emacs', cask: [ 'emacs', 'emacs-nightly' ]
+  conflicts_with formula: 'emacs', cask: ['emacs', 'emacs-nightly']
 
   app 'Emacs.app'
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
