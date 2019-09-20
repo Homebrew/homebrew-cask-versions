17066947f1e4f68f193cc5bc79df1acbfe48e9f2
Vítor Galvão <info@vitorgalvao.com>
2019-09-20 14:38:45 +0100

    Update emacs-nightly.rb

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -10,7 +10,8 @@ cask 'emacs-nightly' do
   name 'Emacs'
   homepage 'https://emacsformacosx.com/'
 
-  conflicts_with formula: 'emacs', cask: ['emacs', 'emacs-pretest']
+  conflicts_with cask: ['emacs', 'emacs-pretest']
+  conflicts_with formula: 'emacs'
 
   app 'Emacs.app'
   binary "#{appdir}/Emacs.app/Contents/MacOS/Emacs", target: 'emacs'
