f82ea52a9033c971b99d657c43508c7fb72dc2e3
Bo Mehlsen <bo.mehlsen@storytel.com>
2019-09-24 14:26:54 +0200

    Add dotnet-sdk2

---
+++ Casks/dotnet-sdk2.rb
@@ -0,0 +1,31 @@
+cask 'dotnet-sdk2' do
+  version '2.2.402,7430e32b-092b-4448-add7-2dcf40a7016d:1076952734fbf775062b48344d1a1587'
+  sha256 'e74d816bc034d0fcdfa847286a6cad097227d4864da1c97fe801012af0c26341'
+
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  appcast 'https://dotnet.microsoft.com/download/dotnet-core/2.2'
+  name '.NET Core SDK'
+  homepage 'https://www.microsoft.com/net/core#macos'
+
+  conflicts_with cask: [
+                         'dotnet',
+                         'dotnet-preview',
+                         'dotnet-sdk',
+                         'dotnet-sdk-preview',
+                       ]
+  depends_on macos: '>= :sierra'
+
+  pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
+  binary '/usr/local/share/dotnet/dotnet'
+
+  uninstall pkgutil: 'com.microsoft.dotnet.*',
+            delete:  [
+                       '/etc/paths.d/dotnet',
+                       '/etc/paths.d/dotnet-cli-tools',
+                     ]
+
+  zap trash: [
+               '~/.dotnet',
+               '~/.nuget',
+             ]
+end
