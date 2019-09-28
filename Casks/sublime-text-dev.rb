b00c097a5ef85811b0c9744ec5e53790a87d353f
Vítor Galvão <info@vitorgalvao.com>
2019-09-28 14:35:35 +0100

    Update sublime-text-dev.rb

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -3,7 +3,8 @@ cask 'sublime-text-dev' do
   sha256 'a52f309f5dc708b016ceb8ee0960d24252050c8fcc9f3a6f22c275b8a27e1767'
 
   url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version.no_dots}.dmg"
-  appcast "https://www.sublimetext.com/updates/#{version.major}/dev/appcast_osx.xml"
+  appcast "https://www.sublimetext.com/updates/#{version.major}/dev/appcast_osx.xml",
+          configuration: version.no_dots
   name 'Sublime Text'
   homepage "https://www.sublimetext.com/#{version.major}dev"
 
