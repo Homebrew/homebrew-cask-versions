e1698b87c0d46dd8124062b5be97f63cd1615f11
Chuanchan <williamtsai1111@gmail.com>
2019-10-01 04:31:05 +0800

    Update wine-devel from 4.16 to 4.17

--- Casks/wine-devel.rb
+++ Casks/wine-devel.rb
@@ -1,6 +1,6 @@
 cask 'wine-devel' do
-  version '4.16'
-  sha256 '7e9cbb4ca8a600cd4aa0cb41d984f72a901649cd18212b2daa57d907087d216b'
+  version '4.17'
+  sha256 '3d1c47048a9f32e953eddcc77996055aad92989f7a6a1aa125134aeaceeb4e9a'
 
   url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-devel-#{version}.pkg"
   appcast 'https://dl.winehq.org/wine-builds/macosx/download.html'
