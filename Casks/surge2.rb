1a15475945ad712a0c4042cd8b05c2eeb7431916
Zach Brown <zach@zachbr.io>
2019-10-03 21:45:25 -0500

    Update surge2 from 2.6.5 to 2.6.7

--- Casks/surge2.rb
+++ Casks/surge2.rb
@@ -1,6 +1,6 @@
 cask 'surge2' do
-  version '2.6.5,652'
-  sha256 '494e714a508e8ab44612179c26c59d2afa663805973ce1bb5f57ec7a08cb7048'
+  version '2.6.7,656'
+  sha256 '6689cea266fb0b45e5272ef6b8bc76fbd111b438511243a929af9391a868cb2f'
 
   url "https://nssurge.com/mac/Surge-#{version.before_comma}-#{version.after_comma}.zip"
   name 'Surge'
