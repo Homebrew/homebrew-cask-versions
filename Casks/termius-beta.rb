07fb19b4cc2f09f71ef87e23ebb397ad44cfb396
Alejandro Leal <ajleal@msn.com>
2019-10-08 18:43:49 -0400

    Update termius-beta.rb

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,7 +1,6 @@
 cask 'termius-beta' do
   version '4.10.4'
   sha256 '157b7fe9fb055062ef22f6ec2f939f253a700c9c8f325add57fad74856357182'
-
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://www.termius.com/beta/download/mac/Termius+Beta.dmg'
   name 'Termius Beta'
