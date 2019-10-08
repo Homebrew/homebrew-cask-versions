85ffb22feec198470d83aab80e5bd23628c31d6e
Gauthier POGAM--LE MONTAGNER <gauthier.pogam-lemontagner@protonmail.com>
2019-10-08 18:52:24 +0900

    Add Adguard Nightly

---
+++ Casks/adguard-nightly.rb
@@ -0,0 +1,25 @@
+cask 'adguard-nightly' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://static.adguard.com/mac/nightly/AdGuard.dmg'
+  name 'Adguard for Mac'
+  homepage 'https://adguard.com/'
+
+  pkg 'AdGuard.pkg'
+
+  uninstall pkgutil: 'com.adguard.mac.adguard-pkg'
+
+  zap trash: [
+               '/Library/Application Support/com.adguard.mac.adguard',
+               '~/Library/Application Support/com.adguard.mac.adguard-loginhelper',
+               '~/Library/Application Support/com.adguard.mac.adguard',
+               '~/Library/Caches/KSCrash/Adguard',
+               '~/Library/Caches/com.adguard.mac.adguard',
+               '~/Library/Cookies/com.adguard.Adguard.binarycookies',
+               '~/Library/Containers/com.adguard.mac.adguard-loginhelper',
+               '~/Library/Logs/com.adguard.mac.adguard',
+               '~/Library/Preferences/com.adguard.mac.adguard.plist',
+               '~/Library/Saved Application State/com.adguard.mac.adguard.savedState',
+             ]
+end
