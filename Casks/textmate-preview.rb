55ad429e910c22f019b3ba6e5b913d04f983030e
Marc Schwede <9077622+schwedenmut@users.noreply.github.com>
2019-10-10 10:47:06 +0200

    Update textmate-preview from 2.0-rc.30 to 2.0.1 (#7978)
    Since 2.0.1 is marked as pre-release in https://github.com/textmate/textmate/releases and Textmate/api states 2.0 as the latest release.

--- Casks/textmate-preview.rb
+++ Casks/textmate-preview.rb
@@ -1,6 +1,6 @@
 cask 'textmate-preview' do
-  version '2.0-rc.30'
-  sha256 '8e2aad3bb2ed64c43283e5697629c831dd087a47aecef85c0072d60bde7765bf'
+  version '2.0.1'
+  sha256 'bb515daecf4f031a7d2f088ef8613a27699c774c08004202c8363d49e87c35f0'
 
   # github.com/textmate/textmate was verified as official when first introduced to the cask
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz"
