d4b56c1671c82073ea2df18f63525f8f95e5636d
Yihui Xie <xie@yihui.name>
2019-10-10 14:34:00 -0500

    Update rstudio-preview from 1.2.1578 to 1.2.5005

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,5 +1,5 @@
 cask 'rstudio-preview' do
-  version '1.2.1578'
+  version '1.2.5005'
   sha256 '9874c0c932e71ab106fa95476403573c3dd6046a3c64b9c0084adc6201bb51c0'
 
   # rstudio-ide-build.s3.amazonaws.com was verified as official when first introduced to the cask
