247728e82c2c22e381473aba555c32385cd37ebd
Yurii Kolesnykov <root@yurikoles.com>
2019-10-11 20:04:34 +0300

    Update microsoft-remote-desktop-beta (#7984)

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-remote-desktop-beta' do
-  version '10.3.4.1669,337'
-  sha256 '1a5544c3e3bf11cecf553d847fd68fcb52ae00f6c1619fdf7182f9b1e4f238ae'
+  version '10.3.4.1676,346'
+  sha256 '93271606a0dc70a7db5cd92dd60a142daffd3731941112fa0156c63def883477'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
