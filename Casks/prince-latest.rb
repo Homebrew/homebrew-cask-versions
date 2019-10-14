9871e48c5509e1f0794271a7b096c227184b3ece
Carlos Precioso <kalprestito@gmail.com>
2019-10-14 13:15:24 +0200

    Improve installation process for prince-latest and update to v20191002
    The bundled installation script is bypassed, as Cask can manage it.

--- Casks/prince-latest.rb
+++ Casks/prince-latest.rb
@@ -1,20 +1,22 @@
 cask 'prince-latest' do
-  version '20190220'
-  sha256 '7c688f3f752421ae8e8b7a08fd4d3a99a195fac8b323799aa16e069f4ece0814'
+  version '20191002'
+  sha256 'a874953d84c7bf2bda08a2ee829fe75bad0c1fc2a9e61dc39a717c55931509e2'
 
   url "https://www.princexml.com/download/prince-#{version}-macosx.tar.gz"
   appcast 'https://www.princexml.com/latest/'
   name 'Prince'
   homepage 'https://www.princexml.com/'
 
-  installer script: "prince-#{version}-macosx/install.sh"
+  conflicts_with cask: 'prince'
 
-  uninstall delete: [
-                      '/usr/local/bin/prince',
-                      '/usr/local/lib/prince',
-                    ]
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/prince-#{version}-macosx/prince.wrapper.sh"
+  binary shimscript, target: 'prince'
 
-  caveats do
-    files_in_usr_local
+  preflight do
+    IO.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{staged_path}/prince-#{version}-macosx/lib/prince/bin/prince' --prefix '#{staged_path}/prince-#{version}-macosx/lib/prince' "$@"
+    EOS
   end
 end
