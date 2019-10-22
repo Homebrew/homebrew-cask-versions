3420132d3d655cbb1e81b7f15265067abeb12fc0
Yurii Kolesnykov <root@yurikoles.com>
2019-10-22 21:55:01 +0300

    Update firefox-esr from 60.9.0 to 68.2.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -1,78 +1,78 @@
 cask 'firefox-esr' do
-  version '60.9.0'
+  version '68.2.0'
 
   language 'cs' do
-    sha256 'b2dae7c8a81100e6ae6388df82bb8fb11c300aedd0d8da93bd58ac6802090d76'
+    sha256 '51b802afa1eec858f132d1bc9f21804e27501c4fa7f4b780169c913f223cf61f'
     'cs'
   end
 
   language 'de' do
-    sha256 'd2fa71f9378189e396be854f0e66b0ad639ec068abc889dbc74f695841a71360'
+    sha256 '3cf657e6a7fa39c983e06c320c662c1dbc413b221c78a9280efd624fd805feeb'
     'de'
   end
 
   language 'en-GB' do
-    sha256 '4bbdd5ea290f2de2979b2595cf0d2224d62d05058c4925f63ad2cfb90e52a284'
+    sha256 '653f7e96d94df932f47ee6283ff277c5441edda2e1fed898c49e841cd89c212d'
     'en-GB'
   end
 
   language 'en', default: true do
-    sha256 'a287051bc72af1d56d417ed30f7d4305f438071134bc087ca4faa84600c6403a'
+    sha256 '7e6b800f528288e3411fe58b3583772ba140587f9e020f2be437d9b5a7400233'
     'en-US'
   end
 
   language 'fr' do
-    sha256 '796f9d2f5be31c7813b6c083d322f10d3103ac75b3167613818267d8e972f416'
+    sha256 'b597d0ac68577cad5055924cdd28db4d245d4943a41528019308afe0a0d6f4f4'
     'fr'
   end
 
   language 'gl' do
-    sha256 '0047c5b356e6c7f22ac67d139c45e60c4a84554386f8f2e3ee17e3212a7a5496'
+    sha256 '5ba7f557736c8b0f3966bc827f6a6827483a48015f62e7d434d5805800b13e55'
     'gl'
   end
 
   language 'it' do
-    sha256 '52ec9991cb816b3a107fa49ce03a98a109ef0a0a51bf3ebe63ba48e024608569'
+    sha256 'afbf946fee0a77e8ec37086615d3e41bed811f07adc67629b5d18e8063bf394d'
     'it'
   end
 
   language 'ja' do
-    sha256 '068b238aeb0b5317e3551a136386362b90f9947efb40b502838b57fdda88dde7'
+    sha256 'b5aae13d65b8a956b8684319c280c2f77a8e26dfd254e2949d21e3a19c7e2418'
     'ja-JP-mac'
   end
 
   language 'nl' do
-    sha256 '74f52349994d25d5d61d7a8b44a4f0ac0c0d072daab7db459b5ccc72bac4f4a1'
+    sha256 '56bd69f9ed937e4ffb51ea970a042d204f790849d17be97ed6dc104e619a4457'
     'nl'
   end
 
   language 'pl' do
-    sha256 '68e294715c45249b7bc57aa6d70be52b04dd974f7b06d359bc91d8b7a851e7f7'
+    sha256 '188e99d2e207a625fcd658a1b1d12152d7b6f9af64b5be72e2e9085cfdc61d80'
     'pl'
   end
 
   language 'pt' do
-    sha256 '4ad45831503ac210de08ea976db4ae9c28d345232b1ffafd68b9715c2ea9e4fb'
+    sha256 '9ee7896a27dd02b3463b2e9dd2c4840b76bf304ce122babfe03e836290bbb565'
     'pt-PT'
   end
 
   language 'ru' do
-    sha256 '6b6979f89e02c766e632b7193485db6438248d706a1acfe61342e4a0ff783922'
+    sha256 'b3f115f7bee0dfc40e5e4efc5b8f4c81c2b5d45c2199ce14770c4d6923af3a38'
     'ru'
   end
 
   language 'uk' do
-    sha256 'db1f8591c469319e95ecfc622b283f70b6666cfd361b843d6ac4c7da52756a73'
+    sha256 '990a013353fa806f9d674c7a486260940abaec7e7f78cfce6413fac1770018e0'
     'uk'
   end
 
   language 'zh-TW' do
-    sha256 '9cd7d0133aa9bf454c7f0acf582c1c34f255b84b9033442ec8f6ac25bc0283bd'
+    sha256 '0df3167bc78057467d26bc808a51ba1c9d729019aee823fba6c67a1b8826bbef'
     'zh-TW'
   end
 
   language 'zh' do
-    sha256 'd9aaa48f5412453aac0e0365d7606f2209bbefc4780d1086c8fed24effb5d6cd'
+    sha256 '7b271fe4e7afc27e424b792835d078976e65f032ce23c1211e97c33b51953e36'
     'zh-CN'
   end
 
