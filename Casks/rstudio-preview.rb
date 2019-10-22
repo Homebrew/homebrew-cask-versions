a9e0a918d031a543955cf34cca3262f04c302fbf
Yihui Xie <xie@yihui.name>
2019-10-22 09:49:38 -0500

    Update rstudio-preview from 1.2.5005 to 1.2.5013 (#8032)
    * Update rstudio-preview from 1.2.5005 to 1.2.5013
    
    * Update rstudio-preview.rb

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.2.5005'
-  sha256 '06854ce4e2703899c6517f141aaf245ff1ba853a280d5937932a7b9b7aaa6c4e'
+  version '1.2.5013'
+  sha256 'c1f96a4e61af4b33e1a15146c8ef4d2fc4b16e9c6bd04492406055809732f912'
 
   # rstudio-ide-build.s3.amazonaws.com was verified as official when first introduced to the cask
   url "https://rstudio-ide-build.s3.amazonaws.com/desktop/macos/RStudio-#{version}.dmg"
