bebcd6a2785c54aae57d37b3d10724a4eed589a8
Yihui Xie <xie@yihui.name>
2019-10-22 10:19:04 -0500

    Update rstudio-preview 1.2.5013
    The download link was changed from https://rstudio-ide-build.s3.amazonaws.com to https://s3.amazonaws.com/rstudio-ide-build

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -3,7 +3,7 @@ cask 'rstudio-preview' do
   sha256 'c1f96a4e61af4b33e1a15146c8ef4d2fc4b16e9c6bd04492406055809732f912'
 
   # rstudio-ide-build.s3.amazonaws.com was verified as official when first introduced to the cask
-  url "https://rstudio-ide-build.s3.amazonaws.com/desktop/macos/RStudio-#{version}.dmg"
+  url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'https://www.rstudio.com/products/rstudio/download/preview/'
 
