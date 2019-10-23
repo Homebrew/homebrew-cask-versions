cb891acb80bac05d1a26f4f94501d73d3473872d
Zachary Cutlip <uid000@gmail.com>
2019-10-22 17:36:45 -0700

    Add Ghidra beta 9.1

---
+++ Casks/ghidra-beta.rb
@@ -0,0 +1,18 @@
+cask 'ghidra-beta' do
+  version '9.1-BETA_DEV_20190923'
+  sha256 '3d61de711b7ea18bdee3ed94c31429e4946603b3e7d082cca5e949bbd651f051'
+
+  url "https://www.ghidra-sre.org/ghidra_#{version}.zip"
+  name 'Ghidra'
+  homepage 'https://www.ghidra-sre.org/'
+
+  conflicts_with cask: 'ghidra'
+
+  binary "ghidra_#{version.major_minor_patch}-BETA_DEV/ghidraRun"
+
+  zap trash: '~/.ghidra'
+
+  caveats do
+    depends_on_java '11+'
+  end
+end
