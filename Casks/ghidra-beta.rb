707fe72b7f88557ce78325803f646881efe208e8
Markus Reiter <me@reitermark.us>
2019-10-27 09:47:49 +0100

    Update ghidra-beta.rb

--- Casks/ghidra-beta.rb
+++ Casks/ghidra-beta.rb
@@ -1,14 +1,14 @@
 cask 'ghidra-beta' do
-  version '9.1-BETA_DEV_20190923'
+  version '9.1-BETA_DEV,20190923'
   sha256 '3d61de711b7ea18bdee3ed94c31429e4946603b3e7d082cca5e949bbd651f051'
 
-  url "https://www.ghidra-sre.org/ghidra_#{version}.zip"
+  url "https://www.ghidra-sre.org/ghidra_#{version.before_comma}_#{version.after_comma}.zip"
   name 'Ghidra'
   homepage 'https://www.ghidra-sre.org/'
 
   conflicts_with cask: 'ghidra'
 
-  binary "ghidra_#{version.major_minor_patch}-BETA_DEV/ghidraRun"
+  binary "ghidra_#{version.before_comma}/ghidraRun"
 
   zap trash: '~/.ghidra'
 
