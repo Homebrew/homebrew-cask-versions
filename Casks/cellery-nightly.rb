2b535e11487b3a33bc0e936f65fc68128fec7dd5
hasinthaindrajee <hasinthaindrajee@gmail.com>
2019-10-29 14:46:07 +0530

    Add Cellery Nightly

---
+++ Casks/cellery-nightly.rb
@@ -0,0 +1,16 @@
+cask 'cellery-nightly' do
+  version :latest
+  sha256 :no_check
+
+  # cellery-installer-resources.s3.amazonaws.com was verified as official when first introduced to the cask
+  url 'https://cellery-installer-resources.s3.amazonaws.com/cellery-macos-installer-x64-latest.pkg'
+  name 'Cellery'
+  homepage 'https://wso2-cellery.github.io/'
+
+  depends_on formula: 'python'
+  depends_on formula: 'torsocks'
+
+  pkg 'cellery-macos-installer-x64-latest.pkg'
+
+  uninstall pkgutil: 'org.cellery.*'
+end
