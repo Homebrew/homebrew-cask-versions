faf443a44865f17fd07da2aa326ec9264482ca02
Ruben <15186467+ROpdebee@users.noreply.github.com>
2019-10-30 19:32:40 +0100

    Update emacs-pretest from 26.1-rc1-1 to 26.3-rc1 (#8081)

--- Casks/emacs-pretest.rb
+++ Casks/emacs-pretest.rb
@@ -1,6 +1,6 @@
 cask 'emacs-pretest' do
-  version '26.1-rc1-1'
-  sha256 '722187515f514c748d36999ef4d638aac8e4352bf3c7c39d4c7266e913a493d2'
+  version '26.3-rc1'
+  sha256 'a78210f215f3eee844ccf5b86c4b5bf0de7ca9fe0fbbe5eb22f60d0dc561261c'
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-pretest-#{version}-universal.dmg"
   appcast 'https://emacsformacosx.com/atom/pretest'
