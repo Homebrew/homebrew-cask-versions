74ab78e17b7abc0d473c31b8f1fc78038bb2f570
Ruben <15186467+ROpdebee@users.noreply.github.com>
2019-10-30 18:18:11 +0100

    Update dolphin-dev from 5.0-10758 to 5.0-11094 (#8077)

--- Casks/dolphin-dev.rb
+++ Casks/dolphin-dev.rb
@@ -1,6 +1,6 @@
 cask 'dolphin-dev' do
-  version '5.0-10758'
-  sha256 'dd93381066fb254b27ba97d284f07d13bdc522aa8c85516029c18a70fac276e7'
+  version '5.0-11094'
+  sha256 '9f29ae7bc4a2a73e1e11eadcff4444209ad62679ef2d207678c7a08d1e6ac88f'
 
   url "https://dl.dolphin-emu.org/builds/dolphin-master-#{version}.dmg"
   appcast 'https://github.com/dolphin-emu/dolphin/releases.atom'
