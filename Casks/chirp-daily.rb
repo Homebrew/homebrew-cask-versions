0846414081b0def58f0440681a50ca020d113fa8
Vítor Galvão <info@vitorgalvao.com>
2019-10-30 16:48:35 +0000

    Update chirp-daily.rb

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -3,7 +3,7 @@ cask 'chirp-daily' do
   sha256 'f688fdf5184b64e1f9df346311b15ac514d67b07ac169faaa751914d861ca2c0'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
-  appcast 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/'
+  appcast 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM'
   name 'CHIRP'
   homepage 'https://chirp.danplanet.com/projects/chirp/wiki/Home'
 
