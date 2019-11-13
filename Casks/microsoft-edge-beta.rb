36be073327d6289819b69e20f35b2eac613684f1
Andrey Izotov <faradaytrs@gmail.com>
2019-11-13 10:16:50 +0800

    Update microsoft-edge-beta from 79.0.309.12 to 79.0.309.18 (#8168)

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-edge-beta' do
-  version '79.0.309.12'
-  sha256 'd6f559fecb7d99c2fe18b09bb9509d1dcdb27006c29c8ea6f9c7d2c140f51b83'
+  version '79.0.309.18'
+  sha256 'f9d8f34504067189d4a72448c2adf1a58db1d86f29d0053aa4996d5731fffeff'
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg"
