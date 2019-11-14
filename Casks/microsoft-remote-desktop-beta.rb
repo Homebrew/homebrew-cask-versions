5524675c69a715a087c4d8475fe3a85d96c5a1c3
Manuel Aristarán <jazzido@jazzido.com>
2019-11-13 21:24:26 -0300

    Update microsoft-remote-desktop-beta (#8170)

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-remote-desktop-beta' do
   version '10.3.5.1713,365'
-  sha256 'e6e90cfb147e5b8657706e2c415bfaaed5583543fb1c427fcff6b6eb3a81e097'
+  sha256 '6ff21ff2f10d34d6d7ff54bc54d55cf34531dd7a19ad2ed898aef536181e3e73'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
