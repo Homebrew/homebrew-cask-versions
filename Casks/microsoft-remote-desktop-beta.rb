94313ec2f0123c325b878cbddf0f063433dc1008
Oli Walker <oliwalker@btinternet.com>
2019-11-16 17:31:26 +0000

    Update microsoft-remote-desktop-beta from 10.3.5.1723,405 to null.null,null

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,5 +1,5 @@
 cask 'microsoft-remote-desktop-beta' do
-  version '10.3.5.1723,405'
+  version 'null.null,null'
   sha256 '7ec1d24fc3e6843751bc67b71c66c1659bdd8345d561f0dbf5dbba9b919852e5'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
