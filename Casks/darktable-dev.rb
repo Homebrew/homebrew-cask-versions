0f692a661c83892b68f83adaeb0650b77303d23b
Yurii Kolesnykov <root@yurikoles.com>
2019-11-17 12:16:59 +0200

    fix checksum, add conflict with stable cask
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/darktable-dev.rb
+++ Casks/darktable-dev.rb
@@ -1,12 +1,14 @@
 cask 'darktable-dev' do
   version '3.0.0rc1'
-  sha256 'fcec1f254b1313904778f0e5a4dc651a753429acc43756a292aee5469a2e6490'
+  sha256 'eb874c80648b26c3b91db7ff77fadef858bd24fcccd63da1ebfdd97a896c00e5'
 
   # github.com/darktable-org/darktable was verified as official when first introduced to the cask
   url "https://github.com/darktable-org/darktable/releases/download/release-#{version}/darktable-#{version}.dmg"
   appcast 'https://github.com/darktable-org/darktable/releases.atom'
   name 'darktable'
   homepage 'https://www.darktable.org/'
+
+  conflicts_with cask: 'darktable'
 
   app 'darktable.app'
 end
