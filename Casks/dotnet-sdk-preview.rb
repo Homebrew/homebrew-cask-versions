6fc0d374099cb6070fe564ea650a5fb099e33d08
iamjoeker <692748+iamjoeker@users.noreply.github.com>
2019-11-19 16:56:01 -0600

    Update dotnet-sdk-preview to 3.1.100-preview3-014645

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,6 +1,6 @@
-cask 'dotnet-sdk-preview' do
-  version '3.1.100-preview1-014459,516f4938-dcb8-4e5a-aefc-0e7b11730a4b:9afc9a668d97ba280aa1962194305ba3'
-  sha256 'c155f28acbe8570457d9e0fb79f5d5428f0f9a287b08e7c55cb4e79a9a582b71'
+cask 'dotnet-sdk-preview' do  
+  version '3.1.100-preview3-014645,f4377189-a171-425b-8ef6-f8f21e89a8b0:b0d3561b13bd43ab4771bb62a2fddd4d'
+  sha256 '61fd5b2630a3ac536cfbf66172f62a246ccf87c7fe7988b188e257089eb0a4aa'
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   appcast "https://dotnet.microsoft.com/download/dotnet-core/#{version.major_minor}"
