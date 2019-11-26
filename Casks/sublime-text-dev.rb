a2286ec21883be3f50eab40c1d5415132709be7f
Johannes Rappen <jrappen@users.noreply.github.com>
2019-11-27 00:02:24 +0100

    Update sublime-text-dev to 4056
    - appcasts now use JSON format instead of XML
      - similar to sublime-merge(-dev)
    - homepage hasn't been, yet
    - running `subl --safe-mode` mimicks a clean install with its own application data
      - cleared upon next launch in safe mode

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,12 +1,11 @@
 cask 'sublime-text-dev' do
-  version '3.210'
-  sha256 'a52f309f5dc708b016ceb8ee0960d24252050c8fcc9f3a6f22c275b8a27e1767'
+  version '4.056'
+  sha256 '475c2fae70ee79e67c994e53e53e8b3d3a4d1bb89c139efabbc495d5fee5cb99'
 
-  url "https://download.sublimetext.com/Sublime%20Text%20Build%20#{version.no_dots}.dmg"
-  appcast "https://www.sublimetext.com/updates/#{version.major}/dev/appcast_osx.xml",
-          configuration: version.no_dots
+  url "https://download.sublimetext.com/sublime_text_build_#{version.no_dots}_mac.zip"
+  appcast "https://www.sublimetext.com/updates/#{version.major}/dev_update_check"
   name 'Sublime Text'
-  homepage "https://www.sublimetext.com/#{version.major}dev"
+  homepage "https://www.sublimetext.com/dev"
 
   auto_updates true
   conflicts_with cask: 'sublime-text'
@@ -17,7 +16,8 @@ cask 'sublime-text-dev' do
   uninstall quit: "com.sublimetext.#{version.major}"
 
   zap trash: [
-               "~/Library/Application Support/Sublime Text #{version.major}",
+               "~/Library/Application Support/Sublime Text",
+               "~/Library/Application Support/Sublime Text (Safe Mode)",
                "~/Library/Caches/com.sublimetext.#{version.major}",
                "~/Library/Preferences/com.sublimetext.#{version.major}.plist",
                "~/Library/Saved Application State/com.sublimetext.#{version.major}.savedState",
