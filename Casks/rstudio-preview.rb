6ad9129803b33fb76dcc56d3fdd99284c4bc4d04
Colin Rundel <rundel@gmail.com>
2019-12-02 10:49:34 +0000

    Update rstudio-preview from 1.2.5025 to 1.2.5031 (#8255)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.2.5025'
-  sha256 '95a5e41c36f7194efd38893854f35555e06ae25fe717140bd2939aba255f618b'
+  version '1.2.5031'
+  sha256 '45d2f45f812d1d51fe457769d098679d8c4f7bfa1cfa81147c3b064cd297d7ac'
 
   # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
