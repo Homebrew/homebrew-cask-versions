a1701a8f62b8489d8d64da46292d562d4462591e
Yannic Haupenthal <yhaupenthal@posteo.de>
2019-12-04 13:26:54 +0100

    Update vivaldi-snapshot from 2.9.1735.3 to 2.9.1745.9

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi-snapshot' do
-  version '2.9.1735.3'
-  sha256 '3eb2c9bfb4d6152825f1a71448a680358885d7a8eb75f6d5e5c3cb02ee2a1c3d'
+  version '2.9.1745.9'
+  sha256 '45bf3d46873e94407c294ee690b45f1072ec8989587fd9caeaee7a806c77942d'
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml'
