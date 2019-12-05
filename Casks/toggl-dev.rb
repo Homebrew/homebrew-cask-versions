a9f5143b005ac3b0201c5488dd2646edaadc514d
Yurii Kolesnykov <root@yurikoles.com>
2019-12-05 10:31:30 +0200

    update toggl-dev.rb
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/toggl-dev.rb
+++ Casks/toggl-dev.rb
@@ -1,9 +1,9 @@
 cask 'toggl-dev' do
-  version '7.4.1065'
-  sha256 '7d5a5d62dbb97d3074ddfc29653279ee9c1b63c03579e0054093d418da41c1d8'
+  version '7.4.1064'
+  sha256 '2392342a759d5405dd65162df04a44cf127367911481c1b85678248cd4125f35'
 
   # github.com/toggl-open-source/toggldesktop was verified as official when first introduced to the cask
-  url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version}.dmg"
+  url "https://github.com/toggl-open-source/toggldesktop/releases/download/v#{version}/TogglDesktop-#{version.dots_to_underscores}.dmg"
   appcast 'https://assets.toggl.com/installers/darwin_dev_appcast.xml'
   name 'TogglDesktop'
   homepage 'https://www.toggl.com/'
