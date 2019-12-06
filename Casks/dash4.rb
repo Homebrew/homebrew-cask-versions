d2eeb6f9a3269d0b898eedcb1aef56fae4937083
Oleg <epushiron@gmall.com>
2019-12-06 12:09:30 +0500

    Add dash4 v4.6.7 for dash v4 license owners
    Copied from https://github.com/Homebrew/homebrew-cask/blob/baf4f35e70c225fe1a8a60ec3b4e22604187238d/Casks/dash.rb

---
+++ Casks/dash4.rb
@@ -0,0 +1,22 @@
+cask 'dash4' do
+  version '4.6.7'
+  sha256 'e2b5eb996645b25f12ccae15e24b1b0d8007bc5fed925e14ce7be45a2b693fb6'
+
+  url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
+  appcast "https://kapeli.com/Dash#{version.major}.xml"
+  name 'Dash'
+  homepage 'https://kapeli.com/dash'
+
+  auto_updates true
+
+  app 'Dash.app'
+
+  zap trash: [
+               '~/Library/Application Support/Dash',
+               '~/Library/Application Support/com.kapeli.dashdoc',
+               '~/Library/Caches/com.kapeli.dashdoc',
+               '~/Library/Cookies/com.kapeli.dashdoc.binarycookies',
+               '~/Library/Logs/Dash',
+               '~/Library/Preferences/com.kapeli.dashdoc.plist',
+             ]
+end
