17aaf62326efbae5d83ebae2ea777d0e2e02e1e3
Vítor Galvão <info@vitorgalvao.com>
2019-12-12 13:51:32 +0000

    microsoft-remote-desktop-beta.rb: update to 10.3.7.1733,420

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-remote-desktop-beta' do
-  version '10.3.7.1731,417'
-  sha256 'e1c9d863663ffb16677a7d7c28df0c7b3b5ba51e9db55e6381900503ced44623'
+  version '10.3.7.1733,420'
+  sha256 '908890fd77ad59f6cb3f5916dedbb6aab553d16f185ce2a6bf5c4cdf5937dca3'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
