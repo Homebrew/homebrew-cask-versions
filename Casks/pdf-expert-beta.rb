86e50608cab6debedef6975502cf4e5985e372df
Robert Blair <robertleeblairjr@gmail.com>
2019-12-12 06:30:10 -0600

    Update pdf-expert-beta from 2.5,657 to 2.5,660

--- Casks/pdf-expert-beta.rb
+++ Casks/pdf-expert-beta.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert-beta' do
-  version '2.5,657'
-  sha256 '64f9b19cb65ecd4ff500b54106698ec6a070b5de99a5f3b795c9174245a96e02'
+  version '2.5,660'
+  sha256 '7407fee12905fd3d1170471cd7ad234796373e16fffad7369b4f7915d174e9fc'
 
   # d1ke680phyeohy.cloudfront.net was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
