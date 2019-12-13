61b07b1c155896b3229cf0f5a61a19e2621d2133
Vítor Galvão <info@vitorgalvao.com>
2019-12-13 17:08:41 +0000

    microsoft-edge-canary.rb: do not block operations

--- Casks/microsoft-edge-canary.rb
+++ Casks/microsoft-edge-canary.rb
@@ -1,19 +1,30 @@
 cask 'microsoft-edge-canary' do
   module Utils
+    @file_version = :latest
+
+    def self.version(version)
+      @file_version = version
+    end
+
     def self.file_version
-      require 'open-uri'
-      appcast = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDCN01-chk.xml'
-      appcast_version = URI(appcast).read.scan(%r{<string>(.*)<\/string>})[2].join.split('.')
-      # Parsing logic clarified by vendor, please review https://github.com/Homebrew/homebrew-cask-versions/issues/7778#issuecomment-525870763 prior to modification
-      "#{appcast_version[0]}.0.#{appcast_version[1]}.#{appcast_version[2][6..-1]}"
+      @file_version
     end
   end
 
   version :latest
   sha256 :no_check
 
-  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeCanary-#{Utils.file_version}.pkg"
+  url do
+    require 'open-uri'
+    appcast = 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDCN01-chk.xml'
+    appcast_version = URI(appcast).read.scan(%r{<string>(.*)<\/string>})[2].join.split('.')
+    # Parsing logic clarified by vendor, please review https://github.com/Homebrew/homebrew-cask-versions/issues/7778#issuecomment-525870763 prior to modification
+    url_version = "#{appcast_version[0]}.0.#{appcast_version[1]}.#{appcast_version[2][6..-1]}"
+    Utils.version(url_version)
+
+    # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
+    "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeCanary-#{Utils.file_version}.pkg"
+  end
   name 'Microsoft Edge Canary'
   homepage 'https://www.microsoftedgeinsider.com/'
 
