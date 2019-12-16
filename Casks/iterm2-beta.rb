52148790a837a099bf8b1af87b882dea8874276a
David Yantis <404NetworkError@users.noreply.github.com>
2019-12-15 20:04:12 -0500

    Update iterm2-beta from 3.3.8beta1 to 3.3.8beta2 (#8320)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 cask 'iterm2-beta' do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version '3.3.8beta1'
-  sha256 'cc71fa2e515a74ee8d4b70b9c91ebb6627dcd409f9dded180b20fcdba454fb00'
+  version '3.3.8beta2'
+  sha256 '9b07fad5549de04b88599d88c4435decb17a41ebf863195f88d0d950405e0a60'
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   appcast 'https://iterm2.com/appcasts/testing3.xml'
