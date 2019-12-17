30d7d8efe332cb7b2e68b3d98fe2cb6b12d7b4a7
ben-ihelputech <50592898+ben-ihelputech@users.noreply.github.com>
2019-12-16 19:49:11 -0600

    Update chirp-daily from 20191029 to 20191206 (#8326)

--- Casks/chirp-daily.rb
+++ Casks/chirp-daily.rb
@@ -1,6 +1,6 @@
 cask 'chirp-daily' do
-  version '20191029'
-  sha256 'f688fdf5184b64e1f9df346311b15ac514d67b07ac169faaa751914d861ca2c0'
+  version '20191206'
+  sha256 'c3f9ab4af1f19971cc75adfa82048c5b2cb66332a47c423256e61630d1029e11'
 
   url "https://trac.chirp.danplanet.com/chirp_daily/LATEST/chirp-daily-#{version}.app.zip"
   appcast 'https://trac.chirp.danplanet.com/chirp_daily/LATEST/SHA1SUM'
