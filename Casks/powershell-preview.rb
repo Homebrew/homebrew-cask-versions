86487973fcc5e4aff2bb1e53e005d5b5e4262a8f
Brett Miller <brett@millerb.co.uk>
2019-12-17 11:29:42 +0000

    Update powershell-preview to 7.0.0-rc.1

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -1,6 +1,6 @@
 cask 'powershell-preview' do
-  version '7.0.0-preview.6'
-  sha256 '37ea862fa8919167c5a9d43664ee71af6250befeecd2d38239999c6ce1c36785'
+  version '7.0.0-rc.1'
+  sha256 '4260EAA0DAFA9A48787D5F967052A774092189B6A614EED170DEC9AAD556FD9C'
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
   appcast 'https://github.com/PowerShell/PowerShell/releases.atom'
