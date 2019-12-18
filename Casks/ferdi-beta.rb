6d4573f24625acf7c27ab43e7a229190ea2168d8
Vijay Raghavan Aravamudhan <vraravam@users.noreply.github.com>
2019-12-18 22:47:16 +0530

    Upgraded ferdi to '5.4.1-beta.3'. (#8337)

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,6 +1,6 @@
 cask 'ferdi-beta' do
-  version '5.4.1-beta.2'
-  sha256 '0afa468dfcd9fd9b82d17590abcdf01a3c98c26ca89ee59f4f535c60409705b8'
+  version '5.4.1-beta.3'
+  sha256 '396f7a049c4cd8cd25df924addeb006aaa6454c79cae18cc69f036d7204333d4'
 
   # github.com/getferdi/ferdi was verified as official when first introduced to the cask
   url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg"
