bd4fb30a3927ec76eedf35da03a39f5c3f3b7fdd
Alejandro Leal <ajleal@msn.com>
2020-01-13 11:12:56 -0500

    Update termius-beta to v.5.3.2

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,6 +1,6 @@
 cask 'termius-beta' do
-  version '5.2.3'
-  sha256 'e1d05b515cca9f27be48e47501592508aa292d56b685767a36a43ca35bcc3f00'
+  version '5.3.2'
+  sha256 '1a695ab916d5e35348a64a35ffd522d27f2019ed96c543eb726bb0e53fcc5769'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://www.termius.com/beta/download/mac/Termius+Beta.dmg'
