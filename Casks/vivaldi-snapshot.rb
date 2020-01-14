3f1663c428fb7bed4371ca9860bd5859c54daadb
Yannic Haupenthal <yhaupenthal@posteo.de>
2020-01-14 23:41:06 +0100

    Update vivaldi-snapshot from 2.10.1745.7 to 2.11.1789.3

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi-snapshot' do
-  version '2.10.1745.7'
-  sha256 '256fb58981df3237575c329fe1c67e2ac3de1d0d3f7cd32adb003ccb03107d5a'
+  version '2.11.1789.3'
+  sha256 'a9c5565e1123a64fea10d4105d809e0b0a38ec3e1161902a7f89c8482bc2f5c0'
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml'
