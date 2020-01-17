f5a0813d1fae71356f760f12443b4022a495e6b9
David Yantis <404NetworkError@users.noreply.github.com>
2020-01-17 06:17:22 -0500

    Update texstudio-beta from 2.12.22beta1 to 2.12.22beta2 (#8426)

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -1,6 +1,6 @@
 cask 'texstudio-beta' do
-  version '2.12.22beta1'
-  sha256 '5231402eba212a5cc3ce990e3e866c9374128b987427e1f0d0327b810277fe6b'
+  version '2.12.22beta2'
+  sha256 'e8b06b108321dbf238327a3645d6b8f21126f14819e09e6df3bad1ca29c63dfa'
 
   # github.com/texstudio-org/texstudio was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg"
