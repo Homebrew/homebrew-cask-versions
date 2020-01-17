c537b110b91ddd5d259aa767e4e5df388f869c05
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-01-17 12:43:44 +0100

    Upgrade java-beta from 14,21 to 14,32

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,6 +1,6 @@
 cask 'java-beta' do
-  version '14,21'
-  sha256 '6d775c1a53b5316fc227370fcd5720ad52482f537a4676c714be4057feee11c8'
+  version '14,32'
+  sha256 'c2a883e5e117f534c3c7d6783dd422101033a0eab172e72f7e38781f727619df'
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK Early Access Java Development Kit'
