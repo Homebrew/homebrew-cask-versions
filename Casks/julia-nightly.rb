7e1f22ea21c462095516f31071924f30d96f0979
Rory Finnegan <rory.finnegan@gmail.com>
2020-01-22 12:18:05 -0600

    Update julia-nightly from 1.4 to 1.5 (#8437)
    * Update julia-nightly from 1.4 to 1.5
    
    * Specify julia-nightly as the target for the nightly cask

--- Casks/julia-nightly.rb
+++ Casks/julia-nightly.rb
@@ -1,5 +1,5 @@
 cask 'julia-nightly' do
-  version '1.4'
+  version '1.5'
   sha256 :no_check # required as upstream package is updated in-place
 
   url 'https://julialangnightlies-s3.julialang.org/bin/mac/x64/julia-latest-mac64.dmg'
@@ -7,7 +7,7 @@ cask 'julia-nightly' do
   homepage 'https://julialang.org/'
 
   app "Julia-#{version}.app"
-  binary "#{appdir}/Julia-#{version}.app/Contents/Resources/julia/bin/julia"
+  binary "#{appdir}/Julia-#{version}.app/Contents/Resources/julia/bin/julia", target: 'julia-nightly'
 
   zap trash: '~/.julia'
 end
