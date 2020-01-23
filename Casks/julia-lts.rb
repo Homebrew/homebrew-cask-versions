1ecb1d76752f37a8f54ae61ebcf19d1d44704dcb
Rory Finnegan <rory.finnegan@gmail.com>
2020-01-23 10:19:52 -0600

    Adds a julia-lts cask set to julia version 1.0.5 (#8438)

---
+++ Casks/julia-lts.rb
@@ -0,0 +1,13 @@
+cask 'julia-lts' do
+  version '1.0.5'
+  sha256 '1ff7bd41f396ba3f34dc17c26b78d11adeadfafdea537f7ad17020ef812a39a0'
+
+  url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
+  name 'Julia'
+  homepage 'https://julialang.org/'
+
+  app "Julia-#{version.major_minor}.app"
+  binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia", target: 'julia-lts'
+
+  zap trash: '~/.julia'
+end
