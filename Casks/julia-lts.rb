23bf0383c58d45b424e5597ee9f8d631866d9902
Alexander Ronald Altman <alexanderaltman@me.com>
2020-01-24 01:18:04 -0800

    Add appcast to `julia-lts`. (#8459)

--- Casks/julia-lts.rb
+++ Casks/julia-lts.rb
@@ -3,6 +3,7 @@ cask 'julia-lts' do
   sha256 '1ff7bd41f396ba3f34dc17c26b78d11adeadfafdea537f7ad17020ef812a39a0'
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
+  appcast 'https://github.com/JuliaLang/julia/releases.atom'
   name 'Julia'
   homepage 'https://julialang.org/'
 
