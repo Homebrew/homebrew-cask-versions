1cfb13c230e3f472826eff345c8390dda257c9a2
Matt C. Wilson <matthew.c.wilson@gmail.com>
2020-01-24 02:15:55 -0500

    Reintroduce airmail-beta due to reavailability (#8446)

---
+++ Casks/airmail-beta.rb
@@ -0,0 +1,14 @@
+cask 'airmail-beta' do
+  version '4.0,450'
+  sha256 '09f6728d73531b98150f888f913dffed5d31dbc01ec93bb9d9ab29ea96d5457f'
+
+  # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
+  url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
+  appcast 'https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04'
+  name 'Airmail'
+  homepage 'https://airmailapp.com/beta/'
+
+  app 'Airmail Beta.app'
+
+  uninstall quit: 'it.bloop.airmail.beta11'
+end
