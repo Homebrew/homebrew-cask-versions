53ea2bc84e8406a6edc5540c9f6d85481a7280c6
kPherox <admin@mail.kr-kp.com>
2020-01-30 02:32:12 +0900

    Update airmail-beta to 4.0,456 (#8481)
    * Update airmail-beta to 4.0,456
    
    Include mctoken in version as it will be needed when downloading old version。
    
    * Update airmail-beta.rb
    
    * Update airmail-beta.rb

--- Casks/airmail-beta.rb
+++ Casks/airmail-beta.rb
@@ -1,6 +1,6 @@
 cask 'airmail-beta' do
-  version '4.0,450'
-  sha256 '09f6728d73531b98150f888f913dffed5d31dbc01ec93bb9d9ab29ea96d5457f'
+  version '4.0,456'
+  sha256 'b319e3ab6e7ffc159bac49c3c882e50d44c472c0745d09e6fb41436d26fb61fd'
 
   # hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04 was verified as official when first introduced to the cask
   url "https://rink.hockeyapp.net/api/2/apps/84be85c3331ee1d222fd7f0b59e41b04/app_versions/#{version.after_comma}?format=zip&"
