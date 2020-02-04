0847d98124eb2173dffed28de5342f63bc977e1e
David Yantis <404NetworkError@users.noreply.github.com>
2020-02-04 17:19:09 -0500

    Update brave-browser-dev from 79.1.4.66,104.66 to 79.1.4.76,104.76 (#8504)

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser-dev' do
-  version '79.1.4.66,104.66'
-  sha256 'eeb03b69d7238fbb09a71fb610ebe169a7f053c147949537a3be70442e46eebc'
+  version '79.1.4.76,104.76'
+  sha256 'a5298975cd2cf22bde89b3ece303f79cc630f310e835385ad00ca2786ff17f55'
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev/#{version.after_comma}/Brave-Browser-Dev.dmg"
