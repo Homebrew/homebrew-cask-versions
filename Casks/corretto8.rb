daf2591998f6374092a68faa037ca96590d171e8
Thomas Van Doren <thomas.vandoren@gmail.com>
2020-02-06 10:03:27 -0800

    Update corretto8 version to 8.242.08.1

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -1,8 +1,8 @@
 cask 'corretto8' do
-  version '8.242.08.1-1'
+  version '8.242.08.1'
   sha256 'bf45f7e741124ef0086e31261c93b5462ac490f350a47c988ad8c41a70d01a26'
 
-  url "https://corretto.aws/downloads/resources/#{version.major_minor_patch}/amazon-corretto-#{version}-1-macosx-x64.pkg"
+  url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-1-macosx-x64.pkg"
   appcast "https://docs.aws.amazon.com/en_us/corretto/latest/corretto-#{version.major}-ug/corretto-#{version.major}-ug.rss"
   name 'Amazon Corretto'
   homepage 'https://corretto.aws/'
