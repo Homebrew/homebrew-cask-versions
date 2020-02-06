893dc5967a7af767d036512268243027adef9a9e
Vítor Galvão <info@vitorgalvao.com>
2020-02-06 17:47:53 +0000

    Update corretto8.rb

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -2,12 +2,12 @@ cask 'corretto8' do
   version '8.242.08.1-1'
   sha256 'bf45f7e741124ef0086e31261c93b5462ac490f350a47c988ad8c41a70d01a26'
 
-  url "https://corretto.aws/downloads/resources/#{version.major_minor_patch}.#{version.split('.').last.split('-').first}/amazon-corretto-#{version}-macosx-x64.pkg"
+  url "https://corretto.aws/downloads/resources/#{version.major_minor_patch}/amazon-corretto-#{version}-1-macosx-x64.pkg"
   appcast "https://docs.aws.amazon.com/en_us/corretto/latest/corretto-#{version.major}-ug/corretto-#{version.major}-ug.rss"
   name 'Amazon Corretto'
   homepage 'https://corretto.aws/'
 
-  pkg "amazon-corretto-#{version}-macosx-x64.pkg"
+  pkg "amazon-corretto-#{version}-1-macosx-x64.pkg"
 
   uninstall pkgutil: "com.amazon.corretto.#{version.major}"
 end
