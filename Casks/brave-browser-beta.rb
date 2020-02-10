d81024807acae229f9ffc1639377022fa464abd5
Julian Mayer <julian@corecode.io>
2020-02-10 16:19:25 +0100

    Update brave-browser-beta from 79.1.4.80,104.80 to 80.1.4.80,104.80

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,5 +1,5 @@
 cask 'brave-browser-beta' do
-  version '79.1.4.80,104.80'
+  version '80.1.4.80,104.80'
   sha256 'a1a1e2f190bd2d7b39cec755b63a7d3423b89fcd4cf65fe5c861ee0312a440ee'
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser was verified as official when first introduced to the cask
