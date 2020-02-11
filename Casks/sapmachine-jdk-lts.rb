22130c3e726fd6673b51330a346c2a38869abca2
Alexander Ronald Altman <alexanderaltman@me.com>
2020-02-10 21:20:29 -0800

    sapmachine-jdk-lts 11.0.6 (new cask)
    Copied and modififed from `homebrew/cask/sapmachine-jdk`.

---
+++ Casks/sapmachine-jdk-lts.rb
@@ -0,0 +1,12 @@
+cask 'sapmachine-jdk-lts' do
+  version '11.0.6'
+  sha256 '53f5770b08e146cd3cf582e1485feb370cd4c393856f3e7c7234ce07af00b041'
+
+  # github.com/SAP/SapMachine was verified as official when first introduced to the cask
+  url "https://github.com/SAP/SapMachine/releases/download/sapmachine-#{version}/sapmachine-jdk-#{version}_osx-x64_bin.dmg"
+  appcast "https://sap.github.io/SapMachine/latest/#{version.major}"
+  name 'SapMachine OpenJDK Development Kit, LTS release'
+  homepage 'https://sapmachine.io/'
+
+  artifact "sapmachine-jdk-#{version}.jdk", target: "/Library/Java/JavaVirtualMachines/sapmachine-jdk-#{version}.jdk"
+end
