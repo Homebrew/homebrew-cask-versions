ff827b3e07a48c1652d442ba50a9133105ae8912
Vijay Raghavan Aravamudhan <vraravam@users.noreply.github.com>
2020-02-11 19:32:58 +0400

    Corrected folder/process names for ferdi installation and uninstallation. (#8525)

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -5,16 +5,25 @@ cask 'ferdi-beta' do
   # github.com/getferdi/ferdi was verified as official when first introduced to the cask
   url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg"
   appcast 'https://github.com/getferdi/ferdi/releases.atom'
-  name 'ferdi'
+  name 'Ferdi'
   homepage 'https://getferdi.com/'
 
   conflicts_with cask: 'ferdi'
 
-  app 'ferdi.app'
-  binary "#{appdir}/ferdi.app/Contents/MacOS/Ferdi"
+  app 'Ferdi.app'
+
+  uninstall quit:   'com.kytwb.ferdi',
+            delete: '/Library/Logs/DiagnosticReports/Ferdi Helper_.*wakeups_resource.diag'
 
   zap trash: [
+               '~/Library/Application Support/Caches/ferdi-updater',
                '~/Library/Application Support/Ferdi',
+               '~/Library/Caches/com.kytwb.ferdi',
+               '~/Library/Caches/com.kytwb.ferdi.ShipIt',
+               '~/Library/Logs/Ferdi',
+               '~/Library/Preferences/ByHost/com.kytwb.ferdi.ShipIt.*.plist',
+               '~/Library/Preferences/com.electron.ferdi.helper.plist',
+               '~/Library/Preferences/com.electron.ferdi.plist',
                '~/Library/Preferences/com.kytwb.ferdi.plist',
                '~/Library/Saved Application State/com.kytwb.ferdi.savedState',
              ]
