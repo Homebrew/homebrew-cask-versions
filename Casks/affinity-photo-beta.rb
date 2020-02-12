a1916b635493d1ada7a5c182b81289f9697860d7
thirdtapbot <w4FYuxsZ2UfF9s@teleosaurs.xyz>
2020-02-12 02:01:17 -0800

    affinity-photo-beta: Updated to 1.8.0.164

--- Casks/affinity-photo-beta.rb
+++ Casks/affinity-photo-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-photo-beta' do
-  version '1.8.0.163'
-  sha256 '82030cd223fc6fe3ada84932263c2e670ce881ee99af61af6a291df5ea355d33'
+  version '1.8.0.164'
+  sha256 '537042cb0a4961e03c554d87c8db3970b028b35385b962fd31b29de2520c0363'
 
   # affinity-beta.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Photo%20Customer%20Beta.zip'
