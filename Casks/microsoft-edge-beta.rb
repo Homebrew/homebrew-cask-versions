bd893753473f9f16095ee04a8b09eef4b750c3d3
CoreCode <core-code@users.noreply.github.com>
2020-02-13 23:11:34 +0100

    Update microsoft-edge-beta.rb

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -4,8 +4,7 @@ cask 'microsoft-edge-beta' do
 
   # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg"
-  appcast 'https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/0409EDBT01-chk.xml',
-          configuration: "#{version.major}.#{version.patch}"
+  appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069439'
   name 'Microsoft Edge Beta'
   homepage 'https://www.microsoftedgeinsider.com/'
 
