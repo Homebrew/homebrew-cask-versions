97eeb5b6ebb69571af10e66120c5e181646a871b
Igor Oliinyk <curtdp@gmail.com>
2020-02-13 20:49:04 +0200

    Added lang parameter to appcast url

--- Casks/firefox-developer-edition.rb
+++ Casks/firefox-developer-edition.rb
@@ -123,7 +123,7 @@ cask 'firefox-developer-edition' do
 
   # download-installer.cdn.mozilla.net/pub/firefox/releases was verified as official when first introduced to the cask
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}/mac/#{language}/Firefox%20#{version}.dmg"
-  appcast 'https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.mozilla.org/%3Fproduct=firefox-devedition-latest-ssl%26os=osx'
+  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.mozilla.org/%3Fproduct=firefox-devedition-latest-ssl%26os=osx%26lang=#{language}"
   name 'Mozilla Firefox Developer Edition'
   homepage 'https://www.mozilla.org/firefox/developer/'
 
