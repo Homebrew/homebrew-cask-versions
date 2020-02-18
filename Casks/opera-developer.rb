4b3b402ef70b11314ebbc2dfb7923bf877fee1f3
Work <brigitte@corecode.io>
2020-02-18 19:19:34 +0100

    Update opera-developer from 68.0.3590.0 to 68.0.3602.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask 'opera-developer' do
-  version '68.0.3590.0'
-  sha256 '2b67cbdad40746f99961966e0b30df5288abd291e5becf25c1e6252ab2bc3ff2'
+  version '68.0.3602.0'
+  sha256 '89c09681e8f549714f5f3a3f9f2c1c6e1c655069aa74c8273910218d203d7812'
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name 'Opera Developer'
