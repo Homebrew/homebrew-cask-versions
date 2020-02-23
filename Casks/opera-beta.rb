b9be770cbc1b0e193923370aa9dadb01d55ba76d
suschizu <brigitte@corecode.io>
2020-02-23 18:47:28 +0100

    Update opera-beta from 67.0.3575.23 to 67.0.3575.28 (#8623)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '67.0.3575.23'
-  sha256 '9de0cb84de30df696c48b634817ee00991cd203f0e7ba7662e7c8d7c3d304921'
+  version '67.0.3575.28'
+  sha256 '5de30ce0bad6b360cc0f52e130c089cd5834afc4d3b1a31a77b5a40edbd7b7c6'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
