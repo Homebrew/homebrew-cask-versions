46b78cfa15d9ddc0a313bb92bf0992361f32377c
Luis Puerto <luis@luispuerto.net>
2020-02-29 21:32:40 +0200

    bump rstudio preview to 1.3.842 (#8628)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.3.820'
-  sha256 '94131415750d5769f69ba85b58df6c8554a7a2956abad9baa55df9d53835aa84'
+  version '1.3.878'
+  sha256 '54373433731e68942c1817031a69091c8c767d3fb02ec432d2de010fbfa7ec67'
 
   # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
