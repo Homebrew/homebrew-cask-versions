26814b2026eba471e731791a299ed26fbb93e15f
BrewTestBot <homebrew-ops@lists.sfconservancy.org>
2020-03-05 21:47:31 +1100

    Update termius-beta from 5.5.0 to 5.5.2 (#8688)

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,6 +1,6 @@
 cask 'termius-beta' do
-  version '5.5.0'
-  sha256 '2493b2d5156f185be59e01408d4249f32586c0a723496a97d78112b9279a9e02'
+  version '5.5.2'
+  sha256 '796e33697ada8dd9ce0bf1d79c7f1d13ef4661f957a7aa785027e34c6a73ce00'
 
   # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
   url 'https://www.termius.com/beta/download/mac/Termius+Beta.dmg'
