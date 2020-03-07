eb97f5d8b45017a251e0d39b54277ae97a1143a2
thirdtapbot <w4FYuxsZ2UfF9s@teleosaurs.xyz>
2020-03-07 02:01:22 -0800

    affinity-designer-beta: Updated to 1.8.2.1

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-designer-beta' do
-  version '1.8.2.0'
-  sha256 '6cebbdde7229744b8e144f207dc9d8850062a15a67deaf34faafea0ca5300353'
+  version '1.8.2.1'
+  sha256 'fb50bbfb867271531d59836dc66dea0e9ab6d7b8abc21d1c78d303f753b25c5c'
 
   # affinity-beta.s3.amazonaws.com was verified as official when first introduced to the cask
   url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg'
