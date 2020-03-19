02e17b13be2c961aa0c0c7faa31871682310bba2
Yannic Haupenthal <yhaupenthal@posteo.de>
2020-03-19 10:24:54 +0100

    Update vivaldi-snapshot from 2.11.1811.28 to 2.12.1854.5

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask 'vivaldi-snapshot' do
-  version '2.11.1811.28'
-  sha256 '2f3b3092c07829a13cdfbcb601c7d1d466331f71755cccf8e05bfb0fb829d444'
+  version '2.12.1854.5'
+  sha256 '57474008497cb62ef5254623a9f2ff032afd1b8c3e6fa06d1cc989d6a34bf71b'
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.dmg"
   appcast 'https://update.vivaldi.com/update/1.0/mac/appcast.xml'
