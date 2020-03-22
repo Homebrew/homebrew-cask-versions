b334a1fce3be934325a473e9092e0c4d40cf3d01
Alexander Ronald Altman <alexanderaltman@me.com>
2020-03-22 14:38:33 -0700

    zulu13 13.0.2,13.29.9-ca (new cask) (#8772)
    * zulu13 13.0.2,13.29.9-ca (new cask)
    
    Ported over from Homebrew/homebrew-cask because 14 is out but 13 is still an MTS release.  See [this page](https://www.azul.com/downloads/zulu-community/):
    > The current LTS (Long Term Supported) major Zulu versions are 7, 8, and 11. The latest MTS (Medium Term Support) major Zulu version is 13 (see DZone Reference Card on Java 13 for more details).
    
    * Update zulu13.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/zulu13.rb
@@ -0,0 +1,13 @@
+cask 'zulu13' do
+  version '13.0.2,13.29.9-ca'
+  sha256 'e4c7f6bc9caa1bba2659e8d0019e75002b84b43b4bd67de7e740d6698f629900'
+
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+      referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
+  name 'Azul Zulu Java Standard Edition Development Kit'
+  homepage 'https://www.azul.com/downloads/zulu/zulu-mac/'
+
+  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+end
