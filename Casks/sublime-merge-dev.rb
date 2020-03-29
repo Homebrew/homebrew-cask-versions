2b3ef9fab2d99df7857d6c29c68bb7329c649666
BSKY <git@bsky.moe>
2020-03-30 04:23:42 +0900

    Bump sublime-merge-dev version from 2000 to 2002

--- Casks/sublime-merge-dev.rb
+++ Casks/sublime-merge-dev.rb
@@ -1,6 +1,6 @@
 cask 'sublime-merge-dev' do
-  version '2000'
-  sha256 '1cdde8b2eb90b60a3154a0d4751a89ba4c9de66fe1e73cc122a622652ae49639'
+  version '2002'
+  sha256 '5ba55e785edab77e8c4917b697b69a4c505e2ef71d7db59accc042f666064909'
 
   # download.sublimetext.com was verified as official when first introduced to the cask
   url "https://download.sublimetext.com/sublime_merge_build_#{version}_mac.zip"
