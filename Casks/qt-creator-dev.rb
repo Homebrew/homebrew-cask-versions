1f54e63f6d27e3c26ac6c70ebf7e20cc9e95ab10
Yurii Kolesnykov <root@yurikoles.com>
2020-03-31 19:57:23 +0300

    Update qt-creator-dev from 4.12.0-beta1 to 4.12.0-beta2
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/qt-creator-dev.rb
+++ Casks/qt-creator-dev.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator-dev' do
-  version '4.12.0-beta1'
-  sha256 'b76dce61602fa09d9dbafe10d05dc4ed6c0dc69b0d6b7b2dd0f47c69acfdb940'
+  version '4.12.0-beta2'
+  sha256 '4ec4f303ba8ec17401a5bcc0556d8ec82c131740c4aadb5e09c6b033c13fd649'
 
   url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator Dev'
