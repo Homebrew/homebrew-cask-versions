29b2542f64a6d0fde61c3b67fa345c3a5e07d17f
Thiago Bernardes de Almeida <thiago.almeida@mpdft.mp.br>
2020-04-04 17:59:26 -0300

    Update microsoft-remote-desktop-beta

--- Casks/microsoft-remote-desktop-beta.rb
+++ Casks/microsoft-remote-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-remote-desktop-beta' do
   version '10.3.9.1763,437'
-  sha256 'b42e35f7eb480f5c4957e3a9053b8da249f2bc3124efc239da21c3d4e3d8fbe1'
+  sha256 '9e7641619757d043dbff99b0a8c5946d4adc7e33dd5cc91421fa15e0f65227c7'
 
   url "https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06/app_versions/#{version.after_comma}?format=zip"
   appcast 'https://rink.hockeyapp.net/api/2/apps/5e0c144289a51fca2d3bfa39ce7f2b06',
