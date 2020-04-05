437aecbe02ce04b4c73ea4d5df70e42e45463f6d
Khaldoon Mutahar <git@mutahar.me>
2020-04-05 11:09:02 +0300

    Update Microsoft Office to 16.16.20 (20030700)
    Dated on March 10, 2020

--- Casks/microsoft-office-2016.rb
+++ Casks/microsoft-office-2016.rb
@@ -1,6 +1,6 @@
 cask 'microsoft-office-2016' do
-  version '16.16.20021000'
-  sha256 '7861fb32589449b37fc2abca813bbace09d17d6afbd3dd46a2bcf2cb2d9ec438'
+  version '16.16.20030700'
+  sha256 'e6b38e17fecf5bf67cb08c5ef405bcefe2079a079637d5800e1815b1fcb8e0b6'
 
   # officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/Microsoft_Office_#{version}_Installer.pkg"
