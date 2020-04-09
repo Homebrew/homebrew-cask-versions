e74460e361fa89592d3bfbb4a3177824d62523f7
CoreCode <core-code@users.noreply.github.com>
2020-04-09 21:02:49 +0200

    Update visual-studio-code-insiders from 1.44.0,15297a9a4ee8d611d37f48cb59c33b3cb6145c70 to 1.45.0,2225d77536d12fea2eda04930f431a5eb0930bba (#8907)

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,6 +1,6 @@
 cask 'visual-studio-code-insiders' do
-  version '1.44.0,15297a9a4ee8d611d37f48cb59c33b3cb6145c70'
-  sha256 '78260134b2f617c991095e4dca59642a9c5a8ed64edd140d08db48910e580b58'
+  version '1.45.0,2225d77536d12fea2eda04930f431a5eb0930bba'
+  sha256 'e7043d026beb00e749d99c8f25d4fb7bb61a8b274c6e741524989059d2e2eca9'
 
   # az764295.vo.msecnd.net/insider was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
