778a30724a491312ae0e2dfb143ea5a57fa124eb
Vijay A <avijayr@protonmail.com>
2020-04-09 22:58:43 +0530

    Upgraded keepassxc to 2.5.4

--- Casks/keepassxc-beta.rb
+++ Casks/keepassxc-beta.rb
@@ -1,6 +1,6 @@
 cask 'keepassxc-beta' do
-  version '2.5.3'
-  sha256 'fb9af9c2eab788e710f93165f063ed26281140266181d3904b4a6b2d62c5c9d8'
+  version '2.5.4'
+  sha256 '3d56ebbcb3471a2f6116abe884d1e7b662347b80e18cc0a12ca9fdf2c9a14d7a'
 
   # github.com/keepassxreboot/keepassxc was verified as official when first introduced to the cask
   url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}.dmg"
@@ -9,9 +9,18 @@ cask 'keepassxc-beta' do
   homepage 'https://keepassxc.org/'
 
   conflicts_with cask: 'keepassxc'
+  depends_on macos: '>= :high_sierra'
 
   app 'KeePassXC.app'
   binary "#{appdir}/KeePassXC.app/Contents/MacOS/keepassxc-cli"
 
-  zap trash: '~/.keepassxc'
+  zap trash: [
+               '~/.keepassxc',
+               '~/Library/Application Support/keepassxc',
+               '~/Library/Caches/org.keepassx.keepassxc',
+               '~/Library/Preferences/org.keepassx.keepassxc.plist',
+               '~/Library/Saved Application State/org.keepassx.keepassxc.savedState',
+               '~/Library/Logs/DiagnosticReports/KeePassXC_*.crash',
+               '~/Library/Application Support/CrashReporter/KeePassXC_*.plist',
+             ]
 end
