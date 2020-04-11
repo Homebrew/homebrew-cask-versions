dc0eb4686de3eb1f45ae597fe7cf2df8752e027c
Khushraj Rathod <khushrajrathod@gmail.com>
2020-04-11 08:11:20 +0000

    Mos 3 - Verify raw.githubusercontent.com as official for Github

--- Casks/mos3.rb
+++ Casks/mos3.rb
@@ -1,7 +1,8 @@
 cask 'mos3' do
   version '3.0.0'
   sha256 '274f984663696c59a3d69b959a4ae9a11c25e171573357e264784e04f9d2d27c'
-
+  
+  # raw.githubusercontent.com was verified as official when first introduced to the cask
   url "https://raw.githubusercontent.com/Caldis/Mos/master/Release/Mos.Versions.#{version}.dmg"
   name 'Mos'
   homepage 'https://github.com/Caldis/Mos/issues/207'
