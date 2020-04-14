2b2924c27aeaa1ebfb3e96594ff9f8806e236183
Robert Blair <robertleeblairjr@gmail.com>
2020-04-14 18:32:26 -0500

    Update zulu7 from 7.0.252,7.36.0.5-ca to 7.0.262,7.38.0.11-ca

--- Casks/zulu7.rb
+++ Casks/zulu7.rb
@@ -1,6 +1,6 @@
 cask 'zulu7' do
-  version '7.0.252,7.36.0.5-ca'
-  sha256 '7991a2c6beeb08bdfe4943d0f667f837f329e116753d4497b820b9f3f97f7d4c'
+  version '7.0.262,7.38.0.11-ca'
+  sha256 '1506d0ab687d6504b1159b048470982376a55a6925214ab26117f21d4fb0e392'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
