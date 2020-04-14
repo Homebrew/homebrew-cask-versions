4113e4dda546988c6a4e0679ad15b23efae9f2e9
Robert Blair <robertleeblairjr@gmail.com>
2020-04-14 18:33:37 -0500

    Update zulu11 from 11.0.6,11.37.17-ca to 11.0.7,11.39.15-ca

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -1,6 +1,6 @@
 cask 'zulu11' do
-  version '11.0.6,11.37.17-ca'
-  sha256 'bcc8ec14b084140941092e16c8d5183964f3c512a354535770897b0cf542730d'
+  version '11.0.7,11.39.15-ca'
+  sha256 '8322cb6498076f171ab5efa4101fe8207d9aa6517d1d743bbe4d753b67abc8cb'
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
