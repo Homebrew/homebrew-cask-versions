1f0849bd480012a5ffc43d43f4f99058ea27f4cd
Vijay A <avijayr@protonmail.com>
2020-04-19 19:24:44 +0530

    Upgraded ferdi to 5.5.0-gm

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,6 +1,6 @@
 cask 'ferdi-beta' do
-  version '5.4.4-beta.3'
-  sha256 '0bd9e5c18e6a076d289d50bd2155ccf467a4881de74d6827e705aba77b4a7dfb'
+  version '5.5.0-gm'
+  sha256 'e9ec9be368a51dcafe008eaa7e2c257ae082c8c6565efd6477e175049cb08754'
 
   # github.com/getferdi/ferdi was verified as official when first introduced to the cask
   url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg"
