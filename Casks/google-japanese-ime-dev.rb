b5fa1a6ed5c2f43effe0c5581440b428d57bec6a
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:26:27 +0100

    Update google-japanese-ime-dev.rb

--- Casks/google-japanese-ime-dev.rb
+++ Casks/google-japanese-ime-dev.rb
@@ -2,7 +2,7 @@ cask 'google-japanese-ime-dev' do
   version :latest
   sha256 :no_check
 
-  # google.com/japanese-ime/ was verified as official when first introduced to the cask
+  # dl.google.com/japanese-ime/ was verified as official when first introduced to the cask
   url 'https://dl.google.com/japanese-ime/dev/GoogleJapaneseInput.dmg'
   name 'Google Japanese Input Method Editor'
   homepage 'https://www.google.co.jp/ime/'
