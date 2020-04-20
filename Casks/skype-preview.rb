ef864a997a246768b4e5225dc024a435fead7880
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:22 +0100

    skype-preview.rb: Add trailing slash to verified comment

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -2,7 +2,7 @@ cask 'skype-preview' do
   version '8.55.0.135'
   sha256 '5c24605f89c5a88fa82c2652edf1926f5d9954c5d61e8bf9c55a895cfdc5817e'
 
-  # endpoint920510.azureedge.net/s4l/s4l/download/mac was verified as official when first introduced to the cask
+  # endpoint920510.azureedge.net/s4l/s4l/download/mac/ was verified as official when first introduced to the cask
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg"
   name 'Skype Preview'
   homepage 'https://www.skype.com/en/insider/'
