49186143bbccb733a2bb619d1d3713f1965d9ff5
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:55 +0100

    vox2.rb: Add trailing slash to verified comment

--- Casks/vox2.rb
+++ Casks/vox2.rb
@@ -2,7 +2,7 @@ cask 'vox2' do
   version '2.8.26'
   sha256 '4d3c3a9e34ba55f5985940cab403aa2bd0057954cf6f954f1e33063111f4ca03'
 
-  # cloud.coppertino.com/vox was verified as official when first introduced to the cask
+  # cloud.coppertino.com/vox/ was verified as official when first introduced to the cask
   url "https://cloud.coppertino.com/vox/downloads/Vox_#{version}.zip"
   name 'VOX'
   homepage 'https://vox.rocks/mac-music-player/old-versions'
