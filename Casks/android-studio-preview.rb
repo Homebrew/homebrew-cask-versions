550fabf55cb6154c9f7618db25953a37b673a9f9
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:11 +0100

    android-studio-preview.rb: Add trailing slash to verified comment

--- Casks/android-studio-preview.rb
+++ Casks/android-studio-preview.rb
@@ -2,7 +2,7 @@ cask 'android-studio-preview' do
   version '4.1.0.6,193.6381907'
   sha256 'c6e6b57add48ec683304c7a669a01edb0aefeb34188472c6502c1feaa2f11675'
 
-  # google.com/dl/android/studio was verified as official when first introduced to the cask
+  # google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name 'Android Studio Preview'
   homepage 'https://developer.android.com/studio/preview/'
