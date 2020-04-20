c0eba00865b5001f7c839c4f1d600a6d5cb7e945
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:04 +0100

    kitematic0176.rb: Add trailing slash to verified comment

--- Casks/kitematic0176.rb
+++ Casks/kitematic0176.rb
@@ -2,7 +2,7 @@ cask 'kitematic0176' do
   version '0.17.6'
   sha256 '827756c4b7de7b23b474aba12ed2b4a333d3a5f1b2fad5bc5819da405d72ed83'
 
-  # github.com/docker/kitematic was verified as official when first introduced to the cask
+  # github.com/docker/kitematic/ was verified as official when first introduced to the cask
   url "https://github.com/docker/kitematic/releases/download/v#{version}/Kitematic-#{version}-Mac.zip"
   appcast 'https://github.com/docker/kitematic/releases.atom'
   name 'Kitematic'
