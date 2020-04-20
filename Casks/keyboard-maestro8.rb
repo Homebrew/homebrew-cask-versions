049d8a2846e6a0c0dbe9e81a6faab1e62a6d3309
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:39 +0100

    keyboard-maestro8.rb: Add trailing slash to verified comment

--- Casks/keyboard-maestro8.rb
+++ Casks/keyboard-maestro8.rb
@@ -2,7 +2,7 @@ cask 'keyboard-maestro8' do
   version '8.2.4'
   sha256 '4547c28e7628b5c95112d18ed388d3f581fe231888bc062c152c279df7da6373'
 
-  # stairways.com was verified as official when first introduced to the cask
+  # stairways.com/ was verified as official when first introduced to the cask
   url "https://files.stairways.com/keyboardmaestro-#{version.no_dots}.zip"
   name 'Keyboard Maestro'
   homepage 'https://www.keyboardmaestro.com/main/'
