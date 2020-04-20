04a6c2505fe78fc325abf1db7e204bf22dfbe1de
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:38 +0100

    texstudio-beta.rb: Add trailing slash to verified comment

--- Casks/texstudio-beta.rb
+++ Casks/texstudio-beta.rb
@@ -2,7 +2,7 @@ cask 'texstudio-beta' do
   version '2.12.22beta2'
   sha256 'e8b06b108321dbf238327a3645d6b8f21126f14819e09e6df3bad1ca29c63dfa'
 
-  # github.com/texstudio-org/texstudio was verified as official when first introduced to the cask
+  # github.com/texstudio-org/texstudio/ was verified as official when first introduced to the cask
   url "https://github.com/texstudio-org/texstudio/releases/download/#{version}/texstudio-#{version}-osx.dmg"
   appcast 'https://github.com/texstudio-org/texstudio/releases.atom'
   name 'TeXstudio'
