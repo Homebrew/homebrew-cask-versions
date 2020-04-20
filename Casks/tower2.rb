eac09c4b068e79c927932218aa5e022c5d395aa4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:05 +0100

    tower2.rb: Add trailing slash to verified comment

--- Casks/tower2.rb
+++ Casks/tower2.rb
@@ -2,7 +2,7 @@ cask 'tower2' do
   version '2.6.7-362,b679e775'
   sha256 '40cf4bbc5712c7b97d2da0e3b6558dfed8f1e85cd3b524141a2a1012183f0e9b'
 
-  # fournova-app-updates.s3.amazonaws.com was verified as official when first introduced to the cask
+  # fournova-app-updates.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://fournova-app-updates.s3.amazonaws.com/apps/tower#{version.major}-mac/#{version.split('-').last.tr(',', '-')}/Tower-#{version.major}-#{version.before_comma}.zip"
   appcast "https://updates.fournova.com/updates/tower#{version.major}-mac/stable"
   name 'Tower'
