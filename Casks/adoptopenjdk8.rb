c55095bdc358b9be30821c5cfd74856caab39a95
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:14 +0100

    adoptopenjdk8.rb: Add trailing slash to verified comment

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -2,7 +2,7 @@ cask 'adoptopenjdk8' do
   version '8,252:b09'
   sha256 '98baa64886b87f91e2c49e5a273899f7b9f4088f46ea17c474e809f61d67e4ad'
 
-  # github.com/AdoptOpenJDK was verified as official when first introduced to the cask
+  # github.com/AdoptOpenJDK/ was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name 'AdoptOpenJDK 8'
