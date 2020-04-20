07647a0dffc220b5265a55e30b0f4d9cb56e0668
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:33 +0100

    keka-beta.rb: Add trailing slash to verified comment

--- Casks/keka-beta.rb
+++ Casks/keka-beta.rb
@@ -2,7 +2,7 @@ cask 'keka-beta' do
   version '1.2.0-dev.3742'
   sha256 'fedaa3b735b93f3c20a229adda74f0fd7f8118da9f3681544c4b1849927a7691'
 
-  # github.com/aonez/Keka was verified as official when first introduced to the cask
+  # github.com/aonez/Keka/ was verified as official when first introduced to the cask
   url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://beta.keka.io'
   name 'Keka'
