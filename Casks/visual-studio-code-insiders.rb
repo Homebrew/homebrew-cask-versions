863d5ce4788912b1b23339df55d055ead1b39377
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:11 +0100

    visual-studio-code-insiders.rb: Add trailing slash to verified comment

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -2,7 +2,7 @@ cask 'visual-studio-code-insiders' do
   version '1.45.0,6e9a34c0de4181a99f11e106733ee3e8c9db79f1'
   sha256 '993d0db73d7b010c3176929c8b9c6bbdf5c7d5c062a058b5cf20029db03f34ba'
 
-  # az764295.vo.msecnd.net/insider was verified as official when first introduced to the cask
+  # az764295.vo.msecnd.net/insider/ was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
   appcast 'https://vscode-update.azurewebsites.net/api/update/darwin/insider/VERSION'
   name 'Microsoft Visual Studio Code'
