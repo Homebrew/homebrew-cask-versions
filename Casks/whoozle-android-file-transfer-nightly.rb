5d166ba6c4955184041b717153f6661c69344949
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:41 +0100

    whoozle-android-file-transfer-nightly.rb: Add trailing slash to verified comment

--- Casks/whoozle-android-file-transfer-nightly.rb
+++ Casks/whoozle-android-file-transfer-nightly.rb
@@ -2,7 +2,7 @@ cask 'whoozle-android-file-transfer-nightly' do
   version :latest
   sha256 :no_check
 
-  # github.com/whoozle/android-file-transfer-linux was verified as official when first introduced to the cask
+  # github.com/whoozle/android-file-transfer-linux/ was verified as official when first introduced to the cask
   url 'https://github.com/whoozle/android-file-transfer-linux/releases/download/continuous/AndroidFileTransferForLinux.dmg'
   name 'Android File Transfer'
   homepage 'https://whoozle.github.io/android-file-transfer-linux/'
