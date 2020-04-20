d5cc1dceb8bc107eb0ccf00f622531649fb39312
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:26 +0100

    rstudio-preview.rb: Add trailing slash to verified comment

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -2,7 +2,7 @@ cask 'rstudio-preview' do
   version '1.3.938'
   sha256 '78e79a510d4e2481c5b34f357da9515ab64328cc6564c02772e8a8a7029b6ea1'
 
-  # s3.amazonaws.com/rstudio-ide-build was verified as official when first introduced to the cask
+  # s3.amazonaws.com/rstudio-ide-build/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
   name 'RStudio'
   homepage 'https://www.rstudio.com/products/rstudio/download/preview/'
