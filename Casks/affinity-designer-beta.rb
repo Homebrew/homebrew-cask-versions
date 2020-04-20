797f76a1216fb3c7a11e862107dc6db2c3fb712b
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:15:48 +0100

    affinity-designer-beta.rb: Add trailing slash to verified comment

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -2,7 +2,7 @@ cask 'affinity-designer-beta' do
   version '1.8.3.2'
   sha256 '01fece3d64016477b44c9c8df32e0c0f377d165956fc4f8125a6ac0d3ecd4847'
 
-  # affinity-beta.s3.amazonaws.com was verified as official when first introduced to the cask
+  # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg'
   appcast 'https://forum.affinity.serif.com/index.php?/forum/15-designer-beta-on-mac/'
   name 'Serif Affinity Designer'
