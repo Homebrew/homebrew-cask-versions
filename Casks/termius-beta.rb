bb87d8aa9374f99e10e8a0626ec4e0dfa66c1bc4
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:12:36 +0100

    termius-beta.rb: Add trailing slash to verified comment

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -2,7 +2,7 @@ cask 'termius-beta' do
   version '5.9.1'
   sha256 '8b4a1d54d1e3dd24224b0f58b50d09f2f6c713262a71d82d3e85a0e42e5a9d86'
 
-  # s3.amazonaws.com/termius.desktop.autoupdate/mac was verified as official when first introduced to the cask
+  # s3.amazonaws.com/termius.desktop.autoupdate/mac/ was verified as official when first introduced to the cask
   url 'https://www.termius.com/beta/download/mac/Termius+Beta.dmg'
   name 'Termius Beta'
   homepage 'https://www.termius.com/beta-program'
