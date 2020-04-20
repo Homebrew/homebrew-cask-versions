baf0ec465a9bc6d3d43c5d6cb109b822426da8ed
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:52 +0100

    chromedriver-beta.rb: Add trailing slash to verified comment

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -2,7 +2,7 @@ cask 'chromedriver-beta' do
   version '83.0.4103.14'
   sha256 '6e4eaa4adf086853f010678917f91c0ce6e59b3de572e3fe939ef49826b632b4'
 
-  # chromedriver.storage.googleapis.com was verified as official when first introduced to the cask
+  # chromedriver.storage.googleapis.com/ was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
   name 'ChromeDriver'
   homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
