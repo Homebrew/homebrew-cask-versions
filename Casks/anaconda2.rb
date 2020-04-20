3d42f2f46fac06609e1f44cbd56c640b2fa1caa0
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:14:34 +0100

    anaconda2.rb: Add trailing slash to verified comment

--- Casks/anaconda2.rb
+++ Casks/anaconda2.rb
@@ -2,7 +2,7 @@ cask 'anaconda2' do
   version '2019.10'
   sha256 '463cbd0b90c47d02ec341377110653870c7cc9d65572c655c5e44aaf2ccb280d'
 
-  # repo.continuum.io/archive was verified as official when first introduced to the cask
+  # repo.continuum.io/archive/ was verified as official when first introduced to the cask
   url "https://repo.continuum.io/archive/Anaconda2-#{version}-MacOSX-x86_64.sh"
   name 'Continuum Analytics Anaconda2'
   homepage 'https://www.anaconda.com/what-is-anaconda/'
