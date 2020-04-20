1277885e6a24d63eb2af9ef18cc088101a2060ee
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:07 +0100

    microsoft-edge-beta.rb: Add trailing slash to verified comment

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -2,7 +2,7 @@ cask 'microsoft-edge-beta' do
   version '81.0.416.58'
   sha256 '05760b8ac14453b186d1d28cf8e4417754525229dd2d49d729e4380496cc9e6b'
 
-  # officecdn-microsoft-com.akamaized.net was verified as official when first introduced to the cask
+  # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069439'
   name 'Microsoft Edge Beta'
