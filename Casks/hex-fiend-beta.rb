1f0ea79234e981871f0b4c6c897445b85ca99753
Vítor Galvão <info@vitorgalvao.com>
2020-04-20 02:13:26 +0100

    hex-fiend-beta.rb: Add trailing slash to verified comment

--- Casks/hex-fiend-beta.rb
+++ Casks/hex-fiend-beta.rb
@@ -2,7 +2,7 @@ cask 'hex-fiend-beta' do
   version '2.13.0b2'
   sha256 'c7e768637b469d5d5fb46c214849b7f256a87610feec00ba986184834bed5a23'
 
-  # github.com/ridiculousfish/HexFiend was verified as official when first introduced to the cask
+  # github.com/ridiculousfish/HexFiend/ was verified as official when first introduced to the cask
   url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.major_minor}.dmg"
   appcast 'https://github.com/ridiculousfish/HexFiend/releases.atom'
   name 'Hex Fiend'
