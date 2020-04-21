01631a4bd63c44a0aeed2fa9ed79e1879612060e
Zhenfu Shi <szf1234@me.com>
2020-04-21 15:02:34 -0400

    Update osu-development from 2020.413.0 to 2020.421.0 (#8972)

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,6 +1,6 @@
 cask 'osu-development' do
-  version '2020.413.0'
-  sha256 'a5698b0cf2e57f1029626dc66d3ef832dce2a86ca77490c6f0198e27837e55d2'
+  version '2020.421.0'
+  sha256 '81becd4c3346a0fdc9e93ac2e878dc1935049939761695c24fcbe3fc411bfe4e'
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   appcast 'https://github.com/ppy/osu/releases.atom'
