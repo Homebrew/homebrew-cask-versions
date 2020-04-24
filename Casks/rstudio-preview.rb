5709bcad6f0bac47f3d93dfb1999f98734c8f18c
Lukas Burk <lukas@quantenbrot.de>
2020-04-25 01:43:57 +0200

    Update rstudio-preview from 1.3.944 to 1.3.947

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask 'rstudio-preview' do
-  version '1.3.944'
-  sha256 '3097a0cbc7e702b1ea61562169e6a56ce04a6747e14eefb8116d0066e8148b76'
+  version '1.3.947'
+  sha256 '7aa906e891be6c950a82f56931670ac90d4806a1d1852a8e3f456a541f50533e'
 
   # s3.amazonaws.com/rstudio-ide-build/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
