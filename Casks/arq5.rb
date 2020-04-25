ebebca3933038df93c5927ffd98f04960a493aca
Andre Gellert HomeOffice <agellert@electronicpartner.de>
2020-04-25 08:41:43 +0200

    Support for the old arq v5 branch As the main arq cask continued with version 6, this adds support fo version 5, old technology and old license support. Be aware with updates to v6 - as currently some major bugs occur

---
+++ Casks/arq5.rb
@@ -0,0 +1,13 @@
+cask 'arq' do
+  version '5.18.0'
+  sha256 '0bdc77398c4c04167181383a84ce9623c2a5e1cac787d30822ccd346b3d5df85'
+
+  url "https://www.arqbackup.com/download/arqbackup/Arq_#{version.major}.#{version.minor}.zip"
+  appcast 'https://www.arqbackup.com/'
+  name 'Arq'
+  homepage 'https://www.arqbackup.com/'
+
+  auto_updates true
+
+  app 'Arq.app'
+end
