80e0b66e112d87f23fef040b8d42ffafca860f23
Andre Gellert HomeOffice <agellert@electronicpartner.de>
2020-04-25 12:20:44 +0200

    Cask is Arq5, App name is Arq

--- Casks/arq5.rb
+++ Casks/arq5.rb
@@ -1,10 +1,10 @@
-cask 'arq' do
+cask 'arq5' do
   version '5.18.0'
   sha256 '0bdc77398c4c04167181383a84ce9623c2a5e1cac787d30822ccd346b3d5df85'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq_#{version.major}.#{version.minor}.zip"
   appcast 'https://www.arqbackup.com/'
-  name 'Arq5'
+  name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
   auto_updates true
