f7bda885ff56a231d404c7b541da169fb891cee7
Vijay A <avijayr@protonmail.com>
2020-04-25 19:29:02 +0530

    Added 'conflicts_with' setting for iterm2

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -9,14 +9,15 @@ cask 'iterm2-beta' do
   homepage 'https://www.iterm2.com/'
 
   auto_updates true
+  conflicts_with cask: 'iterm2'
   depends_on macos: '>= :sierra'
 
   app 'iTerm.app'
 
   zap trash: [
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*',
                '~/Library/Application Support/iTerm',
                '~/Library/Application Support/iTerm2',
+               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*',
                '~/Library/Caches/com.googlecode.iterm2',
                '~/Library/Preferences/com.googlecode.iterm2.plist',
                '~/Library/Saved Application State/com.googlecode.iterm2.savedState',
