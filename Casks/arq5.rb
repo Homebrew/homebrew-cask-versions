6b68e95465bfb874642e3f2e9b816e2ebc37caaa
Andre Gellert HomeOffice <agellert@electronicpartner.de>
2020-04-25 13:40:55 +0200

    Added Download Page for Appcast Parameter

--- Casks/arq5.rb
+++ Casks/arq5.rb
@@ -3,7 +3,7 @@ cask 'arq5' do
   sha256 '0bdc77398c4c04167181383a84ce9623c2a5e1cac787d30822ccd346b3d5df85'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq_#{version.major}.#{version.minor}.zip"
-  appcast 'https://www.arqbackup.com/'
+  appcast 'https://www.arqbackup.com/download/'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
