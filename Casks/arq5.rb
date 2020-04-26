965fe0dc3d5d36ea71a724429d41da67e05725b8
Andre Gellert HomeOffice <agellert@electronicpartner.de>
2020-04-26 09:57:54 +0200

    No more mentions of v5 branch on website, so removed the appcast stanza

--- Casks/arq5.rb
+++ Casks/arq5.rb
@@ -3,7 +3,6 @@ cask 'arq5' do
   sha256 '0bdc77398c4c04167181383a84ce9623c2a5e1cac787d30822ccd346b3d5df85'
 
   url "https://www.arqbackup.com/download/arqbackup/Arq_#{version.major}.#{version.minor}.zip"
-  appcast 'https://www.arqbackup.com/download/'
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
