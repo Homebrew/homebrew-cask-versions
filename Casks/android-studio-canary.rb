ec54152119efe0eb77afd4df93a9a6ba4f4572fd
Jason Atwood <jwatwood@gmail.com>
2020-05-13 12:02:02 -0400

    Split Android Studio preview into beta and canary
    Convert latest 'preview' into canary and add a beta version.

--- Casks/android-studio-canary.rb
+++ Casks/android-studio-canary.rb
@@ -1,10 +1,10 @@
-cask 'android-studio-preview' do
+cask 'android-studio-canary' do
   version '4.1.0.9,201.6466190'
   sha256 '8f45a6f1e286d73a27627cccfa62e197f925bd7b7642facdfa9d71be39a45e99'
 
   # dl.google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
-  name 'Android Studio Preview'
+  name 'Android Studio Preview (Canary)'
   homepage 'https://developer.android.com/studio/preview/'
 
   app "Android Studio #{version.major_minor} Preview.app"
