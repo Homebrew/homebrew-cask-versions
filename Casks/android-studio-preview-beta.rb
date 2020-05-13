60305a8d99455751f0f0c3f146d37d1d38c6ef5e
Jason Atwood <jwatwood@gmail.com>
2020-05-13 12:18:02 -0400

    Add android studio beta

---
+++ Casks/android-studio-preview-beta.rb
@@ -0,0 +1,26 @@
+cask 'android-studio-preview-beta' do
+  version '4.0.0.15,193.645338'
+  sha256 '78d1ec72245f3d5646256f75ce141926293ca7c5f730a0f76eb1963a9c6a6bf3'
+
+  # dl.google.com/dl/android/studio/ was verified as official when first introduced to the cask
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
+  name 'Android Studio Preview (Beta)'
+  homepage 'https://developer.android.com/studio/preview/'
+
+  app "Android Studio #{version.major_minor} Preview.app"
+
+  zap trash: [
+               '~/Library/Android/sdk',
+               "~/Library/Application Support/AndroidStudio#{version.major_minor}",
+               "~/Library/Caches/AndroidStudio#{version.major_minor}",
+               "~/Library/Logs/AndroidStudio#{version.major_minor}",
+               "~/Library/Preferences/AndroidStudio#{version.major_minor}",
+               '~/Library/Preferences/com.android.Emulator.plist',
+               '~/Library/Saved Application State/com.google.android.studio.savedState',
+               '~/.android',
+             ],
+      rmdir: [
+               '~/AndroidStudioProjects',
+               '~/Library/Android',
+             ]
+end
