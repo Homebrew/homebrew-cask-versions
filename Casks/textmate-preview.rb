439cc6e9bbbca7c7762035e71d4dbbc5e206ff14
Marc Schwede <9077622+schwedenmut@users.noreply.github.com>
2020-05-13 12:17:40 +0200

    Update textmate-preview.rb from 2.0.11 to 2.0.12 (#9069)
    * Update textmate-preview.rb from 2.0.11 to 2.0.12
    
    also added preview plist to zap stanca
    
    * removed unnessesary trash path

--- Casks/textmate-preview.rb
+++ Casks/textmate-preview.rb
@@ -1,6 +1,6 @@
 cask 'textmate-preview' do
-  version '2.0.11'
-  sha256 'ec40804776087d25a5ad5b3945413e77279282eeb6ed928e1f001eb0c12073e9'
+  version '2.0.12'
+  sha256 '99b353d8f56442cd600a1d8dcef7ab7534683c7b3cd1555c9d9e6013652f0e5e'
 
   # github.com/textmate/textmate/ was verified as official when first introduced to the cask
   url "https://github.com/textmate/textmate/releases/download/v#{version}/TextMate_#{version}.tbz"
