9571957e0169f12ddde2a1215903f93267ca163b
Robert Blair <robertleeblairjr@gmail.com>
2020-05-20 05:33:45 -0500

    Update dotnet-sdk-preview from 5.0.100-preview.3.20216.6,3fa9a36e-907c-4d7a-a98a-e50ad0aa4990:ff63364b94e98687d5933c1b9a50a5d0 to 5.0.100-preview.4.20258.7,6d724fad-a67b-4fed-8152-f6f98aff6d63:fdf36e0be9ca9a92af106e27f1f9547e

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,6 +1,6 @@
 cask 'dotnet-sdk-preview' do
-  version '5.0.100-preview.3.20216.6,3fa9a36e-907c-4d7a-a98a-e50ad0aa4990:ff63364b94e98687d5933c1b9a50a5d0'
-  sha256 '46220c1bf858d3f41caaedb287de946d0ea2667f6e3cf67d1ce6abe307638164'
+  version '5.0.100-preview.4.20258.7,6d724fad-a67b-4fed-8152-f6f98aff6d63:fdf36e0be9ca9a92af106e27f1f9547e'
+  sha256 '4c2082a8890c6c961583d9cc4480961b4b8a01271d15b41673e130b08bf44098'
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   appcast "https://dotnet.microsoft.com/download/dotnet-core/#{version.major_minor}"
