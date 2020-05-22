1a29ac290828c45c40bd09c484a01b10bca412c5
Yurii Kolesnykov <root@yurikoles.com>
2020-05-22 17:12:27 +0300

    Update 1password-beta from 7.5.1.BETA-0 to 7.6.BETA-0

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask '1password-beta' do
-  version '7.5.1.BETA-0'
-  sha256 '197f0fc62ce8618ea86b459944b88a319ac75974a125cd39354733ee66aea75c'
+  version '7.6.BETA-0'
+  sha256 '953aba2c60643d1aa43aac1529f79704b88dab3921826188761936a84fe59aff'
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
