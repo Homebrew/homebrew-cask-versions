9aa60457476db796943135c1ae4b0d61cbfc5a5a
Kyle Rubenok <kyle@rubenok.ca>
2020-05-23 22:22:47 -0700

    Use `brew cask create` and add figma-beta

--- Casks/figma-beta.rb
+++ Casks/figma-beta.rb
@@ -1,16 +1,10 @@
 cask 'figma-beta' do
-  version :beta
+  version :latest
   sha256 :no_check
 
   url 'https://desktop.figma.com/mac/beta/FigmaBeta.zip'
-  name 'Figma'
-  homepage 'https://www.figma.com/'
+  name 'Figma Beta'
+  homepage 'https://figma.com/'
 
   app 'Figma Beta.app'
-
-  zap trash: [
-               '~/Library/Application Support/Figma/DesktopBeta',
-               '~/Library/Preferences/com.figma.DesktopBeta.plist',
-               '~/Library/Saved Application State/com.figma.DesktopBeta.savedState',
-             ]
 end
