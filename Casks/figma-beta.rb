a723bbe4f6fa0c931143efeef28ddfe695b37429
Kyle Rubenok <kyle@rubenok.ca>
2020-05-24 06:14:44 -0700

    Add Figma Beta (#9119)
    * Create figma-beta.rb
    
    * Rename .app
    
    * Use `brew cask create` and add figma-beta

---
+++ Casks/figma-beta.rb
@@ -0,0 +1,10 @@
+cask 'figma-beta' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://desktop.figma.com/mac/beta/FigmaBeta.zip'
+  name 'Figma Beta'
+  homepage 'https://figma.com/'
+
+  app 'Figma Beta.app'
+end
