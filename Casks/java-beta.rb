50e9b7b6b274c2ba932b75087682cfd03c128f68
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-05-27 20:04:39 +0100

    Update java-beta from 14,32 to 15,24 (#9131)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,6 +1,6 @@
 cask 'java-beta' do
-  version '14,32'
-  sha256 'c2a883e5e117f534c3c7d6783dd422101033a0eab172e72f7e38781f727619df'
+  version '15,24'
+  sha256 '95b22ea3c6151d49b3e2700be4b862aba7a543e0b0577e76ee61dfd46b1f8a67'
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name 'OpenJDK Early Access Java Development Kit'
