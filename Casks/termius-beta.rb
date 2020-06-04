d5fc9fba0a4257df9250d800a562bbcb57588031
Brigitte Zmoelnig <brigitte@corecode.io>
2020-06-04 13:55:18 +0200

    Update termius-beta from 5.13.0 to 5.13.2

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,5 +1,5 @@
 cask 'termius-beta' do
-  version '5.13.0'
+  version '5.13.2'
   sha256 '4652a04ca861a35e93fe90f96025d55a35bcbf4ac3d20a96944ff46f8e357db1'
 
   url 'https://www.termius.com/beta/download/mac/Termius+Beta.dmg'
