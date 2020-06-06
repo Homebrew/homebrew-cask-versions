b5aa94d1f33625a24c73b0aa8f8214ef90b410b6
John Benninghoff <jbenninghoff@mac.com>
2020-06-06 14:02:24 -0500

    Update arq5 from 5.19.0 to 5.19.1 (#9172)

--- Casks/arq5.rb
+++ Casks/arq5.rb
@@ -1,8 +1,8 @@
 cask 'arq5' do
-  version '5.19.0'
-  sha256 '8c26440197b550777e61f3f483f1060364451b1338a45cbd37ddd53e2aefdc3b'
+  version '5.19.1'
+  sha256 '5b4926b90e2ebac01d4e8d8daad5e39d0ba63b5714d6296315500365b4f60a85'
 
-  url "https://www.arqbackup.com/download/arqbackup/Arq_#{version.major}.#{version.minor}.zip"
+  url "https://www.arqbackup.com/download/arqbackup/Arq_#{version}.zip"
   name 'Arq'
   homepage 'https://www.arqbackup.com/'
 
