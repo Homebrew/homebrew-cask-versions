f7acb7be4db51405ba7e2d56c1b5c7ed065603b1
Max <msal.coding@gmail.com>
2020-06-09 22:18:57 +0200

    better name stanza

--- Casks/sonos-s1.rb
+++ Casks/sonos-s1.rb
@@ -5,7 +5,7 @@ cask 'sonos-s1' do
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast 'https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac',
           configuration: version.no_dots
-  name 'Sonos'
+  name 'Sonos S1'
   homepage 'https://www.sonos.com/'
 
   auto_updates true
