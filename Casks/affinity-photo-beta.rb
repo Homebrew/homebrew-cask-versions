7183d6ee49e24e7a0b3731fa85e59cd5e451b898
Randall <ran.dall@icloud.com>
2020-06-08 17:31:09 -0700

    Update affinity-photo-beta from 1.8.3.180 to 1.8.4.183

--- Casks/affinity-photo-beta.rb
+++ Casks/affinity-photo-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-photo-beta' do
-  version '1.8.3.180'
-  sha256 'ae600bf3564a4bc1c41ef848902b3dfa8529218721b6e7158a8a4e7cac75ad3b'
+  version '1.8.4.183'
+  sha256 'e1ded3ef356776358933af3c48343f35b171ea9621ae655e9fd422d5628e7614'
 
   # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Photo%20Customer%20Beta.zip'
