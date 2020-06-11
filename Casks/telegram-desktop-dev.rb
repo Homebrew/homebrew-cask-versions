1cab9eadbba628a7b4544a6626f5dab8a295d5e0
Vítor Galvão <info@vitorgalvao.com>
2020-06-11 13:20:30 +0100

    telegram-desktop-dev: change appcast configuratio to must_contain

--- Casks/telegram-desktop-dev.rb
+++ Casks/telegram-desktop-dev.rb
@@ -5,7 +5,7 @@ cask 'telegram-desktop-dev' do
   # github.com/telegramdesktop/tdesktop/ was verified as official when first introduced to the cask
   url "https://github.com/telegramdesktop/tdesktop/releases/download/v#{version.major_minor_patch}/tsetup.#{version}.dmg"
   appcast 'https://github.com/telegramdesktop/tdesktop/releases.atom',
-          configuration: version.major_minor_patch
+          must_contain: version.major_minor_patch
   name 'Telegram Desktop'
   homepage 'https://desktop.telegram.org/'
 
