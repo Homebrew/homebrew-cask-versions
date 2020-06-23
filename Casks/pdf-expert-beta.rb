c8ed2cb9bf312e2e40738f42f5d8594fe31cb817
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2020-06-23 16:33:26 -0500

    Update pdf-expert-beta from 2.5.8,692 to 2.5.9,702 (#9233)

--- Casks/pdf-expert-beta.rb
+++ Casks/pdf-expert-beta.rb
@@ -1,6 +1,6 @@
 cask 'pdf-expert-beta' do
-  version '2.5.8,692'
-  sha256 'a047806bfada444aceeafb508770e6997d594ce230dc77fcc5167b19dae7d685'
+  version '2.5.9,702'
+  sha256 '22972eeca0bdd86704b826bf799bd2b74168ebf97e5aaa504d1ff98ee8088c17'
 
   # d1ke680phyeohy.cloudfront.net/ was verified as official when first introduced to the cask
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg"
