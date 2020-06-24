1090aea30c160a65d4ce369f1229dffd3c0e3394
Jeff Jones <jeffersonmjones92@gmail.com>
2020-06-24 12:59:15 -0600

    Update android-studio-preview-beta  (#9226)
    * Update android-studio-preview-beta from 4.0.0.15,193.6453388 to 4.1.0.11,201.6565218
    Co-authored-by: CoreCode <core-code@users.noreply.github.com>

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -1,6 +1,6 @@
 cask 'android-studio-preview-beta' do
-  version '4.0.0.15,193.6453388'
-  sha256 '78d1ec72245f3d5646256f75ce141926293ca7c5f730a0f76eb1963a9c6a6bf3'
+  version '4.1.0.11,201.6565218'
+  sha256 'f172e11c9b488065f7e6d90098db57eda0541cec88ce0b19d3284bcb342d64ff'
 
   # dl.google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
@@ -12,7 +12,7 @@ cask 'android-studio-preview-beta' do
                          'android-studio-preview-canary',
                        ]
 
-  app 'Android Studio.app'
+  app "Android Studio #{version.major_minor} Preview.app"
 
   zap trash: [
                '~/Library/Android/sdk',
