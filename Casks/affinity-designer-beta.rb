cb24805c12cd01539917d4f5d99f7a7820a0d2bb
suschizu <brigitte@corecode.io>
2020-06-25 16:58:02 +0200

    Update affinity-designer-beta.rb

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,5 +1,5 @@
 cask 'affinity-designer-beta' do
-  version '1.8.3.2'
+  version '1.8.4.2'
   sha256 '01fece3d64016477b44c9c8df32e0c0f377d165956fc4f8125a6ac0d3ecd4847'
 
   # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
