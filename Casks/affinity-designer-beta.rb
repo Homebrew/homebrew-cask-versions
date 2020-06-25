7d01f8f707b10aefea0319ca617692f1ced68d6a
suschizu <brigitte@corecode.io>
2020-06-25 17:24:26 +0200

    Update affinity-designer-beta.rb

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,6 +1,6 @@
 cask 'affinity-designer-beta' do
   version '1.8.4.2'
-  sha256 '01fece3d64016477b44c9c8df32e0c0f377d165956fc4f8125a6ac0d3ecd4847'
+  sha256 '75554bf0378c3e50ba3fa4ab6ae4f00b5c255d9a574bf4891fc8f1af41dd0bf4'
 
   # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url 'https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg'
