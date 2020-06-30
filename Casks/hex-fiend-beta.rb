9c0a22567a62e02c57a5b479bdbf9074e56ae058
David Yantis <404NetworkError@users.noreply.github.com>
2020-06-30 10:57:54 -0400

    Update hex-fiend-beta from 2.13.0b5 to 2.13.1b1 (#9265)
    * Update hex-fiend-beta from 2.13.0b5 to 2.13.1b1
    
    * Update hex-fiend-beta.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/hex-fiend-beta.rb
+++ Casks/hex-fiend-beta.rb
@@ -1,9 +1,9 @@
 cask 'hex-fiend-beta' do
-  version '2.13.0b5'
-  sha256 '99ad82c4a40bd33c4c7c85523ddde65ae4c96fae851ffe826a319b6961c0687a'
+  version '2.13.1,b1'
+  sha256 '7251b04719d3c58c49a3896ad85ee4b824f67eac36d490a5fd37364ccb580158'
 
   # github.com/ridiculousfish/HexFiend/ was verified as official when first introduced to the cask
-  url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.major_minor}.dmg"
+  url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version.before_comma}#{version.after_comma}/Hex_Fiend_#{version.before_comma}.dmg"
   appcast 'https://github.com/ridiculousfish/HexFiend/releases.atom'
   name 'Hex Fiend'
   homepage 'https://ridiculousfish.com/hexfiend/'
