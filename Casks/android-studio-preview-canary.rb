8ac5487d0b70b3158b1670634cf199f63f8cbd79
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-08 00:37:59 +0200

    Update android-studio-preview-canary from 4.2.0.2,201.6582697 to 4.2.0.3,201.6630367

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,6 +1,6 @@
 cask 'android-studio-preview-canary' do
-  version '4.2.0.2,201.6582697'
-  sha256 '9d145c76ec6dbd410ce34f48a7528a6a9434b427c940a4ff8820403fc4e4681b'
+  version '4.2.0.3,201.6630367'
+  sha256 'c26233ab417a6258fb00c6f243201118e26adbcc273c6f8b6f62745f1eddffa8'
 
   # dl.google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
