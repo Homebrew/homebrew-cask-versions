913082f811363904ec59241da9b7f52b33a4a892
Randall <17261190+ran-dall@users.noreply.github.com>
2020-07-08 19:04:52 -0700

    Update brave-browser-dev from 83.1.11.90,111.90 to 84.1.11.92,111.92 (#9299)

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -1,6 +1,6 @@
 cask 'brave-browser-dev' do
-  version '83.1.11.90,111.90'
-  sha256 '88e7658f51d8a184f0dc9d59bb2728a9754b1c6a43fa979c5dee2688b3c84ad1'
+  version '84.1.11.92,111.92'
+  sha256 'ecfcfff3592fa3e05a7c15a5298376d30e118bbb38003730eb92b2a51ff37edb'
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser/ was verified as official when first introduced to the cask
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev/#{version.after_comma}/Brave-Browser-Dev.dmg"
