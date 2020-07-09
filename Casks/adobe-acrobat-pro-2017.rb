ea78a02bdf8bc53eb450c78a0ceadd575464ef8c
Chad H. G. Allen <chad@chgallen.com>
2020-07-09 13:23:49 +0100

    Change version from :latest to 2017

--- Casks/adobe-acrobat-pro-2017.rb
+++ Casks/adobe-acrobat-pro-2017.rb
@@ -1,21 +1,21 @@
 cask 'adobe-acrobat-pro-2017' do
-  version ':latest'
+  version '2017'
   sha256 'f041971fd2d5167b60d2c16532107e6537879fa86d297cf0a437038191dae775'
 
-  url 'https://trials3.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_2017_Web_WWMUI.dmg',
+  url "https://trials3.adobe.com/AdobeProducts/APRO/Acrobat_HelpX/osx10/Acrobat_#{version}_Web_WWMUI.dmg",
       user_agent: :fake,
       cookies:    { 'MM_TRIALS' => '1234' }
   name 'Adobe Acrobat Pro 2017'
-  homepage 'https://helpx.adobe.com/uk/download-install/kb/acrobat-2017-downloads.html#AcrobatPro2017'
+  homepage "https://helpx.adobe.com/uk/download-install/kb/acrobat-#{version}-downloads.html#AcrobatPro2017"
 
   auto_updates true
 
-  pkg 'Acrobat 2017/Acrobat 2017 Installer.pkg'
+  pkg "Acrobat #{version}/Acrobat #{version} Installer.pkg"
 
   uninstall pkgutil:   [
                          'com.adobe.armdc.app.pkg',
                          'com.adobe.AcroServicesUpdater',
-                         'com.adobe.acrobat.2017.*',
+                         "com.adobe.acrobat.#{version}.*",
                        ],
             launchctl: [
                          'Adobe_Genuine_Software_Integrity_Service',
@@ -24,15 +24,15 @@ cask 'adobe-acrobat-pro-2017' do
                          'com.adobe.ARMDC.SMJobBlessHelper',
                          'com.adobe.ARMDCHelper.cc24aef4a1b90ed56a725c38014c95072f92651fb65e1bf9c8e43c37a23d420d',
                        ],
-            delete:    '/Applications/Adobe Acrobat 2017/'
+            delete:    "/Applications/Adobe Acrobat #{version}/"
 
   zap trash: [
 
                '/Library/Application Support/Adobe/ARMDC',
                '/Library/Application Support/Adobe/ARMNext',
                '/Library/Application Support/Adobe/Acrobat',
-               '/Library/Application Support/Adobe/Acrobat 2017 AMT',
-               '/Library/Application Support/Adobe/Acrobat 2017 Helper Frameworks',
+               "/Library/Application Support/Adobe/Acrobat #{version} AMT",
+               "/Library/Application Support/Adobe/Acrobat #{version} Helper Frameworks",
                '/Library/Application Support/Adobe/Adobe PCD',
                '/Library/Application Support/Adobe/Adobe PDF',
                '/Library/Application Support/Adobe/AdobeGCClient',
@@ -46,7 +46,7 @@ cask 'adobe-acrobat-pro-2017' do
                '/Library/Application Support/Adobe/WebExtnUtils',
                '/Library/Application Support/regid.1986-12.com.adobe/regid.1986-12.com.adobe_V7{}AcrobatESR-17-Mac-GM-MUL.swidtag',
                '/Library/Preferences/com.adobe.PDFAdminSettings.plist',
-               '/Library/Preferences/com.adobe.acrobat.2017.WebResource.plist',
+               "/Library/Preferences/com.adobe.acrobat.#{version}.WebResource.plist",
                '/Library/Preferences/com.adobe.acrobat.pdfviewer.plist',
                '~/Library/Application Support/Adobe/AcroCef',
                '~/Library/Application Support/Adobe/DynamicSonar',
