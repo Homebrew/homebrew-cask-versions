08186689ffa60afa3880ad9ec43952b75ea043c5
Yurii Kolesnykov <root@yurikoles.com>
2020-07-13 14:17:51 +0300

    Update qt-creator-dev from 4.12.0-beta2 to 4.13.0-beta1 (#9316)

--- Casks/qt-creator-dev.rb
+++ Casks/qt-creator-dev.rb
@@ -1,6 +1,6 @@
 cask 'qt-creator-dev' do
-  version '4.12.0-beta2'
-  sha256 '4ec4f303ba8ec17401a5bcc0556d8ec82c131740c4aadb5e09c6b033c13fd649'
+  version '4.13.0-beta1'
+  sha256 'd8e66726b208d8f3f12b53cc6432591676ad01e497d5cadd27844228dc6049cf'
 
   url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name 'Qt Creator Dev'
