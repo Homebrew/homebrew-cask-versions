2108b97aeff42760f9b386371f36b6780c5e4550
Vítor Galvão <info@vitorgalvao.com>
2020-07-14 18:52:13 +0100

    Update papers3.rb

--- Casks/papers3.rb
+++ Casks/papers3.rb
@@ -1,5 +1,5 @@
 cask 'papers3' do
-  version '3.4.20_579'
+  version '3.4.23_587'
   sha256 '48882e7852cd60ee42356e16fc297bbbe4f49c6a01f8a4a9127cfd10e45ff775'
 
   # downloads.mekentosj.com was verified as official when first introduced to the cask
