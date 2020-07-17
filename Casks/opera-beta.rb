a66debff152832a1c17f7371a7a378d0f3cf4031
Hannes Braun <37622889+hannesbraun@users.noreply.github.com>
2020-07-17 15:16:59 +0200

    Update opera-beta from 70.0.3728.21 to 70.0.3728.46 (#9334)

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask 'opera-beta' do
-  version '70.0.3728.21'
-  sha256 '6c8ca8d3c93acf5f5e22d37a7a25e77ef7734839bdec565f8292deaadb41ee0e'
+  version '70.0.3728.46'
+  sha256 '33d11c4af31a2fd5906ea9af3e78af4d57d8fedc924c39d371949eba58f560c0'
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name 'Opera Beta'
