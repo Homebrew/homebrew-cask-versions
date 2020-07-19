996d42385dc29fdb7e5ae4a848c233d8e6f81b2b
秦新 <qinxin_0720@live.com>
2020-07-19 14:48:05 +0800

    Update adoptopenjdk8 to version 8,262:b10 (#9344)

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -1,6 +1,6 @@
 cask 'adoptopenjdk8' do
-  version '8,252:b09'
-  sha256 '98baa64886b87f91e2c49e5a273899f7b9f4088f46ea17c474e809f61d67e4ad'
+  version '8,262:b10'
+  sha256 '2cec673a096ffadf288f64015a87a2492c16296656483c234a17bc0617d67241'
 
   # github.com/AdoptOpenJDK/openjdk8-binaries/ was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
