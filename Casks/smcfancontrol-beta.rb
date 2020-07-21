787e033626e24e82999c2dc785c8a67cd2b0c19b
gottabeleGIT <sandesh.manik@icloud.com>
2020-07-21 04:59:51 +0000

    Update smcfancontrol-beta.rb

--- Casks/smcfancontrol-beta.rb
+++ Casks/smcfancontrol-beta.rb
@@ -3,9 +3,10 @@ cask 'smcfancontrol-beta' do
   sha256 'd9dcd2c01e2583b74e14a6303ffd75d659dea7f99e1e42de4d8fcb0115cbcec3'
 
   url "https://github.com/hholtmann/smcFanControl/releases/download/#{version}%C3%9F1/smcFanControl_#{version.dots_to_underscores}.zip"
+  appcast 'https://github.com/hholtmann/smcFanControl/releases.atom'
   name 'smcFanControl'
   homepage 'https://github.com/hholtmann/smcFanControl'
-  
+
   conflicts_with cask: 'smcfancontrol'
 
   app 'smcFanControl.app'
@@ -16,3 +17,4 @@ cask 'smcfancontrol-beta' do
                "~/Library/Caches/com.eidac.smcFanControl#{version.major}",
              ]
 end
+
