d157385cbf2ff898ba95004f8fbe9c05c6a89a88
Dustin Wheeler <3818510+mskblackbelt@users.noreply.github.com>
2020-07-27 16:45:02 -0400

    Add PDFpenPro v11.

---
+++ Casks/pdfpenpro11.rb
@@ -0,0 +1,12 @@
+cask 'pdfpenpro11' do
+  version '1122.2,1585609999'
+  sha256 'baab7a11eca4345ae466fdba8d43342bb762bc04203f3df234db42e1c314993a'
+
+  url "https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/#{version.before_comma}/#{version.after_comma}/PDFpenPro-#{version.before_comma}.zip"
+  name 'PDFpenPro 11'
+  homepage 'https://smilesoftware.com/PDFpenPro'
+  
+  depends_on macos: ">= :sierra"
+
+  app 'PDFpenPro.app'
+end
