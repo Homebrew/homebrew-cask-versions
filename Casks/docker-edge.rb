eba344d14eea240fb16ee59f56e64a1231a66850
LanikSJ <LanikSJ@users.noreply.github.com>
2020-07-27 11:08:58 -0700

    Update docker-edge.rb

--- Casks/docker-edge.rb
+++ Casks/docker-edge.rb
@@ -1,55 +1,55 @@
-cask "docker-edge" do
-  version "2.3.3.2,46784"
-  sha256 "8c74c06c75200078763cb1b87507010fc4a3ca3ecbf24d314124405abc66d4bc"
+cask 'docker-edge' do
+  version '2.3.3.2,46784'
+  sha256 '8c74c06c75200078763cb1b87507010fc4a3ca3ecbf24d314124405abc66d4bc'
 
   url "https://download.docker.com/mac/edge/#{version.after_comma}/Docker.dmg"
-  appcast "https://download.docker.com/mac/edge/appcast.xml"
-  name "Docker Community Edition for Mac (Edge)"
-  name "Docker CE for Mac (Edge)"
-  homepage "https://www.docker.com/products/docker-desktop"
+  appcast 'https://download.docker.com/mac/edge/appcast.xml'
+  name 'Docker Community Edition for Mac (Edge)'
+  name 'Docker CE for Mac (Edge)'
+  homepage 'https://www.docker.com/products/docker-desktop'
 
   auto_updates true
-  depends_on macos: ">= :yosemite"
+  depends_on macos: '>= :yosemite'
 
-  app "Docker.app"
+  app 'Docker.app'
 
   uninstall delete:    [
-    "/Library/PrivilegedHelperTools/com.docker.vmnetd",
-    "/private/var/tmp/com.docker.vmnetd.socket",
-    "/usr/local/bin/docker",
-    "/usr/local/bin/docker-compose",
-    "/usr/local/bin/docker-credential-desktop",
-    "/usr/local/bin/docker-credential-osxkeychain",
-    "/usr/local/bin/hyperkit",
-    "/usr/local/bin/kubectl",
-    "/usr/local/bin/kubectl.docker",
-    "/usr/local/bin/notary",
-    "/usr/local/bin/vpnkit",
-  ],
+                         '/Library/PrivilegedHelperTools/com.docker.vmnetd',
+                         '/private/var/tmp/com.docker.vmnetd.socket',
+                         '/usr/local/bin/docker',
+                         '/usr/local/bin/docker-compose',
+                         '/usr/local/bin/docker-credential-desktop',
+                         '/usr/local/bin/docker-credential-osxkeychain',
+                         '/usr/local/bin/hyperkit',
+                         '/usr/local/bin/kubectl',
+                         '/usr/local/bin/kubectl.docker',
+                         '/usr/local/bin/notary',
+                         '/usr/local/bin/vpnkit',
+                       ],
             launchctl: [
-              "com.docker.helper",
-              "com.docker.vmnetd",
-            ],
-            quit:      "com.docker.docker"
+                         'com.docker.helper',
+                         'com.docker.vmnetd',
+                       ],
+            quit:      'com.docker.docker'
 
   zap trash: [
-    "/usr/local/bin/docker-compose.backup",
-    "/usr/local/bin/docker.backup",
-    "~/Library/Application Support/Docker Desktop",
-    "~/Library/Application Scripts/com.docker.helper",
-    "~/Library/Caches/KSCrashReports/Docker",
-    "~/Library/Caches/com.docker.docker",
-    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker",
-    "~/Library/Containers/com.docker.docker",
-    "~/Library/Containers/com.docker.helper",
-    "~/Library/Group Containers/group.com.docker",
-    "~/Library/Preferences/com.docker.docker.plist",
-    "~/Library/Preferences/com.electron.docker-frontend.plist",
-    "~/Library/Saved Application State/com.electron.docker-frontend.savedState",
-    "~/Library/Logs/Docker Desktop",
-  ],
+               '/usr/local/bin/docker-compose.backup',
+               '/usr/local/bin/docker.backup',
+               '~/Library/Application Support/Docker Desktop',
+               '~/Library/Application Scripts/com.docker.helper',
+               '~/Library/Caches/KSCrashReports/Docker',
+               '~/Library/Caches/com.docker.docker',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data/com.docker.docker',
+               '~/Library/Containers/com.docker.docker',
+               '~/Library/Containers/com.docker.helper',
+               '~/Library/Group Containers/group.com.docker',
+               '~/Library/Preferences/com.docker.docker.plist',
+               '~/Library/Preferences/com.electron.docker-frontend.plist',
+               '~/Library/Saved Application State/com.electron.docker-frontend.savedState',
+               '~/Library/Logs/Docker Desktop',
+             ],
       rmdir: [
-        "~/Library/Caches/KSCrashReports",
-        "~/Library/Caches/com.plausiblelabs.crashreporter.data",
-      ]
+               '~/Library/Caches/KSCrashReports',
+               '~/Library/Caches/com.plausiblelabs.crashreporter.data',
+             ]
 end
