74f57b5cc132b714193d9dde873c7ed9e601b930
jorisguex <guex.joris@gmail.com>
2020-07-28 22:32:25 +0800

    Add Sonarr v3 beta

---
+++ Casks/sonarr-beta.rb
@@ -0,0 +1,19 @@
+cask 'sonarr-beta' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://services.sonarr.tv/v1/download/phantom-develop/latest?version=3&os=macos&installer=true'
+  name 'sonarr-beta'
+  homepage 'https://sonarr.tv/'
+
+  conflicts_with cask: 'sonarr'
+
+  depends_on cask: "mono-mdk"
+
+  app "Sonarr.app"
+
+  zap trash: [
+    '~/.config/Sonarr',
+    '~/Library/Saved Application State/com.osx.sonarr.tv.savedState'
+  ]
+end
