5dc10d723c7ccf706e074b762b231cb9ff97d011
Randall <ran.dall@icloud.com>
2020-07-27 21:45:13 -0700

    Update sourcetree-beta from 4.0b5_233 to 4.0.2b6_235

--- Casks/sourcetree-beta.rb
+++ Casks/sourcetree-beta.rb
@@ -1,26 +1,26 @@
-cask 'sourcetree-beta' do
-  version '4.0b5_233'
-  sha256 '7b2aa93b02c929ae9e99657f755b32da500ab23bd0efbf20c35a643ddb25c35a'
+cask "sourcetree-beta" do
+  version "4.0.2b6_235"
+  sha256 "57144614a0d6e0dea85b464deae3b3cbdceb3e285a28598969cb3a2fe346af76"
 
   # product-downloads.atlassian.com/software/sourcetree/ was verified as official when first introduced to the cask
   url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree_#{version}.zip"
-  appcast 'https://product-downloads.atlassian.com/software/sourcetree/Appcast/SparkleAppcastBeta.xml'
-  name 'Atlassian Sourctree'
-  homepage 'https://www.sourcetreeapp.com/'
+  appcast "https://product-downloads.atlassian.com/software/sourcetree/Appcast/SparkleAppcastBeta.xml"
+  name "Atlassian Sourctree"
+  homepage "https://www.sourcetreeapp.com/"
 
-  depends_on macos: '>= :mojave'
+  depends_on macos: ">= :mojave"
 
-  app 'Sourcetree-Beta.app'
+  app "Sourcetree-Beta.app"
   binary "#{appdir}/Sourcetree-Beta.app/Contents/Resources/stree"
 
-  uninstall launchctl: 'com.atlassian.SourceTreePrivilegedHelper2',
-            quit:      'com.torusknot.SourceTreeNotMAS'
+  uninstall launchctl: "com.atlassian.SourceTreePrivilegedHelper2",
+            quit:      "com.torusknot.SourceTreeNotMAS"
 
   zap trash: [
-               '~/Library/Application Support/SourceTree',
-               '~/Library/Caches/com.torusknot.SourceTreeNotMAS',
-               '~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist',
-               '~/Library/Preferences/com.torusknot.SourceTreeNotMAS.LSSharedFileList.plist',
-               '~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState',
-             ]
+    "~/Library/Application Support/SourceTree",
+    "~/Library/Caches/com.torusknot.SourceTreeNotMAS",
+    "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist",
+    "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.LSSharedFileList.plist",
+    "~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState",
+  ]
 end
