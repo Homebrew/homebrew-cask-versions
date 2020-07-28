103dd94d0f14badb0fef9cad6c2c27085250f5c6
Hannes Braun <hannesbraun@mail.de>
2020-07-28 13:47:30 +0200

    Update opera-developer from 71.0.3756.0 to 71.0.3763.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,20 +1,20 @@
-cask 'opera-developer' do
-  version '71.0.3756.0'
-  sha256 '9f43210b8bd29c1cfb4fc1c53c7488bd873014eda4fd9c52c8d18e6045826b18'
+cask "opera-developer" do
+  version "71.0.3763.0"
+  sha256 "c68448802e0ec755ca1d60b37359a661c1bbe6afa79f1a23548d2aba15d7df56"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
-  name 'Opera Developer'
-  homepage 'https://www.opera.com/computer/beta'
+  name "Opera Developer"
+  homepage "https://www.opera.com/computer/beta"
 
   auto_updates true
 
-  app 'Opera Developer.app'
+  app "Opera Developer.app"
 
   zap trash: [
-               '~/Library/Application Support/com.operasoftware.OperaDeveloper',
-               '~/Library/Caches/com.operasoftware.OperaDeveloper',
-               '~/Library/Cookies/com.operasoftware.OperaDeveloper.binarycookies',
-               '~/Library/Preferences/com.operasoftware.OperaDeveloper.plist',
-               '~/Library/Saved Application State/com.operasoftware.OperaDeveloper.savedState',
-             ]
+    "~/Library/Application Support/com.operasoftware.OperaDeveloper",
+    "~/Library/Caches/com.operasoftware.OperaDeveloper",
+    "~/Library/Cookies/com.operasoftware.OperaDeveloper.binarycookies",
+    "~/Library/Preferences/com.operasoftware.OperaDeveloper.plist",
+    "~/Library/Saved Application State/com.operasoftware.OperaDeveloper.savedState",
+  ]
 end
