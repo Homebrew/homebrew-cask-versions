9683151459258b1961aebef1408b124f5a8a9139
jorisguex <guex.joris@gmail.com>
2020-07-28 23:09:39 +0800

    Changed single quotes to double quotes

--- Casks/sonarr-beta.rb
+++ Casks/sonarr-beta.rb
@@ -1,19 +1,19 @@
-cask 'sonarr-beta' do
+cask "sonarr-beta" do
   version :latest
   sha256 :no_check
 
-  url 'https://services.sonarr.tv/v1/download/phantom-develop/latest?version=3&os=macos&installer=true'
-  name 'sonarr-beta'
-  homepage 'https://sonarr.tv/'
+  url "https://services.sonarr.tv/v1/download/phantom-develop/latest?version=3&os=macos&installer=true"
+  name "sonarr-beta"
+  homepage "https://sonarr.tv/"
 
-  conflicts_with cask: 'sonarr'
+  conflicts_with cask: "sonarr"
 
   depends_on cask: "mono-mdk"
 
   app "Sonarr.app"
 
   zap trash: [
-    '~/.config/Sonarr',
-    '~/Library/Saved Application State/com.osx.sonarr.tv.savedState'
+    "~/.config/Sonarr",
+    "~/Library/Saved Application State/com.osx.sonarr.tv.savedState"
   ]
 end
