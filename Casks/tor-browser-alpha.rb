8a2cf3c0b79ba57ba0006ee9b003ca9e118ebe8a
miccal <miccal.matthews@gmail.com>
2020-07-29 09:50:21 +0800

    Update tor-browser-alpha from 10.0a2 to 10.0a4

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -1,22 +1,22 @@
-cask 'tor-browser-alpha' do
-  version '10.0a2'
-  sha256 '2ab412d49089ad13a24487709683da0afb1d23254a15f71b1eb347b102211f64'
+cask "tor-browser-alpha" do
+  version "10.0a4"
+  sha256 "8edd64cca3ed11dfebd9857f3ca3d74d61dcac5d09671cab91a7a9697b47f034"
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
-  appcast 'https://www.torproject.org/download/alpha/'
-  name 'Tor Browser'
-  homepage 'https://www.torproject.org/'
+  appcast "https://www.torproject.org/download/alpha/"
+  name "Tor Browser"
+  homepage "https://www.torproject.org/"
 
   auto_updates true
-  conflicts_with cask: 'tor-browser'
+  conflicts_with cask: "tor-browser"
 
-  app 'Tor Browser.app'
+  app "Tor Browser.app"
 
   zap trash: [
-               '~/Library/Application Support/TorBrowser-Data',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.tor browser.sfl*',
-               '~/Library/Preferences/org.mozilla.tor browser.plist',
-               '~/Library/Preferences/org.torproject.torbrowser.plist',
-               '~/Library/SavedApplicationState/org.torproject.torbrowser.savedState',
-             ]
+    "~/Library/Application Support/TorBrowser-Data",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.mozilla.tor browser.sfl*",
+    "~/Library/Preferences/org.mozilla.tor browser.plist",
+    "~/Library/Preferences/org.torproject.torbrowser.plist",
+    "~/Library/SavedApplicationState/org.torproject.torbrowser.savedState",
+  ]
 end
