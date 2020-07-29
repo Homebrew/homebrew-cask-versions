7ddcf445af54fd5fb8a09902bb7718c51c4e3b9b
gottabeleGIT <sandesh.manik@icloud.com>
2020-07-29 17:43:49 -0500

    Update safari-technology-preview.rb

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,10 +1,10 @@
 cask "safari-technology-preview" do
   if MacOS.version <= :catalina
-    version "111,001-32479-20200728-12c458d1-e348-4ec5-9d55-9e9bad9c805e"
-    sha256 "6b199cca77be7407f40a62bc1ec576a8751b24da15613dfc0a951ac3d996f45f"
-  else
     version "111,001-32177-20200728-f459b529-99a7-4707-a85c-e693ddb56eee"
     sha256 "f95f72e8cf6a3160fad71411168c8f0eeb805ca1e7a7eec06b6e92d2a0ab6e85"
+  else
+    version "111,001-32479-20200728-12c458d1-e348-4ec5-9d55-9e9bad9c805e"
+    sha256 "6b199cca77be7407f40a62bc1ec576a8751b24da15613dfc0a951ac3d996f45f"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
