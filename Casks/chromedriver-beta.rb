ab1280bf3dcb3ff624418050d20a9761f9ce8aad
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:25:58 +0100

    chromedriver-beta.rb: fix for new style

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -1,13 +1,13 @@
-cask 'chromedriver-beta' do
-  version '83.0.4103.14'
-  sha256 '6e4eaa4adf086853f010678917f91c0ce6e59b3de572e3fe939ef49826b632b4'
+cask "chromedriver-beta" do
+  version "83.0.4103.14"
+  sha256 "6e4eaa4adf086853f010678917f91c0ce6e59b3de572e3fe939ef49826b632b4"
 
   # chromedriver.storage.googleapis.com/ was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
-  name 'ChromeDriver'
-  homepage 'https://sites.google.com/a/chromium.org/chromedriver/home'
+  name "ChromeDriver"
+  homepage "https://sites.google.com/a/chromium.org/chromedriver/home"
 
-  conflicts_with cask: 'chromedriver'
+  conflicts_with cask: "chromedriver"
 
-  binary 'chromedriver'
+  binary "chromedriver"
 end
