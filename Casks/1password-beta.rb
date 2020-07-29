d24ab72d354ff7699c130ef6dc00bd5499147dda
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:17:42 +0100

    1password-beta.rb: fix for new style

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,21 +1,21 @@
-cask '1password-beta' do
-  version '7.7.BETA-1'
-  sha256 'f37ce9388aa56fade9d7b99a541000712ef546b881672f8d30fa1f483a1a4314'
+cask "1password-beta" do
+  version "7.7.BETA-1"
+  sha256 "f37ce9388aa56fade9d7b99a541000712ef546b881672f8d30fa1f483a1a4314"
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
-  name '1Password'
-  homepage 'https://1password.com/'
+  name "1Password"
+  homepage "https://1password.com/"
 
   auto_updates true
-  conflicts_with cask: '1password'
+  conflicts_with cask: "1password"
 
   app "1Password #{version.major}.app"
 
   zap trash: [
-               '~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
-               '~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper',
-               '~/Library/Containers/com.agilebits.onepassword-osx',
-               '~/Library/Group Containers/2BUA8C4S2C.com.agilebits',
-             ]
+    "~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper",
+    "~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper",
+    "~/Library/Containers/com.agilebits.onepassword-osx",
+    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+  ]
 end
