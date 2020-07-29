9964e21698249e232b62bfc7af94c1e06e1ce911
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:31:08 +0100

    qt-creator-dev.rb: fix for new style

--- Casks/qt-creator-dev.rb
+++ Casks/qt-creator-dev.rb
@@ -1,10 +1,10 @@
-cask 'qt-creator-dev' do
-  version '4.13.0-beta1'
-  sha256 'd8e66726b208d8f3f12b53cc6432591676ad01e497d5cadd27844228dc6049cf'
+cask "qt-creator-dev" do
+  version "4.13.0-beta1"
+  sha256 "d8e66726b208d8f3f12b53cc6432591676ad01e497d5cadd27844228dc6049cf"
 
   url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
-  name 'Qt Creator Dev'
-  homepage 'https://www1.qt.io/developers/'
+  name "Qt Creator Dev"
+  homepage "https://www1.qt.io/developers/"
 
-  app 'Qt Creator.app'
+  app "Qt Creator.app"
 end
