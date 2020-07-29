c9e5e4b8e5b2fd518ec67adf14cb6f31aa7c826f
Vítor Galvão <info@vitorgalvao.com>
2020-07-29 14:33:43 +0100

    zulu13.rb: fix for new style

--- Casks/zulu13.rb
+++ Casks/zulu13.rb
@@ -1,11 +1,11 @@
-cask 'zulu13' do
-  version '13.0.4,13.33.25-ca'
-  sha256 'e8c78f485ef8c67c0cb9da90def67c545c07291aa351cd40d1df9a18ba60ce54'
+cask "zulu13" do
+  version "13.0.4,13.33.25-ca"
+  sha256 "e8c78f485ef8c67c0cb9da90def67c545c07291aa351cd40d1df9a18ba60ce54"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
-      referer: 'https://www.azul.com/downloads/zulu/zulu-mac/'
-  name 'Azul Zulu Java Standard Edition Development Kit'
-  homepage 'https://www.azul.com/downloads/zulu/zulu-mac/'
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  name "Azul Zulu Java Standard Edition Development Kit"
+  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
 
   pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
