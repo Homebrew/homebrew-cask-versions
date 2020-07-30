18a354834a545d564beaa0aa67a509827c3168fd
Brigitte Zmoelnig <brigitte@corecode.io>
2020-07-31 00:35:37 +0200

    Update discord-ptb from 0.0.48 to 0.0.49

--- Casks/discord-ptb.rb
+++ Casks/discord-ptb.rb
@@ -1,6 +1,6 @@
 cask "discord-ptb" do
-  version "0.0.48"
-  sha256 "d4bbbd3fefb53dbbac42823477762df083f139e3cf360877f85f80d3744b6237"
+  version "0.0.49"
+  sha256 "888b1227fa456438342594d0921e758514c09644d6aa23de9ea921be9747a085"
 
   url "https://cdn-ptb.discordapp.com/apps/osx/#{version}/DiscordPTB.dmg"
   appcast "https://discordapp.com/api/ptb/updates?platform=osx"
