ac791b2e9d0485ee6e57faace5aba8753f176356
jorisguex <guex.joris@gmail.com>
2020-07-30 11:28:59 +0800

    Update sonarr-beta.rb
    -added version number
    -added sha256

--- Casks/sonarr-beta.rb
+++ Casks/sonarr-beta.rb
@@ -1,8 +1,8 @@
 cask "sonarr-beta" do
-  version :latest
-  sha256 :no_check
+  version "3.0.3.904"
+  sha256 "a6da5157d3a2c733087cf290684de9773f5bc9502af7d92350cac1e7dd645f08"
 
-  url "https://services.sonarr.tv/v1/download/phantom-develop/latest?version=3&os=macos&installer=true"
+  url "http://download.sonarr.tv/v3/phantom-develop/#{version}/Sonarr.phantom-develop.#{version}.macos.zip"
   name "sonarr-beta"
   homepage "https://sonarr.tv/"
 
