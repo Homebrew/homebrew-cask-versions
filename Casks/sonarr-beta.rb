ac0e656773192291f9f1cd66bc6480d89f440375
jorisguex <guex.joris@gmail.com>
2020-07-31 08:40:15 +0800

    Add Sonarr v3 beta (#9382)
    * Add Sonarr v3 beta
    
    * Changed single quotes to double quotes
    
    * Removed line and added missing comma
    
    * Removed trailing newline
    
    * Update sonarr-beta.rb
    
    -added version number
    -added sha256
    
    * Update sonarr-beta.rb
    
    -added appcast link
    
    * Update sonarr-beta.rb
    
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/sonarr-beta.rb
@@ -0,0 +1,19 @@
+cask "sonarr-beta" do
+  version "3.0.3.904"
+  sha256 "a6da5157d3a2c733087cf290684de9773f5bc9502af7d92350cac1e7dd645f08"
+
+  url "http://download.sonarr.tv/v#{version.major}/phantom-develop/#{version}/Sonarr.phantom-develop.#{version}.macos.zip"
+  appcast "http://download.sonarr.tv/v#{version.major}/phantom-develop/"
+  name "sonarr-beta"
+  homepage "https://sonarr.tv/"
+
+  conflicts_with cask: "sonarr"
+  depends_on cask: "mono-mdk"
+
+  app "Sonarr.app"
+
+  zap trash: [
+    "~/.config/Sonarr",
+    "~/Library/Saved Application State/com.osx.sonarr.tv.savedState",
+  ]
+end
