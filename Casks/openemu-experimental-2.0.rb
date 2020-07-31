990783e12721c19509937b4c898b0416c43607e5
Rogerio Prado de Jesus <rogeriopradoj@gmail.com>
2020-07-30 21:28:33 -0300

    openemu-experimental-2.0: new cask
    Needed for usage with old macs without Metal GPU support but using macos :mojave or newer

---
+++ Casks/openemu-experimental-2.0.rb
@@ -0,0 +1,47 @@
+cask "openemu-experimental-2.0" do
+  version "2.0.9.1"
+    sha256 "62c44e823fef65c583cbf5e6f84faa03618d713f45610f73bc23fb34cbf64762"
+
+  # github.com/OpenEmu/OpenEmu/ was verified as official when first introduced to the cask
+  url "https://github.com/OpenEmu/OpenEmu/releases/download/v#{version}/OpenEmu_#{version}-experimental.zip"
+  appcast "https://github.com/OpenEmu/OpenEmu/releases.atom"
+  name "OpenEmu"
+  homepage "https://openemu.org/"
+
+  depends_on macos: ">= :el_capitan"
+
+  app "OpenEmu.app"
+
+  zap trash: [
+    "~/Library/Application Support/OpenEmu",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.openemu.openemu.sfl*",
+    "~/Library/Application Support/org.openemu.OEXPCCAgent.Agents",
+    "~/Library/Caches/OpenEmu",
+    "~/Library/Caches/org.openemu.OpenEmu",
+    "~/Library/Cookies/org.openemu.OpenEmu.binarycookies",
+    "~/Library/Preferences/org.openemu.Atari800.plist",
+    "~/Library/Preferences/org.openemu.Bliss.plist",
+    "~/Library/Preferences/org.openemu.CrabEmu.plist",
+    "~/Library/Preferences/org.openemu.desmume.plist",
+    "~/Library/Preferences/org.openemu.FCEU.plist",
+    "~/Library/Preferences/org.openemu.Gambatte.plist",
+    "~/Library/Preferences/org.openemu.GenesisPlus.plist",
+    "~/Library/Preferences/org.openemu.Higan.plist",
+    "~/Library/Preferences/org.openemu.Mednafen.plist",
+    "~/Library/Preferences/org.openemu.Mupen64Plus.plist",
+    "~/Library/Preferences/org.openemu.NeoPop.plist",
+    "~/Library/Preferences/org.openemu.Nestopia.plist",
+    "~/Library/Preferences/org.openemu.O2EM.plist",
+    "~/Library/Preferences/org.openemu.OpenEmu.plist",
+    "~/Library/Preferences/org.openemu.Picodrive.plist",
+    "~/Library/Preferences/org.openemu.PPSSPP.plist",
+    "~/Library/Preferences/org.openemu.ProSystem.plist",
+    "~/Library/Preferences/org.openemu.SNES9x.plist",
+    "~/Library/Preferences/org.openemu.Stella.plist",
+    "~/Library/Preferences/org.openemu.TwoMbit.plist",
+    "~/Library/Preferences/org.openemu.VecXGL.plist",
+    "~/Library/Preferences/org.openemu.VisualBoyAdvance.plist",
+    "~/Library/Preferences/org.openemu.mGBA.plist",
+    "~/Library/Saved Application State/org.openemu.OpenEmu.savedState",
+  ]
+end
