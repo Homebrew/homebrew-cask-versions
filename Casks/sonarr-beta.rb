c721a2b6dfe836a46269ad7542695d600ff4a5eb
Vítor Galvão <info@vitorgalvao.com>
2020-07-31 01:07:04 +0100

    Update sonarr-beta.rb

--- Casks/sonarr-beta.rb
+++ Casks/sonarr-beta.rb
@@ -2,8 +2,8 @@ cask "sonarr-beta" do
   version "3.0.3.904"
   sha256 "a6da5157d3a2c733087cf290684de9773f5bc9502af7d92350cac1e7dd645f08"
 
-  url "http://download.sonarr.tv/v3/phantom-develop/#{version}/Sonarr.phantom-develop.#{version}.macos.zip"
-  appcast "http://download.sonarr.tv/v3/phantom-develop/"
+  url "http://download.sonarr.tv/v#{version.major}/phantom-develop/#{version}/Sonarr.phantom-develop.#{version}.macos.zip"
+  appcast "http://download.sonarr.tv/v#{version.major}/phantom-develop/"
   name "sonarr-beta"
   homepage "https://sonarr.tv/"
 
