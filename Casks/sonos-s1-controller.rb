a5da2a61e0cda580154fcf17887adf6dc1bdd610
Josh Tilles <MerelyAPseudonym@gmail.com>
2020-08-05 19:46:56 -0400

    Update sonos-s1-controller from 11.2 to 11.2.2 (#9421)
    * Update sonos-s1-controller from 11.2 to 11.2.2
    
    * sonos-s1-controller: fix appcast stanza
    
    See Homebrew/brew#7606

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,10 +1,10 @@
 cask "sonos-s1-controller" do
-  version "11.2"
-  sha256 "1a98300bd16bb2f727eb1921db80777cc23178a483d56b44a9bb5d95d9ea6602"
+  version "11.2.2"
+  sha256 "ca9d7cdbc8d60970cd4933cbdff3c7600acaa905dc38d131777cef26bdd6eef8"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac",
-          configuration: version.no_dots
+          must_contain: version.no_dots
   name "Sonos S1"
   homepage "https://www.sonos.com/"
 
