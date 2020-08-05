147ef77132a99cd8dbd3f162382523da0873fc35
Randall <17261190+ran-dall@users.noreply.github.com>
2020-08-05 15:18:26 -0700

    Update discord-ptb from 0.0.49 to 0.0.50 (#9424)

--- Casks/discord-ptb.rb
+++ Casks/discord-ptb.rb
@@ -1,6 +1,6 @@
 cask "discord-ptb" do
-  version "0.0.49"
-  sha256 "888b1227fa456438342594d0921e758514c09644d6aa23de9ea921be9747a085"
+  version "0.0.50"
+  sha256 "e29c73a87c4bc3cb44886fd769908c2bec2c417bf10936c2ccb7fe467b7ae061"
 
   url "https://cdn-ptb.discordapp.com/apps/osx/#{version}/DiscordPTB.dmg"
   appcast "https://discordapp.com/api/ptb/updates?platform=osx"
