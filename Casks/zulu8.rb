d14eca21ad815788855738e51bd3fdb316897012
Mike Duigou <mike.duigou@liquid-robotics.com>
2020-08-05 10:04:30 -0700

    Update zulu8 from 8.0.262,8.48.0.49-ca to 8.0.265,8.48.0.53-ca

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -1,6 +1,6 @@
 cask "zulu8" do
-  version "8.0.262,8.48.0.49-ca"
-  sha256 "207af2863204d1d4856c54e4f3a8e046a54fe66958a06bc46a44d01e641f80b6"
+  version "8.0.265,8.48.0.53-ca"
+  sha256 "d2d850c6343a4da6d045e0300ac469929d1c8e2072040d1cfe2f48d264f0ab7d"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
