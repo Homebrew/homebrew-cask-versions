f320b20a845bb30ace662a42456f64abed2b47bd
i0ntempest <szf1234@me.com>
2020-08-06 12:59:30 -0400

    Update osu-development from 2020.714.0 to 2020.806.0

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,6 +1,6 @@
 cask "osu-development" do
-  version "2020.714.0"
-  sha256 "b411252bed7aeaf0a441db927b4d5599aee9215300277f5655101235d8659872"
+  version "2020.806.0"
+  sha256 "8808ecee68d5b2c2bf3d39092ce9cdadab2fb241b21d9b4933c503040e8b75ff"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   appcast "https://github.com/ppy/osu/releases.atom"
