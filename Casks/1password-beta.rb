cf4c079d838aad0b5f8b86f53a83a2135b2ebade
matthiasho <matthias.hortenhuber@ki.se>
2020-08-07 10:01:36 +0200

    Update 1password-beta from 7.7.BETA-2 to 7.7.BETA-3

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask "1password-beta" do
-  version "7.7.BETA-2"
-  sha256 "73379ac4da51966b06a828b79fe8698a386e392e3508a3b91c6a80ff17994a13"
+  version "7.7.BETA-3"
+  sha256 "668a81caf532ad50d007f18d7e5c55bc96518989e7edf8da6896b16d601923a8"
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
