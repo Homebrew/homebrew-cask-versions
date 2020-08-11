271855b05b2c510403d02fc0d42107e801495759
anatawa12 <anatawa12@icloud.com>
2020-08-12 04:16:36 +0900

    upgrade adoptopenjdk8 to 8u265b1 (#9446)

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -1,6 +1,6 @@
 cask "adoptopenjdk8" do
-  version "8,262:b10"
-  sha256 "2cec673a096ffadf288f64015a87a2492c16296656483c234a17bc0617d67241"
+  version "8,265:b01"
+  sha256 "db8b19f943bf96774a805b646b2c0ddd532123391e2e3873fe3720558e6091fe"
 
   # github.com/AdoptOpenJDK/openjdk8-binaries/ was verified as official when first introduced to the cask
   url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
