87611bc4cf3ac9f9abf437f03146c12aa4e4713a
miccal <miccal.matthews@gmail.com>
2020-08-12 08:16:37 +0800

    Update slack-beta from 4.8.0 to 4.9.0-beta1

--- Casks/slack-beta.rb
+++ Casks/slack-beta.rb
@@ -1,6 +1,6 @@
 cask "slack-beta" do
-  version "4.8.0"
-  sha256 "077ed1ef673e2c93d571502c49890d8d8c6f82bc91aff0543c6ff3404a0060b2"
+  version "4.9.0-beta1"
+  sha256 "0ede8236f43ccb062e42730fbcf02a7def3cde5b6752b74d2d1ed978b85e6fc2"
 
   # downloads.slack-edge.com/ was verified as official when first introduced to the cask
   url "https://downloads.slack-edge.com/releases/macos/#{version}/beta/x64/Slack-#{version}-macOS.zip"
