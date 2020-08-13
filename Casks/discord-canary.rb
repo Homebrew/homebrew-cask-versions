394642c67621ef0d7f9fcba7eb9262ab22593748
Andrew Zhou <0az@afzhou.com>
2020-08-12 21:06:20 -0500

    Update discord-canary from 0.0.251 to 0.0.254 (#9455)

--- Casks/discord-canary.rb
+++ Casks/discord-canary.rb
@@ -1,11 +1,12 @@
 cask "discord-canary" do
-  version "0.0.251"
-  sha256 "1fbeff1a34bc98190dc5fb12c1b24ac5c52cad9ac9d537a2798a1a9743a2b3ef"
+  version "0.0.254"
+  sha256 "62cb7a04416be8340259d413dfc093c83f5a83aaa27cf3e85d84b5f99b8f905f"
 
+  # discordapp.com was verified as official when first introduced to the cask
   url "https://cdn-canary.discordapp.com/apps/osx/#{version}/DiscordCanary.dmg"
-  appcast "https://discordapp.com/api/canary/updates?platform=osx"
+  appcast "https://canary.discord.com/api/canary/updates?platform=osx"
   name "Discord Canary"
-  homepage "https://discordapp.com/"
+  homepage "https://canary.discord.com/"
 
   auto_updates true
 
