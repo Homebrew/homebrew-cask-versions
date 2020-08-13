b8112e370e4fb3c730dd8a635b082acddf74c548
Miccal Matthews <miccal.matthews@gmail.com>
2020-08-13 10:06:46 +0800

    Update processing2 url (#9457)

--- Casks/processing2.rb
+++ Casks/processing2.rb
@@ -2,8 +2,7 @@ cask "processing2" do
   version "2.2.1"
   sha256 "8c237b3eb50626e8ffc648bfdeddaa18ceffbd6a48f8fec77a8eab5b774971fc"
 
-  # github.com/processing/processing/ was verified as official when first introduced to the cask
-  url "https://github.com/processing/processing/releases/download/processing-0227-#{version}/processing-#{version}-macosx.zip"
+  url "https://download.processing.org/processing-#{version}-macosx.zip"
   name "Processing"
   homepage "https://processing.org/"
 
