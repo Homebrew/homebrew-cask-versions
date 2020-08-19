f1deaf5e56023a5d3e64cc1bd15cb2458bf53e4d
Mike McQuaid <mike@mikemcquaid.com>
2020-08-19 15:47:40 +0100

    slicer-nightly: fix syntax error.

--- Casks/slicer-nightly.rb
+++ Casks/slicer-nightly.rb
@@ -6,7 +6,7 @@ cask "slicer-nightly" do
     require "open-uri"
     require "json"
     base_url = "https://download.slicer.org"
-    "#{base_url}/JSON.parse(URI(base_url + "/find?os=macosx&stability=nightly").read)["download_url"]"
+    "#{base_url}/#{JSON.parse(URI("#{base_url}/find?os=macosx&stability=nightly").read)["download_url"]}"
   end
   name "3D Slicer"
   homepage "https://www.slicer.org/"
