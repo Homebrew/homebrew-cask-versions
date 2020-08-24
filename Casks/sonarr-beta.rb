481bc857735810344d3f26e6329fbbab8b09f8a9
Viktor Szakats <commit@vsz.me>
2020-08-24 17:54:14 +0000

    sonarr-beta: secure urls

--- Casks/sonarr-beta.rb
+++ Casks/sonarr-beta.rb
@@ -2,8 +2,8 @@ cask "sonarr-beta" do
   version "3.0.3.907"
   sha256 "91c1cf79666afb22bf489e60a4f2b09a1db5aa5b3afba40e2812799e163ad13b"
 
-  url "http://download.sonarr.tv/v#{version.major}/phantom-develop/#{version}/Sonarr.phantom-develop.#{version}.macos.zip"
-  appcast "http://download.sonarr.tv/v#{version.major}/phantom-develop/"
+  url "https://download.sonarr.tv/v#{version.major}/phantom-develop/#{version}/Sonarr.phantom-develop.#{version}.macos.zip"
+  appcast "https://download.sonarr.tv/v#{version.major}/phantom-develop/"
   name "sonarr-beta"
   homepage "https://sonarr.tv/"
 
