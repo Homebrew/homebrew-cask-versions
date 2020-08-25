47fc574c2caef71db99a8e967eaf7d242fa3e7b5
yottalogical <yottalogical@gmail.com>
2020-08-25 18:30:01 -0400

    Add daedalus-flight 2.2.0

---
+++ Casks/daedalus-flight.rb
@@ -0,0 +1,21 @@
+cask "daedalus-flight" do
+  version "2.2.0,14238"
+  sha256 "395e0a57da910903127cc6aaaa793df42edc375e7423ead1df90a870c2f28116"
+
+  # update-cardano-mainnet-flight.iohk.io/ was verified as official when first introduced to the cask
+  url "https://update-cardano-mainnet-flight.iohk.io/daedalus-#{version.before_comma}-mainnet_flight-#{version.after_comma}.pkg"
+  name "Daedalus Flight"
+  homepage "https://daedaluswallet.io/en/flight/"
+
+  depends_on macos: ">= :high_sierra"
+
+  pkg "daedalus-#{version.before_comma}-mainnet_flight-#{version.after_comma}.pkg"
+
+  uninstall pkgutil: "org.Daedalusmainnet_flight.pkg"
+
+  zap trash: [
+    "~/Library/Application Support/Daedalus Flight",
+    "~/Library/Preferences/com.electron.daedalus-flight.plist",
+    "~/Library/Saved Application State/com.electron.daedalus-flight.savedState",
+  ]
+end
