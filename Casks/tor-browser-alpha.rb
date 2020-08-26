e24b76307d9c1ae7a189e9cbc2d645348f3110f8
Markus Reiter <me@reitermark.us>
2020-08-26 07:30:46 +0200

    Update tor-browser-alpha.rb

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -5,7 +5,7 @@ cask "tor-browser-alpha" do
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
   appcast "https://www.torproject.org/download/alpha/"
   name "Tor Browser"
-  desc "Web browser application focusing on security"
+  desc "Web browser focusing on security"
   homepage "https://www.torproject.org/"
 
   auto_updates true
