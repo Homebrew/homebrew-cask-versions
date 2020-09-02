5e32a94aabfc50e2e4e982a21a43fcdcd70cc26d
Johan Walles <johan.walles@gmail.com>
2020-09-02 18:55:25 +0200

    Long Term Support (LTS) version of Blender (#9542)

---
+++ Casks/blender-lts.rb
@@ -0,0 +1,27 @@
+cask "blender-lts" do
+  version "2.83.5"
+  sha256 "00a8e6ef52b84256ab64d33df3a079ebe5ac743aead06b74cd99987382be7f52"
+
+  url "https://download.blender.org/release/Blender#{version.major_minor}/blender-#{version}-macOS.dmg"
+  appcast "https://www.blender.org/download/lts/"
+  name "Blender"
+  desc "Free and open-source 3D creation suite"
+  homepage "https://www.blender.org/"
+
+  conflicts_with cask: "blender"
+
+  app "Blender.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/blender.wrapper.sh"
+  binary shimscript, target: "blender"
+
+  preflight do
+    # make __pycache__ directories writable, otherwise uninstall fails
+    FileUtils.chmod "u+w", Dir.glob("#{staged_path}/*.app/**/__pycache__")
+
+    IO.write shimscript, <<~EOS
+      #!/bin/bash
+      '#{appdir}/Blender.app/Contents/MacOS/Blender' "$@"
+    EOS
+  end
+end
