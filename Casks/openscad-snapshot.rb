5f416d5049d1e03788072d4e5bd5b9d3e65ca654
Joseph Paul <mail@jsph.pl>
2020-09-04 14:38:32 +0200

    Update appcast URL

--- Casks/openscad-snapshot.rb
+++ Casks/openscad-snapshot.rb
@@ -3,7 +3,7 @@ cask "openscad-snapshot" do
   sha256 "1481aee7bc20878da7d0667a009a6a2a0be398be6af93f2cd42df5262a95f55a"
 
   url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
-  appcast "https://www.openscad.org/inc/mac_snapshot_links.js"
+  appcast "https://files.openscad.org/snapshots/.snapshot_macos.js"
   name "OpenSCAD"
   homepage "https://www.openscad.org/downloads.html#snapshots"
 
