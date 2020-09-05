9de135ea40691fd8b7ec45c91e23aea2e850d028
Andrei Neculau <andrei.neculau@gmail.com>
2020-09-05 21:17:43 +0200

    Add a description to parallels15

--- Casks/parallels15.rb
+++ Casks/parallels15.rb
@@ -5,6 +5,7 @@ cask "parallels15" do
   url "https://download.parallels.com/desktop/v#{version.major}/#{version}/ParallelsDesktop-#{version}.dmg"
   appcast "https://kb.parallels.com/eu/124724"
   name "Parallels Desktop"
+  desc "Parallels Desktop for Mac is a desktop virtualization software."
   homepage "https://www.parallels.com/products/desktop/"
 
   auto_updates true
