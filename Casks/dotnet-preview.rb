252a8a51e484fd475528ab1112ef5ef08cf61a62
Robert Blair <robertleeblairjr@gmail.com>
2020-09-09 07:57:26 -0500

    Update dotnet-preview from 5.0.0-preview.5.20278.1,ed4dddfa-d703-451e-bd2f-8dfbe81a735e:9b15085e7d4be3d3c881abc24db523b8 to 5.0.0-preview.8.20407.11,67249b40-ac76-4628-a5b7-e7086e4d1444:ab8ed91c5e9080d4a7bffcb8f901d89e

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -1,6 +1,6 @@
 cask "dotnet-preview" do
-  version "5.0.0-preview.5.20278.1,ed4dddfa-d703-451e-bd2f-8dfbe81a735e:9b15085e7d4be3d3c881abc24db523b8"
-  sha256 "1064d2e25f1b2bd975d827bf5bfb1e009fd4a5c7a96a26d2fa9a3d0f556c473b"
+  version "5.0.0-preview.8.20407.11,67249b40-ac76-4628-a5b7-e7086e4d1444:ab8ed91c5e9080d4a7bffcb8f901d89e"
+  sha256 "71c0dea42411693f90b5da450f1b59554329776a8cc8bac68fb8005c2432cc00"
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
   appcast "https://dotnet.microsoft.com/download/dotnet-core"
