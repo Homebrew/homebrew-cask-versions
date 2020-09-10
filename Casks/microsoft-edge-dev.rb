9a5877cba8579669246e5324cf808bd480972122
suschizu <brigitte@corecode.io>
2020-09-10 13:37:41 +0200

    Update microsoft-edge-dev from 86.0.622.3 to 87.0.634.0 (#9594)

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,6 +1,6 @@
 cask "microsoft-edge-dev" do
-  version "86.0.622.3"
-  sha256 "40713f4f82815866ecd571444755f6fe2e8031f9415386282e1e20bc9110ee72"
+  version "87.0.634.0"
+  sha256 "975fcef79542d4995e8520f7d2ee8d1e9845d74a286ea29c1bda8fc71c9bff26"
 
   # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg"
