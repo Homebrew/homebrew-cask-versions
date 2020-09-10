e33f7327af5ae46ed4080af96496927f1567d2c8
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-10 23:55:07 +0800

    Update libreoffice-still (#9589)

--- Casks/libreoffice-still.rb
+++ Casks/libreoffice-still.rb
@@ -6,6 +6,7 @@ cask "libreoffice-still" do
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg"
   appcast "https://download.documentfoundation.org/libreoffice/stable/"
   name "LibreOffice Still"
+  desc "Free cross-platform office suite"
   homepage "https://www.libreoffice.org/"
 
   conflicts_with cask: "libreoffice"
