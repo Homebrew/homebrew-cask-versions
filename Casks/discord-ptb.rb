39439b9a719d1e7d97c29f1e73858a11223621ed
Work <brigitte@corecode.io>
2020-09-10 13:02:53 +0200

    Update discord-ptb from 0.0.50 to 0.0.51

--- Casks/discord-ptb.rb
+++ Casks/discord-ptb.rb
@@ -1,6 +1,6 @@
 cask "discord-ptb" do
-  version "0.0.50"
-  sha256 "e29c73a87c4bc3cb44886fd769908c2bec2c417bf10936c2ccb7fe467b7ae061"
+  version "0.0.51"
+  sha256 "71d987661b1303f48b6cccf6f6aad601c014a917e173936c6f9ccf91aa164091"
 
   url "https://cdn-ptb.discordapp.com/apps/osx/#{version}/DiscordPTB.dmg"
   appcast "https://discordapp.com/api/ptb/updates?platform=osx"
