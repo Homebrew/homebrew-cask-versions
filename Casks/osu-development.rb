9ffdbebe5f5b88739c441e58ef27e90e2a4d2436
Zhenfu Shi <szf1234@me.com>
2020-09-10 21:59:17 -0400

    Update osu-development from 2020.820.0 to 2020.910.0 (#9598)
    * Update osu-development from 2020.820.0 to 2020.910.0
    
    * Update osu-development.rb

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,13 +1,12 @@
 cask "osu-development" do
-  version "2020.820.0"
-  sha256 "d277523c3d32a24e8eeaf1d216d690a7000255de2504c69327153ad55e04913d"
+  version "2020.910.0"
+  sha256 "9a13ff8f213122ab04d60bf0ba49b5e8f990dffcf12fd7ace6bdebe59cd5d14f"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   appcast "https://github.com/ppy/osu/releases.atom"
   name "osu!lazer"
   homepage "https://github.com/ppy/osu/"
 
-  conflicts_with cask: "osu"
   depends_on macos: ">= :sierra"
 
   app "osu!.app"
