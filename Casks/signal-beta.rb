3df3464828411e991ce67c39d444f870fae920e8
LanikSJ <laniksj@gmail.com>
2020-09-11 10:59:56 -0700

    Update signal-beta from 1.36.1-beta.1 to 1.36.1-beta.2

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,6 +1,6 @@
 cask "signal-beta" do
-  version "1.36.1-beta.1"
-  sha256 "a4b0b7a52fed0cd6e16500bb26547fb80578ee0f9ec7f4c24e96dc3b14960699"
+  version "1.36.1-beta.2"
+  sha256 "3194f4e34beea48d6fd8297033bf80364375e5ee4bf28ccbf0f467e91c5d2814"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{version}.zip"
   appcast "https://github.com/signalapp/Signal-Desktop/releases.atom"
