48936545ba62e656fb435120f607dca50592ba8d
Randall <17261190+ran-dall@users.noreply.github.com>
2020-09-18 16:40:25 +0000

    Update affinity-designer-beta from 1.8.4.5 to 1.9.0.2 (#9641)

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,6 +1,6 @@
 cask "affinity-designer-beta" do
-  version "1.8.4.5"
-  sha256 "46afa474565015501d6c9d954dd5843d6af7e30e04a534e1e27f0d96da6ff294"
+  version "1.9.0.2"
+  sha256 "91a65f3f85b1c661f08fa924ae0192daf20e4a57f43a2ef76a618798df43c1dd"
 
   # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg"
