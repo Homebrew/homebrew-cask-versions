eb741c6dc0253856d3654800f94a0af4ae6ecff8
CoreCode <core-code@users.noreply.github.com>
2020-09-21 20:35:04 +0200

    Update affinity-designer-beta.rb (#9649)

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -4,7 +4,7 @@ cask "affinity-designer-beta" do
 
   # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg"
-  appcast "https://forum.affinity.serif.com/index.php?/forum/15-designer-beta-on-mac/"
+  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?encoding=utf8&url=https://forum.affinity.serif.com/index.php?/topic/83296-affinity-range-mac-beta-release-notifications/&splitter_1=Affinity%20Designer&index_1=1&splitter_2=Affinity%20Photo&index_2=0"
   name "Serif Affinity Designer"
   homepage "https://affinity.serif.com/en-us/"
 
