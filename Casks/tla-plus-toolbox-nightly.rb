981d01baacbdf3103b496154de753e5b49f511a6
Miccal Matthews <miccal.matthews@gmail.com>
2020-09-25 05:23:45 +0800

    Update tla-plus-toolbox-nightly.rb

--- Casks/tla-plus-toolbox-nightly.rb
+++ Casks/tla-plus-toolbox-nightly.rb
@@ -5,11 +5,12 @@ cask "tla-plus-toolbox-nightly" do
   # tla.msr-inria.inria.fr/ was verified as official when first introduced to the cask
   url do
     require "open-uri"
-    base_url = "https://tla.msr-inria.inria.fr/tlatoolbox/ci/products/"
+    base_url = "http://tla.msr-inria.inria.fr/tlatoolbox/ci/products/"
     file = URI(base_url).open.read.scan(/href="([^"]+-macosx.cocoa.x86_64.zip)"/).flatten.first
     "#{base_url}#{file}"
   end
   name "TLA+ Toolbox"
+  desc "IDE for TLA+"
   homepage "https://lamport.azurewebsites.net/tla/toolbox.html"
 
   conflicts_with cask: "tla-plus-toolbox"
