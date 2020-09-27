7f338e03ffe2dc003c449c41ef49683aced9fe76
Jacek Jabłoński <35669512+jacek-jablonski@users.noreply.github.com>
2020-09-27 20:36:09 +0200

    Update visual-studio-code-insiders from 1.50.0,8c1015f5e1a7a686d19ec8… (#9679)

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,6 +1,6 @@
 cask "visual-studio-code-insiders" do
-  version "1.50.0,8c1015f5e1a7a686d19ec8a5f56a495332f16062"
-  sha256 "39dbd92338f3929a7eaf8e19a28a9bd825cf8136fc29529b9a8ed79c283877e9"
+  version "1.50.0,ddc98c3f620ab1d2ecad5942ebf4736060403d67"
+  sha256 "dea31406d9b738a4d762b76d8c34d096f945bd0f28b6b0f89403ac7880efcff4"
 
   # az764295.vo.msecnd.net/insider/ was verified as official when first introduced to the cask
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-insider.zip"
