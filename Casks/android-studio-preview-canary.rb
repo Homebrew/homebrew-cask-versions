fc552ba805204fd73d56596422c2e21256393c94
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-04 16:09:57 +0800

    Update android-studio-preview-canary.rb

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -5,6 +5,7 @@ cask "android-studio-preview-canary" do
   # dl.google.com/dl/android/studio/ was verified as official when first introduced to the cask
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip"
   name "Android Studio Preview (Canary)"
+  desc "Tools for building Android applications"
   homepage "https://developer.android.com/studio/preview/"
 
   conflicts_with cask: "android-studio-preview-beta"
