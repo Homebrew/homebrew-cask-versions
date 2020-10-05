163fce706295738a481d360e400fa87359c1c042
Vítor Galvão <info@vitorgalvao.com>
2020-10-05 01:33:36 +0100

    Update tla-plus-toolbox-nightly.rb

--- Casks/tla-plus-toolbox-nightly.rb
+++ Casks/tla-plus-toolbox-nightly.rb
@@ -6,7 +6,7 @@ cask "tla-plus-toolbox-nightly" do
   url do
     require "open-uri"
     base_url = "https://tla.msr-inria.inria.fr/tlatoolbox/ci/products/"
-    file = URI(base_url).read.match(/href="([^"]+-macosx.cocoa.x86_64.zip)"/).to_s
+    file = URI(base_url).read[/href="([^"]+-macosx.cocoa.x86_64.zip)"/, 1]
     "#{base_url}#{file}"
   end
   name "TLA+ Toolbox"
