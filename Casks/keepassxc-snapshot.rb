051402fc3a842ed5fc1cd20d72e705c7617e7089
Vítor Galvão <info@vitorgalvao.com>
2020-10-05 06:42:59 +0100

    keepassxc-snapshot.rb: replace scan.flatten.first with match (#9713)

--- Casks/keepassxc-snapshot.rb
+++ Casks/keepassxc-snapshot.rb
@@ -5,15 +5,11 @@ cask "keepassxc-snapshot" do
   url do
     require "open-uri"
     base_url = "https://snapshot.keepassxc.org/latest/"
-    path = URI(base_url)
-           .open
-           .read
-           .scan(/href="([^"]+-snapshot\.dmg)">/)
-           .flatten
-           .first
+    path = URI(base_url).read[/href="([^"]+-snapshot\.dmg)"/, 1]
     "#{base_url}#{path}"
   end
   name "KeePassXC"
+  desc "Password manager app"
   homepage "https://keepassxc.org/"
 
   app "KeePassXC.app"
