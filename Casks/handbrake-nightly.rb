dd7566d2ae9eb438924ce80eb7569b7b83fa0957
Vítor Galvão <info@vitorgalvao.com>
2020-10-05 06:43:18 +0100

    handbrake-nightly.rb: replace scan.flatten.first with match (#9714)

--- Casks/handbrake-nightly.rb
+++ Casks/handbrake-nightly.rb
@@ -5,9 +5,10 @@ cask "handbrake-nightly" do
   url do
     require "open-uri"
     base_url = "https://handbrake.fr/nightly.php"
-    URI(base_url).open.read.scan(/href="([^"]+.dmg)"/).flatten.first
+    URI(base_url).read[/href="([^"]+.dmg)"/, 1]
   end
   name "HandBrake"
+  desc "Open-source video transcoder"
   homepage "https://handbrake.fr/nightly.php"
 
   app "HandBrake.app"
