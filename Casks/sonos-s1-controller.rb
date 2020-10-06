7e5474e6afe498e2d7f49a6e9b3a8fc15d945f23
Bob Olde Hampsink <378974+boboldehampsink@users.noreply.github.com>
2020-10-06 08:27:11 +0200

    Update sonos-s1-controller from 11.2.2 to 11.2.3 (#9722)

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,6 +1,6 @@
 cask "sonos-s1-controller" do
-  version "11.2.2"
-  sha256 "ca9d7cdbc8d60970cd4933cbdff3c7600acaa905dc38d131777cef26bdd6eef8"
+  version "11.2.3"
+  sha256 "5176892eff5a52afe07a85a39d2b43438cf0c3b116b9e206e19d119d53ade901"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac",
