3b3a6f5cce9f9cefac386f6aa835e8b43d3cf32a
Fernando Pérez-García <fepegar@gmail.com>
2020-10-12 22:20:02 +0100

    Update and rename slicer-nightly.rb to slicer-preview.rb (#9751)

--- Casks/slicer-preview.rb
+++ Casks/slicer-preview.rb
@@ -1,4 +1,4 @@
-cask "slicer-nightly" do
+cask "slicer-preview" do
   version :latest
   sha256 :no_check
 
@@ -9,7 +9,10 @@ cask "slicer-nightly" do
     "#{base_url}/#{JSON.parse(URI("#{base_url}/find?os=macosx&stability=nightly").read)["download_url"]}"
   end
   name "3D Slicer"
+  desc "Medical image processing and visualization system"
   homepage "https://www.slicer.org/"
+
+  conflicts_with cask: "slicer"
 
   app "Slicer.app"
 end
