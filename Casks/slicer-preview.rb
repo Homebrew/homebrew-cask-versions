da5fb9b8d5eaad3053207847bce5ea6cac442a8c
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-13 04:47:16 +0800

    Update slicer-preview.rb

--- Casks/slicer-preview.rb
+++ Casks/slicer-preview.rb
@@ -9,7 +9,10 @@ cask "slicer-preview" do
     "#{base_url}/#{JSON.parse(URI("#{base_url}/find?os=macosx&stability=nightly").read)["download_url"]}"
   end
   name "3D Slicer"
+  desc "Medical image processing and visualization system"
   homepage "https://www.slicer.org/"
+
+  conflicts_with cask: "slicer"
 
   app "Slicer.app"
 end
