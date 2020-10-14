3e72bdb7d45b11d857798f44f0cde8d8e27f9074
Yoshiki Sato <xight.org@gmail.com>
2020-10-15 02:26:26 +0900

    fix (brew cask style --fix) reports offenses.

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -1,6 +1,7 @@
 cask "chromedriver-beta" do
   version "86.0.4240.22"
   sha256 "351ae30e9feab7ca6ccb94a549afcd62d6355561b78b8386cd4271d480a2fdc6"
+
   # chromedriver.storage.googleapis.com/ was verified as official when first introduced to the cask
   url "https://chromedriver.storage.googleapis.com/#{version}/chromedriver_mac64.zip"
   name "ChromeDriver"
