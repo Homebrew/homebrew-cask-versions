d5371eefcfbf08b415ad707f326086d5dc5b84a6
Miccal Matthews <miccal.matthews@gmail.com>
2020-10-17 06:54:18 +0800

    Update java-beta.rb

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,10 @@
-# frozen_string_literal: true
-
-cask("java-beta") do
+cask "java-beta" do
   version "16,20"
   sha256 "5cc94a6165d433c7070464edebaf971d563f90cda7f59196dffadc03eab40961"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
-  desc "Early access runtime for the Java programming language"
+  desc "Early access development kit for the Java programming language"
   homepage "https://jdk.java.net/"
 
   artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
