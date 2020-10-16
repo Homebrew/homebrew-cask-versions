3f55914f110d94645ce740ff61fe7015b1aefe62
John-Marvin Davis <davisjohnmarvin@gmail.com>
2020-10-17 01:19:23 +0200

    Update broken java-beta to jdk 16, build 20 (#9770)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,9 +1,10 @@
 cask "java-beta" do
-  version "15,26"
-  sha256 "e6587942e4530f55536a9c5572d0219d55c291da459c61284c411ab108abec04"
+  version "16,20"
+  sha256 "5cc94a6165d433c7070464edebaf971d563f90cda7f59196dffadc03eab40961"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
+  desc "Early access development kit for the Java programming language"
   homepage "https://jdk.java.net/"
 
   artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
