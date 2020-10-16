f9f7395bb14b70aa4a1b2045faceb8646a23590f
John-Marvin Davis <johndavis@ebay.com>
2020-10-16 13:44:04 +0200

    Fixes obsolete url and updates to major version 16, minor version 20

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,6 +1,6 @@
 cask "java-beta" do
-  version "15,26"
-  sha256 "e6587942e4530f55536a9c5572d0219d55c291da459c61284c411ab108abec04"
+  version "16,20"
+  sha256 "5cc94a6165d433c7070464edebaf971d563f90cda7f59196dffadc03eab40961"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
