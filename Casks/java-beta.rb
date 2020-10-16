8b71b8ed558a9988f7ae217c1e4537a5ac3e6500
John-Marvin Davis <johndavis@ebay.com>
2020-10-16 13:59:50 +0200

    Adds a description

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -4,6 +4,7 @@ cask "java-beta" do
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
+  desc "Early access runtime for the Java programming language"
   homepage "https://jdk.java.net/"
 
   artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
