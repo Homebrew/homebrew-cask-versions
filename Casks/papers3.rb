6cf03b4affea77118bd090a968d4c1fc92a02a5e
Kyungdahm Yun <tomyun@gmail.com>
2020-10-21 23:04:49 -0700

    Update papers3 from 3.4.23_587 to 3.4.25_600 (#9802)
    * Update papers3 from 3.4.23_587 to 3.4.25_600
    
    * Update papers3.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/papers3.rb
+++ Casks/papers3.rb
@@ -1,10 +1,11 @@
 cask "papers3" do
-  version "3.4.23_587"
-  sha256 "6960a5ad8d886d67cf9ab39730baa04f9fb5fb64a2c28cd0c25073f9b515f565"
+  version "3.4.25_600"
+  sha256 "3a67540bcc9c676301567bf8654ae745fb900cd5736a90715f504458de272282"
 
   # downloads.mekentosj.com was verified as official when first introduced to the cask
   url "https://downloads.mekentosj.com/papers_#{version.no_dots}.dmg"
   name "Papers"
+  desc "Reference manager for researchers"
   homepage "https://support.papersapp.com/support/solutions/articles/30000031865-existing-papers-3-users-accessing-papers-3-program-files-for-additional-device-installs"
 
   app "Papers.app"
