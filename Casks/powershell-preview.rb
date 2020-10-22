6758b4e7d5ed64d15374ddf0c1ce337bc6f44340
Brett Miller <brett@millerb.co.uk>
2020-10-22 17:25:38 +0100

    Update powershell-preview.rb

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -1,5 +1,5 @@
 cask "powershell-preview" do
-  version "v7.1.0-rc.2"
+  version "7.1.0-rc.2"
   sha256 "4fa72e3ac192045ff64c86c3d1498150f71ab60aec8fb7790a46a495b2f0696a"
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
