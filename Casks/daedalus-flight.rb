94cf5ee3771837fc210268dd8b8b72c3caefddf4
yottalogical <yottalogical@gmail.com>
2020-10-28 15:53:29 -0400

    Update daedalus-flight from 2.3.0-FC1 to 2.4.0

--- Casks/daedalus-flight.rb
+++ Casks/daedalus-flight.rb
@@ -1,6 +1,6 @@
 cask "daedalus-flight" do
-  version "2.3.0-FC1,14562"
-  sha256 "8561cde63ce713bac9173e8d243b15ffed65a71507ce3fe83cd71653ec19fbfa"
+  version "2.4.0,14924"
+  sha256 "a6112d60c91246dddbeb6dc201b916c541311cc333f17e977dc8a70737ff0888"
 
   # update-cardano-mainnet-flight.iohk.io/ was verified as official when first introduced to the cask
   url "https://update-cardano-mainnet-flight.iohk.io/daedalus-#{version.before_comma}-mainnet_flight-#{version.after_comma}.pkg"
