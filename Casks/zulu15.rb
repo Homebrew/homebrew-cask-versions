43573795764992b65879b6c5d4429900dea923ca
Piotr Findeisen <piotr.findeisen@gmail.com>
2020-10-29 11:08:02 +0100

    Add zulu15 - Azul Zulu JDK 15 (new cask)

---
+++ Casks/zulu15.rb
@@ -0,0 +1,14 @@
+cask "zulu15" do
+  version "15.0.1,15.28.13-ca"
+  sha256 "705b041d13c2499d3c6b42eacee1190ca28f144d252dc5836583365578b4ec0f"
+
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  name "Azul Zulu Java Standard Edition Development Kit"
+  desc "Azul Zulu Java Standard Edition Development Kit, version 15"
+  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+end
