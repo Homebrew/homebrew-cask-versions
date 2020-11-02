e065df4edf318f34d1ea8a018970bd6832e62746
Colin Rundel <rundel@gmail.com>
2020-11-02 15:38:38 +0000

    Update rstudio-preview from 1.4.953 to 1.4.993

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask "rstudio-preview" do
-  version "1.4.953"
-  sha256 "4ca790cfa16256d37b4f7682a279da8fda9b5d6feee35f4af9a2664ef2ecce9a"
+  version "1.4.993"
+  sha256 "1ad6f6eb88ec01984b0e2a1e085748f34b23503052b04568c67582991cc2e88c"
 
   # s3.amazonaws.com/rstudio-ide-build/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
