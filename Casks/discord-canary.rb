ae404abe04d205b539577d8d2faed02e9f31d6e3
Randall <ran.dall@icloud.com>
2020-11-03 08:57:54 -0800

    Update discord-canary from 0.0.259 to 0.0.260

--- Casks/discord-canary.rb
+++ Casks/discord-canary.rb
@@ -1,6 +1,6 @@
 cask "discord-canary" do
-  version "0.0.259"
-  sha256 "c516297f199078a63e718684a6a54e5ae4d6b9feb69bac55b6abedb3dce586aa"
+  version "0.0.260"
+  sha256 "1303e8172b8f03c98b0b1d8b412c087cfc5876246bd557f2c2e65aca0d936877"
 
   # discordapp.com was verified as official when first introduced to the cask
   url "https://cdn-canary.discordapp.com/apps/osx/#{version}/DiscordCanary.dmg"
