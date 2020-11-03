d3a33ad80426797457f1a1010c7c9896a909b6f7
Chris Jones <chris.r.jones.1983@gmail.com>
2020-11-03 17:18:10 -0600

    fix sha256 mismatch for freecad-pre.rb

--- Casks/freecad-pre.rb
+++ Casks/freecad-pre.rb
@@ -1,6 +1,6 @@
 cask "freecad-pre" do
   version "0.19_pre,22894"
-  sha256 "bf2195381b58272dac246c7dbbe89fdec7d378f853ed79bc41b3aae4f757fa04"
+  sha256 "a74c87512c845569fd356207be86e71c6691cbe26c7700b792591105c1257586"
 
   # github.com/FreeCAD/FreeCAD/releases/download was verified as official when first introduced to the cask
   url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor.delete_suffix("_pre")}-#{version.after_comma}-macOS-x86_64-conda.dmg"
