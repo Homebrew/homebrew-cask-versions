380561f517f5e552866fb32a1a105815036e80a0
Chris <chris.r.jones.1983@gmail.com>
2020-11-03 17:31:58 -0600

    add the development snapshot of FreeCAD (#9863)

---
+++ Casks/freecad-pre.rb
@@ -0,0 +1,15 @@
+cask "freecad-pre" do
+  version "0.19_pre,22894"
+  sha256 "a74c87512c845569fd356207be86e71c6691cbe26c7700b792591105c1257586"
+
+  # github.com/FreeCAD/FreeCAD/ was verified as official when first introduced to the cask
+  url "https://github.com/FreeCAD/FreeCAD/releases/download/#{version.before_comma}/FreeCAD_#{version.major_minor.delete_suffix("_pre")}-#{version.after_comma}-macOS-x86_64-conda.dmg"
+  appcast "https://github.com/freecad/freecad/releases.atom"
+  name "FreeCAD"
+  desc "3D parametric modeler"
+  homepage "https://freecadweb.org/"
+
+  depends_on macos: ">= :sierra"
+
+  app "FreeCAD.app"
+end
