db6f60a3e6944cb2af691a93ef74dbcc70a26472
David Yantis <404NetworkError@users.noreply.github.com>
2020-11-04 06:34:45 -0500

    Update iterm2-beta from 3.4.0beta12 to 3.4.0beta13 (#9861)
    * Update iterm2-beta from 3.4.0beta12 to 3.4.0beta13
    
    * Update iterm2-beta.rb
    
    * Update iterm2-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,11 +1,12 @@
 cask "iterm2-beta" do
   # note: "2" is not a version number, but an intrinsic part of the product name
-  version "3.4.0beta12"
-  sha256 "294a87b54db4e4e765d222d140b9511162997d23842b55c0f56850ca5c406a48"
+  version "3.4.0beta13"
+  sha256 "7a6b21c55233cc452ef3a5e5766abeb0580fb5913c1df83ccc378a4da8c4939c"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   appcast "https://iterm2.com/appcasts/testing_modern.xml"
   name "iTerm2"
+  desc "Terminal emulator as alternative to Apple's Terminal app"
   homepage "https://www.iterm2.com/"
 
   auto_updates true
