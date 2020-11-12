6f3c486d18aec561c973618ffe9300251a68b6d3
Randall <ran.dall@icloud.com>
2020-11-12 11:53:49 -0800

    Update brave-browser-beta from 87.1.17.64,117.64 to 87.1.18.51,118.51

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,6 +1,6 @@
 cask "brave-browser-beta" do
-  version "87.1.17.64,117.64"
-  sha256 "a797b40a86f81c79c166e7346726604abc885bc1d1db6636ad33a68430a1fe3a"
+  version "87.1.18.51,118.51"
+  sha256 "24776f5c998dc2ebd54d02789c1f99b7b4712174027e5f58263ca1f1db6b90dd"
 
   # updates-cdn.bravesoftware.com/sparkle/Brave-Browser/ was verified as official when first introduced to the cask
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta.dmg"
