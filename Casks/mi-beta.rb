b897cc245c58667c3c81ad91d3c97c6c7ee87c0b
anatawa12 <anatawa12@icloud.com>
2020-11-15 21:21:50 +0900

    Add mi beta (#9921)
    * add mi version beta
    
    * add conflicts_with
    
    * fix sha256 hash
    
    * fix urls
    
    * add desc
    
    * fix a little the syntax
    
    * Update Casks/mi-beta.rb
    
    * Update Casks/mi-beta.rb
    
    Co-authored-by: Markus Reiter <me@reitermark.us>

---
+++ Casks/mi-beta.rb
@@ -0,0 +1,19 @@
+cask "mi-beta" do
+  version "3.5b4"
+  sha256 "0586d48d7796932ac88f30c02eeee896cdc9165caa88247c984126d4847ea3bc"
+
+  url "https://www.mimikaki.net/download/mi#{version}.dmg"
+  appcast "https://www.mimikaki.net/download/appcast_beta.xml"
+  name "mi"
+  desc "Text editor"
+  homepage "https://www.mimikaki.net/download/beta.html"
+
+  conflicts_with cask: "mi"
+
+  app "mi.app"
+
+  zap trash: [
+    "~/Library/Caches/net.mimikaki.mi",
+    "~/Library/Preferences/net.mimikaki.mi.plist",
+  ]
+end
