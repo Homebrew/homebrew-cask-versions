9ad1c70eb5ac80933a1b006c7a9d7eedbb81b79f
anatawa12 <anatawa12@icloud.com>
2020-11-15 11:10:07 +0900

    fix sha256 hash

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -1,6 +1,6 @@
 cask "mi-beta" do
   version "3.5b4"
-  sha256 "f6e5d1a6845277a9bb5b994df713f9523a0ce7e9071ed85a3a88917adee281e7"
+  sha256 "0586d48d7796932ac88f30c02eeee896cdc9165caa88247c984126d4847ea3bc"
 
   url "https://www.mimikaki.net/download/mi#{version}.dmg"
   appcast "https://www.mimikaki.net/download/appcast.xml"
