3a12d9275229cad365ceb56e47d743906b689f75
anatawa12 <anatawa12@icloud.com>
2020-11-15 12:04:03 +0900

    fix a little the syntax

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -2,10 +2,10 @@ cask "mi-beta" do
   version "3.5b4"
   sha256 "0586d48d7796932ac88f30c02eeee896cdc9165caa88247c984126d4847ea3bc"
 
-  url "https://www.mimikaki.net/download/mi#{version}.dmg"	
+  url "https://www.mimikaki.net/download/mi#{version}.dmg"
   appcast "https://www.mimikaki.net/download/appcast_beta.xml"
   name "mi"
-  desc "A simple text editor for mac"
+  desc "A simple text editor"
   homepage "https://www.mimikaki.net/download/beta.html"
 
   conflicts_with cask: [
