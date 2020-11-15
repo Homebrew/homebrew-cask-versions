1aa55e71b4e427a61f23852ae3836aa77aff4cbf
Markus Reiter <me@reitermark.us>
2020-11-15 07:16:22 +0100

    Update Casks/mi-beta.rb

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -5,7 +5,7 @@ cask "mi-beta" do
   url "https://www.mimikaki.net/download/mi#{version}.dmg"
   appcast "https://www.mimikaki.net/download/appcast_beta.xml"
   name "mi"
-  desc "A simple text editor"
+  desc "Text editor"
   homepage "https://www.mimikaki.net/download/beta.html"
 
   conflicts_with cask: "mi"
