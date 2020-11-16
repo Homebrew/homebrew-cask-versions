40d9400cdd6675b3c1d54d4a6dac079306976836
Markus Reiter <me@reitermark.us>
2020-11-16 02:35:15 +0100

    Fix RuboCop offenses. (#9923)

--- Casks/iterm2-legacy.rb
+++ Casks/iterm2-legacy.rb
@@ -1,5 +1,5 @@
 cask "iterm2-legacy" do
-  # note: "2" is not a version number, but indicates a different vendor
+  # NOTE: "2" is not a version number, but indicates a different vendor
   version "2.1.4"
   sha256 "1062b83e7808dc1e13362f4a83ef770e1c24ea4ae090d1346b49f6196e9064cd"
 
