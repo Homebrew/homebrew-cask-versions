e71d446b36075dd99d6ebab5c39ad8761bb63597
Emrah Er <emraher@users.noreply.github.com>
2020-11-17 08:36:38 +0300

    Update rstudio-preview from 1.4.1013 to 1.4.1032 (#9928)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask "rstudio-preview" do
-  version "1.4.1013"
-  sha256 "ee2f9f083f7a79be9a0656a640051e46789f5600c530e1c56219b3aecc1dc6ee"
+  version "1.4.1032"
+  sha256 "22fdb49f2b8541bdf3e45f6df230fc036752c1ffd2f984490b34a03b0a97f8b2"
 
   # s3.amazonaws.com/rstudio-ide-build/ was verified as official when first introduced to the cask
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg"
