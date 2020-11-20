89061dcf8b4d78e02a06ba160c44cb7f40de361a
John Benninghoff <jbenninghoff@mac.com>
2020-11-20 06:00:07 -0600

    Update vmware-fusion11 from 11.5.6-16696540 to 11.5.7-17130923

--- Casks/vmware-fusion11.rb
+++ Casks/vmware-fusion11.rb
@@ -1,6 +1,6 @@
 cask "vmware-fusion11" do
-  version "11.5.6-16696540"
-  sha256 "f26f8404204af4fcd8932789d849a56e96d0dbeab1a76e35e6a2b42f2f31ffc2"
+  version "11.5.7-17130923"
+  sha256 "c7d58ca44510de6c1ddffe86129ed19982114e742d71e7d81e4a5882036e06e3"
 
   url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-#{version}.dmg"
   appcast "https://softwareupdate.vmware.com/cds/vmw-desktop/fusion.xml"
