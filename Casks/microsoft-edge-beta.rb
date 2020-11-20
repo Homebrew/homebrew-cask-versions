46000d29618b2a93fe40cff64657a5978a1a34ba
Randall <17261190+ran-dall@users.noreply.github.com>
2020-11-19 17:59:04 -0800

    Update microsoft-edge-beta from 87.0.664.36 to 87.0.664.41 (#9941)
    * Update microsoft-edge-beta from 87.0.664.36 to 87.0.664.40
    
    * Update microsoft-edge-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -1,11 +1,12 @@
 cask "microsoft-edge-beta" do
-  version "87.0.664.36"
-  sha256 "47eca46dbb8e59d48573e71898827453bc7ac7f783e13334fa55e294fd62409b"
+  version "87.0.664.41"
+  sha256 "16cec397f4fb2503696b052778ce35b2dd47640fb63bbe6daf5c2aa1150d8942"
 
   # officecdn-microsoft-com.akamaized.net/ was verified as official when first introduced to the cask
   url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg"
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069439"
   name "Microsoft Edge Beta"
+  desc "Multi-platform web browser"
   homepage "https://www.microsoftedgeinsider.com/"
 
   auto_updates true
