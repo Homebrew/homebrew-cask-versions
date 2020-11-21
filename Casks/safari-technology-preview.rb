1e80fdba8ef27c6aa254dee9bf7c51dd01440518
Peter Lewis <peter@peterlew.is>
2020-11-21 12:59:36 +0000

    Update safari-technology-preview.rb (#9948)
    * Update safari-technology-preview.rb
    
    * Update safari-technology-preview.rb

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,15 +1,16 @@
 cask "safari-technology-preview" do
   if MacOS.version <= :catalina
-    version "113,001-46217-20200908-26bf578a-dcb0-4f70-b930-d9131bbf5d8a"
-    sha256 "09f86d0808e067a46584e9394767040012d7bbae453bb9842cabbf593d9185d9"
+    version "116,001-82546-20201119-d46bca22-a3dc-43c3-9fcf-29f7343956ac"
+    sha256 "37a631632e2d449ce5dfb117d0eb7fd14574f00ca306c27667ffbbe6b1c0bc68"
   else
-    version "113,001-43557-20200908-b620aaf9-e006-4855-8bdf-e7e76b5950bc"
-    sha256 "3dcf197b8c2c181861d0c3f3d00fbb65940d1c1aaf11511c76c94fb153d0a7f0"
+    version "116,001-69966-20201119-d87990cd-c094-40a8-a616-a4497506caed"
+    sha256 "64b82eb45729ee366272da25849039762c5382373ebedbad30168d80a06ec6b7"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
   appcast "https://developer.apple.com/safari/download/"
   name "Safari Technology Preview"
+  desc "Web browser"
   homepage "https://developer.apple.com/safari/download/"
 
   auto_updates true
