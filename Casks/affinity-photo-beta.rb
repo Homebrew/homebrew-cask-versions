b042264610275744ed2c94c6f363405fcceff335
Julian Mayer <julian@corecode.io>
2020-11-27 11:41:44 +0100

    Update affinity-photo-beta from 1.9.0.204 to 1.9.0.205

--- Casks/affinity-photo-beta.rb
+++ Casks/affinity-photo-beta.rb
@@ -1,6 +1,6 @@
 cask "affinity-photo-beta" do
-  version "1.9.0.204"
-  sha256 "dcb4de608a90e0e54ce74eaa6784e301329d531e0bb1f4eb23649676881bfbf8"
+  version "1.9.0.205"
+  sha256 "6931cd26990576547040552329d720d08c6cbbcb77c6421ba6805617309da6c9"
 
   # affinity-beta.s3.amazonaws.com/ was verified as official when first introduced to the cask
   url "https://affinity-beta.s3.amazonaws.com/download/Affinity%20Photo%20Customer%20Beta.zip"
