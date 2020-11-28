412e668ab2db6ce7351d82c9cb7582ba6fc0da8e
Diego Jahn <jahndiego@mailbox.org>
2020-11-28 19:28:15 +0100

    Upgrade Morpheus.app to v2.2.0_rc1

--- Casks/morpheus-beta.rb
+++ Casks/morpheus-beta.rb
@@ -1,6 +1,6 @@
 cask "morpheus-beta" do
-  version "2.2.0_beta3"
-  sha256 "20ba4b5860d02dbdf703f15c9d7828162f8a9d5eba50480f0ac04fde9be7a1c3"
+  version "2.2.0_rc1"
+  sha256 "a095b04df3cdef5d877371873a8540bca7b13ae068b9ce60b96218c7faffc573"
 
   # imc.zih.tu-dresden.de/morpheus/ was verified as official when first introduced to the cask
   url "https://imc.zih.tu-dresden.de/morpheus/packages/mac/Morpheus-#{version}.dmg"
@@ -14,7 +14,15 @@ cask "morpheus-beta" do
   app "Morpheus.app"
 
   zap trash: [
+    "~/Library/Application Support/data/Morpheus",
     "~/Library/Application Support/Morpheus",
+    "~/Library/Application Support/CrashReporter/morpheus_*.plist",
+    "~/Library/Caches/Morpheus",
+    "~/Library/Preferences/morpheus-gui.plist",
+    "~/Library/Preferences/io.gitlab.morpheus.morpheus.plist",
+    "~/Library/Preferences/de.tu-dresden.Morpheus.plist",
     "~/Library/Preferences/org.morpheus.Morpheus.plist",
+    "~/Library/Saved Application State/de.tu-dresden.Morpheus.savedState",
+    "~/Library/Saved Application State/io.gitlab.morpheus.morpheus.savedState",
   ]
 end
