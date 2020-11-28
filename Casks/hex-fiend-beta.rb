6b20bb4fa8ce9e078d17353e42fcaf2d1341a050
404NetworkError <404NetworkError@users.noreply.github.com>
2020-11-27 22:41:45 -0500

    Update hex-fiend-beta from 2.14.0b1 to 2.14.1b

--- Casks/hex-fiend-beta.rb
+++ Casks/hex-fiend-beta.rb
@@ -1,9 +1,9 @@
 cask "hex-fiend-beta" do
-  version "2.14.0b1"
-  sha256 "61b264e27089d7b3ac895ad5dc2cf269f66beff38e789aa033f0faf0c0985b0a"
+  version "2.14.1b"
+  sha256 "1bd685a09811bcebf785864e657c36770ba19a8505d4c3bc2eba12314981f510"
 
   # github.com/ridiculousfish/HexFiend/ was verified as official when first introduced to the cask
-  url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.major_minor}.dmg"
+  url "https://github.com/ridiculousfish/HexFiend/releases/download/v#{version}/Hex_Fiend_#{version.delete("b")}.dmg"
   appcast "https://github.com/ridiculousfish/HexFiend/releases.atom"
   name "Hex Fiend"
   homepage "https://ridiculousfish.com/hexfiend/"
