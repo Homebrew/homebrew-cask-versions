90ef5740c71a660251a76ce4335febfa9d073939
suschizu <brigitte@corecode.io>
2020-12-02 13:51:25 +0100

    Update mongodb-compass-beta from 1.24.0-beta.1 to 1.24.0-beta.2 (#10015)

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass-beta" do
-  version "1.24.0-beta.1"
-  sha256 "180db65442b8e65a282ed5bdedeeb44870551f880d27f87f9d74068262e969b8"
+  version "1.24.0-beta.2"
+  sha256 "1ea363948c2788fe899484187fa641cb98ece395bde316be18fcc5c78298e937"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
   appcast "https://www.mongodb.com/try/download/compass"
