dc74e30a8541e80f731ea7031d4a712ee9de6782
yottalogical <yottalogical@gmail.com>
2020-12-04 20:31:45 -0500

    Update daedalus-flight from 3.0.0-FC3 to 3.0.0-FC4

--- Casks/daedalus-flight.rb
+++ Casks/daedalus-flight.rb
@@ -1,9 +1,9 @@
 cask "daedalus-flight" do
-  version "3.0.0-FC3,15379"
-  sha256 "2eaa908b4db92cb9f4cc15d53e6a240e7a0b73fdf24a3cb5b0653b67efaead86"
+  version "3.0.0-FC4,15506"
+  sha256 "0b70494a9f6b6876af66ba11a53c883b56b297df967be8fa4eafcdcbc4f488fc"
 
-  # update-cardano-mainnet-flight.iohk.io/ was verified as official when first introduced to the cask
-  url "https://update-cardano-mainnet-flight.iohk.io/daedalus-#{version.before_comma}-mainnet_flight-#{version.after_comma}.pkg"
+  url "https://update-cardano-mainnet-flight.iohk.io/daedalus-#{version.before_comma}-mainnet_flight-#{version.after_comma}.pkg",
+      verified: "update-cardano-mainnet-flight.iohk.io/"
   appcast "https://update-cardano-mainnet-flight.iohk.io/daedalus-latest-version.json"
   name "Daedalus Flight"
   desc "Pre-release of Daedalus cryptocurrency wallet for ada on the Cardano blockchain"
