d19a4eebc172a3fee79b4dd7fcbf3b918de0f4f7
Guy Maliar <gmaliar@gmail.com>
2020-12-06 13:42:18 +0200

    fix wrong sha version

--- Casks/bitbar.rb
+++ Casks/bitbar.rb
@@ -1,6 +1,6 @@
 cask "bitbar" do
   version "2.0.0-beta10"
-  sha256 "29e535649b03fbb5bb43fac37bfa62a3074259ab31339e66e534ad938816ae01"
+  sha256 "69731c6686e0f2ccc2a3f19dd4e98cca7244e5a859d5326c627875a20220cea5"
 
   url "https://github.com/matryer/bitbar/releases/download/v#{version}/BitBar-v#{version}.zip"
   appcast "https://github.com/matryer/bitbar/releases.atom"
