0466810e94b380ec37888137d19f4c31283e1bed
Nicholas Ngai <ngai.nicholas@protonmail.com>
2020-12-07 15:10:42 -0800

    Add propresenter6 6.5.3_100991749

---
+++ Casks/propresenter6.rb
@@ -0,0 +1,29 @@
+cask "propresenter6" do
+  version "6.5.3_100991749"
+  sha256 "8a822ae5dbf1f4a17008cee66dcf98487b424a3ef01a381de9310eb9bd6ce530"
+
+  url "https://renewedvision.com/downloads/ProPresenter%20#{version.major}_#{version}.dmg"
+  appcast "https://www.renewedvision.com/update/ProPresenter#{version.major}.php"
+  name "ProPresenter 6"
+  homepage "https://www.renewedvision.com/propresenter.php"
+
+  depends_on macos: ">= :high_sierra"
+
+  app "ProPresenter #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Support/RenewedVision/ProPresenter6",
+    "~/Library/Caches/KSCrashReports/ProPresenter 6",
+    "~/Library/Caches/Sessions/ProPresenter 6",
+    "~/Library/Caches/com.renewedvision.ProPresenter6",
+    "~/Library/Preferences/com.renewedvision.ProPresenter6.plist",
+    "/Library/Application Support/RenewedVision",
+    "/Library/Caches/com.renewedvision.ProPresenter6",
+    "/Users/Shared/Renewed Vision Media",
+  ],
+      rmdir: [
+        "~/Library/Application Support/RenewedVision",
+        "~/Library/Caches/KSCrashReports",
+        "~/Library/Caches/Sessions",
+      ]
+end
