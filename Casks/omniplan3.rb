0131870b1afba5a31f583f6cdf7aa612f10ce3b7
oaoit <oaoist@gmail.com>
2020-12-09 06:02:24 +0800

    Create omniplan3.rb (#10043)
    * Create omniplan3.rb
    
    * Update omniplan3.rb
    
    * Update omniplan3.rb
    
    * Update omniplan3.rb
    
    * Update omniplan3.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

---
+++ Casks/omniplan3.rb
@@ -0,0 +1,28 @@
+cask "omniplan3" do
+  if MacOS.version <= :high_sierra
+    version "3.13"
+    sha256 "82e0d7db2626d751f93f97d80dc032e4bc01bba1e05ea52c553e4771c8cfeec5"
+    url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniPlan-#{version}.dmg"
+  else
+    version "3.14.4"
+    sha256 "34367f22766e5e420224e343d3cc2087c808f121f7c3f0fdfbd0e58f2d596e7e"
+    url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniPlan-#{version}.dmg"
+  end
+
+  name "OmniPlan"
+  desc "Project planning and management software"
+  homepage "https://www.omnigroup.com/omniplan/"
+
+  auto_updates true
+  conflicts_with cask: "omniplan"
+  depends_on macos: ">= :high_sierra"
+
+  app "OmniPlan.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.omnigroup.OmniPlan#{version.major}",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.omnigroup.omniplan#{version.major}.sfl*",
+    "~/Library/Containers/com.omnigroup.OmniPlan#{version.major}",
+    "~/Library/Preferences/com.omnigroup.OmniPlan#{version.major}.plist",
+  ]
+end
