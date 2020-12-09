d904b7aab33ea397159fee54ef2fc26aef4d1d30
bmichaelbyron <39526443+bmichaelbyron@users.noreply.github.com>
2020-12-09 13:00:35 -0500

    Add files via upload

---
+++ Casks/brave-browser-arm.rb
@@ -0,0 +1,23 @@
+cask "brave-browser-arm" do
+  version :latest
+  sha256 :no_check
+
+  # updates.bravesoftware.com/ was verified as official when first introduced to the cask
+  url do
+    require "open-uri"
+    appcast = "https://updates.bravesoftware.com/sparkle/Brave-Browser/nightly-arm64/appcast.xml"
+    URI(appcast).read[/enclosure url="([^"]+.dmg)"/, 1]
+  end
+
+  name "Brave Nightly"
+  desc "Web browser focusing on privacy"
+  homepage "https://brave.com/download-nightly/"
+
+  app "Brave Browser Nightly.app"
+
+  zap trash: [
+    "~/Library/Application Support/brave",
+    "~/Library/Preferences/com.electron.brave.plist",
+    "~/Library/Saved Application State/com.electron.brave.savedState",
+  ]
+end
