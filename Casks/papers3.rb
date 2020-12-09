01480591f6c73d87a10a239752a2c484a36888fb
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-09 08:31:47 +0800

    Update papers3.rb

--- Casks/papers3.rb
+++ Casks/papers3.rb
@@ -3,7 +3,7 @@ cask "papers3" do
   sha256 "3a67540bcc9c676301567bf8654ae745fb900cd5736a90715f504458de272282"
 
   url "https://downloads.mekentosj.com/papers_#{version.no_dots}.dmg",
-      verified: "downloads.mekentosj.com"
+      verified: "downloads.mekentosj.com/"
   name "Papers"
   desc "Your personal library of research"
   homepage "https://support.papersapp.com/support/solutions/articles/30000031865-existing-papers-3-users-accessing-papers-3-program-files-for-additional-device-installs"
