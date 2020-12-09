696f8d8e3463d7c8c17939d94bd541bca3ac3525
Gianni Tam-McMillan <25732197+gtmgianni@users.noreply.github.com>
2020-12-09 00:16:49 +0000

    Update verified parameter – group 6

--- Casks/wine-staging.rb
+++ Casks/wine-staging.rb
@@ -2,8 +2,8 @@ cask "wine-staging" do
   version "5.7"
   sha256 "04a52f91ea515dfdcbead6fc3efefccf4e53377d17b9e818d14b44f29f184f28"
 
-  # dl.winehq.org/wine-builds/macosx/ was verified as official when first introduced to the cask
-  url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-staging-#{version}.pkg"
+  url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-staging-#{version}.pkg",
+      verified: "dl.winehq.org/wine-builds/macosx/"
   appcast "https://dl.winehq.org/wine-builds/macosx/download.html"
   name "WineHQ-staging"
   homepage "https://www.wine-staging.com/"
