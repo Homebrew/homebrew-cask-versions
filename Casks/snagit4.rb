158b43463081b188ecdc7ec596bca2ceb1234337
Markus Reiter <me@reitermark.us>
2020-12-10 09:41:16 +0100

    snagit4: interpolate version

--- Casks/snagit4.rb
+++ Casks/snagit4.rb
@@ -2,7 +2,7 @@ cask "snagit4" do
   version "4.1.10"
   sha256 "9c3d4f3bd89e0dc809cd5d7dd32883594a37cf477a180ac225c4f4bf94072cd9"
 
-  url "https://download.techsmith.com/snagitmac/enu/4110/snagit.dmg"
+  url "https://download.techsmith.com/snagitmac/enu/#{version.no_dots}/snagit.dmg"
   name "Snagit"
   homepage "https://www.techsmith.com/snagit.html"
 
