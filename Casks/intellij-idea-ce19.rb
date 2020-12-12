dda418e466df32ae4ebbde1bf2194a47f68a1e76
sgsh <s.sugano@sgsh.com>
2020-12-12 18:23:34 +0900

    brew style --fix

--- Casks/intellij-idea-ce19.rb
+++ Casks/intellij-idea-ce19.rb
@@ -1,27 +1,32 @@
-cask 'intellij-idea-ce19' do
-  version '2019.3.4,193.6911.18'
-  sha256 'bf4e9ef25e8d09cc3b30ffeeb471ac0eae713c1b1cd30170ffaf55b0670ddd09'
+cask "intellij-idea-ce19" do
+  version "2019.3.4,193.6911.18"
+  sha256 "bf4e9ef25e8d09cc3b30ffeeb471ac0eae713c1b1cd30170ffaf55b0670ddd09"
 
   url "https://download.jetbrains.com/idea/ideaIC-#{version.before_comma}.dmg"
-  appcast 'https://data.services.jetbrains.com/products/releases?code=IIC&latest=false&type=release'
-  name 'IntelliJ IDEA Community Edition'
-  name 'IntelliJ IDEA CE'
-  homepage 'https://www.jetbrains.com/idea/'
+  appcast "https://data.services.jetbrains.com/products/releases?code=IIC&latest=false&type=release"
+  name "IntelliJ IDEA Community Edition"
+  name "IntelliJ IDEA CE"
+  homepage "https://www.jetbrains.com/idea/"
 
   auto_updates true
 
-  app 'IntelliJ IDEA CE.app'
+  app "IntelliJ IDEA CE.app"
 
   uninstall_postflight do
-    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'idea') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
+      if File.exist?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
   end
 
   zap trash: [
-               "~/Library/Application Support/IdeaIC#{version.major_minor}",
-               "~/Library/Caches/IdeaIC#{version.major_minor}",
-               "~/Library/Logs/IdeaIC#{version.major_minor}",
-               "~/Library/Preferences/IdeaIC#{version.major_minor}",
-               '~/Library/Preferences/com.jetbrains.intellij.ce.plist',
-               '~/Library/Saved Application State/com.jetbrains.intellij.ce.savedState',
-             ]
+    "~/Library/Application Support/IdeaIC#{version.major_minor}",
+    "~/Library/Caches/IdeaIC#{version.major_minor}",
+    "~/Library/Logs/IdeaIC#{version.major_minor}",
+    "~/Library/Preferences/IdeaIC#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.intellij.ce.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.ce.savedState",
+  ]
 end
