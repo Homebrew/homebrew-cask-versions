039fa7cda0ff6c207f9f9d53010cbb0dffecd29a
sgsh <s.sugano@sgsh.com>
2020-12-12 17:39:48 +0900

    Copy script from https://raw.githubusercontent.com/Homebrew/homebrew-cask/64b21e2066c92c5b8c4c6259dbe88e897bd3396d/Casks/intellij-idea.rb and https://raw.githubusercontent.com/Homebrew/homebrew-cask/b877c47f51c391dbedef927683b54993c4dbbc40/Casks/intellij-idea-ce.rb

---
+++ Casks/intellij-idea19.rb
@@ -0,0 +1,26 @@
+cask 'intellij-idea' do
+  version '2019.3.4'
+  sha256 '754793f12973c1e78688a4865a43928dcbd3a10c6b2c40522f8337bb10153023'
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
+  appcast 'https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release'
+  name 'IntelliJ IDEA Ultimate'
+  homepage 'https://www.jetbrains.com/idea/'
+
+  auto_updates true
+
+  app 'IntelliJ IDEA.app'
+
+  uninstall_postflight do
+    ENV['PATH'].split(File::PATH_SEPARATOR).map { |path| File.join(path, 'idea') }.each { |path| File.delete(path) if File.exist?(path) && File.readlines(path).grep(%r{# see com.intellij.idea.SocketLock for the server side of this interface}).any? }
+  end
+
+  zap trash: [
+               '~/Library/Preferences/com.jetbrains.intellij.plist',
+               "~/Library/Caches/IntelliJIdea#{version.major_minor}",
+               "~/Library/Logs/IntelliJIdea#{version.major_minor}",
+               "~/Library/Application Support/IntelliJIdea#{version.major_minor}",
+               "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+               '~/Library/Saved Application State/com.jetbrains.intellij.savedState',
+             ]
+end
