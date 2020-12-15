93e2b4d0d3df5f385faa71fd215b8304190c13c2
Egor Kotov <e-kotov@users.noreply.github.com>
2020-12-15 03:09:36 +0300

    Add QGIS.app LTR version (#10084)

---
+++ Casks/qgis-ltr.rb
@@ -0,0 +1,13 @@
+cask "qgis-ltr" do
+  version "3.10.12"
+  sha256 :no_check
+
+  url "https://qgis.org/downloads/macos/qgis-macos-ltr.dmg"
+  appcast "https://qgis.org/downloads/macos/qgis-macos-ltr.sha256sum",
+          must_contain: version.dots_to_underscores
+  name "QGIS LTR"
+  desc "Geographic Information System"
+  homepage "https://www.qgis.org/"
+
+  app "QGIS#{version.major_minor}.app"
+end
