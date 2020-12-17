df164c3d5aaec709fc040538b6497bd6381b5114
Randall <ran.dall@icloud.com>
2020-12-17 09:55:11 -0800

    Update brave-browser-dev from 87.1.19.61,119.61 to 87.1.19.63,119.63

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -1,6 +1,6 @@
 cask "brave-browser-dev" do
-  version "87.1.19.61,119.61"
-  sha256 "908a514ff138c649fe25b9a21a7f2a116dcb5ae2a3714fce4c131d586854b4aa"
+  version "87.1.19.63,119.63"
+  sha256 "bd791b53559d48154fec4cab17415064d32437353a890d3c430aa0ba888722ac"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev/#{version.after_comma}/Brave-Browser-Dev-x64.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
