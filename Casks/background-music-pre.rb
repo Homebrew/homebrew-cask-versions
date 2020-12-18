9b3a974ef135da93549aa8ffee14272695525d34
Miccal Matthews <miccal.matthews@gmail.com>
2020-12-18 14:30:06 +0800

    Update background-music-pre.rb

--- Casks/background-music-pre.rb
+++ Casks/background-music-pre.rb
@@ -12,8 +12,11 @@ cask "background-music-pre" do
 
   pkg "BackgroundMusic-#{version}.pkg"
 
-  uninstall launchctl: "com.bearisdriving.BGM.XPCHelper",
+  uninstall script:     {
+    executable: "/Applications/Background Music.app/Contents/Resources/_uninstall-non-interactive.sh",
+    sudo:       true,
+  },
+            launchctl: "com.bearisdriving.BGM.XPCHelper",
             pkgutil:   "com.bearisdriving.BGM",
-            quit:      "com.bearisdriving.BGM.App",
-            script:    "/Applications/Background Music.app/Contents/Resources/_uninstall-non-interactive.sh"
+            quit:      "com.bearisdriving.BGM.App"
 end
