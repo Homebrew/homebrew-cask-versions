95c2a989d15a952bc23d4b414ac4d11875282016
César Román <thecesrom@gmail.com>
2020-12-17 22:49:20 -0800

    Add Azure Data Studio - Insiders.app v1.26.0 (#10104)
    * Add Azure Data Studio - Insiders.app v1.26.0
    
    * Update azure-data-studio-insiders.rb
    
    * Update azure-data-studio-insiders.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/azure-data-studio-insiders.rb
@@ -0,0 +1,28 @@
+cask "azure-data-studio-insiders" do
+  version "1.26.0,4575f2bbe6648f98f4011375df10d7aff2b3e366"
+  sha256 "4ed75cc8487e9c68ba78bb3b5426a6bbcddb407e32f1b767007b26ecfdd679a4"
+
+  url "https://sqlopsbuilds.azureedge.net/insider/#{version.after_comma}/azuredatastudio-macos-#{version.before_comma}-insider.zip",
+      verified: "sqlopsbuilds.azureedge.net/insider/"
+  appcast "https://azuredatastudio-update.azurewebsites.net/api/update/darwin/insider/VERSION"
+  name "Azure Data Studio - Insiders"
+  desc "Data management tool that enables working with SQL Server"
+  homepage "https://docs.microsoft.com/en-us/sql/azure-data-studio/"
+
+  auto_updates true
+
+  app "Azure Data Studio - Insiders.app"
+  binary "#{appdir}/Azure Data Studio - Insiders.app/Contents/Resources/app/bin/code",
+         target: "azuredatastudio-insiders"
+
+  zap trash: [
+    "~/.azuredatastudio-insiders",
+    "~/Library/Application Support/Azure Data Studio",
+    "~/Library/Application Support/azuredatastudio",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.azuredatastudio.oss.insiders.sfl2",
+    "~/Library/Caches/com.azuredatastudio.oss.insiders",
+    "~/Library/Caches/com.azuredatastudio.oss.insiders.ShipIt",
+    "~/Library/Preferences/com.azuredatastudio.oss.insiders.plist",
+    "~/Library/Saved Application State/com.azuredatastudio.oss.insiders.savedState",
+  ]
+end
