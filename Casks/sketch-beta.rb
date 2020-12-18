7eb13a31816eb40a615b106fe46ac4208995371b
Mark Malström <mark@malstrom.me>
2020-12-18 00:27:07 -0600

    Add Sketch Beta.app v71.0-112664 (#10097)
    * Add Sketch Beta.app v71.0-112664
    
    * Update sketch-beta.rb
    
    * Update sketch-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/sketch-beta.rb
@@ -0,0 +1,27 @@
+cask "sketch-beta" do
+  version "71.0-112664"
+  sha256 :no_check
+
+  url "https://beta-download.sketch.com/sketch.zip"
+  name "Sketch"
+  desc "Digital design and prototyping platform"
+  homepage "https://www.sketch.com/beta"
+
+  auto_updates true
+  depends_on macos: ">= :mojave"
+
+  app "Sketch Beta.app"
+
+  uninstall quit: "com.bohemiancoding.sketch3.beta"
+
+  zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.bohemiancoding.sketch3.beta.sfl*",
+    "~/Library/Application Support/com.bohemiancoding.sketch3.beta",
+    "~/Library/Caches/com.bohemiancoding.sketch3.beta",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.bohemiancoding.sketch3.beta",
+    "~/Library/Cookies/com.bohemiancoding.sketch3.beta.binarycookies",
+    "~/Library/Logs/com.bohemiancoding.sketch3.beta",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.beta.LSSharedFileList.plist",
+    "~/Library/Preferences/com.bohemiancoding.sketch3.beta.plist",
+  ]
+end
