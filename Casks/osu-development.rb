ffdfa45047dffbe4136d39aff9b1d583d6346d77
Andrew Zhou <0az@afzhou.com>
2020-12-26 14:19:07 -0600

    osu-development: Update to 2020.1225.0 (#10143)

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,10 +1,11 @@
 cask "osu-development" do
-  version "2020.1212.0"
-  sha256 "c0ad8286d49dcd18e12925e0888e87a9721c304b5ca8c4b222b1d52480734d9c"
+  version "2020.1225.0"
+  sha256 "0c11e8ee2d9091efe9b92807cd68749c1e96bf76021816ddc12a5ba18c2bd2f2"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   appcast "https://github.com/ppy/osu/releases.atom"
   name "osu!lazer"
+  desc "Rhythm game"
   homepage "https://github.com/ppy/osu/"
 
   depends_on macos: ">= :sierra"
