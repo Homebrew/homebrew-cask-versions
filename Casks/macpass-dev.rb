2e21c6f98ef429689f90ecf8600387819df260d0
Henry Chu <henrytech@outlook.com>
2020-12-27 04:27:39 +0800

    Add MacPass.app development version (#10140)

---
+++ Casks/macpass-dev.rb
@@ -0,0 +1,23 @@
+cask "macpass-dev" do
+  version :latest
+  sha256 :no_check
+
+  url "https://github.com/MacPass/MacPass/releases/download/continuous/MacPass-continuous.zip",
+      verified: "github.com/MacPass/MacPass/"
+  name "MacPass"
+  desc "KeePass-client and password manager"
+  homepage "https://macpass.github.io/"
+
+  conflicts_with cask: "macpass"
+  depends_on macos: ">= :yosemite"
+
+  app "MacPass.app"
+
+  zap delete: [
+    "~/Library/Application Support/MacPass",
+    "~/Library/Caches/com.hicknhacksoftware.MacPass",
+    "~/Library/Cookies/com.hicknhacksoftware.MacPass.binarycookies",
+    "~/Library/Preferences/com.hicknhacksoftware.MacPass.plist",
+    "~/Library/Saved Application State/com.hicknhacksoftware.MacPass.savedState",
+  ]
+end
