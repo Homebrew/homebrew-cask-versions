1c47483ce0335b3f9427cd33c4a1025beb6cc75c
César Román <thecesrom@gmail.com>
2020-12-31 11:38:09 -0800

    Update azure-data-studio-insiders from 1.26.0,1d09dbf5912b3cd67e51227b55fd0856d60a33a2 to 1.26.0,30054e632c75f8df0ea351f2c51b8ec7d5fa5be5 (#10157)

--- Casks/azure-data-studio-insiders.rb
+++ Casks/azure-data-studio-insiders.rb
@@ -1,6 +1,6 @@
 cask "azure-data-studio-insiders" do
-  version "1.26.0,1d09dbf5912b3cd67e51227b55fd0856d60a33a2"
-  sha256 "bd80d0ede29ee0dea1bedcb354a5a1e10ad61a7abf16d012d579ad2144b2adb1"
+  version "1.26.0,30054e632c75f8df0ea351f2c51b8ec7d5fa5be5"
+  sha256 "79283632d92758c2737622b4d2d8b6d9ffc52eab4d5b8f32c775bea6e72800fc"
 
   url "https://sqlopsbuilds.azureedge.net/insider/#{version.after_comma}/azuredatastudio-macos-#{version.before_comma}-insider.zip",
       verified: "sqlopsbuilds.azureedge.net/insider/"
