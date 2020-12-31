eccc1990826962c5f2f51cc64b245c1b3a5ab931
Amanda Santana <amandasantanati@gmail.com>
2020-12-31 11:48:47 -0300

    Update corretto version to 8.275.01.1
    Update amazon-corretto version from 8.272.10.3 to 8.275.01.1

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -1,6 +1,6 @@
 cask "corretto8" do
-  version "8.272.10.3"
-  sha256 "307d27c21f155021facd9c4791cf9df55f157e296ea10a6609a390495d606ba7"
+  version "8.275.01.1"
+  sha256 "d136db770d98725910535b95ca934157e2af6f9f2be0a13ec7834a6aa25028af"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-8-x64-macos-jdk.pkg"
