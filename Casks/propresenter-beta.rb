950da7458a2a52ae208826d5699d05f5abc7616c
Bevan Kay <bevanjkay@gmail.com>
2021-01-07 12:41:17 +1100

    Add propresenter-beta.rb

---
+++ Casks/propresenter-beta.rb
@@ -0,0 +1,31 @@
+cask "propresenter-beta" do
+  version "7.4.1,117702913"
+  sha256 "13c9bc59786d92dc085c7ee23c217745673f0df7861e7bdb420f137fa667be7b"
+
+  url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.before_comma}_#{version.after_comma}.zip"
+  appcast "https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0&channel=beta"
+  name "ProPresenter"
+  desc "Presentation and production application for live events"
+  homepage "https://www.renewedvision.com/propresenter.php"
+
+  conflicts_with cask: "homebrew/cask/propresenter"
+  depends_on macos: ">= :mojave"
+
+  app "ProPresenter.app"
+
+  zap trash: [
+    "~/Library/Application Support/RenewedVision/ProPresenter#{version.major}",
+    "~/Library/Caches/KSCrashReports/ProPresenter #{version.major}",
+    "~/Library/Caches/Sessions/ProPresenter #{version.major}",
+    "~/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
+    "~/Library/Preferences/com.renewedvision.ProPresenter#{version.major}.plist",
+    "/Library/Application Support/RenewedVision",
+    "/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
+    "/Users/Shared/Renewed Vision Media",
+  ],
+      rmdir: [
+        "~/Library/Application Support/RenewedVision",
+        "~/Library/Caches/KSCrashReports",
+        "~/Library/Caches/Sessions",
+      ]
+end
