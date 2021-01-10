737750cc1a83e5ea19d8ef4db92a7807b43ab248
Lukas Burk <lukas@quantenbrot.de>
2021-01-10 21:35:43 +0100

    Update rstudio-preview from 1.4.1103 to 1.4.1103

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask "rstudio-preview" do
-  version "1.4.1099"
-  sha256 "1ca8b9299ec2b7c420edbe09b001f3d29aa1571c87efbfa136c7f198d38bcc71"
+  version "1.4.1103"
+  sha256 :no_check
 
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg",
       verified: "s3.amazonaws.com/rstudio-ide-build/"
