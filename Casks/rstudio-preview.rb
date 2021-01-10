d9d7031636fbbb3e45aab24667ae331f0fcd7b09
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-11 05:13:37 +0800

    Update rstudio-preview.rb

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,6 +1,6 @@
 cask "rstudio-preview" do
   version "1.4.1103"
-  sha256 :no_check
+  sha256 "20148bd6ee7ed80d85ae8b309ebaa5d68df282d610056faeedf4acb8716aaaa2"
 
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg",
       verified: "s3.amazonaws.com/rstudio-ide-build/"
