8fd38129365fc114a8d18b792bab3e143d344e98
Kai Liu <15362012+erinacio@users.noreply.github.com>
2021-01-12 03:10:18 +0800

    Add arm64 specific version / checksum / url for zulu11 (#10190)

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -1,9 +1,18 @@
 cask "zulu11" do
-  version "11.0.9.1,11.43.55-ca"
-  sha256 "18ba26ead6b15b4e8b6f9301053f962f2a7da3fb6f1d49a81957235979783cda"
+  if Hardware::CPU.intel?
+    version "11.0.9.1,11.43.55-ca"
+    sha256 "18ba26ead6b15b4e8b6f9301053f962f2a7da3fb6f1d49a81957235979783cda"
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
-      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  else
+    version "11.0.9.1,11.43.1021-ca"
+    sha256 "260a9d1bd3acda5b82bd3d61096fb3eec9985e5c37817ee2d3bb21f1134f0b36"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  end
+
   name "Azul Zulu Java Standard Edition Development Kit"
   homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
 
