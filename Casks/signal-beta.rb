e094df91350e61d9b6ea7d5ce90cceb6947a038f
LanikSJ <LanikSJ@users.noreply.github.com>
2021-01-12 20:35:34 -0800

    Update signal-beta from 1.39.4-beta.1 to 1.39.5-beta.1 (#10211)

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,6 +1,6 @@
 cask "signal-beta" do
-  version "1.39.4-beta.1"
-  sha256 "cb7fb5128642e8e762128ff4de7abeb1058e50335009c45fb0251c6a138c817e"
+  version "1.39.5-beta.1"
+  sha256 "83422b516c81def69f56493556d5c281cef1568f8a47aa992d3e7df4901e15f1"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{version}.zip"
   appcast "https://github.com/signalapp/Signal-Desktop/releases.atom"
