a2aebdbb34a08adb69e7d93f1afd44d04b5fe1b1
César Román <thecesrom@gmail.com>
2021-01-16 12:53:21 -0800

    Update visual-studio-code-insiders

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,13 +1,13 @@
 cask "visual-studio-code-insiders" do
+  version "1.53.0,1a6eef3170be2a11751ee50d450328d458f7bfac"
+
   if Hardware::CPU.intel?
-    version "1.53.0,1a6eef3170be2a11751ee50d450328d458f7bfac"
     sha256 "a1b2ddc719b1201e63c63268e13a9528e56779f63670120b393e73710022f56a"
 
     url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin.zip",
         verified: "az764295.vo.msecnd.net/insider/"
     appcast "https://update.code.visualstudio.com/api/update/darwin/insider/VERSION"
   else
-    version "1.53.0,1a6eef3170be2a11751ee50d450328d458f7bfac"
     sha256 "5d76d152294a0306aa4e39c8f242e67dd6e69b570319e369d633963094bde980"
 
     url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-arm64.zip",
