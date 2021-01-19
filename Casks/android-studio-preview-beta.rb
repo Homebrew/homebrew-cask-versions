9752d094afb7e629a72c0653f19ac796389ccbd9
Miccal Matthews <miccal.matthews@gmail.com>
2021-01-19 09:27:51 +0800

    Update android-studio-preview-beta.rb

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -5,6 +5,7 @@ cask "android-studio-preview-beta" do
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip",
       verified: "dl.google.com/dl/android/studio/"
   name "Android Studio Preview (Beta)"
+  desc "Tools for building Android applications"
   homepage "https://developer.android.com/studio/preview/"
 
   conflicts_with cask: "android-studio-preview-canary"
