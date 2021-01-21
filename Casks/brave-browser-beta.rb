b31b5f768e4e1d8c00f19931309ceada146acfab
Hannes Braun <hannesbraun@mail.de>
2021-01-21 11:55:49 +0100

    Update brave-browser-beta from 88.1.20.83,120.83 to 88.1.20.85,120.85

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,6 +1,6 @@
 cask "brave-browser-beta" do
-  version "88.1.20.83,120.83"
-  sha256 "36f7ffe296b090cd8e90c5cefbb7058cc02bcd7cc9db352de15fe9502685b525"
+  version "88.1.20.85,120.85"
+  sha256 "2ba6a49f460fbbd2e14f56da79eb9c14d1b93e11ec9ebf5269fe619078e327f9"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta-x64.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
