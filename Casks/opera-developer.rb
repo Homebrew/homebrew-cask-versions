ff46197314b422aa9725f9a58e1c74ae95605506
Hannes Braun <hannesbraun@mail.de>
2021-01-25 12:49:39 +0100

    Update opera-developer from 75.0.3939.0 to 75.0.3946.0 (#10263)

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "75.0.3939.0"
-  sha256 "2a24b74760843802c2c5aeef11ee7c30b5517b33e935c7f7235df5561ac29387"
+  version "75.0.3946.0"
+  sha256 "0e627f6a0aa3819105bfb165bf25e898b8071d06b0c201cd806657c72f1d4f80"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
