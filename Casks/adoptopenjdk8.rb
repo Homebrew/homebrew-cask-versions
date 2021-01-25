cb319809dce4fe89291fe69a5b25f628c40aa4b9
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2021-01-25 21:48:20 +0000

    Update adoptopenjdk8 from 8u275b01 to 8u282b08 (#10262)
    * Update adoptopenjdk8 from 8u275b01 to 8u282b08
    
    * Add description
    
    * Fix ordering of homepage and description stanzas
    
    * Update adoptopenjdk8.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -1,11 +1,12 @@
 cask "adoptopenjdk8" do
-  version "8,275:b01"
-  sha256 "af005a2419b3dbc664fa3f73f2f43e9258da482110e73ff187566484102815f9"
+  version "8,282:b08"
+  sha256 "f12d380ceae806d02c4cae23bdc601402c543692c763122286b99d8ef6059794"
 
   url "https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg",
       verified: "github.com/AdoptOpenJDK/openjdk8-binaries/"
   appcast "https://github.com/adoptopenjdk/openjdk#{version.before_comma}-binaries/releases.atom"
   name "AdoptOpenJDK 8"
+  desc "Prebuilt OpenJDK binaries"
   homepage "https://adoptopenjdk.net/"
 
   pkg "OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
