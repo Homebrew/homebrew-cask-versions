2916799e1f95858b11e0b1f385ab0cd6c4a4854d
Kai Liu <einbert-xeride@outlook.com>
2021-01-26 18:54:34 +0800

    Update zulu8 arm64 from 8.0.275,8.50.0.51-ca to 8.0.282,8.52.0.23-ca

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -1,15 +1,15 @@
 cask "zulu8" do
+  version "8.0.282,8.52.0.23-ca"
+
   if Hardware::CPU.intel?
-    version "8.0.282,8.52.0.23-ca"
     sha256 "4eaf47acd097ad3102f55ddc586f4e1fbb3b8b8115d5cb2c455a54b0d84ee1bc"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
-    version "8.0.275,8.50.0.1017-ca"
-    sha256 "80f1e48d017896e05b5722a3de19f799e5cb9854ec5a1883d0e7a9a9ed4e7b23"
+    sha256 "c6d1795d535cb448de567faf1449610fce69e5fe2e8a443a3f96f4036319f809"
 
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macos_aarch64.dmg",
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   end
 
