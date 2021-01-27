28fec81c29fc43abf22e91e4b9654ca31ee67711
Kai Liu <15362012+erinacio@users.noreply.github.com>
2021-01-27 10:43:24 +0800

    Update zulu13 arm64 from 13.0.5.1,13.35.1017-ca to 13.0.6,13.37.21-ca (#10275)

--- Casks/zulu13.rb
+++ Casks/zulu13.rb
@@ -1,15 +1,15 @@
 cask "zulu13" do
+  version "13.0.6,13.37.21-ca"
+
   if Hardware::CPU.intel?
-    version "13.0.6,13.37.21-ca"
     sha256 "c9de14e85b882fdcb65deef263ea43cfb8c3eb603b7f5a7c51214c7e4e16674b"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
-    version "13.0.5.1,13.35.1017-ca"
-    sha256 "c930f8475daa9878e627521d21dd7eea541759affde1763bbbb13017c52f4115"
+    sha256 "5831abf3669642fdb2f841f4034fe7f2cde7e7c85a04e93a44e49c17e5f5dc7b"
 
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macos_aarch64.dmg",
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   end
 
