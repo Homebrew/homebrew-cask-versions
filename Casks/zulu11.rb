9af2a41278dea25aae00d79cd5c009e15aadffcd
Kai Liu <15362012+erinacio@users.noreply.github.com>
2021-01-27 10:42:59 +0800

    Update zulu11 arm64 from 11.0.9.1,11.43.1021-ca to 11.0.10,11.45.27-ca (#10274)

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -1,13 +1,13 @@
 cask "zulu11" do
+  version "11.0.10,11.45.27-ca"
+
   if Hardware::CPU.intel?
-    version "11.0.10,11.45.27-ca"
     sha256 "1c59885634ad438c9b198d31b9dd2aa310922484b3a0ffec603eec1b02eb898e"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
-    version "11.0.9.1,11.43.1021-ca"
-    sha256 "260a9d1bd3acda5b82bd3d61096fb3eec9985e5c37817ee2d3bb21f1134f0b36"
+    sha256 "dd7f76d637fcf692081ae29af8f694d95e82cf30a9aa9ddea26d8e062c65e4f5"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
