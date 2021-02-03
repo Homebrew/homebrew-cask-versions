60933b03ff17759d0caa6450ae2cc504669b46be
Bevan Kay <bevanjkay@gmail.com>
2021-02-04 09:17:38 +1100

    Update propresenter-beta from 7.4.1,117702926 to 7.4.1,117702927 (#10324)

--- Casks/propresenter-beta.rb
+++ Casks/propresenter-beta.rb
@@ -1,6 +1,6 @@
 cask "propresenter-beta" do
-  version "7.4.1,117702926"
-  sha256 "8c3ff0385d9c0ad78597fb20c116649808c96b7947d2bd36d59baf52866657b9"
+  version "7.4.1,117702927"
+  sha256 "7ff99359355f08fd497a27bddc87ab9d0548f7ae757f5bc767498167d9f6e8bf"
 
   url "https://renewedvision.com/downloads/propresenter/mac/ProPresenter_#{version.before_comma}_#{version.after_comma}.zip"
   appcast "https://api.renewedvision.com/v1/pro/upgrade?platform=macos&osVersion=0&appVersion=0&buildNumber=0&includeNotes=0&channel=beta"
