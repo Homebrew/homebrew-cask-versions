366d539e70fb39e287b959838d2f4dc025795772
Hannes Braun <hannesbraun@mail.de>
2021-02-08 16:55:03 +0100

    Update appcode-eap from 2020.3,203.5981.30 to 2021.1,211.5538.11

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,6 +1,6 @@
 cask "appcode-eap" do
-  version "2020.3,203.5981.30"
-  sha256 "be46c3e4e5b7907d2dec2bf2c530fe81838207438d25936ca18ea43cad549ec4"
+  version "2021.1,211.5538.11"
+  sha256 "af1cb2696cb07af8cf191dfeddfb71e1bcb713cb0ce9f2b66967d53737f52ab8"
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.after_comma}.dmg"
   name "AppCode EAP"
