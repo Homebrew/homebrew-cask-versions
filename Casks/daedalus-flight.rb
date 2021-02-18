312fd3c3015a9148f3fdd3d2cb53f3058c7334ca
miccal <miccal.matthews@gmail.com>
2021-02-18 09:36:46 +0800

    Update daedalus-flight from 3.3.0-FC1,16463 to 3.3.0,16556

--- Casks/daedalus-flight.rb
+++ Casks/daedalus-flight.rb
@@ -1,6 +1,6 @@
 cask "daedalus-flight" do
-  version "3.3.0-FC1,16463"
-  sha256 "866c33d8dc922eb80d94c9c88f81f84af50fecaba9e2cc329b11e333b370f50a"
+  version "3.3.0,16556"
+  sha256 "c4f34ca63f1e7f113b3d35e263f0cd4b1e3173f62830036f70a1ff3be98d566e"
 
   url "https://update-cardano-mainnet-flight.iohk.io/daedalus-#{version.before_comma}-mainnet_flight-#{version.after_comma}.pkg",
       verified: "update-cardano-mainnet-flight.iohk.io/"
