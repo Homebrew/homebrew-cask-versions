9444adc9e746a610a07fd9a2a04a9f6924dd33c5
Andy Fragen <andy@thefragens.com>
2021-02-18 19:50:01 -0800

    add local-beta.rb for Local Beta app

---
+++ Casks/local-beta.rb
@@ -0,0 +1,26 @@
+cask "local-beta" do
+  version "5.9.9,5214"
+  sha256 "f0ae079679bfa14f56fd8fe37357ac1b1a051041"
+
+  url "https://cdn.localwp.com/releases-beta/#{version.before_comma}+local-beta-#{version.after_comma}/local-beta-#{version.before_comma}-b#{version.after_comma}-mac.dmg"
+  name "Local Beta"
+  desc "WordPress local development tool (beta)"
+  homepage "https://localwp.com/community/c/local-beta/17/"
+
+  livecheck do
+    url "https://cdn.localwp.com/beta/latest/mac"
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{/(\d+(?:\.\d+)*)\+(\d+)/})
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  app "Local Beta.app"
+
+  zap trash: [
+    "~/Library/Application Support/Local Beta",
+    "~/Library/Logs/local-beta.log",
+    "~/Library/Preferences/com.getflywheel.lightning.local-beta.plist",
+    "~/Library/Saved Application State/com.getflywheel.lightning.local-beta.savedState",
+  ]
+end
