83ad0041fbab7e99a2fbeb30bd6901381836ed30
Andy Fragen <andy@thefragens.com>
2021-02-19 17:16:56 -0800

    update sha256

--- Casks/local-beta.rb
+++ Casks/local-beta.rb
@@ -1,6 +1,6 @@
 cask "local-beta" do
   version "5.9.9,5214"
-  sha256 "f0ae079679bfa14f56fd8fe37357ac1b1a051041"
+  sha256 "0e33ddee5a81f750a93f012f90d71ad559ce73a7072ad8a0acd25660192d0738"
 
   url "https://cdn.localwp.com/releases-beta/#{version.before_comma}+local-beta-#{version.after_comma}/local-beta-#{version.before_comma}-b#{version.after_comma}-mac.dmg"
   name "Local Beta"
