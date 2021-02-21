00ceaf3e41e464b2ad7ca71d1257a3cf9a69c1a3
Hannes Braun <hannesbraun@mail.de>
2021-02-21 20:37:28 +0100

    Update brave-browser-dev from 88.1.21.60,121.60 to 88.1.21.63,121.63

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -1,6 +1,6 @@
 cask "brave-browser-dev" do
-  version "88.1.21.60,121.60"
-  sha256 "57c0cd2f78d489c479ded2194075c7dc4f3aa76deccb3112586d6cf602901147"
+  version "88.1.21.63,121.63"
+  sha256 "e0d94ef457918b8669eab874e6bbb8b20cb6e06564a54d3ee9161dbe83b7bd44"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev/#{version.after_comma}/Brave-Browser-Dev-x64.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
