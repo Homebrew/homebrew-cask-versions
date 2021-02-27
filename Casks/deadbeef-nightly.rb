c540904487f528754fd038aebdedd08d9445f06d
Yurii Kolesnykov <root@yurikoles.com>
2021-02-27 14:44:35 +0200

    Return deadbeef-devel as deadbeef-nightly
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Casks/deadbeef-nightly.rb
@@ -0,0 +1,20 @@
+cask "deadbeef-nightly" do
+  version :latest
+  sha256 :no_check
+
+  url "https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-osx-x86_64.zip",
+      verified: "downloads.sourceforge.net/deadbeef/"
+  name "DeaDBeeF"
+  desc "Modular audio player"
+  homepage "https://deadbeef.sourceforge.io/"
+
+  conflicts_with cask: "deadbeef"
+
+  app "DeaDBeeF.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.deadbeef.deadbeef.plist",
+    "~/Library/Preferences/deadbeef",
+    "~/Library/Saved Application State/com.deadbeef.deadbeef.savedState",
+  ]
+end
