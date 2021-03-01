c81c9a80c29939c1d39453a77ad21d3cfc0ea199
My2ndAngelic <8387105+My2ndAngelic@users.noreply.github.com>
2021-03-01 13:26:13 +0100

    Update osu-development from 2021.109.0 to 2021.226.0

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,6 +1,6 @@
 cask "osu-development" do
-  version "2021.109.0"
-  sha256 "83da8dc8f43e0d788822629b839cf86ed599a8e8b831c56151034b29c472bc5c"
+  version "2021.226.0"
+  sha256 "093cff4ca82641d94ae9185c6b652c7dd43e06a3be5243608f53fb19c7a1bfcb"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   appcast "https://github.com/ppy/osu/releases.atom"
