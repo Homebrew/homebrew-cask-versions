95735d566278429559d2007654f0e0a46a05f811
Vanilla <neko@hydev.org>
2021-03-01 08:37:56 +0800

    Add Olympus.app build 1891 (#10437)
    * Add Olympus.app build 1891
    
    * Update olympus.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/olympus.rb
@@ -0,0 +1,19 @@
+cask "olympus" do
+  version "1891"
+  sha256 "4d0ccdc6d0af628c05b8e0266f61a25f8a45c519c5d8cf1d0ef73a1498e63aba"
+
+  url "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/#{version}/artifacts?artifactName=macos.main&$format=zip",
+      verified: "dev.azure.com/EverestAPI/Olympus"
+  name "Olympus"
+  desc "Everest (Mod loader for video games Celeste) installer / manager"
+  homepage "https://everestapi.github.io/"
+
+  container nested: "macos.main/dist.zip"
+
+  app "Olympus.app"
+
+  zap trash: [
+    "~/Library/Application Support/Olympus",
+    "~/Library/Saved Application State/everest.olympus.savedState",
+  ]
+end
