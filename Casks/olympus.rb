fce6394673f7333d928c4885f2b2a935d68c7d5c
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-01 08:30:19 +0800

    Update olympus.rb

--- Casks/olympus.rb
+++ Casks/olympus.rb
@@ -2,7 +2,6 @@ cask "olympus" do
   version "1891"
   sha256 "4d0ccdc6d0af628c05b8e0266f61a25f8a45c519c5d8cf1d0ef73a1498e63aba"
 
-  # Source code in GitHub, build & download in azure.
   url "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/#{version}/artifacts?artifactName=macos.main&$format=zip",
       verified: "dev.azure.com/EverestAPI/Olympus"
   name "Olympus"
