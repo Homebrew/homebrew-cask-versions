a0ca2f761c7a832f00b1ce98dcb6dcb1b415add9
Hannes Braun <hannesbraun@mail.de>
2021-03-03 20:01:17 +0100

    Update scribus-dev from 1.5.5 to 1.5.6.1 (#10478)

--- Casks/scribus-dev.rb
+++ Casks/scribus-dev.rb
@@ -1,6 +1,6 @@
 cask "scribus-dev" do
-  version "1.5.5"
-  sha256 "42426b1bf21a1eafc5e5c442e81ca77cec65b83751c8fdcd4f9b258c47063f3b"
+  version "1.5.6.1"
+  sha256 "5a79350884a405e223789bc65038d7c40f41c97bf352e8da0d96a9444a0bddd5"
 
   url "https://downloads.sourceforge.net/scribus/scribus-devel/#{version}/scribus-#{version}.dmg",
       verified: "sourceforge.net/scribus/"
