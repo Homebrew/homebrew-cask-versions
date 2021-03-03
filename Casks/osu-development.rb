603c3e007c8636703eb3ac6b2b844368b5d6a474
Hannes Braun <hannesbraun@mail.de>
2021-03-03 19:41:35 +0100

    Update osu-development from 2021.226.0 to 2021.302.0

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,6 +1,6 @@
 cask "osu-development" do
-  version "2021.226.0"
-  sha256 "093cff4ca82641d94ae9185c6b652c7dd43e06a3be5243608f53fb19c7a1bfcb"
+  version "2021.302.0"
+  sha256 "703c42d1f7fd3db180415d0ba9130437c80416420d272be484f94970ce1d058b"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   appcast "https://github.com/ppy/osu/releases.atom"
