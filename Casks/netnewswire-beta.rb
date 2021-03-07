252c9c459ab5fea6decbe67537a5cd5f47ed758b
Stephen Wade <stephen@stephenwade.me>
2021-03-07 16:11:19 -0500

    Add NetNewsWire version 6 beta

---
+++ Casks/netnewswire-beta.rb
@@ -0,0 +1,26 @@
+cask "netnewswire-beta" do
+  version "6.0beta1"
+  sha256 "450fc95364b59558205bf2118c61bf9c2a6c6f6f017a9b475d1f4866998b5755"
+
+  url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version.sub("beta", "b")}.zip",
+      verified: "github.com/brentsimmons/NetNewsWire/"
+  appcast "https://github.com/brentsimmons/NetNewsWire/releases.atom"
+  name "NetNewsWire"
+  desc "Free and open-source RSS reader"
+  homepage "https://ranchero.com/netnewswire/"
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "NetNewsWire.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.ranchero.NetNewsWire-Evergreen.Subscribe-to-Feed",
+    "~/Library/Application Support/NetNewsWire",
+    "~/Library/Caches/com.ranchero.NetNewsWire-Evergreen",
+    "~/Library/Containers/com.ranchero.NetNewsWire-Evergreen.Subscribe-to-Feed",
+    "~/Library/Preferences/com.ranchero.NetNewsWire-Evergreen.plist",
+    "~/Library/Saved Application State/com.ranchero.NetNewsWire-Evergreen.savedState",
+    "~/Library/WebKit/com.ranchero.NetNewsWire-Evergreen",
+  ]
+end
