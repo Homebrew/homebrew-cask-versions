fc466b2b9f15061714a077958462308500bb74fb
Markus Reiter <me@reitermark.us>
2021-03-13 15:16:58 +0100

    Update dotnet-preview.rb

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -1,11 +1,10 @@
 cask "dotnet-preview" do
-  basic_version = "6.0.0-preview.2.21154.6"
   if Hardware::CPU.intel?
-    version "#{basic_version},51cb50a7-c2e6-49f9-a172-e1dd0dd0f40f:d3cd57a2fc52ff8e85d8f52ba27f2e2f"
+    version "6.0.0-preview.2.21154.6,51cb50a7-c2e6-49f9-a172-e1dd0dd0f40f:d3cd57a2fc52ff8e85d8f52ba27f2e2f"
     sha256 "286ef6a223d8614df4df45e0e167ad68cd2b69de838b809bc3eb5ce0963e244b"
     arch_version="x64"
   else
-    version "#{basic_version},3de6add5-a77c-4621-bf77-1722073ac0a7:4535dfffd67bbbf59b06c1b59c0b2f29"
+    version "6.0.0-preview.2.21154.6,3de6add5-a77c-4621-bf77-1722073ac0a7:4535dfffd67bbbf59b06c1b59c0b2f29"
     sha256 "a05e828c99371b137434987ca7ef44a9ff0b36dc390fb5af9754075872b9506e"
     arch_version="arm64"
   end
