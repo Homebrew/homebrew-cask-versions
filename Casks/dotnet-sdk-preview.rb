d3dd176fb4bdce8be7a4218a14da873615f6d001
Markus Reiter <me@reitermark.us>
2021-03-13 15:17:22 +0100

    Update dotnet-sdk-preview.rb

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,11 +1,10 @@
 cask "dotnet-sdk-preview" do
-  basic_version = "6.0.100-preview.2.21155.3"
   if Hardware::CPU.intel?
-    version "#{basic_version},5e10dc75-294e-49f4-972e-218ae86191a3:e46d3533c30c8a864252a334820263a9"
+    version "6.0.100-preview.2.21155.3,5e10dc75-294e-49f4-972e-218ae86191a3:e46d3533c30c8a864252a334820263a9"
     sha256 "626c3c9beed8bf64797838ccebbc05cae5dc7cf28ebc741e444f45d7c0ed03b3"
     arch_version="x64"
   else
-    version "#{basic_version},e5cbc909-e705-4bc1-9327-15c9f905a149:6da57e95a58cef98444698fa72378e23"
+    version "6.0.100-preview.2.21155.3,e5cbc909-e705-4bc1-9327-15c9f905a149:6da57e95a58cef98444698fa72378e23"
     sha256 "9d9c6fc1a829a40222271c8b312368195dd2a771791120b33ab5a03cebdcc843"
     arch_version="arm64"
   end
