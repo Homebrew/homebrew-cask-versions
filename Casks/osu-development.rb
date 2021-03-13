e6a06c0c41ffd9ef5be3a5dc91a7ed028622660c
Hannes Braun <hannesbraun@mail.de>
2021-03-13 15:03:11 +0100

    osu-development: add livecheck

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -3,10 +3,15 @@ cask "osu-development" do
   sha256 "0de4193b5c122ee1a265d191afaf1e21836cf65a01f3548ec2568b33053d95ff"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
-  appcast "https://github.com/ppy/osu/releases.atom"
   name "osu!lazer"
   desc "Rhythm game"
   homepage "https://github.com/ppy/osu/"
+
+  livecheck do
+    url :homepage
+    strategy :git
+    regex(/^(\d+(?:\.\d+)*)$/)
+  end
 
   depends_on macos: ">= :sierra"
 
