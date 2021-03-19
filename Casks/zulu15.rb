11c75de2102c5202f6e6c0cf1f58b34b5c4eee79
David Schlosnagle <schlosna@gmail.com>
2021-03-19 01:14:37 -0400

    Add zulu15 (#10548)
    Azul Zulu Java Standard Edition Development Kit version 15.0.2+7 15.29.15
    
    JDK 16 is now default in main cask repo, see
    https://github.com/Homebrew/homebrew-cask/pull/101306
    https://github.com/Homebrew/homebrew-cask/blob/021a0b9ad82698049b9a3bac7b7718e55eee8f99/Casks/zulu.rb

---
+++ Casks/zulu15.rb
@@ -0,0 +1,32 @@
+cask "zulu15" do
+  version "15.0.2,15.29.15-ca"
+
+  if Hardware::CPU.intel?
+    sha256 "6284c7fb89cbbc8552788a3db522f6226a64d84454d21e075558c050328f6ed7"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  else
+    sha256 "599c3399eb6edb824f4660ba0a4fbb52118458041700dc71aec891bd383910e5"
+
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
+        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+  end
+  name "Azul Zulu Java Standard Edition Development Kit"
+  desc "Zulu OpenJDK 15"
+  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+
+  livecheck do
+    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
+    strategy :page_match do |page|
+      match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
+      "#{match[2]},#{match[1]}"
+    end
+  end
+
+  depends_on macos: ">= :sierra"
+
+  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+
+  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+end
