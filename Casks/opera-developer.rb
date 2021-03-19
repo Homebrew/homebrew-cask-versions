c1c5510872d94e824716398150eef38a585bdded
Hannes Braun <hannesbraun@mail.de>
2021-03-19 19:50:08 +0100

    Update opera-developer from 76.0.3989.0 to 76.0.3995.0 (#10553)
    * Update opera-developer from 76.0.3989.0 to 76.0.3995.0
    
    * Remove accidentally added livecheck

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "76.0.3989.0"
-  sha256 "a3f0a80164c93301c29651f68457906380312a89a246d9568a77047e8cf92125"
+  version "76.0.3995.0"
+  sha256 "dc1de5d8d92208a009dcc0b3972c08932ec6de4d88adeabecbd4b60704ede39b"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
