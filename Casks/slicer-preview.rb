913df845a82e81d705d5c1969f2ef1fad7e2529f
Bo Anderson <mail@boanderson.me>
2021-03-23 15:02:28 +0000

    slicer-preview: replace open-uri with curl

--- Casks/slicer-preview.rb
+++ Casks/slicer-preview.rb
@@ -3,10 +3,11 @@ cask "slicer-preview" do
   sha256 :no_check
 
   url do
-    require "open-uri"
     require "json"
     base_url = "https://download.slicer.org"
-    "#{base_url}/#{JSON.parse(URI("#{base_url}/find?os=macosx&stability=nightly").read)["download_url"]}"
+    result = curl_output("--fail", "--silent", "#{base_url}/find?os=macosx&stability=nightly")
+    result.assert_success!
+    "#{base_url}/#{JSON.parse(result.stdout)["download_url"]}"
   end
   name "3D Slicer"
   desc "Medical image processing and visualization system"
