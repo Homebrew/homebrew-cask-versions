8bd32f9646bf644341a0fdbc52733844304ada26
Bo Anderson <mail@boanderson.me>
2021-03-23 15:02:27 +0000

    emacs-nightly: replace open-uri with curl

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -3,8 +3,9 @@ cask "emacs-nightly" do
   sha256 :no_check
 
   url do
-    require "open-uri"
-    URI("https://emacsformacosx.com/atom/daily").read[/href="([^"]+.dmg)"/, 1]
+    result = curl_output("--fail", "--silent", "https://emacsformacosx.com/atom/daily")
+    result.assert_success!
+    result.stdout[/href="([^"]+.dmg)"/, 1]
   end
   name "Emacs"
   desc "GNU Emacs text editor"
