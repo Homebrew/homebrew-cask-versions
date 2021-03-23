a30553d8b1b125d190e6d051cdf1f3c204f40edc
Hannes Braun <hannesbraun@mail.de>
2021-03-23 14:59:04 +0100

    Update android-studio-preview-canary from 2020.3.1.10 to 2020.3.1.11 (#10566)

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,6 +1,6 @@
 cask "android-studio-preview-canary" do
-  version "2020.3.1.10"
-  sha256 "3e090c4cdb3e0efa83a84660cc67eb73b2b41c2a6432fd4416b3c7baba1495cf"
+  version "2020.3.1.11"
+  sha256 "b4b56ec6c055ba479778c3d33d57b68496db5809881dc9dae55934bbd7187bb0"
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
       verified: "dl.google.com/dl/android/studio/"
