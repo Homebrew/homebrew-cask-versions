4a770b658964f5483ae9ae048912719565b8b04c
Sorin Muntean <sxn@users.noreply.github.com>
2021-03-24 07:09:51 +0100

    Add Dash5 (#10568)

---
+++ Casks/dash5.rb
@@ -0,0 +1,30 @@
+cask "dash5" do
+  version "5.5.2,950"
+  sha256 "cd98b4496bfa82ff2cd4c98f7d8a35eb83b909b7da605eb291d12a0c162c24a8"
+
+  url "https://kapeli.com/downloads/v#{version.major}/Dash.zip"
+  name "Dash"
+  desc "API documentation browser and code snippet manager"
+  homepage "https://kapeli.com/dash"
+
+  livecheck do
+    url "https://kapeli.com/Dash#{version.major}.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
+
+  app "Dash.app"
+
+  zap trash: [
+    "~/Library/Application Support/Dash",
+    "~/Library/Application Support/com.kapeli.dashdoc",
+    "~/Library/Caches/com.kapeli.dashdoc",
+    "~/Library/Cookies/com.kapeli.dashdoc.binarycookies",
+    "~/Library/HTTPStorages/com.kapeli.dashdoc.binarycookies",
+    "~/Library/Logs/Dash",
+    "~/Library/Preferences/com.kapeli.dashdoc.plist",
+    "~/Library/Saved Application State/com.kapeli.dashdoc.savedState",
+    "~/Library/WebKit/com.kapeli.dashdoc",
+  ]
+end
