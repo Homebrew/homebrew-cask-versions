545d560fec284ecd8a6422d527a81e6c8b5574b8
Andy Fragen <andy@thefragens.com>
2021-03-24 17:21:21 -0700

    update 5.10.2

--- Casks/local-beta.rb
+++ Casks/local-beta.rb
@@ -1,6 +1,6 @@
 cask "local-beta" do
-  version "5.10.1,5269"
-  sha256 "5dd41277a1c9ddff499dc2ccf00e4f592d7f012112fa9bec133b60eff7ba7860"
+  version "5.10.2,5301"
+  sha256 "6b893d2d138777c54877b57555e7cbbd4e54bb43facde9796b5a01a3668cabee"
 
   url "https://cdn.localwp.com/releases-beta/#{version.before_comma}+local-beta-#{version.after_comma}/local-beta-#{version.before_comma}-b#{version.after_comma}-mac.dmg"
   name "Local Beta"
