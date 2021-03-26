52afcd497d76ebb21b9e83e0645491640dcdb15d
Vanilla <neko@hydev.org>
2021-03-26 10:44:46 +0800

    Update olympus from 2012 to 2027 (#10587)

--- Casks/olympus.rb
+++ Casks/olympus.rb
@@ -1,6 +1,6 @@
 cask "olympus" do
-  version "2012"
-  sha256 "317bde78dcd7725e0dbd5ff4b41f2a16c93138944cd79b6d3f877849a490823f"
+  version "2027"
+  sha256 "e5c010025967585f600fe7f0134dc1ae0d91a78a2b709ce03994bb0ba00c9293"
 
   url "https://dev.azure.com/EverestAPI/Olympus/_apis/build/builds/#{version}/artifacts?artifactName=macos.main&$format=zip",
       verified: "dev.azure.com/EverestAPI/Olympus"
