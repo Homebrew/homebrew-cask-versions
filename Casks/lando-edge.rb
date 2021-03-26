5c028bf0cb474b1565788ff499c127d046337547
Phill Tran <philltran@users.noreply.github.com>
2021-03-26 00:24:52 -0400

    Update lando-edge from 3.0.25 to 3.0.27 (#10585)
    * Update lando-edge from 3.0.25 to 3.0.27
    
    * Update lando-edge.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/lando-edge.rb
+++ Casks/lando-edge.rb
@@ -1,10 +1,9 @@
 cask "lando-edge" do
-  version "3.0.25"
-  sha256 "b98f636c303f60b72e2f77524afc8c30cd0aaffc12308716587c0f7986e92efc"
+  version "3.0.27"
+  sha256 "b92e422c16b129894d2f68e0cd6151487a1cb9edf2f584785bc781caf2ad50c9"
 
   url "https://github.com/lando/lando/releases/download/v#{version}/lando-v#{version}.dmg",
       verified: "github.com/lando/lando/"
-  appcast "https://github.com/lando/lando/releases.atom"
   name "Lando Edge"
   homepage "https://docs.lando.dev/"
 
