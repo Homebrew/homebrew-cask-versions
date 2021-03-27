5e1eb8d8f66ae229542bcf6e927362d8b7f751e7
Hannes Braun <hannesbraun@mail.de>
2021-03-27 15:34:29 +0100

    Fix version

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,5 +1,5 @@
 cask "appcode-eap" do
-  version "2020.1,211.6693.28"
+  version "2021.1,211.6693.28"
   sha256 "682951e03bea0c8135edf4dcea9cdf1be3e39c9282d4baa34dcd5e288bc3a3ec"
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.after_comma}.dmg"
