4862e11f498dd69a07806417cff68d8e742555e7
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-30 11:33:19 +0800

    Update arduino-beta.rb

--- Casks/arduino-beta.rb
+++ Casks/arduino-beta.rb
@@ -3,9 +3,16 @@ cask "arduino-beta" do
   sha256 "d535e3deb08b0a5f3fa5762c86702f0f9cc12dc05898f8db72a9aa3f9c1f048e"
 
   url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version}_macOS_64bit.dmg"
-  name "Arduino IDE 2.0 Beta"
+  name "Arduino Beta"
   desc "Open-source electronics prototyping platform"
   homepage "https://www.arduino.cc/"
+
+  livecheck do
+    url "https://www.arduino.cc/en/software/"
+    strategy :page_match do |page|
+      match = page.match(/href=.*?arduino[._-]ide[._-]v?(\d+(?:\.\d+)+)[._-]beta\.(\d+)[._-]macos[._-]64bit\.dmg/i)
+      "#{match[1]}-beta.#{match[2]}"
+  end
 
   app "Arduino IDE.app"
 end
