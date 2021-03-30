15caae74115a8d3cad61f22ff3fe3acb5002cab9
Miccal Matthews <miccal.matthews@gmail.com>
2021-03-30 11:40:23 +0800

    Update skype-preview.rb

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -5,7 +5,13 @@ cask "skype-preview" do
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
   name "Skype Preview"
+  desc "Video chat, voice call and instant messaging application"
   homepage "https://www.skype.com/en/insider/"
+
+  livecheck do
+    url "https://go.skype.com/mac.preview.download"
+    strategy :header_match
+  end
 
   auto_updates true
   conflicts_with cask: "skype"
