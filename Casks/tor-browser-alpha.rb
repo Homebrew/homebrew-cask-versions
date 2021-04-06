91451034bb2eb9acb640011a72b641a8fbf529ea
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-06 22:56:09 +0800

    tor-browser-alpha: add livecheck (#10642)

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -3,10 +3,15 @@ cask "tor-browser-alpha" do
   sha256 "697ed178f649ca5aba2d2259bb2fe959f21bd4fdd667e5b0cfe652fc2cce73be"
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
-  appcast "https://www.torproject.org/download/alpha/"
   name "Tor Browser"
   desc "Web browser focusing on security"
   homepage "https://www.torproject.org/"
+
+  livecheck do
+    url "https://www.torproject.org/download/alpha/"
+    strategy :page_match
+    regex(%r{href=.*?/TorBrowser-(\d+(?:.\d+)*)-osx64_.*\.dmg}i)
+  end
 
   auto_updates true
   conflicts_with cask: "tor-browser"
