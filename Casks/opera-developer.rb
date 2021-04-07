50acbdbbe7fcc3969dd0fc7733369929b0cf3b36
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-07 08:23:08 +0800

    Update opera-developer.rb

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -4,6 +4,7 @@ cask "opera-developer" do
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
+  desc "Web browser"
   homepage "https://www.opera.com/computer/beta"
 
   livecheck do
