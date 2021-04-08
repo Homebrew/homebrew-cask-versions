8c6982cd4c253d409c1f960f1cab12b19ae1aae3
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-08 13:13:35 +0800

    Update firefox-esr.rb

--- Casks/firefox-esr.rb
+++ Casks/firefox-esr.rb
@@ -68,11 +68,15 @@ cask "firefox-esr" do
 
   url "https://download-installer.cdn.mozilla.net/pub/firefox/releases/#{version}esr/mac/#{language}/Firefox%20#{version}esr.dmg",
       verified: "download-installer.cdn.mozilla.net/pub/firefox/releases/"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://download.mozilla.org/%3Fproduct=firefox-esr-latest-ssl%26os=osx"
   name "Mozilla Firefox ESR"
   name "Mozilla Firefox Extended Support Release"
   desc "Web browser"
   homepage "https://www.mozilla.org/firefox/organizations/"
+
+  livecheck do
+    url "https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx"
+    strategy :header_match
+  end
 
   conflicts_with cask: [
     "firefox",
