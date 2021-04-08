2d5de4033d3ecf9d61204b0343d9535a7a986b3b
404NetworkError <404NetworkError@users.noreply.github.com>
2021-04-08 00:09:19 -0400

    Update iterm2-beta from 3.4.5beta2 to 3.4.5beta3

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 cask "iterm2-beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.4.5beta2"
-  sha256 "3c7b71a5d74542590a23e66132a72b1cd469218757033b722bab929216711cfb"
+  version "3.4.5beta3"
+  sha256 "86b42e49bff8400f090ec83f7a15b32d94573bee2c9354f5d46a73ca27290758"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   appcast "https://iterm2.com/appcasts/testing_modern.xml"
