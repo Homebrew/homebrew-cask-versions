e2808a8039eed4c959c0beb0749b55e9c931c32f
David Yantis <404NetworkError@users.noreply.github.com>
2021-04-09 09:16:38 -0400

    Update iterm2-beta from 3.4.5beta2 to 3.4.5beta3 (#10659)

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,13 +1,17 @@
 cask "iterm2-beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.4.5beta2"
-  sha256 "3c7b71a5d74542590a23e66132a72b1cd469218757033b722bab929216711cfb"
+  version "3.4.5beta3"
+  sha256 "86b42e49bff8400f090ec83f7a15b32d94573bee2c9354f5d46a73ca27290758"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
-  appcast "https://iterm2.com/appcasts/testing_modern.xml"
   name "iTerm2"
   desc "Terminal emulator as alternative to Apple's Terminal app"
   homepage "https://www.iterm2.com/"
+
+  livecheck do
+    url "https://iterm2.com/appcasts/testing_modern.xml"
+    strategy :sparkle, &:version
+  end
 
   auto_updates true
   conflicts_with cask: [
