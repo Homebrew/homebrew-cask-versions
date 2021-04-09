0e4a97aed62a8d855e7f13fa7ca676751e99f1bf
Alexander Ronald Altman <alexanderaltman@me.com>
2021-04-09 12:07:51 -0700

    .NET 6 Preview 3
    Updates `dotnet-preview` and `dotnet-sdk-preview`.

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,13 +1,13 @@
 cask "dotnet-sdk-preview" do
   if Hardware::CPU.intel?
-    version "6.0.100-preview.2.21155.3,5e10dc75-294e-49f4-972e-218ae86191a3:e46d3533c30c8a864252a334820263a9"
-    sha256 "626c3c9beed8bf64797838ccebbc05cae5dc7cf28ebc741e444f45d7c0ed03b3"
+    version "6.0.100-preview.3.21202.5,fc5fdd1f-fb4c-4b88-a507-158204030320:98497ef248883404ff5b0604dda944fb"
+    sha256 "02ccd2669e2e27dbfeb23ae526432bfab2076636440b80483edb1a6c250690f6"
 
     url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
     pkg "dotnet-sdk-#{version.before_comma}-osx-x64.pkg"
   else
-    version "6.0.100-preview.2.21155.3,e5cbc909-e705-4bc1-9327-15c9f905a149:6da57e95a58cef98444698fa72378e23"
-    sha256 "9d9c6fc1a829a40222271c8b312368195dd2a771791120b33ab5a03cebdcc843"
+    version "6.0.100-preview.3.21202.5,293e469c-7948-4fc9-9d10-d5d39662e19d:1ae28ceb225f19d9aa922ffa3febb872"
+    sha256 "ccdbc6e0a75da77c1ed72975b2ae4099669454695a3396d4ba01a73525fbac38"
 
     url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-arm64.pkg"
     pkg "dotnet-sdk-#{version.before_comma}-osx-arm64.pkg"
