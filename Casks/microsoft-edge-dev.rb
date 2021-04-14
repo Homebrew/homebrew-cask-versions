89fd19ba60271c6dfc2ebfbb2909424244080c4f
David Hsu <weiche.hsu@outlook.com>
2021-04-14 19:53:56 +0800

    Differentiate Intel and Apple Silicon builds

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,9 +1,18 @@
 cask "microsoft-edge-dev" do
   version "91.0.852.0"
-  sha256 "dbff86d9f12930e674c0433ef9df6d62e6daa6f6b64ea7124b8a74a0bb5585e2"
 
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/"
+  if Hardware::CPU.intel?
+    sha256 "dbff86d9f12930e674c0433ef9df6d62e6daa6f6b64ea7124b8a74a0bb5585e2"
+
+    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
+        verified: "officecdn-microsoft-com.akamaized.net/"
+  else
+    sha256 "89bd9df900c792dad0dd4f76ceb4dd789763295ba413b6034b64b6abe8efff23"
+
+    url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
+        verified: "officecdn-microsoft-com.akamaized.net/"
+  end
+
   appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069340"
   name "Microsoft Edge Dev"
   desc "Multi-platform web browser"
