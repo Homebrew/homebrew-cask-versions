40f4e6aff651bee1bf5eb2e73456749ce8cbe8d3
wzxu <weiche.hsu@outlook.com>
2021-04-15 10:15:00 +0800

    Update microsoft-edge-dev from 89.0.731.0 to 91.0.852.0 & various improvements (#10709)
    * Update microsoft-edge-dev from 89.0.731.0 to 91.0.852.0
    
    * Differentiate Intel and Apple Silicon builds
    
    * Prefer livecheck over appcast
    
    * Fix typo of package identifier in uninstall
    
    * Follow the cleanup procedure of main microsoft-edge Cask

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,13 +1,26 @@
 cask "microsoft-edge-dev" do
-  version "89.0.731.0"
-  sha256 "8c37c6037f214408a331e8ecddd37a9bfd6b3a785262b236909b4d9d2b1497d0"
+  version "91.0.852.0"
 
-  url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
-      verified: "officecdn-microsoft-com.akamaized.net/"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://go.microsoft.com/fwlink/?linkid=2069340"
+  if Hardware::CPU.intel?
+    sha256 "dbff86d9f12930e674c0433ef9df6d62e6daa6f6b64ea7124b8a74a0bb5585e2"
+
+    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
+        verified: "officecdn-microsoft-com.akamaized.net/"
+  else
+    sha256 "89bd9df900c792dad0dd4f76ceb4dd789763295ba413b6034b64b6abe8efff23"
+
+    url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
+        verified: "officecdn-microsoft-com.akamaized.net/"
+  end
+
   name "Microsoft Edge Dev"
   desc "Multi-platform web browser"
   homepage "https://www.microsoftedgeinsider.com/"
+
+  livecheck do
+    url "https://go.microsoft.com/fwlink/?linkid=2069340"
+    strategy :header_match
+  end
 
   auto_updates true
   depends_on cask: "microsoft-auto-update"
@@ -21,14 +34,13 @@ cask "microsoft-edge-dev" do
         },
       ]
 
-  uninstall pkgutil: "com.microsoft.edgemac.Dev.Dev",
-            rmdir:   "/Library/Application Support/Microsoft"
+  uninstall pkgutil: "com.microsoft.edgemac.Dev"
 
   zap trash: [
-    "/Library/Application Support/Microsoft",
     "~/Library/Application Support/Microsoft Edge Dev",
     "~/Library/Caches/Microsoft Edge Dev",
     "~/Library/Preferences/com.microsoft.edgemac.Dev.plist",
     "~/Library/Saved Application State/com.microsoft.edgemac.Dev.savedState",
-  ]
+  ],
+      rmdir: "/Library/Application Support/Microsoft"
 end
