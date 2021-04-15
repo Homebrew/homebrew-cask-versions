16d9bbb39a2304031934d169cc0178ea6a7c1312
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-15 10:18:27 +0800

    Update 1password-beta.rb

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -3,10 +3,15 @@ cask "1password-beta" do
   sha256 "8d760e4a57a65e321965cb2d2056b6da06f5296cb1ed661adcc7300bf6074750"
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
-  appcast "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
   name "1Password"
   desc "Password manager that keeps all passwords secure behind one password"
   homepage "https://1password.com/"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
+    strategy :page_match
+    regex(%r{href=.*?/1Password-(\d+(?:\.\d+)*)\.pkg}i)
+  end
 
   auto_updates true
   conflicts_with cask: "1password"
