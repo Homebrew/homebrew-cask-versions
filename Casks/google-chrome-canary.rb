b9c5eb55d4a9081ab40f3cbd3d78c3eb4d5d8f5a
miccal <miccal.matthews@gmail.com>
2021-04-20 13:09:52 +0800

    Update google-chrome-canary from 92.0.4481.0 to 92.0.4482.2

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "92.0.4480.0"
+  version "92.0.4482.2"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
