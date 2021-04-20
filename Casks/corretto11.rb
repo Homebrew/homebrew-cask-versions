1c97f1e36dddbc95d8dec51d3d705a7141052884
Ben Taylor <benty@amazon.com>
2021-04-20 12:18:09 -0700

    Update corretto11 from 11.0.10.9.1 to 11.0.11.9.1

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,6 +1,6 @@
 cask "corretto11" do
-  version "11.0.10.9.1"
-  sha256 "4d3931e1be67c7a73a8f6c4820f29f954c168ac5b7154cd73ab4c38596d8266a"
+  version "11.0.11.9.1"
+  sha256 "a693e427abaa70871562bd9b629ca4ca6c760109cc3fe3fc4f237fa678d2e9e2"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.pkg"
