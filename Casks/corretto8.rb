405ad4671713643fdc8618f9f07f1e20058c3a5a
Ben Taylor <61255184+benty-amzn@users.noreply.github.com>
2021-04-20 12:25:16 -0700

    Update corretto8 from 8.282.08.1 to 8.292.10.1 (#10749)

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -1,6 +1,6 @@
 cask "corretto8" do
-  version "8.282.08.1"
-  sha256 "3fa660cf19aa21e64492682745fbc80e983e93a1a593b4c5e9452e68582e0e6f"
+  version "8.292.10.1"
+  sha256 "35d07818f31a3636ccee6510f2dfac667f36d27a4d2e024b60a25a475a3ad0f0"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-8-x64-macos-jdk.pkg"
