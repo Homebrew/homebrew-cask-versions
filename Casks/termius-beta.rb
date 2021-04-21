a42788ae849940cf93e50571788ff58a0e387ef2
Michael Cho <cho-m@tuta.io>
2021-04-21 04:02:21 -0700

    Update termius-beta from 7.3.0 to 7.9.0, add livecheck & M1 URL (#10755)

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,11 +1,21 @@
 cask "termius-beta" do
-  version "7.3.0"
+  version "7.9.0"
   sha256 :no_check
 
-  url "https://www.termius.com/beta/download/mac/Termius+Beta.dmg"
+  if Hardware::CPU.intel?
+    url "https://www.termius.com/beta/download/mac/Termius+Beta.dmg"
+  else
+    url "https://www.termius.com/beta/download/mac-arm64/Termius+Beta.dmg"
+  end
+
   name "Termius Beta"
   desc "Cross-platform SSH client"
   homepage "https://www.termius.com/beta-program"
+
+  livecheck do
+    url "https://autoupdate.termius.com/mac-beta/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Termius Beta.app"
 
