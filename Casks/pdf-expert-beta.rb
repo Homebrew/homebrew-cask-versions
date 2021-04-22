2cca5f13b0736b3d780c1bb0b4479a421ec9c906
Michael Cho <cho-m@tuta.io>
2021-04-22 01:41:16 -0700

    Update pdf-expert-beta to 2.5.17,737 and add livecheck (#10769)

--- Casks/pdf-expert-beta.rb
+++ Casks/pdf-expert-beta.rb
@@ -1,17 +1,21 @@
 cask "pdf-expert-beta" do
-  version "2.5.15,734"
-  sha256 "c793faede34b8e553cad5bfc04cd4e4d709b791ca19e6f8cd514a7998bdbd0b3"
+  version "2.5.17,737"
+  sha256 "b2bde16ea50a04045461b56ddf03e741951fac0504920982b8c6fab06bbb0e31"
 
   url "https://d1ke680phyeohy.cloudfront.net/versions/#{version.after_comma}/PDFExpert.dmg",
       verified: "d1ke680phyeohy.cloudfront.net/"
-  appcast "https://d1ke680phyeohy.cloudfront.net/beta/appcast.xml"
   name "PDF Expert"
   desc "PDF reader, editor and annotator"
   homepage "https://pdfexpert.com/"
 
+  livecheck do
+    url "https://d1ke680phyeohy.cloudfront.net/beta/appcast.xml"
+    strategy :sparkle
+  end
+
   auto_updates true
   conflicts_with cask: "pdf-expert"
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :high_sierra"
 
   app "PDF Expert.app"
 end
