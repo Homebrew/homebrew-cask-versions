6bdae75c05bc43742b03a4acca41e40a47364e43
Michael Cho <cho-m@tuta.io>
2021-04-21 18:52:35 -0700

    Update brave-browser-dev to 90.1.24.69,124.69 and add livecheck & M1 support (#10757)

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -1,10 +1,28 @@
 cask "brave-browser-dev" do
-  version "90.1.24.61,124.61"
-  sha256 "456c9993167538125b8823061c991a0afef0333309b23855f7d96b033ee73e5e"
+  version "90.1.24.69,124.69"
 
-  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev/#{version.after_comma}/Brave-Browser-Dev-x64.dmg",
-      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
-  appcast "https://updates.bravesoftware.com/sparkle/Brave-Browser/dev/appcast.xml"
+  if Hardware::CPU.intel?
+    sha256 "57b063be54fa8ddaf165e02fae63d3518eb9e7510b559967236234ce16104868"
+
+    url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev/#{version.after_comma}/Brave-Browser-Dev-x64.dmg",
+        verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
+
+    livecheck do
+      url "https://updates.bravesoftware.com/sparkle/Brave-Browser/dev/appcast.xml"
+      strategy :sparkle
+    end
+  else
+    sha256 "95cc360f652e469000f1114f22d6233d42a5a786abb15d806f0e0cfcbf4b9777"
+
+    url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/dev-arm64/#{version.after_comma}/Brave-Browser-Dev-universal.dmg",
+        verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
+
+    livecheck do
+      url "https://updates.bravesoftware.com/sparkle/Brave-Browser/dev-arm64/appcast.xml"
+      strategy :sparkle
+    end
+  end
+
   name "Brave Dev"
   desc "Web browser focusing on privacy"
   homepage "https://brave.com/download-dev/"
