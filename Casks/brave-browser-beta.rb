fa02727f365f2335d49bd06a0223487446404ae8
Michael Cho <cho-m@tuta.io>
2021-04-21 18:51:48 -0700

    Update brave-browser-beta to 90.1.24.68,124.68 and add livecheck & M1 support

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,10 +1,28 @@
 cask "brave-browser-beta" do
-  version "90.1.24.60,124.60"
-  sha256 "ed1a7972282134c19da43ba2c69abc1624ae378aeb1324be2ef1e15aa9fd0d75"
+  version "90.1.24.68,124.68"
 
-  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta-x64.dmg",
-      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
-  appcast "https://updates.bravesoftware.com/sparkle/Brave-Browser/beta/appcast.xml"
+  if Hardware::CPU.intel?
+    sha256 "ec77d817aa6c97c780aa9ff790b7892ab19445b6c3c662344f175adab43710b9"
+
+    url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta-x64.dmg",
+        verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
+
+    livecheck do
+      url "https://updates.bravesoftware.com/sparkle/Brave-Browser/beta/appcast.xml"
+      strategy :sparkle
+    end
+  else
+    sha256 "9f8a3d2ff0b037a0b622a973899919d2b683b29cbdf6504f601048d156e2e58a"
+
+    url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta-arm64/#{version.after_comma}/Brave-Browser-Beta-universal.dmg",
+        verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
+
+    livecheck do
+      url "https://updates.bravesoftware.com/sparkle/Brave-Browser/beta-arm64/appcast.xml"
+      strategy :sparkle
+    end
+  end
+
   name "Brave Beta"
   desc "Web browser focusing on privacy"
   homepage "https://brave.com/download-beta/"
