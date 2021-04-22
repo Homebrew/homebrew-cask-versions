9cf4c10848ffa597cdecb9db361b2c6124ec6edb
Michael Cho <cho-m@tuta.io>
2021-04-22 01:40:58 -0700

    Update sketch-beta to 72-123734 and add livecheck (#10768)

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,14 +1,20 @@
 cask "sketch-beta" do
-  version "71.0-112664"
-  sha256 :no_check
+  version "72-123734"
+  sha256 "c4fd1f3be51a195a48d80442263bbc5f4ec249e2ad79866ea39b4a0bdf9d447a"
 
-  url "https://beta-download.sketch.com/sketch.zip"
+  url "https://beta-download.sketch.com/sketch-#{version}.zip"
   name "Sketch"
   desc "Digital design and prototyping platform"
   homepage "https://www.sketch.com/beta"
 
+  livecheck do
+    url "https://beta-download.sketch.com/sketch-versions.xml"
+    strategy :page_match
+    regex(%r{url=.*?/sketch-(\d+(?:\.\d+)*-\d+)\.zip}i)
+  end
+
   auto_updates true
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :catalina"
 
   app "Sketch Beta.app"
 
