25a2e58a54b81d9bd19462881afc0b8f2894094f
Michael Cho <cho-m@tuta.io>
2021-04-23 12:29:28 -0700

    Update qt-creator-dev from 4.15.0-beta1 to 4.15.0-rc1 and add livecheck (#10782)

--- Casks/qt-creator-dev.rb
+++ Casks/qt-creator-dev.rb
@@ -1,10 +1,20 @@
 cask "qt-creator-dev" do
-  version "4.15.0-beta1"
-  sha256 "d0ee9cfda45aa49eb8a587b1ddedace27c922440d7948965d918726a561c7cbd"
+  version "4.15.0-rc1"
+  sha256 "02a1dcdc0c8d607b8b8a4493573871fd5832909984d6e73e2226f8a6e2f92437"
 
   url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name "Qt Creator Dev"
+  desc "IDE for application development"
   homepage "https://www1.qt.io/developers/"
+
+  livecheck do
+    url "https://download.qt.io/development_releases/qtcreator/"
+    strategy :page_match do |page|
+      version_major_minor = page[%r{href="(\d+(?:\.\d+)*)/"}i, 1]
+      version_page = Net::HTTP.get(URI.parse("https://download.qt.io/development_releases/qtcreator/#{version_major_minor}/"))
+      version_page[%r{href="(\d+(?:\.\d+)*-(?:rc|beta)\d*)/"}i, 1]
+    end
+  end
 
   app "Qt Creator.app"
 end
