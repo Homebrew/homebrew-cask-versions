48493033d52f300d139a4ec2905219af2e30d1f5
Michael Cho <cho-m@tuta.io>
2021-04-23 23:45:30 -0700

    Update powershell-preview from 7.2.0-preview.3 to 7.2.0-preview.5 and add livecheck

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -1,11 +1,17 @@
 cask "powershell-preview" do
-  version "7.2.0-preview.3"
-  sha256 "90003e3f76056a0b40ecc978a123db29d48726509dce1d60707ef2f9d8c0a793"
+  version "7.2.0-preview.5"
+  sha256 "d4464e9f8441b021cfe4a0bf15b7edbbf047ece3534f45ce85fd1afb95c672e0"
 
   url "https://github.com/PowerShell/PowerShell/releases/download/v#{version}/powershell-#{version}-osx-x64.pkg"
-  appcast "https://github.com/PowerShell/PowerShell/releases.atom"
   name "PowerShell"
+  desc "Command-line shell and scripting language"
   homepage "https://github.com/PowerShell/PowerShell"
+
+  livecheck do
+    url :homepage
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*-(?:preview|rc)(?:\.\d+)?)$/)
+  end
 
   depends_on formula: "openssl"
   depends_on macos: ">= :high_sierra"
