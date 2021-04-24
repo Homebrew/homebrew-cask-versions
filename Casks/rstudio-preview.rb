be6bcd9519d3b8b49658994ef3a7a0de696d7685
Michael Cho <cho-m@tuta.io>
2021-04-24 01:03:29 -0700

    Update rstudio-preview from 1.4.1114 to 1.4.1118 (#10792)

--- Casks/rstudio-preview.rb
+++ Casks/rstudio-preview.rb
@@ -1,10 +1,11 @@
 cask "rstudio-preview" do
-  version "1.4.1114"
-  sha256 "2b87318dd608086b806aec31371852ac7dccc271732df7d0e915e091af7d531e"
+  version "1.4.1118"
+  sha256 "6ac3e278ff1533612a1153cbe08d0b2effb0993319b081a556a8b0f6a31da7c4"
 
   url "https://s3.amazonaws.com/rstudio-ide-build/desktop/macos/RStudio-#{version}.dmg",
       verified: "s3.amazonaws.com/rstudio-ide-build/"
   name "RStudio"
+  desc "Data science software focusing on R and Python"
   homepage "https://www.rstudio.com/products/rstudio/download/preview/"
 
   livecheck do
@@ -12,6 +13,9 @@ cask "rstudio-preview" do
     strategy :page_match
     regex(/RStudio-(\d+(?:\.\d+)*)\.dmg/i)
   end
+
+  conflicts_with cask: "rstudio"
+  depends_on macos: ">= :high_sierra"
 
   app "RStudio.app"
 
