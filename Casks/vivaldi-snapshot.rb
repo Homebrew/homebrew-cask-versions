3510e1d43aae43b1e5940cd62890d978db87bdd9
Michael Cho <cho-m@tuta.io>
2021-04-24 01:04:24 -0700

    Update vivaldi-snapshot from 3.7.2218.3 to 3.8.2259.21 and add livecheck (#10790)

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,13 +1,19 @@
 cask "vivaldi-snapshot" do
-  version "3.7.2218.3.universal"
-  sha256 "69b770257fa6b8d056dd58958c2850d0e632dd95b55b2c2f584e88b5526610c6"
+  version "3.8.2259.21"
+  sha256 "96b74baa51e41acc459069b6c321e8277a1c1d27ad8b15bb2542f777e507b289"
 
-  url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.dmg"
-  appcast "https://update.vivaldi.com/update/1.0/snapshot/mac/appcast.xml"
+  url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
+  desc "Web browser focusing on customization and control"
   homepage "https://vivaldi.com/"
 
+  livecheck do
+    url "https://update.vivaldi.com/update/1.0/snapshot/mac/appcast.xml"
+    strategy :sparkle
+  end
+
   auto_updates true
+  depends_on macos: ">= :el_capitan"
 
   app "Vivaldi Snapshot.app"
 end
