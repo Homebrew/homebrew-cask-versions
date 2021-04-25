a1a7b656b78e4b80b9b2b96c1b5484251d887d20
Michael Cho <cho-m@tuta.io>
2021-04-24 23:48:44 -0700

    mi-beta: add livecheck (#10810)

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -1,12 +1,16 @@
 cask "mi-beta" do
-  version "3.5.2"
+  version "3.5.2,1000"
   sha256 "19646316623e83359b2f5c7096270e29ddc9b7db3b2f53d1350214bad3e23226"
 
-  url "https://www.mimikaki.net/download/mi#{version}.dmg"
-  appcast "https://www.mimikaki.net/download/appcast_beta.xml"
+  url "https://www.mimikaki.net/download/mi#{version.before_comma}.dmg"
   name "mi"
   desc "Text editor"
   homepage "https://www.mimikaki.net/download/beta.html"
+
+  livecheck do
+    url "https://www.mimikaki.net/download/appcast_beta.xml"
+    strategy :sparkle
+  end
 
   conflicts_with cask: "mi"
 
