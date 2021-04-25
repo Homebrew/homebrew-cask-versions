dcee7262b1ae7bb2d4b773e34a271cec4a4203c0
Michael Cho <cho-m@tuta.io>
2021-04-25 16:49:19 -0700

    Update qgis-ltr from 3.16.4 to 3.16.5,20210319_161904 and add livecheck (#10825)

--- Casks/qgis-ltr.rb
+++ Casks/qgis-ltr.rb
@@ -1,13 +1,19 @@
 cask "qgis-ltr" do
-  version "3.16.4"
-  sha256 :no_check
+  version "3.16.5,20210319_161904"
+  sha256 "26faf0b2d0d871a4a5f32fc904e8f7398d9d5eeb0dc7ae596835967f54ecc39f"
 
-  url "https://qgis.org/downloads/macos/qgis-macos-ltr.dmg"
-  appcast "https://qgis.org/downloads/macos/qgis-macos-ltr.sha256sum",
-          must_contain: version.dots_to_underscores
+  url "https://qgis.org/downloads/macos/ltr/qgis_ltr_final-#{version.before_comma.dots_to_underscores}_#{version.after_comma}.dmg"
   name "QGIS LTR"
   desc "Geographic Information System"
   homepage "https://www.qgis.org/"
+
+  livecheck do
+    url "https://qgis.org/downloads/macos/qgis-macos-ltr.sha256sum"
+    strategy :page_match do |page|
+      match = page.match(/qgis_ltr_final-(\d+(?:_\d+)*)_(\d+_\d+)\.dmg/i)
+      "#{match[1].tr("_", ".")},#{match[2]}"
+    end
+  end
 
   app "QGIS-LTR.app"
 end
