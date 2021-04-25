dd32bcff1570f2d9e551a80f1c5037081332cffe
Michael Cho <cho-m@tuta.io>
2021-04-25 12:46:54 -0700

    Update mongodb-compass-beta from 1.24.0-beta.2 to 1.27.0-beta.1 and add livecheck

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,12 +1,17 @@
 cask "mongodb-compass-beta" do
-  version "1.24.0-beta.2"
-  sha256 "1ea363948c2788fe899484187fa641cb98ece395bde316be18fcc5c78298e937"
+  version "1.27.0-beta.1"
+  sha256 "1aa94d818d8373293e8bb1f2509bd9f85f21c480a6691f95e2bfd6039eb20b41"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
-  appcast "https://www.mongodb.com/try/download/compass"
   name "MongoDB Compass"
   desc "GUI for MongoDB"
   homepage "https://www.mongodb.com/try/download/compass"
+
+  livecheck do
+    url "https://github.com/mongodb-js/compass"
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*-beta\.\d+)$/i)
+  end
 
   app "MongoDB Compass Beta.app"
 end
