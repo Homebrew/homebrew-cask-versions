e58080f263c4684de08f33a36d25a245af779f87
Miccal Matthews <miccal.matthews@gmail.com>
2021-04-25 15:06:31 +0800

    Update openzfs-dev.rb

--- Casks/openzfs-dev.rb
+++ Casks/openzfs-dev.rb
@@ -4,17 +4,17 @@ cask "openzfs-dev" do
     sha256 "ccb87037600c486c118afee7d9f3c41e8d5c4e90d41744a6e8dc78484b130e2b"
     pkg "OpenZFSonOsX-#{version.before_comma}-Mojave-10.14.pkg"
   elsif MacOS.version <= :catalina
-    version "2.0.0,266"
-    sha256 "151171a437a05ee5b171ef5b833d8284aba9859697e4601fdade79b2a576a00c"
+    version "2.0.0,272"
+    sha256 "4acb3d8f5c786349308ea14c1d0ba82d4050a3a6f71b8bd04c7e5cbd5c6ac969"
     pkg "OpenZFSonOsX-#{version.before_comma}-Catalina-10.15.pkg"
   elsif Hardware::CPU.intel?
-    version "2.0.0,265"
-    sha256 "5e85c05da5b50c5676e65b0e38d3c7fcc3815aa1cb2aaabc7d340d30cbacedcd"
+    version "2.0.0,271"
+    sha256 "cc6096201ffbb774c94c54b21de6a320d51b431ee161752c0e7aefdc02543646"
     pkg "OpenZFSonOsX-#{version.before_comma}-Big.Sur-11.0.pkg"
   else
-    version "2.0.0,242"
-    sha256 "53deccf5dced128c25c47592078c56cd52f488583c06e8b4f17468aae3041cc6"
-    pkg "OpenZFSonOsX-#{version.before_comma}-Big.Sur-11.2.pkg"
+    version "2.0.0,270"
+    sha256 "271e26b49687b266142adbbbf73d9a91fce4b44fc62d5d2e8f091efb5253f01b"
+    pkg "OpenZFSonOsX-#{version.before_comma}-Big.Sur-11.2-ARM64.pkg"
   end
 
   url "https://openzfsonosx.org/forum/download/file.php?id=#{version.after_comma}"
