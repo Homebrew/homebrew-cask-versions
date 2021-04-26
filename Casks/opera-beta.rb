8e0147d01c4a45140d8932c71b3104d61c7c9ab3
Hannes Braun <hannesbraun@mail.de>
2021-04-26 19:38:24 +0200

    Update opera-beta from 76.0.4017.59 to 76.0.4017.88

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask "opera-beta" do
-  version "76.0.4017.59"
-  sha256 "802ff7b9f9d055b029c673265a1166ce578e463d1b534fb7a7911ff235200813"
+  version "76.0.4017.88"
+  sha256 "a05404679145d8023d7f37bd66c275eab685d698c3a9ef9574159678fbf3a0c5"
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name "Opera Beta"
