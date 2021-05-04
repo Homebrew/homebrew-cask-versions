8d795f2d21e5724ae2cfce2b20ba05ad96ae4b86
Michael Cho <cho-m@tuta.io>
2021-05-04 15:09:26 -0700

    Add xampp7 cask with XAMPP 7.4.16 installer (#10845)
    * Add xampp7 cask with XAMPP 7.4.16 installer
    
    * xampp7: mark conflict with main 'xampp' cask

---
+++ Casks/xampp7.rb
@@ -0,0 +1,32 @@
+cask "xampp7" do
+  version "7.4.16-0"
+  sha256 "be2ae9f742249b4d8f474b5cc320478813a0495b604671fd27d4c6f72963e839"
+
+  url "https://downloadsapachefriends.global.ssl.fastly.net/xampp-files/#{version.split("-").first}/xampp-osx-#{version}-installer.dmg",
+      verified: "downloadsapachefriends.global.ssl.fastly.net/xampp-files/"
+  name "XAMPP"
+  desc "Apache distribution containing MySQL, PHP 7, and Perl"
+  homepage "https://www.apachefriends.org/index.html"
+
+  livecheck do
+    url "https://www.apachefriends.org/download.html"
+    strategy :page_match
+    regex(%r{href=.*?/xampp-osx-(7(?:\.\d+)*-\d+)-installer\.dmg}i)
+  end
+
+  conflicts_with cask: "xampp"
+
+  installer script: {
+    executable: "XAMPP.app/Contents/MacOS/osx-x86_64",
+    args:       ["--mode", "unattended"],
+    sudo:       true,
+  }
+
+  uninstall quit:   "com.bitnami.manager",
+            script: {
+              executable: "/Applications/XAMPP/uninstall.app/Contents/MacOS/osx-x86_64",
+              args:       ["--mode", "unattended"],
+              sudo:       true,
+            },
+            delete: "/Applications/XAMPP/"
+end
