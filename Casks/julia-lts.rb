b6b4c65a860fd60299bd5889ff614b27ee181c4d
Michael Cho <cho-m@tuta.io>
2021-05-09 23:33:26 -0700

    julia-lts: add livecheck (#10913)

--- Casks/julia-lts.rb
+++ Casks/julia-lts.rb
@@ -3,9 +3,14 @@ cask "julia-lts" do
   sha256 "1ff7bd41f396ba3f34dc17c26b78d11adeadfafdea537f7ad17020ef812a39a0"
 
   url "https://julialang-s3.julialang.org/bin/mac/x64/#{version.major_minor}/julia-#{version}-mac64.dmg"
-  appcast "https://github.com/JuliaLang/julia/releases.atom"
   name "Julia"
+  desc "Programming language for technical computing"
   homepage "https://julialang.org/"
+
+  livecheck do
+    url "https://julialang.org/downloads/"
+    regex(/\(LTS\)\s+release:\s+v?(\d+(?:\.\d+)+)/i)
+  end
 
   app "Julia-#{version.major_minor}.app"
   binary "#{appdir}/Julia-#{version.major_minor}.app/Contents/Resources/julia/bin/julia", target: "julia-lts"
