b2de95182395e6b7d9bfd5b6dd996307a4af5386
Jeffrey C <spaz926@me.com>
2021-05-10 17:17:57 -0500

    Update 1password-beta to 7.8.3.BETA-0

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,6 +1,6 @@
 cask "1password-beta" do
-  version "7.8.2"
-  sha256 "a7c6fd0813355e2462c0f65cb74ea37a0728fdf4776e71a8f53ce9208ef9972d"
+  version "7.8.3.BETA-0"
+  sha256 "8a2b13750a9cf3f2651474c4d2411a00f3ef2141b17d6be42f69af4faf2fb8db"
 
   url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
   name "1Password"
