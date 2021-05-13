a24caad0b552a7af77fa3adfcea95902e81db320
mystygage <1107406+mystygage@users.noreply.github.com>
2021-05-13 02:03:20 +0200

    Update java-beta from 17,12 to 17,21 (#10946)
    * Update java-beta from 17,12 to 17,21
    
    * Update java-beta.rb
    
    Co-authored-by: mystygage <mystygage@users.noreply.github.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,11 +1,19 @@
 cask "java-beta" do
-  version "17,12"
-  sha256 "14c6ca3879b0d09a052810db42aac74f7b0c28b38058ced183ece2a827ed9d3d"
+  version "17,21"
+  sha256 "af8a611e7104d4d622bf52c6df27a7191a93af2be22360964ad0a339947566e3"
 
-  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_osx-x64_bin.tar.gz"
+  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-x64_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
   desc "Early access development kit for the Java programming language"
   homepage "https://jdk.java.net/"
+
+  livecheck do
+    url "https://jdk.java.net/#{version.major}/"
+    strategy :page_match do |page|
+      match = page.match(/openjdk-(\d+)-ea\+(\d+)_macos-x64_bin\.tar\.gz/i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   artifact "jdk-#{version.before_comma}.jdk", target: "/Library/Java/JavaVirtualMachines/openjdk-#{version.before_comma}.jdk"
 end
