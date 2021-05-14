a518b673f280c7c129c56dc6fe3315c14f0da930
Miccal Matthews <miccal.matthews@gmail.com>
2021-05-14 09:05:43 +0800

    Update dotnet-preview.rb (#10958)

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -5,44 +5,42 @@ cask "dotnet-preview" do
 
     url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
     pkg "dotnet-runtime-#{version.before_comma}-osx-x64.pkg"
+
+    livecheck do
+      url "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/#{version.major_minor}/releases.json"
+      strategy :page_match do |page|
+        page.scan(%r{/download/pr/([^/]+)/([^/]+)/dotnet-runtime-v?(.+)-osx-x64\.pkg}i).map do |match|
+          "#{match[2]},#{match[0]}:#{match[1]}"
+        end
+      end
+    end
   else
     version "6.0.0-preview.3.21201.4,668c384c-9568-4261-9897-9d76a3d27e6f:60d295f764d49be02666ee1b46658a2e"
     sha256 "709e2ef53d21f690fb0071655d90f31f2c3daec7d65beb65bd6c9e21893e481d"
 
     url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-arm64.pkg"
     pkg "dotnet-runtime-#{version.before_comma}-osx-arm64.pkg"
-  end
 
-  name ".NET Runtime"
-  desc "Preview release of the .NET Runtime"
-  homepage "https://dotnet.microsoft.com/"
-
-  livecheck do
-    url "https://dotnet.microsoft.com/download/dotnet"
-    strategy :page_match do |page|
-      cpu = if Hardware::CPU.intel?
-        "x64"
-      else
-        "arm64"
+    livecheck do
+      url "https://dotnetcli.blob.core.windows.net/dotnet/release-metadata/#{version.major_minor}/releases.json"
+      strategy :page_match do |page|
+        page.scan(%r{/download/pr/([^/]+)/([^/]+)/dotnet-runtime-v?(.+)-osx-arm64\.pkg}i).map do |match|
+          "#{match[2]},#{match[0]}:#{match[1]}"
+        end
       end
-      release = page[%r{<td>(\d+(?:\.\d+)*-(?:preview|rc)(?:\.\d+)*)</td>}i, 1]
-      install_page = Net::HTTP.get(URI.parse("#{url}/thank-you/runtime-#{release}-macos-#{cpu}-installer"))
-      match = install_page.match(%r{
-        href=.*?
-        /([0-9a-f]+(?:-[0-9a-f]+)*)
-        /([0-9a-f]+)
-        /dotnet-runtime-((\d+(?:\.\d+)*-(?:preview|rc)(?:\.\d+)*))-osx-#{cpu}\.pkg
-      }ix)
-      "#{match[3]},#{match[1]}:#{match[2]}"
     end
   end
+
+  name ".Net Runtime"
+  desc "Developer platform"
+  homepage "https://www.microsoft.com/net/core#macos"
 
   conflicts_with cask: [
     "dotnet",
     "dotnet-sdk",
     "dotnet-sdk-preview",
   ]
-  depends_on macos: ">= :sierra"
+  depends_on macos: ">= :mojave"
 
   binary "/usr/local/share/dotnet/dotnet"
 
