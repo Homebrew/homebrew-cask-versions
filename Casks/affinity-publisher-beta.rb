1a86103d2d95cb3ce90b1e90dd85377b573ec840
Hannes Braun <hannesbraun@mail.de>
2021-05-17 13:47:14 +0200

    Update affinity-publisher-beta from 1.9.1.979 to 1.9.2.1024 (#10983)

--- Casks/affinity-publisher-beta.rb
+++ Casks/affinity-publisher-beta.rb
@@ -1,6 +1,6 @@
 cask "affinity-publisher-beta" do
-  version "1.9.1.979"
-  sha256 "bf8d467dae8eb2633eee95f675b3dac6b4bae9daab7ccf682389ddbb2faf14fc"
+  version "1.9.2.1024"
+  sha256 "34d39facae3d67311e0d8630134747164ff01d68f9bba6e296b02184b20a78ee"
 
   url "https://affinity-beta.s3.amazonaws.com/download/affinity-publisher-customer-beta-#{version}.zip",
       verified: "affinity-beta.s3.amazonaws.com/"
