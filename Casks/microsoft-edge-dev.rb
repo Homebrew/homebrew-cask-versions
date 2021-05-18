f7e4420795d3b86e58c88fa42a6827c12a7cb2ce
Michael Cho <cho-m@tuta.io>
2021-05-17 23:33:24 -0700

    Update microsoft-edge-dev from 92.0.878.0 to 92.0.884.2

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,13 +1,13 @@
 cask "microsoft-edge-dev" do
-  version "92.0.878.0"
+  version "92.0.884.2"
 
   if Hardware::CPU.intel?
-    sha256 "c88adef3603d8be3536ffe2adfe3461d8b4be5f9b8cee9ce59b35a87165b2711"
+    sha256 "5c89b83545b9171a7d358e592260733ecb3bc0b9b4302e7a9d6547fc357cdc1d"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
   else
-    sha256 "8a91a190727b298aba6c12cc75369af349c2c97cdfe171a87b2aee368fd000e5"
+    sha256 "c232485a4c97186b599fe7ad48432ea2a599d89fee07a43b228448630406e65a"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
