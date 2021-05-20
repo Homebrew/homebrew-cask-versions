7944b1d8d999f3fc80f7c16ed2d1ec142da7ded5
Seungbin Oh <sboh1214@gmail.com>
2021-05-20 11:39:11 +0900

    Update android-studio-preview-beta 2020.3.1.16 (#10998)
    * Update url and version to 2020.3.1.16
    
    * android-studio-preview-beta 2020.3.1.16
    
    * Update android-studio-preview-beta.rb
    
    * Update android-studio-preview-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Co-authored-by: Michael Cho <cho-m@tuta.io>

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -1,8 +1,8 @@
 cask "android-studio-preview-beta" do
-  version "4.2.0.23,202.7231092"
-  sha256 "5fb1f88a3437f95a27eb535357cf3a45f2ce51377c39a46af5d38bb73dfbc0d9"
+  version "2020.3.1.16"
+  sha256 "7342d856931eb798c1a8d6d0e8857205e6d9c1f3633d2afcea4fca3dc8544853"
 
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version.before_comma}/android-studio-ide-#{version.after_comma}-mac.zip",
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
       verified: "dl.google.com/dl/android/studio/"
   name "Android Studio Preview (Beta)"
   desc "Tools for building Android applications"
@@ -10,15 +10,12 @@ cask "android-studio-preview-beta" do
 
   livecheck do
     url :homepage
-    strategy :page_match do |page|
-      match = page.match(%r{href=.*?/(\d+(?:\.\d+)+)/android-studio-ide-(\d+(?:\.\d+)+)-mac\.zip}i)
-      "#{match[1]},#{match[2]}"
-    end
+    regex(%r{href=.*?/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]mac\.zip[^>]*>[\w\s.-]*?Beta}i)
   end
 
   conflicts_with cask: "android-studio-preview-canary"
 
-  app "Android Studio.app"
+  app "Android Studio Preview.app"
 
   zap trash: [
     "~/Library/Android/sdk",
