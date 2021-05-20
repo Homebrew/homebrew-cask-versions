bfa2342d75460fbac0eb2ae59560f61f1ba9b0c0
Yurii Kolesnykov <root@yurikoles.com>
2021-05-20 23:37:59 +0300

    vmware-fusion-tech-preview: update description
    Co-authored-by: Vítor Galvão <info@vitorgalvao.com>

--- Casks/vmware-fusion-tech-preview.rb
+++ Casks/vmware-fusion-tech-preview.rb
@@ -4,7 +4,7 @@ cask "vmware-fusion-tech-preview" do
 
   url "https://download3.vmware.com/software/fusion/file/VMware-Fusion-e.x.p-#{version}.dmg"
   name "VMware Fusion Tech Preview"
-  desc "App to run other operating systems without rebooting"
+  desc "Create, manage, and run virtual machinces"
   homepage "https://blogs.vmware.com/teamfusion/tech-preview"
 
   livecheck do
