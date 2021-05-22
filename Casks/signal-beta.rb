aebd6f9bfc899a0489ba0954e3df46bb7a450de4
LanikSJ <laniksj@gmail.com>
2021-05-21 21:45:48 -0700

    Update signal-beta from 5.3.0-beta.1 to 5.3.0-beta.2

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,6 +1,6 @@
 cask "signal-beta" do
-  version "5.3.0-beta.1"
-  sha256 "7eb42d200a34bab3d2a9ab93f5668be396cdc9a51d8a22273109eadc49f8a3a0"
+  version "5.3.0-beta.2"
+  sha256 "bee7a245a0f199dad5010b02d4ff33a1e9be34268340e5bd2268e576c6d119ea"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{version}.dmg"
   name "Signal Beta"
