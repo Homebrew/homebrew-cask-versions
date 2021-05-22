c305351d7498f79ce748f2b7008e96685c112818
Hannes Braun <hannesbraun@mail.de>
2021-05-22 09:40:06 +0200

    Update google-chrome-canary from 92.0.4515.3 to 92.0.4515.5

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "92.0.4515.3"
+  version "92.0.4515.5"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
