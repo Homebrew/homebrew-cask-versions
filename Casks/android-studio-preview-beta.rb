db7df411966dbd7950388e5800e05385e3aa27d8
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2021-05-23 18:44:42 +0000

    Add support for ARM64 builds of Android Studio Preview Beta for Apple Silicon (#11027)

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -1,9 +1,18 @@
 cask "android-studio-preview-beta" do
   version "2020.3.1.17"
-  sha256 "8bdded06bd0d1bc50f82ea875f2fd3e472ece8da93100a4636ebe90ceb7af2fa"
 
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
-      verified: "dl.google.com/dl/android/studio/"
+  if Hardware::CPU.intel?
+    sha256 "8bdded06bd0d1bc50f82ea875f2fd3e472ece8da93100a4636ebe90ceb7af2fa"
+
+    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
+        verified: "dl.google.com/dl/android/studio/"
+  else
+    sha256 "48dc0d1c307c0cafc3cf59624d9a7f69274bb0950f35356251d97453dd1e703d"
+
+    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac_arm.zip",
+        verified: "dl.google.com/dl/android/studio/"
+  end
+
   name "Android Studio Preview (Beta)"
   desc "Tools for building Android applications"
   homepage "https://developer.android.com/studio/preview/"
