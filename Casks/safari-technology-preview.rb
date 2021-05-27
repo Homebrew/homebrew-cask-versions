be8ab3b76c47fb73b32464bbad6be2e3041d2586
Logicer <81077770+Logicer16@users.noreply.github.com>
2021-05-27 18:34:19 +1000

    Update safari-technology-preview.rb

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,10 +1,10 @@
 cask "safari-technology-preview" do
   if MacOS.version <= :catalina
-    version "125,071-45899-20210526-3fe7359c-0f20-4850-b6ec-da9b197119c2"
-    sha256 "63e4961fd0cca345b58128c0edb33627847d02b99a77613c54c0a89e6402d36e"
-  else
     version "125,071-44527-20210526-93430244-0334-4fae-878d-56502a656003"
     sha256 "6ea830ec5c9101665e18d77f31a7f220a0fc8afc35b9ac036ee29d5ee3dbdd23"
+  else
+    version "125,071-45899-20210526-3fe7359c-0f20-4850-b6ec-da9b197119c2"
+    sha256 "63e4961fd0cca345b58128c0edb33627847d02b99a77613c54c0a89e6402d36e"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.after_comma}/SafariTechnologyPreview.dmg"
