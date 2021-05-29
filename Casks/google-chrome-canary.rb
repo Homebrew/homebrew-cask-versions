26d7b5cd2219532d0a7ece085cb3360e17b62907
Logicer <81077770+Logicer16@users.noreply.github.com>
2021-05-29 11:26:05 +1000

    Update google-chrome-canary to 93.0.4525.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "93.0.4524.0"
+  version "93.0.4525.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
