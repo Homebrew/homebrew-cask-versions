29a300760aa99fc83fa83215340c972725c85c96
Vijay A <avijayr@protonmail.com>
2021-05-31 07:47:55 +0530

    Ferdi beta '5.6.0-beta.6' and added support for ARM as well.

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,9 +1,18 @@
 cask "ferdi-beta" do
-  version "5.6.0-beta.5"
-  sha256 "6ffcb4a287ebb9e596d8a116d6132b63473064e5f579fbb361f0fd61e8ff4740"
+  version "5.6.0-beta.6"
 
-  url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg",
-      verified: "github.com/getferdi/ferdi/"
+  if Hardware::CPU.intel?
+    sha256 "1be2f499b8d83be616fe7bec473ea3339451b32a2ee151015042931e3282d78f"
+
+    url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg",
+        verified: "github.com/getferdi/ferdi/"
+  else
+    sha256 "b4c2808b7b01eed1849f1514714b477731b1732dd2ff0c2bc79e8164ad35e74e"
+
+    url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}-arm64.dmg",
+        verified: "github.com/getferdi/ferdi/"
+  end
+
   appcast "https://github.com/getferdi/ferdi/releases.atom"
   name "Ferdi"
   desc "Messaging browser which combines several services"
