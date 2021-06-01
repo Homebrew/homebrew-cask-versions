e715db3d0b4c8342ecc92aa8fcc356ec592dd38e
Ling Cong Xiang <ralcx@outlook.com>
2021-06-01 09:31:01 +0800

    Add citra-nightly (#11088)
    * Add citra-nightly
    
    * Update citra-nightly.rb
    
    Co-authored-by: Michael Cho <cho-m@tuta.io>

---
+++ Casks/citra-nightly.rb
@@ -0,0 +1,27 @@
+cask "citra-nightly" do
+  version "1708,20210513-e7671d9"
+  sha256 "3d169e18e40e374d71d76ecfa0c7b856159787e1bed5e23ba252c1e676a925b5"
+
+  url "https://github.com/citra-emu/citra-nightly/releases/download/nightly-#{version.before_comma}/citra-osx-#{version.after_comma}.tar.gz",
+      verified: "github.com/citra-emu/citra-nightly/"
+  name "Citra"
+  desc "Nintendo 3DS emulator"
+  homepage "https://citra-emu.org/"
+
+  livecheck do
+    url :url
+    strategy :github_latest do |page|
+      match = page.match(%r{href=.*?/nightly[._-](\d+)/citra[._-]osx[._-](\d+[._-]\h+)\.tar\.gz}i)
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  app "citra-osx-#{version.after_comma}/citra-qt.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.citra-emu.citra.plist",
+    "~/Library/Saved Application State/com.citra-emu.citra.savedState",
+    "~/.config/citra-emu",
+    "~/.local/share/citra-emu",
+  ]
+end
