32fde7b92a896d92d1338c8c54d47ee936381cd2
Hannes Braun <hannesbraun@mail.de>
2021-06-03 12:16:49 +0200

    Update brave-browser-beta from 91.1.26.47,126.47 to 91.1.26.50,126.50

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,8 +1,8 @@
 cask "brave-browser-beta" do
-  version "91.1.26.47,126.47"
+  version "91.1.26.50,126.50"
 
   if Hardware::CPU.intel?
-    sha256 "476e2e0f66fb75231c25676dbc04eb0aa2181464ea76cd60e9aa209a64f39fe3"
+    sha256 "170ccfb963fef0e65d98e13ae8d55be66127785f0066067d740f6f808a137935"
 
     url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta-x64.dmg",
         verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
@@ -12,7 +12,7 @@ cask "brave-browser-beta" do
       strategy :sparkle
     end
   else
-    sha256 "726cf6cb036125cf70b1b707b9172b0f26c39c6cd414e7bef960837668313c98"
+    sha256 "8cafc1d3aaf8e9c6b0eaffc5b2a3d0391406656239d6dc15ae29b488ac1aad04"
 
     url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta-arm64/#{version.after_comma}/Brave-Browser-Beta-universal.dmg",
         verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
