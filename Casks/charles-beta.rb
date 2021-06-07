6e700326a0206695e8ae21b83d025ffa6950b78f
Misaka13514 <Misaka13514@gmail.com>
2021-06-07 20:13:39 +0800

    Update charles-beta from 4.6.2b5 to 4.6.2b7

--- Casks/charles-beta.rb
+++ Casks/charles-beta.rb
@@ -1,6 +1,6 @@
 cask "charles-beta" do
-  version "4.6.2b5"
-  sha256 "0aeb6ef9d8aeaac6856a4704592097b6b0931adfde46c41d9657d6e373047138"
+  version "4.6.2b7"
+  sha256 "fd7e13e6d6c1bd87fad79a499a4f065a54c60e8c724b4c4e18a002cf6aff7f54"
 
   url "https://www.charlesproxy.com/assets/release/#{version.gsub(/b\d$/, "")}/charles-proxy-#{version}.dmg"
   name "Charles"
