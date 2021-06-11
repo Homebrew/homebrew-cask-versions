42d352171ac141fd7dad989948ceb26434fac6a7
Kyungdahm Yun <tomyun@gmail.com>
2021-06-10 18:05:09 -0700

    Update julia-nightly from 1.7 to 1.8 (#11159)
    * Update julia-nightly from 1.7 to 1.8
    
    * Update julia-nightly.rb
    
    * Update julia-nightly.rb
    
    * Update julia-nightly.rb
    
    * Update julia-nightly.rb
    
    * Update julia-nightly.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/julia-nightly.rb
+++ Casks/julia-nightly.rb
@@ -1,10 +1,15 @@
 cask "julia-nightly" do
-  version "1.7"
+  version "1.8"
   sha256 :no_check
 
   url "https://julialangnightlies-s3.julialang.org/bin/mac/x64/julia-latest-mac64.dmg"
   name "Julia"
+  desc "Programming language for technical computing"
   homepage "https://julialang.org/"
+
+  livecheck do
+    skip "unversioned URL"
+  end
 
   app "Julia-#{version}.app"
   binary "#{appdir}/Julia-#{version}.app/Contents/Resources/julia/bin/julia", target: "julia-nightly"
