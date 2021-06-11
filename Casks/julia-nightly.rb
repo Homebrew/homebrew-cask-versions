f3c8c6f498cc3bd80776e761a8e9df44a9aedcca
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-11 08:03:18 +0800

    Update julia-nightly.rb

--- Casks/julia-nightly.rb
+++ Casks/julia-nightly.rb
@@ -1,5 +1,5 @@
 cask "julia-nightly" do
-  version "1.8"
+  version "1.8.0,1.8.0-DEV-0d94727a38"
   sha256 :no_check
 
   url "https://julialangnightlies-s3.julialang.org/bin/mac/x64/julia-latest-mac64.dmg"
