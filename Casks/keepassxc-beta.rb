0a988b48cf3164c6488ec7a3106bf72f0fa4256d
Vijay A <avijayr@protonmail.com>
2021-06-13 13:02:33 +0530

    Upgraded keepassxc to '2.6.6'.

--- Casks/keepassxc-beta.rb
+++ Casks/keepassxc-beta.rb
@@ -1,14 +1,14 @@
 cask "keepassxc-beta" do
-  version "2.6.5"
+  version "2.6.6"
 
   if Hardware::CPU.intel?
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
-    sha256 "b15008d6ab19ef2449f4941bad71dca8d1406929daa36453e379c9e3214a6e47"
+    sha256 "ab96033c16459de5a95e1f9e5864a5bd8cc47b4f3dee2c68ede6199dd44286ec"
   else
     url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-arm64.dmg",
         verified: "github.com/keepassxreboot/keepassxc/"
-    sha256 "5f8cd4443191ad9340bff99a842d54d66dae29f29dc5b6f22135313b4110717a"
+    sha256 "9dc121bb08f5b46186930ac9ba189553cec2c2ce9688df466e9ce1d9d75fe8c5"
   end
 
   name "KeePassXC"
