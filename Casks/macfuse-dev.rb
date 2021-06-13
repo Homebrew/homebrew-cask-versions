2a705ff8c1e82e285cbfe20da94fa2cfb848f7e0
Yurii Kolesnykov <root@yurikoles.com>
2021-06-13 04:36:30 +0300

    New cask: macfuse-dev 4.2.0 (#11173)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Casks/macfuse-dev.rb
@@ -0,0 +1,27 @@
+cask "macfuse-dev" do
+  version "4.2.0"
+  sha256 "bf413e3aae887b6e8e8f8ad46152eb34575b7ff7f90d6cc55735ad00c5a4c7a6"
+
+  url "https://github.com/osxfuse/osxfuse/releases/download/macfuse-#{version}/macfuse-#{version}.dmg",
+      verified: "github.com/osxfuse/osxfuse/"
+  name "macFUSE"
+  desc "File system integration"
+  homepage "https://osxfuse.github.io/"
+
+  conflicts_with cask: "macfuse"
+
+  pkg "Extras/macFUSE #{version}.pkg"
+
+  postflight do
+    set_ownership ["/usr/local/include", "/usr/local/lib"]
+  end
+
+  uninstall pkgutil: [
+    "io.macfuse.installer.components.core",
+    "io.macfuse.installer.components.preferencepane",
+  ]
+
+  caveats do
+    kext
+  end
+end
