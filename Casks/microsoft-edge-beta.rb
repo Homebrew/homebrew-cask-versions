3ca1b68dd186b2ada939cfa39b26202a3ecb9541
Michael Cho <cho-m@tuta.io>
2021-06-23 22:03:31 -0700

    Update microsoft-edge-beta from 91.0.864.27 to 92.0.902.23

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -1,13 +1,13 @@
 cask "microsoft-edge-beta" do
-  version "91.0.864.27"
+  version "92.0.902.23"
 
   if Hardware::CPU.intel?
-    sha256 "3cb87d8783c33f71e82d41af7f7b569242cd6858e1d63c85d9fc01d76f1fd324"
+    sha256 "4cf90a0b113d449dd4b13cc7579dccbc52c3824ccd8721d8dd6e9dcbf4a8a8b4"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
   else
-    sha256 "555c24141ea81877b2c4faefb467adb9125ea3bddcd877d4f6cdf4f722e89994"
+    sha256 "a209c647de5baa8a5aaecd4cd697fa26e3a85b18adc72abf04ebef6b63ebdf4d"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
