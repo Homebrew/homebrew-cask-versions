27fa822b488f050c9b2afa5d73c91fcf5639d919
Michael Cho <cho-m@tuta.io>
2021-06-23 23:44:47 -0700

    Update qgis-ltr from 3.16.7,20210609_154722 to 3.16.8,20210619_044746 (#11255)

--- Casks/qgis-ltr.rb
+++ Casks/qgis-ltr.rb
@@ -1,6 +1,6 @@
 cask "qgis-ltr" do
-  version "3.16.7,20210609_154722"
-  sha256 "305062bc63915ab2430a1dec3bc78c39573b85850574227568d54bfc7e3b26cc"
+  version "3.16.8,20210619_044746"
+  sha256 "9d233bb074e73f6347168f29a97c65622617f993ad137d11b96c785b431300bc"
 
   url "https://qgis.org/downloads/macos/ltr/qgis_ltr_final-#{version.before_comma.dots_to_underscores}_#{version.after_comma}.dmg"
   name "QGIS LTR"
@@ -10,7 +10,7 @@ cask "qgis-ltr" do
   livecheck do
     url "https://qgis.org/downloads/macos/qgis-macos-ltr.sha256sum"
     strategy :page_match do |page|
-      match = page.match(/qgis_ltr_final-(\d+(?:_\d+)*)_(\d+_\d+)\.dmg/i)
+      match = page.match(/qgis_ltr_final[._-]v?(\d+(?:_\d+)+)[._-](\d+_\d+)\.dmg/i)
       "#{match[1].tr("_", ".")},#{match[2]}"
     end
   end
