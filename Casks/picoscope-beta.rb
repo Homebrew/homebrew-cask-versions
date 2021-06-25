ba4630e68730285437f10111e7460f906dd59b5f
Miccal Matthews <miccal.matthews@gmail.com>
2021-06-25 08:48:33 +0800

    Update picoscope-beta.rb

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -2,15 +2,15 @@ cask "picoscope-beta" do
   version "7.0.75.8573"
   sha256 "aaf35bba0b8522f84984f362110b02caf6b52b9c534c779339d5d95e04c32c30"
 
-  url "https://oem.picotech.com/software/p7beta/PicoScope_#{version}.pkg"
+  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
 
   livecheck do
-    url "https://www.picotech.com/downloads/_lightbox/"
+    url "https://oem.picotech.com/p#{version.major}beta/download/"
     strategy :page_match
-    regex(%r{href=.*?/PicoScope-(\d+(?:.\d+)*)\.pkg}i)
+    regex(%r{href=.*?/PicoScope_(\d+(?:\.\d+)*)\.pkg}i)
   end
 
   pkg "PicoScope_#{version}.pkg"
