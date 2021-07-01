ddc9a9daba8d995530031ba12fc86eb9f50da4d7
Bevan Kay <email@bevankay.me>
2021-07-01 11:27:19 +1000

    Update google-chrome-canary from 93.0.4558.0 to 93.0.4561.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "93.0.4558.0"
+  version "93.0.4561.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
