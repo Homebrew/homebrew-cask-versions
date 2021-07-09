c110e87b2a3660ef4df560787d525384dc445bc3
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2021-07-09 23:33:57 +0000

    Add support for ARM64 builds of Android Studio Preview Canary for Apple Silicon (#11377)

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,9 +1,18 @@
 cask "android-studio-preview-canary" do
   version "2021.1.1.3"
-  sha256 "3b4e7786fd64dfa7737fe2ef37b3c3b51ba0dd08f780e645f51ebb210d9c9e7b"
 
-  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
-      verified: "dl.google.com/dl/android/studio/"
+  if Hardware::CPU.intel?
+    sha256 "3b4e7786fd64dfa7737fe2ef37b3c3b51ba0dd08f780e645f51ebb210d9c9e7b"
+
+    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
+        verified: "dl.google.com/dl/android/studio/"
+  else
+    sha256 "c2132b83d41e893f8f1204918a7ee746a73a61efd6b72b0aefab56dc7d807791"
+
+    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac_arm.zip",
+        verified: "dl.google.com/dl/android/studio/"
+  end
+
   name "Android Studio Preview (Canary)"
   desc "Tools for building Android applications"
   homepage "https://developer.android.com/studio/preview/"
