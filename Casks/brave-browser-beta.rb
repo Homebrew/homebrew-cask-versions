073055dbc95fe29865e1d5165a51757dccadcce2
Hannes Braun <hannesbraun@mail.de>
2021-07-13 16:04:07 +0200

    Update brave-browser-beta from 1.27.100.0,127.100 to 1.27.102.0,127.102 (#11391)

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,8 +1,8 @@
 cask "brave-browser-beta" do
-  version "1.27.100.0,127.100"
+  version "1.27.102.0,127.102"
 
   if Hardware::CPU.intel?
-    sha256 "97fdcbe59e7e59facb88948cc8a7d9e3422152b9ad3e29ea9f5eba39ac2b6b5a"
+    sha256 "9b1c638a42d38dca2104af357ecf00a8f734fb0a053bd700b1e012930db84952"
 
     url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta-x64.dmg",
         verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
@@ -12,7 +12,7 @@ cask "brave-browser-beta" do
       strategy :sparkle
     end
   else
-    sha256 "d9a6043265c956d157d93abc010c7c9ccd5d00be73e2557fc0bda789ed14df92"
+    sha256 "24fd7012702cc9420937f9e2b7fd68eb24a15df8097fa1d77cc60555fa30a61d"
 
     url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta-arm64/#{version.after_comma}/Brave-Browser-Beta-arm64.dmg",
         verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
