e1f4968bc95268d1f010d047b813749e141cdfe3
Bevan Kay <email@bevankay.me>
2021-07-21 09:25:00 +1000

    add livecheck

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -3,10 +3,16 @@ cask "corretto8" do
   sha256 "4a9f0c6d1ddb04a68f5ccd22e0f15893f5bbdae66c82b16b3599e87db23316cb"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-8-x64-macos-jdk.pkg"
   name "Amazon Corretto JDK"
   desc "OpenJDK distribution from Amazon"
   homepage "https://corretto.aws/"
+
+  livecheck do
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-x64\.pkg}i, 1]
+    end
+  end
 
   pkg "amazon-corretto-#{version}-macosx-x64.pkg"
 
