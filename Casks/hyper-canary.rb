4581fe7f81cfcc64c162340e8ea5b881ed816444
Bevan Kay <email@bevankay.me>
2021-07-20 11:50:20 +1000

    Update hyper-canary from 3.1.0-canary.6 to 3.1.1 (#11434)

--- Casks/hyper-canary.rb
+++ Casks/hyper-canary.rb
@@ -1,12 +1,12 @@
 cask "hyper-canary" do
-  version "3.1.0-canary.6"
+  version "3.1.1"
 
   if Hardware::CPU.intel?
-    sha256 "033735bec4cef2796e5c40d84b186843941347a768fdbb3d1323c9bc4379ebd3"
+    sha256 "3d6a3b5b7e95fdacd63cb80973d411b9f5314dd26de2accdd318acecbb53c60d"
     url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-x64.zip",
         verified: "github.com/vercel/hyper/"
   else
-    sha256 "d2cd7409dbc1454bc5c67454228572f8c27ef9e5434893f8d1bc81b9613441e0"
+    sha256 "1b82a131d27aa77f722d5d4792c03ddb4d50795d7fddc8c367c1cf80e1dd7074"
     url "https://github.com/vercel/hyper/releases/download/v#{version}/Hyper-#{version}-mac-arm64.zip",
         verified: "github.com/vercel/hyper/"
   end
