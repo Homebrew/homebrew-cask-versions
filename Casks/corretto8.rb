573f12a9d4a3fca26ee06f4f97dd99c912e8c16e
Ben Taylor <61255184+benty-amzn@users.noreply.github.com>
2021-07-20 18:46:18 -0700

    Update corretto8 from 8.292.10.1 to 8.302.08.1 (#11440)

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -1,12 +1,18 @@
 cask "corretto8" do
-  version "8.292.10.1"
-  sha256 "35d07818f31a3636ccee6510f2dfac667f36d27a4d2e024b60a25a475a3ad0f0"
+  version "8.302.08.1"
+  sha256 "4a9f0c6d1ddb04a68f5ccd22e0f15893f5bbdae66c82b16b3599e87db23316cb"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-8-x64-macos-jdk.pkg"
   name "Amazon Corretto JDK"
   desc "OpenJDK distribution from Amazon"
   homepage "https://corretto.aws/"
+
+  livecheck do
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-x64\.pkg}i, 1]
+    end
+  end
 
   pkg "amazon-corretto-#{version}-macosx-x64.pkg"
 
