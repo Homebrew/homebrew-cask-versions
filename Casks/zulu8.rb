cea86ec44b5c1dd3e9c421fcc0ea7b454292c40e
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2021-07-22 13:10:04 -0500

    Update zulu8.rb

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -4,7 +4,6 @@ cask "zulu8" do
     version "8.0.292,8.54.0.21-ca"
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
-    
   else
     version "8.0.292,8.54.0.21-ca"
     sha256 "78bfc25d73568f94a2f3f46be4ef2f4723250f8a7b3691e68f33528e7517d2d4"
