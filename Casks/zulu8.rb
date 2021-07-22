fe4d07a2a0290a6e8936790dae5993346c822c10
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2021-07-22 11:52:31 -0500

    Update zulu8.rb
    Update check version for Intel and Arm architecture builds.

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -1,12 +1,14 @@
 cask "zulu8" do
-  version "8.0.292,8.54.0.21-ca"
 
   if Hardware::CPU.intel?
     sha256 "a60a31b33a8386f9b446cad2615e33d0190eb0f18cdf8d6812b74fd6e0f4f716"
+    version "8.0.292,8.54.0.21-ca"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
+    
   else
+    version "8.0.292,8.54.0.21-ca"
     sha256 "78bfc25d73568f94a2f3f46be4ef2f4723250f8a7b3691e68f33528e7517d2d4"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
