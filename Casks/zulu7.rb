0c7fc94a5ef7311ac0dd07cdf85489887789842a
Bevan Kay <email@bevankay.me>
2021-07-23 01:15:48 +1000

    Update zulu7 from 7.0.302,7.46.0.11-ca to 7.0.312,7.48.0.11-ca (#11470)
    * Update zulu7 from 7.0.302,7.46.0.11-ca to 7.0.312,7.48.0.11-ca
    
    * Update zulu7.rb
    
    * Update zulu7.rb

--- Casks/zulu7.rb
+++ Casks/zulu7.rb
@@ -1,11 +1,12 @@
 cask "zulu7" do
-  version "7.0.302,7.46.0.11-ca"
-  sha256 "216f9da2bc32431103bb0a2ad17981aa89e54ae3260fe1c69542fa355fedf88e"
+  version "7.0.312,7.48.0.11-ca"
+  sha256 "97c9bc69f957e76a34aa1d45246c43021ccc2d5cf33e7b777bc448c1018bb4b7"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java Standard Edition Development Kit"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+  desc "OpenJDK distribution from Azul"
+  homepage "https://www.azul.com/products/core/"
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
@@ -15,7 +16,7 @@ cask "zulu7" do
     end
   end
 
-  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 end
