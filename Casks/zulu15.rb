11d792c4ac60dbb9668178511f9c3faaa64fac8e
Bevan Kay <email@bevankay.me>
2021-07-23 01:15:57 +1000

    Update zulu15 from 15.0.3,15.32.15-ca to 15.0.4,15.34.17-ca (#11469)
    * Update zulu15 from 15.0.3,15.32.15-ca to 15.0.4,15.34.17-ca
    
    * Update zulu15.rb
    
    * Update zulu15.rb

--- Casks/zulu15.rb
+++ Casks/zulu15.rb
@@ -1,20 +1,20 @@
 cask "zulu15" do
-  version "15.0.3,15.32.15-ca"
+  version "15.0.4,15.34.17-ca"
 
   if Hardware::CPU.intel?
-    sha256 "e061cf2509fcb2dbe135a553cbc3f36704e63ab6418c7429bbc138664ada33c0"
+    sha256 "d19f580d9197626dab0b242e9f4a4c1b88a3a9d55d18ac97474f73744c34bd79"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
-    sha256 "b3ef089a99b8ea49efe2e0665493b161970b7b6554611a126586a7882acef605"
+    sha256 "0ce1eb4c2013da40bafe79becda9a3d9d994a3eab9ce4010f5f9270f71a16a81"
 
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   end
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "Zulu OpenJDK 15"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+  homepage "https://www.azul.com/products/core/"
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
@@ -26,7 +26,7 @@ cask "zulu15" do
 
   depends_on macos: ">= :sierra"
 
-  pkg "Double-Click to Install Zulu #{version.major}.pkg"
+  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 end
