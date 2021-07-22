a54989cc74374591656722e23067b53eb37321e5
Bevan Kay <email@bevankay.me>
2021-07-22 20:56:19 +1000

    Update zulu7.rb

--- Casks/zulu7.rb
+++ Casks/zulu7.rb
@@ -5,7 +5,7 @@ cask "zulu7" do
   url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java Standard Edition Development Kit"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+  homepage "https://www.azul.com/products/core/"
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
