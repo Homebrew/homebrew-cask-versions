a0e5a4f1ba0988fb77d2003295597b5801e1b02d
Bevan Kay <email@bevankay.me>
2021-07-22 20:56:00 +1000

    Update zulu15.rb

--- Casks/zulu15.rb
+++ Casks/zulu15.rb
@@ -14,7 +14,7 @@ cask "zulu15" do
   end
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "Zulu OpenJDK 15"
-  homepage "https://www.azul.com/downloads/zulu/zulu-mac/"
+  homepage "https://www.azul.com/products/core/"
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos"
