1637e3c6d47c1092745f17de404ad968fa1fead3
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2021-07-22 18:06:39 -0500

    Update zulu8.rb

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -2,6 +2,7 @@ cask "zulu8" do
   if Hardware::CPU.intel?
     version "8.0.302,8.56.0.21-ca"
     sha256 "4e667ac2b946d8890a0d9ffe2168e1ad1ccb3f82c3683ccd3f10ce5349c874a7"
+    
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
     
@@ -15,6 +16,7 @@ cask "zulu8" do
   else
     version "8.0.302,8.56.0.23-ca"
     sha256 "ee6c3ceaa3ef384c322c50b64feeaf0e7a9b54895300a4935b50a8e5df22e858"
+    
     url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
     
