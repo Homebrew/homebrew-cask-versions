6bb01909074d882d969ad87b6223d606a309b6b5
Prasannajit Acharya <prasannajit.acharya.kanhu@gmail.com>
2021-07-22 02:50:18 +0000

    Update android-studio-preview-canary from 2021.1.1.3 to 2021.1.1.4 (#11452)

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,13 +1,13 @@
 cask "android-studio-preview-canary" do
-  version "2021.1.1.3"
+  version "2021.1.1.4"
 
   if Hardware::CPU.intel?
-    sha256 "3b4e7786fd64dfa7737fe2ef37b3c3b51ba0dd08f780e645f51ebb210d9c9e7b"
+    sha256 "258d390de1f11486257e8a584c0ae6ebc8967dd0b2d5b1a58dbb9018788e0c74"
 
     url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
         verified: "dl.google.com/dl/android/studio/"
   else
-    sha256 "c2132b83d41e893f8f1204918a7ee746a73a61efd6b72b0aefab56dc7d807791"
+    sha256 "34faedbcf88b82b6aa17f97e2ba0de65a33b093b8db58aa965bcde408f64813f"
 
     url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac_arm.zip",
         verified: "dl.google.com/dl/android/studio/"
