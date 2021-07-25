412c2b10e51986e3431adeaea9b327ce56a19860
Bevan Kay <email@bevankay.me>
2021-07-25 14:06:17 +1000

    Update java-beta from 17,31 to 17,32

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,12 @@
 cask "java-beta" do
-  version "17,31"
+  version "17,32"
 
   if Hardware::CPU.intel?
-    sha256 "0460c9fd5bbcfdbe72e603826fdbc7ad4771ac3b9cc88ff9a0a13aa246b5666a"
+    sha256 "2d32701522bcb0b66d2344eb8280c802f04e220ff63c859ede16d9a916770a74"
 
     url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-x64_bin.tar.gz"
   else
-    sha256 "bf0acde8615ad1bef9c3696128531bec13bcdc3c28baca687bab6902c4b5c7f7"
+    sha256 "69323ca39c00cc4de350ebb85956022f9f5b5b3e6bc9133512247c6dd388342c"
 
     url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-aarch64_bin.tar.gz"
   end
