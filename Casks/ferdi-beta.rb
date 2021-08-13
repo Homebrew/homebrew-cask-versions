5f4994244c90d18fca67ad72ef15d1ad3050108f
miccal <miccal.matthews@gmail.com>
2021-08-13 08:42:52 +0800

    Update ferdi-beta from 5.6.0-beta.8 to 5.6.1-beta.1

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,13 +1,13 @@
 cask "ferdi-beta" do
-  version "5.6.0-beta.8"
+  version "5.6.1-beta.1"
 
   if Hardware::CPU.intel?
-    sha256 "9e9c48771cbfda22894dad3434c2e5951d6d4219093af1bf6dc700f89fdb18d1"
+    sha256 "f1d7773f4b08b8acf00c49957484c2042ad2f8614233fc8aed3210a385b7522b"
 
     url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg",
         verified: "github.com/getferdi/ferdi/"
   else
-    sha256 "28a4d16638589bdf69deaae48694bae3bead50bce0fd83daee1aabe9fca12d8a"
+    sha256 "7811447b3cf52340a0bfc3823c328f51a57d756963c14babbc7365fc179ecdb1"
 
     url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}-arm64.dmg",
         verified: "github.com/getferdi/ferdi/"
