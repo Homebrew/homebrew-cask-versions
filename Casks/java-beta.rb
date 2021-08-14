d36251ab8b09ccf6fe42a45dfa33dc0860e4d8f0
miccal <miccal.matthews@gmail.com>
2021-08-14 23:10:58 +0800

    Update java-beta from 17,33 to 18,10

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,12 @@
 cask "java-beta" do
-  version "17,33"
+  version "18,10"
 
   if Hardware::CPU.intel?
-    sha256 "5d2497d7d9e82eb7d5e99ae5852878e1e919dbed8eae31706194785d6f0b6f26"
+    sha256 "8bc1fcf92a67511706f4943c1f8bae8f473c18ec0ec84eabb98459d55f87e344"
 
     url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-x64_bin.tar.gz"
   else
-    sha256 "db40090e7d9f20a2b71cd396393cbdd4e0ef6bbb90cb0f119701c006fba82744"
+    sha256 "60c777f52e9e0db83bca2a8e34f14eb6f055fd318db3ec2b0a206d720137ae0d"
 
     url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-aarch64_bin.tar.gz"
   end
