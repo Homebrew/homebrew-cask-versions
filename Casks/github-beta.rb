18acc1d452c24cf383862f89e641282f862047b7
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2021-08-17 21:49:21 +0200

    Update github-beta from 2.9.1-beta2-73965846 to 2.9.1-beta4-6c2926ea

--- Casks/github-beta.rb
+++ Casks/github-beta.rb
@@ -1,13 +1,13 @@
 cask "github-beta" do
-  version "2.9.1-beta2-73965846"
+  version "2.9.1-beta4-6c2926ea"
 
   if Hardware::CPU.intel?
-    sha256 "881d26b7569cf796cbcaee05b6d519a9bd43fc409b05bc56b4c29028c4b78dfb"
+    sha256 "39f22e094310d3bbb872e657127ba53dd559a206049adcbf1b176179b0fb5b81"
 
     url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-x64.zip",
         verified: "desktop.githubusercontent.com/"
   else
-    sha256 "5a56a56e609ded0288cb6a4c7436f67131c4821eb75f2c9b5890001584086125"
+    sha256 "b4cf7d77135f60c28d8048a8771a8cc98d07e5218140f60d6a4883bd88ccb838"
 
     url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-arm64.zip",
         verified: "desktop.githubusercontent.com/"
