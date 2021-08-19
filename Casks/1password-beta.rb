dfe69cdc2998376aed105e958237326230ab56a6
Miccal Matthews <miccal.matthews@gmail.com>
2021-08-19 08:06:38 +0800

    Update 1password-beta.rb (#11696)

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,27 +1,34 @@
 cask "1password-beta" do
-  version "7.8.8.BETA-0"
-  sha256 "203ee01358269bec8efade30530aee8dc1140259ebff79363bed57f808c0d3bf"
+  version "8.2.0-56.BETA"
 
-  url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
+  if Hardware::CPU.intel?
+    sha256 "5569970e5ea9b4fb2ed1ce2a0de226612e52e32a285c35720ce9eda68af150a1"
+    url "https://downloads.1password.com/mac/1Password-#{version}-x86_64.zip"
+  else
+    sha256 "fd647e20d5c9506cec27eba6dec6b317fb77efd7a6b17da293c89da51a415417"
+    url "https://downloads.1password.com/mac/1Password-#{version}-aarch64.zip"
+  end
+
   name "1Password"
   desc "Password manager that keeps all passwords secure behind one password"
   homepage "https://1password.com/"
 
   livecheck do
     url "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
-    regex(%r{href=.*?/1Password[._-]?v?(\d+(?:\.\d+)*(?:[._-]BETA[._-]?\d*)?)\.pkg}i)
+    regex(%r{href=.*?/1Password[._-]?v?(\d+(?:.\d+)*(?:[._-]BETA))[._-]?\$ARCH\.zip}i)
   end
 
   auto_updates true
   conflicts_with cask: "1password"
   depends_on macos: ">= :high_sierra"
 
-  app "1Password #{version.major}.app"
+  app "1Password.app"
 
   zap trash: [
-    "~/Library/Application Scripts/2BUA8C4S2C.com.agilebits.onepassword-osx-helper",
-    "~/Library/Containers/2BUA8C4S2C.com.agilebits.onepassword-osx-helper",
-    "~/Library/Containers/com.agilebits.onepassword-osx",
-    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+    "~/Library/Application Scripts/2BUA8C4S2C.com.1password.browser-helper",
+    "~/Library/Application Scripts/2BUA8C4S2C.com.1password.1password",
+    "~/Library/Containers/2BUA8C4S2C.com.1password.browser-helper",
+    "~/Library/Containers/com.1password.1password",
+    "~/Library/Group Containers/2BUA8C4S2C.com.1password",
   ]
 end
