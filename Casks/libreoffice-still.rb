d4af1f19f0593a74043ae2191cb4f441897cdbde
Bevan Kay <email@bevankay.me>
2021-08-19 23:51:36 +1000

    Update libreoffice-still from 7.0.6 to 7.1.5

--- Casks/libreoffice-still.rb
+++ Casks/libreoffice-still.rb
@@ -1,6 +1,6 @@
 cask "libreoffice-still" do
-  version "7.0.6"
-  sha256 "cd99c75b4becb853dbd37a501300b07f1f39b33dc0c1e418c346744829fc7738"
+  version "7.1.5"
+  sha256 "6b3e5741ba787f70c65411d7e1128b24d621c46e883b1fc61530bc10a38a18c5"
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg",
       verified: "download.documentfoundation.org/"
