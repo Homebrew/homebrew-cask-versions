8ba022e0e46d18a0f3e7cd1754a218982c6a0df7
miccal <miccal.matthews@gmail.com>
2021-08-21 13:47:03 +0800

    Update microsoft-edge-beta from 93.0.961.18 to 93.0.961.27

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -1,13 +1,13 @@
 cask "microsoft-edge-beta" do
-  version "93.0.961.18"
+  version "93.0.961.27"
 
   if Hardware::CPU.intel?
-    sha256 "1b101e99fc64b62a9780c305419954a75a40ecd254061177bb3dbd87308e932b"
+    sha256 "8bab34c27720a3ba7e7133ffb109a6e82331c063ad1b87a36cb4f59cd98dce6d"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
   else
-    sha256 "de717fe30679d1cb7e7e60ea882e7678aee710cf15dd9b1a552dc0ade01231de"
+    sha256 "0af26ab3cd2565c67d653343991318bd6a586899bb789775b31414d113e38458"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
