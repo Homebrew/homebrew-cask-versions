eb8c44611c4214c696ae5b20cb0c231aa341d8a0
Jacob Ford <j@cobford.com>
2021-08-22 19:14:29 -0400

    Add casks basics

---
+++ Casks/mimestream.rb
@@ -0,0 +1,11 @@
+cask "mimestream" do
+  version "0.26.2"
+  sha256 "0644cbb3d97b8b9a00bc31013a559a607e52a102e1d3e8a72b7f2dfe76583cd3"
+
+  url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg"
+  name "mimestream"
+  desc "Native macOS email client for Gmail"
+  homepage "https://mimestream.com"
+
+  app "Mimestream.app"
+end
