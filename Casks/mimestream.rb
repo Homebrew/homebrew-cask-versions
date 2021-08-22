7ecb730ca1ba77fc9046b040703130264a80330e
Jacob Ford <j@cobford.com>
2021-08-22 19:35:55 -0400

    Verify download URL

--- Casks/mimestream.rb
+++ Casks/mimestream.rb
@@ -2,7 +2,8 @@ cask "mimestream" do
   version "0.26.2"
   sha256 "0644cbb3d97b8b9a00bc31013a559a607e52a102e1d3e8a72b7f2dfe76583cd3"
 
-  url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg"
+  url "https://storage.googleapis.com/mimestream-releases/Mimestream_#{version}.dmg",
+    verified: "storage.googleapis.com/mimestream-releases/"
   name "mimestream"
   desc "Native app email client for Gmail"
   homepage "https://mimestream.com/"
