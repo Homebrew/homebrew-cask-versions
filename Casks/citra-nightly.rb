7286cc65eaf1b567d7319f9ccbc528706c9a2e72
miccal <miccal.matthews@gmail.com>
2021-08-23 09:46:12 +0800

    Update citra-nightly from 1720,20210801-c40871f to 1721,20210823-89410c1

--- Casks/citra-nightly.rb
+++ Casks/citra-nightly.rb
@@ -1,6 +1,6 @@
 cask "citra-nightly" do
-  version "1720,20210801-c40871f"
-  sha256 "a6eeec420b28ed907257b1dc6f18e0cdf2a170c30743bd8cae8615af1c0e4988"
+  version "1721,20210823-89410c1"
+  sha256 "e47f03d19aefa62411b0dd24bb552e4def18372bef29c889cf807e899278ff8b"
 
   url "https://github.com/citra-emu/citra-nightly/releases/download/nightly-#{version.before_comma}/citra-osx-#{version.after_comma}.tar.gz",
       verified: "github.com/citra-emu/citra-nightly/"
