90756a7e8a7198378fb513dc480028008a6f6a2b
Davlatjon Shavkatov <opensource@dalisoft.uz>
2021-08-27 16:07:35 +0500

    feat(adguard-vpn): add new cask

---
+++ Casks/adguard-vpn-beta.rb
@@ -0,0 +1,16 @@
+cask "adguard-vpn-beta" do
+  version :latest
+  sha256 :no_check
+
+  url "https://static.adguard-vpn.com/mac/release/AdGuardVPN.dmg"
+  name "Adguard VPN"
+  homepage "https://adguard.com/"
+
+  pkg "AdGuard VPN.pkg"
+
+  uninstall pkgutil: "com.adguard.mac.adguard-vpn-pkg"
+
+  zap trash: [
+    "/Library/Application Support/com.adguard.mac.vpn"
+  ]
+end
