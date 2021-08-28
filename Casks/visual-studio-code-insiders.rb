784d7dd0bc909aaf3d654f96324729a260180e63
Bevan Kay <email@bevankay.me>
2021-08-28 14:49:08 +1000

    Update visual-studio-code-insiders from 1.60.0,6a2daec767703dad42461333fbf8666aa481a32b to 1.60.0,8dbad73de575e47e127c8b1a4324f2b61257fbdc (#11792)

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,13 +1,13 @@
 cask "visual-studio-code-insiders" do
-  version "1.60.0,6a2daec767703dad42461333fbf8666aa481a32b"
+  version "1.60.0,8dbad73de575e47e127c8b1a4324f2b61257fbdc"
 
   if Hardware::CPU.intel?
-    sha256 "7fb4c0ef86709e083cdef6c522024ba6db44bb13a2e09d45c67b14d190832c02"
+    sha256 "281ada435b3c81a82bd1b04b1d498910a542efde4c770767a09d1ab0a70f41f7"
 
     url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin.zip",
         verified: "az764295.vo.msecnd.net/insider/"
   else
-    sha256 "2fe8d9ae3437a04b2a2aa32007ee79bc297f63271597f909cdcfd962baf9740c"
+    sha256 "04d6eb780a66e36ec440bd43d65bc006db76a6e60ea59d83a631b9b75f3f5ac6"
 
     url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-darwin-arm64.zip",
         verified: "az764295.vo.msecnd.net/insider/"
