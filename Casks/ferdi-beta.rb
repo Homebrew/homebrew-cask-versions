1bd24fd0549cea70c6f436a49040f825e3df5ac0
Vijay Raghavan Aravamudhan <vraravam@users.noreply.github.com>
2021-09-01 16:13:40 +0530

    Upgraded 'ferdi' to '5.6.1-beta.3' (#11827)

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,13 +1,13 @@
 cask "ferdi-beta" do
-  version "5.6.1-beta.2"
+  version "5.6.1-beta.3"
 
   if Hardware::CPU.intel?
-    sha256 "b5d9c01cfcf0e7084fc015b400032f715cfafe3dc38ee6b23ffc70866fc767ec"
+    sha256 "6a91b62f6f19c1470581b99c505643b5e2e77ddfe7ef2bfaa2d19faba81f00c4"
 
     url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg",
         verified: "github.com/getferdi/ferdi/"
   else
-    sha256 "2eabc475ad095dd485f82f39b067a9f915b6165841a75bc7cab2bfe1b58bb265"
+    sha256 "a289acf58d8217b79f4c40824110c01fdb74e6eaa1441f5da166d6bdeb3581c6"
 
     url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}-arm64.dmg",
         verified: "github.com/getferdi/ferdi/"
