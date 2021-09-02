6113a15f74aa34d492b871c40fc811368987769b
Bevan Kay <email@bevankay.me>
2021-09-02 21:21:54 +1000

    fix style

--- Casks/whatsapp-beta.rb
+++ Casks/whatsapp-beta.rb
@@ -9,22 +9,21 @@ cask "whatsapp-beta" do
   desc "Desktop client for WhatsApp"
   homepage "https://www.whatsapp.com/"
 
+  livecheck do
+    url "https://web.whatsapp.com/desktop-beta/mac/releases"
+    regex(/release-(\d+(?:\.\d+)*)\.zip/i)
+  end
+
   app "WhatsApp.app"
 
-  livecheck do
-      url "https://web.whatsapp.com/desktop-beta/mac/releases"
-      strategy :page_match
-      regex(/release-(\d+(?:\.\d+)*)\.zip/i)
-    end
-
-    zap trash: [
-        "~/Library/Application Support/WhatsApp",
-        "~/Library/Application Support/WhatsApp.ShipIt",
-        "~/Library/Caches/WhatsApp",
-        "~/Library/Caches/WhatsApp.ShipIt",
-        "~/Library/Preferences/ByHost/WhatsApp.ShipIt.*.plist",
-        "~/Library/Preferences/WhatsApp.plist",
-        "~/Library/Preferences/WhatsApp-Helper.plist",
-        "~/Library/Saved Application State/WhatsApp.savedState",
-      ]
+  zap trash: [
+    "~/Library/Application Support/WhatsApp",
+    "~/Library/Application Support/WhatsApp.ShipIt",
+    "~/Library/Caches/WhatsApp",
+    "~/Library/Caches/WhatsApp.ShipIt",
+    "~/Library/Preferences/ByHost/WhatsApp.ShipIt.*.plist",
+    "~/Library/Preferences/WhatsApp.plist",
+    "~/Library/Preferences/WhatsApp-Helper.plist",
+    "~/Library/Saved Application State/WhatsApp.savedState",
+  ]
 end
