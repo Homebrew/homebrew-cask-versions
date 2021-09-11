be0b5e1055fec3a6af087ae048a399ea1453c49b
Bevan Kay <email@bevankay.me>
2021-09-11 14:04:56 +1000

    add livecheck

--- Casks/affinity-publisher-beta.rb
+++ Casks/affinity-publisher-beta.rb
@@ -3,10 +3,14 @@ cask "affinity-publisher-beta" do
   sha256 "95eda2d699a736cba93b3a0488938a9d336e6920c5c9735703c432b4a3ebfca1"
 
   url "https://beta-downloads.serif.com/download/f6b89099-eace-44b3-8ce7-76ae7b0e036d/affinity-publisher-customer-beta-#{version}.zip"
-  appcast "https://forum.affinity.serif.com/index.php?/forum/57-publisher-beta-on-mac/"
   name "Affinity Publisher"
   desc "Professional desktop publishing software"
   homepage "https://affinity.serif.com/en-us/publisher/"
+
+  livecheck do
+    url "https://forum.affinity.serif.com/index.php?/topic/83296-affinity-range-mac-beta-release-notifications/"
+    regex(/Affinity\s+Publisher\s+\|\s+MacOS.+\|\s+(\d+(?:\.\d+)+)\s+\|/i)
+  end
 
   app "Affinity Publisher Beta.app"
 end
