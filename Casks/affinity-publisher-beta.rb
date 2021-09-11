29867318505cc793fae9f6b041a4db45b5d75715
Jacob Ford <j@cobford.com>
2021-09-11 01:34:38 -0400

    Update affinity-publisher-beta from 1.9.2.1024 to 1.10.2.1156 (#11920)
    * Update affinity-publisher-beta from 1.10.2.1156 to 1.10.2.1156
    
    * Fix name, add desc
    
    * add livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/affinity-publisher-beta.rb
+++ Casks/affinity-publisher-beta.rb
@@ -1,12 +1,16 @@
 cask "affinity-publisher-beta" do
-  version "1.9.2.1024"
-  sha256 "34d39facae3d67311e0d8630134747164ff01d68f9bba6e296b02184b20a78ee"
+  version "1.10.2.1156"
+  sha256 "95eda2d699a736cba93b3a0488938a9d336e6920c5c9735703c432b4a3ebfca1"
 
-  url "https://affinity-beta.s3.amazonaws.com/download/affinity-publisher-customer-beta-#{version}.zip",
-      verified: "affinity-beta.s3.amazonaws.com/"
-  appcast "https://forum.affinity.serif.com/index.php?/forum/57-publisher-beta-on-mac/"
-  name "Serif Affinity Publisher"
+  url "https://beta-downloads.serif.com/download/f6b89099-eace-44b3-8ce7-76ae7b0e036d/affinity-publisher-customer-beta-#{version}.zip"
+  name "Affinity Publisher"
+  desc "Professional desktop publishing software"
   homepage "https://affinity.serif.com/en-us/publisher/"
+
+  livecheck do
+    url "https://forum.affinity.serif.com/index.php?/topic/83296-affinity-range-mac-beta-release-notifications/"
+    regex(/Affinity\s+Publisher\s+\|\s+MacOS.+\|\s+(\d+(?:\.\d+)+)\s+\|/i)
+  end
 
   app "Affinity Publisher Beta.app"
 end
