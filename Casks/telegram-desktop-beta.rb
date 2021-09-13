f9644193329eb93eb095affbba343ab1fa24307a
Yurii Kolesnykov <root@yurikoles.com>
2021-09-13 17:17:51 +0300

    Update telegram-desktop-beta from 2.9.14 to 3.0.2 (#11929)

--- Casks/telegram-desktop-beta.rb
+++ Casks/telegram-desktop-beta.rb
@@ -1,6 +1,6 @@
 cask "telegram-desktop-beta" do
-  version "2.9.14"
-  sha256 "302e392be54742751fb7d2df3d2058774f15111073442b58f6f00d0327bdccc8"
+  version "3.0.2"
+  sha256 "bd717c7cc5ff5519801e0daeed86daf823ed9460b02ed55891e8a64fb4713d17"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.beta.dmg",
       verified: "updates.tdesktop.com/tmac"
