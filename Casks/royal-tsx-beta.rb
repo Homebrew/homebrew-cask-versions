df45227a6814d834a4fe979a162108a4f438fe75
Bevan Kay <email@bevankay.me>
2021-09-20 20:11:00 +1000

    Update royal-tsx-beta from 5.0.5.1 to 5.0.5.2

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,6 +1,6 @@
 cask "royal-tsx-beta" do
-  version "5.0.5.1"
-  sha256 "42e66c26bf9ab52984f011c28721f4dad6de4fbe5115ebe079989bd9034e71e0"
+  version "5.0.5.2"
+  sha256 "ab370d28fa4bfaa32a6f5685f91f8e0116b583201edde67cf9ba9a7eb96636ec"
 
   url "https://royaltsx-v#{version.major}.royalapps.com/updates/royaltsx_#{version}.dmg"
   name "Royal TSX"
