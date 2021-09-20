7a6a7a18c5e6dc1f62e2f92850c75e7d81c4160f
Bevan Kay <email@bevankay.me>
2021-09-20 19:37:23 +1000

    Update java-beta from 18,14 to 18,15

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,12 @@
 cask "java-beta" do
-  version "18,14"
+  version "18,15"
 
   if Hardware::CPU.intel?
-    sha256 "5cfa94c84983bbe22e575d8f059aac12e19855e336681224de00f06cba892ac0"
+    sha256 "a1b538f76390ce89b0bc77da7dd2507e1f257d911efb21f23a932a5a0476f4b1"
 
     url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-x64_bin.tar.gz"
   else
-    sha256 "6427258598e38445228d49925081f14e1fd1b04fcf0eed3711b07fb6aa74bfdb"
+    sha256 "a2c456c9fbacbe26b4c49cef1cbb7268a17c217962ebad575cf2c2f49b5a6edd"
 
     url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-aarch64_bin.tar.gz"
   end
