36660f3312a7049a81cbdf251b0cd2ef610c05da
Hannes Braun <hannesbraun@mail.de>
2021-09-28 00:23:21 +0200

    Update osu-development from 2021.916.0 to 2021.927.0

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,6 +1,6 @@
 cask "osu-development" do
-  version "2021.916.0"
-  sha256 "39d0f54f5aba493c206b6458ade9fe71faaea1c0ad6d097fb58ef1de60aa5386"
+  version "2021.927.0"
+  sha256 "a4ab988b1ffbf80eacd3196103ab64a34705301b7ef7605f21a4c0e16afce22b"
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
   name "osu!lazer"
