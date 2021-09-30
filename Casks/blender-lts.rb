e66e6ef5d669165c501cda3aad737d818e3ab19f
Bevan Kay <email@bevankay.me>
2021-09-30 22:44:30 +1000

    blender-lts: update livecheck, bump version

--- Casks/blender-lts.rb
+++ Casks/blender-lts.rb
@@ -1,16 +1,30 @@
 cask "blender-lts" do
-  version "2.83.13"
-  sha256 "54224c01c859e44ffbfe852e98e21f0c4d237fb87393798773b5d62e9d36ebfc"
+  version "2.93.4"
 
-  url "https://download.blender.org/release/Blender#{version.major_minor}/blender-#{version}-macOS.dmg"
+  if Hardware::CPU.intel?
+    sha256 "9aab9e161e326f02d5ca51b9f77187e931ebc779bd85a604d797c187cd1611f4"
+    url "https://download.blender.org/release/Blender#{version.major_minor}/blender-#{version}-macos-x64.dmg"
+  else
+    sha256 "0478a156829213a683b6be92cd12fe63c4f58fce3d0c2b4b839c157737eb14bf"
+    url "https://download.blender.org/release/Blender#{version.major_minor}/blender-#{version}-macos-arm64.dmg"
+  end
+
   name "Blender"
   desc "Free and open-source 3D creation suite"
   homepage "https://www.blender.org/"
 
   livecheck do
     url "https://www.blender.org/download/lts/"
-    strategy :page_match
-    regex(%r{href=.*?/blender-(\d+(?:\.\d+)*)-macOS\.dmg}i)
+    regex(%r{href=.*?/blender[._-]v?(\d+(?:\.\d+)+)-macOS-x64\.dmg}i)
+    strategy :page_match do |page, regex|
+      minor_version = page[%r{href=["'].*/download/lts/(\d+(?:[.-]\d+)+)/["' >]}i, 1]
+      next [] if minor_version.blank?
+
+      version_page = Homebrew::Livecheck::Strategy.page_content("https://www.blender.org/download/lts/#{minor_version}/")
+      next [] if version_page[:content].blank?
+
+      version_page[:content].scan(regex).flatten
+    end
   end
 
   conflicts_with cask: "blender"
