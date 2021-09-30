f4c81287b7f7fdd81fdad3ceef7d31e85270fc8e
eloyaldea <35566945+eloyaldea@users.noreply.github.com>
2021-09-30 02:54:14 +0200

    Update whatsapp-beta from 2.2138.2 to 2.2139.3 (#12074)

--- Casks/whatsapp-beta.rb
+++ Casks/whatsapp-beta.rb
@@ -1,6 +1,6 @@
 cask "whatsapp-beta" do
-  version "2.2138.2"
-  sha256 "94c5add88ff97fefd0b2d03da3143c7584a7b58a5d80c4caf31ff0aae6f22374"
+  version "2.2139.3"
+  sha256 "f94b8ef84c27d52fb188639993c2e1265c7408b0744525366b5c307ce36df581"
 
   url "https://web.whatsapp.com/desktop/mac/files/release-#{version}.zip"
   name "WhatsApp Beta"
