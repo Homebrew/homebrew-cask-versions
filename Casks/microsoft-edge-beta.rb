514d6d58cfc009f05af0e5e8d8dc314dc2d50d46
Hannes Braun <hannesbraun@mail.de>
2021-10-05 23:45:55 +0200

    Update microsoft-edge-beta from 95.0.1020.9 to 95.0.1020.14

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -1,13 +1,13 @@
 cask "microsoft-edge-beta" do
-  version "95.0.1020.9"
+  version "95.0.1020.14"
 
   if Hardware::CPU.intel?
-    sha256 "dc759ee1bdcf2e8b21e1089209f37adf14e48d2ab2402079c5fc09fdfca312d4"
+    sha256 "f2d486a2c11dd86c37740ef54ef0850ba204b474b56672226d9b498743fcea40"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
   else
-    sha256 "c9dbf96ec7254c85f5430da67ff49ca1e48ae2423811a9930da966540097e9a7"
+    sha256 "b92a4d40403c90581fede21056f4af5ce802a89626d9d9b6d5cd05298a712ae3"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
