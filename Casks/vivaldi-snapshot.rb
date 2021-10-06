0443e9b1573ad63f5fa5e071691110893499eb2e
Hannes Braun <hannesbraun@mail.de>
2021-10-06 10:37:23 +0200

    Update vivaldi-snapshot from 4.3.2439.29 to 4.3.2439.37

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask "vivaldi-snapshot" do
-  version "4.3.2439.29"
-  sha256 "a483b190c8734e31893293a11ae56d3259c0b61ec8ea24015ffc4685dc73d908"
+  version "4.3.2439.37"
+  sha256 "8c3e7826088e973e98e330a131241c19255a84776b1a890f6185d4a95db877d2"
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
