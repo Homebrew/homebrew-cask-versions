fa940858eeeb0a9ea3062b08dc6c736c93da577b
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-11 09:06:37 +0800

    Update kaleidoscope2.rb

--- Casks/kaleidoscope2.rb
+++ Casks/kaleidoscope2.rb
@@ -2,7 +2,7 @@ cask "kaleidoscope2" do
   version "2.4.2,1455:sep-7-2021"
   sha256 "d2ccd4c63781f836f2b5018ba51c286829929d43d74ea6300144e7c7a50f911a"
 
-  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
+  url "https://updates.kaleidoscope.app/v#{version.major}/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
   name "Kaleidoscope v2"
   desc "Spot and merge differences in text and image files or folders"
   homepage "https://www.kaleidoscope.app/"
