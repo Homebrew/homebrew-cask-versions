fe2d2a84d60e63c8c8404b128e602d04d246d96a
Naveen Kumar <naveenk2908@gmail.com>
2021-10-11 05:47:13 +0530

    Add cask `Kaleidoscope.app` `v2`  (#12165)
    * Add cask
    
    * Update kaleidoscope2.rb
    
    * Update kaleidoscope2.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/kaleidoscope2.rb
@@ -0,0 +1,32 @@
+cask "kaleidoscope2" do
+  version "2.4.2,1455:sep-7-2021"
+  sha256 "d2ccd4c63781f836f2b5018ba51c286829929d43d74ea6300144e7c7a50f911a"
+
+  url "https://updates.kaleidoscope.app/v2/prod/Kaleidoscope-#{version.before_comma}-#{version.after_comma.before_colon}-#{version.after_colon}.app.zip"
+  name "Kaleidoscope v2"
+  desc "Spot and merge differences in text and image files or folders"
+  homepage "https://www.kaleidoscope.app/"
+
+  livecheck do
+    url "https://updates.kaleidoscope.app/v#{version.major}/prod/appcast"
+    regex(/Kaleidoscope-(\d+(?:\.\d+)*)-(\d+)-(\w+(?:-\d+)*)\.app\.zip/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}:#{match[2]}" }
+    end
+  end
+
+  auto_updates true
+  conflicts_with cask: "kaleidoscope"
+  depends_on macos: ">= :high_sierra"
+
+  app "Kaleidoscope.app"
+  binary "#{appdir}/Kaleidoscope.app/Contents/Resources/bin/ksdiff"
+
+  zap trash: [
+    "~/Library/Application Support/com.blackpixel.kaleidoscope",
+    "~/Library/Caches/com.blackpixel.kaleidoscope",
+    "~/Library/Caches/com.plausiblelabs.crashreporter.data/com.blackpixel.kaleidoscope",
+    "~/Library/Preferences/com.blackpixel.kaleidoscope.plist",
+    "~/Library/Saved Application State/com.blackpixel.kaleidoscope.savedState",
+  ]
+end
