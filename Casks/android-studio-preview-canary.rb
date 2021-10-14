2f09b43baa88ce8860361ffa6934af1125971c1b
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 07:47:02 +0800

    Update android-studio-preview-canary.rb

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,16 +1,14 @@
 cask "android-studio-preview-canary" do
+  arch = Hardware::CPU.intel? ? "mac" : "mac_arm"
+
   version "2021.2.1.1"
 
+  url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-#{arch}.zip",
+      verified: "dl.google.com/dl/android/studio/"
   if Hardware::CPU.intel?
     sha256 "35f92fb1cea003ab331bfad276a64f2f0a0df52912560b03bf42718da5a9fe32"
-
-    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac.zip",
-        verified: "dl.google.com/dl/android/studio/"
   else
     sha256 "8a7fb0aa9c3e2b68ef1fa7f866d677f979703d40f80d6aa12962f445d54aa458"
-
-    url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-mac_arm.zip",
-        verified: "dl.google.com/dl/android/studio/"
   end
 
   name "Android Studio Preview (Canary)"
@@ -19,7 +17,7 @@ cask "android-studio-preview-canary" do
 
   livecheck do
     url :homepage
-    regex(%r{href=.*?/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]mac\.zip}i)
+    regex(%r{href=.*?/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.zip}i)
   end
 
   conflicts_with cask: "android-studio-preview-beta"
