7115e22691893dfd4cd6853d8d92984d178a0eae
Hannes Braun <hannesbraun@mail.de>
2021-10-15 01:48:13 +0200

    Update microsoft-edge-dev from 96.0.1032.0 to 96.0.1043.1 (#12213)

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,13 +1,13 @@
 cask "microsoft-edge-dev" do
-  version "96.0.1032.0"
+  version "96.0.1043.1"
 
   if Hardware::CPU.intel?
-    sha256 "f5dcb0bdb9425cbd2889499c2d35e7f41e1295b6f6684cf883bb7c6011cd2443"
+    sha256 "faa412b9b6158846515e88923f93b095b78e3133dd542239b3f1cc1d1cd4b47e"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
   else
-    sha256 "277c0f69ec369a29ec567a4e2d466f30203e82e8feb2eeb1143df9822629b7a6"
+    sha256 "ad1297cbecb963144795e35761c89f12984fa872076dc408cf739f99055891b2"
 
     url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
         verified: "officecdn-microsoft-com.akamaized.net/"
