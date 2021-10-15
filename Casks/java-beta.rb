fd9cf3655361011cb5c89ccef0cf499b09e7ea82
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 11:54:13 +0800

    Update java-beta.rb (#12218)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,14 +1,13 @@
 cask "java-beta" do
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+
   version "18,18"
 
+  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
   if Hardware::CPU.intel?
     sha256 "890b3b0d99ba69a2f243cbbf5d5fb036e6c9f96227bbb9679bf1d0e9ae52bbbc"
-
-    url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-x64_bin.tar.gz"
   else
     sha256 "3cacbf0c977a043cead09125e5a75bce005beb4b1fd78aafc91a0afd225fcd9d"
-
-    url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-aarch64_bin.tar.gz"
   end
 
   name "OpenJDK Early Access Java Development Kit"
@@ -18,7 +17,7 @@ cask "java-beta" do
   livecheck do
     url "https://jdk.java.net/#{version.major}/"
     strategy :page_match do |page|
-      match = page.match(/openjdk-(\d+)-ea\+(\d+)_macos-x64_bin\.tar\.gz/i)
+      match = page.match(/openjdk-(\d+)-ea\+(\d+)_macos-#{arch}_bin\.tar\.gz/i)
       "#{match[1]},#{match[2]}"
     end
   end
