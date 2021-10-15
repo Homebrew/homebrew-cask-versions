e3b08d1ea0ba9394a92f2b178cdbe3a91c8bbbc6
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 12:16:56 +0800

    Update brave-browser-beta.rb

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -1,31 +1,25 @@
 cask "brave-browser-beta" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+  folder = Hardware::CPU.intel? ? "beta" : "beta-arm64"
+
   version "1.32.69.0,132.69"
 
+  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.after_comma}/Brave-Browser-Beta-#{arch}.dmg",
+      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
   if Hardware::CPU.intel?
     sha256 "4cc202f448c05e029446c19150cbc96bb1e8cdb89f533dd616744939041ee41d"
-
-    url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta/#{version.after_comma}/Brave-Browser-Beta-x64.dmg",
-        verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
-
-    livecheck do
-      url "https://updates.bravesoftware.com/sparkle/Brave-Browser/beta/appcast.xml"
-      strategy :sparkle
-    end
   else
     sha256 "d3f9dbbf4cf4c5b0cce786c2bdb602733b0247a2ca9232be1dd2ba30673bcc66"
-
-    url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/beta-arm64/#{version.after_comma}/Brave-Browser-Beta-arm64.dmg",
-        verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
-
-    livecheck do
-      url "https://updates.bravesoftware.com/sparkle/Brave-Browser/beta-arm64/appcast.xml"
-      strategy :sparkle
-    end
   end
 
   name "Brave Beta"
   desc "Web browser focusing on privacy"
   homepage "https://brave.com/download-beta/"
+
+  livecheck do
+    url "https://updates.bravesoftware.com/sparkle/Brave-Browser/#{folder}/appcast.xml"
+    strategy :sparkle
+  end
 
   auto_updates true
 
