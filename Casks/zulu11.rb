0ca5e6eb97bcb65978bf656c7f84bc339424d845
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 21:15:59 +0800

    zulu11: various updates (#12229)
    * Update zulu11.rb
    
    * Update zulu11.rb
    
    * Update zulu11.rb

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -1,16 +1,15 @@
 cask "zulu11" do
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  choice = Hardware::CPU.intel? ? "x86" : "arm"
+
   version "11.0.12,11.50.19-ca"
 
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_#{arch}.dmg",
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   if Hardware::CPU.intel?
     sha256 "4820c5ecbdfe199398aa361864ef9812a8af15c221540f8016d05d70fde6db94"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
     sha256 "6c8cbf1a685d04348e92e7c104987fdfe40688b3f51fe98056a5570cec0564e8"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   end
 
   name "Azul Zulu Java Standard Edition Development Kit"
@@ -18,10 +17,12 @@ cask "zulu11" do
   homepage "https://www.azul.com/products/core/"
 
   livecheck do
-    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&javafx=false"
-    regex(/url":"https:.*zulu(.*)-jdk(.*)-macos/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| match[1] + "," + match[0] }
+    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
+    strategy :page_match do |page|
+      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
     end
   end
 
