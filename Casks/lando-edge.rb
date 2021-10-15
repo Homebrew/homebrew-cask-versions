6a512874a6cd502d88d5a5b654eefcf23cfe1aaf
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 11:18:38 +0800

    Update lando-edge.rb

--- Casks/lando-edge.rb
+++ Casks/lando-edge.rb
@@ -1,16 +1,14 @@
 cask "lando-edge" do
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "3.4.3"
 
+  url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
+      verified: "github.com/lando/lando/"
   if Hardware::CPU.intel?
     sha256 "5e8527125dc29e54a5353dab141836830fdfcd11fe2f78ac75b7d60da89caee4"
-
-    url "https://github.com/lando/lando/releases/download/v#{version}/lando-x64-v#{version}.dmg",
-        verified: "github.com/lando/lando/"
   else
     sha256 "7b2a7219c8dcb08f6494262754fcdedc782ecc2bb6b51aac32f437109ce12647"
-
-    url "https://github.com/lando/lando/releases/download/v#{version}/lando-arm64-v#{version}.dmg",
-        verified: "github.com/lando/lando/"
   end
 
   name "Lando Edge"
