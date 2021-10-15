1145ed2cd4b93afa730a57c9019980971407c344
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 20:03:56 +0800

    zulu15: various updates (#12227)
    * Update zulu15.rb
    
    * Update zulu15.rb
    
    * Update zulu15.rb
    
    * Update zulu15.rb
    
    * update livecheck
    
    * update livecheck
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/zulu15.rb
+++ Casks/zulu15.rb
@@ -1,25 +1,27 @@
 cask "zulu15" do
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  choice = Hardware::CPU.intel? ? "x86" : "arm"
+
   version "15.0.4,15.34.17-ca"
 
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_#{arch}.dmg",
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   if Hardware::CPU.intel?
     sha256 "d19f580d9197626dab0b242e9f4a4c1b88a3a9d55d18ac97474f73744c34bd79"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_x64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   else
     sha256 "0ce1eb4c2013da40bafe79becda9a3d9d994a3eab9ce4010f5f9270f71a16a81"
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_aarch64.dmg",
-        referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   end
+
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "Zulu OpenJDK 15"
   homepage "https://www.azul.com/products/core/"
 
   livecheck do
-    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&ext=dmg&os=macos&javafx=false"
+    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
     strategy :page_match do |page|
-      match = page.match(%r{url"\s*:\s*"https:.*?/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macos}i)
+      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+      next if match.blank?
+
       "#{match[2]},#{match[1]}"
     end
   end
