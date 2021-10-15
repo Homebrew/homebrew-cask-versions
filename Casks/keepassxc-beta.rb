1d73a718bf8b96a9c378981c792914616f06991d
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-15 12:41:36 +0800

    Update keepassxc-beta.rb (#12220)

--- Casks/keepassxc-beta.rb
+++ Casks/keepassxc-beta.rb
@@ -1,24 +1,19 @@
 cask "keepassxc-beta" do
+  arch = Hardware::CPU.intel? ? "x86_64" : "arm64"
+
   version "2.6.6"
 
+  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-#{arch}.dmg",
+      verified: "github.com/keepassxreboot/keepassxc/"
   if Hardware::CPU.intel?
-    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-x86_64.dmg",
-        verified: "github.com/keepassxreboot/keepassxc/"
     sha256 "ab96033c16459de5a95e1f9e5864a5bd8cc47b4f3dee2c68ede6199dd44286ec"
   else
-    url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-arm64.dmg",
-        verified: "github.com/keepassxreboot/keepassxc/"
     sha256 "9dc121bb08f5b46186930ac9ba189553cec2c2ce9688df466e9ce1d9d75fe8c5"
   end
 
   name "KeePassXC"
   desc "Password manager app"
   homepage "https://keepassxc.org/"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   conflicts_with cask: "keepassxc"
   depends_on macos: ">= :high_sierra"
