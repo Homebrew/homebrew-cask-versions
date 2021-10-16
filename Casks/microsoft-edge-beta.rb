fa2dd33b35b947881ea3a639134e7ad5f1c0820a
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 15:12:40 +0800

    Update microsoft-edge-beta.rb (#12240)

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -1,16 +1,15 @@
 cask "microsoft-edge-beta" do
+  folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
+  linkid = Hardware::CPU.intel? ? "2069439" : "2099618"
+
   version "95.0.1020.20"
 
+  url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/"
   if Hardware::CPU.intel?
     sha256 "28d6e980c07e50d8f3d941af56fd63a8f6748294c971676ea1cf385f9ef69d29"
-
-    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
-        verified: "officecdn-microsoft-com.akamaized.net/"
   else
     sha256 "756580eb8e32b69bc16f960818d4a8e79a84f476e9e7cc23bd017c22ccc00a46"
-
-    url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
-        verified: "officecdn-microsoft-com.akamaized.net/"
   end
 
   name "Microsoft Edge Beta"
@@ -18,7 +17,7 @@ cask "microsoft-edge-beta" do
   homepage "https://www.microsoftedgeinsider.com/"
 
   livecheck do
-    url "https://go.microsoft.com/fwlink/?linkid=2069439"
+    url "https://go.microsoft.com/fwlink/?linkid=#{linkid}"
     strategy :header_match
   end
 
