d542f0fed6ae6e42f62cf55e27d3c5082fee7e83
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 15:12:28 +0800

    Update microsoft-edge-dev.rb (#12239)

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -1,16 +1,15 @@
 cask "microsoft-edge-dev" do
+  folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
+  linkid = Hardware::CPU.intel? ? "2069340" : "2099619"
+
   version "96.0.1043.1"
 
+  url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
+      verified: "officecdn-microsoft-com.akamaized.net/"
   if Hardware::CPU.intel?
     sha256 "faa412b9b6158846515e88923f93b095b78e3133dd542239b3f1cc1d1cd4b47e"
-
-    url "https://officecdn-microsoft-com.akamaized.net/pr/C1297A47-86C4-4C1F-97FA-950631F94777/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
-        verified: "officecdn-microsoft-com.akamaized.net/"
   else
     sha256 "ad1297cbecb963144795e35761c89f12984fa872076dc408cf739f99055891b2"
-
-    url "https://officecdn-microsoft-com.akamaized.net/pr/03adf619-38c6-4249-95ff-4a01c0ffc962/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
-        verified: "officecdn-microsoft-com.akamaized.net/"
   end
 
   name "Microsoft Edge Dev"
@@ -18,7 +17,7 @@ cask "microsoft-edge-dev" do
   homepage "https://www.microsoftedgeinsider.com/"
 
   livecheck do
-    url "https://go.microsoft.com/fwlink/?linkid=2069340"
+    url "https://go.microsoft.com/fwlink/?linkid=#{linkid}"
     strategy :header_match
   end
 
