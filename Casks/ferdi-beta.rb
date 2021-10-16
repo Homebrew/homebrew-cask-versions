324a6f5e50a948e9c70a3d55270fe3c748e2ca94
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-16 14:16:39 +0800

    Update ferdi-beta.rb

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,16 +1,14 @@
 cask "ferdi-beta" do
+  arch = Hardware::CPU.intel? ? "" : "-arm64"
+
   version "5.6.3-beta.1"
 
+  url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
+      verified: "github.com/getferdi/ferdi/"
   if Hardware::CPU.intel?
     sha256 "4825fbbcf4a7016f8722ea476a3d521d7ac88fcb744f7f899f61f07cfdf554d8"
-
-    url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}.dmg",
-        verified: "github.com/getferdi/ferdi/"
   else
     sha256 "92a1c2e6d4015395be43b2261fa1e06f9074c0252c57f338b997dcccf0091ba8"
-
-    url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}-arm64.dmg",
-        verified: "github.com/getferdi/ferdi/"
   end
 
   name "Ferdi"
@@ -18,9 +16,9 @@ cask "ferdi-beta" do
   homepage "https://getferdi.com/"
 
   livecheck do
-    url "https://github.com/getferdi/ferdi/releases/"
-    strategy :page_match
-    regex(/Ferdi[._-](\d+(?:\.\d+)*((?:[._-]beta)?)*([._-]\d+)?)\.dmg/i)
+    url :url
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*((?:[._-]beta)?)*([._-]\d+)?)$/i)
   end
 
   auto_updates true
