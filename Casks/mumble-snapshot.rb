254f17fcbfb010e25c38f197f346daab4f8ffeb9
JuniorJPDJ <github.com@juniorjpdj.pl>
2021-10-18 01:48:57 +0200

    feat: add mumble-snapshot (#12243)
    * feat: add mumble-snapshot
    
    * Update mumble-snapshot.rb
    
    * Update mumble-snapshot.rb
    
    * Update mumble-snapshot.rb
    
    * Update mumble-snapshot.rb
    
    * zap
    
    * Update mumble-snapshot.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/mumble-snapshot.rb
@@ -0,0 +1,29 @@
+cask "mumble-snapshot" do
+  version "1.4.0_2021-09-04_g37ee67716"
+  sha256 "d66818e6e7f6775da70fc5fbafbe81a9f77b4a95282c3e3dfff65aca4db6cfb7"
+
+  url "https://dl.mumble.info/snapshot/mumble_client-#{version.tr("_", "~")}~snapshot.dmg"
+  name "Mumble Snapshot"
+  desc "Open-source, low-latency, high quality voice chat software for gaming"
+  homepage "https://mumble.info/"
+
+  livecheck do
+    url "https://dl.mumble.info/latest/snapshot/client-macos-x64"
+    strategy :header_match do |headers|
+      headers["content-disposition"][/mumble[._-]client[._-](.*)~snapshot\.dmg/i, 1].tr("~", "_")
+    end
+  end
+
+  conflicts_with cask: "mumble"
+  depends_on macos: ">= :high_sierra"
+
+  app "Mumble.app"
+
+  zap trash: [
+    "~/Library/Application Support/Mumble",
+    "~/Library/Logs/Mumble.log",
+    "~/Library/Preferences/net.sourceforge.mumble.Mumble.plist",
+    "~/Library/Saved Application State/net.sourceforge.mumble.Mumble.savedState",
+    "/Library/ScriptingAdditions/MumbleOverlay.osax",
+  ]
+end
