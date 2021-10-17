5499f021c3d73ce4e9c209345849b6fc367e2030
Bevan Kay <email@bevankay.me>
2021-10-17 19:19:55 +1100

    Update java-beta from 18,18 to 18,19 (#12251)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,13 +1,13 @@
 cask "java-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "18,18"
+  version "18,19"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
   if Hardware::CPU.intel?
-    sha256 "890b3b0d99ba69a2f243cbbf5d5fb036e6c9f96227bbb9679bf1d0e9ae52bbbc"
+    sha256 "6b77f7e7ec4d02aa969943195236e312484bc92b65b57c3d135895bbdb31ea85"
   else
-    sha256 "3cacbf0c977a043cead09125e5a75bce005beb4b1fd78aafc91a0afd225fcd9d"
+    sha256 "70937d14301597a39e465956c55044d1ff5779198e8dcb776f062c136d5f2cb7"
   end
 
   name "OpenJDK Early Access Java Development Kit"
