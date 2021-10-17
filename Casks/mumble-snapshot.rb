f2d6a2fb1317a9d33bfa3d96b6db813a39a942c0
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-17 08:34:32 +0800

    Update mumble-snapshot.rb

--- Casks/mumble-snapshot.rb
+++ Casks/mumble-snapshot.rb
@@ -10,7 +10,7 @@ cask "mumble-snapshot" do
   livecheck do
     url "https://dl.mumble.info/latest/snapshot/client-macos-x64"
     strategy :header_match do |headers|
-      headers["content-disposition"][/mumble[._-]client[._-](.*)~snapshot\.dmg/i, 1].tr("_", "~")
+      headers["content-disposition"][/mumble[._-]client[._-](.*)~snapshot\.dmg/i, 1].tr("~", "_")
     end
   end
 
