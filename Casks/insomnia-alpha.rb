ef2719b9acb67c549a2d7182fc555f82d3b464f4
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-20 06:27:31 +0800

    insomnia-alpha 2021.6.0-alpha.4 (#12271)
    * Create insomnia-alpha.rb
    
    * Update insomnia-alpha.rb
    
    * Update insomnia-alpha.rb
    
    * Update insomnia-alpha.rb

---
+++ Casks/insomnia-alpha.rb
@@ -0,0 +1,32 @@
+cask "insomnia-alpha" do
+  version "2021.6.0-alpha.4"
+  sha256 "13243769294ad2135e1658f2f45d4652bd61cf6316338a339ff030c3c3ae879f"
+
+  url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
+      verified: "github.com/Kong/insomnia/"
+  name "Insomnia"
+  desc "HTTP and GraphQL Client"
+  homepage "https://insomnia.rest/"
+
+  livecheck do
+    url "https://github.com/Kong/insomnia/releases"
+    strategy :page_match
+    regex(/Insomnia[._-]Core[._-](\d+(?:\.\d+)+[._-](?:alpha|beta)[._-]\d*)\.dmg/i)
+  end
+
+  auto_updates true
+  conflicts_with cask: "insomnia"
+
+  app "Insomnia.app"
+
+  zap trash: [
+    "~/Library/Application Support/Insomnia",
+    "~/Library/Caches/com.insomnia.app",
+    "~/Library/Caches/com.insomnia.app.ShipIt",
+    "~/Library/Cookies/com.insomnia.app.binarycookies",
+    "~/Library/Preferences/ByHost/com.insomnia.app.ShipIt.*.plist",
+    "~/Library/Preferences/com.insomnia.app.helper.plist",
+    "~/Library/Preferences/com.insomnia.app.plist",
+    "~/Library/Saved Application State/com.insomnia.app.savedState",
+  ]
+end
