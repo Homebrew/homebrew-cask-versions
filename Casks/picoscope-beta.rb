6266e850534b24897929a56bcc5ce0a55b4cde82
Hannes Braun <hannesbraun@mail.de>
2021-10-20 12:32:43 +0200

    Update picoscope-beta from 7.0.82.9228 to 7.0.83.9320 (#12291)

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,6 +1,6 @@
 cask "picoscope-beta" do
-  version "7.0.82.9228"
-  sha256 "19af863133a80bcaa76951d4233613305746e871274f17c0a715e8f3c501ab8b"
+  version "7.0.83.9320"
+  sha256 "d36489ebd89e46857b1677b8163c022ebb62f26b18146ef1b04d0c615c92512f"
 
   url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}.pkg"
   name "PicoScope beta"
