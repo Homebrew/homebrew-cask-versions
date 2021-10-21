1f7f084b6c13c48dbdc33e043840f34482b8fd0f
Bevan Kay <email@bevankay.me>
2021-10-21 11:51:01 +1100

    figma-beta: various updates (#12301)

--- Casks/figma-beta.rb
+++ Casks/figma-beta.rb
@@ -1,10 +1,31 @@
 cask "figma-beta" do
-  version :latest
-  sha256 :no_check
+  arch = Hardware::CPU.intel? ? "mac" : "mac-arm"
 
-  url "https://desktop.figma.com/mac/beta/FigmaBeta.zip"
+  version "104.1.0"
+
+  if Hardware::CPU.intel?
+    sha256 "c3aadfa3c15c9e80c521f82a1803a785759c245cf9684095f3bb0f1c047085f9"
+  else
+    sha265 "6889d4d45a81c84e748dcb2b3312ef6f3bfcb82a14e83196edbdba2cae1c189a"
+  end
+
+  url "https://desktop.figma.com/#{arch}/beta/FigmaBeta-#{version}.zip"
   name "Figma Beta"
+  desc "Collaborative team software"
   homepage "https://figma.com/"
 
+  livecheck do
+    url "https://desktop.figma.com/#{arch}/beta/RELEASE.json"
+    strategy :page_match do |page|
+      JSON.parse(page)["version"]
+    end
+  end
+
   app "Figma Beta.app"
+
+  zap trash: [
+    "~/Library/Caches/com.figma.agent",
+    "~/Library/Preferences/com.figma.DesktopBeta.plist",
+    "~/Library/Saved Application State/com.figma.DesktopBeta.savedState",
+  ]
 end
