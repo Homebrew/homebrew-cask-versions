bed872bf777b4d4b53e40a180a96f8e45bed42d3
Alexander Ronald Altman <alexanderaltman@me.com>
2021-10-22 19:36:58 -0700

    Update sonos-s1-controller from 11.2.10 to 11.2.12 (#12319)
    * Update sonos-s1-controller from 11.2.10 to 11.2.12
    
    * add livecheck, add desc
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,12 +1,16 @@
 cask "sonos-s1-controller" do
-  version "11.2.10"
-  sha256 "571ed7e918d7b857acc1d7ce605ee6745cd71318f4586a866f9ffbe7bcb62dc0"
+  version "11.2.12,57.8.21220"
+  sha256 "10e51eaae890a8a3f64efcc60b31c5ba88392e4dc031ae019912045700daac0a"
 
-  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac",
-          must_contain: version.no_dots
+  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.before_comma.no_dots}.dmg"
   name "Sonos S1"
+  desc "Controller for Gen 1 Sonos products"
   homepage "https://www.sonos.com/"
+
+  livecheck do
+    url "https://www.sonos.com/en/redir/controller_software_mac"
+    strategy :extract_plist
+  end
 
   auto_updates true
 
