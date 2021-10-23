601430ebba4db514e5ca89fccce2ee1d355b6b87
Bevan Kay <email@bevankay.me>
2021-10-23 13:12:35 +1100

    add livecheck, add desc

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,12 +1,16 @@
 cask "sonos-s1-controller" do
-  version "11.2.12"
+  version "11.2.12,57.8.21220"
   sha256 "10e51eaae890a8a3f64efcc60b31c5ba88392e4dc031ae019912045700daac0a"
 
-  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.no_dots}.dmg"
-  appcast "https://macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://www.sonos.com/en/redir/controller_software_mac",
-          must_contain: version.no_dots
+  url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.before_comma.no_dots}.dmg"
   name "Sonos S1"
+  desc "Controller for Gen 1 Sonos products"
   homepage "https://www.sonos.com/"
+
+  livecheck do
+    url "https://www.sonos.com/en/redir/controller_software_mac"
+    strategy :extract_plist
+  end
 
   auto_updates true
 
