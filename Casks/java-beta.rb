e5571d707c9ca726f28a7f21ab24df1fd1ca9705
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:21:16 +0800

    Update java-beta from 18,19 to 18,20 (#12331)
    * Update java-beta from 18,19 to 18,20
    
    * Update java-beta.rb

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,15 +1,15 @@
 cask "java-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "18,19"
+  version "18,20"
 
-  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
   if Hardware::CPU.intel?
-    sha256 "6b77f7e7ec4d02aa969943195236e312484bc92b65b57c3d135895bbdb31ea85"
+    sha256 "93c5b60cf693bf612519fce7c5bf9cae50410ac4338d6a4acb975a192ba0487f"
   else
-    sha256 "70937d14301597a39e465956c55044d1ff5779198e8dcb776f062c136d5f2cb7"
+    sha256 "08f77b56280716074244455d49780c68242c7dab4fd207cac725766323ec711a"
   end
 
+  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
   desc "Early access development kit for the Java programming language"
   homepage "https://jdk.java.net/"
