ceec11c67ff055ef9fab5c4688e33b3dab8f9ddd
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:05:25 +0800

    Update keepassxc-beta.rb

--- Casks/keepassxc-beta.rb
+++ Casks/keepassxc-beta.rb
@@ -3,14 +3,14 @@ cask "keepassxc-beta" do
 
   version "2.6.6"
 
-  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-#{arch}.dmg",
-      verified: "github.com/keepassxreboot/keepassxc/"
   if Hardware::CPU.intel?
     sha256 "ab96033c16459de5a95e1f9e5864a5bd8cc47b4f3dee2c68ede6199dd44286ec"
   else
     sha256 "9dc121bb08f5b46186930ac9ba189553cec2c2ce9688df466e9ce1d9d75fe8c5"
   end
 
+  url "https://github.com/keepassxreboot/keepassxc/releases/download/#{version}/KeePassXC-#{version}-#{arch}.dmg",
+      verified: "github.com/keepassxreboot/keepassxc/"
   name "KeePassXC"
   desc "Password manager app"
   homepage "https://keepassxc.org/"
