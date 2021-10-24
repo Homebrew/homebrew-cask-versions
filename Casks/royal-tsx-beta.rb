ecc6ff17e0560d5aa03ced115b1c499863b67799
Hannes Braun <hannesbraun@mail.de>
2021-10-24 15:00:58 +0200

    Update royal-tsx-beta from 5.0.5.5 to 5.0.5.6

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,6 +1,6 @@
 cask "royal-tsx-beta" do
-  version "5.0.5.5"
-  sha256 "1245f0f6a3ca77b6bdb52501ba69f7c95e191e873e03f956fc1f834f7faea0d6"
+  version "5.0.5.6"
+  sha256 "2dd37510f5bab354243b58b252e79df32d55314a0ce4502f892415a63de98136"
 
   url "https://royaltsx-v#{version.major}.royalapps.com/updates/royaltsx_#{version}.dmg"
   name "Royal TSX"
