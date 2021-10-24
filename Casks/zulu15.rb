bad967bf03eecfc2f31fc1f1d462d2c436333486
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:01:34 +0800

    Update zulu15.rb

--- Casks/zulu15.rb
+++ Casks/zulu15.rb
@@ -4,14 +4,14 @@ cask "zulu15" do
 
   version "15.0.5,15.36.13-ca"
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_#{arch}.dmg",
-      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   if Hardware::CPU.intel?
     sha256 "374538c82778804e287bda2b660c36a63bd67f4c0fd9985b236935ec831a749e"
   else
     sha256 "c255dcf3e1165864846f3d67b7da96dd07230a8def64b4693ad66b12cea0a301"
   end
 
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-jdk#{version.before_comma}-macosx_#{arch}.dmg",
+      referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "Zulu OpenJDK 15"
   homepage "https://www.azul.com/products/core/"
