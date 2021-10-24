1bb230d28cac90a6c1022f4643066e64e9fb9ba1
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:04:33 +0800

    Update lando-edge.rb

--- Casks/lando-edge.rb
+++ Casks/lando-edge.rb
@@ -3,14 +3,14 @@ cask "lando-edge" do
 
   version "3.4.3"
 
-  url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
-      verified: "github.com/lando/lando/"
   if Hardware::CPU.intel?
     sha256 "5e8527125dc29e54a5353dab141836830fdfcd11fe2f78ac75b7d60da89caee4"
   else
     sha256 "7b2a7219c8dcb08f6494262754fcdedc782ecc2bb6b51aac32f437109ce12647"
   end
 
+  url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
+      verified: "github.com/lando/lando/"
   name "Lando Edge"
   desc "Local development environment and DevOps tool built on Docker"
   homepage "https://docs.lando.dev/"
