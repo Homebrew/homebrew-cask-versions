e849930f8ec818b1a99ac25561fb44c62d74bf4e
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:05:51 +0800

    Update ferdi-beta.rb

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -3,14 +3,14 @@ cask "ferdi-beta" do
 
   version "5.6.3-beta.2"
 
-  url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
-      verified: "github.com/getferdi/ferdi/"
   if Hardware::CPU.intel?
     sha256 "70d7daae1ac30d1127c4c0f73b10df4da5e9388ee1cd308f2db799269f11f04a"
   else
     sha256 "ca65bdad34d0a53c6ba3c7db76efcbba2fc6a25fcae20d3ea3d7ac130e2379b5"
   end
 
+  url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
+      verified: "github.com/getferdi/ferdi/"
   name "Ferdi"
   desc "Messaging browser which combines several services"
   homepage "https://getferdi.com/"
