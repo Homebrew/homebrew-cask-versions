16f489451e084f30c04982564890792528e95c1d
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:08:35 +0800

    Update java-beta.rb

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -3,13 +3,13 @@ cask "java-beta" do
 
   version "18,20"
 
-  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
   if Hardware::CPU.intel?
     sha256 "93c5b60cf693bf612519fce7c5bf9cae50410ac4338d6a4acb975a192ba0487f"
   else
     sha256 "08f77b56280716074244455d49780c68242c7dab4fd207cac725766323ec711a"
   end
 
+  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
   desc "Early access development kit for the Java programming language"
   homepage "https://jdk.java.net/"
