38580f82be9836096c6b9d6a82d534297849f5b9
Miccal Matthews <miccal.matthews@gmail.com>
2021-10-25 02:01:08 +0800

    Update brave-browser-beta.rb

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -4,14 +4,14 @@ cask "brave-browser-beta" do
 
   version "1.32.76.0,132.76"
 
-  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.after_comma}/Brave-Browser-Beta-#{arch}.dmg",
-      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
   if Hardware::CPU.intel?
     sha256 "859c1aed92b63cb88e2c1539a467a63e4f2e88efec2cab7e9e974018a57d7a9f"
   else
     sha256 "923d58662da6a2b77260494fb00fa3a6b3740da21d1037ed0937fa226a49899a"
   end
 
+  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.after_comma}/Brave-Browser-Beta-#{arch}.dmg",
+      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
   name "Brave Beta"
   desc "Web browser focusing on privacy"
   homepage "https://brave.com/download-beta/"
