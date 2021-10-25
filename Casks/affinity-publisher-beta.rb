0ac7de6cb03dc73515903a592ca8ae2010cd32e9
Bevan Kay <email@bevankay.me>
2021-10-25 11:37:02 +1100

    Update affinity-publisher-beta from 1.10.2.1167 to 1.10.2.1187

--- Casks/affinity-publisher-beta.rb
+++ Casks/affinity-publisher-beta.rb
@@ -1,8 +1,8 @@
 cask "affinity-publisher-beta" do
-  version "1.10.2.1167"
-  sha256 "97a9e6fa4374e52c4e3740ffbd6bc07690c78996fe2cbb123de8f57d19d392f8"
+  version "1.10.2.1187"
+  sha256 "aec8f2ea4eb5c4022b76bb5625fe8b096a2f80f10be4f03d38e5d9c1ba97fb9d"
 
-  url "https://beta-downloads.serif.com/download/ece3d4cf-3976-4bff-a084-583cc53c6176/affinity-publisher-customer-beta-#{version}.zip"
+  url "https://beta-downloads.serif.com/download/056ac8f5-2438-4245-9663-8f60d61feda4/affinity-publisher-customer-beta-#{version}.zip"
   name "Affinity Publisher"
   desc "Professional desktop publishing software"
   homepage "https://affinity.serif.com/en-us/publisher/"
