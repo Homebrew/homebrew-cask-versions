9a4d2691cdfb1009530b39d07366585c79cc6a80
Bevan Kay <email@bevankay.me>
2021-10-27 15:26:08 +1100

    Update qgis-ltr from 3.16.11,20210910_192934 to 3.16.12,20211025_052210

--- Casks/qgis-ltr.rb
+++ Casks/qgis-ltr.rb
@@ -1,6 +1,6 @@
 cask "qgis-ltr" do
-  version "3.16.11,20210910_192934"
-  sha256 "6d0954429cc67d5eed684de5d1d728464d8da23300d42116e2f6c774f9b89a52"
+  version "3.16.12,20211025_052210"
+  sha256 "90ae806f2974c275288ee36778518228aa7b1f36e7c9d7e41bac111fadd2890e"
 
   url "https://qgis.org/downloads/macos/ltr/qgis_ltr_final-#{version.before_comma.dots_to_underscores}_#{version.after_comma}.dmg"
   name "QGIS LTR"
