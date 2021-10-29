8d9534d6849b613961c2573f3d0c28d14903c079
Phill Tran <philltran@users.noreply.github.com>
2021-10-29 10:59:34 -0400

    Update lando-edge from 3.5.0 to 3.5.1 (#12392)

--- Casks/lando-edge.rb
+++ Casks/lando-edge.rb
@@ -1,12 +1,12 @@
 cask "lando-edge" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "3.5.0"
+  version "3.5.1"
 
   if Hardware::CPU.intel?
-    sha256 "00f479c37dd8c3095fc55ea9766e0670d614abe5d53a0e99dccb06b75b6b6c5b"
+    sha256 "61d761711b0b3c6829a3d1e8376359717dca4186b44ef4d6254a4131c1ea34c5"
   else
-    sha256 "3b500458115a37bd0c28a79bf7aff6ca610ec72a801ae29b700d71c7356dea2f"
+    sha256 "f518f6580b9e9f68515a4eac986e81024faf873791cf37f21c707279d67e0a87"
   end
 
   url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
