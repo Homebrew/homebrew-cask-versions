ad92d0393d21dc1e74f6a140d631181202aad187
Hannes Braun <hannesbraun@mail.de>
2021-10-31 02:31:45 +0200

    Update microsoft-edge-beta from 95.0.1020.38 to 95.0.1020.40 (#12395)

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge-beta" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069439" : "2099618"
 
-  version "95.0.1020.38"
+  version "95.0.1020.40"
 
   if Hardware::CPU.intel?
-    sha256 "41b87ced3a83be1305bea4b0f019131b4a25681aaf05e70e543dcab5699c2b3f"
+    sha256 "b7ac52a23c3501aa20eeb10bb0e0bfee75d1476199fd59bf473085b0d97ddda7"
   else
-    sha256 "03adb38e2d60855b6e616f1226d1f631d75b9279cf8a7ddf49727d22c792640b"
+    sha256 "9a06960653c731f5fba6aa458558bbb7d05f80083e0d6e11dcbc32c5455322e1"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
