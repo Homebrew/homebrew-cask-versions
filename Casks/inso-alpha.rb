4c95d122d0990dc4fb60a9d97e90b889f9d2150f
Miccal Matthews <miccal.matthews@gmail.com>
2021-11-01 07:46:43 +0800

    Update inso-alpha.rb

--- Casks/inso-alpha.rb
+++ Casks/inso-alpha.rb
@@ -1,6 +1,6 @@
 cask "inso-alpha" do
   version "2.4.0-alpha.1"
-  sha256 :no_check
+  sha256 "e62af5751cb985f27e103eb4546c601082e3b4072bd530e0e1f9d04c7193f714"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
@@ -14,9 +14,7 @@ cask "inso-alpha" do
     regex(/href=.*?inso-macos-(?:latest-)*(\d+(?:\.\d+)+[._-](?:alpha|beta)[._-]\d*)\.zip/i)
   end
 
-  conflicts_with cask: [
-    "inso",
-  ]
+  conflicts_with cask: "inso"
 
   binary "inso"
 end
