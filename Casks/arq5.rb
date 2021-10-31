8968e989bc283de64128b54931238c1fc0f43745
John Benninghoff <jbenninghoff@mac.com>
2021-10-31 10:34:15 -0500

    Update arq5 from 5.21.1 to 5.22

--- Casks/arq5.rb
+++ Casks/arq5.rb
@@ -1,10 +1,16 @@
 cask "arq5" do
-  version "5.21.1"
-  sha256 "e43c4036dc963c39893d80fdd057aaa5fd801bd306f02f54191b9ce21e25574c"
+  version "5.22"
+  sha256 "9aaa28e5d20155865d6d5ed510a0bf92f34954ff86b5d9830b97d08efb4584c9"
 
   url "https://www.arqbackup.com/download/arqbackup/Arq_#{version}.zip"
   name "Arq"
+  desc "Multi-cloud backup application"
   homepage "https://www.arqbackup.com/"
+
+  livecheck do
+    url "https://www.arqbackup.com/download/arqbackup/arq#{version.major}_release_notes.html"
+    regex(/Version\s*(\d+(?:\.\d+)+)/i)
+  end
 
   auto_updates true
   conflicts_with cask: "arq"
