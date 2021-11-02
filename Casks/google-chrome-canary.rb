42b1940d30abe0e2df2be5b4f7c9ab3ac9b8c0d4
Hannes Braun <hannesbraun@mail.de>
2021-11-02 01:56:04 +0100

    Update google-chrome-canary from 97.0.4687.2 to 97.0.4688.0 (#12401)

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-canary" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "97.0.4687.2"
+  version "97.0.4688.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}canary/googlechromecanary.dmg"
