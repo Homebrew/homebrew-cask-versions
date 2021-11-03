b6af58fbf9f9933c4cfd48fa7719ea61fc1ead3b
Hannes Braun <hannesbraun@mail.de>
2021-11-03 12:16:45 +0100

    Update citra-nightly from 1730,20211023-a5d01c0 to 1732,20211101-5a7d801 (#12426)

--- Casks/citra-nightly.rb
+++ Casks/citra-nightly.rb
@@ -1,6 +1,6 @@
 cask "citra-nightly" do
-  version "1730,20211023-a5d01c0"
-  sha256 "ad520f8f65b4664d9badf6daacbf2408b6c2a69c4bb5a54b3fd505114ff21df8"
+  version "1732,20211101-5a7d801"
+  sha256 "0d8424d4fe68054caeb21e93ccd5ddf4e8f70ddcf79d97d5a2ec6636b202ce9b"
 
   url "https://github.com/citra-emu/citra-nightly/releases/download/nightly-#{version.before_comma}/citra-osx-#{version.after_comma}.tar.gz",
       verified: "github.com/citra-emu/citra-nightly/"
