54ac2439e527780c011398304ab0672ee827c615
LanikSJ <LanikSJ@users.noreply.github.com>
2021-11-04 15:41:14 -0700

    Update dropbox-beta from 135.3.4173 to 135.3.4177 (#12447)

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,6 +1,6 @@
 cask "dropbox-beta" do
-  version "135.3.4173"
-  sha256 "e05b4267852c114ee61269b8dca34eb6df87c9653692b5ab78b60fe4664e9b59"
+  version "135.3.4177"
+  sha256 "5af5f00a868bf6f4599033f9837eadd8466828ebc2f1706f76fbb77064d690fe"
 
   url "https://www.dropbox.com/download?build=#{version}&plat=mac&type=full",
       verified: "dropbox.com/"
