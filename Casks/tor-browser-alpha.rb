d76e76109d2ed46abf28b56fa7f2f9eb1494bc5f
miccal <miccal.matthews@gmail.com>
2021-11-08 09:55:29 +0800

    Update tor-browser-alpha from 11.0a9 to 11.0a10

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -1,6 +1,6 @@
 cask "tor-browser-alpha" do
-  version "11.0a9"
-  sha256 "17f8b2f299f178d35cfa76f4eeb5763310f12e4a99ea97118136c21a4857a32e"
+  version "11.0a10"
+  sha256 "c6823a28fd28205437564815f93011ff93b7972da2a8ce16919adfc65909e7b9"
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-osx64_en-US.dmg"
   name "Tor Browser"
