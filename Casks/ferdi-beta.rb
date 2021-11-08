52173afff4caf04f1ea7e4ab9ec7831f207d74ae
Vijay Aravamudhan <vraravam@users.noreply.github.com>
2021-11-08 05:52:32 +0530

    Bumped up ferdi to '5.6.3' (#12465)
    * Bumped up ferdi to '5.6.3'
    
    * Update ferdi-beta.rb
    
    Co-authored-by: Bevan Kay <email@bevankay.me>

--- Casks/ferdi-beta.rb
+++ Casks/ferdi-beta.rb
@@ -1,12 +1,12 @@
 cask "ferdi-beta" do
   arch = Hardware::CPU.intel? ? "" : "-arm64"
 
-  version "5.6.3-beta.3"
+  version "5.6.3"
 
   if Hardware::CPU.intel?
-    sha256 "0c428569cb140fb20089694d0593b9a7d027d28f1ce965ccfdb6364966bbddfc"
+    sha256 "a5f1386b6468629e417b112353781ac86ab85284e861f50df4b800c1dc75c346"
   else
-    sha256 "754f4ffe8d1f9e9cafbc196a1337acd1c5b1a2de9688126c88d9eceaae8e12e8"
+    sha256 "830dd2cdf006c8b2fa8e2bf48554da3089dbe263807194de3bf57e1cda73d68e"
   end
 
   url "https://github.com/getferdi/ferdi/releases/download/v#{version}/Ferdi-#{version}#{arch}.dmg",
