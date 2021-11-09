f2270c908930b99c8c1ede555edddfa659034022
Bevan Kay <email@bevankay.me>
2021-11-09 21:18:21 +1100

    Update java-beta from 18,21 to 18,22 (#12482)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,12 @@
 cask "java-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "18,21"
+  version "18,22"
 
   if Hardware::CPU.intel?
-    sha256 "fea5879bad743ca94c1860e2568f9058a0edbe8281a9d8aefee85a877bf612ee"
+    sha256 "a1ab16d13fea718d92d9700f91896099f56beea05fc1cf66d3dcdc8d7b6a89b4"
   else
-    sha256 "2fcaa76c0f05f6e1a66c92796b80f31198248e214fc728c9e8c8e1d2eed6590f"
+    sha256 "87fa616cde7913c22e05d83f27335e6f4b34e07d1eb2f533add61e89d1bfc003"
   end
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
