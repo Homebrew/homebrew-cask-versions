52f0e04ec1f1fcf04bb77b531cec2dfabfacaff5
Bevan Kay <email@bevankay.me>
2021-11-09 16:33:56 +1100

    Update geogebra5 from 5.0.672.0 to 5.0.674.0

--- Casks/geogebra5.rb
+++ Casks/geogebra5.rb
@@ -1,6 +1,6 @@
 cask "geogebra5" do
-  version "5.0.672.0"
-  sha256 "ff4724a4678442679439a497a67914a60ff919934eec56b9e386e5304f7770fe"
+  version "5.0.674.0"
+  sha256 "214e42b14c9f14b04fd4cf2af73729461db57bbc884cf63416156d3323b074e9"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-MacOS-Installer-withJava-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
