d36f53842efbb6524685d725691d2117a4ae2d02
Hannes Braun <hannesbraun@mail.de>
2021-11-10 21:58:38 +0100

    Update visual-studio-code-insiders from 1.63.0,93ce4397a2cbc88707c3ed8b34a813b87457568c to 1.63.0,bef4dba21f68bbdb8d9f08fe04b2e1858597722b (#12501)

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,12 +1,12 @@
 cask "visual-studio-code-insiders" do
   arch = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
 
-  version "1.63.0,93ce4397a2cbc88707c3ed8b34a813b87457568c"
+  version "1.63.0,bef4dba21f68bbdb8d9f08fe04b2e1858597722b"
 
   if Hardware::CPU.intel?
-    sha256 "ef3ccb6fe430d51198e49458834c26f899a08cf3862000c7ec3a4d4689e19de0"
+    sha256 "3416aaf3975cd5b6f8017f6b597d69d20d9040b948d396cc7210830f9d7d60bb"
   else
-    sha256 "4a2fe4a54e05f370a9d1f80ea55a0150290d6fce05fa49c4141a0c724224f63b"
+    sha256 "4c83df91097c7f84bb9aede35044f8e01d1723bb297f19c6e9675a948a4f9687"
   end
 
   url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-#{arch}.zip",
