3bd214c3101547d1d676cf9ea551c9efc693763b
Bevan Kay <email@bevankay.me>
2021-11-10 16:26:44 +1100

    Update google-chrome-dev from 97.0.4688.2 to 97.0.4692.8

--- Casks/google-chrome-dev.rb
+++ Casks/google-chrome-dev.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-dev" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "97.0.4688.2"
+  version "97.0.4692.8"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}dev/googlechromedev.dmg"
