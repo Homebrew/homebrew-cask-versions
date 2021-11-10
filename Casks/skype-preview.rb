828bce1d6114ffaab002e1572838770bee2bebf2
Bevan Kay <email@bevankay.me>
2021-11-10 16:29:27 +1100

    Update skype-preview from 8.79.76.22 to 8.79.76.43

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask "skype-preview" do
-  version "8.79.76.22"
-  sha256 "a2e117097c89a239a073bd25e2bf41b4d255718dfd40b1cf164764e2aca8a734"
+  version "8.79.76.43"
+  sha256 "3a4926dbe77bd40cda037649f457daff57555a3f73e474bda4a8582fc0a5795d"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
