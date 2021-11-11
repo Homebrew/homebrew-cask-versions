df0d4a0a8d187923f5f7f6450670db4da148064a
Hannes Braun <hannesbraun@mail.de>
2021-11-11 15:34:43 +0100

    Update mongodb-compass-beta from 1.29.0-beta.6 to 1.29.1-beta.0 (#12511)

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass-beta" do
-  version "1.29.0-beta.6"
-  sha256 "6f5a380158f97438920915c9aada6cd8dd1af4b9e8ca414b557776b4bb09d1b5"
+  version "1.29.1-beta.0"
+  sha256 "6680d2efdc0e987c2000331df794f224483989b508dc3c76823ad549c7904ca8"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
   name "MongoDB Compass"
