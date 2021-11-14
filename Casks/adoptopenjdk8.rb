8612475d23ed9aafcedd0fbe3549a935d49c5848
Sam Ford <1584702+samford@users.noreply.github.com>
2021-11-14 11:38:35 -0500

    adoptopenjdk8: remove livecheck

--- Casks/adoptopenjdk8.rb
+++ Casks/adoptopenjdk8.rb
@@ -8,16 +8,6 @@ cask "adoptopenjdk8" do
   desc "Prebuilt OpenJDK binaries"
   homepage "https://adoptopenjdk.net/"
 
-  livecheck do
-    url :url
-    strategy :github_latest do |page|
-      match = page.match(%r{href=.*/jdk(\d+)u(\d+)-(b\d+).+["' >]}i)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}:#{match[3]}"
-    end
-  end
-
   pkg "OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
 
   uninstall pkgutil: "net.adoptopenjdk.#{version.before_comma}.jdk"
