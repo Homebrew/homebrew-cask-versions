a67dc5c1785b47ec7cf389e0cf106a7a7cdfb2b0
Nato Boram <NatoBoram@users.noreply.github.com>
2021-11-13 23:27:00 -0500

    Add Discord Development (#12529)

---
+++ Casks/discord-development.rb
@@ -0,0 +1,29 @@
+cask "discord-development" do
+  version "0.0.8733"
+  sha256 "7b85681454ba9e21ca64755693a3f5d54842fbd2f8c22b470868bebe2e50ff07"
+
+  url "https://dl-development.discordapp.net/apps/osx/#{version}/DiscordDevelopment.dmg",
+      verified: "dl-development.discordapp.net/"
+  name "Discord Development"
+  desc "Voice and text chat software"
+  homepage "https://discord.com/"
+
+  livecheck do
+    url "https://discord.com/api/download/development?platform=osx"
+    strategy :header_match
+  end
+
+  auto_updates true
+
+  app "Discord Development.app"
+
+  zap trash: [
+    "~/Library/Application Support/discorddevelopment",
+    "~/Library/Caches/com.hnc.DiscordDevelopment",
+    "~/Library/Caches/com.hnc.DiscordDevelopment.ShipIt",
+    "~/Library/Cookies/com.hnc.DiscordDevelopment.binarycookies",
+    "~/Library/Preferences/com.hnc.DiscordDevelopment.helper.plist",
+    "~/Library/Preferences/com.hnc.DiscordDevelopment.plist",
+    "~/Library/Saved Application State/com.hnc.DiscordDevelopment.savedState",
+  ]
+end
