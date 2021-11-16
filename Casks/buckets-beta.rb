2b1154ab8ba0a389048cd29fd5466a20d05b2cbb
Alex Wolfe <3718652+afwolfe@users.noreply.github.com>
2021-11-15 21:51:16 -0500

    Add Buckets Beta v0.63.2 (#12548)
    * Add Buckets Beta v0.63.2
    
    * Fix buckets-beta style

---
+++ Casks/buckets-beta.rb
@@ -0,0 +1,22 @@
+cask "buckets-beta" do
+  version "0.63.2"
+  sha256 "e24f2de00a1e5f91ec853f16732a50299b2328dcdbad55b7a896249f752b383a"
+
+  url "https://github.com/buckets/desktop-beta/releases/download/v#{version}/Buckets-Beta-#{version}.dmg",
+      verified: "github.com/buckets/desktop-beta/"
+  name "Buckets Beta"
+  desc "Budgeting tool"
+  homepage "https://www.budgetwithbuckets.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "Buckets Beta.app"
+
+  zap trash: [
+    "~/Library/Application Support/Buckets Beta",
+    "~/Library/Preferences/com.onepartrain.buckets.desktopbeta.plist",
+  ]
+end
