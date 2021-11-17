9edfbfc4bb80039a6adfa2a8a549111e6de85121
miccal <miccal.matthews@gmail.com>
2021-11-17 20:29:21 +0800

    Update dotnet-preview from 6.0.0-rc.2.21480.5 to 6.0.0

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -2,8 +2,8 @@ cask "dotnet-preview" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.0-rc.2.21480.5,39fb50be-17b9-45b2-9f3e-eb03e31b8219:3f67fc8d06a1d163ac817cd116fd4719"
-    sha256 "347f22634d9cf3867cf573efda35359313a0558c399128f3a1b25f1eaf2aa7e3"
+    version "6.0.0,03e62824-4061-45dc-8777-93e82205148d:81489074c065675bd713a9cbeb995bef"
+    sha256 "fedbddb71e18d68279433e22326220225b3ee45209966fce818ac34349eefef4"
   else
     version "6.0.0-rc.2.21480.5,2ac2aeec-4262-41e1-9544-8fc21579952c:8bfe416a0ce0b43b354481cacaa57f35"
     sha256 "e2287e63599895c7560f609a176eb88d7a32096e18dde5c5f3f0c96bfd1cf09d"
