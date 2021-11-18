2f9e2112d43ab0d470105518c153eac4ef80d0f4
Bevan Kay <email@bevankay.me>
2021-11-18 12:49:12 +1100

    Update figma-beta from 105.2.0 to 107.0.0

--- Casks/figma-beta.rb
+++ Casks/figma-beta.rb
@@ -1,12 +1,12 @@
 cask "figma-beta" do
   arch = Hardware::CPU.intel? ? "mac" : "mac-arm"
 
-  version "105.2.0"
+  version "107.0.0"
 
   if Hardware::CPU.intel?
-    sha256 "49bb8e7f810d848162befea0a4537335fa8d9fbdf5aaaeaf88edfc1124f2984d"
+    sha256 "47731cff487f2deb9b2db82fece17c81ba556591bb94881383de8da22e5d3d24"
   else
-    sha256 "be696b66ba076c1737be7c383dd9be0fc950a8b84d3db33ed9ca32d1cbc43608"
+    sha256 "83f3776eee50bb0cfd1c69d9705a7cc9e64667e10f75ca2eb6be3e67fea5e644"
   end
 
   url "https://desktop.figma.com/#{arch}/beta/FigmaBeta-#{version}.zip"
