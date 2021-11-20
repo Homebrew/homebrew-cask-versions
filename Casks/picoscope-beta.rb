effe8c8350e41eea5d0bbaf6a1ea088ca055a80c
Constantin Dullo <constantin.dullo@me.com>
2021-11-20 15:43:50 +0100

    Update Picoscope Beta to v7.0.85

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,8 +1,8 @@
 cask "picoscope-beta" do
-  version "7.0.83.9320"
-  sha256 "d36489ebd89e46857b1677b8163c022ebb62f26b18146ef1b04d0c615c92512f"
+  version "7.0.85.9526"
+  sha256 "b43c50316c551ca250e9f717d29bc3aae0d6efc42f2274e0eccf3254ddf33e8b"
 
-  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}.pkg"
+  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_7_TM_Early_Access_#{version}.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
@@ -13,7 +13,7 @@ cask "picoscope-beta" do
     regex(%r{href=.*?/PicoScope_(\d+(?:\.\d+)+)\.pkg}i)
   end
 
-  pkg "PicoScope_#{version}.pkg"
+  pkg "PicoScope_7_TM_Early_Access_#{version}.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}"
 end
