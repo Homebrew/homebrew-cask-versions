e7af20e54f956a89ebf8d2bf57e86cd8a3e5da15
flieger19 <constantin.dullo@gmail.com>
2021-11-20 22:38:24 +0100

    Update Picoscope Beta to v7.0.85 (#12599)
    * Update Picoscope Beta to v7.0.85
    
    * Fix regesx in the livecheck of Picoscope Beta v7.0.85
    
    * Update picoscope-beta.rb
    
    Co-authored-by: Constantin Dullo <constantin.dullo@me.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,8 +1,8 @@
 cask "picoscope-beta" do
-  version "7.0.83.9320"
-  sha256 "d36489ebd89e46857b1677b8163c022ebb62f26b18146ef1b04d0c615c92512f"
+  version "7.0.85.9526"
+  sha256 "b43c50316c551ca250e9f717d29bc3aae0d6efc42f2274e0eccf3254ddf33e8b"
 
-  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}.pkg"
+  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version.major}_TM_Early_Access_#{version}.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
@@ -10,10 +10,10 @@ cask "picoscope-beta" do
   livecheck do
     url "https://oem.picotech.com/p#{version.major}beta/download/"
     strategy :page_match
-    regex(%r{href=.*?/PicoScope_(\d+(?:\.\d+)+)\.pkg}i)
+    regex(%r{href=.*?/PicoScope_#{version.major}_TM_Early_Access_(\d+(?:\.\d+)+)\.pkg}i)
   end
 
-  pkg "PicoScope_#{version}.pkg"
+  pkg "PicoScope_#{version.major}_TM_Early_Access_#{version}.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}"
 end
