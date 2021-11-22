a15ee8430d66d9b9e0b44d2087cab36604ef94db
Marius Petcu <marius@petcu.me>
2021-11-22 10:55:07 +0200

    Add Defold (alpha and beta channels)

---
+++ Casks/defold-beta.rb
@@ -0,0 +1,28 @@
+cask "defold-beta" do
+  version :latest
+  sha256 :no_check
+
+  release_channel = "beta"
+  url "https://d.defold.com/#{release_channel}/info.json" do |json_content|
+    require "json"
+    version_sha = JSON.parse(json_content)["sha1"]
+    "https://d.defold.com/archive/#{release_channel}/#{version_sha}/#{release_channel}/editor2/Defold-x86_64-darwin.dmg"
+  end
+  name "Defold"
+  desc "Game engine for development of desktop, mobile and web games"
+  homepage "https://defold.com/"
+
+  conflicts_with cask: [
+    "defold",
+    "defold-alpha",
+  ]
+
+  app "Defold.app"
+
+  zap trash: [
+    "~/Library/Application Support/Defold",
+    "~/Library/Caches/com.defold.editor",
+    "~/Library/Preferences/com.defold.editor.plist",
+    "~/Library/Saved Application State/com.defold.editor.savedState",
+  ]
+end
