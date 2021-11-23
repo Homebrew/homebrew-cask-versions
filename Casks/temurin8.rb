20c49180e8dc89cec68d87aebd7709ea45c2a97e
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 20:02:51 -0800

    temurin8: replace colon in version string with comma

--- Casks/temurin8.rb
+++ Casks/temurin8.rb
@@ -1,8 +1,8 @@
 cask "temurin8" do
-  version "8,312:b07"
+  version "8,312,b07"
   sha256 "231cb0450c603cc861ade707b1deb01a8cbe2efd6c93e49c1707f3d899f92a93"
 
-  url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk#{version.before_comma}u#{version.after_comma.before_colon}-#{version.after_colon}/OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg",
+  url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk#{version.csv[0]}u#{version.csv[1]}-#{version.csv[2]}/OpenJDK#{version.csv[0]}U-jdk_x64_mac_hotspot_#{version.csv[0]}u#{version.csv[1]}#{version.csv[2]}.pkg",
       verified: "github.com/adoptium/temurin8-binaries/"
   name "Eclipse Temurin 8"
   desc "JDK from the Eclipse Foundation (Adoptium)"
@@ -14,11 +14,11 @@ cask "temurin8" do
       match = page.match(%r{href=.*/jdk(\d+)u(\d+)-(b\d+).+["' >]}i)
       next if match.blank?
 
-      "#{match[1]},#{match[2]}:#{match[3]}"
+      "#{match[1]},#{match[2]},#{match[3]}"
     end
   end
 
-  pkg "OpenJDK#{version.before_comma}U-jdk_x64_mac_hotspot_#{version.before_comma}u#{version.after_comma.before_colon}#{version.after_comma.after_colon}.pkg"
+  pkg "OpenJDK#{version.csv[0]}U-jdk_x64_mac_hotspot_#{version.csv[0]}u#{version.csv[1]}#{version.csv[2]}.pkg"
 
-  uninstall pkgutil: "net.temurin.#{version.before_comma}.jdk"
+  uninstall pkgutil: "net.temurin.#{version.csv[0]}.jdk"
 end
