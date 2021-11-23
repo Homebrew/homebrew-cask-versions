330d0741bfad95e252cf2ced96dff86330cd91e9
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 19:35:08 -0800

    dotnet-sdk-preview: replace colon in version string with comma (#12615)

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -2,14 +2,14 @@ cask "dotnet-sdk-preview" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.100,14a45451-4cc9-48e1-af69-0aff75891d09:ff6e83986a2a9a535015fb3104a90a1b"
+    version "6.0.100,14a45451-4cc9-48e1-af69-0aff75891d09,ff6e83986a2a9a535015fb3104a90a1b"
     sha256 "9203560506408d8f88774358b03cdcfcfa0495682fde6034b24f7ccaeddce2ef"
   else
-    version "6.0.100-rc.2.21505.57,3336b6fe-1f40-4c66-a1ef-81cabd3df46c:dd56dc201e5c0a95b86f41c1cf36df17"
+    version "6.0.100-rc.2.21505.57,3336b6fe-1f40-4c66-a1ef-81cabd3df46c,dd56dc201e5c0a95b86f41c1cf36df17"
     sha256 "4db0bb52f7cd6cfd3969a8b24ea4d09b851a9d3243915798e3f3d0c48308370e"
   end
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-sdk-#{version.before_comma}-osx-#{arch}.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-sdk-#{version.csv[0]}-osx-#{arch}.pkg"
   name ".NET SDK"
   desc "Developer platform"
   homepage "https://www.microsoft.com/net/core#macos"
@@ -19,7 +19,7 @@ cask "dotnet-sdk-preview" do
     regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-sdk-v?(.+)-osx-#{arch}\.pkg}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
-        "#{match[2]},#{match[0]}:#{match[1]}"
+        "#{match[2]},#{match[0]},#{match[1]}"
       end
     end
   end
@@ -31,7 +31,7 @@ cask "dotnet-sdk-preview" do
   ]
   depends_on macos: ">= :mojave"
 
-  pkg "dotnet-sdk-#{version.before_comma}-osx-#{arch}.pkg"
+  pkg "dotnet-sdk-#{version.csv[0]}-osx-#{arch}.pkg"
   binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: [
