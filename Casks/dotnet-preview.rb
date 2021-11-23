81c6d5795a06280e275c780bf7e6ecadff2377be
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-22 19:19:30 -0800

    dotnet-preview: replace colon in version string with comma

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -2,14 +2,14 @@ cask "dotnet-preview" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.0,03e62824-4061-45dc-8777-93e82205148d:81489074c065675bd713a9cbeb995bef"
+    version "6.0.0,03e62824-4061-45dc-8777-93e82205148d,81489074c065675bd713a9cbeb995bef"
     sha256 "fedbddb71e18d68279433e22326220225b3ee45209966fce818ac34349eefef4"
   else
-    version "6.0.0-rc.2.21480.5,2ac2aeec-4262-41e1-9544-8fc21579952c:8bfe416a0ce0b43b354481cacaa57f35"
+    version "6.0.0-rc.2.21480.5,2ac2aeec-4262-41e1-9544-8fc21579952c,8bfe416a0ce0b43b354481cacaa57f35"
     sha256 "e2287e63599895c7560f609a176eb88d7a32096e18dde5c5f3f0c96bfd1cf09d"
   end
 
-  url "https://download.visualstudio.microsoft.com/download/pr/#{version.after_comma.before_colon}/#{version.after_colon}/dotnet-runtime-#{version.before_comma}-osx-#{arch}.pkg"
+  url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-runtime-#{version.csv[0]}-osx-#{arch}.pkg"
   name ".Net Runtime"
   desc "Developer platform"
   homepage "https://www.microsoft.com/net/core#macos"
@@ -19,7 +19,7 @@ cask "dotnet-preview" do
     regex(%r{/download/pr/([^/]+)/([^/]+)/dotnet-runtime-v?(.+)-osx-#{arch}\.pkg}i)
     strategy :page_match do |page, regex|
       page.scan(regex).map do |match|
-        "#{match[2]},#{match[0]}:#{match[1]}"
+        "#{match[2]},#{match[0]},#{match[1]}"
       end
     end
   end
@@ -31,7 +31,7 @@ cask "dotnet-preview" do
   ]
   depends_on macos: ">= :mojave"
 
-  pkg "dotnet-runtime-#{version.before_comma}-osx-#{arch}.pkg"
+  pkg "dotnet-runtime-#{version.csv[0]}-osx-#{arch}.pkg"
   binary "/usr/local/share/dotnet/dotnet"
 
   uninstall pkgutil: "com.microsoft.dotnet.*",
