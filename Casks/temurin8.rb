4e1462e3d1222c310ef04a2335a852370a4c5a07
fn ⌃ ⌥ <70830482+FnControlOption@users.noreply.github.com>
2021-11-23 01:16:07 -0800

    Update livecheck

--- Casks/temurin8.rb
+++ Casks/temurin8.rb
@@ -9,12 +9,14 @@ cask "temurin8" do
   homepage "https://adoptium.net/"
 
   livecheck do
-    url "https://github.com/adoptium/temurin8-binaries/releases/"
+    url "https://api.adoptium.net/v3/assets/feature_releases/8/ga?architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
     strategy :page_match do |page|
-      match = page.match(/jdk_x64_mac_hotspot_(\d+)u(\d+)b(\d+)\.pkg/i)
-      next if match.blank?
+      JSON.parse(page).map do |release|
+        match = release["release_name"].match(/^jdk(\d+)u(\d+)-b(\d+)$/)
+        next if match.blank?
 
-      "#{match[1]},#{match[2]},#{match[3]}"
+        "#{match[1]},#{match[2]},#{match[3]}"
+      end
     end
   end
 
