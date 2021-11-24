a05853c05bc0d9184228da48eeadc8b4a46325c3
Hannes Braun <hannesbraun@mail.de>
2021-11-24 17:20:18 +0100

    Update github-beta from 2.9.5-beta2-79f51c05 to 2.9.5-beta3-da07a207 (#12637)

--- Casks/github-beta.rb
+++ Casks/github-beta.rb
@@ -2,12 +2,12 @@ cask "github-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
   platform = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
 
-  version "2.9.5-beta2-79f51c05"
+  version "2.9.5-beta3-da07a207"
 
   if Hardware::CPU.intel?
-    sha256 "e9786e1b2f9394858eaae18ac7481b9a7a6637da73502a8da3f3bb1e88c8362f"
+    sha256 "52ed388a69ea441193b592578a76a5287789888d7f4363dbadbf6b60ce3f1a04"
   else
-    sha256 "3dc3f78b0d4e453fbb50da393535704961597a1c2d2d60a31ee8c53bc360388d"
+    sha256 "22b9712f9863741d3c313450c4872a7a0503b54e94f2100901d0855664673ad3"
   end
 
   url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-#{arch}.zip",
