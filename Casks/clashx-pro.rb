6b204a0ca0252bafdcca2dfa604fa10d6a49ea98
JounQin <admin@1stg.me>
2021-11-29 07:19:34 +0800

    Update clashx-pro from 1.72.0.1 to 1.72.0.2 (#12662)

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -1,6 +1,6 @@
 cask "clashx-pro" do
-  version "1.72.0.1"
-  sha256 "36751e6f18073c1dcd5924b632d7a135123b3a0f6fd6af17040a7fbc4abe0bc3"
+  version "1.72.0.2"
+  sha256 "2655f77c66e85537fbb17bb11e0561d3550d81795ea741980e032b80e7103370"
 
   url "https://appcenter.vercel.app/clashx/clashx-pro/#{version}",
       verified: "appcenter.vercel.app/clashx"
