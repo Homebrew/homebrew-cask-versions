0e76a2edb689324788328c0810afa58721393e3a
Hannes Braun <hannesbraun@mail.de>
2021-11-28 17:38:08 +0100

    Update java-beta from 18,24 to 18,25

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,12 @@
 cask "java-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "18,24"
+  version "18,25"
 
   if Hardware::CPU.intel?
-    sha256 "2a6d033bf34dc5bd66d1f07b9ab2aeac7c1e07f55212f1128ab65f6f33d483af"
+    sha256 "aabaa9b5f6d67f3430ea3fc7dd5f732a8d147a1aa2f676c003584d4b7a6e56bb"
   else
-    sha256 "b36312151faf5d2a8ba1b4dbcb9744dcb3efcdfa7b704808811d7d035fd487c3"
+    sha256 "7f67db509f3d002fbb544a51dc7b88f013697945ed21ad729020970978077fc4"
   end
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.after_comma}/GPL/openjdk-#{version.before_comma}-ea+#{version.after_comma}_macos-#{arch}_bin.tar.gz"
