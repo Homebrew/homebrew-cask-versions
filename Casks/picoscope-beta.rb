67a30cb5664838a9ee3f582e43580933f113e5da
Constantin Dullo <constantin.dullo@me.com>
2021-11-30 18:09:06 +0100

    Update picoscope-beta to 7.0.86

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,8 +1,8 @@
 cask "picoscope-beta" do
-  version "7.0.85.9526"
-  sha256 "b43c50316c551ca250e9f717d29bc3aae0d6efc42f2274e0eccf3254ddf33e8b"
+  version "7.0.86"
+  sha256 "1bc7df5ce822c92127fc7681e15318b2317ffe22cef6ff83d2530d4882861d0a"
 
-  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version.major}_TM_Early_Access_#{version}.pkg"
+  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
@@ -10,10 +10,10 @@ cask "picoscope-beta" do
   livecheck do
     url "https://oem.picotech.com/p#{version.major}beta/download/"
     strategy :page_match
-    regex(%r{href=.*?/PicoScope_#{version.major}_TM_Early_Access_(\d+(?:\.\d+)+)\.pkg}i)
+    regex(%r{href=.*?/PicoScope_#{version.major}_TnM_Early_Access_(\d+(?:\.\d+)+)\.pkg}i)
   end
 
-  pkg "PicoScope_#{version.major}_TM_Early_Access_#{version}.pkg"
+  pkg "PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}"
 end
