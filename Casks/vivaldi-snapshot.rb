a2dff933a9c8a0481255ae592b8b6a5417484b31
Hannes Braun <hannesbraun@mail.de>
2021-11-30 18:17:09 +0100

    Update vivaldi-snapshot from 5.0.2497.10 to 5.0.2497.14

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask "vivaldi-snapshot" do
-  version "5.0.2497.10"
-  sha256 "0097a082ec2e12738826438c1492c7121bbc26cf1fac5bacf1e22dbc4004c396"
+  version "5.0.2497.14"
+  sha256 "162ee6b97fc2fe3290c05a721d1dd89a3bd9e30c30ed2abc5b91acd95ff428ac"
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
