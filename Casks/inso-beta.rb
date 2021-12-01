11893b065d5bf95e1a53948a5c2fda060a76fa96
Isa Farnik <if_then@hey.com>
2021-11-30 16:01:31 -0800

    Rename `inso` from "alpha to "beta" (#12685)
    * rename inso prerelease cask files to beta
    
    * target beta versions of inso

--- Casks/inso-beta.rb
+++ Casks/inso-beta.rb
@@ -1,6 +1,6 @@
-cask "inso-alpha" do
-  version "2.4.0-alpha.1"
-  sha256 "e62af5751cb985f27e103eb4546c601082e3b4072bd530e0e1f9d04c7193f714"
+cask "inso-beta" do
+  version "2.4.1-beta.0"
+  sha256 "389fa642f5e63a5e3880e226b55c70c3b57b7da3c6c0384de65878e9e9384a6e"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
@@ -11,7 +11,7 @@ cask "inso-alpha" do
   livecheck do
     url "https://github.com/Kong/insomnia/releases"
     strategy :page_match
-    regex(/href=.*?inso-macos-(?:latest-)*(\d+(?:\.\d+)+[._-](?:alpha|beta)[._-]\d*)\.zip/i)
+    regex(/href=.*?inso-macos-(?:latest-)*(\d+(?:\.\d+)+[._-](?:beta)[._-]\d*)\.zip/i)
   end
 
   conflicts_with cask: "inso"
