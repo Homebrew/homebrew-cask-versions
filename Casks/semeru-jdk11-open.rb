c5a704e65d6349f6b64cb03f31917985fe7a66a0
Yihe Li <winmikedows@hotmail.com>
2021-12-01 22:37:59 +0800

    Add IBM Semeru OpenJDK 8 & 11 (LTS) (#12690)
    * Add: IBM Semeru JDK 8 & 11
    
    * FIX: fetch by github_latest instead

---
+++ Casks/semeru-jdk11-open.rb
@@ -0,0 +1,24 @@
+cask "semeru-jdk11-open" do
+  version "11.0.13+8,openj9-0.29.0"
+  sha256 "19787a8728a2cb2d264c7656a5f494be0874046157610b6b248f6d46cc12a592"
+
+  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.before_comma}_#{version.after_comma}/ibm-semeru-open-jdk_x64_mac_#{version.before_comma.tr("+", "_")}_#{version.after_comma}.pkg",
+      verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
+  name "IBM Semeru Runtime (JDK 11) Open Edition"
+  desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
+  homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
+
+  livecheck do
+    url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases"
+    strategy :github_latest do |page|
+      match = page.match(%r{href=.*?/tag/jdk[._-](\d+(?:[._+]\d+)+)[._-]([^&]+)&quot;}i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  pkg "ibm-semeru-open-jdk_x64_mac_#{version.before_comma.tr("+", "_")}_#{version.after_comma}.pkg"
+
+  uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
+end
---
+++ Casks/semeru-jdk8-open.rb
@@ -0,0 +1,24 @@
+cask "semeru-jdk8-open" do
+  version "8u312-b07,openj9-0.29.0"
+  sha256 "f3ce369d3627bce344c9c5d2d76af3ae5b8c8f59c642f04c669d1e4269c29bba"
+
+  url "https://github.com/ibmruntimes/semeru8-binaries/releases/download/jdk#{version.before_comma}_#{version.after_comma}/ibm-semeru-open-jdk_x64_mac_#{version.before_comma.tr("-", "")}_#{version.after_comma}.pkg",
+      verified: "github.com/ibmruntimes/semeru8-binaries/"
+  name "IBM Semeru Runtime (JDK 8) Open Edition"
+  desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
+  homepage "https://developer.ibm.com/languages/java/semeru-runtimes"
+
+  livecheck do
+    url "https://github.com/ibmruntimes/semeru8-binaries/releases"
+    strategy :github_latest do |page|
+      match = page.match(%r{href=.*?/tag/jdk(\d+u\d+)[._-](b\d+)[._-]([^&]+)&quot;}i)
+      next if match.blank?
+
+      "#{match[1]}-#{match[2]},#{match[3]}"
+    end
+  end
+
+  pkg "ibm-semeru-open-jdk_x64_mac_#{version.before_comma.tr("-", "")}_#{version.after_comma}.pkg"
+
+  uninstall pkgutil: "net.ibm-semeru-open.8.jdk"
+end
