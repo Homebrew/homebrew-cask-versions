02597e90240adabab3df60e02ada165874f7b982
Julian Pawlowski <jpawlowski@users.noreply.github.com>
2021-12-01 11:15:38 +0100

    Better use Better use profile.ps1 for all hosts

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -33,6 +33,6 @@ cask "powershell-preview" do
 
   caveats <<~EOS
     To use Homebrew in PowerShell, set:
-      Add-Content -Path $PROFILE -Value '$(#{HOMEBREW_PREFIX}/bin/brew shellenv) | Invoke-Expression'
+      Add-Content -Path $PROFILE.CurrentUserAllHosts -Value '$(#{HOMEBREW_PREFIX}/bin/brew shellenv) | Invoke-Expression'
   EOS
 end
