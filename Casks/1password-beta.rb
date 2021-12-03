df430e37eb7b7cdd0e4923846ba8207df4072d39
owine <owine@users.noreply.github.com>
2021-12-03 15:00:19 -0600

    Update 1password-beta from 8.5.0-17.BETA to 8.5.0-31.BETA

--- Casks/1password-beta.rb
+++ Casks/1password-beta.rb
@@ -1,12 +1,12 @@
 cask "1password-beta" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
-  version "8.5.0-17.BETA"
+  version "8.5.0-31.BETA"
 
   if Hardware::CPU.intel?
-    sha256 "929f352a6b7fda15f94420dde32ed8d44be9d0079ec01717c866ae2630c4c268"
+    sha256 "c9a740bda0b4c523e6b61111c61b729c237a0ed3a72445567a6f76b3e20ac865"
   else
-    sha256 "8f68f5751cb916c5a8956585e92b0446cbd341479c3a401599e0c4fd303667a7"
+    sha256 "4954da628d47e96ce86a86abbee4f63cf814a642c1135e75848b416c9bcfd9d3"
   end
 
   url "https://downloads.1password.com/mac/1Password-#{version}-#{arch}.zip"
