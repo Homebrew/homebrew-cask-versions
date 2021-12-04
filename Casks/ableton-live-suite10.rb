b6201c15f55daa239e6d98d89f70661b461ba026
Hannes Braun <hannesbraun@mail.de>
2021-12-04 15:06:33 +0100

    Update ableton-live-suite10 from 10.1.41 to 10.1.42

--- Casks/ableton-live-suite10.rb
+++ Casks/ableton-live-suite10.rb
@@ -1,6 +1,6 @@
 cask "ableton-live-suite10" do
-  version "10.1.41"
-  sha256 "5a0149933c0197e1c4a3f1374d00a857da354dd425f1f7365d70784356c66210"
+  version "10.1.42"
+  sha256 "2143722cfd1e7ef17742802baed7e398da2d8851eb855cafa993c688b76b0bd8"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_64.dmg"
   appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
