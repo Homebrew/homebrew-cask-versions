0eb7dc62474ad48085f3a1982e9d5b0b290ac0b9
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-09 07:20:30 +0800

    Update safari-technology-preview.rb

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,7 +1,7 @@
 cask "safari-technology-preview" do
   if MacOS.version <= :big_sur
-    version "e05f8ee9c1c650bb69eb051c053fa53cf45c1bbc1e3d89f8b1264cc49da244ee"
-    sha256 "156788fafc935b2d26ff6ba5725b6e960c31c826306c4e28f1ff3869ac2a4ab6"
+    version "136,002-40417-20211208-C267452B-9B20-410C-83B9-D5740E745638"
+    sha256 "e05f8ee9c1c650bb69eb051c053fa53cf45c1bbc1e3d89f8b1264cc49da244ee"
   else
     version "136,002-41283-20211208-8C6426E9-436F-4CBF-858C-D2486B620976"
     sha256 "3368c2fa034ae81705f9e2fd75333f6a3c68a71eea5097acc591da24b773f257"
