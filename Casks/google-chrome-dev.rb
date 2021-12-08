9a8a67bdf3645c98afd67b4141ab09dc8d4be87d
miccal <miccal.matthews@gmail.com>
2021-12-08 09:26:14 +0800

    Update google-chrome-dev from 98.0.4736.0 to 98.0.4750.0

--- Casks/google-chrome-dev.rb
+++ Casks/google-chrome-dev.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-dev" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "98.0.4736.0"
+  version "98.0.4750.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}dev/googlechromedev.dmg"
