2797da3f433ab7d0ffa4f41fd672ade71678d63e
Hannes Braun <hannesbraun@mail.de>
2021-12-09 12:46:24 +0100

    Update microsoft-edge-beta from 97.0.1072.21 to 97.0.1072.28

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge-beta" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069439" : "2099618"
 
-  version "97.0.1072.21"
+  version "97.0.1072.28"
 
   if Hardware::CPU.intel?
-    sha256 "0a5cdea5b0bf45110bd294dc15494c2256eed6c73bc5d963e896d05169d8ce98"
+    sha256 "986d2ae6a332b40f27cc32c3652a23829d086460d48d38d1462e621038836385"
   else
-    sha256 "4623e5d9225a2f5795e2f95868914f125f72f72bb1930362e2845f7cb6fd9f2a"
+    sha256 "1a4ea94fabb3b1d7c1d0ad32ad72acd1673a71d4233b119ba38ecc73e4c08ff3"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
