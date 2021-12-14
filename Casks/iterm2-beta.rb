ef08637a3eff23f9812d03120ab1ad5e9c3d0647
owine <owine@users.noreply.github.com>
2021-12-14 15:45:10 -0600

    Update iterm2-beta from 3.5.0beta3 to 3.5.0beta4 (#12805)
    * Update iterm2-beta from 3.5.0beta3 to 3.5.0beta4
    
    * Update iterm2-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 cask "iterm2-beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.5.0beta3"
-  sha256 "73a993e070ea2bbcfebda7190f7210a22ce9986d91dfb37f80fbbca6f0c367d3"
+  version "3.5.0beta4"
+  sha256 "91792cce595cc90c1cc4be77259f75aa80f1bea084b73c4ed9d4ef478c2a6425"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"
@@ -24,9 +24,9 @@ cask "iterm2-beta" do
   app "iTerm.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*",
     "~/Library/Application Support/iTerm",
     "~/Library/Application Support/iTerm2",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*",
     "~/Library/Caches/com.googlecode.iterm2",
     "~/Library/Preferences/com.googlecode.iterm2.plist",
     "~/Library/Saved Application State/com.googlecode.iterm2.savedState",
