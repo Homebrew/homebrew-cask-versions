4014e05add206c4a8208ace7b8c1ad224122866d
Miccal Matthews <miccal.matthews@gmail.com>
2021-12-15 04:56:46 +0800

    Update visual-studio-code-insiders.rb

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -9,7 +9,7 @@ cask "visual-studio-code-insiders" do
     sha256 "21903b507ea06d46ce0e2c6321232fe865eb84478b080554a60038e13c17df58"
   end
 
-  url "https://az764295.vo.msecnd.net/insider/#{version.after_comma}/VSCode-#{arch}.zip",
+  url "https://az764295.vo.msecnd.net/insider/#{version.csv.second}/VSCode-#{arch}.zip",
       verified: "az764295.vo.msecnd.net/insider/"
   name "Microsoft Visual Studio Code"
   name "Visual Studio Code Insiders"
@@ -33,14 +33,14 @@ cask "visual-studio-code-insiders" do
   binary "#{appdir}/Visual Studio Code - Insiders.app/Contents/Resources/app/bin/code", target: "code-insiders"
 
   zap trash: [
+    "~/.vscode-insiders",
     "~/Library/Application Support/Code - Insiders",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.vscodeinsiders.sfl*",
     "~/Library/Caches/Code - Insiders",
-    "~/Library/Caches/com.microsoft.VSCodeInsiders",
     "~/Library/Caches/com.microsoft.VSCodeInsiders.ShipIt",
+    "~/Library/Caches/com.microsoft.VSCodeInsiders",
     "~/Library/Preferences/com.microsoft.VSCodeInsiders.helper.plist",
     "~/Library/Preferences/com.microsoft.VSCodeInsiders.plist",
     "~/Library/Saved Application State/com.microsoft.VSCodeInsiders.savedState",
-    "~/.vscode-insiders",
   ]
 end
