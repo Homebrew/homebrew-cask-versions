8b1ddf9033bc880cce50460cee445c076292389a
Hannes Braun <hannesbraun@mail.de>
2021-12-21 14:53:22 +0100

    Update qgis-ltr from 3.16.14 to 3.16.15

--- Casks/qgis-ltr.rb
+++ Casks/qgis-ltr.rb
@@ -1,6 +1,6 @@
 cask "qgis-ltr" do
-  version "3.16.14,20211121_134155"
-  sha256 "376f336ede47a9fc3eb6fbb85612140570d5dda015710e96140e6240a1cf583a"
+  version "3.16.15,20211217_170815"
+  sha256 "b27ba912c537c9bd6d2639967d805ce87a4f12d58d8201aaf1b6c3a990dbea6d"
 
   url "https://qgis.org/downloads/macos/ltr/qgis_ltr_final-#{version.before_comma.dots_to_underscores}_#{version.after_comma}.dmg"
   name "QGIS LTR"
