7a3456d5028ca70887cc9d3e76fa2931cf5591e4
owine <owine@users.noreply.github.com>
2021-12-21 16:50:24 -0600

    Update signal-beta from 5.27.0-beta.2 to 5.27.0-beta.3 (#12876)

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,12 +1,12 @@
 cask "signal-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.27.0-beta.2"
+  version "5.27.0-beta.3"
 
   if Hardware::CPU.intel?
-    sha256 "63a28fd2a0b23289e9924bc1275963babe3f4dde1479a2c318c0ceb9871c7bcb"
+    sha256 "ab367b67464af8aadabbdbabc5b9f140fd9b8e2da085557be76e42c06001729c"
   else
-    sha256 "b682a5c95d506ea6eac65a2fe51bb70e0d909289d858da1ca14edbfd4968a373"
+    sha256 "2bc41cdb475f5561ae1ac001f6dec8e6dc8a80a46124bc90d5bbaf5fd3c554d5"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
