19a5494f773c19f59b34c238fe96677b452e087f
Sky.Gu <5527811+Sky-Gu@users.noreply.github.com>
2021-12-27 10:26:18 +0800

    Update temurin8 sha256 value

--- Casks/temurin8.rb
+++ Casks/temurin8.rb
@@ -1,6 +1,6 @@
 cask "temurin8" do
   version "8,312,07"
-  sha256 "231cb0450c603cc861ade707b1deb01a8cbe2efd6c93e49c1707f3d899f92a93"
+  sha256 "468bd64621e7fb2e1d80becfd8d53127a41182e283f94cb75a204ef00976adfb"
 
   url "https://github.com/adoptium/temurin8-binaries/releases/download/jdk#{version.csv[0]}u#{version.csv[1]}-b#{version.csv[2]}/OpenJDK#{version.csv[0]}U-jdk_x64_mac_hotspot_#{version.csv[0]}u#{version.csv[1]}b#{version.csv[2]}.pkg",
       verified: "github.com/adoptium/temurin8-binaries/"
