b036481857af972a3d145947fc21d1f160f31abd
Hannes Braun <hannesbraun@mail.de>
2022-01-01 08:37:14 +0100

    Update mi-beta from 3.6b1 to 3.6b2 (#12919)

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -1,6 +1,6 @@
 cask "mi-beta" do
-  version "3.6b1,1085"
-  sha256 "d9f1ab054d66128bf3671f6dfe71670a4fa5c36004c7362d1eea9bb7f7cdb349"
+  version "3.6b2,1098"
+  sha256 "fd9dbbb65e1b41259b46f5a0a0619944c80a812ba1643930e196f7af0c4b733a"
 
   url "https://www.mimikaki.net/download/mi#{version.before_comma}.dmg"
   name "mi"
