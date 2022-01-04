25c9f922e0dc7b86da4fa8cf8b54a7b711e02917
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-04 10:06:44 +0800

    Update aerial-beta.rb

--- Casks/aerial-beta.rb
+++ Casks/aerial-beta.rb
@@ -8,23 +8,24 @@ cask "aerial-beta" do
   desc "Apple TV Aerial screensaver"
   homepage "https://aerialscreensaver.github.io/"
 
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^(\d+(?:\.\d+)*beta\d+)$/i)
+  end
+
   conflicts_with cask: "aerial"
   depends_on macos: ">= :sierra"
 
   screen_saver "Aerial.saver"
 
   zap trash: [
-    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Application Support/Aerial",
-    "~/Library/Screen Savers/Aerial.saver",
-    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Preferences/"\
-    "ByHost/com.JohnCoates.Aerial*.plist",
-    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Caches/Aerial",
-    "/Library/Screen Savers/Aerial.saver",
-    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver.x86-64/Data/Library/Caches/Aerial",
-    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver.x86-64/Data/Library/Application Support/"\
-    "Aerial",
-    "~/Library/Preferences/ByHost/com.JohnCoates.Aerial*",
     "~/Library/Application Support/Aerial",
     "~/Library/Caches/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver.x86-64/Data/Library/Caches/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Application Support/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Caches/Aerial",
+    "~/Library/Preferences/ByHost/com.JohnCoates.Aerial*",
+    "~/Library/Screen Savers/Aerial.saver",
   ]
 end
