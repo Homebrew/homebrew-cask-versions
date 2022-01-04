35aa064ec9efd94d47931a7265187e86d2fc81e9
David <notDavid@users.noreply.github.com>
2022-01-04 04:07:25 +0100

    Add Aerial-beta 2.3.4beta8 (#12926)
    * Add Aerial-beta 2.3.4beta8
    
    * Update aerial-beta.rb
    
    * Update aerial-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/aerial-beta.rb
@@ -0,0 +1,31 @@
+cask "aerial-beta" do
+  version "2.3.4beta8"
+  sha256 "59140497c9b74dd30176974babdf2a1d750d414336fb18219b8727e6c6fc99ec"
+
+  url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip",
+      verified: "github.com/JohnCoates/Aerial/"
+  name "Aerial Screensaver"
+  desc "Apple TV Aerial screensaver"
+  homepage "https://aerialscreensaver.github.io/"
+
+  livecheck do
+    url :url
+    strategy :git
+    regex(/^v?(\d+(?:\.\d+)*beta\d+)$/i)
+  end
+
+  conflicts_with cask: "aerial"
+  depends_on macos: ">= :sierra"
+
+  screen_saver "Aerial.saver"
+
+  zap trash: [
+    "~/Library/Application Support/Aerial",
+    "~/Library/Caches/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver.x86-64/Data/Library/Caches/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Application Support/Aerial",
+    "~/Library/Containers/com.apple.ScreenSaver.Engine.legacyScreenSaver/Data/Library/Caches/Aerial",
+    "~/Library/Preferences/ByHost/com.JohnCoates.Aerial*",
+    "~/Library/Screen Savers/Aerial.saver",
+  ]
+end
