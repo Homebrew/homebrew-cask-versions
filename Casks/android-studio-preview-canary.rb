a5ade80945518c8d3ff1a8b4d49e286a890a7b6c
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-06 13:25:08 +0800

    Update android-studio-preview-canary.rb (#12941)

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -20,19 +20,17 @@ cask "android-studio-preview-canary" do
     regex(%r{href=.*?/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.zip}i)
   end
 
-  conflicts_with cask: "android-studio-preview-beta"
-
   app "Android Studio Preview.app"
 
   zap trash: [
+    "~/.android",
     "~/Library/Android/sdk",
     "~/Library/Application Support/Google/AndroidStudioPreview#{version.major_minor}",
     "~/Library/Caches/Google/AndroidStudioPreview#{version.major_minor}",
     "~/Library/Logs/Google/AndroidStudioPreview#{version.major_minor}",
-    "~/Library/Preferences/com.google.android.studio-EAP.plist",
     "~/Library/Preferences/com.android.Emulator.plist",
+    "~/Library/Preferences/com.google.android.studio-EAP.plist",
     "~/Library/Saved Application State/com.google.android.studio-EAP.savedState",
-    "~/.android",
   ],
       rmdir: [
         "~/AndroidStudioProjects",
