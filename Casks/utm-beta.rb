2b7d2d334c4145a9ac71a818c854844b3ab6758d
Shreyans Jain <shreyansthebest2007@gmail.com>
2022-01-07 00:03:24 +0530

    Add utm-beta

---
+++ Casks/utm-beta.rb
@@ -0,0 +1,24 @@
+cask "utm-beta" do
+  version "3.0.0"
+  sha256 "e6556a1c04ff8b17185f8f0be320375a7926d7dbc82431cf81571c7a4efd3ccb"
+
+  url "https://github.com/utmapp/UTM/releases/download/v#{version}/UTM.dmg",
+      verified: "github.com/utmapp/UTM/"
+  name "UTM"
+  desc "Virtual machines UI using QEMU"
+  homepage "https://mac.getutm.app/"
+
+  app "UTM.app"
+
+  uninstall quit: "com.utmapp.UTM"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.utmapp.QEMUHelper",
+    "~/Library/Application Scripts/com.utmapp.UTM",
+    "~/Library/Containers/com.utmapp.QEMUHelper",
+    "~/Library/Containers/com.utmapp.UTM",
+    "~/Library/Group Containers/*.com.utmapp.UTM",
+    "~/Library/Preferences/com.utmapp.UTM.plist",
+    "~/Library/Saved Application State/com.utmapp.UTM.savedState",
+  ]
+end
