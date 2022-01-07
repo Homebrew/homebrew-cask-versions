5d8b299019631b915ccc88b377268bc34315b4d0
Hannes Braun <hannesbraun@mail.de>
2022-01-07 21:55:12 +0100

    Update signal-beta from 5.27.0-beta.3 to 5.28.0-beta.1 (#12958)

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,12 +1,12 @@
 cask "signal-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.27.0-beta.3"
+  version "5.28.0-beta.1"
 
   if Hardware::CPU.intel?
-    sha256 "ab367b67464af8aadabbdbabc5b9f140fd9b8e2da085557be76e42c06001729c"
+    sha256 "6e51340951746fe9effc06c07cd8a6a6944e59973dbd677da14898fa5035319b"
   else
-    sha256 "2bc41cdb475f5561ae1ac001f6dec8e6dc8a80a46124bc90d5bbaf5fd3c554d5"
+    sha256 "5dd5006b8695630769a234e69fb58dfca82a9c2a6b953dc06a7f2730f4026b55"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
