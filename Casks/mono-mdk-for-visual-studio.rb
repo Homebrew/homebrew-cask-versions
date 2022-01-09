fe36e011ad86d618510db98ff391903c8a9c2025
Bevan Kay <email@bevankay.me>
2022-01-09 12:09:47 +1100

    Update mono-mdk-for-visual-studio from 6.12.0.163 to 6.12.0.164 (#12971)

--- Casks/mono-mdk-for-visual-studio.rb
+++ Casks/mono-mdk-for-visual-studio.rb
@@ -1,6 +1,6 @@
 cask "mono-mdk-for-visual-studio" do
-  version "6.12.0.163"
-  sha256 "ad66c9b2ce7707e7559564dfede1540e7564190095315afabd836fad140d38c6"
+  version "6.12.0.164"
+  sha256 "8ce0feccb2b1bb0605346dddf7d4bce396de11b3da0635196f8b4da5a1802bc1"
 
   url "https://download.mono-project.com/archive/#{version.major_minor_patch}/macos-10-universal/MonoFramework-MDK-#{version}.macos10.xamarin.universal.pkg"
   name "Mono"
