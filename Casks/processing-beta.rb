60855d3562a952fca08e1d6fb5308e0a26d1ee90
Marten Biehl <marten.biehl@pwc.com>
2022-01-10 15:28:17 +0100

    Add processing-beta for Processing 4 Beta

---
+++ Casks/processing-beta.rb
@@ -0,0 +1,29 @@
+cask "processing-beta" do
+  version "4.0b2,1277"
+  sha256 "80b8238d717976894f55b20461395bc5448467cbe60f0e15ee8cb19436ed2d87"
+
+  url "https://github.com/processing/processing4/releases/download/processing-#{version.after_comma}-#{version.before_comma}/processing-#{version.before_comma}-macosx.zip",
+      verified: "github.com/processing/processing4/"
+  name "Processing Beta"
+  homepage "https://processing.org/"
+  desc "Processing is a flexible software sketchbook and a language for learning how to code within the context of the visual arts."
+
+  livecheck do
+    url :url
+    regex(%r{href=.*?tree/processing[._-](\d+)[._-]v?(\d+(?:\.\d+b\d)+)}i)
+    strategy :github_latest do |page|
+      page.scan(regex)
+          .map { |match| "#{match[1]},#{match[0]}" }
+    end
+  end
+
+  app "Processing.app", target: "Processing Beta.app"
+
+  uninstall quit: "org.processing.app"
+
+  zap trash: [
+    "~/Library/Processing",
+    "~/Preferences/org.processing.app.plist",
+    "~/Preferences/processing.app.tools.plist",
+  ]
+end
