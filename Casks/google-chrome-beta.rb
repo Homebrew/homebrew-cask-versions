a014da9a796f3714d7c3702d4f72b0ba41d8acf1
owine <owine@users.noreply.github.com>
2022-01-10 12:44:41 -0600

    Update google-chrome-beta from 97.0.4692.71 to 98.0.4758.48

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-beta" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "97.0.4692.71"
+  version "98.0.4758.48"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}beta/googlechromebeta.dmg"
