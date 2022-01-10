8c2d15daf86ed8a600292dc69993f1d93234f24a
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-10 11:46:05 +0800

    Update mono-mdk-for-visual-studio.rb

--- Casks/mono-mdk-for-visual-studio.rb
+++ Casks/mono-mdk-for-visual-studio.rb
@@ -7,6 +7,7 @@ cask "mono-mdk-for-visual-studio" do
   desc "Open source implementation of Microsoft's .NET Framework"
   homepage "https://www.mono-project.com/"
 
+  # The stable version is that listed on the download page.
   livecheck do
     url "https://www.mono-project.com/download/vs/#download-mac"
     regex(/MonoFramework-MDK-(\d+(?:\.\d+)+).macos10.xamarin.universal\.pkg/i)
