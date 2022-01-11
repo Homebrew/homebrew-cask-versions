92746311f5956b37bdd98eb22821b664e5d005e5
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-12 07:39:25 +0800

    Update vmware-fusion-tech-preview.rb

--- Casks/vmware-fusion-tech-preview.rb
+++ Casks/vmware-fusion-tech-preview.rb
@@ -29,9 +29,9 @@ cask "vmware-fusion-tech-preview" do
 
   app "VMware Fusion Tech Preview.app"
   if Hardware::CPU.intel?
-    binary "#{appdir}/VMware Fusion Tech Preview.app/Contents/Library/VMware OVF Tool/ovftool"
     binary "#{appdir}/VMware Fusion Tech Preview.app/Contents/Library/vkd/bin/vctl"
     binary "#{appdir}/VMware Fusion Tech Preview.app/Contents/Library/vmrest"
+    binary "#{appdir}/VMware Fusion Tech Preview.app/Contents/Library/VMware OVF Tool/ovftool"
   end
   binary "#{appdir}/VMware Fusion Tech Preview.app/Contents/Library/vmnet-bridge"
   binary "#{appdir}/VMware Fusion Tech Preview.app/Contents/Library/vmnet-cfgcli"
@@ -67,36 +67,38 @@ cask "vmware-fusion-tech-preview" do
     set_ownership "#{appdir}/VMware Fusion Tech Preview.app"
   end
 
-  uninstall delete: "/etc/paths.d/com.vmware.fusion.public"
-
-  zap trash: [
+  uninstall delete: [
+    "/etc/paths.d/com.vmware.fusion.public",
     "/Library/Application Support/VMware",
     "/Library/Logs/VMware Fusion Services.log",
     "/Library/Logs/VMware USB Arbitrator Service.log",
     "/Library/Logs/VMware",
     "/Library/Preferences/VMware Fusion",
+  ]
+
+  zap trash: [
+    "~/.nautilus",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.vmware.fusion.sfl*",
-    "~/Library/Application Support/VMware Fusion",
     "~/Library/Application Support/VMware Fusion Applications Menu",
+    "~/Library/Application Support/VMware Fusion",
     "~/Library/Caches/com.vmware.fusion",
-    "~/Library/Logs/VMware Fusion",
     "~/Library/Logs/VMware Fusion Applications Menu",
+    "~/Library/Logs/VMware Fusion",
     "~/Library/Logs/VMware Graphics Service.log",
     "~/Library/Logs/VMware",
-    "~/Library/Preferences/VMware Fusion",
-    "~/Library/Preferences/com.vmware.fusion.plist",
+    "~/Library/Preferences/com.vmware.fusion.LSSharedFileList.plist.lockfile",
+    "~/Library/Preferences/com.vmware.fusion.LSSharedFileList.plist",
     "~/Library/Preferences/com.vmware.fusion.plist.lockfile",
+    "~/Library/Preferences/com.vmware.fusion.plist",
     "~/Library/Preferences/com.vmware.fusionApplicationsMenu.helper.plist",
     "~/Library/Preferences/com.vmware.fusionApplicationsMenu.plist",
-    "~/Library/Preferences/com.vmware.fusionDaemon.plist",
     "~/Library/Preferences/com.vmware.fusionDaemon.plist.lockfile",
-    "~/Library/Preferences/com.vmware.fusionStartMenu.plist",
+    "~/Library/Preferences/com.vmware.fusionDaemon.plist",
     "~/Library/Preferences/com.vmware.fusionStartMenu.plist.lockfile",
-    "~/Library/Preferences/com.vmware.fusion.LSSharedFileList.plist",
-    "~/Library/Preferences/com.vmware.fusion.LSSharedFileList.plist.lockfile",
+    "~/Library/Preferences/com.vmware.fusionStartMenu.plist",
+    "~/Library/Preferences/VMware Fusion",
     "~/Library/Saved Application State/com.vmware.fusion.savedState",
     "~/Library/WebKit/com.vmware.fusion",
-    "~/.nautilus",
   ]
 
   caveats do
