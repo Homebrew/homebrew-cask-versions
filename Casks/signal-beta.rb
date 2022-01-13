2aa578eeff0c895b6160705dee6b42ce8882e70b
LanikSJ <LanikSJ@users.noreply.github.com>
2022-01-12 17:14:40 -0800

    Update signal-beta from 5.28.0-beta.1 to 5.29.0-beta.1 (#13012)

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,12 +1,12 @@
 cask "signal-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.28.0-beta.1"
+  version "5.29.0-beta.1"
 
   if Hardware::CPU.intel?
-    sha256 "6e51340951746fe9effc06c07cd8a6a6944e59973dbd677da14898fa5035319b"
+    sha256 "d1d9c72040157c22eab5b2adca5f681cafac8f6f55a986de0bea24300180a19a"
   else
-    sha256 "5dd5006b8695630769a234e69fb58dfca82a9c2a6b953dc06a7f2730f4026b55"
+    sha256 "2307368272e7360b25943499ebfa88c3dc03ca28c9bb4c4e197a64f97ac7b2a1"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
