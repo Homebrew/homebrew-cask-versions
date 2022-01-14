91f029caf44153257cb7d05d34e36df5593740e6
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-15 07:12:05 +0800

    Update discord-ptb.rb

--- Casks/discord-ptb.rb
+++ Casks/discord-ptb.rb
@@ -3,7 +3,7 @@ cask "discord-ptb" do
   sha256 "2d2eca13155790ebfc3bf1ab8ac3cc6c1c60fe9c280d720ea3574af70935c81f"
 
   url "https://dl-ptb.discordapp.net/apps/osx/#{version}/DiscordPTB.dmg",
-      verified: "dl-ptb.discordapp.net/"
+      verified: "dl-ptb.discordapp.net/apps/osx/"
   name "Discord PTB"
   desc "Voice and text chat software"
   homepage "https://discord.com/"
@@ -18,9 +18,9 @@ cask "discord-ptb" do
   app "Discord PTB.app"
 
   zap trash: [
+    "~/Library/Application Support/com.hnc.DiscordPTB.ShipIt",
+    "~/Library/Caches/com.hnc.DiscordPTB",
     "~/Library/Preferences/com.hnc.DiscordPTB.plist",
     "~/Library/Saved Application State/com.hnc.DiscordPTB.savedState",
-    "~/Library/Caches/com.hnc.DiscordPTB",
-    "~/Library/Application Support/com.hnc.DiscordPTB.ShipIt",
   ]
 end
