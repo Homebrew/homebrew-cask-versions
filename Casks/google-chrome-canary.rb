c3a72d15e37a1add6ee2742357c013d5cb4db292
Hannes Braun <hannesbraun@mail.de>
2022-01-14 09:07:57 +0100

    Update google-chrome-canary from 99.0.4826.0 to 99.0.4828.0 (#13027)

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "99.0.4826.0"
+  version "99.0.4828.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
