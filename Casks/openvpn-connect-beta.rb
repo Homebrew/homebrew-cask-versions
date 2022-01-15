1596f73866d6af71ddf84c48b104f5ef2de96336
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-16 01:41:51 +0300

    Update openvpn-connect-beta.rb

--- Casks/openvpn-connect-beta.rb
+++ Casks/openvpn-connect-beta.rb
@@ -6,18 +6,34 @@ cask "openvpn-connect-beta" do
   appcast "https://openvpn.net/client-connect-vpn-for-mac-os/",
           must_contain: version.major_minor_patch
   name "OpenVPN Connect"
+  desc "Client program for the OpenVPN Access Server"
   homepage "https://openvpn.net/client-connect-vpn-for-mac-os/"
+
+  conflicts_with cask: "openvpn-connect"
+  depends_on macos: ">= :yosemite"
 
   pkg "OpenVPN_Connect_#{version.major_minor_patch.dots_to_underscores}(#{version.split(".").last})_Installer_signed.pkg"
 
-  uninstall script:  {
-    executable: "/Applications/OpenVPN Connect/Uninstall OpenVPN Connect.app/Contents/Resources/remove.sh",
-    sudo:       true,
-  },
-            pkgutil: [
-              "org.openvpn.client.pkg",
+  uninstall quit:      "org.openvpn.client.app",
+            launchctl: [
+              "org.openvpn.client",
+              "org.openvpn.helper",
+            ],
+            script:    {
+              executable: "/Applications/OpenVPN Connect/Uninstall OpenVPN Connect.app/Contents/Resources/remove.sh",
+              sudo:       true,
+            },
+            pkgutil:   [
               "org.openvpn.client_framework.pkg",
               "org.openvpn.client_launch.pkg",
               "org.openvpn.client_uninstall.pkg",
+              "org.openvpn.client.pkg",
+              "org.openvpn.helper_framework.pkg",
+              "org.openvpn.helper_launch.pkg",
             ]
+
+  zap trash: [
+    "~/Library/Application Support/OpenVPN Connect",
+    "~/Library/Preferences/org.openvpn.client.app.plist",
+  ]
 end
