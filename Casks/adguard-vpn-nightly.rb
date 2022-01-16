354d0fdb6b8a81420ae116b176ac2f107428a415
Aleksandr Fedotov <mezonix@gmail.com>
2022-01-16 13:20:36 +0300

    Add adguard-vpn-nightly.rb

---
+++ Casks/adguard-vpn-nightly.rb
@@ -0,0 +1,35 @@
+cask "adguard-vpn-nightly" do
+  version :latest
+  sha256 :no_check
+
+  url "https://static.adguard-vpn.com/mac/nightly/AdGuardVPN.dmg"
+  name "AdGuard VPN"
+  desc "VPN for privacy and security"
+  homepage "https://adguard-vpn.com/"
+
+  conflicts_with cask: "adguard-vpn"
+  depends_on macos: ">= :sierra"
+
+  pkg "AdGuard VPN.pkg"
+
+  uninstall quit:      "com.adguard.mac.vpn",
+            launchctl: "com.adguard.mac.vpn.tun-helper",
+            pkgutil:   "com.adguard.mac.vpn-pkg",
+            delete:    [
+              "/Library/Application Support/AdGuard Software/com.adguard.mac.vpn",
+              "/Library/Application Support/com.adguard.mac.vpn",
+            ],
+            rmdir:     "/Library/Application Support/AdGuard Software"
+
+  zap trash: [
+    "/Library/Logs/com.adguard.mac.vpn",
+    "~/Library/Application Scripts/*.com.adguard.mac",
+    "~/Library/Application Scripts/com.adguard.mac.vpn.launchatlogin",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.adguard.mac.vpn.launchatlogin.sfl*",
+    "~/Library/Caches/com.adguard.mac.vpn",
+    "~/Library/Containers/com.adguard.mac.vpn.launchatlogin",
+    "~/Library/Group Containers/*.com.adguard.mac",
+    "~/Library/HTTPStorages/com.adguard.mac.vpn",
+    "~/Library/Preferences/com.adguard.mac.vpn.plist",
+  ]
+end
