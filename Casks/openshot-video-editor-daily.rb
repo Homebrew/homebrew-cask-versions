1a725a4e1601c5ae1666906fdb106979fd59cc2d
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 07:30:33 +0800

    Add openshot-video-editor-daily (#13057)
    * Create openshot-video-editor-daily.rb
    
    * Update openshot-video-editor-daily.rb
    
    * Update openshot-video-editor-daily.rb
    
    * Update openshot-video-editor-daily.rb
    
    * Update openshot-video-editor-daily.rb
    
    * Update openshot-video-editor-daily.rb
    
    * Update openshot-video-editor-daily.rb

---
+++ Casks/openshot-video-editor-daily.rb
@@ -0,0 +1,31 @@
+cask "openshot-video-editor-daily" do
+  version "2.6.1,8946-3c8dc71b-2b8b05c7"
+  sha256 "daac5ab0d46df7ed1fe20acf6626118628b6d7e888dcfd8cd5e95f0a3043434f"
+
+  url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-dev-daily-#{version.csv.second}-x86_64.dmg",
+      verified: "github.com/OpenShot/openshot-qt/"
+  name "OpenShot Video Editor (Daily Build)"
+  desc "Cross-platform video editor"
+  homepage "https://openshot.org/"
+
+  livecheck do
+    url "https://www.openshot.org/download/"
+    strategy :page_match do |page|
+      match = page.match(/OpenShot[._-]v?(\d+(?:\.\d+)+)[._-]dev[._-]daily[._-](.*)[._-]x86[._-]64\.dmg"/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  conflicts_with cask: "openshot-video-editor"
+  depends_on macos: ">= :catalina"
+
+  app "OpenShot Video Editor.app"
+
+  zap trash: [
+    "~/.openshot_qt",
+    "~/Library/Application Support/openshot",
+    "~/Library/Preferences/openshot-qt.plist",
+  ]
+end
