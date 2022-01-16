e4836af368d9d43ee84bf41dec00ab2631112543
Miccal Matthews <miccal.matthews@gmail.com>
2022-01-17 06:54:51 +0800

    Update openshot-video-editor-daily.rb

--- Casks/openshot-video-editor-daily.rb
+++ Casks/openshot-video-editor-daily.rb
@@ -1,6 +1,6 @@
 cask "openshot-video-editor-daily" do
   version "2.6.1,8946-3c8dc71b-2b8b05c7"
-  sha256 "da40d4361810bf131cd06a1afe71e9a7a15918f2b154e1399db7306b2a3b72d3"
+  sha256 "daac5ab0d46df7ed1fe20acf6626118628b6d7e888dcfd8cd5e95f0a3043434f"
 
   url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-dev-daily-#{version.csv.second}-x86_64.dmg",
       verified: "github.com/OpenShot/openshot-qt/"
