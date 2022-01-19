89d3f82165eccdfddf280742d059802e117b58df
Porkepix <porkepix@gmail.com>
2022-01-19 03:20:27 +0100

    Update netnewswire-beta from 6.0.3b2 to 6.1b2

--- Casks/netnewswire-beta.rb
+++ Casks/netnewswire-beta.rb
@@ -1,6 +1,6 @@
 cask "netnewswire-beta" do
-  version "6.0.3b2"
-  sha256 "f1d645cfd7499fc29258587e9edc6de467a9b63fec3d588dc6a6042d38e61900"
+  version "6.1b2"
+  sha256 "4483fee577a51ecacb1d06a7ba2c0d5c33aa993d822d0a4022b0f78eb0c8a389"
 
   url "https://github.com/brentsimmons/NetNewsWire/releases/download/mac-#{version}/NetNewsWire#{version}.zip",
       verified: "github.com/brentsimmons/NetNewsWire/"
