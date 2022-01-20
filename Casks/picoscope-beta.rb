db4dc815bf512a41089e0466469eabd50e74dca4
Constantin Dullo <constantin.dullo@me.com>
2022-01-20 08:42:23 +0100

    Update picoscope-beta from 7.0.88 to 7.0.90

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,6 +1,6 @@
 cask "picoscope-beta" do
-  version "7.0.88"
-  sha256 "c4e8cc6f94443957f3eb2057ea1626f34e87d46f893091bad74b0abc5d23bb6b"
+  version "7.0.90"
+  sha256 "84f335f0b4ef3f372fd294a01df1ca89875197ebe9cd159800be0eb8544c666b"
 
   url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}_TnM_Early_Access.pkg"
   name "PicoScope beta"
@@ -10,7 +10,7 @@ cask "picoscope-beta" do
   livecheck do
     url "https://oem.picotech.com/p#{version.major}beta/download/"
     strategy :page_match
-    regex(%r{href=.*?/PicoScope_(\d+(?:\.\d+)+)\_TnM_Early_Access.pkg}i)
+    regex(%r{href=.*?/PicoScope_(\d+(?:\.\d+)+)_TnM_Early_Access.pkg}i)
   end
 
   pkg "PicoScope_#{version}_TnM_Early_Access.pkg"
