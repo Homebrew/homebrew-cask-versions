0c5cfa9169ddf5721a980a2853d645285913dc07
Rulicz <26757514+rulicz@users.noreply.github.com>
2022-01-21 13:11:12 +0700

    Update corretto11 from 11.0.13.8.1 to 11.0.14.9.1 (#13100)
    * Update corretto11 from 11.0.13.8.1 to 11.0.14.9.1
    
    * Update corretto11.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,12 +1,18 @@
 cask "corretto11" do
-  version "11.0.13.8.1"
-  sha256 "84d8e57ddd934c00d6216ac413aed80f3405979b6feb7678e748ed41c2c8f093"
+  version "11.0.14.9.1"
+  sha256 "3d76035ea371806eb8e2c809da3f588f30464d5784d0c161d504f69a0bf3446f"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
-  appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.pkg"
   name "Amazon Corretto JDK"
   desc "OpenJDK distribution from Amazon"
   homepage "https://corretto.aws/"
+
+  livecheck do
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-x64\.pkg}i, 1]
+    end
+  end
 
   pkg "amazon-corretto-#{version}-macosx-x64.pkg"
 
