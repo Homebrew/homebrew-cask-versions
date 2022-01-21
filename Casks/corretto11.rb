f58588fdd20a51c010cda481614aa32d0198b8cd
Rulicz <26757514+rulicz@users.noreply.github.com>
2022-01-21 11:41:24 +0700

    Update corretto11 from 11.0.13.8.1 to 11.0.14.9.1

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,6 +1,6 @@
 cask "corretto11" do
-  version "11.0.13.8.1"
-  sha256 "84d8e57ddd934c00d6216ac413aed80f3405979b6feb7678e748ed41c2c8f093"
+  version "11.0.14.9.1"
+  sha256 "3d76035ea371806eb8e2c809da3f588f30464d5784d0c161d504f69a0bf3446f"
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
   appcast "https://www.macupdater.net/cgi-bin/check_urls/check_url_redirect.cgi?url=https://corretto.aws/downloads/latest/amazon-corretto-11-x64-macos-jdk.pkg"
