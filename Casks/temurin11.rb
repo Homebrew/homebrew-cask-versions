51e75785cbd369935e6af43591a9b1a6745b793c
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2022-01-22 17:37:38 +0300

    Update temurin11 from 11.0.13 to 11.0.14 (#13111)
    * Update temurin11 from 11.0.13 to 11.0.14
    
    * Update temurin11.rb
    
    Co-authored-by: Yusuf Kürşad Kaya <yusufkursad.kaya@trendyol.com>
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/temurin11.rb
+++ Casks/temurin11.rb
@@ -1,6 +1,6 @@
 cask "temurin11" do
-  version "11.0.13,8"
-  sha256 "f34e98dd585abb3f79b6ad650ea8fc36f19f39e79e0ef2ae5bc1c7d7cb20c717"
+  version "11.0.14,9"
+  sha256 "52329ec2174d825ccc645477a4dabfa80071b81662bd48dc1439072ba661f116"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
@@ -9,7 +9,7 @@ cask "temurin11" do
   homepage "https://adoptium.net/"
 
   livecheck do
-    url "https://api.adoptium.net/v3/assets/feature_releases/11/ga?architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=x64&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
     strategy :page_match do |page|
       JSON.parse(page).map do |release|
         match = release["release_name"].match(/^jdk-(\d+(?:\.\d+)+)\+(\d+(?:\.\d+)*)$/i)
