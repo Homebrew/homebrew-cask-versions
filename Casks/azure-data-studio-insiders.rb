0f2b8a535ce573addbb87e373777d0fdb3cb6280
Hannes Braun <hannesbraun@mail.de>
2022-01-28 12:32:04 +0100

    Update azure-data-studio-insiders from 1.35.0,d1b72e1b33155640a940affdac1c06ec3c0956a2 to 1.35.0,d23edbb3380911a072ad344a8f0ea02079e67d27

--- Casks/azure-data-studio-insiders.rb
+++ Casks/azure-data-studio-insiders.rb
@@ -1,6 +1,6 @@
 cask "azure-data-studio-insiders" do
-  version "1.35.0,d1b72e1b33155640a940affdac1c06ec3c0956a2"
-  sha256 "b194955e52fbd396b19f07066fee66781c3abcd4c6670b1d681ddcc26cdee170"
+  version "1.35.0,d23edbb3380911a072ad344a8f0ea02079e67d27"
+  sha256 "cb76bbcf9af82064e09b7b57e8f5b9ccad7ff288b40843f0ecdd8e6f9cc40c62"
 
   url "https://sqlopsbuilds.azureedge.net/insider/#{version.csv.second}/azuredatastudio-macos-#{version.csv.first}-insider.zip",
       verified: "sqlopsbuilds.azureedge.net/insider/"
