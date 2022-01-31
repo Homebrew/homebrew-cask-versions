0c90fe5df039297f41ef6a46d52955fe10334728
Porkepix <porkepix@gmail.com>
2022-01-31 11:22:23 +0100

    Update geogebra5 from 5.0.683.0 to 5.0.686.0

--- Casks/geogebra5.rb
+++ Casks/geogebra5.rb
@@ -1,6 +1,6 @@
 cask "geogebra5" do
-  version "5.0.683.0"
-  sha256 "4856cef06caba215a07595460df22dc3d022ae748ef9335d8e30caad1cd5c8b2"
+  version "5.0.686.0"
+  sha256 "e8c978ca612ecda9866da8503d1cee0b436d76b24302ec4f3db8beba61cd7915"
 
   url "https://download.geogebra.org/installers/#{version.major_minor}/GeoGebra-MacOS-Installer-withJava-#{version.dots_to_hyphens}.zip"
   name "GeoGebra"
