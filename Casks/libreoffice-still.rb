74e2e5630ac00924b46f9773334fdb03efc4372e
Clément L <porkepix@gmail.com>
2022-02-03 01:40:14 +0100

    Update libreoffice-still from 7.1.8 to 7.2.5 (#13203)
    * Update libreoffice-still from 7.1.8 to 7.2.5
    
    * Update libreoffice-still.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/libreoffice-still.rb
+++ Casks/libreoffice-still.rb
@@ -1,6 +1,6 @@
 cask "libreoffice-still" do
-  version "7.1.8"
-  sha256 "aae69f2e9b30bc5482a59cba011433145dc3cbf6339aa7d9e6ba8bf87cc53532"
+  version "7.2.5"
+  sha256 "0b7ef18ed08341ac6c15339fe9a161ad17f6b469009d987cfc7d50c628d12a4e"
 
   url "https://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86_64/LibreOffice_#{version}_MacOS_x86-64.dmg",
       verified: "download.documentfoundation.org/"
@@ -19,13 +19,13 @@ cask "libreoffice-still" do
   end
 
   conflicts_with cask: "libreoffice"
+  depends_on macos: ">= :yosemite"
 
   app "LibreOffice.app"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/gengal"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/regmerge"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/regview"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/senddoc"
-  binary "#{appdir}/LibreOffice.app/Contents/MacOS/ui-previewer"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/uno"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/unoinfo"
   binary "#{appdir}/LibreOffice.app/Contents/MacOS/unopkg"
@@ -43,8 +43,8 @@ cask "libreoffice-still" do
   end
 
   zap trash: [
-    "~/Library/Application Support/LibreOffice",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.libreoffice.script.sfl*",
+    "~/Library/Application Support/LibreOffice",
     "~/Library/Preferences/org.libreoffice.script.plist",
     "~/Library/Saved Application State/org.libreoffice.script.savedState",
   ]
