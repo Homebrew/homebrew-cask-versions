fa141eaad00d1d54da19e7f05df164074449cc2e
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-08 05:42:51 +0800

    Update and rename kindavim1-beta.rb to kindavim-beta.rb

--- Casks/kindavim-beta.rb
+++ Casks/kindavim-beta.rb
@@ -1,12 +1,14 @@
-cask "kindavim1-beta" do
-  version "1b25,0.9.25"
-  sha256 :no_check
+cask "kindavim-beta" do
+  version "1.0.0"
+  sha256 "4c94284b2a31f2e4959005ff0f771d65f5d4f9b69c3d5326fcd0359423eb361e"
 
-  url "https://github.com/godbout/kindaVim.theapp/releases/download/1b.25/kindaVim.zip", verified: "github.com/godbout/kindaVim.theapp"
+  url "https://github.com/godbout/kindaVim.theapp/releases/download/#{version}/kindaVim.zip",
+      verified: "github.com/godbout/kindaVim.theapp/"
   name "kindaVim"
   desc "Use Vim in input fields and non input fields"
   homepage "https://kindavim.app/"
 
+  conflicts_with cask: "kindavim"
   depends_on macos: ">= :monterey"
 
   app "kindaVim.app"
