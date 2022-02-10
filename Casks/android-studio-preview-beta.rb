cdf4cb870e4eb6db6681b01e7a2a3bb6a518042b
Hannes Braun <hannesbraun@mail.de>
2022-02-10 12:14:12 +0100

    Update android-studio-preview-beta from 2021.2.1.8 to 2021.2.1.9

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -1,12 +1,12 @@
 cask "android-studio-preview-beta" do
   arch = Hardware::CPU.intel? ? "mac" : "mac_arm"
 
-  version "2021.2.1.8"
+  version "2021.2.1.9"
 
   if Hardware::CPU.intel?
-    sha256 "d33a785ecb55a22063e3a74be48353b4b39e4b686f01e4576150c762bd55fc69"
+    sha256 "2f919e621a3df4c6ec424f3b6b6eb1627041e666ea7a9e475db21a6ee0c714f2"
   else
-    sha256 "32e523cfd99bf9fd14927f7a3c7d40779d50eb9426046703980e6bed5337db30"
+    sha256 "6c7c3e25f561937913b04ce7fd04e422c25e8371aae9216391210edb2d54ec63"
   end
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-#{arch}.zip",
