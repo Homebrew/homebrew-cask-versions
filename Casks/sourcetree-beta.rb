f99110e2c2765501480188407ce85549074db661
Steffen Matthischke <HeEAaD@users.noreply.github.com>
2022-02-10 16:13:54 +0100

    Update sourcetree-beta from 4.0.2b6_235 to 4.1.6b8_242 (#13275)
    * Update sourcetree-beta from 4.0.2b6_235 to 4.1.6b8_242
    
    * Update sourcetree-beta.rb
    
    * Update sourcetree-beta.rb
    
    * Update sourcetree-beta.rb
    
    * Update sourcetree-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/sourcetree-beta.rb
+++ Casks/sourcetree-beta.rb
@@ -1,26 +1,32 @@
 cask "sourcetree-beta" do
-  version "4.0.2b6_235"
-  sha256 "57144614a0d6e0dea85b464deae3b3cbdceb3e285a28598969cb3a2fe346af76"
+  version "4.1.3b8,239"
+  sha256 "ad36a592a4c776a70db48ddbbd3dcdfeb939bb47066d798cf3cc4a1e754ab9c1"
 
-  url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree_#{version}.zip",
+  url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree_#{version.csv.first}_#{version.csv.second}.zip",
       verified: "product-downloads.atlassian.com/software/sourcetree/"
-  appcast "https://product-downloads.atlassian.com/software/sourcetree/Appcast/SparkleAppcastBeta.xml"
   name "Atlassian Sourctree"
+  desc "Graphical client for Git version control"
   homepage "https://www.sourcetreeapp.com/"
+
+  livecheck do
+    url "https://product-downloads.atlassian.com/software/sourcetree/Appcast/SparkleAppcastBeta.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
   app "Sourcetree-Beta.app"
-  binary "#{appdir}/Sourcetree-Beta.app/Contents/Resources/stree"
+  binary "#{appdir}/Sourcetree-Beta.app/Contents/Resources/stree", target: "stree-beta"
 
   uninstall launchctl: "com.atlassian.SourceTreePrivilegedHelper2",
             quit:      "com.torusknot.SourceTreeNotMAS"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.torusknot.sourcetreenotmas.sfl2",
     "~/Library/Application Support/SourceTree",
     "~/Library/Caches/com.torusknot.SourceTreeNotMAS",
-    "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist",
     "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.LSSharedFileList.plist",
+    "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist",
     "~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState",
   ]
 end
