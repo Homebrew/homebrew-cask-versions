fe04adc76d5c8fff1e1e7b2a41e7606d86183b2b
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-10 22:30:42 +0800

    Update sourcetree-beta.rb

--- Casks/sourcetree-beta.rb
+++ Casks/sourcetree-beta.rb
@@ -2,7 +2,7 @@ cask "sourcetree-beta" do
   version "4.1.6b8,242"
   sha256 "9bb113a6af8d62b79b68a580d8973a71088c0bcd91be03dbdcb0a899fefc185c"
 
-  url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree__#{version.csv.first}_#{version.csv.second}.zip",
+  url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree_#{version.csv.first}_#{version.csv.second}.zip",
       verified: "product-downloads.atlassian.com/software/sourcetree/"
   name "Atlassian Sourctree"
   desc "Graphical client for Git version control"
