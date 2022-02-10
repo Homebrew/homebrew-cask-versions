6863671a390753816473661483f6235a38ee2c95
Miccal Matthews <miccal.matthews@gmail.com>
2022-02-10 22:30:13 +0800

    Update sourcetree-beta.rb

--- Casks/sourcetree-beta.rb
+++ Casks/sourcetree-beta.rb
@@ -1,26 +1,32 @@
 cask "sourcetree-beta" do
-  version "4.1.6b8_242"
+  version "4.1.6b8,242"
   sha256 "9bb113a6af8d62b79b68a580d8973a71088c0bcd91be03dbdcb0a899fefc185c"
 
-  url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree_#{version}.zip",
+  url "https://product-downloads.atlassian.com/software/sourcetree/beta/Sourcetree__#{version.csv.first}_#{version.csv.second}.zip",
       verified: "product-downloads.atlassian.com/software/sourcetree/"
-  appcast "https://product-downloads.atlassian.com/software/sourcetree/Appcast/SparkleAppcastBeta.xml"
   name "Atlassian Sourctree"
+  desc "Graphical client for Git version control"
   homepage "https://www.sourcetreeapp.com/"
+
+  livecheck do
+    url "https://product-downloads.atlassian.com/software/sourcetree/Appcast/SparkleAppcastBeta.xml"
+    strategy :sparkle
+  end
 
   depends_on macos: ">= :mojave"
 
   app "Sourcetree-Beta.app"
-  binary "#{appdir}/Sourcetree-Beta.app/Contents/Resources/stree"
+  binary "#{appdir}/Sourcetree-Beta.app/Contents/Resources/stree", terget: "stree-beta"
 
   uninstall launchctl: "com.atlassian.SourceTreePrivilegedHelper2",
             quit:      "com.torusknot.SourceTreeNotMAS"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.torusknot.sourcetreenotmas.sfl2",
     "~/Library/Application Support/SourceTree",
     "~/Library/Caches/com.torusknot.SourceTreeNotMAS",
-    "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist",
     "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.LSSharedFileList.plist",
+    "~/Library/Preferences/com.torusknot.SourceTreeNotMAS.plist",
     "~/Library/Saved Application State/com.torusknot.SourceTreeNotMAS.savedState",
   ]
 end
