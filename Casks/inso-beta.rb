1740f76b9fc16448e44c2c0644eae705e2fb331c
Hannes Braun <hannesbraun@mail.de>
2022-02-14 00:50:38 +0100

    Update inso-beta from 2.5.0-beta.0 to 2.5.0-beta.1 (#13297)

--- Casks/inso-beta.rb
+++ Casks/inso-beta.rb
@@ -1,6 +1,6 @@
 cask "inso-beta" do
-  version "2.5.0-beta.0"
-  sha256 "28381bae34d60c092f0518eac6ee50b9628dab523b2ff0836e7aa2fa3c6b0be0"
+  version "2.5.0-beta.1"
+  sha256 "34db61c5584b7aa2bfd0650104449f921e3a59a45e5afe2f1eb9e07a287919ee"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
