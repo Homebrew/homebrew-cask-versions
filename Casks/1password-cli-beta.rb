d3592c8d79dd33cf028bfef8ffd6c4ba244973f5
Mohammed Tangestani <tangestani@gmail.com>
2022-02-17 14:36:24 +0300

    Add 1Password CLI V2 Early Access

---
+++ Casks/1password-cli-beta.rb
@@ -0,0 +1,23 @@
+cask "1password-cli-beta" do
+  version "2.0.0-beta.12"
+  sha256 "3dc558ec40dd514829ec8fd0e322ef6961477ca50a5a4cc2df8b9dd859bd4a21"
+
+  url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
+      verified: "https://cache.agilebits.com/dist/1P/op2/pkg/"
+  name "1Password CLI Early Access"
+  desc "Early access version of 1Password CLI"
+  homepage "https://developer.1password.com/docs/cli"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/CLI2"
+    regex(%r{href=.*?/op_apple_universal[._-]v?(\d+(?:\.\d+)+-beta\.\d+)\.pkg}i)
+  end
+
+  conflicts_with cask: "1password-cli"
+
+  pkg "op_apple_universal_v#{version}.pkg"
+
+  uninstall pkgutil: "com.1password.op"
+
+  zap trash: "~/.config/op"
+end
