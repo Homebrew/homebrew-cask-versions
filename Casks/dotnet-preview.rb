114d22699ce8b65bc3e1cc76da7814235fef199b
Hannes Braun <hannesbraun@mail.de>
2022-02-18 11:52:52 +0100

    Update dotnet-preview from 6.0.2,26d5913f-8f8b-4894-a548-f9612b34f669,4765d40fddb545a7437812ae4d6efe12 to 6.0.2,26c9c72e-65be-4241-b048-11858350afb3,0dc4dee2b444eb7b0e8120a76fd69b09

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -2,8 +2,8 @@ cask "dotnet-preview" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "6.0.2,26d5913f-8f8b-4894-a548-f9612b34f669,4765d40fddb545a7437812ae4d6efe12"
-    sha256 "9eed24348b1c42d642668a3c617117570778c9a483d93993b4eea0b2bfe7dd76"
+    version "6.0.2,26c9c72e-65be-4241-b048-11858350afb3,0dc4dee2b444eb7b0e8120a76fd69b09"
+    sha256 "e5153d7ab5cc5b702d11a9eeb16a1fec1f016a8f11b65ecc2de12cb20b8a918b"
   else
     version "6.0.2,5394003a-5562-49f6-b3ae-10c4a7c24a2b,842e6bf147bfe393c6b1abe668762f91"
     sha256 "a34d5b70fb23f40294f825b540ff93eeecd4e042c64afb3a272fb469cf247c17"
