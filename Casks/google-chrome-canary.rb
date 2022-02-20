8e1f914a251f24a09c8a650dd712268754ce2951
Bevan Kay <email@bevankay.me>
2022-02-21 10:30:22 +1100

    Update google-chrome-canary.rb

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "101.0.4897.0"
+  version "101.0.4900.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
