474085b56f8f737d02776810e7456191ae8a20af
Hannes Braun <hannesbraun@mail.de>
2022-02-20 23:17:48 +0100

    Update zulu11 from 11.0.14 to 11.0.14.1 (#13361)

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -2,12 +2,12 @@ cask "zulu11" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "11.0.14,11.54.23-ca"
+  version "11.0.14.1,11.54.25-ca"
 
   if Hardware::CPU.intel?
-    sha256 "ff2a0ce85a489e9a47c3e74c5b87d91b8315627119892fd3d4a7924a5325df2a"
+    sha256 "91b14eaf4f84b9caac2248d1a1e601ce6d7a50cb4d6f73ca64821141a22ab08b"
   else
-    sha256 "1551391ad32d798e04cf564ccf8fbf1644ec6605f1fce2a5a0d1a5f30940f6b0"
+    sha256 "37c12c6b01f63eac5e2a4276f6dd73603a68d60d3ca4158cdb5e21a633dd1f97"
   end
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
