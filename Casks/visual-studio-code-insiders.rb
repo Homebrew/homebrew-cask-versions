4fdfeb32d51e87960b272b58772bafda7c009f93
César Román <thecesrom@gmail.com>
2022-02-28 07:42:40 -0800

    feat: modify `visual-studio-code-insiders` to use `:latest` version
    Similar to #13398
    
    >Discussed in https://github.com/Homebrew/discussions/discussions/3020
    >
    >The intention is to decrease burden on maintainers, infrastructure,
    >and contributors, as this Cask updates daily.
    
    CC'ing the most recent contributors: @hannesbraun, @owine

--- Casks/visual-studio-code-insiders.rb
+++ Casks/visual-studio-code-insiders.rb
@@ -1,33 +1,19 @@
 cask "visual-studio-code-insiders" do
+  version :latest
+  sha256 :no_check
+
   arch = Hardware::CPU.intel? ? "darwin" : "darwin-arm64"
 
-  version "1.65.0,3112460dc48ce7e557ea9feeaae04b912164b48b"
+  url "https://update.code.visualstudio.com/api/update/#{arch}/insider/VERSION" do |version_page|
+    version = JSON.parse(version_page)["version"]
 
-  if Hardware::CPU.intel?
-    sha256 "5561b7cbcb01c5f472a75fb68dd8a4d18d6322f77006a5e889261e1fc920549c"
-  else
-    sha256 "25b62034359195cdce4d7a44617740de6a7b47804c0ac642222a5bd86ffcdc90"
+    ["https://az764295.vo.msecnd.net/insider/#{version}/VSCode-#{arch}.zip",
+     { verified: "az764295.vo.msecnd.net/insider/" }]
   end
-
-  url "https://az764295.vo.msecnd.net/insider/#{version.csv.second}/VSCode-#{arch}.zip",
-      verified: "az764295.vo.msecnd.net/insider/"
   name "Microsoft Visual Studio Code"
   name "Visual Studio Code Insiders"
   desc "Code editor"
   homepage "https://code.visualstudio.com/insiders"
-
-  livecheck do
-    url "https://update.code.visualstudio.com/api/update/#{arch}/insider/VERSION"
-    strategy :page_match do |page|
-      name = page[/"name":"(\d+(?:\.\d+)+)/i, 1]
-      version = page[/"version":"(\w+)/i, 1]
-      next if name.blank? || version.blank?
-
-      "#{name},#{version}"
-    end
-  end
-
-  auto_updates true
 
   app "Visual Studio Code - Insiders.app"
   binary "#{appdir}/Visual Studio Code - Insiders.app/Contents/Resources/app/bin/code", target: "code-insiders"
