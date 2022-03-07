6b8a8718ddb0b3addf9af6cda1ab3efc1eb2e975
Yurii Kolesnykov <root@yurikoles.com>
2022-03-07 16:31:15 +0200

    openzfs-dev 2.1.99 (#13434)
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/openzfs-dev.rb
+++ Casks/openzfs-dev.rb
@@ -1,20 +1,12 @@
 cask "openzfs-dev" do
-  if MacOS.version <= :mojave
-    version "2.1.0rc4,341"
-    sha256 "14d5de6c0723c71ce96ab8052fd24a52719c8618b167f6eebec5d7ca8f110dee"
-    pkg "OpenZFSonOsX-#{version.csv.first}-Mojave-10.14.pkg"
-  elsif MacOS.version <= :catalina
-    version "2.1.0rc4,340"
-    sha256 "7d4b9111956abaf1c9a7339d22fd4f30c1c897e0e99955b3c3ee418f16fe42c1"
-    pkg "OpenZFSonOsX-#{version.csv.first}-Catalina-10.15.pkg"
-  elsif Hardware::CPU.intel?
-    version "2.1.0rc4,339"
-    sha256 "36035302b9833a81c9f25caabc8d19031cd1575e6e4d4df39f4759b8402bf67e"
-    pkg "OpenZFSonOsX-#{version.csv.first}-Big.Sur-11.pkg"
+  if MacOS.version < :el_capitan
+    version "2.1.99,369"
+    sha256 "932db021cddc7de34aa9d50cdad723b0b437dfdf2161150ca416fc68cad8e1c9"
+    pkg "OpenZFSonOsX-#{version.csv.first}-EL.CAPITAN-10.11.pkg"
   else
-    version "2.1.0rc4,338"
-    sha256 "9e25ff3d7701b6f84128714b0f5ef81b774fca30cd80103b67430775c98e5805"
-    pkg "OpenZFSonOsX-#{version.csv.first}-Big.Sur-11-arm64.pkg"
+    version "2.1.99,368"
+    sha256 "c64f103b696218202df7f758cc9d4c9995f56b2108fa072eb4c03aef9457219c"
+    pkg "OpenZFSonOsX-#{version.csv.first}-Catalina-10.15.pkg"
   end
 
   url "https://openzfsonosx.org/forum/download/file.php?id=#{version.csv.second}"
@@ -27,7 +19,8 @@ cask "openzfs-dev" do
   end
 
   conflicts_with cask: "openzfs"
-  depends_on macos: ">= :mojave"
+  depends_on macos: ">= :el_capitan"
+  depends_on arch: :intel
 
   postflight do
     set_ownership "/usr/local/zfs"
