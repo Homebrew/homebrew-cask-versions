1517509213f9893dfd18913c79707c1e734b6fbb
owine <owine@users.noreply.github.com>
2022-03-10 16:58:40 -0600

    Update 1password-cli-beta from 2.0.0-beta.14 to 2.0.0-beta.16

--- Casks/1password-cli-beta.rb
+++ Casks/1password-cli-beta.rb
@@ -1,6 +1,6 @@
 cask "1password-cli-beta" do
-  version "2.0.0-beta.14"
-  sha256 "0ef47af899cc9bd8a0934f9d8efabae9cdc6e28960f63082ce6fc64e83bb40c0"
+  version "2.0.0-beta.16"
+  sha256 "76266f2f351726ccadd50cb6c9d3a1399d39081ffbe972fc17406de56f607750"
 
   url "https://cache.agilebits.com/dist/1P/op2/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
       verified: "https://cache.agilebits.com/dist/1P/op2/pkg/"
