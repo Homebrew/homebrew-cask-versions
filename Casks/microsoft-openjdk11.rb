692376a10d4db7930390a469670c3f72d98c8d2d
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2022-03-10 19:40:13 -0600

    Update microsoft-openjdk11.rb
    Update version

--- Casks/microsoft-openjdk11.rb
+++ Casks/microsoft-openjdk11.rb
@@ -1,8 +1,8 @@
 cask "microsoft-openjdk11" do
-  version "11.0.14.1"
-  sha256 "a2c1887978f59dcca278ce9ed9d8255874d4bc69340f948f6fdd5d5dca86aded"
+  version "11.0.14.1,1-31205"
+  sha256 "257d8cb3124eaa71614fac0b9ddb48753d3e481bfa1f3ed49dd7827d102fd12e"
 
-  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-x64.pkg",
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version.major}_{version.csv.second}-macOS-x64.pkg",
       verified: "aka.ms/download-jdk/"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
@@ -13,7 +13,7 @@ cask "microsoft-openjdk11" do
     regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(11(?:\.\d+)+)[._-]macOS[._-]x64\.pkg}i)
   end
 
-  pkg "microsoft-jdk-#{version}-macOS-x64.pkg"
+  pkg "microsoft-jdk-#{version.major}-macOS-x64.pkg"
 
   uninstall pkgutil: "com.microsoft.#{version.major}.jdk"
 end
