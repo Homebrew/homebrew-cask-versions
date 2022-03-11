1e171c85aff718a6fdb91e71e5ec5430a250735e
Robert Blair <12276457+robertleeblairjr@users.noreply.github.com>
2022-03-11 12:02:01 -0600

    Update microsoft-openjdk11.rb
    Update to missing tag

--- Casks/microsoft-openjdk11.rb
+++ Casks/microsoft-openjdk11.rb
@@ -2,7 +2,7 @@ cask "microsoft-openjdk11" do
   version "11.0.14.1,1-31205"
   sha256 "257d8cb3124eaa71614fac0b9ddb48753d3e481bfa1f3ed49dd7827d102fd12e"
 
-  url "https://aka.ms/download-jdk/microsoft-jdk-#{version.csv.first}_{version.csv.second}-macOS-x64.pkg",
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version.csv.first}_#{version.csv.second}-macOS-x64.pkg",
       verified: "aka.ms/download-jdk/"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
@@ -13,7 +13,7 @@ cask "microsoft-openjdk11" do
     regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(11(?:\.\d+)+)[._-]macOS[._-]x64\.pkg}i)
   end
 
-  pkg "microsoft-jdk-#{version.csv.first}_{version.csv.second}-macOS-x64.pkg"
+  pkg "microsoft-jdk-#{version.csv.first}_#{version.csv.second}-macOS-x64.pkg"
 
-  uninstall pkgutil: "com.microsoft.#{version.csv.first}_{version.csv.second}.jdk"
+  uninstall pkgutil: "com.microsoft.#{version.csv.first}_#{version.csv.second}.jdk"
 end
