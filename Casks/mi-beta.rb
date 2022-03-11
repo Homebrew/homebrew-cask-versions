3e2ba50c49c68fb83910e2f1066c8d988dee346a
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-10 22:57:35 -0800

    Update mi-beta from 3.6b2 to 3.6

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -1,6 +1,6 @@
 cask "mi-beta" do
-  version "3.6b2,1098"
-  sha256 "fd9dbbb65e1b41259b46f5a0a0619944c80a812ba1643930e196f7af0c4b733a"
+  version "3.6,1100"
+  sha256 "3cbc4d3c69b1511ff5a518e3fc32a4eb867accf460855599354bf765e4195b80"
 
   url "https://www.mimikaki.net/download/mi#{version.csv.first}.dmg"
   name "mi"
