7fd471d949452d214af3aabdf3a12f9e95c59dbf
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-11 22:59:53 -0800

    Update picoscope-beta from 7.0.92 to 7.0.94 (#13496)

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,6 +1,6 @@
 cask "picoscope-beta" do
-  version "7.0.92"
-  sha256 "58285b1de02d6892fd2eb765aad1727342b5dca20925018bf06470d64351b01d"
+  version "7.0.94"
+  sha256 "d39f4953ff171819fd87220292050692fa186b7ffa99fd8db187a3a7aa218f2a"
 
   url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}_TnM_Early_Access.pkg"
   name "PicoScope beta"
