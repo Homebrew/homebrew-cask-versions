f4c7dc42b9617b04f768543ad1f79de0ab934132
Jackson Chen <jackson@jacksonchen666.com>
2022-03-19 19:57:58 +0100

    Update osu-development from 2022.306.0 to 2022.319.0

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,8 +1,14 @@
 cask "osu-development" do
-  version "2022.306.0"
-  sha256 "4b93247476103c884c0136a767656c43401aa83f39c027823b5732a10705cf79"
+  version "2022.319.0"
 
-  url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.zip"
+  if Hardware::CPU.intel?
+    sha256 "3178c9c0b5c35e9a16f120f5a775f76a77e72c6df22450e18dfc95752475ffa9"
+  else
+    sha256 "bcc306951fb308359730f27f81a139614176f5dd147ce490daa9f9c6ecfe54da"
+  end
+
+  arch = Hardware::CPU.intel? ? "Intel" : "Apple.Silicon"
+  url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
   name "osu!lazer"
   desc "Rhythm game"
   homepage "https://github.com/ppy/osu/"
