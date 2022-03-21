32db21d23f9273387135bd65051dc9068391cb46
Robert Conrad <rob@geekmind.org>
2022-03-21 08:53:42 +0100

    Add 1password-cli1 (#13540)

---
+++ Casks/1password-cli1.rb
@@ -0,0 +1,23 @@
+cask "1password-cli1" do
+  version "1.12.4"
+  sha256 "a484de66fa64252c51f3a90d6b8a1dad70bd4537b786466f897b99badb51a1ee"
+
+  url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
+      verified: "cache.agilebits.com/dist/1P/op/pkg/"
+  name "1Password CLI"
+  desc "Command-line helper for the 1Password password manager"
+  homepage "https://developer.1password.com/docs/cli/v1/usage/"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/CLI"
+    regex(%r{href=.*?/op_apple_universal[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
+  end
+
+  conflicts_with cask: "1password-cli"
+
+  pkg "op_apple_universal_v#{version}.pkg"
+
+  uninstall pkgutil: "com.1password.op"
+
+  zap trash: "~/.op"
+end
