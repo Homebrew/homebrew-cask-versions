8451f067e7c1c48c06f9609253ae52e9cc215530
Hannes Braun <hannesbraun@mail.de>
2022-03-25 11:58:23 +0100

    Add aarch64 version

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,8 +1,15 @@
 cask "appcode-eap" do
-  version "2022.1,221.5080.57"
-  sha256 "eb51014b7fb7404196870766cec5c3030c3446b4f4cd1d840c5a51d22d2d04ce"
+  arch = Hardware::CPU.intel? ? "" : "-aarch64"
 
-  url "https://download.jetbrains.com/objc/AppCode-#{version.csv.second}.dmg"
+  version "2022.1,221.5080.57"
+
+  if Hardware::CPU.intel?
+    sha256 "eb51014b7fb7404196870766cec5c3030c3446b4f4cd1d840c5a51d22d2d04ce"
+  else
+    sha256 "380af65c2f0ef0cc3e913255eef0ebff9222a8c6e183dfd557ebaf647df66a63"
+  end
+
+  url "https://download.jetbrains.com/objc/AppCode-#{version.csv.second}#{arch}.dmg"
   name "AppCode EAP"
   desc "IDE for Swift, Objective-C, C, and C++ development"
   homepage "https://www.jetbrains.com/objc/nextversion/"
