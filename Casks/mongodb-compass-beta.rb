608391a35444c09e1bb1c05e180e685d8cf652d2
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-29 00:22:36 -0700

    Update mongodb-compass-beta from 1.31.0-beta.18 to 1.31.0-beta.19 (#13614)

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass-beta" do
-  version "1.31.0-beta.18"
-  sha256 "8271e357ab7560eee3f67e0135e3954a676528e2de971c86c2dadf8d87d131b6"
+  version "1.31.0-beta.19"
+  sha256 "f24beec1cea0ddec5a410cfd322f27c9cc982b65078ac6a735a40acfb9211abc"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
   name "MongoDB Compass"
