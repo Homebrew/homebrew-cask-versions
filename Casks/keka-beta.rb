a807b1142fb38cf4d2b41b04ea1b3468f8e6b899
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-29 00:04:54 -0700

    Update keka-beta from 1.2.52 to 1.2.53-beta.1

--- Casks/keka-beta.rb
+++ Casks/keka-beta.rb
@@ -1,6 +1,6 @@
 cask "keka-beta" do
-  version "1.2.52"
-  sha256 "62d586d70936177ce1f2e93bd3bd0d935cf5207bab6c6436e3f2b5388dddd5ab"
+  version "1.2.53-beta.1"
+  sha256 "f41819436513017c2fd8679cb4154de99780462bba5137166148318d5818db6e"
 
   url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg",
       verified: "github.com/aonez/Keka/"
