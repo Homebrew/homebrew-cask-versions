8ba8e65e4dc6519fbbe65c75288539dac405e4ac
Alexander Ronald Altman <alexanderaltman@me.com>
2022-03-29 00:23:18 -0700

    Update google-chrome-canary from 101.0.4936.0 to 102.0.4970.2 (#13606)

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "101.0.4936.0"
+  version "102.0.4970.2"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
