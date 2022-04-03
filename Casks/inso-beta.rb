9892706a385e6d1c32f56837ec8cbca898d317c8
insomnia-infra <88453862+insomnia-infra@users.noreply.github.com>
2022-04-03 03:53:29 +0200

    Update inso-beta from 2.10.0-beta.0 to 2.11.0-beta.0 (#13639)

--- Casks/inso-beta.rb
+++ Casks/inso-beta.rb
@@ -1,6 +1,6 @@
 cask "inso-beta" do
-  version "2.10.0-beta.0"
-  sha256 "55e1f4f3005df70a392e86fe0ee9eb743f5d22a0fc5d75ede0b7b332a15e1277"
+  version "2.11.0-beta.0"
+  sha256 "ed67d70000ccad358dbb7bfbe67e206aad3c454678cf0d833b81d11857c0e923"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
