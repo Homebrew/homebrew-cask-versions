187bb1d0a43fa6dfc2adc3ca2ce011c59b7a520f
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2022-04-18 15:11:35 +0200

    Update osu-development from 2022.409.0 to 2022.418.0 (#13723)

--- Casks/osu-development.rb
+++ Casks/osu-development.rb
@@ -1,12 +1,12 @@
 cask "osu-development" do
   arch = Hardware::CPU.intel? ? "Intel" : "Apple.Silicon"
 
-  version "2022.409.0"
+  version "2022.418.0"
 
   if Hardware::CPU.intel?
-    sha256 "2b6e98074e335986889e77907896ee401d8ddcd8364cb72ab6be759e665e97cf"
+    sha256 "e4a7ae95e553fbf32006a548e2fda77c8716ef0a6ce6a05dee350fd4ea1d1a65"
   else
-    sha256 "351524254d23425414f563be7c2cf13768b46d4a32d1c52e51aafb338703be48"
+    sha256 "6b3218d3fc0602fccfbbeb743a1fe6348bf3784bb199d1366b86257ad41878ee"
   end
 
   url "https://github.com/ppy/osu/releases/download/#{version}/osu.app.#{arch}.zip"
