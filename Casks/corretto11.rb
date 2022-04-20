9121fc6d36c7d20b5cb40f8f8d436c771eebfa68
Ben Taylor <61255184+benty-amzn@users.noreply.github.com>
2022-04-20 06:14:13 -0700

    Update corretto11 from 11.0.14.10.1 to 11.0.15.9.1 (#13729)

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,20 +1,27 @@
 cask "corretto11" do
-  version "11.0.14.10.1"
-  sha256 "b5db350456838b4e0c3236f6f830357998ef5ee316eef2a11282b5d821dd6b30"
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-x64.pkg"
-  name "Amazon Corretto JDK"
+  version "11.0.15.9.1"
+
+  if Hardware::CPU.intel?
+    sha256 "fdc8c55c21e25b930c5b8a75aaac7ccf711e3885b188ecbae41dc8a55fe440af"
+  else
+    sha256 "598d38776bcd7588155025f309530ae1f82fabe97530b6589210aa844516ed48"
+  end
+
+  url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
+  name "AWS Corretto JDK"
   desc "OpenJDK distribution from Amazon"
   homepage "https://corretto.aws/"
 
   livecheck do
-    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-#{arch}-macos-jdk.pkg"
     strategy :header_match do |headers|
-      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-x64\.pkg}i, 1]
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)+)-macosx-#{arch}\.pkg}i, 1]
     end
   end
 
-  pkg "amazon-corretto-#{version}-macosx-x64.pkg"
+  pkg "amazon-corretto-#{version}-macosx-#{arch}.pkg"
 
   uninstall pkgutil: "com.amazon.corretto.#{version.major}"
 end
