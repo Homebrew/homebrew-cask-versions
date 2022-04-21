8691a0e7a3d3828ccbd46f262a6f707d994e5bdb
Lex.Chen <ctyytc@gmail.com>
2022-04-21 15:26:30 +0800

    Update zulu17 from 17.0.2 to 17.0.3

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -2,15 +2,15 @@ cask "zulu17" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "17.0.2,17.32.13-ca"
+  version "17.0.3,17.34.19-ca"
 
   if Hardware::CPU.intel?
-    sha256 "73d9fd535a690c1fdcc9099edbb7f9e9b8897e30573c0be95360e6896804c203"
+    sha256 "a889b2c550b6cb6421c6e559c1a98a3f2a38ebe9feef2b48157a582347bac367"
   else
-    sha256 "74d8e059865fdb8cfbc202bbd93a05c5a67a2d12947b09e30d637dd9b7e4011f"
+    sha256 "79a457f106bf32aafd261a4748471fd10f5ce2a9aa2cc816a91864104c008dff"
   end
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.tar.gz",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "OpenJDK distribution from Azul"
@@ -19,7 +19,7 @@ cask "zulu17" do
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
     strategy :page_match do |page|
-      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.tar.gz/i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
@@ -28,7 +28,5 @@ cask "zulu17" do
 
   depends_on macos: ">= :mojave"
 
-  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
-
-  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+  artifact "zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}/zulu-#{version.major}.jdk", target: "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk"
 end
