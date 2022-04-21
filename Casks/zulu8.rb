0dcf27fca8e032cdafa37baf1f07b8a98b5929db
Lex.Chen <ctyytc@gmail.com>
2022-04-21 15:27:21 +0800

    Update zulu8 from 8.0.322 to 8.0.332

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -2,15 +2,15 @@ cask "zulu8" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "8.0.322,8.60.0.21-ca"
+  version "8.0.332,8.62.0.19-ca"
 
   if Hardware::CPU.intel?
-    sha256 "44b3ea6856c92230e2c1ca27f2b0ddc533830f3774c955c83c463c13858678c7"
+    sha256 "9b2a14112de141bdcef0ee34a57072b3310568b365ca5ecf54724a8100039339"
   else
-    sha256 "6cea661f8bf457c3514cb8cb28d10f7ade1141a2ed6472c5c6f569a66dfbd87c"
+    sha256 "e5c84a46bbd985c3a53358db9c97a6fd4930f92b833c3163a0d1e47dab59768c"
   end
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.tar.gz",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java 8 Standard Edition Development Kit"
   desc "OpenJDK distribution from Azul"
@@ -19,7 +19,7 @@ cask "zulu8" do
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
     strategy :page_match do |page|
-      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.dmg/i)
+      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.tar.gz/i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
@@ -28,7 +28,5 @@ cask "zulu8" do
 
   depends_on macos: ">= :yosemite"
 
-  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
-
-  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+  artifact "zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}/zulu-#{version.major}.jdk", target: "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk"
 end
