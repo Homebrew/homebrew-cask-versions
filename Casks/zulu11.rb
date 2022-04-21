f3d0fc336e65bdbd07c8eb3ab7e2a37672a5ebaf
Lex.Chen <ctyytc@gmail.com>
2022-04-21 15:24:17 +0800

    Update zulu11 from 11.0.14.1 to 11.0.15

--- Casks/zulu11.rb
+++ Casks/zulu11.rb
@@ -2,15 +2,15 @@ cask "zulu11" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "11.0.14.1,11.54.25-ca"
+  version "11.0.15,11.56.19-ca"
 
   if Hardware::CPU.intel?
-    sha256 "91b14eaf4f84b9caac2248d1a1e601ce6d7a50cb4d6f73ca64821141a22ab08b"
+    sha256 "2614e5c5de8e989d4d81759de4c333aa5b867b17ab9ee78754309ba65c7f6f55"
   else
-    sha256 "37c12c6b01f63eac5e2a4276f6dd73603a68d60d3ca4158cdb5e21a633dd1f97"
+    sha256 "6bb0d2c6e8a29dcd9c577bbb2986352ba12481a9549ac2c0bcfd00ed60e538d2"
   end
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.tar.gz",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
   name "Azul Zulu Java Standard Edition Development Kit"
   desc "OpenJDK distribution from Azul"
@@ -19,14 +19,12 @@ cask "zulu11" do
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
     strategy :page_match do |page|
-      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.dmg/i)
+      match = page.match(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.tar.gz/i)
       next if match.blank?
 
       "#{match[2]},#{match[1]}"
     end
   end
 
-  pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
-
-  uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
+  artifact "zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}/zulu-#{version.major}.jdk", target: "/Library/Java/JavaVirtualMachines/zulu-#{version.major}.jdk"
 end
