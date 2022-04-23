bbfff3cbaf58aad6dbf9d4f10a27a064182aa2be
Hannes Braun <hannesbraun@mail.de>
2022-04-23 14:29:55 +0200

    Update android-studio-preview-beta.rb

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -20,7 +20,7 @@ cask "android-studio-preview-beta" do
     regex(%r{href=.*?/android[._-]studio[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.zip(.*\n*\s*.*)(Beta|RC)}i)
   end
 
-  conflicts_with cask: "android-studio-preview-canary"
+  conflicts_with cask: "android-studio"
 
   app "Android Studio.app"
 
