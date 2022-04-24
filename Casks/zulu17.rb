658b94697b031b407ccaaf54c6483e7f233fc98f
zuisong <378818573@qq.com>
2022-04-25 04:04:01 +0800

    Update zulu17 from 17.0.2 to 17.0.3 (#13766)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -2,12 +2,12 @@ cask "zulu17" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "17.0.2,17.32.13-ca"
+  version "17.0.3,17.34.19-ca"
 
   if Hardware::CPU.intel?
-    sha256 "73d9fd535a690c1fdcc9099edbb7f9e9b8897e30573c0be95360e6896804c203"
+    sha256 "275a6bd5719bfc740f2ec729f42c0e032e8887351f651a00ff7f254e7ab9c2ed"
   else
-    sha256 "74d8e059865fdb8cfbc202bbd93a05c5a67a2d12947b09e30d637dd9b7e4011f"
+    sha256 "100c4cd1325a3bf8e5731ff4ba625d04d0e460198b548626d711e48b9ad6fba9"
   end
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
