43f8c9075701daff46d46c9bdd518ab3fba2e392
Bob Olde Hampsink <bob@robuust.digital>
2022-04-25 07:35:43 +0200

    Update sonos-s1-controller from 11.3 to 11.4

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,6 +1,6 @@
 cask "sonos-s1-controller" do
-  version "11.3,57.10.25040"
-  sha256 "ca58d868c0000ee72316a3ee52b9ea4c6d9eabb3d0e06c96fc8649b4975388b1"
+  version "11.4,57.11.28060"
+  sha256 "4ad48ed5d7cb56596a228eeb6e7fc11cb35943c6dcb0cb1b0a7470c21b2192d2"
 
   url "https://update.sonos.com/software/mac/mdcr/SonosDesktopController#{version.csv.first.no_dots}.dmg"
   name "Sonos S1"
