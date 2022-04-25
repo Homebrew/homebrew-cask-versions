12ec433e2ef6e62f0c250d984738a3ad69e682fb
Hannes Braun <hannesbraun@mail.de>
2022-04-25 13:03:33 +0200

    Update zulu13 from 13.0.10 to 13.0.11 (#13775)

--- Casks/zulu13.rb
+++ Casks/zulu13.rb
@@ -2,12 +2,12 @@ cask "zulu13" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
   choice = Hardware::CPU.intel? ? "x86" : "arm"
 
-  version "13.0.10,13.46.15-ca"
+  version "13.0.11,13.48.19-ca"
 
   if Hardware::CPU.intel?
-    sha256 "108ab2def40e4f70885bb32a33dfa6801ac00d21db3cb5051aa62aa28114302a"
+    sha256 "6effb3541553d665985a054daae4e17e4fa29fdeb3176b84cfd9070fce57cc8a"
   else
-    sha256 "78d967aa00dcf6396042f3d917fc0a00179884fe7e98213704b783c3a2e05899"
+    sha256 "4873d3812f53f5f70d3664d7271b3071a83774a526421ac43ba5c150deb7783d"
   end
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
