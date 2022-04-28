42e520cd678134165d34be7765909e47f89abf9e
moonfruit <dkmoonfruit@gmail.com>
2022-04-28 09:51:24 +0800

    Update semeru-jdk11-open from 11.0.14.1+1 to 11.0.15+10

--- Casks/semeru-jdk11-open.rb
+++ Casks/semeru-jdk11-open.rb
@@ -1,6 +1,6 @@
 cask "semeru-jdk11-open" do
-  version "11.0.14.1+1,openj9-0.30.1"
-  sha256 "a448d77b97141976fe25fc351f606170df00867944ca840c7dbbf6c93d90c68b"
+  version "11.0.15+10,openj9-0.32.0"
+  sha256 "4449aea158c44b25d141c8fdd4f9a0d2dd1e3bf7ef94ecac25cf3213ad3f327e"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
