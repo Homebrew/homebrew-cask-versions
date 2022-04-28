4ff56f93baed11f432186206d2add92f640379f8
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2022-04-28 03:22:11 +0300

    Update temurin11 from 11.0.14.1 to 11.0.15 (#13786)

--- Casks/temurin11.rb
+++ Casks/temurin11.rb
@@ -1,6 +1,6 @@
 cask "temurin11" do
-  version "11.0.14.1,1"
-  sha256 "1c165d086d89cc540d01efbf60fcfe9ead6a7ede64d6d3a75ff6e2a8c94bd8f4"
+  version "11.0.15,10"
+  sha256 "ade3d0e0ace5783a097c094cc91a304e7ceaaba0b65bd6ac901529c1c0a157bc"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_x64_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
