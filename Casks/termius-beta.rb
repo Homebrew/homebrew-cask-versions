2f2e43db189af1ee6e1724363e9b79cdccecf2eb
Porkepix <porkepix@gmail.com>
2022-05-04 15:16:33 +0200

    Update termius-beta from 7.40.1 to 7.40.2

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,7 +1,7 @@
 cask "termius-beta" do
   arch = Hardware::CPU.intel? ? "mac-beta" : "mac-beta-arm64"
 
-  version "7.40.1"
+  version "7.40.2"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius%20Beta.dmg"
