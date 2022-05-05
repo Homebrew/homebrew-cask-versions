2bb892812c5cb44bc4bddab27448f078a85e5421
seludeft <daniel@eftodi.se>
2022-05-05 23:56:05 +0200

    Add 1password7
    Since the main homebrew-casks has been updated to the 8-version.

---
+++ Casks/1password7.rb
@@ -0,0 +1,28 @@
+cask "1password7" do
+  version "7.9.4"
+  sha256 "2d41a01d0bcc51f822170f36ddc067977f636efb71e419b588b366aa37a21a94"
+
+  url "https://c.1password.com/dist/1P/mac#{version.major}/1Password-#{version}.zip"
+  name "1Password"
+  desc "Password manager that keeps all passwords secure behind one password"
+  homepage "https://1password.com/"
+
+  livecheck do
+    url "https://app-updates.agilebits.com/product_history/OPM#{version.major}"
+    regex(%r{href=.*?/1Password-(\d+(?:\.\d+)+)\.pkg}i)
+  end
+
+  auto_updates true
+  conflicts_with cask: "homebrew/cask-versions/1password-beta"
+  depends_on macos: ">= :high_sierra"
+
+  app "1Password #{version.major}.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/*.agilebits.onepassword*",
+    "~/Library/Containers/*.agilebits.onepassword*",
+    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+    "~/Library/Logs/1Password",
+    "~/Library/Preferences/com.agilebits.onepassword*",
+  ]
+end
