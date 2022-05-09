ec81ad980262c79bb87f051b5cf2ad8f5010bf3b
zuisong <378818573@qq.com>
2022-05-09 08:07:15 +0800

    Update microsoft-edge-dev from 102.0.1220.1 to 102.0.1235.1 (#13850)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge-dev" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069340" : "2099619"
 
-  version "102.0.1220.1"
+  version "102.0.1245.3"
 
   if Hardware::CPU.intel?
-    sha256 "c11a90d246bbbc19e7e56e01db4f5ebf25d8e94fbce41e725c11f2064d6f104c"
+    sha256 "acd07da2fa0bcd024e5525717f14375bb98455ff01b0e04d47f8167ff82e40e0"
   else
-    sha256 "a7498e09dcb1e39bdca947b9537020b0ab2af6eec9f4e762ee7b138e5e2f2261"
+    sha256 "4368d5c7a6265b85d3204fd128f864ed53cee692760bb40c8f5140db7de10cef"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
@@ -22,7 +22,6 @@ cask "microsoft-edge-dev" do
   end
 
   auto_updates true
-  depends_on cask: "microsoft-auto-update"
 
   pkg "MicrosoftEdgeDev-#{version}.pkg",
       choices: [
