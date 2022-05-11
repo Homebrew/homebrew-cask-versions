51d161e7ea0599abef87b1ce284526b47749224c
George Adams <georgeadams1995@gmail.com>
2022-05-11 10:32:44 +0100

    Update temurin17 from 17.0.2 to 17.0.3 (#13873)
    * Update temurin17 from 17.0.2 to 17.0.3
    
    * Update temurin17.rb
    
    * Update temurin17.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/temurin17.rb
+++ Casks/temurin17.rb
@@ -1,12 +1,12 @@
 cask "temurin17" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "17.0.2,8"
+  version "17.0.3,7"
 
   if Hardware::CPU.intel?
-    sha256 "b82d3ba3ff4c453bd954979a20e67754478294da8650e3b9e50f6d1a7cf5c628"
+    sha256 "70afd9b4f76d69a5f9ab3f740a9289f41d23558f02b133e684c80380c265455f"
   else
-    sha256 "0904c11eb5f7b53aac53e6279992ee2a900c9517b26659dee3704b2d11c149cd"
+    sha256 "46da5d1620e9ac231eb8d01cca6714dee67c0db9c67ad340dca2c17bc29763ff"
   end
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
@@ -16,11 +16,11 @@ cask "temurin17" do
   homepage "https://adoptium.net/"
 
   livecheck do
-    url "https://api.adoptium.net/v3/info/release_versions?release_type=ga&architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
-    regex(/^(\d+(?:\.\d+)*)\+(\d+(?:\.\d+)*)$/i)
+    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    regex(/^jdk-(\d+(?:\.\d+)+)\+(\d+(?:\.\d+)*)$/i)
     strategy :page_match do |page, regex|
-      JSON.parse(page)["versions"].map do |version|
-        match = version["openjdk_version"]&.match(regex)
+      JSON.parse(page).map do |release|
+        match = release["release_name"]&.match(regex)
         next if match.blank?
 
         "#{match[1]},#{match[2]}"
