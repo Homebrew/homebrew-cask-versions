7cadd8b7b0ab2337c3a4e406e9387f0d58600949
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-11 17:03:06 +0800

    Update temurin17.rb

--- Casks/temurin17.rb
+++ Casks/temurin17.rb
@@ -16,7 +16,7 @@ cask "temurin17" do
   homepage "https://adoptium.net/"
 
   livecheck do
-    url "https://api.adoptium.net/v3/info/release_versions?release_type=ga&architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
+    url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
     regex(/^(\d+(?:\.\d+)*)\+(\d+(?:\.\d+)*)$/i)
     strategy :page_match do |page, regex|
       JSON.parse(page)["versions"].map do |version|
