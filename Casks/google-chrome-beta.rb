e23f4d26df0f6ec9c6a0ae3da84d51fad4c9f24b
owine <owine@users.noreply.github.com>
2022-05-11 18:05:45 -0500

    Update google-chrome-beta from 102.0.5005.40 to 102.0.5005.49 (#13878)

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-beta" do
-  version "102.0.5005.40"
+  version "102.0.5005.49"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg"
