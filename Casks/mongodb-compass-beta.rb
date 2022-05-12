bdcfb7a945998559a35ce6f3aa378fe01cdf1227
Bevan Kay <email@bevankay.me>
2022-05-12 12:22:33 +1000

    Update mongodb-compass-beta from 1.31.2-beta.5 to 1.31.3-beta.12 (#13881)

--- Casks/mongodb-compass-beta.rb
+++ Casks/mongodb-compass-beta.rb
@@ -1,6 +1,6 @@
 cask "mongodb-compass-beta" do
-  version "1.31.2-beta.5"
-  sha256 "a4305db713a0bc6a7a4392c7b4e6d873e2b11199764d69e4a5e32f248115568f"
+  version "1.31.3-beta.12"
+  sha256 "ed315e38aa87ba16c0f194bc8d359b57551b6d362cb7d5dd85bef1d9587e25fc"
 
   url "https://downloads.mongodb.com/compass/beta/mongodb-compass-#{version}-darwin-x64.dmg"
   name "MongoDB Compass"
