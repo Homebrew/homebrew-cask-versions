9ccb574bc1937e13db556b89a6ef472313b38c0e
Clément L <porkepix@gmail.com>
2022-05-13 11:47:30 +0200

    Update java-beta from 19,21 to 19,22 (#13897)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,12 +1,12 @@
 cask "java-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
 
-  version "19,21"
+  version "19,22"
 
   if Hardware::CPU.intel?
-    sha256 "b77a2151e977539b201b379e95fe44c6fc1dc4b728eec44bc7f904ee7311aa63"
+    sha256 "06ae650996061795a1ba9eb90bc45bf1a17ca0bf1c8894da5a72911ce95051d1"
   else
-    sha256 "666e7a0f0214fd57443dd114f951b91679ffe42f1421dbac8b0400269e26c8d4"
+    sha256 "fb90bcafaf8db52f0ce1ab3ebf99ac45345228ddb1e6de03772ec85486033bcf"
   end
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
