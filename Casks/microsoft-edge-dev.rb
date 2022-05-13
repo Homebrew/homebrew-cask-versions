9edc68ba506bf35af0118d18a9e9834c430f0a41
Hannes Braun <hannesbraun@mail.de>
2022-05-13 02:07:35 +0200

    Update microsoft-edge-dev from 102.0.1245.3 to 103.0.1253.0 (#13889)

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge-dev" do
   folder = Hardware::CPU.intel? ? "C1297A47-86C4-4C1F-97FA-950631F94777" : "03adf619-38c6-4249-95ff-4a01c0ffc962"
   linkid = Hardware::CPU.intel? ? "2069340" : "2099619"
 
-  version "102.0.1245.3"
+  version "103.0.1253.0"
 
   if Hardware::CPU.intel?
-    sha256 "acd07da2fa0bcd024e5525717f14375bb98455ff01b0e04d47f8167ff82e40e0"
+    sha256 "bd8c1c33bece520fbf8990b77af57cd8a457e633e1ac182c1a5b39411686005a"
   else
-    sha256 "4368d5c7a6265b85d3204fd128f864ed53cee692760bb40c8f5140db7de10cef"
+    sha256 "3b25392c5ec9001dfbe95119c772658e348075c2eb58dc04ab32cbd8323ba194"
   end
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
