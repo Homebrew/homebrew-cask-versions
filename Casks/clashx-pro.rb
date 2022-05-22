a9e41b319508894541a921ef7d9905b80c410461
Hannes Braun <hannesbraun@mail.de>
2022-05-22 19:24:33 +0200

    Update clashx-pro from 1.92.1.1 to 1.93.1.1

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -1,6 +1,6 @@
 cask "clashx-pro" do
-  version "1.92.1.1"
-  sha256 "e1c1df5b0a36857d858bde9eb9cf2acb3ea47919e6938a77755ccd1336877780"
+  version "1.93.1.1"
+  sha256 "677e8c7835cda77c750711f49b69411fffdbcffede8bfa43586a730990bd4135"
 
   url "https://appcenter.vercel.app/clashx/clashx-pro/#{version}",
       verified: "appcenter.vercel.app/clashx"
