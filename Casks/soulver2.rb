3ae8801ec6bdde6aeb5e83a7ee73513bfb56dfc3
Ian Lesperance <ian@elliterate.com>
2022-05-23 20:04:27 -0400

    Update soulver2 from 2.7.1-6074 to 2.9.0-6104 (#13958)

--- Casks/soulver2.rb
+++ Casks/soulver2.rb
@@ -1,16 +1,15 @@
 cask "soulver2" do
-  version "2.7.1-6074"
-  sha256 "24cac29ffd9f28a4a6cc6c90f65c7b7ad9aa76ac3e31aa0b91f271c40d9c1112"
+  version "2.9.0-6104"
+  sha256 :no_check
 
-  url "https://www.acqualia.com/files/sparkle/soulver_#{version}.zip"
-  appcast "https://www.acqualia.com/soulver/appcast/soulver#{version.major}.xml"
+  url "https://soulver.app/download-soulver-2"
   name "Soulver"
-  homepage "https://www.acqualia.com/soulver/"
+  homepage "https://soulver.app/"
 
   auto_updates true
   depends_on macos: ">= :yosemite"
 
-  app "Soulver.app"
+  app "Soulver 2.app"
 
   zap trash: [
     "~/Library/Application Support/Soulver",
