b59b243c35a4918f333b728929dcc4b88aded11d
Hannes Braun <hannesbraun@mail.de>
2022-05-24 10:21:52 +0200

    Update opera-developer from 89.0.4422.0 to 89.0.4428.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "89.0.4422.0"
-  sha256 "ada6e88215f747e60d0968ffa6d58c6920c1ecf631279bcc63cd1a59e4ba9181"
+  version "89.0.4428.0"
+  sha256 "d23753e5b00e5d344c7f8937fdbd313a3acac45d9aa8bda8d3ac472312efa1b4"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
