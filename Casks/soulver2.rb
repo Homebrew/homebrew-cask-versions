f868e4837e1e7f2390ee37c0268fa31865665a8e
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-24 08:10:18 +0800

    Update soulver2.rb

--- Casks/soulver2.rb
+++ Casks/soulver2.rb
@@ -1,19 +1,25 @@
 cask "soulver2" do
-  version "2.9.0-6104"
+  version "2.9.0,6104"
   sha256 :no_check
 
-  url "https://soulver.app/download-soulver-2"
+  url "https://d1k3ac1niusr4e.cloudfront.net/soulver#{version.major}.zip",
+      verified: "d1k3ac1niusr4e.cloudfront.net/"
   name "Soulver"
   homepage "https://soulver.app/"
+
+  livecheck do
+    url :url
+    stategy :extract_plist
+  end
 
   auto_updates true
   depends_on macos: ">= :yosemite"
 
-  app "Soulver 2.app"
+  app "Soulver #{version.major}.app"
 
   zap trash: [
     "~/Library/Application Support/Soulver",
-    "~/Library/Preferences/com.acqualia.soulver.plist",
     "~/Library/Autosave Information/Unsaved Soulver Document*",
+    "~/Library/Preferences/com.acqualia.soulver.plist",
   ]
 end
