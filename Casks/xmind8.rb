5fbfb8b741a389bc2d65d0a20a3dd8b77584611a
Miccal Matthews <miccal.matthews@gmail.com>
2022-05-25 13:52:12 +0800

    Update xmind8.rb

--- Casks/xmind8.rb
+++ Casks/xmind8.rb
@@ -2,7 +2,7 @@ cask "xmind8" do
   version "8-update9"
   sha256 "120745d98a6122af56f944f666c59a6c3e06099be3838c365e6554e82db19211"
 
-  url "https://dl3.xmind.net/xmind-#{version.csv.second}-macosx.dmg"
+  url "https://dl3.xmind.net/xmind-#{version}-macosx.dmg"
   name "XMind"
   desc "Mind mapping and brainstorming tool"
   homepage "https://www.xmind.net/xmind8-pro/"
