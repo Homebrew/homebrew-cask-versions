fa2c66bf252d77d9fd575b92461f8929e0b7f086
Clément L <porkepix@gmail.com>
2022-05-25 02:31:07 +0200

    Update vivaldi-snapshot from 5.3.2679.3 to 5.3.2679.13 (#13970)

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask "vivaldi-snapshot" do
-  version "5.3.2679.3"
-  sha256 "7ff0fb7339c088acb8aa59855fc14068976874031f792f98f4d7323816b52d5b"
+  version "5.3.2679.13"
+  sha256 "1a588ff6b001d4a66f04794024be21367b1fd4f338181ae23e7bd45398b75db1"
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
