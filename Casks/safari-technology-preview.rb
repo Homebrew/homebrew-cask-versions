ce441745e82a675d4cb298faa69e0a742f95d89c
owine <owine@users.noreply.github.com>
2022-05-25 16:48:46 -0500

    Update safari-technology-preview from 145 to 146

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -3,8 +3,8 @@ cask "safari-technology-preview" do
     version "145,012-02933-20220511-B69DC656-4904-4957-BB27-0CD4E65770E0"
     sha256 "2dc800b17529e7ad75f0656921bd24c1acb9659acf17ede62a63486e259e8671"
   else
-    version "145,012-01341-20220511-067B826E-7921-4625-8D92-E8648B6351C3"
-    sha256 "0e986721a7eb5ec91615eda72e7f93a467ba97989e439eafc3592d99b4b6c021"
+    version "146,012-08405-20220525-72BCCE23-C6E8-460A-851A-A29AC9C9BCF7"
+    sha256 "3cd3652691cc89d71c69db118323b0fe7bb62b49275ad4e49680ba2e1378c341"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.csv.second}/SafariTechnologyPreview.dmg"
