dbe70b5e61e1c47b3425f32366711cb812fb503c
Hannes Braun <hannesbraun@mail.de>
2022-06-02 14:04:03 +0200

    Update appcode-eap from 2022.2,222.2270.19 to 2022.2,222.2889.2

--- Casks/appcode-eap.rb
+++ Casks/appcode-eap.rb
@@ -1,12 +1,12 @@
 cask "appcode-eap" do
   arch = Hardware::CPU.intel? ? "" : "-aarch64"
 
-  version "2022.2,222.2270.19"
+  version "2022.2,222.2889.2"
 
   if Hardware::CPU.intel?
-    sha256 "a2cecd974616456678344b24d70ad19500617aa342daadeb09d4275cd04e3166"
+    sha256 "d51f2a6844bf4b6bcd9dbfc6c0fe7e9f822d24dfadc9adfcc6cf5513d1990495"
   else
-    sha256 "14d445716f29259b87df1964b485818928c11f8d5ff93a0f6325b65d081d2b2b"
+    sha256 "7f58bee6e91d804cbce1b213c3281f3f2f81399a40dd04fcc6af7215525fb5ae"
   end
 
   url "https://download.jetbrains.com/objc/AppCode-#{version.csv.second}#{arch}.dmg"
