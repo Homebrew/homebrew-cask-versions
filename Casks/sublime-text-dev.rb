0f65b1bf0be6adea47adedc63d888cdeb9511730
Bevan Kay <email@bevankay.me>
2022-06-04 23:50:50 +1000

    update version, livecheck

--- Casks/sublime-text-dev.rb
+++ Casks/sublime-text-dev.rb
@@ -1,21 +1,15 @@
 cask "sublime-text-dev" do
-  version "4.134"
+  version "4134"
   sha256 "05a1e27694952ce505f090935d9b0792ecd2eb4646a61dae578046d1b13ae672"
 
-  url "https://download.sublimetext.com/sublime_text_build_#{version.no_dots}_mac.zip"
+  url "https://download.sublimetext.com/sublime_text_build_#{version}_mac.zip"
   name "Sublime Text"
   desc "Text editor for code, markup and prose"
   homepage "https://www.sublimetext.com/dev"
 
   livecheck do
-    url :homepage
+    url "https://www.sublimetext.com/download_thanks_dev?target=mac"
     regex(/href=.*?v?(\d+)_mac\.zip/i)
-    strategy :page_match do |page, regex|
-      match = page.match(regex)[1]
-      next if match.blank?
-
-      "#{match[0]}.#{match[1..]}"
-    end
   end
 
   auto_updates true
@@ -24,21 +18,21 @@ cask "sublime-text-dev" do
   app "Sublime Text.app"
   binary "#{appdir}/Sublime Text.app/Contents/SharedSupport/bin/subl"
 
-  uninstall quit: "com.sublimetext.#{version.major}"
+  uninstall quit: "com.sublimetext.#{version[0]}"
 
   # Sublime Text 4 uses `Sublime Text 3` and `com.sublimetext.3` dirs if they exist
   # Otherwise, it creates `Sublime Text` and `com.sublimetext.4`
   # More info: https://www.sublimetext.com/docs/side_by_side.html
   zap trash: [
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.sublimetext.#{version.major}.sfl*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.sublimetext.#{version[0]}.sfl*",
     "~/Library/Application Support/Sublime Text",
     "~/Library/Application Support/Sublime Text (Safe Mode)",
     "~/Library/Application Support/Sublime Text 3",
-    "~/Library/Caches/com.sublimetext.#{version.major}",
+    "~/Library/Caches/com.sublimetext.#{version[0]}",
     "~/Library/Caches/com.sublimetext.3",
-    "~/Library/Preferences/com.sublimetext.#{version.major}.plist",
+    "~/Library/Preferences/com.sublimetext.#{version[0]}.plist",
     "~/Library/Preferences/com.sublimetext.3.plist",
-    "~/Library/Saved Application State/com.sublimetext.#{version.major}.savedState",
+    "~/Library/Saved Application State/com.sublimetext.#{version[0]}.savedState",
     "~/Library/Saved Application State/com.sublimetext.3.savedState",
   ]
 end
