bac8468df1dfc1fd3c78e228952d9bebc88fc98c
owine <owine@users.noreply.github.com>
2022-06-08 11:50:21 -0500

    Update signal-beta from 5.46.0-beta.1 to 5.46.0-beta.2

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,12 +1,12 @@
 cask "signal-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.46.0-beta.1"
+  version "5.46.0-beta.2"
 
   if Hardware::CPU.intel?
-    sha256 "0c573b29b17c72eceffe1967448a69ec4a00080164903eadfa4eb80b0b77446d"
+    sha256 "1142d835c5f27246fbafd9473a11dfcdb544b3288cc5839c72062817ac00ed37"
   else
-    sha256 "8926dea29a6ed9d1548c46463c5cb5ef5bac10e99201a679fc94783e7d6b5b16"
+    sha256 "b83549a3757902e33499ccc4590d04b8287e904b4ab86cb72d0ed3add672a805"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
