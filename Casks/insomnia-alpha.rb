e5a5b40857a313f6090f069d2439df95e05e8788
PythonCoderAS <sarkaraoyan@gmail.com>
2022-06-10 00:26:07 -0400

    Update insomnia-alpha from 2022.4.0-beta.3 to 2022.4.0-beta.4 (#14068)

--- Casks/insomnia-alpha.rb
+++ Casks/insomnia-alpha.rb
@@ -1,6 +1,6 @@
 cask "insomnia-alpha" do
-  version "2022.4.0-beta.3"
-  sha256 "c596c1332efc74af841ff996e0b35a4c238f6cb83488ef570127c084fcc6510a"
+  version "2022.4.0-beta.4"
+  sha256 "79871771303e7de0e153ffa4365e7c37329caffd95df2789b84555306231911e"
 
   url "https://github.com/Kong/insomnia/releases/download/core%40#{version}/Insomnia.Core-#{version}.dmg",
       verified: "github.com/Kong/insomnia/"
