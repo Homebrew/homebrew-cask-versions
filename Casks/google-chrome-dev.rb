bda4b1b876ab48939bf56c587eebc3176a6ffb75
PythonCoderAS <sarkaraoyan@gmail.com>
2022-06-11 12:16:05 -0400

    Update google-chrome-dev from 104.0.5098.0 to 104.0.5110.0 (#14083)

--- Casks/google-chrome-dev.rb
+++ Casks/google-chrome-dev.rb
@@ -1,7 +1,7 @@
 cask "google-chrome-dev" do
   arch = Hardware::CPU.intel? ? "" : "universal/"
 
-  version "104.0.5098.0"
+  version "104.0.5110.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/#{arch}dev/googlechromedev.dmg"
