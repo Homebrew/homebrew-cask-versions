2c82b4985f342a684a6089b587d56a805996d258
Lex.Chen <ctyytc@gmail.com>
2022-06-14 09:57:49 +0800

    Update Opera Crypto Browser Developer from 88.0.4412.11 to 88.0.4412.37
    Update Opera Crypto Browser Developer from 88.0.4412.11 to 88.0.4412.37

--- Casks/opera-crypto-developer.rb
+++ Casks/opera-crypto-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-crypto-developer" do
-  version "88.0.4412.11"
-  sha256 "58fd80998ed34fd0ff9ef5df640d852ff1d12b48337748f7688913c28c123f12"
+  version "88.0.4412.37"
+  sha256 "dab91bbf73fe6938fa01b1b58bab2142c2abb01628c6b734397afd3dbe5ec867"
 
   url "https://ftp.opera.com/pub/opera_crypto-developer/#{version}/mac/Opera_Crypto_Developer_#{version}_Setup.dmg"
   name "Opera Crypto Browser Developer"
