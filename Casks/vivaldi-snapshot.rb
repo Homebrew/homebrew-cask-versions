22a3263aa0f0a0d21f5050489131869e1e99b399
Hannes Braun <hannesbraun@mail.de>
2022-06-15 16:30:19 +0200

    Update vivaldi-snapshot from 5.4.2704.3 to 5.4.2706.3

--- Casks/vivaldi-snapshot.rb
+++ Casks/vivaldi-snapshot.rb
@@ -1,6 +1,6 @@
 cask "vivaldi-snapshot" do
-  version "5.4.2704.3"
-  sha256 "dbebc697ea1c000b404257d5396d94bab6040719631b868d1763b8062559c67f"
+  version "5.4.2706.3"
+  sha256 "f34b3b88b8321f415dc55b6b94e752e2f72af566e46e858571c72f2e01bd58fc"
 
   url "https://downloads.vivaldi.com/snapshot/Vivaldi.#{version}.universal.dmg"
   name "Vivaldi"
