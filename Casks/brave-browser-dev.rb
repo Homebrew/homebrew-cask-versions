7143822101f9f153fd50bd230b5fd91c501f5232
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2022-06-16 09:45:17 -0400

    Update brave-browser-dev from 1.40.97.0 to 1.41.69.0

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -2,12 +2,12 @@ cask "brave-browser-dev" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
   folder = Hardware::CPU.intel? ? "dev" : "dev-arm64"
 
-  version "1.40.97.0,140.97"
+  version "1.41.69.0,141.69"
 
   if Hardware::CPU.intel?
-    sha256 "e95646413f1ce1d579acfb348873b8b27297cd6addedb7ed63647c2b1c7e0f37"
+    sha256 "b1a2a0aa8fa61e8a751a7ea3c66fe6c78cba124bbae63ea684f4046f0b00c8f6"
   else
-    sha256 "c39df6b68f4c37db65f0a905f3b5dc7689270c71945acd887f856ec32b4ca661"
+    sha256 "c00a6329b19e9da3af11759e01bfe7c31f1ff7206301bd541881805512aa705b"
   end
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Dev-#{arch}.dmg",
