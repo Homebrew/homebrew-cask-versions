f26aec17a51ba73c1366c1759c85bcdf6bca24af
Hannes Braun <hannesbraun@mail.de>
2022-06-23 14:07:27 +0200

    Update openshot-video-editor-daily from 2.6.1,9519-7c6fcb0c-46255e46 to 2.6.1,9537-5776efd7-46255e46

--- Casks/openshot-video-editor-daily.rb
+++ Casks/openshot-video-editor-daily.rb
@@ -1,6 +1,6 @@
 cask "openshot-video-editor-daily" do
-  version "2.6.1,9519-7c6fcb0c-46255e46"
-  sha256 "983632214fd0de9550f669d6c2b6f2aff497ee78dedb7bbeedefcdd0cbafa944"
+  version "2.6.1,9537-5776efd7-46255e46"
+  sha256 "0cbb5c2e2d12d08d5318738b7d935bdd460df02f98c051bef419959eb9d2334b"
 
   url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-dev-daily-#{version.csv.second}-x86_64.dmg",
       verified: "github.com/OpenShot/openshot-qt/"
