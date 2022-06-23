e83939a83f7c91f9d56f743ea24c871a8fc60c21
Hannes Braun <hannesbraun@mail.de>
2022-06-23 14:02:10 +0200

    Update opera-beta from 89.0.4447.12 to 89.0.4447.20

--- Casks/opera-beta.rb
+++ Casks/opera-beta.rb
@@ -1,6 +1,6 @@
 cask "opera-beta" do
-  version "89.0.4447.12"
-  sha256 "9f8ab52e14cdd9bf22aad475c7ed83aa9a05665bf1ae64e05daf76fd0ddf6040"
+  version "89.0.4447.20"
+  sha256 "36aa054fff6ad5ec6c0950ceec4129823dd036ae3a72d83885757a28c2d4b36a"
 
   url "https://get.geo.opera.com/pub/opera-beta/#{version}/mac/Opera_beta_#{version}_Setup.dmg"
   name "Opera Beta"
