484ef5be3bee4e15f8b9c9a3c0761e419ae833dc
George Lukyanov <dissfall@icloud.com>
2022-06-25 17:15:08 +0000

    fix: download link

--- Casks/godot-alpha.rb
+++ Casks/godot-alpha.rb
@@ -2,7 +2,7 @@ cask "godot-alpha" do
     version "4.0-alpha10"
     sha256 "a4f6e466338253cbf2e5334534d9d7aad42773691f7a7749ebe97a3b95d9788a"
 
-    url "https://downloads.tuxfamily.org/godotengine/#{version.major_minor}/#{version.delete("#{version.major_minor}-")}/Godot_v#{version}_osx.universal.zip",
+    url "https://downloads.tuxfamily.org/godotengine/#{version.sub(%r{-alpha\d+}, '')}/#{version.minor.sub(%r{\d+-}, '')}/Godot_v#{version}_osx.universal.zip",
       verified: "downloads.tuxfamily.org/godotengine/"
     name "Godot Engine"
     desc "Game development engine"
