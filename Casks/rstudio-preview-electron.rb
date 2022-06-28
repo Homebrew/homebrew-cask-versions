600d77376fa12e688792dec3dbe31fce0d71ca23
D-Berg <danielfjberg@gmail.com>
2022-06-28 14:03:19 +0200

    New cask for electron version of rstudio-preview.

---
+++ Casks/rstudio-preview-electron.rb
@@ -0,0 +1,25 @@
+cask "rstudio-preview-electron" do
+  version "2022.07.0,543"
+  sha256 "6643fc31845de49e8a985338b872b6f2183e09b1519193b4a1dccde352e9cca8"
+
+  url "https://s3.amazonaws.com/rstudio-ide-build/electron/macos/RStudio-#{version.csv.first}-preview-#{version.csv.second}.dmg",
+      verified: "s3.amazonaws.com/rstudio-ide-build/"
+  name "RStudio"
+  desc "Data science software focusing on R and Python"
+  homepage "https://www.rstudio.com/products/rstudio/download/preview/"
+
+  livecheck do
+    url :homepage
+    regex(/RStudio-(\d{4}\.\d{2}\.\d+)-preview-(\d+)\.dmg/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+    end
+  end
+
+  conflicts_with cask: "rstudio"
+  depends_on macos: ">= :high_sierra"
+
+  app "RStudio.app"
+
+  zap trash: "~/.rstudio-desktop"
+end
