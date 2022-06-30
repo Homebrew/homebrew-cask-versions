f0b478f23c2994f38c4b12a62e303d075173da5b
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:29:42 +0100

    propresenter6: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/propresenter6.rb
+++ Casks/propresenter6.rb
@@ -12,15 +12,15 @@ cask "propresenter6" do
   app "ProPresenter #{version.major}.app"
 
   zap trash: [
-    "~/Library/Application Support/RenewedVision/ProPresenter#{version.major}",
-    "~/Library/Caches/KSCrashReports/ProPresenter #{version.major}",
-    "~/Library/Caches/Sessions/ProPresenter #{version.major}",
-    "~/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
-    "~/Library/Preferences/com.renewedvision.ProPresenter#{version.major}.plist",
-    "/Library/Application Support/RenewedVision",
-    "/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
-    "/Users/Shared/Renewed Vision Media",
-  ],
+        "~/Library/Application Support/RenewedVision/ProPresenter#{version.major}",
+        "~/Library/Caches/KSCrashReports/ProPresenter #{version.major}",
+        "~/Library/Caches/Sessions/ProPresenter #{version.major}",
+        "~/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
+        "~/Library/Preferences/com.renewedvision.ProPresenter#{version.major}.plist",
+        "/Library/Application Support/RenewedVision",
+        "/Library/Caches/com.renewedvision.ProPresenter#{version.major}",
+        "/Users/Shared/Renewed Vision Media",
+      ],
       rmdir: [
         "~/Library/Application Support/RenewedVision",
         "~/Library/Caches/KSCrashReports",
