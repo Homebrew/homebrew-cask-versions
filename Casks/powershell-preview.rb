e3b4a7a3f0b0d25d1940916093204ed7fefe9570
Mike McQuaid <mike@mikemcquaid.com>
2022-06-30 08:29:41 +0100

    powershell-preview: brew style --fix
    For https://github.com/Homebrew/brew/pull/13477

--- Casks/powershell-preview.rb
+++ Casks/powershell-preview.rb
@@ -27,10 +27,10 @@ cask "powershell-preview" do
   uninstall pkgutil: "com.microsoft.powershell-preview"
 
   zap trash: [
-    "~/.cache/powershell",
-    "~/.config/PowerShell",
-    "~/.local/share/powershell",
-  ],
+        "~/.cache/powershell",
+        "~/.config/PowerShell",
+        "~/.local/share/powershell",
+      ],
       rmdir: [
         "~/.cache",
         "~/.config",
