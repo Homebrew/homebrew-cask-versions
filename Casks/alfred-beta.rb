124057313544a4ad3ab31f8e0bfc26fb26dc2364
Jacob Ford <j@cobford.com>
2022-07-06 11:55:55 -0400

    Add Alfred 5 Early Access program

---
+++ Casks/alfred-beta.rb
@@ -0,0 +1,34 @@
+cask "alfred-beta" do
+  version "5.0,2042"
+  sha256 "2b6dbac94210629eecf9caa8daf99a472b5a93846d11525b3469e1bf593068cb"
+
+  url "https://cachefly.alfredapp.com/Alfred_#{version.csv.first}_EA6_#{version.csv.second}.dmg"
+  name "Alfred"
+  desc "Application launcher and productivity software"
+  homepage "https://www.alfredapp.com/alfred-5-whats-new/"
+
+  livecheck do
+    url "https://www.alfredapp.com/app/update#{version.major}/general.xml"
+    strategy :page_match do |page|
+      match = page.match(/Alfred[._-]v?(\d(?:\.\d+)+)[._-]EA6[._-](\d+)\.tar\.gz/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
+  auto_updates true
+
+  app "Alfred #{version.major}.app"
+
+  uninstall quit: "com.runningwithcrayons.Alfred"
+
+  zap trash: [
+    "~/Library/Application Support/Alfred",
+    "~/Library/Caches/com.runningwithcrayons.Alfred",
+    "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred.plist",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist",
+    "~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState",
+  ]
+end
