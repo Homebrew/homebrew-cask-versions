5280267f49a505e6547e27f5e0fca945d67db66c
Jacob Ford <j@cobford.com>
2022-07-07 06:39:46 +0200

    Add Alfred 5 Early Access program (#14257)
    * Add Alfred 5 Early Access program
    
    * Include 'EA6' in version string
    
    (not sure what 6 refers to)
    
    * Version bump + url format changed
    
    * Rename to alfred-earlyaccess
    
    https://github.com/Homebrew/homebrew-cask-versions/pull/14257\#issuecomment-1176418349

---
+++ Casks/alfred-earlyaccess.rb
@@ -0,0 +1,34 @@
+cask "alfred-earlyaccess" do
+  version "5.0.0,EA7,2043"
+  sha256 "6540c45fda4e5791a0b4ab19ba2835a6e3f369e33aa12f59078132c6fc5be7ca"
+
+  url "https://cachefly.alfredapp.com/Alfred_#{version.csv.first}-#{version.csv.second}_#{version.csv.third}.dmg"
+  name "Alfred"
+  desc "Application launcher and productivity software"
+  homepage "https://www.alfredapp.com/alfred-5-whats-new/"
+
+  livecheck do
+    url "https://www.alfredapp.com/app/update#{version.major}/general.xml"
+    strategy :page_match do |page|
+      match = page.match(/Alfred[._-]v?(\d(?:\.\d+)+)[._-](EA\d+)[._-](\d+)\.tar\.gz/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]},#{match[3]}"
+    end
+  end
+
+  auto_updates true
+
+  app "Alfred #{version.major}.app"
+
+  uninstall quit: "com.runningwithcrayons.Alfred"
+
+  zap trash: [
+    "~/Library/Application Support/Alfred",
+    "~/Library/Caches/com.runningwithcrayons.Alfred",
+    "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred.plist",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist",
+    "~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState",
+  ]
+end
