823ea1567542bbcdce621df1beed3ac53bb6d638
flieger19 <constantin.dullo@gmail.com>
2022-07-07 17:04:44 +0200

    Update picoscope-beta from 7.0.102.11677 to 7.0.103.11796 (#14261)

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,6 +1,6 @@
 cask "picoscope-beta" do
-  version "7.0.102.11677"
-  sha256 "9a970daf57f9ec22909ba17d9991ba8fe046b98a796beb7e7057803aba3f70b3"
+  version "7.0.103.11796"
+  sha256 "d91232c654b220ea7dcb9b536a9e1b6608e08c7b6c53f9433f454e37a6b76361"
 
   url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
   name "PicoScope beta"
