708965ff12b13cee8255064bc1b68407efc3c3d0
Jiahao Lu <lujjjh@gmail.com>
2022-07-13 06:40:41 +0800

    tinymediamanager3: fix url and livecheck (#14284)
    * tinymediamanager3: fix url
    
    * tinymediamanager3: fix livecheck
    
    * Update tinymediamanager3.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/tinymediamanager3.rb
+++ Casks/tinymediamanager3.rb
@@ -2,14 +2,14 @@ cask "tinymediamanager3" do
   version "3.1.16.1"
   sha256 "f363f3062c6524aded5437ee14adb4d63baaf1dfdc8bb09454fd7255031aec5d"
 
-  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
+  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_macos.zip"
   name "tinyMediaManager"
   desc "Media management tool, V3 version no license required"
   homepage "https://www.tinymediamanager.org/"
 
   livecheck do
-    url "https://release.tinymediamanager.org/"
-    regex(%r{href=.*?/tmm[._-]v?(3(?:\.\d+)+)[._-]mac\.zip}i)
+    url "https://release.tinymediamanager.org/download_v#{version.major}.html"
+    regex(%r{href=.*?/tmm[._-]v?(3(?:\.\d+)+)[._-]macos\.zip}i)
   end
 
   auto_updates true
