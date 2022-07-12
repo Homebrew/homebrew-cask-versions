10d9adb7514567f0f1fb2b4f1fbc59d445b67f01
Jiahao Lu <lujjjh@gmail.com>
2022-07-12 17:53:17 +0800

    tinymediamanager3: fix url

--- Casks/tinymediamanager3.rb
+++ Casks/tinymediamanager3.rb
@@ -2,7 +2,7 @@ cask "tinymediamanager3" do
   version "3.1.16.1"
   sha256 "f363f3062c6524aded5437ee14adb4d63baaf1dfdc8bb09454fd7255031aec5d"
 
-  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
+  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_macos.zip"
   name "tinyMediaManager"
   desc "Media management tool, V3 version no license required"
   homepage "https://www.tinymediamanager.org/"
