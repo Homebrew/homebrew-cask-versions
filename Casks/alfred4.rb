8a871a7ee18835231d7ffcf0eddafd0e4ca3c0cd
p-linnane <patrick@linnane.io>
2022-07-13 16:21:53 -0700

    Readd livecheck

--- Casks/alfred4.rb
+++ Casks/alfred4.rb
@@ -7,6 +7,16 @@ cask "alfred4" do
   desc "Application launcher and productivity software"
   homepage "https://www.alfredapp.com/"
 
+  livecheck do
+    url "https://www.alfredapp.com/app/update#{version.major}/general.xml"
+    strategy :page_match do |page|
+      match = page.match(/Alfred[._-]v?(\d(?:\.\d+)+)[._-](\d+)\.t/i)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
+
   auto_updates true
 
   app "Alfred #{version.major}.app"
