bbd4a36554583b430c9e15136fcfbe3a9bf63583
Hannes Braun <hannesbraun@mail.de>
2022-07-20 21:17:15 +0700

    Update discord-ptb from 0.0.62 to 0.0.63 (#14334)

--- Casks/discord-ptb.rb
+++ Casks/discord-ptb.rb
@@ -1,6 +1,6 @@
 cask "discord-ptb" do
-  version "0.0.62"
-  sha256 "b5a56ef4bc73b386fc8bb3da3c08e275b57de553aad3b9840a24ff4267e6b534"
+  version "0.0.63"
+  sha256 "25b037e7e16037f321a262a7715f2f8bd19f21d25ee8e1402f4f24a1b54efde6"
 
   url "https://dl-ptb.discordapp.net/apps/osx/#{version}/DiscordPTB.dmg",
       verified: "dl-ptb.discordapp.net/apps/osx/"
