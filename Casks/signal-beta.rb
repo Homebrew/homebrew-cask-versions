0cb156d29850a9f4fece7b14283e3951eb54235c
LanikSJ <laniksj@gmail.com>
2022-07-20 23:07:49 -0700

    Update signal-beta from 5.51.0-beta.1 to 5.52.0-beta.1

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,12 +1,12 @@
 cask "signal-beta" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-  version "5.51.0-beta.1"
+  version "5.52.0-beta.1"
 
   if Hardware::CPU.intel?
-    sha256 "1180d2046dd576e18f98e6d7062fa178833c14b48fee11a1c98de2ad49d85076"
+    sha256 "acfc246195fbb9b008348cbc3bd517b73f42dd24e55713757bb507b5f286958e"
   else
-    sha256 "deed1b35191c752bbd82272f678a0894fed2ccc6dfc6d80033502df6b69f335d"
+    sha256 "a301b0804a6d98368b9881aa8b3d503cb4a90f336f46e1179bf4e79a7373f6cb"
   end
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
