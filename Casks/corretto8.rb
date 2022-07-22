fc4944e7401cb5747c57c3a8ba27c7dbd33c5483
Ben Taylor <benty@amazon.com>
2022-07-22 10:12:42 -0700

    Update corretto8 from 8.342.07.1 to 8.342.07.3

--- Casks/corretto8.rb
+++ Casks/corretto8.rb
@@ -1,11 +1,11 @@
 cask "corretto8" do
   arch = Hardware::CPU.intel? ? "x64" : "aarch64"
-  version "8.342.07.1"
+  version "8.342.07.3"
 
   if Hardware::CPU.intel?
-    sha256 "d93a108ef9dae8f8979fc5d2ae53613fb2b60e9d42be41bac5a22f064c17493e"
+    sha256 "5bde0f3682e9f809d620220bbca414a3493af41ce219509d840b17da85e0070a"
   else
-    sha256 "76e98930dcd72bf0f9a529e3d713fa3a9a2a3a0c7f09c8add2a8e47ffa3e7ef4"
+    sha256 "159d27625838c12f0fa6f6edc7b3c50924362f022c4490605f3798d947bcf9bf"
   end
 
   url "https://corretto.aws/downloads/resources/#{version}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
