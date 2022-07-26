11722e90b68bf4697fa27f0736a9b96e14659d78
Bevan Kay <email@bevankay.me>
2022-07-26 19:42:44 +1000

    update version, livecheck

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,28 +1,15 @@
 cask "sonos-s1-controller" do
-  version "11.5,57.12-31030"
+  version "57.12-31030"
   sha256 "d1a2f22dc0a9e4e7563b301698cdf8c5c5ddb54471216ce25ba8c5d225d95859"
 
-  url "https://update-software.sonos.com/software/ukxehnnu/Sonos_#{version.csv.second}.dmg"
+  url "https://update-software.sonos.com/software/ukxehnnu/Sonos_#{version}.dmg"
   name "Sonos S1"
   desc "Controller for Gen 1 Sonos products"
   homepage "https://www.sonos.com/"
 
   livecheck do
     url "https://www.sonos.com/en/redir/controller_software_mac"
-    strategy :header_match do |headers|
-      download_url = headers["location"]
-      next if download_url.blank?
-
-      cask = CaskLoader.load("sonos-s1-controller")
-
-      build = Homebrew::Livecheck::Strategy::ExtractPlist.find_versions(cask: cask,
-                                                                        url:  download_url)[:matches].values.first
-      next if build.blank?
-
-      formatted_version = version.include?(".") ? version : version.sub(/(\d\d)(\d+)/, '\1.\2')
-
-      "#{formatted_version},#{build}"
-    end
+    strategy :header_match
   end
 
   auto_updates true
