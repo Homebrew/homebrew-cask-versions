7ae99f978f3d8fdd29bd44c62e1f80d9261f5292
neutric <5984479+neutric@users.noreply.github.com>
2022-07-29 00:15:48 +0200

    Update aerial-beta from 3.0.8beta2 to 3.1.0beta2

--- Casks/aerial-beta.rb
+++ Casks/aerial-beta.rb
@@ -1,6 +1,6 @@
 cask "aerial-beta" do
-  version "3.0.8beta2"
-  sha256 "6ef8fb55bdc1855d1b056f01408b568f378aa652c3404290b8d57297696d6f09"
+  version "3.1.0beta2"
+  sha256 "7988ce7e50b227a7b454da86f83405e04ff1ef28a6545008b9fe589ebf947589"
 
   url "https://github.com/JohnCoates/Aerial/releases/download/v#{version}/Aerial.saver.zip",
       verified: "github.com/JohnCoates/Aerial/"
