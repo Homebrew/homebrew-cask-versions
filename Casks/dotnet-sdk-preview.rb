13cf7c391190dc4c7bd89af89fced29e2abe2008
neutric <5984479+neutric@users.noreply.github.com>
2022-08-01 23:55:10 +0200

    Update dotnet-sdk-preview from 7.0.100-preview.5.22307.18 to 7.0.100-preview.6.22352.1

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -2,8 +2,8 @@ cask "dotnet-sdk-preview" do
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   if Hardware::CPU.intel?
-    version "7.0.100-preview.5.22307.18,eb929c1e-1148-49fe-ab31-a711a678a023,f8b7c7eaec03e178e8a515ce8a3a7955"
-    sha256 "edb588842b5be03fbce29e13ff6d262236c785704cf6af3ba5543ac12596cf0a"
+    version "7.0.100-preview.6.22352.1,fc8101ee-2814-44a2-8851-4422ee2c135a,a819b175ab87c85f900648e38c3bb435"
+    sha256 "792cedba1df76aaafc67ebb2fe988d54c6062dad6802b4c11d2a81a4497b9664"
   else
     version "7.0.100-preview.3.22179.4,3b73829a-6e68-487d-ace5-f96b993f2383,9f1e0a9d4414c9aae6ce64b4f8c620c5"
     sha256 "7c4d779addb6c379c68fd1d3c2afa532063747097c7f5ef13d4815720e4f1ac9"
