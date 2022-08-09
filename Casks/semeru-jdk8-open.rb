1a6139042400f915cd37b8a179cdbf2585a27c4d
MoonFruit <dkmoonfruit@gmail.com>
2022-08-09 14:02:46 +0800

    Update semeru-jdk8-open from 8u332-b09 to 8u345-b01 (#14462)

--- Casks/semeru-jdk8-open.rb
+++ Casks/semeru-jdk8-open.rb
@@ -1,6 +1,6 @@
 cask "semeru-jdk8-open" do
-  version "8u332-b09,openj9-0.32.0"
-  sha256 "3a8e720aa1b14bd39ee5a16a46215b17bba43e4734e6c7e373204553e13360f2"
+  version "8u345-b01,openj9-0.33.0"
+  sha256 "f66581f1c898f6949c63482558b4861486923d1d583e8976cb5fdde73c2f6829"
 
   url "https://github.com/ibmruntimes/semeru8-binaries/releases/download/jdk#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("-", "")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru8-binaries/"
