33a1548905dc1541ad9907a91bd9079b2d3c91e9
Logicer <81077770+Logicer16@users.noreply.github.com>
2022-08-10 21:10:36 +1000

    Update arduino-ide-beta to rc9.1

--- Casks/arduino-ide-beta.rb
+++ Casks/arduino-ide-beta.rb
@@ -1,15 +1,15 @@
 cask "arduino-ide-beta" do
-  version "2.0.0,12"
-  sha256 "80e09f472d947b70d7777ca997e5c197ef70c7bee72817f3a0b1b6bfc784eb8f"
+  version "2.0.0,rc9.1"
+  sha256 "26097bbb8e921930cd4fbed76ad61268637cc08fc436f60232a314f5b05588ba"
 
-  url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version.csv.first}-beta.#{version.csv.second}_macOS_64bit.dmg"
+  url "https://downloads.arduino.cc/arduino-ide/arduino-ide_#{version.csv.first}-#{version.csv.second}_macOS_64bit.dmg"
   name "Arduino IDE"
   desc "Electronics prototyping platform"
   homepage "https://www.arduino.cc/en/software#experimental-software"
 
   livecheck do
     url "https://www.arduino.cc/en/software/"
-    regex(/href=.*?arduino[._-]ide[._-]v?(\d+(?:\.\d+)+)[._-]beta[._-]?(\d+)[._-]macos[._-]64bit\.dmg/i)
+    regex(/href=.*?arduino[._-]ide[._-]v?(\d+(?:\.\d+)+)[._-](\S+?)[._-]macos[._-]64bit\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
