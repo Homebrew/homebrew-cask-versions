a917e6a02f3385df7145cacb4466a908833b83a5
Rylan Polster <rslpolster@gmail.com>
2022-08-10 19:29:10 -0400

    zulu8: use `arch` and `on_arch_conditional` DSLs

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -1,6 +1,6 @@
 cask "zulu8" do
-  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
-  choice = Hardware::CPU.intel? ? "x86" : "arm"
+  arch arm: "aarch64", intel: "x64"
+  choice = on_arch_conditional arm: "arm", intel: "x86"
 
   version "8.0.345,8.64.0.19-ca"
 
