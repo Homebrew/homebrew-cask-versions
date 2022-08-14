93f8508671078894a15b896c6b5d53ffc0637477
Mikl Wolfe <wolfemikl@gmail.com>
2022-08-14 03:13:05 -0600

    Update google-chrome-canary from 106.0.5238.1 to 106.0.5239.0 (#14491)
    * Update google-chrome-canary from 106.0.5238.1 to 106.0.5239.0
    
    * Update google-chrome-canary.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "106.0.5197.0"
+  version "106.0.5239.1"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
