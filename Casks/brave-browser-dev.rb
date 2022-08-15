a96b39cbfa23d16f4937bd3953b04b2f1ebdfb8b
Rylan Polster <rslpolster@gmail.com>
2022-08-15 09:49:34 -0400

    brave-browser-dev: use `on_intel` and `on_arm` blocks

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -4,9 +4,10 @@ cask "brave-browser-dev" do
 
   version "1.43.68.0,143.68"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "d776865b06b338008eb4811460b7e1ee847a12d42fc3ec1c4ee418a2bf20b695"
-  else
+  end
+  on_arm do
     sha256 "2c9389cee5b6831e686f4a9998053f0c02b6b31f382f625ccd2b6db34ff1a369"
   end
 
