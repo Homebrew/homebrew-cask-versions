289f698927fc90443e407a4dc6f3783915666a24
Rylan Polster <rslpolster@gmail.com>
2022-08-15 09:49:35 -0400

    keepassxc-beta: use `on_intel` and `on_arm` blocks

--- Casks/keepassxc-beta.rb
+++ Casks/keepassxc-beta.rb
@@ -3,9 +3,10 @@ cask "keepassxc-beta" do
 
   version "2.7.1"
 
-  if Hardware::CPU.intel?
+  on_intel do
     sha256 "473f994698ec082f16bb20b4824dadbfb744f53a01b737b4016f6cc45f403b83"
-  else
+  end
+  on_arm do
     sha256 "97e7c57b4695cf4a558186cd36b89605ec6d6dec8791f7add043a3a387089f01"
   end
 
