aad6e6ab5f98b019750dc9fb1d8251c6404d34c0
Rylan Polster <rslpolster@gmail.com>
2022-08-15 17:51:11 -0400

    Update dotnet-preview from 7.0.0-preview.1.22076.8 to 7.0.0-preview.7.22375.6 (#14495)

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -1,12 +1,13 @@
 cask "dotnet-preview" do
   arch arm: "arm64", intel: "x64"
 
-  if Hardware::CPU.intel?
+  on_intel do
     version "7.0.0-preview.7.22375.6,b3d72f3f-cbb6-40cf-8f19-2df35554ad35,3f247901e4f567fdf5defcda474ce080"
     sha256 "a115c8e3e2de64bd8db74f47f625eebbc7d73822322111a582dacbefb56c8849"
-  else
-    version "7.0.0-preview.1.22076.8,17b7ed90-2cac-4a18-9d04-787fb76a0175,52773df7c2c4982728cc153111c8a0aa"
-    sha256 "1910170a8f4c1223a3b7a5cbcbdfb4763e7897e57570d4dd74ece98f08203814"
+  end
+  on_arm do
+    version "7.0.0-preview.7.22375.6,b84c6db5-5c65-4d26-b705-0c3f5320d2fa,48b49ac04cc7929e05db6a76cd7cd621"
+    sha256 "e3c7b2d4e324e18335e5fc514a67b9e0c2971cf2c8b92ca9d7617640bf8a5023"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-runtime-#{version.csv[0]}-osx-#{arch}.pkg"
