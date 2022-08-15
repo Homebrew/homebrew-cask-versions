055e6c8c754d0edb7fdd8d0d8a01fc7f6ac9aac7
Rylan Polster <rslpolster@gmail.com>
2022-08-15 10:15:28 -0400

    Update dotnet-sdk-preview from 7.0.100-preview.3.22179.4 to 7.0.100-preview.7.22377.5

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,12 +1,13 @@
 cask "dotnet-sdk-preview" do
   arch arm: "arm64", intel: "x64"
 
-  if Hardware::CPU.intel?
+  on_intel do
     version "7.0.100-preview.7.22377.5,c4688e7c-6076-4e7c-bc8d-99ff138f91ce,059f21d41a3e06aeba8ef02d465887ab"
     sha256 "d37cd53a737ff1c05310e3e71dcee260b17196476b9c8c4bfa5bb3ac8b6d06a9"
-  else
-    version "7.0.100-preview.3.22179.4,3b73829a-6e68-487d-ace5-f96b993f2383,9f1e0a9d4414c9aae6ce64b4f8c620c5"
-    sha256 "7c4d779addb6c379c68fd1d3c2afa532063747097c7f5ef13d4815720e4f1ac9"
+  end
+  on_arm do
+    version "7.0.100-preview.7.22377.5,064356b7-bf90-4c32-bfa9-f5acad1b24fb,dafa5b5f0b7c57d851f5af4d78db2f61"
+    sha256 "dd2cac1674c8208c9bfe9735833d18cabd57e116b998133c14e1eb3bd7251816"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-sdk-#{version.csv[0]}-osx-#{arch}.pkg"
