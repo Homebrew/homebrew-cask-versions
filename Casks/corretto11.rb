e300f7f9a1c53308b3e313a2e91357240d1e4202
Ben Taylor <61255184+benty-amzn@users.noreply.github.com>
2022-08-16 01:39:46 -0700

    Update corretto11 from 11.0.16.8.3 to 11.0.16.9.1 (#14501)

--- Casks/corretto11.rb
+++ Casks/corretto11.rb
@@ -1,10 +1,10 @@
 cask "corretto11" do
   arch arm: "aarch64", intel: "x64"
 
-  version "11.0.16.8.3"
+  version "11.0.16.9.1"
 
   on_intel do
-    sha256 "6ea0caa2d5cedb859ddb470e54dac08377c216b5921c10dedfb1c2f2e0c58545"
+    sha256 "1b256d7104ebc0f72d7745962e0ab47cbbbd976f93481f31bd4a7d40812d739a"
   end
   on_arm do
     sha256 "ba5fdc6ce029f8aa2e2789408266334c480616a058f49b3c8696a591fbc997a3"
