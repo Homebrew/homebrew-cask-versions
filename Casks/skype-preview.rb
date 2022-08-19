db2a5b0e2ac0d381a2dc9e3225522398a3448987
Alexander Ronald Altman <alexanderaltman@me.com>
2022-08-18 20:34:00 -0700

    Update skype-preview from 8.88.76.200 to 8.88.76.303

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask "skype-preview" do
-  version "8.88.76.200"
-  sha256 "c0c713221ce09d32bdf2b5d5a17ab4d79643ef56fb56c0ed2e030ee387df18f8"
+  version "8.88.76.303"
+  sha256 "6fe9e84304a3bb4b13484438a38449f3ef6a465ec009551eb37c1cb749a088bc"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
