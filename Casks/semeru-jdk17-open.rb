7a60f310a626f3a5c3b3f1c5fe118ffa7be3ce28
j9build <j9build@ca.ibm.com>
2022-08-22 19:26:18 -0400

    Update semeru-jdk17-open from 17.0.4+8 to 17.0.4.1+1 (#14543)

--- Casks/semeru-jdk17-open.rb
+++ Casks/semeru-jdk17-open.rb
@@ -1,6 +1,6 @@
 cask "semeru-jdk17-open" do
-  version "17.0.4+8,openj9-0.33.0"
-  sha256 "3ea181cf419ab5d591d342697f15dd3e95c672bca217f3d29da759f466bc4dbe"
+  version "17.0.4.1+1,openj9-0.33.1"
+  sha256 "e0bdb96cbe2693bd08cd5d71d42f99795bed6fade684fd1f4018abd01accfa16"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
