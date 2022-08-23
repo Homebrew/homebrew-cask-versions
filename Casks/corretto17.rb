8e6680d7bbeca822ac3afe9606d0ea162cc1fb5f
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:29:41 -0400

    corretto17: use `sha256 arm: "...", intel: "..."`

--- Casks/corretto17.rb
+++ Casks/corretto17.rb
@@ -2,13 +2,8 @@ cask "corretto17" do
   arch arm: "aarch64", intel: "x64"
 
   version "17.0.4.9.1"
-
-  on_intel do
-    sha256 "a91db130cbf99ab993d5d579a71ee7cd7c2682c2b6698d9353db1a5393b01f7f"
-  end
-  on_arm do
-    sha256 "d46bf6798a54c283cc4297dc86cb0173571b7dfc70b3609846911bb5ac3ffdb8"
-  end
+  sha256 arm:   "d46bf6798a54c283cc4297dc86cb0173571b7dfc70b3609846911bb5ac3ffdb8",
+         intel: "a91db130cbf99ab993d5d579a71ee7cd7c2682c2b6698d9353db1a5393b01f7f"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
   name "AWS Corretto JDK"
