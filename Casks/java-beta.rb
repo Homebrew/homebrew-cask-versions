7b7b34a866b14ba707db2a421eef8e2d00b84b19
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:29:42 -0400

    java-beta: use `sha256 arm: "...", intel: "..."`

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -2,13 +2,8 @@ cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
   version "19,877d6127e982470ba2a7faa31cc93d04,35"
-
-  on_intel do
-    sha256 "e78eae7d8957f27d530196c59f9b6cba68931ee94ee30fb49f2fe6d17a44a882"
-  end
-  on_arm do
-    sha256 "a54aa5134e797c41c516937e555caa671ba70dafbac1b78d74751333f673061d"
-  end
+  sha256 arm:   "a54aa5134e797c41c516937e555caa671ba70dafbac1b78d74751333f673061d",
+         intel: "e78eae7d8957f27d530196c59f9b6cba68931ee94ee30fb49f2fe6d17a44a882"
 
   url "https://download.java.net/java/GA/jdk#{version.major}/#{version.csv.second}/#{version.csv.third}/GPL/openjdk-#{version.csv.first}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
