316f3f8dde7672760ced24d2fb13254200b8561c
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:29:41 -0400

    brave-browser-dev: use `sha256 arm: "...", intel: "..."`

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -3,13 +3,8 @@ cask "brave-browser-dev" do
   folder = on_arch_conditional arm: "dev-arm64", intel: "dev"
 
   version "1.43.75.0,143.75"
-
-  on_intel do
-    sha256 "451b0030323131721e743c24ebbc36460cbe7da234264bddd9a851cce5dbb847"
-  end
-  on_arm do
-    sha256 "e3ce0108d8ba2420284272ffe550bd777223949e1ec92e205b6e5c4a79437a57"
-  end
+  sha256 arm:   "e3ce0108d8ba2420284272ffe550bd777223949e1ec92e205b6e5c4a79437a57",
+         intel: "451b0030323131721e743c24ebbc36460cbe7da234264bddd9a851cce5dbb847"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Dev-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
