c1f42ce74781c4f1db5e68f680d945fb54c89828
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:29:43 -0400

    zulu17: use `sha256 arm: "...", intel: "..."`

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -3,13 +3,8 @@ cask "zulu17" do
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
   version "17.0.4,17.36.13-ca"
-
-  on_intel do
-    sha256 "3a3143b0f89ad5b07b44fa81078f38ef181d6f4e03649ab04360a261be64284a"
-  end
-  on_arm do
-    sha256 "7a945596e479b2e3aeb40a89d05fd54652ba9c96d69b8dddae7656c7e171eb47"
-  end
+  sha256 arm:   "7a945596e479b2e3aeb40a89d05fd54652ba9c96d69b8dddae7656c7e171eb47",
+         intel: "3a3143b0f89ad5b07b44fa81078f38ef181d6f4e03649ab04360a261be64284a"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
