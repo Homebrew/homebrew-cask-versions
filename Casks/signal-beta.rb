41975abd042f6463cfa5ba66ab17c4d3872d6ddd
Rylan Polster <rslpolster@gmail.com>
2022-08-23 02:29:42 -0400

    signal-beta: use `sha256 arm: "...", intel: "..."`

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -2,13 +2,8 @@ cask "signal-beta" do
   arch arm: "arm64", intel: "x64"
 
   version "5.56.0-beta.1"
-
-  on_intel do
-    sha256 "91117ef684ef8985bea49b04f39fcb6ce9873100399c39594fd2a1715c6dfdb8"
-  end
-  on_arm do
-    sha256 "da3ccf3039de3f48ce07701823da218802b8c1be8fee8e71978d6b5c797f95f1"
-  end
+  sha256 arm:   "da3ccf3039de3f48ce07701823da218802b8c1be8fee8e71978d6b5c797f95f1",
+         intel: "91117ef684ef8985bea49b04f39fcb6ce9873100399c39594fd2a1715c6dfdb8"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
   name "Signal Beta"
