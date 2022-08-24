7b3b79dbafd29ba6df8cd53796db673942b0e2af
Yusuf Kürşad Kaya <ykursadkaya@gmail.com>
2022-08-24 16:17:30 +0300

    Update temurin11 from 11.0.16 to 11.0.16.1 (#14557)

--- Casks/temurin11.rb
+++ Casks/temurin11.rb
@@ -1,9 +1,9 @@
 cask "temurin11" do
   arch arm: "aarch64", intel: "x64"
 
-  version "11.0.16,8"
-  sha256 arm:   "7478f20b24db0c7904902fbaa2915943a9d9fd492d556d85a4cc83a802735ecb",
-         intel: "d0607b02fbaf95bffc5ccb44798883111e5fd277e589865f58647335a1334139"
+  version "11.0.16.1,1"
+  sha256 arm:   "ba2e37d5ed3652e42536a60563e4b44681b44c9de395e635aefe8b9bede49f25",
+         intel: "87975fbabc6ac91320eb87520e10b39b08f596b27a874f42235883348647196e"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
