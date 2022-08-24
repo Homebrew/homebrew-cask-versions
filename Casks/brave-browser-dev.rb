232f5a5c62585e86fa6398fbff5bcbc720af7878
Hannes Braun <hannesbraun@mail.de>
2022-08-24 16:05:36 +0200

    Update brave-browser-dev from 1.43.75.0 to 1.43.80.0 (#14558)

--- Casks/brave-browser-dev.rb
+++ Casks/brave-browser-dev.rb
@@ -2,9 +2,9 @@ cask "brave-browser-dev" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "dev-arm64", intel: "dev"
 
-  version "1.43.75.0,143.75"
-  sha256 arm:   "e3ce0108d8ba2420284272ffe550bd777223949e1ec92e205b6e5c4a79437a57",
-         intel: "451b0030323131721e743c24ebbc36460cbe7da234264bddd9a851cce5dbb847"
+  version "1.43.80.0,143.80"
+  sha256 arm:   "4f856a4d5bcb7835eb3eafd94c73cb9fa3605eee8f9ffacf09d31ebc918a2a7a",
+         intel: "96122584955c1700ff832263784ed86254e04d308fb96171d7478d4fd038f5b6"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Dev-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
