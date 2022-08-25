21627b1f3cbf022e4b21c0a098f429ed96ed5d7e
chiefmikey <wolfemikl@gmail.com>
2022-08-25 05:00:40 -0600

    Update zulu17 from 17.0.4 to 17.0.4.1

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -2,9 +2,9 @@ cask "zulu17" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "17.0.4,17.36.13-ca"
-  sha256 arm:   "7a945596e479b2e3aeb40a89d05fd54652ba9c96d69b8dddae7656c7e171eb47",
-         intel: "3a3143b0f89ad5b07b44fa81078f38ef181d6f4e03649ab04360a261be64284a"
+  version "17.0.4.1,17.36.17-ca"
+  sha256 arm:   "92245d2a7876d465875822eb487340e8f11ba7e00e249bcec3e801bfea45dff6",
+         intel: "e46dcae9aa96d42b35442f0a379f3c7c8f8ade20f081b5804ba9e6e47421cc7c"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
