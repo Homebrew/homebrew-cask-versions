67cbce1344a0241fd7bd6ad7c4d2f5319a36e366
chiefmikey <wolfemikl@gmail.com>
2022-08-25 04:40:03 -0600

    Update google-chrome-canary from 107.0.5259.1 to 107.0.5260.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "107.0.5259.1"
+  version "107.0.5260.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
