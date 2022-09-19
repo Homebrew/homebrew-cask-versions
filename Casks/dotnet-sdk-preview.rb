d812d06659047c9903fefb9ae20195514c602134
Alexander Ronald Altman <alexanderaltman@me.com>
2022-09-18 20:32:57 -0700

    Update dotnet-sdk-preview from 7.0.100-preview.7.22377.5 to 7.0.100-rc.1.22431.12 (#14707)
    * Update dotnet-sdk-preview from 7.0.100-preview.7.22377.5 to 7.0.100-rc.1.22431.12
    
    * Update dotnet-sdk-preview.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -2,12 +2,12 @@ cask "dotnet-sdk-preview" do
   arch arm: "arm64", intel: "x64"
 
   on_intel do
-    version "7.0.100-preview.7.22377.5,c4688e7c-6076-4e7c-bc8d-99ff138f91ce,059f21d41a3e06aeba8ef02d465887ab"
-    sha256 "d37cd53a737ff1c05310e3e71dcee260b17196476b9c8c4bfa5bb3ac8b6d06a9"
+    version "7.0.100-rc.1.22431.12,dae368f5-c03c-48b6-a7de-6dc423c3a5b7,180e601942c810f45f87fc75be783fbc"
+    sha256 "dd504b6f5cd42e331932d6e91730794622babbfcf1e897ec93de5b813a1a2339"
   end
   on_arm do
-    version "7.0.100-preview.7.22377.5,064356b7-bf90-4c32-bfa9-f5acad1b24fb,dafa5b5f0b7c57d851f5af4d78db2f61"
-    sha256 "dd2cac1674c8208c9bfe9735833d18cabd57e116b998133c14e1eb3bd7251816"
+    version "7.0.100-rc.1.22431.12,a600948e-4b2b-40d2-8551-57d85492df13,196fed4c3c6c5498a0b861127353cc07"
+    sha256 "681917e612a9cc5b623e0585bd6f0c5fedccc2c8a0c151611b0dbf2ffc9257de"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-sdk-#{version.csv[0]}-osx-#{arch}.pkg"
