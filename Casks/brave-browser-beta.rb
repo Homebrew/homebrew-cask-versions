8b74a9abb35ee86d0289bf9a717544cd85fe8622
Hannes Braun <hannesbraun@mail.de>
2022-09-21 12:58:09 +0200

    Update brave-browser-beta from 1.44.86.0 to 1.44.93.0

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -2,9 +2,9 @@ cask "brave-browser-beta" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "beta-arm64", intel: "beta"
 
-  version "1.44.86.0,144.86"
-  sha256 arm:   "20a8eee3f4b24c9ea0b06f3e48d72d3bca772250b965b5f8b563d10dce5164c6",
-         intel: "dcf945c17e357ff4afa420300716e8f1d88ca8b0e9a2b7b6cb01c9027e6a3204"
+  version "1.44.93.0,144.93"
+  sha256 arm:   "b7564c80d46ef6f38148e3de4754176e0bc1cfbd04be4961b692b4f56d2d43b5",
+         intel: "81771a7fcde77fd0a1ca3d674c4fa398d8421e95f306c3ffc3458de6d5142486"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Beta-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
