035d72ba2e1617d31dad96c2cf4f85dd4591a063
Hannes Braun <hannesbraun@mail.de>
2022-09-21 13:00:14 +0200

    Update google-chrome-canary from 107.0.5272.0 to 108.0.5313.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "107.0.5272.0"
+  version "108.0.5313.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
