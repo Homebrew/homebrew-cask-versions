02247a123885deaa6898b08a0a351541b009246c
xxs0 <mircowish@gmail.com>
2022-09-25 04:42:30 +0800

    Fix download url and Upgrade Tinymediamanager to v3.1.17 (#14736)
    Fix download url and Upgrade to v3.1.17

--- Casks/tinymediamanager3.rb
+++ Casks/tinymediamanager3.rb
@@ -1,15 +1,15 @@
 cask "tinymediamanager3" do
-  version "3.1.16.1"
-  sha256 "f363f3062c6524aded5437ee14adb4d63baaf1dfdc8bb09454fd7255031aec5d"
+  version "3.1.17"
+  sha256 "4d23975d03daa2d1a77662bdc712f7d4621e35323d28214e2f25566231ca1ae3"
 
-  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_macos.zip"
+  url "https://release.tinymediamanager.org/v#{version.major}/dist/tmm_#{version}_mac.zip"
   name "tinyMediaManager"
   desc "Media management tool, V3 version no license required"
   homepage "https://www.tinymediamanager.org/"
 
   livecheck do
     url "https://release.tinymediamanager.org/download_v#{version.major}.html"
-    regex(%r{href=.*?/tmm[._-]v?(3(?:\.\d+)+)[._-]macos\.zip}i)
+    regex(%r{href=.*?/tmm[._-]v?(3(?:\.\d+)+)[._-]mac\.zip}i)
   end
 
   auto_updates true
