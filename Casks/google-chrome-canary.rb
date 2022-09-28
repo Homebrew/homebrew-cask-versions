42eb32cf391c28470f9fbf1299e0b403b802129b
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-09-28 20:17:35 +0500

    Update google-chrome-canary.rb

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "108.0.5321.0"
+  version "108.0.5325.2"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
