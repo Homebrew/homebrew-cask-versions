b44e22bc87f57e997723e02de06f39bfeffd5668
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-09-29 08:44:31 +0500

    Update Google Chrome (Canary) to v108.0.5326.0 (#14755)
    * Update Google Chrome (Canary) to v108.0.5321.0
    
    * Update google-chrome-canary.rb
    
    * Update google-chrome-canary.rb
    
    * Update google-chrome-canary.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "108.0.5313.0"
+  version "108.0.5328.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
