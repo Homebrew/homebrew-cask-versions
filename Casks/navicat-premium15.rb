9bc897afa7ab8098bd0e47993d7f09cb891d9a19
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-04 08:38:56 +0800

    Update navicat-premium15.rb

--- Casks/navicat-premium15.rb
+++ Casks/navicat-premium15.rb
@@ -1,19 +1,19 @@
 cask "navicat-premium15" do
   version "15.0.35"
-  sha256 "892b55aaa17d8d9fc34cdf26206147956686e0cfcd9ff0f6dd1cfe7a5e615bc9"
+  sha256 :no_check
 
-  language "en", default: true do
-    url "http://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
-    homepage "https://www.navicat.com/en/company/aboutus/blog/1300-navicat-premium-15-the-most-powerful-yet-2"
-  end
-
-  name "Navicat Premium v15"
-  desc "Database administration and development tool, version 15"
+  url "https://download.navicat.com/download/navicat#{version.major_minor.no_dots}_premium_en.dmg"
+  name "Navicat Premium 15"
+  desc "Database administration and development tool"
+  homepage "https://www.navicat.com/products/navicat-premium"
 
   livecheck do
     url "https://www.navicat.com/en/products/navicat-premium-release-note#M"
     regex(/Navicat\sPremium\s\(macOS\)\sversion\s(15\.\d+(?:\.\d+)+)/i)
   end
 
-  app "Navicat Premium.app", target: "Navicat Premium 15.app"
+  conflicts_with cask: "navicat-premium"
+  depends_on macos: ">= :mojave"
+
+  app "Navicat Premium.app"
 end
