36ee16f998482e4af8fbb173a275dc022393cf93
Constantin Dullo <constantin.dullo@gmail.com>
2022-10-06 12:12:02 +0200

    picoscope-beta 7.0.109.12622
    * Adapt url of picoscope-beta cask to the new format
    * Update picoscope-beta from 7.0.107.12341 to 7.0.109.12622
    * Adapt package file name to the new format
    
    Closes #14787.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,18 +1,18 @@
 cask "picoscope-beta" do
-  version "7.0.107.12341"
-  sha256 "04eadcec65631cc71cb3a8767a7516140ffffd64f7d0a6691462924c42ed1561"
+  version "7.0.109.12622"
+  sha256 "f6334d3c6da2e65220248f3dfe854832acda9aada7be54e6af4a459d69686db9"
 
-  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
+  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
 
   livecheck do
     url "https://oem.picotech.com/p#{version.major}beta/download/"
-    regex(%r{href=.*?/PicoScope_(?:\d+)_TnM_Early_Access[._-]v?(\d+(?:\.\d+)+).pkg}i)
+    regex(%r{href=.*?/PicoScope_?(\d+(?:\.\d+)+).pkg}i)
   end
 
-  pkg "PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
+  pkg "PicoScope_#{version}.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}tnmbeta"
 end
