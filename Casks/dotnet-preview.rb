344fa2d24b074d43500c9cac2d9350c6aecd2d9d
Patrick Linnane <patrick@linnane.io>
2022-10-12 00:04:09 -0700

    Update dotnet-preview from 7.0.0-rc.1.22426.10 to 7.0.0-rc.2.22472.3 (#14812)

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -2,12 +2,12 @@ cask "dotnet-preview" do
   arch arm: "arm64", intel: "x64"
 
   on_intel do
-    version "7.0.0-rc.1.22426.10,7138e084-facc-423c-9291-d42905daf0fe,4fd55041dab67a68fea19cfd3fc08c41"
-    sha256 "7eb7a2aeef8ee693ca821d555ed36987e7a3397a562f87264148fcb739a7ddd0"
+    version "7.0.0-rc.2.22472.3,c90e3ba9-00e3-451a-b785-ac1b0abe3e06,9238bb73aa338eda296d9edd909f341e"
+    sha256 "6ca6feb2fc306988875f12649d48016b54c4210c8d0bf955063d560ebee07bb7"
   end
   on_arm do
-    version "7.0.0-rc.1.22426.10,427e5eda-5b38-4705-9c3e-231a1a937b4f,28be328d327f708fd7c53728696d44bf"
-    sha256 "a54cf8822ad5889b5ed874064a40a164b48d2dc3bc2f5eb09cfe5398a8c7b9fd"
+    version "7.0.0-rc.2.22472.3,441979d9-1ec7-4065-9aa3-70ed3cb02c78,5e8bebf1c0c48ae8a55265c9d645c254"
+    sha256 "73b99f2bf166c27924bbd5a8c0dde37584b599369abce3ea7423f195b4096454"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-runtime-#{version.csv[0]}-osx-#{arch}.pkg"
