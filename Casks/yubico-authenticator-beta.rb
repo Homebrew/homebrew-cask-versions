5fe7ad41d5c19cefb422043ae50c5764b519c5c8
Miccal Matthews <miccal.matthews@gmail.com>
2022-10-14 23:15:49 +0800

    Update yubico-authenticator-beta.rb

--- Casks/yubico-authenticator-beta.rb
+++ Casks/yubico-authenticator-beta.rb
@@ -12,7 +12,6 @@ cask "yubico-authenticator-beta" do
     regex(/yubioath[._-]desktop[._-]v?(\d+(?:\.\d+)*-beta(?:\.\d+)*)[._-]mac\.dmg/i)
   end
 
-  conflicts_with cask: "yubico-authenticator"
-
-  app "Yubico Authenticator.app"
+  # Renamed to avoid conflict with yubico-authenticator.
+  app "Yubico Authenticator.app", target: "Yubico Authenticator Beta.app"
 end
