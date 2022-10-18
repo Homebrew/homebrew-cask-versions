3ad2b3c2ccef1d900540a00c5632adfd070a8676
Patrick Linnane <patrick@linnane.io>
2022-10-18 07:53:57 -0700

    Revert "update openzfs-dev (#14870)"
    This reverts commit c0482603897214979a159669683e976e140ae9de.

--- Casks/openzfs-dev.rb
+++ Casks/openzfs-dev.rb
@@ -1,14 +1,17 @@
 cask "openzfs-dev" do
   on_intel do
-    version "2.1.99,374"
-    sha256 "0708be51f448be5e85a763f240ebcb7ec8f980694c68a00beb0e852764292b52"
-    pkg "OpenZFSonOsX-#{version.csv.first}-Catalina-10.15.pkg"
+    on_catalina :or_newer do
+      version "2.1.99,374"
+      sha256 "0708be51f448be5e85a763f240ebcb7ec8f980694c68a00beb0e852764292b52"
+      pkg "OpenZFSonOsX-#{version.csv.first}-Catalina-10.15.pkg"
+    end
   end
   on_arm do
-    depends_on macos: ">= :monterey"
-    version "2.1.99,378"
-    sha256 "160fa584a59e00a1ac71f8807cd3822ed8a55d985ae25ea69e699c5a20de0e66"
-    pkg "OpenZFSonOsX-#{version.csv.first}-Monterey-12.pkg"
+    on_monterey :or_newer do
+      version "2.1.99,378"
+      sha256 "160fa584a59e00a1ac71f8807cd3822ed8a55d985ae25ea69e699c5a20de0e66"
+      pkg "OpenZFSonOsX-#{version.csv.first}-Monterey-12.pkg"
+    end
   end
 
   url "https://openzfsonosx.org/forum/download/file.php?id=#{version.csv.second}"
