22b40a52b138bb04b00d6db290729c9655b3ae4d
Patrick Linnane <patrick@linnane.io>
2022-10-17 20:32:38 -0700

    Update opera-crypto-developer from 88.0.4412.64 to 90.0.4480.128

--- Casks/opera-crypto-developer.rb
+++ Casks/opera-crypto-developer.rb
@@ -1,8 +1,8 @@
 cask "opera-crypto-developer" do
-  version "88.0.4412.64"
-  sha256 "b6d6d83148d7bc3f4da7be1a8f2c1237e38dfdc9240083eba87ab84d88b7caf7"
+  version "90.0.4480.128"
+  sha256 "f70e9b6a0e99799d4e1d28e9f4f0e66958e74d0c360a915fe25b8b9db4cb2341"
 
-  url "https://ftp.opera.com/pub/opera_crypto-developer/#{version}/mac/Opera_Crypto_Developer_#{version}_Setup.dmg"
+  url "https://ftp.opera.com/pub/opera_crypto-developer/#{version}/mac/Opera_Crypto_#{version}_Setup.dmg"
   name "Opera Crypto Browser Developer"
   desc "Web3 browser"
   homepage "https://www.opera.com/crypto/next"
