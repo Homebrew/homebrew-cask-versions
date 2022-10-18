87736b02356dcd9373e8c0c2a9c0964db4bc1e26
Patrick Linnane <patrick@linnane.io>
2022-10-18 09:15:38 -0700

    google-chrome-canary 109.0.5366.0
    Update google-chrome-canary from 109.0.5365.0 to 109.0.5366.0
    
    Closes #14875.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "109.0.5365.0"
+  version "109.0.5366.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
