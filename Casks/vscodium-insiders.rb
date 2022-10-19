6d1ed2be89876cd82c2c5f9e23c22dfcfff21854
Patrick Linnane <patrick@linnane.io>
2022-10-19 12:47:35 -0700

    Update vscodium-insiders from 1.73.0.22285-insider to 1.73.0.22292-insider

--- Casks/vscodium-insiders.rb
+++ Casks/vscodium-insiders.rb
@@ -1,9 +1,9 @@
 cask "vscodium-insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.73.0.22285-insider"
-  sha256 arm:   "fa9bbd5a8af978977f2581d20b6f4fad9601b41e0c3e2444d9ae7e56fe62ca21",
-         intel: "4e9c01ddd6056f805007318bac04eaebb1f05379bcd8fc2cf9c5ae44e9502c6b"
+  version "1.73.0.22292-insider"
+  sha256 arm:   "0b335de68cae490f8b6f5aa0c3b61f8bc2b75d147c5932f3041cba96245c9f80",
+         intel: "7a72a8ec02bdd0aa263faca18c21b403f23f065eea4923558491c962c33ca170"
 
   url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg",
       verified: "github.com/VSCodium/vscodium-insiders/"
