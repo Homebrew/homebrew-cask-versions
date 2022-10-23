0d1429d688fbd2308fa889099d373105b3c9a635
Patrick Linnane <patrick@linnane.io>
2022-10-23 15:31:06 -0700

    Update wine-devel from 7.18 to 7.19 (#14936)

--- Casks/wine-devel.rb
+++ Casks/wine-devel.rb
@@ -1,6 +1,6 @@
 cask "wine-devel" do
-  version "7.18"
-  sha256 "d4a7d39c25cc418c18c7250bf26f16a80714fd117394462c7db0371954028983"
+  version "7.19"
+  sha256 "496ccaad8b7be019ad83677a45869aad251911fefd4a748d92cfe671964cb271"
 
   # Current winehq packages are deprecated and these are packages from
   # the new maintainers that will eventually be pushed to Winehq.
