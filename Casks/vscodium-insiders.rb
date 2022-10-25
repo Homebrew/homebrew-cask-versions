77b1270b0b2864bcafa8e08df98eb6081826ec32
Patrick Linnane <patrick@linnane.io>
2022-10-25 15:39:54 -0700

    vscodium-insiders 1.73.0.22297-insider
    Update vscodium-insiders from 1.73.0.22295-insider to 1.73.0.22297-insider
    
    Closes #14961.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/vscodium-insiders.rb
+++ Casks/vscodium-insiders.rb
@@ -1,9 +1,9 @@
 cask "vscodium-insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.73.0.22295-insider"
-  sha256 arm:   "6200a583e3af6a9d8740333444c4a3bc5464ef5c4fdea3509a35281493ae0375",
-         intel: "4efa2b72c4d22e142cf6bbc7b4345c436aaf136cfff8773e2ab9d75895660535"
+  version "1.73.0.22297-insider"
+  sha256 arm:   "36c2f21acf38ec1bf836c3da2aa20ee26202f1b23348aa1396836b3afc2e9f8f",
+         intel: "62599946ecc40b1513707d5b0a75c2e048f65dd19938153ff9ac256be82f30a2"
 
   url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg",
       verified: "github.com/VSCodium/vscodium-insiders/"
