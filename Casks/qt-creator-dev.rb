bd544a2109a1e58267a5ab549c37938b81ba64df
Yurii Kolesnykov <root@yurikoles.com>
2022-10-26 21:53:58 +0300

    Update qt-creator-dev from 9.0.0-beta1 to 9.0.0-beta2 (#14963)

--- Casks/qt-creator-dev.rb
+++ Casks/qt-creator-dev.rb
@@ -1,6 +1,6 @@
 cask "qt-creator-dev" do
-  version "9.0.0-beta1"
-  sha256 "7a6dbd5224568038bf00a87337f233d4b4aaef5d865bd53c10a71c992e2bf7dc"
+  version "9.0.0-beta2"
+  sha256 "73b49c74bd2d360d9d574bea225d5053924697041ab2a1a7d566655d0cf5f852"
 
   url "https://download.qt.io/development_releases/qtcreator/#{version.major_minor}/#{version}/qt-creator-opensource-mac-x86_64-#{version}.dmg"
   name "Qt Creator Dev"
