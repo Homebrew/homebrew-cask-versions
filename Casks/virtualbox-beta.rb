ad32bdcb6c443cff01f10b5281da9afed477915a
Sam Ford <1584702+samford@users.noreply.github.com>
2022-10-27 22:27:30 -0400

    virtualbox-beta: update livecheck

--- Casks/virtualbox-beta.rb
+++ Casks/virtualbox-beta.rb
@@ -4,11 +4,15 @@ cask "virtualbox-beta" do
     sha256 "a97ad4e37f975ec3ec093a1dfc58f456cac2066f27ccc743a523a261235785b0"
     url "https://download.virtualbox.org/virtualbox/#{version.csv.first}/VirtualBox-#{version.csv.first}-#{version.csv.second}-OSX.dmg"
 
+    # `LATEST-BETA.TXT` contains version text (e.g., `1.2.3_BETA4\n`) that
+    # should correspond to a related directory where we can identify all the
+    # version parts from a filename.
     livecheck do
-      url :homepage
+      url "https://download.virtualbox.org/virtualbox/LATEST-BETA.TXT"
       regex(/href=.*?VirtualBox[._-]v?(\d+(?:\.\d+)+[._-][^._-]+?)[._-](\d+)[._-]OSX\.dmg/i)
       strategy :page_match do |page, regex|
-        page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
+        dir_page = Homebrew::Livecheck::Strategy.page_content("https://download.virtualbox.org/virtualbox/#{page.strip}/")
+        dir_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
       end
     end
   end
@@ -17,13 +21,12 @@ cask "virtualbox-beta" do
     sha256 "c5b85d3168faabc269035d82d2510b92937d4e389ef93139b4333960cd683582"
     url "https://download.virtualbox.org/virtualbox/#{version.csv.first}/VirtualBox-#{version.csv.first}_#{version.csv.second}-#{version.csv.third}-macOSArm64.dmg"
 
-    # TODO: Add a `livecheck` block if/when ARM64 dmg files are linked on
-    # the "Testbuilds" page (https://www.virtualbox.org/wiki/Testbuilds) or
-    # stored in a predictable directory on download.virtualbox.org. ARM64 files
-    # are currently shoehorned into stable version directories (instead of
-    # being in an unstable directory like 7.0.0_BETA3). Unless this remains
-    # true going forward, we would end up making a number of wasted requests in
-    # an attempt to identify a new unstable ARM64 version.
+    # TODO: Add a `livecheck` block if/when ARM64 dmg files are stored in a
+    # predictable directory on download.virtualbox.org. ARM64 files are
+    # currently shoehorned into stable version directories instead of the
+    # usual unstable version directories (e.g., 7.0.0_BETA3). At the moment,
+    # we would have to check both the highest unstable and stable version
+    # directories for an ARM64 file to find the newest version from each.
   end
 
   name "Oracle VirtualBox"
