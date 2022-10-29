abde0ebdb0694b02a1ad60cb79e01cf3db1df171
Patrick Linnane <patrick@linnane.io>
2022-10-28 22:23:57 -0700

    affinity-designer-beta 1.10.5.1
    Bump version, add livecheck
    
    Closes #15003.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/affinity-designer-beta.rb
+++ Casks/affinity-designer-beta.rb
@@ -1,13 +1,17 @@
 cask "affinity-designer-beta" do
-  version "1.9.4.2"
+  version "1.10.5.1"
   sha256 :no_check
 
   url "https://affinity-beta.s3.amazonaws.com/download/Affinity%20Designer%20Beta.dmg",
       verified: "affinity-beta.s3.amazonaws.com/"
-  appcast "https://www.macupdater.net/cgi-bin/extract_text/extract_text_split_easy.cgi?encoding=utf8&url=https://forum.affinity.serif.com/index.php?/topic/83296-affinity-range-mac-beta-release-notifications/&splitter_1=Affinity%20Designer&index_1=1&splitter_2=Affinity%20Photo&index_2=0"
   name "Serif Affinity Designer"
   desc "Photo editing and graphic design software"
   homepage "https://affinity.serif.com/en-us/"
+
+  livecheck do
+    url "https://forum.affinity.serif.com/index.php?/topic/83296-affinity-range-mac-beta-release-notifications/"
+    regex(/Designer.*?\|\s\s(\d+(?:\.\d+)+)/i)
+  end
 
   app "Affinity Designer Beta.app"
 end
