4999b3f39b1ea98fefd19e31252b8ffe82bfced9
Patrick Linnane <patrick@linnane.io>
2022-10-29 10:59:38 -0700

    azure-data-studio-insider: Add version, livecheck (#15013)
    Add version, livecheck

--- Casks/azure-data-studio-insiders.rb
+++ Casks/azure-data-studio-insiders.rb
@@ -1,12 +1,17 @@
 cask "azure-data-studio-insiders" do
-  version :latest
-  sha256 :no_check
+  version "1.40.0"
+  sha256 "d4c46777507066dcc6b940446dc3c57e1935629cd74f0e06d005abc298e93b49"
 
-  url "https://azuredatastudio-update.azurewebsites.net/latest/darwin/insider",
+  url "https://azuredatastudio-update.azurewebsites.net/#{version}/darwin/insider",
       verified: "azuredatastudio-update.azurewebsites.net"
   name "Azure Data Studio - Insiders"
   desc "Data management tool that enables working with SQL Server"
   homepage "https://docs.microsoft.com/en-us/sql/azure-data-studio/"
+
+  livecheck do
+    url "https://azuredatastudio-update.azurewebsites.net/api/update/darwin/insider/VERSION"
+    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)+)"/i)
+  end
 
   app "Azure Data Studio - Insiders.app"
   binary "#{appdir}/Azure Data Studio - Insiders.app/Contents/Resources/app/bin/code",
