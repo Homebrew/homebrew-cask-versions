920ab233d28c45edde7e836f52517808d87aa723
Patrick Linnane <patrick@linnane.io>
2022-10-29 14:04:23 -0700

    emacs-nightly 2022-10-29_00-09-23,ce527464440ca070e752ca18a68ebf2d369a14df
    Add version, livecheck
    
    Closes #15028.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,13 +1,22 @@
 cask "emacs-nightly" do
-  version :latest
-  sha256 :no_check
+  version "2022-10-29_00-09-23,ce527464440ca070e752ca18a68ebf2d369a14df"
+  sha256 "da2c3d2314179ad590a02de8a481f0f2348f1139e9eeb0a7301a2a217f660016"
 
-  url "https://emacsformacosx.com/atom/daily" do |page|
-    page[/href="([^"]+.dmg)"/, 1]
-  end
+  url "https://emacsformacosx.com/emacs-builds/Emacs-#{version.csv.first}-#{version.csv.second}-universal.dmg"
   name "Emacs"
   desc "GNU Emacs text editor"
   homepage "https://emacsformacosx.com/"
+
+  livecheck do
+    url "https://emacsformacosx.com/atom/daily"
+    regex(/href=.*?Emacs[._-](\d+-\d+-\d+_\d+-\d+-\d+)[._-]([a-f0-9]+)[._-]universal\.dmg/i)
+    strategy :page_match do |page|
+      match = page.match(regex)
+      next if match.blank?
+
+      "#{match[1]},#{match[2]}"
+    end
+  end
 
   conflicts_with cask:    [
                    "emacs",
