bc8b593dffb6438f4c3b1ade0261c42d992d7ac4
Patrick Linnane <patrick@linnane.io>
2022-10-29 12:35:30 -0700

    deadbeef-nightly: rebuild
    Fix URL
    
    Closes #15024.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/deadbeef-nightly.rb
+++ Casks/deadbeef-nightly.rb
@@ -2,7 +2,7 @@ cask "deadbeef-nightly" do
   version :latest
   sha256 :no_check
 
-  url "https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-macos-universal.zip",
+  url "https://downloads.sourceforge.net/deadbeef/travis/macOS/master/deadbeef-devel-macos-universal.zip",
       verified: "downloads.sourceforge.net/deadbeef/"
   name "DeaDBeeF"
   desc "Modular audio player"
