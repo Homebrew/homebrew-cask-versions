c82f429f5de3ba79774f752c22c4670b85f1d8a7
Patrick Linnane <patrick@linnane.io>
2022-10-29 13:26:23 -0700

    Add version, livecheck

--- Casks/defold-alpha.rb
+++ Casks/defold-alpha.rb
@@ -1,15 +1,22 @@
 cask "defold-alpha" do
-  version :latest
-  sha256 :no_check
+  version "1.3.8,73da9c5dd6ff4ab2d3f175107d8d343d8decae51"
+  sha256 "45fd7ce8441aed807d570e0c37fa5cf633d0aacb94493ece2d726cdb68465f6e"
 
-  url "https://d.defold.com/alpha/info.json" do |json_content|
-    require "json"
-    version_sha = JSON.parse(json_content)["sha1"]
-    "https://d.defold.com/archive/alpha/#{version_sha}/alpha/editor2/Defold-x86_64-darwin.dmg"
-  end
+  url "https://d.defold.com/archive/alpha/#{version.csv.second}/alpha/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
   desc "Game engine for development of desktop, mobile and web games"
   homepage "https://defold.com/"
+
+  livecheck do
+    url "https://d.defold.com/alpha/info.json"
+    strategy :page_match do |page|
+      version = JSON.parse(page)["version"]
+      sha1 = JSON.parse(page)["sha1"]
+      next if version.blank? || sha1.blank?
+
+      "#{version},#{sha1}"
+    end
+  end
 
   conflicts_with cask: [
     "defold",
