40a7ca1859cf1f4b97c6c7d543fa1fd676754ca9
Patrick Linnane <patrick@linnane.io>
2022-10-29 12:30:28 -0700

    Split by architecture

--- Casks/db-browser-for-sqlite-nightly.rb
+++ Casks/db-browser-for-sqlite-nightly.rb
@@ -1,8 +1,10 @@
 cask "db-browser-for-sqlite-nightly" do
+  arch arm: "arm64", intel: "intel"
+
   version :latest
   sha256 :no_check
 
-  url "https://nightlies.sqlitebrowser.org/latest/DB.Browser.for.SQLite.dmg"
+  url "https://nightlies.sqlitebrowser.org/latest/DB.Browser.for.SQLite-#{arch}.dmg"
   name "DB Browser for SQLite"
   desc "Database browser for SQLite"
   homepage "https://sqlitebrowser.org/"
