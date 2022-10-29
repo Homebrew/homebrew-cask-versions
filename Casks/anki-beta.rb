50a637adbde404e87b74c935a3a33709aa0f4eee
Patrick Linnane <patrick@linnane.io>
2022-10-29 08:52:12 -0700

    anki-beta 2.1.55+beta3_e5c4ccf0
    Update anki-beta from 2.1.54+rc3_a8e34ce4 to 2.1.55+beta3_e5c4ccf0
    
    Closes #15009.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -1,9 +1,9 @@
 cask "anki-beta" do
   arch arm: "apple", intel: "intel"
 
-  version "2.1.54+rc3_a8e34ce4"
-  sha256 arm:   "1094c787826295aace1a89c624516aa48c9d533375db8cba82f694d2e0424fe9",
-         intel: "05c02714d2f71ee6cc7b1332f7c90e10e88df4a3e23f07336817cb15bd0c71b5"
+  version "2.1.55+beta3_e5c4ccf0"
+  sha256 arm:   "696d84cf5bfce7285dd805d11140e732f87023cb6c0b80f92c74fdbcb5c4f813",
+         intel: "cc848d8ba2069aabbb81c038f18d153c541dbae34bb7fbb7b86ad2473e47e01d"
 
   url "https://apps.ankiweb.net/downloads/beta/anki-#{version}-mac-#{arch}-qt6.dmg"
   name "Anki Beta"
@@ -11,10 +11,8 @@ cask "anki-beta" do
   homepage "https://apps.ankiweb.net/"
 
   livecheck do
-    url "https://apps.ankiweb.net/downloads/beta/?C=N&O=D"
-    # It looks for title instead of href because "+" character in version number
-    # is escaped to %2B in href:
-    regex(/title=["']?anki[._-]?(\d+\.\d+.\d+\+rc\d+_[a-f0-9]+)-mac(?:-#{arch})?(?:-qt\d+)?\.dmg["'\s>]/i)
+    url "https://apps.ankiweb.net/downloads/beta/"
+    regex(/title=["']?anki[._-]?(\d+\.\d+.\d+\+beta\d+_[a-f0-9]+)-mac(?:-#{arch})?(?:-qt\d+)?\.dmg["'\s>]/i)
   end
 
   conflicts_with cask: "anki"
