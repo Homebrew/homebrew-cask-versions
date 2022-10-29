1b2187dfd50fb7325f9a3e75c0cb325755b9c9ff
Patrick Linnane <patrick@linnane.io>
2022-10-29 11:01:08 -0700

    brave-browser-nightly 1.47.27.0,147.27
    Add version, livecheck
    
    Closes #15014.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/brave-browser-nightly.rb
+++ Casks/brave-browser-nightly.rb
@@ -1,13 +1,23 @@
 cask "brave-browser-nightly" do
-  version :latest
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
+  folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
 
-  url "https://updates.bravesoftware.com/sparkle/Brave-Browser/nightly/appcast.xml" do |page|
-    [page[/enclosure url="([^"]+.dmg)"/, 1], { verified: "bravesoftware.com" }]
-  end
+  version "1.47.27.0,147.27"
+  sha256 arm:   "3a59a9e750b8ab32d516794f4083e4a61a4b9a817a5594acf87a5fae3cc98449",
+         intel: "dbeb28b1df5c78fdee7b5270960c17162beea1c0e39702471aff3b79e680b452"
+
+  url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Nightly-#{arch}.dmg",
+      verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
   name "Brave Nightly"
   desc "Web browser focusing on privacy"
   homepage "https://brave.com/download-nightly/"
+
+  livecheck do
+    url "https://updates.bravesoftware.com/sparkle/Brave-Browser/#{folder}/appcast.xml"
+    strategy :sparkle
+  end
+
+  auto_updates true
 
   app "Brave Browser Nightly.app"
 
