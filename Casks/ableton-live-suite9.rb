ead9d4cff1ab2f2bf9e460604f73d21c3f01f342
Patrick Linnane <patrick@linnane.io>
2022-10-28 21:47:59 -0700

    Add desc, discontinue

--- Casks/ableton-live-suite9.rb
+++ Casks/ableton-live-suite9.rb
@@ -3,12 +3,18 @@ cask "ableton-live-suite9" do
   sha256 "582547c75197e0f88cb4b88d579ce982f03a32cd2b48739fe3c77a516b49191d"
 
   url "https://cdn-downloads.ableton.com/channels/#{version}/ableton_live_suite_#{version}_64.dmg"
-  appcast "https://www.ableton.com/en/release-notes/live-#{version.major}/"
   name "Ableton Live 9 Suite"
+  desc "Sound and music editor"
   homepage "https://www.ableton.com/en/live/"
 
   app "Ableton Live #{version.major} Suite.app"
 
-  zap trash: "~/Library/*/*[Aa]bleton*",
-      rmdir: "~/Music/Ableton/Factory Packs"
+  zap trash: [
+    "~/Library/*/*[Aa]bleton*",
+    "~/Music/Ableton/Factory Packs",
+  ]
+
+  caveats do
+    discontinued
+  end
 end
