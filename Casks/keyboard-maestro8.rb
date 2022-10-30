26fd0f957d17503adc81c894b45c1b36286337b9
Patrick Linnane <patrick@linnane.io>
2022-10-29 21:10:30 -0700

    keyboard-maestro8: rebuild
    * Discontinue
    * Add desc
    
    Closes #15041.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/keyboard-maestro8.rb
+++ Casks/keyboard-maestro8.rb
@@ -5,6 +5,7 @@ cask "keyboard-maestro8" do
   url "https://files.stairways.com/keyboardmaestro-#{version.no_dots}.zip",
       verified: "files.stairways.com/"
   name "Keyboard Maestro"
+  desc "Automation software"
   homepage "https://www.keyboardmaestro.com/main/"
 
   app "Keyboard Maestro.app"
@@ -20,4 +21,8 @@ cask "keyboard-maestro8" do
     "~/Library/Preferences/com.stairways.keyboardmaestro.plist",
     "~/Library/Saved Application State/com.stairways.keyboardmaestro.editor.savedState",
   ]
+
+  caveats do
+    discontinued
+  end
 end
