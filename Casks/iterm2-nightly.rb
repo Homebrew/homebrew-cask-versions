1cd673a505546fa87b8f43327b1fdbf9b67e60e8
Patrick Linnane <patrick@linnane.io>
2022-10-29 20:10:04 -0700

    Add version, livecheck

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,11 +1,17 @@
 cask "iterm2-nightly" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version :latest
-  sha256 :no_check
+  version "3_5_20221026"
+  sha256 "61ab824a2a132d6daf2471b8b424f8833c6fe8b7fc5f828637e576f1023e3d43"
 
-  url "https://www.iterm2.com/nightly/latest"
+  url "https://www.iterm2.com/nightly/iTerm2-#{version}-nightly.zip"
   name "iTerm2"
+  desc "Terminal emulator as alternative to Apple's Terminal app"
   homepage "https://www.iterm2.com/"
+
+  livecheck do
+    url "https://www.iterm2.com/nightly/latest"
+    strategy :header_match
+  end
 
   conflicts_with cask: [
     "iterm2",
