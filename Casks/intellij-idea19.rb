1729c59c794d37343cf7a76b20970b58b364dcc2
Patrick Linnane <patrick@linnane.io>
2022-10-29 19:57:29 -0700

    intellij-idea19: rebuild
    * Discontinue
    * Adjust uninstall
    
    Closes #15034.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/intellij-idea19.rb
+++ Casks/intellij-idea19.rb
@@ -3,7 +3,6 @@ cask "intellij-idea19" do
   sha256 "196b739562012f2bbab22362d6a3094a1806acf1e1d0279b536f5f0327796a0d"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version}.dmg"
-  appcast "https://data.services.jetbrains.com/products/releases?code=IIU&latest=false&type=release"
   name "IntelliJ IDEA Ultimate"
   desc "IDE for JVM languages"
   homepage "https://www.jetbrains.com/idea/"
@@ -14,7 +13,8 @@ cask "intellij-idea19" do
 
   uninstall_postflight do
     ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
-      if File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+      if File.readable?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
         File.delete(path)
       end
     end
@@ -28,4 +28,8 @@ cask "intellij-idea19" do
     "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
     "~/Library/Saved Application State/com.jetbrains.intellij.savedState",
   ]
+
+  caveats do
+    discontinued
+  end
 end
