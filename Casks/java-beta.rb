8b5f4ff365849ea95b22c53df47854dcba22173c
Patrick Linnane <patrick@linnane.io>
2022-10-29 20:37:21 -0700

    java-beta 20,21
    Update java-beta from 19 to 20
    
    Closes #15039.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,20 +1,20 @@
 cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "19,877d6127e982470ba2a7faa31cc93d04,36"
-  sha256 arm:   "6691c199fdc6d71826f37d89d2e1a2089ba8bdd98b7001c1d4e8d5d01d6b022b",
-         intel: "bfd33f5b2590fd552ae2d9231340c6b4704a872f927dce1c52860b78c49a5a11"
+  version "20,21"
+  sha256 arm:   "6a196c59a14464fbb257e87aa5d4c46e033d70f572796228eb6b0a2e55c7a6d1",
+         intel: "23a54a1b842eb0ee6aba65f00a65261576da74d63b6ed3de17fe324434da9079"
 
-  url "https://download.java.net/java/GA/jdk#{version.major}/#{version.csv.second}/#{version.csv.third}/GPL/openjdk-#{version.csv.first}_macos-#{arch}_bin.tar.gz"
+  url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
   desc "Early access development kit for the Java programming language"
   homepage "https://jdk.java.net/"
 
   livecheck do
     url "https://jdk.java.net/#{version.major}/"
-    regex(%r{([^/]+)/(\d+)/GPL/openjdk-(\d+)_macos-#{arch}_bin\.t}i)
+    regex(%r{href=.*?/GPL/openjdk-(\d+)-ea\+(\d+)_macos-#{arch}_bin\.t}i)
     strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[2]},#{match[0]},#{match[1]}" }
+      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
