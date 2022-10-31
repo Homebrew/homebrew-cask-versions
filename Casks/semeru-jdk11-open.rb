11a8a61543c1c9104933a7fd89d4fa1f719f292b
zuisong <19145952+zuisong@users.noreply.github.com>
2022-10-31 11:58:38 +0800

    add arm pkg for semeru-jdk (#15046)
    Co-authored-by: zuisong <com.me@foxmail.com>

--- Casks/semeru-jdk11-open.rb
+++ Casks/semeru-jdk11-open.rb
@@ -1,8 +1,11 @@
 cask "semeru-jdk11-open" do
-  version "11.0.16.1+1,openj9-0.33.1"
-  sha256 "49de96851b30852a50928e01d2929cfc24805b8b8a9a8fdc6468f6302d4461a0"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
+  version "11.0.16.1+1,openj9-0.33.1"
+  sha256 arm:   "93f5b4d57e0baad0bf04a46f4b05392dd6f51e3923b51c7268b402a8c5b48da6",
+         intel: "49de96851b30852a50928e01d2929cfc24805b8b8a9a8fdc6468f6302d4461a0"
+
+  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
   name "IBM Semeru Runtime (JDK 11) Open Edition"
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
@@ -16,7 +19,7 @@ cask "semeru-jdk11-open" do
     end
   end
 
-  pkg "ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
+  pkg "ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
 
   uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
 end
--- Casks/semeru-jdk17-open.rb
+++ Casks/semeru-jdk17-open.rb
@@ -1,8 +1,11 @@
 cask "semeru-jdk17-open" do
-  version "17.0.4.1+1,openj9-0.33.1"
-  sha256 "e0bdb96cbe2693bd08cd5d71d42f99795bed6fade684fd1f4018abd01accfa16"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
+  version "17.0.4.1+1,openj9-0.33.1"
+  sha256 arm:   "dcb7adc4a1beeba4a4783e76801d4a6bb19870c76849d6ae1d2021a677f0206f",
+         intel: "e0bdb96cbe2693bd08cd5d71d42f99795bed6fade684fd1f4018abd01accfa16"
+
+  url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
   name "IBM Semeru Runtime (JDK 17) Open Edition"
   desc "Production-ready JDK with the OpenJDK class libraries and the Eclipse OpenJ9 JVM"
@@ -18,7 +21,7 @@ cask "semeru-jdk17-open" do
     end
   end
 
-  pkg "ibm-semeru-open-jdk_x64_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
+  pkg "ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg"
 
   uninstall pkgutil: "net.ibm-semeru-open.#{version.major}.jdk"
 end
