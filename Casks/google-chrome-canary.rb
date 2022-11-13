f64098f8888971efc9564954c8088ed8867600f2
Patrick Linnane <patrick@linnane.io>
2022-11-12 20:11:48 -0800

    google-chrome-canary 110.0.5415.0
    Update google-chrome-canary from 109.0.5409.0 to 110.0.5415.0
    
    Closes #15165.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "109.0.5409.0"
+  version "110.0.5415.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
