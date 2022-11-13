4bef412ca98438b6a464081813c47e07811367b8
Patrick Linnane <patrick@linnane.io>
2022-11-12 20:17:07 -0800

    skype-preview 8.91.76.304
    Update skype-preview from 8.90.76.401 to 8.91.76.304
    
    Closes #15171.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask "skype-preview" do
-  version "8.90.76.401"
-  sha256 "6ed4e073daa09284934f4224c75ed1dafa9bb8a63ff2e92e86edd6a767684a24"
+  version "8.91.76.304"
+  sha256 "0961c3071599ff5e49936dcb3d48dc1cd5ff3dc1236a83765de8918ccb730250"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
