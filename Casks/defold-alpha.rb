b7181ec26c35d91081a7260878d17d89306d66e5
Patrick Linnane <patrick@linnane.io>
2022-11-13 21:13:25 -0800

    Update defold-alpha from 1.4.0,01e7ac3e737f19e28388769c0d21dfc144fc738f to 1.4.0,2ff781be002e605c22599eb752f4f107881c55b7

--- Casks/defold-alpha.rb
+++ Casks/defold-alpha.rb
@@ -1,6 +1,6 @@
 cask "defold-alpha" do
-  version "1.4.0,01e7ac3e737f19e28388769c0d21dfc144fc738f"
-  sha256 "d817da764cb5bebbb5050c6335d9ee057f93e3cee46def1829d82e10ad0786a8"
+  version "1.4.0,2ff781be002e605c22599eb752f4f107881c55b7"
+  sha256 "886f55708096f48bcf7b29631db36fcd75c94899a185dea36b9e56eee75d3ca2"
 
   url "https://d.defold.com/archive/alpha/#{version.csv.second}/alpha/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
