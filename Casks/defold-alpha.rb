96099e1c004e749e6bb739ddb22de4e1f1ee2fac
Patrick Linnane <patrick@linnane.io>
2022-11-14 11:10:27 -0800

    defold-alpha 1.4.1,85c661125aa9969adaf5ade10fad467bf8954daf
    Update defold-alpha from 1.4.0 to 1.4.1
    
    Closes #15178.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/defold-alpha.rb
+++ Casks/defold-alpha.rb
@@ -1,6 +1,6 @@
 cask "defold-alpha" do
-  version "1.4.0,2ff781be002e605c22599eb752f4f107881c55b7"
-  sha256 "886f55708096f48bcf7b29631db36fcd75c94899a185dea36b9e56eee75d3ca2"
+  version "1.4.1,85c661125aa9969adaf5ade10fad467bf8954daf"
+  sha256 "c5f00b5c1ccdc60197f74120a7086d6f3ae09d30d4f5032e6ee181f23718e3a7"
 
   url "https://d.defold.com/archive/alpha/#{version.csv.second}/alpha/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
