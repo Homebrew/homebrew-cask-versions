0e62a82ea947d0bb1ed095e731da47d574d3dd3a
Mike L <cl.jeremy@qq.com>
2022-11-16 00:28:14 +0100

    microsoft-openjdk11: add arm64 build (#15187)

--- Casks/microsoft-openjdk11.rb
+++ Casks/microsoft-openjdk11.rb
@@ -1,8 +1,11 @@
 cask "microsoft-openjdk11" do
-  version "11.0.17"
-  sha256 "0644c23cd0d665ff90d988c839be39d8a467305839c019aa32c837fdbb47cef5"
+  arch arm: "aarch64", intel: "x64"
 
-  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-x64.pkg",
+  version "11.0.17"
+  sha256 arm:   "4a12ba32832d19ca985005d1910b6f9aef15e95fa5d2096a76a0cf4ac4e5fb2b",
+         intel: "0644c23cd0d665ff90d988c839be39d8a467305839c019aa32c837fdbb47cef5"
+
+  url "https://aka.ms/download-jdk/microsoft-jdk-#{version}-macOS-#{arch}.pkg",
       verified: "aka.ms/download-jdk/"
   name "Microsoft Build of OpenJDK"
   desc "OpenJDK distribution from Microsoft"
@@ -10,10 +13,10 @@ cask "microsoft-openjdk11" do
 
   livecheck do
     url "https://docs.microsoft.com/java/openjdk/download"
-    regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(11(?:\.\d+)+)[._-]macOS[._-]x64\.pkg}i)
+    regex(%r{href=.*?/microsoft[._-]jdk[._-]v?(11(?:\.\d+)+)[._-]macOS[._-]#{arch}\.pkg}i)
   end
 
-  pkg "microsoft-jdk-#{version}-macOS-x64.pkg"
+  pkg "microsoft-jdk-#{version}-macOS-#{arch}.pkg"
 
   uninstall pkgutil: "com.microsoft.#{version.major}.jdk"
 end
