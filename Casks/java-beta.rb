68ea7c9b852c38c8735ab06f3d3f76b91d18c125
Patrick Linnane <patrick@linnane.io>
2022-11-17 15:12:37 -0800

    java-beta 20,24
    Update java-beta from 20,23 to 20,24
    
    Closes #15223.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,9 +1,9 @@
 cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "20,23"
-  sha256 arm:   "a77c8d0565cc644de3976b60441f7a18e2bcdef0d3ae303d264d1efcca0622a7",
-         intel: "dfe68773c9ca35ad5fb00ce88e30bfef6c2eefbde1db5a35088787b66354e8b3"
+  version "20,24"
+  sha256 arm:   "2122d0c94f748b870f3479d23e67ff636310729f177dc854b18e29494e3d22f2",
+         intel: "997acdedf50e55529148fc390e1c3a9d2fea1bd0255ed1319cfe1f3b5302aabe"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
