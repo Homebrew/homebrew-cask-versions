687064b817f5185d9c0067e331bb0fd0eb749d61
Sean Molenaar <smillerdev@me.com>
2022-11-18 14:32:55 +0100

    Update yubico-authenticator-beta

--- Casks/yubico-authenticator-beta.rb
+++ Casks/yubico-authenticator-beta.rb
@@ -2,7 +2,7 @@ cask "yubico-authenticator-beta" do
   version "6.0.0-beta.1"
   sha256 "d7fb7862453ccea3e2ae66b9c7e10b0eec4e767276514bd0c9a56110fe99a4b2"
 
-  url "https://developers.yubico.com/yubioath-desktop/Beta/yubioath-desktop-#{version}-mac.dmg"
+  url "https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-mac.dmg"
   name "Yubico Authenticator Beta"
   desc "Application for generating TOTP and HOTP codes"
   homepage "https://www.yubico.com/products/yubico-authenticator/yubico-authenticator-desktop-beta/"
