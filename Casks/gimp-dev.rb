d597fb8deaed2a3ff143d9f2462f38e88affa726
Miccal Matthews <miccal.matthews@gmail.com>
2022-11-19 19:40:38 +0800

    Update gimp-dev.rb

--- Casks/gimp-dev.rb
+++ Casks/gimp-dev.rb
@@ -1,10 +1,11 @@
-# frozen_string_literal: true
-
 cask "gimp-dev" do
-  version "2.99.14"
-  sha256 "cc3cb7e63aaa16bfb51eaa553d2466fa2935e7282378b50c9e9156d4f2dab3f8"
+  arch arm: "arm64", intel: "x86_64"
 
-  url "https://download.gimp.org/pub/gimp/v#{version.major_minor}/osx/gimp-#{version}-x86_64.dmg"
+  version "2.99.14"
+  sha256 arm:   "2c23f9d1706514f4017ed2f90531931bfeedaacbef84e29e59c4d490e196c796",
+         intel: "cc3cb7e63aaa16bfb51eaa553d2466fa2935e7282378b50c9e9156d4f2dab3f8"
+
+  url "https://download.gimp.org/pub/gimp/v#{version.major_minor}/osx/gimp-#{version}-#{arch}.dmg"
   name "GIMP development version"
   desc "Free and open-source image editor"
   homepage "https://www.gimp.org/"
@@ -12,14 +13,14 @@ cask "gimp-dev" do
   livecheck do
     url "https://www.gimp.org/downloads/devel/"
     strategy :page_match
-    regex(%r{href=.*?/gimp-(\d+(?:\.\d+)+)-x86_64\.dmg}i)
+    regex(%r{href=.*?/gimp[._-]v?(\d+(?:\.\d+)+)[._-]#{arch}\.dmg}i)
   end
 
-  app "GIMP-#{version.major_minor}.app"
-  binary "#{appdir}/GIMP-#{version.major_minor}.app/Contents/MacOS/gimp"
+  app "GIMP.app"
+  binary "#{appdir}/GIMP.app/Contents/MacOS/gimp"
 
   postflight do
-    set_permissions "#{appdir}/GIMP-#{version.major_minor}.app/Contents/MacOS/gimp", "a+rx"
+    set_permissions "#{appdir}/GIMP.app/Contents/MacOS/gimp", "a+rx"
   end
 
   zap trash: [
