ebf874433ce9a6d1ea3654a72f80134872a3b10b
Patrick Linnane <patrick@linnane.io>
2022-11-28 21:10:54 -0800

    Update java-beta from 20,24 to 20,25

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,9 +1,9 @@
 cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "20,24"
-  sha256 arm:   "2122d0c94f748b870f3479d23e67ff636310729f177dc854b18e29494e3d22f2",
-         intel: "997acdedf50e55529148fc390e1c3a9d2fea1bd0255ed1319cfe1f3b5302aabe"
+  version "20,25"
+  sha256 arm:   "36f1f7a523d970f98b5fdd48b08c10252b0dcf1272053a7fec1dd6eb1f0e44a9",
+         intel: "99b8f6ab246b6b8dfdd7b4802bf8dbd2934d1d27dc9c7aa256336d98ff4b97d0"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
