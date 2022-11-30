b39cd46813b562ef6403074abac29d95202a98fc
Patrick Linnane <patrick@linnane.io>
2022-11-29 18:40:22 -0800

    Update microsoft-edge-dev from 109.0.1495.2 to 109.0.1518.5

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-dev" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099619", intel: "2069340"
 
-  version "109.0.1495.2"
-  sha256 arm:   "994ee37edb0d78e3f3a31a456a1b8ad5f3bddb6f28a386410b1a0e540bacbb08",
-         intel: "e62aa01a0b24708397bf76b3337f4be07fac298d162de22fbf61777eeb412251"
+  version "109.0.1518.5"
+  sha256 arm:   "fde646a2ca0c36c41d4bdd8bb9e295de96048877bf06d2abc3a80c6890c20299",
+         intel: "a09e3041d1b12ebe8eeb1216ec00d852ec940ad6ebaa585be15b77efded449cc"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
