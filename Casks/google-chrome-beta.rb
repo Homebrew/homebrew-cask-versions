c4efa26df5a4d7853da40502ebc951681945ad99
Patrick Linnane <patrick@linnane.io>
2022-11-30 14:47:46 -0800

    Update google-chrome-beta from 108.0.5359.62 to 108.0.5359.71 (#15294)

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-beta" do
-  version "108.0.5359.62"
+  version "108.0.5359.71"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg"
