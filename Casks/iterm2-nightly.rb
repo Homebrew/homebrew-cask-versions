f1765fe0b8d5d95119da642d98d50ac789cc7540
Patrick Linnane <patrick@linnane.io>
2022-11-30 14:47:58 -0800

    Update iterm2-nightly from 3_5_20221128 to 3_5_20221129 (#15297)

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,7 +1,7 @@
 cask "iterm2-nightly" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3_5_20221128"
-  sha256 "9cf848628a61f21123b859bff69d13ad9ac91144cd41e69c751a830b188ae399"
+  version "3_5_20221129"
+  sha256 "b83999fae769b91ba5ccea3a1a2da6e34f23018d816be96bd1bf28b7a6f83fdb"
 
   url "https://www.iterm2.com/nightly/iTerm2-#{version}-nightly.zip"
   name "iTerm2"
