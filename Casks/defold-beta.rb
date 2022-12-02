b6c3961e1d2706543364a098669be8cf61fcb0b2
Patrick Linnane <patrick@linnane.io>
2022-12-02 12:58:26 -0800

    Update defold-beta from 1.4.1,ba61b10664fb9b96dfae367ba088960601bd8560 to 1.4.1,8f96e450ddfb006a99aa134fdd373cace3760571

--- Casks/defold-beta.rb
+++ Casks/defold-beta.rb
@@ -1,6 +1,6 @@
 cask "defold-beta" do
-  version "1.4.1,ba61b10664fb9b96dfae367ba088960601bd8560"
-  sha256 "eff9b067e69377aaad3ffc3734d2961538c94d10c0c7978aae09b58e2e111752"
+  version "1.4.1,8f96e450ddfb006a99aa134fdd373cace3760571"
+  sha256 "39e614bbf73dbfe132a36fa2ddf1197e884e91ae7b0d3af71b7da3abf0fc8f13"
 
   url "https://d.defold.com/archive/beta/#{version.csv.second}/beta/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
