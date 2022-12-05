5f888aa20d1bd0c3b78fe8f77b6a75b5a28f16e8
Bevan Kay <email@bevankay.me>
2022-12-05 21:49:26 +1100

    openscad-snapshot 2022.12.04
    * Update openscad-snapshot from 2022.08.26 to 2022.12.04
    * Update openscad-snapshot.rb
    
    Closes #15340.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/openscad-snapshot.rb
+++ Casks/openscad-snapshot.rb
@@ -1,11 +1,24 @@
 cask "openscad-snapshot" do
-  version "2022.08.26"
-  sha256 "f98f223bd4439030ca1dac9cab62b8a78fb66301edbbfd7e935b975d126c0265"
+  version "2022.12.04"
+  sha256 "16f4316022a7fb707632dd1f9971435ab936cd00c5e98359773c3f27b56e49e3"
 
   url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
-  appcast "https://files.openscad.org/snapshots/.snapshot_macos.js"
   name "OpenSCAD"
+  desc "Programmable solid 3D CAD modeller"
   homepage "https://www.openscad.org/downloads.html#snapshots"
 
+  livecheck do
+    url "https://files.openscad.org/snapshots/.snapshot_macos.js"
+    regex(/OpenSCAD[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
+  end
+
+  conflicts_with cask: "openscad"
+
   app "OpenSCAD.app"
+
+  zap trash: [
+    "~/Library/Caches/org.openscad.OpenSCAD",
+    "~/Library/Preferences/org.openscad.OpenSCAD.plist",
+    "~/Library/Saved Application State/org.openscad.OpenSCAD.savedState",
+  ]
 end
