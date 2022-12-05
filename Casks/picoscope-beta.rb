27532666f7ac50da56cf16fdcb527f1e98fb5797
Bevan Kay <email@bevankay.me>
2022-12-05 23:08:49 +1100

    Update picoscope-beta from 7.0.112.13028 to 7.0.114.13320

--- Casks/picoscope-beta.rb
+++ Casks/picoscope-beta.rb
@@ -1,18 +1,18 @@
 cask "picoscope-beta" do
-  version "7.0.112.13028"
-  sha256 "bfe3d2a3444856c0f10ceec0698da0594b10c7fa9c66f556b4923cb111abd9ba"
+  version "7.0.114.13320"
+  sha256 "2d98c2fe3a7744391b72cb7a37be9199e643368fae7b15faa9154d6dcc4cc952"
 
-  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version}.pkg"
+  url "https://oem.picotech.com/software/p#{version.major}beta/PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
   name "PicoScope beta"
   desc "Test and measurement oscilloscope software for PicoScope oscilloscops"
   homepage "https://www.picotech.com/"
 
   livecheck do
     url "https://oem.picotech.com/p#{version.major}beta/download/"
-    regex(%r{href=.*?/PicoScope_?(\d+(?:\.\d+)+).pkg}i)
+    regex(%r{href=.*?/PicoScope_(?:\d)_TnM_Early_Access[._-]v?(\d+(?:\.\d+)+)\.pkg}i)
   end
 
-  pkg "PicoScope_#{version}.pkg"
+  pkg "PicoScope_#{version.major}_TnM_Early_Access_#{version}.pkg"
 
   uninstall pkgutil: "com.picotech.picoscope#{version.major}tnmbeta"
 end
