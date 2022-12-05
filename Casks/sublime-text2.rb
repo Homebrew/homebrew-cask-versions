6bd6a3e0c96ca9fbdcf10f2d22a1d71dcf5ceea0
Bevan Kay <email@bevankay.me>
2022-12-06 00:12:36 +1100

    sublime-text2: rebuild
    * Update sublime-text2 from 2.0.2 to 2.0.2
    * Update sublime-text2.rb
    
    Closes #15356.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/sublime-text2.rb
+++ Casks/sublime-text2.rb
@@ -3,8 +3,8 @@ cask "sublime-text2" do
   sha256 "906e71e19ae5321f80e7cf42eab8355146d8f2c3fd55be1f7fe5c62c57165add"
 
   url "https://download.sublimetext.com/Sublime%20Text%20#{version}.dmg"
-  appcast "https://www.sublimetext.com/updates/2/stable/appcast_osx.xml"
   name "Sublime Text 2"
+  desc "Text editor for code, markup and prose"
   homepage "https://www.sublimetext.com/2"
 
   app "Sublime Text 2.app"
@@ -16,4 +16,8 @@ cask "sublime-text2" do
     "~/Library/Caches/com.sublimetext.2",
     "~/Library/Saved Application State/com.sublimetext.2.savedState",
   ]
+
+  caveats do
+    discontinued
+  end
 end
