58e0b74b88cf3fc8662cd1bd7841557805b1345e
Bevan Kay <email@bevankay.me>
2022-12-05 22:53:50 +1100

    papers3: rebuild
    Update papers3 from 3.4.25_600 to 3.4.25_600
    
    Closes #15342.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/papers3.rb
+++ Casks/papers3.rb
@@ -5,7 +5,7 @@ cask "papers3" do
   url "https://downloads.mekentosj.com/papers_#{version.no_dots}.dmg",
       verified: "downloads.mekentosj.com/"
   name "Papers"
-  desc "Your personal library of research"
+  desc "Reference management software for researchers"
   homepage "https://support.papersapp.com/support/solutions/articles/30000031865-existing-papers-3-users-accessing-papers-3-program-files-for-additional-device-installs"
 
   app "Papers.app"
@@ -19,4 +19,8 @@ cask "papers3" do
     "~/Library/Preferences/com.mekentosj.Citations.plist",
     "~/Library/Preferences/com.mekentosj.papers#{version.major}",
   ]
+
+  caveats do
+    discontinued
+  end
 end
