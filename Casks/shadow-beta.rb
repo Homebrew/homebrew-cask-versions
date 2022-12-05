36ade1a091faa6e7e5f282c8f8af7ccfd578268a
Bevan Kay <email@bevankay.me>
2022-12-05 23:39:17 +1100

    shadow-beta 8.0.10012
    Update shadow-beta from 8.0.10011 to 8.0.10012
    
    Closes #15350.
    
    Signed-off-by: Patrick Linnane <patrick@linnane.io>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/shadow-beta.rb
+++ Casks/shadow-beta.rb
@@ -1,10 +1,19 @@
 cask "shadow-beta" do
-  version :latest
-  sha256 :no_check
+  arch arm: "arm64", intel: "x64"
 
-  url "https://update.shadow.tech/launcher/preprod/mac/ShadowBeta.dmg"
+  version "8.0.10012"
+  sha256 arm:   "2a32a60afb01394d61cd10258dcff3c71d4c3e6053ac52b52f1325ea1e4d36db",
+         intel: "eb4ea2f1cf8611ac42fff1ff277e26f6690292f11b7cf823e9022e57dc08d6be"
+
+  url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowBeta-#{version}.dmg"
   name "Shadow Beta"
+  desc "Online virtualized computer"
   homepage "https://shadow.tech/"
+
+  livecheck do
+    url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/latest-mac.yml"
+    strategy :electron_builder
+  end
 
   app "Shadow Beta.app"
 
