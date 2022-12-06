aa93ed8674380772b55e9056ea1894cc447ceb75
Patrick Linnane <patrick@linnane.io>
2022-12-05 22:47:29 -0800

    transmit4: remove appcast

--- Casks/transmit4.rb
+++ Casks/transmit4.rb
@@ -3,8 +3,8 @@ cask "transmit4" do
   sha256 "0d7095351378aa3c581de064f99fedecbfac683377048a4c0457976f5ce79f3b"
 
   url "https://download.panic.com/transmit/Transmit%20#{version.major}/Transmit%20#{version}.zip"
-  appcast "https://library.panic.com/releasenotes/transmit#{version.major}/"
   name "Transmit"
+  desc "File transfer application"
   homepage "https://panic.com/transmit/"
 
   app "Transmit.app"
