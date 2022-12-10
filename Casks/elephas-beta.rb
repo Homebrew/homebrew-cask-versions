d83834353d17372e9fceb689342f6bcb504c1fdb
Alexander Goldstein <alexg@alexland.org>
2022-12-10 03:02:01 -0600

    beta of elephas cask (#15422)
    * beta of elephas cask
    
    * Update elephas-beta.rb
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>

---
+++ Casks/elephas-beta.rb
@@ -0,0 +1,26 @@
+cask "elephas-beta" do
+  version "4.4"
+  sha256 :no_check
+
+  url "https://elephas.app/downloads/Elephas_beta.dmg"
+  name "Elephas"
+  desc "Personal AI Writing Assistant"
+  homepage "https://elephas.app/"
+
+  livecheck do
+    url :url
+    strategy :extract_plist
+  end
+
+  conflicts_with cask: "elephas"
+  depends_on macos: ">= :catalina"
+
+  app "Elephas.app"
+
+  zap trash: [
+    "~/Library/Application Support/Elephas",
+    "~/Library/Caches/com.kamban.elephas",
+    "~/Library/HTTPStorages/com.kamban.elephas",
+    "~/Library/Preferences/com.kamban.elephas.plist",
+  ]
+end
