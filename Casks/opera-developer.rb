3554dfb56a27c9b3e99b7eec26707b08371592f7
Patrick Linnane <patrick@linnane.io>
2022-12-12 14:53:49 -0800

    opera-developer 95.0.4632.0
    Update opera-developer from 95.0.4625.0 to 95.0.4632.0
    
    Closes #15450.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "95.0.4625.0"
-  sha256 "cc25885ac443a3e783082b835d6efef62caee8a95dcb46c306f7adb2804992fd"
+  version "95.0.4632.0"
+  sha256 "14be03c5d2f9c255a4e04de6ac7ddb538bc2c182c749c9d433d3adef9fac8253"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
