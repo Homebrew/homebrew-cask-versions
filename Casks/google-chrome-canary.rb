625edef878ea7988f172bae40114c7631aaf2918
Patrick Linnane <patrick@linnane.io>
2022-12-12 14:49:55 -0800

    Update google-chrome-canary from 110.0.5468.0 to 110.0.5474.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "110.0.5468.0"
+  version "110.0.5474.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
