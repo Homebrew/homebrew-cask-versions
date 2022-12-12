7271b67c50a3b0efbbc3cec7de3f37fee4a31df7
Patrick Linnane <patrick@linnane.io>
2022-12-12 14:51:20 -0800

    inso-beta 3.7.0-alpha.0
    Update inso-beta from 3.6.1-beta.5 to 3.7.0-alpha.0
    
    Closes #15447.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/inso-beta.rb
+++ Casks/inso-beta.rb
@@ -1,6 +1,6 @@
 cask "inso-beta" do
-  version "3.6.1-beta.5"
-  sha256 "1cd1f47d9d9fb5648b57189b4ed93d4a64b5ab03739f847232dd47aa56ed310c"
+  version "3.7.0-alpha.0"
+  sha256 "c323ab8ce72d32204edf4ca570f5d5fc62825ba1f59b7203d35c716f874ba996"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
