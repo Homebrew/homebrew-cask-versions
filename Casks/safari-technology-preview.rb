28de1cc6162435983f90a6b210dd977b53e95c98
miccal <miccal.matthews@gmail.com>
2022-12-15 05:36:28 +0800

    safari-technology-preview 160,032-13309-20221213-7EFE8FB7-39FF-44BB-B1E4-7394A1C1A821
    * Update safari-technology-preview from 159 to 160
    * Update safari-technology-preview.rb
    * Update safari-technology-preview.rb
    
    Closes #15460.
    
    Co-authored-by: Miccal Matthews <miccal.matthews@gmail.com>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,10 +1,10 @@
 cask "safari-technology-preview" do
   if MacOS.version <= :monterey
-    version "159,032-11080-20221129-D2CEFB02-E1B1-44BC-9AC8-C555787D7B4C"
-    sha256 "9abfc921354d838426451f66ef1068d3acc337b1e49a37c3aff47f9478ab9737"
+    version "160,032-13309-20221213-7EFE8FB7-39FF-44BB-B1E4-7394A1C1A821"
+    sha256 "6179932f99524ef5cdbf80ccd2243d3d93c9aac1c4066c91f505ad10a449fb7d"
   else
-    version "159,032-10387-20221129-6DAC7465-3037-4525-B563-AF020DEC96F4"
-    sha256 "637d9d337f254734da4707de2a779478b8c01021edb8c3ce484258d175f92ac6"
+    version "160,032-13301-20221213-4525DA9A-DDC3-477A-9FF5-384F3D2486DD"
+    sha256 "56adbbfc838800a053bb3c8d194dc672b534d840c99c6dcee9e93e0cd4df19bf"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.csv.second}/SafariTechnologyPreview.dmg"
