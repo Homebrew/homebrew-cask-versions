53c6ef34538d04fcef70a2a0a4faac1075e24122
César Román <thecesrom@gmail.com>
2022-12-19 11:10:49 -0800

    revert(casks): use latest version for azure-data-studio-insiders
    Refs: 4999b3f

--- Casks/azure-data-studio-insiders.rb
+++ Casks/azure-data-studio-insiders.rb
@@ -1,20 +1,14 @@
 cask "azure-data-studio-insiders" do
   arch arm: "-arm64"
 
-  version "1.41.0"
-  sha256 arm:   "9c621fbc13dfed72477463485ad74a9d1dd31e084d54116b3bc8ff615edd5a32",
-         intel: "686f0142423466741c1610e227324aa6b389c5f2f16aa9f08923207b986e88d3"
+  version :latest
+  sha256 :no_check
 
-  url "https://azuredatastudio-update.azurewebsites.net/#{version}/darwin#{arch}/insider",
+  url "https://azuredatastudio-update.azurewebsites.net/latest/darwin#{arch}/insider",
       verified: "azuredatastudio-update.azurewebsites.net"
   name "Azure Data Studio - Insiders"
   desc "Data management tool that enables working with SQL Server"
   homepage "https://docs.microsoft.com/en-us/sql/azure-data-studio/"
-
-  livecheck do
-    url "https://azuredatastudio-update.azurewebsites.net/api/update/darwin/insider/VERSION"
-    regex(/"productVersion"\s*:\s*"(\d+(:?\.\d+)+)"/i)
-  end
 
   app "Azure Data Studio - Insiders.app"
   binary "#{appdir}/Azure Data Studio - Insiders.app/Contents/Resources/app/bin/code",
