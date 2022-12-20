897ab2649c0af6ce4e1691ac66842a0f5b841f45
Patrick Linnane <patrick@linnane.io>
2022-12-20 14:03:18 -0800

    Update opera-developer from 95.0.4632.0 to 96.0.4640.0 (#15522)

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "95.0.4632.0"
-  sha256 "14be03c5d2f9c255a4e04de6ac7ddb538bc2c182c749c9d433d3adef9fac8253"
+  version "96.0.4640.0"
+  sha256 "121fd2287e13488495121d77549cd20a3070805c019cb91df40c12dfbadc2c4c"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
