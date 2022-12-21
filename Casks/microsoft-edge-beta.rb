8ffe5f940a8337988a0a8ca90d97559b592946b8
Hannes Braun <hannesbraun@mail.de>
2022-12-21 12:31:06 +0100

    Update microsoft-edge-beta from 109.0.1518.23 to 109.0.1518.26

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-beta" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099618", intel: "2069439"
 
-  version "109.0.1518.23"
-  sha256 arm:   "eef38b094f063608adebe88657ca3e650cf5fdf7ccee7f652ce704f7ab5db026",
-         intel: "b616715c55b3808cb10d9c4f07b6816b86724f7da49f05e74ba57ad587b2017d"
+  version "109.0.1518.26"
+  sha256 arm:   "3986e4895e68ee08ccff6468aa2558556df457c0bbae82514eba7aa06ff2e2a2",
+         intel: "9f75d5e16e1ce16b044763136b320c24ef39324e909c2674696ff46eac826779"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
