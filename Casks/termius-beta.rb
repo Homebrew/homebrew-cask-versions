f4605d64a473fe4a6fe17cfaa0e34b1bddf3e687
Patrick Linnane <patrick@linnane.io>
2022-12-21 14:00:42 -0800

    Update termius-beta from 7.54.1 to 7.54.2 (#15534)

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,7 +1,7 @@
 cask "termius-beta" do
   arch arm: "mac-beta-arm64", intel: "mac-beta"
 
-  version "7.54.1"
+  version "7.54.2"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius%20Beta.dmg"
