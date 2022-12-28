2bcab1b213b172915884738d73c41a08e3363c78
Bevan Kay <email@bevankay.me>
2022-12-28 17:14:42 +1100

    omnifocus2: convert to `on_system` blocks

--- Casks/omnifocus2.rb
+++ Casks/omnifocus2.rb
@@ -1,9 +1,10 @@
 cask "omnifocus2" do
-  if MacOS.version <= :el_capitan
+  on_el_capitan :or_older do
     version "2.10"
     sha256 "e808a72e60cdff9ff5aa1046d856bf62d6418e4915248816c4640e32e52fd8e8"
     url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniFocus-#{version}.dmg"
-  else
+  end
+  on_sierra :or_newer do
     version "2.12.4"
     sha256 "8a2dc53331dba804f6781773fef546a03c181fc4ff0eb7ee4f871c10342621f0"
     url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniFocus-#{version}.dmg"
