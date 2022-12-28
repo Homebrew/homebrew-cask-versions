74259061d62ca8ef8c7c5b0540726df5fdb0c680
Bevan Kay <email@bevankay.me>
2022-12-28 17:16:49 +1100

    openemu-experimental: convert to `on_system` blocks

--- Casks/openemu-experimental.rb
+++ Casks/openemu-experimental.rb
@@ -1,8 +1,13 @@
 cask "openemu-experimental" do
-  if MacOS.version <= :high_sierra
+  on_high_sierra :or_older do
     version "2.0.9.1"
     sha256 "62c44e823fef65c583cbf5e6f84faa03618d713f45610f73bc23fb34cbf64762"
-  else
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_mojave :or_newer do
     version "2.3.3"
     sha256 "8b91fecfb0fd58ffc03abca00d6df2c519c396b955072c6c209d53018f921cf7"
   end
@@ -26,29 +31,7 @@ cask "openemu-experimental" do
     "~/Library/Caches/org.openemu.OpenEmu",
     "~/Library/Cookies/org.openemu.OpenEmu.binarycookies",
     "~/Library/HTTPStorages/org.openemu.OpenEmu.binarycookies",
-    "~/Library/Preferences/org.openemu.Atari800.plist",
-    "~/Library/Preferences/org.openemu.Bliss.plist",
-    "~/Library/Preferences/org.openemu.CrabEmu.plist",
-    "~/Library/Preferences/org.openemu.desmume.plist",
-    "~/Library/Preferences/org.openemu.FCEU.plist",
-    "~/Library/Preferences/org.openemu.Gambatte.plist",
-    "~/Library/Preferences/org.openemu.GenesisPlus.plist",
-    "~/Library/Preferences/org.openemu.Higan.plist",
-    "~/Library/Preferences/org.openemu.Mednafen.plist",
-    "~/Library/Preferences/org.openemu.Mupen64Plus.plist",
-    "~/Library/Preferences/org.openemu.NeoPop.plist",
-    "~/Library/Preferences/org.openemu.Nestopia.plist",
-    "~/Library/Preferences/org.openemu.O2EM.plist",
-    "~/Library/Preferences/org.openemu.OpenEmu.plist",
-    "~/Library/Preferences/org.openemu.Picodrive.plist",
-    "~/Library/Preferences/org.openemu.PPSSPP.plist",
-    "~/Library/Preferences/org.openemu.ProSystem.plist",
-    "~/Library/Preferences/org.openemu.SNES9x.plist",
-    "~/Library/Preferences/org.openemu.Stella.plist",
-    "~/Library/Preferences/org.openemu.TwoMbit.plist",
-    "~/Library/Preferences/org.openemu.VecXGL.plist",
-    "~/Library/Preferences/org.openemu.VisualBoyAdvance.plist",
-    "~/Library/Preferences/org.openemu.mGBA.plist",
+    "~/Library/Preferences/org.openemu.*.plist",
     "~/Library/Saved Application State/org.openemu.OpenEmu.savedState",
   ]
 end
