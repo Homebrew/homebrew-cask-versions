2db5886c975aa484fe14799c317e5a98911edf99
Miccal Matthews <miccal.matthews@gmail.com>
2022-12-31 07:15:49 +0800

    Update microsoft-office-2016.rb

--- Casks/microsoft-office-2016.rb
+++ Casks/microsoft-office-2016.rb
@@ -10,10 +10,10 @@ cask "microsoft-office-2016" do
 
   auto_updates true
   conflicts_with cask: %w[
-    microsoft-office
-    microsoft-word
     microsoft-excel
+    microsoft-office
     microsoft-powerpoint
+    microsoft-word
     onedrive
   ]
   depends_on macos: ">= :sierra"
@@ -44,63 +44,61 @@ cask "microsoft-office-2016" do
             ],
             launchctl: [
               "com.microsoft.autoupdate.helper",
+              "com.microsoft.office.licensing.helper",
+              "com.microsoft.office.licensingV2.helper",
               "com.microsoft.OneDriveStandaloneUpdater",
               "com.microsoft.OneDriveStandaloneUpdaterDaemon",
               "com.microsoft.OneDriveUpdaterDaemon",
-              "com.microsoft.office.licensing.helper",
-              "com.microsoft.office.licensingV2.helper",
               "com.microsoft.update.agent",
             ],
-            quit:      [
-              "com.microsoft.autoupdate2",
-            ]
+            quit:      "com.microsoft.autoupdate2"
 
   zap trash:     [
+        "~/Library/Application Scripts/com.microsoft.errorreporting",
         "~/Library/Application Scripts/com.microsoft.Excel",
         "~/Library/Application Scripts/com.microsoft.Office365ServiceV2",
+        "~/Library/Application Scripts/com.microsoft.onenote.mac",
         "~/Library/Application Scripts/com.microsoft.Outlook",
         "~/Library/Application Scripts/com.microsoft.Powerpoint",
         "~/Library/Application Scripts/com.microsoft.Word",
-        "~/Library/Application Scripts/com.microsoft.errorreporting",
-        "~/Library/Application Scripts/com.microsoft.onenote.mac",
         "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.excel.sfl*",
         "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.powerpoint.sfl*",
         "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.microsoft.word.sfl*",
-        "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba",
-        "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate2",
         "~/Library/Caches/com.microsoft.autoupdate.fba",
         "~/Library/Caches/com.microsoft.autoupdate2",
+        "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate.fba",
+        "~/Library/Caches/Microsoft/uls/com.microsoft.autoupdate2",
+        "~/Library/Containers/com.microsoft.com.microsoft.RMS-XPCService",
+        "~/Library/Containers/com.microsoft.errorreporting",
         "~/Library/Containers/com.microsoft.Excel",
+        "~/Library/Containers/com.microsoft.netlib.shipassertprocess",
         "~/Library/Containers/com.microsoft.Office365ServiceV2",
+        "~/Library/Containers/com.microsoft.onenote.mac",
         "~/Library/Containers/com.microsoft.Outlook",
         "~/Library/Containers/com.microsoft.Powerpoint",
-        "~/Library/Containers/com.microsoft.com.microsoft.RMS-XPCService",
         "~/Library/Containers/com.microsoft.Word",
-        "~/Library/Containers/com.microsoft.errorreporting",
-        "~/Library/Containers/com.microsoft.netlib.shipassertprocess",
-        "~/Library/Containers/com.microsoft.onenote.mac",
         "~/Library/Cookies/com.microsoft.autoupdate.fba.binarycookies",
         "~/Library/Cookies/com.microsoft.autoupdate2.binarycookies",
+        "~/Library/Group Containers/UBF8T346G9.ms",
         "~/Library/Group Containers/UBF8T346G9.Office",
         "~/Library/Group Containers/UBF8T346G9.OfficeOneDriveSyncIntegration",
         "~/Library/Group Containers/UBF8T346G9.OfficeOsfWebHost",
-        "~/Library/Group Containers/UBF8T346G9.ms",
+        "~/Library/Preferences/com.microsoft.autoupdate.fba.plist",
+        "~/Library/Preferences/com.microsoft.autoupdate2.plist",
         "~/Library/Preferences/com.microsoft.Excel.plist",
         "~/Library/Preferences/com.microsoft.Outlook.plist",
         "~/Library/Preferences/com.microsoft.Powerpoint.plist",
         "~/Library/Preferences/com.microsoft.Word.plist",
-        "~/Library/Preferences/com.microsoft.autoupdate.fba.plist",
-        "~/Library/Preferences/com.microsoft.autoupdate2.plist",
         "~/Library/Saved Application State/com.microsoft.autoupdate2.savedState",
         "~/Library/Saved Application State/com.microsoft.office.setupassistant.savedState",
       ],
       rmdir:     [
-        "~/Library/Caches/Microsoft/uls",
         "~/Library/Caches/Microsoft",
+        "~/Library/Caches/Microsoft/uls",
       ],
       launchctl: [
-        "com.microsoft.autoupdate.helpertool",
         "com.microsoft.autoupdate.helper",
+        "com.microsoft.autoupdate.helpertool",
         "com.microsoft.update.agent",
       ],
       pkgutil:   "com.microsoft.package.Microsoft_AutoUpdate.app"
