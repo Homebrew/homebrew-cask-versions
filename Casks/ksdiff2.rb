d2815235e4a5cecd9cdab6dfaeacc6fcaeb8e85d
Patrick Linnane <patrick@linnane.io>
2023-01-05 08:53:24 -0800

    ksdiff2: discontinue

--- Casks/ksdiff2.rb
+++ Casks/ksdiff2.rb
@@ -7,20 +7,6 @@ cask "ksdiff2" do
   desc "Command-line tool for the App Store version of Kaleidoscope v2"
   homepage "https://kaleidoscope.app/ksdiff#{version.major}"
 
-  livecheck do
-    url :homepage
-    regex(%r{/ksdiff[._-]v?(\d+(?:\.\d+)+)[._-](\d+)[._-](\w+(?:-\d+)*)\.zip}i)
-    strategy :page_match do |page, regex|
-      js_file = page[%r{src=["']?(/ksdiff\d*\.\w+\.js)["' >]}i, 1]
-      next [] if js_file.blank?
-
-      js_file_data = Homebrew::Livecheck::Strategy.page_content("https://kaleidoscope.app#{js_file}")
-      next [] if js_file_data[:content].blank?
-
-      js_file_data[:content].scan(regex).map { |match| "#{match[0]},#{match[1]},#{match[2]}" }
-    end
-  end
-
   conflicts_with cask: [
     "kaleidoscope",
     "kaleidoscope2",
@@ -31,8 +17,7 @@ cask "ksdiff2" do
 
   uninstall pkgutil: "com.blackpixel.kaleidoscope.ksdiff.installer.pkg"
 
-  caveats <<~EOS
-    The #{token} Cask is not needed when installing Kaleidoscope v2 via Cask. It
-    is provided for users who have purchased Kaleidoscope v2 via the App Store.
-  EOS
+  caveats do
+    discontinued
+  end
 end
