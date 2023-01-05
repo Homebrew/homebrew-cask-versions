065e695e3821ce7c87092b8b2a18cf3670fdc6e2
Patrick Linnane <patrick@linnane.io>
2023-01-05 08:39:45 -0800

    anki-beta 2.1.56+rc1_bc697597
    Update anki-beta from 2.1.55+beta7_fad1ff74 to 2.1.56+rc1_bc697597
    
    Closes #15623.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -1,9 +1,9 @@
 cask "anki-beta" do
   arch arm: "apple", intel: "intel"
 
-  version "2.1.55+beta7_fad1ff74"
-  sha256 arm:   "684e260e6504a78b1db67e8e5fe0bb4998dcdba1cded2589313aad42eda41451",
-         intel: "80c158b8b9e2a28dc73eb6fa332965079722bc3c8a5702d0f463eb97058c181d"
+  version "2.1.56+rc1_bc697597"
+  sha256 arm:   "3a9df42fd7bfbb8ece959af1d0ae3f2864becc8012299d1d64585f604e4ed938",
+         intel: "8b851c2cc62249e6d5652c9a19c504ce845939373644d98a4cccd4d31cac7205"
 
   url "https://apps.ankiweb.net/downloads/beta/anki-#{version}-mac-#{arch}-qt6.dmg"
   name "Anki Beta"
@@ -12,7 +12,7 @@ cask "anki-beta" do
 
   livecheck do
     url "https://apps.ankiweb.net/downloads/beta/"
-    regex(/title=["']?anki[._-]?(\d+\.\d+.\d+\+beta\d+_[a-f0-9]+)-mac(?:-#{arch})?(?:-qt\d+)?\.dmg["'\s>]/i)
+    regex(/title=["']?anki[._-]?(\d+\.\d+.\d+\+(?:beta|rc)\d+_[a-f0-9]+)-mac(?:-#{arch})?(?:-qt\d+)?\.dmg["'\s>]/i)
   end
 
   conflicts_with cask: "anki"
