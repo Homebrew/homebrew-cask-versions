a99071d5cf5bf385278989bebb8a757b3ea0bd58
Patrick Linnane <patrick@linnane.io>
2023-01-11 09:55:08 -0800

    google-chrome-canary 111.0.5532.0
    Update google-chrome-canary from 111.0.5531.0 to 111.0.5532.0
    
    Closes #15689.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "111.0.5531.0"
+  version "111.0.5532.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
