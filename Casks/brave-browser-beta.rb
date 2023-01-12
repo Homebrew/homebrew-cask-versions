f2225e40c6e172875a92514ea4253eb9eeeac29a
Hannes Braun <hannesbraun@mail.de>
2023-01-12 22:29:52 +0100

    brave-browser-beta 1.48.122.0,148.122
    Update brave-browser-beta from 1.48.119.0 to 1.48.122.0
    
    Closes #15698.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -2,9 +2,9 @@ cask "brave-browser-beta" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "beta-arm64", intel: "beta"
 
-  version "1.48.119.0,148.119"
-  sha256 arm:   "6422881297c41a5e5f171d29676853d6183a34511f447c9fe7df6d36cfe69597",
-         intel: "2d448eacc8dda5a6d8478c3621eef27297c971dab042a16dc487cbd8d18660c1"
+  version "1.48.122.0,148.122"
+  sha256 arm:   "e16c38df8c9d814e5f2d7f36e162e40a4abd6d1772cc168840e795d8e05015a6",
+         intel: "f7700e4fb46e9ee9ea0e1c88e1e77b8260d47d2dd65e391c93da2c8ff7e8bd53"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Beta-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
