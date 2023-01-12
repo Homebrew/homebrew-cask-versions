b6ac29b268c015bffc60fdf7d42397d77f554dff
Hannes Braun <hannesbraun@mail.de>
2023-01-12 22:32:17 +0100

    microsoft-edge-beta 109.0.1518.49
    Update microsoft-edge-beta from 109.0.1518.44 to 109.0.1518.49
    
    Closes #15701.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-beta" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099618", intel: "2069439"
 
-  version "109.0.1518.44"
-  sha256 arm:   "9462eb39b073c0e454e8892cce4c5a99cdba35701a2d76939754a9231e3af10f",
-         intel: "bb6b59e5ca92e0bca14bade3eb6bc855d9e0a609c202578510c1743fe07e6695"
+  version "109.0.1518.49"
+  sha256 arm:   "96d3bfce92c23d4792c1e9640fa27eb26320b3c441d93cea0fd932a0c6864e02",
+         intel: "669da071ebe40afd1544bdad356b1656dd9feec1bd338c86826a77136aac0b45"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
