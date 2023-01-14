82d4c5e9e63fd683f0aa9ca452c53ce3d7890e3b
Patrick Linnane <patrick@linnane.io>
2023-01-13 22:15:01 -0800

    termius-beta 7.55.2
    Update termius-beta from 7.54.3 to 7.55.2
    
    Closes #15718.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,7 +1,7 @@
 cask "termius-beta" do
   arch arm: "mac-beta-arm64", intel: "mac-beta"
 
-  version "7.54.3"
+  version "7.55.2"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius%20Beta.dmg"
