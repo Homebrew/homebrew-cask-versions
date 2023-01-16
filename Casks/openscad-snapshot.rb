c1dc5d934d8d4250b64afd0207a5a9f6375d14ac
Bevan Kay <email@bevankay.me>
2023-01-16 21:59:23 +1100

    Update openscad-snapshot from 2023.01.13 to 2023.01.15

--- Casks/openscad-snapshot.rb
+++ Casks/openscad-snapshot.rb
@@ -1,6 +1,6 @@
 cask "openscad-snapshot" do
-  version "2023.01.13"
-  sha256 "eee911921a3a1d71d93631280ea32752923fdcf3dfd9ebc9b26fe4facfa19b83"
+  version "2023.01.15"
+  sha256 "fc8a8dc0c1d6331c6fb5f5aae3b4b026c52cbd0f9f6887aeee968db2a5987f5b"
 
   url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
   name "OpenSCAD"
