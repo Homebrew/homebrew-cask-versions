3b3f1b757694e1a8ab34e9abeb25916f6db9afc9
Bevan Kay <email@bevankay.me>
2023-01-18 22:49:22 +1100

    Update zulu15 from 15.0.9 to 15.0.10

--- Casks/zulu15.rb
+++ Casks/zulu15.rb
@@ -2,9 +2,9 @@ cask "zulu15" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "15.0.9,15.44.13-ca"
-  sha256 arm:   "0c3628efdf22c387d41d68889e90b7045c6e99fb6e9e1e5f9e32c9ca423226bc",
-         intel: "b8d81ed577237b94c15b86f7db449519ef5b7ca540451eb74eac64f7bdf90a11"
+  version "15.0.10,15.46.17-ca"
+  sha256 arm:   "f5d123cd149f245792d0d807086f4c58c493bd52db8162ed2d2609f97e493ac1",
+         intel: "9e8a2b162826af9ac9db248e4c9e7a8983bbe31ae63686a19a3c291770f9427c"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
