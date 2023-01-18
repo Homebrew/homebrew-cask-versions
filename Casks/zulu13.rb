08d619c943e5ad6b125deb32003fbe606ef74692
Bevan Kay <email@bevankay.me>
2023-01-18 22:47:51 +1100

    Update zulu13 from 13.0.13 to 13.0.14

--- Casks/zulu13.rb
+++ Casks/zulu13.rb
@@ -2,9 +2,9 @@ cask "zulu13" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "13.0.13,13.52.15-ca"
-  sha256 arm:   "1e4050a8aeaa88af0e5f67e84743a0b205e3ffdb7a78be0cfef666dd969af0a1",
-         intel: "f49a48687a9443215d46f560b06b87869f217348fc590a7231720ac5b75ad3fa"
+  version "13.0.14,13.54.17-ca"
+  sha256 arm:   "b7b41f43f6a76f84d579e1d7b4abfd536928b6472e0ca3a8188f474235137b8e",
+         intel: "e83a0fb55f3d736c88d9fff33ee6c4a11901507a932477df15bed220c84714c4"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
