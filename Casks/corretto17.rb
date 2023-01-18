8d3c4018e46d3360c4f4d52091886516e1c1df76
Victor Rudometov <2694233+Rudometov@users.noreply.github.com>
2023-01-17 22:08:35 -0800

    Update corretto17 from 17.0.5.8.1 to 17.0.6.10.1 (#15736)

--- Casks/corretto17.rb
+++ Casks/corretto17.rb
@@ -1,9 +1,9 @@
 cask "corretto17" do
   arch arm: "aarch64", intel: "x64"
 
-  version "17.0.5.8.1"
-  sha256 arm:   "1e0d967f7e030e710b03fa283d7b735fe515c2cae78206fd6530868fe5b97156",
-         intel: "d523f0a7ae4232e7eeebed5608336cdfdc5c1780243a6353d3842f68a5a05a59"
+  version "17.0.6.10.1"
+  sha256 arm:   "526d2f664bc5d93aeff6b5d8e1b45d9d644d09b1939bcabbe04cdf692c1f2db2",
+         intel: "4de291b2c5e4535fce01a8ced0416424352d2d320794087f62c3a6fd42a3ab0f"
 
   url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-#{arch}.pkg"
   name "AWS Corretto JDK"
