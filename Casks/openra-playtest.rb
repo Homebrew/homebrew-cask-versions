c9e360326e7900e6de68f388faaa4f20b2529519
Alexander Filippov <a.filippov@technodeep.net>
2023-01-21 11:01:25 +0300

    Add: openra playtest version

---
+++ Casks/openra-playtest.rb
@@ -0,0 +1,26 @@
+cask "openra-playtest" do
+  version "20230110"
+  sha256 "611be7801e9e0579a83c150db24a924e764569a357de8d35476eb9bfe28570a5"
+
+  url "https://github.com/OpenRA/OpenRA/releases/download/playtest-#{version}/OpenRA-playtest-#{version}.dmg",
+      verified: "github.com/OpenRA/OpenRA/"
+  name "OpenRA (playtest)"
+  desc "Real-time strategy game engine for Westwood games"
+  homepage "https://www.openra.net/"
+
+  livecheck do
+    url :url
+    regex(/^playtest[._-]v?(\d+(?:[.-]\d+)*)$/i)
+  end
+
+  app "OpenRA - Dune 2000.app"
+  app "OpenRA - Red Alert.app"
+  app "OpenRA - Tiberian Dawn.app"
+
+  zap trash: [
+    "~/Library/Application Support/OpenRA",
+    "~/Library/Saved Application State/net.openra.mod.cnc.savedState",
+    "~/Library/Saved Application State/net.openra.mod.d2k.savedState",
+    "~/Library/Saved Application State/net.openra.mod.ra.savedState",
+  ]
+end
