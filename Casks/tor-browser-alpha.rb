f615be27c06e5fb44b073d2d8db1178a9f36a2ff
owine <owine@users.noreply.github.com>
2023-01-25 15:48:03 -0600

    Update tor-browser-alpha from 12.5a1 to 12.5a2

--- Casks/tor-browser-alpha.rb
+++ Casks/tor-browser-alpha.rb
@@ -1,6 +1,6 @@
 cask "tor-browser-alpha" do
-  version "12.5a1"
-  sha256 "73464d385387918558a5d1ce01e287a33e97d0a1822c88c48c440d345f8137db"
+  version "12.5a2"
+  sha256 "00136021a0bf5eb582a718e837ab43503b272c8e488e30d378b4258de7a63737"
 
   url "https://dist.torproject.org/torbrowser/#{version}/TorBrowser-#{version}-macos_ALL.dmg"
   name "Tor Browser"
