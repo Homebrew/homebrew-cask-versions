7d0bdd50c997b84d793be7d521166f30fd3cd5a9
owine <owine@users.noreply.github.com>
2023-01-26 13:51:18 -0600

    Update signal-beta from 6.4.0-beta.1 to 6.4.0-beta.2

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,9 +1,9 @@
 cask "signal-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.4.0-beta.1"
-  sha256 arm:   "d1fd00121aa7263214dffed27fc7da57e04768b2d745bf355213a4cd0f9a14bb",
-         intel: "50a77f836f7fbd495105e4d6acc290611d85f031270f81457c26c2d309973c5d"
+  version "6.4.0-beta.2"
+  sha256 arm:   "31591d714985da62f44e52405bb6819bbf473dade98ad3803991f2ace6ef235e",
+         intel: "c24c29d35a3f509e2ca47acc697e5c7aa9c574ab995e9da11265d64165e8b1ea"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
   name "Signal Beta"
