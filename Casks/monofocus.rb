7faf0846bfe54eef51a1f4acbac4175b08c440ab
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-28 12:25:41 +1100

    add MonoFocus v1.0.beta35

---
+++ Casks/monofocus.rb
@@ -0,0 +1,11 @@
+cask "monofocus" do
+  version "1.0.beta35"
+  sha256 :no_check
+
+  url "https://updates.monofocus.app/MonoFocus.latest.dmg"
+  name "MonoFocus"
+  desc "Keep all tasks from your todo apps in one place and one focused on your menu bar"
+  homepage "https://monofocus.app/"
+
+  app "MonoFocus.app"
+end
