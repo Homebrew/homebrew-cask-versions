84fd212110cd72d72145fa15aa7d0e112a1dfad8
Hannes Braun <hannesbraun@mail.de>
2023-01-29 18:37:05 +0100

    google-chrome-canary 112.0.5566.0
    Update google-chrome-canary from 111.0.5558.0 to 112.0.5566.0
    
    Closes #15828.
    
    Signed-off-by: Bevan Kay <email@bevankay.me>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "111.0.5558.0"
+  version "112.0.5566.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
