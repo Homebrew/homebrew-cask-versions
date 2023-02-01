64ed3ae12e31714d995f60ea7dd0c3e357f75a26
Hannes Braun <hannesbraun@mail.de>
2023-02-01 12:19:38 +0100

    microsoft-edge-dev 111.0.1652.0
    Update microsoft-edge-dev from 111.0.1633.0 to 111.0.1652.0
    
    Closes #15843.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-dev" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099619", intel: "2069340"
 
-  version "111.0.1633.0"
-  sha256 arm:   "db8e84bce0e9fcad31d11383763ee10c1479d0ad3b1a421f8cfa6428f23f784b",
-         intel: "b0e750decb439f8e8ef371836a53f9ab9fbe57d311a06bf9c1823a6fd7e73156"
+  version "111.0.1652.0"
+  sha256 arm:   "1178264c621a4576a1d74ceb840029b5eab3b1cc98adc954c3d96e2a8f2cfef4",
+         intel: "79c4695d2ec504f3aab3225f7fe1521f479f123b0af5c0fb287f6f3656665934"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
