4e44aaf24eca6d9fa94d9d3a9c87ef9a5a92b3b8
LanikSJ <laniksj@gmail.com>
2023-02-01 20:08:19 -0800

    Update signal-beta from 6.4.0-beta.2 to 6.5.0-beta.1

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,9 +1,9 @@
 cask "signal-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.4.0-beta.2"
-  sha256 arm:   "31591d714985da62f44e52405bb6819bbf473dade98ad3803991f2ace6ef235e",
-         intel: "c24c29d35a3f509e2ca47acc697e5c7aa9c574ab995e9da11265d64165e8b1ea"
+  version "6.5.0-beta.1"
+  sha256 arm:   "f973f61dbbb2215e671c991a22d03aca955e782c92d05e0a4768211efc3fe21c",
+         intel: "f0a8cf06bc8bc2ed5e969bbc7d27981595a1117ffc489ba917a0573245d6d3ce"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
   name "Signal Beta"
