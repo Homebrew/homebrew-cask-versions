456c6c032b44bb98adf6bbdd9a6226509628d9f7
Hannes Braun <hannesbraun@mail.de>
2023-02-03 12:00:28 +0100

    anki-beta 2.1.57+rc1_14189a91
    Update anki-beta from 2.1.57+beta1_5cdfa7a6 to 2.1.57+rc1_14189a91
    
    Closes #15864.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -1,9 +1,9 @@
 cask "anki-beta" do
   arch arm: "apple", intel: "intel"
 
-  version "2.1.57+beta1_5cdfa7a6"
-  sha256 arm:   "74520bade5f00a14eb7f7fe241c33cd58488fd474a8965c8235dea01ff94f3bc",
-         intel: "fa534d6e394384dac3f78f8166eaacb2b1707879e3f7b1f099fb0c4acfc61f2a"
+  version "2.1.57+rc1_14189a91"
+  sha256 arm:   "bbf685029db7290419aedd33340105c981dd62f26ecff3761cb158445e922b0f",
+         intel: "73740ccb39e8096e3075eb191881ad759a584bbafe9fbe62e7ca415796a863f4"
 
   url "https://apps.ankiweb.net/downloads/beta/anki-#{version}-mac-#{arch}-qt6.dmg"
   name "Anki Beta"
