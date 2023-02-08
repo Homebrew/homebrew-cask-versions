3dc246efaf52b7412869753ac0af1a2cf9f1dc54
Hannes Braun <hannesbraun@mail.de>
2023-02-08 22:51:31 +0100

    Update iterm2-nightly from 3_5_20230110 to 3_5_20230204 (#15909)

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,7 +1,7 @@
 cask "iterm2-nightly" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3_5_20230110"
-  sha256 "4278a4f8daaffb0a06cffbd898b7b1b783b17155b5813ce1966e29e8000a2927"
+  version "3_5_20230204"
+  sha256 "ff6cfafca93e352568e9db4de4dc849a95e0bda7adec5f8e3da96b6a2251e2ae"
 
   url "https://www.iterm2.com/nightly/iTerm2-#{version}-nightly.zip"
   name "iTerm2"
