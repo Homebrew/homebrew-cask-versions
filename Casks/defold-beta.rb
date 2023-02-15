46e831f78c49878401267eb6535e2e8f3689abb5
Hannes Braun <hannesbraun@mail.de>
2023-02-15 17:03:38 +0100

    Update defold-beta from 1.4.2 to 1.4.3 (#15959)

--- Casks/defold-beta.rb
+++ Casks/defold-beta.rb
@@ -1,6 +1,6 @@
 cask "defold-beta" do
-  version "1.4.2,8cd3a634b13f4db51a37607bf32bf3a3b362c8e6"
-  sha256 "50ff954e9d9453384f2edf2945fdd329f98759cbf425b529f8369b5729154048"
+  version "1.4.3,716de595e188a720203c338f947d24d6288be0fc"
+  sha256 "d5db014c36a485fc89b88edfce25292a881bbc6e3d1d18aa6f8368fda674875d"
 
   url "https://d.defold.com/archive/beta/#{version.csv.second}/beta/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
