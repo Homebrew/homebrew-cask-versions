272e3051f6a2323dc172ac31dc5f70edee4062ca
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-17 21:16:39 +0800

    Update dolphin-beta.rb

--- Casks/dolphin-beta.rb
+++ Casks/dolphin-beta.rb
@@ -9,10 +9,11 @@ cask "dolphin-beta" do
   desc "Emulator to play GameCube and Wii games"
   homepage "https://dolphin-emu.org/"
 
-  conflicts_with cask: [
-    "dolphin",
-    "homebrew/cask-versions/dolphin-dev",
+  conflicts_with cask: %w[
+    dolphin
+    homebrew/cask-versions/dolphin-dev
   ]
+  depends_on macos: ">= :catalina"
 
   app "Dolphin.app"
   app "Dolphin Updater.app"
