fd1b4455771f7fccc5bdfccc135a1064e1af80d9
Miccal Matthews <miccal.matthews@gmail.com>
2023-02-21 18:16:39 +0800

    Update royal-tsx-beta from 6.0.0.4 to 6.0.0.5 (#16021)
    * Update royal-tsx-beta from 6.0.0.4 to 6.0.0.5
    
    * Update royal-tsx-beta.rb

--- Casks/royal-tsx-beta.rb
+++ Casks/royal-tsx-beta.rb
@@ -1,6 +1,6 @@
 cask "royal-tsx-beta" do
-  version "6.0.0.4"
-  sha256 "c0ea01920dea6e4369958360edf92fdf49842afa35527645515c334f6092ae19"
+  version "6.0.0.5"
+  sha256 "d88dff37ac2fc25ba17f59f7e4876d8ef3d9b137b7036cb23eb419bbc3638a9a"
 
   url "https://royaltsx-v#{version.major}.royalapps.com/updates/royaltsx_#{version}.dmg"
   name "Royal TSX"
@@ -13,6 +13,7 @@ cask "royal-tsx-beta" do
   end
 
   auto_updates true
+  conflicts_with cask: "royal-tsx"
   depends_on macos: ">= :big_sur"
 
   app "Royal TSX.app"
