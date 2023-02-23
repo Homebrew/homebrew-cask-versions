fc9f2f0fce189c3244477bcd9ae13d8a2867a141
Yurii Kolesnykov <root@yurikoles.com>
2023-02-23 22:27:36 +0200

    Add qbittorrent-lt20 4.5.1
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Casks/qbittorrent-lt20.rb
@@ -0,0 +1,31 @@
+cask "qbittorrent-lt20" do
+  version "4.5.1"
+  sha256 "f9a95119eebab419d4943d623d72c531e11bcc795e35c88d289abb6e85272bec"
+
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}_lt20.dmg",
+      verified: "sourceforge.net/qbittorrent/"
+  name "qBittorrent"
+  desc "Edition of qBitorrent that is based on libtorrent-rasterbar 2.0.x series"
+  homepage "https://www.qbittorrent.org/"
+
+  livecheck do
+    url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
+    strategy :page_match
+    regex(/qbittorrent-(\d+(?:\.\d+)+)_lt20\.dmg/i)
+  end
+
+  conflicts_with cask: "qbittorrent"
+  depends_on macos: ">= :mojave"
+
+  # Renamed for consistency: app name is different in the Finder and in a shell.
+  app "qbittorrent.app", target: "qBittorrent.app"
+
+  zap trash: [
+    "~/.config/qBittorrent",
+    "~/Library/Application Support/qBittorrent",
+    "~/Library/Caches/qBittorrent",
+    "~/Library/Preferences/org.qbittorrent.qBittorrent.plist",
+    "~/Library/Preferences/qBittorrent",
+    "~/Library/Saved Application State/org.qbittorrent.qBittorrent.savedState",
+  ]
+end
