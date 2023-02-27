372fea9321e2e296730b25bbd592ec8b33d09839
Bevan Kay <email@bevankay.me>
2023-02-27 17:56:32 +1100

    Update google-chrome-canary from 112.0.5594.0 to 113.0.5618.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "112.0.5594.0"
+  version "113.0.5618.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
