8dfc2284993809401c7164820131f4d74c6853d0
Friedrich Altheide <11352905+FriedrichAltheide@users.noreply.github.com>
2023-02-28 01:10:37 +0100

    Update monal-beta from 854 to 855 (#16076)

--- Casks/monal-beta.rb
+++ Casks/monal-beta.rb
@@ -1,6 +1,6 @@
 cask "monal-beta" do
-  version "854"
-  sha256 "f186538a18cf5ac730267b1c8f81b4c8f5c4eacc4bb098aaf58d20bd3e29a65b"
+  version "855"
+  sha256 "ccd94c09bb95a77a00dac2a5bf36f5c7f511aa76c90958ce857a460a27d01d21"
 
   url "https://downloads.monal-im.org/monal-im/beta/macOS/Monal-#{version}.zip"
   name "Monal"
