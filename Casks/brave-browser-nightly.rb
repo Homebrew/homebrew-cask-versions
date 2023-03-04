39f0e925b8201152565a59c0f36e0f2446412b2a
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-04 09:24:46 +0800

    Update brave-browser-nightly from 1.51.2.0 to 1.51.5.0 (#16108)

--- Casks/brave-browser-nightly.rb
+++ Casks/brave-browser-nightly.rb
@@ -2,9 +2,9 @@ cask "brave-browser-nightly" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
 
-  version "1.51.2.0,151.2"
-  sha256 arm:   "f7f7d3e2b3559cb0d490f3969ac0d9e1e8c9d517846bac1d64806c444c274ace",
-         intel: "c2d2ff60205db7e502141bbf3c38099aab56f46f5a3b4fb9a0d37f73c6d9b699"
+  version "1.51.5.0,151.5"
+  sha256 arm:   "4c6b7d40166e94a8a7e305853c6d98efba41f0a8b0c0e7d45bf95eb65bb7c257",
+         intel: "896c55e4a15923c9452d9a5771b380ec484d97382e30c937d604187ba9e4af0e"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Nightly-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
