93134c1f75eed97244edf5b9ec07197ea6b4d642
Hannes Braun <hannesbraun@mail.de>
2023-03-05 14:46:39 +0100

    Update defold-alpha from 1.4.3 to 1.4.4 (#16122)

--- Casks/defold-alpha.rb
+++ Casks/defold-alpha.rb
@@ -1,6 +1,6 @@
 cask "defold-alpha" do
-  version "1.4.3,28312323a00e664d10f25f6a57b89ebfa9c09e04"
-  sha256 "e7c43ed9b5eeda4e1bb3a5f40453ac8bb9aaa5ac42c5f2d98243b967b9ea5f0d"
+  version "1.4.4,031a34a523444f79ae884e940698d3da3d73d303"
+  sha256 "8d90016ae77c5c2ab75ef7d284edcfa56b17c7cb81ae0794252df8db95664c72"
 
   url "https://d.defold.com/archive/alpha/#{version.csv.second}/alpha/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
