82a76602bdf226519a6782bb9da5810fae83d73a
Hannes Braun <hannesbraun@mail.de>
2023-03-09 00:59:47 +0100

    Update microsoft-edge-dev from 111.0.1661.27 to 111.0.1661.36 (#16161)

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-dev" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099619", intel: "2069340"
 
-  version "111.0.1661.27"
-  sha256 arm:   "c023e07b3415ee50d747ba69bd3c6e5c6012f7f6cde785e5b4824b9ced28321c",
-         intel: "4d4548027d5875e729c263019199248763bee1ee9d5f1f228daea9bda3dd7777"
+  version "111.0.1661.36"
+  sha256 arm:   "366ddaae232fdbd2d25ff893206828146763f628cb46707d5af6fef4a0c5d995",
+         intel: "4903f91c2b72c2e6046f8401333d1c913525282f5ede98d305d614199855930a"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
