6774ed811bff990977579c13fafcf3423752cee8
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-11 10:19:36 +0800

    Update sketch-beta from 96,167055 to 96,167069 (#16171)

--- Casks/sketch-beta.rb
+++ Casks/sketch-beta.rb
@@ -1,6 +1,6 @@
 cask "sketch-beta" do
-  version "96,167055"
-  sha256 "bbdfcc57434d2362da47ed4bd9424fc0cc57cf1c2b52d0df8d5eb59f361f29a3"
+  version "96,167069"
+  sha256 "5f36141d27071137f15a93eee48b02398f3405edd1d62cb5aa8d3b76a6592c74"
 
   url "https://beta-download.sketch.com/sketch-#{version.csv.first}-#{version.csv.second}.zip"
   name "Sketch"
