6c32761b6e25a819c09422108a0b0a42738080f6
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-13 09:13:03 +0800

    Update telegram-desktop-beta from 4.6.9 to 4.6.10 (#16187)
    * Update telegram-desktop-beta from 4.6.9 to 4.6.10
    
    * Update telegram-desktop-beta.rb
    
    * Update telegram-desktop-beta.rb

--- Casks/telegram-desktop-beta.rb
+++ Casks/telegram-desktop-beta.rb
@@ -1,9 +1,9 @@
 cask "telegram-desktop-beta" do
-  version "4.6.9"
-  sha256 "ba050df04bf2f5371da382a56174e4cb0a94511414f2993099e8b6fe5eeab6d2"
+  version "4.6.10"
+  sha256 "cdcec2acb8e5c9a92007cabb4e4e218e9d9145722cfbe91ee79e54e7f8e6c359"
 
   url "https://updates.tdesktop.com/tmac/tsetup.#{version}.beta.dmg",
-      verified: "updates.tdesktop.com/tmac"
+      verified: "updates.tdesktop.com/tmac/"
   name "Telegram Desktop"
   desc "Desktop client for Telegram messenger"
   homepage "https://desktop.telegram.org/"
@@ -11,7 +11,7 @@ cask "telegram-desktop-beta" do
   livecheck do
     url "https://telegram.org/dl/desktop/mac?beta=1"
     strategy :header_match
-    regex(/tsetup.(\d+(?:\.\d+)+)\.beta.dmg/i)
+    regex(/tsetup[._-]v?(\d+(?:\.\d+)+)[._-]beta\.dmg/i)
   end
 
   auto_updates true
