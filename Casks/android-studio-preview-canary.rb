a90cc4c86e59c1132dcbe6cad888a4a8553c22a3
brewin <brewin@gmail.com>
2023-03-14 14:09:25 -0500

    Update android-studio-preview-canary from 2022.3.1.8 to 2022.3.1.9 (#16199)

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview-canary" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2022.3.1.8"
-  sha256 arm:   "a32c50e7b1aad29cf1ebfef50dfbb1ce5847ca9cf1a45c6a13b9763b76e66293",
-         intel: "3e57fd3d71cf7c1369ccb8eed7e8955fa1413fcae4ffa71813456b7c6fd0dcb3"
+  version "2022.3.1.9"
+  sha256 arm:   "41840889c3bcbb1cc4ffca9e8a6ea5729625d323e7de6d04a451cc978efb80af",
+         intel: "ea2d300010c3fbe22690466c83a5a65829285b1040a466e4f5854cfac025fbda"
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-#{arch}.zip",
       verified: "dl.google.com/dl/android/studio/"
