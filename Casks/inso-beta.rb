6399698043b1e2c3a8929cdfd5796d1f5eefda2a
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-15 07:37:32 +0800

    Update inso-beta from 3.13.0-beta.5 to 3.14.0-beta.0 (#16204)
    * Update inso-beta from 3.13.0-beta.5 to 3.14.0-beta.0
    
    * Update inso-beta.rb
    
    * Update inso-beta.rb
    
    ---------
    
    Co-authored-by: Bevan Kay <bevanjkay@gmail.com>

--- Casks/inso-beta.rb
+++ Casks/inso-beta.rb
@@ -1,6 +1,6 @@
 cask "inso-beta" do
-  version "3.13.0-beta.5"
-  sha256 "532f405668284de3166d443f602f63746544f061bb258c782479588559e42906"
+  version "3.14.0-beta.0"
+  sha256 "305e3dafcc2f1206b1b45fd717be2c88bdcf92c35f7aedeae66a19cab82be3d6"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
@@ -10,11 +10,13 @@ cask "inso-beta" do
 
   livecheck do
     url "https://github.com/Kong/insomnia/releases?q=prerelease%3Atrue+Inso+CLI"
-    regex(%r{href=["']?[^"' >]*?/tag/lib%40([^"' >]+?)["' >]}i)
     strategy :page_match
+    regex(%r{href=["']?[^"' >]*?/tag/lib%40([^"' >]+?)["' >]}i)
   end
 
   conflicts_with cask: "inso"
 
   binary "inso"
+
+  # No zap stanza required
 end
