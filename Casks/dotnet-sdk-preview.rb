57b83f3d6c026fb5cafb92a04a5a5c2c43f2c141
Miccal Matthews <miccal.matthews@gmail.com>
2023-03-15 08:03:44 +0800

    Update dotnet-sdk-preview.rb

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -1,13 +1,13 @@
 cask "dotnet-sdk-preview" do
   arch arm: "arm64", intel: "x64"
 
-  on_intel do
-    version "8.0.100-preview.1.23115.2,993e8460-176f-4669-9337-b4725c355146,f903c2ff758fee42121da67772dfd0c5"
-    sha256 "14c99125c3862ef2fad7de863b1f3a98738ae1220d3e6acaaee579ab8ef514e3"
-  end
   on_arm do
     version "8.0.100-preview.2.23157.25,9bb7054e-4547-4021-b46f-edee2428b10d,1dbe4945aabec0cd9c8b15080ec98b37"
     sha256 "e6ce9caf13bc7770b34e5aec38150db1836b6e3b9e137729a050d5e37c2a7e45"
+  end
+  on_intel do
+    version "8.0.100-preview.1.23115.2,993e8460-176f-4669-9337-b4725c355146,f903c2ff758fee42121da67772dfd0c5"
+    sha256 "14c99125c3862ef2fad7de863b1f3a98738ae1220d3e6acaaee579ab8ef514e3"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv[1]}/#{version.csv[2]}/dotnet-sdk-#{version.csv[0]}-osx-#{arch}.pkg"
