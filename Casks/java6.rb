5e8a527532e57ee0e90f5b51d1a37f68ea3bfdfc
Issy Long <me@issyl0.co.uk>
2023-03-27 22:59:49 +0100

    java6: Fix `Cask/StanzaOrder` offenses for `on_*` blocks

--- Casks/java6.rb
+++ Casks/java6.rb
@@ -2,12 +2,6 @@ cask "java6" do
   version "1.6.0_65-b14-468,2019,041-88384-20191011-3d8da658-dca4-4a5b-b67c-26e686876403"
   sha256 "3a91bd24a0524df4cde9433f2ac56182818f78aacda36c7529b3d548e0c12e63"
 
-  url "https://updates.cdn-apple.com/#{version.csv[1]}/cert/#{version.csv[2]}/JavaForOSX.dmg",
-      verified: "updates.cdn-apple.com/"
-  name "Apple Java 6 Standard Edition Development Kit"
-  desc "Legacy runtime for the Java programming language"
-  homepage "https://support.apple.com/kb/DL1572"
-
   on_mojave :or_older do
     pkg "JavaForOSX.pkg"
 
@@ -21,6 +15,12 @@ cask "java6" do
       system_command "pkgutil", chdir: staged_path, args: ["--expand-full", "JavaForOSX.pkg", "JavaForOSX"]
     end
   end
+
+  url "https://updates.cdn-apple.com/#{version.csv[1]}/cert/#{version.csv[2]}/JavaForOSX.dmg",
+      verified: "updates.cdn-apple.com/"
+  name "Apple Java 6 Standard Edition Development Kit"
+  desc "Legacy runtime for the Java programming language"
+  homepage "https://support.apple.com/kb/DL1572"
 
   caveats do
     discontinued
