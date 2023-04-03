9e981e5ed11ebb0ce278f1fc231f6d42ecde180b
Ivan Kruger <haythamnikolaidis@gmail.com>
2023-04-03 12:40:20 +0200

    Add Godot LTS version (#16339)

---
+++ Casks/godot3.rb
@@ -0,0 +1,28 @@
+cask "godot3" do
+  version "3.5.2"
+  sha256 "2a010b8fbf8241a20224c14ecfac01f4d364d68e14e477459c61ca40716bc0a9"
+
+  url "https://downloads.tuxfamily.org/godotengine/#{version}/Godot_v#{version}-stable_osx.universal.zip",
+      verified: "downloads.tuxfamily.org/godotengine/"
+  name "Godot Engine"
+  desc "Game development engine"
+  homepage "https://godotengine.org/"
+
+  livecheck do
+    url "https://github.com/godotengine/godot"
+    regex(/^v?(3(?:\.\d+)+)[._-]stable$/i)
+  end
+
+  conflicts_with cask: "godot"
+
+  app "Godot.app"
+  binary "#{appdir}/Godot.app/Contents/MacOS/Godot", target: "godot"
+
+  uninstall quit: "org.godotengine.godot"
+
+  zap trash: [
+    "~/Library/Application Support/Godot",
+    "~/Library/Caches/Godot",
+    "~/Library/Saved Application State/org.godotengine.godot.savedState",
+  ]
+end
