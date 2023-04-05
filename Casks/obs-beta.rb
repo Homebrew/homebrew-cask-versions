4276e1afc8afac91685b6aff9fa76e6d0b7f5de4
Bevan Kay <email@bevankay.me>
2023-04-05 22:41:48 +1000

    obs-beta: update livecheck, update url

--- Casks/obs-beta.rb
+++ Casks/obs-beta.rb
@@ -1,25 +1,21 @@
 cask "obs-beta" do
   arch arm: "arm64", intel: "x86_64"
+  livecheck_folder = on_arch_conditional arm: "_arm64"
 
   version "29.1.0-beta2"
   sha256 arm:   "b8dbbfc8a5e3b41eb13c13f0e309575f2a94649e65ee197abef390dae8f1c25a",
          intel: "6d56f6bb0cd0f758e422fc1841c5a6eba11434ecd05cfae593d25c33b29edc1b"
 
-  url "https://github.com/obsproject/obs-studio/releases/download/#{version}/obs-studio-#{version}-macos-#{arch}.dmg",
-      verified: "github.com/obsproject/obs-studio/"
+  url "https://cdn-fastly.obsproject.com/downloads/obs-studio-#{version}-macos-#{arch}.dmg"
   name "OBS"
   desc "Open-source software for live streaming and screen recording"
   homepage "https://obsproject.com/forum/list/test-builds.20/"
 
-  # We use the first matching tag on the releases page, as a version with an
-  # unstable suffix (e.g., `1.2.3-beta1`) would be erroneously treated as
-  # newer than a subsequent stable version (e.g., `1.2.3`) due to how `Version`
-  # comparison works.
   livecheck do
-    url "https://github.com/obsproject/obs-studio/releases"
-    regex(%r{href=["']?[^"' >]*?/tag/v?(\d+(?:\.\d+)+[^"' >]*)["' >]}i)
-    strategy :page_match do |page, regex|
-      page[regex, 1]
+    url "https://obsproject.com/osx_update/updates#{livecheck_folder}_v2.xml"
+    regex(/obs[._-]studio[._-](\d+(?:[.-]\d+)+(?:-beta)\d+)[._-]macos/i)
+    strategy :sparkle do |item|
+      item.url[regex, 1]
     end
   end
 
