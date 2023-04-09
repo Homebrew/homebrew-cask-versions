eff1776f6ada74f3047bfa1d0a6714205dd3b16e
Hannes Braun <hannesbraun@mail.de>
2023-04-09 13:24:09 +0200

    lando-edge 3.15.0 (#16381)

--- Casks/lando-edge.rb
+++ Casks/lando-edge.rb
@@ -1,9 +1,9 @@
 cask "lando-edge" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.14.0"
-  sha256 arm:   "617e47c1e8e11e6cd22cfa7aaacec5b7a26fb8f74598f2e042118e1e91c26d87",
-         intel: "3870855fff35a186ed478eca37c3c095b1a196267cd52a1e34f22a8a79084a7d"
+  version "3.15.0"
+  sha256 arm:   "69712b3a658f7d1a936db77f28e2a86e80b1c59e35713e1b58b8882485a6e189",
+         intel: "a4172d2cf1ef6759009e166763217fcf75b042ee45c5a2f113718550dd8752d9"
 
   url "https://github.com/lando/lando/releases/download/v#{version}/lando-#{arch}-v#{version}.dmg",
       verified: "github.com/lando/lando/"
