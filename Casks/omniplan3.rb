7127f8b7931647ef35e3b10f8e848b09be4e1986
Issy Long <me@issyl0.co.uk>
2023-04-12 18:10:08 +0100

    omniplan3: Autofix `Cask/StanzaGrouping` offenses in `on_*` blocks

--- Casks/omniplan3.rb
+++ Casks/omniplan3.rb
@@ -2,11 +2,13 @@ cask "omniplan3" do
   on_high_sierra :or_older do
     version "3.13"
     sha256 "82e0d7db2626d751f93f97d80dc032e4bc01bba1e05ea52c553e4771c8cfeec5"
+
     url "https://downloads.omnigroup.com/software/MacOSX/10.13/OmniPlan-#{version}.dmg"
   end
   on_mojave :or_newer do
     version "3.14.4"
     sha256 "34367f22766e5e420224e343d3cc2087c808f121f7c3f0fdfbd0e58f2d596e7e"
+
     url "https://downloads.omnigroup.com/software/MacOSX/10.14/OmniPlan-#{version}.dmg"
   end
 
