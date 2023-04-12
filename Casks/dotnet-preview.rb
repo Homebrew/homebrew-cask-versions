8f4fee60ea81cfe2e5206720da4a19c39b6b80ea
Laine Taffin Altman <alexanderaltman@me.com>
2023-04-11 19:46:00 -0700

    dotnet-preview 8.0.0-preview.3.23174.8

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -6,8 +6,8 @@ cask "dotnet-preview" do
     sha256 "3fd3ca03e202aaccec79c71d05f59faa22c0ba57d754e4accea18577c879d4d2"
   end
   on_intel do
-    version "8.0.0-preview.2.23128.3,94271da7-60cd-435c-b273-c03c3b866ac4,9f3612d079ced4b017aec3e917a3fb45"
-    sha256 "861ea948126b58d1b44a4fb96a1dfa32008155f637bf808fccee9d0fa5cd8371"
+    version "8.0.0-preview.3.23174.8,018cf078-39d9-450f-a8fe-d8d84418e220,0b2e9114fa3cb3201f5a8ec39f7c2b2c"
+    sha256 "c4cdf5066398f37b5c17a7e93745f4b28ca2fdd6f66316292cfcc9815e6c157c"
   end
 
   url "https://download.visualstudio.microsoft.com/download/pr/#{version.csv.second}/#{version.csv.third}/dotnet-runtime-#{version.csv.first}-osx-#{arch}.pkg"
