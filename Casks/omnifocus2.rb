73f8a07373ec795e3cef5897f50365f1a1da7481
Issy Long <me@issyl0.co.uk>
2023-04-12 18:09:49 +0100

    omnifocus2: Autofix `Cask/StanzaGrouping` offenses in `on_*` blocks

--- Casks/omnifocus2.rb
+++ Casks/omnifocus2.rb
@@ -2,11 +2,13 @@ cask "omnifocus2" do
   on_el_capitan :or_older do
     version "2.10"
     sha256 "e808a72e60cdff9ff5aa1046d856bf62d6418e4915248816c4640e32e52fd8e8"
+
     url "https://downloads.omnigroup.com/software/MacOSX/10.11/OmniFocus-#{version}.dmg"
   end
   on_sierra :or_newer do
     version "2.12.4"
     sha256 "8a2dc53331dba804f6781773fef546a03c181fc4ff0eb7ee4f871c10342621f0"
+
     url "https://downloads.omnigroup.com/software/MacOSX/10.12/OmniFocus-#{version}.dmg"
   end
 
