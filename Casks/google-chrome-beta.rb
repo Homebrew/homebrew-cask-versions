fde6978f0fed03f10b6e6dba424fbfb0e6a61183
Miccal Matthews <miccal.matthews@gmail.com>
2023-04-15 13:45:06 +0800

    google-chrome-beta 113.0.5672.35 (#16423)

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-beta" do
-  version "113.0.5672.24"
+  version "113.0.5672.35"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg"
