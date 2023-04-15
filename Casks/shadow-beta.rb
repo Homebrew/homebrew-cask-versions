8b26288da4a34e8202487fd443ee77fa8aa62f23
Alexander Lauster <alexander@macwinlin.de>
2023-04-15 12:08:11 +0200

    Fix Shadow Beta Cask to use the new App name

--- Casks/shadow-beta.rb
+++ Casks/shadow-beta.rb
@@ -1,12 +1,12 @@
 cask "shadow-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.10041"
-  sha256 arm:   "309af3518a51aa3539e574e7fcdc360d629c0b32bfb05d688a8e9fffebf5526b",
-         intel: "5cd4c2fb53a3153b381821972eb65aabab7dcbe54719007d5747bfe675c2a17a"
+  version "8.0.10084"
+  sha256 arm:   "8124db9ca5a9c2aa9acfc4b9cb88390f1771bd1d262bf470b9656c77e2b3fec8",
+         intel: "2807a81c349a61ee42655ad832c5a016205b23ec5d10e6508501c409724b7620"
 
-  url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowBeta-#{version}.dmg"
-  name "Shadow Beta"
+  url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowPCBeta-#{version}.dmg"
+  name "Shadow PC Beta"
   desc "Online virtualized computer"
   homepage "https://shadow.tech/"
 
