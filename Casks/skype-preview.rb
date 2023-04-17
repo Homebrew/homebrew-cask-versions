8c20fe9035447656e6a422c0abf683b2f76a18f5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-17 18:02:14 -0400

    skype-preview 8.96.76.407 (#16447)
    * skype-preview 8.96.76.407
    
    * add zap

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask "skype-preview" do
-  version "8.96.76.300"
-  sha256 "efd1105b1db2918539072cba48ba0435c2536e7262d40e1a0e9b6b71062ffa57"
+  version "8.96.76.407"
+  sha256 "1234074f185a7eefd15dede36e5214271dbf95a68e8562f3c398e4e6e02abf8f"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
@@ -17,4 +17,12 @@ cask "skype-preview" do
   conflicts_with cask: "skype"
 
   app "Skype.app"
+
+  zap trash: [
+    "~/Library/Application Support/Microsoft/Skype for Desktop",
+    "~/Library/Caches/com.skype.s4l",
+    "~/Library/Caches/com.skype.s4l.ShipIt",
+    "~/Library/HTTPStorages/com.skype.s4l",
+    "~/Library/Preferences/com.skype.s4l.plist",
+  ]
 end
