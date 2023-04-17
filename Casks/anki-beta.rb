86ecbcfe60882ab1a21b80836b1ad4791a85c8d7
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-17 18:03:05 -0400

    anki-beta 2.1.62+beta1_bf05fae9 (#16430)

--- Casks/anki-beta.rb
+++ Casks/anki-beta.rb
@@ -1,9 +1,9 @@
 cask "anki-beta" do
   arch arm: "apple", intel: "intel"
 
-  version "2.1.61+beta2_d0ab7acb"
-  sha256 arm:   "fd1b6144fbd59afdccd25d8e2daf5e7c811fb206cbb3d5c7ce3f243bd5dad86c",
-         intel: "8c43bb394733a1370006ebae4d299e4fc8e113cca446a432e13a1ada3ea95703"
+  version "2.1.62+beta1_bf05fae9"
+  sha256 arm:   "a3fcd1a031917e715c80b33a1504ee0fd1f30567637b646cfec1e234123efba6",
+         intel: "f0d6b51269d8c0e277901ba71aa5486cd1bba5bc2ab2cd2c261d888cd6bf4767"
 
   url "https://apps.ankiweb.net/downloads/beta/anki-#{version}-mac-#{arch}-qt6.dmg"
   name "Anki Beta"
