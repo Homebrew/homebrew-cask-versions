a6260d9a3e44bf8a36660946646f3708092a7343
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-04-19 18:18:23 -0400

    brave-browser-nightly 1.52.54.0

--- Casks/brave-browser-nightly.rb
+++ Casks/brave-browser-nightly.rb
@@ -2,9 +2,9 @@ cask "brave-browser-nightly" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "nightly-arm64", intel: "nightly"
 
-  version "1.52.42.0,152.42"
-  sha256 arm:   "dbd0a528255b174cdb5436f53c3e19db464910f74a27658f6c3c6da060bbafee",
-         intel: "d60e1d81a8fd90c264334449b17462d9fcbcd422ea23c196495054b57a52a8bb"
+  version "1.52.54.0,152.54"
+  sha256 arm:   "24a3968099eadc81af07f3fdf8f983a51d8a438c48e5e27c74bb3e5db5f36dcd",
+         intel: "f84b1e2cffed4a2fc68fd78b2a4341703284570e78d68162a9328faa3be38853"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.csv.second}/Brave-Browser-Nightly-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
