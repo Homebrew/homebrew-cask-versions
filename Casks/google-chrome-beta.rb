54587e6f18c15c1fd2d7a1ba52e15ecda9a1f7ee
owine <owine@users.noreply.github.com>
2023-04-20 09:42:31 -0500

    google-chrome-beta 113.0.5672.53

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-beta" do
-  version "113.0.5672.35"
+  version "113.0.5672.53"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/beta/googlechromebeta.dmg"
