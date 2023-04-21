d2824dfd49f501c4a187de1a96ea6483cde9b648
Andrew Leonard <31470007+andrew-m-leonard@users.noreply.github.com>
2023-04-21 10:46:27 +0100

    Update temurin17.rb cask (#16489)
    * Update temurin17.rb cask
    
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>
    
    * Update temurin17.rb
    
    ---------
    
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>
    Co-authored-by: George Adams <georgeadams1995@gmail.com>

--- Casks/temurin17.rb
+++ Casks/temurin17.rb
@@ -1,9 +1,9 @@
 cask "temurin17" do
   arch arm: "aarch64", intel: "x64"
 
-  version "17.0.6,10"
-  sha256 arm:   "3fa82e94761d19b25a15bf823d97f585a4d3ee8d7bb835244727ec5db6fb5fe9",
-         intel: "5a04f6d7e643bdb232a6efa178188df8e1efd3c4a0476b32772beb6a4c4649f5"
+  version "17.0.7,7"
+  sha256 arm:   "9605fd0146fa0498435697c6e08b7d66a5ac937768131b0b55dd182f096b3ebe",
+         intel: "52d5f3fd46dc75c2b1078edb04e2c91d706287a616e6949bde28c3f1676d2551"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
