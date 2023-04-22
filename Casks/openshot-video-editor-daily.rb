a1b8949a7ed55a5273d2b6a0f6faa8086985c33c
Hannes Braun <hannes@hannesbraun.net>
2023-04-22 06:43:02 +0200

    openshot-video-editor-daily 3.1.1 (#16501)

--- Casks/openshot-video-editor-daily.rb
+++ Casks/openshot-video-editor-daily.rb
@@ -1,6 +1,6 @@
 cask "openshot-video-editor-daily" do
-  version "3.1.0,11349-96650934-0884bd96"
-  sha256 "d134c5283089247d40f914b4e74fc5ac763c286d72007c23932b17cca6bc486d"
+  version "3.1.1,11406-8ab0cb4c-75076545"
+  sha256 "faf4702aa09722ffc097be3c708ba404393fc7b1aff56801ccc596b235e9705d"
 
   url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-daily-#{version.csv.second}-x86_64.dmg",
       verified: "github.com/OpenShot/openshot-qt/"
