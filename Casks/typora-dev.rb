38481efab86393a5990695889a34a12525a62259
Hannes Braun <hannes@hannesbraun.net>
2023-04-28 14:19:52 +0100

    typora-dev 1.6.1 (#16557)

--- Casks/typora-dev.rb
+++ Casks/typora-dev.rb
@@ -1,6 +1,6 @@
 cask "typora-dev" do
-  version "1.6.0"
-  sha256 "ba1eb27bbc0768a7fe1cdf44ac915cfd5b6c462b83cfaa42613222122d1b86ba"
+  version "1.6.1"
+  sha256 "112d31ddef666e30f0aa2a6710d9a068d2c1e54211e7f4fd2242f831a976282a"
 
   language "zh-Hans-CN" do # use official Chinese mirror
     url "https://download2.typoraio.cn/mac/Typora-#{version}-dev.dmg",
