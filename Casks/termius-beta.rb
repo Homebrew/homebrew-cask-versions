f65e2bc59c71f6f3308bf4705ab55c1c09e4070b
Hannes Braun <hannes@hannesbraun.net>
2023-04-28 14:00:55 +0100

    termius-beta 7.58.6

--- Casks/termius-beta.rb
+++ Casks/termius-beta.rb
@@ -1,7 +1,7 @@
 cask "termius-beta" do
   arch arm: "mac-beta-arm64", intel: "mac-beta"
 
-  version "7.58.5"
+  version "7.58.6"
   sha256 :no_check
 
   url "https://autoupdate.termius.com/#{arch}/Termius%20Beta.dmg"
