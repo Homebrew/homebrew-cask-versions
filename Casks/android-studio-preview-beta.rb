f230b5ca55b8e529fcbbe0c0c96fadaaa537b5c1
Braden Ewing <brewin@gmail.com>
2023-05-08 11:29:04 -0500

    android-studio-preview-beta 2022.3.1.13

--- Casks/android-studio-preview-beta.rb
+++ Casks/android-studio-preview-beta.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview-beta" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2022.3.1.12"
-  sha256 arm:   "a89e9a0e7c915ccfd3254e9f7c3f86fd6c94232501b5dee536de9fa191abcb98",
-         intel: "09f3a5b1204cff143d985f6fdab82d83e957428c31c9fac3d698b2ecf655c1bf"
+  version "2022.3.1.13"
+  sha256 arm:   "5a0b5ae11533b5259b22b43b494b6c315a97ccb9f8c15bcb4632c98a172fb449",
+         intel: "be6a1e6ea06e89a9d06440c206807876e0acef297e5ff2e5d7a33c55c89b0097"
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-#{arch}.zip",
       verified: "dl.google.com/dl/android/studio/"
