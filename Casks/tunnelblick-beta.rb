53a642eca1a1ef701220e68dc5ca09d6083ce78c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-05-09 17:27:24 -0400

    tunnelblick-beta 4.0.0beta05

--- Casks/tunnelblick-beta.rb
+++ Casks/tunnelblick-beta.rb
@@ -1,6 +1,6 @@
 cask "tunnelblick-beta" do
-  version "4.0.0beta04,5840"
-  sha256 "b88aab6adfb5eba478c08bf20a30fe661d511c7741dbeac133c4ed93c63ae3cc"
+  version "4.0.0beta05,5850"
+  sha256 "026a6800c911208d5233b4fc28cd185f7dec95f7366801aa5c46e1a29351efb7"
 
   url "https://github.com/Tunnelblick/Tunnelblick/releases/download/v#{version.csv.first}/Tunnelblick_#{version.csv.first}_build_#{version.csv.second}.dmg",
       verified: "github.com/Tunnelblick/Tunnelblick/"
