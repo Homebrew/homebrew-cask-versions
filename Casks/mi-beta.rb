7aebed97c4b49bbdf49796782e719160a73e5bb6
miccal <miccal.matthews@gmail.com>
2023-05-09 09:17:51 +0800

    mi-beta: update livecheck

--- Casks/mi-beta.rb
+++ Casks/mi-beta.rb
@@ -1,15 +1,15 @@
 cask "mi-beta" do
-  version "3.7.5,1250"
+  version "3.7.5"
   sha256 "12085323b38e9facffad676a0080344831cee58f88c602468e5a15ea647ec54b"
 
-  url "https://www.mimikaki.net/download/mi#{version.csv.first}.dmg"
+  url "https://www.mimikaki.net/download/mi#{version}.dmg"
   name "mi"
   desc "Text editor"
   homepage "https://www.mimikaki.net/download/beta.html"
 
   livecheck do
     url "https://www.mimikaki.net/download/appcast_beta.xml"
-    strategy :sparkle
+    strategy :sparkle, &:short_version
   end
 
   conflicts_with cask: "mi"
