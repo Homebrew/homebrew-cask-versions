f0e1465af5b31aa1bbd78c01c82cb27aa5b2e57f
fwcd <fwcdmail@gmail.com>
2023-05-14 15:15:14 +0200

    Add Mixxx snapshot cask

---
+++ Casks/mixxx-snapshot.rb
@@ -0,0 +1,27 @@
+cask "mixxx-snapshot" do
+  arch arm: "arm", intel: "intel"
+
+  version "2.5-alpha-33-g3e500b1abc"
+  sha256 arm:   "6b67cf6c8559189b1ccfad7e8b011a23a470334d831deb7f497a300798399c45",
+         intel: "d0c0a4405961ec2f4cb256b0221d05380ebb828d3879226adcf572cece302988"
+
+  url "https://downloads.mixxx.org/snapshots/main/mixxx-#{version}-macos#{arch}.dmg"
+  name "Mixxx"
+  desc "Open-source DJ software"
+  homepage "https://www.mixxx.org/"
+
+  livecheck do
+    url "https://www.mixxx.org/download/"
+    regex(%r{href=["']?https://downloads\.mixxx\.org/snapshots/main/mixxx[._-]v?(\S+)[._-]macos(?:intel|arm)\.dmg}i)
+  end
+
+  conflicts_with cask: "mixxx"
+
+  app "Mixxx.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/org.mixxx.mixxx",
+    "~/Library/Containers/org.mixxx.mixxx",
+    "~/Music/Mixxx",
+  ]
+end
