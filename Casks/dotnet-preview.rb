c615466d2a2eedc63c057a3fd53eedb4d363b054
Laine Taffin Altman <alexanderaltman@me.com>
2023-05-17 20:49:11 -0700

    Update dotnet-preview.rb

--- Casks/dotnet-preview.rb
+++ Casks/dotnet-preview.rb
@@ -2,8 +2,8 @@ cask "dotnet-preview" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "8.0.0-preview.3.23174.8,4d50ef46-86ee-4be2-bcf4-1d27ab4f2a21,29a3ab71de1327a7976e475290e11ca7"
-    sha256 "c5d2b3098b111701ca8ead536abc29ec712d87076bbc00d43cf945e9c06b2758"
+    version "8.0.0-preview.4.23259.5,860cf807-c96d-406f-9122-a2d0fc1d23b7,3ed6d45cf281601671305dd8160e09bd"
+    sha256 "fb2b55a90e4d63f754f99cbe5dd49b7a691af85f0f24b06fd710d967741b7e63"
   end
   on_intel do
     version "8.0.0-preview.4.23259.5,b9f27a14-46ea-4213-ad1b-f31bb41d3ff3,f9512906f1127157f23bfe0d583fb347"
