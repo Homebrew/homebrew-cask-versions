9e37844d5e239eeb3cfbf08fe02c4edfe7455bec
owine <owine@users.noreply.github.com>
2023-05-17 19:29:04 -0500

    safari-technology-preview 170 (#16700)
    * safari-technology-preview 170
    
    * safari-technology-preview: Update for Monterey

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,11 +1,11 @@
 cask "safari-technology-preview" do
   on_monterey :or_older do
-    version "169,20230502-032-79612-68FAF52A-4A10-43AE-B3A0-4A843724DA93"
-    sha256 "0d18964855ccf7e1f5592d744e280535f1a46ef2355b466a8c7a05ec88bf19ed"
+    version "170,032-84621-20230516-CD5492F5-FD9F-4BBA-86ED-FCBA6E49C899"
+    sha256 "6882c53ba8f1698eeef2662309a9b870633661ef42b08d1f4d1a9312ac32a0b9"
   end
   on_ventura :or_newer do
-    version "169,20230502-032-79610-7D8A4C32-B30C-4172-8FDE-B62ECFC61360"
-    sha256 "346b9f616faa3713e0fe6efcf5cefa85f269bca4f7946d54f30f6adf4ee0cd4d"
+    version "170,032-84619-20230516-56AB9599-8629-4B30-9F11-428A02F4279B"
+    sha256 "7a787d46833d9de98da6eedafbd2cfbf1e6882b92957fc8518a9bb4ac193e612"
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.csv.second}/SafariTechnologyPreview.dmg"
