10d76948e034f6a34dc3d4332b30654c5499b034
Laine Taffin Altman <alexanderaltman@me.com>
2023-05-17 20:54:55 -0700

    Update dotnet-sdk-preview.rb

--- Casks/dotnet-sdk-preview.rb
+++ Casks/dotnet-sdk-preview.rb
@@ -2,8 +2,8 @@ cask "dotnet-sdk-preview" do
   arch arm: "arm64", intel: "x64"
 
   on_arm do
-    version "8.0.100-preview.3.23178.7,3fa4c32e-6c76-4055-b6ea-e9dee2785da9,117d47e8b1cbbaa762390b30329e935f"
-    sha256 "aab199305069727ed7a3c13033d26a04249e41eb98a909d60caeaa0cda6d32fa"
+    version "8.0.100-preview.4.23260.5,593bd437-c6f3-412c-9cb0-206addab56d3,330ac8f331f7c9b9e8860669c767cb43"
+    sha256 "1422e73da826f1d06ca5125f3c9d697dbf4ab5dd74e14083ce2f528be1cd5898"
   end
   on_intel do
     version "8.0.100-preview.4.23260.5,c06279c7-f65e-4da0-bca8-5ae18a652e75,f645c5e53f6d3d51120b1e62cec2cc78"
