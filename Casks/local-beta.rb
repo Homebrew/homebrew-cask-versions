20c967b203f2511e09515a090bc419bda4fd8a0f
Andy Fragen <andy@thefragens.com>
2023-05-19 16:35:03 -0700

    update local-beta 6.7.2 (#16694)
    * update local-beta 6.7.2
    
    * Update local-beta.rb
    
    ---------
    
    Co-authored-by: Razvan Azamfirei <azamfireirazvan@gmail.com>

--- Casks/local-beta.rb
+++ Casks/local-beta.rb
@@ -1,19 +1,19 @@
 cask "local-beta" do
-  arch arm: "mac-arm64", intel: "mac"
+  arch arm: "-arm64"
 
-  version "6.7.1,6365"
-  sha256  arm:   "ea5f09f136fee8a8f59d0a24f73ac79d43a499337de514afa451b4c042f4c4c4",
-          intel: "992becccebb4d8316ca5e6bed68aff7fbe1e83d0f3afb79ee12293d3c781fea4"
+  version "6.7.2,6374"
+  sha256  arm:   "28d8bdeb257ac4734b42c061e91589146903ee5be37bc7efc10f319cdde07f4b",
+          intel: "18d46b3ee9e6a6a87dee542c6cc7621cc66f80a3f13cca0b715d04276a4b4862"
 
-  url "https://cdn.localwp.com/releases-beta/#{version.csv.first}+local-beta-#{version.csv.second}/local-beta-#{version.csv.first}-b#{version.csv.second}-#{arch}.dmg"
+  url "https://cdn.localwp.com/releases-beta/#{version.csv.first}+local-beta-#{version.csv.second}/local-beta-#{version.csv.first}-b#{version.csv.second}-mac#{arch}.dmg"
   name "Local Beta"
   desc "WordPress local development tool by Flywheel (beta)"
   homepage "https://localwp.com/"
 
   livecheck do
-    url "https://cdn.localwp.com/beta/latest/#{arch}"
+    url "https://cdn.localwp.com/beta/latest/mac#{arch}"
     regex(%r{/(\d+(?:\.\d+)+)\+local-beta-(\d+)/}i)
-    strategy :header_match do |headers, regex|
+    strategy :header_match do |headers|
       match = headers["location"]&.match(regex)
       next if match.blank?
 
