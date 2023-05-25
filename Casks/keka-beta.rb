3205a9941b8c55aa2c0f81265baed9310a880e2c
Hannes Braun <hannes@hannesbraun.net>
2023-05-25 16:12:20 +0200

    keka-beta 1.2.62-beta.2

--- Casks/keka-beta.rb
+++ Casks/keka-beta.rb
@@ -1,6 +1,6 @@
 cask "keka-beta" do
-  version "1.2.62-beta.1"
-  sha256 "1acd1c63ccf6af2ac7de54acc7e95850605e185dd25b9dcee0a41b37553e9bb1"
+  version "1.2.62-beta.2"
+  sha256 "fe351b39ac0b1ed6d4c79bd6beb97b9d9698202fba3bdcd2a75cb3c4c6f5eb31"
 
   url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg",
       verified: "github.com/aonez/Keka/"
