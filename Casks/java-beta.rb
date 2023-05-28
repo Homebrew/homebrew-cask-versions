087e8df421150c79fee9c438f6f6b4e935744520
Patrick Linnane <patrick@linnane.io>
2023-05-27 20:24:08 -0700

    java-beta 21,24 (#16774)

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,9 +1,9 @@
 cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "21,23"
-  sha256 arm:   "9abb45acd93b36c639c7c33762fd6a0eaf3aefdf1495094ff2ee87f111d95a65",
-         intel: "f1502c12abdfe897e35123ee44225936415d20116f0537312f915a8065812745"
+  version "21,24"
+  sha256 arm:   "3d9eee88bf2deee3ee1ea127f47b19ffcc2c01fc1bc4b2a83de3f340b71f5626",
+         intel: "b986bd5f08b25c08aaa44bed806035412084c82b483f384f7a96ccb3551944fe"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
