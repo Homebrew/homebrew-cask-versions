2170251ea734534d884d1deffe0a63c5558fa0bd
Patrick Linnane <patrick@linnane.io>
2023-05-27 20:23:44 -0700

    semeru-jdk17-open 17.0.7+7 (#16783)

--- Casks/semeru-jdk17-open.rb
+++ Casks/semeru-jdk17-open.rb
@@ -1,9 +1,9 @@
 cask "semeru-jdk17-open" do
   arch arm: "aarch64", intel: "x64"
 
-  version "17.0.6+10,openj9-0.36.0"
-  sha256 arm:   "f8fa1849f1b8128b40893eddd7f154ed5efcbad16ae31afaf8e66e5d27e0c047",
-         intel: "4f22cd8f2a75c052f6febeee617902c83d74e6ddf0c8f1383585253f790bbfe9"
+  version "17.0.7+7,openj9-0.38.0"
+  sha256 arm:   "8b8fdda01676b500853c9310a0769e5c2d94e4a64b91c924d2dff217072d0f54",
+         intel: "5e4e6178b3b32ff599ae07d2f73f803a07b9db79d97efa2cc66555f930d6f451"
 
   url "https://github.com/ibmruntimes/semeru#{version.major}-binaries/releases/download/jdk-#{version.csv.first}_#{version.csv.second}/ibm-semeru-open-jdk_#{arch}_mac_#{version.csv.first.tr("+", "_")}_#{version.csv.second}.pkg",
       verified: "github.com/ibmruntimes/semeru#{version.major}-binaries/"
