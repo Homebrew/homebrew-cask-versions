071b9d2d743f3b552d30ac3ac932169cf70f9080
Patrick Linnane <patrick@linnane.io>
2023-05-27 20:23:42 -0700

    shadow-beta 8.0.10117 (#16784)

--- Casks/shadow-beta.rb
+++ Casks/shadow-beta.rb
@@ -1,9 +1,9 @@
 cask "shadow-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.10100"
-  sha256 arm:   "bada3126b554f9eeeceae94b56cc233f2b18f0b942fe35583751054c4281f6d3",
-         intel: "c63a691f058c9e53d5baa8e1d35fea1ded883610a7414bb7a673ad04675a2e24"
+  version "8.0.10117"
+  sha256 arm:   "54579fc574c42ba408e6f27a5ce0cefe2eda4163951edadda00074db6b4e5f38",
+         intel: "02901d0c1b509327cd2c22ca3c82859dba57876203fb28a0b72e87451d81c8c5"
 
   url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowPCBeta-#{version}.dmg"
   name "Shadow PC Beta"
