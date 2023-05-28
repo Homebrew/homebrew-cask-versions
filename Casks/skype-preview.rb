113a68d1f93eeacf8a4b24be2f09a73dd858b152
Patrick Linnane <patrick@linnane.io>
2023-05-27 20:23:34 -0700

    skype-preview 8.98.76.205 (#16787)

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask "skype-preview" do
-  version "8.98.76.103"
-  sha256 "d762eb9015224eafd9d521cd56a757543de8594192ba9da97cec5e387fd354da"
+  version "8.98.76.205"
+  sha256 "0e8e9d292211a369495805f306223634f09bc00e9376b12a45c571acf1cc8327"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
