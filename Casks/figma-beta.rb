21c0ad42465f95a00aba02a643ec9f0c168fe4ff
Patrick Linnane <patrick@linnane.io>
2023-05-27 20:24:16 -0700

    figma-beta 116.10.4 (#16771)

--- Casks/figma-beta.rb
+++ Casks/figma-beta.rb
@@ -1,9 +1,9 @@
 cask "figma-beta" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "116.10.2"
-  sha256 arm:   "4df5a1cf2abfa56e2a2442982f31d043b63b02cbea6140ff8bc7f9a6b4ce695e",
-         intel: "27895df30889ab55ed8845a0d99123cf342424114d90e677edf624dd284b57a8"
+  version "116.10.4"
+  sha256 arm:   "17d5a75debc17ecac05dd638e659bc3a61abe26a5bb1d350226ecf80740b685b",
+         intel: "9c05229cdb1456a10c6274760ddc6eb8ee05243429421275d150e99b8af9e04e"
 
   url "https://desktop.figma.com/#{arch}/beta/FigmaBeta-#{version}.zip"
   name "Figma Beta"
