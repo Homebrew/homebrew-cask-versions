91e0ca81a7fd1d53a55da4afc258bb238e7e5ff4
Miccal Matthews <miccal.matthews@gmail.com>
2023-06-02 05:32:52 +0800

    Update sonos-s1-controller.rb

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,15 +1,20 @@
 cask "sonos-s1-controller" do
-  version "57.16-41110"
+  version "57.16-41110,wzhipjet"
   sha256 "c1cd7a6e2573e0d046517336f02404befc505567f4ed22f388a0026ecae11b68"
 
-  url "https://update-software.sonos.com/software/wzhipjet/Sonos_#{version}.dmg"
+  url "https://update-software.sonos.com/software/#{version.csv.second}/Sonos_#{version.csv.first}.dmg"
   name "Sonos S1"
   desc "Controller for Gen 1 Sonos products"
   homepage "https://www.sonos.com/"
 
   livecheck do
     url "https://www.sonos.com/en/redir/controller_software_mac"
-    strategy :header_match
+    strategy :header_match do |headers|
+      match = headers["location"].match(%r{software/(\w+)/Sonos[._-]v?(\d+(?:.\d+)+)\.dmg}i)
+      next if match.blank?
+
+      "#{match[2]},#{match[1]}"
+    end
   end
 
   auto_updates true
