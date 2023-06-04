bb0da9d7448f5ce628ec4468da0912eb53eec290
Edward <73746306+WangEdward@users.noreply.github.com>
2023-06-04 14:52:21 +0800

    Add xmind23
    https://github.com/Homebrew/homebrew-cask/pull/148435#issuecomment-1575415219

---
+++ Casks/xmind23.rb
@@ -0,0 +1,21 @@
+cask "xmind" do
+  version "23.05.3170-202305291822"
+  sha256 "290849b2385281ba9905775bc85353309a4625256e7080546a1f8f7e7af347de"
+
+  url "https://dl3.xmind.net/Xmind-for-macOS-#{version}.dmg"
+  name "XMind"
+  desc "Mind mapping and brainstorming tool"
+  homepage "https://www.xmind.net/"
+
+  conflicts_with cask: [
+    "homebrew/cask/xmind",
+    "xmind8"
+  ]
+
+  app "Xmind.app"
+
+  zap trash: [
+    "~/Library/Saved Application State/org.xmind.cathy.application.savedState",
+    "~/Library/XMind",
+  ]
+end
