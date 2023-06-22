4dea3c9635d0a097bf547d9baae2be09c014dbf5
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-06-22 19:58:00 -0400

    skype-preview 8.99.76.300

--- Casks/skype-preview.rb
+++ Casks/skype-preview.rb
@@ -1,6 +1,6 @@
 cask "skype-preview" do
-  version "8.99.76.102"
-  sha256 "3988d1956e80e87a90ce94dc33909bdad4527b14a1cf0c3bf34aff93e9d27275"
+  version "8.99.76.300"
+  sha256 "d67908378ecfb129ae9e78c7a1400676fbfc2a0a4ba8cc1a6e7c5082d788c3f8"
 
   url "https://endpoint920510.azureedge.net/s4l/s4l/download/mac/Skype-#{version}.dmg",
       verified: "endpoint920510.azureedge.net/s4l/s4l/download/mac/"
