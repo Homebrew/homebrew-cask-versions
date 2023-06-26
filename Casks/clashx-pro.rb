07da73d0df76cf438ab916ef764ca6da1148a8d2
mrchi <i@mrchi.cc>
2023-06-26 10:51:33 +0800

    clashx-pro 1.116.3.1

--- Casks/clashx-pro.rb
+++ Casks/clashx-pro.rb
@@ -1,6 +1,6 @@
 cask "clashx-pro" do
-  version "1.116.2.1"
-  sha256 "54211f52fec0ea761f865e04d7d82582162108e40c0d32fd0161c844b2e5796b"
+  version "1.116.3.1"
+  sha256 "8d5e42e4711c133820f7edb323a7e24571cddd587dc207197f20ce366101b4b3"
 
   url "https://appcenter.clashx.workers.dev/api/1cd052f7-e118-4d13-87fb-35176f9702c1/#{version}",
       verified: "appcenter.clashx.workers.dev/api/1cd052f7-e118-4d13-87fb-35176f9702c1/"
