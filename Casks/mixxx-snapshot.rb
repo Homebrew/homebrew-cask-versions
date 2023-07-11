ac9d3f3558f41479517afe92037f75da6440450b
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-11 16:19:01 -0400

    mixxx-snapshot 2.5-alpha-63-g3de29630a8

--- Casks/mixxx-snapshot.rb
+++ Casks/mixxx-snapshot.rb
@@ -1,9 +1,9 @@
 cask "mixxx-snapshot" do
   arch arm: "arm", intel: "intel"
 
-  version "2.5-alpha-54-g5879cd844d"
-  sha256 arm:   "f3600396d316ad47b57ab5d5e0f41e9352dfaf71a89d223f37a400e9427c9033",
-         intel: "a874eb3179c52e20051ffa429b035a6d1aa8021d28483bd8ef55866f6180c81b"
+  version "2.5-alpha-63-g3de29630a8"
+  sha256 arm:   "a3c9fb509c3587982752c4c5fc0691e800f1ea8e66e15f7625e4477e72c957b9",
+         intel: "8f87cfb80ebeb1e58302e782a47106d4dda32dfce23c499caaea047e82b2fe84"
 
   url "https://downloads.mixxx.org/snapshots/main/mixxx-#{version}-macos#{arch}.dmg"
   name "Mixxx"
