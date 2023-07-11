b3ce6bc47188d3c4bab13309efe1954dec2e23e6
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-11 15:57:03 -0400

    figma-beta 116.12.0

--- Casks/figma-beta.rb
+++ Casks/figma-beta.rb
@@ -1,9 +1,9 @@
 cask "figma-beta" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "116.11.1"
-  sha256 arm:   "c07eb401a47699a112a72dfd4342609c31ab0306e4f5b9a67ff41643d4f5e0da",
-         intel: "28f458ca90b34ca548e2823a86928a864a02a1a13c6960e9d270b231d70093fd"
+  version "116.12.0"
+  sha256 arm:   "b26ed6bd41d2ecd3788c98895f30f2ab70267b3396834fefeace8bc76a554600",
+         intel: "8cc80769d2ca4454d7b53bb223abe83b960b52c6af04a95c1ade379029b2cc2b"
 
   url "https://desktop.figma.com/#{arch}/beta/FigmaBeta-#{version}.zip"
   name "Figma Beta"
