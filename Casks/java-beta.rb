5b622671361143441583773e1532a08f5c18c1ba
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-07-16 14:43:24 -0400

    java-beta 21,31

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,9 +1,9 @@
 cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "21,30"
-  sha256 arm:   "f36687a234cac461c9b391c2029ae3577ff2827bd5a021402f3982301927f8a1",
-         intel: "6ecb1b615f51d1305841989d7ecd9b28df2ecee8ac36ea7b7ba371b5fa42d581"
+  version "21,31"
+  sha256 arm:   "88cff610124ebb08c9e88bfbba0d8b05328f04a473b1f141f63b5df1d3d95aa3",
+         intel: "0b04011e71957ce5d2ce456400644386c63dfc8eb7c4f323d740f3097e49e1f4"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
