d001ca48ddc988d6265fd92c3601fc138dbf0e21
Hannes Braun <hannes@hannesbraun.net>
2023-07-22 11:38:49 +0200

    vscodium-insiders 1.81.0.23203-insider

--- Casks/vscodium-insiders.rb
+++ Casks/vscodium-insiders.rb
@@ -1,9 +1,9 @@
 cask "vscodium-insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.80.0.23165-insider"
-  sha256 arm:   "16e35e8145895c57b27b0802c856dbdfcd1e471c6d12729e20b43eac6b0626af",
-         intel: "7f69ce8a4497601e4a2734e824d3a6a8c6b26a5ba0e9f126bbe073f04889a6d3"
+  version "1.81.0.23203-insider"
+  sha256 arm:   "cbd94580f44ea9aafbb617b7fbdaf381c8ced951ac567ec93183542d6544b3ed",
+         intel: "6ae13f456d677f7b3ca0f4b8f0b82deaaed002761d379edf27d5b6a839e2581e"
 
   url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg",
       verified: "github.com/VSCodium/vscodium-insiders/"
