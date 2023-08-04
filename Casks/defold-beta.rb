2488c324f3ca20438b3d090c2219a76b1873fdae
Hannes Braun <hannes@hannesbraun.net>
2023-08-04 14:54:33 +0200

    defold-beta 1.5.0

--- Casks/defold-beta.rb
+++ Casks/defold-beta.rb
@@ -1,6 +1,6 @@
 cask "defold-beta" do
-  version "1.4.8,ff7a3f987df5081d2680485ab90f2c7cd6c62f93"
-  sha256 "f19eafe02389f48c6bfb80053c483283cf33160c32f231313a44c67116eafd6b"
+  version "1.5.0,0e99832847333eb5290fd692e13b93a34c7d3373"
+  sha256 "0e33563692d4b23fb56b9db86a7b56db542d503ce14f054310c166cbe7a404af"
 
   url "https://d.defold.com/archive/beta/#{version.csv.second}/beta/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
