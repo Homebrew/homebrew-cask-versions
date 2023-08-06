3c359c395106b102b51c92d570955a1582ea0615
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-06 18:20:45 -0400

    google-chrome-canary 117.0.5933.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "117.0.5931.0"
+  version "117.0.5933.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
