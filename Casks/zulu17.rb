8bfec1299ff9875c8254e4b819ee1b4cc537f82d
Bevan Kay <email@bevankay.me>
2023-08-07 10:18:01 +1000

    zulu17 17.0.8,17.44.15_1-ca

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -2,9 +2,9 @@ cask "zulu17" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "17.0.8,17.44.15-ca"
-  sha256 arm:   "b939350c5901fd8027356d03ecd2614526a1f682a4cf030ab440aa50b3165ef5",
-         intel: "d8e2e67b7a814062fb34309a6dc2b636f8d062714451ea76ccb56639d46fc1f6"
+  version "17.0.8,17.44.15_1-ca"
+  sha256 arm:   "35d5bf045579a08110b91a8e37cda859cda021b1ef7dc4ef8a97b79a14147d60",
+         intel: "ca746b9d24808a54c1a0d8ddc862700ec533aa3c69639ca8e775fd2c8c1a745f"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
@@ -14,7 +14,7 @@ cask "zulu17" do
 
   livecheck do
     url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
-    regex(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
+    regex(/zulu(\d+(?:[._]\d+)*-.*?)-jdk(\d+(?:\.\d+)*)-macosx_#{arch}\.dmg/i)
     strategy :page_match do |page, regex|
       page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
     end
