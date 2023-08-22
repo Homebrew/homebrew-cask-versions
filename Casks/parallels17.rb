3e7fc0005e2035aa0f2f6b9deeff0cf10168028d
Miccal Matthews <miccal.matthews@gmail.com>
2023-08-23 05:35:01 +0800

    Update parallels17.rb

--- Casks/parallels17.rb
+++ Casks/parallels17.rb
@@ -25,6 +25,7 @@ cask "parallels17" do
     "homebrew/cask-versions/parallels14",
     "homebrew/cask-versions/parallels15",
     "homebrew/cask-versions/parallels16",
+    "homebrew/cask-versions/parallels18",
   ]
   depends_on macos: ">= :high_sierra"
 
