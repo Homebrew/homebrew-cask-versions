f79978aa6f24d4cb39b1319a63248d36e89ba057
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-08-22 12:49:11 -0400

    opera-developer 103.0.4899.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "103.0.4892.0"
-  sha256 "113850e52b14e3300a9225af03dbd8d513914b9593b07d58f040e4c7e1c30f13"
+  version "103.0.4899.0"
+  sha256 "f2e315b46af85c78cb93820fcf29629b2db852db733b75b73b01250c27eca1cf"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
