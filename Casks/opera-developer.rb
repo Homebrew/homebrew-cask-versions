b17c833875f0365266b1de2b7a86f83513aded38
Hannes Braun <hannes@hannesbraun.net>
2023-08-30 13:49:40 +0200

    opera-developer 103.0.4906.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "103.0.4899.0"
-  sha256 "f2e315b46af85c78cb93820fcf29629b2db852db733b75b73b01250c27eca1cf"
+  version "103.0.4906.0"
+  sha256 "46d6fc9f6ac38d3d2b490cf9c3be14fd2e0a6182fa1988daadaf20b1aeeb7685"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
