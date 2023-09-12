d485465608679bbd85c235fe3ca820c5e1f3a282
Eric Knibbe <enk3@outlook.com>
2023-09-12 15:42:01 -0400

    xmind-beta 23.09.03101

--- Casks/xmind-beta.rb
+++ Casks/xmind-beta.rb
@@ -1,25 +1,17 @@
 cask "xmind-beta" do
-  version "23.05.3170,202305291822"
-  sha256 "290849b2385281ba9905775bc85353309a4625256e7080546a1f8f7e7af347de"
+  version "23.09.03101,202309031945"
+  sha256 "158c85d6018d483495c443d7202e09ffcf8ea3f9f01b32fd74f4a27ff2c3d79c"
 
-  url "https://dl3.xmind.net/Xmind-for-macOS-#{version.csv.first}-#{version.csv.second}.dmg"
+  url "https://dl3.xmind.net/Xmind-for-macOS-#{version.csv.first}-beta-#{version.csv.second}.dmg"
   name "XMind"
   desc "Mind mapping and brainstorming tool"
   homepage "https://www.xmind.net/"
 
   livecheck do
-    url "https://xmind.app/download/"
-    regex(/xmind\.app.*?xmind[._-]for[._-]macOS[._-](\d+(?:\.\d+)+)-(\d*)\.dmg/i)
-    strategy :page_match do |page, regex|
-      # Find the script containing the beta versions
-      js = page.scan(%r{assets.xmind.net/www/javascripts/download\.(\w+)\.js}i).flatten.first
-      next if js.blank?
-
-      # Check the js for version text
-      v_page = Homebrew::Livecheck::Strategy.page_content(
-        "https://assets.xmind.net/www/javascripts/download.#{js}.js",
-      )
-      v_page[:content]&.scan(regex)&.map { |match| "#{match[0]},#{match[1]}" }
+    url "https://xmind.app/desktop-beta/download/mac/"
+    regex(/Xmind[._-]for[._-]macOS[._-](\d+(?:\.\d+)+)-beta-(\d*)\.dmg/i)
+    strategy :header_match do |headers, regex|
+      headers["location"].scan(regex).map { |match| "#{match[0]},#{match[1]}" }
     end
   end
 
