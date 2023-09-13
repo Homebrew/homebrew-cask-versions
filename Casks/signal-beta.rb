6e8408b24d282d7542aca248f136ab56561265c0
LanikSJ <laniksj@gmail.com>
2023-09-12 23:38:24 -0700

    signal-beta 6.31.0-beta.2

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,9 +1,9 @@
 cask "signal-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.31.0-beta.1"
-  sha256 arm:   "2c17db510e57a718b4270dfae1734594e3b37db430742c1f0cadba87a7d1e08e",
-         intel: "81d910124b33bd7426d9d958f8121648b5f7de04369770ecf9970c40a60d08d3"
+  version "6.31.0-beta.2"
+  sha256 arm:   "008a5aba83b1b0cf10954c68ae2955ba8f84afe72d8233373821b1c48fc86d7f",
+         intel: "b845f3b7f714cccfa55879f5314117bfbaa9e3e58f9e2b7cd1136b530fc682d5"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
   name "Signal Beta"
