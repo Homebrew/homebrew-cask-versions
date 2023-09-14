1e3e733607ec5b5a67ca9b04789bd16ae2bd9da4
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-09-14 07:30:50 -0400

    sonos-s1-controller 57.18-44230

--- Casks/sonos-s1-controller.rb
+++ Casks/sonos-s1-controller.rb
@@ -1,6 +1,6 @@
 cask "sonos-s1-controller" do
-  version "57.17-43140,jqeqdtdi"
-  sha256 "071cbb3692ac5bad7547798a8e4c0680f84de1f39c05bcb409bcac404f91a90a"
+  version "57.18-44230,rdiahrpn"
+  sha256 "3ace7d55daa9a66be88ac49cc494fd14949603c586159947d77fc4fc16486abf"
 
   url "https://update-software.sonos.com/software/#{version.csv.second}/Sonos_#{version.csv.first}.dmg"
   name "Sonos S1"
