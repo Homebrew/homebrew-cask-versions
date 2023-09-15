292a330d1231c4844f11bad011b4cb6a2a42a517
Hannes Braun <hannes@hannesbraun.net>
2023-09-15 22:51:43 +0200

    zulu17 17.0.8.1

--- Casks/zulu17.rb
+++ Casks/zulu17.rb
@@ -2,9 +2,9 @@ cask "zulu17" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "17.0.8,17.44.15_1-ca"
-  sha256 arm:   "35d5bf045579a08110b91a8e37cda859cda021b1ef7dc4ef8a97b79a14147d60",
-         intel: "ca746b9d24808a54c1a0d8ddc862700ec533aa3c69639ca8e775fd2c8c1a745f"
+  version "17.0.8.1,17.44.53-ca"
+  sha256 arm:   "2467656096bc298e79be1526ea475e3e4cdd86ff17f9051b00194387993f54e8",
+         intel: "44001eb7b660ea5d768168b2746a9fab51c16822bcab9d968a341164e3f07f0d"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
