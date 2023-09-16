3a2403c0607314e7c2bd0cbeff864b7eed6d1f95
ik-fib <ik11235@gmail.com>
2023-09-16 19:04:53 +0900

    google-chrome-canary 119.0.6011.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "119.0.6010.2"
+  version "119.0.6011.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
