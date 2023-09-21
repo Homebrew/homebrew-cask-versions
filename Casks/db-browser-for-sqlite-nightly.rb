40dd033136d9657111d32dbfb691f88edecfaed5
SeongTae Jeong <seongtaejg@gmail.com>
2023-09-21 17:13:18 +0900

    db-browser-for-sqlite: Change the URL

--- Casks/db-browser-for-sqlite-nightly.rb
+++ Casks/db-browser-for-sqlite-nightly.rb
@@ -1,10 +1,8 @@
 cask "db-browser-for-sqlite-nightly" do
-  arch arm: "arm64", intel: "intel"
-
   version :latest
   sha256 :no_check
 
-  url "https://nightlies.sqlitebrowser.org/latest/DB.Browser.for.SQLite-#{arch}.dmg"
+  url "https://nightlies.sqlitebrowser.org/latest/DB.Browser.for.SQLite-universal.dmg"
   name "DB Browser for SQLite Nightly"
   desc "Database browser for SQLite"
   homepage "https://sqlitebrowser.org/"
