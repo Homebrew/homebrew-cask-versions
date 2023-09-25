c20955debdc55fb8b60181e8eae9dd5170cdc96c
miccal <miccal.matthews@gmail.com>
2023-09-25 16:01:44 +0800

    iterm2-nightly 3_5_20230923

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,7 +1,7 @@
 cask "iterm2-nightly" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3_5_20230914"
-  sha256 "b6aa26e0326d3e8f45928d851832a099625eed8b81c5571e0a4fa563a42b6c16"
+  version "3_5_20230923"
+  sha256 "fb0e38416d40807f9d3407a77a3fde1a3dfc236865a3d3db8c668d57b5df2e0b"
 
   url "https://www.iterm2.com/nightly/iTerm2-#{version}-nightly.zip"
   name "iTerm2"
