cc94d49c035890b57551bcb25d8acc6956adcb64
Colton Parsons <colton.parsons@verygoodsecurity.com>
2023-09-25 17:45:50 -0700

    Added tsh13

---
+++ Casks/tsh13.rb
@@ -0,0 +1,24 @@
+cask "tsh13" do
+  version "13.3.8"
+  sha256 "c4678ce55cf030388363d7e12780e6b79e54b89e2e9d0b0635959a41c3ab7931"
+
+  url "https://cdn.teleport.dev/tsh-#{version}.pkg",
+      verified: "cdn.teleport.dev/"
+  name "Teleport TSH (v13)"
+  desc "SSH server for teams managing distributed infrastructure"
+  homepage "https://goteleport.com/"
+
+  livecheck do
+    url "https://goteleport.com/download/"
+    regex(/tsh[._-]v?(\d+(?:\.\d+)+)\.pkg/i)
+  end
+
+  conflicts_with formula: "teleport",
+                 cask:    "tsh"
+
+  pkg "tsh-#{version}.pkg"
+
+  uninstall pkgutil: "(.*).com.gravitational.teleport.tsh"
+
+  zap trash: "~/.tsh"
+end
