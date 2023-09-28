43cbc59b80c06de736efa43f60626e653b3c1325
owine <owine@users.noreply.github.com>
2023-09-28 16:21:36 -0500

    iterm2-beta 3.5.0beta14

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 cask "iterm2-beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.5.0beta13"
-  sha256 "346a67e329e284fa106971ff3ffc8cf01ea0e8d957eb55e85f9c880dc9763e1a"
+  version "3.5.0beta14"
+  sha256 "07ea8939b97d191b97ca9f41b57cdd82809ff4f90b1432a002dee556b0bbe29b"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"
