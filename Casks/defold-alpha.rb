0d035f8bac8f77d5e8ea289da00c09675abd076b
Hannes Braun <hannes@hannesbraun.net>
2023-10-06 12:45:35 +0200

    defold-alpha 1.6.1,c5f1715d9f9eca0b2a13ee1cd7cfa58cb86a6694

--- Casks/defold-alpha.rb
+++ Casks/defold-alpha.rb
@@ -1,6 +1,6 @@
 cask "defold-alpha" do
-  version "1.6.1,8994a2d551388cbf5c9431aaede339c83d7748da"
-  sha256 "e8c8a7c84958eeb61ea883ca44dca4bc6c4193e5490a0d018d3898a8fd17ed71"
+  version "1.6.1,c5f1715d9f9eca0b2a13ee1cd7cfa58cb86a6694"
+  sha256 "e9b90c50d6a2bf4de8997b703b8ab253880ddb3fe09c32c543dc0c768ad1e904"
 
   url "https://d.defold.com/archive/alpha/#{version.csv.second}/alpha/editor2/Defold-x86_64-macos.dmg"
   name "Defold"
