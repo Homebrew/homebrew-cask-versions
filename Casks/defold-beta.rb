bdc7e9f03ba78e9309e6018884cc484ac3909143
Miccal Matthews <miccal.matthews@gmail.com>
2023-10-10 06:21:40 +0800

    Update defold-beta.rb

--- Casks/defold-beta.rb
+++ Casks/defold-beta.rb
@@ -1,26 +1,22 @@
 cask "defold-beta" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.6.1,1d6568311bbf02f2e0c834809708a654d7b73561"
-  sha256 arm:   "86bcf14c6e242dd9152fc395f5920ef9ebd0e9a29ebe7363078a52822742372b",
-         intel: "22c5a7d848e92432b414447f8c78babc21ba4a4ad1d692af73c5fe79b99c74e8"
+  version "1.6.1"
+  sha256 :no_check # required as upstream package is updated in-place
 
-  url "https://d.defold.com/archive/beta/#{version.csv.second}/beta/editor2/Defold-#{arch}-macos.dmg"
+  url "https://github.com/defold/defold/releases/download/#{version}-beta/Defold-#{arch}-macos.dmg",
+      verified: "github.com/defold/defold/"
   name "Defold"
   desc "Game engine for development of desktop, mobile and web games"
   homepage "https://defold.com/"
 
   livecheck do
-    url "https://d.defold.com/beta/info.json"
-    strategy :page_match do |page|
-      version = JSON.parse(page)["version"]
-      sha1 = JSON.parse(page)["sha1"]
-      next if version.blank? || sha1.blank?
-
-      "#{version},#{sha1}"
-    end
+    url "https://github.com/defold/defold/releases"
+    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)[._-]beta["' >]}i)
+    strategy :page_match
   end
 
+  auto_updates true
   conflicts_with cask: [
     "defold",
     "defold-alpha",
