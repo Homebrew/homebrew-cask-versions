705354c30e335f6eed3a477e7a912a96b8f67b45
Bevan Kay <email@bevankay.me>
2023-10-13 07:53:43 +1000

    Update temurin21.rb

--- Casks/temurin21.rb
+++ Casks/temurin21.rb
@@ -14,8 +14,8 @@ cask "temurin21" do
   livecheck do
     url "https://api.adoptium.net/v3/assets/feature_releases/#{version.major}/ga?architecture=#{arch}&image_type=jdk&jvm_impl=hotspot&os=mac&page=0&page_size=1&project=jdk&sort_method=DEFAULT&sort_order=DESC&vendor=eclipse"
     regex(/^jdk-(\d+(?:\.\d+)*?)\+(\d+(?:\.\d+)*)(?:-LTS)?$/i)
-    strategy :page_match do |page, regex|
-      JSON.parse(page).map do |release|
+    strategy :json do |json, regex|
+      json.map do |release|
         match = release["release_name"]&.match(regex)
         next if match.blank?
 
