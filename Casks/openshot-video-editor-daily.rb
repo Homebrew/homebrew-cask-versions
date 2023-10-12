21e3908c16069ebc63b9b3b75f60333b37d4b277
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-10-12 08:20:18 -0400

    openshot-video-editor-daily 3.1.1,11627-2034f45d-08c2cdd1

--- Casks/openshot-video-editor-daily.rb
+++ Casks/openshot-video-editor-daily.rb
@@ -1,6 +1,6 @@
 cask "openshot-video-editor-daily" do
-  version "3.1.1,11583-95eccafc-08c2cdd1"
-  sha256 "d98b558aaf9e2b35215f4fb5550e332b8334d6c9b7f22fa1e7bcdb9e3b128b3e"
+  version "3.1.1,11627-2034f45d-08c2cdd1"
+  sha256 "f8225841fe954dfd4a541903141e32894e21284bb562117d4e83f6c7b620c621"
 
   url "https://github.com/OpenShot/openshot-qt/releases/download/daily/OpenShot-v#{version.csv.first}-daily-#{version.csv.second}-x86_64.dmg",
       verified: "github.com/OpenShot/openshot-qt/"
