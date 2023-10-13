138072209ef53e9e08e2189ce2b0c877dececf1e
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-10-13 06:10:58 -0400

    zotero-beta (new cask)

---
+++ Casks/zotero-beta.rb
@@ -0,0 +1,31 @@
+cask "zotero-beta" do
+  version "7.0.0-beta.45,64a78412c"
+  sha256 "a30d27872dcf0dda3a60e70657e2869dc0755417afdfa0da336cd181e663ea05"
+
+  url "https://download.zotero.org/client/beta/#{version.csv.first}%2B#{version.csv.second}/Zotero-#{version.csv.first}%2B#{version.csv.second}.dmg"
+  name "Zotero Beta"
+  desc "Reference manager"
+  homepage "https://www.zotero.org/"
+
+  livecheck do
+    url "https://www.zotero.org/download/client/update/0/0/Darwin/en-US/beta/Darwin%25/update.xml"
+    strategy :xml do |xml|
+      xml.get_elements("//update[@type='major']").map do |element|
+        element.attributes["displayVersion"].split("+").join(",")
+      end
+    end
+  end
+
+  auto_updates true
+
+  app "Zotero.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/org.zotero.SafariExtensionApp.SafariExtension",
+    "~/Library/Application Support/Zotero",
+    "~/Library/Caches/Zotero",
+    "~/Library/Containers/org.zotero.SafariExtensionApp.SafariExtension",
+    "~/Library/Preferences/org.zotero.zotero-beta.plist",
+    "~/Library/Saved Application State/org.zotero.zotero-beta.savedState",
+  ]
+end
