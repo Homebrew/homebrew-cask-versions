c62da2308e62eda45da81375fb9cc4058ec7ca2b
Rui Chen <rui@chenrui.dev>
2023-10-18 14:58:46 -0400

    zulu8 8.0.392

--- Casks/zulu8.rb
+++ Casks/zulu8.rb
@@ -2,9 +2,9 @@ cask "zulu8" do
   arch arm: "aarch64", intel: "x64"
   choice = on_arch_conditional arm: "arm", intel: "x86"
 
-  version "8.0.382,8.72.0.17-ca"
-  sha256 arm:   "71c487dd8dfe01d2e8a332f97a9e3f8c77f3661a2618b15299fc451ff9195892",
-         intel: "2a9b7c5b4221389f1c23776adbc2cec2f858df684639719e42f724a91a954daa"
+  version "8.0.392,8.74.0.17-ca"
+  sha256 arm:   "aa4126185a8b7c2d2f1b923ae5db216c5cd2e58a4072f39fed0e3db04acee4f2",
+         intel: "843112f7543ea191c044b20f51ecdf7a3e17e3615b83e826d5b550820892228e"
 
   url "https://cdn.azul.com/zulu/bin/zulu#{version.csv.second}-jdk#{version.csv.first}-macosx_#{arch}.dmg",
       referer: "https://www.azul.com/downloads/zulu/zulu-mac/"
