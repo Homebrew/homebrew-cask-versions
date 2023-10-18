8f86ee6ecf95e4b602d11b61a162432d8b4ea565
Rui Chen <rui@chenrui.dev>
2023-10-18 14:45:36 -0400

    microsoft-edge-dev 120.0.2165.1

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-dev" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099619", intel: "2069340"
 
-  version "119.0.2151.2"
-  sha256 arm:   "a80c06a67f101f9ffaadb7fa464d3e6434c03f21bcd63a92ee900f108bb75ed5",
-         intel: "44a897bce6aa423693d20dc5d14182f4027264a0bc5c5d0e61e752409c0eb11f"
+  version "120.0.2165.1"
+  sha256 arm:   "b6f96e71933e50fce1a9ce6967cd7eb921ac9f323155c24bd08c4a56f2802e06",
+         intel: "f0f54bb7657911b2166acaa18c8c5a0d9de2c158262f9b9bb3ccce055d343964"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeDev-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
