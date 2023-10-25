118b7a00d50681380e3e1fa33dff02011a375781
Mikl Wolfe <wolfemikl@gmail.com>
2023-10-25 17:16:00 -0600

    google-chrome-canary 120.0.6089.0

--- Casks/google-chrome-canary.rb
+++ Casks/google-chrome-canary.rb
@@ -1,5 +1,5 @@
 cask "google-chrome-canary" do
-  version "120.0.6088.2"
+  version "120.0.6089.0"
   sha256 :no_check
 
   url "https://dl.google.com/chrome/mac/universal/canary/googlechromecanary.dmg"
