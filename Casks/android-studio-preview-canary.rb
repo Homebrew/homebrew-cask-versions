bee5169944ccb17dc19d6ac562ea2a1965931bf5
Hannes Braun <hannes@hannesbraun.net>
2023-11-03 13:00:53 +0100

    android-studio-preview-canary 2023.2.1.12

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview-canary" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2023.2.1.11"
-  sha256 arm:   "97159e0dd9c9f06f0007fed688bd77f594c9d106cee85d07654648811390000e",
-         intel: "60c1ee2e7c07b1021611e8f0587f8dff7663b3e9d66c6f86fbb5b68f1f3018a1"
+  version "2023.2.1.12"
+  sha256 arm:   "5d852a2296b2417f51d0df81f3ca1d233222ee58d69a4635de0333646742d5e2",
+         intel: "235a83794ac05713ff2eabbc5f312bee0c46a4b2f3bcd3996e3e8dbc769408dc"
 
   url "https://dl.google.com/dl/android/studio/ide-zips/#{version}/android-studio-#{version}-#{arch}.zip",
       verified: "dl.google.com/dl/android/studio/"
