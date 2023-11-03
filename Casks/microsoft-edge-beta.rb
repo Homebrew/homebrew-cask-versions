8ae97e8cfcd4acff03e1abd64e92434c68e5c7aa
Razvan Azamfirei <azamfireirazvan@gmail.com>
2023-11-03 18:54:57 -0400

    microsoft-edge-beta 119.0.2151.46

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-beta" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099618", intel: "2069439"
 
-  version "119.0.2151.42"
-  sha256 arm:   "9fb7e3ef34f17047b499cbdfdac095340cc84c68bbe175cf23a911d92461cc5d",
-         intel: "e9f9668ad38862cf9ac98e540367d4bae714d13d3476502f0ad64574a76b9685"
+  version "119.0.2151.46"
+  sha256 arm:   "6eecd29dcdeefaccc70d5ce727f8ddc3d259caefeeb3b1a6c8793039dded1943",
+         intel: "9149bd3f09006b66dfffd51b796dffeee7e39e8ede3ff7580fd18259a63617d7"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
