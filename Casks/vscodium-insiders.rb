ec2a41be83e6a65c5e7ecd86e810973058adf233
Patrick Linnane <patrick@linnane.io>
2023-11-08 10:28:07 -0800

    vscodium-insiders 1.85.0.23310-insider

--- Casks/vscodium-insiders.rb
+++ Casks/vscodium-insiders.rb
@@ -1,9 +1,9 @@
 cask "vscodium-insiders" do
   arch arm: "arm64", intel: "x64"
 
-  version "1.85.0.23307-insider"
-  sha256 arm:   "2c0c1388e75ede3c42874a2e11735d0be11c160c57be98ba4164db9558b028db",
-         intel: "a64c6b365c47c1393291f3386aca64a2dbf66aac67edcd72a914688f8551a59a"
+  version "1.85.0.23310-insider"
+  sha256 arm:   "a0f0915b0eeed74d4fc17b2dbfcef40665d234c8777a2a67c35f30e99e734f5f",
+         intel: "fd6b0e6c6037f89f624580f7f6178761f608d8f5305cc52b6f025c309f06ee24"
 
   url "https://github.com/VSCodium/vscodium-insiders/releases/download/#{version}/VSCodium.#{arch}.#{version}.dmg",
       verified: "github.com/VSCodium/vscodium-insiders/"
