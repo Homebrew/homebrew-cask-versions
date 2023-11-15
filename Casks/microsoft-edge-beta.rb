3bbe9c03f50d364def32c9a2af18a44ecd3c0545
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-15 08:30:11 -0700

    microsoft-edge-beta 120.0.2210.8

--- Casks/microsoft-edge-beta.rb
+++ Casks/microsoft-edge-beta.rb
@@ -3,9 +3,9 @@ cask "microsoft-edge-beta" do
                                intel: "C1297A47-86C4-4C1F-97FA-950631F94777"
   linkid = on_arch_conditional arm: "2099618", intel: "2069439"
 
-  version "119.0.2151.46"
-  sha256 arm:   "6eecd29dcdeefaccc70d5ce727f8ddc3d259caefeeb3b1a6c8793039dded1943",
-         intel: "9149bd3f09006b66dfffd51b796dffeee7e39e8ede3ff7580fd18259a63617d7"
+  version "120.0.2210.8"
+  sha256 arm:   "9c2e62d496f5014a5af046016dde1eaa9bacc8e386f34e1e88f1b611d1fa7b69",
+         intel: "65b56e8f33698e4d2ac8b8ee6ad582b492bd8fc6a20471730693156aa8146c3f"
 
   url "https://officecdn-microsoft-com.akamaized.net/pr/#{folder}/MacAutoupdate/MicrosoftEdgeBeta-#{version}.pkg",
       verified: "officecdn-microsoft-com.akamaized.net/"
