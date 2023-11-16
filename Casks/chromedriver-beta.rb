592aae9374d8463256f9f3c59f83860242b9b0a4
Patrick Linnane <patrick@linnane.io>
2023-11-16 11:39:16 -0800

    chromedriver-beta 120.0.6099.28

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -1,9 +1,9 @@
 cask "chromedriver-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "120.0.6099.18"
-  sha256 arm:   "141e0abedfe2a1df6c3eb334becdf1993dce8b4fc0f906457606470c6542f236",
-         intel: "f645302e046e4314477b72244335c60098563d202fc0c9467df7d71b850c21f1"
+  version "120.0.6099.28"
+  sha256 arm:   "85a6b574e9061eb2a7d130f3e375c31bb66248a245f93dc9aba07bb552b89a0e",
+         intel: "e156528264ea9c5ff89f436ad8f52fc5d975b4cc6cf729e240556ee51a81664e"
 
   url "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/"
