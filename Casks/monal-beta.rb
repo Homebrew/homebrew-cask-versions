d3a93593ab9b653ab2f3cc73ae2d72567fe30566
Patrick Linnane <patrick@linnane.io>
2023-11-16 11:46:31 -0800

    monal-beta 871

--- Casks/monal-beta.rb
+++ Casks/monal-beta.rb
@@ -1,6 +1,6 @@
 cask "monal-beta" do
-  version "870"
-  sha256 "94fc0017cadd8217b750a006b5eaad1e057386db622fe809f1c49cbbf9c2de80"
+  version "871"
+  sha256 "640018b4db2783ca777e557ea41797994c2a5f3a2d67dad4d1a1dcd5ae1a4e62"
 
   url "https://downloads.monal-im.org/monal-im/beta/macOS/Monal-#{version}.zip"
   name "Monal"
