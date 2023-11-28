384307211349b0cf2d54c03af48ae8a6ba54473a
Mikl Wolfe <wolfemikl@gmail.com>
2023-11-28 08:25:37 -0700

    qbittorrent-lt20 4.6.2

--- Casks/qbittorrent-lt20.rb
+++ Casks/qbittorrent-lt20.rb
@@ -1,6 +1,6 @@
 cask "qbittorrent-lt20" do
-  version "4.6.1"
-  sha256 "77b75f35bb9d2da49546ed71a601c0a5ceccef128219eae286391737707f725b"
+  version "4.6.2"
+  sha256 "d554d18a5512ecd46a4a03f86d18872789f655997abddca1c0e8797e6de2ca94"
 
   url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}_lt20.dmg",
       verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
