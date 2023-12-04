4ca00c9fbbb5bd38688bb4433b4de9515e9b4bdb
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-04 14:22:33 -0700

    opera-developer 107.0.5004.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "106.0.4998.0"
-  sha256 "d5d613b03e9c0a6082c983e6f723889d559c2802a3ba5f1b8bdbaa29f531161e"
+  version "107.0.5004.0"
+  sha256 "7671752acd467347724b9265ff66c68bd75dc25c53f4c9eeb5cac4aa352db774"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
