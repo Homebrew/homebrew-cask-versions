8d1cf8c43d764392a13ada3d77b231336960ef4e
Sam Ford <1584702+samford@users.noreply.github.com>
2023-12-05 12:51:57 -0500

    zulu15: discontinued

--- Casks/zulu15.rb
+++ Casks/zulu15.rb
@@ -1,6 +1,5 @@
 cask "zulu15" do
   arch arm: "aarch64", intel: "x64"
-  choice = on_arch_conditional arm: "arm", intel: "x86"
 
   version "15.0.10,15.46.17-ca"
   sha256 arm:   "f5d123cd149f245792d0d807086f4c58c493bd52db8162ed2d2609f97e493ac1",
@@ -12,14 +11,6 @@ cask "zulu15" do
   desc "Zulu OpenJDK 15"
   homepage "https://www.azul.com/products/core/"
 
-  livecheck do
-    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
-    regex(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.dmg/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
-    end
-  end
-
   depends_on macos: ">= :sierra"
 
   pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
@@ -27,4 +18,8 @@ cask "zulu15" do
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 
   zap trash: "~/Library/Saved Application State/com.azul.zulu.#{version.major}*.java.savedState"
+
+  caveats do
+    discontinued
+  end
 end
