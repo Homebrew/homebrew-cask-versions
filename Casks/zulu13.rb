0fd4f47d477d1948531559463551ae6f83ad5f0c
Sam Ford <1584702+samford@users.noreply.github.com>
2023-12-05 12:51:45 -0500

    zulu13: discontinued

--- Casks/zulu13.rb
+++ Casks/zulu13.rb
@@ -1,6 +1,5 @@
 cask "zulu13" do
   arch arm: "aarch64", intel: "x64"
-  choice = on_arch_conditional arm: "arm", intel: "x86"
 
   version "13.0.14,13.54.17-ca"
   sha256 arm:   "b7b41f43f6a76f84d579e1d7b4abfd536928b6472e0ca3a8188f474235137b8e",
@@ -12,17 +11,13 @@ cask "zulu13" do
   desc "OpenJDK distribution from Azul"
   homepage "https://www.azul.com/products/core/"
 
-  livecheck do
-    url "https://api.azul.com/zulu/download/community/v1.0/bundles/latest/?jdk_version=#{version.major}&bundle_type=jdk&javafx=false&ext=dmg&os=macos&arch=#{choice}"
-    regex(/zulu(\d+(?:\.\d+)*-.*?)-jdk(\d+(?:\.\d+)+)-macosx_#{arch}\.dmg/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[1]},#{match[0]}" }
-    end
-  end
-
   pkg "Double-Click to Install Azul Zulu JDK #{version.major}.pkg"
 
   uninstall pkgutil: "com.azulsystems.zulu.#{version.major}"
 
   zap trash: "~/Library/Saved Application State/com.azul.zulu.#{version.major}*.java.savedState"
+
+  caveats do
+    discontinued
+  end
 end
