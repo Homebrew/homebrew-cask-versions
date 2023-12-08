709f99fc0df4629d7f824a230428397a52abcad9
Hannes Braun <hannes@hannesbraun.net>
2023-12-08 14:49:11 +0100

    mixxx-snapshot 2.5-alpha-167-gbdfb495e24

--- Casks/mixxx-snapshot.rb
+++ Casks/mixxx-snapshot.rb
@@ -1,9 +1,9 @@
 cask "mixxx-snapshot" do
   arch arm: "arm", intel: "intel"
 
-  version "2.5-alpha-165-gdabca58d22"
-  sha256 arm:   "e35dd0908bd4019e5d1b324a71358e8dcc35618ce70fc7d9190c43e9a673cc0e",
-         intel: "ff8ea3c760a68a0ea89b7746b69ce1828e4881a2b1dc5e918d66e45b28290053"
+  version "2.5-alpha-167-gbdfb495e24"
+  sha256 arm:   "219055f83a70b893405aa55f13c84cd21d491bc44f9d98c13f152db5038dfcb0",
+         intel: "f82d1274232523fc5a52c15f70eb30de4469047ee8e0fa94ae24fdfd4a316c57"
 
   url "https://downloads.mixxx.org/snapshots/main/mixxx-#{version}-macos#{arch}.dmg"
   name "Mixxx"
