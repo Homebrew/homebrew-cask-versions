a9267f9e08ff4f4bca1ea7b7a65bcae620e0431d
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-08 21:09:49 -0500

    java-beta 22,27

--- Casks/java-beta.rb
+++ Casks/java-beta.rb
@@ -1,9 +1,9 @@
 cask "java-beta" do
   arch arm: "aarch64", intel: "x64"
 
-  version "22,26"
-  sha256 arm:   "265c0e46cd1a5e137385f2a4344e0b2d5b1b1200af5e292539bd3eae1c1373c9",
-         intel: "738ce2ac3cbd17bb99b5c22c4c96e721888817ed16c027a22946971bcb8237b1"
+  version "22,27"
+  sha256 arm:   "16b988127c289904a146b6a24c9454692197387e3878b7749b86a372686e03bb",
+         intel: "2d427ea463711dbe87b8cb4797c5d99a0c259191b0559a85d968ebcf6ea4bb7a"
 
   url "https://download.java.net/java/early_access/jdk#{version.major}/#{version.csv.second}/GPL/openjdk-#{version.csv.first}-ea+#{version.csv.second}_macos-#{arch}_bin.tar.gz"
   name "OpenJDK Early Access Java Development Kit"
