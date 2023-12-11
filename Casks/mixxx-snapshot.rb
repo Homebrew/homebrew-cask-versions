c031a04e35710786aca23e68bfba25627f49121f
Justin Krehel <39449589+krehel@users.noreply.github.com>
2023-12-10 21:34:16 -0500

    mixxx-snapshot 2.5-alpha-169-g865c206eb4

--- Casks/mixxx-snapshot.rb
+++ Casks/mixxx-snapshot.rb
@@ -1,9 +1,9 @@
 cask "mixxx-snapshot" do
   arch arm: "arm", intel: "intel"
 
-  version "2.5-alpha-167-gbdfb495e24"
-  sha256 arm:   "219055f83a70b893405aa55f13c84cd21d491bc44f9d98c13f152db5038dfcb0",
-         intel: "f82d1274232523fc5a52c15f70eb30de4469047ee8e0fa94ae24fdfd4a316c57"
+  version "2.5-alpha-169-g865c206eb4"
+  sha256 arm:   "a8c427a498a7ea1604059674b5126fdec7710363aca1034bf5c65ef2b5c83b79",
+         intel: "d38751b0b9953c6bf7ffd4a334525da70422ec224557ba98f6781ece6d33daea"
 
   url "https://downloads.mixxx.org/snapshots/main/mixxx-#{version}-macos#{arch}.dmg"
   name "Mixxx"
