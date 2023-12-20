757922ba76dcbdda8fc3c1dd9aba29a3ef2457dc
Mikl Wolfe <wolfemikl@gmail.com>
2023-12-20 14:30:57 -0700

    signal-beta 6.43.0-beta.3

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,9 +1,9 @@
 cask "signal-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "6.43.0-beta.2"
-  sha256 arm:   "7a1f9b166a4c1ef3cfc5ca4cf2364b8c348dc9dcdae32d1b5a372de62989c780",
-         intel: "fc316dfc57b8578c9693de481c108c61b87217e0dee51671a754b416a75aac9b"
+  version "6.43.0-beta.3"
+  sha256 arm:   "a1b17a4cd92a78b645358a0c142c087f2eb3614aa76851f6b5d7ec1d67303fc9",
+         intel: "58ef6b275b522d056ad3c994a46c7c6ea10e545e7ff8a10bb7803b10412afad4"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
   name "Signal Beta"
