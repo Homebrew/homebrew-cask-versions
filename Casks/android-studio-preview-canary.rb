23be344a20ec9d3dd38650cf352cae40e28bf802
Miccal Matthews <miccal.matthews@gmail.com>
2024-01-09 10:39:38 +0800

    Update android-studio-preview-canary.rb

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -2,8 +2,8 @@ cask "android-studio-preview-canary" do
   arch arm: "mac_arm", intel: "mac"
 
   version "2023.3.1.3"
-  sha256 arm:   "418a487eeb25fd8f29d9bf031eb20e1b16437c68ddbe7eecc7676515d2cd8912",
-         intel: "94f4cca568c749a648985ce3c8a90a1dba845ef03ae3f7e936f6a80c357d9035"
+  sha256 arm:   "fbaeac225b31bbfd01790fcdfa9afc9fa14f88121ec2b6f5a32c8b73c965df25",
+         intel: "7e727c85c2bb22268c9eb3f9ba5e0e139859e7f9456aaa772c6e4ed912282697"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/install/"
