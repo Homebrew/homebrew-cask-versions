b9f69ea463f95b8de68c5e25d76392677be01e46
Hannes Braun <hannes@hannesbraun.net>
2024-01-18 00:44:42 +0100

    discord-development 0.0.20

--- Casks/discord-development.rb
+++ Casks/discord-development.rb
@@ -1,6 +1,6 @@
 cask "discord-development" do
-  version "0.0.19"
-  sha256 "305b5222ef8efbcc8fb10ea12aafd5ba609cc50c97667035bcff18b7a7506c8b"
+  version "0.0.20"
+  sha256 "c4df12a61af27e0a7e29d670bc26895305fc0b3c1825d9d7510247cbdec22ed3"
 
   url "https://dl-development.discordapp.net/apps/osx/#{version}/DiscordDevelopment.dmg",
       verified: "dl-development.discordapp.net/"
