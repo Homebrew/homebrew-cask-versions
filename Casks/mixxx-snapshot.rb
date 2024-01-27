d18f6fe4a3899dd13cd6c081f554c5e304a8207c
Rui Chen <rui@chenrui.dev>
2024-01-27 11:04:29 -0500

    mixxx-snapshot 2.5-alpha-212-ga7af5725c7

--- Casks/mixxx-snapshot.rb
+++ Casks/mixxx-snapshot.rb
@@ -1,9 +1,9 @@
 cask "mixxx-snapshot" do
   arch arm: "arm", intel: "intel"
 
-  version "2.5-alpha-211-g59ca3a25b7"
-  sha256 arm:   "694dc5cabf27b26c9521f43ce9a00a53ad42bf11aa753e77472b98c2c386a07a",
-         intel: "54ffc8ded5c217f6313d03b8f91a580a88bb1dde47a554acc91b4ca3d65b64c0"
+  version "2.5-alpha-212-ga7af5725c7"
+  sha256 arm:   "e4864261120719215c617522a7cbc23107e3b3afb982ec4a9838d2099c20a7ef",
+         intel: "d39dad349c5d5ee8a4b6b6773d8c6c03f7a87d1d97c994aa327582a2d5271957"
 
   url "https://downloads.mixxx.org/snapshots/main/mixxx-#{version}-macos#{arch}.dmg"
   name "Mixxx"
