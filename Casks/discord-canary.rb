5cb2727971635afdadf1ecd1f4d94482000e4298
Rui Chen <rui@chenrui.dev>
2024-01-29 18:53:56 -0500

    discord-canary 0.0.403

--- Casks/discord-canary.rb
+++ Casks/discord-canary.rb
@@ -1,6 +1,6 @@
 cask "discord-canary" do
-  version "0.0.401"
-  sha256 "7f3fcb0d027f07c96e37a15fa1ba7cda19f270138b83d29d8b49172c696d0837"
+  version "0.0.403"
+  sha256 "30b5e47f8a8de4641377b3640843c5d438d82560ce5b529482cc804d2639e2d6"
 
   url "https://dl-canary.discordapp.net/apps/osx/#{version}/DiscordCanary.dmg",
       verified: "dl-canary.discordapp.net/"
