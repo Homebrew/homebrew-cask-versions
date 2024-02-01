9a6b480d1a4fe3b68fc67466885479a98449d8a2
Rui Chen <rui@chenrui.dev>
2024-01-31 23:42:13 -0500

    chromedriver-beta 122.0.6261.18

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -1,9 +1,9 @@
 cask "chromedriver-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "122.0.6261.6"
-  sha256 arm:   "d68278e5173c179c0000456f03f304daf685f5e2dbf68937f65142be81cbc490",
-         intel: "a3aed446fab7b123d0a1cd93579156e144a61dad513fc4b451d291f059e62db3"
+  version "122.0.6261.18"
+  sha256 arm:   "9b197aa6bdc544a9b79777047c0230e7d48626521c3170d21a3dc32468947ca9",
+         intel: "f01ecfc145dfbdd7fe0a68dbdae6ae6c205573a90180bc3f2850ee413a47acfe"
 
   url "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/"
