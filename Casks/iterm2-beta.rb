d63f1b4a4790bc37efddcf5bbd18988d9b58f4fe
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-02-06 19:31:12 -0500

    iterm2-beta 3.5.0beta20

--- Casks/iterm2-beta.rb
+++ Casks/iterm2-beta.rb
@@ -1,7 +1,7 @@
 cask "iterm2-beta" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3.5.0beta19"
-  sha256 "6e6ae37394c79ccbecde882183d4fc79da4fcbd31e535732afb83daab4a9428d"
+  version "3.5.0beta20"
+  sha256 "088694a52b1cffecf51cb243df3048438a2e86db17a7043b54dd1fecb0f5cbd3"
 
   url "https://iterm2.com/downloads/beta/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"
