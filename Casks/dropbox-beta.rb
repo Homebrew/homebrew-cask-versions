5a3c08afd79e2b28409b5c634785ba045e0781f7
Rui Chen <rui@chenrui.dev>
2024-02-14 08:14:53 +0100

    dropbox-beta 193.3.5535

--- Casks/dropbox-beta.rb
+++ Casks/dropbox-beta.rb
@@ -1,9 +1,9 @@
 cask "dropbox-beta" do
   arch arm: "&arch=arm64"
 
-  version "193.3.5459"
-  sha256 arm:   "76bd32907e011934e894d62ff135a80559b48fc574ca6d78ad2748b1ce4adbc3",
-         intel: "50c2cd8aa97b4625531dbb952089a36bbe947cb58b5449460e56a91561f18745"
+  version "193.3.5535"
+  sha256 arm:   "573013e3f87e3e89e0c7ddf9b03ca6aae78bca51bfaea165007d36081fc1a9ff",
+         intel: "22a8ea4bde7dd1ac248fc8a5776c687d1394ccf2bb6a26a495726be69d864cf8"
 
   url "https://www.dropbox.com/download?build=#{version}&plat=mac&rtoken=&type=full#{arch}",
       verified: "dropbox.com/"
