e18512aa3405ce4b1cbea1f6a909ad79fa3ef7eb
Hannes Braun <hannes@hannesbraun.net>
2024-02-18 13:59:47 +0100

    iterm2-nightly 3_5_20240218

--- Casks/iterm2-nightly.rb
+++ Casks/iterm2-nightly.rb
@@ -1,7 +1,7 @@
 cask "iterm2-nightly" do
   # NOTE: "2" is not a version number, but an intrinsic part of the product name
-  version "3_5_20240214"
-  sha256 "35a117b72294c872f1ee9af5d98de52b4e4552db926cea097a62a2edc106cd66"
+  version "3_5_20240218"
+  sha256 "c92b05d9f3c40ac5d68c4681becb20b384299b9ce60c9fd8e0554475e69ec207"
 
   url "https://www.iterm2.com/nightly/iTerm2-#{version}-nightly.zip"
   name "iTerm2"
