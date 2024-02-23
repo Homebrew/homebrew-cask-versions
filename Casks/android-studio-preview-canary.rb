63e1a8ed8b04d4e2f88e9bad53c9fd1dee4215bf
Rui Chen <rui@chenrui.dev>
2024-02-22 19:56:38 -0500

    android-studio-preview-canary 2023.3.1.11

--- Casks/android-studio-preview-canary.rb
+++ Casks/android-studio-preview-canary.rb
@@ -1,9 +1,9 @@
 cask "android-studio-preview-canary" do
   arch arm: "mac_arm", intel: "mac"
 
-  version "2023.3.1.10"
-  sha256 arm:   "b3ad75f66bc30320741751d8d37fe76db3398b757cb2530bc57773dc9ff24c00",
-         intel: "201904696de274ca12a36245990534ca975b45ddb7794312d56cd2772d46079c"
+  version "2023.3.1.11"
+  sha256 arm:   "f16d091067600223a57fdda9c4a5872158cc8666335a61baf1d2e829adaeffcc",
+         intel: "3e3994c3b833148c0e29f6eeb2c220fbb72d1159beae71f626499211677b55ab"
 
   url "https://redirector.gvt1.com/edgedl/android/studio/install/#{version}/android-studio-#{version}-#{arch}.dmg",
       verified: "redirector.gvt1.com/edgedl/android/studio/install/"
