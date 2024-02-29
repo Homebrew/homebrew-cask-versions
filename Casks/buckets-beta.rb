8cb6330c1e86d06a4e11ca076a12acb0bafdb526
Hannes Braun <hannes@hannesbraun.net>
2024-02-29 13:07:55 +0100

    buckets-beta 0.73.4

--- Casks/buckets-beta.rb
+++ Casks/buckets-beta.rb
@@ -1,9 +1,9 @@
 cask "buckets-beta" do
   arch arm: "arm64", intel: "amd64"
 
-  version "0.73.3"
-  sha256 arm:   "58adeb682d1eb7e01dab945286af2ac2c9b30a0b60a28af9e442295a045cbbfd",
-         intel: "ba5604b49c8fe9aa657ec4f541188bf8a7383cf162650c16bc0205c5eccc0058"
+  version "0.73.4"
+  sha256 arm:   "f5d5062af54205b3277319c66227f117d97718857fe89b42ec1a9db87ac3d429",
+         intel: "c7f8da54de6d6f2cba56223a0cd5f50871c2e9b8f54b2d2649080b4dabfa669d"
 
   url "https://github.com/buckets/desktop-beta/releases/download/v#{version}/Buckets-Beta-#{arch}-#{version}.dmg",
       verified: "github.com/buckets/desktop-beta/"
