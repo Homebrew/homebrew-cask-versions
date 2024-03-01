c9e057e65ccf7278d80daccc8dd0eed33799b5a9
Hannes Braun <hannes@hannesbraun.net>
2024-03-01 15:26:26 +0100

    tsh13 13.4.17

--- Casks/tsh13.rb
+++ Casks/tsh13.rb
@@ -1,6 +1,6 @@
 cask "tsh13" do
-  version "13.4.16"
-  sha256 "855e9f08e13e045136476f9e60fd79f1f5d7438ab90a3be2866404562fb01cfd"
+  version "13.4.17"
+  sha256 "06cee0049ad466f99acc0043da4e8a9bf768c2877b8d5a078158e7b8d6e23451"
 
   url "https://cdn.teleport.dev/tsh-#{version}.pkg",
       verified: "cdn.teleport.dev/"
