3a7845278ea7c84a2a1a776676e53a85b1e697a6
Hannes Braun <hannes@hannesbraun.net>
2024-03-07 11:43:04 +0100

    chromedriver-beta 123.0.6312.28

--- Casks/chromedriver-beta.rb
+++ Casks/chromedriver-beta.rb
@@ -1,9 +1,9 @@
 cask "chromedriver-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "123.0.6312.22"
-  sha256 arm:   "b5851f58e12068329c959a2fa3a7e3ec51ee909f2cd3b5b4e28772a2f5a03143",
-         intel: "7c7ab7c52eeea8e2526e2ddfe356f89cafd6ee4242561ae11de9cbb9736b5374"
+  version "123.0.6312.28"
+  sha256 arm:   "8bf8499b8346c2bbfae774e902304c01a254f87e979006fdd850e4b55e73da46",
+         intel: "01dc14f29877d24c14b167b66011f34f37b38812ed117af0a3ab57a0fe89de8f"
 
   url "https://storage.googleapis.com/chrome-for-testing-public/#{version}/mac-#{arch}/chromedriver-mac-#{arch}.zip",
       verified: "storage.googleapis.com/chrome-for-testing-public/"
