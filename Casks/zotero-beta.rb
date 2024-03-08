05b669ed42006d08e1ec3d63bdc122d61b1adcca
Rui Chen <rui@chenrui.dev>
2024-03-08 09:39:45 -0500

    zotero-beta 7.0.0-beta.66

--- Casks/zotero-beta.rb
+++ Casks/zotero-beta.rb
@@ -1,6 +1,6 @@
 cask "zotero-beta" do
-  version "7.0.0-beta.65,b047f3d90"
-  sha256 "9ad12e6eb44544076b5e04cf4c58c0a65aa8d7a40bead457d27bc7a6144ea9d7"
+  version "7.0.0-beta.66,d0299a1ac"
+  sha256 "2538f68784c50d92ca58143fee52a8c9d7d5910c3e1364e9d5cfd8f88b86a9c6"
 
   url "https://download.zotero.org/client/beta/#{version.csv.first}%2B#{version.csv.second}/Zotero-#{version.csv.first}%2B#{version.csv.second}.dmg"
   name "Zotero Beta"
