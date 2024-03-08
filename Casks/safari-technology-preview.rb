84b5daf82527b20f39980ba94ae5d6b946020357
Bevan Kay <email@bevankay.me>
2024-03-09 10:41:28 +1100

    safari-technology-preview: update livecheck

--- Casks/safari-technology-preview.rb
+++ Casks/safari-technology-preview.rb
@@ -1,30 +1,44 @@
 cask "safari-technology-preview" do
-  on_ventura do
+  # when adjusting the on_{os} scoping, also update the livecheck regex
+  on_ventura :or_older do
     version "190,052-60097-20240306-3AFB286B-60B0-4A35-B3C1-4A3980C950E4"
     sha256 "9b9b6e10b1b836263a18c9b9c79c76c04202dd0321e9e92a6d3a035b51600d4e"
+
+    livecheck do
+      url :homepage
+      regex(%r{
+        href=.*?/([^/]+)/Safari(?:%20)?Technology(?:%20)?Preview\.dmg
+        .*?macOS(?:\s|&nbsp;)*13[\s.<]
+      }ix)
+      strategy :page_match do |page, regex|
+        release = page[%r{>\s*Release\s*</p>\s*<p[^>]*>\s*(\d+)\s*<}i, 1]
+        id = page[regex, 1]
+        "#{release},#{id}"
+      end
+    end
   end
   on_sonoma :or_newer do
     version "190,052-60096-20240306-D7F49226-65E4-4804-B8FA-D0C672DEAFD1"
     sha256 "226337589817418cb788e809206956ac007086f2604ca006345ce184f14eb35f"
+
+    livecheck do
+      url :homepage
+      regex(%r{
+        href=.*?/([^/]+)/Safari(?:%20)?Technology(?:%20)?Preview\.dmg
+        .*?macOS(?:\s|&nbsp;)*14[\s.<]
+      }ix)
+      strategy :page_match do |page, regex|
+        release = page[%r{>\s*Release\s*</p>\s*<p[^>]*>\s*(\d+)\s*<}i, 1]
+        id = page[regex, 1]
+        "#{release},#{id}"
+      end
+    end
   end
 
   url "https://secure-appldnld.apple.com/STP/#{version.csv.second}/SafariTechnologyPreview.dmg"
   name "Safari Technology Preview"
   desc "Web browser"
   homepage "https://developer.apple.com/safari/resources/"
-
-  livecheck do
-    url :homepage
-    regex(%r{
-      href=.*?/([^/]+)/Safari(?:%20)?Technology(?:%20)?Preview\.dmg
-      .*?macOS(?:\s|&nbsp;)*#{Regexp.escape(MacOS.version.to_s)}[\s.<]
-    }ix)
-    strategy :page_match do |page, regex|
-      release = page[%r{>\s*Release\s*</p>\s*<p[^>]*>\s*(\d+)\s*<}i, 1]
-      id = page[regex, 1]
-      "#{release},#{id}"
-    end
-  end
 
   auto_updates true
   depends_on macos: ">= :ventura"
