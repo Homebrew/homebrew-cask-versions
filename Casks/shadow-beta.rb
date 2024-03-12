ba5b6b52dbb90dbe18c11a242351ef0e3f360aaf
Klaus Hipp <khipp@users.noreply.github.com>
2024-03-12 22:53:33 +0100

    shadow-beta 8.0.10244

--- Casks/shadow-beta.rb
+++ Casks/shadow-beta.rb
@@ -1,9 +1,9 @@
 cask "shadow-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.10225"
-  sha256 arm:   "91bc6e1407359464d7e2fe6de7bb62954763ea32c655f29f4aba4bb79f0a92d4",
-         intel: "cd020535dc763eea669e1d5ab1ba2d1d7c4facd82a2e68ecda77e485dca30bbf"
+  version "8.0.10244"
+  sha256 arm:   "ecc20a8e7442cde3647e91eae5bededd63c1def4a1fe806e6b58b384287b0be0",
+         intel: "a058bc14a125bad7a05a98732a7a583cf321ea2d9eb37875363b4d1309e4bb38"
 
   url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowPCBeta-#{version}.dmg"
   name "Shadow PC Beta"
