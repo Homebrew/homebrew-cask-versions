0a594b8d92630d45e10815b31dce25ee952bbbb3
Miccal Matthews <miccal.matthews@gmail.com>
2024-03-15 08:29:52 +0800

    Update brave-browser-beta.rb

--- Casks/brave-browser-beta.rb
+++ Casks/brave-browser-beta.rb
@@ -2,9 +2,9 @@ cask "brave-browser-beta" do
   arch arm: "arm64", intel: "x64"
   folder = on_arch_conditional arm: "beta-arm64", intel: "beta"
 
-  version "1.64.102.0"
-  sha256 arm:   "90ad54b81204f7fd87ff944ee9591a3de645cd538fdb9b854522c2e87a6a90b6",
-         intel: "ed72da6b4ed09bd02d1166d858553a58ef5e1ba3346639c7e1e79fffa1f08c5e"
+  version "1.65.82.0"
+  sha256 arm:   "10837472eec8fcd828b265708627134b30675ce009267a0d30b0e571b2afe3d4",
+         intel: "2fe19c351eb9a9e84d674769060f4de5a8d67c6fbdaea4e90f3514cfad891828"
 
   url "https://updates-cdn.bravesoftware.com/sparkle/Brave-Browser/#{folder}/#{version.major_minor_patch.sub(".", "")}/Brave-Browser-Beta-#{arch}.dmg",
       verified: "updates-cdn.bravesoftware.com/sparkle/Brave-Browser/"
