e19c0c729f430974bf01083d70663b4644cf26de
Hannes Braun <hannes@hannesbraun.net>
2024-03-19 18:46:05 +0100

    github-beta 3.3.12-beta3-9dfeb2bf

--- Casks/github-beta.rb
+++ Casks/github-beta.rb
@@ -2,9 +2,9 @@ cask "github-beta" do
   arch arm: "arm64", intel: "x64"
   platform = on_arch_conditional arm: "darwin-arm64", intel: "darwin"
 
-  version "3.3.12-beta2-918e98a4"
-  sha256 arm:   "af7533ab1bd5f4e1b934b46e11479d69b5029be9080c38bd57c7142849f894d7",
-         intel: "65228844f8cc2361044a8cc09e9299b131c6eca1a3a5569b5a47f33ec7c03ed2"
+  version "3.3.12-beta3-9dfeb2bf"
+  sha256 arm:   "9e4c8911606ec3cc7f51d19096a3bd69b3c2cb2293f10bbf98dce72da709dc65",
+         intel: "01f88e4018ba9f80966e5e200c51cd1f3414e30dea9543127e30ac7806a61ce4"
 
   url "https://desktop.githubusercontent.com/github-desktop/releases/#{version}/GitHubDesktop-#{arch}.zip",
       verified: "desktop.githubusercontent.com/github-desktop/releases/"
