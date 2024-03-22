46030a8ec28a0fd91f2ff3a12fc468e6aa9926df
LanikSJ <laniksj@gmail.com>
2024-03-22 16:32:16 -0700

    signal-beta 7.4.0-beta.2

--- Casks/signal-beta.rb
+++ Casks/signal-beta.rb
@@ -1,9 +1,9 @@
 cask "signal-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "7.4.0-beta.1"
-  sha256 arm:   "bae83df7c7fcdf08248a28797aaee86567f022bbb99e0be3379c08e7759d5ae5",
-         intel: "38cc4c624e9f70bc50b1f655aba8245fc2afdbba81c7893b4f3ea1905106a608"
+  version "7.4.0-beta.2"
+  sha256 arm:   "0a16a083f327e99046e8633cdf1f40969e35b49888680b866015e16f32e444bf",
+         intel: "557229900ec107d6f6d4b3df4b46ec665f3307f9e4a08c7254ea981a590f387b"
 
   url "https://updates.signal.org/desktop/signal-desktop-beta-mac-#{arch}-#{version}.dmg"
   name "Signal Beta"
