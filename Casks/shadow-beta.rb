b61e873ec662e200e06e7a7f4f4bec2558ff61c7
Hannes Braun <hannes@hannesbraun.net>
2024-03-26 12:17:29 +0100

    shadow-beta 8.0.10261

--- Casks/shadow-beta.rb
+++ Casks/shadow-beta.rb
@@ -1,9 +1,9 @@
 cask "shadow-beta" do
   arch arm: "arm64", intel: "x64"
 
-  version "8.0.10244"
-  sha256 arm:   "ecc20a8e7442cde3647e91eae5bededd63c1def4a1fe806e6b58b384287b0be0",
-         intel: "a058bc14a125bad7a05a98732a7a583cf321ea2d9eb37875363b4d1309e4bb38"
+  version "8.0.10261"
+  sha256 arm:   "01848832d845ec452f83dc0eaf34b9bfb14d25ee1771af82d5e03a28c2a6a429",
+         intel: "8421d5e71733063f091ede31af8d4184ec1f9828892dd4794e2793c787e30a20"
 
   url "https://update.shadow.tech/launcher/preprod/mac/#{arch}/ShadowPCBeta-#{version}.dmg"
   name "Shadow PC Beta"
