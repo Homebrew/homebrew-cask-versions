7e7088ef1e6a0ae16025ea5cb5c27dd8fcde66e7
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-03 12:16:00 +0200

    microsoft-edge-dev 125.0.2492.1

--- Casks/microsoft-edge-dev.rb
+++ Casks/microsoft-edge-dev.rb
@@ -2,12 +2,12 @@ cask "microsoft-edge-dev" do
   linkid = on_arch_conditional arm: "2099619", intel: "2069340"
 
   on_arm do
-    version "124.0.2478.10,d67e791a-1d93-428d-a81d-7fc0263380c0"
-    sha256 "33fd19878d67610a56eb82a2f63e89f167000527830c72dae085be4d36e03409"
+    version "125.0.2492.1,3cd1d49d-77f2-459d-8ae6-e4b55c506068"
+    sha256 "52d27c3be83b000802d1ba3a86957c45a8342bc2eeba3d58613cf0f843c75cb0"
   end
   on_intel do
-    version "124.0.2478.10,14dd5a34-fe0c-422d-95ba-19017d34238e"
-    sha256 "37b16e956274e1e7f9cabdf4c410e3736bbabe9ffa58c8479aa0e8d60cf18bc8"
+    version "125.0.2492.1,5e91d31f-df41-49be-81dd-b4a43c5de887"
+    sha256 "771279d9c6a81fae93380546e2f51b430f478aae54b13abec4701f8599573996"
   end
 
   url "https://msedge.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/#{version.csv.second}/MicrosoftEdgeDev-#{version.csv.first}.pkg"
