199c86083093655972d61cebfbc9275307bd01f6
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-03 22:21:58 +0200

    figma-beta 116.18.4

--- Casks/figma-beta.rb
+++ Casks/figma-beta.rb
@@ -1,9 +1,9 @@
 cask "figma-beta" do
   arch arm: "mac-arm", intel: "mac"
 
-  version "116.18.2"
-  sha256 arm:   "6461efe850163865ea5c959779450732efd819aa5e11546d5b4571fcc2276629",
-         intel: "c61bf0042df21e5d7d33de925976daa69da84c5282cfac1bd60558b572b50cf9"
+  version "116.18.4"
+  sha256 arm:   "852e57a20c832e43847ee5336d2c8f7e47cf4a3f39f7715b8f0dbf66f97dff10",
+         intel: "9ea259870ba878b8cbd391db52a6b3ae2c4a1a43ad95260eabe1654247e01d9f"
 
   url "https://desktop.figma.com/#{arch}/beta/FigmaBeta-#{version}.zip"
   name "Figma Beta"
