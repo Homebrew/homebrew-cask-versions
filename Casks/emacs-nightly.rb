203be17f9e8dcd8baf0b589876946a06342d8727
Hannes Braun <hannes@hannesbraun.net>
2024-04-06 12:39:24 +0200

    emacs-nightly 2024-04-06_00-15-21

--- Casks/emacs-nightly.rb
+++ Casks/emacs-nightly.rb
@@ -1,8 +1,8 @@
 cask "emacs-nightly" do
   arch arm: "arm64-11", intel: "x86_64-10_11"
 
-  version "2024-04-05_00-15-00,aad63f935f8737598835612b53bc3b53c124661f"
-  sha256 "b28d609f9977218c0905c109c615f5250d5de9f9c30d5c371a2d6b0ab37b6f9f"
+  version "2024-04-06_00-15-21,617bd85cd0bd1d0a73d405d623ee3dd41c7e8a37"
+  sha256 "77b8230d97552899e07f4cafa90f3bb12cb369467128e81c1f286d88f1de9f44"
 
   url "https://emacsformacosx.com/emacs-builds/Emacs-#{version.csv.first}-#{version.csv.second}-universal.dmg"
   name "Emacs"
