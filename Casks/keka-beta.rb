167afb9165a168c252a78842352044123eaaa041
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-13 11:11:38 +0200

    keka-beta: update url

--- Casks/keka-beta.rb
+++ Casks/keka-beta.rb
@@ -2,7 +2,7 @@ cask "keka-beta" do
   version "1.4.0-dev.r5355"
   sha256 "24de36e624ee53a06a2f643a550b5bdca3a0f1e32dd71e633d2c6677bcd37673"
 
-  url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-v#{version}.7z",
+  url "https://github.com/aonez/Keka/releases/download/v#{version}/Keka-#{version}.dmg",
       verified: "github.com/aonez/Keka/"
   name "Keka"
   desc "File archiver"
