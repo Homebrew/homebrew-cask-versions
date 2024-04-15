e4aaf8e12b34b09d3e6b94826ea0920add767b87
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-15 11:32:39 +0200

    openscad-snapshot 2024.04.14

--- Casks/openscad-snapshot.rb
+++ Casks/openscad-snapshot.rb
@@ -1,6 +1,6 @@
 cask "openscad-snapshot" do
-  version "2024.04.12"
-  sha256 "8a555479c2791d413f1fe9f8a8e3d6e963225e395baddac0c94b1442062f4d00"
+  version "2024.04.14"
+  sha256 "b1de85b28306c0d1b600a23ca4e7a5814c5715a238f2bebf988de431c279d258"
 
   url "https://files.openscad.org/snapshots/OpenSCAD-#{version}.dmg"
   name "OpenSCAD"
