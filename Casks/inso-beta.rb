91c6b9a5681489cab9ac00cf850bd624a8290f87
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-17 23:39:39 +0200

    inso-beta 9.0.0-beta.4

--- Casks/inso-beta.rb
+++ Casks/inso-beta.rb
@@ -1,6 +1,6 @@
 cask "inso-beta" do
-  version "9.0.0-beta.3"
-  sha256 "2498b75eb64bb4c737153d77ffa159ad2b2b861b7f075ee9bac2d7a0ab83bcb2"
+  version "9.0.0-beta.4"
+  sha256 "f0cec313b386c3701e20ccf4fb1ef6700af59131d7d0f7571a8d925e1025bd61"
 
   url "https://github.com/Kong/insomnia/releases/download/lib%40#{version}/inso-macos-#{version}.zip",
       verified: "github.com/Kong/insomnia/"
