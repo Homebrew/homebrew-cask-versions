d72315d4e6ee9c2992b4efe74bb68d46173b5df6
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-18 18:36:17 +0200

    temurin17 17.0.11

--- Casks/temurin17.rb
+++ Casks/temurin17.rb
@@ -1,9 +1,9 @@
 cask "temurin17" do
   arch arm: "aarch64", intel: "x64"
 
-  version "17.0.10,7"
-  sha256 arm:   "a6db8e5519f113ded7c30b02d7fc86610abe2050d8c110ab6a619f1bb647ecb8",
-         intel: "f4cd8b6c187148ebfe7bc256a51e3eff98c271c242a152d5b5247efbbc83ca0f"
+  version "17.0.11,9"
+  sha256 arm:   "c54555dbeb2ecb1c38f04fe079a0eeecb0b48bfb9ba038f764eb76fad399483b",
+         intel: "3553524fa3c86388cfecadd3a11ed1e4b62c64bc36962792544b5a0269b7048c"
 
   url "https://github.com/adoptium/temurin#{version.major}-binaries/releases/download/jdk-#{version.csv.first}%2B#{version.csv.second}/OpenJDK#{version.major}U-jdk_#{arch}_mac_hotspot_#{version.csv.first}_#{version.csv.second.major}.pkg",
       verified: "github.com/adoptium/"
