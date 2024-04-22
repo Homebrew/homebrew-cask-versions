8448158191adf3cf6eac3b715c2c0ee76aa87c98
Klaus Hipp <khipp@users.noreply.github.com>
2024-04-22 15:34:43 +0200

    opera-developer 111.0.5145.0

--- Casks/opera-developer.rb
+++ Casks/opera-developer.rb
@@ -1,6 +1,6 @@
 cask "opera-developer" do
-  version "111.0.5138.0"
-  sha256 "48c3af714121bb871f3dc010fde2bcba6ac2f7200ca847f6eea68a2eeb224d13"
+  version "111.0.5145.0"
+  sha256 "aa4884bde6707e4c52cb845a20e62f05978a2b093227a09bcb193cdccfc1f2da"
 
   url "https://get.geo.opera.com/pub/opera-developer/#{version}/mac/Opera_Developer_#{version}_Setup.dmg"
   name "Opera Developer"
