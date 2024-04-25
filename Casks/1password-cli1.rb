11426739c9d6f26596d36a4d48248a0631fac5bc
Rui Chen <rui@chenrui.dev>
2024-04-25 09:39:59 -0400

    1password-cli1 1.12.9

--- Casks/1password-cli1.rb
+++ Casks/1password-cli1.rb
@@ -1,6 +1,6 @@
 cask "1password-cli1" do
-  version "1.12.8"
-  sha256 "8979e79ce2c1eeb90b4664e02656e4f39d03b324600be85a5ffecfd6605a20b7"
+  version "1.12.9"
+  sha256 "ff9af6a6aef58111f2279b30cebf158a2e14651176b40a649b30a12b772724a3"
 
   url "https://cache.agilebits.com/dist/1P/op/pkg/v#{version}/op_apple_universal_v#{version}.pkg",
       verified: "cache.agilebits.com/dist/1P/op/pkg/"
