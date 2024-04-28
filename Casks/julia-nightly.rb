8884462954de5fedad5dea36d87627cdcdfb1c4c
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-04-28 14:28:43 -0400

    julia-nightly: move to version :latest

--- Casks/julia-nightly.rb
+++ Casks/julia-nightly.rb
@@ -1,17 +1,13 @@
 cask "julia-nightly" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "1.12"
+  version :latest
   sha256 :no_check
 
   url "https://julialangnightlies-s3.julialang.org/bin/macos/#{arch}/julia-latest-macos-#{arch}.dmg"
   name "Julia"
   desc "Programming language for technical computing"
   homepage "https://julialang.org/"
-
-  livecheck do
-    skip "No reliable version info"
-  end
 
   app "Julia-#{version}.app"
   binary "#{appdir}/Julia-#{version}.app/Contents/Resources/julia/bin/julia", target: "julia-nightly"
