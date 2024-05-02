1c1c153bc7c981944adf039d2dff7eadf3466ee9
Razvan Azamfirei <azamfireirazvan@gmail.com>
2024-05-02 12:31:54 -0400

    daedalus-flight: discontinue

--- Casks/daedalus-flight.rb
+++ Casks/daedalus-flight.rb
@@ -8,13 +8,7 @@ cask "daedalus-flight" do
   desc "Pre-release of Daedalus cryptocurrency wallet for ada on the Cardano blockchain"
   homepage "https://daedaluswallet.io/en/flight/"
 
-  livecheck do
-    url "https://update-cardano-mainnet-flight.iohk.io/daedalus-latest-version.json"
-    regex(%r{/daedalus-(\d+(?:\.\d+)*(?:-FC\d*)?)-mainnet_flight-(\d+)\.pkg}i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
-    end
-  end
+  deprecate! date: "2024-05-02", because: :discontinued
 
   auto_updates true
   depends_on macos: ">= :high_sierra"
